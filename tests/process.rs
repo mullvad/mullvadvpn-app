b364aa395ee0ad9a418c3a75e19192cb817c7cf3
Linus Färnstrand <linus@mullvad.net>
2016-12-05 10:03:25 +0100

    Limit RUST_TEST_THREADS to 1

--- tests/process.rs
+++ tests/process.rs
@@ -6,6 +6,16 @@ use talpid_core::process::OpenVpnBuilder;
 
 #[cfg(target_os = "linux")]
 #[test]
+fn check_test_environment() {
+    use std::env;
+    let test_threads = env::var("RUST_TEST_THREADS");
+    if !test_threads.is_ok() || test_threads.unwrap() != "1" {
+        panic!("Tests must be run with environment variable RUST_TEST_THREADS=1");
+    }
+}
+
+#[cfg(target_os = "linux")]
+#[test]
 fn openvpn_builder_starts_correct_process() {
     let mut child = OpenVpnBuilder::new("echo").spawn().unwrap();
     let args = util::read_args_for_proc(child.id());
