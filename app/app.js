a85a8cc4b513e701ed97a5427fc9746f42040a53
Andrej Mihajlov <and@codeispoetry.ru>
2017-02-10 11:49:00 +0000

    Disable smart pinch

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { remote } from 'electron';
+import { remote, webFrame } from 'electron';
 import path from 'path';
 import routes from './routes';
 import configureStore from './store';
@@ -16,6 +16,9 @@ const initialState = {};
 const store = configureStore(initialState);
 const routerHistory = syncHistoryWithStore(hashHistory, store);
 const rootElement = document.querySelector(document.currentScript.getAttribute('data-container'));
+
+// disable smart pinch.
+webFrame.setVisualZoomLevelLimits(1, 1);
 
 ReactDOM.render(
   <div>
