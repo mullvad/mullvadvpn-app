466ec31815baecdd51d1366ea89c2e57bae4f33c
Andrej Mihajlov <and@codeispoetry.ru>
2017-02-14 10:55:22 +0000

    Add HeaderBar.Style

--- app/components/HeaderBar.js
+++ app/components/HeaderBar.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import Enum from '../lib/enum';
 
 export default class HeaderBar extends Component {
+
+  /** Bar style */
+  static Style = Enum('default', 'error');
+
+  static propTypes = {
+    style: PropTypes.string
+  };
+
   render() {
+    const style = this.props.style;
+    let containerClass = ['headerbar'];
+
+    if(HeaderBar.Style.isValid(style)) {
+      containerClass.push(`header--style-${style}`);
+    }
+
     return (
-      <div className="headerbar">
+      <div className={ containerClass.join(' ') }>
         <img className="headerbar__logo" src="./assets/images/logo-icon.svg" />
         <h2 className="headerbar__title">MULLVAD VPN</h2>
       </div>
