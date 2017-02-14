edec4f2dc254d70c970f87608de2cbeba86288e0
Andrej Mihajlov <and@codeispoetry.ru>
2017-02-14 11:14:00 +0000

    Add test for USER_LOGIN_CHANGE reducer

--- test/reducers/user.spec.js
+++ test/reducers/user.spec.js
@@ -1,21 +1,23 @@
 import { expect } from 'chai';
 import reducer from '../../app/reducers/user';
+import { LoginState } from '../../app/constants';
 
 describe('reducers', () => {
 
   describe('user', () => {    
     
-    // it('should handle USER_LOGIN', () => {
-    //   const action = { 
-    //     type: 'USER_LOGIN',
-    //     payload: {
-    //       username: 'John Doe', 
-    //       loggedIn: true 
-    //     }
-    //   };
-    //   const test = Object.assign({}, action.payload);
-    //   expect(reducer({}, action)).to.deep.equal(test);
-    // });
+    it('should handle USER_LOGIN_CHANGE', () => {
+      const action = { 
+        type: 'USER_LOGIN_CHANGE',
+        payload: {
+          account: '1111',
+          status: LoginState.failed,
+          error: new Error('Something went wrong')
+        }
+      };
+      const test = Object.assign({}, action.payload);
+      expect(reducer({}, action)).to.deep.equal(test);
+    });
     
   });
 
