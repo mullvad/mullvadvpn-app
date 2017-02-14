9e45a7de25f01975bd5a5baf63f35b35421ab619
Erik Larkö <erik@mullvad.net>
2017-02-14 19:30:32 +0800

    Address review problems

--- build-deps/install-build-deps-osx.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-set -e
-
-## Everything printed to stdout will be run by travis. So only print
-## stuff that needs to be set, e.g. environment variables.
-
-brew update > /dev/stderr
-brew install czmq > /dev/stderr
---
+++ build-deps/zmq/install-build-deps-osx.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+set -eux
+
+brew update
+brew install libtool autoconf automake pkg-config
