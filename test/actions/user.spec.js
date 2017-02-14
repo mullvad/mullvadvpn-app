af6e1ecd9051ecb590cb189de37978d366da6cdd
Andrej Mihajlov <and@codeispoetry.ru>
2017-02-14 15:57:35 +0000

    Add tests for login action

--- test/actions/user.spec.js
+++ test/actions/user.spec.js
@@ -1,6 +1,9 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import spy from 'chai-spies';
 import actions from '../../app/actions/user';
 import { LoginState } from '../../app/constants';
+
+chai.use(spy);
 
 describe('actions', () => {
 
@@ -17,6 +20,70 @@ describe('actions', () => {
       };
       const payload = Object.assign({}, test.payload);
       expect(actions.loginChange(payload)).to.deep.equal(test);
+    });
+
+    it('should successfully login', (done) => {
+      const actionType = actions.loginChange.toString();
+      const account = '1234';
+      const getState = () => ({});
+      let callCounter = 0;
+      const dispatch = chai.spy((action) => {
+        callCounter += 1;
+
+        if(callCounter == 2) {
+          expect(dispatch).to.have.been.called.with({ 
+            type: actionType, 
+            payload: { account, status: LoginState.connecting } 
+          });
+
+          expect(dispatch).to.have.been.called.with({ 
+            type: actionType, 
+            payload: { status: LoginState.ok } 
+          });
+
+          done();
+        }
+      });
+
+      const backend = {
+        login: () => Promise.resolve()
+      };
+
+      const action = actions.login(backend, account);
+
+      action(dispatch, getState);
+    });
+
+    it('should fail login', (done) => {
+      const actionType = actions.loginChange.toString();
+      const account = '1234';
+      const getState = () => ({});
+      let callCounter = 0;
+      const dispatch = chai.spy((action) => {
+        callCounter += 1;
+
+        if(callCounter == 2) {
+          expect(dispatch).to.have.been.called.with({ 
+            type: actionType, 
+            payload: { account, status: LoginState.connecting } 
+          });
+
+          expect(dispatch).to.have.been.called.with({ 
+            type: actionType, 
+            payload: { status: LoginState.failed, error: new Error('Failed') } 
+          });
+
+          done();
+        }
+      });
+
+      const backend = {
+        login: () => Promise.reject(new Error('Failed'))
+      };
+
+      const action = actions.login(backend, account);
+
+      action(dispatch, getState);
     });
 
   });
