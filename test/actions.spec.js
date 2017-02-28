f96fa839bea76023ec683538a49944f26c10d9e6
Andrej Mihajlov <and@codeispoetry.ru>
2017-02-28 13:06:07 +0000

    Trash old useless actions tests

--- test/actions.spec.js
+++ test/actions.spec.js
@@ -1,47 +1,25 @@
 import { expect } from 'chai';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import Backend from '../app/lib/backend';
 import userActions from '../app/actions/user';
-import connectActions from '../app/actions/connect';
-import settingsActions from '../app/actions/settings';
 import { LoginState, ConnectionState, defaultServer } from '../app/constants';
+
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
 
 describe('actions', () => {
 
   it('should create action for USER_LOGIN_CHANGE', () => {
-    const test = { 
-      type: userActions.loginChange.toString(), 
-      payload: { 
-        account: '1111',
-        status: LoginState.failed,
-        error: new Error('Something went wrong')
-      }
-    };
-    const payload = Object.assign({}, test.payload);
-    expect(userActions.loginChange(payload)).to.deep.equal(test);
-  });
+    const expectedActions = [
+      { type: 'USER_LOGIN_CHANGE', payload: {} }
+    ];
 
-  it('should create action for CONNECTION_CHANGE', () => {
-    const test = { 
-      type: connectActions.connectionChange.toString(), 
-      payload: { 
-        status: ConnectionState.connected,
-        serverAddress: '2.1.1.2',
-        clientIp: '2.1.1.1'
-      }
-    };
-    const payload = Object.assign({}, test.payload);
-    expect(connectActions.connectionChange(payload)).to.deep.equal(test);
-  });
-
-  it('should create action for SETTINGS_UPDATE', () => {
-    const test = { 
-      type: settingsActions.updateSettings.toString(), 
-      payload: { 
-        autoSecure: true,
-        preferredServer: defaultServer
-      }
-    };
-    const payload = Object.assign({}, test.payload);
-    expect(settingsActions.updateSettings(payload)).to.deep.equal(test);
+    const backend = new Backend();
+    const store = mockStore({});
+    
+    // @TODO: Figure out how to test actions in event based system
+    // store.dispatch(userActions.login(backend, '111123456789'));
   });
 
 });
