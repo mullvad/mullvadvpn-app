6e3ec931bcf1f0f3ca220c3a1baabf5b6197db80
Erik Larkö <erik@mullvad.net>
2017-03-01 12:37:52 +0800

    Try a bunch of ports when starting the IpcServer

--- talpid_ipc/src/zmq_ipc.rs
+++ talpid_ipc/src/zmq_ipc.rs
@@ -1,18 +1,17 @@
 extern crate zmq;
+extern crate rand;
 
+use self::rand::distributions::{IndependentSample, Range};
 use super::{OnMessage, ErrorKind, Result, ResultExt};
 use std::thread;
 
 type IpcServerId = String;
 
 /// The server end of our Inter-Process Communcation implementation.
-pub struct Server {
-    port: u16,
-}
-
+pub struct Server;
 impl Server {
-    pub fn new(port: u16) -> Self {
-        Server { port: port }
+    pub fn new() -> Self {
+        Server {}
     }
 
     /// Starts listening to incoming IPC connections on the specified port.
@@ -24,10 +23,24 @@ impl Server {
     /// This function is non-blocking and thus spawns a thread where it
     /// listens to messages.
     pub fn start(self, on_message: Box<OnMessage<Vec<u8>>>) -> Result<IpcServerId> {
-        let socket =
-            Self::start_zmq_server(self.port).chain_err(|| ErrorKind::CouldNotStartServer)?;
-        let _ = Self::start_receive_loop(socket, on_message);
-        Ok(format!("localhost:{}", self.port))
+
+        let port_range = Range::new(1024, 65535); // Docs doesn't say if this is inclusive or exclusive...
+        let mut rng = rand::thread_rng();
+        for _ in 0..10 {
+
+            let port = port_range.ind_sample(&mut rng);
+            if let Ok(socket) = Self::attempt_to_start_on_port(port) {
+                let _ = Self::start_receive_loop(socket, on_message);
+                return Ok(format!("tcp://localhost:{}", port));
+            }
+        }
+
+        return Err(ErrorKind::CouldNotStartServer.into());
+    }
+
+    fn attempt_to_start_on_port(port: u16) -> Result<zmq::Socket> {
+        let socket = Self::start_zmq_server(port).chain_err(|| ErrorKind::CouldNotStartServer)?;
+        Ok(socket)
     }
 
     fn start_zmq_server(port: u16) -> zmq::Result<zmq::Socket> {
--- talpid_ipc/tests/zmq_integration_tests.rs
+++ talpid_ipc/tests/zmq_integration_tests.rs
@@ -1,9 +1,11 @@
 extern crate talpid_ipc;
 extern crate zmq;
+extern crate regex;
 
 #[macro_use]
 extern crate assert_matches;
 
+use regex::Regex;
 use std::result;
 use std::sync::mpsc::{self, Receiver};
 use std::time::Duration;
@@ -14,37 +16,19 @@ const AN_INVALID_MESSAGE: u8 = 2;
 
 #[test]
 fn returns_connection_string_when_started() {
-    let port = 1341;
-    let connection_string = talpid_ipc::Server::new(port)
+    let connection_string = talpid_ipc::Server::new()
         .start(Box::new(|_| {}))
         .expect("Unable to start server");
 
-    assert!(connection_string.contains("localhost"),
-            format!("'{}' did not contain 'localhost'", connection_string));
-    assert!(connection_string.contains(&port.to_string()),
-            format!("'{}' did not contain the port", connection_string));
-}
-
-#[test]
-fn gives_error_when_unable_to_start() {
-    let port = 1340;
-
-    let ipc_server1 = talpid_ipc::Server::new(port);
-    let ipc_server2 = talpid_ipc::Server::new(port);
-
-    ipc_server1.start(Box::new(|_| {}))
-        .expect("Unable to start the first server. Results inconclusive");
-    let start_res = ipc_server2.start(Box::new(|_| {}));
-
-    assert!(start_res.is_err());
-    let err = start_res.unwrap_err();
-    assert_matches!(err.kind(), &ErrorKind::CouldNotStartServer);
-    assert!(err.iter().count() > 1)
+    let re = Regex::new(r"^tcp://localhost:\d+$").unwrap();
+    assert!(re.is_match(&connection_string),
+            format!("'{}' does not match 'tcp://localhost:port'",
+                    connection_string));
 }
 
 #[test]
 fn publishes_incoming_messages_to_channel() {
-    let new_messages_rx = connect_and_send(1337, A_VALID_MESSAGE);
+    let new_messages_rx = connect_and_send(A_VALID_MESSAGE);
 
     let message = new_messages_rx.recv_timeout(Duration::from_millis(1000))
         .expect("Did not receive a message");
@@ -53,7 +37,7 @@ fn publishes_incoming_messages_to_channel() {
 
 #[test]
 fn does_not_publish_unknown_messages() {
-    let rx = connect_and_send(1338, AN_INVALID_MESSAGE);
+    let rx = connect_and_send(AN_INVALID_MESSAGE);
 
     let message = rx.recv_timeout(Duration::from_millis(1000))
         .expect("Did not receive message");
@@ -61,26 +45,25 @@ fn does_not_publish_unknown_messages() {
     assert_matches!(message.unwrap_err().kind(), &ErrorKind::InvalidMessage(_));
 }
 
-fn connect_and_send(port: u16, message: u8) -> Receiver<Result<TestMessage>> {
+fn connect_and_send(message: u8) -> Receiver<Result<TestMessage>> {
     let (tx, rx) = mpsc::channel();
 
-    let ipc_server = talpid_ipc::Server::new(port);
-    ipc_server.start(Box::new(move |message| {
-        let _ = tx.send(message.and_then(parse_to_test_enum));
-    })).expect("Could not start the server");
+    let ipc_server = talpid_ipc::Server::new();
+    let connection_string = ipc_server.start(Box::new(move |message| {
+            let _ = tx.send(message.and_then(parse_to_test_enum));
+        })).expect("Could not start the server");
 
-    let socket = connect_to_server(port).expect("Could not connect to the server");
+    let socket = connect_to_server(&connection_string).expect("Could not connect to the server");
     socket.send(&[message], 0).expect("Could not send message");
 
     rx
 }
 
-fn connect_to_server(port: u16) -> result::Result<zmq::Socket, zmq::Error> {
+fn connect_to_server(connection_string: &str) -> result::Result<zmq::Socket, zmq::Error> {
     let ctx = zmq::Context::new();
 
     let socket = ctx.socket(zmq::PUSH)?;
-    let connection_string: String = format!("tcp://127.0.0.1:{}", port);
-    try!(socket.connect(connection_string.as_str()));
+    try!(socket.connect(connection_string));
     Ok(socket)
 }
 
