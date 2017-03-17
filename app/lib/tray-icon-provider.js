e1fc38b58291df6592ed4615791eb2c6511c1092
Andrej Mihajlov <and@codeispoetry.ru>
2017-03-17 17:43:38 +0000

    Drop spinner and update assets

--- app/lib/tray-icon-provider.js
+++ app/lib/tray-icon-provider.js
@@ -1,19 +1,11 @@
 import path from 'path';
 import { EventEmitter } from 'events';
-import { systemPreferences } from 'electron';
 import TrayAnimation from './tray-animation';
 import Enum from './enum';
 
 const menubarIcons = {
   base: path.join(path.resolve(__dirname, '..'), 'assets/images/menubar icons'),
-  spinner: {
-    light: 'light ui/spinner/spinner-{s}-light.png',
-    dark: 'dark ui/spinner/spinner-{s}-dark.png'
-  },
-  lock: {
-    light: 'light ui/lock/lock-{s}-light.png',
-    dark: 'dark ui/lock/lock-{s}-dark.png'
-  }
+  lock: 'lock-{s}.png'
 };
 
 /**
@@ -22,39 +14,7 @@ const menubarIcons = {
  * @export
  * @class TrayIconProvider
  */
-export default class TrayIconProvider extends EventEmitter {
-  
-  /**
-   * EventType
-   * @type {TrayIconProvider.EventType}
-   * @property {string} themeChanged - event fired when menubar theme is changed
-   */
-  static EventType = new Enum('themeChanged');
-
-  /**
-   * Creates an instance of TrayIconProvider.
-   * 
-   * @memberOf TrayIconProvider
-   */
-  constructor() {
-    super();
-
-    this._themeChangeObserver = systemPreferences.subscribeNotification('AppleInterfaceThemeChangedNotification', () => {
-      this.emit(TrayIconProvider.EventType.themeChanged);
-    });
-  }
-
-  /**
-   * Destroys TrayIconProvider
-   * 
-   * @memberOf TrayIconProvider
-   */
-  destroy() {
-    if(this._themeChangeObserver) {
-      systemPreferences.unsubscribeNotification(this._themeChangeObserver);
-      this._themeChangeObserver = null;
-    }
-  }
+export default class TrayIconProvider {
 
   /**
    * Get lock animation
@@ -65,7 +25,8 @@ export default class TrayIconProvider extends EventEmitter {
    * @memberOf TrayIconProvider
    */
   lockAnimation(isReverse = false) {
-    let animation = TrayAnimation.fromFileSequence(this._lockPath, [1, 9]);
+    let filePath = path.join(menubarIcons.base, menubarIcons.lock);
+    let animation = TrayAnimation.fromFileSequence(filePath, [1, 9]);
     animation.speed = 100;
     animation.reverse = isReverse;
 
@@ -81,54 +42,6 @@ export default class TrayIconProvider extends EventEmitter {
    */
   unlockAnimation() {
     return this.lockAnimation(true);
-  }
-
-  /**
-   * Get spinner animation
-   * 
-   * @returns TrayIconAnimator
-   * 
-   * @memberOf TrayIconProvider
-   */
-  spinnerAnimation() {
-    let animation = TrayAnimation.fromFileSequence(this._spinnerPath, [1, 9]);
-    animation.speed = 100;
-    animation.repeat = true;
-
-    return animation;
-  }
-
-  /**
-   * Pattern to spinner animation assets based on macOS menubar theme
-   * 
-   * @readonly
-   * 
-   * @memberOf TrayIconProvider
-   */
-  get _spinnerPath() {
-    return path.join(menubarIcons.base, menubarIcons.spinner[this._colorTheme]);
-  }
-  
-  /**
-   * Pattern to lock/unlock animation assets based on macOS menubar theme
-   * 
-   * @readonly
-   * 
-   * @memberOf TrayIconProvider
-   */
-  get _lockPath() {
-    return path.join(menubarIcons.base, menubarIcons.lock[this._colorTheme]);
-  }
-
-  /**
-   * Current theme name based on macOS menubar theme.
-   * 
-   * @readonly
-   * 
-   * @memberOf TrayIconProvider
-   */
-  get _colorTheme() {
-    return systemPreferences.isDarkMode() ? 'dark' : 'light';
   }
 
 }
