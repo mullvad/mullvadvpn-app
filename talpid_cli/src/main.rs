a13391b0d8b8484b33039a8c19097a3f4c55a065
Linus Färnstrand <linus@mullvad.net>
2017-04-05 15:07:53 +0200

    Use quick_main!() from error_chain

--- talpid_cli/src/main.rs
+++ talpid_cli/src/main.rs
@@ -32,18 +32,7 @@ error_chain! {
 }
 
 
-fn main() {
-    if let Err(ref e) = run() {
-        println!("error: {}", e);
-        for e in e.iter().skip(1) {
-            println!("caused by: {}", e);
-        }
-        if let Some(backtrace) = e.backtrace() {
-            println!("backtrace: {:?}", backtrace);
-        }
-        ::std::process::exit(1);
-    }
-}
+quick_main!(run);
 
 fn run() -> Result<()> {
     init_logger()?;
