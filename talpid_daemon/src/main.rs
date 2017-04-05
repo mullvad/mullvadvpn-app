b527821174cde1de5123ae3d0e6827639522a44c
Linus Färnstrand <linus@mullvad.net>
2017-04-05 15:26:50 +0200

    Use quick_main in daemon

--- talpid_daemon/src/main.rs
+++ talpid_daemon/src/main.rs
@@ -10,42 +10,27 @@ extern crate talpid_ipc;
 
 mod frontend_ipc_router;
 
-error_chain! {
-    errors {
-        InitLoggingFailed {
-            description("Failed to bootstrap logging system")
-        }
-    }
-}
+error_chain!{}
 
-fn main() {
+quick_main!(run);
 
-    if let Err(e) = init_logger() {
-        log_error_chain_error(&e);
-        ::std::process::exit(1);
-    }
-
-    let ipc_res = talpid_ipc::http_ipc::start(frontend_ipc_router::build_router);
-    if let Err(e) = ipc_res {
-        error!("Failed to start IPC server");
-        log_error_chain_error(&e);
-        ::std::process::exit(2);
-    }
-
-    let (tx, rx) = ::std::sync::mpsc::channel::<u8>();
-    rx.recv();
+fn run() -> Result<()> {
+    init_logger()?;
+    let _server = start_ipc()?;
+    main_loop()
 }
 
 fn init_logger() -> Result<()> {
-    env_logger::init().chain_err(|| ErrorKind::InitLoggingFailed)
+    env_logger::init().chain_err(|| "Failed to bootstrap logging system")
 }
 
-fn log_error_chain_error<E: ::error_chain::ChainedError>(e: &E) {
-    println!("error: {}", e);
-    for e in e.iter().skip(1) {
-        println!("caused by: {}", e);
-    }
-    if let Some(backtrace) = e.backtrace() {
-        println!("backtrace: {:?}", backtrace);
-    }
+fn start_ipc() -> Result<talpid_ipc::http_ipc::ServerHandle> {
+    talpid_ipc::http_ipc::start(frontend_ipc_router::build_router)
+        .chain_err(|| "Failed to start IPC server")
+}
+
+fn main_loop() -> Result<()> {
+    let (_tx, rx) = ::std::sync::mpsc::channel::<u8>();
+    let _ = rx.recv();
+    Ok(())
 }
