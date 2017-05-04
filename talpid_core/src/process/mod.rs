3ee8af5fbe38815f53b8354c6f2ea6fd54e37ccf
Linus Färnstrand <linus@mullvad.net>
2017-05-04 17:29:44 +0200

    Simplify ChildMonitor to use duct and no generics

--- talpid_core/src/process/mod.rs
+++ talpid_core/src/process/mod.rs
@@ -1,31 +1,5 @@
-use std::io;
-
-use std::process::{ChildStderr, ChildStdout};
-
 /// A module for monitoring child processes and get notified of events on them.
 pub mod monitor;
-use self::monitor::MonitoredChild;
 
 /// A module for all OpenVPN related process management.
 pub mod openvpn;
-
-use clonablechild::ClonableChild;
-
-
-impl MonitoredChild for ClonableChild {
-    fn wait(&self) -> io::Result<bool> {
-        ClonableChild::wait(self).map(|exit_status| exit_status.success())
-    }
-
-    fn kill(&self) -> io::Result<()> {
-        ClonableChild::kill(self)
-    }
-
-    fn stdout(&mut self) -> Option<ChildStdout> {
-        self.stdout()
-    }
-
-    fn stderr(&mut self) -> Option<ChildStderr> {
-        self.stderr()
-    }
-}
