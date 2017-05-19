611ffe8ebbc07c127788414ba31add574984d965
Linus Färnstrand <linus@mullvad.net>
2017-05-19 11:22:03 +0200

    Add new OpenVpnMonitor under the tunnel module

--- talpid_core/src/tunnel/openvpn.rs
+++ talpid_core/src/tunnel/openvpn.rs
@@ -1,7 +1,120 @@
 use jsonrpc_core::{Error, IoHandler};
 use openvpn_ffi;
+use process::monitor::ChildMonitor;
+use process::openvpn::OpenVpnCommand;
+use std::io;
+
+use std::path::{Path, PathBuf};
+use std::process;
+use std::result::Result as StdResult;
+use std::sync::{Arc, Mutex};
 
 use talpid_ipc;
+
+mod errors {
+    error_chain!{
+        errors {
+            /// The `OpenVpnMonitor` is in an invalid state for the requested operation.
+            InvalidState {
+                description("Invalid state. OpenVPN is already running")
+            }
+            /// Unable to start or kill the OpenVPN process.
+            ChildProcessError {
+                description("Unable to start or kill the OpenVPN process")
+            }
+            /// Unable to start or manage the IPC server listening for events from OpenVPN
+            IpcServerError {
+                description("Unable to start or manage the IPC server")
+            }
+        }
+    }
+}
+pub use self::errors::*;
+
+
+/// Possible events from OpenVPN
+pub enum OpenVpnEvent {
+    /// An event from the plugin loaded into OpenVPN.
+    PluginEvent(openvpn_ffi::OpenVpnPluginEvent, openvpn_ffi::OpenVpnEnv),
+    /// The OpenVPN process exited. Containing the result of waiting for the process.
+    Shutdown(io::Result<process::ExitStatus>),
+}
+
+/// Struct for monitoring OpenVPN processes.
+pub struct OpenVpnMonitor {
+    on_event: Arc<Fn(OpenVpnEvent) + Send + Sync + 'static>,
+    plugin_path: PathBuf,
+    child: Arc<Mutex<Option<ChildMonitor>>>,
+    event_dispatcher: OpenVpnEventDispatcher,
+}
+
+impl OpenVpnMonitor {
+    /// Creates a new `OpenVpnMonitor` with the given listener and using the plugin at the given
+    /// path.
+    pub fn new<L, P>(on_event: L, plugin_path: P) -> Result<Self>
+        where L: Fn(OpenVpnEvent) + Send + Sync + 'static,
+              P: AsRef<Path>
+    {
+        let on_event = Arc::new(on_event);
+        let event_dispatcher = Self::start_event_dispatcher(on_event.clone())?;
+        Ok(
+            OpenVpnMonitor {
+                on_event,
+                plugin_path: plugin_path.as_ref().to_owned(),
+                child: Arc::new(Mutex::new(None)),
+                event_dispatcher,
+            },
+        )
+    }
+
+    fn start_event_dispatcher(on_event: Arc<Fn(OpenVpnEvent) + Send + Sync + 'static>)
+                              -> Result<OpenVpnEventDispatcher> {
+        let on_plugin_event = move |event, env| (*on_event)(OpenVpnEvent::PluginEvent(event, env));
+        OpenVpnEventDispatcher::start(on_plugin_event).chain_err(|| ErrorKind::IpcServerError)
+    }
+
+    /// Tries to start a new OpenVPN process if one is not already running.
+    /// If this `OpenVpnMonitor is already monitoring a running process it will return an
+    /// `InvalidState` error.
+    pub fn start(&self, cmd: OpenVpnCommand) -> Result<()> {
+        let mut child_lock = self.child.lock().unwrap();
+        if child_lock.is_some() {
+            bail!(ErrorKind::InvalidState);
+        }
+        *child_lock = Some(self.start_child_monitor(cmd)?);
+        Ok(())
+    }
+
+    fn start_child_monitor(&self, mut cmd: OpenVpnCommand) -> Result<ChildMonitor> {
+        self.set_plugin(&mut cmd);
+
+        let child = self.child.clone();
+        let on_event = self.on_event.clone();
+
+        let on_exit = move |exit_status: io::Result<&process::Output>| {
+            *child.lock().unwrap() = None;
+            (*on_event)(OpenVpnEvent::Shutdown(exit_status.map(|output| output.status)),)
+        };
+        ChildMonitor::start(&cmd.build(), on_exit).chain_err(|| ErrorKind::ChildProcessError)
+    }
+
+    fn set_plugin(&self, cmd: &mut OpenVpnCommand) {
+        let arg0 = self.event_dispatcher.address().to_string();
+        cmd.plugin(&self.plugin_path, vec![arg0]);
+    }
+
+    /// Tries to kill the OpenVPN process if it is running. If it is already dead, this does
+    /// nothing.
+    pub fn kill(&self) -> Result<()> {
+        if let Some(ref child) = *self.child.lock().unwrap() {
+            child.kill().chain_err(|| ErrorKind::ChildProcessError)?;
+        }
+        Ok(())
+    }
+}
+
+
+
 
 /// IPC server for listening to events coming from plugin loaded into OpenVPN.
 pub struct OpenVpnEventDispatcher {
@@ -41,7 +154,7 @@ mod api {
             fn openvpn_event(&self,
                              openvpn_ffi::OpenVpnPluginEvent,
                              openvpn_ffi::OpenVpnEnv)
-                             -> Result<(), Error>;
+                             -> StdResult<(), Error>;
         }
     }
 }
@@ -59,7 +172,7 @@ impl<L> OpenVpnEventApi for OpenVpnEventApiImpl<L>
     fn openvpn_event(&self,
                      event: openvpn_ffi::OpenVpnPluginEvent,
                      env: openvpn_ffi::OpenVpnEnv)
-                     -> Result<(), Error> {
+                     -> StdResult<(), Error> {
         debug!("OpenVPN event {:?}", event);
         (self.on_event)(event, env);
         Ok(())
