8f99e0c22c4f873c6690ceb03a6282e4129758d3
Andrej Mihajlov <and@codeispoetry.ru>
2017-06-26 18:54:20 +0300

    Annotate WindowChrome component

--- app/components/WindowChrome.js
+++ app/components/WindowChrome.js
@@ -1,20 +1,10 @@
+// @flow
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-/**
- * A component used to chip out arrow in the app header using CSS mask
- *
- * @export
- * @class WindowChrome
- * @extends {Component}
- */
 export default class WindowChrome extends Component {
-  static propTypes = {
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.element),
-      PropTypes.element,
-    ])
-  };
+  props: {
+    children: Array<React.Element<*>> | React.Element<*>
+  }
 
   render() {
     const chromeClass = ['window-chrome', 'window-chrome--' + process.platform];
