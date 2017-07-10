22d6e1347ee8803bf07388d461a90dbeb8fe5c7c
Linus Färnstrand <linus@mullvad.net>
2017-07-10 17:00:19 +0200

    Automatically cast commands with type annotation

--- mullvad_cli/src/cmds/mod.rs
+++ mullvad_cli/src/cmds/mod.rs
@@ -15,11 +15,11 @@ pub use self::disconnect::Disconnect;
 
 /// Returns a map of all available subcommands with their name as key.
 pub fn get_commands() -> HashMap<&'static str, Box<Command>> {
-    let commands = vec![
-        Box::new(Account) as Box<Command>,
-        Box::new(Status) as Box<Command>,
-        Box::new(Connect) as Box<Command>,
-        Box::new(Disconnect) as Box<Command>,
+    let commands: Vec<Box<Command>> = vec![
+        Box::new(Account),
+        Box::new(Status),
+        Box::new(Connect),
+        Box::new(Disconnect),
     ];
     let mut map = HashMap::new();
     for cmd in commands {
