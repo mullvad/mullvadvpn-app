218e70ea95cf2b4d3caecd07faa72313b1bdaae9
Andrej Mihajlov <and@codeispoetry.ru>
2017-07-17 23:16:37 +0100

    Add Switch tests

--- test/global.js
+++ test/global.js
@@ -2,10 +2,14 @@ import log from 'electron-log';
 import jsdom from 'jsdom';
 
 before(() => {
+  log.transports.console.level = false;
+  log.transports.file.level = false;
+});
+
+beforeEach(() => {
   global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
   global.window = document.defaultView;
   global.navigator = window.navigator;
-
-  log.transports.console.level = false;
-  log.transports.file.level = false;
+  global.HTMLInputElement = window.HTMLInputElement;
+  global.MouseEvent = window.MouseEvent;
 });
