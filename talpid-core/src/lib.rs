282d1e5d14c4bedb9b16941c764bde81de5a68df
Linus Färnstrand <linus@mullvad.net>
2017-09-08 11:30:55 +0200

    Add license metadata

--- talpid-core/src/lib.rs
+++ talpid-core/src/lib.rs
@@ -1,6 +1,15 @@
 #![deny(missing_docs)]
 
 //! The core components of the talpidaemon VPN client.
+//!
+//! # License
+//!
+//! Copyright (C) 2017  Amagicom AB
+//!
+//! This program is free software: you can redistribute it and/or modify it under the terms of the
+//! GNU General Public License as published by the Free Software Foundation, either version 3 of
+//! the License, or (at your option) any later version.
+
 
 #[cfg(test)]
 #[macro_use]
