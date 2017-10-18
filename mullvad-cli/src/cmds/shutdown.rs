f9c7cb281fde9974e71233a386de7375e31c0c05
Linus Färnstrand <linus@mullvad.net>
2017-10-18 13:49:36 +0200

    Add shutdown command to cli

---
+++ mullvad-cli/src/cmds/shutdown.rs
@@ -0,0 +1,21 @@
+use {Command, Result};
+use clap;
+
+use mullvad_types::account::{AccountData, AccountToken};
+use rpc;
+
+pub struct Shutdown;
+
+impl Command for Shutdown {
+    fn name(&self) -> &'static str {
+        "shutdown"
+    }
+
+    fn clap_subcommand(&self) -> clap::App<'static, 'static> {
+        clap::SubCommand::with_name(self.name()).about("Makes the backend daemon quit")
+    }
+
+    fn run(&self, _matches: &clap::ArgMatches) -> Result<()> {
+        rpc::call("shutdown", &[] as &[u8; 0])
+    }
+}
