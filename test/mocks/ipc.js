8ab14bad27343bd1ab74e6de29ff8de157cad3e3
Erik Larkö <erik@mullvad.net>
2017-10-20 07:58:46 +0200

    Refactored the mock IPC to use Promise.resolve

--- test/mocks/ipc.js
+++ test/mocks/ipc.js
@@ -18,56 +18,53 @@ export function newMockIpc() {
   const mockIpc: IpcFacade & MockIpc = {
 
     setConnectionString: (_str: string) => {},
-    getAccountData: (accountToken) => {
-      return new Promise(r => r({
-        accountToken: accountToken,
-        expiry: '',
-      }));
-    },
-    getAccount: () => {
-      return new Promise(r => r('1111'));
-    },
-    setAccount: () => {
-      return new Promise(r => r());
-    },
-    setCustomRelay: () => {
-      return new Promise(r => r());
-    },
-    connect: () => {
-      return new Promise(r => r());
-    },
-    disconnect: () => {
-      return new Promise(r => r());
-    },
-    shutdown: Promise.resolve,
-    getIp: () => {
-      return new Promise(r => r('1.2.3.4'));
-    },
-    getLocation: () => {
-      return new Promise(r => r({
-        city: '',
-        country: '',
-        latlong: [0, 0],
-      }));
-    },
-    getState: () => {
-      return new Promise(r => r({
-        state: 'unsecured',
-        target_state:'unsecured',
-      }));
-    },
+
+    getAccountData: (accountToken) => Promise.resolve({
+      accountToken: accountToken,
+      expiry: '',
+    }),
+
+    getAccount: () => Promise.resolve('1111'),
+
+    setAccount: () => Promise.resolve(),
+
+    setCustomRelay: () => Promise.resolve(),
+
+    connect: () => Promise.resolve(),
+
+    disconnect: () => Promise.resolve(),
+
+    shutdown: () => Promise.resolve(),
+
+    getIp: () => Promise.resolve('1.2.3.4'),
+
+    getLocation: () => Promise.resolve({
+      city: '',
+      country: '',
+      latlong: [0, 0],
+    }),
+
+    getState: () => Promise.resolve({
+      state: 'unsecured',
+      target_state:'unsecured',
+    }),
+
     registerStateListener: (listener: (BackendState) => void) => {
       stateListeners.push(listener);
     },
+
     sendNewState: (state: BackendState) => {
       for(const l of stateListeners) {
         l(state);
       }
     },
+
     authenticate: (_secret: string) => Promise.resolve(),
+
     setCloseConnectionHandler: (listener: () => void) => {
       connectionCloseListeners.push(listener);
     },
+
     killWebSocket: () => {
       for(const l of connectionCloseListeners) {
         l();
