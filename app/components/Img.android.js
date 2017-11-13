cc2395f0accd36ede1b9c44848269630e51b1303
anderklander <anderklander@gmail.com>
2017-11-13 18:00:29 +0100

    Img
    Indentation, flow, whitespace

--- app/components/Img.android.js
+++ app/components/Img.android.js
@@ -1,8 +1,9 @@
+// @flow
 import React from 'react';
 import { Image, Component } from 'reactxp';
 
 export default class Img extends Component {
-	render(): React.Element<*> {
+  render(): React.Element<*> {
     return (<Image style={ this.props.style } source={ this.props.source }/>);
   }
 }
--- app/components/Img.js
+++ app/components/Img.js
@@ -1,12 +1,13 @@
+// @flow
 import React from 'react';
 import { View, Component } from 'reactxp';
 
 export default class Img extends Component {
-	render(): React.Element<*> {
+  render(): React.Element<*> {
 
-	const url = "./assets/images/" + this.props.source + ".svg";
+    const url = "./assets/images/" + this.props.source + ".svg";
 
-	const style = this.props.style;
+    const style = this.props.style;
 
     return (<View style={ style }> <img src={ url } /> </View>);
   }
