73514e3194dfbafc246876d19b6d0bd8c802e9a6
Linus Färnstrand <linus@mullvad.net>
2017-11-16 12:18:48 +0100

    Add location and IP to CLI status command

--- mullvad-cli/src/cmds/status.rs
+++ mullvad-cli/src/cmds/status.rs
@@ -2,8 +2,11 @@ use Command;
 use Result;
 use clap;
 
+use mullvad_types::location::Location;
 use mullvad_types::states::{DaemonState, SecurityState, TargetState};
 use rpc;
+
+use std::net::IpAddr;
 
 pub struct Status;
 
@@ -18,12 +21,24 @@ impl Command for Status {
 
     fn run(&self, _matches: &clap::ArgMatches) -> Result<()> {
         let state: DaemonState = rpc::call("get_state", &[] as &[u8; 0])?;
+        print!("Tunnel status: ");
         match (state.state, state.target_state) {
             (SecurityState::Unsecured, TargetState::Unsecured) => println!("Disconnected"),
             (SecurityState::Unsecured, TargetState::Secured) => println!("Connecting..."),
             (SecurityState::Secured, TargetState::Unsecured) => println!("Disconnecting..."),
             (SecurityState::Secured, TargetState::Secured) => println!("Connected"),
         }
+
+        let location: Location = rpc::call("get_current_location", &[] as &[u8; 0])?;
+        println!("Location: {}, {}", location.city, location.country);
+        println!(
+            "Position: {:.5}°N, {:.5}°W",
+            location.position[0],
+            location.position[1]
+        );
+
+        let ip: IpAddr = rpc::call("get_public_ip", &[] as &[u8; 0])?;
+        println!("IP: {}", ip);
         Ok(())
     }
 }
