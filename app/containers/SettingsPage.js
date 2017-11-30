42474fc280f890eb248bf4ebc1726dee73249b33
Andrej Mihajlov <and@mullvad.net>
2017-11-30 13:56:34 +0100

    Enable flow for SettingsPage

--- app/containers/SettingsPage.js
+++ app/containers/SettingsPage.js
@@ -1,20 +1,24 @@
+// @flow
+
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { push } from 'react-router-redux';
 import Settings from '../components/Settings';
 import { remote, shell } from 'electron';
 import { links } from '../config';
 
-const mapStateToProps = (state) => {
-  return state;
-};
+import type { ReduxState, ReduxDispatch } from '../redux/store';
+import type { SharedRouteProps } from '../routes';
 
-const mapDispatchToProps = (dispatch, _props) => {
+const mapStateToProps = (state: ReduxState) => state;
+const mapDispatchToProps = (dispatch: ReduxDispatch, _props: SharedRouteProps) => {
+  const { push: pushHistory } = bindActionCreators({ push }, dispatch);
   return {
     onQuit: () => remote.app.quit(),
-    onClose: () => dispatch(push('/connect')),
-    onViewAccount: () => dispatch(push('/settings/account')),
-    onViewSupport: () => dispatch(push('/settings/support')),
-    onViewAdvancedSettings: () => dispatch(push('/settings/advanced')),
+    onClose: () => pushHistory('/connect'),
+    onViewAccount: () => pushHistory('/settings/account'),
+    onViewSupport: () => pushHistory('/settings/support'),
+    onViewAdvancedSettings: () => pushHistory('/settings/advanced'),
     onExternalLink: (type) => shell.openExternal(links[type]),
   };
 };
