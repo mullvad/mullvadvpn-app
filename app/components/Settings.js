82ff9c91a8f99c0d83ae27659b32798818b55a3b
anderklander <anderklander@gmail.com>
2017-12-14 13:55:39 +0100

    Lint-stuff

--- app/components/Settings.js
+++ app/components/Settings.js
@@ -1,13 +1,13 @@
 // @flow
 import moment from 'moment';
 import React from 'react';
-import { Component, Text, Image, Button, View } from "reactxp";
+import { Component, Text, Button, View } from 'reactxp';
 import { Layout, Container, Header } from './Layout';
 import CustomScrollbars from './CustomScrollbars';
 
 import ChevronRightSVG from '../assets/images/icon-chevron.svg';
 import ExternalLinkSVG from '../assets/images/icon-extLink.svg';
-import styles from "./SettingsStyles";
+import styles from './SettingsStyles';
 import Img from './Img';
 
 import type { AccountReduxState } from '../redux/account/reducers';
@@ -44,8 +44,8 @@ export default class Settings extends Component {
         <Header hidden={ true } style={ 'defaultDark' } />
         <Container>
           <View style={styles.settings}>
-            <Button style={styles.settings__close} onPress={ this.props.onClose } testName="settings__close">
-              <Img style={styles.settings__close_icon} source="icon-close"/>
+            <Button style={styles.settings__close} onPress={ this.props.onClose } testName='settings__close'>
+              <Img style={styles.settings__close_icon} source='icon-close'/>
             </Button>
 
             <View style={styles.settings__container}>
@@ -59,18 +59,18 @@ export default class Settings extends Component {
                   <View style={styles.settings__main}>
                     {/* show account options when logged in */}
                     {isLoggedIn ? (
-                      <View style={styles.settings_account} testName="settings__account">
-                        <Button onPress={ this.props.onViewAccount } testName="settings__view_account">
+                      <View style={styles.settings_account} testName='settings__account'>
+                        <Button onPress={ this.props.onViewAccount } testName='settings__view_account'>
                           <View style={styles.settings__cell}>
                             <Text style={styles.settings__cell_label}>Account</Text>
                             <View style={styles.settings__cell_value}>
                               {isOutOfTime ? (
-                                <Text style={styles.settings__account_paid_until_label__error} testName="settings__account_paid_until_label">OUT OF TIME</Text>
+                                <Text style={styles.settings__account_paid_until_label__error} testName='settings__account_paid_until_label'>OUT OF TIME</Text>
                               ) : (
-                                <Text style={styles.settings__account_paid_until_label} testName="settings__account_paid_until_label">{formattedExpiry}</Text>
+                                <Text style={styles.settings__account_paid_until_label} testName='settings__account_paid_until_label'>{formattedExpiry}</Text>
                               )}
                             </View>
-                            <Img style={styles.settings__cell_disclosure} source="icon-chevron"/>
+                            <Img style={styles.settings__cell_disclosure} source='icon-chevron'/>
                           </View>
                         </Button>
                         <View style={styles.settings__cell_spacer} />
@@ -81,32 +81,32 @@ export default class Settings extends Component {
                       <Button onPress={ this.props.onViewAdvancedSettings }>
                         <View style={styles.settings__cell}>
                           <Text style={styles.settings__cell_label}>Advanced</Text>
-                          <Img style={styles.settings__cell_disclosure} source="icon-chevron"/>
+                          <Img style={styles.settings__cell_disclosure} source='icon-chevron'/>
                         </View>
                         <View style={styles.settings__cell_spacer}></View>
                       </Button>
                     ) : null}
 
-                    <View style={styles.settings__external} testName="settings__external">
+                    <View style={styles.settings__external} testName='settings__external'>
 
-                      <Button onPress={ this.props.onExternalLink.bind(this, "faq") } testName="settings__external_link">
+                      <Button onPress={ this.props.onExternalLink.bind(this, 'faq') } testName='settings__external_link'>
                         <View style={styles.settings__cell}>
                           <Text style={styles.settings__cell_label}>FAQs</Text>
-                          <Img style={styles.settings__cell_icon} source="icon-extLink"/>
+                          <Img style={styles.settings__cell_icon} source='icon-extLink'/>
                         </View>
                       </Button>
 
-                      <Button onPress={ this.props.onExternalLink.bind(this, "guides") } testName="settings__external_link">
+                      <Button onPress={ this.props.onExternalLink.bind(this, 'guides') } testName='settings__external_link'>
                         <View style={styles.settings__cell}>
                           <Text style={styles.settings__cell_label}>Guides</Text>
-                          <Img style={styles.settings__cell_icon} source="icon-extLink"/>
+                          <Img style={styles.settings__cell_icon} source='icon-extLink'/>
                         </View>
                       </Button>
 
-                      <Button onPress={ this.props.onViewSupport }  testName="settings__view_support">
+                      <Button onPress={ this.props.onViewSupport }  testName='settings__view_support'>
                         <View style={styles.settings__cell}>
                           <Text style={styles.settings__cell_label}>Contact support</Text>
-                          <Img style={styles.settings__cell_icon} source="icon-chevron"/>
+                          <Img style={styles.settings__cell_icon} source='icon-chevron'/>
                         </View>
                       </Button>
 
@@ -114,8 +114,8 @@ export default class Settings extends Component {
                   </View>
 
                   <View style={styles.settings__footer}>
-                    <Button style={styles.settings__footer_button} onPress={this.props.onQuit} testName="settings__quit">
-                        <Text style={styles.settings__footer_button_label}>Quit app</Text>
+                    <Button style={styles.settings__footer_button} onPress={this.props.onQuit} testName='settings__quit'>
+                      <Text style={styles.settings__footer_button_label}>Quit app</Text>
                     </Button>
                   </View>
                 </View>
