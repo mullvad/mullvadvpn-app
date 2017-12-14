3cf03ea1d0e84a404ab0ef657f08afe6f8e69cc7
anderklander <anderklander@gmail.com>
2017-12-14 14:02:13 +0100

    lint styles

--- app/components/SettingsStyles.js
+++ app/components/SettingsStyles.js
@@ -1,4 +1,4 @@
-import { Styles } from "reactxp";
+import { Styles } from 'reactxp';
 
 const styles = {
   settings:
@@ -8,7 +8,7 @@ const styles = {
   }),
   settings__container:
   Styles.createViewStyle({
-    flexDirection: "column",
+    flexDirection: 'column',
     height: '100%'
   }),
   settings__header:
@@ -18,18 +18,18 @@ const styles = {
     paddingBottom: 12,
     paddingLeft: 24,
     paddingRight: 24,
-    position: "relative" /* anchor for close button */
+    position: 'relative' /* anchor for close button */
   }),
   settings__content:
   Styles.createViewStyle({
-    flexDirection: "column",
+    flexDirection: 'column',
     flex: 1,
   }),
   settings__close:
   Styles.createViewStyle({
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-start",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
     marginTop: 0,
     marginLeft: 12,
   }),
@@ -43,15 +43,15 @@ const styles = {
   }),
   settings__title:
   Styles.createTextStyle({
-    fontFamily: "DINPro",
+    fontFamily: 'DINPro',
     fontSize: 32,
-    fontWeight: "900",
+    fontWeight: '900',
     lineHeight: 40,
-    color: "#FFFFFF"
+    color: '#FFFFFF'
   }),
   settings__cell:
   Styles.createViewStyle({
-    backgroundColor: "rgba(41,71,115,1)",
+    backgroundColor: 'rgba(41,71,115,1)',
     paddingTop: 15,
     paddingBottom: 15,
     paddingLeft: 24,
@@ -61,9 +61,9 @@ const styles = {
     marginTop: -1, //Because of button.css, when removed remove this
     marginBottom: 0, //Because of button.css, when removed remove this
     flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between"
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   }),
   settings__cell_disclosure:
   Styles.createViewStyle({
@@ -76,15 +76,15 @@ const styles = {
   }),
   settings__cell__active_hover:
   Styles.createViewStyle({
-    backgroundColor: "rgba(41,71,115,0.9)"
+    backgroundColor: 'rgba(41,71,115,0.9)'
   }),
   settings__cell_label:
   Styles.createTextStyle({
-    fontFamily: "DINPro",
+    fontFamily: 'DINPro',
     fontSize: 20,
-    fontWeight: "900",
+    fontWeight: '900',
     lineHeight: 26,
-    color: "#FFFFFF"
+    color: '#FFFFFF'
   }),
   settings__cell_icon:
   Styles.createViewStyle({
@@ -96,40 +96,40 @@ const styles = {
   }),
   settings__account_paid_until_label:
   Styles.createTextStyle({
-    fontFamily: "Open Sans",
+    fontFamily: 'Open Sans',
     fontSize: 13,
-    fontWeight: "800",
-    color: "rgba(255, 255, 255, 0.8)"
+    fontWeight: '800',
+    color: 'rgba(255, 255, 255, 0.8)'
   }),
   settings__account_paid_until_label__error:
   Styles.createViewStyle({
-    color: "#d0021b"
+    color: '#d0021b'
   }),
   settings__footer_button_label:
   Styles.createTextStyle({
-    fontFamily: "DINPro",
+    fontFamily: 'DINPro',
     fontSize: 20,
-    fontWeight: "900",
+    fontWeight: '900',
     lineHeight: 26,
-    color: "rgba(255,255,255,0.8)"
+    color: 'rgba(255,255,255,0.8)'
   }),
   settings__footer_button:
   Styles.createViewStyle({
-    backgroundColor: "rgba(208,2,27,1)",
+    backgroundColor: 'rgba(208,2,27,1)',
     paddingTop: 7,
     paddingLeft: 12,
     paddingRight: 12,
     paddingBottom: 9,
     borderRadius: 4,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     width: '100%',
   }),
   settings__footer:
   Styles.createViewStyle({
     width: '100%',
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingTop: 24,
     paddingLeft: 24,
     paddingRight: 24,
