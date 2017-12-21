3af432abdaeff5243b6ce55ec4d7fa1e114016d6
anderklander <anderklander@gmail.com>
2017-12-21 21:14:55 +0100

    tintColor for android

--- app/components/Img.android.js
+++ app/components/Img.android.js
@@ -1,9 +1,29 @@
 // @flow
-import React from 'react';
-import { Image, Component } from 'reactxp';
+import React, { Component }  from 'react';
+import { StyleSheet } from 'react-native';
+import { Image } from 'reactxp';
 
 export default class Img extends Component {
+  props: {
+    source: string,
+    style: Object,
+    tintColor?: string
+  };
+
   render(){
-    return (<Image style={ this.props.style } source={ this.props.source }/>);
+    const { source, tintColor, style } = this.props;
+    var _style = StyleSheet.flatten(style);
+
+    if (tintColor === 'currentColor' && Object.prototype.hasOwnProperty.call(_style, 'color')) {
+      const tint = _style.color;
+      delete _style.color;
+      return(
+        <Image style={[ _style, { tintColor: tint } ]} source={ source }/>
+      );
+    } else {
+      return(
+        <Image style={ style } source={ source }/>
+      );
+    }
   }
 }
