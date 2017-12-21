ba6baf5364951f40b6df459cfa01fe864ea09904
Andrej Mihajlov <and@mullvad.net>
2017-12-21 13:15:40 +0100

    Add tint color to Img component

--- app/components/Img.js
+++ app/components/Img.js
@@ -3,11 +3,37 @@ import React from 'react';
 import { View, Component } from 'reactxp';
 
 export default class Img extends Component {
+  props: {
+    source: string,
+    tintColor?: string
+  };
+
   render() {
-    const url = './assets/images/' + this.props.source + '.svg';
+    const { source, tintColor, ...otherProps } = this.props;
+    const url = './assets/images/' + source + '.svg';
+    let image;
 
-    const style = this.props.style;
+    if(tintColor) {
+      image = (
+        <div style={{
+          WebkitMaskImage: `url('${url}')`,
+          WebkitMaskRepeat: 'no-repeat',
+          backgroundColor: tintColor,
+        }}>
+          <img src={ url } style={{
+            visibility: 'hidden',
+          }} />
+        </div>
+      );
+    } else {
+      image = (
+        <img src={ url } />
+      );
+    }
 
-    return (<View style={ style }> <img src={ url } /> </View>);
+    return (
+      <View { ...otherProps }>
+        { image }
+      </View>);
   }
 }
