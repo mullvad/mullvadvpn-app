86476bde25b27827d771d8569c2877b4701d7874
Erik Larkö <erik@mullvad.net>
2018-01-02 09:07:41 +0100

    Use default-styled button in the settings view

---
+++ app/components/styled/Button.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React from 'react';
+import ReactXP from 'reactxp';
+const defaultStyle = {
+  cursor: 'default',
+};
+export function Button(props: *) {
+  const { style, cursor, ...rest } = props;
+
+  const concreteStyle = Object.assign({}, defaultStyle, style);
+
+  // Can be removed when we upgrade to ReactXP 0.51
+  const concreteCursor = cursor || concreteStyle.cursor || 'default';
+
+  return <ReactXP.Button style={concreteStyle} cursor={concreteCursor} {...rest} />;
+}
