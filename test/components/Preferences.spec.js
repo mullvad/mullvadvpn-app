c78de1b3c9695ff0d472e71f4f6c602468375636
Andrej Mihajlov <and@mullvad.net>
2018-01-02 11:58:10 +0100

    Add test for Preferences

---
+++ test/components/Preferences.spec.js
@@ -0,0 +1,29 @@
+// @flow
+
+import { expect } from 'chai';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Preferences from '../../app/components/Preferences';
+
+require('../setup/enzyme');
+
+describe('components/Preferences', () => {
+
+  it('Should call close handler', (done) => {
+    const props = makeProps({ onClose: done });
+    const component = shallow(<Preferences { ...props } />);
+    const button = component.find({ testName: 'closeButton' });
+    expect(button).to.have.length(1);
+    button.simulate('press');
+  });
+
+});
+
+function makeProps(props) {
+  return {
+    onClose: () => {},
+    onChangeAllowLan: () => {},
+    allowLan: false,
+    ...props
+  };
+}
