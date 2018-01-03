b2ae7d5648316f24c1d00d04ce722ab6bddc2f58
Andrej Mihajlov <and@mullvad.net>
2018-01-03 12:30:00 +0100

    Use createXXXStyles helpers

--- app/components/HeaderBarStyles.js
+++ app/components/HeaderBarStyles.js
@@ -1,69 +1,62 @@
 // @flow
-import { Styles } from 'reactxp';
+import { createTextStyles, createViewStyles } from '../lib/styles';
 
-const styles = {
-  headerbar:
-  Styles.createViewStyle({
-    paddingTop: 12,
-    paddingBottom: 12,
-    paddingLeft: 12,
-    paddingRight: 12,
-    backgroundColor: '#294D73',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+export default {
+  ...createViewStyles({
+    headerbar: {
+      paddingTop: 12,
+      paddingBottom: 12,
+      paddingLeft: 12,
+      paddingRight: 12,
+      backgroundColor: '#294D73',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    headerbar__hidden: {
+      paddingTop: 24,
+      paddingBottom: 0,
+      paddingLeft: 0,
+      paddingRight: 0,
+    },
+    headerbar__darwin: {
+      paddingTop: 24,
+    },
+    headerbar__style_defaultDark: {
+      backgroundColor: '#192E45',
+    },
+    headerbar__style_error: {
+      backgroundColor: '#D0021B',
+    },
+    headerbar__style_success: {
+      backgroundColor: '#44AD4D',
+    },
+    headerbar__container: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    headerbar__logo: {
+      height: 50,
+      width: 50,
+    },
+    headerbar__settings: {
+      padding: 0
+    },
+    headerbar__settings_icon: {
+      width: 24,
+      height: 24,
+    },
   }),
-  headerbar__hidden:
-  Styles.createViewStyle({
-    paddingTop: 24,
-    paddingBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-  }),
-  headerbar__darwin: Styles.createViewStyle({
-    paddingTop: 24,
-  }),
-  headerbar__style_defaultDark:
-  Styles.createViewStyle({
-    backgroundColor: '#192E45',
-  }),
-  headerbar__style_error:
-  Styles.createViewStyle({
-    backgroundColor: '#D0021B',
-  }),
-  headerbar__style_success:
-  Styles.createViewStyle({
-    backgroundColor: '#44AD4D',
-  }),
-  headerbar__container:
-  Styles.createViewStyle({
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  }),
-  headerbar__title:
-  Styles.createTextStyle({
-    fontFamily: 'DINPro',
-    fontSize: 24,
-    fontWeight: '900',
-    lineHeight: 30,
-    letterSpacing: -0.5,
-    color: 'rgba(255,255,255,0.6)',
-    marginLeft: 8,
-  }),
-  headerbar__logo:
-  Styles.createViewStyle({
-    height: 50,
-    width: 50,
-  }),
-  headerbar__settings: {
-    padding: 0
-  },
-  headerbar__settings_icon:
-  Styles.createViewStyle({
-    width: 24,
-    height: 24,
-  }),
+  ...createTextStyles({
+    headerbar__title: {
+      fontFamily: 'DINPro',
+      fontSize: 24,
+      fontWeight: '900',
+      lineHeight: 30,
+      letterSpacing: -0.5,
+      color: 'rgba(255,255,255,0.6)',
+      marginLeft: 8,
+    }
+  })
 };
-
-module.exports = styles;
