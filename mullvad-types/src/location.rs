3ba7c4bba646d207cf18170586f3f372e7de6c80
Linus Färnstrand <linus@mullvad.net>
2018-01-11 10:47:41 +0100

    Download geoip data from am.i.mullvad on request

--- mullvad-types/src/location.rs
+++ mullvad-types/src/location.rs
@@ -1,3 +1,5 @@
+use std::net::IpAddr;
+
 pub type CountryCode = String;
 pub type CityCode = String;
 
@@ -8,4 +10,24 @@ pub struct Location {
     pub city: String,
     pub city_code: CityCode,
     pub position: [f64; 2],
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GeoIpLocation {
+    pub ip: IpAddr,
+    pub country: String,
+    pub city: Option<String>,
+    pub latitude: f64,
+    pub longitude: f64,
+    pub mullvad_exit_ip: bool,
+}
+
+impl GeoIpLocation {
+    // pub fn ip(&self) -> Option<IpAddr> {
+    //     IpAddr::from_str(&self.ip).ok()
+    // }
+
+    pub fn position(&self) -> [f64; 2] {
+        [self.latitude, self.longitude]
+    }
 }
