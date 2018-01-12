c0a171dc617b757df1fb54792bd16453ccab19c9
Andrej Mihajlov <and@mullvad.net>
2018-01-12 13:14:44 +0100

    Show Country and IP when disconnected

--- app/components/Connect.js
+++ app/components/Connect.js
@@ -155,19 +155,6 @@ export default class Connect extends Component {
           : './assets/images/location-marker-unsecure.svg' } />
     */
 
-    let ipComponent = undefined;
-    if (isConnected || isDisconnected) {
-      if (this.state.showCopyIPMessage) {
-        ipComponent = (<span>{ 'IP copied to clipboard!' }</span>);
-      } else {
-        // TODO: remove empty IP placeholder when implemented in backend.
-        if(isDisconnected) {
-          ipComponent = (<span>{ '\u2003' }</span>);
-        } else {
-          ipComponent = (<span>{ this.props.connection.ip }</span>);
-        }
-      }
-    }
     return (
       <div className="connect">
         <div className="connect__map">
@@ -190,19 +177,8 @@ export default class Connect extends Component {
               **********************************
             */ }
 
-            { /* location when disconnected.
-            TODO: merge with the isConnecting block below when implemented in backend.
-            */ }
-            <If condition={ isDisconnected }>
-              <Then>
-                <div className="connect__status-location">
-                  <span>{ '\u2002' }</span>
-                </div>
-              </Then>
-            </If>
-
-            { /* location when connecting */ }
-            <If condition={ isConnecting }>
+            { /* location when connecting or disconnected */ }
+            <If condition={ isConnecting || isDisconnected }>
               <Then>
                 <div className="connect__status-location">
                   <span>{ this.props.connection.country }</span>
@@ -214,7 +190,9 @@ export default class Connect extends Component {
             <If condition={ isConnected }>
               <Then>
                 <div className="connect__status-location">
-                  { this.props.connection.city }<br/>{ this.props.connection.country }
+                  { this.props.connection.city }
+                  { this.props.connection.city && <br/> }
+                  { this.props.connection.country }
                 </div>
               </Then>
             </If>
@@ -226,7 +204,12 @@ export default class Connect extends Component {
             */ }
 
             <div className={ this.ipAddressClass() } onClick={ this.onIPAddressClick.bind(this) }>
-              { ipComponent }
+              { (isConnected || isDisconnected) ? (
+                <span>{
+                  this.state.showCopyIPMessage ?
+                    'IP copied to clipboard!' :
+                    this.props.connection.ip
+                }</span>) : null }
             </div>
           </div>
 
