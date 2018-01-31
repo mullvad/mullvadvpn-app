d1229cd66a04566422671f91a3a2d894cbe01e71
Erik Larkö <erik@mullvad.net>
2018-01-31 15:53:31 +0100

    Show the app version in the settings view

--- app/components/Settings.js
+++ app/components/Settings.js
@@ -7,6 +7,7 @@ import { Layout, Container, Header } from './Layout';
 import CustomScrollbars from './CustomScrollbars';
 import styles from './SettingsStyles';
 import Img from './Img';
+import { remote } from 'electron';
 
 import type { AccountReduxState } from '../redux/account/reducers';
 import type { SettingsReduxState } from '../redux/settings/reducers';
@@ -47,6 +48,9 @@ export default class Settings extends Component {
                 <View style={styles.settings__content}>
                   <View>
                     { this._renderTopButtons() }
+                    <View style={styles.settings__cell_spacer}/>
+                    { this._renderMiddleButtons() }
+                    <View style={styles.settings__cell_spacer}/>
                     { this._renderBottomButtons() }
                   </View>
                   { this._renderQuitButton() }
@@ -83,7 +87,7 @@ export default class Settings extends Component {
             {isOutOfTime ? (
               <Text style={styles.settings__account_paid_until_label__error} testName='settings__account_paid_until_label'>OUT OF TIME</Text>
             ) : (
-              <Text style={styles.settings__account_paid_until_label} testName='settings__account_paid_until_label'>{formattedExpiry}</Text>
+              <Text style={styles.settings__cell_subtext} testName='settings__account_paid_until_label'>{formattedExpiry}</Text>
             )}
             <Img style={styles.settings__cell_disclosure} source='icon-chevron' tintColor='currentColor'/>
           </View>
@@ -100,8 +104,26 @@ export default class Settings extends Component {
         <Img style={styles.settings__cell_disclosure} source='icon-chevron' tintColor='currentColor'/>
       </ButtonCell>
 
-      <View style={styles.settings__cell_spacer}></View>
     </View>;
+  }
+
+  _renderMiddleButtons() {
+    return <View>
+      <ButtonCell testName='settings__version'>
+        <Text style={styles.settings__cell_label}>App version</Text>
+        <Text style={styles.settings__cell_subtext}>{this._formattedVersion()}</Text>
+      </ButtonCell>
+    </View>;
+  }
+
+  _formattedVersion() {
+    // the version in package.json has to be semver, but we use a YEAR.release-channel
+    // version scheme. in package.json we thus have to write YEAR.release.X-channel and
+    // this function is responsible for removing .X part.
+    const version = remote.app.getVersion();
+    return version
+      .replace('.0-', '-')  // remove the .0 in 2018.1.0-beta9
+      .replace(/\.0$/, ''); // remove the .0 in 2018.1.0
   }
 
   _renderBottomButtons() {
