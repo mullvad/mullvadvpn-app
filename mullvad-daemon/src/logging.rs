20d52844af57661a2f42c05c2c11ee73295264ed
Linus Färnstrand <linus@mullvad.net>
2018-03-01 19:38:53 +0100

    Move silenced crates and colors out to constants

--- mullvad-daemon/src/logging.rs
+++ mullvad-daemon/src/logging.rs
@@ -20,34 +20,36 @@ error_chain! {
     }
 }
 
+const SILENCED_CRATES: &[&str] = &[
+    "jsonrpc_core",
+    // jsonrpc_core does some logging under the "rpc" target as well.
+    "rpc",
+    "tokio_core",
+    "tokio_proto",
+    "jsonrpc_ws_server",
+    "ws",
+    "mio",
+    "hyper",
+];
+
+const COLORS: ColoredLevelConfig = ColoredLevelConfig {
+    error: Color::Red,
+    warn: Color::Yellow,
+    info: Color::Green,
+    debug: Color::Blue,
+    trace: Color::Black,
+};
+
 pub const DATE_TIME_FORMAT_STR: &str = "%Y-%m-%d %H:%M:%S%.3f";
 
 pub fn init_logger(log_level: log::LevelFilter, log_file: Option<&PathBuf>) -> Result<()> {
-    let silenced_crates = [
-        "jsonrpc_core",
-        // jsonrpc_core does some logging under the "rpc" target as well.
-        "rpc",
-        "tokio_core",
-        "tokio_proto",
-        "jsonrpc_ws_server",
-        "ws",
-        "mio",
-        "hyper",
-    ];
-    let colors = ColoredLevelConfig::new()
-        .error(Color::Red)
-        .warn(Color::Yellow)
-        .info(Color::Green)
-        .debug(Color::Blue)
-        .trace(Color::Black);
-
     let mut top_dispatcher = fern::Dispatch::new().level(log_level);
-    for silenced_crate in &silenced_crates {
+    for silenced_crate in SILENCED_CRATES {
         top_dispatcher = top_dispatcher.level_for(*silenced_crate, log::LevelFilter::Warn);
     }
 
     let stdout_dispatcher = fern::Dispatch::new()
-        .format(move |out, message, record| format_log_msg(out, message, record, Some(&colors)))
+        .format(move |out, message, record| format_log_msg(out, message, record, true))
         .chain(io::stdout());
     top_dispatcher = top_dispatcher.chain(stdout_dispatcher);
 
@@ -55,7 +57,7 @@ pub fn init_logger(log_level: log::LevelFilter, log_file: Option<&PathBuf>) -> R
         let f = fern::log_file(log_file)
             .chain_err(|| ErrorKind::WriteFileError(log_file.to_path_buf()))?;
         let file_dispatcher = fern::Dispatch::new()
-            .format(|out, message, record| format_log_msg(out, message, record, None))
+            .format(|out, message, record| format_log_msg(out, message, record, false))
             .chain(f);
         top_dispatcher = top_dispatcher.chain(file_dispatcher);
     }
@@ -67,15 +69,15 @@ fn format_log_msg(
     out: fern::FormatCallback,
     message: &fmt::Arguments,
     record: &log::Record,
-    colors: Option<&ColoredLevelConfig>,
+    color_output: bool,
 ) {
     let timestamp = chrono::Local::now().format(DATE_TIME_FORMAT_STR);
-    if let (Some(colors), false) = (colors, cfg!(windows)) {
+    if color_output && cfg!(not(windows)) {
         out.finish(format_args!(
             "[{}][{}][{}] {}",
             timestamp,
             record.target(),
-            colors.color(record.level()),
+            COLORS.color(record.level()),
             message,
         ))
     } else {
