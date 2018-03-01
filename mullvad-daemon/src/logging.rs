fef91e9616e42347a09edfc103ede2faa15b0587
Linus Färnstrand <linus@mullvad.net>
2018-03-01 14:40:31 +0100

    Add coloring to stdout logging

--- mullvad-daemon/src/logging.rs
+++ mullvad-daemon/src/logging.rs
@@ -1,5 +1,6 @@
 extern crate fern;
 
+use self::fern::colors::{Color, ColoredLevelConfig};
 use chrono;
 use log;
 
@@ -20,7 +21,7 @@ error_chain! {
 
 pub const DATE_TIME_FORMAT_STR: &str = "%Y-%m-%d %H:%M:%S%.3f";
 
-pub fn init_logger(log_level: log::LogLevelFilter, log_file: Option<&PathBuf>) -> Result<()> {
+pub fn init_logger(log_level: log::LevelFilter, log_file: Option<&PathBuf>) -> Result<()> {
     let silenced_crates = [
         "jsonrpc_core",
         // jsonrpc_core does some logging under the "rpc" target as well.
@@ -32,26 +33,47 @@ pub fn init_logger(log_level: log::LogLevelFilter, log_file: Option<&PathBuf>) -
         "mio",
         "hyper",
     ];
-    let mut config = fern::Dispatch::new()
-        .format(|out, message, record| {
+    let colors = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::Green)
+        .debug(Color::Blue)
+        .trace(Color::Black);
+
+    let mut top_dispatcher = fern::Dispatch::new().level(log_level);
+    for silenced_crate in &silenced_crates {
+        top_dispatcher = top_dispatcher.level_for(*silenced_crate, log::LevelFilter::Warn);
+    }
+
+    let stdout_dispatcher = fern::Dispatch::new()
+        .format(move |out, message, record| {
             out.finish(format_args!(
                 "[{}][{}][{}] {}",
                 chrono::Local::now().format(DATE_TIME_FORMAT_STR),
                 record.target(),
-                record.level(),
+                colors.color(record.level()),
                 message
             ))
         })
-        .level(log_level)
         .chain(io::stdout());
-    for silenced_crate in &silenced_crates {
-        config = config.level_for(*silenced_crate, log::LogLevelFilter::Warn);
-    }
+    top_dispatcher = top_dispatcher.chain(stdout_dispatcher);
+
     if let Some(ref log_file) = log_file {
         let f = fern::log_file(log_file)
             .chain_err(|| ErrorKind::WriteFileError(log_file.to_path_buf()))?;
-        config = config.chain(f);
+        let file_dispatcher = fern::Dispatch::new()
+            .format(move |out, message, record| {
+                out.finish(format_args!(
+                    "[{}][{}][{}] {}",
+                    chrono::Local::now().format(DATE_TIME_FORMAT_STR),
+                    record.target(),
+                    record.level(),
+                    message
+                ))
+            })
+            .chain(f);
+        top_dispatcher = top_dispatcher.chain(file_dispatcher);
     }
-    config.apply()?;
+    top_dispatcher.apply()?;
     Ok(())
 }
