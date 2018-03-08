31be698ccf3e0dca9530c6b665484fa9e7834afa
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-08 00:56:42 -0300

    Test RPC address file creation on daemon start-up

---
+++ mullvad-daemon/tests/startup.rs
@@ -0,0 +1,59 @@
+#[macro_use]
+extern crate duct;
+#[cfg(unix)]
+extern crate libc;
+extern crate os_pipe;
+
+mod common;
+
+use std::fs::{self, Metadata};
+use std::io;
+use std::time::Duration;
+
+use common::{rpc_file_path, DaemonRunner};
+
+use platform_specific::*;
+
+#[test]
+fn rpc_info_file_permissions() {
+    let rpc_file = rpc_file_path();
+
+    if let Err(error) = fs::remove_file(&rpc_file) {
+        if error.kind() != io::ErrorKind::NotFound {
+            panic!("failed to remove existing RPC address file");
+        }
+    }
+
+    assert!(!rpc_file.exists());
+
+    let mut daemon = DaemonRunner::spawn();
+
+    daemon.assert_output("Wrote RPC connection info to", Duration::from_secs(10));
+
+    assert!(rpc_file.exists());
+
+    ensure_only_admin_can_write(
+        fs::metadata(&rpc_file).expect("failed to read RPC address file metadata"),
+    );
+}
+
+#[cfg(unix)]
+mod platform_specific {
+    use super::*;
+    use std::os::unix::fs::MetadataExt;
+
+    pub fn ensure_only_admin_can_write(metadata: Metadata) {
+        let process_uid = unsafe { libc::getuid() };
+        assert_eq!(metadata.uid(), process_uid);
+        assert_eq!(metadata.mode() & 0o022, 0);
+    }
+}
+
+#[cfg(not(unix))]
+mod platform_specific {
+    use super::*;
+
+    pub fn ensure_only_admin_can_write(_metadata: Metadata) {
+        // TODO: Test when correctly implemented on Windows
+    }
+}
