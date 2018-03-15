c592861148e4ba23500446b10aab9f3189b1542c
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-15 07:10:27 -0300

    Use cross-platform RPC file path

--- mullvad-daemon/tests/startup.rs
+++ mullvad-daemon/tests/startup.rs
@@ -15,7 +15,7 @@ use common::DaemonInstance;
 
 #[test]
 fn rpc_info_file_permissions() {
-    let rpc_file = Path::new("/tmp/.mullvad_rpc_address");
+    let rpc_file = rpc_file_path();
 
     if let Err(error) = fs::remove_file(&rpc_file) {
         if error.kind() != io::ErrorKind::NotFound {
@@ -37,4 +37,14 @@ fn rpc_info_file_permissions() {
     let metadata = fs::metadata(&rpc_file).expect("failed to inspect RPC address file");
     assert_eq!(metadata.uid(), uid);
     assert_eq!(metadata.mode() & 0o022, 0);
+}
+
+#[cfg(target(unix))]
+fn rpc_file_path() -> Path {
+    Path::new("/tmp/.mullvad_rpc_address")
+}
+
+#[cfg(not(target(unix)))]
+fn rpc_file_path() -> Path {
+    ::std::env::temp_dir().join("/tmp/.mullvad_rpc_address")
 }
