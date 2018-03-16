513df17acbb203b530938653f99fad1c26374a01
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-16 16:08:53 -0300

    Test `AddressCache`

--- mullvad-rpc/src/address_cache.rs
+++ mullvad-rpc/src/address_cache.rs
@@ -91,3 +91,74 @@ impl AddressCache {
             .map_err(|error| io::Error::new(io::ErrorKind::Other, error))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    extern crate tempdir;
+
+    use std::fs::File;
+    use std::io::{BufRead, BufReader, Write};
+
+    use self::tempdir::TempDir;
+    use super::*;
+
+    #[test]
+    fn uses_cached_address() {
+        let temp_dir = TempDir::new("ip-cache-test").unwrap();
+        let cached_address = AddressCacheData {
+            ip: "127.0.0.1".to_string(),
+            port: 52780,
+        };
+
+        {
+            let cache_file_path = temp_dir.path().join("api_address.json");
+            let mut cache_file = File::create(cache_file_path).unwrap();
+            writeln!(
+                cache_file,
+                "{{ \"ip\": \"{}\", \"port\": {} }}",
+                cached_address.ip, cached_address.port
+            ).unwrap();
+        }
+
+        let cache = AddressCache::new(temp_dir.path());
+        let address = cache.api_address().unwrap();
+
+        assert_eq!(
+            address,
+            format!("{}:{}", cached_address.ip, cached_address.port)
+        );
+    }
+
+    #[test]
+    fn caches_resolved_ip() {
+        let temp_dir = TempDir::new("ip-cache-test").unwrap();
+        let cache = AddressCache::new(temp_dir.path());
+        let ip = cache.api_address().unwrap();
+
+        let cache_file_path = temp_dir.path().join("api_address.json");
+        assert!(cache_file_path.exists());
+
+        let cache_file = File::open(cache_file_path).unwrap();
+        let mut cache_reader = BufReader::new(cache_file);
+        let mut cached_address = String::new();
+        cache_reader.read_line(&mut cached_address).unwrap();
+
+        let mut address_parts = ip.split(":");
+        let ip = address_parts.next().unwrap();
+        let port = address_parts.next().unwrap();
+        assert!(address_parts.next().is_none());
+
+        assert_eq!(
+            cached_address,
+            format!("{{\"ip\":\"{}\",\"port\":{}}}", ip, port)
+        );
+    }
+
+    #[test]
+    fn resolves_even_if_impossible_to_store_in_cache() {
+        let temp_dir_path = TempDir::new("ip-cache-test").unwrap().path().to_path_buf();
+        let cache = AddressCache::new(&temp_dir_path);
+
+        assert!(cache.api_address().is_some());
+    }
+}
