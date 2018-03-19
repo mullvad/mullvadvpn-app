968eb5a69f7115cc7c4ed0a817a4af4169af2941
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-19 11:06:30 -0300

    Use `SocketAddr` instead of `AddressCacheData`

--- mullvad-rpc/src/address_cache.rs
+++ mullvad-rpc/src/address_cache.rs
@@ -1,39 +1,11 @@
-use std::fmt::{self, Display, Formatter};
 use std::fs::File;
 use std::io;
-use std::net::{IpAddr, SocketAddr, ToSocketAddrs};
+use std::net::{SocketAddr, ToSocketAddrs};
 use std::path::{Path, PathBuf};
 
 use serde_json;
 
 use super::MASTER_API_HOST;
-
-#[derive(Deserialize, Serialize)]
-struct AddressCacheData {
-    ip: String,
-    port: u16,
-}
-
-impl AddressCacheData {
-    fn is_valid(&self) -> bool {
-        self.ip.parse::<IpAddr>().is_ok()
-    }
-}
-
-impl From<SocketAddr> for AddressCacheData {
-    fn from(address: SocketAddr) -> Self {
-        AddressCacheData {
-            ip: address.ip().to_string(),
-            port: address.port(),
-        }
-    }
-}
-
-impl Display for AddressCacheData {
-    fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {
-        write!(fmt, "{}:{}", self.ip, self.port)
-    }
-}
 
 pub struct AddressCache {
     cache_file: PathBuf,
@@ -53,21 +25,14 @@ impl AddressCache {
             .ok()
     }
 
-    fn load_from_cache(&self) -> Result<AddressCacheData, io::Error> {
+    fn load_from_cache(&self) -> Result<SocketAddr, io::Error> {
         let cache_file = File::open(&self.cache_file)?;
-        let address: AddressCacheData = serde_json::from_reader(cache_file)?;
+        let address = serde_json::from_reader(cache_file)?;
 
-        if address.is_valid() {
-            Ok(address)
-        } else {
-            Err(io::Error::new(
-                io::ErrorKind::Other,
-                "cached address is invalid",
-            ))
-        }
+        Ok(address)
     }
 
-    fn resolve_into_cache(&self) -> Result<AddressCacheData, io::Error> {
+    fn resolve_into_cache(&self) -> Result<SocketAddr, io::Error> {
         let address = Self::resolve_address()?.into();
 
         let _ = self.store_in_cache(&address);
@@ -84,7 +49,7 @@ impl AddressCache {
             })
     }
 
-    fn store_in_cache(&self, address: &AddressCacheData) -> Result<(), io::Error> {
+    fn store_in_cache(&self, address: &SocketAddr) -> Result<(), io::Error> {
         let cache_file = File::create(&self.cache_file)?;
 
         serde_json::to_writer(&cache_file, &address)
@@ -105,19 +70,12 @@ mod tests {
     #[test]
     fn uses_cached_address() {
         let temp_dir = TempDir::new("ip-cache-test").unwrap();
-        let cached_address = AddressCacheData {
-            ip: "127.0.0.1".to_string(),
-            port: 52780,
-        };
+        let cached_address = SocketAddr::new("127.0.0.1".parse().unwrap(), 52780);
 
         {
             let cache_file_path = temp_dir.path().join("api_address.json");
             let mut cache_file = File::create(cache_file_path).unwrap();
-            writeln!(
-                cache_file,
-                "{{ \"ip\": \"{}\", \"port\": {} }}",
-                cached_address.ip, cached_address.port
-            ).unwrap();
+            writeln!(cache_file, "\"{}\"", cached_address).unwrap();
         }
 
         let cache = AddressCache::new(temp_dir.path());
@@ -125,7 +83,7 @@ mod tests {
 
         assert_eq!(
             address,
-            format!("{}:{}", cached_address.ip, cached_address.port)
+            format!("{}:{}", cached_address.ip(), cached_address.port())
         );
     }
 
@@ -133,7 +91,7 @@ mod tests {
     fn caches_resolved_ip() {
         let temp_dir = TempDir::new("ip-cache-test").unwrap();
         let cache = AddressCache::new(temp_dir.path());
-        let ip = cache.api_address().unwrap();
+        let address = cache.api_address().unwrap();
 
         let cache_file_path = temp_dir.path().join("api_address.json");
         assert!(cache_file_path.exists());
@@ -143,15 +101,7 @@ mod tests {
         let mut cached_address = String::new();
         cache_reader.read_line(&mut cached_address).unwrap();
 
-        let mut address_parts = ip.split(":");
-        let ip = address_parts.next().unwrap();
-        let port = address_parts.next().unwrap();
-        assert!(address_parts.next().is_none());
-
-        assert_eq!(
-            cached_address,
-            format!("{{\"ip\":\"{}\",\"port\":{}}}", ip, port)
-        );
+        assert_eq!(cached_address, format!("\"{}\"", address));
     }
 
     #[test]
