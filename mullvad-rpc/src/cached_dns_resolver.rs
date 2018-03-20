7f303efc789d5ddbd9e2d5d01da18cfd35c67268
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-20 20:42:03 -0300

    Use a mock DNS resolver for testing

--- mullvad-rpc/src/cached_dns_resolver.rs
+++ mullvad-rpc/src/cached_dns_resolver.rs
@@ -3,19 +3,50 @@ use std::io::{self, BufRead, BufReader, Write};
 use std::net::{IpAddr, ToSocketAddrs};
 use std::path::{Path, PathBuf};
 
-pub struct CachedDnsResolver {
+pub trait DnsResolver {
+    fn resolve(&self, host: &str) -> io::Result<IpAddr>;
+}
+
+pub struct SystemDnsResolver;
+
+impl DnsResolver for SystemDnsResolver {
+    fn resolve(&self, host: &str) -> io::Result<IpAddr> {
+        (host, 0)
+            .to_socket_addrs()?
+            .next()
+            .map(|socket_address| socket_address.ip())
+            .ok_or_else(|| {
+                io::Error::new(io::ErrorKind::NotFound, format!("Host not found: {}", host))
+            })
+    }
+}
+
+pub struct CachedDnsResolver<R: DnsResolver = SystemDnsResolver> {
     hostname: String,
+    dns_resolver: R,
     cache_file: PathBuf,
     cached_address: Option<IpAddr>,
 }
 
-impl CachedDnsResolver {
+impl CachedDnsResolver<SystemDnsResolver> {
     pub fn new(hostname: String, cache_dir: &Path, filename: &str) -> Self {
+        Self::with_dns_resolver(SystemDnsResolver, hostname, cache_dir, filename)
+    }
+}
+
+impl<R: DnsResolver> CachedDnsResolver<R> {
+    pub fn with_dns_resolver(
+        dns_resolver: R,
+        hostname: String,
+        cache_dir: &Path,
+        filename: &str,
+    ) -> Self {
         let cache_file = cache_dir.join(filename);
         let cached_address = Self::load_from_file(&cache_file).ok();
 
         CachedDnsResolver {
             hostname,
+            dns_resolver,
             cache_file,
             cached_address,
         }
@@ -40,21 +71,15 @@ impl CachedDnsResolver {
     }
 
     fn resolve_into_cache(&self) -> io::Result<IpAddr> {
-        let address = Self::resolve_address(&self.hostname)?;
+        let address = self.resolve_address()?.into();
 
         let _ = self.store_in_cache(address);
 
         Ok(address)
     }
 
-    fn resolve_address(hostname: &str) -> io::Result<IpAddr> {
-        (hostname, 0)
-            .to_socket_addrs()?
-            .next()
-            .map(|socket_address| socket_address.ip())
-            .ok_or_else(|| {
-                io::Error::new(io::ErrorKind::NotFound, "Mullvad RPC API host not found")
-            })
+    fn resolve_address(&self) -> io::Result<IpAddr> {
+        self.dns_resolver.resolve(&self.hostname)
     }
 
     fn store_in_cache(&self, address: IpAddr) -> io::Result<()> {
@@ -70,6 +95,7 @@ mod tests {
 
     use std::fs::{self, File};
     use std::io::{BufRead, BufReader, Write};
+    use std::sync::atomic::{AtomicBool, Ordering};
 
     use self::tempdir::TempDir;
     use super::*;
@@ -77,34 +103,39 @@ mod tests {
     #[test]
     fn uses_cached_address() {
         let (_temp_dir, cache_dir) = create_test_dirs();
+        let mock_resolver = MockDnsResolver::from_str("192.168.1.206");
         let cached_address = "127.0.0.1".parse().unwrap();
 
         write_address(&cache_dir, cached_address);
 
-        let cache = create_cached_dns_resolver(&cache_dir);
+        let cache = create_cached_dns_resolver(&mock_resolver, &cache_dir);
         let address = cache.resolve().unwrap();
 
+        assert!(!mock_resolver.was_called());
         assert_eq!(address, cached_address);
     }
 
     #[test]
     fn caches_resolved_ip() {
         let (_temp_dir, cache_dir) = create_test_dirs();
-        let cache = create_cached_dns_resolver(&cache_dir);
+        let mock_resolver = MockDnsResolver::from_str("192.168.1.206");
+        let cache = create_cached_dns_resolver(&mock_resolver, &cache_dir);
 
         let address = cache.resolve().unwrap();
 
+        assert_eq!(address, mock_resolver.address());
         assert_eq!(get_cached_address(&cache_dir), address.to_string());
     }
 
     #[test]
     fn resolves_even_if_impossible_to_store_in_cache() {
         let (temp_dir, cache_dir) = create_test_dirs();
-        let cache = create_cached_dns_resolver(&cache_dir);
+        let mock_resolver = MockDnsResolver::from_str("192.168.1.206");
+        let cache = create_cached_dns_resolver(&mock_resolver, &cache_dir);
 
         ::std::mem::drop(temp_dir);
 
-        assert!(cache.resolve().is_some());
+        assert_eq!(cache.resolve().unwrap(), mock_resolver.address());
     }
 
     fn create_test_dirs() -> (TempDir, PathBuf) {
@@ -139,10 +170,48 @@ mod tests {
         cached_address.trim().to_string()
     }
 
-    fn create_cached_dns_resolver(cache_dir: &Path) -> CachedDnsResolver {
-        let hostname = "api.mullvad.net".to_owned();
+    fn create_cached_dns_resolver<'a>(
+        mock_resolver: &'a MockDnsResolver,
+        cache_dir: &'a Path,
+    ) -> CachedDnsResolver<&'a MockDnsResolver> {
+        let hostname = "dummy.host".to_owned();
         let filename = "api_ip_address.txt";
 
-        CachedDnsResolver::new(hostname, cache_dir, filename)
+        CachedDnsResolver::with_dns_resolver(mock_resolver, hostname, cache_dir, filename)
+    }
+
+    struct MockDnsResolver {
+        address: Option<IpAddr>,
+        called: AtomicBool,
+    }
+
+    impl MockDnsResolver {
+        pub fn from_str(ip_address: &str) -> Self {
+            MockDnsResolver {
+                address: Some(ip_address.parse().unwrap()),
+                called: AtomicBool::new(false),
+            }
+        }
+
+        pub fn address(&self) -> IpAddr {
+            self.address.unwrap()
+        }
+
+        pub fn was_called(&self) -> bool {
+            self.called.load(Ordering::Acquire)
+        }
+    }
+
+    impl<'r> DnsResolver for &'r MockDnsResolver {
+        fn resolve(&self, host: &str) -> io::Result<IpAddr> {
+            self.called.store(true, Ordering::Release);
+
+            self.address.ok_or_else(|| {
+                io::Error::new(
+                    io::ErrorKind::NotFound,
+                    format!("Failed to resolve address for {:?}", host),
+                )
+            })
+        }
     }
 }
