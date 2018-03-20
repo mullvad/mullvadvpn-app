ada46e34ae8cbd59f23d7f25d9b76301b4773fae
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-20 16:43:33 -0300

    Revert "Mock DNS resolver for testing"
    This reverts commit 9722571338d89f3cd86a86da61898372ce3907c5.

--- mullvad-rpc/src/api_address.rs
+++ mullvad-rpc/src/api_address.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io;
-use std::marker::PhantomData;
 use std::net::{IpAddr, ToSocketAddrs};
 use std::path::{Path, PathBuf};
 
@@ -13,66 +12,48 @@ pub static MASTER_API_HOST: &str = "api.mullvad.net";
 /// Returns the IP address of the Mullvad API server from cache if it exists, otherwise it tries to
 /// resolve it based on its hostname and cache the result.
 pub fn api_address(resource_dir: Option<&Path>) -> String {
-    ApiAddress::<(&'static str, u16)>::get(resource_dir)
-}
-
-struct ApiAddress<R>
-where
-    R: From<(&'static str, u16)> + ToSocketAddrs,
-{
-    _resolver: PhantomData<R>,
-}
-
-impl<R> ApiAddress<R>
-where
-    R: From<(&'static str, u16)> + ToSocketAddrs,
-{
-    fn get(resource_dir: Option<&Path>) -> String {
-        if let Some(cache_file) = resource_dir.map(Self::get_cache_file_path) {
-            if let Ok(address) = Self::read_cached_address(&cache_file) {
-                address
-            } else if let Ok(address) = Self::resolve_address_from_hostname() {
-                let _ = Self::store_address_in_cache(&address, &cache_file);
-                address.to_string()
-            } else {
-                MASTER_API_HOST.to_string()
-            }
+    if let Some(cache_file) = resource_dir.map(get_cache_file_path) {
+        if let Ok(address) = read_cached_address(&cache_file) {
+            address
+        } else if let Ok(address) = resolve_address_from_hostname() {
+            let _ = store_address_in_cache(&address, &cache_file);
+            address.to_string()
         } else {
             MASTER_API_HOST.to_string()
         }
+    } else {
+        MASTER_API_HOST.to_string()
     }
+}
 
-    fn get_cache_file_path(resource_dir: &Path) -> PathBuf {
-        resource_dir.join("api_address.json")
-    }
+fn get_cache_file_path(resource_dir: &Path) -> PathBuf {
+    resource_dir.join("api_address.json")
+}
 
-    fn read_cached_address(cache_file: &Path) -> Result<String, io::Error> {
-        let reader = File::open(cache_file)?;
-        let address: IpAddr = serde_json::from_reader(reader)?;
+fn read_cached_address(cache_file: &Path) -> Result<String, io::Error> {
+    let reader = File::open(cache_file)?;
+    let address: IpAddr = serde_json::from_reader(reader)?;
 
-        Ok(address.to_string())
-    }
+    Ok(address.to_string())
+}
 
-    fn resolve_address_from_hostname() -> Result<IpAddr, io::Error> {
-        let resolver = R::from((MASTER_API_HOST, 0));
+fn resolve_address_from_hostname() -> Result<IpAddr, io::Error> {
+    (MASTER_API_HOST, 0)
+        .to_socket_addrs()?
+        .next()
+        .map(|socket_address| socket_address.ip())
+        .ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::NotFound,
+                format!("could not resolve hostname {}", MASTER_API_HOST),
+            )
+        })
+}
 
-        resolver
-            .to_socket_addrs()?
-            .next()
-            .map(|socket_address| socket_address.ip())
-            .ok_or_else(|| {
-                io::Error::new(
-                    io::ErrorKind::NotFound,
-                    format!("could not resolve hostname {}", MASTER_API_HOST),
-                )
-            })
-    }
-
-    fn store_address_in_cache(address: &IpAddr, cache_file: &Path) -> Result<(), io::Error> {
-        let file = File::create(cache_file)?;
-        serde_json::to_writer(file, address)
-            .map_err(|error| io::Error::new(io::ErrorKind::Other, error))
-    }
+fn store_address_in_cache(address: &IpAddr, cache_file: &Path) -> Result<(), io::Error> {
+    let file = File::create(cache_file)?;
+    serde_json::to_writer(file, address)
+        .map_err(|error| io::Error::new(io::ErrorKind::Other, error))
 }
 
 #[cfg(test)]
@@ -81,8 +62,6 @@ mod tests {
 
     use std::fs::File;
     use std::io::{BufRead, BufReader, Write};
-    use std::net::SocketAddr;
-    use std::vec::IntoIter;
 
     use self::tempdir::TempDir;
     use super::*;
@@ -90,7 +69,7 @@ mod tests {
     #[test]
     fn uses_cached_address() {
         let temp_dir = TempDir::new("address-cache-test").unwrap();
-        let cached_address = MockResolver::resolved_address().ip().to_string();
+        let cached_address = "127.0.0.1";
 
         {
             let cache_file_path = temp_dir.path().join("api_address.json");
@@ -98,7 +77,7 @@ mod tests {
             writeln!(cache_file, "\"{}\"", cached_address).unwrap();
         }
 
-        let address = ApiAddress::<MockResolver>::get(Some(temp_dir.path()));
+        let address = api_address(Some(temp_dir.path()));
 
         assert_eq!(address, cached_address);
     }
@@ -106,7 +85,7 @@ mod tests {
     #[test]
     fn caches_resolved_address() {
         let temp_dir = TempDir::new("address-cache-test").unwrap();
-        let address = ApiAddress::<MockResolver>::get(Some(temp_dir.path()));
+        let address = api_address(Some(temp_dir.path()));
 
         let cache_file_path = temp_dir.path().join("api_address.json");
         assert!(cache_file_path.exists());
@@ -116,7 +95,6 @@ mod tests {
         let mut cached_address = String::new();
         cache_reader.read_line(&mut cached_address).unwrap();
 
-        assert_eq!(address, MockResolver::resolved_address().ip().to_string());
         assert_eq!(cached_address, format!("\"{}\"", address));
     }
 
@@ -127,35 +105,6 @@ mod tests {
 
         ::std::mem::drop(temp_dir);
 
-        assert_eq!(
-            ApiAddress::<MockResolver>::get(Some(&temp_dir_path)),
-            MockResolver::resolved_address().ip().to_string(),
-        );
-    }
-
-    struct MockResolver {
-        resolved_address: SocketAddr,
-    }
-
-    impl MockResolver {
-        pub fn resolved_address() -> SocketAddr {
-            "127.0.0.1:48719".parse().unwrap()
-        }
-    }
-
-    impl From<(&'static str, u16)> for MockResolver {
-        fn from(_: (&'static str, u16)) -> Self {
-            MockResolver {
-                resolved_address: MockResolver::resolved_address(),
-            }
-        }
-    }
-
-    impl ToSocketAddrs for MockResolver {
-        type Iter = IntoIter<SocketAddr>;
-
-        fn to_socket_addrs(&self) -> io::Result<Self::Iter> {
-            Ok(vec![self.resolved_address.clone()].into_iter())
-        }
+        assert_ne!(api_address(Some(&temp_dir_path)), MASTER_API_HOST);
     }
 }
