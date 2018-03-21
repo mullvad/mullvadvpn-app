5b3cd5a0316e13a7026254931477c4816bd0ab2e
Emīls Piņķis <pinkisemils@gmail.com>
2018-03-21 17:01:27 +0000

    Add AppVersionInfo, AppVersion and LatestReleases to mullvad-types

---
+++ mullvad-types/src/version.rs
@@ -0,0 +1,16 @@
+/// AppVersionInfo represents the current stable and the current latest release versions of the
+/// Mullvad VPN app.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AppVersionInfo {
+    pub current_is_supported: bool,
+    pub latest: LatestReleases,
+}
+
+/// LatestReleases represent the latest released versions of the Mullvad VPN app.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LatestReleases {
+    pub latest_stable: AppVersion,
+    pub latest: AppVersion,
+}
+
+pub type AppVersion = String;
