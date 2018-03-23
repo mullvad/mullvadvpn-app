3295819faa93cf9347ba8f096c15a386e008748d
anderklander <anderklander@gmail.com>
2018-03-23 08:41:31 +0100

    SelectLocation in reactxp

---
+++ app/components/SelectLocationStyles.js
@@ -0,0 +1,88 @@
+// @flow
+import { createViewStyles, createTextStyles } from '../lib/styles';
+import { colors } from '../config';
+
+export default {
+  ...createViewStyles({
+    select_location: {
+      backgroundColor: colors.darkBlue,
+      height: '100%',
+    },
+    container: {
+      flexDirection: 'column',
+      flex: 1,
+    },
+    header:{
+      flex: 0,
+      marginBottom: 16,
+    },
+    close: {
+      marginLeft: 12,
+      marginTop: 12,
+      cursor: 'default',
+    },
+    close_icon:{
+      width: 24,
+      height: 24,
+      flex: 0,
+      opacity: 0.6,
+    },
+    relay_status: {
+      width: 16,
+      height: 16,
+      borderRadius: 8,
+      marginLeft: 8,
+      marginRight: 8,
+    },
+    relay_status__inactive: {
+      backgroundColor: colors.red95,
+    },
+    relay_status__active: {
+      backgroundColor: colors.green90,
+    },
+    country: {
+      flexDirection: 'column',
+      flex: 0,
+    },
+    collapse_button: {
+      height: 24,
+      width: 24,
+      alignSelf: 'flex-end',
+    },
+    sub_cell: {
+      paddingLeft: 40,
+      backgroundColor: colors.blue40,
+    },
+    sub_cell__selected: {
+      paddingLeft: 40,
+      backgroundColor: colors.green,
+    },
+    cell_selected: {
+      backgroundColor: colors.green,
+    },
+  }),
+  ...createTextStyles({
+    title: {
+      fontFamily: 'DINPro',
+      fontSize: 32,
+      fontWeight: '900',
+      lineHeight: 40,
+      color: colors.white,
+      paddingTop: 16,
+      paddingLeft: 24,
+      paddingRight: 24,
+    },
+    subtitle: {
+      fontFamily: 'Open Sans',
+      fontSize: 13,
+      fontWeight: '600',
+      color: colors.white60,
+      letterSpacing: -0.2,
+      paddingTop: 0,
+      paddingLeft: 24,
+      paddingRight: 24,
+      paddingBottom: 24,
+      flex: 0,
+    },
+  }),
+};
