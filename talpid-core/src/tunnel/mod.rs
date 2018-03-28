f0dc731667de8a58fa3238ccb4b69d71711c0dc2
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-03-28 15:15:53 -0300

    Cancel connection when authentication fails

--- talpid-core/src/tunnel/mod.rs
+++ talpid-core/src/tunnel/mod.rs
@@ -52,6 +52,8 @@ pub use self::errors::*;
 /// Possible events from the VPN tunnel and the child process managing it.
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum TunnelEvent {
+    /// Sent when the tunnel fails to connect due to an authentication error.
+    AuthFailed,
     /// Sent when the tunnel comes up and is ready for traffic.
     Up(TunnelMetadata),
     /// Sent when the tunnel goes down.
@@ -77,6 +79,7 @@ impl TunnelEvent {
         env: &HashMap<String, String>,
     ) -> Option<TunnelEvent> {
         match *event {
+            OpenVpnPluginEvent::AuthFailed => Some(TunnelEvent::AuthFailed),
             OpenVpnPluginEvent::Up => {
                 let interface = env.get("dev")
                     .expect("No \"dev\" in tunnel up event")
