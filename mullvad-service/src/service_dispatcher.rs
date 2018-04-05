5bdb45969fc49c96e18d1264af618e762a8363a7
Andrej Mihajlov <and@mullvad.net>
2018-04-05 13:18:04 +0200

    Refine service_dispatcher macro

--- mullvad-service/src/service_dispatcher.rs
+++ mullvad-service/src/service_dispatcher.rs
@@ -1,9 +1,17 @@
-/// Macro to generate the windows service struct that includes all what's needed to support the
-/// service's lifecycle..
+/// Macro to generate a boilerplate for Windows service.
 ///
-/// `$struct_name` - the name of structure to produce
-/// `$service_name` - the name of windows service
-/// `$service_main` - the `service_main` handler
+/// The generated struct contains a helper method to start the service control dispatcher and
+/// a private FFI callback (`service_main`) that is invoked by the dispatcher.
+///
+/// The `service_main` callback parses service arguments provided by the system
+/// and passes them with a call to `$service_main_handler`.
+///
+/// `$struct_name` - name of the struct to produce.
+/// `$service_name` - name of the windows service.
+/// `$service_main_handler` - the function that's called from `service_main`. Accepts parsed
+/// service arguments as `Vec<OsString>`. Its responsibility is to create a
+/// `ServiceControlHandler`, start processing control events and report the service status to the
+/// system.
 ///
 /// The signature for the `service_main` function is:
 ///
@@ -12,7 +20,7 @@
 /// ```
 ///
 macro_rules! define_windows_service {
-    ($struct_name:ident, $service_name:ident, $service_main:ident) => {
+    ($struct_name:ident, $service_name:ident, $service_main_handler:ident) => {
         struct $struct_name;
 
         impl $struct_name {
@@ -22,8 +30,8 @@ macro_rules! define_windows_service {
             /// until the service is stopped.
             ///
             /// Upon successful initialization, system calls the `$struct_name::service_main` in
-            /// background thread which parses raw arguments and passes them to higher level
-            /// `$service_main` handler.
+            /// background thread which parses service arguments received from the system and
+            /// passes them to higher level `$service_main_handler` handler.
             ///
             /// On failure: immediately returns an error, no threads are spawned.
             ///
@@ -31,7 +39,7 @@ macro_rules! define_windows_service {
                 use winapi::um::winsvc;
 
                 let service_name =
-                    unsafe { widestring::WideCString::from_str_unchecked($service_name) };
+                    unsafe { ::widestring::WideCString::from_str_unchecked($service_name) };
                 let service_table: &[winsvc::SERVICE_TABLE_ENTRYW] = &[
                     winsvc::SERVICE_TABLE_ENTRYW {
                         lpServiceName: service_name.as_ptr(),
@@ -46,7 +54,7 @@ macro_rules! define_windows_service {
 
                 let result = unsafe { winsvc::StartServiceCtrlDispatcherW(service_table.as_ptr()) };
                 if result == 0 {
-                    Err(io::Error::last_os_error())
+                    Err(::std::io::Error::last_os_error())
                 } else {
                     Ok(())
                 }
@@ -62,22 +70,23 @@ macro_rules! define_windows_service {
             extern "system" fn service_main(argc: u32, argv: *mut *mut u16) {
                 let arguments = unsafe { Self::parse_raw_arguments(argc, argv) };
 
-                $service_main(arguments);
+                $service_main_handler(arguments);
             }
 
-            /// Parse an unsafe array of raw string pointers received in `service_main` from system.
+            /// Parse an unsafe array of raw string pointers received in
+            /// `$struct_name::service_main` from the system.
             unsafe fn parse_raw_arguments(
                 argc: u32,
                 argv: *mut *mut u16,
-            ) -> Vec<std::ffi::OsString> {
+            ) -> Vec<::std::ffi::OsString> {
                 (0..argc)
                     .into_iter()
                     .map(|i| {
                         let array_element_ptr: *mut *mut u16 = argv.offset(i as isize);
-                        widestring::WideCStr::from_ptr_str(*array_element_ptr).to_os_string()
+                        ::widestring::WideCStr::from_ptr_str(*array_element_ptr).to_os_string()
                     })
                     .collect()
             }
         }
-    };
+    }
 }
