db6dba4bf601c949587c20f852ac938fb560271b
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-04-12 10:52:41 -0300

    Configure DNS in a separate thread

--- talpid-core/src/firewall/linux/dns.rs
+++ talpid-core/src/firewall/linux/dns.rs
@@ -3,11 +3,17 @@ extern crate resolv_conf;
 use std::fs::File;
 use std::io::{self, Read, Write};
 use std::net::IpAddr;
+use std::sync::mpsc;
+use std::thread;
 
 use self::resolv_conf::{Config, ScopedIp};
 
 error_chain!{
     errors {
+        ConfiguratorStopped {
+            description("DNS configurator thread has unexpectedly stopped")
+        }
+
         ParseResolvConf {
             description("failed to parse contents of /etc/resolv.conf")
         }
@@ -20,6 +26,89 @@ error_chain!{
             description("failed to write to /etc/resolv.conf")
         }
     }
+}
+
+pub struct DnsSettings {
+    configurator: mpsc::Sender<DnsEvent>,
+}
+
+impl DnsSettings {
+    pub fn new() -> Result<Self> {
+        let (event_tx, event_rx) = mpsc::channel();
+
+        Self::spawn_configurator_thread(event_rx)?;
+
+        Ok(DnsSettings {
+            configurator: event_tx,
+        })
+    }
+
+    pub fn set_dns(&mut self, servers: Vec<IpAddr>) -> Result<()> {
+        let (result_tx, result_rx) = mpsc::channel();
+
+        self.send_to_configurator(DnsEvent::Set(servers, result_tx))?;
+
+        Self::receive_from_configurator(result_rx)
+    }
+
+    pub fn reset(&mut self) -> Result<()> {
+        let (result_tx, result_rx) = mpsc::channel();
+
+        self.send_to_configurator(DnsEvent::Reset(result_tx))?;
+
+        Self::receive_from_configurator(result_rx)
+    }
+
+    fn spawn_configurator_thread(events: mpsc::Receiver<DnsEvent>) -> Result<()> {
+        let (result_tx, result_rx) = mpsc::channel();
+
+        thread::spawn(move || match DnsConfigurator::new() {
+            Ok(configurator) => {
+                if result_tx.send(Ok(())).is_ok() {
+                    Self::run_configurator_thread(events, configurator);
+                }
+            }
+            Err(error) => {
+                let _ = result_tx.send(Err(error));
+            }
+        });
+
+        Self::receive_from_configurator(result_rx)
+    }
+
+    fn run_configurator_thread(
+        events: mpsc::Receiver<DnsEvent>,
+        mut configurator: DnsConfigurator,
+    ) {
+        for event in events {
+            match event {
+                DnsEvent::Set(servers, reply) => {
+                    let _ = reply.send(configurator.set_dns(servers));
+                }
+                DnsEvent::Reset(reply) => {
+                    let _ = reply.send(configurator.reset());
+                }
+            };
+        }
+    }
+
+    fn send_to_configurator(&mut self, event: DnsEvent) -> Result<()> {
+        self.configurator
+            .send(event)
+            .chain_err(|| ErrorKind::ConfiguratorStopped)
+    }
+
+    fn receive_from_configurator(result: mpsc::Receiver<Result<()>>) -> Result<()> {
+        result
+            .recv()
+            .chain_err(|| ErrorKind::ConfiguratorStopped)
+            .and_then(|result| result)
+    }
+}
+
+enum DnsEvent {
+    Set(Vec<IpAddr>, mpsc::Sender<Result<()>>),
+    Reset(mpsc::Sender<Result<()>>),
 }
 
 struct State {
@@ -40,16 +129,16 @@ impl State {
     }
 }
 
-pub struct DnsSettings {
+struct DnsConfigurator {
     state: Option<State>,
 }
 
-impl DnsSettings {
-    pub fn new() -> Result<Self> {
-        Ok(DnsSettings { state: None })
+impl DnsConfigurator {
+    fn new() -> Result<Self> {
+        Ok(DnsConfigurator { state: None })
     }
 
-    pub fn set_dns(&mut self, servers: Vec<IpAddr>) -> Result<()> {
+    fn set_dns(&mut self, servers: Vec<IpAddr>) -> Result<()> {
         let new_state = match self.state.take() {
             None => State {
                 backup: read_config()?,
@@ -68,7 +157,7 @@ impl DnsSettings {
         write_config(&new_config)
     }
 
-    pub fn reset(&mut self) -> Result<()> {
+    fn reset(&mut self) -> Result<()> {
         if let Some(state) = self.state.take() {
             write_config(&state.backup)
         } else {
