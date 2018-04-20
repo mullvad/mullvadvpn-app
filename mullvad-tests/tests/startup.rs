6fbd20f18c0d6753f45e7984e68c1282052e6d51
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-04-19 21:09:07 -0300

    Test if daemon starts in not connected state

--- mullvad-tests/tests/startup.rs
+++ mullvad-tests/tests/startup.rs
@@ -2,12 +2,14 @@
 
 extern crate mullvad_paths;
 extern crate mullvad_tests;
+extern crate mullvad_types;
 
 use std::fs::{self, Metadata};
 use std::io;
 use std::time::Duration;
 
 use mullvad_tests::DaemonRunner;
+use mullvad_types::states::{DaemonState, SecurityState, TargetState};
 
 use platform_specific::*;
 
@@ -32,6 +34,20 @@ fn rpc_info_file_permissions() {
     ensure_only_admin_can_write(
         fs::metadata(&rpc_file).expect("Failed to read RPC address file metadata"),
     );
+}
+
+#[test]
+fn starts_in_not_connected_state() {
+    let mut daemon = DaemonRunner::spawn();
+    let mut rpc_client = daemon.rpc_client().expect("Failed to create RPC client");
+
+    let state = rpc_client.get_state().expect("Failed to read daemon state");
+    let not_connected_state = DaemonState {
+        state: SecurityState::Unsecured,
+        target_state: TargetState::Unsecured,
+    };
+
+    assert_eq!(state, not_connected_state);
 }
 
 #[cfg(unix)]
