240ba6103e854e5b235a441aead57e342d2c69b6
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-04-28 09:01:58 -0300

    Test if account token is updated

--- mullvad-tests/tests/account.rs
+++ mullvad-tests/tests/account.rs
@@ -26,6 +26,41 @@ fn uses_account_token() {
     assert_eq!(account_token_sent_to_plugin, specified_account);
 }
 
+#[test]
+fn uses_updated_account_token() {
+    let mut daemon = DaemonRunner::spawn();
+    let mut rpc_client = daemon.rpc_client().unwrap();
+    let openvpn_args_file = daemon.mock_openvpn_args_file();
+    let mut openvpn_args_file_events = PathWatcher::watch(&openvpn_args_file).unwrap();
+
+    let first_account_specified = "123456";
+    rpc_client
+        .set_account(Some(first_account_specified.to_owned()))
+        .unwrap();
+    rpc_client.connect().unwrap();
+    assert_eq!(openvpn_args_file_events.next(), Some(watch_event::CREATE));
+
+    let second_account_specified = "654321";
+    rpc_client
+        .set_account(Some(second_account_specified.to_owned()))
+        .unwrap();
+
+    let mut openvpn_args_file_events = openvpn_args_file_events
+        .skip_while(|&event| event == watch_event::WRITE || event == watch_event::CLOSE_WRITE);
+
+    assert_eq!(openvpn_args_file_events.next(), Some(watch_event::REMOVE));
+    assert_eq!(openvpn_args_file_events.next(), Some(watch_event::CREATE));
+    assert!(
+        openvpn_args_file_events
+            .find(|&event| event == watch_event::CLOSE_WRITE)
+            .is_some()
+    );
+
+    let account_token_sent_to_plugin = read_account_token(openvpn_args_file).unwrap();
+
+    assert_eq!(account_token_sent_to_plugin, second_account_specified);
+}
+
 fn read_account_token<P: AsRef<Path>>(openvpn_args_file_path: P) -> Result<String, String> {
     let args_file_path = openvpn_args_file_path.as_ref();
 
