02f76355d339166d259c60420358e35553753c35
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-05-04 15:29:04 -0300

    Borrow server ID instead of moving it

--- talpid-ipc/tests/ipc-client-server.rs
+++ talpid-ipc/tests/ipc-client-server.rs
@@ -36,7 +36,7 @@ fn can_call_rpcs_on_server() {
 
     let (server, rx) = create_server();
     let server_id = server.address().to_owned();
-    let mut client = create_client(server_id);
+    let mut client = create_client(&server_id);
 
     let _result: () = client.call("foo", &[97]).unwrap();
     assert_eq!(Ok(97), rx.recv_timeout(Duration::from_millis(500)));
@@ -51,12 +51,12 @@ fn can_call_rpcs_on_server() {
 #[test]
 #[should_panic]
 fn ipc_client_invalid_url() {
-    create_client("INVALID ID".to_owned());
+    create_client(&"INVALID ID".to_owned());
 }
 
 #[test]
 fn ipc_client_bad_connection() {
-    let mut client = create_client("ws://127.0.0.1:9876".to_owned());
+    let mut client = create_client(&"ws://127.0.0.1:9876".to_owned());
     let result: Result<(), _> = client.call("invalid_method", &[0]);
     assert_matches!(result, Err(_));
 }
@@ -71,6 +71,6 @@ fn create_server() -> (talpid_ipc::IpcServer, mpsc::Receiver<i64>) {
     (server, rx)
 }
 
-fn create_client(id: talpid_ipc::IpcServerId) -> talpid_ipc::WsIpcClient {
+fn create_client(id: &talpid_ipc::IpcServerId) -> talpid_ipc::WsIpcClient {
     talpid_ipc::WsIpcClient::connect(id).unwrap()
 }
