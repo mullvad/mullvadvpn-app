11c6597e5c1f103b47bc178d0dea9aa7fb7d8a2b
Emīls Piņķis <emils@mullvad.net>
2018-05-15 14:45:28 +0100

    Change uninstall script behaviour depending on package management action

--- linux/uninstall_script.sh
+++ linux/uninstall_script.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
-set -ux
-systemctl stop mullvad-daemon.service
-systemctl disable mullvad-daemon.service
-exit 0
+set -eu
+function remove_systemd_unit {
+  systemctl stop mullvad-daemon.service
+  systemctl disable mullvad-daemon.service
+}
+
+function remove_logs_and_cache {
+  rm -rf /var/log/mullvad-daemon/
+  rm -rf /var/cache/mullvad-daemon/
+}
+
+function remove_config {
+  rm -rf /etc/mullvad-daemon
+}
+
+# checking what kind of an action is taking place
+case $@ in
+  # apt purge passes "purge"
+  "purge")
+    remove_logs_and_cache
+    remove_config
+    ;;
+  # apt remove passes "remove"
+  "remove")
+    remove_systemd_unit
+    ;;
+  # yum remove passes a 0
+  "0")
+    remove_logs_and_cache
+    remove_systemd_unit
+    remove_config
+    ;;
+esac
