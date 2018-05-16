d744a1a99034ea031adbc932f3ac4926a18fe8fb
Emīls Piņķis <emils@mullvad.net>
2018-05-16 09:25:13 +0100

    Ensure the uninstall script doesn't fail

--- linux/uninstall_script.sh
+++ linux/uninstall_script.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 set -eu
+
 function remove_systemd_unit {
-  systemctl stop mullvad-daemon.service
-  systemctl disable mullvad-daemon.service
+  systemctl stop mullvad-daemon.service || \
+    echo "Failed to stop mullvad-daemon service"
+  systemctl disable mullvad-daemon.service || \
+    echo "Failed to disable mullvad-daemon service"
 }
 
 function remove_logs_and_cache {
-  rm -rf /var/log/mullvad-daemon/
-  rm -rf /var/cache/mullvad-daemon/
+  rm -rf /var/log/mullvad-daemon/ || \
+    echo "Failed to remove mullvad-daemon logs"
+  rm -rf /var/cache/mullvad-daemon/ || \
+    echo "Failed to remove mullvad-daemon cache"
 }
 
 function remove_config {
-  rm -rf /etc/mullvad-daemon
+  rm -rf /etc/mullvad-daemon || \
+    echo "Failed to remove mullvad-daemon config"
 }
 
 # checking what kind of an action is taking place
