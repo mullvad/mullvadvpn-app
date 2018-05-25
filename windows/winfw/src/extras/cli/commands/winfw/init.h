32e1bd67dd67652c89762a327e44ba3d47780c48
Odd Stranne <odd@mullvad.net>
2018-05-25 11:22:01 +0200

    Move wfpctl -> windows/winfw

---
+++ windows/winfw/src/extras/cli/commands/winfw/init.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "cli/commands/icommand.h"
+#include "cli/util.h"
+#include "winfw/winfw.h"
+
+namespace commands::winfw
+{
+
+class Init : public ICommand
+{
+public:
+
+	Init(MessageSink messageSink);
+
+	std::wstring name() override;
+	std::wstring description() override;
+
+	void handleRequest(const std::vector<std::wstring> &arguments) override;
+
+private:
+
+	MessageSink m_messageSink;
+
+	static void WINFW_API ErrorForwarder(const char *errorMessage, void *context);
+};
+
+}
