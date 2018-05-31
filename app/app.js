4a0e02b6b60bc575fd50f4eb805c508bc2217f1f
Andrej Mihajlov <and@mullvad.net>
2018-05-31 10:38:23 +0200

    Update electron to v2.0.2

--- app/app.js
+++ app/app.js
@@ -104,7 +104,7 @@ updateTrayIcon();
 //////////////////////////////////////////////////////////////////////////
 
 // disable smart pinch.
-webFrame.setZoomLevelLimits(1, 1);
+webFrame.setVisualZoomLevelLimits(1, 1);
 
 ipcRenderer.send('on-browser-window-ready');
 
