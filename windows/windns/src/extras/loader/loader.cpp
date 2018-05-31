464f0e1788c4143ad8ed95c70cba3ca360c4faea
Odd Stranne <odd@mullvad.net>
2018-05-31 23:20:39 +0200

    Update WINDNS testing project with config sink

--- windows/windns/src/extras/loader/loader.cpp
+++ windows/windns/src/extras/loader/loader.cpp
@@ -8,6 +8,11 @@ void WINDNS_API ErrorSink(const char *errorMessage, void *context)
 	std::cout << "Error: " << errorMessage << std::endl;
 }
 
+void WINDNS_API ConfigSink(const void *configData, uint32_t dataLength, void *context)
+{
+	std::wcout << L"Updated config was delivered to WINDNS client code" << std::endl;
+}
+
 int main()
 {
 	std::wcout << L"Init: " << WinDns_Initialize(ErrorSink, nullptr) << std::endl;
@@ -18,14 +23,14 @@ int main()
 		L"1.1.1.1"
 	};
 
-	std::wcout << L"Set: " << WinDns_Set(servers, _countof(servers)) << std::endl;
+	std::wcout << L"Set: " << WinDns_Set(servers, _countof(servers), ConfigSink, nullptr) << std::endl;
 
 	std::wcout << L"Press a key to abort DNS monitoring + enforcing..." << std::endl;
 	_getwch();
 
 	std::wcout << L"Reset: " << WinDns_Reset() << std::endl;
 
-	std::wcout << L"Set: " << WinDns_Set(servers, _countof(servers)) << std::endl;
+	std::wcout << L"Set: " << WinDns_Set(servers, _countof(servers), ConfigSink, nullptr) << std::endl;
 
 	std::wcout << L"Press a key to abort DNS monitoring + enforcing..." << std::endl;
 	_getwch();
