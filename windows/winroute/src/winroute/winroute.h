dac8674cf2c3f7d8d828017054ea88b2b4089dcf
Emīls <emils@mullvad.net>
2018-06-12 17:09:10 +0100

    Add NetworkInterfaces class

--- windows/winroute/src/winroute/winroute.h
+++ windows/winroute/src/winroute/winroute.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <cstdint>
+
+//
+// WINFW public API
+//
+
+#ifdef WINROUTE_EXPORTS
+#define WINROUTE_LINKAGE __declspec(dllexport)
+#else
+#define WINROUTE_LINKAGE __declspec(dllimport)
+#endif
+
+#define WINROUTE_API __stdcall
+
+// Callback for logging.
+typedef void (WINROUTE_API *WinRouteErrorSink)(const char *errorMessage, void *context);
+
+
+extern "C"
+WINROUTE_LINKAGE
+bool
+WINROUTE_API
+WinRoute_EnsureTopMetric(
+		const wchar_t *deviceAlias,
+		WinRouteErrorSink errorSink,
+	    void* errorSinkContext
+);
