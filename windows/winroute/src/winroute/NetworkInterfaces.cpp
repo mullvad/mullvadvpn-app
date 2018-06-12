1f2d15b9922215204077850f8d5124c463829d23
Emīls <emils@mullvad.net>
2018-06-12 17:09:10 +0100

    Add NetworkInterfaces class

---
+++ windows/winroute/src/winroute/NetworkInterfaces.cpp
@@ -0,0 +1,135 @@
+#include "stdafx.h"
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2ipdef.h>
+#include <iphlpapi.h>
+#include <netioapi.h>
+
+#include "NetworkInterfaces.h"
+
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <cstdint>
+
+#include <libcommon/string.h>
+
+
+
+
+
+PMIB_IPINTERFACE_ROW NetworkInterfaces::RowByLuid(NET_LUID rowId)
+{
+	for (int i = 0; i < (int)mInterfaces->NumEntries; ++i)
+	{
+		if (mInterfaces->Table[i].InterfaceLuid.Value == rowId.Value)
+		{
+			return &mInterfaces->Table[i];
+		}
+	}
+	return nullptr;
+}
+
+
+void NetworkInterfaces::EnsureIfaceMetricIsHighest(PMIB_IPINTERFACE_ROW targetIface)
+{
+	MIB_IPINTERFACE_ROW *iface;
+	DWORD success = 0;
+	for (int i = 0; i < (int)mInterfaces->NumEntries; ++i)
+	{
+		iface = &mInterfaces->Table[i];
+		// Ignoring the target interface.
+		if (iface->InterfaceLuid.Value == targetIface->InterfaceLuid.Value)
+		{
+			continue;
+		}
+
+		iface->UseAutomaticMetric = false;
+		iface->Metric++;
+		success = SetIpInterfaceEntry(iface);
+		if (success != NO_ERROR)
+		{
+			std::wstringstream ss;
+			ss << L"Failed to increment metric for interface with LUID "
+				<< &iface->InterfaceLuid.Value
+				<< ": "
+				<< success;
+			throw std::runtime_error(common::string::ToAnsi(ss.str()));
+		}
+
+	}
+}
+
+NetworkInterfaces::NetworkInterfaces()
+{
+	mInterfaces = nullptr;
+	DWORD success = 0;
+
+	 success = GetIpInterfaceTable(AF_UNSPEC, &mInterfaces);
+	 if (success != NO_ERROR)
+	 {
+		 std::wstringstream ss;
+		 ss << L"Failed to enumerate network interfaces: " << success;
+		 throw std::runtime_error(common::string::ToAnsi(ss.str()));
+	 }
+}
+
+bool NetworkInterfaces::SetTopMetricForInterfaceByAlias(const wchar_t * deviceAlias)
+{
+	NET_LUID targetIfaceLuid;
+	DWORD success = 0;
+	success = ConvertInterfaceAliasToLuid(deviceAlias, &targetIfaceLuid);
+	if (success != NO_ERROR)
+	{
+		std::wstringstream ss;
+		ss << L"Failed to convert interface alias '"
+			<< deviceAlias
+			<< "' into LUID: "
+			<< success;
+		throw std::runtime_error(common::string::ToAnsi(ss.str()));
+	}
+	return SetTopMetricForInterfaceWithLuid(targetIfaceLuid);
+}
+
+bool NetworkInterfaces::SetTopMetricForInterfaceWithLuid(NET_LUID targetIfaceId)
+{
+
+	DWORD success = 0;
+
+	PMIB_IPINTERFACE_ROW targetIface = RowByLuid(targetIfaceId);
+	if (targetIface == nullptr)
+	{
+		std::wstringstream ss;
+		ss << L"No interface with LUID " << targetIfaceId.Value;
+		throw std::runtime_error(common::string::ToAnsi(ss.str()));
+	}
+
+	if (targetIface->Metric == MAX_METRIC)
+	{
+		return false;
+	}
+	
+	targetIface->Metric = MAX_METRIC;
+	targetIface->UseAutomaticMetric = false;
+	success = SetIpInterfaceEntry(targetIface);
+	if (success != NO_ERROR)
+	{
+		std::wstringstream ss;
+		ss << L"Failed to set metric " 
+			<< MAX_METRIC
+			<< " for interface with LUID "
+			<< targetIfaceId.Value
+			<< ". Error code - "
+			<< success;
+		throw std::runtime_error(common::string::ToAnsi(ss.str()));
+	}
+	EnsureIfaceMetricIsHighest(targetIface);
+	return true;
+}
+
+
+NetworkInterfaces::~NetworkInterfaces()
+{
+	FreeMibTable(mInterfaces);
+}
