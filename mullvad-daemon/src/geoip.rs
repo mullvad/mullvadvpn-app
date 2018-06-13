8d3258fe6bd1eb0196b6a5507b95a50ec6c3e830
Linus Färnstrand <linus@mullvad.net>
2018-06-13 17:24:07 +0200

    TMP Support certificate pinning

--- mullvad-daemon/src/geoip.rs
+++ mullvad-daemon/src/geoip.rs
@@ -4,7 +4,7 @@ use mullvad_types::location::GeoIpLocation;
 use serde_json;
 
 
-static URI: &str = "https://am.i.mullvad.net/json";
+const URI: &str = "https://am.i.mullvad.net/json";
 
 error_chain! {
     errors {
@@ -18,12 +18,13 @@ error_chain! {
     }
 }
 
+
 pub fn send_location_request(
     request_sender: mullvad_rpc::rest::RequestSender,
 ) -> Box<Future<Item = GeoIpLocation, Error = Error>> {
     let (response_tx, response_rx) = futures::sync::oneshot::channel();
     let request = mullvad_rpc::rest::create_get_request(URI.parse().unwrap());
-    let future = futures::Sink::send(request_sender, (request, response_tx))
+    let future = futures::Sink::send(request_sender.clone(), (request, response_tx))
         .map_err(|e| Error::with_chain(e, ErrorKind::NoResponse))
         .and_then(|_| response_rx.map_err(|e| Error::with_chain(e, ErrorKind::NoResponse)))
         .and_then(|response_result| response_result.map_err(Error::from))
