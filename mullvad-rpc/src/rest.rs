891b91867a8b56616fc70b4ab982ad598bef2e54
Linus Färnstrand <linus@mullvad.net>
2018-06-13 17:24:07 +0200

    Use bundled CA

--- mullvad-rpc/src/rest.rs
+++ mullvad-rpc/src/rest.rs
@@ -1,14 +1,16 @@
+use std::path::Path;
+
 use futures::sync::{mpsc, oneshot};
 use futures::{future, Future, Stream};
 
 use hyper;
 use hyper::client::Client;
 use hyper::{Request, StatusCode, Uri};
-use hyper_tls::HttpsConnector;
-use native_tls;
+use hyper_openssl::openssl::error::ErrorStack;
 
 use tokio_core::reactor::Handle;
 
+use HttpsConnectorWithSni;
 
 error_chain! {
     errors {
@@ -19,9 +21,9 @@ error_chain! {
         }
     }
     foreign_links {
-        Tls(native_tls::Error);
         Hyper(hyper::Error) #[doc = "An error occured in Hyper."];
         Uri(hyper::error::UriError) #[doc = "The string given was not a valid URI."];
+        OpenSsl(ErrorStack) #[doc = "Error in OpenSSL"];
     }
 }
 
@@ -29,9 +31,10 @@ error_chain! {
 pub type RequestSender = mpsc::UnboundedSender<(Request, oneshot::Sender<Result<Vec<u8>>>)>;
 type RequestReceiver = mpsc::UnboundedReceiver<(Request, oneshot::Sender<Result<Vec<u8>>>)>;
 
-pub fn create_http_client(handle: &Handle) -> Result<RequestSender> {
-    let connector = HttpsConnector::new(1, handle)?;
+pub fn create_https_client<P: AsRef<Path>>(ca_path: P, handle: &Handle) -> Result<RequestSender> {
+    let connector = HttpsConnectorWithSni::new(ca_path, handle)?;
     let client = Client::configure().connector(connector).build(handle);
+
     let (request_tx, request_rx) = mpsc::unbounded();
     handle.spawn(create_request_processing_future(request_rx, client));
     Ok(request_tx)
