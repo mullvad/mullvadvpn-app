1d71326c88ef10c51547e1e35f721247b10c6f47
Emīls <emils@mullvad.net>
2018-06-13 08:53:49 +0100

    Fix windows/route.rs in talpid-core

--- talpid-core/src/firewall/windows/route.rs
+++ talpid-core/src/firewall/windows/route.rs
@@ -5,6 +5,7 @@ use self::widestring::WideCString;
 use std::ffi::CStr;
 use std::os::raw::c_char;
 use std::ptr;
+use super::ffi;
 
 error_chain!{
     errors{
@@ -15,25 +16,20 @@ error_chain!{
     }
 }
 
-pub fn ensure_metric_stuff(tunnel_alias: &str) -> Result<()> {
-    let tunnel_alias_widestring =
-        WideCString::new(tunnel_alias.encode_utf16().collect::<Vec<_>>()).unwrap();
+// Returns true if metrics were changed, false otherwise
+pub fn ensure_top_metric_for_interface(interface_alias: &str) -> Result<bool> {
+    let interface_alias_ws =
+        WideCString::new(interface_alias.encode_utf16().collect::<Vec<_>>()).unwrap();
     match unsafe {
-        ffi::WinRoute_EnsureTopMetric(
-            tunnel_alias_widestring.as_wide_c_str().as_ptr(),
-            Some(error_sink),
+        winroute::WinRoute_EnsureTopMetric(
+            interface_alias_ws.as_wide_c_str().as_ptr(),
+            Some(ffi::error_sink),
             ptr::null_mut(),
         )
     } {
-        0 => {
-            debug!("No metrics were set for network interfaces, tunnel interface already has lowest metric");
-            Ok(())
-        }
-        1 => {
-            debug!("Set new metrics for interfaces");
-            Ok(())
-        }
-        -1 => Err(Error::from(ErrorKind::MetricApplication)),
+        0 => Ok(false),
+        1 => Ok(true),
+	-1 => Err(Error::from(ErrorKind::MetricApplication)),
         _ => {
             error!("Unexpected return code from WinRoute_EnsureTopMetric");
             Err(Error::from(ErrorKind::MetricApplication))
@@ -41,28 +37,18 @@ pub fn ensure_metric_stuff(tunnel_alias: &str) -> Result<()> {
     }
 }
 
-pub type ErrorSink = extern "system" fn(msg: *const c_char, ctx: *mut libc::c_void);
-
-extern "system" fn error_sink(msg: *const c_char, _ctx: *mut libc::c_void) {
-    if msg == ptr::null() {
-        error!("log message from WinFw is NULL");
-    } else {
-        error!("{}", unsafe { CStr::from_ptr(msg).to_string_lossy() });
-    }
-}
-
-mod ffi {
+mod winroute {
     // 	const wchar_t *deviceAlias,
     // 	WinRouteErrorSink errorSink,
     // 	void* errorSinkContext
     use super::libc;
-    use super::ErrorSink;
+    use super::ffi;
 
     extern "system" {
         #[link_name(WinRoute_EnsureTopMetric)]
         pub fn WinRoute_EnsureTopMetric(
             tunnel_interface_alias: *const libc::wchar_t,
-            sink: Option<ErrorSink>,
+            sink: Option<ffi::ErrorSink>,
             sink_context: *mut libc::c_void,
         ) -> i32;
     }
