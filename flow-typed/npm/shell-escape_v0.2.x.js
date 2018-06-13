e51037e0976ea83110984eb8555603bf79f050a8
Andrej Mihajlov <and@mullvad.net>
2018-06-13 15:34:49 +0200

    Remove flow-typed annotations from repository

--- flow-typed/npm/shell-escape_v0.2.x.js
+++
@@ -1,9 +0,0 @@
-// flow-typed signature: f853bb0fb3366c34bd1b85b8d6f8d0bf
-// flow-typed version: 4213cadbe8/shell-escape_v0.2.x/flow_>=v0.58.x
-
-// https://github.com/xxorax/node-shell-escape
-// https://www.npmjs.com/package/shell-escape
-
-declare module 'shell-escape' {
-  declare module.exports: (a: Array<string>) => string;
-}
