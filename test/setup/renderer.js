69da56287e82a3dcf1b32dc35e30f312822c657d
Andrej Mihajlov <and@mullvad.net>
2018-06-14 14:28:22 +0200

    Migrate from sinon to chai-spies

--- test/setup/renderer.js
+++ test/setup/renderer.js
@@ -1,5 +1,9 @@
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
+const chai = require('chai');
+const spies = require('chai-spies');
+
+chai.use(spies);
 
 Enzyme.configure({
   adapter: new Adapter(),
