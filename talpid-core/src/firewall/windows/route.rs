0908630b12f1a93043a3be3a3a1e78256934ef81
Emīls <emils@mullvad.net>
2018-06-21 17:20:54 +0100

    Fix talpid-core/src/firewall/windows/route.rs

--- talpid-core/src/firewall/windows/route.rs
+++ talpid-core/src/firewall/windows/route.rs
@@ -1,15 +1,11 @@
-extern crate libc;
-extern crate widestring;
-
-use self::widestring::WideCString;
-use std::ffi::CStr;
-use std::os::raw::c_char;
-use std::ptr;
 use super::ffi;
+use super::widestring::WideCString;
+use libc;
+use std::ptr;
 
 error_chain!{
     errors{
-        #[doc = "Failure to set metrics of network interfaces"]
+        /// Failure to set metrics of network interfaces
         MetricApplication{
             description("Failed to set the metrics for a network interface")
         }
@@ -21,7 +17,7 @@ pub fn ensure_top_metric_for_interface(interface_alias: &str) -> Result<bool> {
     let interface_alias_ws =
         WideCString::new(interface_alias.encode_utf16().collect::<Vec<_>>()).unwrap();
     match unsafe {
-        winroute::WinRoute_EnsureTopMetric(
+        WinRoute_EnsureTopMetric(
             interface_alias_ws.as_wide_c_str().as_ptr(),
             Some(ffi::error_sink),
             ptr::null_mut(),
@@ -29,7 +25,7 @@ pub fn ensure_top_metric_for_interface(interface_alias: &str) -> Result<bool> {
     } {
         0 => Ok(false),
         1 => Ok(true),
-	-1 => Err(Error::from(ErrorKind::MetricApplication)),
+        -1 => Err(Error::from(ErrorKind::MetricApplication)),
         _ => {
             error!("Unexpected return code from WinRoute_EnsureTopMetric");
             Err(Error::from(ErrorKind::MetricApplication))
@@ -37,20 +33,11 @@ pub fn ensure_top_metric_for_interface(interface_alias: &str) -> Result<bool> {
     }
 }
 
-mod winroute {
-    // 	const wchar_t *deviceAlias,
-    // 	WinRouteErrorSink errorSink,
-    // 	void* errorSinkContext
-    use super::libc;
-    use super::ffi;
-
-    extern "system" {
-        #[link_name(WinRoute_EnsureTopMetric)]
-        pub fn WinRoute_EnsureTopMetric(
-            tunnel_interface_alias: *const libc::wchar_t,
-            sink: Option<ffi::ErrorSink>,
-            sink_context: *mut libc::c_void,
-        ) -> i32;
-    }
-
+extern "system" {
+    #[link_name(WinRoute_EnsureTopMetric)]
+    pub fn WinRoute_EnsureTopMetric(
+        tunnel_interface_alias: *const libc::wchar_t,
+        sink: Option<ffi::ErrorSink>,
+        sink_context: *mut libc::c_void,
+    ) -> i32;
 }
