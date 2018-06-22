67e731864fcec1021c4b1e4de9faee5cf92d0a25
Emīls <emils@mullvad.net>
2018-06-22 17:28:35 +0100

    Flush backup file in SystemStateWriter

--- talpid-core/src/firewall/windows/system_state.rs
+++ talpid-core/src/firewall/windows/system_state.rs
@@ -1,8 +1,8 @@
 //! A writer for a blob that would persistently store the system state. Useful
 //! for when the application of a secuirty policy proves to be persistent across
 //! reboots
-use std::fs;
-use std::io;
+use std::fs::{self, File};
+use std::io::{self, Write};
 use std::path::Path;
 
 /// This struct is responsible for saving a binary blob to disk. The binary blob is intended to
@@ -24,7 +24,7 @@ impl SystemStateWriter {
     /// Writes a binary blob representing the system state to the backup location before any
     /// security policies are applied.
     pub fn write_backup(&self, data: &[u8]) -> io::Result<()> {
-        let mut backup_file = File::create(&self.backup_path)?;
+        let mut backup_file = File::open(&self.backup_path)?;
         backup_file.write_all(data)?;
         backup_file.sync_all()
     }
