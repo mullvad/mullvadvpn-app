14cc75514579caa5cb7dbf1b48c03ed78e0d195a
Emīls Piņķis <emils@mullvad.net>
2018-07-05 10:34:46 +0100

    Add env.sh file for intializing default env vars

---
+++ env.sh
@@ -0,0 +1,22 @@
+# Sourcing this file should set up the environment to build the app
+
+SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+case "$(uname -s)" in
+  Linux*)
+    export LIBMNL_LIB_DIR="$SCRIPT_DIR/dist-assets/binaries/linux"
+    export LIBNFTNL_LIB_DIR="$SCRIPT_DIR/dist-assets/binaries/linux"
+    PLATFORM="linux"
+    ;;
+  Darwin*)
+    export MACOSX_DEPLOYMENT_TARGET="10.7"
+    PLATFORM="macos"
+    ;;
+  MINGW*)
+    PLATFORM="windows"
+    ;;
+esac
+
+export OPENSSL_STATIC="1"
+export OPENSSL_LIB_DIR="$SCRIPT_DIR/dist-assets/binaries/$PLATFORM"
+export OPENSSL_INCLUDE_DIR="$SCRIPT_DIR/dist-assets/binaries/$PLATFORM/include"
