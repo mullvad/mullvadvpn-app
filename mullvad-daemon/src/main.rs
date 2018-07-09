dbc0fb673b6c4f955097bbbc700fa10f288e4338
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-07-09 15:52:23 -0300

    Keep relays in an `Arc<Mutex<ParsedRelays>>`

--- mullvad-daemon/src/main.rs
+++ mullvad-daemon/src/main.rs
@@ -465,11 +465,7 @@ impl Daemon {
     }
 
     fn on_get_relay_locations(&mut self, tx: OneshotSender<RelayList>) {
-        Self::oneshot_send(
-            tx,
-            self.relay_selector.get_locations().clone(),
-            "relay locations",
-        );
+        Self::oneshot_send(tx, self.relay_selector.get_locations(), "relay locations");
     }
 
 
