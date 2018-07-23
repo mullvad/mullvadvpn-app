02e1d75d0d2dcef4e16e9ab6bff3415bd9b3d05d
Andrej Mihajlov <and@mullvad.net>
2018-07-23 22:09:48 +0200

    Make sure to apply the platform window mask only on mac

--- app/components/PlatformWindow.js
+++ app/components/PlatformWindow.js
@@ -2,14 +2,16 @@
 import * as React from 'react';
 import { Component, View, Styles } from 'reactxp';
 
-const platformWindowStyle = Styles.createViewStyle({
-  WebkitMask: `
-    url(./assets/images/app-triangle.svg) 50% 0% no-repeat,
-    url(./assets/images/app-header-backdrop.svg) no-repeat`,
-});
+const styles = {
+  darwin: Styles.createViewStyle({
+    WebkitMask: `
+      url(./assets/images/app-triangle.svg) 50% 0% no-repeat,
+      url(./assets/images/app-header-backdrop.svg) no-repeat`,
+  }),
+};
 
 export default class PlatformWindow extends Component {
   render() {
-    return <View style={platformWindowStyle}>{this.props.children}</View>;
+    return <View style={styles[process.platform]}>{this.props.children}</View>;
   }
 }
