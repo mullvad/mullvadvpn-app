33fef23d43fc560159e62ca01ec1c42ea5af7445
Linus Färnstrand <linus@mullvad.net>
2018-07-31 15:00:52 +0200

    use dirs instead of app_dirs

--- mullvad-paths/src/settings.rs
+++ mullvad-paths/src/settings.rs
@@ -1,33 +1,28 @@
-use {ErrorKind, Result, ResultExt};
+use Result;
 
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 /// Creates and returns the settings directory pointed to by `MULLVAD_SETTINGS_DIR`, or the default
 /// one if that variable is unset.
 pub fn settings_dir() -> Result<PathBuf> {
-    let dir = get_settings_dir()?;
-    fs::create_dir_all(&dir).chain_err(|| ErrorKind::CreateDirFailed(dir.clone()))?;
-    Ok(dir)
+    ::create_and_return(get_settings_dir)
 }
 
 fn get_settings_dir() -> Result<PathBuf> {
     match env::var_os("MULLVAD_SETTINGS_DIR") {
         Some(path) => Ok(PathBuf::from(path)),
-        None => get_default_settings_dir(),
+        None => get_default_settings_dir().map(|dir| dir.join(::PRODUCT_NAME)),
     }
 }
 
-#[cfg(unix)]
 fn get_default_settings_dir() -> Result<PathBuf> {
-    Ok(PathBuf::from("/etc/mullvad-daemon"))
-}
-
-#[cfg(windows)]
-fn get_default_settings_dir() -> Result<PathBuf> {
-    Ok(::app_dirs::get_app_root(
-        ::app_dirs::AppDataType::UserData,
-        &::metadata::APP_INFO,
-    )?)
+    #[cfg(unix)]
+    {
+        Ok(PathBuf::from("/etc"))
+    }
+    #[cfg(windows)]
+    {
+        ::dirs::data_local_dir().ok_or_else(|| ::ErrorKind::FindDirError.into())
+    }
 }
