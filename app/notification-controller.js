b7fccae454943a113022fe4403ca9a6920ffec13
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-08-01 17:32:52 -0300

    Implement system notifications of connection state

---
+++ app/notification-controller.js
@@ -0,0 +1,25 @@
+// @flow
+import { remote } from 'electron';
+
+export default class NotificationController {
+  _activeNotification: ?Notification;
+
+  show(message: string) {
+    const lastNotification = this._activeNotification;
+    const newNotification = new Notification(remote.app.getName(), { body: message, silent: true });
+
+    this._activeNotification = newNotification;
+
+    newNotification.addEventListener('show', () => {
+      // If the notification is closed too soon, it might still get shown. If that happens, close()
+      // should be called again so that it is closed immediately.
+      if (this._activeNotification !== newNotification) {
+        newNotification.close();
+      }
+    });
+
+    if (lastNotification) {
+      lastNotification.close();
+    }
+  }
+}
