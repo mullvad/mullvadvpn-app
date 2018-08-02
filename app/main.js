cce1eca23e8b847aac5e09c5fd732739931f84b2
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-08-02 14:33:43 -0300

    Set application user model ID on Windows

--- app/main.js
+++ app/main.js
@@ -33,6 +33,10 @@ const ApplicationMain = {
 
     log.info(`Running version ${app.getVersion()}`);
 
+    if (process.platform === 'win32') {
+      app.setAppUserModelId('net.mullvad.vpn');
+    }
+
     app.on('activate', () => this._onActivate());
     app.on('ready', () => this._onReady());
     app.on('window-all-closed', () => app.quit());
