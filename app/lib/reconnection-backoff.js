b044dc9cd6adc1a4a5e392944e0263f89a6f8dc8
Andrej Mihajlov <and@mullvad.net>
2018-08-08 14:33:02 +0200

    Fix flow warnings

--- app/lib/reconnection-backoff.js
+++ app/lib/reconnection-backoff.js
@@ -1,13 +1,11 @@
+// @flow
+
 /*
  * Used to calculate the time to wait before reconnecting to the daemon.
  * It uses a linear backoff function that goes from 500ms to 3000ms.
  */
 export default class ReconnectionBackoff {
-  _attempt: number;
-
-  constructor() {
-    this._attempt = 0;
-  }
+  _attempt = 0;
 
   attempt(handler: () => void) {
     setTimeout(handler, this._getIncreasedBackoff());
