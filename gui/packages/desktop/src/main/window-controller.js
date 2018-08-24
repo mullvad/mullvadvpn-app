53e2417c3ea6ab03e494bb23d4e47e57dfee15a7
Andrej Mihajlov <and@mullvad.net>
2018-08-24 16:08:34 +0300

    Detect the current active screen based tray icon location

--- gui/packages/desktop/src/main/window-controller.js
+++ gui/packages/desktop/src/main/window-controller.js
@@ -84,8 +84,11 @@ export default class WindowController {
     const windowBounds = this._window.getBounds();
     const trayBounds = this._tray.getBounds();
 
-    const primaryDisplay = screen.getPrimaryDisplay();
-    const workArea = primaryDisplay.workArea;
+    const activeDisplay = screen.getDisplayNearestPoint({
+      x: trayBounds.x,
+      y: trayBounds.y,
+    });
+    const workArea = activeDisplay.workArea;
     const placement = this._getTrayPlacement();
     const maxX = workArea.x + workArea.width - windowBounds.width;
     const maxY = workArea.y + workArea.height - windowBounds.height;
