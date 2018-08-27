adab60630fb5a6944789524bdbc72da9e71cd612
Emīls Piņķis <emils@mullvad.net>
2018-08-27 14:58:08 +0100

    Replace address file with socket path in mullvad-paths

--- mullvad-paths/src/rpc_address.rs
+++ mullvad-paths/src/rpc_address.rs
@@ -1,24 +1,12 @@
-use Result;
-
 use std::env;
 use std::path::PathBuf;
 
-const RPC_ADDRESS_FILENAME: &str = ".mullvad_rpc_address";
-
-pub fn get_rpc_address_path() -> Result<PathBuf> {
-    match env::var_os("MULLVAD_RPC_ADDRESS_PATH") {
-        Some(path) => Ok(PathBuf::from(path)),
-        None => get_default_rpc_address_dir().map(|dir| dir.join(RPC_ADDRESS_FILENAME)),
-    }
-}
-
-fn get_default_rpc_address_dir() -> Result<PathBuf> {
-    #[cfg(unix)]
-    {
-        Ok(PathBuf::from("/tmp"))
-    }
-    #[cfg(windows)]
-    {
-        ::get_allusersprofile_dir().map(|dir| dir.join(::PRODUCT_NAME))
+pub fn get_rpc_socket_path() -> PathBuf {
+    match env::var_os("MULLVAD_RPC_SOCKET_PATH") {
+        Some(path) => PathBuf::from(path),
+        #[cfg(unix)]
+        None => PathBuf::from("/var/run/mullvad-vpn"),
+        #[cfg(windows)]
+        None => PathBuf::from("//./pipe/Mullvad VPN"),
     }
 }
