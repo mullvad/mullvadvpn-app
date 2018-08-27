2116ada52b499a158c29ea88da8b5f5d693c3ff2
Emīls Piņķis <emils@mullvad.net>
2018-08-27 16:15:02 +0100

    Adjust frontend to use IPC rather than WebSockets

--- gui/packages/desktop/src/renderer/lib/daemon-rpc.js
+++ gui/packages/desktop/src/renderer/lib/daemon-rpc.js
@@ -1,8 +1,9 @@
 // @flow
 
-import JsonRpcTransport, {
+import JsonRpcCliet, {
   RemoteError as JsonRpcRemoteError,
   TimeOutError as JsonRpcTimeOutError,
+  SocketTransport,
 } from './jsonrpc-transport';
 import { CommunicationError, InvalidAccountError, NoDaemonError } from '../errors';
 
@@ -222,7 +223,7 @@ const AppVersionInfoSchema = object({
 });
 
 export interface DaemonRpcProtocol {
-  connect(string): void;
+  connect(Object): void;
   disconnect(): void;
   getAccountData(AccountToken): Promise<AccountData>;
   getRelayLocations(): Promise<RelayList>;
@@ -268,14 +269,14 @@ export type ConnectionObserver = {
 };
 
 export class DaemonRpc implements DaemonRpcProtocol {
-  _transport = new JsonRpcTransport();
+  _transport = new JsonRpcCliet(new SocketTransport());
 
   async authenticate(sharedSecret: string): Promise<void> {
     await this._transport.send('auth', sharedSecret);
   }
 
-  connect(connectionString: string) {
-    this._transport.connect(connectionString);
+  connect(connectionParams: { path: string }) {
+    this._transport.connect(connectionParams);
   }
 
   disconnect() {
