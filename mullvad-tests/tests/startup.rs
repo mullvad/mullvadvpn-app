31251821ce3ae6d5d09c7d68cf526e28c99a0d35
Emīls Piņķis <emils@mullvad.net>
2018-08-27 14:48:07 +0100

    Fix mullvad-tests

--- mullvad-tests/tests/startup.rs
+++ mullvad-tests/tests/startup.rs
@@ -4,38 +4,8 @@ extern crate mullvad_paths;
 extern crate mullvad_tests;
 extern crate mullvad_types;
 
-use std::fs::{self, Metadata};
-use std::io;
-use std::time::Duration;
-
-use mullvad_tests::{DaemonRunner, PathWatcher};
+use mullvad_tests::DaemonRunner;
 use mullvad_types::states::{DaemonState, SecurityState, TargetState};
-
-use platform_specific::*;
-
-#[test]
-fn rpc_info_file_permissions() {
-    let rpc_file = mullvad_paths::get_rpc_address_path().unwrap();
-
-    if let Err(error) = fs::remove_file(&rpc_file) {
-        if error.kind() != io::ErrorKind::NotFound {
-            panic!("failed to remove existing RPC address file");
-        }
-    }
-
-    assert!(!rpc_file.exists());
-
-    let mut rpc_file_watcher = PathWatcher::watch(&rpc_file).unwrap();
-    let _daemon = DaemonRunner::spawn_with_real_rpc_address_file();
-
-    rpc_file_watcher.wait_for_burst_of_events(Duration::from_secs(10));
-
-    assert!(rpc_file.exists());
-
-    ensure_only_admin_can_write(
-        fs::metadata(&rpc_file).expect("Failed to read RPC address file metadata"),
-    );
-}
 
 #[test]
 fn starts_in_not_connected_state() {
@@ -49,27 +19,4 @@ fn starts_in_not_connected_state() {
     };
 
     assert_eq!(state, not_connected_state);
-}
-
-#[cfg(unix)]
-mod platform_specific {
-    extern crate libc;
-
-    use super::*;
-    use std::os::unix::fs::MetadataExt;
-
-    pub fn ensure_only_admin_can_write(metadata: Metadata) {
-        let process_uid = unsafe { libc::getuid() };
-        assert_eq!(metadata.uid(), process_uid);
-        assert_eq!(metadata.mode() & 0o022, 0);
-    }
-}
-
-#[cfg(not(unix))]
-mod platform_specific {
-    use super::*;
-
-    pub fn ensure_only_admin_can_write(_metadata: Metadata) {
-        // TODO: Test when correctly implemented on Windows
-    }
 }
