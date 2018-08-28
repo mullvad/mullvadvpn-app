174d10e194247d211bc4d4fb707d4e5aeceac366
Emīls Piņķis <emils@mullvad.net>
2018-08-28 23:37:53 +0100

    Change rpc path in mullvad-paths

--- mullvad-paths/src/rpc_address.rs
+++ mullvad-paths/src/rpc_address.rs
@@ -5,8 +5,8 @@ pub fn get_rpc_socket_path() -> PathBuf {
     match env::var_os("MULLVAD_RPC_SOCKET_PATH") {
         Some(path) => PathBuf::from(path),
         #[cfg(unix)]
-        None => PathBuf::from("/tmp/mullvad_rpc_socket"),
+        None => PathBuf::from("/var/run/mullvad-vpn"),
         #[cfg(windows)]
-        None => PathBuf::from("//./pipe/mullvad_rpc_socket"),
+        None => PathBuf::from("//./pipe/Mullvad VPN"),
     }
 }
