0c73901f75bf938c0b842d12eed4ef48a8cfd27c
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-08-30 15:11:17 -0300

    Block if security policy can't be set

--- talpid-types/src/tunnel.rs
+++ talpid-types/src/tunnel.rs
@@ -21,6 +21,8 @@ pub enum TunnelStateTransition {
 #[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum BlockReason {
+    /// Failed to set security policy
+    SetSecurityPolicyError,
     /// Failed to start connection to remote server
     StartTunnelError,
 }
@@ -28,6 +30,7 @@ pub enum BlockReason {
 impl fmt::Display for BlockReason {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         let description = match *self {
+            BlockReason::SetSecurityPolicyError => "Failed to set security policy",
             BlockReason::StartTunnelError => "Failed to start connection to remote server",
         };
 
