ae940cae50f92b7f7287e47d00ee58c664edea03
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-08-31 16:01:05 -0300

    Stop and disable service before files are removed

--- dist-assets/linux/after-remove.sh
+++ dist-assets/linux/after-remove.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-function remove_systemd_unit {
-  systemctl stop mullvad-daemon.service || \
-    echo "Failed to stop mullvad-daemon service"
-  systemctl disable mullvad-daemon.service || \
-    echo "Failed to disable mullvad-daemon service"
-}
-
 function remove_logs_and_cache {
   rm -rf /var/log/mullvad-vpn/ || \
     echo "Failed to remove mullvad-vpn logs"
@@ -29,12 +22,10 @@ case $@ in
     ;;
   # apt remove passes "remove"
   "remove")
-    remove_systemd_unit
     ;;
   # yum remove passes a 0
   "0")
     remove_logs_and_cache
-    remove_systemd_unit
     remove_config
     ;;
 esac
