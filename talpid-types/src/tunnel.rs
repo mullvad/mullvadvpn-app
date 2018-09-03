0772073fe5bfb0f17a50899686eaab30d3d6459e
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-09-03 16:59:11 -0300

    Add authentication failure reason

--- talpid-types/src/tunnel.rs
+++ talpid-types/src/tunnel.rs
@@ -29,9 +29,10 @@ impl TunnelStateTransition {
 /// Reason for entering the blocked state.
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
+#[serde(tag = "reason", content = "details")]
 pub enum BlockReason {
     /// Authentication with remote server failed.
-    AuthFailed,
+    AuthFailed(Option<String>),
     /// Failed to configure IPv6 because it's disabled in the platform.
     Ipv6Unavailable,
     /// Failed to set security policy.
@@ -45,7 +46,16 @@ pub enum BlockReason {
 impl fmt::Display for BlockReason {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         let description = match *self {
-            BlockReason::AuthFailed => "Authentication with remote server failed",
+            BlockReason::AuthFailed(ref reason) => {
+                return write!(
+                    formatter,
+                    "Authentication with remote server failed: {}",
+                    match reason {
+                        Some(ref reason) => reason.as_str(),
+                        None => "No reason provided",
+                    }
+                );
+            }
             BlockReason::Ipv6Unavailable => {
                 "Failed to configure IPv6 because it's disabled in the platform"
             }
