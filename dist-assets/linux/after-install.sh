af6b9569a084943755e5897e1fe3f7206a08a271
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-09-03 07:17:52 -0300

    Move init files to separate directory
    Rename them in the repository so that they become clearer.

--- dist-assets/linux/after-install.sh
+++ dist-assets/linux/after-install.sh
@@ -2,10 +2,10 @@
 set -eu
 
 if which systemctl &> /dev/null; then
-    systemctl enable "/opt/Mullvad VPN/mullvad-daemon.service"
+    systemctl enable "/opt/Mullvad VPN/init/systemd/mullvad-daemon.service"
     systemctl start mullvad-daemon.service
 elif /sbin/init --version | grep upstart &> /dev/null; then
-    ln -s "/opt/Mullvad VPN/mullvad-daemon.conf" /etc/init/
+    ln -s "/opt/Mullvad VPN/init/upstart/mullvad-daemon.conf" /etc/init/
     initctl reload-configuration
     start mullvad-daemon
 fi
