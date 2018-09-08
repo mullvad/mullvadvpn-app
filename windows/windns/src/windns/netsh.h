df4d2b64449cae87ddab43c634b7f35485e919b3
Odd Stranne <odd@mullvad.net>
2018-09-08 22:46:03 +0200

    Include output from 'netsh' if requested operation fails or times out

--- windows/windns/src/windns/netsh.h
+++ windows/windns/src/windns/netsh.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <cstdint>
+#include <stdexcept>
 
 class NetSh
 {
@@ -23,4 +24,26 @@ public:
 private:
 
 	NetSh();
+};
+
+class NetShError : public std::exception
+{
+public:
+
+	NetShError(std::string &&error, std::vector<std::string> &&details)
+		: std::exception(error.c_str())
+		, m_error(std::move(error))
+		, m_details(std::move(details))
+	{
+	}
+
+	const std::vector<std::string> &details()
+	{
+		return m_details;
+	}
+
+private:
+
+	const std::string m_error;
+	const std::vector<std::string> m_details;
 };
