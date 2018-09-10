1e7500a3defd81f185a5073f1c847ff7d1511a21
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-09-10 11:03:27 -0300

    Implement support for systemd-resolved

--- talpid-core/src/security/linux/dns/mod.rs
+++ talpid-core/src/security/linux/dns/mod.rs
@@ -1,29 +1,45 @@
 mod static_resolv_conf;
+mod systemd_resolved;
 
 use std::net::IpAddr;
 
 use self::static_resolv_conf::StaticResolvConf;
+use self::systemd_resolved::SystemdResolved;
 
 error_chain! {
+    errors {
+        NoDnsSettingsManager {
+            description("No DNS settings manager detected")
+        }
+    }
+
     links {
         StaticResolvConf(static_resolv_conf::Error, static_resolv_conf::ErrorKind);
+        SystemdResolved(systemd_resolved::Error, systemd_resolved::ErrorKind);
     }
 }
 
 pub enum DnsSettings {
     StaticResolvConf(StaticResolvConf),
+    SystemdResolved(SystemdResolved),
 }
 
 impl DnsSettings {
     pub fn new() -> Result<Self> {
-        Ok(DnsSettings::StaticResolvConf(StaticResolvConf::new()?))
+        SystemdResolved::new()
+            .map(DnsSettings::SystemdResolved)
+            .or_else(|_| StaticResolvConf::new().map(DnsSettings::StaticResolvConf))
+            .chain_err(|| ErrorKind::NoDnsSettingsManager)
     }
 
-    pub fn set_dns(&mut self, servers: Vec<IpAddr>) -> Result<()> {
+    pub fn set_dns(&mut self, interface: &str, servers: Vec<IpAddr>) -> Result<()> {
         use self::DnsSettings::*;
 
         match self {
             StaticResolvConf(ref mut static_resolv_conf) => static_resolv_conf.set_dns(servers)?,
+            SystemdResolved(ref mut systemd_resolved) => {
+                systemd_resolved.set_dns(interface, servers)?
+            }
         }
 
         Ok(())
@@ -34,6 +50,7 @@ impl DnsSettings {
 
         match self {
             StaticResolvConf(ref mut static_resolv_conf) => static_resolv_conf.reset()?,
+            SystemdResolved(ref mut systemd_resolved) => systemd_resolved.reset()?,
         }
 
         Ok(())
