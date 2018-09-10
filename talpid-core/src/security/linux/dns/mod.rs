a8925c7d5ba0b2cf42d701e66922e29f9341124c
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-09-10 19:53:45 -0300

    Implement support for resolvconf

--- talpid-core/src/security/linux/dns/mod.rs
+++ talpid-core/src/security/linux/dns/mod.rs
@@ -1,28 +1,42 @@
+mod resolvconf;
 mod static_resolv_conf;
 
 use std::net::IpAddr;
 
+use self::resolvconf::Resolvconf;
 use self::static_resolv_conf::StaticResolvConf;
 
 error_chain! {
+    errors {
+        NoDnsSettingsManager {
+            description("No DNS settings manager detected")
+        }
+    }
+
     links {
+        Resolvconf(resolvconf::Error, resolvconf::ErrorKind);
         StaticResolvConf(static_resolv_conf::Error, static_resolv_conf::ErrorKind);
     }
 }
 
 pub enum DnsSettings {
+    Resolvconf(Resolvconf),
     StaticResolvConf(StaticResolvConf),
 }
 
 impl DnsSettings {
     pub fn new() -> Result<Self> {
-        Ok(DnsSettings::StaticResolvConf(StaticResolvConf::new()?))
+        Resolvconf::new()
+            .map(DnsSettings::Resolvconf)
+            .or_else(|_| StaticResolvConf::new().map(DnsSettings::StaticResolvConf))
+            .chain_err(|| ErrorKind::NoDnsSettingsManager)
     }
 
-    pub fn set_dns(&mut self, servers: Vec<IpAddr>) -> Result<()> {
+    pub fn set_dns(&mut self, interface: &str, servers: Vec<IpAddr>) -> Result<()> {
         use self::DnsSettings::*;
 
         match self {
+            Resolvconf(ref mut resolvconf) => resolvconf.set_dns(interface, servers)?,
             StaticResolvConf(ref mut static_resolv_conf) => static_resolv_conf.set_dns(servers)?,
         }
 
@@ -33,6 +47,7 @@ impl DnsSettings {
         use self::DnsSettings::*;
 
         match self {
+            Resolvconf(ref mut resolvconf) => resolvconf.reset()?,
             StaticResolvConf(ref mut static_resolv_conf) => static_resolv_conf.reset()?,
         }
 
