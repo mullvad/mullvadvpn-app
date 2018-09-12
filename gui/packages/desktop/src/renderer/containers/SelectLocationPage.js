4f304822baa38caa5b8f9d07b58d4183dd9f93ea
Andrej Mihajlov <and@mullvad.net>
2018-09-12 14:21:37 +0300

    Dismiss SelectLocation first to make sure it does not freeze for the duration of updateRelaySettings + connectTunnel

--- gui/packages/desktop/src/renderer/containers/SelectLocationPage.js
+++ gui/packages/desktop/src/renderer/containers/SelectLocationPage.js
@@ -19,6 +19,9 @@ const mapDispatchToProps = (dispatch: ReduxDispatch, props: SharedRouteProps) =>
   return {
     onClose: () => history.goBack(),
     onSelect: async (relayLocation) => {
+      // dismiss the view first
+      history.goBack();
+
       try {
         const relayUpdate = RelaySettingsBuilder.normal()
           .location.fromRaw(relayLocation)
@@ -26,8 +29,6 @@ const mapDispatchToProps = (dispatch: ReduxDispatch, props: SharedRouteProps) =>
 
         await props.app.updateRelaySettings(relayUpdate);
         await props.app.connectTunnel();
-
-        history.goBack();
       } catch (e) {
         log.error(`Failed to select server: ${e.message}`);
       }
