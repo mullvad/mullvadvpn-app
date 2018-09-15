5bef2df9f74d82e982db9cfee0657c019666b8ab
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-09-15 10:20:26 -0300

    Notify tunnel states in notification controller

--- gui/packages/desktop/src/renderer/lib/notification-controller.js
+++ gui/packages/desktop/src/renderer/lib/notification-controller.js
@@ -1,11 +1,36 @@
 // @flow
 
 import { remote } from 'electron';
+import log from 'electron-log';
+
+import type { TunnelState } from './daemon-rpc';
 
 export default class NotificationController {
   _activeNotification: ?Notification;
 
-  show(message: string) {
+  notify(tunnelState: TunnelState) {
+    switch (tunnelState) {
+      case 'connecting':
+        this._show('Connecting');
+        break;
+      case 'connected':
+        this._show('Secured');
+        break;
+      case 'disconnected':
+        this._show('Unsecured');
+        break;
+      case 'blocked':
+        this._show('Blocked all connections');
+        break;
+      case 'disconnecting':
+        // no-op
+        break;
+      default:
+        log.error(`Unexpected TunnelStateTransition: ${(tunnelState: empty)}`);
+    }
+  }
+
+  _show(message: string) {
     const lastNotification = this._activeNotification;
     const sameAsLastNotification = lastNotification && lastNotification.body === message;
 
