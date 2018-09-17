ae32bab9eacf56d1aa72df88dbdec3080a0f4525
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-09-17 07:53:00 -0300

    Use `TunnelStateTransition` in redux state

--- gui/packages/desktop/src/renderer/components/Connect.js
+++ gui/packages/desktop/src/renderer/components/Connect.js
@@ -13,7 +13,7 @@ import Map from './Map';
 import styles from './ConnectStyles';
 import { NoCreditError, NoInternetError } from '../errors';
 import WindowStateObserver from '../lib/window-state-observer';
-import type { BlockReason, TunnelState } from '../lib/daemon-rpc';
+import type { BlockReason, TunnelStateTransition } from '../lib/daemon-rpc';
 
 import type { HeaderBarStyle } from './HeaderBar';
 import type { ConnectionReduxState } from '../redux/connection/reducers';
@@ -73,7 +73,7 @@ export default class Connect extends Component<Props, State> {
     const connection = props.connection;
     this.state = {
       ...this.state,
-      banner: this.getBannerState(connection.status, connection.blockReason),
+      banner: this.getBannerState(connection.status),
     };
   }
 
@@ -98,11 +98,11 @@ export default class Connect extends Component<Props, State> {
     const newConnection = this.props.connection;
 
     if (
-      oldConnection.status !== newConnection.status ||
-      oldConnection.blockReason !== newConnection.blockReason
+      oldConnection.status.state !== newConnection.status.state ||
+      oldConnection.status.details !== newConnection.status.details
     ) {
       this.setState({
-        banner: this.getBannerState(newConnection.status, newConnection.blockReason),
+        banner: this.getBannerState(newConnection.status),
       });
     }
   }
@@ -122,11 +122,8 @@ export default class Connect extends Component<Props, State> {
     );
   }
 
-  getBannerState(
-    tunnelState: TunnelState,
-    blockReason: ?BlockReason,
-  ): $PropertyType<State, 'banner'> {
-    switch (tunnelState) {
+  getBannerState(tunnelState: TunnelStateTransition): $PropertyType<State, 'banner'> {
+    switch (tunnelState.state) {
       case 'connecting':
         return {
           visible: true,
@@ -138,7 +135,7 @@ export default class Connect extends Component<Props, State> {
         return {
           visible: true,
           title: 'BLOCKING INTERNET',
-          subtitle: blockReason ? getBlockReasonMessage(blockReason) : '',
+          subtitle: getBlockReasonMessage(tunnelState.details),
         };
 
       default:
@@ -186,16 +183,17 @@ export default class Connect extends Component<Props, State> {
 
   _getMapProps() {
     const { longitude, latitude, status } = this.props.connection;
+    const state = status.state;
 
     // when the user location is known
     if (typeof longitude === 'number' && typeof latitude === 'number') {
       return {
         center: [longitude, latitude],
         // do not show the marker when connecting
-        showMarker: status !== 'connecting',
-        markerStyle: status === 'connected' || status === 'blocked' ? 'secure' : 'unsecure',
+        showMarker: state !== 'connecting',
+        markerStyle: state === 'connected' || state === 'blocked' ? 'secure' : 'unsecure',
         // zoom in when connected
-        zoomLevel: status === 'connected' ? 'low' : 'medium',
+        zoomLevel: state === 'connected' ? 'low' : 'medium',
         // a magic offset to align marker with spinner
         offset: [0, 123],
       };
@@ -220,7 +218,7 @@ export default class Connect extends Component<Props, State> {
       false,
       false,
     ];
-    switch (this.props.connection.status) {
+    switch (this.props.connection.status.state) {
       case 'connecting':
         isConnecting = true;
         break;
@@ -388,8 +386,8 @@ export default class Connect extends Component<Props, State> {
   // Private
 
   headerBarStyle(): HeaderBarStyle {
-    const { status } = this.props.connection;
-    switch (status) {
+    const { state } = this.props.connection.status;
+    switch (state) {
       case 'disconnecting':
       case 'disconnected':
         return 'error';
@@ -398,23 +396,23 @@ export default class Connect extends Component<Props, State> {
       case 'blocked':
         return 'success';
       default:
-        throw new Error(`Invalid TunnelState: ${(status: empty)}`);
+        throw new Error(`Invalid TunnelState: ${(state: empty)}`);
     }
   }
 
   networkSecurityStyle(): Types.Style {
     const classes = [styles.status_security];
-    const { status } = this.props.connection;
-    if (status === 'connected' || status === 'blocked') {
+    const { state } = this.props.connection.status;
+    if (state === 'connected' || state === 'blocked') {
       classes.push(styles.status_security__secure);
-    } else if (status === 'disconnected' || status === 'disconnecting') {
+    } else if (state === 'disconnected' || state === 'disconnecting') {
       classes.push(styles.status_security__unsecured);
     }
     return classes;
   }
 
   networkSecurityMessage(): string {
-    switch (this.props.connection.status) {
+    switch (this.props.connection.status.state) {
       case 'connected':
         return 'SECURE CONNECTION';
       case 'blocked':
@@ -428,7 +426,7 @@ export default class Connect extends Component<Props, State> {
 
   ipAddressStyle(): Types.Style {
     var classes = [styles.status_ipaddress];
-    if (this.props.connection.status === 'connecting') {
+    if (this.props.connection.status.state === 'connecting') {
       classes.push(styles.status_ipaddress__invisible);
     }
     return classes;
