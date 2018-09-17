aba583c94d269ccc9c616228b8b5b817babe2d3a
Andrej Mihajlov <and@mullvad.net>
2018-09-17 16:39:49 +0300

    Break down Connect on many smaller components

---
+++ gui/packages/components/src/SecuredLabel.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { Clipboard, Component, Styles, Text, Types } from 'reactxp';
+
+export enum SecuredDisplayStyle {
+  secured,
+  blocked,
+  securing,
+  unsecured,
+}
+
+interface IProps {
+  displayStyle: SecuredDisplayStyle;
+  style: Types.TextStyleRuleSet;
+}
+
+const styles = {
+  securing: Styles.createTextStyle({
+    color: 'rgb(255, 255, 255)', // white
+  }),
+  secured: Styles.createTextStyle({
+    color: 'rgb(68, 173, 77)', // green
+  }),
+  unsecured: Styles.createTextStyle({
+    color: 'rgb(208, 2, 27)', // red
+  }),
+};
+
+export default class SecuredLabel extends Component<IProps> {
+  public render() {
+    return <Text style={[this.props.style, this.getTextStyle()]}>{this.getText()}</Text>;
+  }
+
+  private getText() {
+    switch (this.props.displayStyle) {
+      case SecuredDisplayStyle.secured:
+        return 'SECURE CONNECTION';
+
+      case SecuredDisplayStyle.blocked:
+        return 'BLOCKED CONNECTION';
+
+      case SecuredDisplayStyle.securing:
+        return 'CREATING SECURE CONNECTION';
+
+      case SecuredDisplayStyle.unsecured:
+        return 'UNSECURED CONNECTION';
+    }
+  }
+
+  private getTextStyle() {
+    switch (this.props.displayStyle) {
+      case SecuredDisplayStyle.secured:
+      case SecuredDisplayStyle.blocked:
+        return styles.secured;
+
+      case SecuredDisplayStyle.securing:
+        return styles.securing;
+
+      case SecuredDisplayStyle.unsecured:
+        return styles.unsecured;
+    }
+  }
+}
