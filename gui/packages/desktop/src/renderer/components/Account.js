a242d7ff6d67faa5cf90758478737927f8075d1f
Andrej Mihajlov <and@mullvad.net>
2018-09-17 16:39:49 +0300

    Break down Connect on many smaller components

--- gui/packages/desktop/src/renderer/components/Account.js
+++ gui/packages/desktop/src/renderer/components/Account.js
@@ -2,7 +2,8 @@
 
 import moment from 'moment';
 import * as React from 'react';
-import { Component, Clipboard, Text, View } from 'reactxp';
+import { Component, Text, View } from 'reactxp';
+import { ClipboardLabel } from '@mullvad/components';
 import * as AppButton from './AppButton';
 import { Layout, Container } from './Layout';
 import NavigationBar, { BackBarItem } from './NavigationBar';
@@ -21,14 +22,7 @@ type Props = {
   onBuyMore: () => void,
 };
 
-type State = {
-  showAccountTokenCopiedMessage: boolean,
-};
-
-export default class Account extends Component<Props, State> {
-  state = {
-    showAccountTokenCopiedMessage: false,
-  };
+export default class Account extends Component<Props> {
 
   _copyTimer: ?TimeoutID;
 
@@ -36,18 +30,6 @@ export default class Account extends Component<Props, State> {
     if (this._copyTimer) {
       clearTimeout(this._copyTimer);
     }
-  }
-
-  onAccountTokenClick() {
-    if (this._copyTimer) {
-      clearTimeout(this._copyTimer);
-    }
-    this._copyTimer = setTimeout(
-      () => this.setState({ showAccountTokenCopiedMessage: false }),
-      3000,
-    );
-    this.setState({ showAccountTokenCopiedMessage: true });
-    Clipboard.setText(this.props.accountToken);
   }
 
   render() {
@@ -68,13 +50,11 @@ export default class Account extends Component<Props, State> {
                 <View style={styles.account__main}>
                   <View style={styles.account__row}>
                     <Text style={styles.account__row_label}>Account ID</Text>
-                    <Text
+                    <ClipboardLabel
                       style={styles.account__row_value}
-                      onPress={this.onAccountTokenClick.bind(this)}>
-                      {this.state.showAccountTokenCopiedMessage
-                        ? 'COPIED TO CLIPBOARD!'
-                        : this.props.accountToken}
-                    </Text>
+                      value={this.props.accountToken}
+                      message={'COPIED TO CLIPBOARD!'}
+                    />
                   </View>
 
                   <View style={styles.account__row}>
