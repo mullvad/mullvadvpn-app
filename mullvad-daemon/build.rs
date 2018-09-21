601fdcf485fbf568e526d79009be4f08fde844b8
Linus Färnstrand <linus@mullvad.net>
2018-09-21 09:30:17 +0200

    Split mullvad_daemon into lib.rs and main.rs

--- mullvad-daemon/build.rs
+++ mullvad-daemon/build.rs
@@ -4,18 +4,28 @@ use std::path::PathBuf;
 use std::process::Command;
 
 #[cfg(windows)]
-extern crate windres;
+extern crate winapi;
+#[cfg(windows)]
+extern crate winres;
 
 fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
     let product_version = env!("CARGO_PKG_VERSION").replacen(".0", "", 1);
-    fs::write(out_dir.join("product-version.txt"), product_version).unwrap();
+    fs::write(out_dir.join("product-version.txt"), &product_version).unwrap();
     fs::write(out_dir.join("git-commit-date.txt"), commit_date()).unwrap();
 
     #[cfg(windows)]
     {
-        windres::Build::new().compile("resources.rc").unwrap();
+        let mut res = winres::WindowsResource::new();
+        res.set("ProductVersion", &product_version);
+        res.set_icon("../dist-assets/icon.ico");
+        res.set_language(winapi::um::winnt::MAKELANGID(
+            winapi::um::winnt::LANG_ENGLISH,
+            winapi::um::winnt::SUBLANG_ENGLISH_US,
+        ));
+        res.set_manifest_file("mullvad-daemon.manifest");
+        res.compile().expect("Unable to generate windows resources");
     }
 }
 
