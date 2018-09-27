9e115aeb403345910a620d01aadbcf826ae14b52
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-09-27 11:32:56 -0300

    Use `Cell` components in `AdvancedSettings`

--- gui/packages/desktop/src/renderer/components/AdvancedSettings.js
+++ gui/packages/desktop/src/renderer/components/AdvancedSettings.js
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 import { Button, Component, Text, View } from 'reactxp';
+import * as Cell from './Cell';
 import { Layout, Container } from './Layout';
 import NavigationBar, { BackBarItem } from './NavigationBar';
 import SettingsHeader, { HeaderTitle } from './SettingsHeader';
@@ -43,19 +44,11 @@ export class AdvancedSettings extends Component<Props> {
                 <HeaderTitle>Advanced</HeaderTitle>
               </SettingsHeader>
               <CustomScrollbars style={styles.advanced_settings__scrollview} autoHide={true}>
-                <View style={styles.advanced_settings__ipv6}>
-                  <View style={styles.advanced_settings__cell_label_container}>
-                    <Text style={styles.advanced_settings__cell_label}>Enable IPv6</Text>
-                  </View>
-                  <View style={styles.advanced_settings__ipv6_accessory}>
-                    <Switch isOn={this.props.enableIpv6} onChange={this.props.setEnableIpv6} />
-                  </View>
-                </View>
-                <View style={styles.advanced_settings__cell_footer}>
-                  <Text style={styles.advanced_settings__cell_footer_label}>
-                    {'Enable IPv6 communication through the tunnel.'}
-                  </Text>
-                </View>
+                <Cell.Container>
+                  <Cell.Label>Enable IPv6</Cell.Label>
+                  <Switch isOn={this.props.enableIpv6} onChange={this.props.setEnableIpv6} />
+                </Cell.Container>
+                <Cell.Footer>Enable IPv6 communication through the tunnel.</Cell.Footer>
 
                 <View style={styles.advanced_settings__content}>
                   <Selector
