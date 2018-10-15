9df95d9dab7b6f4f1000783e4a8fe825bfbbb9b4
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-10-15 08:48:40 -0300

    Show out IP address when connected

--- gui/packages/desktop/src/renderer/app.js
+++ gui/packages/desktop/src/renderer/app.js
@@ -599,7 +599,7 @@ export default class AppRenderer {
   }
 
   async _updateUserLocation(tunnelState: TunnelState) {
-    if (tunnelState === 'connecting' || tunnelState === 'disconnected') {
+    if (['connected', 'connecting', 'disconnected'].includes(tunnelState)) {
       try {
         await this._fetchLocation();
       } catch (error) {
