a8409574102a8dd29c89ea2abf294d46902fe498
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-10-15 12:12:59 -0300

    Show connection info in GUI

--- gui/packages/components/src/index.ts
+++ gui/packages/components/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Accordion } from './Accordion';
 export { default as ClipboardLabel } from './ClipboardLabel';
+export { default as ConnectionInfo } from './ConnectionInfo';
 export { default as SecuredLabel, SecuredDisplayStyle } from './SecuredLabel';
