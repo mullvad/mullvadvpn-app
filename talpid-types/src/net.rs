c6cc78d6fc0db9897a1491922ff6fe00d45b6b7a
Odd Stranne <odd@mullvad.net>
2018-10-23 12:57:27 +0200

    Add OpenVPN proxy support in Daemon

--- talpid-types/src/net.rs
+++ talpid-types/src/net.rs
@@ -161,7 +161,7 @@ impl Error for TransportProtocolParseError {
 
 /// TunnelOptions holds optional settings for tunnels, that are to be applied to any tunnel of the
 /// appropriate type.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(default)]
 pub struct TunnelOptions {
     /// openvpn holds OpenVPN specific tunnel options.
@@ -184,10 +184,68 @@ impl Default for TunnelOptions {
 /// OpenVpnTunnelOptions contains options for an openvpn tunnel that should be applied irrespective
 /// of the relay parameters - i.e. have nothing to do with the particular OpenVPN server, but do
 /// affect the connection.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, Default)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, Default)]
 #[serde(default)]
 pub struct OpenVpnTunnelOptions {
     /// Optional argument for openvpn to try and limit TCP packet size,
     /// as discussed [here](https://openvpn.net/archive/openvpn-users/2003-11/msg00154.html)
     pub mssfix: Option<u16>,
+    /// Proxy settings, for when the relay connection should be via a proxy.
+    pub proxy: Option<OpenVpnProxySettings>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum OpenVpnProxySettings {
+    Local(LocalOpenVpnProxySettings),
+    Remote(RemoteOpenVpnProxySettings),
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Deserialize, Serialize)]
+pub struct LocalOpenVpnProxySettings {
+    pub port: u16,
+    pub peer: SocketAddr,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Deserialize, Serialize)]
+pub struct RemoteOpenVpnProxySettings {
+    pub address: SocketAddr,
+    pub auth: Option<OpenVpnProxyAuth>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Deserialize, Serialize)]
+pub struct OpenVpnProxyAuth {
+    pub username: String,
+    pub password: String,
+}
+
+pub struct OpenVpnProxySettingsValidation;
+
+impl OpenVpnProxySettingsValidation {
+    pub fn validate(proxy: &OpenVpnProxySettings) -> Result<(), String> {
+        match proxy {
+            OpenVpnProxySettings::Local(local) => {
+                if local.port == 0 {
+                    return Err(String::from("Invalid local port number"));
+                }
+                if local.peer.ip().is_loopback() {
+                    return Err(String::from(
+                        "localhost is not a valid peer in this context",
+                    ));
+                }
+                if local.peer.port() == 0 {
+                    return Err(String::from("Invalid remote port number"));
+                }
+            }
+            OpenVpnProxySettings::Remote(remote) => {
+                if remote.address.port() == 0 {
+                    return Err(String::from("Invalid port number"));
+                }
+                if remote.address.ip().is_loopback() {
+                    return Err(String::from("localhost is not a valid remote server"));
+                }
+            }
+        };
+        Ok(())
+    }
 }
