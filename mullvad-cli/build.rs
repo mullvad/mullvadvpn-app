0f687695c6927e60060d43946f107e4db29a73e9
Linus Färnstrand <linus@mullvad.net>
2018-11-01 10:59:01 +0100

    Make all binaries use same version format

--- mullvad-cli/build.rs
+++ mullvad-cli/build.rs
@@ -3,10 +3,15 @@ extern crate winapi;
 #[cfg(windows)]
 extern crate winres;
 
+use std::{env, fs, path::PathBuf};
+
 fn main() {
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let product_version = env!("CARGO_PKG_VERSION").replacen(".0", "", 1);
+    fs::write(out_dir.join("product-version.txt"), &product_version).unwrap();
+
     #[cfg(windows)]
     {
-        let product_version = env!("CARGO_PKG_VERSION").replacen(".0", "", 1);
         let mut res = winres::WindowsResource::new();
         res.set("ProductVersion", &product_version);
         res.set_icon("../dist-assets/icon.ico");
