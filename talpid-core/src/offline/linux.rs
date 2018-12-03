cf630bef777661d4ad544b0f2028632aad4465a0
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-12-03 16:58:04 -0200

    Implement `is_offline` for Linux

---
+++ talpid-core/src/offline/linux.rs
@@ -0,0 +1,71 @@
+extern crate iproute2;
+extern crate rtnetlink;
+
+use std::io;
+
+use error_chain::ChainedError;
+use futures::{future::Either, sync::mpsc::UnboundedSender, Future};
+use log::warn;
+
+use self::iproute2::Link;
+use self::rtnetlink::LinkLayerType;
+
+use tunnel_state_machine::TunnelCommand;
+
+error_chain! {
+    errors {
+        GetLinksError {
+            description("Failed to get list of IP links")
+        }
+        NetlinkDisconnected {
+            description("Netlink connection has unexpectedly disconnected")
+        }
+    }
+
+    foreign_links {
+        IoError(io::Error);
+    }
+}
+
+pub struct MonitorHandle;
+
+pub fn spawn_monitor(_sender: UnboundedSender<TunnelCommand>) -> Result<MonitorHandle> {
+    Ok(MonitorHandle)
+}
+
+pub fn is_offline() -> bool {
+    check_if_offline().unwrap_or_else(|error| {
+        let chained_error = error.chain_err(|| "Failed to check for internet connection");
+        warn!("{}", chained_error.display_chain());
+        false
+    })
+}
+
+fn check_if_offline() -> Result<bool> {
+    Ok(list_links_providing_connectivity()?.next().is_none())
+}
+
+fn list_links_providing_connectivity() -> Result<impl Iterator<Item = Link>> {
+    Ok(list_links()?.into_iter().filter(link_provides_connectivity))
+}
+
+fn link_provides_connectivity(link: &Link) -> bool {
+    link.link_layer_type() != LinkLayerType::Loopback
+        && link.link_layer_type() != LinkLayerType::None
+        && link.flags().is_running()
+}
+
+fn list_links() -> Result<Vec<Link>> {
+    let (connection, connection_handle) = iproute2::new_connection()?;
+    let links_request = connection_handle.link().get().execute();
+
+    match connection.select2(links_request).wait() {
+        Ok(Either::A(_)) => bail!(ErrorKind::NetlinkDisconnected),
+        Ok(Either::B((links, _))) => Ok(links),
+        Err(Either::A((error, _))) => bail!(error),
+        Err(Either::B((error, _))) => Err(Error::with_chain(
+            failure::Fail::compat(error),
+            ErrorKind::GetLinksError,
+        )),
+    }
+}
