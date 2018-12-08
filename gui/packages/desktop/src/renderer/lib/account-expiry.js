ba9a3fbcde36093fc1a1fa11a58e72dae736b957
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-12-08 12:27:15 -0200

    Create `AccountExpiry` helper class

---
+++ gui/packages/desktop/src/renderer/lib/account-expiry.js
@@ -0,0 +1,19 @@
+// @flow
+
+import moment from 'moment';
+
+export default class AccountExpiry {
+  _expiry: ?moment;
+
+  constructor(expiry: ?string) {
+    this._expiry = expiry ? moment(expiry) : null;
+  }
+
+  hasExpired(): boolean {
+    return this._expiry ? this._expiry.isSameOrBefore(moment()) : false;
+  }
+
+  remainingTime(): string {
+    return this._expiry ? this._expiry.fromNow(true) + ' left' : '';
+  }
+}
