69d4cc202014e364b7ea5c005cc4be6ef50f9b01
Emīls Piņķis <emils@mullvad.net>
2019-01-07 17:34:48 +0000

    Factor out PingMonitor into 3 functions

--- talpid-core/src/tunnel/wireguard/ping_monitor.rs
+++ talpid-core/src/tunnel/wireguard/ping_monitor.rs
@@ -2,66 +2,58 @@ use super::{CloseHandle, ErrorKind, Result, ResultExt};
 use std::{net::IpAddr, thread, time};
 
 
-/// Monitors a tunnel device by pinging an IP and closes a tunnel once it fails.
-pub struct PingMonitor {
+pub fn spawn_ping_monitor(
     ip: IpAddr,
+    timeout: time::Duration,
     interface: String,
-    handle: Box<dyn CloseHandle>,
+    mut close_handle: Box<dyn CloseHandle>,
+) {
+    thread::spawn(move || loop {
+        let start = time::Instant::now();
+        if let Err(e) = ping(ip, timeout, &interface) {
+            close_handle.close_with_error(e);
+            return;
+        }
+        let elapsed = time::Instant::now() - start;
+        if let Some(remaining) = timeout.checked_sub(elapsed) {
+            thread::sleep(remaining);
+        }
+    });
 }
 
-impl PingMonitor {
-    pub fn new(ip: IpAddr, interface: String, handle: Box<dyn CloseHandle>) -> Self {
-        Self {
-            ip,
-            interface,
-            handle,
-        }
+pub fn ping(ip: IpAddr, timeout: time::Duration, interface: &str) -> Result<()> {
+    let output = ping_cmd(ip, timeout, interface)
+        .run()
+        .chain_err(|| ErrorKind::PingError)?;
+    if !output.status.success() {
+        bail!(ErrorKind::PingTimeoutError);
     }
+    Ok(())
+}
 
-    fn cmd(&self, timeout: u64) -> duct::Expression {
-        let interface_flag = if cfg!(target_os = "linux") {
-            "-I"
-        } else {
-            "-b"
-        };
-        let timeout_flag = if cfg!(target_os = "linux") {
-            "-w"
-        } else {
-            "-t"
-        };
-        duct::cmd!(
-            "ping",
-            "-n",
-            "-c",
-            "1",
-            &interface_flag,
-            &self.interface,
-            timeout_flag,
-            timeout.to_string(),
-            self.ip.to_string()
-        )
-        .stdin_null()
-        .stdout_null()
-        .unchecked()
-    }
-
-    pub fn wait(&self, timeout: u64) -> Result<()> {
-        let output = self.cmd(timeout).run().chain_err(|| ErrorKind::PingError)?;
-        if !output.status.success() {
-            bail!(ErrorKind::PingTimeoutError);
-        }
-        Ok(())
-    }
-
-    pub fn monitor_ping(mut self, timeout: u64) {
-        thread::spawn(move || loop {
-            let deadline = time::Instant::now() + time::Duration::from_secs(timeout);
-            if let Err(e) = self.wait(timeout) {
-                self.handle.close_with_error(e);
-                return;
-            }
-            let diff = deadline - time::Instant::now();
-            thread::sleep(diff);
-        });
-    }
+fn ping_cmd(ip: IpAddr, timeout: time::Duration, interface: &str) -> duct::Expression {
+    let interface_flag = if cfg!(target_os = "linux") {
+        "-I"
+    } else {
+        "-b"
+    };
+    let timeout_flag = if cfg!(target_os = "linux") {
+        "-w"
+    } else {
+        "-t"
+    };
+    duct::cmd!(
+        "ping",
+        "-n",
+        "-c",
+        "1",
+        &interface_flag,
+        &interface,
+        timeout_flag,
+        &timeout.as_secs().to_string(),
+        ip.to_string()
+    )
+    .stdin_null()
+    .stdout_null()
+    .unchecked()
 }
