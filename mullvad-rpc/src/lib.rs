02df1c5e0aa6f7f0e4c40d62dcf51d7d1eeb1dbe
Emīls Piņķis <emils@mullvad.net>
2019-01-29 14:10:34 +0000

    Change relay_list RPC to relay_list_v2

--- mullvad-rpc/src/lib.rs
+++ mullvad-rpc/src/lib.rs
@@ -122,7 +122,7 @@ jsonrpc_client!(pub struct ProblemReportProxy {
 });
 
 jsonrpc_client!(pub struct RelayListProxy {
-    pub fn relay_list(&mut self) -> RpcRequest<RelayList>;
+    pub fn relay_list_v2(&mut self) -> RpcRequest<RelayList>;
 });
 
 jsonrpc_client!(pub struct AppVersionProxy {
