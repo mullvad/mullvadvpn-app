e3f11594601f4cff07f7bd4fc67189546e842f14
Andrej Mihajlov <and@mullvad.net>
2019-03-07 14:27:29 +0100

    Adjust catalogue names to Crowdin and fix the shell script

--- gui/scripts/update-translations.sh
+++ gui/scripts/update-translations.sh
@@ -8,18 +8,11 @@ POT_FILE="$ROOT_DIR/locales/messages.pot"
 
 for PO_FILE_DIR in $ROOT_DIR/locales/* ; do
   if [ -d $PO_FILE_DIR ] ; then
-    PO_FILE="$PO_FILE_DIR/messages.po"
-    GITKEEP_FILE="$PO_FILE_DIR/.gitkeep"
+    PO_FILE="$PO_FILE_DIR/messages-$PO_FILE_DIR.po"
 
     if [ -f $PO_FILE ] ; then
       echo "Update $PO_FILE_DIR\c"
       msgmerge --no-fuzzy-matching --update $PO_FILE $POT_FILE
-    else
-      if [ -f $GITKEEP_FILE ] ; then
-        echo "Remove $GITKEEP_FILE to initialize the new translation"
-      else
-        msginit --input $POT_FILE --output $PO_FILE --no-translator
-      fi
     fi
   fi
 done
