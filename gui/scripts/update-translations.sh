1e8f342d2b4def948867b8541b3cc4bc72dc9f19
Andrej Mihajlov <and@mullvad.net>
2019-03-07 14:27:29 +0100

    Adjust catalogue names to Crowdin and fix the shell script

--- gui/scripts/update-translations.sh
+++ gui/scripts/update-translations.sh
@@ -3,23 +3,18 @@
 # This script creates or updates the existing gettext catalogues using the POT template located
 # under locales/messages.pot
 
-ROOT_DIR=$(dirname $(dirname "${BASH_SOURCE[0]}"))
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=$( dirname $SCRIPT_DIR )
 POT_FILE="$ROOT_DIR/locales/messages.pot"
 
 for PO_FILE_DIR in $ROOT_DIR/locales/* ; do
   if [ -d $PO_FILE_DIR ] ; then
-    PO_FILE="$PO_FILE_DIR/messages.po"
-    GITKEEP_FILE="$PO_FILE_DIR/.gitkeep"
+    LOCALE=$( basename $PO_FILE_DIR )
+    PO_FILE="$PO_FILE_DIR/messages-$LOCALE.po"
 
     if [ -f $PO_FILE ] ; then
-      echo "Update $PO_FILE_DIR\c"
+      echo "Update $PO_FILE"
       msgmerge --no-fuzzy-matching --update $PO_FILE $POT_FILE
-    else
-      if [ -f $GITKEEP_FILE ] ; then
-        echo "Remove $GITKEEP_FILE to initialize the new translation"
-      else
-        msginit --input $POT_FILE --output $PO_FILE --no-translator
-      fi
     fi
   fi
 done
