08ca024745d10804ea7db893bd867fe6e842ebad
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-11 11:42:46 +0000

    Implement Login screen

--- android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+
+        if (savedInstanceState == null) {
+            addInitialFragment()
+        }
+    }
+
+    private fun addInitialFragment() {
+        supportFragmentManager?.beginTransaction()?.apply {
+            add(R.id.main_fragment, LoginFragment())
+            commit()
+        }
     }
 }
