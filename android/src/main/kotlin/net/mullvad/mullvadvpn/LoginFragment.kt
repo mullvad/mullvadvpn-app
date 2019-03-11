2bb45644a81c3a1e4b6b6c1de5c3df61f6e4ef08
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-11 20:18:32 +0000

    Implement "Logging in" screen

--- android/src/main/kotlin/net/mullvad/mullvadvpn/LoginFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/LoginFragment.kt
@@ -1,5 +1,6 @@
 package net.mullvad.mullvadvpn
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.text.Editable
@@ -9,11 +10,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 
 const val MIN_ACCOUNT_TOKEN_LENGTH = 10
 
 class LoginFragment : Fragment() {
+    private lateinit var title: TextView
+    private lateinit var subtitle: TextView
+    private lateinit var statusIcon: View
+    private lateinit var accountInput: EditText
     private lateinit var loginButton: ImageButton
+
+    private var accountInputDisabledBackgroundColor: Int = 0
+    private var accountInputDisabledTextColor: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,14 +30,25 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.login, container, false)
-        val accountInput: EditText = view.findViewById(R.id.account_input)
 
+        title = view.findViewById(R.id.title)
+        subtitle = view.findViewById(R.id.subtitle)
+        statusIcon = view.findViewById(R.id.status_icon)
+        accountInput = view.findViewById(R.id.account_input)
         loginButton = view.findViewById(R.id.login_button)
 
         accountInput.addTextChangedListener(AccountInputWatcher())
+        loginButton.setOnClickListener { login() }
         setLoginButtonEnabled(false)
 
         return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        accountInputDisabledBackgroundColor = context.getColor(R.color.white20)
+        accountInputDisabledTextColor = context.getColor(R.color.white)
     }
 
     private fun setLoginButtonEnabled(enabled: Boolean) {
@@ -38,6 +58,22 @@ class LoginFragment : Fragment() {
                 setClickable(enabled)
                 setFocusable(enabled)
             }
+        }
+    }
+
+    private fun login() {
+        title.setText(R.string.logging_in_title)
+        subtitle.setText(R.string.logging_in_description)
+        statusIcon.setVisibility(View.VISIBLE)
+        loginButton.setVisibility(View.GONE)
+        disableAccountInput()
+    }
+
+    private fun disableAccountInput() {
+        accountInput.apply {
+            setEnabled(false)
+            setBackgroundColor(accountInputDisabledBackgroundColor)
+            setTextColor(accountInputDisabledTextColor)
         }
     }
 
