a0d67ee66e7a58a7c82dc29161b6093803034996
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-12 15:06:55 +0000

    Format `MainActivity.kt`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
@@ -3,7 +3,7 @@ package net.mullvad.mullvadvpn
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 
-class MainActivity: FragmentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
