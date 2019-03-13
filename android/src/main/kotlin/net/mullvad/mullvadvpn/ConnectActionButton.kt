614ff99d121f3e09d3b5bfeed3757e2be0c1c20f
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-13 19:56:19 +0000

    Refactor to creater helper `ConnectActionButton`

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ConnectActionButton.kt
@@ -0,0 +1,42 @@
+package net.mullvad.mullvadvpn
+
+import android.view.View
+import android.widget.Button
+
+class ConnectActionButton(val parentView: View) {
+    private val button: Button = parentView.findViewById(R.id.action_button)
+
+    var state = ConnectionState.Disconnected
+        set(value) {
+            when (value) {
+                ConnectionState.Disconnected -> disconnected()
+                ConnectionState.Connecting -> connecting()
+            }
+
+            field = value
+        }
+
+    var onConnect: (() -> Unit)? = null
+    var onCancel: (() -> Unit)? = null
+
+    init {
+        button.setOnClickListener { action() }
+    }
+
+    private fun action() {
+        when (state) {
+            ConnectionState.Disconnected -> onConnect?.invoke()
+            ConnectionState.Connecting -> onCancel?.invoke()
+        }
+    }
+
+    private fun disconnected() {
+        button.setBackgroundResource(R.drawable.green_button_background)
+        button.setText(R.string.connect)
+    }
+
+    private fun connecting() {
+        button.setBackgroundResource(R.drawable.transparent_red_button_background)
+        button.setText(R.string.cancel)
+    }
+}
