5e927d0854714c586a43fb307d8965047157eb71
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-13 19:36:02 +0000

    Implement Connecting screen

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ConnectFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ConnectFragment.kt
@@ -5,9 +5,16 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 
 class ConnectFragment : Fragment() {
+    private lateinit var actionButton: Button
+    private lateinit var connectingSpinner: View
+    private lateinit var headerBar: View
+    private lateinit var notificationBanner: View
+    private lateinit var status: TextView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,6 +22,27 @@ class ConnectFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.connect, container, false)
 
+        actionButton = view.findViewById(R.id.action_button)
+        connectingSpinner = view.findViewById(R.id.connecting_spinner)
+        headerBar = view.findViewById(R.id.header_bar)
+        notificationBanner = view.findViewById(R.id.notification_banner)
+        status = view.findViewById(R.id.connection_status)
+
+        actionButton.setOnClickListener { connect() }
+
         return view
+    }
+
+    private fun connect() {
+        actionButton.setBackgroundResource(R.drawable.transparent_red_button_background)
+        actionButton.setText(R.string.cancel)
+
+        connectingSpinner.visibility = View.VISIBLE
+        notificationBanner.visibility = View.VISIBLE
+
+        headerBar.setBackgroundColor(context!!.getColor(R.color.green))
+
+        status.setTextColor(context!!.getColor(R.color.white))
+        status.setText(R.string.creating_secure_connection)
     }
 }
