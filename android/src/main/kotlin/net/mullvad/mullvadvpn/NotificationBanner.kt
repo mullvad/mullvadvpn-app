4ce678fb31bdd5db80a00da6df270d3f2cf2b02b
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-18 20:24:06 +0000

    Animate notification banner

--- android/src/main/kotlin/net/mullvad/mullvadvpn/NotificationBanner.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/NotificationBanner.kt
@@ -4,15 +4,34 @@ import android.view.View
 
 class NotificationBanner(val parentView: View) {
     private val banner: View = parentView.findViewById(R.id.notification_banner)
+    private var visible = false
 
     var state = ConnectionState.Disconnected
         set(value) {
             when (value) {
-                ConnectionState.Disconnected -> banner.visibility = View.GONE
-                ConnectionState.Connecting -> banner.visibility = View.VISIBLE
-                ConnectionState.Connected -> banner.visibility = View.GONE
+                ConnectionState.Disconnected -> hide()
+                ConnectionState.Connecting -> show()
+                ConnectionState.Connected -> hide()
             }
 
             field = value
         }
+
+    private fun show() {
+        if (!visible) {
+            visible = true
+            banner.visibility = View.VISIBLE
+            banner.translationY = -banner.height.toFloat()
+            banner.animate().translationY(0.0F).setDuration(350).start()
+        }
+    }
+
+    private fun hide() {
+        if (visible) {
+            visible = false
+            banner.animate().translationY(-banner.height.toFloat()).setDuration(350).withEndAction {
+                banner.visibility = View.INVISIBLE
+            }
+        }
+    }
 }
