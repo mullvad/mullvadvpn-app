cf4851d4aede2c0f4665b72d7262a5bc7ffa5fe3
Emīls Piņķis <emils@mullvad.net>
2019-03-21 10:06:06 +0000

    Remove cached relay list during installation

--- dist-assets/linux/before-install.sh
+++ dist-assets/linux/before-install.sh
@@ -7,3 +7,6 @@ if which systemctl &> /dev/null; then
         systemctl disable mullvad-daemon.service
     fi
 fi
+
+#TODO: Remove after releasing 2019.2
+rm /var/cache/mullvad-vpn/relays.json || true
