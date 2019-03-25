f51e43aaa4bc5fe7ee6d77d206acb22c5176818d
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-03-25 15:48:33 +0000

    Create view-model representation of relay list
    Constructed from the model relay list.

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/RelayList.kt
@@ -0,0 +1,17 @@
+package net.mullvad.mullvadvpn.relaylist
+
+class RelayList {
+    val countries: List<RelayCountry>
+
+    constructor(model: net.mullvad.mullvadvpn.model.RelayList) {
+        countries = model.countries.map { country ->
+            val cities = country.cities.map { city -> 
+                val relays = city.relays.map { relay -> Relay(relay.hostname) }
+
+                RelayCity(city.name, "${country.code}-${city.code}", false, relays)
+            }
+
+            RelayCountry(country.name, country.code, false, cities)
+        }
+    }
+}
