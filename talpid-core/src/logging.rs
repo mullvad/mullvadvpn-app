5012e2d08c488f69f91b777895389296c9cba538
Linus Färnstrand <linus@mullvad.net>
2019-04-01 11:10:10 +0200

    Create better error type for talpid_core::logging

--- talpid-core/src/logging.rs
+++ talpid-core/src/logging.rs
@@ -1,19 +1,25 @@
 use std::{fs, io, path::Path};
 
-error_chain! {}
+/// Unable to create new log file
+#[derive(err_derive::Error, Debug)]
+#[error(display = "Unable to create new log file")]
+pub struct RotateLogError(#[error(cause)] io::Error);
 
 /// Create a new log file while backing up a previous version of it.
 ///
 /// A new log file is created with the given file name, but if a file with that name already exists
 /// it is backed up with the extension changed to `.old.log`.
-pub fn rotate_log(file: &Path) -> Result<()> {
+pub fn rotate_log(file: &Path) -> Result<(), RotateLogError> {
     let backup = file.with_extension("old.log");
-    if let Err(error) = fs::rename(file, backup) {
+    if let Err(error) = fs::rename(file, &backup) {
         if error.kind() != io::ErrorKind::NotFound {
-            log::warn!("Failed to rotate log file ({})", error);
+            log::warn!(
+                "Failed to rotate log file to {}: {}",
+                backup.display(),
+                error
+            );
         }
     }
 
-    fs::File::create(file).chain_err(|| "Unable to create new log file")?;
-    Ok(())
+    fs::File::create(file).map(|_| ()).map_err(RotateLogError)
 }
