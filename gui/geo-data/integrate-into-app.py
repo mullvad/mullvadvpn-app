e0d0422bfc1a6c54e5981cb6797a59e96083d679
Andrej Mihajlov <and@mullvad.net>
2019-04-02 20:32:57 +0200

    Generate cities.pot, countries.pot and fix integration script to copy translations to locales folder

--- gui/geo-data/integrate-into-app.py
+++ gui/geo-data/integrate-into-app.py
@@ -7,23 +7,38 @@ import shutil
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 SOURCE_DIR = os.path.join(SCRIPT_DIR, "out")
-DESTINATION_DIR = os.path.realpath(os.path.join(SCRIPT_DIR, "../assets/geo"))
+GEO_ASSETS_DEST_DIR = os.path.realpath(os.path.join(SCRIPT_DIR, "../assets/geo"))
+TRANSLATIONS_DEST_DIR = os.path.realpath(os.path.join(SCRIPT_DIR, "../locales"))
 
-FILES_TO_COPY = [
+GEO_ASSETS_TO_COPY = [
   "cities.rbush.json",
   "countries.rbush.json",
   "geometry.json",
   "geometry.rbush.json",
   "states-provinces-lines.json",
-  "states-provinces-lines.rbush.json"
+  "states-provinces-lines.rbush.json",
 ]
 
-if not os.path.exists(DESTINATION_DIR):
-  os.makedirs(DESTINATION_DIR)
+TRANSLATIONS_TO_COPY = [
+  "countries.pot",
+  "cities.pot",
+]
 
-for f in FILES_TO_COPY:
+if not os.path.exists(GEO_ASSETS_DEST_DIR):
+  os.makedirs(GEO_ASSETS_DEST_DIR)
+
+for f in GEO_ASSETS_TO_COPY:
   src = os.path.join(SOURCE_DIR, f)
-  dst = os.path.join(DESTINATION_DIR, f)
+  dst = os.path.join(GEO_ASSETS_DEST_DIR, f)
+  prefix_len = len(os.path.commonprefix((src, dst)))
+
+  print "Copying {} to {}".format(src[prefix_len:], dst[prefix_len:])
+
+  shutil.copyfile(src, dst)
+
+for f in TRANSLATIONS_TO_COPY:
+  src = os.path.join(SOURCE_DIR, f)
+  dst = os.path.join(TRANSLATIONS_DEST_DIR, f)
   prefix_len = len(os.path.commonprefix((src, dst)))
 
   print "Copying {} to {}".format(src[prefix_len:], dst[prefix_len:])
