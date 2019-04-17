46903298d84840da499fd9e72d8c1daba854877b
Odd Stranne <odd@mullvad.net>
2019-04-17 13:55:55 +0200

    Update 'winnet' loader used for testing

--- windows/winnet/src/extras/loader/loader.cpp
+++ windows/winnet/src/extras/loader/loader.cpp
@@ -1,9 +1,26 @@
 #include "stdafx.h"
 #include "../../winnet/winnet.h"
+#include <iostream>
 
 int main()
 {
-	const auto status = WinNet_GetTapInterfaceIpv6Status(nullptr, nullptr);
+	wchar_t *alias = nullptr;
+
+	const auto status = WinNet_GetTapInterfaceAlias(&alias, nullptr, nullptr);
+
+	switch (status)
+	{
+		case WINNET_GTIA_STATUS::FAILURE:
+		{
+			std::wcout << L"Could not determine alias" << std::endl;
+			break;
+		}
+		case WINNET_GTIA_STATUS::SUCCESS:
+		{
+			std::wcout << L"Interface alias: " << alias << std::endl;
+			WinNet_ReleaseString(alias);
+		}
+	};
 
     return 0;
 }
