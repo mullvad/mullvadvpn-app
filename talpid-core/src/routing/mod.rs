47f1a2566b12c6a61674e389c09dcecc4b7d2040
Emīls Piņķis <emils@mullvad.net>
2019-04-22 23:00:29 +0100

    Change route manager to be active rather than passive

--- talpid-core/src/routing/mod.rs
+++ talpid-core/src/routing/mod.rs
@@ -1,107 +1,179 @@
+use futures::{sync::oneshot, Future};
 use ipnetwork::IpNetwork;
-use std::net::IpAddr;
+use std::{collections::HashMap, net::IpAddr};
+use tokio_executor::Executor;
 
 #[cfg(target_os = "macos")]
 #[path = "macos.rs"]
 mod imp;
 
 #[cfg(target_os = "linux")]
-#[path = "linux.rs"]
+#[path = "linux/mod.rs"]
 mod imp;
 
 #[cfg(target_os = "android")]
 #[path = "android.rs"]
 mod imp;
 
-pub use self::imp::Error;
+pub use imp::Error as PlatformError;
 
-#[cfg(any(target_os = "macos", target_os = "linux"))]
-mod subprocess;
-
-/// A single route
-#[derive(Hash, Eq, PartialEq)]
-pub struct Route {
-    /// Route prefix
-    pub prefix: IpNetwork,
-    /// Route node
-    pub node: NetNode,
+/// Errors that can be encountered whilst initializing RouteManager
+#[derive(err_derive::Error, Debug)]
+pub enum Error {
+    /// Platform sepcific error occured
+    #[error(display = "Failed to create route manager")]
+    FailedToInitializeManager(#[error(cause)] imp::Error),
+    /// Failed to spawn route manager future
+    #[error(display = "Failed to spawn route manager on the provided executor")]
+    FailedToSpawnManager,
 }
 
-impl Route {
-    /// Create a new route
-    pub fn new(prefix: IpNetwork, node: NetNode) -> Self {
-        Self { prefix, node }
-    }
-}
-
-/// A network node for a given route
-#[derive(Hash, Eq, PartialEq, Clone)]
-pub enum NetNode {
-    /// For routing something through a network host
-    Address(IpAddr),
-    /// For routing something through an interface
-    Device(String),
-}
-
-/// Contains a set of routes to be added
-pub struct RequiredRoutes {
-    /// List of routes to be applied to the routing table.
-    pub routes: Vec<Route>,
-}
-
-/// Manages adding and removing routes from the routing table.
+/// RouteManager applies a set of routes to the route table.
+/// If a destination has to be routed through the default node,
+/// the route will be adjusted dynamically when the default route changes.
 pub struct RouteManager {
-    inner: imp::RouteManager,
+    tx: Option<oneshot::Sender<oneshot::Sender<()>>>,
 }
 
 impl RouteManager {
-    /// Creates a new RouteManager.
-    pub fn new() -> Result<Self, Error> {
-        Ok(RouteManager {
-            inner: imp::RouteManager::new()?,
-        })
+    /// Constructs a RouteManager and applies the required routes.
+    /// Takes a map of network destinations and network nodes as an argument, and applies said
+    /// routes.
+    pub fn new(
+        required_routes: HashMap<IpNetwork, NetNode>,
+        exec: &mut impl Executor,
+    ) -> Result<Self, Error> {
+        let (tx, rx) = oneshot::channel();
+
+
+        let route_manager = imp::RouteManagerImpl::new(required_routes, rx)
+            .map_err(Error::FailedToInitializeManager)?;
+        exec.spawn(Box::new(
+            route_manager.map_err(|e| log::error!("Routing manager failed - {}", e)),
+        ))
+        .map_err(|_| Error::FailedToSpawnManager)?;
+
+        Ok(Self { tx: Some(tx) })
     }
 
-    /// Set routes in the routing table.
-    pub fn add_routes(&mut self, required_routes: RequiredRoutes) -> Result<(), Error> {
-        self.inner.add_routes(required_routes)
-    }
+    /// Stops RouteManager and removes all of the applied routes.
+    pub fn stop(&mut self) {
+        if let Some(tx) = self.tx.take() {
+            let (wait_tx, wait_rx) = oneshot::channel();
+            if let Err(_) = tx.send(wait_tx) {
+                log::error!("RouteManager already down!");
+                return;
+            }
 
-    /// Remove previously set routes from the routing table.
-    pub fn delete_routes(&mut self) -> Result<(), Error> {
-        self.inner.delete_routes()
-    }
-
-    /// Retrieves the gateway for the default route.
-    pub fn get_default_route_node(&mut self) -> Result<std::net::IpAddr, Error> {
-        // use routing::RoutingT;
-        self.inner.get_default_route_node()
+            if let Err(_) = wait_rx.wait() {
+                log::error!("RouteManager paniced while shutting down");
+            }
+        }
     }
 }
 
 impl Drop for RouteManager {
     fn drop(&mut self) {
-        if let Err(e) = self.delete_routes() {
-            log::error!("Failed to reset routes on drop - {}", e);
+        self.stop();
+    }
+}
+
+
+/// A netowrk route with a specific network node, destinaiton and an optional metric.
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+struct Route {
+    node: Node,
+    prefix: IpNetwork,
+    metric: Option<u32>,
+}
+
+impl Route {
+    fn new(node: Node, prefix: IpNetwork) -> Self {
+        Self {
+            node,
+            prefix,
+            metric: None,
         }
     }
 }
 
-/// This trait unifies platform specific implementations of route managers
-pub trait RoutingT: Sized {
-    /// Error type of the implementation
-    type Error: std::error::Error;
+/// A network route that should be applied by the RouteManager.
+/// It can either be routed through a specific network node or it can be routed through the current
+/// default route.
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+pub struct RequiredRoute {
+    prefix: IpNetwork,
+    node: NetNode,
+}
 
-    /// Creates a new router
-    fn new() -> Result<Self, Self::Error>;
+impl RequiredRoute {
+    /// Constructs a new required route.
+    pub fn new(prefix: IpNetwork, node: impl Into<NetNode>) -> Self {
+        Self {
+            node: node.into(),
+            prefix,
+        }
+    }
+}
 
-    /// Adds routes to the system routing table.
-    fn add_routes(&mut self, required_routes: RequiredRoutes) -> Result<(), Self::Error>;
+/// A NetNode represents a network node - either a real one or a symbolic default one.
+/// A route with a symbolic default node will be changed whenever a new default route is created.
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+pub enum NetNode {
+    /// A real node will be used to set a regular route that will remain unchanged for the lifetime
+    /// of the RouteManager
+    RealNode(Node),
+    /// A default node is a symbolic node that will resolve to the network node used in the current
+    /// most preferable default route
+    DefaultNode,
+}
 
-    /// Removes previously set routes. If routes were set for specific tables, the whole tables
-    /// will be removed.
-    fn delete_routes(&mut self) -> Result<(), Self::Error>;
+impl From<Node> for NetNode {
+    fn from(node: Node) -> NetNode {
+        NetNode::RealNode(node)
+    }
+}
 
-    /// Retrieves the gateway for the default route
-    fn get_default_route_node(&mut self) -> Result<std::net::IpAddr, Self::Error>;
+/// Node represents a real network node - it can be identified by a network interface name, an IP
+/// address or both.
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+pub struct Node {
+    ip: Option<IpAddr>,
+    device: Option<String>,
+}
+
+impl Node {
+    /// Construct an Node with both an IP address and an interface name.
+    pub fn new(address: IpAddr, iface_name: String) -> Self {
+        Self {
+            ip: Some(address),
+            device: Some(iface_name),
+        }
+    }
+
+    /// Construct an Node from an IP address.
+    pub fn address(address: IpAddr) -> Node {
+        Self {
+            ip: Some(address),
+            device: None,
+        }
+    }
+
+    /// Construct a Node from a network interface name.
+    pub fn device(iface_name: String) -> Node {
+        Self {
+            ip: None,
+            device: Some(iface_name),
+        }
+    }
+
+    /// Retrieve a node's IP address
+    pub fn get_address(&self) -> Option<IpAddr> {
+        self.ip
+    }
+
+    /// Retrieve a node's network interface name
+    pub fn get_device(&self) -> Option<&str> {
+        self.device.as_ref().map(|s| s.as_ref())
+    }
 }
