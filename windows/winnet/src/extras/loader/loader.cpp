b2937f7ae36e1d86a8b3cc68fe85664d6e1c03d7
Odd Stranne <odd@mullvad.net>
2019-05-10 13:19:38 +0200

    Update WINNET testing app

--- windows/winnet/src/extras/loader/loader.cpp
+++ windows/winnet/src/extras/loader/loader.cpp
@@ -2,25 +2,39 @@
 #include "../../winnet/winnet.h"
 #include <iostream>
 
+void __stdcall ConnectivityChanged(uint8_t connected)
+{
+	std::wcout << (0 != connected? L"Connected" : L"NOT connected") << std::endl;
+}
+
 int main()
 {
-	wchar_t *alias = nullptr;
+	//wchar_t *alias = nullptr;
 
-	const auto status = WinNet_GetTapInterfaceAlias(&alias, nullptr, nullptr);
+	//const auto status = WinNet_GetTapInterfaceAlias(&alias, nullptr, nullptr);
 
-	switch (status)
-	{
-		case WINNET_GTIA_STATUS::FAILURE:
-		{
-			std::wcout << L"Could not determine alias" << std::endl;
-			break;
-		}
-		case WINNET_GTIA_STATUS::SUCCESS:
-		{
-			std::wcout << L"Interface alias: " << alias << std::endl;
-			WinNet_ReleaseString(alias);
-		}
-	};
+	//switch (status)
+	//{
+	//	case WINNET_GTIA_STATUS::FAILURE:
+	//	{
+	//		std::wcout << L"Could not determine alias" << std::endl;
+	//		break;
+	//	}
+	//	case WINNET_GTIA_STATUS::SUCCESS:
+	//	{
+	//		std::wcout << L"Interface alias: " << alias << std::endl;
+	//		WinNet_ReleaseString(alias);
+	//	}
+	//};
+
+	uint8_t currentConnectivity = 0;
+
+	const auto status = WinNet_ActivateConnectivityMonitor(ConnectivityChanged, &currentConnectivity, nullptr, nullptr);
+
+	std::wcout << L"Current connectivity: "
+		<< (0 != currentConnectivity ? L"Connected" : L"NOT connected") << std::endl;
+
+	_getwch();
 
     return 0;
 }
