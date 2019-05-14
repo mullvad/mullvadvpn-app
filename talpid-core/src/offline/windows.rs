cb3c0d012da194b39d19e5f6a47741b78c417599
Emīls Piņķis <emils@mullvad.net>
2019-05-14 18:35:28 +0100

    Add new windows connectivity checks to daemon

--- talpid-core/src/offline/windows.rs
+++ talpid-core/src/offline/windows.rs
@@ -6,17 +6,20 @@
 //! GNU General Public License as published by the Free Software Foundation, either version 3 of
 //! the License, or (at your option) any later version.
 
-use crate::tunnel_state_machine::TunnelCommand;
+use crate::{tunnel_state_machine::TunnelCommand, winnet};
 use futures::sync::mpsc::UnboundedSender;
-use log::debug;
+use parking_lot::Mutex;
 use std::{
     ffi::c_void,
     io,
     mem::zeroed,
     os::windows::io::{IntoRawHandle, RawHandle},
-    ptr, thread,
+    ptr,
+    sync::Arc,
+    thread,
     time::Duration,
 };
+use talpid_types::ErrorExt;
 use winapi::{
     shared::{
         basetsd::LONG_PTR,
@@ -46,32 +49,61 @@ const REQUEST_THREAD_SHUTDOWN: UINT = WM_USER + 1;
 pub enum Error {
     #[error(display = "Unable to create listener thread")]
     ThreadCreationError(#[error(cause)] io::Error),
+    #[error(display = "Failed to start connectivity monitor")]
+    ConnectivityMonitorError,
 }
 
 
 pub struct BroadcastListener {
     thread_handle: RawHandle,
     thread_id: DWORD,
+    _system_state: Arc<Mutex<SystemState>>,
 }
 
 unsafe impl Send for BroadcastListener {}
 
 impl BroadcastListener {
-    pub fn start<F>(client_callback: F) -> Result<Self, Error>
-    where
-        F: Fn(UINT, WPARAM, LPARAM) + 'static + Send,
-    {
+    pub fn start(sender: UnboundedSender<TunnelCommand>) -> Result<Self, Error> {
+        let mut system_state = Arc::new(Mutex::new(SystemState {
+            network_connectivity: false,
+            suspended: false,
+            daemon_channel: sender,
+        }));
+
+        let power_broadcast_state_ref = system_state.clone();
+
+        let power_broadcast_callback = move |message: UINT, wparam: WPARAM, _lparam: LPARAM| {
+            let state = power_broadcast_state_ref.clone();
+            if message == WM_POWERBROADCAST {
+                if wparam == PBT_APMSUSPEND {
+                    log::debug!("Machine is preparing to enter sleep mode");
+                    apply_system_state_change(state, StateChange::Suspended(true));
+                } else if wparam == PBT_APMRESUMEAUTOMATIC {
+                    log::debug!("Machine is returning from sleep mode");
+                    thread::spawn(move || {
+                        // TAP will be unavailable for approximately 2 seconds on a healthy machine.
+                        thread::sleep(Duration::from_secs(5));
+                        log::debug!("TAP is presumed to have been re-initialized");
+                        apply_system_state_change(state, StateChange::Suspended(false));
+                    });
+                }
+            }
+        };
+
         let join_handle = thread::Builder::new()
             .spawn(move || unsafe {
-                Self::message_pump(client_callback);
+                Self::message_pump(power_broadcast_callback);
             })
             .map_err(Error::ThreadCreationError)?;
 
         let real_handle = join_handle.into_raw_handle();
 
+        unsafe { Self::setup_network_connectivity_listener(&mut system_state)? };
+
         Ok(BroadcastListener {
             thread_handle: real_handle,
             thread_id: unsafe { GetThreadId(real_handle) },
+            _system_state: system_state,
         })
     }
 
@@ -156,6 +188,33 @@ impl BroadcastListener {
 
         DefWindowProcW(window, message, wparam, lparam)
     }
+
+    /// The caller must make sure the `system_state` reference is valid
+    /// until after `WinNet_DeactivateConnectivityMonitor` has been called.
+    unsafe fn setup_network_connectivity_listener(
+        system_state: &Mutex<SystemState>,
+    ) -> Result<(), Error> {
+        let callback_context = system_state as *const _ as *mut libc::c_void;
+        let mut state = system_state.lock();
+        let mut current_connectivity = true;
+        if !winnet::WinNet_ActivateConnectivityMonitor(
+            Some(Self::connectivity_callback),
+            callback_context,
+            &mut current_connectivity as *mut _,
+            Some(winnet::error_sink),
+            ptr::null_mut(),
+        ) {
+            return Err(Error::ConnectivityMonitorError);
+        }
+        state.network_connectivity = current_connectivity;
+        Ok(())
+    }
+
+    unsafe extern "system" fn connectivity_callback(connectivity: bool, context: *mut c_void) {
+        let state_lock: &mut Mutex<SystemState> = &mut *(context as *mut _);
+        let mut state = state_lock.lock();
+        state.apply_change(StateChange::NetworkConnectivity(connectivity));
+    }
 }
 
 impl Drop for BroadcastListener {
@@ -164,35 +223,74 @@ impl Drop for BroadcastListener {
             PostThreadMessageW(self.thread_id, REQUEST_THREAD_SHUTDOWN, 0, 0);
             WaitForSingleObject(self.thread_handle, INFINITE);
             CloseHandle(self.thread_handle);
+            if !winnet::WinNet_DeactivateConnectivityMonitor() {
+                log::error!("Failed to deactivate connectivity monitor");
+            }
         }
+    }
+}
+
+#[derive(Debug)]
+enum StateChange {
+    NetworkConnectivity(bool),
+    Suspended(bool),
+}
+
+struct SystemState {
+    network_connectivity: bool,
+    suspended: bool,
+    daemon_channel: UnboundedSender<TunnelCommand>,
+}
+
+impl SystemState {
+    fn apply_change(&mut self, change: StateChange) {
+        let old_state = self.is_offline_currently();
+        match change {
+            StateChange::NetworkConnectivity(connectivity) => {
+                self.network_connectivity = connectivity;
+            }
+
+            StateChange::Suspended(suspended) => {
+                self.suspended = suspended;
+            }
+        };
+
+        let new_state = self.is_offline_currently();
+        if old_state != new_state {
+            if let Err(e) = self
+                .daemon_channel
+                .unbounded_send(TunnelCommand::IsOffline(new_state))
+            {
+                log::error!("Failed to send new offline state to daemon: {}", e);
+            }
+        }
+    }
+
+    fn is_offline_currently(&self) -> bool {
+        !self.network_connectivity || self.suspended
     }
 }
 
 pub type MonitorHandle = BroadcastListener;
 
 pub fn spawn_monitor(sender: UnboundedSender<TunnelCommand>) -> Result<MonitorHandle, Error> {
-    let listener =
-        BroadcastListener::start(move |message: UINT, wparam: WPARAM, _lparam: LPARAM| {
-            if message == WM_POWERBROADCAST {
-                if wparam == PBT_APMSUSPEND {
-                    debug!("Machine is preparing to enter sleep mode");
-                    let _ = sender.unbounded_send(TunnelCommand::IsOffline(true));
-                } else if wparam == PBT_APMRESUMEAUTOMATIC {
-                    debug!("Machine is returning from sleep mode");
-                    let cloned_sender = sender.clone();
-                    thread::spawn(move || {
-                        // TAP will be unavailable for approximately 2 seconds on a healthy machine.
-                        thread::sleep(Duration::from_secs(5));
-                        debug!("TAP is presumed to have been re-initialized");
-                        let _ = cloned_sender.unbounded_send(TunnelCommand::IsOffline(false));
-                    });
-                }
-            }
-        })?;
+    BroadcastListener::start(sender)
+}
 
-    Ok(listener)
+fn apply_system_state_change(state: Arc<Mutex<SystemState>>, change: StateChange) {
+    let mut state = state.lock();
+    state.apply_change(change);
 }
 
 pub fn is_offline() -> bool {
-    false
+    match winnet::is_offline() {
+        Ok(state) => state,
+        Err(e) => {
+            log::error!(
+                "{}",
+                e.display_chain_with_msg("Failed to get current connectivity")
+            );
+            false
+        }
+    }
 }
