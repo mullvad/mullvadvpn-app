b4ebcb3217066263af9aa95f8ac592b99c4dced2
Odd Stranne <odd@mullvad.net>
2019-05-15 14:17:08 +0200

    Use 'bool' and handle all cases in 'WinNet_CheckConnectivity'

--- windows/winnet/src/winnet/winnet.h
+++ windows/winnet/src/winnet/winnet.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <cstdint>
+#include <stdint.h>
+#include <stdbool.h>
 
 #ifdef WINNET_EXPORTS
 #define WINNET_LINKAGE __declspec(dllexport)
@@ -25,7 +26,7 @@ WINNET_API
 WinNet_EnsureTopMetric(
 	const wchar_t *deviceAlias,
 	WinNetErrorSink errorSink,
-	void* errorSinkContext
+	void *errorSinkContext
 );
 
 enum class WINNET_GTII_STATUS : uint32_t
@@ -41,23 +42,17 @@ WINNET_GTII_STATUS
 WINNET_API
 WinNet_GetTapInterfaceIpv6Status(
 	WinNetErrorSink errorSink,
-	void* errorSinkContext
+	void *errorSinkContext
 );
-
-enum class WINNET_GTIA_STATUS : uint32_t
-{
-	SUCCESS = 0,
-	FAILURE = 1,
-};
 
 extern "C"
 WINNET_LINKAGE
-WINNET_GTIA_STATUS
+bool
 WINNET_API
 WinNet_GetTapInterfaceAlias(
 	wchar_t **alias,
 	WinNetErrorSink errorSink,
-	void* errorSinkContext
+	void *errorSinkContext
 );
 
 //
@@ -72,24 +67,18 @@ WinNet_ReleaseString(
 	wchar_t *str
 );
 
-typedef void (WINNET_API *WinNetConnectivityMonitorCallback)(uint8_t connected, void* context);
-
-enum class WINNET_ACM_STATUS : uint32_t
-{
-	SUCCESS = 0,
-	FAILURE = 1,
-};
+typedef void (WINNET_API *WinNetConnectivityMonitorCallback)(bool connected, void *context);
 
 extern "C"
 WINNET_LINKAGE
-WINNET_ACM_STATUS
+bool
 WINNET_API
 WinNet_ActivateConnectivityMonitor(
 	WinNetConnectivityMonitorCallback callback,
-	void* callbackContext,
-	uint8_t *currentConnectivity,
+	void *callbackContext,
+	bool *currentConnectivity,
 	WinNetErrorSink errorSink,
-	void* errorSinkContext
+	void *errorSinkContext
 );
 
 extern "C"
@@ -99,10 +88,18 @@ WINNET_API
 WinNet_DeactivateConnectivityMonitor(
 );
 
+enum class WINNET_CC_STATUS : uint32_t
+{
+	NOT_CONNECTED = 0,
+	CONNECTED = 1,
+	CONNECTIVITY_UNKNOWN = 2,
+};
+
 extern "C"
 WINNET_LINKAGE
-uint8_t
+WINNET_CC_STATUS
 WINNET_API
 WinNet_CheckConnectivity(
+	WinNetErrorSink errorSink,
+	void *errorSinkContext
 );
-
