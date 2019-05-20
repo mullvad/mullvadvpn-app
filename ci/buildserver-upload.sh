02bdf177645bee52caf59fb7b67879206b7790b7
Linus Färnstrand <linus@mullvad.net>
2019-05-20 17:04:45 +0200

    Verify checksums in upload script

--- ci/buildserver-upload.sh
+++ ci/buildserver-upload.sh
@@ -7,12 +7,16 @@ shopt -s nullglob
 
 cd $UPLOAD_DIR
 
-while true;
-do
+while true; do
   sleep 10
-  for f in MullvadVPN-*;
-  do
-    sleep 10
+  for f_checksum in MullvadVPN-*.sha256; do
+    sleep 1
+    f="${f_checksum/.sha256/}"
+    if ! sha256sum --quiet -c "$f_checksum"; then
+      echo "Failed to verify checksum for $f"
+      continue
+    fi
+
     version=$(echo $f | sed -Ee 's/MullvadVPN-(.*)(\.exe|\.pkg|_amd64\.deb|_x86_64\.rpm)/\1/g')
     ssh build.mullvad.net mkdir -p "app/$version" || continue
     scp -B "$f" build.mullvad.net:app/$version/ || continue
@@ -20,6 +24,6 @@ do
     rm -f "$f.asc"
     gpg -u A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF --pinentry-mode loopback --sign --armor --detach-sign "$f"
     scp -B "$f.asc" build.mullvad.net:app/$version/ || true
-    yes | rm "$f" "$f.asc"
+    yes | rm "$f" "$f_checksum" "$f.asc"
   done
 done
