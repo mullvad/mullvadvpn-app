860dbebbfe49226508765dc04072104971fa41a2
Andrej Mihajlov <and@mullvad.net>
2019-05-21 18:52:27 +0200

    Scope colors into UIColor sub-structs

--- ios/MullvadVPN/UIColor+Palette.swift
+++ ios/MullvadVPN/UIColor+Palette.swift
@@ -31,13 +31,16 @@ extension UIColor {
     }
 
     // Relay availability indicator view
-    static let relayStatusIndicatorActiveColor = UIColor(red: 0.27, green: 0.68, blue: 0.30, alpha: 0.9)
-    static let relayStatusIndicatorInactiveColor = UIColor(red: 0.82, green: 0.01, blue: 0.11, alpha: 0.95)
+    struct RelayStatusIndicator {
+        static let activeColor = UIColor(red: 0.27, green: 0.68, blue: 0.30, alpha: 0.9)
+        static let inactiveColor = UIColor(red: 0.82, green: 0.01, blue: 0.11, alpha: 0.95)
+    }
 
     // Cells
-    static let cellBackgroundColor = UIColor(red: 0.16, green: 0.30, blue: 0.45, alpha: 1.0)
-    static let subCellBackgroundColor = UIColor(red:0.15, green:0.23, blue:0.33, alpha:1.0)
-    static let subSubCellBackgroundColor = UIColor(red:0.13, green:0.20, blue:0.30, alpha:1.0)
-
-    static let cellSelectedBackgroundColor = UIColor(red: 0.27, green: 0.68, blue: 0.30, alpha: 1.0)
+    struct Cell {
+        static let backgroundColor = UIColor(red: 0.16, green: 0.30, blue: 0.45, alpha: 1.0)
+        static let selectedBackgroundColor = UIColor(red: 0.27, green: 0.68, blue: 0.30, alpha: 1.0)
+        static let subCellBackgroundColor = UIColor(red:0.15, green:0.23, blue:0.33, alpha:1.0)
+        static let subSubCellBackgroundColor = UIColor(red:0.13, green:0.20, blue:0.30, alpha:1.0)
+    }
 }
