3f10c7f16434441ce0bdd0cfaf28cf46ef4bb3ce
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-05-21 14:32:10 +0000

    Include API root certificate

--- android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/MainActivity.kt
@@ -1,18 +1,36 @@
 package net.mullvad.mullvadvpn
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 
 class MainActivity : FragmentActivity() {
+    private val extractApiRootCaFile = doExtractApiRootCaFile()
+
+    val activityCreated = CompletableDeferred<Unit>()
+
     var selectedRelayItemCode: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
 
+        activityCreated.complete(Unit)
+
         if (savedInstanceState == null) {
             addInitialFragment()
         }
+    }
+
+    override fun onDestroy() {
+        extractApiRootCaFile.cancel()
+
+        super.onDestroy()
     }
 
     private fun addInitialFragment() {
@@ -20,5 +38,10 @@ class MainActivity : FragmentActivity() {
             add(R.id.main_fragment, LoginFragment())
             commit()
         }
+    }
+
+    private fun doExtractApiRootCaFile() = GlobalScope.launch(Dispatchers.Default) {
+        activityCreated.await()
+        ApiRootCaFile().extract(this@MainActivity)
     }
 }
