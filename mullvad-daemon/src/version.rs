4625ca12f83eb32d198c5a7ac691c0039889fe51
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-05-22 11:41:16 +0000

    Move `log_version` function to `version` module

--- mullvad-daemon/src/version.rs
+++ mullvad-daemon/src/version.rs
@@ -3,3 +3,12 @@ pub const PRODUCT_VERSION: &str = include_str!(concat!(env!("OUT_DIR"), "/produc
 
 /// Contains the date of the git commit this was built from
 pub const COMMIT_DATE: &str = include_str!(concat!(env!("OUT_DIR"), "/git-commit-date.txt"));
+
+pub fn log_version() {
+    log::info!(
+        "Starting {} - {} {}",
+        env!("CARGO_PKG_NAME"),
+        PRODUCT_VERSION,
+        COMMIT_DATE,
+    )
+}
