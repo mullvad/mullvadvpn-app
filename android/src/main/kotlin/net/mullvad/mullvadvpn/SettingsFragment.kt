1b9b23c0a3900daa07c055b7663b1880900165ba
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-06-05 11:22:12 +0000

    Add close button to Settings fragment

--- android/src/main/kotlin/net/mullvad/mullvadvpn/SettingsFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/SettingsFragment.kt
@@ -4,12 +4,22 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 
 class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = inflater.inflate(R.layout.settings, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.settings, container, false)
+
+        view.findViewById<ImageButton>(R.id.close).setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        return view
+    }
 }
