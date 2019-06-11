453665c7f4f9e282a4cb3e330847a06ee98fd046
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-06-11 19:06:29 +0000

    Show account number in Account screen

--- android/src/main/kotlin/net/mullvad/mullvadvpn/AccountFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/AccountFragment.kt
@@ -1,12 +1,31 @@
 package net.mullvad.mullvadvpn
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class AccountFragment : Fragment() {
+    private lateinit var parentActivity: MainActivity
+    private lateinit var accountNumberContainer: View
+    private lateinit var accountNumberDisplay: TextView
+
+    private var updateViewJob: Job? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        parentActivity = context as MainActivity
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,9 +34,24 @@ class AccountFragment : Fragment() {
         val view = inflater.inflate(R.layout.account, container, false)
 
         view.findViewById<View>(R.id.back).setOnClickListener {
-            activity?.onBackPressed()
+            parentActivity.onBackPressed()
         }
 
+        accountNumberContainer = view.findViewById<View>(R.id.account_number_container)
+        accountNumberDisplay = view.findViewById<TextView>(R.id.account_number_display)
+
+        updateViewJob = updateView()
+
         return view
+    }
+
+    private fun updateView() = GlobalScope.launch(Dispatchers.Main) {
+        val accountCache = parentActivity.accountCache
+        val accountNumber = accountCache.accountNumber.await()
+
+        if (accountNumber != null) {
+            accountNumberDisplay.setText(accountCache.accountNumber.await())
+            accountNumberContainer.visibility = View.VISIBLE
+        }
     }
 }
