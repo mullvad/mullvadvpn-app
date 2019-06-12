54c9528d87ce702090ad9ddd7cacc6d9af4842af
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-06-12 11:50:11 +0000

    Show account expiry in Account screen

--- android/src/main/kotlin/net/mullvad/mullvadvpn/AccountFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/AccountFragment.kt
@@ -1,5 +1,7 @@
 package net.mullvad.mullvadvpn
 
+import java.text.DateFormat
+
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -13,8 +15,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
+import org.joda.time.DateTime
+
 class AccountFragment : Fragment() {
     private lateinit var parentActivity: MainActivity
+
+    private lateinit var accountExpiryContainer: View
+    private lateinit var accountExpiryDisplay: TextView
     private lateinit var accountNumberContainer: View
     private lateinit var accountNumberDisplay: TextView
 
@@ -37,7 +44,10 @@ class AccountFragment : Fragment() {
             parentActivity.onBackPressed()
         }
 
+        accountExpiryContainer = view.findViewById<View>(R.id.account_expiry_container)
         accountNumberContainer = view.findViewById<View>(R.id.account_number_container)
+
+        accountExpiryDisplay = view.findViewById<TextView>(R.id.account_expiry_display)
         accountNumberDisplay = view.findViewById<TextView>(R.id.account_number_display)
 
         updateViewJob = updateView()
@@ -52,6 +62,20 @@ class AccountFragment : Fragment() {
         if (accountNumber != null) {
             accountNumberDisplay.setText(accountCache.accountNumber.await())
             accountNumberContainer.visibility = View.VISIBLE
+
+            val accountExpiry = accountCache.accountExpiry.await()
+
+            if (accountExpiry != null) {
+                accountExpiryDisplay.setText(formatExpiry(accountExpiry))
+                accountExpiryContainer.visibility = View.VISIBLE
+            }
         }
+    }
+
+    private fun formatExpiry(expiry: DateTime): String {
+        val expiryInstant = expiry.toDate()
+        val formatter = DateFormat.getDateTimeInstance()
+
+        return formatter.format(expiryInstant)
     }
 }
