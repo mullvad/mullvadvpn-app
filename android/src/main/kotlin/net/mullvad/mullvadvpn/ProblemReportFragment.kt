95a3ab923f48a779b2b0ba97f1e02461edf13e55
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-06-18 19:51:12 +0000

    Implement back to settings button behavior

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ProblemReportFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ProblemReportFragment.kt
@@ -3,6 +3,7 @@ package net.mullvad.mullvadvpn
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 
 import org.joda.time.DateTime
@@ -12,5 +13,13 @@ class ProblemReportFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = inflater.inflate(R.layout.problem_report, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.problem_report, container, false)
+
+        view.findViewById<View>(R.id.back).setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        return view
+    }
 }
