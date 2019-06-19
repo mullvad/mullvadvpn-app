9a786068b32438be6ace0150038b8eb84059a9cc
Andrej Mihajlov <and@mullvad.net>
2019-06-19 11:36:21 +0200

    Add missing localizations and refine UI text

--- gui/src/renderer/components/ConnectionInfo.tsx
+++ gui/src/renderer/components/ConnectionInfo.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Component, Styles, Text, Types, View } from 'reactxp';
+import { sprintf } from 'sprintf-js';
 import { proxyTypeToString, TunnelType, tunnelTypeToString } from '../../shared/daemon-rpc-types';
 import { messages } from '../../shared/gettext';
 import { default as ConnectionInfoDisclosure } from './ConnectionInfoDisclosure';
@@ -16,7 +17,6 @@ const styles = {
     fontWeight: '600',
     color: 'rgb(255, 255, 255)',
     flex: 0,
-    flexBasis: 30,
     marginRight: 8,
   }),
   value: Styles.createTextStyle({
@@ -29,14 +29,6 @@ const styles = {
   header: Styles.createViewStyle({
     flexDirection: 'row',
     alignItems: 'center',
-  }),
-  hostname: Styles.createTextStyle({
-    fontFamily: 'Open Sans',
-    fontSize: 16,
-    lineHeight: 20,
-    fontWeight: '600',
-    color: 'rgb(255, 255, 255)',
-    flex: 1,
   }),
 };
 
@@ -78,28 +70,26 @@ export default class ConnectionInfo extends Component<IProps, IState> {
 
   public render() {
     const { inAddress, outAddress, bridgeInfo } = this.props;
-    const transportLine =
-      (inAddress ? tunnelTypeToString(inAddress.tunnelType) : '') +
-      (bridgeInfo && inAddress ? ' via ' + proxyTypeToString(bridgeInfo.bridgeType) : '');
-
     const entryPoint = bridgeInfo && inAddress ? bridgeInfo : inAddress;
 
     return (
       <View style={this.props.style}>
-        <View style={styles.header}>
-          <ConnectionInfoDisclosure defaultOpen={this.props.defaultOpen} onToggle={this.onToggle}>
-            <Text style={styles.hostname}>
-              {this.props.hostname || ''}{' '}
-              {this.props.bridgeHostname ? `via ${this.props.bridgeHostname}` : ''}
-            </Text>
-          </ConnectionInfoDisclosure>
-        </View>
+        {this.props.hostname && (
+          <View style={styles.header}>
+            <ConnectionInfoDisclosure defaultOpen={this.props.defaultOpen} onToggle={this.onToggle}>
+              {this.hostnameLine()}
+            </ConnectionInfoDisclosure>
+          </View>
+        )}
 
-        {this.state.isOpen && (
+        {this.state.isOpen && this.props.hostname && (
           <React.Fragment>
             {this.props.inAddress && (
-              <View style={styles.row}>{<Text style={styles.value}>{transportLine}</Text>}</View>
+              <View style={styles.row}>
+                <Text style={styles.value}>{this.transportLine()}</Text>
+              </View>
             )}
+
             {entryPoint && (
               <View style={styles.row}>
                 <Text style={styles.caption}>{messages.pgettext('connection-info', 'In')}</Text>
@@ -122,6 +112,52 @@ export default class ConnectionInfo extends Component<IProps, IState> {
         )}
       </View>
     );
+  }
+
+  private hostnameLine() {
+    if (this.props.hostname && this.props.bridgeHostname) {
+      return sprintf(
+        // TRANSLATORS: The hostname line displayed below the country on the main screen
+        // TRANSLATORS: Available placeholders:
+        // TRANSLATORS: %(relay)s - the relay hostname
+        // TRANSLATORS: %(bridge)s - the bridge hostname
+        messages.pgettext('connection-info', '%(relay)s via %(bridge)s'),
+        {
+          relay: this.props.hostname,
+          bridge: this.props.bridgeHostname,
+        },
+      );
+    } else {
+      return this.props.hostname || '';
+    }
+  }
+
+  private transportLine() {
+    const { inAddress, bridgeInfo } = this.props;
+
+    if (inAddress) {
+      const tunnelType = tunnelTypeToString(inAddress.tunnelType);
+
+      if (bridgeInfo) {
+        const bridgeType = proxyTypeToString(bridgeInfo.bridgeType);
+
+        return sprintf(
+          // TRANSLATORS: The tunnel type line displayed below the hostname line on the main screen
+          // TRANSLATORS: Available placeholders:
+          // TRANSLATORS: %(tunnelType)s - the tunnel type, i.e OpenVPN
+          // TRANSLATORS: %(bridgeType)s - the bridge type, i.e Shadowsocks
+          messages.pgettext('connection-info', '%(tunnelType)s via %(bridgeType)s'),
+          {
+            tunnelType,
+            bridgeType,
+          },
+        );
+      } else {
+        return tunnelType;
+      }
+    } else {
+      return '';
+    }
   }
 
   private onToggle = (isOpen: boolean) => {
