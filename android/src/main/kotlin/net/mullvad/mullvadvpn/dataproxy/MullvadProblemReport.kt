c4ecd93d4fd3dea702e8876f25fea0babddba380
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-06-20 15:38:43 +0000

    Add `MullvadProblemReport::collect` async method

--- android/src/main/kotlin/net/mullvad/mullvadvpn/dataproxy/MullvadProblemReport.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/dataproxy/MullvadProblemReport.kt
@@ -1,13 +1,39 @@
 package net.mullvad.mullvadvpn.dataproxy
 
+import java.io.File
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+
 const val PROBLEM_REPORT_PATH = "/data/data/net.mullvad.mullvadvpn/problem_report.txt"
 
 class MullvadProblemReport {
+    private var collectJob: Deferred<Boolean>? = null
+
     var userEmail = ""
     var userMessage = ""
 
     init {
         System.loadLibrary("mullvad_jni")
+    }
+
+    fun collect() {
+        synchronized(this) {
+            val currentJob = collectJob
+
+            if (currentJob == null || currentJob.isCompleted) {
+                collectJob = GlobalScope.async(Dispatchers.Default) {
+                    deleteReportFile()
+                    collectReport(PROBLEM_REPORT_PATH)
+                }
+            }
+        }
+    }
+
+    private fun deleteReportFile() {
+        File(PROBLEM_REPORT_PATH).delete()
     }
 
     private external fun collectReport(reportPath: String): Boolean
