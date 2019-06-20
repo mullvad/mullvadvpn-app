7d1ac91d36761cefbbd0771629bfcdbf5d3f00cb
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-06-20 19:05:00 +0000

    Enable button when user message isn't empty

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ProblemReportFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ProblemReportFragment.kt
@@ -3,14 +3,19 @@ package net.mullvad.mullvadvpn
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 import net.mullvad.mullvadvpn.dataproxy.MullvadProblemReport
 
 class ProblemReportFragment : Fragment() {
     private lateinit var problemReport: MullvadProblemReport
+    private lateinit var sendButton: Button
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -32,8 +37,26 @@ class ProblemReportFragment : Fragment() {
             activity?.onBackPressed()
         }
 
-        view.findViewById<View>(R.id.send_button).alpha = 0.5F
+        sendButton = view.findViewById<Button>(R.id.send_button)
+        setSendButtonEnabled(false)
+
+        view.findViewById<EditText>(R.id.user_message).addTextChangedListener(InputWatcher())
 
         return view
+    }
+
+    private fun setSendButtonEnabled(enabled: Boolean) {
+        sendButton.setEnabled(enabled)
+        sendButton.alpha = if (enabled) 1.0F else 0.5F
+    }
+
+    inner class InputWatcher : TextWatcher {
+        override fun beforeTextChanged(text: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(text: CharSequence, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(text: Editable) {
+            setSendButtonEnabled(!text.isEmpty())
+        }
     }
 }
