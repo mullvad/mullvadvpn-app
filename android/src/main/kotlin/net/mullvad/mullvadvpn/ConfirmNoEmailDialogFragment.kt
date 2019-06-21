555e482b4e8b2a872b790121a8e0a9c52ce068d1
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-06-21 14:46:52 +0000

    Implement Back button behavior

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ConfirmNoEmailDialogFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ConfirmNoEmailDialogFragment.kt
@@ -5,14 +5,24 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class ConfirmNoEmailDialogFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = inflater.inflate(R.layout.confirm_no_email, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.confirm_no_email, container, false)
+
+        view.findViewById<Button>(R.id.back_button).setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        return view
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState)
