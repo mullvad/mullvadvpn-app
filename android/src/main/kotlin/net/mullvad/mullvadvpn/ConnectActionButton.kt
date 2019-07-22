88fe74eca7a3bab33c092b2007b2dc896b88ce92
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-07-22 21:16:40 +0000

    Disable connect button on key generation issues

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ConnectActionButton.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ConnectActionButton.kt
@@ -3,12 +3,35 @@ package net.mullvad.mullvadvpn
 import android.view.View
 import android.widget.Button
 
+import net.mullvad.mullvadvpn.model.KeygenEvent
 import net.mullvad.mullvadvpn.model.TunnelState
 
 class ConnectActionButton(val parentView: View) {
     private val button: Button = parentView.findViewById(R.id.action_button)
 
-    var state: TunnelState = TunnelState.Disconnected()
+    private var enabled = true
+        set(value) {
+            if (field != value) {
+                field = value
+
+                button.setEnabled(value)
+                button.setAlpha(if (value) 1.0F else 0.5F)
+            }
+        }
+
+    var keyState: KeygenEvent? = null
+        set(value) {
+            when (value) {
+                null -> enabled = true
+                is KeygenEvent.NewKey -> enabled = true
+                is KeygenEvent.TooManyKeys -> enabled = false
+                is KeygenEvent.GenerationFailure -> enabled = false
+            }
+
+            field = value
+        }
+
+    var tunnelState: TunnelState = TunnelState.Disconnected()
         set(value) {
             when (value) {
                 is TunnelState.Disconnected -> disconnected()
@@ -30,7 +53,7 @@ class ConnectActionButton(val parentView: View) {
     }
 
     private fun action() {
-        when (state) {
+        when (tunnelState) {
             is TunnelState.Disconnected -> onConnect?.invoke()
             is TunnelState.Disconnecting -> onConnect?.invoke()
             is TunnelState.Connecting -> onCancel?.invoke()
