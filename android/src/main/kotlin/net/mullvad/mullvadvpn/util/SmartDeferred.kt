59804bb794fd670a8e956a1d1794b3c44276e185
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-08-23 22:15:28 +0000

    Create `SmartDeferred` helper type

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/util/SmartDeferred.kt
@@ -0,0 +1,32 @@
+package net.mullvad.mullvadvpn.util
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+
+class SmartDeferred<T>(private val deferred: Deferred<T>) {
+    private val jobTracker = JobTracker()
+
+    private var active = true
+
+    fun awaitThen(action: T.() -> Unit): Long? {
+        if (active) {
+            return jobTracker.newJob(GlobalScope.launch(Dispatchers.Default) {
+                deferred.await().action()
+            })
+        } else {
+            return null
+        }
+    }
+
+    fun cancelJob(jobId: Long) {
+        jobTracker.cancelJob(jobId)
+    }
+
+    fun cancel() {
+        active = false
+        jobTracker.cancelAllJobs()
+    }
+}
