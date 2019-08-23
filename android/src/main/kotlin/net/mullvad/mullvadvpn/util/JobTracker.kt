6fcce536e4bc06ea2b68df4b192de5cca98cfdc1
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-08-23 22:14:50 +0000

    Create `JobTracker` helper class

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/util/JobTracker.kt
@@ -0,0 +1,46 @@
+package net.mullvad.mullvadvpn.util
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+
+class JobTracker {
+    private val jobs = HashMap<Long, Job>()
+
+    private var jobIdCounter = 0L
+
+    fun newJob(job: Job): Long {
+        synchronized(jobs) {
+            val jobId = jobIdCounter
+
+            jobIdCounter += 1
+
+            jobs.put(jobId, GlobalScope.launch(Dispatchers.Default) {
+                job.join()
+
+                synchronized(jobs) {
+                    jobs.remove(jobId)
+                }
+            })
+
+            return jobId
+        }
+    }
+
+    fun cancelJob(jobId: Long) {
+        synchronized(jobs) {
+            jobs.remove(jobId)?.cancel()
+        }
+    }
+
+    fun cancelAllJobs() {
+        synchronized(jobs) {
+            for (job in jobs.values) {
+                job.cancel()
+            }
+
+            jobs.clear()
+        }
+    }
+}
