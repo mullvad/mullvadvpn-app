bd262028600a8dd811ffe324767fbb657fc9a3bf
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-08-23 10:40:57 +0000

    Show foreground notification for VPN service

--- android/src/main/kotlin/net/mullvad/mullvadvpn/MullvadVpnService.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/MullvadVpnService.kt
@@ -9,14 +9,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 
 import android.app.Activity
+import android.app.Notification
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.net.VpnService
 import android.os.Binder
 import android.os.IBinder
+import android.support.v4.app.NotificationCompat
 
 import net.mullvad.mullvadvpn.dataproxy.AppVersionInfoFetcher
 import net.mullvad.mullvadvpn.model.TunConfig
+
+val ONGOING_NOTIFICATION_ID: Int = 1
 
 class MullvadVpnService : VpnService() {
     private val created = CompletableDeferred<Unit>()
@@ -28,6 +33,7 @@ class MullvadVpnService : VpnService() {
 
     override fun onCreate() {
         versionInfoFetcher = AppVersionInfoFetcher(daemon, this)
+        startForeground(ONGOING_NOTIFICATION_ID, buildNotification())
         created.complete(Unit)
     }
 
@@ -39,6 +45,7 @@ class MullvadVpnService : VpnService() {
         versionInfoFetcher.stop()
         daemon.cancel()
         created.cancel()
+        stopForeground(ONGOING_NOTIFICATION_ID)
     }
 
     fun createTun(config: TunConfig): Int {
@@ -80,6 +87,20 @@ class MullvadVpnService : VpnService() {
 
             stopSelf()
         }
+    }
+
+    private fun buildNotification(): Notification {
+        val intent = Intent(this, MainActivity::class.java)
+            .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            .setAction(Intent.ACTION_MAIN)
+
+        val pendingIntent =
+            PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        return NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.notification)
+            .setContentIntent(pendingIntent)
+            .build()
     }
 
     private fun startDaemon() = GlobalScope.async(Dispatchers.Default) {
