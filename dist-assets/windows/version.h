7d491f3ea4078debb1130906a5ff9c20f7872153
Linus Färnstrand <linus@mullvad.net>
2019-08-30 16:14:17 +0200

    Updating version in package files

--- dist-assets/windows/version.h
+++ dist-assets/windows/version.h
@@ -1,4 +1,4 @@
-#define MAJOR_VERSION 1
-#define MINOR_VERSION 2
-#define PATCH_VERSION 3
-#define PRODUCT_VERSION "1.2.3-internal"
+#define MAJOR_VERSION 2019
+#define MINOR_VERSION 7
+#define PATCH_VERSION 0
+#define PRODUCT_VERSION "2019.7"
