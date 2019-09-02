2e1f598944880ace141b8ca1825eaf01e1716141
Emīls Piņķis <emils@mullvad.net>
2019-09-02 15:27:57 +0100

    Add button to regenerate key

--- gui/src/renderer/containers/WireguardKeysPage.tsx
+++ gui/src/renderer/containers/WireguardKeysPage.tsx
@@ -4,20 +4,23 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { links } from '../../config.json';
 import WireguardKeys from '../components/WireguardKeys';
+import { IWgKey } from '../redux/settings/reducers';
 import { IReduxState, ReduxDispatch } from '../redux/store';
 import { ISharedRouteProps } from '../routes';
 
-const mapStateToProps = (state: IReduxState, _props: ISharedRouteProps) => ({
+const mapStateToProps = (state: IReduxState, props: ISharedRouteProps) => ({
   keyState: state.settings.wireguardKeyState,
   isOffline: state.connection.isBlocked,
+  locale: props.locale,
 });
 const mapDispatchToProps = (dispatch: ReduxDispatch, props: ISharedRouteProps) => {
   const history = bindActionCreators({ push, goBack }, dispatch);
   return {
-    onGenerateKey: () => props.app.generateWireguardKey(),
-    onVerifyKey: (publicKey: string) => props.app.verifyWireguardKey(publicKey),
-    onVisitWebsiteKey: () => shell.openExternal(links.manageKeys),
     onClose: () => history.goBack(),
+    onGenerateKey: () => props.app.generateWireguardKey(),
+    onReplaceKey: (oldKey: IWgKey) => props.app.replaceWireguardKey(oldKey),
+    onVerifyKey: (publicKey: IWgKey) => props.app.verifyWireguardKey(publicKey),
+    onVisitWebsiteKey: () => shell.openExternal(links.manageKeys),
   };
 };
 
