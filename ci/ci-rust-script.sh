7c55b2137902a6922c335aa596701250dacf77c6
Linus Färnstrand <linus@mullvad.net>
2019-09-03 07:50:41 +0200

    Use same CI script on AppVeyor as on Travis

--- ci/ci-rust-script.sh
+++ ci/ci-rust-script.sh
@@ -1,10 +1,20 @@
+#!/usr/bin/env bash
+
 set -eu
+
 RUST_TOOLCHAIN_CHANNEL=$1
 RUSTFLAGS="--deny unused_imports --deny dead_code"
 
 source env.sh ""
+
 rustup update $RUST_TOOLCHAIN_CHANNEL
 rustup default $RUST_TOOLCHAIN_CHANNEL
+
+case "$(uname -s)" in
+  MINGW*|MSYS_NT*)
+    ./build_windows_modules.sh --dev-build
+    ;;
+esac
 
 # FIXME: Becaues of our old jsonrpc dependency our Rust code won't build
 # on latest nightly.
