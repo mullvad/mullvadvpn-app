4656c48e7e613d3f0f35e3fcbe84041ea0710a0d
Linus Färnstrand <linus@mullvad.net>
2019-09-03 13:30:05 +0200

    Add current_is_outdated flag and AppVersionInfo documentation

--- mullvad-types/src/version.rs
+++ mullvad-types/src/version.rs
@@ -4,8 +4,19 @@ use serde::{Deserialize, Serialize};
 /// Mullvad VPN app.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AppVersionInfo {
+    /// False if Mullvad has stopped supporting the currently running version. This could mean
+    /// a number of things. For example:
+    /// * API endpoints it uses might not work any more.
+    /// * Software bundled with this version, such as OpenVPN or OpenSSL, has known security
+    ///   issues, so using it is no longer recommended.
+    /// The user should really upgrade when this is false.
     pub current_is_supported: bool,
+    /// True if there is a newer version that contains any functional differences compared to the
+    /// running version. User should upgrade if they want the latest features and bugfixes.
+    pub current_is_outdated: bool,
     pub latest_stable: AppVersion,
+    /// Equal to `latest_stable` when the newest release is a stable release. But will contain
+    /// beta versions when those are out for testing.
     pub latest: AppVersion,
 }
 
