c164aa5bc74f44de070a33fd824677a931bc9574
Linus Färnstrand <linus@mullvad.net>
2019-09-03 07:35:48 +0200

    Don't build Rust on nightly for now

--- ci/travis-rust-script.sh
+++ ci/travis-rust-script.sh
@@ -6,8 +6,13 @@ source env.sh ""
 rustup update $RUST_TOOLCHAIN_CHANNEL
 rustup default $RUST_TOOLCHAIN_CHANNEL
 
-cargo build --verbose
-cargo test --verbose
+# FIXME: Becaues of our old jsonrpc dependency our Rust code won't build
+# on latest nightly.
+if [ "${RUST_TOOLCHAIN_CHANNEL}" != "nightly" ]; then
+  cargo build --verbose
+  cargo test --verbose
+fi
+
 if [ "${RUST_TOOLCHAIN_CHANNEL}" = "nightly" ]; then
   rustup component add rustfmt-preview;
   rustfmt --version;
