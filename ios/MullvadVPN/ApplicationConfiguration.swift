f2c84ce1ecf60019e072c1eb78b2c9ad6157f258
Andrej Mihajlov <and@mullvad.net>
2019-09-19 14:03:39 +0200

    Add RelayCache

---
+++ ios/MullvadVPN/ApplicationConfiguration.swift
@@ -0,0 +1,18 @@
+//
+//  ApplicationConfiguration.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 05/06/2019.
+//  Copyright © 2019 Amagicom AB. All rights reserved.
+//
+
+import Foundation
+
+class ApplicationConfiguration {
+
+    /// The application group identifier used for sharing application preferences between processes
+    static let securityGroupIdentifier = "group.net.mullvad.MullvadVPN"
+
+    /// The application identifier for the PacketTunnel extension
+    static let packetTunnelExtensionIdentifier = "net.mullvad.MullvadVPN.PacketTunnel"
+}
--- ios/MullvadVPN/RelayList.swift
+++ ios/MullvadVPN/RelayList.swift
@@ -8,15 +8,16 @@
 //
 
 import Foundation
+import Network
 
-struct RelayList: Decodable {
-    struct Country: Decodable {
+struct RelayList: Codable {
+    struct Country: Codable {
         let name: String
         let code: String
         let cities: [City]
     }
 
-    struct City: Decodable {
+    struct City: Codable {
         let name: String
         let code: String
         let latitude: Double
@@ -24,11 +25,23 @@ struct RelayList: Decodable {
         let relays: [Hostname]
     }
 
-    struct Hostname: Decodable {
+    struct Hostname: Codable {
         let hostname: String
-        let ipv4AddrIn: String
+        let ipv4AddrIn: IPv4Address
         let includeInCountry: Bool
         let weight: Int32
+        let tunnels: Tunnels?
+    }
+
+    struct Tunnels: Codable {
+        let wireguard: [WireguardTunnel]?
+    }
+
+    struct WireguardTunnel: Codable {
+        let ipv4Gateway: IPv4Address
+        let ipv6Gateway: IPv6Address
+        let publicKey: Data
+        let portRanges: [ClosedRange<UInt16>]
     }
 
     let countries: [Country]
