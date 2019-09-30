bd0b27b7a5952e9c1283a843846e18659f86ec7a
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-09-30 15:39:14 +0000

    Add settings button to login screen

--- android/src/main/kotlin/net/mullvad/mullvadvpn/LoginFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/LoginFragment.kt
@@ -44,6 +44,8 @@ class LoginFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.login, container, false)
 
+        view.findViewById<View>(R.id.settings).setOnClickListener { parentActivity.openSettings() }
+
         title = view.findViewById(R.id.title)
         subtitle = view.findViewById(R.id.subtitle)
         loggingInStatus = view.findViewById(R.id.logging_in_status)
