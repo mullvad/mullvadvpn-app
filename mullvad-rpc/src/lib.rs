07a7ed107909dfbea77029c732c8c4c47dc424a0
Emīls Piņķis <emils@mullvad.net>
2019-10-07 11:36:33 +0100

    Add submit-voucher RPC

--- mullvad-rpc/src/lib.rs
+++ mullvad-rpc/src/lib.rs
@@ -107,6 +107,7 @@ jsonrpc_client!(pub struct AccountsProxy {
     pub fn create_account(&mut self) -> RpcRequest<AccountToken>;
     pub fn get_expiry(&mut self, account_token: AccountToken) -> RpcRequest<DateTime<Utc>>;
     pub fn get_www_auth_token(&mut self, account_token: AccountToken) -> RpcRequest<String>;
+    pub fn submit_voucher(&mut self, account_token: AccountToken) -> RpcRequest<String>;
 });
 
 jsonrpc_client!(pub struct ProblemReportProxy {
