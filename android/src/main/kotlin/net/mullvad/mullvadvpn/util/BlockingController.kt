d0eebe522d48e392825763cdad3e53a9ac003b85
Emīls Piņķis <emils@mullvad.net>
2019-10-10 21:47:40 +0100

    Add BlockingController

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/util/BlockingController.kt
@@ -0,0 +1,37 @@
+package net.mullvad.mullvadvpn
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class BlockingController(val blockableView: BlockableView) {
+    var job: Job? = null
+    var innerJob: Job? = null
+
+    fun action() {
+        if (!(job?.isActive ?: false)) {
+            job = GlobalScope.launch(Dispatchers.Main) {
+                blockableView.setEnabled(false)
+                innerJob = blockableView.onClick()
+                innerJob?.join()
+                blockableView.setEnabled(true)
+            }
+        }
+    }
+
+    fun onPause() {
+        innerJob?.cancel()
+        job?.cancel()
+        blockableView.setEnabled(true)
+    }
+
+    fun onDestroy() {
+        onPause()
+    }
+}
+
+interface BlockableView {
+    fun setEnabled(enabled: Boolean)
+    fun onClick(): Job
+}
