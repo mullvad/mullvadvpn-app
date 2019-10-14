039ab79c28e54b512215b3da8c70c06084f339d4
David Lönnhager <david.l@mullvad.net>
2019-10-15 00:18:41 +0200

    Zero SP_DEVINFO_DATA initially

--- windows/nsis-plugins/src/driverlogic/context.cpp
+++ windows/nsis-plugins/src/driverlogic/context.cpp
@@ -272,7 +272,7 @@ void Context::DeleteMullvadAdapter()
 		SetupDiDestroyDeviceInfoList(devInfo);
 	};
 
-	SP_DEVINFO_DATA devInfoData;
+	SP_DEVINFO_DATA devInfoData = { 0 };
 	devInfoData.cbSize = sizeof(devInfoData);
 
 	std::vector<wchar_t> buffer;
