b8f03284cd5f072961d77b8a14b8edc81912bb16
Linus Färnstrand <linus@mullvad.net>
2019-10-29 13:43:32 +0100

    Remove duplicated verbose logging of state changes

--- mullvad-daemon/src/management_interface.rs
+++ mullvad-daemon/src/management_interface.rs
@@ -308,7 +308,6 @@ pub struct ManagementInterfaceEventBroadcaster {
 impl EventListener for ManagementInterfaceEventBroadcaster {
     /// Sends a new state update to all `new_state` subscribers of the management interface.
     fn notify_new_state(&self, new_state: TunnelState) {
-        log::debug!("Broadcasting new state: {:?}", new_state);
         self.notify(DaemonEvent::TunnelState(new_state));
     }
 
