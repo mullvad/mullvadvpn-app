fab8e238189bac05ac988f7febb3441a739c5a4b
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-11-01 20:35:53 +0000

    Add `TunProvider::create_tun` method
    Can be used to forcefully create (or recreate) the tun device.

--- talpid-core/src/tunnel/tun_provider/stub.rs
+++ talpid-core/src/tunnel/tun_provider/stub.rs
@@ -16,6 +16,11 @@ impl TunProvider for StubTunProvider {
     }
 
     #[cfg(target_os = "android")]
+    fn create_tun(&mut self) -> Result<(), BoxedError> {
+        unimplemented!();
+    }
+
+    #[cfg(target_os = "android")]
     fn create_tun_if_closed(&mut self) -> Result<(), BoxedError> {
         unimplemented!();
     }
