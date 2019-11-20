a7703cd748f73e2c35e4de83a818a7c03cc03319
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-11-20 09:35:24 +0000

    Implement `Clone` for `AndroidContext`

--- talpid-types/src/android/mod.rs
+++ talpid-types/src/android/mod.rs
@@ -4,3 +4,16 @@ pub struct AndroidContext {
     pub jvm: JavaVM,
     pub vpn_service: GlobalRef,
 }
+
+impl Clone for AndroidContext {
+    fn clone(&self) -> Self {
+        let jvm_pointer = self.jvm.get_java_vm_pointer();
+        let jvm =
+            unsafe { JavaVM::from_raw(jvm_pointer).expect("Failed to get pointer to Java VM") };
+
+        AndroidContext {
+            jvm,
+            vpn_service: self.vpn_service.clone(),
+        }
+    }
+}
