b9e2ad5a3dc3dd622c0ba33de03171ec622a8a6a
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-11-21 15:15:02 +0000

    Derive `IntoJava` for `TunConfig`

--- android/src/main/kotlin/net/mullvad/talpid/tun_provider/TunConfig.kt
+++ android/src/main/kotlin/net/mullvad/talpid/tun_provider/TunConfig.kt
@@ -1,10 +1,11 @@
 package net.mullvad.talpid.tun_provider
 
 import java.net.InetAddress
+import java.util.ArrayList
 
 data class TunConfig(
-    val addresses: List<InetAddress>,
-    val dnsServers: List<InetAddress>,
-    val routes: List<InetNetwork>,
+    val addresses: ArrayList<InetAddress>,
+    val dnsServers: ArrayList<InetAddress>,
+    val routes: ArrayList<InetNetwork>,
     val mtu: Int
 )
