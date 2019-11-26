3d5504c6a3a10e0185a1a552c1e30995a5c8f374
Odd Stranne <odd@mullvad.net>
2019-11-26 17:33:52 +0100

    Move Windows logging into logging module

--- talpid-core/src/logging/mod.rs
+++ talpid-core/src/logging/mod.rs
@@ -1,5 +1,9 @@
 use std::{fs, io, path::Path};
 
+/// Types/implementations for logging through a callback.
+#[cfg(windows)]
+pub mod windows;
+
 /// Unable to create new log file
 #[derive(err_derive::Error, Debug)]
 #[error(display = "Unable to create new log file")]
