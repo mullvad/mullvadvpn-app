fca09b5096775935126f1b68cc5aaf49a4b05662
Odd Stranne <odd@mullvad.net>
2019-11-29 13:36:23 +0100

    Update logging in windns

--- windows/windns/src/windns/netsh.h
+++ windows/windns/src/windns/netsh.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "ilogsink.h"
+#include <libcommon/logging/ilogsink.h>
 #include <libcommon/applicationrunner.h>
 #include <string>
 #include <vector>
@@ -12,7 +12,7 @@ class NetSh
 {
 public:
 
-	NetSh(std::shared_ptr<ILogSink> logSink);
+	NetSh(std::shared_ptr<common::logging::ILogSink> logSink);
 
 	void setIpv4StaticDns(uint32_t interfaceIndex,
 		const std::vector<std::wstring> &nameServers, uint32_t timeout = 0);
@@ -26,30 +26,8 @@ public:
 
 private:
 
-	std::shared_ptr<ILogSink> m_logSink;
+	std::shared_ptr<common::logging::ILogSink> m_logSink;
 	std::wstring m_netShPath;
 
 	void validateShellOut(common::ApplicationRunner &netsh, uint32_t timeout);
-};
-
-class NetShError : public std::exception
-{
-public:
-
-	NetShError(std::string &&error, std::vector<std::string> &&details)
-		: std::exception(error.c_str())
-		, m_error(std::move(error))
-		, m_details(std::move(details))
-	{
-	}
-
-	const std::vector<std::string> &details()
-	{
-		return m_details;
-	}
-
-private:
-
-	const std::string m_error;
-	const std::vector<std::string> m_details;
 };
