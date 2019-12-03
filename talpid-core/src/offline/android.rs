cf7f1c4fb77b7f37f06425a18676b2e778f4f7f2
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-12-03 15:13:51 +0000

    Notify connectivity changes to the offline monitor

--- talpid-core/src/offline/android.rs
+++ talpid-core/src/offline/android.rs
@@ -1,6 +1,10 @@
 use crate::tunnel_state_machine::TunnelCommand;
 use futures::sync::mpsc::UnboundedSender;
-use jnix::jni::{objects::JObject, sys::jlong, JNIEnv};
+use jnix::jni::{
+    objects::JObject,
+    sys::{jboolean, jlong, JNI_FALSE},
+    JNIEnv,
+};
 use std::sync::Weak;
 
 #[derive(err_derive::Error, Debug)]
@@ -12,6 +16,25 @@ pub struct MonitorHandle;
 impl MonitorHandle {
     pub fn is_offline(&self) -> bool {
         false
+    }
+}
+
+/// Entry point for Android Java code to notify the connectivity status.
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "system" fn Java_net_mullvad_talpid_ConnectivityListener_notifyConnectivityChange(
+    _: JNIEnv<'_>,
+    _: JObject<'_>,
+    is_connected: jboolean,
+    sender_address: jlong,
+) {
+    let sender_ref = Box::leak(unsafe { get_sender_from_address(sender_address) });
+    let tunnel_command = TunnelCommand::IsOffline(is_connected == JNI_FALSE);
+
+    if let Some(sender) = sender_ref.upgrade() {
+        if sender.unbounded_send(tunnel_command).is_err() {
+            log::warn!("Failed to send offline change event");
+        }
     }
 }
 
