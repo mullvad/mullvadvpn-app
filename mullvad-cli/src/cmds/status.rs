d25ceff0482ce324adb82f14b5d1de89f8084c37
Emīls <emils@mullvad.net>
2019-12-04 18:59:20 +0000

    Update CLI to yell at user if the error state doesn't block traffic

--- mullvad-cli/src/cmds/status.rs
+++ mullvad-cli/src/cmds/status.rs
@@ -2,7 +2,7 @@ use crate::{new_rpc_client, Command, Error, Result};
 use futures::{Future, Stream};
 use mullvad_ipc_client::DaemonRpcClient;
 use mullvad_types::{auth_failed::AuthFailed, states::TunnelState, DaemonEvent};
-use talpid_types::tunnel::BlockReason;
+use talpid_types::tunnel::{BlockReason, ErrorState};
 
 pub struct Status;
 
@@ -91,7 +91,7 @@ fn print_state(state: &TunnelState) {
     use self::TunnelState::*;
     print!("Tunnel status: ");
     match state {
-        Blocked(reason) => print_blocked_reason(reason),
+        Error(reason) => print_error_state(reason),
         Connected { endpoint, .. } => {
             println!("Connected to {}", endpoint);
         }
@@ -99,6 +99,15 @@ fn print_state(state: &TunnelState) {
         Disconnected => println!("Disconnected"),
         Disconnecting(_) => println!("Disconnecting..."),
     }
+}
+
+fn print_error_state(error_state: &ErrorState) {
+    if !error_state.is_blocking() {
+        eprintln!("Mullvad daemon failed to setup firewall rules!");
+        eprintln!("Deamon cannot block traffic from flowing, non-local traffic will leak");
+    }
+
+    print_blocked_reason(error_state.reason());
 }
 
 fn print_blocked_reason(reason: &BlockReason) {
