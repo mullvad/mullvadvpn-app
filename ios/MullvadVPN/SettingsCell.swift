f0e08152b60cae8a4c65c9c4e1361d0c52791d43
Andrej Mihajlov <and@mullvad.net>
2019-12-05 16:45:36 +0100

    Adjust settings cell margins and fix disclosure tint color

--- ios/MullvadVPN/SettingsCell.swift
+++ ios/MullvadVPN/SettingsCell.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Combine
 
 class SettingsCell: BasicTableViewCell {
 
-    private let preferredMargins = UIEdgeInsets(top: 14, left: 24, bottom: 14, right: 12)
+    private let preferredMargins = UIEdgeInsets(top: 16, left: 24, bottom: 16, right: 12)
+    private var appDidBecomeActiveSubscriber: AnyCancellable?
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,6 +21,29 @@ class SettingsCell: BasicTableViewCell {
         selectedBackgroundView?.backgroundColor = UIColor.Cell.selectedAltBackgroundColor
 
         contentView.layoutMargins = preferredMargins
+
+        enableDisclosureViewTintColorFix()
     }
 
+    /// `UITableViewCell` resets the disclosure view image when the app goes in background
+    /// This fix ensures that the image is tinted when the app becomes active again.
+    private func enableDisclosureViewTintColorFix() {
+        appDidBecomeActiveSubscriber = NotificationCenter.default
+            .publisher(for: UIApplication.didBecomeActiveNotification, object: nil)
+            .sink { [weak self] (_) in
+                self?.updateDisclosureViewTintColor()
+        }
+
+        updateDisclosureViewTintColor()
+    }
+
+    /// For some reason the `tintColor` is not applied to standard accessory views.
+    /// Fix this by looking for the accessory button and changing the image rendering mode
+    private func updateDisclosureViewTintColor() {
+        for case let button as UIButton in subviews {
+            if let image = button.backgroundImage(for: .normal)?.withRenderingMode(.alwaysTemplate) {
+                button.setBackgroundImage(image, for: .normal)
+            }
+        }
+    }
 }
