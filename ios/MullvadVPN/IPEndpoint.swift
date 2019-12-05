9c4bd3cdacc5e7ac32f99e4334500e82b606e20a
Andrej Mihajlov <and@mullvad.net>
2019-12-05 16:55:03 +0100

    Add packet tunnel implementation

---
+++ ios/MullvadVPN/IPEndpoint.swift
@@ -0,0 +1,63 @@
+//
+//  IPEndpoint.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 06/12/2019.
+//  Copyright © 2019 Amagicom AB. All rights reserved.
+//
+
+import Foundation
+import Network
+
+/// An abstract struct describing IP address based endpoint
+struct IPEndpont<T> where T: IPAddress {
+    let ip: T
+    let port: UInt16
+}
+
+extension IPEndpont: Codable where T: Codable {}
+
+extension IPEndpont: Equatable {
+    static func == (lhs: IPEndpont<T>, rhs: IPEndpont<T>) -> Bool {
+        lhs.ip.rawValue == rhs.ip.rawValue && lhs.port == rhs.port
+    }
+}
+
+extension IPEndpont: CustomStringConvertible {
+    var description: String {
+        return "\(ip):\(port)"
+    }
+}
+
+/// An alias for IPv4 endpoint
+typealias IPv4Endpoint = IPEndpont<IPv4Address>
+
+/// An alias for IPv6 endpoint
+typealias IPv6Endpoint = IPEndpont<IPv6Address>
+
+/// A enum describing any IP endpoint
+enum AnyIPEndpoint {
+    case ipv4(IPv4Endpoint)
+    case ipv6(IPv6Endpoint)
+}
+
+/// Convenience methods for accessing endpoint fields
+extension AnyIPEndpoint {
+    var ip: IPAddress {
+        switch self {
+        case .ipv4(let ipv4Endpoint):
+            return ipv4Endpoint.ip
+        case .ipv6(let ipv6Endpoint):
+            return ipv6Endpoint.ip
+        }
+    }
+
+    var port: UInt16 {
+        switch self {
+        case .ipv4(let ipv4Endpoint):
+            return ipv4Endpoint.port
+        case .ipv6(let ipv6Endpoint):
+            return ipv6Endpoint.port
+        }
+    }
+}
---
+++ ios/PacketTunnel/AnyIPEndpoint+DNS64.swift
@@ -0,0 +1,62 @@
+//
+//  AnyIPEndpoint+DNS64.swift
+//  PacketTunnel
+//
+//  Created by pronebird on 24/06/2019.
+//  Copyright © 2019 Amagicom AB. All rights reserved.
+//
+
+import Foundation
+import Network
+import os
+
+extension AnyIPEndpoint {
+
+    /// Returns new `AnyIPEndpoint` resolved using DNS64
+    func withReresolvedIP() -> Result<AnyIPEndpoint, Error> {
+        var resultPointer = UnsafeMutablePointer<addrinfo>(OpaquePointer(bitPattern: 0))
+        var hints = addrinfo(
+            ai_flags: 0, // We set this to zero so that we actually resolve this using DNS64
+            ai_family: AF_UNSPEC,
+            ai_socktype: SOCK_DGRAM,
+            ai_protocol: IPPROTO_UDP,
+            ai_addrlen: 0,
+            ai_canonname: nil,
+            ai_addr: nil,
+            ai_next: nil)
+
+        let err = getaddrinfo("\(self.ip)", "\(self.port)", &hints, &resultPointer)
+        if err != 0 || resultPointer == nil {
+            return .failure(NSError(
+                domain: NSPOSIXErrorDomain,
+                code: Int(err),
+                userInfo: [
+                    NSLocalizedDescriptionKey: String(cString: gai_strerror(err))
+            ]))
+        }
+
+        var resolvedAddress = self
+        let result = resultPointer!.pointee
+        if result.ai_family == AF_INET && result.ai_addrlen == MemoryLayout<sockaddr_in>.size {
+            var sa4 = UnsafeRawPointer(result.ai_addr)!.assumingMemoryBound(to: sockaddr_in.self).pointee
+            let addr = IPv4Address(Data(bytes: &sa4.sin_addr, count: MemoryLayout<in_addr>.size))
+
+            resolvedAddress = .ipv4(IPv4Endpoint(ip: addr!, port: self.port))
+        } else if result.ai_family == AF_INET6 && result.ai_addrlen == MemoryLayout<sockaddr_in6>.size {
+            var sa6 = UnsafeRawPointer(result.ai_addr)!.assumingMemoryBound(to: sockaddr_in6.self).pointee
+            let addr = IPv6Address(Data(bytes: &sa6.sin6_addr, count: MemoryLayout<in6_addr>.size))
+
+            resolvedAddress = .ipv6(IPv6Endpoint(ip: addr!, port: self.port))
+        }
+
+        freeaddrinfo(resultPointer)
+
+        if "\(resolvedAddress.ip)" == "\(self.ip)" {
+            os_log(.debug, "DNS64: mapped %{public}s to itself", "\(resolvedAddress.ip)")
+        } else {
+            os_log(.debug, "DNS64: mapped %{public}s to %{public}s", "\(self.ip)", "\(resolvedAddress.ip)")
+        }
+
+        return .success(resolvedAddress)
+    }
+}
