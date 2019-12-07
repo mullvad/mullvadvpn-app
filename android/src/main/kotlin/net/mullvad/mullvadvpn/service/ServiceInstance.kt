bf513dd8f7e297da4dab8ba1a6cfd8837b69caa2
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-12-07 14:24:39 +0000

    Create `ServiceInstance` type

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceInstance.kt
@@ -0,0 +1,10 @@
+package net.mullvad.mullvadvpn.service
+
+import net.mullvad.mullvadvpn.dataproxy.ConnectionProxy
+import net.mullvad.talpid.ConnectivityListener
+
+data class ServiceInstance(
+    val daemon: MullvadDaemon,
+    val connectionProxy: ConnectionProxy,
+    val connectivityListener: ConnectivityListener
+)
