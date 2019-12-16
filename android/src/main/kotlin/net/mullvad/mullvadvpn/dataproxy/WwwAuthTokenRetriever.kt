b6798f1d348a0cb3ee56dbc56f986f1c5a704a10
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-12-16 13:43:32 +0000

    Use synchronous daemon in `WwwAuthTokenRetriever`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/dataproxy/WwwAuthTokenRetriever.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/dataproxy/WwwAuthTokenRetriever.kt
@@ -1,14 +1,13 @@
 package net.mullvad.mullvadvpn.dataproxy
 
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import net.mullvad.mullvadvpn.service.MullvadDaemon
 
-class WwwAuthTokenRetriever(val daemon: Deferred<MullvadDaemon>) {
+class WwwAuthTokenRetriever(val daemon: MullvadDaemon) {
     suspend fun getAuthToken() = GlobalScope.async(Dispatchers.Default) {
         // returning an empty string is valid in case of any failures
-        daemon.await().getWwwAuthToken()
+        daemon.getWwwAuthToken()
     }
 }
--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceConnection.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceConnection.kt
@@ -1,6 +1,5 @@
 package net.mullvad.mullvadvpn.ui
 
-import kotlinx.coroutines.CompletableDeferred
 import net.mullvad.mullvadvpn.dataproxy.AccountCache
 import net.mullvad.mullvadvpn.dataproxy.AppVersionInfoCache
 import net.mullvad.mullvadvpn.dataproxy.KeyStatusListener
@@ -8,14 +7,9 @@ import net.mullvad.mullvadvpn.dataproxy.LocationInfoCache
 import net.mullvad.mullvadvpn.dataproxy.RelayListListener
 import net.mullvad.mullvadvpn.dataproxy.SettingsListener
 import net.mullvad.mullvadvpn.dataproxy.WwwAuthTokenRetriever
-import net.mullvad.mullvadvpn.service.MullvadDaemon
 import net.mullvad.mullvadvpn.service.ServiceInstance
-import net.mullvad.talpid.ConnectivityListener
 
 class ServiceConnection(private val service: ServiceInstance, val mainActivity: MainActivity) {
-    private val asyncDaemon = CompletableDeferred<MullvadDaemon>()
-    private val asyncConnectivityListener = CompletableDeferred<ConnectivityListener>()
-
     val daemon = service.daemon
     val connectionProxy = service.connectionProxy
     val connectivityListener = service.connectivityListener
@@ -26,18 +20,14 @@ class ServiceConnection(private val service: ServiceInstance, val mainActivity: 
     val accountCache = AccountCache(settingsListener, daemon)
     var relayListListener = RelayListListener(daemon, settingsListener)
     val locationInfoCache = LocationInfoCache(daemon, connectivityListener, relayListListener)
-    val wwwAuthTokenRetriever = WwwAuthTokenRetriever(asyncDaemon)
+    val wwwAuthTokenRetriever = WwwAuthTokenRetriever(daemon)
 
     init {
-        asyncDaemon.complete(daemon)
         appVersionInfoCache.onCreate()
-
         connectionProxy.mainActivity = mainActivity
     }
 
     fun onDestroy() {
-        asyncDaemon.cancel()
-
         accountCache.onDestroy()
         appVersionInfoCache.onDestroy()
         keyStatusListener.onDestroy()
