18f226c0a65abcad920320093bf06d1cb8671731
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-01-09 19:56:32 +0000

    Derive `FromJava` for `LocationConstraint`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
@@ -1,11 +1,22 @@
 package net.mullvad.mullvadvpn.model
 
 sealed class LocationConstraint(val code: Array<String>) {
-    class Country(var countryCode: String) : LocationConstraint(arrayOf(countryCode))
+    class Country(var countryCode: String) : LocationConstraint(arrayOf(countryCode)) {
+        fun get0() = countryCode
+    }
 
     class City(var countryCode: String, var cityCode: String) :
         LocationConstraint(arrayOf(countryCode, cityCode))
+    {
+        fun get0() = countryCode
+        fun get1() = cityCode
+    }
 
     class Hostname(var countryCode: String, var cityCode: String, var hostname: String) :
         LocationConstraint(arrayOf(countryCode, cityCode, hostname))
+    {
+        fun get0() = countryCode
+        fun get1() = cityCode
+        fun get2() = hostname
+    }
 }
