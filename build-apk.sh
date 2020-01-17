c536473d31d5ee3cf7730e2379f6d6701a0f6950
Emīls <emils@mullvad.net>
2020-01-17 15:59:14 +0000

    Run build-wireguard-go.sh from build-apk.sh

--- build-apk.sh
+++ build-apk.sh
@@ -5,6 +5,8 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
+bash build-wireguard-go.sh --android
+
 PRODUCT_VERSION="$(node -p "require('$SCRIPT_DIR/gui/package.json').version" | sed -Ee 's/\.0//g')"
 
 if [[ "${1:-""}" == "--dev-build" ]]; then
