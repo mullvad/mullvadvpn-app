0f0891e7be519c357ad8445360995ac98218b6f1
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-01-28 20:11:01 +0000

    Update toggle if allow LAN settings changes

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/PreferencesFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/PreferencesFragment.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.R
 
 class PreferencesFragment : ServiceDependentFragment(OnNoService.GoBack) {
+    private lateinit var allowLanToggle: CellSwitch
+
+    private var updateUiJob: Job? = null
+
     override fun onSafelyCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +26,25 @@ class PreferencesFragment : ServiceDependentFragment(OnNoService.GoBack) {
             parentActivity.onBackPressed()
         }
 
+        allowLanToggle = view.findViewById(R.id.allow_lan_toggle)
+
+        settingsListener.onAllowLanChange = { allowLan ->
+            updateUiJob?.cancel()
+            updateUiJob = updateUi(allowLan)
+        }
+
         return view
+    }
+
+    override fun onSafelyDestroyView() {
+        settingsListener.onAllowLanChange = null
+    }
+
+    private fun updateUi(allowLan: Boolean) = GlobalScope.launch(Dispatchers.Main) {
+        if (allowLan) {
+            allowLanToggle.state = CellSwitch.State.ON
+        } else {
+            allowLanToggle.state = CellSwitch.State.OFF
+        }
     }
 }
