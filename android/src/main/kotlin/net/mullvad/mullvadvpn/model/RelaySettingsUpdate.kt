3209091c5800207cd142725d01d16623dcaf0fdb
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-02-05 13:17:11 +0000

    Remove obsoleted `from_java` module

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelaySettingsUpdate.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelaySettingsUpdate.kt
@@ -2,8 +2,6 @@ package net.mullvad.mullvadvpn.model
 
 sealed class RelaySettingsUpdate {
     class CustomTunnelEndpoint() : RelaySettingsUpdate()
-    class RelayConstraintsUpdate(var location: Constraint<LocationConstraint>?) :
-        RelaySettingsUpdate()
 
     class Normal(var constraints: RelayConstraintsUpdate) : RelaySettingsUpdate() {
         fun get0() = constraints
