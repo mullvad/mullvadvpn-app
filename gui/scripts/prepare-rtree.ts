32f504123de3e7e39d3d67a67c9496326f0a708d
Oskar Nyberg <oskar@mullvad.net>
2020-02-07 13:20:36 +0100

    Remove parts of scripts that generate geo data and city/country translations

--- gui/scripts/prepare-rtree.ts
+++ gui/scripts/prepare-rtree.ts
@@ -6,7 +6,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { Topology, GeometryCollection } from 'topojson-specification';
-import { GeoJSON } from 'geojson';
 import rbush from 'rbush';
 
 interface GeometryTopologyObjects {
@@ -16,7 +15,6 @@ interface GeometryTopologyObjects {
 
 function main() {
   const GEOMETRY_DATA_FILES = ['geometry', 'states-provinces-lines'];
-  const POINT_DATA_FILES = ['countries', 'cities'];
   const OUTPUT_DIR = path.join(__dirname, 'out');
 
   for (const name of GEOMETRY_DATA_FILES) {
@@ -25,17 +23,6 @@ function main() {
 
     try {
       processGeometry(source, destination);
-    } catch (error) {
-      console.error(`Failed to process ${name}: ${error.message}`);
-    }
-  }
-
-  for (const name of POINT_DATA_FILES) {
-    const source = path.join(OUTPUT_DIR, `${name}.json`);
-    const destination = path.join(OUTPUT_DIR, `${name}.rbush.json`);
-
-    try {
-      processPoints(source, destination);
     } catch (error) {
       console.error(`Failed to process ${name}: ${error.message}`);
     }
@@ -60,37 +47,6 @@ function processGeometry(source: string, destination: string) {
       minY,
       maxX,
       maxY,
-    };
-  });
-
-  const tree = rbush();
-  tree.load(treeData);
-  fs.writeFileSync(destination, JSON.stringify(tree.toJSON()));
-
-  console.log(`Saved a rbush to ${destination}`);
-}
-
-function processPoints(source: string, destination: string) {
-  const collection = JSON.parse(fs.readFileSync(source, { encoding: 'utf8' })) as GeoJSON;
-
-  if (collection.type !== 'FeatureCollection') {
-    throw new Error(
-      `Invalid collection type ${collection.type} in ${source}. Expected FeatureCollection`,
-    );
-  }
-
-  const treeData = collection.features.map((feat) => {
-    if (feat.geometry.type !== 'Point') {
-      throw new Error(`Invalid geometry in ${source}. Expected "Point", got ${feat.geometry.type}`);
-    }
-
-    const { coordinates } = feat.geometry;
-    return {
-      ...feat,
-      minX: coordinates[0],
-      minY: coordinates[1],
-      maxX: coordinates[0],
-      maxY: coordinates[1],
     };
   });
 
