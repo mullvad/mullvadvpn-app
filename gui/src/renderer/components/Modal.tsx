74c9ecfa08f2c204dd503c3fa9464e59ffe5d9b1
Oskar Nyberg <oskar@mullvad.net>
2020-02-11 18:00:59 +0100

    Add dialog icon

--- gui/src/renderer/components/Modal.tsx
+++ gui/src/renderer/components/Modal.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Component, Styles, Text, View } from 'reactxp';
 import { colors } from '../../config.json';
+import ImageView from './ImageView';
 
 const styles = {
   modalAlertBackground: Styles.createViewStyle({
@@ -13,6 +14,11 @@ const styles = {
     backgroundColor: colors.darkBlue,
     borderRadius: 11,
     padding: 16,
+  }),
+  modalAlertIcon: Styles.createViewStyle({
+    alignItems: 'center',
+    marginBottom: 12,
+    marginTop: 4,
   }),
   modalAlertMessage: Styles.createTextStyle({
     fontFamily: 'Open Sans',
@@ -67,7 +73,13 @@ export const ModalContainer: React.FC = ({ children }) => {
   return <div style={{ position: 'relative', flex: 1 }}>{children}</div>;
 };
 
+export enum ModalAlertType {
+  Info = 1,
+  Warning,
+}
+
 interface IModalAlertProps {
+  type?: ModalAlertType;
   message: string;
   buttons: React.ReactNode[];
 }
@@ -78,6 +90,9 @@ export class ModalAlert extends Component<IModalAlertProps> {
       <ModalBackground>
         <View style={styles.modalAlertBackground}>
           <View style={styles.modalAlert}>
+            {this.props.type && (
+              <View style={styles.modalAlertIcon}>{this.renderTypeIcon(this.props.type)}</View>
+            )}
             <Text style={styles.modalAlertMessage}>{this.props.message}</Text>
             {this.props.buttons.map((button, index) => (
               <View key={index} style={styles.modalAlertButtonContainer}>
@@ -88,5 +103,21 @@ export class ModalAlert extends Component<IModalAlertProps> {
         </View>
       </ModalBackground>
     );
+  }
+
+  private renderTypeIcon(type: ModalAlertType) {
+    let source = '';
+    let color = '';
+    switch (type) {
+      case ModalAlertType.Info:
+        source = 'icon-alert';
+        color = colors.white;
+        break;
+      case ModalAlertType.Warning:
+        source = 'icon-alert';
+        color = colors.red;
+        break;
+    }
+    return <ImageView height={44} width={44} source={source} tintColor={color} />;
   }
 }
