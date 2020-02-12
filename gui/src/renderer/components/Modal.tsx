910cb54059dfa1cdb156c79ece6bdcc468b8f2ed
Oskar Nyberg <oskar@mullvad.net>
2020-02-12 13:06:45 +0100

    Add outdated version dialog when navigating to problem report

--- gui/src/renderer/components/Modal.tsx
+++ gui/src/renderer/components/Modal.tsx
@@ -1,4 +1,30 @@
 import * as React from 'react';
+import { Component, Styles, Text, View } from 'reactxp';
+import { colors } from '../../config.json';
+
+const styles = {
+  dialogBackground: Styles.createViewStyle({
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 14,
+    paddingRight: 14,
+  }),
+  dialog: Styles.createViewStyle({
+    backgroundColor: colors.darkBlue,
+    borderRadius: 11,
+    padding: 16,
+  }),
+  dialogWarning: Styles.createTextStyle({
+    fontFamily: 'Open Sans',
+    fontSize: 16,
+    fontWeight: '500',
+    lineHeight: 20,
+    color: colors.white80,
+  }),
+  dialogButtonContainer: Styles.createViewStyle({
+    marginTop: 16,
+  }),
+};
 
 export class ModalContent extends React.Component {
   public render() {
@@ -48,5 +74,27 @@ interface IModalContainerProps {
 export class ModalContainer extends React.Component<IModalContainerProps> {
   public render() {
     return <div style={{ position: 'relative', flex: 1 }}>{this.props.children}</div>;
+  }
+}
+
+interface IDialogProps {
+  message: string;
+  buttons: React.ReactNode[];
+}
+
+export class Dialog extends Component<IDialogProps> {
+  public render() {
+    return (
+      <View style={styles.dialogBackground}>
+        <View style={styles.dialog}>
+          <Text style={styles.dialogWarning}>{this.props.message}</Text>
+          {this.props.buttons.map((button, index) => (
+            <View key={index} style={styles.dialogButtonContainer}>
+              {button}
+            </View>
+          ))}
+        </View>
+      </View>
+    );
   }
 }
