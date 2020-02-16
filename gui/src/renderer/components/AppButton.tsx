1337381d585a328c96d4c38fb39eac60b0b0f86d
Oskar Nyberg <oskar@mullvad.net>
2020-02-16 23:34:55 +0100

    Remove export on BaseButton

--- gui/src/renderer/components/AppButton.tsx
+++ gui/src/renderer/components/AppButton.tsx
@@ -84,7 +84,7 @@ interface IState {
   textAdjustment: number;
 }
 
-export class BaseButton extends Component<IProps, IState> {
+class BaseButton extends Component<IProps, IState> {
   public state: IState = {
     hovered: false,
     textAdjustment: 0,
