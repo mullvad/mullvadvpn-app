49141cfcf5fd3e7436dbaf9aab6be1d97999ae67
Oskar Nyberg <oskar@mullvad.net>
2020-02-19 16:06:02 +0100

    Update typescript linting rules

--- gui/.eslintrc.js
+++ gui/.eslintrc.js
@@ -36,20 +36,17 @@ module.exports = {
       'error',
       { argsIgnorePattern: '^_', ignoreRestSiblings: true },
     ],
-    // TODO: Enable these
-    // 'require-await': 'error',
-    // '@typescript-eslint/no-floating-promises': 'error',
 
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    // TODO: Enable these
+    'require-await': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
     // TODO: This should eventually be removed.
-    '@typescript-eslint/ban-types': 'warn',
-    '@typescript-eslint/no-non-null-assertion': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'react/no-find-dom-node': 'warn',
     '@typescript-eslint/interface-name-prefix': 'off',
     // TODO: The rules below should be enabled when move from ReactXP is completed.
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
+    'react/no-find-dom-node': 'off',
   },
 };
