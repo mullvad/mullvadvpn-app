2531197f9648c623e6b1b2988ca1fdf964eb73df
Andrej Mihajlov <and@mullvad.net>
2020-02-20 15:40:19 +0100

    Remove create account URL

--- ios/MullvadVPN/WebLinks.swift
+++ ios/MullvadVPN/WebLinks.swift
@@ -12,7 +12,6 @@ enum WebLinks {}
 
 extension WebLinks {
 
-    static let createAccountURL = URL(string: "https://mullvad.net/account/create/")!
     static let faqURL = URL(string: "https://mullvad.net/faq/")!
     static let manageKeysURL = URL(string: "https://mullvad.net/en/account/ports/")!
 
