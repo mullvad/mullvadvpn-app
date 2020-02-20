4dd8ea630ed3728cf845d2a273309715fbc499d8
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-02-20 11:53:55 +0000

    Create `Sender` trait

--- talpid-core/src/mpsc.rs
+++ talpid-core/src/mpsc.rs
@@ -1,6 +1,12 @@
 use futures::sync::mpsc::{SendError, UnboundedSender};
 use std::marker::PhantomData;
 
+/// Abstraction over any type that can be used similarly to an `std::mpsc::Sender`.
+pub trait Sender<T> {
+    /// Sends an item over the underlying channel, failing only if the channel is closed.
+    fn send(&self, item: T) -> Result<(), ()>;
+}
+
 /// Abstraction over an `mpsc::Sender` that first converts the value to another type before sending.
 #[derive(Debug, Clone)]
 pub struct IntoSender<T, U> {
