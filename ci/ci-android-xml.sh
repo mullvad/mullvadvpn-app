25aad749715522ea072296097bf30101be499538
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-02-27 19:46:19 +0000

    Update XML formatting script to include manifest

--- ci/ci-android-xml.sh
+++ ci/ci-android-xml.sh
@@ -12,17 +12,17 @@ function tidy-up-android-xml {
         --indent-attributes yes \
         --indent-spaces 4 \
         --literal-attributes yes \
-        android/src/main/res/*/*.xml
+        android/src/main/AndroidManifest.xml android/src/main/res/*/*.xml
     # FIXME - when tidy learns to not leave whitespace around, remove the line below - https://github.com/htacg/tidy-html5/issues/864
-    find android/src/main/res/ -name '*.xml' -exec sed -i -e 's/[ \t]*$//' '{}' ';'
+    find android/src/main/ -name '*.xml' -exec sed -i -e 's/[ \t]*$//' '{}' ';'
 }
 
 # Autoformats Android XML files and returns 0 if no files were actually changed, or 1 if files were changed
 function tidy-verify-xml {
     tidy-up-android-xml
-    if (( $(git diff android/src/main/res/ | wc -l) > 0 )); then
-        echo "android/src/main/res contains that were changed, XML is not formatted properly"
-        git diff android/src/main/res/
+    if (( $(git diff android/src/main/AndroidManifest.xml android/src/main/res/ | wc -l) > 0 )); then
+        echo "android/src/main contains that were changed, XML is not formatted properly"
+        git diff -- android/src/main/AndroidManifest.xml android/src/main/res
         return 1;
     else
         echo "Android XML files are correctly formatted"
