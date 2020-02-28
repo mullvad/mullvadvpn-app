fded606538075006e744db27bcf8cc5709988a0b
David Lönnhager <david.l@mullvad.net>
2020-02-28 14:26:50 +0100

    Set routing table for marked packets

--- talpid-core/src/firewall/linux.rs
+++ talpid-core/src/firewall/linux.rs
@@ -18,7 +18,6 @@ use talpid_types::net::{Endpoint, TransportProtocol};
 
 /// Priority for rules that tag split tunneling packets. Equals NF_IP_PRI_MANGLE.
 const MANGLE_CHAIN_PRIORITY: i32 = libc::NF_IP_PRI_MANGLE;
-const SPLIT_TUNNEL_MARK: i32 = 0xf41;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -243,20 +242,20 @@ impl<'a> PolicyBatch<'a> {
         let mut rule = Rule::new(&self.mangle_chain);
         rule.add_expr(&nft_expr!(meta cgroup));
         rule.add_expr(&nft_expr!(cmp == split::NETCLS_CLASSID));
-        rule.add_expr(&nft_expr!(immediate data SPLIT_TUNNEL_MARK));
+        rule.add_expr(&nft_expr!(immediate data split::MARK));
         rule.add_expr(&nft_expr!(ct mark set));
         rule.add_expr(&nft_expr!(meta mark set));
         self.batch.add(&rule, nftnl::MsgType::Add);
 
         let mut rule = Rule::new(&self.in_chain);
         rule.add_expr(&nft_expr!(ct mark));
-        rule.add_expr(&nft_expr!(cmp == SPLIT_TUNNEL_MARK));
+        rule.add_expr(&nft_expr!(cmp == split::MARK));
         add_verdict(&mut rule, &Verdict::Accept);
         self.batch.add(&rule, nftnl::MsgType::Add);
 
         let mut rule = Rule::new(&self.out_chain);
         rule.add_expr(&nft_expr!(meta mark));
-        rule.add_expr(&nft_expr!(cmp == SPLIT_TUNNEL_MARK));
+        rule.add_expr(&nft_expr!(cmp == split::MARK));
         add_verdict(&mut rule, &Verdict::Accept);
         self.batch.add(&rule, nftnl::MsgType::Add);
     }
