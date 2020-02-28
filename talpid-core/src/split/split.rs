8be36210dd6244ecf2ff9439cb07e97f0067d8b0
David Lönnhager <david.l@mullvad.net>
2020-02-28 10:29:08 +0100

    Expose and document create_cgroup()

--- talpid-core/src/split/split.rs
+++ talpid-core/src/split/split.rs
@@ -35,7 +35,8 @@ pub enum Error {
     ListCGroupPids(#[error(source)] io::Error),
 }
 
-fn create_cgroup() -> Result<(), Error> {
+/// Set up cgroup used to track PIDs for split tunneling.
+pub fn create_cgroup() -> Result<(), Error> {
     let mut exclusions_dir = PathBuf::from(NETCLS_PATH);
     exclusions_dir.push(CGROUP_NAME);
 
