007912efd329fe1b31747a144ededd00cd8f7323
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-02-28 12:49:33 +0000

    Persist tunnel state in shared preferences

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/tunnelstate/TunnelStateUpdater.kt
@@ -0,0 +1,29 @@
+package net.mullvad.mullvadvpn.service.tunnelstate
+
+import android.content.Context
+import net.mullvad.mullvadvpn.dataproxy.ConnectionProxy
+import net.mullvad.mullvadvpn.service.ServiceInstance
+import net.mullvad.talpid.util.EventNotifier
+
+class TunnelStateUpdater(context: Context, serviceNotifier: EventNotifier<ServiceInstance?>) {
+    private val persistence = Persistence(context)
+
+    private var connectionProxy: ConnectionProxy? = null
+    private var stateSubscriptionId: Int? = null
+
+    init {
+        serviceNotifier.subscribe { serviceInstance ->
+            onNewServiceInstance(serviceInstance)
+        }
+    }
+
+    private fun onNewServiceInstance(serviceInstance: ServiceInstance?) {
+        stateSubscriptionId?.let { id -> connectionProxy?.onStateChange?.unsubscribe(id) }
+
+        connectionProxy = serviceInstance?.connectionProxy?.apply {
+            stateSubscriptionId = onStateChange.subscribe { newState ->
+                persistence.state = newState
+            }
+        }
+    }
+}
