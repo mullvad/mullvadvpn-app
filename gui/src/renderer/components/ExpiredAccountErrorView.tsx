af9e63534f23ecc7faf1e7be9ff6ad132346e152
Oskar Nyberg <oskar@mullvad.net>
2020-03-02 17:50:24 +0100

    Merge NewAccountView.tsx into ExpiredAccountErrorView.tsx

--- gui/src/renderer/components/ExpiredAccountErrorView.tsx
+++ gui/src/renderer/components/ExpiredAccountErrorView.tsx
@@ -1,174 +1,162 @@
 import * as React from 'react';
-import { Component, Styles, View } from 'reactxp';
-import { colors } from '../../config.json';
+import { Component, Text, View } from 'reactxp';
+import { sprintf } from 'sprintf-js';
+import { links } from '../../config.json';
+import AccountExpiry from '../../shared/account-expiry';
+import { AccountToken } from '../../shared/daemon-rpc-types';
 import { messages } from '../../shared/gettext';
+import { LoginState } from '../redux/account/reducers';
+import AccountTokenLabel from './AccountTokenLabel';
 import * as AppButton from './AppButton';
+import styles from './ExpiredAccountErrorViewStyles';
 import ImageView from './ImageView';
 
 export enum RecoveryAction {
   openBrowser,
-  disconnectAndOpenBrowser,
+  disconnect,
   disableBlockedWhenDisconnected,
 }
 
-interface IProps {
+interface IExpiredAccountErrorViewProps {
   isBlocked: boolean;
   blockWhenDisconnected: boolean;
-  action: (recoveryAction: RecoveryAction) => Promise<void>;
+  accountToken?: AccountToken;
+  accountExpiry?: AccountExpiry;
+  loginState: LoginState;
+  hideWelcomeView: () => void;
+  onExternalLinkWithAuth: (url: string) => Promise<void>;
+  onDisconnect: () => Promise<void>;
 }
 
-interface IState {
-  recoveryAction: RecoveryAction;
-}
-
-const styles = {
-  container: Styles.createViewStyle({
-    flex: 1,
-    paddingTop: 94,
-  }),
-  body: Styles.createViewStyle({
-    flex: 1,
-    paddingHorizontal: 24,
-  }),
-  title: Styles.createTextStyle({
-    fontFamily: 'DINPro',
-    fontSize: 32,
-    fontWeight: '900',
-    lineHeight: 40,
-    color: colors.white,
-    marginBottom: 8,
-  }),
-  message: Styles.createTextStyle({
-    fontFamily: 'Open Sans',
-    fontSize: 13,
-    lineHeight: 20,
-    fontWeight: '600',
-    color: colors.white,
-    marginBottom: 24,
-  }),
-  statusIcon: Styles.createViewStyle({
-    alignSelf: 'center',
-    width: 60,
-    height: 60,
-    marginBottom: 32,
-  }),
-};
-
-export default class ExpiredAccountErrorView extends Component<IProps, IState> {
-  public static getDerivedStateFromProps(props: IProps): IState {
-    const { blockWhenDisconnected, isBlocked } = props;
-
-    if (blockWhenDisconnected && isBlocked) {
-      return { recoveryAction: RecoveryAction.disableBlockedWhenDisconnected };
-    } else if (!blockWhenDisconnected && isBlocked) {
-      return { recoveryAction: RecoveryAction.disconnectAndOpenBrowser };
-    } else {
-      return { recoveryAction: RecoveryAction.openBrowser };
+export default class ExpiredAccountErrorView extends Component<IExpiredAccountErrorViewProps> {
+  public componentDidUpdate() {
+    if (this.props.accountExpiry && !this.props.accountExpiry.hasExpired()) {
+      this.props.hideWelcomeView();
     }
   }
-  public state: IState = { recoveryAction: RecoveryAction.openBrowser };
 
   public render() {
     return (
       <View style={styles.container}>
-        <View style={styles.statusIcon}>
-          <ImageView source="icon-fail" height={60} width={60} />
-        </View>
-        <View style={styles.body}>
-          <View style={styles.title}>{messages.pgettext('connect-view', 'Out of time')}</View>
-          {this.renderContent()}
+        <View style={styles.body}>{this.renderContent()}</View>
+
+        <View style={styles.footer}>
+          {this.getRecoveryAction() === RecoveryAction.disconnect && (
+            <AppButton.BlockingButton onPress={this.props.onDisconnect}>
+              <AppButton.RedButton style={styles.button}>
+                {messages.pgettext('connect-view', 'Disconnect')}
+              </AppButton.RedButton>
+            </AppButton.BlockingButton>
+          )}
+
+          {this.renderExternalPaymentButton()}
         </View>
       </View>
     );
   }
 
   private renderContent() {
-    switch (this.state.recoveryAction) {
-      case RecoveryAction.disconnectAndOpenBrowser:
-        return <DisconnectAndOpenBrowserContentView actionHandler={this.handleAction} />;
+    if (this.isNewAccount()) {
+      return this.renderWelcomeView();
+    }
+
+    return (
+      <>
+        <View style={styles.statusIcon}>
+          <ImageView source="icon-fail" height={60} width={60} />
+        </View>
+        <View style={styles.title}>{messages.pgettext('connect-view', 'Out of time')}</View>
+        <View style={styles.message}>
+          {sprintf('%(introduction)s %(recoveryMessage)s', {
+            introduction: messages.pgettext(
+              'connect-view',
+              'You have no more VPN time left on this account.',
+            ),
+            recoveryMessage: this.getRecoveryActionMessage(),
+          })}
+        </View>
+      </>
+    );
+  }
+
+  private renderWelcomeView() {
+    return (
+      <>
+        <View style={styles.title}>{messages.pgettext('connect-view', 'Congrats!')}</View>
+        <View style={styles.message}>
+          <Text style={styles.fieldLabel}>
+            {messages.pgettext('connect-view', 'Here’s your account number. Save it!')}
+          </Text>
+          <AccountTokenLabel
+            style={styles.accountToken}
+            messageStyle={styles.accountTokenCopiedMessage}
+            accountToken={this.props.accountToken || ''}
+          />
+        </View>
+
+        <View style={styles.message}>
+          {sprintf('%(introduction)s %(recoveryMessage)s', {
+            introduction: messages.pgettext(
+              'connect-view',
+              'To start using the app, you first need to add time to your account.',
+            ),
+            recoveryMessage: this.getRecoveryActionMessage(),
+          })}
+        </View>
+      </>
+    );
+  }
+
+  private getRecoveryActionMessage() {
+    switch (this.getRecoveryAction()) {
       case RecoveryAction.openBrowser:
-        return <OpenBrowserContentView actionHandler={this.handleAction} />;
       case RecoveryAction.disableBlockedWhenDisconnected:
-        return <DisableBlockWhenDisconnectedContentView />;
+        return messages.pgettext(
+          'connect-view',
+          'Either buy credit on our website or redeem a voucher.',
+        );
+      case RecoveryAction.disconnect:
+        return messages.pgettext(
+          'connect-view',
+          'To add more, you will need to disconnect and access the Internet with an unsecured connection.',
+        );
     }
   }
 
-  private handleAction = (): Promise<void> => {
-    return this.props.action(this.state.recoveryAction);
+  private renderExternalPaymentButton() {
+    const buttonText = this.isNewAccount()
+      ? messages.pgettext('connect-view', 'Buy credit')
+      : messages.pgettext('connect-view', 'Buy more credit');
+
+    return (
+      <AppButton.BlockingButton
+        disabled={this.props.isBlocked}
+        onPress={this.onOpenExternalPayment}>
+        <AppButton.GreenButton>
+          <AppButton.Label>{buttonText}</AppButton.Label>
+          <AppButton.Icon source="icon-extLink" height={16} width={16} />
+        </AppButton.GreenButton>
+      </AppButton.BlockingButton>
+    );
+  }
+
+  private isNewAccount() {
+    return this.props.loginState.type === 'ok' && this.props.loginState.method === 'new_account';
+  }
+
+  private onOpenExternalPayment = async (): Promise<void> => {
+    await this.props.onExternalLinkWithAuth(links.purchase);
   };
-}
 
-class DisconnectAndOpenBrowserContentView extends Component<{
-  actionHandler: () => Promise<void>;
-}> {
-  public render() {
-    return (
-      <View>
-        <View style={styles.message}>
-          {messages.pgettext(
-            'connect-view',
-            'You have no more VPN time left on this account. To buy more credit on our website, you will need to access the Internet with an unsecured connection.',
-          )}
-        </View>
-        <View>
-          <AppButton.BlockingButton onPress={this.props.actionHandler}>
-            <AppButton.RedButton>
-              <AppButton.Label>
-                {messages.pgettext('connect-view', 'Disconnect and buy more credit')}
-              </AppButton.Label>
-              <AppButton.Icon source="icon-extLink" height={16} width={16} />
-            </AppButton.RedButton>
-          </AppButton.BlockingButton>
-        </View>
-      </View>
-    );
-  }
-}
+  private getRecoveryAction() {
+    const { blockWhenDisconnected, isBlocked } = this.props;
 
-class OpenBrowserContentView extends Component<{ actionHandler: () => Promise<void> }> {
-  public render() {
-    return (
-      <View>
-        <View style={styles.message}>
-          {messages.pgettext(
-            'connect-view',
-            'You have no more VPN time left on this account. Please log in on our website to buy more credit.',
-          )}
-        </View>
-        <View>
-          <AppButton.BlockingButton onPress={this.props.actionHandler}>
-            <AppButton.GreenButton>
-              <AppButton.Label>
-                {messages.pgettext('connect-view', 'Buy more credit')}
-              </AppButton.Label>
-              <AppButton.Icon source="icon-extLink" height={16} width={16} />
-            </AppButton.GreenButton>
-          </AppButton.BlockingButton>
-        </View>
-      </View>
-    );
-  }
-}
-
-class DisableBlockWhenDisconnectedContentView extends Component {
-  public render() {
-    return (
-      <View>
-        <View style={styles.message}>
-          {messages.pgettext(
-            'connect-view',
-            'You have no more VPN time left on this account. Before you can buy more credit on our website, you first need to turn off the app\'s "Block when disconnected" option under Advanced settings.',
-          )}
-        </View>
-        <View>
-          <AppButton.GreenButton disabled={true}>
-            <AppButton.Label>
-              {messages.pgettext('connect-view', 'Buy more credit')}
-            </AppButton.Label>
-            <AppButton.Icon source="icon-extLink" height={16} width={16} />
-          </AppButton.GreenButton>
-        </View>
-      </View>
-    );
+    if (blockWhenDisconnected && isBlocked) {
+      return RecoveryAction.disableBlockedWhenDisconnected;
+    } else if (!blockWhenDisconnected && isBlocked) {
+      return RecoveryAction.disconnect;
+    } else {
+      return RecoveryAction.openBrowser;
+    }
   }
 }
