325c00f8cae90e957e1db9f5d2df62e5c34ca227
David Lönnhager <david.l@mullvad.net>
2020-03-06 15:51:34 +0100

    Prevent reset-firewall in daemon setup tool from being run if the daemon is running

--- mullvad-setup/src/main.rs
+++ mullvad-setup/src/main.rs
@@ -11,6 +11,9 @@ pub enum Error {
     #[error(display = "Failed to connect to daemon")]
     DaemonConnect(#[error(source)] io::Error),
 
+    #[error(display = "This command cannot be run if the daemon is active")]
+    DaemonIsRunning,
+
     #[error(display = "Firewall error")]
     FirewallError(#[error(source)] firewall::Error),
 }
@@ -24,12 +27,16 @@ fn prepare_restart() -> Result<(), Error> {
 }
 
 fn reset_firewall() -> Result<(), Error> {
-    // TODO: ensure daemon isn't running
+    // Ensure that the daemon isn't running
+    if let Ok(_) = new_rpc_client() {
+        return Err(Error::DaemonIsRunning);
+    }
 
     let mut firewall = Firewall::new(FirewallArguments {
         initialize_blocked: false,
         allow_lan: None,
-    }).map_err(Error::FirewallError)?;
+    })
+    .map_err(Error::FirewallError)?;
 
     // FIXME: Hack. `reset_policy` is only supposed to remove rules
     //        from the current firewall instance.
