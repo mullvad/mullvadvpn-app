3d08a64d70dd9555ba10eadc725f350ccfbaabe8
David Lönnhager <david.l@mullvad.net>
2020-03-18 11:17:59 +0100

    Add mullvad-exclude

---
+++ mullvad-exclude/src/main.rs
@@ -0,0 +1,77 @@
+use nix::unistd::{execv, getgid, getpid, getuid, setgid, setuid};
+use std::{
+    env,
+    error::Error as StdError,
+    ffi::{CStr, CString},
+    fs, io,
+};
+
+#[cfg(target_os = "linux")]
+const CGROUP_PROCS_PATH: &str = "/sys/fs/cgroup/net_cls/mullvad-exclusions/cgroup.procs";
+
+#[derive(err_derive::Error, Debug)]
+#[error(no_from)]
+enum Error {
+    #[error(display = "Invalid arguments")]
+    InvalidArguments,
+
+    #[error(display = "Cannot set the cgroup")]
+    AddProcToCGroup(#[error(source)] io::Error),
+
+    #[error(display = "Cannot set the uid")]
+    SetUid(#[error(source)] nix::Error),
+
+    #[error(display = "Cannot set the gid")]
+    SetGid(#[error(source)] nix::Error),
+
+    #[error(display = "Failed to launch the process")]
+    Exec(#[error(source)] nix::Error),
+}
+
+fn main() {
+    match run() {
+        Err(Error::InvalidArguments) => {
+            let mut args = env::args();
+            eprintln!("Usage: {} <command>", args.next().unwrap());
+            std::process::exit(1);
+        }
+        Err(e) => {
+            let mut s = format!("{}", e);
+            let mut source = e.source();
+            while let Some(error) = source {
+                s.push_str(&format!("\nCaused by: {}", error));
+                source = error.source();
+            }
+            eprintln!("{}", s);
+
+            std::process::exit(1);
+        }
+        _ => (),
+    }
+}
+
+fn run() -> Result<(), Error> {
+    if env::args().count() < 2 {
+        return Err(Error::InvalidArguments);
+    }
+
+    // Set the cgroup of this process
+    fs::write(CGROUP_PROCS_PATH, getpid().to_string().as_bytes())
+        .map_err(Error::AddProcToCGroup)?;
+
+    // Drop root privileges
+    let real_uid = getuid();
+    setuid(real_uid).map_err(Error::SetUid)?;
+    let real_gid = getgid();
+    setgid(real_gid).map_err(Error::SetGid)?;
+
+    // Launch the process
+    // TODO: execv vs execvp
+    let args: Vec<CString> = env::args().map(|arg| CString::new(arg).unwrap()).collect();
+    let args: Vec<&CStr> = args.iter().map(|arg| &**arg).collect();
+    execv(args[1], &args[2..]).map_err(Error::Exec)?;
+
+    // Does not return if successful
+
+    Ok(())
+}
