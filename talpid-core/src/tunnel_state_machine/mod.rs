32633fdf20fe48c54887915e96501ed5e13d5e65
David Lönnhager <david.l@mullvad.net>
2020-03-24 16:01:46 +0100

    Encapsulate functions in split

--- talpid-core/src/tunnel_state_machine/mod.rs
+++ talpid-core/src/tunnel_state_machine/mod.rs
@@ -235,15 +235,14 @@ impl TunnelStateMachine {
         };
 
         #[cfg(unix)]
-        {
-            split::initialize_routing_table().map_err(Error::InitSplitTunneling)?;
-            split::create_cgroup().map_err(Error::InitSplitTunneling)?;
-        }
+        let split_tunnel = split::SplitTunnel::new().map_err(Error::InitSplitTunneling)?;
 
         let firewall = Firewall::new(args).map_err(Error::InitFirewallError)?;
         let dns_monitor = DnsMonitor::new(cache_dir).map_err(Error::InitDnsMonitorError)?;
         let mut shared_values = SharedTunnelStateValues {
             firewall,
+            #[cfg(unix)]
+            split_tunnel,
             dns_monitor,
             allow_lan,
             block_when_disconnected,
@@ -327,6 +326,7 @@ pub trait TunnelParametersGenerator: Send + 'static {
 /// Values that are common to all tunnel states.
 struct SharedTunnelStateValues {
     firewall: Firewall,
+    split_tunnel: split::SplitTunnel,
     dns_monitor: DnsMonitor,
     /// Should LAN access be allowed outside the tunnel.
     allow_lan: bool,
