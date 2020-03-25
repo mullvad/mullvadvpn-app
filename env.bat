2208d40ff4e58860beb9fe3a1d8ac86f3bce6062
Emīls <emils@mullvad.net>
2020-03-25 15:05:27 +0000

    Use REST endpoint for relay list in buildscript

--- env.bat
+++
@@ -1,4 +0,0 @@
-SET SCRIPT_DIR=%~dp0
-SET OPENSSL_STATIC=1
-SET OPENSSL_LIB_DIR=%SCRIPT_DIR%\dist-assets\binaries\x86_64-pc-windows-msvc
-SET OPENSSL_INCLUDE_DIR=%SCRIPT_DIR%\dist-assets\binaries\x86_64-pc-windows-msvc\include
