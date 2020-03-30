da700859e6a3d3a22f1784e520c650be067e5c19
Andrej Mihajlov <and@mullvad.net>
2020-03-30 12:40:45 +0200

    Format account token in Account view

---
+++ ios/MullvadVPN/String+Split.swift
@@ -0,0 +1,22 @@
+//
+//  String+Split.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 27/03/2020.
+//  Copyright © 2020 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    /// Returns the array of the longest possible subsequences of the given length.
+    func split(every length: Int) -> [Substring] {
+        guard length > 0 else { return [prefix(upTo: endIndex)] }
+
+        let resultCount = Int((Float(count) / Float(length)).rounded(.up))
+
+        return (0 ..< resultCount)
+            .map { dropFirst($0 * length).prefix(length) }
+    }
+}
---
+++ ios/MullvadVPNTests/StringTests.swift
@@ -0,0 +1,29 @@
+//
+//  StringTests.swift
+//  MullvadVPNTests
+//
+//  Created by pronebird on 27/03/2020.
+//  Copyright © 2020 Mullvad VPN AB. All rights reserved.
+//
+
+import XCTest
+
+class StringTests: XCTestCase {
+
+    func testEmptyString() {
+        XCTAssertTrue("".split(every: 4).isEmpty)
+    }
+
+    func testString() {
+        XCTAssertEqual("12345678".split(every: 4), ["1234", "5678"])
+    }
+
+    func testOddString() {
+        XCTAssertEqual("123456789".split(every: 4), ["1234", "5678", "9"])
+    }
+
+    func testStringShorterThanLength() {
+        XCTAssertEqual("1".split(every: 4), ["1"])
+    }
+
+}
