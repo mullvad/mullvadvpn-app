8a8ee9594d3f46efdd9cd1143bf10313eed3aec9
David Lönnhager <david.l@mullvad.net>
2020-03-30 13:32:14 +0200

    Fix upgrade script on Linux for older versions

--- dist-assets/linux/before-install.sh
+++ dist-assets/linux/before-install.sh
@@ -3,7 +3,9 @@ set -eu
 
 if which systemctl &> /dev/null; then
     if systemctl status mullvad-daemon &> /dev/null; then
-        /opt/Mullvad\ VPN/resources/mullvad-setup prepare-restart
+        if [[ -f /opt/Mullvad\ VPN/resources/mullvad-setup ]]; then
+            /opt/Mullvad\ VPN/resources/mullvad-setup prepare-restart
+        fi
         systemctl stop mullvad-daemon.service
         systemctl disable mullvad-daemon.service
     fi
