bbe8589d5609b927a95b6f16353dff2aa7376045
Oskar Nyberg <oskar@mullvad.net>
2020-03-30 11:37:17 +0200

    Update Electron to 8.2.0

--- gui/test/setup/main.js
+++ gui/test/setup/main.js
@@ -1,5 +1,7 @@
 const log = require('electron-log');
+const { app } = require('electron');
 
 log.transports.console.level = false;
 log.transports.file.level = false;
-log.transports.rendererConsole = null;
+
+app.allowRendererProcessReuse = true;
