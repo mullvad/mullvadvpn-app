1092757a5ef50caa82ec3626a882360b0b3fa02c
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-03-31 19:46:17 +0000

    Use new widgets in `AccountFragment`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/AccountFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/AccountFragment.kt
@@ -1,15 +1,10 @@
 package net.mullvad.mullvadvpn.ui
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.FragmentManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import java.text.DateFormat
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -19,10 +14,8 @@ import net.mullvad.mullvadvpn.R
 import org.joda.time.DateTime
 
 class AccountFragment : ServiceDependentFragment(OnNoService.GoBack) {
-    private lateinit var accountExpiryContainer: View
-    private lateinit var accountExpiryDisplay: TextView
-    private lateinit var accountNumberContainer: View
-    private lateinit var accountNumberDisplay: TextView
+    private lateinit var accountExpiryView: InformationView
+    private lateinit var accountNumberView: CopyableInformationView
 
     private var updateViewJob: Job? = null
 
@@ -39,13 +32,8 @@ class AccountFragment : ServiceDependentFragment(OnNoService.GoBack) {
 
         view.findViewById<View>(R.id.logout).setOnClickListener { logout() }
 
-        accountExpiryContainer = view.findViewById<View>(R.id.account_expiry_container)
-        accountNumberContainer = view.findViewById<View>(R.id.account_number_container)
-
-        accountExpiryDisplay = view.findViewById<TextView>(R.id.account_expiry_display)
-        accountNumberDisplay = view.findViewById<TextView>(R.id.account_number_display)
-
-        accountNumberContainer.setOnClickListener { copyAccountNumberToClipboard() }
+        accountNumberView = view.findViewById(R.id.account_number)
+        accountExpiryView = view.findViewById(R.id.account_expiry)
 
         return view
     }
@@ -63,17 +51,17 @@ class AccountFragment : ServiceDependentFragment(OnNoService.GoBack) {
     private fun updateView(accountNumber: String?, accountExpiry: DateTime?) =
             GlobalScope.launch(Dispatchers.Main) {
         if (accountNumber != null) {
-            accountNumberDisplay.setText(accountNumber)
-            accountNumberContainer.visibility = View.VISIBLE
+            accountNumberView.information = accountNumber
+            accountNumberView.visibility = View.VISIBLE
         } else {
-            accountNumberContainer.visibility = View.INVISIBLE
+            accountNumberView.visibility = View.INVISIBLE
         }
 
         if (accountExpiry != null) {
-            accountExpiryDisplay.setText(formatExpiry(accountExpiry))
-            accountExpiryContainer.visibility = View.VISIBLE
+            accountExpiryView.information = formatExpiry(accountExpiry)
+            accountExpiryView.visibility = View.VISIBLE
         } else {
-            accountExpiryContainer.visibility = View.INVISIBLE
+            accountExpiryView.visibility = View.INVISIBLE
         }
     }
 
@@ -88,18 +76,6 @@ class AccountFragment : ServiceDependentFragment(OnNoService.GoBack) {
         clearAccountNumber()
         clearBackStack()
         goToLoginScreen()
-    }
-
-    private fun copyAccountNumberToClipboard() {
-        val clipboard =
-            parentActivity.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clipLabel = parentActivity.resources.getString(R.string.mullvad_account_number)
-        val clipData = ClipData.newPlainText(clipLabel, accountNumberDisplay.text)
-
-        clipboard.primaryClip = clipData
-
-        Toast.makeText(parentActivity, R.string.copied_mullvad_account_number, Toast.LENGTH_SHORT)
-            .show()
     }
 
     private fun clearAccountNumber() = GlobalScope.launch(Dispatchers.Default) {
