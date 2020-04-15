565c044b029d1dd4b22271de83c7245d1c8e6f3b
Odd Stranne <odd@mullvad.net>
2020-04-15 17:51:25 +0200

    Move logging code

--- windows/winnet/src/winnet/offlinemonitor.cpp
+++ windows/winnet/src/winnet/offlinemonitor.cpp
@@ -58,6 +58,114 @@ bool IsConnectedAdapter(const MIB_IF_ROW2 &iface)
 	);
 }
 
+void LogAdapter(std::shared_ptr<common::logging::ILogSink> logSink, const MIB_IF_ROW2 &iface)
+{
+	//
+	// Don't flood the log with garbage.
+	//
+	static const auto blacklist = std::vector<std::wstring>
+	{
+		L"WFP Native MAC Layer LightWeight Filter",
+		L"QoS Packet Scheduler",
+		L"WFP 802.3 MAC Layer LightWeight Filter",
+		L"Microsoft Kernel Debug Network Adapter",
+		L"Software Loopback Interface",
+		L"Microsoft Teredo Tunneling Adapter",
+		L"Microsoft IP-HTTPS Platform Adapter",
+		L"Microsoft 6to4 Adapter",
+		L"WAN Miniport",
+		L"WiFi Filter Driver",
+		L"Microsoft Wi-Fi Direct Virtual Adapter",
+	};
+
+	for (const auto &black : blacklist)
+	{
+		if (nullptr != wcsstr(iface.Description, black.c_str()))
+		{
+			return;
+		}
+	}
+
+	std::stringstream ss;
+
+	ss << "Detailed interface logging" << std::endl;
+
+	{
+		const auto s = std::wstring(L"  Alias: ").append(iface.Alias);
+		ss << common::string::ToAnsi(s) << std::endl;
+	}
+
+	{
+		const auto s = std::wstring(L"  Description: ").append(iface.Description);
+		ss << common::string::ToAnsi(s) << std::endl;
+	}
+
+	ss << "  PhysicalAddressLength: " << iface.PhysicalAddressLength << std::endl;
+	ss << "  Type: " << iface.Type << std::endl;
+	ss << "  MediaType: " << iface.MediaType << std::endl;
+	ss << "  PhysicalMediumType: " << iface.PhysicalMediumType << std::endl;
+	ss << "  AccessType: " << iface.AccessType << std::endl;
+
+	//
+	// Bool cast prevents idiot stream from inserting literal 0/1.
+	//
+
+	ss << "  InterfaceAndOperStatusFlags.HardwareInterface: " << (bool)iface.InterfaceAndOperStatusFlags.HardwareInterface << std::endl;
+	ss << "  InterfaceAndOperStatusFlags.FilterInterface: " << (bool)iface.InterfaceAndOperStatusFlags.FilterInterface << std::endl;
+	ss << "  InterfaceAndOperStatusFlags.ConnectorPresent: " << (bool)iface.InterfaceAndOperStatusFlags.ConnectorPresent << std::endl;
+	ss << "  InterfaceAndOperStatusFlags.NotAuthenticated: " << (bool)iface.InterfaceAndOperStatusFlags.NotAuthenticated << std::endl;
+	ss << "  InterfaceAndOperStatusFlags.NotMediaConnected: " << (bool)iface.InterfaceAndOperStatusFlags.NotMediaConnected << std::endl;
+	ss << "  InterfaceAndOperStatusFlags.Paused: " << (bool)iface.InterfaceAndOperStatusFlags.Paused << std::endl;
+	ss << "  InterfaceAndOperStatusFlags.LowPower: " << (bool)iface.InterfaceAndOperStatusFlags.LowPower << std::endl;
+	ss << "  InterfaceAndOperStatusFlags.EndPointInterface: " << (bool)iface.InterfaceAndOperStatusFlags.EndPointInterface << std::endl;
+
+	ss << "  OperStatus: " << iface.OperStatus << std::endl;
+	ss << "  AdminStatus: " << iface.AdminStatus << std::endl;
+	ss << "  MediaConnectState: " << iface.MediaConnectState << std::endl;
+	ss << "  TransmitLinkSpeed: " << iface.TransmitLinkSpeed << std::endl;
+
+	ss << "  ReceiveLinkSpeed: " << iface.ReceiveLinkSpeed << std::endl;
+	ss << "  InUcastPkts:" << iface.InUcastPkts;
+
+	logSink->info(ss.str().c_str());
+}
+
+void LogAdapters(std::shared_ptr<common::logging::ILogSink> logSink)
+{
+	//
+	// There is a race condition here because logging is not done using the
+	// same data set that the online/offline logic processes.
+	//
+	// Not much of a problem really, this is temporary logging.
+	//
+
+	MIB_IF_TABLE2 *table;
+
+	const auto status = GetIfTable2(&table);
+
+	if (NO_ERROR != status)
+	{
+		logSink->error("Failed to acquire list of network interfaces. Aborting detailed logging");
+		return;
+	}
+
+	common::memory::ScopeDestructor sd;
+
+	sd += [table]()
+	{
+		FreeMibTable(table);
+	};
+
+	logSink->info("Begin detailed listing of network interfaces");
+
+	for (ULONG i = 0; i < table->NumEntries; ++i)
+	{
+		LogAdapter(logSink, table->Table[i]);
+	}
+
+	logSink->info("End detailed listing of network interfaces");
+}
+
 } // anonymous namespace
 
 OfflineMonitor::OfflineMonitor
@@ -100,123 +208,9 @@ void OfflineMonitor::callback(const std::vector<MIB_IF_ROW2> &adapters, const MI
 
 		if (false == m_connected)
 		{
-			LogOfflineState();
+			LogAdapters(m_logSink);
 		}
 
 		m_notifier(m_connected);
 	}
-}
-
-void OfflineMonitor::LogOfflineState()
-{
-	//
-	// There is a race condition here because logging is not done using the
-	// same data set that the online/offline logic processes.
-	//
-	// Not much of a problem really, this is temporary logging.
-	//
-
-	MIB_IF_TABLE2 *table;
-
-	const auto status = GetIfTable2(&table);
-
-	if (NO_ERROR != status)
-	{
-		m_logSink->error("Failed to acquire list of network interfaces. Aborting detailed logging");
-		return;
-	}
-
-	common::memory::ScopeDestructor sd;
-
-	sd += [table]()
-	{
-		FreeMibTable(table);
-	};
-
-	m_logSink->info("Begin detailed listing of network interfaces");
-
-	for (ULONG i = 0; i < table->NumEntries; ++i)
-	{
-		const auto &iface = table->Table[i];
-
-		//
-		// Don't flood the log with garbage.
-		//
-		const auto blacklist = std::vector<std::wstring>
-		{
-			L"WFP Native MAC Layer LightWeight Filter",
-			L"QoS Packet Scheduler",
-			L"WFP 802.3 MAC Layer LightWeight Filter",
-			L"Microsoft Kernel Debug Network Adapter",
-			L"Software Loopback Interface",
-			L"Microsoft Teredo Tunneling Adapter",
-			L"Microsoft IP-HTTPS Platform Adapter",
-			L"Microsoft 6to4 Adapter",
-			L"WAN Miniport",
-			L"WiFi Filter Driver",
-			L"Microsoft Wi-Fi Direct Virtual Adapter",
-		};
-
-		bool blacklisted = false;
-
-		for (const auto &black : blacklist)
-		{
-			if (nullptr != wcsstr(iface.Description, black.c_str()))
-			{
-				blacklisted = true;
-				break;
-			}
-		}
-
-		if (blacklisted)
-		{
-			continue;
-		}
-
-		std::stringstream ss;
-
-		ss << "Detailed interface logging" << std::endl;
-		ss << "Interface ordinal " << i << std::endl;
-
-		{
-			const auto s = std::wstring(L"  Alias: ").append(iface.Alias);
-			ss << common::string::ToAnsi(s) << std::endl;
-		}
-
-		{
-			const auto s = std::wstring(L"  Description: ").append(iface.Description);
-			ss << common::string::ToAnsi(s) << std::endl;
-		}
-
-		ss << "  PhysicalAddressLength: " << iface.PhysicalAddressLength << std::endl;
-		ss << "  Type: " << iface.Type << std::endl;
-		ss << "  MediaType: " << iface.MediaType << std::endl;
-		ss << "  PhysicalMediumType: " << iface.PhysicalMediumType << std::endl;
-		ss << "  AccessType: " << iface.AccessType << std::endl;
-
-		//
-		// Bool cast prevents idiot stream from inserting literal 0/1.
-		//
-
-		ss << "  InterfaceAndOperStatusFlags.HardwareInterface: " << (bool)iface.InterfaceAndOperStatusFlags.HardwareInterface << std::endl;
-		ss << "  InterfaceAndOperStatusFlags.FilterInterface: " << (bool)iface.InterfaceAndOperStatusFlags.FilterInterface << std::endl;
-		ss << "  InterfaceAndOperStatusFlags.ConnectorPresent: " << (bool)iface.InterfaceAndOperStatusFlags.ConnectorPresent << std::endl;
-		ss << "  InterfaceAndOperStatusFlags.NotAuthenticated: " << (bool)iface.InterfaceAndOperStatusFlags.NotAuthenticated << std::endl;
-		ss << "  InterfaceAndOperStatusFlags.NotMediaConnected: " << (bool)iface.InterfaceAndOperStatusFlags.NotMediaConnected << std::endl;
-		ss << "  InterfaceAndOperStatusFlags.Paused: " << (bool)iface.InterfaceAndOperStatusFlags.Paused << std::endl;
-		ss << "  InterfaceAndOperStatusFlags.LowPower: " << (bool)iface.InterfaceAndOperStatusFlags.LowPower << std::endl;
-		ss << "  InterfaceAndOperStatusFlags.EndPointInterface: " << (bool)iface.InterfaceAndOperStatusFlags.EndPointInterface << std::endl;
-
-		ss << "  OperStatus: " << iface.OperStatus << std::endl;
-		ss << "  AdminStatus: " << iface.AdminStatus << std::endl;
-		ss << "  MediaConnectState: " << iface.MediaConnectState << std::endl;
-		ss << "  TransmitLinkSpeed: " << iface.TransmitLinkSpeed << std::endl;
-
-		ss << "  ReceiveLinkSpeed: " << iface.ReceiveLinkSpeed << std::endl;
-		ss << "  InUcastPkts:" << iface.InUcastPkts;
-
-		m_logSink->info(ss.str().c_str());
-	}
-
-	m_logSink->info("End detailed listing of network interfaces");
 }
