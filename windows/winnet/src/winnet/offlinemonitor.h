926c13becd8a7d2075f8228be3515a91cb19428b
Odd Stranne <odd@mullvad.net>
2020-04-15 17:51:25 +0200

    Move logging code

--- windows/winnet/src/winnet/offlinemonitor.h
+++ windows/winnet/src/winnet/offlinemonitor.h
@@ -29,7 +29,5 @@ private:
 	bool m_connected;
 	NetworkAdapterMonitor m_netAdapterMonitor;
 
-	void LogOfflineState();
-
 	void callback(const std::vector<MIB_IF_ROW2> &adapters, const MIB_IF_ROW2 *adapter, NetworkAdapterMonitor::UpdateType type);
 };
