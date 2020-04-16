ab2e445b16932f0504f31f24e3ef036d30a4975e
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-04-16 19:34:47 +0000

    Remove unused import

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.VpnService
 import android.os.Binder
 import android.os.IBinder
-import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
