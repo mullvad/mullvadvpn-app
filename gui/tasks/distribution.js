e8b5869195882fd7aec2aa8993d00450de901e4d
Andrej Mihajlov <and@mullvad.net>
2020-04-20 13:17:49 +0200

    Reformat code

--- gui/tasks/distribution.js
+++ gui/tasks/distribution.js
@@ -128,7 +128,8 @@ const config = {
       '/opt/Mullvad VPN/resources/mullvad-daemon.conf',
       distAssets('mullvad') + '=/usr/bin/',
       distAssets('linux/problem-report-link') + '=/usr/bin/mullvad-problem-report',
-      distAssets('shell-completions/mullvad.bash') + '=/usr/share/bash-completion/completions/mullvad',
+      distAssets('shell-completions/mullvad.bash') +
+        '=/usr/share/bash-completion/completions/mullvad',
       distAssets('shell-completions/_mullvad') + '=/usr/local/share/zsh/site-functions/_mullvad',
     ],
     afterInstall: distAssets('linux/after-install.sh'),
@@ -150,7 +151,8 @@ const config = {
       '/opt/Mullvad VPN/resources/mullvad-daemon.conf',
       distAssets('mullvad') + '=/usr/bin/',
       distAssets('linux/problem-report-link') + '=/usr/bin/mullvad-problem-report',
-      distAssets('shell-completions/mullvad.bash') + '=/usr/share/bash-completion/completions/mullvad',
+      distAssets('shell-completions/mullvad.bash') +
+        '=/usr/share/bash-completion/completions/mullvad',
       distAssets('shell-completions/_mullvad') + '=/usr/local/share/zsh/site-functions/_mullvad',
     ],
     afterInstall: distAssets('linux/after-install.sh'),
