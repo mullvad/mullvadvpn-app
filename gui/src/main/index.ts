3651aadb964e58e46398acee2655f20e04adf0e8
Oskar Nyberg <oskar@mullvad.net>
2020-04-21 13:14:31 +0200

    Revert "Fix tray icon menu on Ubuntu"
    This reverts commit 0a3bdfbd38c3948d504325b03ad6a39d82ad0d16.

--- gui/src/main/index.ts
+++ gui/src/main/index.ts
@@ -4,7 +4,6 @@ import log from 'electron-log';
 import mkdirp from 'mkdirp';
 import moment from 'moment';
 import * as path from 'path';
-import { sprintf } from 'sprintf-js';
 import * as uuid from 'uuid';
 import AccountExpiry from '../shared/account-expiry';
 import BridgeSettingsBuilder from '../shared/bridge-settings-builder';
@@ -373,7 +372,6 @@ class ApplicationMain {
         this.installGenericMenubarAppWindowHandlers(tray, windowController);
         this.installLinuxWindowCloseHandler(windowController);
         this.setLinuxAppMenu();
-        this.setLinuxTrayMenu(tray, windowController);
         window.setMenuBarVisibility(false);
         break;
       default:
@@ -1355,28 +1353,6 @@ class ApplicationMain {
       },
     ];
     Menu.setApplicationMenu(Menu.buildFromTemplate(template));
-  }
-
-  private setLinuxTrayMenu(tray: Tray, windowController: WindowController) {
-    tray.setContextMenu(
-      Menu.buildFromTemplate([
-        {
-          label: sprintf(
-            // TRANSLATORS: The label displayed in the context menu that is opened when the user
-            // TRANSLATORS: clicks the icon in the linux system tray/status bar.
-            // TRANSLATORS: Available placeholder:
-            // TRANSLATORS: %(appname) - Name of the app, i.e. Mullvad VPN
-            messages.pgettext('status-icon-menu', 'Open %(appname)s'),
-            {
-              appname: app.getName(),
-            },
-          ),
-          click: () => {
-            windowController.show();
-          },
-        },
-      ]),
-    );
   }
 
   private addContextMenu(window: BrowserWindow) {
