a861ff05a1dd194258b07aaba6548212ed3346dc
Oskar Nyberg <oskar@mullvad.net>
2020-04-27 17:29:25 +0200

    Convert Layout and Container to styled components

--- gui/src/renderer/components/LayoutStyles.tsx
+++ gui/src/renderer/components/LayoutStyles.tsx
@@ -1,17 +1,7 @@
 import { Styles } from 'reactxp';
-import { colors } from '../../config.json';
 
 export default {
-  layout: Styles.createViewStyle({
-    flexDirection: 'column',
-    flex: 1,
-  }),
   header: Styles.createViewStyle({
     flex: 0,
-  }),
-  container: Styles.createViewStyle({
-    flex: 1,
-    backgroundColor: colors.blue,
-    overflow: 'hidden',
   }),
 };
