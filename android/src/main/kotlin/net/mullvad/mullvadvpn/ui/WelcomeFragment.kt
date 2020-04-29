ef567c6b00403a6f3f7a6534b96d3dd8e6c366c4
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-04-29 16:42:16 +0000

    Copy account token to clipboard when clicked

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/WelcomeFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/WelcomeFragment.kt
@@ -1,10 +1,14 @@
 package net.mullvad.mullvadvpn.ui
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.coroutines.delay
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.ui.widget.UrlButton
@@ -29,7 +33,9 @@ class WelcomeFragment : ServiceDependentFragment(OnNoService.GoToLaunchScreen) {
             parentActivity.openSettings()
         }
 
-        accountLabel = view.findViewById<TextView>(R.id.account_number)
+        accountLabel = view.findViewById<TextView>(R.id.account_number).apply {
+            setOnClickListener { copyAccountTokenToClipboard() }
+        }
 
         view.findViewById<UrlButton>(R.id.buy_credit).apply {
             prepare(daemon, jobTracker)
@@ -68,6 +74,7 @@ class WelcomeFragment : ServiceDependentFragment(OnNoService.GoToLaunchScreen) {
 
         jobTracker.newUiJob("updateAccountNumber") {
             accountLabel.text = accountText ?: ""
+            accountLabel.setEnabled(accountText != null && accountText.length > 0)
         }
     }
 
@@ -105,5 +112,19 @@ class WelcomeFragment : ServiceDependentFragment(OnNoService.GoToLaunchScreen) {
             replace(R.id.main_fragment, ConnectFragment())
             commit()
         }
+    }
+
+    private fun copyAccountTokenToClipboard() {
+        val accountToken = accountLabel.text
+        val clipboardLabel = resources.getString(R.string.mullvad_account_number)
+        val toastMessage = resources.getString(R.string.copied_mullvad_account_number)
+
+        val context = parentActivity
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData = ClipData.newPlainText(clipboardLabel, accountToken)
+
+        clipboard.primaryClip = clipData
+
+        Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
     }
 }
