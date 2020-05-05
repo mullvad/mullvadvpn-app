b1e8a92d8fbabea29d76aa782663b4388cef8223
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-05-05 19:35:57 +0000

    Implement Cancel button behavior

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/RedeemVoucherDialogFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/RedeemVoucherDialogFragment.kt
@@ -5,16 +5,29 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.ui.widget.Button
+import net.mullvad.mullvadvpn.util.JobTracker
 
 class RedeemVoucherDialogFragment : DialogFragment() {
+    private val jobTracker = JobTracker()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = inflater.inflate(R.layout.redeem_voucher, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.redeem_voucher, container, false)
+
+        view.findViewById<Button>(R.id.cancel).setOnClickAction("action", jobTracker) {
+            activity?.onBackPressed()
+        }
+
+        return view
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState)
@@ -28,5 +41,11 @@ class RedeemVoucherDialogFragment : DialogFragment() {
         super.onStart()
 
         dialog?.window?.setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onDestroyView() {
+        jobTracker.cancelAllJobs()
+
+        super.onDestroyView()
     }
 }
