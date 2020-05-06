cd8f2af7d92d327240b53e6dfe74d670e965e370
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-05-06 01:15:31 +0000

    Only enable Redeem button if voucher code is valid

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/RedeemVoucherDialogFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/RedeemVoucherDialogFragment.kt
@@ -4,6 +4,8 @@ import android.app.Dialog
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,9 +16,12 @@ import net.mullvad.mullvadvpn.ui.widget.Button
 import net.mullvad.mullvadvpn.util.JobTracker
 import net.mullvad.mullvadvpn.util.SegmentedInputFormatter
 
+const val FULL_VOUCHER_CODE_LENGTH = "XXXX-XXXX-XXXX-XXXX".length
+
 class RedeemVoucherDialogFragment : DialogFragment() {
     private val jobTracker = JobTracker()
 
+    private lateinit var redeemButton: Button
     private lateinit var voucherInput: EditText
 
     override fun onCreateView(
@@ -26,13 +31,22 @@ class RedeemVoucherDialogFragment : DialogFragment() {
     ): View {
         val view = inflater.inflate(R.layout.redeem_voucher, container, false)
 
-        voucherInput = view.findViewById(R.id.voucher_code)
+        voucherInput = view.findViewById<EditText>(R.id.voucher_code).apply {
+            addTextChangedListener(ValidVoucherCodeChecker())
+        }
 
         SegmentedInputFormatter(voucherInput, '-').apply {
             allCaps = true
 
             isValidInputCharacter = { character ->
                 ('A' <= character && character <= 'Z') || ('0' <= character && character <= '9')
+            }
+        }
+
+        redeemButton = view.findViewById<Button>(R.id.redeem).apply {
+            setEnabled(false)
+            setOnClickAction("action", jobTracker) {
+                dismiss()
             }
         }
 
@@ -61,5 +75,23 @@ class RedeemVoucherDialogFragment : DialogFragment() {
         jobTracker.cancelAllJobs()
 
         super.onDestroyView()
+    }
+
+    inner class ValidVoucherCodeChecker : TextWatcher {
+        private var editRecursionCount = 0
+
+        override fun beforeTextChanged(text: CharSequence, start: Int, count: Int, after: Int) {
+            editRecursionCount += 1
+        }
+
+        override fun onTextChanged(text: CharSequence, start: Int, before: Int, count: Int) {}
+
+        override fun afterTextChanged(text: Editable) {
+            editRecursionCount -= 1
+
+            if (editRecursionCount == 0) {
+                redeemButton.setEnabled(text.length == FULL_VOUCHER_CODE_LENGTH)
+            }
+        }
     }
 }
