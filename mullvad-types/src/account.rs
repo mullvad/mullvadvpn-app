ca6b51fde35b4625d8484122a20064956a33d2b4
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-05-06 19:14:34 +0000

    Create `VoucherSubmission` class

--- mullvad-types/src/account.rs
+++ mullvad-types/src/account.rs
@@ -18,10 +18,14 @@ pub struct AccountData {
 /// Data structure that's returned from successful invocation of the mullvad API's
 /// `/v1/submit-voucher` RPC.
 #[derive(Deserialize, Serialize, Debug)]
+#[cfg_attr(target_os = "android", derive(IntoJava))]
+#[cfg_attr(target_os = "android", jnix(package = "net.mullvad.mullvadvpn.model"))]
 pub struct VoucherSubmission {
     /// Amount of time added to the account
+    #[cfg_attr(target_os = "android", jnix(map = "|time_added| time_added as i64"))]
     pub time_added: u64,
     /// Updated expiry time
+    #[cfg_attr(target_os = "android", jnix(map = "|expiry| expiry.to_string()"))]
     pub new_expiry: DateTime<Utc>,
 }
 
