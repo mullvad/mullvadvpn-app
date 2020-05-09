2f898ef4cc48d8142d8985205e238a641aab8f8f
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-05-09 14:47:26 +0000

    Hide disconnect button when already disconnected

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/OutOfTimeFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/OutOfTimeFragment.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.model.TunnelState
 import net.mullvad.mullvadvpn.ui.widget.Button
 import net.mullvad.mullvadvpn.ui.widget.UrlButton
 import net.mullvad.mullvadvpn.util.JobTracker
+import net.mullvad.talpid.tunnel.ActionAfterDisconnect
 
 class OutOfTimeFragment : ServiceDependentFragment(OnNoService.GoToLaunchScreen) {
     private val jobTracker = JobTracker()
 
     private lateinit var disconnectButton: Button
+
+    private var tunnelStateListener: Int? = null
+
+    private var tunnelState: TunnelState = TunnelState.Disconnected()
+        set(value) {
+            field = value
+            updateDisconnectButton()
+        }
 
     override fun onSafelyCreateView(
         inflater: LayoutInflater,
@@ -41,11 +51,21 @@ class OutOfTimeFragment : ServiceDependentFragment(OnNoService.GoToLaunchScreen)
             }
         }
 
+        tunnelStateListener = connectionProxy.onStateChange.subscribe() { newState ->
+            jobTracker.newUiJob("updateTunnelState") {
+                tunnelState = newState
+            }
+        }
+
         return view
     }
 
     override fun onSafelyDestroyView() {
         jobTracker.cancelAllJobs()
+
+        tunnelStateListener?.let { id ->
+            connectionProxy.onStateChange.unsubscribe(id)
+        }
     }
 
     private fun showRedeemVoucherDialog() {
@@ -54,5 +74,28 @@ class OutOfTimeFragment : ServiceDependentFragment(OnNoService.GoToLaunchScreen)
         transaction?.addToBackStack(null)
 
         RedeemVoucherDialogFragment().show(transaction, null)
+    }
+
+    private fun updateDisconnectButton() {
+        val state = tunnelState
+
+        val showButton = when (state) {
+            is TunnelState.Disconnected -> false
+            is TunnelState.Connecting, is TunnelState.Connected -> true
+            is TunnelState.Disconnecting -> {
+                state.actionAfterDisconnect != ActionAfterDisconnect.Nothing
+            }
+            is TunnelState.Error -> state.errorState.isBlocking
+        }
+
+        disconnectButton.apply {
+            if (showButton) {
+                setEnabled(true)
+                visibility = View.VISIBLE
+            } else {
+                setEnabled(false)
+                visibility = View.GONE
+            }
+        }
     }
 }
