6b5544113486f96cb7078cefccd497b21ec10ecc
Oskar Nyberg <oskar@mullvad.net>
2020-05-12 22:38:48 +0200

    Convert RedeemVoucher.tsx to styled components and functional components

--- gui/src/renderer/components/RedeemVoucherStyles.tsx
+++ gui/src/renderer/components/RedeemVoucherStyles.tsx
@@ -1,46 +1,46 @@
-import { Styles } from 'reactxp';
 import styled from 'styled-components';
 import { colors } from '../../config.json';
 import ImageView from './ImageView';
 
-export const Spinner = styled(ImageView)({
+export const StyledInput = styled.input({
+  flex: 1,
+  overflow: 'hidden',
+  padding: '14px',
+  fontFamily: 'Open Sans',
+  fontSize: '13px',
+  fontWeight: 600,
+  lineHeight: '26px',
+  color: colors.blue,
+  backgroundColor: colors.white,
+  border: 'none',
+  borderRadius: '4px',
+  '::placeholder': {
+    color: colors.blue40,
+  },
+});
+
+export const StyledResponse = styled.span({
+  marginTop: '8px',
+  fontFamily: 'Open Sans',
+  fontSize: '13px',
+  lineHeight: '20px',
+});
+
+export const StyledSuccessResponse = styled(StyledResponse)({
+  fontWeight: 600,
+  color: colors.green,
+});
+
+export const StyledErrorResponse = styled(StyledResponse)({
+  fontWeight: 800,
+  color: colors.red,
+});
+
+export const StyledEmptyResponse = styled.span({
+  height: '20px',
   marginTop: '8px',
 });
 
-export default {
-  textInput: Styles.createTextInputStyle({
-    flex: 1,
-    overflow: 'hidden',
-    paddingTop: 14,
-    paddingLeft: 14,
-    paddingRight: 14,
-    paddingBottom: 14,
-    fontFamily: 'Open Sans',
-    fontSize: 13,
-    fontWeight: '600',
-    lineHeight: 26,
-    color: colors.blue,
-    backgroundColor: colors.white,
-    borderRadius: 4,
-  }),
-  redeemVoucherResponseSuccess: Styles.createTextStyle({
-    marginTop: 8,
-    fontFamily: 'Open Sans',
-    fontSize: 13,
-    fontWeight: '600',
-    lineHeight: 20,
-    color: colors.green,
-  }),
-  redeemVoucherResponseError: Styles.createTextStyle({
-    marginTop: 8,
-    fontFamily: 'Open Sans',
-    fontSize: 13,
-    fontWeight: '800',
-    lineHeight: 20,
-    color: colors.red,
-  }),
-  redeemVoucherResponseEmpty: Styles.createViewStyle({
-    height: 20,
-    marginTop: 8,
-  }),
-};
+export const StyledSpinner = styled(ImageView)({
+  marginTop: '8px',
+});
