8557e5aa95f06c1d6ebea6eae7631a024a482260
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-05-22 14:56:19 +0000

    Document `EventNotifier`'s object ID usage risks

--- android/src/main/kotlin/net/mullvad/talpid/util/EventNotifier.kt
+++ android/src/main/kotlin/net/mullvad/talpid/util/EventNotifier.kt
@@ -1,5 +1,13 @@
 package net.mullvad.talpid.util
 
+// Manages listeners interested in receiving events of type T
+//
+// The listeners subscribe using an ID object. This ID is used later on for unsubscribing. The only
+// requirement is that the object uses the default implementation of the `hashCode` and `equals`
+// methods inherited from `Any` (or `Object` in Java).
+//
+// If the ID object class (or any of its super-classes) overrides `hashCode` or `equals`,
+// unsubscribe might not work correctly.
 class EventNotifier<T>(private val initialValue: T) {
     private val listeners = HashMap<Any, (T) -> Unit>()
 
