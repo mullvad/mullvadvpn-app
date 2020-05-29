4bdb7ff09606cf739e25019eeb6a07e8aa8301c3
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-05-29 14:17:20 +0000

    Create `LinearInterpolation` helper class

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/util/LinearInterpolation.kt
@@ -0,0 +1,46 @@
+package net.mullvad.mullvadvpn.util
+
+import kotlin.properties.Delegates.observable
+import kotlin.reflect.KProperty
+
+class LinearInterpolation {
+    private val observer = { property: KProperty<*>, oldValue: Float, newValue: Float ->
+        if (!updated && oldValue != newValue) {
+            updated = true
+        }
+    }
+
+    private val realStart
+        get() = start - reference
+
+    private val realEnd
+        get() = end - reference
+
+    var reference by observable(0.0f, observer)
+    var start by observable(0.0f, observer)
+    var end by observable(0.0f, observer)
+
+    var updated = true
+        get() {
+            if (field == true) {
+                field = false
+                return true
+            } else {
+                return false
+            }
+        }
+
+    fun interpolate(progress: Float): Float {
+        return progress * (realEnd - realStart) + realStart
+    }
+
+    fun progress(interpolation: Float): Float {
+        val length = realEnd - realStart
+
+        if (length == 0.0f) {
+            return 0.0f
+        }
+
+        return (interpolation - realStart) / length
+    }
+}
