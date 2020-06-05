5038cf7814a1cdca66cb6c45a6d140027fde33e3
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-06-05 14:06:22 +0000

    Create `TimeLeftFormatter` helper class

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/util/TimeLeftFormatter.kt
@@ -0,0 +1,36 @@
+package net.mullvad.mullvadvpn.util
+
+import android.content.res.Resources
+import android.view.View
+import android.widget.TextView
+import net.mullvad.mullvadvpn.R
+import org.joda.time.DateTime
+import org.joda.time.Duration
+import org.joda.time.PeriodType
+
+class TimeLeftFormatter(val resources: Resources) {
+    fun format(accountExpiry: DateTime): String {
+        val remainingTime = Duration(DateTime.now(), accountExpiry)
+
+        if (remainingTime.isShorterThan(Duration.ZERO)) {
+            return resources.getString(R.string.out_of_time)
+        } else {
+            val remainingTimeInfo =
+                remainingTime.toPeriodTo(accountExpiry, PeriodType.yearMonthDayTime())
+
+            if (remainingTimeInfo.years > 0) {
+                return getRemainingText(R.plurals.years_left, remainingTimeInfo.years)
+            } else if (remainingTimeInfo.months >= 3) {
+                return getRemainingText(R.plurals.months_left, remainingTimeInfo.months)
+            } else if (remainingTimeInfo.months > 0 || remainingTimeInfo.days >= 1) {
+                return getRemainingText(R.plurals.days_left, remainingTime.standardDays.toInt())
+            } else {
+                return resources.getString(R.string.less_than_a_day_left)
+            }
+        }
+    }
+
+    private fun getRemainingText(pluralId: Int, quantity: Int): String {
+        return resources.getQuantityString(pluralId, quantity, quantity)
+    }
+}
