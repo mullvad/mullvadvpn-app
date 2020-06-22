dcb0fddb237a7dedf35c207efff394bb825cd570
Emīls <emils@mullvad.net>
2020-06-22 16:34:04 +0100

    Use new version info in Android

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/AppVersionInfo.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/AppVersionInfo.kt
@@ -2,7 +2,5 @@ package net.mullvad.mullvadvpn.model
 
 data class AppVersionInfo(
     val supported: Boolean,
-    val latest: String,
-    val latestStable: String,
-    val latestBeta: String
+    val suggestedUpgrade: String?
 )
