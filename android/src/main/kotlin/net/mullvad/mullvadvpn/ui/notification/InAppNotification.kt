c2fbf06016da682ac26cec220bebbbe67069af43
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-06-26 16:53:22 +0000

    Create `InAppNotification` abstract class

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/notification/InAppNotification.kt
@@ -0,0 +1,40 @@
+package net.mullvad.mullvadvpn.ui.notification
+
+import net.mullvad.mullvadvpn.util.ChangeMonitor
+import net.mullvad.mullvadvpn.util.JobTracker
+
+abstract class InAppNotification {
+    private val changeMonitor = ChangeMonitor()
+    protected val jobTracker = JobTracker()
+
+    var status by changeMonitor.monitor(StatusLevel.Error)
+        protected set
+
+    var title by changeMonitor.monitor("")
+        protected set
+
+    var message by changeMonitor.monitor<String?>(null)
+        protected set
+
+    var onClick by changeMonitor.monitor<(suspend () -> Unit)?>(null)
+        protected set
+
+    var showIcon by changeMonitor.monitor(false)
+        protected set
+
+    var shouldShow by changeMonitor.monitor(false)
+        protected set
+
+    open fun onResume() {}
+    open fun onPause() {}
+
+    open fun onDestroy() {
+        jobTracker.cancelAllJobs()
+    }
+
+    protected fun update() {
+        if (changeMonitor.changed) {
+            changeMonitor.reset()
+        }
+    }
+}
