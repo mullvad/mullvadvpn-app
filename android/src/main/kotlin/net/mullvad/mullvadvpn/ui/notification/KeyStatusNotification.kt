580524edb7d7d6d6dd27bba20ec3e71adb6ed534
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-06-26 17:09:05 +0000

    Create WireGuard key error in-app notification

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/notification/KeyStatusNotification.kt
@@ -0,0 +1,58 @@
+package net.mullvad.mullvadvpn.ui.notification
+
+import android.content.Context
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.model.KeygenEvent
+import net.mullvad.mullvadvpn.service.KeyStatusListener
+import net.mullvad.mullvadvpn.service.MullvadDaemon
+
+class KeyStatusNotification(
+    context: Context,
+    daemon: MullvadDaemon,
+    private val keyStatusListener: KeyStatusListener
+) : NotificationWithUrlWithToken(context, daemon, R.string.wg_key_url) {
+    private val failedToGenerateKey = context.getString(R.string.failed_to_generate_key)
+    private val tooManyKeys = context.getString(R.string.too_many_keys)
+
+    init {
+        status = StatusLevel.Error
+        title = context.getString(R.string.wireguard_error)
+    }
+
+    override fun onResume() {
+        keyStatusListener.onKeyStatusChange.subscribe(this) { keyStatus ->
+            jobTracker.newUiJob("updateKeyStatus") {
+                updateKeyStatus(keyStatus)
+            }
+        }
+    }
+
+    override fun onPause() {
+        keyStatusListener.onKeyStatusChange.unsubscribe(this)
+    }
+
+    private fun updateKeyStatus(keyStatus: KeygenEvent?) {
+        when (keyStatus) {
+            null -> shouldShow = false
+            is KeygenEvent.NewKey -> shouldShow = false
+            is KeygenEvent.TooManyKeys -> showTooManyKeys()
+            is KeygenEvent.GenerationFailure -> showGenerationFailure()
+        }
+
+        update()
+    }
+
+    private fun showTooManyKeys() {
+        onClick = openUrl
+        message = tooManyKeys
+        showIcon = true
+        shouldShow = true
+    }
+
+    private fun showGenerationFailure() {
+        onClick = null
+        message = failedToGenerateKey
+        showIcon = false
+        shouldShow = true
+    }
+}
