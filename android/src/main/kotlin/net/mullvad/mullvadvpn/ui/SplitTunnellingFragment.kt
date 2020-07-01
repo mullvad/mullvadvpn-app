4425d16e0e7871de4e2955716498b61b7f9cfd9d
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-01 14:10:44 +0000

    Create initial Split Tunnelling screen

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/SplitTunnellingFragment.kt
@@ -0,0 +1,43 @@
+package net.mullvad.mullvadvpn.ui
+
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.applist.AppListAdapter
+import net.mullvad.mullvadvpn.ui.widget.CustomRecyclerView
+import net.mullvad.mullvadvpn.util.AdapterWithHeader
+
+class SplitTunnellingFragment : ServiceDependentFragment(OnNoService.GoToLaunchScreen) {
+    private val appListAdapter = AppListAdapter()
+
+    private lateinit var titleController: CollapsibleTitleController
+
+    override fun onSafelyCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.split_tunnelling, container, false)
+
+        titleController = CollapsibleTitleController(view, R.id.app_list)
+
+        view.findViewById<CustomRecyclerView>(R.id.app_list).apply {
+            layoutManager = LinearLayoutManager(parentActivity)
+
+            adapter = AdapterWithHeader(appListAdapter, R.layout.split_tunnelling_header).apply {
+                onHeaderAvailable = { headerView ->
+                    titleController.expandedTitleView = headerView.findViewById(R.id.expanded_title)
+                }
+            }
+        }
+
+        return view
+    }
+
+    override fun onSafelyDestroyView() {
+        titleController.onDestroy()
+    }
+}
