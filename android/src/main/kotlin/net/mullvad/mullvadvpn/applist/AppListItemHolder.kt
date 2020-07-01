abc0499c33e47a7d09fcf7a6632ef207b517b3f1
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-01 13:19:36 +0000

    Create initial empty `AppListItemHolder` class

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppListItemHolder.kt
@@ -0,0 +1,6 @@
+package net.mullvad.mullvadvpn.applist
+
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.View
+
+class AppListItemHolder(view: View) : ViewHolder(view)
