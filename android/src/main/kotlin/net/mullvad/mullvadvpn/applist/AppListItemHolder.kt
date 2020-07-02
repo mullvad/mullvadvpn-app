16320ba69320cf1f5cd986c764aa56e8315781e7
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-02 20:58:44 +0000

    Show app name in the list

--- android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppListItemHolder.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppListItemHolder.kt
@@ -1,6 +1,25 @@
 package net.mullvad.mullvadvpn.applist
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.properties.Delegates.observable
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.ui.CellSwitch
 
-class AppListItemHolder(view: View) : ViewHolder(view)
+class AppListItemHolder(private val packageManager: PackageManager, view: View) : ViewHolder(view) {
+    private val icon: ImageView = view.findViewById(R.id.icon)
+    private val name: TextView = view.findViewById(R.id.name)
+    private val excluded: CellSwitch = view.findViewById(R.id.excluded)
+
+    var appInfo by observable<ApplicationInfo?>(null) { _, _, info ->
+        if (info != null) {
+            name.text = packageManager.getApplicationLabel(info)
+        } else {
+            name.text = ""
+        }
+    }
+}
