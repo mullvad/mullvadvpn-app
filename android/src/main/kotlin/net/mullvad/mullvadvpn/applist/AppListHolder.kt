3aa4fb38caefb8cad00329ca3b550edb6be4141d
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-03 17:12:24 +0000

    Make app list items clickable

--- android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppListHolder.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppListHolder.kt
@@ -39,6 +39,12 @@ class AppListHolder(
         }
     }
 
+    init {
+        view.setOnClickListener {
+            excluded.toggle()
+        }
+    }
+
     private fun hideIcon() {
         icon.visibility = View.GONE
         loading.visibility = View.VISIBLE
