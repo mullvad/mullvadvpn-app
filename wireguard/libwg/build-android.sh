d88e9fbac396cd5a33a503caad9d97fb1cb76c17
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-06 21:28:54 +0000

    Use prebuild toolchain

--- wireguard/libwg/build-android.sh
+++ wireguard/libwg/build-android.sh
@@ -14,47 +14,34 @@ for arch in arm arm64 x86_64 x86; do
     case "$arch" in
         "arm64")
             export ANDROID_LLVM_TRIPLE="aarch64-linux-android"
-            export ANDROID_LIB_TRIPLE="aarch64-linux-android"
             export RUST_TARGET_TRIPLE="aarch64-linux-android"
             export ANDROID_ABI="arm64-v8a"
             ;;
         "x86_64")
             export ANDROID_LLVM_TRIPLE="x86_64-linux-android"
-            export ANDROID_LIB_TRIPLE="x86_64-linux-android"
             export RUST_TARGET_TRIPLE="x86_64-linux-android"
             export ANDROID_ABI="x86_64"
             ;;
         "arm")
             export ANDROID_LLVM_TRIPLE="armv7a-linux-androideabi"
-            export ANDROID_LIB_TRIPLE="arm-linux-androideabi"
             export RUST_TARGET_TRIPLE="armv7-linux-androideabi"
             export ANDROID_ABI="armeabi-v7a"
             ;;
         "x86")
             export ANDROID_LLVM_TRIPLE="i686-linux-android"
-            export ANDROID_LIB_TRIPLE="i686-linux-android"
             export RUST_TARGET_TRIPLE="i686-linux-android"
             export ANDROID_ABI="x86"
             ;;
     esac
 
-    if which install-ndk-toolchain > /dev/null; then
-        eval "$(install-ndk-toolchain $arch)"
-    else
-        export ANDROID_TOOLCHAIN_ROOT="$(eval "echo \$ANDROID_TOOLCHAIN_ROOT_$arch")"
-        export ANDROID_SYSROOT="${ANDROID_TOOLCHAIN_ROOT}/sysroot"
-        export ANDROID_C_COMPILER="${ANDROID_TOOLCHAIN_ROOT}/bin/${ANDROID_LIB_TRIPLE}-clang"
-    fi
-
+    export ANDROID_C_COMPILER="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/${ANDROID_LLVM_TRIPLE}21-clang"
     export ANDROID_ARCH_NAME=$arch
-    export PATH="$PATH:${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin"
 
     # Build Wireguard-Go
     echo $(pwd)
     make -f Android.mk clean
 
     export CFLAGS="-D__ANDROID_API__=21"
-    export LDFLAGS="-L${ANDROID_SYSROOT}/usr/lib/${ANDROID_LIB_TRIPLE}/21"
 
     make -f Android.mk
     # Copy build artifacts to `build/libs/$RUST_TARGET_TRIPLE` to be able to build `mullvad-jni`
