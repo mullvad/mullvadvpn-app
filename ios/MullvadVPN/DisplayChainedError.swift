dbf4232b503ad264a98d8d65b20063986dda77f4
Andrej Mihajlov <and@mullvad.net>
2020-07-06 13:44:20 +0200

    Add ChainedError and DisplayChainedError protocols

---
+++ ios/MullvadVPN/DisplayChainedError.swift
@@ -0,0 +1,13 @@
+//
+//  DisplayChainedError.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 04/06/2020.
+//  Copyright © 2020 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+
+protocol DisplayChainedError {
+    var errorChainDescription: String? { get }
+}
