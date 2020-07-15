615182c5ef6e112876826ff910ad9669d715d13b
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-15 20:37:39 +0000

    Add `isIpv6` property to `InetNetwork`

--- android/src/main/kotlin/net/mullvad/talpid/tun_provider/InetNetwork.kt
+++ android/src/main/kotlin/net/mullvad/talpid/tun_provider/InetNetwork.kt
@@ -1,5 +1,8 @@
 package net.mullvad.talpid.tun_provider
 
 import java.net.InetAddress
+import java.net.Inet6Address
 
-data class InetNetwork(val address: InetAddress, val prefixLength: Short)
+data class InetNetwork(val address: InetAddress, val prefixLength: Short) {
+    val isIpv6 = address is Inet6Address
+}
