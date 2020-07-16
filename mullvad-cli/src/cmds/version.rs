538a197efd71436233e785ba7d8c3df045465225
David Lönnhager <david.l@mullvad.net>
2020-07-16 17:05:52 +0200

    Use gRPC for version command

--- mullvad-cli/src/cmds/version.rs
+++ mullvad-cli/src/cmds/version.rs
@@ -1,4 +1,4 @@
-use crate::{new_rpc_client, Command, Result};
+use crate::{new_grpc_client, Command, Error, Result};
 
 pub struct Version;
 
@@ -13,23 +13,27 @@ impl Command for Version {
             .about("Shows current version, and the currently supported versions")
     }
 
-    async fn run(&self, matches: &clap::ArgMatches<'_>) -> Result<()> {
-        let mut rpc = new_rpc_client()?;
-        let current_version = rpc.get_current_version()?;
+    async fn run(&self, _: &clap::ArgMatches<'_>) -> Result<()> {
+        let mut rpc = new_grpc_client().await?;
+        let current_version = rpc.get_current_version(()).await?.into_inner();
         println!("Current version: {}", current_version);
-        let version_info = rpc.get_version_info()?;
+        let version_info = rpc.get_version_info(()).await?.into_inner();
         println!("\tIs supported: {}", version_info.supported);
 
-        match version_info.suggested_upgrade {
-            Some(version) => println!("\tSuggested update: {}", version),
-            None => println!("\tNo newer version is available"),
+        if !version_info.suggested_upgrade.is_empty() {
+            println!("\tSuggested update: {}", version_info.suggested_upgrade);
+        } else {
+            println!("\tNo newer version is available");
         }
 
         if !version_info.latest_stable.is_empty() {
             println!("\tLatest stable version: {}", version_info.latest_stable);
         }
 
-        let settings = rpc.get_settings()?;
+        let settings = rpc.get_settings(())
+            .await
+            .map_err(Error::GrpcClientError)?
+            .into_inner();
         if settings.show_beta_releases {
             println!("\t Latest beta version: {}", version_info.latest_beta);
         };
