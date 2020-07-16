c4a3005c52552cff86df283f6a7ed6577479ee7c
David Lönnhager <david.l@mullvad.net>
2020-07-16 16:54:26 +0200

    Use gRPC for block-when-disconnected command

--- mullvad-cli/src/cmds/block_when_disconnected.rs
+++ mullvad-cli/src/cmds/block_when_disconnected.rs
@@ -1,4 +1,4 @@
-use crate::{new_rpc_client, Command, Result};
+use crate::{new_grpc_client, Command, Error, Result};
 use clap::value_t_or_exit;
 
 pub struct BlockWhenDisconnected;
@@ -31,9 +31,9 @@ impl Command for BlockWhenDisconnected {
     async fn run(&self, matches: &clap::ArgMatches<'_>) -> Result<()> {
         if let Some(set_matches) = matches.subcommand_matches("set") {
             let block_when_disconnected = value_t_or_exit!(set_matches.value_of("policy"), String);
-            self.set(block_when_disconnected == "on")
+            self.set(block_when_disconnected == "on").await
         } else if let Some(_matches) = matches.subcommand_matches("get") {
-            self.get()
+            self.get().await
         } else {
             unreachable!("No block-when-disconnected command given");
         }
@@ -41,16 +41,22 @@ impl Command for BlockWhenDisconnected {
 }
 
 impl BlockWhenDisconnected {
-    fn set(&self, block_when_disconnected: bool) -> Result<()> {
-        let mut rpc = new_rpc_client()?;
-        rpc.set_block_when_disconnected(block_when_disconnected)?;
+    async fn set(&self, block_when_disconnected: bool) -> Result<()> {
+        let mut rpc = new_grpc_client().await?;
+        rpc.set_block_when_disconnected(block_when_disconnected)
+            .await
+            .map_err(Error::GrpcClientError)?;
         println!("Changed always require VPN setting");
         Ok(())
     }
 
-    fn get(&self) -> Result<()> {
-        let mut rpc = new_rpc_client()?;
-        let block_when_disconnected = rpc.get_settings()?.block_when_disconnected;
+    async fn get(&self) -> Result<()> {
+        let mut rpc = new_grpc_client().await?;
+        let block_when_disconnected = rpc.get_settings(())
+            .await
+            .map_err(Error::GrpcClientError)?
+            .into_inner()
+            .block_when_disconnected;
         println!(
             "Network traffic will be {} when the VPN is disconnected",
             if block_when_disconnected {
