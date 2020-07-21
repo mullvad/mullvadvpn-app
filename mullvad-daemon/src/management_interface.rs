8f54a5c1e67308b9b2ae71cd043ae1b5f3fca1ca
David Lönnhager <david.l@mullvad.net>
2020-07-21 10:17:28 +0200

    Make current location optional

--- mullvad-daemon/src/management_interface.rs
+++ mullvad-daemon/src/management_interface.rs
@@ -359,7 +359,6 @@ impl ManagementService for ManagementServiceImpl {
     }
 
     async fn get_current_location(&self, _: Request<()>) -> ServiceResult<proto::GeoIpLocation> {
-        // NOTE: optional result
         log::debug!("get_current_location");
         let (tx, rx) = sync::oneshot::channel();
         self.send_command_to_daemon(DaemonCommand::GetCurrentLocation(tx))
@@ -368,8 +367,7 @@ impl ManagementService for ManagementServiceImpl {
                 if let Some(geoip) = geoip {
                     Ok(Response::new(convert_geoip_location(geoip)))
                 } else {
-                    // FIXME: handle error properly
-                    Err(tonic::Status::internal("internal error"))
+                    Err(tonic::Status::not_found("no location was found"))
                 }
             })
             .compat()
