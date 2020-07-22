65ae56d25eb6ef9640f77bbed6490f735d138fca
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-22 23:04:32 +0000

    Create `NavigateCell` widget

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/NavigateCell.kt
@@ -0,0 +1,36 @@
+package net.mullvad.mullvadvpn.ui.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+import net.mullvad.mullvadvpn.R
+
+class NavigateCell : Cell {
+    private val chevron = ImageView(context).apply {
+        val width = resources.getDimensionPixelSize(R.dimen.chevron_width)
+        val height = resources.getDimensionPixelSize(R.dimen.chevron_height)
+
+        layoutParams = LayoutParams(width, height, 0.0f)
+        alpha = 0.6f
+
+        setImageResource(R.drawable.icon_chevron)
+    }
+
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attributes: AttributeSet) : super(context, attributes) {}
+
+    constructor(context: Context, attributes: AttributeSet, defaultStyleAttribute: Int) :
+        super(context, attributes, defaultStyleAttribute) {}
+
+    constructor(
+        context: Context,
+        attributes: AttributeSet,
+        defaultStyleAttribute: Int,
+        defaultStyleResource: Int
+    ) : super(context, attributes, defaultStyleAttribute, defaultStyleResource) {}
+
+    init {
+        addView(chevron)
+    }
+}
