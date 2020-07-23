ab771bdfb4752d2140e94071f381cdffb2160fe8
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-23 16:50:10 +0000

    Create `ToggleCell` helper widget

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/ToggleCell.kt
@@ -0,0 +1,45 @@
+package net.mullvad.mullvadvpn.ui.widget
+
+import android.content.Context
+import android.util.AttributeSet
+
+class ToggleCell : Cell {
+    private val toggle = CellSwitch(context).apply {
+        layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 0.0f)
+    }
+
+    var state
+        get() = toggle.state
+        set(value) { toggle.state = value }
+
+    var listener
+        get() = toggle.listener
+        set(value) { toggle.listener = value }
+
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attributes: AttributeSet) : super(context, attributes) {}
+
+    constructor(context: Context, attributes: AttributeSet, defaultStyleAttribute: Int) :
+        super(context, attributes, defaultStyleAttribute) {}
+
+    constructor(
+        context: Context,
+        attributes: AttributeSet,
+        defaultStyleAttribute: Int,
+        defaultStyleResource: Int
+    ) : super(context, attributes, defaultStyleAttribute, defaultStyleResource) {}
+
+    init {
+        onClickListener = { toggle() }
+        cell.addView(toggle)
+    }
+
+    fun toggle() {
+        toggle.toggle()
+    }
+
+    fun forcefullySetState(state: CellSwitch.State) {
+        toggle.forcefullySetState(state)
+    }
+}
