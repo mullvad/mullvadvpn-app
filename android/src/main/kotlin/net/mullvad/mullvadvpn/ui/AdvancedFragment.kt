0ad325047380968ac22012010c1ea81f204db2dc
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-23 23:16:46 +0000

    Use `MtuCell` widget

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/AdvancedFragment.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/AdvancedFragment.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.model.Settings
+import net.mullvad.mullvadvpn.ui.widget.MtuCell
 import net.mullvad.mullvadvpn.ui.widget.NavigateCell
 
-private const val MIN_MTU_VALUE = 1280
-private const val MAX_MTU_VALUE = 1420
-
 class AdvancedFragment : ServiceDependentFragment(OnNoService.GoBack) {
-    private lateinit var wireguardMtuInput: CellInput
+    private lateinit var wireguardMtuInput: MtuCell
     private lateinit var titleController: CollapsibleTitleController
 
     override fun onSafelyCreateView(
@@ -27,17 +24,12 @@ class AdvancedFragment : ServiceDependentFragment(OnNoService.GoBack) {
             parentActivity.onBackPressed()
         }
 
-        wireguardMtuInput =
-            CellInput(view.findViewById(R.id.wireguard_mtu_input), MIN_MTU_VALUE, MAX_MTU_VALUE)
-
-        wireguardMtuInput.onSubmit = { mtu ->
-            jobTracker.newBackgroundJob("updateMtu") {
-                daemon.setWireguardMtu(mtu)
+        wireguardMtuInput = view.findViewById<MtuCell>(R.id.wireguard_mtu).apply {
+            onSubmit = { mtu ->
+                jobTracker.newBackgroundJob("updateMtu") {
+                    daemon.setWireguardMtu(mtu)
+                }
             }
-        }
-
-        view.findViewById<TextView>(R.id.wireguard_mtu_footer).apply {
-            text = context.getString(R.string.wireguard_mtu_footer, MIN_MTU_VALUE, MAX_MTU_VALUE)
         }
 
         view.findViewById<NavigateCell>(R.id.wireguard_keys).apply {
