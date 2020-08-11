8425b66001f7f09e6666942a5a2ef848f95a122c
David Lönnhager <david.l@mullvad.net>
2020-08-11 11:31:13 +0200

    Remove WinUtil

--- mullvad-daemon/build.rs
+++ mullvad-daemon/build.rs
@@ -1,51 +1,5 @@
 use std::{env, fs, path::PathBuf, process::Command};
 
-#[cfg(windows)]
-mod win {
-    use std::{env, path::PathBuf};
-
-    pub static WINUTIL_BUILD_DIR: &'static str = "..\\windows\\winutil\\bin";
-
-    pub fn default_windows_build_artifact_dir(build_dir: &str) -> PathBuf {
-        manifest_dir().join(build_dir).join(&target_platform_dir())
-    }
-
-    fn target_platform_dir() -> PathBuf {
-        let target = env::var("TARGET").expect("TARGET env var not set");
-
-        let target_dir = match target.as_str() {
-            "i686-pc-windows-msvc" => format!("Win32-{}", get_build_mode()),
-            "x86_64-pc-windows-msvc" => format!("x64-{}", get_build_mode()),
-            _ => panic!("uncrecognized target: {}", target),
-        };
-        target_dir.into()
-    }
-
-    fn get_build_mode() -> &'static str {
-        let profile = env::var("PROFILE").expect("PROFILE env var not set");
-        if profile == "release" {
-            "Release"
-        } else {
-            "Debug"
-        }
-    }
-
-    pub fn declare_library(env_var: &str, default_dir: &str, lib_name: &str) {
-        println!("cargo:rerun-if-env-changed={}", env_var);
-        let lib_dir = env::var_os(env_var)
-            .map(PathBuf::from)
-            .unwrap_or_else(|| default_windows_build_artifact_dir(default_dir));
-        println!("cargo:rustc-link-search={}", lib_dir.display());
-        println!("cargo:rustc-link-lib=dylib={}", lib_name);
-    }
-
-    fn manifest_dir() -> PathBuf {
-        env::var("CARGO_MANIFEST_DIR")
-            .map(PathBuf::from)
-            .expect("CARGO_MANIFEST_DIR env var not set")
-    }
-}
-
 fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
@@ -72,14 +26,6 @@ fn main() {
             println!("cargo:warning=Skipping mullvad-daemon manifest");
         }
         res.compile().expect("Unable to generate windows resources");
-    }
-
-    #[cfg(windows)]
-    {
-        use crate::win::*;
-
-        const WINUTIL_DIR_VAR: &str = "WINUTIL_LIB_DIR";
-        declare_library(WINUTIL_DIR_VAR, WINUTIL_BUILD_DIR, "winutil");
     }
 }
 
