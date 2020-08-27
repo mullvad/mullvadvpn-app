3b32d0fdca4d41f5d7e71271e4e94bd5b7e2cac9
Oskar Nyberg <oskar@mullvad.net>
2020-08-27 12:13:24 +0200

    Convert WireGuard keys view from ReactXP

--- gui/src/renderer/components/WireguardKeysStyles.tsx
+++ gui/src/renderer/components/WireguardKeysStyles.tsx
@@ -1,70 +1,62 @@
-import { Styles } from 'reactxp';
 import styled from 'styled-components';
 import { colors } from '../../config.json';
+import { smallText } from './common-styles';
+import { Container } from './Layout';
 import { NavigationScrollbars } from './NavigationBar';
 
 export const StyledNavigationScrollbars = styled(NavigationScrollbars)({
   flex: 1,
 });
 
-export default {
-  wgkeys: Styles.createViewStyle({
-    backgroundColor: colors.darkBlue,
-    flex: 1,
-  }),
-  wgkeys__container: Styles.createViewStyle({
-    flexDirection: 'column',
-    flex: 1,
-  }),
-  wgkeys__content: Styles.createViewStyle({
-    // ReactXP don't allow setting 'minHeight' and don't allow percentages. This will work well
-    // without the '@ts-ignore' when moving away from ReactXP.
-    // @ts-ignore
-    minHeight: '100%',
-  }),
-  wgkeys__messages: Styles.createViewStyle({
-    flex: 1,
-  }),
-  wgkeys__row: Styles.createViewStyle({
-    paddingVertical: 0,
-    paddingHorizontal: 22,
-    marginBottom: 20,
-  }),
-  wgkeys__button_row: Styles.createViewStyle({
-    paddingHorizontal: 22,
-    marginBottom: 18,
-  }),
-  wgkeys__last_button: Styles.createViewStyle({
-    marginBottom: 22,
-  }),
-  wgkeys__row_label: Styles.createTextStyle({
-    flex: 1,
-    fontFamily: 'Open Sans',
-    fontSize: 13,
-    fontWeight: '600',
-    lineHeight: 20,
-    color: colors.white60,
-    marginBottom: 9,
-  }),
-  wgkeys__row_value: Styles.createTextStyle({
-    fontFamily: 'Open Sans',
-    fontSize: 16,
-    lineHeight: 19,
-    fontWeight: '800',
-    color: colors.white,
-  }),
-  wgkeys__invalid_key: Styles.createTextStyle({
-    fontFamily: 'Open Sans',
-    fontSize: 13,
-    fontWeight: '800',
-    lineHeight: 20,
-    color: colors.red,
-  }),
-  wgkeys__valid_key: Styles.createTextStyle({
-    fontFamily: 'Open Sans',
-    fontSize: 13,
-    fontWeight: '600',
-    lineHeight: 20,
-    color: colors.green,
-  }),
-};
+export const StyledContainer = styled(Container)({
+  backgroundColor: colors.darkBlue,
+});
+
+export const StyledContent = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100%',
+});
+
+export const StyledMessages = styled.div({
+  padding: '0 22px 20px',
+  flex: 1,
+});
+
+export const StyledMessage = styled.span(smallText, (props: { success: boolean }) => ({
+  fontWeight: props.success ? 600 : 800,
+  color: props.success ? colors.green : colors.red,
+}));
+
+export const StyledRow = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '0 22px',
+  marginBottom: '20px',
+});
+
+export const StyledButtonRow = styled(StyledRow)({
+  marginBottom: '18px',
+});
+
+export const StyledLastButtonRow = styled(StyledButtonRow)({
+  marginBottom: '22px',
+});
+
+export const StyledRowLabel = styled.span(smallText, {
+  display: 'flex',
+  color: colors.white60,
+  marginBottom: '9px',
+});
+
+export const StyledRowLabelSpacer = styled.div({
+  flex: 1,
+});
+
+export const StyledRowValue = styled.span({
+  fontFamily: 'Open Sans',
+  fontSize: '16px',
+  lineHeight: '19px',
+  fontWeight: 800,
+  color: colors.white,
+});
