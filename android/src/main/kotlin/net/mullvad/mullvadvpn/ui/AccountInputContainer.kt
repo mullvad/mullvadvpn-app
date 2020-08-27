6630480014888d0dc06dcb382741e7e56e4d325f
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-08-27 22:19:23 +0000

    Remove border overlay

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/AccountInputContainer.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/AccountInputContainer.kt
@@ -26,9 +26,6 @@ class AccountInputContainer : RelativeLayout {
     private val focusedCorner = resources.getDrawable(R.drawable.account_input_corner_focused, null)
     private val unfocusedCorner = resources.getDrawable(R.drawable.account_input_corner, null)
 
-    private val errorBorder = resources.getDrawable(R.drawable.account_input_border_error, null)
-    private val focusedBorder = resources.getDrawable(R.drawable.account_input_border_focused, null)
-
     private val topLeftCorner: ImageView = container.findViewById(R.id.top_left_corner)
     private val topRightCorner: ImageView = container.findViewById(R.id.top_right_corner)
     private val bottomLeftCorner: ImageView = container.findViewById(R.id.bottom_left_corner)
@@ -43,18 +40,10 @@ class AccountInputContainer : RelativeLayout {
         set(value) {
             field = value
 
-            overlay.clear()
-
             when (value) {
                 BorderState.UNFOCUSED -> setBorder(unfocusedCorner)
-                BorderState.FOCUSED -> {
-                    setBorder(focusedCorner)
-                    overlay.add(focusedBorder)
-                }
-                BorderState.ERROR -> {
-                    setBorder(errorCorner)
-                    overlay.add(errorBorder)
-                }
+                BorderState.FOCUSED -> setBorder(focusedCorner)
+                BorderState.ERROR -> setBorder(errorCorner)
             }
         }
 
@@ -85,24 +74,6 @@ class AccountInputContainer : RelativeLayout {
         defaultStyleAttribute: Int,
         defaultStyleResource: Int
     ) : super(context, attributes, defaultStyleAttribute, defaultStyleResource) {
-    }
-
-    protected override fun onLayout(
-        changed: Boolean,
-        left: Int,
-        top: Int,
-        right: Int,
-        bottom: Int
-    ) {
-        super.onLayout(changed, left, top, right, bottom)
-
-        if (changed) {
-            val width = right - left
-            val height = bottom - top
-
-            errorBorder.setBounds(0, 0, width, height)
-            focusedBorder.setBounds(0, 0, width, height)
-        }
     }
 
     private fun setBorder(corner: Drawable) {
