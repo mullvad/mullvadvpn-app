033c0d42f8135581f88d5842c2c659f00e20d968
David Lönnhager <david.l@mullvad.net>
2020-08-31 12:50:21 +0200

    Remove unused Android dependencies

--- talpid-core/src/tunnel/openvpn.rs
+++ talpid-core/src/tunnel/openvpn.rs
@@ -110,7 +110,7 @@ static OPENVPN_DIE_TIMEOUT: Duration = Duration::from_secs(30);
 
 #[cfg(target_os = "macos")]
 const OPENVPN_PLUGIN_FILENAME: &str = "libtalpid_openvpn_plugin.dylib";
-#[cfg(any(target_os = "linux", target_os = "android"))]
+#[cfg(target_os = "linux")]
 const OPENVPN_PLUGIN_FILENAME: &str = "libtalpid_openvpn_plugin.so";
 #[cfg(windows)]
 const OPENVPN_PLUGIN_FILENAME: &str = "talpid_openvpn_plugin.dll";
