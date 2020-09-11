c4d3e9b19cefe06200f824bf41bce8c4537194f9
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-09-11 12:24:35 +0000

    Create `PluralForm` type

--- android/translations-converter/src/gettext.rs
+++ android/translations-converter/src/gettext.rs
@@ -18,6 +18,16 @@ pub struct Translation {
     entries: Vec<MsgEntry>,
 }
 
+/// Known plural forms.
+#[derive(Clone, Copy, Debug)]
+pub enum PluralForm {
+    Single,
+    SingularForOne,
+    SingularForZeroAndOne,
+    Polish,
+    Russian,
+}
+
 /// A message entry in a gettext translation file.
 #[derive(Clone, Debug)]
 pub struct MsgEntry {
@@ -108,6 +118,28 @@ impl IntoIterator for Translation {
 
     fn into_iter(self) -> Self::IntoIter {
         self.entries.into_iter()
+    }
+}
+
+impl PluralForm {
+    /// Obtain an instance based on a known plural formula.
+    ///
+    /// Plural variants need to be obtained using a formula. However, some locales have known
+    /// formulas, so they can be represented as a known plural form. This constructor can return a
+    /// plural form based on the formulas that are known to be used in the project.
+    pub fn from_formula(formula: &str) -> Self {
+        match formula {
+            "nplurals=1; plural=0" => PluralForm::Single,
+            "nplurals=2; plural=(n != 1)" => PluralForm::SingularForOne,
+            "nplurals=2; plural=(n > 1)" => PluralForm::SingularForZeroAndOne,
+            "nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3)" => {
+                PluralForm::Polish
+            }
+            "nplurals=4; plural=((n%10==1 && n%100!=11) ? 0 : ((n%10 >= 2 && n%10 <=4 && (n%100 < 12 || n%100 > 14)) ? 1 : ((n%10 == 0 || (n%10 >= 5 && n%10 <=9)) || (n%100 >= 11 && n%100 <= 14)) ? 2 : 3))" => {
+                PluralForm::Russian
+            }
+            other => panic!("Unknown plural formula: {}", other),
+        }
     }
 }
 
