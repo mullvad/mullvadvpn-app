edce8a7c717caad2f4142c282f87fa628613580b
Andrej Mihajlov <and@mullvad.net>
2020-09-14 17:23:58 +0200

    Remove never unused options

--- ios/MullvadVPN/Data+HexCoding.swift
+++ ios/MullvadVPN/Data+HexCoding.swift
@@ -9,13 +9,7 @@
 import Foundation
 
 extension Data {
-    struct HexEncodingOptions: OptionSet {
-        let rawValue: Int
-        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
-    }
-
-    func hexEncodedString(options: HexEncodingOptions = []) -> String {
-        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
-        return map { String(format: format, $0) }.joined()
+    func hexEncodedString() -> String {
+        return map { String(format: "%02hhx", $0) }.joined()
     }
 }
