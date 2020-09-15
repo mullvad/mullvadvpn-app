add7519b99276b0378bd01dea636c35677823de4
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-09-15 21:36:28 +0000

    Remove unnecessary property

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ListItemDividerDecoration.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ListItemDividerDecoration.kt
@@ -8,7 +8,7 @@ import android.support.v7.widget.RecyclerView.State
 import android.view.View
 import net.mullvad.mullvadvpn.R
 
-class ListItemDividerDecoration(private val context: Context) : ItemDecoration() {
+class ListItemDividerDecoration(context: Context) : ItemDecoration() {
     private val dividerHeight = context.resources.getDimensionPixelSize(R.dimen.list_item_divider)
 
     override fun getItemOffsets(offsets: Rect, view: View, parent: RecyclerView, state: State) {
