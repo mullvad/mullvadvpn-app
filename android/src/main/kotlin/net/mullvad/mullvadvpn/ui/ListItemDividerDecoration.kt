947cfe09cffb5a3f1d4956a7c192623fe5b6b43f
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-09-15 21:51:10 +0000

    Allow setting the top offset of the decoration

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ListItemDividerDecoration.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ListItemDividerDecoration.kt
@@ -10,6 +10,7 @@ import kotlin.properties.Delegates.observable
 
 class ListItemDividerDecoration(context: Context) : ItemDecoration() {
     private var bottomOffset = 0
+    private var topOffset = 0
 
     var bottomOffsetId by observable<Int?>(null) { _, _, id ->
         if (id != null) {
@@ -19,7 +20,16 @@ class ListItemDividerDecoration(context: Context) : ItemDecoration() {
         }
     }
 
+    var topOffsetId by observable<Int?>(null) { _, _, id ->
+        if (id != null) {
+            topOffset = context.resources.getDimensionPixelSize(id)
+        } else {
+            topOffset = 0
+        }
+    }
+
     override fun getItemOffsets(offsets: Rect, view: View, parent: RecyclerView, state: State) {
         offsets.bottom = bottomOffset
+        offsets.top = topOffset
     }
 }
