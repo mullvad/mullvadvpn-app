66859a9f130ade461cdae5051caeffd7a4d5b551
Oskar Nyberg <oskar@mullvad.net>
2020-09-16 17:08:16 +0200

    Add aria-invalid attribute to mssfix and mtu inputs

--- gui/src/renderer/components/Cell.tsx
+++ gui/src/renderer/components/Cell.tsx
@@ -154,12 +154,15 @@ export class Input extends React.Component<IInputProps, IInputState> {
       ...otherProps
     } = this.props;
 
+    const valid = validateValue?.(this.state.value);
+
     return (
       <CellDisabledContext.Consumer>
         {(disabled) => (
           <StyledInput
             type="text"
-            valid={validateValue?.(this.state.value)}
+            valid={valid}
+            aria-invalid={!valid}
             onChange={this.onChange}
             onFocus={this.onFocus}
             onBlur={this.onBlur}
