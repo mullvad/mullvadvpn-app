9562ea7296027925fa28f1e9778235a223116af1
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-09-16 21:43:58 +0000

    Format account history entries

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/AccountHistoryAdapter.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/AccountHistoryAdapter.kt
@@ -5,8 +5,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import kotlin.properties.Delegates.observable
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.util.SegmentedTextFormatter
 
 class AccountHistoryAdapter : Adapter<AccountHistoryHolder>() {
+    private val formatter = SegmentedTextFormatter(' ').apply {
+        isValidInputCharacter = { character ->
+            '0' <= character && character <= '9'
+        }
+    }
+
     var accountHistory by observable(ArrayList<String>()) { _, _, _ ->
         notifyDataSetChanged()
     }
@@ -17,7 +24,7 @@ class AccountHistoryAdapter : Adapter<AccountHistoryHolder>() {
         val inflater = LayoutInflater.from(parentView.context)
         val view = inflater.inflate(R.layout.account_history_entry, parentView, false)
 
-        return AccountHistoryHolder(view).apply {
+        return AccountHistoryHolder(view, formatter).apply {
             onSelect = { account -> onSelectEntry?.invoke(account) }
         }
     }
--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/AccountHistoryHolder.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/AccountHistoryHolder.kt
@@ -5,12 +5,16 @@ import android.view.View
 import android.widget.TextView
 import kotlin.properties.Delegates.observable
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.util.SegmentedTextFormatter
 
-class AccountHistoryHolder(view: View) : ViewHolder(view) {
+class AccountHistoryHolder(
+    view: View,
+    private val formatter: SegmentedTextFormatter
+) : ViewHolder(view) {
     private val label: TextView = view.findViewById(R.id.label)
 
     var accountToken by observable("") { _, _, account ->
-        label.text = account
+        label.text = formatter.format(account)
     }
 
     var onSelect: ((String) -> Unit)? = null
