fceb72c90c0e7a320ebdb760c22a198f757c14a0
Oskar Nyberg <oskar@mullvad.net>
2020-09-21 15:08:42 +0200

    Make BlockingButton use a context instead of cloning children

---
+++ gui/src/renderer/lib/utilityHooks.ts
@@ -0,0 +1,15 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+export function useMounted() {
+  const mountedRef = useRef(false);
+  const isMounted = useCallback(() => mountedRef.current, []);
+
+  useEffect(() => {
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+
+  return isMounted;
+}
