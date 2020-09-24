e361bd19597aaec8c00aae29430ef88d3418d1b4
Emīls <emils@mullvad.net>
2020-09-24 12:40:07 +0100

    Always use fallback address

--- mullvad-rpc/src/address_cache.rs
+++ mullvad-rpc/src/address_cache.rs
@@ -134,6 +134,10 @@ impl AddressCacheInner {
             }
         }
 
+        if !addresses.contains(&FALLBACK_API_ADDRESS.into()) {
+            addresses.push(FALLBACK_API_ADDRESS.into());
+        }
+
         Ok(Self {
             addresses,
             ..Default::default()
