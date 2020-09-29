d8c3b77352fec329514f71d172346190a68f7fe0
David Lönnhager <david.l@mullvad.net>
2020-09-29 15:34:52 +0200

    Remove dead code in driverlogic

--- windows/driverlogic/src/driverlogic.cpp
+++ windows/driverlogic/src/driverlogic.cpp
@@ -25,10 +25,6 @@
 namespace
 {
 
-constexpr wchar_t DEPRECATED_TAP_HARDWARE_ID[] = L"tap0901";
-constexpr wchar_t TAP_HARDWARE_ID[] = L"tapmullvad0901";
-constexpr wchar_t TAP_BASE_ALIAS[] = L"Mullvad";
-
 constexpr std::chrono::milliseconds REGISTRY_GET_TIMEOUT_MS{ 10000 };
 
 enum ReturnCodes
@@ -538,7 +534,41 @@ void throwUpdateException(DWORD lastError, const char *operation)
 	THROW_SETUPAPI_ERROR(lastError, operation);
 }
 
-void RenameAdapter(const std::wstring &guid, const std::wstring &baseName);
+//
+// Broken adapters may use our "Mullvad" name, so find one that is not in use.
+// NOTE: Enumerating adapters first and picking the next free name is not sufficient,
+//       because the broken adapter may not be included.
+//
+void RenameAdapter(const std::wstring &guid, const std::wstring &baseName)
+{
+	common::network::Nci nci;
+
+	try
+	{
+		nci.setConnectionName(common::Guid::FromString(guid), baseName.c_str());
+		return;
+	}
+	catch (...)
+	{
+	}
+
+	for (int i = 1; i < 10; i++)
+	{
+		std::wstringstream ss;
+		ss << baseName << L"-" << i;
+
+		try
+		{
+			nci.setConnectionName(common::Guid::FromString(guid), ss.str().c_str());
+			return;
+		}
+		catch (...)
+		{
+		}
+	}
+
+	THROW_ERROR("Unable to rename network adapter");
+}
 
 void CreateNetDevice(const std::wstring &hardwareId, const std::optional<std::wstring> alias, bool installDeviceDriver)
 {
@@ -634,85 +664,6 @@ void CreateNetDevice(const std::wstring &hardwareId, const std::optional<std::ws
 	}
 }
 
-void UpdateTapDriver(const std::wstring &infPath)
-{
-	Log(L"Attempting to install new driver");
-
-	DWORD installFlags = 0;
-	BOOL rebootRequired = FALSE;
-
-ATTEMPT_UPDATE:
-
-	auto result = UpdateDriverForPlugAndPlayDevicesW(
-		nullptr,
-		TAP_HARDWARE_ID,
-		infPath.c_str(),
-		installFlags,
-		&rebootRequired
-	);
-
-	if (FALSE == result)
-	{
-		const auto lastError = GetLastError();
-
-		if (ERROR_NO_MORE_ITEMS == lastError
-			&& (installFlags ^ INSTALLFLAG_FORCE))
-		{
-			Log(L"Driver update failed. Attempting forced install.");
-			installFlags |= INSTALLFLAG_FORCE;
-
-			goto ATTEMPT_UPDATE;
-		}
-
-		throwUpdateException(lastError, "UpdateDriverForPlugAndPlayDevicesW");
-	}
-
-	//
-	// Driver successfully installed or updated
-	//
-
-	std::wstringstream ss;
-	ss << L"TAP driver update complete. Reboot required: "
-		<< rebootRequired;
-	Log(ss.str());
-}
-
-//
-// Broken adapters may use our "Mullvad" name, so find one that is not in use.
-// NOTE: Enumerating adapters first and picking the next free name is not sufficient,
-//       because the broken TAP may not be included.
-//
-void RenameAdapter(const std::wstring &guid, const std::wstring &baseName)
-{
-	common::network::Nci nci;
-
-	try
-	{
-		nci.setConnectionName(common::Guid::FromString(guid), baseName.c_str());
-		return;
-	}
-	catch (...)
-	{
-	}
-
-	for (int i = 1; i < 10; i++)
-	{
-		std::wstringstream ss;
-		ss << baseName << L"-" << i;
-
-		try
-		{
-			nci.setConnectionName(common::Guid::FromString(guid), ss.str().c_str());
-			return;
-		}
-		catch (...)
-		{
-		}
-	}
-
-	THROW_ERROR("Unable to rename network adapter");
-}
-
 std::optional<NetworkAdapter> FindAdapterByAlias(const std::set<NetworkAdapter> &tapAdapters, const std::wstring &baseName)
 {
 	if (tapAdapters.empty())
@@ -721,7 +672,7 @@ std::optional<NetworkAdapter> FindAdapterByAlias(const std::set<NetworkAdapter> 
 	}
 
 	//
-	// Look for TAP adapter with alias "Mullvad".
+	// Look for TAP adapter with aliases starting with baseName.
 	//
 
 	auto findByAlias = [](const std::set<NetworkAdapter> &adapters, const std::wstring &alias)
@@ -764,24 +715,6 @@ std::optional<NetworkAdapter> FindAdapterByAlias(const std::set<NetworkAdapter> 
 	return std::nullopt;
 }
 
-NetworkAdapter FindNetAdapter(const std::wstring &hardwareId)
-{
-	std::set<NetworkAdapter> added = GetNetworkAdapters(hardwareId);
-
-	if (added.empty())
-	{
-		THROW_ERROR("Could not identify virtual network adapter");
-	}
-	else if (added.size() > 1)
-	{
-		LogAdapters(L"Enumerable virtual network adapters", added);
-
-		THROW_ERROR("Identified more network adapters than expected");
-	}
-
-	return *added.begin();
-}
-
 bool RemoveNetDevice(const std::wstring &tapHardwareId, const std::wstring &guid)
 {
 	bool deletedAdapter = false;
@@ -810,28 +743,6 @@ bool RemoveNetDevice(const std::wstring &tapHardwareId, const std::wstring &guid
 	});
 
 	return deletedAdapter;
-}
-
-void RemoveTapDriver(const std::wstring &tapHardwareId)
-{
-	ForEachNetworkDevice(tapHardwareId, [](HDEVINFO devInfo, const SP_DEVINFO_DATA &devInfoData) {
-		try
-		{
-			DeleteDevice(devInfo, devInfoData);
-		}
-		catch (const std::exception & e)
-		{
-			//
-			// Skip this adapter
-			//
-
-			std::wstringstream ss;
-			ss << L"Skipping TAP adapter due to exception caught while iterating: "
-				<< common::string::ToWide(e.what());
-			LogError(ss.str());
-		}
-		return true;
-	});
 }
 
 void RemoveNetAdapterByAlias(const std::wstring &hardwareId, const std::wstring &baseName)
@@ -874,27 +785,7 @@ int wmain(int argc, const wchar_t * argv[], const wchar_t * [])
 
 	try
 	{
-		if (0 == _wcsicmp(argv[1], L"install"))
-		{
-			if (3 != argc)
-			{
-				goto INVALID_ARGUMENTS;
-			}
-
-			CreateNetDevice(TAP_HARDWARE_ID, std::nullopt, false);
-			UpdateTapDriver(argv[2]);
-			RenameAdapter(FindNetAdapter(TAP_HARDWARE_ID).guid, TAP_BASE_ALIAS);
-		}
-		else if (0 == _wcsicmp(argv[1], L"update"))
-		{
-			if (3 != argc)
-			{
-				goto INVALID_ARGUMENTS;
-			}
-
-			UpdateTapDriver(argv[2]);
-		}
-		else if (0 == _wcsicmp(argv[1], L"new-device"))
+		if (0 == _wcsicmp(argv[1], L"new-device"))
 		{
 			if (4 != argc)
 			{
@@ -935,19 +826,6 @@ int wmain(int argc, const wchar_t * argv[], const wchar_t * [])
 			{
 				return ADAPTER_NOT_FOUND;
 			}
-		}
-		else if (0 == _wcsicmp(argv[1], L"remove"))
-		{
-			if (3 != argc)
-			{
-				goto INVALID_ARGUMENTS;
-			}
-
-			RemoveTapDriver(argv[2]);
-		}
-		else if (0 == _wcsicmp(argv[1], L"remove-vanilla-tap"))
-		{
-			RemoveNetAdapterByAlias(DEPRECATED_TAP_HARDWARE_ID, TAP_BASE_ALIAS);
 		}
 		else
 		{
