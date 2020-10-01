94f6e2ca7a7e492316f015da82c8140f04ae64f7
Oskar Nyberg <oskar@mullvad.net>
2020-10-01 13:22:02 +0200

    Add --wait argument to disconnect and reconnect commands

--- mullvad-cli/src/cmds/disconnect.rs
+++ mullvad-cli/src/cmds/disconnect.rs
@@ -1,4 +1,5 @@
-use crate::{new_rpc_client, Command, Result};
+use crate::{format, new_rpc_client, state, Command, Result};
+use mullvad_management_interface::types::tunnel_state::State::Disconnected;
 
 pub struct Disconnect;
 
@@ -11,11 +12,40 @@ impl Command for Disconnect {
     fn clap_subcommand(&self) -> clap::App<'static, 'static> {
         clap::SubCommand::with_name(self.name())
             .about("Command the client to disconnect the VPN tunnel")
+            .arg(
+                clap::Arg::with_name("wait")
+                    .long("wait")
+                    .short("w")
+                    .help("Wait until disconnected before exiting"),
+            )
     }
 
-    async fn run(&self, _: &clap::ArgMatches<'_>) -> Result<()> {
+    async fn run(&self, matches: &clap::ArgMatches<'_>) -> Result<()> {
         let mut rpc = new_rpc_client().await?;
+
+        let status_listen_handle = if matches.is_present("wait") {
+            state::state_listen(
+                &mut rpc,
+                |state| match state {
+                    Disconnected(_) => false,
+                    _ => true,
+                },
+                Box::new(|state| match state {
+                    Disconnected(_) => false,
+                    _ => true,
+                }),
+                Box::new(format::print_state),
+            )
+            .await?
+        } else {
+            None
+        };
+
         rpc.disconnect_tunnel(()).await?;
+        if let Some(handle) = status_listen_handle {
+            handle.await.expect("Failed to listen to status updates");
+        }
+
         Ok(())
     }
 }
