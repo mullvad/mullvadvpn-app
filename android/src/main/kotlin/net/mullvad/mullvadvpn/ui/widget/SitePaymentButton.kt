0640f773151e2a053a6717a889b06cffda0af667
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-10-03 01:20:43 +0000

    Create `SitePaymentButton` widget

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/SitePaymentButton.kt
@@ -0,0 +1,35 @@
+package net.mullvad.mullvadvpn.ui.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import kotlin.properties.Delegates.observable
+import net.mullvad.mullvadvpn.R
+
+class SitePaymentButton : UrlButton {
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attributes: AttributeSet) : super(context, attributes) {}
+
+    constructor(context: Context, attributes: AttributeSet, defaultStyleAttribute: Int) :
+        super(context, attributes, defaultStyleAttribute) {}
+
+    constructor(
+        context: Context,
+        attributes: AttributeSet,
+        defaultStyleAttribute: Int,
+        defaultStyleResource: Int
+    ) : super(context, attributes, defaultStyleAttribute, defaultStyleResource) {}
+
+    var newAccount by observable(false) { _, _, isNewAccount ->
+        if (isNewAccount) {
+            label = context.getString(R.string.buy_credit)
+        } else {
+            label = context.getString(R.string.buy_more_credit)
+        }
+    }
+
+    init {
+        url = context.getString(R.string.account_url)
+        withToken = true
+    }
+}
