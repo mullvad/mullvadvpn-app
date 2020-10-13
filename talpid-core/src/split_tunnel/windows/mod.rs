5de110b654b34ca9ae5b1c40a1e7d7e25e6633b2
David Lönnhager <david.l@mullvad.net>
2020-10-13 14:40:53 +0200

    Log error events from split tunnel driver

--- talpid-core/src/split_tunnel/windows/mod.rs
+++ talpid-core/src/split_tunnel/windows/mod.rs
@@ -5,8 +5,15 @@ use std::{
     ffi::OsStr,
     io,
     net::{Ipv4Addr, Ipv6Addr},
+    os::windows::{
+        io::{AsRawHandle, IntoRawHandle, RawHandle},
+        thread,
+    },
 };
 use talpid_types::ErrorExt;
+use winapi::um::processthreadsapi::TerminateThread;
+
+const DRIVER_EVENT_BUFFER_SIZE: usize = 2048;
 
 /// Errors that may occur in [`SplitTunnel`].
 #[derive(err_derive::Error, Debug)]
@@ -26,22 +33,80 @@ pub enum Error {
 }
 
 /// Manages applications whose traffic to exclude from the tunnel.
-pub struct SplitTunnel(driver::DeviceHandle);
+pub struct SplitTunnel {
+    handle: driver::DeviceHandle,
+    event_thread: Option<std::thread::JoinHandle<()>>,
+}
+
+struct HandleContainer {
+    handle: RawHandle,
+}
+// FIXME: ! This is not safe. The handle will be invalidated when SplitTunnel is dropped
+unsafe impl Send for HandleContainer {}
 
 impl SplitTunnel {
     /// Initialize the driver.
     pub fn new() -> Result<Self, Error> {
-        Ok(SplitTunnel(
-            driver::DeviceHandle::new().map_err(Error::InitializationFailed)?,
-        ))
+        // TODO: spawn event monitor
+        let handle = driver::DeviceHandle::new().map_err(Error::InitializationFailed)?;
+
+        // FIXME: Want to use same pointer, but must be certain that the thread dies after this dies
+
+        let raw_handle = HandleContainer {
+            handle: handle.as_raw_handle(),
+        };
+
+        let event_thread = std::thread::spawn(move || {
+            use driver::{EventBody, EventId};
+
+            let mut data_buffer = Vec::with_capacity(DRIVER_EVENT_BUFFER_SIZE);
+
+            loop {
+                match driver::deque_event(raw_handle.handle, &mut data_buffer) {
+                    Ok((event_id, event_body)) => {
+                        let event_str = match &event_id {
+                            EventId::StartSplittingProcess
+                            | EventId::ErrorStartSplittingProcess => "Start splitting process",
+                            EventId::StopSplittingProcess | EventId::ErrorStopSplittingProcess => {
+                                "Stop splitting process"
+                            }
+                        };
+
+                        match event_body {
+                            EventBody::SplittingError { process_id, image } => {
+                                log::error!(
+                                    "FAILED: {}:\n\tpid: {}\n\timage: {:?}",
+                                    event_str,
+                                    process_id,
+                                    image,
+                                );
+                            }
+                            _ => (),
+                        }
+                    }
+                    Err(error) => {
+                        log::error!("{}", error.display_chain_with_msg("deque_event failed"));
+                    }
+                }
+
+                // TODO: Quit when signaled. Overlapping + WaitForMultipleObjects?
+            }
+        });
+
+        Ok(SplitTunnel {
+            handle,
+            event_thread: Some(event_thread),
+        })
     }
 
     /// Set a list of applications to exclude from the tunnel.
     pub fn set_paths<T: AsRef<OsStr>>(&self, paths: &[T]) -> Result<(), Error> {
         if paths.len() > 0 {
-            self.0.set_config(paths).map_err(Error::SetConfiguration)
+            self.handle
+                .set_config(paths)
+                .map_err(Error::SetConfiguration)
         } else {
-            self.0.clear_config().map_err(Error::SetConfiguration)
+            self.handle.clear_config().map_err(Error::SetConfiguration)
         }
     }
 
@@ -53,7 +118,7 @@ impl SplitTunnel {
         internet_ipv4: Ipv4Addr,
         internet_ipv6: Option<Ipv6Addr>,
     ) -> Result<(), Error> {
-        self.0
+        self.handle
             .register_ips(tunnel_ipv4, tunnel_ipv6, internet_ipv4, internet_ipv6)
             .map_err(Error::RegisterIps)
     }
@@ -61,6 +126,13 @@ impl SplitTunnel {
 
 impl Drop for SplitTunnel {
     fn drop(&mut self) {
+        // FIXME: Use signals to close the thread gracefully, followed by a join
+        if let Some(event_thread) = self.event_thread.take() {
+            unsafe {
+                TerminateThread(event_thread.into_raw_handle(), 0);
+            }
+        }
+
         let paths: [&OsStr; 0] = [];
         if let Err(error) = self.set_paths(&paths) {
             log::error!("{}", error.display_chain());
