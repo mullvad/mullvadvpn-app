63f7862295dc9a765f29e771d4222ba8457f8b8a
David Lönnhager <david.l@mullvad.net>
2020-10-16 13:33:48 +0200

    Add CLI interface for custom DNS

---
+++ mullvad-cli/src/cmds/custom_dns.rs
@@ -0,0 +1,74 @@
+use crate::{new_rpc_client, Command, Result};
+use mullvad_management_interface::types;
+
+pub struct CustomDns;
+
+#[mullvad_management_interface::async_trait]
+impl Command for CustomDns {
+    fn name(&self) -> &'static str {
+        "custom-dns"
+    }
+
+    fn clap_subcommand(&self) -> clap::App<'static, 'static> {
+        clap::SubCommand::with_name(self.name())
+            .about("Configure custom DNS servers to use when connected")
+            .setting(clap::AppSettings::SubcommandRequiredElseHelp)
+            .subcommand(
+                clap::SubCommand::with_name("set")
+                    .about("Change custom DNS setting")
+                    .arg(
+                        clap::Arg::with_name("servers")
+                            .multiple(true)
+                            .required(false),
+                    ),
+            )
+            .subcommand(
+                clap::SubCommand::with_name("get").about("Display the current custom DNS setting"),
+            )
+    }
+
+    async fn run(&self, matches: &clap::ArgMatches<'_>) -> Result<()> {
+        if let Some(set_matches) = matches.subcommand_matches("set") {
+            self.set(set_matches.values_of_lossy("servers")).await
+        } else if let Some(_matches) = matches.subcommand_matches("get") {
+            self.get().await
+        } else {
+            unreachable!("No custom-dns command given");
+        }
+    }
+}
+
+impl CustomDns {
+    async fn set(&self, servers: Option<Vec<String>>) -> Result<()> {
+        let mut rpc = new_rpc_client().await?;
+        rpc.set_custom_dns(types::CustomDns {
+            addresses: servers.unwrap_or_default(),
+        })
+        .await?;
+        println!("Updated custom DNS settings");
+        Ok(())
+    }
+
+    async fn get(&self) -> Result<()> {
+        let mut rpc = new_rpc_client().await?;
+        let custom_dns = rpc
+            .get_settings(())
+            .await?
+            .into_inner()
+            .tunnel_options
+            .unwrap()
+            .generic
+            .unwrap()
+            .custom_dns;
+        match custom_dns {
+            None => println!("No DNS servers are configured"),
+            Some(types::CustomDns { addresses }) => {
+                println!("Custom DNS servers:");
+                for server in &addresses {
+                    println!("\t{}", server);
+                }
+            }
+        }
+        Ok(())
+    }
+}
