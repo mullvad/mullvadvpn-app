67f0d2927ecdd9eb536540089363877638d11119
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-10-26 20:56:36 +0000

    Include `DnsOptions` in `TunnelOptions` class

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/TunnelOptions.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/TunnelOptions.kt
@@ -2,4 +2,4 @@ package net.mullvad.mullvadvpn.model
 
 import net.mullvad.talpid.net.wireguard.TunnelOptions as WireguardTunnelOptions
 
-data class TunnelOptions(val wireguard: WireguardTunnelOptions)
+data class TunnelOptions(val wireguard: WireguardTunnelOptions, val dnsOptions: DnsOptions)
