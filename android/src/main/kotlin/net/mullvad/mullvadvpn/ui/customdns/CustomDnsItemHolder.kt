0d52e029b1e481dac99d01ea1ab7a00ac8b34a00
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-10-30 17:25:43 +0000

    Add custom DNS footer message to the list

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsItemHolder.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsItemHolder.kt
@@ -3,4 +3,4 @@ package net.mullvad.mullvadvpn.ui.customdns
 import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.View
 
-class CustomDnsItemHolder(view: View) : ViewHolder(view)
+abstract class CustomDnsItemHolder(view: View) : ViewHolder(view)
