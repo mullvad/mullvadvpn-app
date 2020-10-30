9f16e4afbab14d6665d21bfa45ebd5f39a4030c8
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-10-30 17:25:43 +0000

    Add custom DNS footer message to the list

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsAdapter.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsAdapter.kt
@@ -6,13 +6,13 @@ import android.view.ViewGroup
 import net.mullvad.mullvadvpn.R
 
 class CustomDnsAdapter : Adapter<CustomDnsItemHolder>() {
-    override fun getItemCount() = 0
+    override fun getItemCount() = 1
 
     override fun onCreateViewHolder(parentView: ViewGroup, type: Int): CustomDnsItemHolder {
         val inflater = LayoutInflater.from(parentView.context)
-        val view = inflater.inflate(R.layout.app_list_item, parentView, false)
+        val view = inflater.inflate(R.layout.custom_dns_footer, parentView, false)
 
-        return CustomDnsItemHolder(view)
+        return CustomDnsFooterHolder(view)
     }
 
     override fun onBindViewHolder(holder: CustomDnsItemHolder, position: Int) {}
---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsFooterHolder.kt
@@ -0,0 +1,5 @@
+package net.mullvad.mullvadvpn.ui.customdns
+
+import android.view.View
+
+class CustomDnsFooterHolder(view: View) : CustomDnsItemHolder(view)
