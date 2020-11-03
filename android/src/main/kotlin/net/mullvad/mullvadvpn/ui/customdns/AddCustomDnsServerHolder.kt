a60116a2b26888cc9e3e51e520205ab6f3215efc
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-11-03 20:49:19 +0000

    Show "Add a server" row when enabled

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/AddCustomDnsServerHolder.kt
@@ -0,0 +1,5 @@
+package net.mullvad.mullvadvpn.ui.customdns
+
+import android.view.View
+
+class AddCustomDnsServerHolder(view: View) : CustomDnsItemHolder(view)
--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsAdapter.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsAdapter.kt
@@ -3,14 +3,24 @@ package net.mullvad.mullvadvpn.ui.customdns
 import android.support.v7.widget.RecyclerView.Adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import kotlin.properties.Delegates.observable
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.service.CustomDns
 import net.mullvad.mullvadvpn.util.JobTracker
 
 class CustomDnsAdapter(val customDns: CustomDns) : Adapter<CustomDnsItemHolder>() {
+    private enum class ViewTypes {
+        ADD_SERVER,
+        FOOTER,
+    }
+
     private val jobTracker = JobTracker()
 
-    private var enabled = false
+    private var enabled by observable(false) { _, oldValue, newValue ->
+        if (oldValue != newValue) {
+            notifyDataSetChanged()
+        }
+    }
 
     init {
         customDns.onEnabledChanged.subscribe(this) { value ->
@@ -20,13 +30,33 @@ class CustomDnsAdapter(val customDns: CustomDns) : Adapter<CustomDnsItemHolder>(
         }
     }
 
-    override fun getItemCount() = 1
+    override fun getItemCount() = if (enabled) { 2 } else { 1 }
+
+    override fun getItemViewType(position: Int): Int {
+        if (enabled) {
+            if (position == 0) {
+                return ViewTypes.ADD_SERVER.ordinal
+            } else {
+                return ViewTypes.FOOTER.ordinal
+            }
+        } else {
+            return ViewTypes.FOOTER.ordinal
+        }
+    }
 
     override fun onCreateViewHolder(parentView: ViewGroup, type: Int): CustomDnsItemHolder {
         val inflater = LayoutInflater.from(parentView.context)
-        val view = inflater.inflate(R.layout.custom_dns_footer, parentView, false)
 
-        return CustomDnsFooterHolder(view)
+        when (ViewTypes.values()[type]) {
+            ViewTypes.FOOTER -> {
+                val view = inflater.inflate(R.layout.custom_dns_footer, parentView, false)
+                return CustomDnsFooterHolder(view)
+            }
+            ViewTypes.ADD_SERVER -> {
+                val view = inflater.inflate(R.layout.add_custom_dns_server, parentView, false)
+                return AddCustomDnsServerHolder(view)
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: CustomDnsItemHolder, position: Int) {}
