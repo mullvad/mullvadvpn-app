922817fd8c4e4be5e58c9848f5c6ed8c62f05232
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-11-05 17:45:27 +0000

    Show only IP address, discarding the host name

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsServerHolder.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsServerHolder.kt
@@ -5,11 +5,12 @@ import android.widget.TextView
 import java.net.InetAddress
 import kotlin.properties.Delegates.observable
 import net.mullvad.mullvadvpn.R
+import net.mullvad.talpid.util.addressString
 
 class CustomDnsServerHolder(view: View) : CustomDnsItemHolder(view) {
     private val label: TextView = view.findViewById(R.id.label)
 
     var serverAddress by observable<InetAddress?>(null) { _, _, address ->
-        label.text = address.toString()
+        label.text = address?.addressString() ?: ""
     }
 }
