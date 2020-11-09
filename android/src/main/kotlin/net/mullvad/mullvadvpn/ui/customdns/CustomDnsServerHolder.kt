e0fb1fc375d5448a1fb3214244807706d4759041
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-11-09 13:46:06 +0000

    Implement removal of DNS addresses

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsServerHolder.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsServerHolder.kt
@@ -7,10 +7,18 @@ import kotlin.properties.Delegates.observable
 import net.mullvad.mullvadvpn.R
 import net.mullvad.talpid.util.addressString
 
-class CustomDnsServerHolder(view: View) : CustomDnsItemHolder(view) {
+class CustomDnsServerHolder(view: View, adapter: CustomDnsAdapter) : CustomDnsItemHolder(view) {
     private val label: TextView = view.findViewById(R.id.label)
 
     var serverAddress by observable<InetAddress?>(null) { _, _, address ->
         label.text = address?.addressString() ?: ""
+    }
+
+    init {
+        view.findViewById<View>(R.id.remove).setOnClickListener {
+            serverAddress?.let { address ->
+                adapter.removeDnsServer(address)
+            }
+        }
     }
 }
