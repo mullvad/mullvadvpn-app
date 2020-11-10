a8c460410f5bef243bbad45ba041f2f23c494993
David Lönnhager <david.l@mullvad.net>
2020-11-10 13:43:10 +0100

    Bundle API address cache with the app

--- gui/tasks/distribution.js
+++ gui/tasks/distribution.js
@@ -21,6 +21,7 @@ const config = {
   extraResources: [
     { from: distAssets('ca.crt'), to: '.' },
     { from: distAssets('relays.json'), to: '.' },
+    { from: distAssets('api-ip-address.txt'), to: '.' },
     { from: root('CHANGELOG.md'), to: '.' },
   ],
 
