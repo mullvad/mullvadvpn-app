221ed6b52a6df797b64deb3ceed4113f43575df0
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-11-12 13:38:47 +0000

    Make input text red when adding invalid address

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/EditCustomDnsServerHolder.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/EditCustomDnsServerHolder.kt
@@ -1,5 +1,7 @@
 package net.mullvad.mullvadvpn.ui.customdns
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.View.OnFocusChangeListener
 import android.widget.EditText
@@ -9,6 +11,10 @@ import net.mullvad.mullvadvpn.R
 import net.mullvad.talpid.util.addressString
 
 class EditCustomDnsServerHolder(view: View, adapter: CustomDnsAdapter) : CustomDnsItemHolder(view) {
+    private val context = view.context
+    private val errorColor = context.getColor(R.color.red)
+    private val normalColor = context.getColor(R.color.blue)
+
     private val input: EditText = view.findViewById<EditText>(R.id.input).apply {
         onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
             if (!hasFocus) {
@@ -17,6 +23,17 @@ class EditCustomDnsServerHolder(view: View, adapter: CustomDnsAdapter) : CustomD
                 }
             }
         }
+    }
+
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(text: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun afterTextChanged(text: Editable) {
+            input.setTextColor(normalColor)
+            input.removeTextChangedListener(this)
+        }
+
+        override fun onTextChanged(text: CharSequence, start: Int, before: Int, count: Int) {}
     }
 
     var serverAddress by observable<InetAddress?>(null) { _, _, address ->
@@ -34,7 +51,12 @@ class EditCustomDnsServerHolder(view: View, adapter: CustomDnsAdapter) : CustomD
 
     init {
         view.findViewById<View>(R.id.save).setOnClickListener {
-            adapter.saveDnsServer(input.text.toString())
+            adapter.saveDnsServer(input.text.toString()) {
+                input.apply {
+                    setTextColor(errorColor)
+                    addTextChangedListener(watcher)
+                }
+            }
         }
     }
 }
