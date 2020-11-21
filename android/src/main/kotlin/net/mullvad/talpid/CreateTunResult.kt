9af7832fdaf1c763db3e308f29281e81eba0c7b0
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-11-21 20:54:26 +0000

    Add `CreateTunResult.InvalidDnsServers` variant

--- android/src/main/kotlin/net/mullvad/talpid/CreateTunResult.kt
+++ android/src/main/kotlin/net/mullvad/talpid/CreateTunResult.kt
@@ -1,10 +1,20 @@
 package net.mullvad.talpid
 
+import java.net.InetAddress
+
 sealed class CreateTunResult {
     open val isOpen
         get() = false
 
     class Success(val tunFd: Int) : CreateTunResult() {
+        override val isOpen
+            get() = true
+    }
+
+    class InvalidDnsServers(
+        val addresses: ArrayList<InetAddress>,
+        val tunFd: Int
+    ) : CreateTunResult() {
         override val isOpen
             get() = true
     }
