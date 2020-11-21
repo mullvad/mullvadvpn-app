2b70f64462bc39487228de2713ca24f812e2dff5
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-11-21 19:58:48 +0000

    Add `isOpen` property to `CreateTunResult`

--- android/src/main/kotlin/net/mullvad/talpid/CreateTunResult.kt
+++ android/src/main/kotlin/net/mullvad/talpid/CreateTunResult.kt
@@ -1,7 +1,14 @@
 package net.mullvad.talpid
 
 sealed class CreateTunResult {
-    class Success(val tunFd: Int) : CreateTunResult()
+    open val isOpen
+        get() = false
+
+    class Success(val tunFd: Int) : CreateTunResult() {
+        override val isOpen
+            get() = true
+    }
+
     class PermissionDenied : CreateTunResult()
     class TunnelDeviceError : CreateTunResult()
 }
