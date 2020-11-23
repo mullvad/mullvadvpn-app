134a91ebd75fd557337dbe7c320d5984cf7a9644
Oskar Nyberg <oskar@mullvad.net>
2020-11-23 17:08:15 +0100

    Remove assignment of app.allowRendererProcessReuse

--- gui/test/setup/main.js
+++ gui/test/setup/main.js
@@ -1,9 +1,4 @@
-const { app } = require('electron');
 const log = require('electron-log');
 
 log.transports.console.level = false;
 log.transports.file.level = false;
-
-// The default value has previously been false but will be changed to true in Electron 9. The
-// false value has been deprecated in Electron 8. This can be removed when Electron 9 is used.
-app.allowRendererProcessReuse = true;
