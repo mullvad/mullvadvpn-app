0757bc984980568a00f91ed84698aebb4152c629
David Lönnhager <david.l@mullvad.net>
2020-11-25 12:27:21 +0100

    Rename TAP adapter error cause on Android

--- android/src/main/kotlin/net/mullvad/talpid/tunnel/ErrorStateCause.kt
+++ android/src/main/kotlin/net/mullvad/talpid/tunnel/ErrorStateCause.kt
@@ -8,6 +8,6 @@ sealed class ErrorStateCause {
     class StartTunnelError : ErrorStateCause()
     class TunnelParameterError(val error: ParameterGenerationError) : ErrorStateCause()
     class IsOffline : ErrorStateCause()
-    class TapAdapterProblem : ErrorStateCause()
+    class VirtualAdapterProblem : ErrorStateCause()
     class VpnPermissionDenied : ErrorStateCause()
 }
