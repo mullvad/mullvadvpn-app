740097bf97d069895fdb405b361cfa4702545171
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-01 21:01:11 +0000

    Make `GeoIpLocation` parcelable

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/GeoIpLocation.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/GeoIpLocation.kt
@@ -1,11 +1,14 @@
 package net.mullvad.mullvadvpn.model
 
+import android.os.Parcelable
 import java.net.InetAddress
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class GeoIpLocation(
     val ipv4: InetAddress?,
     val ipv6: InetAddress?,
     val country: String,
     val city: String?,
     val hostname: String?
-)
+) : Parcelable
