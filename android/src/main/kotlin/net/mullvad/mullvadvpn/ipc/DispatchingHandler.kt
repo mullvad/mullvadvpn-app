5ec5bf9072f3a55b09031d7a8e0350ab59c8dc52
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-04 21:56:39 +0000

    Create `DispatchingHandler` helper class

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ipc/DispatchingHandler.kt
@@ -0,0 +1,40 @@
+package net.mullvad.mullvadvpn.ipc
+
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.withLock
+import kotlin.reflect.KClass
+
+class DispatchingHandler<T : Any>(
+    looper: Looper,
+    private val extractor: (Message) -> T
+) : Handler(looper) {
+    private val handlers = HashMap<KClass<out T>, (T) -> Unit>()
+    private val lock = ReentrantReadWriteLock()
+
+    fun <V : T> registerHandler(variant: KClass<V>, handler: (V) -> Unit) {
+        lock.writeLock().withLock {
+            handlers.put(variant) { instance ->
+                @Suppress("UNCHECKED_CAST")
+                handler(instance as V)
+            }
+        }
+    }
+
+    override fun handleMessage(message: Message) {
+        lock.readLock().withLock {
+            val instance = extractor(message)
+            val handler = handlers.get(instance::class)
+
+            handler?.invoke(instance)
+        }
+    }
+
+    fun onDestroy() {
+        lock.writeLock().withLock {
+            handlers.clear()
+        }
+    }
+}
