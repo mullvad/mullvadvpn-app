0eb6e3b557fe151f5f6689d28114a49d895ab3f8
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-04 21:58:40 +0000

    Update `LocationInfoCache` to listen for locations

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/LocationInfoCache.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/LocationInfoCache.kt
@@ -1,7 +1,21 @@
 package net.mullvad.mullvadvpn.ui.serviceconnection
 
-class LocationInfoCache(val serviceCache: net.mullvad.mullvadvpn.service.LocationInfoCache) {
-    var onNewLocation
-        get() = serviceCache.onNewLocation
-        set(value) { serviceCache.onNewLocation = value }
+import kotlin.properties.Delegates.observable
+import net.mullvad.mullvadvpn.model.GeoIpLocation
+import net.mullvad.mullvadvpn.service.Event
+
+class LocationInfoCache(val eventDispatcher: EventDispatcher) {
+    private var location: GeoIpLocation? by observable(null) { _, _, newLocation ->
+        onNewLocation?.invoke(newLocation)
+    }
+
+    var onNewLocation by observable<((GeoIpLocation?) -> Unit)?>(null) { _, _, callback ->
+        callback?.invoke(location)
+    }
+
+    init {
+        eventDispatcher.registerHandler(Event.Type.NewLocation) { event: Event.NewLocation ->
+            location = event.location
+        }
+    }
 }
