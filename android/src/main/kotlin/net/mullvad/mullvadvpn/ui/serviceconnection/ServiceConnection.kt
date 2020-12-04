2e99a79e6bf2d3e6d8e1b86495fd6320b3af5988
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-04 21:58:18 +0000

    Register for service events in `ServiceConnection`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/ServiceConnection.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/ServiceConnection.kt
@@ -1,11 +1,22 @@
 package net.mullvad.mullvadvpn.ui.serviceconnection
 
+import android.os.Looper
+import android.os.Messenger
+import android.os.RemoteException
+import android.util.Log
 import net.mullvad.mullvadvpn.dataproxy.AppVersionInfoCache
 import net.mullvad.mullvadvpn.dataproxy.RelayListListener
+import net.mullvad.mullvadvpn.ipc.DispatchingHandler
+import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.service.ServiceInstance
 import net.mullvad.mullvadvpn.ui.MainActivity
 
 class ServiceConnection(private val service: ServiceInstance, val mainActivity: MainActivity) {
+    val dispatcher = DispatchingHandler(Looper.getMainLooper()) { message ->
+        Event.fromMessage(message)
+    }
+
     val daemon = service.daemon
     val accountCache = service.accountCache
     val connectionProxy = service.connectionProxy
@@ -21,11 +32,25 @@ class ServiceConnection(private val service: ServiceInstance, val mainActivity: 
     init {
         appVersionInfoCache.onCreate()
         connectionProxy.mainActivity = mainActivity
+        registerListener()
     }
 
     fun onDestroy() {
+        dispatcher.onDestroy()
+
         appVersionInfoCache.onDestroy()
         relayListListener.onDestroy()
         connectionProxy.mainActivity = null
+    }
+
+    private fun registerListener() {
+        val listener = Messenger(dispatcher)
+        val request = Request.RegisterListener(listener)
+
+        try {
+            service.messenger.send(request.message)
+        } catch (exception: RemoteException) {
+            Log.e("mullvad", "Failed to register listener for service events", exception)
+        }
     }
 }
