3390ead9a8fb5b84cf489c1f1aad65566edcaf59
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-10 16:29:23 +0000

    Create a `match_str!` helper macro
    Allows matching a string to different prefix and suffix combinations.

--- android/translations-converter/src/gettext.rs
+++ android/translations-converter/src/gettext.rs
@@ -46,6 +46,23 @@ pub enum MsgValue {
     },
 }
 
+/// A helper macro to match a string to various prefix and suffix combinations.
+macro_rules! match_str {
+    (
+        ( $string:expr )
+        $( [$start:expr, $middle:ident, $end:expr] => $body:tt )*
+        _ => $else:expr $(,)*
+    ) => {
+        $(
+            if let Some($middle) = parse_line($string, $start, $end) {
+                $body
+            } else
+        )* {
+            $else
+        }
+    };
+}
+
 impl Translation {
     /// Load message entries from a gettext translation file.
     ///
