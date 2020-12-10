a01974f9065ab5d5d1d6ce67553745ab89df6f4c
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-10 16:30:00 +0000

    Refactor to use the new `match_str!` macro

--- android/translations-converter/src/gettext.rs
+++ android/translations-converter/src/gettext.rs
@@ -85,61 +85,71 @@ impl Translation {
             let line = line.expect("Failed to read from gettext file");
             let line = line.trim();
 
-            if let Some(msg_id) = parse_line(line, "msgid \"", "\"") {
-                current_id = Some(normalize(msg_id));
-            } else if let Some(translation) = parse_line(line, "msgstr \"", "\"") {
-                if let Some(id) = current_id.take() {
-                    let value = MsgValue::from(normalize(translation));
-
-                    parsing_header = id.is_empty() && translation.is_empty();
-
-                    entries.push(MsgEntry { id, value });
+            match_str! { (line)
+                ["msgid \"", msg_id, "\""] => {
+                    current_id = Some(normalize(msg_id));
                 }
+                ["msgstr \"", translation, "\""] => {
+                    if let Some(id) = current_id.take() {
+                        let value = MsgValue::from(normalize(translation));
 
-                current_id = None;
-                current_plural_id = None;
-            } else if let Some(plural_id) = parse_line(line, "msgid_plural \"", "\"") {
-                current_plural_id = Some(normalize(plural_id));
-                parsing_header = false;
-            } else if let Some(plural_translation) = parse_line(line, "msgstr[", "\"") {
-                let variant_id_end = plural_translation
-                    .chars()
-                    .position(|character| character == ']')
-                    .expect("Invalid plural msgstr");
-                let variant_id: usize = plural_translation[..variant_id_end]
-                    .parse()
-                    .expect("Invalid variant index");
-                let variant_msg = parse_line(&plural_translation[variant_id_end..], "] \"", "")
-                    .expect("Invalid plural msgstr");
+                        parsing_header = id.is_empty() && translation.is_empty();
 
-                variants.insert(variant_id, normalize(variant_msg));
-                parsing_header = false;
-            } else if let Some(header) = parse_line(line, "\"", "\\n\"") {
-                if parsing_header {
-                    if let Some(plural_formula) = parse_line(header, "Plural-Forms: ", ";") {
-                        plural_form = Some(PluralForm::from_formula(plural_formula));
+                        entries.push(MsgEntry { id, value });
+                    }
+
+                    current_id = None;
+                    current_plural_id = None;
+                }
+                ["msgid_plural \"", plural_id, "\""] => {
+                    current_plural_id = Some(normalize(plural_id));
+                    parsing_header = false;
+                }
+                ["msgstr[", plural_translation, "\""] => {
+                    let variant_id_end = plural_translation
+                        .chars()
+                        .position(|character| character == ']')
+                        .expect("Invalid plural msgstr");
+                    let variant_id: usize = plural_translation[..variant_id_end]
+                        .parse()
+                        .expect("Invalid variant index");
+                    let variant_msg = parse_line(&plural_translation[variant_id_end..], "] \"", "")
+                        .expect("Invalid plural msgstr");
+
+                    variants.insert(variant_id, normalize(variant_msg));
+                    parsing_header = false;
+                }
+                ["\"", header, "\\n\""] => {
+                    if parsing_header {
+                        if let Some(plural_formula) = parse_line(header, "Plural-Forms: ", ";") {
+                            plural_form = Some(PluralForm::from_formula(plural_formula));
+                        }
                     }
                 }
-            } else {
-                if let Some(plural_id) = current_plural_id.take() {
-                    let id = current_id.take().expect("Missing msgid for plural message");
-                    let values = mem::replace(&mut variants, BTreeMap::new())
-                        .into_iter()
-                        .enumerate()
-                        .inspect(|(index, (variant_id, _))| {
-                            assert_eq!(index, variant_id, "Unexpected variant ID for plural msgstr")
-                        })
-                        .map(|(_, (_, value))| value)
-                        .collect();
-                    let value = MsgValue::Plural { plural_id, values };
+                _ => {
+                    if let Some(plural_id) = current_plural_id.take() {
+                        let id = current_id.take().expect("Missing msgid for plural message");
+                        let values = mem::replace(&mut variants, BTreeMap::new())
+                            .into_iter()
+                            .enumerate()
+                            .inspect(|(index, (variant_id, _))| {
+                                assert_eq!(
+                                    index, variant_id,
+                                    "Unexpected variant ID for plural msgstr"
+                                )
+                            })
+                            .map(|(_, (_, value))| value)
+                            .collect();
+                        let value = MsgValue::Plural { plural_id, values };
 
-                    entries.push(MsgEntry { id, value });
+                        entries.push(MsgEntry { id, value });
+                    }
+
+                    current_id = None;
+                    current_plural_id = None;
+                    variants.clear();
+                    parsing_header = false;
                 }
-
-                current_id = None;
-                current_plural_id = None;
-                variants.clear();
-                parsing_header = false;
             }
         }
 
