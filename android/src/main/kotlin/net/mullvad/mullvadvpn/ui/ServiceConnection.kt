589e7cafbd3a1fdc4b1b4f79768c7ca390d98ecd
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-10 23:05:20 +0000

    Use UI side `SettingsListener`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceConnection.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceConnection.kt
@@ -9,6 +9,7 @@ import net.mullvad.mullvadvpn.dataproxy.RelayListListener
 import net.mullvad.mullvadvpn.service.Request
 import net.mullvad.mullvadvpn.service.ServiceInstance
 import net.mullvad.mullvadvpn.ui.serviceconnection.EventDispatcher
+import net.mullvad.mullvadvpn.ui.serviceconnection.SettingsListener
 
 class ServiceConnection(private val service: ServiceInstance, val mainActivity: MainActivity) {
     val dispatcher = EventDispatcher(Looper.getMainLooper())
@@ -19,7 +20,7 @@ class ServiceConnection(private val service: ServiceInstance, val mainActivity: 
     val customDns = service.customDns
     val keyStatusListener = service.keyStatusListener
     val locationInfoCache = service.locationInfoCache
-    val settingsListener = service.settingsListener
+    val settingsListener = SettingsListener(dispatcher)
     val splitTunneling = service.splitTunneling
 
     val appVersionInfoCache = AppVersionInfoCache(mainActivity, daemon, settingsListener)
