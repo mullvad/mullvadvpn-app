d195702bd1a400739e9130999461369a6f54edd5
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-11 19:55:29 +0000

    Use an intermittent daemon in `KeyStatusListener`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceInstance.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceInstance.kt
@@ -13,7 +13,7 @@ class ServiceInstance(
     val splitTunneling: SplitTunneling
 ) {
     val accountCache = AccountCache(daemon, settingsListener)
-    val keyStatusListener = KeyStatusListener(daemon)
+    val keyStatusListener = KeyStatusListener(intermittentDaemon)
 
     fun onDestroy() {
         accountCache.onDestroy()
