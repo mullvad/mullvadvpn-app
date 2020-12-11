efc1aa81dc2284e5dbc341ef805e7c21f5d77c06
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-11 22:03:40 +0000

    Make `KeygenEvent` parcelable

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/KeygenEvent.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/KeygenEvent.kt
@@ -1,6 +1,10 @@
 package net.mullvad.mullvadvpn.model
 
-sealed class KeygenEvent {
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+sealed class KeygenEvent : Parcelable {
+    @Parcelize
     class NewKey(
         val publicKey: PublicKey,
         val verified: Boolean?,
@@ -9,7 +13,10 @@ sealed class KeygenEvent {
         constructor(publicKey: PublicKey) : this (publicKey, null, null)
     }
 
+    @Parcelize
     object TooManyKeys : KeygenEvent()
+
+    @Parcelize
     object GenerationFailure : KeygenEvent()
 
     fun failure(): KeygenFailure? {
@@ -21,7 +28,8 @@ sealed class KeygenEvent {
     }
 }
 
-enum class KeygenFailure {
+@Parcelize
+enum class KeygenFailure : Parcelable {
     TooManyKeys,
     GenerationFailure,
 }
--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/PublicKey.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/PublicKey.kt
@@ -1,3 +1,7 @@
 package net.mullvad.mullvadvpn.model
 
-data class PublicKey(val key: ByteArray, val dateCreated: String)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PublicKey(val key: ByteArray, val dateCreated: String) : Parcelable
