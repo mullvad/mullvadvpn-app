991f9fbdbb41d29668eb537fcac97722c931764c
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-14 18:24:48 +0000

    Create UI-side `KeyStatusListener` class

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/KeyStatusListener.kt
@@ -0,0 +1,33 @@
+package net.mullvad.mullvadvpn.ui.serviceconnection
+
+import android.os.Messenger
+import net.mullvad.mullvadvpn.ipc.DispatchingHandler
+import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.Request
+import net.mullvad.mullvadvpn.model.KeygenEvent
+import net.mullvad.talpid.util.EventNotifier
+
+class KeyStatusListener(val connection: Messenger, val eventDispatcher: DispatchingHandler<Event>) {
+    val onKeyStatusChange = EventNotifier<KeygenEvent?>(null)
+
+    var keyStatus by onKeyStatusChange.notifiable()
+        private set
+
+    init {
+        eventDispatcher.registerHandler(Event.WireGuardKeyStatus::class) { event ->
+            keyStatus = event.keyStatus
+        }
+    }
+
+    fun generateKey() {
+        connection.send(Request.WireGuardGenerateKey.message)
+    }
+
+    fun verifyKey() {
+        connection.send(Request.WireGuardVerifyKey.message)
+    }
+
+    fun onDestroy() {
+        onKeyStatusChange.unsubscribeAll()
+    }
+}
