1efd3c9de3e9dc675644f63ff9d649f18e318b3e
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-17 13:51:26 +0000

    Allow registering event listener messengers

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ipc/Request.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ipc/Request.kt
@@ -1,12 +1,17 @@
 package net.mullvad.mullvadvpn.ipc
 
 import android.os.Message as RawMessage
+import android.os.Messenger
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 // Requests that the service can handle
 sealed class Request : Message(), Parcelable {
     protected override val messageId = 2
     protected override val messageKey = MESSAGE_KEY
+
+    @Parcelize
+    class RegisterListener(val listener: Messenger) : Request(), Parcelable
 
     companion object {
         private const val MESSAGE_KEY = "request"
