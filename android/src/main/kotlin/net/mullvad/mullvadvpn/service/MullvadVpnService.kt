e7242208e5540767d2a09189c6907b333ed85fae
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-17 18:44:33 +0000

    Remove `AccountCache` from `ServiceInstance`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
@@ -58,7 +58,7 @@ class MullvadVpnService : TalpidVpnService() {
             oldInstance?.onDestroy()
 
             accountExpiryNotification = newInstance?.let { instance ->
-                AccountExpiryNotification(this, instance.daemon, instance.accountCache)
+                AccountExpiryNotification(this, instance.daemon, endpoint.accountCache)
             }
 
             serviceNotifier.notify(newInstance)
@@ -256,7 +256,6 @@ class MullvadVpnService : TalpidVpnService() {
                 endpoint.messenger,
                 daemon,
                 daemonInstance.intermittentDaemon,
-                endpoint.accountCache,
                 connectionProxy,
                 customDns,
                 splitTunneling
--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceInstance.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceInstance.kt
@@ -1,14 +1,12 @@
 package net.mullvad.mullvadvpn.service
 
 import android.os.Messenger
-import net.mullvad.mullvadvpn.service.endpoint.AccountCache
 import net.mullvad.mullvadvpn.util.Intermittent
 
 class ServiceInstance(
     val messenger: Messenger,
     val daemon: MullvadDaemon,
     val intermittentDaemon: Intermittent<MullvadDaemon>,
-    val accountCache: AccountCache,
     val connectionProxy: ConnectionProxy,
     val customDns: CustomDns,
     val splitTunneling: SplitTunneling
