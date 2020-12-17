f64612f57220ce8bc4ca82632eb1981444c3fc30
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-17 13:51:26 +0000

    Allow registering event listener messengers

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
@@ -6,11 +6,19 @@ import net.mullvad.mullvadvpn.ipc.DispatchingHandler
 import net.mullvad.mullvadvpn.ipc.Request
 
 class ServiceEndpoint(looper: Looper) {
+    private val listeners = mutableSetOf<Messenger>()
+
     internal val dispatcher = DispatchingHandler(looper) { message ->
         Request.fromMessage(message)
     }
 
     val messenger = Messenger(dispatcher)
+
+    init {
+        dispatcher.registerHandler(Request.RegisterListener::class) { request ->
+            listeners.add(request.listener)
+        }
+    }
 
     fun onDestroy() {
         dispatcher.onDestroy()
