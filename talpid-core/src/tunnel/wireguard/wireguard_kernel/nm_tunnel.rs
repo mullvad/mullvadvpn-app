65e642e56a2b8bae37e64f5602b202e483c58bdc
Emīls <emils@mullvad.net>
2020-12-18 12:25:24 +0000

    Rely on talpid-dbus in talpid-core

--- talpid-core/src/tunnel/wireguard/wireguard_kernel/nm_tunnel.rs
+++ talpid-core/src/tunnel/wireguard/wireguard_kernel/nm_tunnel.rs
@@ -2,8 +2,13 @@ use super::{
     super::stats::{Error as StatsError, Stats},
     Config, Error as WgKernelError, Tunnel, TunnelError, MULLVAD_INTERFACE_NAME,
 };
-use crate::linux::network_manager::{
-    Error as NetworkManagerError, NetworkManager, WireguardTunnel,
+use std::collections::HashMap;
+use talpid_dbus::{
+    dbus,
+    network_manager::{
+        DeviceConfig, Error as NetworkManagerError, NetworkManager, Variant, VariantMap,
+        WireguardTunnel,
+    },
 };
 use talpid_types::ErrorExt;
 
@@ -31,8 +36,9 @@ impl NetworkManagerTunnel {
         let network_manager = NetworkManager::new()
             .map_err(Error::NetworkManager)
             .map_err(WgKernelError::NetworkManager)?;
+        let config_map = convert_config_to_dbus(config);
         let tunnel = network_manager
-            .create_wg_tunnel(config)
+            .create_wg_tunnel(&config_map)
             .map_err(|err| WgKernelError::NetworkManager(err.into()))?;
 
         network_manager
@@ -99,4 +105,98 @@ impl Tunnel for NetworkManagerTunnel {
             }
         }
     }
+}
+
+fn convert_config_to_dbus(config: &Config) -> DeviceConfig {
+    let mut ipv6_config: VariantMap = HashMap::new();
+    let mut ipv4_config: VariantMap = HashMap::new();
+    let mut wireguard_config: VariantMap = HashMap::new();
+    let mut connection_config: VariantMap = HashMap::new();
+    let mut peer_configs = vec![];
+
+    wireguard_config.insert("mtu".into(), Variant(Box::new(config.mtu as u32)));
+    wireguard_config.insert("fwmark".into(), Variant(Box::new(config.fwmark as u32)));
+    wireguard_config.insert("peer-routes".into(), Variant(Box::new(false)));
+    wireguard_config.insert(
+        "private-key".into(),
+        Variant(Box::new(config.tunnel.private_key.to_base64())),
+    );
+    wireguard_config.insert("private-key-flags".into(), Variant(Box::new(0x0u32)));
+
+    for peer in config.peers.iter() {
+        let mut peer_config: VariantMap = HashMap::new();
+        let allowed_ips = peer
+            .allowed_ips
+            .iter()
+            .map(ToString::to_string)
+            .collect::<Vec<_>>();
+
+
+        peer_config.insert("allowed-ips".into(), Variant(Box::new(allowed_ips)));
+        peer_config.insert(
+            "endpoint".into(),
+            Variant(Box::new(peer.endpoint.to_string())),
+        );
+        peer_config.insert(
+            "public-key".into(),
+            Variant(Box::new(peer.public_key.to_base64())),
+        );
+
+        peer_configs.push(peer_config);
+    }
+    wireguard_config.insert("peers".into(), Variant(Box::new(peer_configs)));
+
+    connection_config.insert("type".into(), Variant(Box::new("wireguard".to_string())));
+    connection_config.insert(
+        "id".into(),
+        Variant(Box::new(MULLVAD_INTERFACE_NAME.to_string())),
+    );
+    connection_config.insert(
+        "interface-name".into(),
+        Variant(Box::new(MULLVAD_INTERFACE_NAME.to_string())),
+    );
+    connection_config.insert("autoconnect".into(), Variant(Box::new(true)));
+
+
+    let ipv4_addrs: Vec<_> = config
+        .tunnel
+        .addresses
+        .iter()
+        .filter(|ip| ip.is_ipv4())
+        .map(NetworkManager::convert_address_to_dbus)
+        .collect();
+
+    let ipv6_addrs: Vec<_> = config
+        .tunnel
+        .addresses
+        .iter()
+        .filter(|ip| ip.is_ipv6())
+        .map(NetworkManager::convert_address_to_dbus)
+        .collect();
+
+    ipv4_config.insert("address-data".into(), Variant(Box::new(ipv4_addrs)));
+    ipv4_config.insert("ignore-auto-routes".into(), Variant(Box::new(true)));
+    ipv4_config.insert("ignore-auto-dns".into(), Variant(Box::new(true)));
+    ipv4_config.insert("may-fail".into(), Variant(Box::new(true)));
+    ipv4_config.insert("method".into(), Variant(Box::new("manual".to_string())));
+    ipv4_config.insert("never-default".into(), Variant(Box::new(true)));
+
+    if !ipv6_addrs.is_empty() {
+        ipv6_config.insert("method".into(), Variant(Box::new("manual".to_string())));
+        ipv6_config.insert("address-data".into(), Variant(Box::new(ipv6_addrs)));
+        ipv6_config.insert("ignore-auto-routes".into(), Variant(Box::new(true)));
+        ipv6_config.insert("ignore-auto-dns".into(), Variant(Box::new(true)));
+        ipv6_config.insert("may-fail".into(), Variant(Box::new(true)));
+    }
+
+
+    let mut settings = HashMap::new();
+    settings.insert("ipv4".into(), ipv4_config);
+    if !ipv6_config.is_empty() {
+        settings.insert("ipv6".into(), ipv6_config);
+    }
+    settings.insert("wireguard".into(), wireguard_config);
+    settings.insert("connection".into(), connection_config);
+
+    settings
 }
