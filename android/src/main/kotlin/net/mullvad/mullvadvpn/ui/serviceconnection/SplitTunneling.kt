b4956ac00e2ab6c3dc47398e7297097b93d4f78d
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-12-18 15:30:19 +0000

    Create UI-side `SplitTunneling` class

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/SplitTunneling.kt
@@ -0,0 +1,42 @@
+package net.mullvad.mullvadvpn.ui.serviceconnection
+
+import android.os.Messenger
+import kotlin.properties.Delegates.observable
+import net.mullvad.mullvadvpn.ipc.DispatchingHandler
+import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.Request
+
+class SplitTunneling(val connection: Messenger, eventDispatcher: DispatchingHandler<Event>) {
+    private var excludedApps: Set<String> = emptySet()
+
+    var enabled by observable(false) { _, wasEnabled, isEnabled ->
+        if (wasEnabled != isEnabled) {
+            connection.send(Request.SetEnableSplitTunneling(isEnabled).message)
+        }
+    }
+
+    init {
+        eventDispatcher.registerHandler(Event.SplitTunnelingUpdate::class) { event ->
+            if (event.excludedApps != null) {
+                enabled = true
+                excludedApps = event.excludedApps.toSet()
+            } else {
+                enabled = false
+            }
+        }
+    }
+
+    fun isAppExcluded(appPackageName: String): Boolean = excludedApps.contains(appPackageName)
+
+    fun excludeApp(appPackageName: String) {
+        connection.send(Request.ExcludeApp(appPackageName).message)
+    }
+
+    fun includeApp(appPackageName: String) {
+        connection.send(Request.IncludeApp(appPackageName).message)
+    }
+
+    fun persist() {
+        connection.send(Request.PersistExcludedApps.message)
+    }
+}
