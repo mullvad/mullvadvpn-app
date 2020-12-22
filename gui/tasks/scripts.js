83eaabacbb2396ffc1f237173baae9635521cf38
Oskar Nyberg <oskar@mullvad.net>
2020-12-22 16:10:36 +0100

    Bundle renderer JavaScript

--- gui/tasks/scripts.js
+++ gui/tasks/scripts.js
@@ -3,6 +3,9 @@ const { src, dest, series } = require('gulp');
 const ts = require('gulp-typescript');
 const inject = require('gulp-inject-string');
 const TscWatchClient = require('tsc-watch/client');
+const browserify = require('browserify');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
 
 function makeWatchCompiler(onFirstSuccess) {
   const compileScripts = function () {
@@ -26,13 +29,22 @@ function compileScripts() {
     .pipe(dest('build'));
 }
 
+function browserifyRenderer() {
+  return browserify({ entries: './build/src/renderer/index.js' })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(dest('./build/src/renderer/'));
+}
+
 function buildProto(callback) {
   exec('bash ./scripts/build-proto.sh', (err) => callback(err));
 }
 
 compileScripts.displayName = 'compile-scripts';
+browserifyRenderer.displayName = 'browserify-renderer';
 buildProto.displayName = 'build-proto';
 
-exports.build = compileScripts;
+exports.build = series(compileScripts, browserifyRenderer);
 exports.buildProto = buildProto;
 exports.makeWatchCompiler = makeWatchCompiler;
