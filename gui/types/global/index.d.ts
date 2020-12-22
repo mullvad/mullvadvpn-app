cdfa833719aae00f56295cbfde9feec45f24d2f1
Oskar Nyberg <oskar@mullvad.net>
2020-12-22 17:59:13 +0100

    Add IpcRendererEventChannel to window

--- gui/types/global/index.d.ts
+++ gui/types/global/index.d.ts
@@ -1,7 +1,9 @@
 import Gettext from 'node-gettext';
+import { IpcRendererEventChannel } from '../../src/shared/ipc-event-channel';
 
 declare global {
   interface Window {
     loadTranslations(locale: string, catalogue: Gettext): void;
+    ipc: typeof IpcRendererEventChannel;
   }
 }
