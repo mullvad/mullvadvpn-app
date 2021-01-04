6ba485222a70d5c067a0b7a575adb12e27b0bc1b
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-04 17:58:22 +0000

    Use an `Intermittent<Boolean>` for VPN permission

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/MainActivity.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/MainActivity.kt
@@ -214,6 +214,6 @@ class MainActivity : FragmentActivity() {
     }
 
     private fun setVpnPermission(allow: Boolean) = GlobalScope.launch(Dispatchers.Default) {
-        serviceConnection?.connectionProxy?.vpnPermission?.complete(allow)
+        serviceConnection?.connectionProxy?.vpnPermission?.update(allow)
     }
 }
