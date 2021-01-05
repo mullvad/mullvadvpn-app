650f27e965b14a9880edce34d3f26af727f583a8
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-05 14:53:31 +0000

    Use `ConnectionProxy` in `TunnelStateUpdater`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
@@ -102,7 +102,6 @@ class MullvadVpnService : TalpidVpnService() {
 
         daemonInstance = DaemonInstance(this)
         keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
-        tunnelStateUpdater = TunnelStateUpdater(this, serviceNotifier)
 
         endpoint = ServiceEndpoint(
             Looper.getMainLooper(),
@@ -110,6 +109,8 @@ class MullvadVpnService : TalpidVpnService() {
             connectivityListener,
             this
         )
+
+        tunnelStateUpdater = TunnelStateUpdater(this, connectionProxy)
 
         notificationManager =
             ForegroundNotificationManager(this, connectionProxy, keyguardManager).apply {
--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/tunnelstate/TunnelStateUpdater.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/tunnelstate/TunnelStateUpdater.kt
@@ -1,29 +1,14 @@
 package net.mullvad.mullvadvpn.service.tunnelstate
 
 import android.content.Context
-import net.mullvad.mullvadvpn.service.ServiceInstance
 import net.mullvad.mullvadvpn.service.endpoint.ConnectionProxy
-import net.mullvad.talpid.util.EventNotifier
 
-class TunnelStateUpdater(context: Context, serviceNotifier: EventNotifier<ServiceInstance?>) {
+class TunnelStateUpdater(context: Context, private val connectionProxy: ConnectionProxy) {
     private val persistence = Persistence(context)
 
-    private var connectionProxy: ConnectionProxy? = null
-    private var stateSubscriptionId: Int? = null
-
     init {
-        serviceNotifier.subscribe(this) { serviceInstance ->
-            onNewServiceInstance(serviceInstance)
-        }
-    }
-
-    private fun onNewServiceInstance(serviceInstance: ServiceInstance?) {
-        connectionProxy?.onStateChange?.unsubscribe(this)
-
-        connectionProxy = serviceInstance?.connectionProxy?.apply {
-            onStateChange.subscribe(this@TunnelStateUpdater) { newState ->
-                persistence.state = newState
-            }
+        connectionProxy.onStateChange.subscribe(this) { newState ->
+            persistence.state = newState
         }
     }
 }
