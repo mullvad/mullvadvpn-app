8f1eed723a1cc6ccb44ea768eee21d435e302dc9
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-05 20:47:56 +0000

    Create initial UI-side `CustomDns` class

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/CustomDns.kt
@@ -0,0 +1,21 @@
+package net.mullvad.mullvadvpn.ui.serviceconnection
+
+import android.os.Messenger
+import java.net.InetAddress
+import net.mullvad.talpid.util.EventNotifier
+
+class CustomDns(val connection: Messenger, settingsListener: SettingsListener) {
+    val onEnabledChanged = EventNotifier(false)
+    val onDnsServersChanged = EventNotifier<List<InetAddress>>(emptyList())
+
+    init {
+        settingsListener.dnsOptionsNotifier.subscribe(this) { maybeDnsOptions ->
+            maybeDnsOptions?.let { dnsOptions ->
+                synchronized(this) {
+                    onEnabledChanged.notifyIfChanged(dnsOptions.custom)
+                    onDnsServersChanged.notifyIfChanged(dnsOptions.addresses)
+                }
+            }
+        }
+    }
+}
