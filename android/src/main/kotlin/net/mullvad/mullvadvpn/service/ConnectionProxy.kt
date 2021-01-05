6736a3e62c65ec01e1ec40a760030f2ed8affa53
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-05 13:43:53 +0000

    Remove UI state from service `ConnectionProxy`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ConnectionProxy.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ConnectionProxy.kt
@@ -6,20 +6,15 @@ import android.net.VpnService
 import kotlin.properties.Delegates.observable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.channels.sendBlocking
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.model.TunnelState
 import net.mullvad.mullvadvpn.ui.MainActivity
 import net.mullvad.mullvadvpn.util.Intermittent
-import net.mullvad.talpid.tunnel.ActionAfterDisconnect
 import net.mullvad.talpid.util.EventNotifier
-
-val ANTICIPATED_STATE_TIMEOUT_MS = 1500L
 
 class ConnectionProxy(val context: Context) {
     private enum class Command {
@@ -30,54 +25,41 @@ class ConnectionProxy(val context: Context) {
 
     private val availableDaemon = Intermittent<MullvadDaemon>()
     private val commandChannel = spawnActor()
+    private val initialState = TunnelState.Disconnected()
 
     var mainActivity: MainActivity? = null
 
-    private var resetAnticipatedStateJob: Job? = null
-
-    private val initialState: TunnelState = TunnelState.Disconnected()
-
     val vpnPermission = Intermittent<Boolean>()
 
-    var onStateChange = EventNotifier(initialState)
-    var onUiStateChange = EventNotifier(initialState)
+    var onStateChange = EventNotifier<TunnelState>(initialState)
 
     var daemon by observable<MullvadDaemon?>(null) { _, oldDaemon, newDaemon ->
         oldDaemon?.onTunnelStateChange = null
-        newDaemon?.onTunnelStateChange = { newState -> handleNewState(newState) }
+        newDaemon?.onTunnelStateChange = { newState -> state = newState }
 
         availableDaemon.spawnUpdate(newDaemon)
     }
 
     var state by onStateChange.notifiable()
         private set
-    var uiState by onUiStateChange.notifiable()
-        private set
 
     private val fetchInitialStateJob = fetchInitialState()
 
     fun connect() {
-        if (anticipateConnectingState()) {
-            commandChannel.sendBlocking(Command.CONNECT)
-        }
+        commandChannel.sendBlocking(Command.CONNECT)
     }
 
     fun reconnect() {
-        if (anticipateReconnectingState()) {
-            commandChannel.sendBlocking(Command.RECONNECT)
-        }
+        commandChannel.sendBlocking(Command.RECONNECT)
     }
 
     fun disconnect() {
-        if (anticipateDisconnectingState()) {
-            commandChannel.sendBlocking(Command.DISCONNECT)
-        }
+        commandChannel.sendBlocking(Command.DISCONNECT)
     }
 
     fun onDestroy() {
         daemon = null
 
-        onUiStateChange.unsubscribeAll()
         onStateChange.unsubscribeAll()
 
         fetchInitialStateJob.cancel()
@@ -104,88 +86,6 @@ class ConnectionProxy(val context: Context) {
         }
     }
 
-    private fun handleNewState(newState: TunnelState) {
-        synchronized(this) {
-            resetAnticipatedStateJob?.cancel()
-            state = newState
-            uiState = newState
-        }
-    }
-
-    private fun anticipateConnectingState(): Boolean {
-        synchronized(this) {
-            val currentState = uiState
-
-            if (currentState is TunnelState.Connecting || currentState is TunnelState.Connected) {
-                return false
-            } else {
-                scheduleToResetAnticipatedState()
-                uiState = TunnelState.Connecting(null, null)
-                return true
-            }
-        }
-    }
-
-    private fun anticipateReconnectingState(): Boolean {
-        synchronized(this) {
-            val currentState = uiState
-
-            val willReconnect = when (currentState) {
-                is TunnelState.Disconnected -> false
-                is TunnelState.Disconnecting -> {
-                    when (currentState.actionAfterDisconnect) {
-                        ActionAfterDisconnect.Nothing -> false
-                        ActionAfterDisconnect.Reconnect -> true
-                        ActionAfterDisconnect.Block -> true
-                    }
-                }
-                is TunnelState.Connecting -> true
-                is TunnelState.Connected -> true
-                is TunnelState.Error -> true
-            }
-
-            if (willReconnect) {
-                scheduleToResetAnticipatedState()
-                uiState = TunnelState.Disconnecting(ActionAfterDisconnect.Reconnect)
-            }
-
-            return willReconnect
-        }
-    }
-
-    private fun anticipateDisconnectingState(): Boolean {
-        synchronized(this) {
-            val currentState = uiState
-
-            if (currentState is TunnelState.Disconnected) {
-                return false
-            } else {
-                scheduleToResetAnticipatedState()
-                uiState = TunnelState.Disconnecting(ActionAfterDisconnect.Nothing)
-                return true
-            }
-        }
-    }
-
-    private fun scheduleToResetAnticipatedState() {
-        resetAnticipatedStateJob?.cancel()
-
-        var currentJob: Job? = null
-
-        val newJob = GlobalScope.launch(Dispatchers.Default) {
-            delay(ANTICIPATED_STATE_TIMEOUT_MS)
-
-            synchronized(this@ConnectionProxy) {
-                if (!currentJob!!.isCancelled) {
-                    uiState = state
-                }
-            }
-        }
-
-        currentJob = newJob
-        resetAnticipatedStateJob = newJob
-    }
-
     private suspend fun requestVpnPermission() {
         val intent = VpnService.prepare(context)
 
@@ -204,8 +104,6 @@ class ConnectionProxy(val context: Context) {
                     addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
                     putExtra(MainActivity.KEY_SHOULD_CONNECT, true)
                 }
-
-                uiState = state
 
                 context.startActivity(activityIntent)
             }
