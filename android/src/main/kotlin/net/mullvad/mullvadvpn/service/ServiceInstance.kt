08328338020c3aecaf8c0c50734e11241093796d
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-05 14:54:39 +0000

    Remove `ConnectionProxy` from `ServiceInstance`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceInstance.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceInstance.kt
@@ -1,14 +1,12 @@
 package net.mullvad.mullvadvpn.service
 
 import android.os.Messenger
-import net.mullvad.mullvadvpn.service.endpoint.ConnectionProxy
 import net.mullvad.mullvadvpn.util.Intermittent
 
 class ServiceInstance(
     val messenger: Messenger,
     val daemon: MullvadDaemon,
     val intermittentDaemon: Intermittent<MullvadDaemon>,
-    val connectionProxy: ConnectionProxy,
     val customDns: CustomDns,
 ) {
     fun onDestroy() {
