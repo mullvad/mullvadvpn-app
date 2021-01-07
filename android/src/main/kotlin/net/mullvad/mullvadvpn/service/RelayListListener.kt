9da585289e4bfe4e3215e2648cbe86cb4000c0aa
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-07 12:21:25 +0000

    Create new service side `RelayListListener`

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/RelayListListener.kt
@@ -0,0 +1,35 @@
+package net.mullvad.mullvadvpn.service
+
+import kotlin.properties.Delegates.observable
+import net.mullvad.mullvadvpn.model.RelayList
+import net.mullvad.talpid.util.EventNotifier
+
+class RelayListListener {
+    val relayListNotifier = EventNotifier<RelayList?>(null)
+
+    var relayList by relayListNotifier.notifiable()
+        private set
+
+    var daemon by observable<MullvadDaemon?>(null) { _, oldDaemon, newDaemon ->
+        oldDaemon?.onRelayListChange = null
+
+        if (newDaemon != null) {
+            setUpListener(newDaemon)
+            fetchInitialRelayList(newDaemon)
+        }
+    }
+
+    private fun setUpListener(daemon: MullvadDaemon) {
+        daemon.onRelayListChange = { relayLocations ->
+            relayList = relayLocations
+        }
+    }
+
+    private fun fetchInitialRelayList(daemon: MullvadDaemon) {
+        synchronized(this) {
+            if (relayList == null) {
+                relayList = daemon.getRelayLocations()
+            }
+        }
+    }
+}
