e977beadd7364c75f0d7bf5d8fe8f7a2144f0ecb
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-07 17:26:12 +0000

    Make `RelayList` parcelable

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelayTunnels.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelayTunnels.kt
@@ -1,5 +1,8 @@
 package net.mullvad.mullvadvpn.model
 
+import android.os.Parcelable
 import java.util.ArrayList
+import kotlinx.parcelize.Parcelize
 
-data class RelayTunnels(val wireguard: ArrayList<WireguardEndpointData>)
+@Parcelize
+data class RelayTunnels(val wireguard: ArrayList<WireguardEndpointData>) : Parcelable
