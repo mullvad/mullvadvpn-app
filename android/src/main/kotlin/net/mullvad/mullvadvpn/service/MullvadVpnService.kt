ad272177c0c1dee30cfb4f0c71accbb9310e3791
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-12 13:34:00 +0000

    Remove requirement for initial settings

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
@@ -77,10 +77,9 @@ class MullvadVpnService : TalpidVpnService() {
 
     private var pendingAction by observable<PendingAction?>(null) { _, _, _ ->
         instance?.let { activeInstance ->
-            handlePendingAction(
-                activeInstance.connectionProxy,
-                activeInstance.settingsListener.settings
-            )
+            activeInstance.settingsListener.settings?.let { currentSettings ->
+                handlePendingAction(activeInstance.connectionProxy, currentSettings)
+            }
         }
     }
 
@@ -230,7 +229,7 @@ class MullvadVpnService : TalpidVpnService() {
     }
 
     private suspend fun setUpInstance(daemon: MullvadDaemon, settings: Settings) {
-        val settingsListener = SettingsListener(settings, daemonInstance.intermittentDaemon)
+        val settingsListener = SettingsListener(daemonInstance.intermittentDaemon)
         val connectionProxy = ConnectionProxy(this, daemon)
         val customDns = CustomDns(daemon, settingsListener)
         val splitTunneling = splitTunneling.await()
