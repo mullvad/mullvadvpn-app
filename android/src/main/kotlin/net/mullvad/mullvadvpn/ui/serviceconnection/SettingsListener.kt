ce811c961ab921116ebc50b8849353fd501ab8b9
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-13 18:36:11 +0000

    Allow setting Auto-Connect through connection

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/SettingsListener.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/SettingsListener.kt
@@ -24,6 +24,10 @@ class SettingsListener(val connection: Messenger, eventDispatcher: EventDispatch
         get() = settingsNotifier.latestEvent?.allowLan ?: false
         set(value) { connection.send(Request.SetAllowLan(value).message) }
 
+    var autoConnect: Boolean
+        get() = settingsNotifier.latestEvent?.autoConnect ?: false
+        set(value) { connection.send(Request.SetAutoConnect(value).message) }
+
     var wireguardMtu: Int?
         get() = settingsNotifier.latestEvent?.tunnelOptions?.wireguard?.mtu
         set(value) { connection.send(Request.SetWireGuardMtu(value).message) }
