6fdaec1b5dd4ece808ab7cd327b7e97990256e09
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-15 21:20:40 +0000

    Create UI side `VoucherRedeemer`

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/VoucherRedeemer.kt
@@ -0,0 +1,30 @@
+package net.mullvad.mullvadvpn.ui.serviceconnection
+
+import android.os.Messenger
+import kotlinx.coroutines.CompletableDeferred
+import net.mullvad.mullvadvpn.model.VoucherSubmissionResult
+import net.mullvad.mullvadvpn.service.Event
+import net.mullvad.mullvadvpn.service.Request
+
+class VoucherRedeemer(val connection: Messenger, eventDispatcher: EventDispatcher) {
+    private val activeSubmissions =
+        mutableMapOf<String, CompletableDeferred<VoucherSubmissionResult>>()
+
+    init {
+        eventDispatcher.registerHandler(
+            Event.Type.VoucherSubmissionResult
+        ) { event: Event.VoucherSubmissionResult ->
+            activeSubmissions.remove(event.voucher)?.complete(event.result)
+        }
+    }
+
+    suspend fun submit(voucher: String): VoucherSubmissionResult {
+        val result = CompletableDeferred<VoucherSubmissionResult>()
+
+        activeSubmissions.put(voucher, result)
+
+        connection.send(Request.SubmitVoucher(voucher).message)
+
+        return result.await()
+    }
+}
