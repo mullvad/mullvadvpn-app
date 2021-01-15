48550f5867c31f7557246053156b43b0af51abc9
Oskar Nyberg <oskar@mullvad.net>
2021-01-15 17:29:09 +0100

    Add IPC and RPC methods and connect to Windows split tunneling functions

--- gui/src/main/daemon-rpc.ts
+++ gui/src/main/daemon-rpc.ts
@@ -463,6 +463,18 @@ export class DaemonRpc {
     return response.toObject();
   }
 
+  public async addSplitTunnelingApplication(path: string): Promise<void> {
+    await this.callString(this.client.addSplitTunnelApp, path);
+  }
+
+  public async removeSplitTunnelingApplication(path: string): Promise<void> {
+    await this.callString(this.client.removeSplitTunnelApp, path);
+  }
+
+  public async setSplitTunnelingState(enabled: boolean): Promise<void> {
+    await this.callBool(this.client.setSplitTunnelState, enabled);
+  }
+
   private subscriptionId(): number {
     const current = this.nextSubscriptionId;
     this.nextSubscriptionId += 1;
