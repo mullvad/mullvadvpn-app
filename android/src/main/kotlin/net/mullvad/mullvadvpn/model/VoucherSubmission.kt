2e28cb410a67ecd0b4e66b72179763b545ff7c7d
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-15 21:17:15 +0000

    Make `VoucherSubmissionResult` parcelable

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/VoucherSubmission.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/VoucherSubmission.kt
@@ -1,3 +1,7 @@
 package net.mullvad.mullvadvpn.model
 
-data class VoucherSubmission(val timeAdded: Long, val newExpiry: String)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class VoucherSubmission(val timeAdded: Long, val newExpiry: String) : Parcelable
