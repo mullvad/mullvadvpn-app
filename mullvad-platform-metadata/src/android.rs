70c84a80d2084434a2a27b66b17f47507d059d65
Oskar Nyberg <oskar@mullvad.net>
2021-01-21 11:23:26 +0100

    Shorten Android platform_metadata::short_version() value

--- mullvad-platform-metadata/src/android.rs
+++ mullvad-platform-metadata/src/android.rs
@@ -4,7 +4,7 @@ mod command;
 use command::command_stdout_lossy;
 
 pub fn version() -> String {
-    let version = get_prop("ro.build.version.release").unwrap_or_else(|| "N/A".to_owned());
+    let version = os_version();
     let api_level = get_prop("ro.build.version.sdk").unwrap_or_else(|| "N/A".to_owned());
 
     let manufacturer =
@@ -18,7 +18,13 @@ pub fn version() -> String {
 }
 
 pub fn short_version() -> String {
-    version()
+    let version = os_version();
+
+    format!("Android {}", version)
+}
+
+fn os_version() -> String {
+    get_prop("ro.build.version.release").unwrap_or_else(|| "N/A".to_owned())
 }
 
 pub fn extra_metadata() -> HashMap<String, String> {
