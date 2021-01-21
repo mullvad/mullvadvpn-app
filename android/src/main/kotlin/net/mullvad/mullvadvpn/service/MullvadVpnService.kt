d145dd00cd4c4c6ab000ea5b9abdcb26a2f86db9
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-21 18:17:29 +0000

    Remove `LocalBinder` class

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
@@ -4,7 +4,6 @@ import android.app.KeyguardManager
 import android.content.Context
 import android.content.Intent
 import android.net.VpnService
-import android.os.Binder
 import android.os.IBinder
 import android.os.Looper
 import android.util.Log
@@ -44,7 +43,6 @@ class MullvadVpnService : TalpidVpnService() {
         Stopped,
     }
 
-    private val binder = LocalBinder()
     private val serviceNotifier = EventNotifier<ServiceInstance?>(null)
 
     private val connectionProxy
@@ -192,15 +190,6 @@ class MullvadVpnService : TalpidVpnService() {
         daemonInstance.onDestroy()
         instance = null
         super.onDestroy()
-    }
-
-    inner class LocalBinder : Binder() {
-        val serviceNotifier
-            get() = this@MullvadVpnService.serviceNotifier
-
-        var isUiVisible
-            get() = this@MullvadVpnService.isUiVisible
-            set(value) { this@MullvadVpnService.isUiVisible = value }
     }
 
     private fun handleDaemonInstance(daemon: MullvadDaemon?) {
