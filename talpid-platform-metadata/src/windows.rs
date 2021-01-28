6f0a7c775fb50bae18c600026d95bb8f74046c23
Emīls Piņķis <emils@mullvad.net>
2021-01-28 21:42:16 +0000

    Change version info on Windows

--- talpid-platform-metadata/src/windows.rs
+++ talpid-platform-metadata/src/windows.rs
@@ -1,54 +1,98 @@
-use std::collections::HashMap;
-
-mod command;
-use command::command_stdout_lossy;
+use std::{
+    ffi::OsString,
+    io, iter,
+    mem::{self, MaybeUninit},
+    os::windows::ffi::OsStrExt,
+    ptr,
+};
+use winapi::um::{
+    libloaderapi::{GetModuleHandleW, GetProcAddress},
+    winnt::RTL_OSVERSIONINFOW,
+};
 
 pub fn version() -> String {
-    let system_info = system_info();
-    let os_name = system_info.get("OS Name");
-    let os_version = system_info.get("OS Version");
-    let version = os_name.map(parse_version).unwrap_or(String::from("N/A"));
-    let full_version = os_version
-        .map(parse_full_version)
-        .unwrap_or(String::from("N/A"));
-    format!("Windows {} ({})", version, full_version)
+    let (major, minor, build) = WindowsVersion::new()
+        .map(|version_info| {
+            (
+                version_info.major_version().to_string(),
+                version_info.minor_version().to_string(),
+                version_info.build_number().to_string(),
+            )
+        })
+        .unwrap_or_else(|_| ("N/A".to_string(), "N/A".to_string(), "N/A".to_string()));
+
+    format!("Windows {}.{} Build {}", major, minor, build)
 }
 
 pub fn short_version() -> String {
-    let system_info = system_info();
-    let os_name = system_info.get("OS Name");
-    let version = os_name.map(parse_version).unwrap_or(String::from("N/A"));
-    format!("Windows {}", version)
-}
-
-fn system_info() -> HashMap<String, String> {
-    let system_info =
-        command_stdout_lossy("systeminfo", &["/FO", "LIST"]).unwrap_or_else(String::new);
-
-    let mut info_map = HashMap::new();
-    system_info.lines().for_each(|line| {
-        let mut split = line.split(":");
-        if let Some(key) = split.next() {
-            if let Some(value) = split.next() {
-                info_map.insert(key.to_owned(), value.to_owned());
-            }
-        }
-    });
-
-    info_map
-}
-
-fn parse_version(os_name: &String) -> String {
-    os_name
-        .trim()
-        .trim_start_matches("Microsoft Windows ")
-        .to_owned()
-}
-
-fn parse_full_version(os_version: &String) -> String {
-    os_version.trim().to_owned()
+    let version_string = WindowsVersion::new()
+        .map(|version| version.major_version().to_string())
+        .unwrap_or("N/A".into());
+    format!("Windows {}", version_string)
 }
 
 pub fn extra_metadata() -> impl Iterator<Item = (String, String)> {
     std::iter::empty()
+}
+
+
+pub struct WindowsVersion {
+    inner: RTL_OSVERSIONINFOW,
+}
+
+impl WindowsVersion {
+    pub fn new() -> Result<WindowsVersion, io::Error> {
+        let module_name: Vec<u16> = OsString::from("ntdll")
+            .as_os_str()
+            .encode_wide()
+            .chain(iter::once(0u16))
+            .collect();
+
+
+        let ntdll = unsafe { GetModuleHandleW(module_name.as_ptr()) };
+        if ntdll == ptr::null_mut() {
+            return Err(io::Error::last_os_error());
+        }
+
+        let function_address =
+            unsafe { GetProcAddress(ntdll, b"RtlGetVersion\0" as *const _ as *const i8) };
+        if function_address == ptr::null_mut() {
+            return Err(io::Error::last_os_error());
+        }
+
+        let rtl_get_version: extern "stdcall" fn(*mut RTL_OSVERSIONINFOW) =
+            unsafe { *(&function_address as *const _ as *const _) };
+
+        let mut version_info: MaybeUninit<RTL_OSVERSIONINFOW> = mem::MaybeUninit::zeroed();
+        unsafe {
+            (*version_info.as_mut_ptr()).dwOSVersionInfoSize =
+                mem::size_of_val(&version_info) as u32;
+            rtl_get_version(version_info.as_mut_ptr());
+
+            Ok(WindowsVersion {
+                inner: version_info.assume_init(),
+            })
+        }
+    }
+
+    pub fn major_version(&self) -> u32 {
+        self.inner.dwMajorVersion
+    }
+
+    pub fn minor_version(&self) -> u32 {
+        self.inner.dwMinorVersion
+    }
+
+    pub fn build_number(&self) -> u32 {
+        self.inner.dwBuildNumber
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_windows_version() {
+        WindowsVersion::new().unwrap();
+    }
 }
