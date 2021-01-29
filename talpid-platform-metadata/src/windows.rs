7c628e5a35d2ac00ec3a79d21dd84b316f19e70f
Emīls Piņķis <emils@mullvad.net>
2021-01-29 11:51:24 +0000

    Fix things further still

--- talpid-platform-metadata/src/windows.rs
+++ talpid-platform-metadata/src/windows.rs
@@ -1,22 +1,29 @@
-use std::{ffi::OsString, io, iter, mem, os::windows::ffi::OsStrExt, ptr};
+use std::{
+    io,
+    mem::{self, MaybeUninit},
+    ptr,
+};
 use winapi::um::{
     libloaderapi::{GetModuleHandleW, GetProcAddress},
     winnt::RTL_OSVERSIONINFOW,
 };
 
 pub fn version() -> String {
-    short_version()
+    let (major, build) = WindowsVersion::new()
+        .map(|version_info| {
+            (
+                version_info.major_version().to_string(),
+                version_info.build_number().to_string(),
+            )
+        })
+        .unwrap_or_else(|_| ("N/A".to_string(), "N/A".to_string()));
+
+    format!("Windows {} Build {}", major, build)
 }
 
 pub fn short_version() -> String {
     let version_string = WindowsVersion::new()
-        .map(|version| {
-            format!(
-                "{} {}",
-                version.major_version().to_string(),
-                version.build_number().to_string()
-            )
-        })
+        .map(|version| version.major_version().to_string())
         .unwrap_or("N/A".into());
     format!("Windows {}", version_string)
 }
@@ -32,13 +39,7 @@ pub struct WindowsVersion {
 
 impl WindowsVersion {
     pub fn new() -> Result<WindowsVersion, io::Error> {
-        let rtl_get_version: extern "stdcall" fn(*mut RTL_OSVERSIONINFOW);
-
-        let module_name: Vec<u16> = OsString::from("ntdll")
-            .as_os_str()
-            .encode_wide()
-            .chain(iter::once(0u16))
-            .collect();
+        let module_name = utf16_lit::utf16!("ntdll");
 
         let ntdll = unsafe { GetModuleHandleW(module_name.as_ptr()) };
         if ntdll == ptr::null_mut() {
@@ -51,15 +52,19 @@ impl WindowsVersion {
             return Err(io::Error::last_os_error());
         }
 
-        rtl_get_version = unsafe { mem::transmute(function_address) };
+        let rtl_get_version: extern "stdcall" fn(*mut RTL_OSVERSIONINFOW) =
+            unsafe { mem::transmute(function_address) };
 
-        let mut version_info: RTL_OSVERSIONINFOW = unsafe { std::mem::zeroed() };
-        version_info.dwOSVersionInfoSize = mem::size_of_val(&version_info) as u32;
-        rtl_get_version(&mut version_info);
+        let mut version_info: MaybeUninit<RTL_OSVERSIONINFOW> = mem::MaybeUninit::zeroed();
+        unsafe {
+            (*version_info.as_mut_ptr()).dwOSVersionInfoSize =
+                mem::size_of_val(&version_info) as u32;
+            rtl_get_version(version_info.as_mut_ptr());
 
-        Ok(WindowsVersion {
-            inner: version_info,
-        })
+            Ok(WindowsVersion {
+                inner: version_info.assume_init(),
+            })
+        }
     }
 
     pub fn major_version(&self) -> u32 {
