edc042a583aa914a7d8197dc7ee9fc4efd84e923
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-29 11:50:28 +0000

    Send an event when listener is registered

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
@@ -3,6 +3,7 @@ package net.mullvad.mullvadvpn.service.endpoint
 import android.os.Looper
 import android.os.Messenger
 import net.mullvad.mullvadvpn.ipc.DispatchingHandler
+import net.mullvad.mullvadvpn.ipc.Event
 import net.mullvad.mullvadvpn.ipc.Request
 
 class ServiceEndpoint(looper: Looper) {
@@ -16,11 +17,23 @@ class ServiceEndpoint(looper: Looper) {
 
     init {
         dispatcher.registerHandler(Request.RegisterListener::class) { request ->
-            listeners.add(request.listener)
+            registerListener(request.listener)
         }
     }
 
     fun onDestroy() {
         dispatcher.onDestroy()
+    }
+
+    private fun registerListener(listener: Messenger) {
+        synchronized(this) {
+            listeners.add(listener)
+
+            val initialEvents = listOf(Event.ListenerReady)
+
+            initialEvents.forEach { event ->
+                listener.send(event.message)
+            }
+        }
     }
 }
