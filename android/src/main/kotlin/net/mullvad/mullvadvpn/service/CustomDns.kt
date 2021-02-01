4eb3f92a239efbd0a62f99e00ba0e4443b1e2cef
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-02-01 20:52:46 +0000

    Remove notifiers from service side `CustomDns`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/CustomDns.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/CustomDns.kt
@@ -2,29 +2,17 @@ package net.mullvad.mullvadvpn.service
 
 import java.net.InetAddress
 import java.util.ArrayList
-import kotlin.properties.Delegates.observable
 import kotlinx.coroutines.runBlocking
 import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.model.DnsOptions
 import net.mullvad.mullvadvpn.service.endpoint.ServiceEndpoint
-import net.mullvad.talpid.util.EventNotifier
 
 class CustomDns(private val endpoint: ServiceEndpoint) {
-    private var enabled by observable(false) { _, oldValue, newValue ->
-        if (oldValue != newValue) {
-            onEnabledChanged.notify(newValue)
-        }
-    }
-
     private val daemon
         get() = runBlocking { endpoint.intermittentDaemon.await() }
 
-    private var dnsServers by observable<ArrayList<InetAddress>>(ArrayList()) { _, _, servers ->
-        onDnsServersChanged.notify(servers.toList())
-    }
-
-    val onEnabledChanged = EventNotifier(false)
-    val onDnsServersChanged = EventNotifier<List<InetAddress>>(emptyList())
+    private var dnsServers = ArrayList<InetAddress>()
+    private var enabled = false
 
     init {
         endpoint.settingsListener.dnsOptionsNotifier.subscribe(this) { maybeDnsOptions ->
