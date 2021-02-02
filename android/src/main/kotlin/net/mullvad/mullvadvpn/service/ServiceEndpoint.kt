9c96c6166dd67c957a5dd16d0e71b265b8ccaa00
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-02-02 18:02:19 +0000

    Create a `SplitTunneling.onDestroy()` method

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceEndpoint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceEndpoint.kt
@@ -50,6 +50,7 @@ class ServiceEndpoint(
         keyStatusListener.onDestroy()
         locationInfoCache.onDestroy()
         settingsListener.onDestroy()
+        splitTunneling.onDestroy()
     }
 
     internal fun sendEvent(event: Event) {
