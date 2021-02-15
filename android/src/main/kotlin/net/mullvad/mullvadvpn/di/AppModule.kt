28a5a01c7954c1a91e9d8c189f888a580b2cab62
Aleksandr Granin <aleksandr@mullvad.net>
2021-02-15 18:47:31 +0100

    Clean code and finalize refactoring

--- android/src/main/kotlin/net/mullvad/mullvadvpn/di/AppModule.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/di/AppModule.kt
@@ -2,22 +2,26 @@ package net.mullvad.mullvadvpn.di
 
 import android.content.Context
 import android.content.pm.PackageManager
+import net.mullvad.mullvadvpn.applist.ApplicationsIconManager
 import net.mullvad.mullvadvpn.applist.ApplicationsProvider
 import net.mullvad.mullvadvpn.service.SplitTunneling
 import net.mullvad.mullvadvpn.viewmodel.SplitTunnelingViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import org.koin.dsl.onClose
 
 val appModule = module {
 
     single { SplitTunneling(get()) }
-
-    single { ApplicationsProvider(get<PackageManager>(), get<String>(named("packagename"))) }
-
     single<PackageManager> { get<Context>().packageManager }
+    single<String> (named(SELF_PACKAGE)) { get<Context>().packageName }
 
-    single<String> (named("packagename")) { get<Context>().packageName }
-
-    viewModel { SplitTunnelingViewModel(get(), get()) }
+    scope(named(APPS_SCOPE)) {
+        viewModel { SplitTunnelingViewModel(get(), get()) }
+        scoped { ApplicationsIconManager(get<PackageManager>()) } onClose { it?.dispose() }
+        scoped { ApplicationsProvider(get<PackageManager>(), get<String>(named(SELF_PACKAGE))) }
+    }
 }
+const val APPS_SCOPE = "APPS_SCOPE"
+const val SELF_PACKAGE = "SELF_PACKAGE"
