8b7a6c3212bc11f320f847a12f67c86f18c210e0
Emīls Piņķis <emils@mullvad.net>
2021-02-16 12:03:45 +0000

    Drop the patch version from short version

--- talpid-platform-metadata/src/macos.rs
+++ talpid-platform-metadata/src/macos.rs
@@ -2,17 +2,31 @@ mod command;
 use command::command_stdout_lossy;
 
 pub fn version() -> String {
-    format!(
-        "macOS {}",
-        command_stdout_lossy("sw_vers", &["-productVersion"])
-            .unwrap_or(String::from("[Failed to detect version]"))
-    )
+    let version = run_sw_vers().unwrap_or(String::from("N/A"));
+    format!("macOS {}", version)
 }
 
+
 pub fn short_version() -> String {
-    version()
+    let version = run_sw_vers()
+        .and_then(parse_short_version_output)
+        .map(|(major, minor)| format!("{}.{}", major, minor))
+        .unwrap_or(String::from("N/A"));
+    format!("macOS {}", version)
 }
 
 pub fn extra_metadata() -> impl Iterator<Item = (String, String)> {
     std::iter::empty()
+}
+
+/// Outputs a string in a format `$major.$minor.$patch`, e.g. `11.0.1`
+fn run_sw_vers() -> Option<String> {
+    command_stdout_lossy("sw_vers", &["-productVersion"])
+}
+
+fn parse_short_version_output(output: String) -> Option<(u32, u32)> {
+    let mut parts = output.split('.');
+    let major = parts.next()?.parse().ok()?;
+    let minor = parts.next()?.parse().ok()?;
+    Some((major, minor))
 }
