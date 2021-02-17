8662bc97def4049cb8ef46cf980b562b103d5338
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-02-17 15:49:35 +0000

    Make some enum variants data classes

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
@@ -1,17 +1,17 @@
 package net.mullvad.mullvadvpn.model
 
 sealed class LocationConstraint(val code: Array<String>) {
-    class Country(var countryCode: String) : LocationConstraint(arrayOf(countryCode)) {
+    data class Country(var countryCode: String) : LocationConstraint(arrayOf(countryCode)) {
         fun get0() = countryCode
     }
 
-    class City(var countryCode: String, var cityCode: String) :
+    data class City(var countryCode: String, var cityCode: String) :
         LocationConstraint(arrayOf(countryCode, cityCode)) {
         fun get0() = countryCode
         fun get1() = cityCode
     }
 
-    class Hostname(var countryCode: String, var cityCode: String, var hostname: String) :
+    data class Hostname(var countryCode: String, var cityCode: String, var hostname: String) :
         LocationConstraint(arrayOf(countryCode, cityCode, hostname)) {
         fun get0() = countryCode
         fun get1() = cityCode
