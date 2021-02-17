518e37a7cb5180fab3270bbcfebfd5285ea33dad
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-02-17 15:49:35 +0000

    Make some enum variants data classes

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/Constraint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/Constraint.kt
@@ -2,7 +2,8 @@ package net.mullvad.mullvadvpn.model
 
 sealed class Constraint<T>() {
     class Any<T>() : Constraint<T>()
-    class Only<T>(val value: T) : Constraint<T>() {
+
+    data class Only<T>(val value: T) : Constraint<T>() {
         fun get0() = value
     }
 }
--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelaySettingsUpdate.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelaySettingsUpdate.kt
@@ -3,7 +3,7 @@ package net.mullvad.mullvadvpn.model
 sealed class RelaySettingsUpdate {
     class CustomTunnelEndpoint() : RelaySettingsUpdate()
 
-    class Normal(var constraints: RelayConstraintsUpdate) : RelaySettingsUpdate() {
+    data class Normal(var constraints: RelayConstraintsUpdate) : RelaySettingsUpdate() {
         fun get0() = constraints
     }
 }
