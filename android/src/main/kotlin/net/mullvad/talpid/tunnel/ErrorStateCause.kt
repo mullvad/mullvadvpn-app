12c76fd261c40070101afcb6a53283dadc7071f2
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-02-18 17:44:50 +0000

    Make some `ErrorStateCause` variants singletons

--- android/src/main/kotlin/net/mullvad/talpid/tunnel/ErrorStateCause.kt
+++ android/src/main/kotlin/net/mullvad/talpid/tunnel/ErrorStateCause.kt
@@ -4,50 +4,12 @@ import java.net.InetAddress
 
 sealed class ErrorStateCause {
     class AuthFailed(val reason: String?) : ErrorStateCause()
-
-    class Ipv6Unavailable : ErrorStateCause() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = Ipv6Unavailable()
-        }
-    }
-
-    class SetFirewallPolicyError : ErrorStateCause() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = SetFirewallPolicyError()
-        }
-    }
-
-    class SetDnsError : ErrorStateCause() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = SetDnsError()
-        }
-    }
-
+    object Ipv6Unavailable : ErrorStateCause()
+    object SetFirewallPolicyError : ErrorStateCause()
+    object SetDnsError : ErrorStateCause()
     class InvalidDnsServers(val addresses: ArrayList<InetAddress>) : ErrorStateCause()
-
-    class StartTunnelError : ErrorStateCause() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = StartTunnelError()
-        }
-    }
-
+    object StartTunnelError : ErrorStateCause()
     class TunnelParameterError(val error: ParameterGenerationError) : ErrorStateCause()
-
-    class IsOffline : ErrorStateCause() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = IsOffline()
-        }
-    }
-
-    class VpnPermissionDenied : ErrorStateCause() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = VpnPermissionDenied()
-        }
-    }
+    object IsOffline : ErrorStateCause()
+    object VpnPermissionDenied : ErrorStateCause()
 }
