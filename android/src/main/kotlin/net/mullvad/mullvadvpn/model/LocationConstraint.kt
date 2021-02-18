a3a5202dd590b4809e5de18e93a00b23ccdada38
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-02-18 19:03:06 +0000

    Remove numbered getters that are no longer needed

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
@@ -1,20 +1,11 @@
 package net.mullvad.mullvadvpn.model
 
 sealed class LocationConstraint(val code: Array<String>) {
-    data class Country(var countryCode: String) : LocationConstraint(arrayOf(countryCode)) {
-        fun get0() = countryCode
-    }
+    data class Country(var countryCode: String) : LocationConstraint(arrayOf(countryCode))
 
     data class City(var countryCode: String, var cityCode: String) :
-        LocationConstraint(arrayOf(countryCode, cityCode)) {
-        fun get0() = countryCode
-        fun get1() = cityCode
-    }
+        LocationConstraint(arrayOf(countryCode, cityCode))
 
     data class Hostname(var countryCode: String, var cityCode: String, var hostname: String) :
-        LocationConstraint(arrayOf(countryCode, cityCode, hostname)) {
-        fun get0() = countryCode
-        fun get1() = cityCode
-        fun get2() = hostname
-    }
+        LocationConstraint(arrayOf(countryCode, cityCode, hostname))
 }
