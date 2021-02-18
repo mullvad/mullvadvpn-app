1b135c432e4ef019c89c69f15ba986317b71a495
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-02-18 19:03:06 +0000

    Remove numbered getters that are no longer needed

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/Constraint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/Constraint.kt
@@ -2,8 +2,5 @@ package net.mullvad.mullvadvpn.model
 
 sealed class Constraint<T>() {
     class Any<T>() : Constraint<T>()
-
-    data class Only<T>(val value: T) : Constraint<T>() {
-        fun get0() = value
-    }
+    data class Only<T>(val value: T) : Constraint<T>()
 }
--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelaySettingsUpdate.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelaySettingsUpdate.kt
@@ -2,8 +2,5 @@ package net.mullvad.mullvadvpn.model
 
 sealed class RelaySettingsUpdate {
     object CustomTunnelEndpoint : RelaySettingsUpdate()
-
-    data class Normal(var constraints: RelayConstraintsUpdate) : RelaySettingsUpdate() {
-        fun get0() = constraints
-    }
+    data class Normal(var constraints: RelayConstraintsUpdate) : RelaySettingsUpdate()
 }
