0c96f490aeecab3ac1555879a84af237fe15c2f7
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-02-18 17:02:07 +0000

    Make unit variants singleton objects

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/GetAccountDataResult.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/GetAccountDataResult.kt
@@ -2,25 +2,7 @@ package net.mullvad.mullvadvpn.model
 
 sealed class GetAccountDataResult {
     class Ok(val accountData: AccountData) : GetAccountDataResult()
-
-    class InvalidAccount : GetAccountDataResult() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = InvalidAccount()
-        }
-    }
-
-    class RpcError : GetAccountDataResult() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = RpcError()
-        }
-    }
-
-    class OtherError : GetAccountDataResult() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = OtherError()
-        }
-    }
+    object InvalidAccount : GetAccountDataResult()
+    object RpcError : GetAccountDataResult()
+    object OtherError : GetAccountDataResult()
 }
--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelaySettings.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/RelaySettings.kt
@@ -1,12 +1,6 @@
 package net.mullvad.mullvadvpn.model
 
 sealed class RelaySettings {
-    class CustomTunnelEndpoint() : RelaySettings() {
-        companion object {
-            @JvmStatic
-            val INSTANCE = CustomTunnelEndpoint()
-        }
-    }
-
+    object CustomTunnelEndpoint : RelaySettings()
     class Normal(var relayConstraints: RelayConstraints) : RelaySettings()
 }
