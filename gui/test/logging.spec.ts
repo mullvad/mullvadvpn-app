1ec22390750a1eb35e899b2853ce3aa89017306a
Oskar Nyberg <oskar@mullvad.net>
2021-02-19 14:40:36 +0100

    Add test for the log message formatter

--- gui/test/logging.spec.ts
+++ gui/test/logging.spec.ts
@@ -1,8 +1,10 @@
-import { expect } from 'chai';
+import { expect, spy } from 'chai';
 import fs from 'fs';
 import sinon from 'sinon';
 import { it, describe, before, beforeEach, after } from 'mocha';
+import { Logger } from '../src/shared/logging';
 import { backupLogFile, rotateOrDeleteFile } from '../src/main/logging';
+import { LogLevel } from '../src/shared/logging-types';
 
 const aPath = 'log-directory/a.log';
 const oldAPath = 'log-directory/a.old.log';
@@ -77,5 +79,54 @@ describe('Logging', () => {
 
     expect(fs.accessSync.bind(null, bPath)).to.throw();
     expect(fs.accessSync.bind(null, oldBPath)).to.throw();
+  });
+
+  it('should only log for the correct log level', () => {
+    const logger = new Logger();
+
+    const errorSpy = spy();
+    const warningSpy = spy();
+    const infoSpy = spy();
+    const verboseSpy = spy();
+    const debugSpy = spy();
+
+    logger.addOutput({ level: LogLevel.error, write: errorSpy });
+    logger.addOutput({ level: LogLevel.warning, write: warningSpy });
+    logger.addOutput({ level: LogLevel.info, write: infoSpy });
+    logger.addOutput({ level: LogLevel.verbose, write: verboseSpy });
+    logger.addOutput({ level: LogLevel.debug, write: debugSpy });
+
+    logger.error();
+    logger.warn();
+    logger.info();
+    logger.verbose();
+    logger.debug();
+
+    expect(errorSpy).to.have.been.called.exactly(1);
+    expect(warningSpy).to.have.been.called.exactly(2);
+    expect(infoSpy).to.have.been.called.exactly(3);
+    expect(verboseSpy).to.have.been.called.exactly(4);
+    expect(debugSpy).to.have.been.called.exactly(5);
+  });
+
+  it('should format JavaScript types correctly', async () => {
+    const logger = new Logger();
+
+    const promise = new Promise((resolve, _reject) => {
+      logger.addOutput({
+        level: LogLevel.info,
+        write: (_, message) => resolve(message.replace(/^.*\[info\] /, '')),
+      });
+    });
+
+    logger.info('zero', 'one two', 3, [4, 5, 'six', { seven: 'eight' }], {
+      nine: 10,
+      eleven: [true],
+    });
+    await expect(promise).to.eventually.be.fulfilled.then((result) => {
+      expect(result).to.equal(
+        'zero one two 3 [4,5,"six",{"seven":"eight"}] {"nine":10,"eleven":[true]}',
+      );
+    });
   });
 });
