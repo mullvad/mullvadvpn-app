a880da8a503f0e4dbdc9b75c4c60ff0732e470e5
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-02-24 15:17:56 +0000

    Make some model class properties read-only

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
@@ -1,11 +1,11 @@
 package net.mullvad.mullvadvpn.model
 
 sealed class LocationConstraint(val code: Array<String>) {
-    data class Country(var countryCode: String) : LocationConstraint(arrayOf(countryCode))
+    data class Country(val countryCode: String) : LocationConstraint(arrayOf(countryCode))
 
-    data class City(var countryCode: String, var cityCode: String) :
+    data class City(val countryCode: String, val cityCode: String) :
         LocationConstraint(arrayOf(countryCode, cityCode))
 
-    data class Hostname(var countryCode: String, var cityCode: String, var hostname: String) :
+    data class Hostname(val countryCode: String, val cityCode: String, val hostname: String) :
         LocationConstraint(arrayOf(countryCode, cityCode, hostname))
 }
