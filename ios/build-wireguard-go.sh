88984749cbe593e1e1e030802d8665744b7a8cb5
Andrej Mihajlov <and@mullvad.net>
2021-03-02 11:30:39 +0100

    Add a build script for wireguard-go

---
+++ ios/build-wireguard-go.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# build-wireguard-go.sh
+# A helper build script for WireGuardGoBridge via ExternalBuildSystem target in Xcode.
+#
+# ExternalBuildSystem target configuration:
+# Build Tool: /bin/sh
+# Arguments: build-wireguard-go.sh $(ACTION)
+# Directory: $(SOURCE_ROOT)
+# Pass build settings in environment: YES
+
+# Passed by Xcode
+ACTION=$1
+
+# When archiving, Xcode sets the action to "install"
+if [ "$ACTION" == "install" ]; then
+	SOURCE_PACKAGES_PATH="$BUILD_DIR/../../../../../SourcePackages"
+else
+	SOURCE_PACKAGES_PATH="$BUILD_DIR/../../SourcePackages"
+fi
+
+# Resolve SourcesPackages path
+RESLVED_SOURCE_PACKAGES_PATH="$( cd "$SOURCE_PACKAGES_PATH" && pwd -P )"
+if [ "$RESLVED_SOURCE_PACKAGES_PATH" == "" ]; then
+	echo "Failed to resolve the SourcePackages path: $SOURCE_PACKAGES_PATH"
+	exit -1
+fi
+
+# Compile the path to the Makefile directory
+WIREGUARD_KIT_GO_PATH="$RESLVED_SOURCE_PACKAGES_PATH/checkouts/wireguard-apple/Sources/WireGuardKitGo"
+echo "WireGuardKitGo path resolved to $WIREGUARD_KIT_GO_PATH"
+
+# Run make
+/usr/bin/make -C "$WIREGUARD_KIT_GO_PATH" $ACTION
