48c790cfd50526955bf8d87a72007ef681c25368
Emīls <emils@mullvad.net>
2021-03-05 14:22:21 +0000

    Always set src_valid_mark when connecting

--- talpid-core/src/linux/mod.rs
+++ talpid-core/src/linux/mod.rs
@@ -1,7 +1,9 @@
 use std::{
     ffi::{self, CString},
-    io,
+    fs, io,
 };
+
+const PROC_SYS_NET_IPV4_CONF_SRC_VALID_MARK: &str = "/proc/sys/net/ipv4/conf/all/src_valid_mark";
 
 /// Converts an interface name into the corresponding index.
 pub fn iface_index(name: &str) -> Result<libc::c_uint, IfaceIndexLookupError> {
@@ -29,3 +31,7 @@ pub enum IfaceIndexLookupError {
 // b"mole" is [ 0x6d, 0x6f 0x6c, 0x65 ]
 pub const TUNNEL_FW_MARK: u32 = 0x6d6f6c65;
 pub const TUNNEL_TABLE_ID: u32 = 0x6d6f6c65;
+
+pub fn set_src_valid_mark_sysctl() -> io::Result<()> {
+    fs::write(PROC_SYS_NET_IPV4_CONF_SRC_VALID_MARK, b"1")
+}
