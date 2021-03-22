2db87d1b22896476cbbdf1fd3c27d1d7ecdbae95
David Lönnhager <david.l@mullvad.net>
2021-03-22 19:53:12 +0100

    Silence logs from noisy 'tracing' crate

--- mullvad-daemon/src/logging.rs
+++ mullvad-daemon/src/logging.rs
@@ -39,7 +39,7 @@ pub const SILENCED_CRATES: &[&str] = &[
     "rustls",
     "netlink_proto",
     "netlink_sys",
-    "iproute2",
+    "tracing",
 ];
 const SLIGHTLY_SILENCED_CRATES: &[&str] = &["mnl", "nftnl"];
 
