1047d8fdd65fe63361cf0e2f88dde51cbd9a83ec
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-03-25 13:50:16 +0000

    Remove unused `code` property

--- android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/LocationConstraint.kt
@@ -3,16 +3,15 @@ package net.mullvad.mullvadvpn.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-sealed class LocationConstraint(val code: Array<String>) : Parcelable {
+sealed class LocationConstraint : Parcelable {
     @Parcelize
-    data class Country(val countryCode: String) :
-        LocationConstraint(arrayOf(countryCode)), Parcelable
+    data class Country(val countryCode: String) : LocationConstraint(), Parcelable
 
     @Parcelize
     data class City(val countryCode: String, val cityCode: String) :
-        LocationConstraint(arrayOf(countryCode, cityCode)), Parcelable
+        LocationConstraint(), Parcelable
 
     @Parcelize
     data class Hostname(val countryCode: String, val cityCode: String, val hostname: String) :
-        LocationConstraint(arrayOf(countryCode, cityCode, hostname)), Parcelable
+        LocationConstraint(), Parcelable
 }
