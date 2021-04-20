8cda25a7bcf6ba54cbf0dd0d0fca9ee9afd6bd10
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-04-20 19:56:56 +0000

    Send requests from service side `VpnPermission`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/VpnPermission.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/VpnPermission.kt
@@ -3,11 +3,12 @@ package net.mullvad.mullvadvpn.service.endpoint
 import android.content.Context
 import android.content.Intent
 import android.net.VpnService
+import net.mullvad.mullvadvpn.ipc.Event
 import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.ui.MainActivity
 import net.mullvad.mullvadvpn.util.Intermittent
 
-class VpnPermission(private val context: Context, endpoint: ServiceEndpoint) {
+class VpnPermission(private val context: Context, private val endpoint: ServiceEndpoint) {
     private val isGranted = Intermittent<Boolean>()
 
     init {
@@ -31,6 +32,7 @@ class VpnPermission(private val context: Context, endpoint: ServiceEndpoint) {
             isGranted.update(null)
 
             context.startActivity(activityIntent)
+            endpoint.sendEvent(Event.VpnPermissionRequest)
         }
 
         return isGranted.await()
