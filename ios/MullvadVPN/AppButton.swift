9adc24e7ca0c0d2e01ed25c75f8d662964a9f29c
Andrej Mihajlov <and@mullvad.net>
2021-04-28 17:39:07 +0200

    LinkButton: add titleString to handle the link title

--- ios/MullvadVPN/AppButton.swift
+++ ios/MullvadVPN/AppButton.swift
@@ -62,24 +62,22 @@ private extension UIControl.State {
         commonInit()
     }
 
-    override func setTitle(_ title: String?, for state: UIControl.State) {
-        if let title = title {
-            setAttributedTitle(makeAttributedTitle(title, for: state), for: state)
-        } else {
-            setAttributedTitle(nil, for: state)
+    var titleString: String? {
+        didSet {
+            updateAttributedTitle(string: titleString)
         }
     }
 
     private func commonInit() {
         imageAlignment = .trailing
         contentHorizontalAlignment = .leading
+    }
 
+    private func updateAttributedTitle(string: String?) {
         let states: [UIControl.State] = [.normal, .highlighted, .disabled]
         states.forEach { (state) in
-            if let title = self.title(for: state) {
-                let attributedTitle = makeAttributedTitle(title, for: state)
-                self.setAttributedTitle(attributedTitle, for: state)
-            }
+            let attributedTitle = string.flatMap { makeAttributedTitle($0, for: state) }
+            self.setAttributedTitle(attributedTitle, for: state)
         }
     }
 
