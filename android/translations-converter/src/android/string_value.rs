fd734cf33689746fb6c699cb42a4b208dd9642af
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-05-10 20:18:41 +0000

    Remove previous Android string normalization

--- android/translations-converter/src/android/string_value.rs
+++ android/translations-converter/src/android/string_value.rs
@@ -1,22 +1,10 @@
-use lazy_static::lazy_static;
-use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::{
     fmt::{self, Display, Formatter},
     ops::Deref,
 };
 
-lazy_static! {
-    static ref LINE_BREAKS: Regex = Regex::new(r"\s*\n\s*").unwrap();
-    static ref APOSTROPHES: Regex = Regex::new(r"\\'").unwrap();
-    static ref DOUBLE_QUOTES: Regex = Regex::new(r#"\\""#).unwrap();
-    static ref PARAMETERS: Regex = Regex::new(r"%[0-9]*\$").unwrap();
-}
-
 /// An Android string value
-///
-/// Handles escaping the string when it is created but also allows normalizing it for comparing it
-/// with gettext messages through a `normalize` method.
 #[derive(Clone, Debug, Eq, Deserialize, Hash, PartialEq, Serialize)]
 pub struct StringValue(String);
 
@@ -40,23 +28,6 @@ impl From<&str> for StringValue {
 }
 
 impl StringValue {
-    /// Normalize the string value into a common format.
-    ///
-    /// Makes it possible to compare the Android strings with the gettext messages.
-    pub fn normalize(&mut self) {
-        // Collapse line breaks present in the XML file
-        let value = LINE_BREAKS.replace_all(&self.0, " ");
-        // Unescape apostrophes
-        let value = APOSTROPHES.replace_all(&value, "'");
-        // Unescape double quotes
-        let value = DOUBLE_QUOTES.replace_all(&value, r#"""#);
-        // Mark where parameters are positioned, removing the parameter index
-        let value = PARAMETERS.replace_all(&value, "%");
-
-        // Unescape XML characters
-        self.0 = htmlize::unescape(value.as_bytes());
-    }
-
     /// Clones the internal string value.
     pub fn to_string(&self) -> String {
         self.0.clone()
