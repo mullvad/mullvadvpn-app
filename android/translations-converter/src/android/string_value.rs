adaff9a5516cce11d4a2da4efb0b576097458df6
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-06-05 20:58:02 +0000

    Remove deserialization of `android::StringValue`
    Not longer used because it was replaced by parsing using `roxmltree`.

--- android/translations-converter/src/android/string_value.rs
+++ android/translations-converter/src/android/string_value.rs
@@ -1,13 +1,12 @@
 use lazy_static::lazy_static;
 use regex::Regex;
-use serde::{Deserialize, Deserializer, Serialize};
 use std::{
     fmt::{self, Display, Formatter},
     ops::Deref,
 };
 
 /// An Android string value
-#[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct StringValue(String);
 
 impl StringValue {
@@ -186,15 +185,6 @@ impl Deref for StringValue {
 impl Display for StringValue {
     fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
         write!(formatter, "{}", self.0)
-    }
-}
-
-impl<'de> Deserialize<'de> for StringValue {
-    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let raw_string = String::deserialize(deserializer)?;
-        let string_with_collapsed_newlines = Self::collapse_line_breaks(raw_string);
-
-        Ok(StringValue(string_with_collapsed_newlines))
     }
 }
 
