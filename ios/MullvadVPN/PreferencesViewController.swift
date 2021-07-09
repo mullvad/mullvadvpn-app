5f485135b75aa34ab327a66d99bdb22231d85768
Andrej Mihajlov <and@mullvad.net>
2021-07-09 17:27:43 +0200

    Accessibility: make toggle cells in preferences behave as switch buttons

--- ios/MullvadVPN/PreferencesViewController.swift
+++ ios/MullvadVPN/PreferencesViewController.swift
@@ -75,7 +75,7 @@ class PreferencesViewController: UITableViewController, TunnelObserver {
             let cell = cell as! SettingsSwitchCell
 
             cell.titleLabel.text = NSLocalizedString("Block ads", comment: "")
-            cell.switchControl.setOn(self.dnsSettings?.blockAdvertising ?? false, animated: false)
+            cell.setOn(self.dnsSettings?.blockAdvertising ?? false, animated: false)
             cell.action = { [weak self] (isOn) in
                 self?.dnsSettings?.blockAdvertising = isOn
                 self?.saveDNSSettings()
@@ -87,7 +87,7 @@ class PreferencesViewController: UITableViewController, TunnelObserver {
             let cell = cell as! SettingsSwitchCell
 
             cell.titleLabel.text = NSLocalizedString("Block trackers", comment: "")
-            cell.switchControl.setOn(self.dnsSettings?.blockTracking ?? false, animated: false)
+            cell.setOn(self.dnsSettings?.blockTracking ?? false, animated: false)
             cell.action = { [weak self] (isOn) in
                 self?.dnsSettings?.blockTracking = isOn
                 self?.saveDNSSettings()
--- ios/MullvadVPN/SettingsSwitchCell.swift
+++ ios/MullvadVPN/SettingsSwitchCell.swift
@@ -10,27 +10,80 @@ import UIKit
 
 class SettingsSwitchCell: SettingsCell {
 
-    let switchContainer = CustomSwitchContainer()
-    var switchControl: CustomSwitch {
-        return switchContainer.control
-    }
+    private let switchContainer = CustomSwitchContainer()
 
     var action: ((Bool) -> Void)?
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-        self.accessoryView = switchContainer
+        accessoryView = switchContainer
 
-        switchControl.addTarget(self, action: #selector(switchValueDidChange), for: .valueChanged)
+        switchContainer.control.addTarget(self, action: #selector(switchValueDidChange), for: .valueChanged)
+
+        // Use UISwitch traits to make the entire cell behave as "Switch button"
+        accessibilityTraits = switchContainer.control.accessibilityTraits
+        isAccessibilityElement = true
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    @objc private func switchValueDidChange() {
-        self.action?(self.switchControl.isOn)
+    func setOn(_ isOn: Bool, animated: Bool) {
+        switchContainer.control.setOn(isOn, animated: animated)
     }
 
+    // MARK: - Actions
+
+    @objc private func switchValueDidChange() {
+        action?(self.switchContainer.control.isOn)
+    }
+
+    // MARK: -  Accessibility
+
+    override var accessibilityLabel: String? {
+        set {
+            super.accessibilityLabel = newValue
+        }
+        get {
+            return titleLabel.text
+        }
+    }
+
+    override var accessibilityValue: String? {
+        set {
+            super.accessibilityValue = newValue
+        }
+        get {
+            return self.switchContainer.control.accessibilityValue
+        }
+    }
+
+    override var accessibilityFrame: CGRect {
+        set {
+            super.accessibilityFrame = newValue
+        }
+        get {
+            return UIAccessibility.convertToScreenCoordinates(self.bounds, in: self)
+        }
+    }
+
+    override var accessibilityPath: UIBezierPath? {
+        set {
+            super.accessibilityPath = newValue
+        }
+        get {
+            return UIBezierPath(roundedRect: accessibilityFrame, cornerRadius: 4)
+        }
+    }
+
+    override func accessibilityActivate() -> Bool {
+        let newValue = !self.switchContainer.control.isOn
+
+        setOn(newValue, animated: true)
+        action?(newValue)
+
+        return true
+    }
 }
