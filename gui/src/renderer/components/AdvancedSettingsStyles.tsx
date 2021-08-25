b720305a57eb1c6d4f7eb4d4ccd58dd2990bd2d6
Oskar Nyberg <oskar@mullvad.net>
2021-08-25 09:11:00 +0200

    Refactor custom DNS and make editable

--- gui/src/renderer/components/AdvancedSettingsStyles.tsx
+++ gui/src/renderer/components/AdvancedSettingsStyles.tsx
@@ -32,29 +32,3 @@ export const StyledNoWireguardKeyError = styled(Cell.FooterText)({
   fontWeight: 800,
   color: colors.red,
 });
-
-export const StyledCustomDnsSwitchContainer = styled(Cell.Container)({
-  marginBottom: '1px',
-});
-
-export const StyledCustomDnsFooter = styled(Cell.Footer)({
-  marginBottom: '2px',
-});
-
-export const StyledAddCustomDnsButton = styled(Cell.CellButton)({
-  backgroundColor: colors.blue40,
-});
-
-export const StyledAddCustomDnsLabel = styled(Cell.Label)(
-  {},
-  (props: { paddingLeft?: number }) => ({
-    fontFamily: 'Open Sans',
-    fontWeight: 'normal',
-    fontSize: '16px',
-    paddingLeft: (props.paddingLeft ?? 32) + 'px',
-    whiteSpace: 'pre-wrap',
-    overflowWrap: 'break-word',
-    width: '171px',
-    marginRight: '25px',
-  }),
-);
