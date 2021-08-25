6c45ecf5774699cd30943d876098b83123fe5a3f
Aleksandr Granin <aleksandr@mullvad.net>
2021-08-25 11:21:26 +0200

    Add ViewIntent for system apps appearing

--- android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppData.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppData.kt
@@ -1,3 +1,8 @@
 package net.mullvad.mullvadvpn.applist
 
-data class AppData(val packageName: String, val iconRes: Int, val name: String, val isSystemApp: Boolean = false)
+data class AppData(
+    val packageName: String,
+    val iconRes: Int,
+    val name: String,
+    val isSystemApp: Boolean = false
+)
--- android/src/main/kotlin/net/mullvad/mullvadvpn/applist/ViewIntent.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/applist/ViewIntent.kt
@@ -6,4 +6,5 @@ sealed class ViewIntent {
     // In future we will have search intent
     data class ChangeApplicationGroup(val item: ListItemData) : ViewIntent()
     object ViewIsReady : ViewIntent()
+    data class ShowSystemApps(internal val show: Boolean) : ViewIntent()
 }
