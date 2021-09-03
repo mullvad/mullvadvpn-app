556b2d6316321a97d19c9ad1f32e11d8ab0b7a15
David Lönnhager <david.l@mullvad.net>
2021-09-03 16:11:32 +0200

    Track data received by each peer in the ping monitor

--- talpid-core/src/tunnel/wireguard/stats.rs
+++ talpid-core/src/tunnel/wireguard/stats.rs
@@ -4,11 +4,11 @@ use super::wireguard_kernel::wg_message::{DeviceMessage, DeviceNla, PeerNla};
 
 #[derive(err_derive::Error, Debug, PartialEq)]
 pub enum Error {
+    #[error(display = "Failed to parse peer pubkey from string \"_0\"")]
+    PubKeyParseError(String, #[error(source)] hex::FromHexError),
+
     #[error(display = "Failed to parse integer from string \"_0\"")]
     IntParseError(String, #[error(source)] std::num::ParseIntError),
-
-    #[error(display = "Config key not found")]
-    KeyNotFoundError,
 
     #[error(display = "Device no longer exists")]
     NoTunnelDevice,
@@ -21,8 +21,14 @@ pub struct Stats {
     pub rx_bytes: u64,
 }
 
+/// A map from peer pubkeys to peer stats.
+pub type StatsMap = std::collections::HashMap<[u8; 32], Stats>;
+
 impl Stats {
-    pub fn parse_config_str(config: &str) -> Result<Self, Error> {
+    pub fn parse_config_str(config: &str) -> Result<StatsMap, Error> {
+        let mut map = StatsMap::new();
+
+        let mut peer = None;
         let mut tx_bytes = None;
         let mut rx_bytes = None;
 
@@ -36,6 +42,14 @@ impl Stats {
 
         for (key, value) in parts {
             match key {
+                "public_key" => {
+                    let mut buffer = [0u8; 32];
+                    hex::decode_to_slice(value, &mut buffer)
+                        .map_err(|err| Error::PubKeyParseError(value.to_string(), err))?;
+                    peer = Some(buffer);
+                    tx_bytes = None;
+                    rx_bytes = None;
+                }
                 "rx_bytes" => {
                     rx_bytes = Some(
                         value
@@ -55,35 +69,53 @@ impl Stats {
 
                 _ => continue,
             }
-        }
 
-        match (tx_bytes, rx_bytes) {
-            (Some(tx_bytes), Some(rx_bytes)) => Ok(Self { tx_bytes, rx_bytes }),
-            _ => Err(Error::KeyNotFoundError),
+            match (peer, tx_bytes, rx_bytes) {
+                (Some(peer_val), Some(tx_bytes_val), Some(rx_bytes_val)) => {
+                    map.insert(
+                        peer_val,
+                        Self {
+                            tx_bytes: tx_bytes_val,
+                            rx_bytes: rx_bytes_val,
+                        },
+                    );
+                    peer = None;
+                    tx_bytes = None;
+                    rx_bytes = None;
+                }
+                _ => (),
+            }
         }
+        Ok(map)
     }
 
     #[cfg(target_os = "linux")]
-    pub fn parse_device_message(message: &DeviceMessage) -> Self {
-        // iterate over device attributes
-        let mut tx_bytes = 0;
-        let mut rx_bytes = 0;
+    pub fn parse_device_message(message: &DeviceMessage) -> StatsMap {
+        let mut map = StatsMap::new();
+
         for nla in &message.nlas {
             if let DeviceNla::Peers(peers) = nla {
-                // iterate over all peer attributes
-                let peer_iter = peers.iter().map(|peer| peer.0.as_slice()).flatten();
+                for msg in peers {
+                    let mut tx_bytes = 0;
+                    let mut rx_bytes = 0;
+                    let mut pub_key = None;
 
-                for peer_nla in peer_iter {
-                    match peer_nla {
-                        PeerNla::TxBytes(bytes) => tx_bytes += *bytes,
-                        PeerNla::RxBytes(bytes) => rx_bytes += *bytes,
-                        _ => continue,
-                    };
+                    for nla in &msg.0 {
+                        match nla {
+                            PeerNla::TxBytes(bytes) => tx_bytes = *bytes,
+                            PeerNla::RxBytes(bytes) => rx_bytes = *bytes,
+                            PeerNla::PublicKey(key) => pub_key = Some(*key),
+                            _ => continue,
+                        }
+                    }
+                    if let Some(key) = pub_key {
+                        map.insert(key, Stats { tx_bytes, rx_bytes });
+                    }
                 }
             }
         }
 
-        Self { tx_bytes, rx_bytes }
+        map
     }
 }
 
@@ -95,10 +127,14 @@ mod test {
     #[test]
     fn test_parsing() {
         let valid_input = "private_key=0000000000000000000000000000000000000000000000000000000000000000\npublic_key=0000000000000000000000000000000000000000000000000000000000000000\npreshared_key=0000000000000000000000000000000000000000000000000000000000000000\nprotocol_version=1\nendpoint=000.000.000.000:00000\nlast_handshake_time_sec=1578420649\nlast_handshake_time_nsec=369416131\ntx_bytes=2740\nrx_bytes=2396\npersistent_keepalive_interval=0\nallowed_ip=0.0.0.0/0\n";
+        let pubkey = [0u8; 32];
 
         let stats = Stats::parse_config_str(valid_input).expect("Failed to parse valid input");
-        assert_eq!(stats.rx_bytes, 2396);
-        assert_eq!(stats.tx_bytes, 2740);
+        assert_eq!(stats.len(), 1);
+        let actual_keys: Vec<[u8; 32]> = stats.keys().cloned().collect();
+        assert_eq!(actual_keys, [pubkey]);
+        assert_eq!(stats[&pubkey].rx_bytes, 2396);
+        assert_eq!(stats[&pubkey].tx_bytes, 2740);
     }
 
     #[test]
@@ -110,15 +146,6 @@ mod test {
         assert_eq!(
             Stats::parse_config_str(invalid_input),
             Err(Error::IntParseError(invalid_str, int_err))
-        );
-    }
-
-    #[test]
-    fn test_parsing_missing_keys() {
-        let invalid_input = "private_key=0000000000000000000000000000000000000000000000000000000000000000\npublic_key=0000000000000000000000000000000000000000000000000000000000000000\npreshared_key=0000000000000000000000000000000000000000000000000000000000000000\nprotocol_version=1\nendpoint=000.000.000.000:00000\nlast_handshake_time_sec=1578420649\nlast_handshake_time_nsec=369416131\ntx_bytes=2740\npersistent_keepalive_interval=0\nallowed_ip=0.0.0.0/0\n";
-        assert_eq!(
-            Stats::parse_config_str(invalid_input),
-            Err(Error::KeyNotFoundError)
         );
     }
 }
