7fee1788e6a736e57dbe843d0c73533ddad804ba
Andrej Mihajlov <and@mullvad.net>
2021-09-14 12:40:21 +0200

    AlertPresenter: refactor

--- ios/MullvadVPN/AlertPresenter.swift
+++ ios/MullvadVPN/AlertPresenter.swift
@@ -9,20 +9,20 @@
 import Foundation
 import UIKit
 
-private let kUIAlertControllerDidDissmissNotification = Notification.Name("UIAlertControllerDidDismiss")
-
 class AlertPresenter {
-    private enum ExclusivityCategory {
-        case exclusive
-    }
+    static let alertControllerDidDismissNotification = Notification.Name("UIAlertControllerDidDismiss")
 
-    private let operationQueue = OperationQueue()
-    private lazy var exclusivityController = ExclusivityController<ExclusivityCategory>(operationQueue: operationQueue)
+    private let operationQueue: OperationQueue = {
+        let operationQueue = OperationQueue()
+        operationQueue.name = "AlertPresenterQueue"
+        operationQueue.maxConcurrentOperationCount = 1
+        return operationQueue
+    }()
 
     private static let initClass: Void = {
         /// Swizzle `viewDidDisappear` on `UIAlertController` in order to be able to
         /// detect when the controller disappears.
-        /// The event is broadcasted via `kUIAlertControllerDidDissmissNotification` notification.
+        /// The event is broadcasted via `AlertPresenter.alertControllerDidDismissNotification` notification.
         swizzleMethod(aClass: UIAlertController.self, originalSelector: #selector(UIAlertController.viewDidDisappear(_:)), newSelector: #selector(UIAlertController.alertPresenter_viewDidDisappear(_:)))
     }()
 
@@ -37,11 +37,14 @@ class AlertPresenter {
             presentCompletion: presentCompletion
         )
 
-        exclusivityController.addOperation(operation, categories: [.exclusive])
+        operationQueue.addOperation(operation)
+    }
+
+    func cancelAll() {
+        operationQueue.cancelAllOperations()
     }
 
 }
-
 
 fileprivate extension UIAlertController {
     @objc dynamic func alertPresenter_viewDidDisappear(_ animated: Bool) {
@@ -49,37 +52,7 @@ fileprivate extension UIAlertController {
         alertPresenter_viewDidDisappear(animated)
 
         if presentingViewController == nil {
-            NotificationCenter.default.post(name: kUIAlertControllerDidDissmissNotification, object: self)
-        }
-    }
-}
-
-
-private class PresentAlertOperation: AsyncOperation {
-    private let alertController: UIAlertController
-    private let presentingController: UIViewController
-    private var dismissalObserver: NSObjectProtocol?
-    private let presentCompletion: (() -> Void)?
-
-    init(alertController: UIAlertController, presentingController: UIViewController, presentCompletion: (() -> Void)? = nil) {
-        self.alertController = alertController
-        self.presentingController = presentingController
-        self.presentCompletion = presentCompletion
-
-        super.init()
-    }
-
-    override func main() {
-        DispatchQueue.main.async {
-            self.dismissalObserver = NotificationCenter.default.addObserver(
-                forName: kUIAlertControllerDidDissmissNotification,
-                object: self.alertController,
-                queue: nil,
-                using: { [weak self] (note) in
-                    self?.finish()
-            })
-
-            self.presentingController.present(self.alertController, animated: true, completion: self.presentCompletion)
+            NotificationCenter.default.post(name: AlertPresenter.alertControllerDidDismissNotification, object: self)
         }
     }
 }
