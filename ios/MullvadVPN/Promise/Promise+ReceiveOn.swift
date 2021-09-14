2ab8fbb3a5a2719dab53cb7a2028c8cbb9dfd5bf
Andrej Mihajlov <and@mullvad.net>
2021-09-14 11:03:59 +0200

    Promise: make receive(on:) calls cancellable and add timer type

--- ios/MullvadVPN/Promise/Promise+ReceiveOn.swift
+++ ios/MullvadVPN/Promise/Promise+ReceiveOn.swift
@@ -9,23 +9,47 @@
 import Foundation
 
 extension Promise {
+    /// A type of timer.
+    enum TimerType {
+        case deadline
+        case walltime
+    }
+
     /// Dispatch the upstream value on another queue.
     func receive(on queue: DispatchQueue) -> Promise<Value> {
         return Promise<Value> { resolver in
-            _ = self.observe { completion in
-                queue.async {
+            self.observe { completion in
+                let work = DispatchWorkItem {
                     resolver.resolve(completion: completion, queue: queue)
                 }
+
+                resolver.setCancelHandler {
+                    work.cancel()
+                }
+
+                queue.async(execute: work)
             }
         }
     }
 
     /// Dispatch the upstream value on another queue after delay.
-    func receive(on queue: DispatchQueue, after deadline: DispatchTime) -> Promise<Value> {
+    func receive(on queue: DispatchQueue, after timeInterval: DispatchTimeInterval, timerType: TimerType) -> Promise<Value> {
         return Promise<Value> { resolver in
-            _ = self.observe { completion in
-                queue.asyncAfter(deadline: deadline) {
+            self.observe { completion in
+                let work = DispatchWorkItem {
                     resolver.resolve(completion: completion, queue: queue)
+                }
+
+                resolver.setCancelHandler {
+                    work.cancel()
+                }
+
+                switch timerType {
+                case .deadline:
+                    queue.asyncAfter(deadline: .now() + timeInterval, execute: work)
+
+                case .walltime:
+                    queue.asyncAfter(wallDeadline: .now() + timeInterval, execute: work)
                 }
             }
         }
