353beab2f92562db8e036e02f11cb21fee7a1600
Andrej Mihajlov <and@mullvad.net>
2021-09-17 15:08:38 +0200

    Actions: cache spm

--- ios/build-wireguard-go.sh
+++ ios/build-wireguard-go.sh
@@ -11,22 +11,24 @@
 # Passed by Xcode
 ACTION=$1
 
-# When archiving, Xcode sets the action to "install"
-if [ "$ACTION" == "install" ]; then
-	SOURCE_PACKAGES_PATH="$BUILD_DIR/../../../../../SourcePackages"
-else
-	SOURCE_PACKAGES_PATH="$BUILD_DIR/../../SourcePackages"
+if [ "$SOURCE_PACKAGES_PATH" == "" ]; then
+  # When archiving, Xcode sets the action to "install"
+  if [ "$ACTION" == "install" ]; then
+    SOURCE_PACKAGES_PATH="$BUILD_DIR/../../../../../SourcePackages"
+  else
+    SOURCE_PACKAGES_PATH="$BUILD_DIR/../../SourcePackages"
+  fi
 fi
 
 # Resolve SourcesPackages path
-RESLVED_SOURCE_PACKAGES_PATH="$( cd "$SOURCE_PACKAGES_PATH" && pwd -P )"
-if [ "$RESLVED_SOURCE_PACKAGES_PATH" == "" ]; then
-	echo "Failed to resolve the SourcePackages path: $SOURCE_PACKAGES_PATH"
-	exit -1
+RESOLVED_SOURCE_PACKAGES_PATH="$( cd "$SOURCE_PACKAGES_PATH" && pwd -P )"
+if [ "$RESOLVED_SOURCE_PACKAGES_PATH" == "" ]; then
+  echo "Failed to resolve the SourcePackages path: $SOURCE_PACKAGES_PATH"
+  exit -1
 fi
 
 # Compile the path to the Makefile directory
-WIREGUARD_KIT_GO_PATH="$RESLVED_SOURCE_PACKAGES_PATH/checkouts/wireguard-apple/Sources/WireGuardKitGo"
+WIREGUARD_KIT_GO_PATH="$RESOLVED_SOURCE_PACKAGES_PATH/checkouts/wireguard-apple/Sources/WireGuardKitGo"
 echo "WireGuardKitGo path resolved to $WIREGUARD_KIT_GO_PATH"
 
 # Run make
