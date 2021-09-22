b2d0d371088b3b3461000be381394b7cf5ccae2d
David Lönnhager <david.l@mullvad.net>
2021-09-22 20:40:07 +0200

    Add retries to frontend account requests

--- mullvad-daemon/src/account.rs
+++ mullvad-daemon/src/account.rs
@@ -2,16 +2,21 @@ use chrono::{DateTime, Utc};
 use futures::future::{abortable, AbortHandle};
 use mullvad_rpc::{
     availability::ApiAvailabilityHandle,
-    rest::{self, MullvadRestHandle},
+    rest::{self, Error as RestError, MullvadRestHandle},
     AccountsProxy,
 };
 use mullvad_types::account::{AccountToken, VoucherSubmission};
-use std::time::Duration;
-use talpid_core::future_retry::{retry_future_with_backoff, ExponentialBackoff, Jittered};
+use std::{future::Future, time::Duration};
+use talpid_core::future_retry::{
+    constant_interval, retry_future_n, retry_future_with_backoff, ExponentialBackoff, Jittered,
+};
 
-const RETRY_INTERVAL_INITIAL: Duration = Duration::from_secs(4);
-const RETRY_INTERVAL_FACTOR: u32 = 5;
-const RETRY_INTERVAL_MAX: Duration = Duration::from_secs(24 * 60 * 60);
+const RETRY_ACTION_INTERVAL: Duration = Duration::from_millis(500);
+const RETRY_ACTION_MAX_RETRIES: usize = 2;
+
+const RETRY_EXPIRY_CHECK_INTERVAL_INITIAL: Duration = Duration::from_secs(4);
+const RETRY_EXPIRY_CHECK_INTERVAL_FACTOR: u32 = 5;
+const RETRY_EXPIRY_CHECK_INTERVAL_MAX: Duration = Duration::from_secs(24 * 60 * 60);
 
 
 pub struct Account(());
@@ -20,12 +25,42 @@ pub struct Account(());
 pub struct AccountHandle {
     api_availability: ApiAvailabilityHandle,
     initial_check_abort_handle: AbortHandle,
-    pub proxy: AccountsProxy,
+    proxy: AccountsProxy,
 }
 
 impl AccountHandle {
+    pub fn create_account(&self) -> impl Future<Output = Result<AccountToken, rest::Error>> {
+        let mut proxy = self.proxy.clone();
+        retry_future_n(
+            move || proxy.create_account(),
+            Self::should_retry,
+            constant_interval(RETRY_ACTION_INTERVAL),
+            RETRY_ACTION_MAX_RETRIES,
+        )
+    }
+
+    pub fn get_www_auth_token(
+        &self,
+        account: AccountToken,
+    ) -> impl Future<Output = Result<String, rest::Error>> {
+        let proxy = self.proxy.clone();
+        retry_future_n(
+            move || proxy.get_www_auth_token(account.clone()),
+            Self::should_retry,
+            constant_interval(RETRY_ACTION_INTERVAL),
+            RETRY_ACTION_MAX_RETRIES,
+        )
+    }
+
     pub async fn check_expiry(&self, token: AccountToken) -> Result<DateTime<Utc>, rest::Error> {
-        let result = self.proxy.get_expiry(token).await;
+        let proxy = self.proxy.clone();
+        let result = retry_future_n(
+            move || proxy.get_expiry(token.clone()),
+            Self::should_retry,
+            constant_interval(RETRY_ACTION_INTERVAL),
+            RETRY_ACTION_MAX_RETRIES,
+        )
+        .await;
         if handle_expiry_result_inner(&result, &self.api_availability) {
             self.initial_check_abort_handle.abort();
         }
@@ -37,12 +72,31 @@ impl AccountHandle {
         account_token: AccountToken,
         voucher: String,
     ) -> Result<VoucherSubmission, rest::Error> {
-        let result = self.proxy.submit_voucher(account_token, voucher).await;
+        let mut proxy = self.proxy.clone();
+        let result = retry_future_n(
+            move || proxy.submit_voucher(account_token.clone(), voucher.clone()),
+            Self::should_retry,
+            constant_interval(RETRY_ACTION_INTERVAL),
+            RETRY_ACTION_MAX_RETRIES,
+        )
+        .await;
         if result.is_ok() {
             self.initial_check_abort_handle.abort();
             self.api_availability.resume();
         }
         result
+    }
+
+    fn should_retry<T>(result: &Result<T, RestError>) -> bool {
+        match result {
+            Ok(_) => false,
+            Err(RestError::ApiError(_status, code)) => {
+                code != mullvad_rpc::INVALID_ACCOUNT
+                    && code != mullvad_rpc::INVALID_AUTH
+                    && code != mullvad_rpc::VOUCHER_USED
+            }
+            _ => true,
+        }
     }
 }
 
@@ -68,8 +122,11 @@ impl Account {
             };
 
             let retry_strategy = Jittered::jitter(
-                ExponentialBackoff::new(RETRY_INTERVAL_INITIAL, RETRY_INTERVAL_FACTOR)
-                    .max_delay(RETRY_INTERVAL_MAX),
+                ExponentialBackoff::new(
+                    RETRY_EXPIRY_CHECK_INTERVAL_INITIAL,
+                    RETRY_EXPIRY_CHECK_INTERVAL_FACTOR,
+                )
+                .max_delay(RETRY_EXPIRY_CHECK_INTERVAL_MAX),
             );
             let future_generator = move || {
                 let wait_online = api_availability.wait_online();
