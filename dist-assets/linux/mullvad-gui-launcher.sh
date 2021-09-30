c62d7780f6b98745d7a45980b78b407607ee2692
Oskar Nyberg <oskar@mullvad.net>
2021-09-30 11:14:08 +0200

    Disable sandbox on Ubuntu 21.10

--- dist-assets/linux/mullvad-gui-launcher.sh
+++ dist-assets/linux/mullvad-gui-launcher.sh
@@ -5,7 +5,13 @@ UNPRIVILEGED_USERNS_PATH="/proc/sys/kernel/unprivileged_userns_clone"
 if [ -e $UNPRIVILEGED_USERNS_PATH ] && grep -q 0 $UNPRIVILEGED_USERNS_PATH; then
     SANDBOX_FLAG="--no-sandbox"
 else
-    SANDBOX_FLAG=""
+    if command -v lsb_release > /dev/null && \
+        [[ "$(lsb_release -i | awk -F : '{print $2}' | xargs)" == "Ubuntu" ]] && \
+        [[ "$(lsb_release -r | awk -F : '{print $2}' | xargs)" == "21.10" ]]; then
+        SANDBOX_FLAG="--no-sandbox"
+    else
+        SANDBOX_FLAG=""
+    fi
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
