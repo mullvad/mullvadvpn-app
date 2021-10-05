c9ce85e66a4760b67d0dbf5192ca0a9b1ac1a39e
Andrej Mihajlov <and@mullvad.net>
2021-10-05 15:04:28 +0200

    SettingsCell: set layout margins

--- ios/MullvadVPN/UIMetrics.swift
+++ ios/MullvadVPN/UIMetrics.swift
@@ -15,6 +15,9 @@ extension UIMetrics {
     /// Common layout margins for content presentation
     static var contentLayoutMargins = UIEdgeInsets(top: 24, left: 24, bottom: 24, right: 24)
 
+    /// Common layout margins for settings cell presentation
+    static var settingsCellLayoutMargins = UIEdgeInsets(top: 16, left: 24, bottom: 16, right: 12)
+
     /// Common layout margins for location cell presentation
     static var selectLocationCellLayoutMargins = UIEdgeInsets(top: 16, left: 28, bottom: 16, right: 12)
 
