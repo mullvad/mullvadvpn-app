bf67a4eb0b043c57dbacf18ae3eef9533b1149fe
Andrej Mihajlov <and@mullvad.net>
2021-10-08 11:43:27 +0200

    Add CharacterSet extension to validate IP address input

---
+++ ios/MullvadVPN/CharacterSet+IPAddress.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterSet+IPAddress.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 07/10/2021.
+//  Copyright © 2021 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+
+extension CharacterSet {
+    static var ipv4AddressCharset: CharacterSet {
+        return CharacterSet(charactersIn: "0123456789.")
+    }
+
+    static var ipv6AddressCharset: CharacterSet {
+        return CharacterSet(charactersIn: "0123456789abcdef:.")
+    }
+
+}
