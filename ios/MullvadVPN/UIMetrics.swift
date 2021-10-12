f49ef476a8b6af8d7fd4b66f77b6d91ceb0455db
Andrej Mihajlov <and@mullvad.net>
2021-10-12 14:18:06 +0200

    UIMetrics: add cellIndentationWidth and selectLocationCellLayoutMargins

--- ios/MullvadVPN/UIMetrics.swift
+++ ios/MullvadVPN/UIMetrics.swift
@@ -15,6 +15,12 @@ extension UIMetrics {
     /// Common layout margins for content presentation
     static var contentLayoutMargins = UIEdgeInsets(top: 24, left: 24, bottom: 24, right: 24)
 
+    /// Common layout margins for location cell presentation
+    static var selectLocationCellLayoutMargins = UIEdgeInsets(top: 16, left: 28, bottom: 16, right: 12)
+
+    /// Common cell indentation width
+    static var cellIndentationWidth: CGFloat = 16
+
     /// Layout margins for in-app notification banner presentation
     static var inAppBannerNotificationLayoutMargins = UIEdgeInsets(top: 16, left: 24, bottom: 16, right: 24)
 
