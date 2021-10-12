e9a7124ee8f923eebf90c824079b203420f614a8
Andrej Mihajlov <and@mullvad.net>
2021-10-12 15:09:03 +0200

    Update translations

--- ios/MullvadVPN/SettingsDNSTextCell.swift
+++ ios/MullvadVPN/SettingsDNSTextCell.swift
@@ -30,7 +30,7 @@ class SettingsDNSTextCell: SettingsCell, UITextFieldDelegate {
         textField.backgroundColor = .clear
         textField.textColor = UIColor.TextField.textColor
         textField.textMargins = UIMetrics.settingsCellLayoutMargins
-        textField.placeholder = NSLocalizedString("DNS_TEXT_CELL_PLACEHOLDER", tableName: "Preferences", value: "Enter IP", comment: "")
+        textField.placeholder = NSLocalizedString("DNS_TEXT_CELL_PLACEHOLDER", tableName: "Settings", value: "Enter IP", comment: "")
         textField.cornerRadius = 0
         textField.keyboardType = .numbersAndPunctuation
         textField.returnKeyType = .done
