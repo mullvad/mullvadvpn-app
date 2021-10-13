ba988ed29a2d956c805a0ae0d0f1e7ce7254bd50
Andrej Mihajlov <and@mullvad.net>
2021-10-13 12:31:05 +0200

    Preferences: add data source

---
+++ ios/MullvadVPN/PreferencesDataSourceDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  PreferencesDataSourceDelegate.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 11/10/2021.
+//  Copyright © 2021 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+
+protocol PreferencesDataSourceDelegate: AnyObject {
+    func preferencesDataSource(_ dataSource: PreferencesDataSource, didChangeViewModel viewModel: PreferencesViewModel)
+}
