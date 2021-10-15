aebc7642fec738c32b6a54a4ac2caa547f0e6ce5
Oskar Nyberg <oskar@mullvad.net>
2021-10-15 10:46:26 +0200

    Reduce output in extract-geo-data script
    The large amounts of output in the `extract-geo-data.py` script makes it
    difficult to spot real errors. Reducing the amount of output saying
    whether or not a translation was found and which one is being translated
    will make it easier to follow the output and make it easier to spot more
    critical errors.

--- gui/scripts/extract-geo-data.py
+++ gui/scripts/extract-geo-data.py
@@ -160,7 +160,6 @@ def extract_relay_locations_pot(countries):
       comment=country["code"].upper()
     )
     pot.append(entry)
-    print("{} ({})".format(country["name"], country.get("code")))
 
     cities = country["cities"]
     for city in cities:
@@ -175,8 +174,6 @@ def extract_relay_locations_pot(countries):
           pot.append(entry)
         except ValueError as err:
           print(c.orange("Cannot add an entry: {}".format(err)))
-
-        print("{} ({})".format(city["name"], city["code"]))
 
   pot.save(output_path)
 
@@ -255,19 +252,12 @@ def translate_single_relay_locations(country_translator, city_translator, countr
     country_code = country["code"]
 
     translated_country_name = country_translator.translate(locale, country_code)
-    found_country_translation = translated_country_name is not None
     # Default to empty string if no translation was found
-    if found_country_translation:
+    if translated_country_name is not None:
       hits += 1
     else:
       translated_country_name = ""
       misses += 1
-
-    log_message = "{} ({}) -> \"{}\"".format(country_name, country_code, translated_country_name)
-    if found_country_translation:
-      print(c.green(log_message))
-    else:
-      print(c.orange(log_message))
 
     # translate country
     entry = POEntry(
@@ -307,12 +297,6 @@ def translate_single_relay_locations(country_translator, city_translator, countr
       else:
         translated_name = ""
         misses += 1
-
-      log_message = "{} ({}) -> \"{}\"".format(city_name, city_code, translated_name)
-      if found_translation:
-        print(c.green(log_message))
-      else:
-        print(c.orange(log_message))
 
       entry = POEntry(
         msgid=city_name,
@@ -355,10 +339,7 @@ class CountryTranslator:
       name_key = "name_" + map_locale(locale)
       value = props.get(name_key)
 
-      if value is None:
-        print(c.orange("Missing translation for {} ({}) under the {} key"
-          .format(iso_a2, locale, name_key)))
-      else:
+      if value is not None:
         return value
 
     return None
@@ -408,10 +389,7 @@ class CityTranslator:
       name_key = "name_" + map_locale(locale)
       value = props.get(name_key)
 
-      if value is None:
-        print(c.orange("Missing translation for {} ({}) under the {} key"
-          .format(english_name, locale, name_key)))
-      else:
+      if value is not None:
         return value
 
     return None
