610d09960a68e33abad8206e3db34aa14d197269
David Lönnhager <david.l@mullvad.net>
2021-11-02 13:02:49 +0100

    Convert expected REST device errors to specific types

--- mullvad-daemon/src/device.rs
+++ mullvad-daemon/src/device.rs
@@ -6,7 +6,7 @@ use futures::{
     stream::StreamExt,
 };
 use mullvad_rpc::{
-    availability::{self, ApiAvailabilityHandle},
+    availability::ApiAvailabilityHandle,
     rest::{self, Error as RestError, MullvadRestHandle},
     AccountsProxy, DevicesProxy,
 };
@@ -49,20 +49,20 @@ pub struct DeviceKeyEvent(pub DeviceData);
 
 #[derive(err_derive::Error, Debug)]
 pub enum Error {
-    #[error(display = "The account has reached the maximum number of devices")]
-    TooManyDevices,
+    #[error(display = "The account already has a maximum number of devices")]
+    MaxDevicesReached,
     #[error(display = "No device is set")]
     NoDevice,
-    #[error(display = "The login attempt was aborted")]
-    LoginAborted,
-    #[error(display = "Unexpected HTTP request error")]
-    RestError(#[error(source)] rest::Error),
-    #[error(display = "API availability check was interrupted")]
-    ApiCheckError(#[error(source)] availability::Error),
+    #[error(display = "Device not found")]
+    InvalidDevice,
+    #[error(display = "Invalid account")]
+    InvalidAccount,
     #[error(display = "Failed to read or write device cache")]
     DeviceIoError(#[error(source)] io::Error),
     #[error(display = "Failed parse device cache")]
     ParseDeviceCache(#[error(source)] serde_json::Error),
+    #[error(display = "Unexpected HTTP request error")]
+    OtherRestError(#[error(source)] rest::Error),
 }
 
 pub(crate) struct AccountManager {
@@ -352,7 +352,8 @@ impl DeviceService {
             constant_interval(RETRY_ACTION_INTERVAL),
             RETRY_ACTION_MAX_RETRIES,
         )
-        .await?;
+        .await
+        .map_err(map_rest_error)?;
 
         Ok(DeviceData {
             token,
@@ -395,7 +396,8 @@ impl DeviceService {
             should_retry_backoff,
             retry_strategy,
         )
-        .await?;
+        .await
+        .map_err(map_rest_error)?;
 
         Ok(DeviceData {
             token,
@@ -417,7 +419,8 @@ impl DeviceService {
             constant_interval(RETRY_ACTION_INTERVAL),
             RETRY_ACTION_MAX_RETRIES,
         )
-        .await?;
+        .await
+        .map_err(map_rest_error)?;
         Ok(())
     }
 
@@ -448,7 +451,8 @@ impl DeviceService {
             should_retry_backoff,
             retry_strategy,
         )
-        .await?;
+        .await
+        .map_err(map_rest_error)?;
 
         Ok(())
     }
@@ -469,7 +473,8 @@ impl DeviceService {
             constant_interval(RETRY_ACTION_INTERVAL),
             RETRY_ACTION_MAX_RETRIES,
         )
-        .await?;
+        .await
+        .map_err(map_rest_error)?;
 
         Ok(WireguardData {
             private_key,
@@ -508,7 +513,8 @@ impl DeviceService {
             should_retry_backoff,
             retry_strategy,
         )
-        .await?;
+        .await
+        .map_err(map_rest_error)?;
 
         Ok(WireguardData {
             private_key,
@@ -527,7 +533,7 @@ impl DeviceService {
             RETRY_ACTION_MAX_RETRIES,
         )
         .await
-        .map_err(Error::RestError)
+        .map_err(map_rest_error)
     }
 
     pub async fn list_devices_with_backoff(
@@ -557,7 +563,7 @@ impl DeviceService {
             retry_strategy,
         )
         .await
-        .map_err(Error::RestError)
+        .map_err(map_rest_error)
     }
 }
 
@@ -792,5 +798,21 @@ fn should_retry_backoff<T>(result: &Result<T, RestError>) -> bool {
                 true
             }
         }
+    }
+}
+
+fn map_rest_error(error: rest::Error) -> Error {
+    match error {
+        RestError::ApiError(status, ref code) => {
+            if status == rest::StatusCode::NOT_FOUND {
+                return Error::InvalidDevice;
+            }
+            match code.as_str() {
+                mullvad_rpc::INVALID_ACCOUNT => Error::InvalidAccount,
+                mullvad_rpc::MAX_DEVICES_REACHED => Error::MaxDevicesReached,
+                _ => Error::OtherRestError(error),
+            }
+        }
+        error => Error::OtherRestError(error),
     }
 }
