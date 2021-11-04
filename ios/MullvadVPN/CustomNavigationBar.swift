d5028a976f86420949ba8658e9fba204f003c6ac
Andrej Mihajlov <and@mullvad.net>
2021-11-04 13:35:09 +0100

    Add missing navigation bar large title text configuration on iOS 12

--- ios/MullvadVPN/CustomNavigationBar.swift
+++ ios/MullvadVPN/CustomNavigationBar.swift
@@ -69,7 +69,10 @@ class CustomNavigationBar: UINavigationBar {
             backIndicatorImage = customBackIndicatorImage
             backIndicatorTransitionMaskImage = customBackIndicatorTransitionMask
             barTintColor = .secondaryColor
-            titleTextAttributes = [.foregroundColor: UIColor.white]
+
+            let titleAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.white]
+            titleTextAttributes = titleAttributes
+            largeTitleTextAttributes = titleAttributes
             shadowImage = UIImage()
         }
     }
