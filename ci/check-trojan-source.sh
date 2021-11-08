1167729e68ea1a8b6c5a891a7952799281ae7bdc
David Lönnhager <david.l@mullvad.net>
2021-11-08 17:18:27 +0100

    fixup: remove tests

--- ci/check-trojan-source.sh
+++ ci/check-trojan-source.sh
@@ -8,47 +8,19 @@ set -eu
 
 export LC_ALL=en_US.UTF-8
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SCRIPT_RELPATH="./$(basename "${SCRIPT_DIR}")/$(basename "${BASH_SOURCE[0]}")"
-cd "${SCRIPT_DIR}/.." || exit 1
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-# List all non-binary files
 FILES=()
-while IFS='' read -r line; do FILES+=("$line"); done < <( find . -type f -not -path "$SCRIPT_RELPATH" -exec grep -Il . {} + )
+while IFS='' read -r line; do FILES+=("$line"); done < <( find . -type f -exec grep -Il . {} + )
 
 CODEPOINT_REGEX=$( printf "\u202a\|\u202b\|\u202c\|\u202d\|\u202e\|\u2066\|\u2067\|\u2068\|\u2069" )
-
-function trojan_source_scan() {
-    grep -q "${CODEPOINT_REGEX}"
-}
-
-################################################################################
-# Sanity check.
-################################################################################
-
-UNSAFE_STR="nonsense ‪"
-SAFE_STR="nonsense x"
-
-if ! trojan_source_scan <<< "${UNSAFE_STR}"; then
-    echo "Failed to detect code point in test string"
-    exit 1
-fi
-
-if trojan_source_scan <<< "${SAFE_STR}"; then
-    echo "Incorrectly detected code point in test string"
-    exit 1
-fi
-
-################################################################################
-# Scan all files for the malicious code points.
-################################################################################
 
 matched=0
 
 echo "Scanning files: ${FILES[*]}"
 
 for file in "${FILES[@]}"; do
-    if trojan_source_scan < "$file"; then
+    if grep -q "${CODEPOINT_REGEX}" "$file"; then
         echo "Found code points in $file"
         matched=1
     fi
