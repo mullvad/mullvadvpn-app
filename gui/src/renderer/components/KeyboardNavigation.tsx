ca2822b0d33f63d4839fd3d0bc50b85183125556
Oskar Nyberg <oskar@mullvad.net>
2021-11-12 15:30:44 +0100

    Disable dismiss keyboard shortcut for some views

--- gui/src/renderer/components/KeyboardNavigation.tsx
+++ gui/src/renderer/components/KeyboardNavigation.tsx
@@ -1,5 +1,7 @@
 import React, { useCallback, useEffect } from 'react';
+import { useLocation } from 'react-router';
 import { useHistory } from '../lib/history';
+import { disableDismissForRoutes, RoutePath } from '../lib/routes';
 
 interface IKeyboardNavigationProps {
   children: React.ReactElement;
@@ -7,14 +9,18 @@ interface IKeyboardNavigationProps {
 
 export default function KeyboardNavigation(props: IKeyboardNavigationProps) {
   const history = useHistory();
+  const location = useLocation();
 
   const handleKeyDown = useCallback(
     (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
-        history.dismiss(true);
+        const path = location.pathname as RoutePath;
+        if (!disableDismissForRoutes.includes(path)) {
+          history.dismiss(true);
+        }
       }
     },
-    [history.reset],
+    [history.reset, location.pathname],
   );
 
   useEffect(() => {
