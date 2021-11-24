3ee64e43c477481ebc6351f0217cf61d58bb4cf5
Andrej Mihajlov <and@mullvad.net>
2021-11-24 14:14:42 +0100

    UIMetrics: change var to let

--- ios/MullvadVPN/UIMetrics.swift
+++ ios/MullvadVPN/UIMetrics.swift
@@ -13,25 +13,25 @@ enum UIMetrics {}
 extension UIMetrics {
 
     /// Common layout margins for content presentation
-    static var contentLayoutMargins = UIEdgeInsets(top: 24, left: 24, bottom: 24, right: 24)
+    static let contentLayoutMargins = UIEdgeInsets(top: 24, left: 24, bottom: 24, right: 24)
 
     /// Common layout margins for settings cell presentation
-    static var settingsCellLayoutMargins = UIEdgeInsets(top: 16, left: 24, bottom: 16, right: 12)
+    static let settingsCellLayoutMargins = UIEdgeInsets(top: 16, left: 24, bottom: 16, right: 12)
 
     /// Common layout margins for location cell presentation
-    static var selectLocationCellLayoutMargins = UIEdgeInsets(top: 16, left: 28, bottom: 16, right: 12)
+    static let selectLocationCellLayoutMargins = UIEdgeInsets(top: 16, left: 28, bottom: 16, right: 12)
 
     /// Common cell indentation width
-    static var cellIndentationWidth: CGFloat = 16
+    static let cellIndentationWidth: CGFloat = 16
 
     /// Layout margins for in-app notification banner presentation
-    static var inAppBannerNotificationLayoutMargins = UIEdgeInsets(top: 16, left: 24, bottom: 16, right: 24)
+    static let inAppBannerNotificationLayoutMargins = UIEdgeInsets(top: 16, left: 24, bottom: 16, right: 24)
 
     /// Spacing used in stack views of buttons
-    static var interButtonSpacing: CGFloat = 16
+    static let interButtonSpacing: CGFloat = 16
 
     /// Spacing used between distinct sections of views
-    static var sectionSpacing: CGFloat = 24
+    static let sectionSpacing: CGFloat = 24
 
     /// Text field margins
     static let textFieldMargins = UIEdgeInsets(top: 12, left: 14, bottom: 12, right: 14)
@@ -40,12 +40,12 @@ extension UIMetrics {
     static let controlCornerRadius: CGFloat = 4
 
     /// Maximum width of the split view content container on iPad
-    static var maximumSplitViewContentContainerWidth: CGFloat = 810 * 0.7
+    static let maximumSplitViewContentContainerWidth: CGFloat = 810 * 0.7
 
     /// Minimum sidebar width in points
-    static var minimumSplitViewSidebarWidth: CGFloat = 300
+    static let minimumSplitViewSidebarWidth: CGFloat = 300
 
     /// Maximum sidebar width in percentage points (0...1)
-    static var maximumSplitViewSidebarWidthFraction: CGFloat = 0.3
+    static let maximumSplitViewSidebarWidthFraction: CGFloat = 0.3
 
 }
