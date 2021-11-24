4cb64e950dd164706421fb484766b3f928c52cb4
Albin <albin@mullvad.net>
2021-11-24 13:58:47 +0100

    Improve tile service lifecycle

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/model/ServiceResult.kt
@@ -0,0 +1,25 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.IBinder
+
+data class ServiceResult(
+    val binder: IBinder?
+) {
+    enum class ConnectionState {
+        CONNECTED,
+        DISCONNECTED;
+    }
+
+    val connectionState: ConnectionState
+        get() {
+            return if (binder == null) {
+                ConnectionState.DISCONNECTED
+            } else {
+                ConnectionState.CONNECTED
+            }
+        }
+
+    companion object {
+        val NOT_CONNECTED = ServiceResult(null)
+    }
+}
