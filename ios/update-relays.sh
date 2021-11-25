dc87b897f294ed83d4dbb2bf741ac628603ba311
Andrej Mihajlov <and@mullvad.net>
2021-11-25 12:25:40 +0100

    Bundle api IP addresses with the app

--- ios/update-relays.sh
+++ ios/update-relays.sh
@@ -5,7 +5,10 @@ if [ -z "$PROJECT_DIR" ]; then
   exit 1
 fi
 
-RELAYS_FILE="$PROJECT_DIR/Assets/relays.json"
+ASSETS_DIR_PATH="$PROJECT_DIR/Assets"
+
+RELAYS_FILE="$ASSETS_DIR_PATH/relays.json"
+API_IP_ADDRESS_LIST_FILE="$ASSETS_DIR_PATH/api-ip-address.json"
 
 if [ $CONFIGURATION == "Release" ]; then
   echo "Remove relays file"
@@ -14,9 +17,21 @@ if [ $CONFIGURATION == "Release" ]; then
   else
     echo "Relays file does not exist"
   fi
+
+  echo "Remove API address list file"
+  if [ -f "$API_IP_ADDRESS_LIST_FILE" ]; then
+    rm "$API_IP_ADDRESS_LIST_FILE"
+  else
+    echo "API IP address list file does not exist"
+  fi
 fi
 
 if [ ! -f "$RELAYS_FILE" ]; then
   echo "Download relays file"
   curl https://api.mullvad.net/app/v1/relays -s -o "$RELAYS_FILE"
+fi
+
+if [ ! -f "$API_IP_ADDRESS_LIST_FILE" ]; then
+  echo "Download API address list file"
+  curl https://api.mullvad.net/app/v1/api-addrs -s -o "$API_IP_ADDRESS_LIST_FILE"
 fi
