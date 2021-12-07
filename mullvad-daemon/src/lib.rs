c018bc7c9fec329632fb4ee2309f10d6726e7d6e
David Lönnhager <david.l@mullvad.net>
2021-12-07 13:42:14 +0100

    Add 'remote' boolean to device event

--- mullvad-daemon/src/lib.rs
+++ mullvad-daemon/src/lib.rs
@@ -1196,11 +1196,13 @@ where
                             match status {
                                 device::ValidationResult::Valid => (),
                                 device::ValidationResult::Removed => {
-                                    self.event_listener.notify_device_event(DeviceEvent(None));
+                                    self.event_listener
+                                        .notify_device_event(DeviceEvent::revoke(true));
                                 }
                                 device::ValidationResult::RotatedKey(_) => {
-                                    self.event_listener.notify_device_event(DeviceEvent::from(
+                                    self.event_listener.notify_device_event(DeviceEvent::new(
                                         self.account_manager.get(),
+                                        true,
                                     ));
                                 }
                             }
@@ -1389,7 +1391,7 @@ where
             self.schedule_reconnect(WG_RECONNECT_DELAY).await;
         }
         self.event_listener
-            .notify_device_event(DeviceEvent::from(event.0));
+            .notify_device_event(DeviceEvent::from_device(event.0, false));
     }
 
     async fn handle_device_migration_event(&mut self, data: DeviceData) {
@@ -1397,7 +1399,7 @@ where
             // Discard stale device
             return;
         }
-        let event = DeviceEvent::from(data.clone());
+        let event = DeviceEvent::from_device(data.clone(), false);
         self.account_manager.set(data);
         self.reconnect_tunnel();
         self.event_listener.notify_device_event(event);
@@ -1667,11 +1669,12 @@ where
                     .await
                     .map_err(Error::LoginError)?;
                 self.event_listener
-                    .notify_device_event(DeviceEvent::from(device_data));
+                    .notify_device_event(DeviceEvent::from_device(device_data, false));
             }
             None => {
                 self.account_manager.logout();
-                self.event_listener.notify_device_event(DeviceEvent(None));
+                self.event_listener
+                    .notify_device_event(DeviceEvent::revoke(false));
             }
         }
 
@@ -2347,7 +2350,7 @@ where
     async fn on_rotate_wireguard_key(&mut self, tx: ResponseTx<(), Error>) {
         let result = self.account_manager.rotate_key().await;
         if let Ok(ref _wg_data) = result {
-            let device = DeviceEvent::from(self.account_manager.get());
+            let device = DeviceEvent::new(self.account_manager.get(), false);
             self.event_listener.notify_device_event(device);
         }
         let _ = tx.send(result.map(|_| ()).map_err(Error::KeyRotationError));
