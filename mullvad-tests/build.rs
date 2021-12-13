b150de5b501b368e11dcb8cdc6aa26761352c538
Emīls Piņķis <emils@mullvad.net>
2021-12-13 14:05:07 +0000

    Remove mullvad-tests

--- mullvad-tests/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    const OPENVPN_PROTO_FILE: &str = "../talpid-openvpn-plugin/proto/openvpn_plugin.proto";
-    tonic_build::compile_protos(OPENVPN_PROTO_FILE).unwrap();
-    println!("cargo:rerun-if-changed={}", OPENVPN_PROTO_FILE);
-}
