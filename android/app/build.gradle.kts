06b9617f924ae2d00be2e3fcf534f29ae81376e1
Albin <albin@mullvad.net>
2022-01-27 16:11:32 +0100

    Updating version in package files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "net.mullvad.mullvadvpn"
         minSdkVersion(Versions.Android.minSdkVersion)
         targetSdkVersion(Versions.Android.targetSdkVersion)
-        versionCode = 22010001
-        versionName = "2022.1-beta1"
+        versionCode = 22010002
+        versionName = "2022.1-beta2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
