bb549db6c6e203e1d76abb7f858e04490822ad65
Albin <albin@mullvad.net>
2022-03-07 11:02:40 +0100

    Add basic Android e2e login tests

--- android/e2e/src/main/java/net/mullvad/mullvadvpn/e2e/constant/TimeoutConstants.kt
+++ android/e2e/src/main/java/net/mullvad/mullvadvpn/e2e/constant/TimeoutConstants.kt
@@ -2,3 +2,5 @@ package net.mullvad.mullvadvpn.e2e.constant
 
 const val APP_LAUNCH_TIMEOUT = 5000L
 const val DEFAULT_INTERACTION_TIMEOUT = 3000L
+const val LOGIN_TIMEOUT = 30000L
+const val LOGIN_FAILURE_TIMEOUT = 60000L
