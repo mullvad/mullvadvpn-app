85f965bee056df1733eb3f5703fa23ca49c11f37
Albin <albin@mullvad.net>
2022-03-07 10:43:01 +0100

    Add Android e2e screenshot rule
    This rule will capture screenshot of any failed test and download it to
    the host executing the test.

--- android/e2e/src/main/java/net/mullvad/mullvadvpn/e2e/EndToEndTest.kt
+++ android/e2e/src/main/java/net/mullvad/mullvadvpn/e2e/EndToEndTest.kt
@@ -5,11 +5,17 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
 import androidx.test.uiautomator.UiDevice
 import net.mullvad.mullvadvpn.e2e.interactor.AppInteractor
+import net.mullvad.mullvadvpn.e2e.misc.CaptureScreenshotOnFailedTestRule
 import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 abstract class EndToEndTest {
+
+    @Rule
+    @JvmField
+    val rule = CaptureScreenshotOnFailedTestRule()
 
     lateinit var device: UiDevice
     lateinit var targetContext: Context
