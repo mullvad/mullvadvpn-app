53dd69c58f866776bf5ea4ecdedb93ccd28fb597
Albin <albin@mullvad.net>
2022-03-07 13:26:03 +0100

    Add Android e2e connection test

--- android/e2e/src/main/java/net/mullvad/mullvadvpn/e2e/interactor/AppInteractor.kt
+++ android/e2e/src/main/java/net/mullvad/mullvadvpn/e2e/interactor/AppInteractor.kt
@@ -10,6 +10,8 @@ import net.mullvad.mullvadvpn.e2e.constant.APP_LAUNCH_TIMEOUT
 import net.mullvad.mullvadvpn.e2e.constant.LOGIN_TIMEOUT
 import net.mullvad.mullvadvpn.e2e.constant.MULLVAD_PACKAGE
 import net.mullvad.mullvadvpn.e2e.constant.SETTINGS_COG_ID
+import net.mullvad.mullvadvpn.e2e.constant.TUNNEL_INFO_ID
+import net.mullvad.mullvadvpn.e2e.constant.TUNNEL_OUT_ADDRESS_ID
 import net.mullvad.mullvadvpn.e2e.extension.findObjectWithTimeout
 
 class AppInteractor(
@@ -37,6 +39,12 @@ class AppInteractor(
         )
     }
 
+    fun launchAndEnsureLoggedIn(accountToken: String = validTestAccountToken) {
+        launch()
+        attemptLogin(accountToken)
+        ensureLoggedIn()
+    }
+
     fun attemptLogin(accountToken: String = validTestAccountToken) {
         device.findObjectWithTimeout(By.text("Login"))
         val loginObject = device.findObjectWithTimeout(By.clazz("android.widget.EditText"))
@@ -48,10 +56,9 @@ class AppInteractor(
         device.findObjectWithTimeout(By.text("UNSECURED CONNECTION"), LOGIN_TIMEOUT)
     }
 
-    fun launchAndEnsureLoggedIn(accountToken: String = validTestAccountToken) {
-        launch()
-        attemptLogin(accountToken)
-        ensureLoggedIn()
+    fun extractIpAddress(): String {
+        device.findObjectWithTimeout(By.res(TUNNEL_INFO_ID)).click()
+        return device.findObjectWithTimeout(By.res(TUNNEL_OUT_ADDRESS_ID)).text.extractIpAddress()
     }
 
     fun clickSettingsCog() {
@@ -64,5 +71,9 @@ class AppInteractor(
 
     fun clickActionButtonByText(text: String) {
         device.findObjectWithTimeout(By.text(text)).click()
+    }
+
+    private fun String.extractIpAddress(): String {
+        return split("  ")[1].split(" ")[0]
     }
 }
