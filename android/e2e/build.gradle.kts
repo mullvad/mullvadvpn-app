5d4e97b17d447e1ca9acd68fff5dd69fddd14b0f
Albin <albin@mullvad.net>
2022-03-11 11:47:14 +0100

    Exclude e2e lintClassPath from dependency check

--- android/e2e/build.gradle.kts
+++ android/e2e/build.gradle.kts
@@ -94,6 +94,14 @@ tasks.whenTaskAdded {
     }
 }
 
+configure<org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension> {
+    // Skip the lintClassPath configuration, which relies on many dependencies that has been flagged
+    // to have CVEs, as it's related to the lint tooling rather than the project's compilation class
+    // path. The alternative would be to suppress specific CVEs, however that could potentially
+    // result in suppressed CVEs in project compilation class path.
+    skipConfigurations = listOf("lintClassPath")
+}
+
 dependencies {
     implementation(Dependencies.AndroidX.testCore)
     implementation(Dependencies.AndroidX.testOrchestrator)
