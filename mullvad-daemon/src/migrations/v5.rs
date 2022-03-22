d91e14f3a53dc26c0ba93b1c9cd75eaf3928d875
David Lönnhager <david.l@mullvad.net>
2022-03-22 09:50:22 +0100

    Move imports out of v5 module

--- mullvad-daemon/src/migrations/v5.rs
+++ mullvad-daemon/src/migrations/v5.rs
@@ -1,16 +1,18 @@
 use super::{Error, Result};
-use crate::{device::DeviceService, DaemonEventSender, InternalDaemonEvent};
-use mullvad_types::{
-    account::AccountToken, device::DeviceData, settings::SettingsVersion, wireguard::WireguardData,
-};
-use talpid_core::mpsc::Sender;
-use talpid_types::ErrorExt;
+use mullvad_types::settings::SettingsVersion;
 
 // ======================================================
 // Section for vendoring types and values that
 // this settings version depend on. See `mod.rs`.
 
+pub type AccountToken = String;
+
 // ======================================================
+
+pub(crate) struct MigrationData {
+    pub token: AccountToken,
+    pub wg_data: Option<serde_json::Value>,
+}
 
 /// This is an open ended migration. There is no v6 yet!
 /// The migrations performed by this function are still backwards compatible.
@@ -29,42 +31,8 @@ use talpid_types::ErrorExt;
 /// It is also no longer valid to have `entry_location` set to null. So remove the field if it
 /// is null in order to make it default back to the default location.
 ///
-/// This also removes the account token and WireGuard key from the settings, looks up the
-/// corresponding device, and eventually stores them in `device.json` instead. This is done by
-/// sending the `DeviceMigrationEvent` event to the daemon. Because this is fallible, it can
-/// result in the account token and private key being lost. This should not be not critical since
-/// the account token is also stored in the account history.
-pub(crate) async fn migrate(
-    settings: &mut serde_json::Value,
-    rest_handle: mullvad_api::rest::MullvadRestHandle,
-    daemon_tx: DaemonEventSender,
-) -> Result<()> {
-    let migration_data = migrate_inner(settings).await?;
-
-    if let Some(migration_data) = migration_data {
-        let api_handle = rest_handle.availability.clone();
-        let service = DeviceService::new(rest_handle, api_handle);
-        match (migration_data.token, migration_data.wg_data) {
-            (token, Some(wg_data)) => {
-                log::info!("Creating a new device cache from previous settings");
-                tokio::spawn(cache_from_wireguard_key(daemon_tx, service, token, wg_data));
-            }
-            (token, None) => {
-                log::info!("Generating a new device for the account");
-                tokio::spawn(cache_from_account(daemon_tx, service, token));
-            }
-        }
-    }
-
-    Ok(())
-}
-
-struct MigrationData {
-    token: AccountToken,
-    wg_data: Option<WireguardData>,
-}
-
-async fn migrate_inner(settings: &mut serde_json::Value) -> Result<Option<MigrationData>> {
+/// This also removes the account token and WireGuard key from the settings.
+pub(crate) async fn migrate(settings: &mut serde_json::Value) -> Result<Option<MigrationData>> {
     if !version_matches(settings) {
         return Ok(None);
     }
@@ -94,25 +62,24 @@ async fn migrate_inner(settings: &mut serde_json::Value) -> Result<Option<Migrat
     if let Some(token) = settings.get("account_token").filter(|t| !t.is_null()) {
         let token: AccountToken =
             serde_json::from_value(token.clone()).map_err(Error::ParseError)?;
-        let mig_data = if let Some(wg_data) = settings.get("wireguard").filter(|wg| !wg.is_null()) {
-            let wg_data: WireguardData =
-                serde_json::from_value(wg_data.clone()).map_err(Error::ParseError)?;
-            Ok(Some(MigrationData {
-                token,
-                wg_data: Some(wg_data),
-            }))
-        } else {
-            Ok(Some(MigrationData {
-                token,
-                wg_data: None,
-            }))
-        };
+        let migration_data =
+            if let Some(wg_data) = settings.get("wireguard").filter(|wg| !wg.is_null()) {
+                Ok(Some(MigrationData {
+                    token,
+                    wg_data: Some(wg_data.clone()),
+                }))
+            } else {
+                Ok(Some(MigrationData {
+                    token,
+                    wg_data: None,
+                }))
+            };
 
         let settings_map = settings.as_object_mut().ok_or(Error::NoMatchingVersion)?;
         settings_map.remove("account_token");
         settings_map.remove("wireguard");
 
-        return mig_data;
+        return migration_data;
     }
 
     // Note: Not incrementing the version number yet, since this migration is still open
@@ -129,56 +96,9 @@ fn version_matches(settings: &mut serde_json::Value) -> bool {
         .unwrap_or(false)
 }
 
-async fn cache_from_wireguard_key(
-    daemon_tx: DaemonEventSender,
-    service: DeviceService,
-    token: AccountToken,
-    wg_data: WireguardData,
-) {
-    let devices = match service.list_devices_with_backoff(token.clone()).await {
-        Ok(devices) => devices,
-        Err(error) => {
-            log::error!(
-                "{}",
-                error.display_chain_with_msg("Failed to enumerate devices for account")
-            );
-            return;
-        }
-    };
-
-    for device in devices.into_iter() {
-        if device.pubkey == wg_data.private_key.public_key() {
-            let _ = daemon_tx.send(InternalDaemonEvent::DeviceMigrationEvent(DeviceData {
-                token,
-                device,
-                wg_data,
-            }));
-            return;
-        }
-    }
-    log::info!("The existing WireGuard key is not valid; generating a new device");
-    cache_from_account(daemon_tx, service, token).await;
-}
-
-async fn cache_from_account(
-    daemon_tx: DaemonEventSender,
-    service: DeviceService,
-    token: AccountToken,
-) {
-    match service.generate_for_account_with_backoff(token).await {
-        Ok(device_data) => {
-            let _ = daemon_tx.send(InternalDaemonEvent::DeviceMigrationEvent(device_data));
-        }
-        Err(error) => log::error!(
-            "{}",
-            error.display_chain_with_msg("Failed to generate new device for account")
-        ),
-    }
-}
-
 #[cfg(test)]
 mod test {
-    use super::{migrate_inner, version_matches};
+    use super::{migrate, version_matches};
     use serde_json;
 
     pub const V5_SETTINGS_V1: &str = r#"
@@ -324,7 +244,7 @@ mod test {
         let mut old_settings = serde_json::from_str(V5_SETTINGS_V1).unwrap();
 
         assert!(version_matches(&mut old_settings));
-        migrate_inner(&mut old_settings).await.unwrap();
+        migrate(&mut old_settings).await.unwrap();
         let new_settings: serde_json::Value = serde_json::from_str(V5_SETTINGS_V2).unwrap();
 
         assert_eq!(&old_settings, &new_settings);
