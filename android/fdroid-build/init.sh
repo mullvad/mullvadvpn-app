f0f5a86d390b5f8e2cc69426a1e8c6b0d0e8da59
Albin <albin@mullvad.net>
2022-04-29 07:32:07 +0200

    Unify golang download urls

--- android/fdroid-build/init.sh
+++ android/fdroid-build/init.sh
@@ -21,7 +21,7 @@ rustup target add \
 
 # Install Go
 cd "$HOME"
-curl -sf -L -O https://golang.org/dl/go1.16.linux-amd64.tar.gz
+curl -sf -L -O https://go.dev/dl/go1.16.linux-amd64.tar.gz
 echo "013a489ebb3e24ef3d915abe5b94c3286c070dfe0818d5bca8108f1d6e8440d2 go1.16.linux-amd64.tar.gz" | sha256sum -c
 tar -xzvf go1.16.linux-amd64.tar.gz
 patch -p1 -f -N -r- -d "$HOME/go" < "$REPO_DIR/wireguard/libwg/goruntime-boottime-over-monotonic.diff"
