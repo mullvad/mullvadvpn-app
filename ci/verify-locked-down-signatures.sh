a6b73f3c3bf616b8ddfe075dd8ee197f2de17d58
Jonathan <jonathan@mullvad.net>
2022-05-11 10:33:11 +0200

    Create verification script and github action
    Create a script which verifies that a set of "locked down" files are not
    changed in commits that have not been signed.
    
    Create a github workflow that runs the script in the CI.
    
    The script accepts --whitelist <commit> and --import-gpg-keys arguments.
    The default settings are supposed to work on the build server without
    importing the gpg keys from the trusted_keys.pub file and running with a
    hardcoded whitelist commit.
    Make the CI workflow use these arguments as it is supposed to in
    .github.
    
    The public keys that can be imported are specified as files in the ci/keys/
    directory.
    
    The files that are locked down are specified in the .github workflow as
    a single source of truth. This requires some complicated parsing in the
    verification script as well as a dependency from the verification script
    to the workflow YAML. These are not ideal design choices however the
    alternative is to not have a single source of truth for the locked down
    files as the github workflow can not depend on an external file.
    
    The mullvad signing key is named to be first in the list in order to be
    imported first.
    
    The whitelisted commit is the latest master before this commit

---
+++ ci/verify-locked-down-signatures.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+set -eu
+shopt -s nullglob
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly SCRIPT_DIR
+
+# In the CI environment we would like to import trusted public keys from a file,
+# but not in our build environment
+import_gpg_keys="false"
+
+# The policy of enforcing lockfiles to be signed was not in place before this commit and
+# as such some of the commits before are not signed
+# The whitelisted commit can be set in order to allow github actions to only check changes
+# since origin/master
+whitelisted_commit="618130520"
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        "--import-gpg-keys")
+            import_gpg_keys="true"
+            ;;
+        "--whitelist")
+            whitelisted_commit="$2"
+            shift
+            ;;
+        *)
+            echo "Unknown argument $1
+The options are --import-gpg-keys and --whitelist"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [[ "$import_gpg_keys" == "true" ]]; then
+    GNUPGHOME=$(mktemp -d)
+    export GNUPGHOME
+    for key in "$SCRIPT_DIR"/keys/*; do
+        gpg --import "$key"
+    done
+fi
+
+# Parse the locked down files from the github actions workflow file.
+# We need to define them there since github has no way to trigger on filepaths specified in a file.
+# We parse them from there in order to avoid duplicating the locked down files in multiple places.
+#
+# This regexp line is using a regexp to parse the github .yml file for the YAML list
+# that follows the `paths` key.
+# It uses `tr` in order to turn the multi-lined file into a single-line that sed can parse
+# correctly. This is done by replacing all new-lines with a `;`
+readonly SEPARATOR=';'
+locked_down_paths=$(\
+    < "$SCRIPT_DIR/../.github/workflows/verify-locked-down-signatures.yml" tr '\n' $SEPARATOR \
+    | sed "s/.*paths:$SEPARATOR\(\(\s*-\s[a-zA-Z\/\.-]*$SEPARATOR\)*\).*/\1/" \
+    | tr $SEPARATOR '\n' \
+    | awk '{print $2}')
+
+unsigned_commits_exist=0
+for locked_path in $locked_down_paths; do
+    locked_path_commit_hashes=$(git rev-list --oneline "$whitelisted_commit"..HEAD \
+        "$SCRIPT_DIR/../$locked_path" | awk '{print $1}')
+
+    for commit in $locked_path_commit_hashes; do
+        if ! git verify-commit "$commit" 2> /dev/null; then
+            echo "Commit $commit which changed $locked_path is not signed."
+            unsigned_commits_exist=1
+        fi
+    done
+done
+
+if [[ $unsigned_commits_exist == 0 ]]; then
+    echo "SUCCESS: Could not find any unsigned commits which modified a locked down path"
+fi
+
+exit $unsigned_commits_exist
