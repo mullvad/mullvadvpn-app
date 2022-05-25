fe638f5533a56038f7186e38923e701403e8b3c9
David Lönnhager <david.l@mullvad.net>
2022-05-25 08:55:26 +0200

    Sort devices chronologically in the "too many devices" view

--- gui/src/main/daemon-rpc.ts
+++ gui/src/main/daemon-rpc.ts
@@ -1443,11 +1443,13 @@ function convertFromDeviceRemoval(deviceRemoval: grpcTypes.RemoveDeviceEvent): A
 }
 
 function convertFromDevice(device: grpcTypes.Device): IDevice {
+  const created = ensureExists(device.getCreated(), "no 'created' field for device").toDate();
   const asObject = device.toObject();
 
   return {
     ...asObject,
     ports: asObject.portsList.map((port) => port.id),
+    created: created,
   };
 }
 
