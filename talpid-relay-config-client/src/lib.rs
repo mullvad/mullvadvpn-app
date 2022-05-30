2c1b2faa744d6222d08506bfe329faab41db4b72
David Lönnhager <david.l@mullvad.net>
2022-05-30 17:46:56 +0200

    Switch from liboqs to classic-mceliece-rust

--- talpid-relay-config-client/src/lib.rs
+++ talpid-relay-config-client/src/lib.rs
@@ -1,6 +1,10 @@
 use std::net::IpAddr;
 
-use oqs::kem::{self, Algorithm, Kem, SecretKey};
+use classic_mceliece_rust::{
+    crypto_kem_dec, crypto_kem_keypair, AesState, RNGState, CRYPTO_BYTES, CRYPTO_CIPHERTEXTBYTES,
+    CRYPTO_PUBLICKEYBYTES, CRYPTO_SECRETKEYBYTES,
+};
+use rand::RngCore;
 use talpid_types::net::wireguard::{PresharedKey, PrivateKey, PublicKey};
 use tonic::transport::{Channel, Endpoint, Uri};
 
@@ -11,14 +15,15 @@ mod types {
 type RelayConfigService = types::post_quantum_secure_client::PostQuantumSecureClient<Channel>;
 
 const CONFIG_SERVICE_PORT: u16 = 1337;
-const ALGORITHM: Algorithm = Algorithm::ClassicMcEliece8192128f;
 const STACK_SIZE: usize = 8 * 1024 * 1024;
+const ALGORITHM_NAME: &str = "Classic-McEliece-8192128f";
 
 #[derive(Debug)]
 pub enum Error {
     GrpcTransportError(tonic::transport::Error),
     GrpcError(tonic::Status),
-    OqsError(oqs::Error),
+    KeyGenerationFailed,
+    DecapsulationError,
     InvalidCiphertext,
 }
 
@@ -37,31 +42,49 @@ pub async fn push_pq_key(
             wg_pubkey: current_pubkey.as_bytes().to_vec(),
             wg_psk_pubkey: oqs_key.public_key().as_bytes().to_vec(),
             oqs_pubkey: Some(types::OqsPubkey {
-                algorithm_name: algorithm_to_string(&ALGORITHM),
-                key_data: pubkey.into_vec(),
+                algorithm_name: ALGORITHM_NAME.to_string(),
+                key_data: pubkey.to_vec(),
             }),
         })
         .await
         .map_err(Error::GrpcError)?;
 
     let ciphertext = response.into_inner().ciphertext;
-    let kem = Kem::new(ALGORITHM).map_err(Error::OqsError)?;
-    let ciphertext = kem
-        .ciphertext_from_bytes(&ciphertext)
-        .ok_or(Error::InvalidCiphertext)?;
-    let psk = kem
-        .decapsulate(&secret, ciphertext)
-        .map(|key| PresharedKey::from(<[u8; 32]>::try_from(key.as_ref()).unwrap()))
-        .map_err(Error::OqsError)?;
-    Ok((oqs_key, psk))
+    let ct: [u8; CRYPTO_CIPHERTEXTBYTES] = ciphertext
+        .try_into()
+        .map_err(|_| Error::InvalidCiphertext)?;
+    let mut psk = [0u8; CRYPTO_BYTES];
+
+    crypto_kem_dec(&mut psk, &ct, &secret).map_err(|error| {
+        log::error!("KEM decapsulation failed: {error}");
+        Error::DecapsulationError
+    })?;
+    Ok((oqs_key, PresharedKey::from(psk)))
 }
 
-async fn generate_key() -> Result<(kem::PublicKey, SecretKey), Error> {
+async fn generate_key() -> Result<
+    (
+        Box<[u8; CRYPTO_PUBLICKEYBYTES]>,
+        Box<[u8; CRYPTO_SECRETKEYBYTES]>,
+    ),
+    Error,
+> {
     let (tx, rx) = tokio::sync::oneshot::channel();
 
     let gen_key = move || {
-        let kem = Kem::new(ALGORITHM).map_err(Error::OqsError)?;
-        let (pubkey, secret) = kem.keypair().map_err(Error::OqsError)?;
+        let mut rng = AesState::new();
+
+        let mut entropy = [0u8; 48];
+        rand::thread_rng().fill_bytes(&mut entropy);
+        rng.randombytes_init(entropy);
+
+        let mut pubkey = Box::new([0u8; CRYPTO_PUBLICKEYBYTES]);
+        let mut secret = Box::new([0u8; CRYPTO_SECRETKEYBYTES]);
+        crypto_kem_keypair(&mut pubkey, &mut secret, &mut rng).map_err(|error| {
+            log::error!("KEM keypair generation failed: {error}");
+            Error::KeyGenerationFailed
+        })?;
+
         Ok((pubkey, secret))
     };
 
@@ -73,13 +96,6 @@ async fn generate_key() -> Result<(kem::PublicKey, SecretKey), Error> {
         .unwrap();
 
     rx.await.unwrap()
-}
-
-fn algorithm_to_string(algorithm: &Algorithm) -> String {
-    match algorithm {
-        Algorithm::ClassicMcEliece8192128f => "Classic-McEliece-8192128f".to_string(),
-        _ => unimplemented!(),
-    }
 }
 
 async fn new_client(addr: IpAddr) -> Result<RelayConfigService, Error> {
