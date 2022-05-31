519d35e599a93467b7c106442ddc9caffb6563f9
Albin <albin@mullvad.net>
2022-05-31 16:58:23 +0200

    Add initial compose components

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/Theme.kt
@@ -0,0 +1,13 @@
+package net.mullvad.mullvadvpn.compose.component
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
