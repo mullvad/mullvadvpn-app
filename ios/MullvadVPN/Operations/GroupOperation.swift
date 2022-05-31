fa2c0c5788a1a3991ed6f66893ac0044db0530bf
Andrej Mihajlov <and@mullvad.net>
2022-05-31 14:11:12 +0200

    Add GroupOperation

---
+++ ios/MullvadVPN/Operations/GroupOperation.swift
@@ -0,0 +1,34 @@
+//
+//  GroupOperation.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 31/05/2022.
+//  Copyright © 2022 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+
+class GroupOperation: AsyncOperation {
+    private let operationQueue = AsyncOperationQueue()
+    private let children: [Operation]
+
+    init(operations: [Operation]) {
+        children = operations
+
+        super.init(dispatchQueue: nil)
+    }
+
+    override func main() {
+        let finishingOperation = BlockOperation()
+        finishingOperation.completionBlock = { [weak self] in
+            self?.finish()
+        }
+
+        operationQueue.addOperations(children, waitUntilFinished: false)
+        operationQueue.addOperation(finishingOperation)
+    }
+
+    override func operationDidCancel() {
+        operationQueue.cancelAllOperations()
+    }
+}
