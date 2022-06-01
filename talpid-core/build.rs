1934718e6e696dc3ae7e524980726a5e62934312
David Lönnhager <david.l@mullvad.net>
2022-06-01 21:52:13 +0200

    Remove WinDns module

--- talpid-core/build.rs
+++ talpid-core/build.rs
@@ -6,7 +6,6 @@ mod win {
     use std::{env, path::PathBuf};
 
     pub static WINFW_BUILD_DIR: &'static str = "..\\windows\\winfw\\bin";
-    pub static WINDNS_BUILD_DIR: &'static str = "..\\windows\\windns\\bin";
     pub static WINNET_BUILD_DIR: &'static str = "..\\windows\\winnet\\bin";
 
     pub fn default_windows_build_artifact_dir(build_dir: &str) -> PathBuf {
@@ -50,10 +49,8 @@ fn main() {
     use crate::win::*;
 
     const WINFW_DIR_VAR: &str = "WINFW_LIB_DIR";
-    const WINDNS_DIR_VAR: &str = "WINDNS_LIB_DIR";
     const WINNET_DIR_VAR: &str = "WINNET_LIB_DIR";
     declare_library(WINFW_DIR_VAR, WINFW_BUILD_DIR, "winfw");
-    declare_library(WINDNS_DIR_VAR, WINDNS_BUILD_DIR, "windns");
     declare_library(WINNET_DIR_VAR, WINNET_BUILD_DIR, "winnet");
     let lib_dir = manifest_dir().join("../build/lib/x86_64-pc-windows-msvc");
     println!("cargo:rustc-link-search={}", &lib_dir.display());
