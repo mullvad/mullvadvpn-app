1c625bf8910b2b57a3968bec653bf0b3a0f809c0
user <user@user.user>
2022-06-07 11:21:02 +0200

    Screen more off from non-linux - squash

--- talpid-core/src/tunnel/mod.rs
+++ talpid-core/src/tunnel/mod.rs
@@ -211,6 +211,7 @@ impl TunnelMonitor {
         })
     }
 
+    #[cfg(target_os = "linux")]
     fn set_mtu(params: &mut wireguard_types::TunnelParameters, mtu: u16) {
         const WIREGUARD_HEADER_SIZE: u16 = 80;
         // The largest tunnel MTU that we allow. Standard MTU - Wireguard header
