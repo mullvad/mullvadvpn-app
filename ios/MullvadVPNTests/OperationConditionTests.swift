f8d358c5a17c7e09e97e7da4a8e307c5fd96c662
Andrej Mihajlov <and@mullvad.net>
2022-06-08 12:23:57 +0200

    Add tests

---
+++ ios/MullvadVPNTests/OperationConditionTests.swift
@@ -0,0 +1,144 @@
+//
+//  OperationConditionTests.swift
+//  MullvadVPNTests
+//
+//  Created by pronebird on 02/06/2022.
+//  Copyright © 2022 Mullvad VPN AB. All rights reserved.
+//
+
+import XCTest
+
+class OperationConditionTests: XCTestCase {
+    func testTrueCondition() {
+        let expectConditionEvaluation = expectation(description: "Expect condition evaluation")
+        let expectOperationToExecute = expectation(description: "Expect operation to execute")
+
+        let operation = AsyncBlockOperation {
+            expectOperationToExecute.fulfill()
+        }
+
+        let blockCondition = BlockCondition { op, completion in
+            expectConditionEvaluation.fulfill()
+            completion(true)
+        }
+
+        operation.addCondition(blockCondition)
+
+        let operationQueue = AsyncOperationQueue()
+        operationQueue.addOperation(operation)
+
+        waitForExpectations(timeout: 1)
+    }
+
+    func testFalseCondition() {
+        let expectConditionEvaluation = expectation(description: "Expect condition evaluation")
+        let expectOperationToNeverExecute = expectation(
+            description: "Expect operation to never execute"
+        )
+        expectOperationToNeverExecute.isInverted = true
+
+        let operation = AsyncBlockOperation {
+            expectOperationToNeverExecute.fulfill()
+        }
+
+        let blockCondition = BlockCondition { op, completion in
+            expectConditionEvaluation.fulfill()
+            completion(false)
+        }
+
+        operation.addCondition(blockCondition)
+
+        let operationQueue = AsyncOperationQueue()
+        operationQueue.addOperation(operation)
+
+        waitForExpectations(timeout: 1)
+    }
+
+    func testNoCancelledDependenciesCondition() {
+        let expectToNeverExecute = expectation(description: "Expect child to never execute.")
+        expectToNeverExecute.isInverted = true
+
+        let parent = BlockOperation()
+        parent.cancel()
+
+        let child = AsyncBlockOperation {
+            expectToNeverExecute.fulfill()
+        }
+        child.addDependency(parent)
+        child.addCondition(NoCancelledDependenciesCondition())
+
+        let operationQueue = AsyncOperationQueue()
+        operationQueue.addOperations([parent, child], waitUntilFinished: false)
+
+        waitForExpectations(timeout: 1)
+    }
+
+    func testNoFailedDependenciesCondition() {
+        let expectToNeverExecute = expectation(description: "Expect child to never execute.")
+        expectToNeverExecute.isInverted = true
+
+        let parent = ResultBlockOperation<(), URLError> {
+            throw URLError(.badURL)
+        }
+
+        let child = AsyncBlockOperation {
+            expectToNeverExecute.fulfill()
+        }
+        child.addDependency(parent)
+        child.addCondition(NoFailedDependenciesCondition(ignoreCancellations: false))
+
+        let operationQueue = AsyncOperationQueue()
+        operationQueue.addOperations([parent, child], waitUntilFinished: false)
+
+        waitForExpectations(timeout: 1)
+    }
+
+    func testNoFailedDependenciesIgnoringCancellationsCondition() {
+        let expectToExecute = expectation(description: "Expect child to execute.")
+
+        let parent = BlockOperation()
+        parent.cancel()
+
+        let child = AsyncBlockOperation {
+            expectToExecute.fulfill()
+        }
+        child.addDependency(parent)
+        child.addCondition(NoFailedDependenciesCondition(ignoreCancellations: true))
+
+        let operationQueue = AsyncOperationQueue()
+        operationQueue.addOperations([parent, child], waitUntilFinished: false)
+
+        waitForExpectations(timeout: 1)
+    }
+
+    func testMutuallyExclusiveCondition() {
+        let expectFirstOperationExecution = expectation(
+            description: "Expect first operation to execute first"
+        )
+        let expectSecondOperationExecution = expectation(
+            description: "Expect second operation to execute last"
+        )
+
+        let exclusiveCategory = "exclusiveOperations"
+        let operationQueue = AsyncOperationQueue()
+
+        let firstOperation = AsyncBlockOperation { op in
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+                expectFirstOperationExecution.fulfill()
+                op.finish()
+            }
+        }
+        firstOperation.addCondition(MutuallyExclusive(category: exclusiveCategory))
+
+        let secondOperation = AsyncBlockOperation {
+            expectSecondOperationExecution.fulfill()
+        }
+        secondOperation.addCondition(MutuallyExclusive(category: exclusiveCategory))
+
+        operationQueue.addOperations([firstOperation, secondOperation], waitUntilFinished: false)
+
+        let expectations = [expectFirstOperationExecution, expectSecondOperationExecution]
+        wait(for: expectations, timeout: 2, enforceOrder: true)
+    }
+
+}
---
+++ ios/MullvadVPNTests/OperationObserverTests.swift
@@ -0,0 +1,68 @@
+//
+//  OperationObserverTests.swift
+//  MullvadVPNTests
+//
+//  Created by pronebird on 02/06/2022.
+//  Copyright © 2022 Mullvad VPN AB. All rights reserved.
+//
+
+import XCTest
+
+class OperationObserverTests: XCTestCase {
+
+    func testBlockObserver() throws {
+        let expectDidAttach = expectation(description: "didAttach handler")
+        let expectDidStart = expectation(description: "didStart handler")
+        let expectDidCancel = expectation(description: "didCancel handler")
+        expectDidCancel.isInverted = true
+        let expectDidFinish = expectation(description: "didAttach handler")
+
+        let operation = AsyncBlockOperation()
+        operation.addBlockObserver(OperationBlockObserver(
+            didAttach: { op in
+                expectDidAttach.fulfill()
+            }, didStart: { op in
+                expectDidStart.fulfill()
+            }, didCancel: { op in
+                expectDidCancel.fulfill()
+            }, didFinish: { op in
+                expectDidFinish.fulfill()
+            }
+        ))
+
+        let operationQueue = AsyncOperationQueue()
+        operationQueue.addOperation(operation)
+
+        let expectations = [expectDidCancel, expectDidAttach, expectDidStart, expectDidFinish]
+        wait(for: expectations, timeout: 1, enforceOrder: true)
+    }
+
+    func testBlockObserverWithCancelledOperation() {
+        let expectDidAttach = expectation(description: "didAttach handler")
+        let expectDidStart = expectation(description: "didStart handler")
+        expectDidStart.isInverted = true
+        let expectDidCancel = expectation(description: "didCancel handler")
+        let expectDidFinish = expectation(description: "didAttach handler")
+
+        let operation = AsyncBlockOperation()
+        operation.addBlockObserver(OperationBlockObserver(
+            didAttach: { op in
+                expectDidAttach.fulfill()
+            }, didStart: { op in
+                expectDidStart.fulfill()
+            }, didCancel: { op in
+                expectDidCancel.fulfill()
+            }, didFinish: { op in
+                expectDidFinish.fulfill()
+            }
+        ))
+        operation.cancel()
+
+        let operationQueue = AsyncOperationQueue()
+        operationQueue.addOperation(operation)
+
+        let expectations = [expectDidAttach, expectDidCancel, expectDidStart, expectDidFinish]
+        wait(for: expectations, timeout: 1, enforceOrder: true)
+    }
+
+}
