7894f2178a042b12b5d5c562c3f9d50ee622a6b7
David Lönnhager <david.l@mullvad.net>
2022-06-17 13:50:29 +0200

    fixup

--- mullvad-api/src/fs.rs
+++ mullvad-api/src/fs.rs
@@ -1,6 +1,6 @@
 use std::{
     ops::{Deref, DerefMut},
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 use talpid_types::ErrorExt;
 use tokio::{fs, io};
@@ -27,25 +27,41 @@ impl AtomicFile {
     /// Flushes and moves the file to `self.target_path`, replacing it if it exists.
     pub async fn finalize(mut self) -> io::Result<()> {
         let file = self.file.take().unwrap();
+        let result = Self::finalize_inner(file, &self.temp_path, &self.target_path).await;
+        if result.is_err() {
+            consume_removal_result(fs::remove_file(&self.temp_path).await);
+        }
+        result
+    }
 
+    async fn finalize_inner(
+        file: fs::File,
+        temp_path: &Path,
+        target_path: &Path,
+    ) -> io::Result<()> {
         file.sync_all().await?;
         let std_file = file.into_std().await;
         let _ = tokio::task::spawn_blocking(move || drop(std_file)).await;
-
-        fs::rename(&self.temp_path, &self.target_path).await
+        fs::rename(temp_path, target_path).await
     }
 }
 
 impl Drop for AtomicFile {
     fn drop(&mut self) {
-        // The file will be removed when all file handles are closed
-        if let Err(error) = std::fs::remove_file(&self.temp_path) {
-            if error.kind() != io::ErrorKind::NotFound {
-                log::warn!(
-                    "{}",
-                    error.display_chain_with_msg("Failed to delete AtomicFile")
-                );
-            }
+        if self.file.is_some() {
+            log::warn!("Object was not finalized");
+            consume_removal_result(std::fs::remove_file(&self.temp_path));
+        }
+    }
+}
+
+fn consume_removal_result(result: io::Result<()>) {
+    if let Err(error) = result {
+        if error.kind() != io::ErrorKind::NotFound {
+            log::warn!(
+                "{}",
+                error.display_chain_with_msg("Failed to delete temp file")
+            );
         }
     }
 }
