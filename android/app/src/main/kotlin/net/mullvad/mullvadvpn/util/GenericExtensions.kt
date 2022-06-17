319c524f55729a14c62bb6d8a8985e4d87030fb6
Albin <albin@mullvad.net>
2022-06-17 20:23:12 +0200

    Add safelet extension

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/GenericExtensions.kt
@@ -0,0 +1,5 @@
+package net.mullvad.mullvadvpn.util
+
+inline fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R?): R? {
+    return if (p1 != null && p2 != null) block(p1, p2) else null
+}
