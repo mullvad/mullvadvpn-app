4075c331f3df22023c2022a11676b3280d2193f8
Albin <albin@mullvad.net>
2022-06-20 13:35:00 +0200

    Add PQ-flag to android tunnel options

--- android/app/src/main/kotlin/net/mullvad/talpid/net/wireguard/TunnelOptions.kt
+++ android/app/src/main/kotlin/net/mullvad/talpid/net/wireguard/TunnelOptions.kt
@@ -4,4 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class TunnelOptions(val mtu: Int?) : Parcelable
+data class TunnelOptions(
+    val mtu: Int?,
+    val usePqSafePsk: Boolean
+) : Parcelable
