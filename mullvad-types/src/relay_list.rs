94d892604fdce1f4832c7a5aef9e6ec2f6d15a7e
Albin <albin@mullvad.net>
2022-06-22 11:02:29 +0200

    Adapt Android to new relay structure

--- mullvad-types/src/relay_list.rs
+++ mullvad-types/src/relay_list.rs
@@ -22,6 +22,7 @@ pub struct RelayList {
     pub openvpn: OpenVpnEndpointData,
     #[cfg_attr(target_os = "android", jnix(skip))]
     pub bridge: BridgeEndpointData,
+    #[cfg_attr(target_os = "android", jnix(skip))]
     pub wireguard: WireguardEndpointData,
 }
 
@@ -75,7 +76,6 @@ pub struct Relay {
     pub provider: String,
     #[cfg_attr(target_os = "android", jnix(skip))]
     pub weight: u64,
-    #[cfg_attr(target_os = "android", jnix(skip))]
     pub endpoint_data: RelayEndpointData,
     #[cfg_attr(target_os = "android", jnix(skip))]
     pub location: Option<Location>,
@@ -84,6 +84,8 @@ pub struct Relay {
 /// Specifies the type of a relay or relay-specific endpoint data.
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
+#[cfg_attr(target_os = "android", derive(IntoJava))]
+#[cfg_attr(target_os = "android", jnix(package = "net.mullvad.mullvadvpn.model"))]
 pub enum RelayEndpointData {
     Openvpn,
     Bridge,
