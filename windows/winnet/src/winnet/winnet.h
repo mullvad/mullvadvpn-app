2958c5dc60cd34dc283854bb8bc6124a09f73f9b
David Lönnhager <david.l@mullvad.net>
2022-06-23 17:32:05 +0200

    Remove WinNet_InterfaceLuidToIpAddress

--- windows/winnet/src/winnet/winnet.h
+++ windows/winnet/src/winnet/winnet.h
@@ -134,18 +134,6 @@ WinNet_GetBestDefaultRoute(
 	void *logSinkContext
 );
 
-extern "C"
-WINNET_LINKAGE
-WINNET_STATUS
-WINNET_API
-WinNet_InterfaceLuidToIpAddress(
-	WINNET_ADDR_FAMILY family,
-	uint64_t interfaceLuid,
-	WINNET_IP *ip,
-	MullvadLogSink logSink,
-	void *logSinkContext
-);
-
 enum WINNET_DEFAULT_ROUTE_CHANGED_EVENT_TYPE
 {
 	// Best default route changed.
