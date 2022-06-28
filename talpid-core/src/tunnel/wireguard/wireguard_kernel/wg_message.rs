cd042957719d0a53bb59e5139c634dedaa8e6399
Jonathan <jonathan@mullvad.net>
2022-06-28 11:54:52 +0200

    Update formatting to new nightly
    Nightly cargo fmt changed and wants to change wg_message.rs
    In order to have passing CI checks this needs to be changed.

--- talpid-core/src/tunnel/wireguard/wireguard_kernel/wg_message.rs
+++ talpid-core/src/tunnel/wireguard/wireguard_kernel/wg_message.rs
@@ -725,16 +725,14 @@ mod test {
                 ProtocolVersion(1),
                 Endpoint(InetAddr::from_std(&"192.168.40.1:9797".parse().unwrap())),
                 AllowedIps(
-                    [
-                        AllowedIpMessage(
-                            [
-                                CidrMask(32),
-                                AddressFamily(2),
-                                IpAddr(Ipv4Addr::new(192, 168, 39, 1).into()),
-                            ]
-                            .to_vec(),
-                        ),
-                    ]
+                    [AllowedIpMessage(
+                        [
+                            CidrMask(32),
+                            AddressFamily(2),
+                            IpAddr(Ipv4Addr::new(192, 168, 39, 1).into()),
+                        ]
+                        .to_vec(),
+                    )]
                     .to_vec()
                     .to_vec(),
                 ),
@@ -759,16 +757,14 @@ mod test {
                 ProtocolVersion(1),
                 Endpoint(InetAddr::from_std(&"192.168.40.2:9797".parse().unwrap())),
                 AllowedIps(
-                    [
-                        AllowedIpMessage(
-                            vec![
-                                CidrMask(32),
-                                AddressFamily(2),
-                                IpAddr(Ipv4Addr::new(192, 168, 39, 2).into()),
-                            ]
-                            .to_vec(),
-                        ),
-                    ]
+                    [AllowedIpMessage(
+                        vec![
+                            CidrMask(32),
+                            AddressFamily(2),
+                            IpAddr(Ipv4Addr::new(192, 168, 39, 2).into()),
+                        ]
+                        .to_vec(),
+                    )]
                     .to_vec(),
                 ),
             ]
@@ -813,16 +809,14 @@ mod test {
                 Endpoint(InetAddr::from_std(&"192.168.40.1:9797".parse().unwrap())),
                 PeerNla::Flags(WGPEER_F_REPLACE_ALLOWEDIPS),
                 AllowedIps(
-                    [
-                        AllowedIpMessage(
-                            [
-                                AddressFamily(2),
-                                IpAddr(Ipv4Addr::new(192, 168, 39, 1).into()),
-                                CidrMask(32),
-                            ]
-                            .to_vec(),
-                        ),
-                    ]
+                    [AllowedIpMessage(
+                        [
+                            AddressFamily(2),
+                            IpAddr(Ipv4Addr::new(192, 168, 39, 1).into()),
+                            CidrMask(32),
+                        ]
+                        .to_vec(),
+                    )]
                     .to_vec()
                     .to_vec(),
                 ),
@@ -839,16 +833,14 @@ mod test {
                 Endpoint(InetAddr::from_std(&"192.168.40.2:9797".parse().unwrap())),
                 PeerNla::Flags(WGPEER_F_REPLACE_ALLOWEDIPS),
                 AllowedIps(
-                    [
-                        AllowedIpMessage(
-                            vec![
-                                AddressFamily(2),
-                                IpAddr(Ipv4Addr::new(192, 168, 39, 2).into()),
-                                CidrMask(32),
-                            ]
-                            .to_vec(),
-                        ),
-                    ]
+                    [AllowedIpMessage(
+                        vec![
+                            AddressFamily(2),
+                            IpAddr(Ipv4Addr::new(192, 168, 39, 2).into()),
+                            CidrMask(32),
+                        ]
+                        .to_vec(),
+                    )]
                     .to_vec(),
                 ),
             ]
