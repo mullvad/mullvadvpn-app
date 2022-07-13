e47722cd17bd8cfd61f52445847bf931a5108892
Albin <albin@mullvad.net>
2022-07-13 15:30:37 +0200

    Add requireMainActivity extension

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/extension/ContextExtensions.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/extension/ContextExtensions.kt
@@ -3,7 +3,9 @@ package net.mullvad.mullvadvpn.ui.extension
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import androidx.fragment.app.Fragment
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.ui.MainActivity
 
 fun Context.openAccountPageInBrowser(authToken: String) {
     startActivity(
@@ -12,4 +14,14 @@ fun Context.openAccountPageInBrowser(authToken: String) {
             Uri.parse(getString(R.string.account_url) + "?token=$authToken")
         )
     )
+}
+
+fun Fragment.requireMainActivity(): MainActivity {
+    return if (this.activity is MainActivity) {
+        this.activity as MainActivity
+    } else {
+        throw IllegalStateException(
+            "Fragment $this not attached to ${MainActivity::class.simpleName}."
+        )
+    }
 }
