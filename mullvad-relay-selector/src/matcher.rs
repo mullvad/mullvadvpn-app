f9193ed904cd4cf7a49ad9dee90c688979152d2b
Linus Färnstrand <faern@faern.net>
2022-07-18 09:46:10 +0200

    Upgrade x25519-dalek and rand to latest versions

--- mullvad-relay-selector/src/matcher.rs
+++ mullvad-relay-selector/src/matcher.rs
@@ -9,7 +9,7 @@ use mullvad_types::{
     },
 };
 use rand::{
-    prelude::{IteratorRandom, SliceRandom},
+    seq::{IteratorRandom, SliceRandom},
     Rng,
 };
 use std::net::{IpAddr, SocketAddr};
@@ -270,7 +270,7 @@ impl WireguardMatcher {
                     return None;
                 }
 
-                let mut port_index = rand::thread_rng().gen_range(0, port_amount);
+                let mut port_index = rand::thread_rng().gen_range(0..port_amount);
 
                 for range in data.port_ranges.iter() {
                     let ports_in_range = get_port_amount(range);
