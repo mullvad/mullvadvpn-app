c4a9a4ff44b5e370df364d205cbdf306a48b51c0
Oskar Nyberg <oskar@mullvad.net>
2022-07-18 14:48:02 +0200

    Start using Cell Group for spacing between and after cells

--- gui/src/renderer/components/cell/Footer.tsx
+++ gui/src/renderer/components/cell/Footer.tsx
@@ -4,7 +4,7 @@ import { colors } from '../../../config.json';
 import { tinyText } from '../common-styles';
 
 export const Footer = styled.div({
-  padding: '6px 22px 20px',
+  padding: '6px 22px 0px',
 });
 
 export const FooterText = styled.span(tinyText, {
