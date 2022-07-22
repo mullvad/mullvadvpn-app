a2053c1ad1574d56525260f1eb2568d01445c416
Albin <albin@mullvad.net>
2022-07-22 06:23:13 +0200

    Remove redundant expiry fetching

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/SettingsFragment.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/SettingsFragment.kt
@@ -15,7 +15,6 @@ import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.model.DeviceState
@@ -159,7 +158,6 @@ class SettingsFragment : BaseFragment(), StatusBarPainter, NavigationBarPainter 
 
     private fun CoroutineScope.launchUpdateExpiryTextOnExpiryChanges() = launch {
         accountRepository.accountExpiryState
-            .onStart { accountRepository.fetchAccountExpiry() }
             .map { state -> state.date() }
             .collect { expiryDate ->
                 accountMenu.accountExpiry = expiryDate
