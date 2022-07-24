b1e5893713be7e9c725e0dddffe748130cdb6b2e
Oskar Nyberg <oskar@mullvad.net>
2022-07-24 15:05:17 +0200

    Add flag that prevents navigation to be reset 2 minutes after blur

--- gui/tasks/electron.js
+++ gui/tasks/electron.js
@@ -1,10 +1,17 @@
 const { spawn } = require('child_process');
 const electron = require('electron');
 
+const DISABLE_RESET_NAVIGATION = '--disable-reset-navigation';
+
 let subprocess;
 
 function startElectron(done) {
-  subprocess = spawn(electron, ['.'], {
+  const args = [];
+  if (process.argv.includes(DISABLE_RESET_NAVIGATION)) {
+    args.push(DISABLE_RESET_NAVIGATION);
+  }
+
+  subprocess = spawn(electron, ['.', ...args], {
     env: { ...process.env, NODE_ENV: 'development' },
     stdio: 'inherit',
   });
