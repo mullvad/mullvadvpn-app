f1b76fd1f9d2ea6a09f1252e09827147a32ea597
Albin <albin@mullvad.net>
2022-07-28 08:41:28 +0200

    Refactor compose list

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/List.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/List.kt
@@ -1,10 +1,10 @@
 package net.mullvad.mullvadvpn.compose.component
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,17 +13,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import net.mullvad.mullvadvpn.R
 
 @Composable
-fun DeviceRow(
-    name: String,
-    painter: Painter? = null,
-    onItemClicked: () -> Unit
+fun ListItem(
+    text: String,
+    @DrawableRes iconResourceId: Int? = null,
+    onClick: () -> Unit
 ) {
     val itemColor = colorResource(id = R.color.blue)
 
@@ -35,7 +35,7 @@ fun DeviceRow(
             .background(itemColor),
     ) {
         Text(
-            text = name,
+            text = text,
             fontSize = 18.sp,
             color = Color.White,
             modifier = Modifier
@@ -45,35 +45,19 @@ fun DeviceRow(
                 .align(Alignment.CenterStart)
         )
 
-        if (painter != null) {
-            Image(
-                painter = painter,
-                contentDescription = "Remove",
-                modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .padding(horizontal = 12.dp)
-                    .clickable {
-                        onItemClicked()
-                    }
-            )
-        }
-    }
-}
-
-@Composable
-fun <T> ItemList(
-    items: List<T>,
-    itemText: (T) -> String,
-    onItemClicked: (T) -> Unit,
-    itemPainter: Painter? = null,
-    modifier: Modifier = Modifier,
-) {
-    Column(
-        modifier = modifier
-    ) {
-        items.forEach { item ->
-            DeviceRow(itemText.invoke(item), itemPainter) {
-                onItemClicked(item)
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .padding(horizontal = 12.dp)
+        ) {
+            if (iconResourceId != null) {
+                Image(
+                    painter = painterResource(id = iconResourceId),
+                    contentDescription = "Remove",
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .clickable { onClick() }
+                )
             }
         }
     }
