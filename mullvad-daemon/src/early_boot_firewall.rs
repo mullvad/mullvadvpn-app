470ff6c22d414b525f4e7bae87f075d846806e15
Emīls Piņķis <emils@mullvad.net>
2022-09-01 22:56:21 +0200

    Add argument to initialize firewall to a blocking state

---
+++ mullvad-daemon/src/early_boot_firewall.rs
@@ -0,0 +1,43 @@
+use mullvad_daemon::settings::{self, SettingsPersister};
+use talpid_core::firewall::{self, Firewall, FirewallPolicy};
+
+#[derive(err_derive::Error, Debug)]
+pub enum Error {
+    #[error(display = "Failed to initialize firewall")]
+    Firewall(#[error(source)] firewall::Error),
+
+    #[error(display = "Failed to get settings path")]
+    Path(#[error(source)] mullvad_paths::Error),
+
+    #[error(display = "Failed to get settings")]
+    Settings(#[error(source)] settings::Error),
+}
+
+pub async fn initialize_firewall() -> Result<(), Error> {
+    let mut firewall = Firewall::new()?;
+    let allow_lan = get_allow_lan().await.unwrap_or_else(|err| {
+        log::info!(
+            "Not allowing LAN traffic due to failing to read settings: {}",
+            err
+        );
+        false
+    });
+    let policy = FirewallPolicy::Blocked {
+        allow_lan,
+        allowed_endpoint: talpid_types::net::AllowedEndpoint {
+            endpoint: talpid_types::net::Endpoint {
+                address: (std::net::Ipv4Addr::LOCALHOST, 0).into(),
+                protocol: talpid_types::net::TransportProtocol::Tcp,
+            },
+        },
+    };
+    log::info!("Applying firewall policy {policy}");
+    firewall.apply_policy(policy)?;
+    Ok(())
+}
+
+async fn get_allow_lan() -> Result<bool, Error> {
+    let path = mullvad_paths::settings_dir()?;
+    let settings = SettingsPersister::load(&path).await;
+    Ok(settings.allow_lan)
+}
