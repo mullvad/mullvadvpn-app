29c01df296f69857548ac8e9b42b0e90d92e73d0
Emīls Piņķis <emils@mullvad.net>
2022-09-02 10:56:54 +0200

    Add early-boot-blocking unit file

--- dist-assets/linux/post-transaction.sh
+++ dist-assets/linux/post-transaction.sh
@@ -2,5 +2,6 @@
 # This is to mitigate post-uninstall hooks being ran AFTER post-install hooks
 # during an upgrade on Fedora.
 set -eu
-systemctl enable "/opt/Mullvad VPN/resources/mullvad-daemon.service" || true
+systemctl enable "/etc/systemd/system/mullvad-daemon.service" || true
 systemctl start mullvad-daemon.service || true
+systemctl enable "/etc/systemd/system/mullvad-early-boot-blocking.service" || true
