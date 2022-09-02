06a3fb2a67de756b41a8fc219f664fbc31ed4c20
Emīls Piņķis <emils@mullvad.net>
2022-09-02 15:10:55 +0200

    Improve daemon unit file
    Set the `MULLVAD_RESOURCE_DIR` environment variable and specify the
    path as a required mount path.

--- dist-assets/linux/post-transaction.sh
+++ dist-assets/linux/post-transaction.sh
@@ -2,6 +2,12 @@
 # This is to mitigate post-uninstall hooks being ran AFTER post-install hooks
 # during an upgrade on Fedora.
 set -eu
-systemctl enable "/etc/systemd/system/mullvad-daemon.service" || true
-systemctl start mullvad-daemon.service || true
-systemctl enable "/etc/systemd/system/mullvad-early-boot-blocking.service" || true
+
+# Repeated enablement of the daemon service will result in the early-boot unit
+# being executed when the daemon is already running, which results in the
+# firewall rules being applied.
+if ! systemctl is-enabled mullvad-daemon; then
+    systemctl enable "/usr/lib/systemd/system/mullvad-daemon.service" || true
+    systemctl start mullvad-daemon.service || true
+    systemctl enable "/usr/lib/systemd/system/mullvad-early-boot-blocking.service" || true
+fi
