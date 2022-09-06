149733d2c5297e9445431ac5fd3abae34711503a
Oskar Nyberg <oskar@mullvad.net>
2022-09-06 15:47:20 +0200

    Move default settings to its own file

---
+++ gui/src/main/default-settings.ts
@@ -0,0 +1,70 @@
+import { ISettings, ObfuscationType, Ownership } from '../shared/daemon-rpc-types';
+
+export function getDefaultSettings(): ISettings {
+  return {
+    allowLan: false,
+    autoConnect: false,
+    blockWhenDisconnected: false,
+    showBetaReleases: false,
+    splitTunnel: {
+      enableExclusions: false,
+      appsList: [],
+    },
+    relaySettings: {
+      normal: {
+        location: 'any',
+        tunnelProtocol: 'any',
+        providers: [],
+        ownership: Ownership.any,
+        openvpnConstraints: {
+          port: 'any',
+          protocol: 'any',
+        },
+        wireguardConstraints: {
+          port: 'any',
+          ipVersion: 'any',
+          useMultihop: false,
+          entryLocation: 'any',
+        },
+      },
+    },
+    bridgeSettings: {
+      normal: {
+        location: 'any',
+        providers: [],
+        ownership: Ownership.any,
+      },
+    },
+    bridgeState: 'auto',
+    tunnelOptions: {
+      generic: {
+        enableIpv6: false,
+      },
+      openvpn: {
+        mssfix: undefined,
+      },
+      wireguard: {
+        mtu: undefined,
+      },
+      dns: {
+        state: 'default',
+        defaultOptions: {
+          blockAds: false,
+          blockTrackers: false,
+          blockMalware: false,
+          blockAdultContent: false,
+          blockGambling: false,
+        },
+        customOptions: {
+          addresses: [],
+        },
+      },
+    },
+    obfuscationSettings: {
+      selectedObfuscation: ObfuscationType.auto,
+      udp2tcpSettings: {
+        port: 'any',
+      },
+    },
+  };
+}
