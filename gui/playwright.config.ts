1677933ba8cacddb25d2c126d9db99224090df81
Hank <hank@mullvad.net>
2022-09-08 14:46:41 +0200

    Add screenshot diffing

--- gui/playwright.config.ts
+++ gui/playwright.config.ts
@@ -2,6 +2,12 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
   maxFailures: 2,
+  expect: {
+    toMatchSnapshot: {
+      threshold: 0.1,
+      maxDiffPixelRatio: 0.01,
+    },
+  },
   use: {
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
