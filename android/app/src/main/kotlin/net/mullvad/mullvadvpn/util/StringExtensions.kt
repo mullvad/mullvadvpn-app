e60b2761617826b0ca982f75ea21deec00a94d9a
Albin <albin@mullvad.net>
2022-09-12 13:33:58 +0200

    Refactor parsing of date string

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/StringExtensions.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/StringExtensions.kt
@@ -1,9 +1,22 @@
 package net.mullvad.mullvadvpn.util
 
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+private const val EXPIRY_FORMAT = "YYYY-MM-dd HH:mm:ss z"
+
 fun String.capitalizeFirstCharOfEachWord(): String {
     return split(" ")
         .joinToString(" ") { word ->
             word.replaceFirstChar { firstChar -> firstChar.uppercase() }
         }
         .trimEnd()
+}
+
+fun String.parseAsDateTime(): DateTime? {
+    return try {
+        DateTime.parse(this, DateTimeFormat.forPattern(EXPIRY_FORMAT))
+    } catch (ex: Exception) {
+        null
+    }
 }
