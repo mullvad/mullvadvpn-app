473b2ab1c22c733acdff0551e3135228bfeb70e5
David Lönnhager <david.l@mullvad.net>
2022-09-20 10:01:57 +0200

    Add shutdown detection for Windows service

--- mullvad-daemon/src/main.rs
+++ mullvad-daemon/src/main.rs
@@ -91,12 +91,6 @@ fn get_log_dir(config: &cli::Config) -> Result<Option<PathBuf>, String> {
 async fn run_platform(config: &cli::Config, log_dir: Option<PathBuf>) -> Result<(), String> {
     if config.run_as_service {
         system_service::run()
-    } else if config.restart_service {
-        let restart_result = system_service::restart_service().map_err(|e| e.display_chain());
-        if restart_result.is_ok() {
-            log::info!("Restarted the service.");
-        }
-        restart_result
     } else {
         if config.register_service {
             let install_result = system_service::install_service().map_err(|e| e.display_chain());
@@ -144,7 +138,7 @@ async fn run_standalone(log_dir: Option<PathBuf>) -> Result<(), String> {
     let daemon = create_daemon(log_dir).await?;
 
     let shutdown_handle = daemon.shutdown_handle();
-    shutdown::set_shutdown_signal_handler(move || shutdown_handle.shutdown())
+    shutdown::set_shutdown_signal_handler(move || shutdown_handle.shutdown(true))
         .map_err(|e| e.display_chain())?;
 
     daemon.run().await.map_err(|e| e.display_chain())?;
