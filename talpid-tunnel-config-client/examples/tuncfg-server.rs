02fea8b29a3d8918c497b3f13941aed9a4b482e4
Linus Färnstrand <faern@faern.net>
2022-09-21 12:10:12 +0200

    TMP: Add support for Kyber1024 to prove the protocol works

--- talpid-tunnel-config-client/examples/tuncfg-server.rs
+++ talpid-tunnel-config-client/examples/tuncfg-server.rs
@@ -34,29 +34,44 @@ impl PostQuantumSecure for PostQuantumSecureImpl {
         let mut rng = rand::thread_rng();
         let request = request.into_inner();
 
-        eprintln!("wg_pubkey: {:?}", request.wg_pubkey);
-        eprintln!("wg_psk_pubkey: {:?}", request.wg_psk_pubkey);
+        println!("wg_pubkey: {:?}", request.wg_pubkey);
+        println!("wg_psk_pubkey: {:?}", request.wg_psk_pubkey);
 
+        // The ciphertexts that will be returned to the client
         let mut ciphertexts = Vec::new();
+        // The final PSK that is computed by XORing together all the KEM outputs.
         let mut psk_data = [0u8; 32];
-        for kem_pubkey in request.kem_pubkeys {
-            eprintln!("\tkem_algorithm: {}", kem_pubkey.algorithm_name);
-            let key_data: [u8; CRYPTO_PUBLICKEYBYTES] =
-                kem_pubkey.key_data.as_slice().try_into().unwrap();
-            let public_key = PublicKey::from(&key_data);
-            let (ciphertext, shared_secret) =
-                classic_mceliece_rust::encapsulate_boxed(&public_key, &mut rng);
 
-            let shared_secret = shared_secret.as_array();
-            ciphertexts.push(ciphertext.as_array().to_vec());
-            eprintln!("\tshared secret: {:?}", shared_secret);
+        for kem_pubkey in request.kem_pubkeys {
+            println!("\tKEM algorithm: {}", kem_pubkey.algorithm_name);
+            let (ciphertext, shared_secret) = match kem_pubkey.algorithm_name.as_str() {
+                "Classic-McEliece-460896f" => {
+                    let key_data: [u8; CRYPTO_PUBLICKEYBYTES] =
+                        kem_pubkey.key_data.as_slice().try_into().unwrap();
+                    let public_key = PublicKey::from(&key_data);
+                    let (ciphertext, shared_secret) =
+                        classic_mceliece_rust::encapsulate_boxed(&public_key, &mut rng);
+                    (ciphertext.as_array().to_vec(), *shared_secret.as_array())
+                }
+                "Kyber1024" => {
+                    let public_key = kem_pubkey.key_data.as_slice();
+                    let (ciphertext, shared_secret) =
+                        pqc_kyber::encapsulate(public_key, &mut rng).unwrap();
+                    (ciphertext.to_vec(), shared_secret)
+                }
+                name => panic!("Unsupported KEM algorithm: {name}"),
+            };
+
+            ciphertexts.push(ciphertext);
+            println!("\tshared secret: {:?}", shared_secret);
             for (psk_byte, shared_secret_byte) in psk_data.iter_mut().zip(shared_secret.iter()) {
-                *psk_byte |= shared_secret_byte;
+                *psk_byte ^= shared_secret_byte;
             }
         }
 
         let psk = PresharedKey::from(psk_data);
-        eprintln!("psk: {:?}", psk);
+        println!("psk: {:?}", psk);
+        println!("==============================================");
         Ok(Response::new(PskResponseExperimentalV1 { ciphertexts }))
     }
 }
