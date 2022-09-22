8e9bb97d28b1e2d5fdd166d4ea8f0d281fb9c4b7
Albin <albin@mullvad.net>
2022-09-22 16:57:23 +0200

    Fix pending intent flags

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/SdkUtils.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/SdkUtils.kt
@@ -1,10 +1,19 @@
 package net.mullvad.mullvadvpn.util
 
+import android.app.PendingIntent
 import android.net.VpnService
 import android.os.Build
 import android.service.quicksettings.Tile
 
 object SdkUtils {
+    fun getSupportedPendingIntentFlags(): Int {
+        return if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S) {
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+        } else {
+            PendingIntent.FLAG_UPDATE_CURRENT
+        }
+    }
+
     fun VpnService.Builder.setMeteredIfSupported(isMetered: Boolean) {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
             this.setMetered(isMetered)
