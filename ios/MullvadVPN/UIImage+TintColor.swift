6504139cc12904a3a12a13a9f7fda1dd568c5133
Sajad Vishkai <sajaclvishkai@gmail.com>
2022-09-26 12:06:19 +0200

    Update UIImage+TintColor.swift

--- ios/MullvadVPN/UIImage+TintColor.swift
+++ ios/MullvadVPN/UIImage+TintColor.swift
@@ -14,19 +14,6 @@ extension UIImage {
     }
 
     func backport_withTintColor(_ tintColor: UIColor, renderingMode: RenderingMode) -> UIImage {
-        if #available(iOS 13, *) {
-            return withTintColor(tintColor, renderingMode: renderingMode)
-        }
-
-        let rect = CGRect(origin: .zero, size: size)
-        let renderer = UIGraphicsImageRenderer(size: size)
-
-        let renderedImage = renderer.image { context in
-            tintColor.setFill()
-            context.fill(rect)
-            draw(in: rect, blendMode: .destinationIn, alpha: 1)
-        }
-
-        return renderedImage.withRenderingMode(renderingMode)
+        withTintColor(tintColor, renderingMode: renderingMode)
     }
 }
