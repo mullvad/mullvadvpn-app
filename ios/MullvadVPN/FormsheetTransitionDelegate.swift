aca854134b0cdd7a47ec03cbf37f58d8cba53e7d
Andrej Mihajlov <and@mullvad.net>
2022-09-29 18:17:42 +0200

    Add vouchers support

---
+++ ios/MullvadVPN/FormsheetTransitionDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  FormsheetTransitionDelegate.swift
+//  MullvadVPN
+//
+//  Created by Andreas Lif on 2022-08-30.
+//  Copyright © 2022 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FormsheetTransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
+    func animationController(
+        forPresented presented: UIViewController,
+        presenting: UIViewController,
+        source: UIViewController
+    ) -> UIViewControllerAnimatedTransitioning? {
+        return FormsheetPresentationAnimator()
+    }
+
+    func animationController(forDismissed dismissed: UIViewController)
+        -> UIViewControllerAnimatedTransitioning?
+    {
+        return FormsheetPresentationAnimator()
+    }
+
+    func presentationController(
+        forPresented presented: UIViewController,
+        presenting: UIViewController?,
+        source: UIViewController
+    ) -> UIPresentationController? {
+        return FormsheetPresentationController(
+            presentedViewController: presented,
+            presenting: source
+        )
+    }
+}
