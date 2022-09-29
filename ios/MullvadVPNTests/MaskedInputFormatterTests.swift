845b74a3d07f6350eae7c2477c3882648c20c5f4
Andrej Mihajlov <and@mullvad.net>
2022-09-29 18:17:42 +0200

    Add vouchers support

--- ios/MullvadVPNTests/MaskedInputFormatterTests.swift
+++ ios/MullvadVPNTests/MaskedInputFormatterTests.swift
@@ -1,5 +1,5 @@
 //
-//  AccountTokenInputTests.swift
+//  MaskedInputFormatterTests.swift
 //  MullvadVPNTests
 //
 //  Created by pronebird on 10/04/2020.
@@ -8,18 +8,45 @@
 
 import XCTest
 
-private let kSampleToken = "12345678"
+class MaskedInputFormatterTests: XCTestCase {
+    private static let sampleToken = "12345678"
 
-class AccountTokenInputTests: XCTestCase {
+    private static func makeConfiguration() -> MaskedInputFormatter.Configuration {
+        return MaskedInputFormatter.Configuration(
+            allowedInput: .numeric,
+            groupSeparator: .space,
+            groupSize: 4,
+            shouldUseAllCaps: false
+        )
+    }
+
+    func testMaxGroupCount() {
+        var configuration = Self.makeConfiguration()
+        configuration.maxGroups = 1
+
+        let input = MaskedInputFormatter(
+            string: Self.sampleToken,
+            configuration: configuration
+        )
+
+        XCTAssertEqual(input.formattedString, "1234")
+        XCTAssertEqual(input.caretPosition, 4)
+    }
+
     func testInitialValue() {
-        let input = AccountTokenInput(string: kSampleToken)
+        let input = MaskedInputFormatter(
+            string: Self.sampleToken,
+            configuration: Self.makeConfiguration()
+        )
 
         XCTAssertEqual(input.formattedString, "1234 5678")
         XCTAssertEqual(input.caretPosition, 9)
     }
 
     func testReplacingValue() {
-        let input = AccountTokenInput()
+        let input = MaskedInputFormatter(
+            configuration: Self.makeConfiguration()
+        )
         input.replace(with: "00000000")
 
         XCTAssertEqual(input.formattedString, "0000 0000")
@@ -27,7 +54,10 @@ class AccountTokenInputTests: XCTestCase {
     }
 
     func testRemovingSeparator() {
-        let input = AccountTokenInput(string: kSampleToken)
+        let input = MaskedInputFormatter(
+            string: Self.sampleToken,
+            configuration: Self.makeConfiguration()
+        )
 
         input.replaceCharacters(
             in: input.formattedString.range(withOffset: 4, length: 1),
@@ -40,7 +70,10 @@ class AccountTokenInputTests: XCTestCase {
     }
 
     func testRemovingSeparatorRange() {
-        let input = AccountTokenInput(string: kSampleToken)
+        let input = MaskedInputFormatter(
+            string: Self.sampleToken,
+            configuration: Self.makeConfiguration()
+        )
 
         input.replaceCharacters(
             in: input.formattedString.range(withOffset: 4, length: 1),
@@ -53,7 +86,10 @@ class AccountTokenInputTests: XCTestCase {
     }
 
     func testRemovingRange() {
-        let input = AccountTokenInput(string: kSampleToken)
+        let input = MaskedInputFormatter(
+            string: Self.sampleToken,
+            configuration: Self.makeConfiguration()
+        )
 
         input.replaceCharacters(
             in: input.formattedString.range(withOffset: 7, length: 2),
@@ -66,7 +102,10 @@ class AccountTokenInputTests: XCTestCase {
     }
 
     func testInserting() {
-        let input = AccountTokenInput(string: kSampleToken)
+        let input = MaskedInputFormatter(
+            string: Self.sampleToken,
+            configuration: Self.makeConfiguration()
+        )
 
         input.replaceCharacters(
             in: input.formattedString.range(withOffset: 5, length: 0),
@@ -79,7 +118,10 @@ class AccountTokenInputTests: XCTestCase {
     }
 
     func testReplacingRange() {
-        let input = AccountTokenInput(string: kSampleToken)
+        let input = MaskedInputFormatter(
+            string: Self.sampleToken,
+            configuration: Self.makeConfiguration()
+        )
 
         input.replaceCharacters(
             in: input.formattedString.range(withOffset: 5, length: 4),
@@ -93,7 +135,7 @@ class AccountTokenInputTests: XCTestCase {
 }
 
 private extension String {
-    func range(withOffset offset: String.IndexDistance, length: Int) -> Range<String.Index> {
+    func range(withOffset offset: Int, length: Int) -> Range<String.Index> {
         let start = index(startIndex, offsetBy: offset)
         let end = index(start, offsetBy: length)
 
