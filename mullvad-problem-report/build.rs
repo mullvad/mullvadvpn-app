f2f541da916f3401ad2924e189ba1bf8a12a0dca
Linus Färnstrand <faern@faern.net>
2022-09-30 15:30:32 +0200

    Adapt all crates to use mullvad-version

--- mullvad-problem-report/build.rs
+++ mullvad-problem-report/build.rs
@@ -1,20 +1,13 @@
-use std::{env, fs, path::PathBuf};
-
 #[cfg(windows)]
 fn make_lang_id(p: u16, s: u16) -> u16 {
     (s << 10) | p
 }
 
 fn main() {
-    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-
-    let product_version = env!("CARGO_PKG_VERSION").replacen(".0", "", 1);
-    fs::write(out_dir.join("product-version.txt"), &product_version).unwrap();
-
     #[cfg(windows)]
     {
         let mut res = winres::WindowsResource::new();
-        res.set("ProductVersion", &product_version);
+        res.set("ProductVersion", mullvad_version::VERSION);
         res.set_icon("../dist-assets/icon.ico");
         res.set_language(make_lang_id(
             windows_sys::Win32::System::SystemServices::LANG_ENGLISH as u16,
--- mullvad-setup/build.rs
+++
@@ -1,7 +0,0 @@
-use std::{env, fs, path::PathBuf};
-
-fn main() {
-    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    let product_version = env!("CARGO_PKG_VERSION").replacen(".0", "", 1);
-    fs::write(out_dir.join("product-version.txt"), &product_version).unwrap();
-}
