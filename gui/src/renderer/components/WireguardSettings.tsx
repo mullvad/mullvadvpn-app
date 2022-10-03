7f6148253ef73640e9aa69860b0c6e3f12691430
Oskar Nyberg <oskar@mullvad.net>
2022-10-03 09:52:47 +0200

    Handle edge cases for custom option selector

--- gui/src/renderer/components/WireguardSettings.tsx
+++ gui/src/renderer/components/WireguardSettings.tsx
@@ -162,12 +162,7 @@ function PortSelector() {
     [relaySettings],
   );
 
-  const setCustomPort = useCallback(
-    async (port: string) => {
-      await setWireguardPort(parseInt(port));
-    },
-    [setWireguardPort],
-  );
+  const parseValue = useCallback((port: string) => parseInt(port), []);
 
   const validateValue = useCallback(
     (value) => allowedPortRanges.some(([start, end]) => value >= start && value <= end),
@@ -187,9 +182,9 @@ function PortSelector() {
           items={wireguardPortItems}
           value={port}
           onSelect={setWireguardPort}
-          onSelectCustom={setCustomPort}
           inputPlaceholder={messages.pgettext('wireguard-settings-view', 'Port')}
           automaticValue={null}
+          parseValue={parseValue}
           modifyValue={removeNonNumericCharacters}
           validateValue={validateValue}
           maxLength={5}
