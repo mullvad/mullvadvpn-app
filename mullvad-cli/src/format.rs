7c514179458841f991574344c446f66483daa350
Emīls Piņķis <emils@mullvad.net>
2022-10-05 10:23:10 +0200

    Handle empty location in CLI

--- mullvad-cli/src/format.rs
+++ mullvad-cli/src/format.rs
@@ -34,7 +34,7 @@ pub fn print_state(state: &TunnelState, verbose: bool) {
 
 fn format_relay_connection(relay_info: &TunnelStateRelayInfo, verbose: bool) -> String {
     let endpoint = relay_info.tunnel_endpoint.as_ref().unwrap();
-    let location = &relay_info.location.as_ref().unwrap();
+    let location = &relay_info.location.as_ref();
 
     let prefix_separator = if verbose { "\n\t" } else { " " };
     let mut obfuscator_overlaps = false;
@@ -43,7 +43,10 @@ fn format_relay_connection(relay_info: &TunnelStateRelayInfo, verbose: bool) -> 
         let mut address = Cow::Borrowed(endpoint.address.as_str());
         let mut protocol = endpoint.protocol;
         if let Some(obfuscator) = endpoint.obfuscation.as_ref() {
-            if location.hostname == location.obfuscator_hostname {
+            if location
+                .map(|l| l.hostname == l.obfuscator_hostname)
+                .unwrap_or(false)
+            {
                 obfuscator_overlaps = true;
                 address = Cow::Owned(format!("{}:{}", obfuscator.address, obfuscator.port));
                 protocol = obfuscator.protocol;
@@ -51,11 +54,15 @@ fn format_relay_connection(relay_info: &TunnelStateRelayInfo, verbose: bool) -> 
         };
 
         let exit = format_endpoint(
-            &location.hostname,
+            location.map(|l| l.hostname.as_str()),
             protocol,
             Some(address).filter(|_| verbose).as_deref(),
         );
-        format!("{exit} in {}, {}", &location.city, &location.country)
+        if let Some(location) = location {
+            format!("{exit} in {}, {}", &location.city, &location.country)
+        } else {
+            exit
+        }
     };
 
     let first_hop = endpoint.entry_endpoint.as_ref().map(|entry| {
@@ -63,14 +70,17 @@ fn format_relay_connection(relay_info: &TunnelStateRelayInfo, verbose: bool) -> 
         let mut protocol = entry.protocol;
         if let Some(obfuscator) = endpoint.obfuscation.as_ref() {
             obfuscator_overlaps = true;
-            if location.entry_hostname == location.obfuscator_hostname {
+            if location
+                .map(|l| l.hostname == l.obfuscator_hostname)
+                .unwrap_or(false)
+            {
                 address = &obfuscator.address;
                 protocol = obfuscator.protocol;
             }
         };
 
         let endpoint = format_endpoint(
-            &location.entry_hostname,
+            location.map(|l| l.entry_hostname.as_str()),
             protocol,
             Some(address).filter(|_| verbose),
         );
@@ -80,7 +90,7 @@ fn format_relay_connection(relay_info: &TunnelStateRelayInfo, verbose: bool) -> 
     let obfuscator = endpoint.obfuscation.as_ref().map(|obfuscator| {
         if !obfuscator_overlaps {
             let endpoint_str = format_endpoint(
-                &location.obfuscator_hostname,
+                location.map(|l| l.obfuscator_hostname.as_str()),
                 obfuscator.protocol,
                 Some(obfuscator.address.as_str()).filter(|_| verbose),
             );
@@ -92,7 +102,7 @@ fn format_relay_connection(relay_info: &TunnelStateRelayInfo, verbose: bool) -> 
 
     let bridge = endpoint.proxy.as_ref().map(|proxy| {
         let proxy_endpoint = format_endpoint(
-            &location.bridge_hostname,
+            location.map(|l| l.bridge_hostname.as_str()),
             proxy.protocol,
             Some(proxy.address.as_str()).filter(|_| verbose),
         );
@@ -148,13 +158,14 @@ fn convert_obfuscator_type(obfuscator: i32) -> &'static str {
     }
 }
 
-fn format_endpoint(hostname: &String, protocol_enum: i32, addr: Option<&str>) -> String {
+fn format_endpoint(hostname: Option<&str>, protocol_enum: i32, addr: Option<&str>) -> String {
     let protocol = format_protocol(
         TransportProtocol::from_i32(protocol_enum).expect("invalid transport protocol"),
     );
     let sockaddr_suffix = addr
         .map(|addr| format!(" ({addr}/{protocol})"))
         .unwrap_or_else(String::new);
+    let hostname = hostname.or(addr).unwrap_or("unknown");
     format!("{hostname}{sockaddr_suffix}")
 }
 
