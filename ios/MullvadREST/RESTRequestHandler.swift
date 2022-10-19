b3fb876baccd450fa4a167af4c68e8b0ce791abb
sajacl <sajaclvishkai@gmail.com>
2022-10-19 15:20:39 +0200

    Moved IPEndpoint to MullvadTypes

--- ios/MullvadREST/RESTRequestHandler.swift
+++ ios/MullvadREST/RESTRequestHandler.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import MullvadTypes
 
 protocol RESTRequestHandler {
     func createURLRequest(
--- ios/MullvadVPN/RelaySelector.swift
+++ ios/MullvadVPN/RelaySelector.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import MullvadREST
+import MullvadTypes
 
 struct RelaySelectorResult: Codable {
     var endpoint: MullvadEndpoint
