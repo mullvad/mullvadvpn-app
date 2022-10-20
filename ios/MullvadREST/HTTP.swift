09a043a7b11d914d698dcc4ceaf53610fcbf2ae6
Andrej Mihajlov <and@mullvad.net>
2022-10-20 09:56:52 +0200

    Fix access levels in HTTP.swift

--- ios/MullvadREST/HTTP.swift
+++ ios/MullvadREST/HTTP.swift
@@ -10,32 +10,32 @@ import Foundation
 
 /// HTTP method
 struct HTTPMethod: RawRepresentable {
-    public static let get = HTTPMethod(rawValue: "GET")
-    public static let post = HTTPMethod(rawValue: "POST")
-    public static let delete = HTTPMethod(rawValue: "DELETE")
-    public static let put = HTTPMethod(rawValue: "PUT")
+    static let get = HTTPMethod(rawValue: "GET")
+    static let post = HTTPMethod(rawValue: "POST")
+    static let delete = HTTPMethod(rawValue: "DELETE")
+    static let put = HTTPMethod(rawValue: "PUT")
 
-    public let rawValue: String
-    public init(rawValue: String) {
+    let rawValue: String
+    init(rawValue: String) {
         self.rawValue = rawValue.uppercased()
     }
 }
 
 struct HTTPStatus: RawRepresentable, Equatable {
-    public static let notModified = HTTPStatus(rawValue: 304)
-    public static let badRequest = HTTPStatus(rawValue: 400)
-    public static let notFound = HTTPStatus(rawValue: 404)
+    static let notModified = HTTPStatus(rawValue: 304)
+    static let badRequest = HTTPStatus(rawValue: 400)
+    static let notFound = HTTPStatus(rawValue: 404)
 
-    public static func isSuccess(_ code: Int) -> Bool {
+    static func isSuccess(_ code: Int) -> Bool {
         return (200 ..< 300).contains(code)
     }
 
-    public let rawValue: Int
-    public init(rawValue: Int) {
+    let rawValue: Int
+    init(rawValue: Int) {
         self.rawValue = rawValue
     }
 
-    public var isSuccess: Bool {
+    var isSuccess: Bool {
         return Self.isSuccess(rawValue)
     }
 }
