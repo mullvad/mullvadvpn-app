a56156610f2550219cf987b95c348c1ff62c4a4d
Albin <albin@mullvad.net>
2022-11-30 16:49:26 +0100

    Clarify build command

--- android/build-container-wrapper.sh
+++ android/build-container-wrapper.sh
@@ -13,7 +13,7 @@ cd "$SCRIPT_DIR"
 CONTAINER_COMMAND=${CONTAINER_COMMAND:-"podman"}
 
 image_tag=$(cat "$REPO_DIR"/building/android-container-image-tag.txt)
-build_command="./build-apk.sh --no-docker $*"
+build_command="/build/build-apk.sh --no-docker $*"
 
-printf "Building in podman using command: %s\n\n" "$build_command"
-$CONTAINER_COMMAND run --rm -v "$REPO_DIR":/build ghcr.io/mullvad/mullvadvpn-app-build-android:"$image_tag" bash -c "$build_command"
+printf "Building in $CONTAINER_COMMAND using command: %s\n\n" "$build_command"
+$CONTAINER_COMMAND run --rm -v "$REPO_DIR":/build ghcr.io/mullvad/mullvadvpn-app-build-android:"$image_tag" /bin/bash -c "$build_command"
