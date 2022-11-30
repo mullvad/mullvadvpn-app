05e293779e6c6eb3609091ea0536ea35bd99c24a
Mullvad build server <app@mullvad.net>
2022-11-30 10:09:30 +0100

    Fix build-and-publish to retain signatures for both tags

--- build-containers/build-and-publish.sh
+++ build-containers/build-and-publish.sh
@@ -31,15 +31,49 @@ case ${1-:""} in
         exit 1
 esac
 
-log_header "Building container $container_name tagged as '$tag' and 'latest'"
+log_header "Building $container_name tagged as '$tag' and 'latest'"
 podman build "$container_context_dir" \
-    --sign-by $CONTAINER_SIGNING_KEY_FINGERPRINT \
     -t "$container_name:$tag" \
     -t "$container_name:latest"
 
-log_header "Pushing container $container_name tagged as '$tag' and 'latest'"
-podman push --sign-by $CONTAINER_SIGNING_KEY_FINGERPRINT "$container_name:$tag"
-podman push "$container_name:latest"
+# Temporary directory to store image digest and signature in.
+# This is a hack since two consecutive `podman push` seems
+# to overwrite the signatures. We want to keep the signature
+# for both the 'latest' and '$tag' tags.
+tmp_signature_dir=$(mktemp -d)
+
+function delete_tmp_signature_dir {
+    log_info "Removing $tmp_signature_dir"
+    rm -rf "$tmp_signature_dir"
+}
+trap 'delete_tmp_signature_dir' EXIT
+
+log_header "Pushing $container_name:latest'"
+podman push "$container_name:latest" \
+    --sign-by $CONTAINER_SIGNING_KEY_FINGERPRINT \
+    --digestfile "$tmp_signature_dir/digest_latest"
+
+digest=$(cat "$tmp_signature_dir/digest_latest")
+log_success "Pushed image with digest $digest"
+# Backup the signature so we can restore it after the second podman push later
+signature_dir="build-containers/sigstore/mullvad/mullvadvpn-app-build@${digest/:/=}"
+if [[ -f "$signature_dir/signature-2" ]]; then
+    log_error "Did not expect $signature_dir/signature-2 to exist"
+    exit 1
+fi
+cp "$signature_dir/signature-1" "$tmp_signature_dir/signature-2"
+
+log_header "Pushing $container_name:$tag"
+podman push "$container_name:$tag" \
+    --sign-by $CONTAINER_SIGNING_KEY_FINGERPRINT \
+    --digestfile "$tmp_signature_dir/digest_$tag"
+
+if ! cmp -s "$tmp_signature_dir/digest_latest" "$tmp_signature_dir/digest_$tag"; then
+    log_error "Digests differ between 'latest' and '$tag' pushes"
+    exit 1
+fi
+
+cp "$tmp_signature_dir/signature-2" "$signature_dir/"
 
 log_info "Storing container tag to $container_image_tag_path"
 echo "$tag" > "$container_image_tag_path"
