780afa7f4284ba83bd00d5180e5f785ea84a7b95
Albin <albin@mullvad.net>
2022-11-30 16:07:49 +0100

    Make Android build server script Android specific

--- ci/buildserver-build-android.sh
+++ ci/buildserver-build-android.sh
@@ -4,12 +4,10 @@
 #
 # * Follow the instructions in ../README.md
 # * Import and trust the GPG keys of everyone who the build server should trust code from
+# TODO: Below config is probably not relevant any longer since this script will run on
+# app-build-linux2. However, app-build-linux2 needs access to upload to releases.mullvad.net.
 # * Set up an entry in `~/.ssh/config` for app-build-linux
 # * Add the build servers public ssh key to the upload account on app-build-linux
-#
-# ## Windows
-#
-# * Add signtool.exe to your PATH: C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x64
 
 set -eu
 shopt -s nullglob
@@ -17,55 +15,18 @@ shopt -s nullglob
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILD_DIR="$SCRIPT_DIR/mullvadvpn-app"
 LAST_BUILT_DIR="$SCRIPT_DIR/last-built"
-PDB_DIR="$SCRIPT_DIR/pdb"
 UPLOAD_DIR="/home/upload/upload"
 
+# TODO: Should master still always be built?
 BRANCHES_TO_BUILD=("origin/master")
-
-case "$(uname -s)" in
-  Darwin*|MINGW*|MSYS_NT*)
-    if [[ -z ${CSC_KEY_PASSWORD-} ]]; then
-      read -sp "CSC_KEY_PASSWORD = " CSC_KEY_PASSWORD
-      echo ""
-      export CSC_KEY_PASSWORD
-    fi
-    ;;
-esac
-
-# Uploads whatever matches the first argument to the Linux build server
-upload_sftp() {
-  echo "Uploading Mullvad VPN installers to app-build-linux:upload/"
-  sftp app-build-linux <<EOF
-cd upload
-put $1
-bye
-EOF
-}
+TAG_PATTERN_TO_BUILD=("^android/")
 
 upload() {
-  for f in MullvadVPN-*.{deb,rpm,exe,pkg,apk,aab}; do
+  for f in MullvadVPN-*.{apk,aab}; do
     sha256sum "$f" > "$f.sha256"
-    case "$(uname -s)" in
       # Linux is both the build and upload server. Just move directly to target dir
-      Linux*)
-        mv "$f" "$f.sha256" "$UPLOAD_DIR/"
-        ;;
-      # Other platforms need to transfer their artifacts to the Linux build machine.
-      Darwin*|MINGW*|MSYS_NT*)
-        upload_sftp "$f" || return 1
-        upload_sftp "$f.sha256" || return 1
-        ;;
-    esac
+    mv "$f" "$f.sha256" "$UPLOAD_DIR/"
   done
-}
-
-upload_pdb() {
-  current_hash=$1
-  f="pdb-$current_hash.tar.xz"
-
-  sha256sum "$f" > "$f.sha256"
-  upload_sftp "$f" || return 1
-  upload_sftp "$f.sha256" || return 1
 }
 
 build_ref() {
@@ -102,32 +63,8 @@ build_ref() {
   git submodule update
   git clean -df
 
-  # Make sure we have the latest Rust and Node toolchains before the build
-  rustup update
-
-  BUILD_ARGS=(--optimize --sign)
-  if [[ "$(uname -s)" == "Darwin" ]]; then
-    BUILD_ARGS+=(--universal)
-  fi
-  ./build.sh "${BUILD_ARGS[@]}" || return 0
-
-  case "$(uname -s)" in
-    MINGW*|MSYS_NT*)
-      echo "Packaging all PDB files..."
-      find ./windows/ \
-        ./target/release/mullvad-daemon.pdb \
-        ./target/release/mullvad.pdb \
-        ./target/release/mullvad-problem-report.pdb \
-        -iname "*.pdb" | tar -cJf $PDB_DIR/pdb-$current_hash.tar.xz -T -
-      ;;
-    Linux*)
-      echo "Building ARM64 installers"
-      TARGETS=aarch64-unknown-linux-gnu ./build.sh "${BUILD_ARGS[@]}" || return 0
-
-      echo "Building Android APK"
-      ./build-apk.sh --app-bundle || return 0
-      ;;
-  esac
+  echo "Building Android app"
+  ./build-apk.sh --app-bundle || return 0
 
   # If there is a tag for this commit then we append that to the produced artifacts
   # A version suffix should only be created if there is a tag for this commit and it is not a release build
@@ -137,21 +74,17 @@ build_ref() {
       # Will only match paths that include *-dev-* which means release builds will not be included
       # Pipes all matching names and their new name to mv
       pushd dist
-      for original_file in MullvadVPN-*-dev-*{.deb,.rpm,.exe,.pkg,.apk,.aab}; do
-          new_file=$(echo $original_file | sed -nE "s/^(MullvadVPN-.*-dev-.*)(_amd64\.deb|_x86_64\.rpm|_arm64\.deb|_aarch64\.rpm|\.exe|\.pkg|\.apk|\.aab)$/\1$version_suffix\2/p")
+      for original_file in MullvadVPN-*-dev-*{.apk,.aab}; do
+          new_file=$(echo $original_file | sed -nE "s/^(MullvadVPN-.*-dev-.*)(\.apk|\.aab)$/\1$version_suffix\2/p")
           mv $original_file $new_file
       done
       popd
   fi
 
   (cd dist/ && upload) || return 0
-  case "$(uname -s)" in
-    MINGW*|MSYS_NT*)
-      (cd "$PDB_DIR" && upload_pdb $current_hash) || return 0
-    ;;
-  esac
+
   touch "$LAST_BUILT_DIR/$current_hash"
-  echo "Successfully finished build at $(date)"
+  echo "Successfully finished Android build at $(date)"
 }
 
 cd "$BUILD_DIR"
@@ -161,7 +94,7 @@ while true; do
   git tag | xargs git tag -d > /dev/null
 
   git fetch --prune --tags 2> /dev/null || continue
-  tags=( $(git tag) )
+  tags=( $(git tag | grep "$TAG_PATTERN_TO_BUILD") )
 
   for tag in "${tags[@]}"; do
     build_ref "refs/tags/$tag" "$tag"
