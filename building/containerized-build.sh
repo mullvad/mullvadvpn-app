142624deed8059903d081d6fc824cf3ad37986b7
Albin <albin@mullvad.net>
2022-12-01 11:36:51 +0100

    Use image name instead for containerized builds

--- building/containerized-build.sh
+++ building/containerized-build.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 # Builds the Android or Linux app in the current build container, as designated
-# by the *-container-image-tag.txt files. Uses podman unless overridden using
-# the environment variable `CONTAINER_RUNNER`. Note that this script uses named
+# by the *-container-image.txt files. Uses podman unless overridden using the
+# environment variable `CONTAINER_RUNNER`. Note that this script uses named
 # docker volumes for caching between builds (see script for more details).
 
 set -eu
 
-REGISTRY_HOST="ghcr.io"
-REGISTRY_ORG="mullvad"
 REPO_MOUNT_TARGET="/build"
 CARGO_TARGET_VOLUME_NAME="cargo-target"
 CARGO_REGISTRY_VOLUME_NAME="cargo-registry"
@@ -23,17 +21,15 @@ source "$REPO_DIR/scripts/utils/log"
 
 case ${1-:""} in
     linux)
-        container_name="mullvadvpn-app-build"
-        container_tag_path="$SCRIPT_DIR/linux-container-image-tag.txt"
+        container_image_name=$(cat "$SCRIPT_DIR/linux-container-image.txt")
         build_script="$REPO_MOUNT_TARGET/build.sh"
-        default_build_args=""
+        build_script_args="$*"
         shift 1
     ;;
     android)
-        container_name="mullvadvpn-app-build-android"
-        container_tag_path="$SCRIPT_DIR/android-container-image-tag.txt"
+        container_image_name=$(cat "$SCRIPT_DIR/android-container-image.txt")
         build_script="$REPO_MOUNT_TARGET/build-apk.sh"
-        default_build_args="--no-docker"
+        build_script_args="--no-docker $*"
         shift 1
     ;;
     *)
@@ -41,19 +37,15 @@ case ${1-:""} in
         exit 1
 esac
 
-full_container_name_with_tag="$REGISTRY_HOST/$REGISTRY_ORG/$container_name:$(cat "$container_tag_path")"
-build_script_args="$default_build_args $*"
-
-echo ""
-echo "Runner      : $CONTAINER_RUNNER"
-echo "Container   : $full_container_name_with_tag"
-echo "Build script: $build_script"
-echo "Build args  : $build_script_args"
-echo ""
+log_info ""
+log_info "Runner   : $CONTAINER_RUNNER"
+log_info "Container: $container_image_name"
+log_info "Command  : $build_script $build_script_args"
+log_info ""
 
 "$CONTAINER_RUNNER" run --rm \
     -v "$REPO_DIR":"$REPO_MOUNT_TARGET":Z \
     -v "$CARGO_TARGET_VOLUME_NAME":/root/.cargo/target:Z \
     -v "$CARGO_REGISTRY_VOLUME_NAME":/root/.cargo/registry:Z \
     -v "$GRADLE_CACHE_VOLUME_NAME":/root/.gradle:Z \
-    "$full_container_name_with_tag" "$build_script" "${build_script_args[@]}"
+    "$container_image_name" "$build_script" "${build_script_args[@]}"
