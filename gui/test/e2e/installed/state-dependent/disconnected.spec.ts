41f70e93563ce552eb722d160e64944865034e02
Oskar Nyberg <oskar@mullvad.net>
2022-12-09 18:41:41 +0100

    Add unsecured test for installed

---
+++ gui/test/e2e/installed/state-dependent/disconnected.spec.ts
@@ -0,0 +1,22 @@
+import { test } from '@playwright/test';
+import { Page } from 'playwright';
+import { assertDisconnected } from '../../shared/tunnel-state';
+
+import { startInstalledApp } from '../installed-utils';
+
+// This test expects the daemon to be logged into an account that has time left and to be
+// disconnected.
+
+let page: Page;
+
+test.beforeAll(async () => {
+  ({ page } = await startInstalledApp());
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test('App should show disconnected tunnel state', async () => {
+  await assertDisconnected(page)
+});
