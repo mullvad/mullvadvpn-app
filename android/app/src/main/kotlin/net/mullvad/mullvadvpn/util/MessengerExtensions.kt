fa9f5a0d8fe897ce65a63cee4918fcf77442c761
saber safavi <saber.safavi@codic.se>
2022-12-13 14:18:37 +0100

    Fix crash on device messenger communication

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/MessengerExtensions.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/MessengerExtensions.kt
@@ -6,9 +6,14 @@ import android.os.Messenger
 import android.os.RemoteException
 import android.util.Log
 import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.Request
 
 fun Messenger.trySendEvent(event: Event, logErrors: Boolean): Boolean {
     return trySend(event.message, logErrors, event::class.qualifiedName)
+}
+
+fun Messenger.trySendRequest(request: Request, logErrors: Boolean): Boolean {
+    return trySend(request.message, logErrors, request::class.qualifiedName)
 }
 
 private fun Messenger.trySend(message: Message, logErrors: Boolean, messageName: String?): Boolean {
