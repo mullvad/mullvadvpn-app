f1874692a0235c853018276594276579692942b1
sajacl <sajaclvishkai@gmail.com>
2022-12-15 15:45:33 +0100

    Introduce `VersionChangeLogReaderOperation`.

---
+++ ios/MullvadVPN/VersionChangeLogReaderOperation.swift
@@ -0,0 +1,37 @@
+//
+//  VersionChangeLogReaderOperation.swift
+//  MullvadVPN
+//
+//  Created by Sajad Vishkai on 2022-12-15.
+//  Copyright © 2022 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import Operations
+
+/// Change log file name.
+private let fileName = "changes.txt"
+
+final class VersionChangeLogReaderOperation: ResultOperation<[String], Error> {
+    override func main() {
+        do {
+            guard let changesFileURL = Bundle.main.url(forResource: fileName, withExtension: nil)
+            else {
+                finish(completion: .failure(URLError(.fileDoesNotExist)))
+                return
+            }
+
+            let changes = try String(contentsOf: changesFileURL, encoding: .utf8)
+            // Consider each line as a new entry.
+                .split(separator:"\n")
+            // Remove white spaces.
+                .map { $0.trimmingCharacters(in: .whitespaces) }
+            // Remove empty entries.
+                .filter { !$0.isEmpty }
+
+            finish(completion: .success(changes))
+        } catch {
+            finish(completion: .failure(error))
+        }
+    }
+}
