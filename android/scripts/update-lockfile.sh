d9c778db587600c955fe2a107d67ddc74b26ef9f
Albin <albin@mullvad.net>
2022-12-16 11:58:29 +0100

    Add script to easily update lockfile

---
+++ android/scripts/update-lockfile.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
+echo "### Updating dependency lockfile ###"
+echo ""
+
+echo "Removing old components..."
+sed -i '/<components>/,/<\/components>/d' ../gradle/verification-metadata.xml
+
+echo "Generating new components..."
+android_container_image_name=$(cat "../../building/android-container-image.txt")
+podman run --rm -it \
+    -v ../..:/build:Z \
+    "$android_container_image_name" \
+    android/gradlew -q -p android -M sha256 assembleAndroidTest
