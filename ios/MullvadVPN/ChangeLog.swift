6590c48c2f8aedc67feceb918c1d38612ec15005
sajacl <sajaclvishkai@gmail.com>
2022-12-19 12:47:09 +0100

    Update showing change log logic.

--- ios/MullvadVPN/ChangeLog.swift
+++ ios/MullvadVPN/ChangeLog.swift
@@ -10,15 +10,34 @@ import Foundation
 
 enum ChangeLog {
     /// Key to store in storage.
-    private static let key = "isShownChangeLogAlready"
+    private static let key = "changeLogPresentedForVersion"
 
     /// Value that indicates changes has been already presented to user.
     static var isShown: Bool {
-        return UserDefaults.standard.bool(forKey: key)
+        let appVersion = Bundle.main.releaseVersionNumber
+        let storedVersion = UserDefaults.standard.integer(forKey: key)
+
+        if appVersion <= storedVersion {
+            return true
+        }
+
+        return false
     }
 
-    /// Update `isShown` value in storage.
-    static func setShown(_ isShown: Bool) {
-        UserDefaults.standard.set(isShown, forKey: key)
+    /// Update version value in storage.
+    static func setVersion(_ version: Int = Bundle.main.releaseVersionNumber) {
+        UserDefaults.standard.set(version, forKey: key)
+    }
+}
+
+extension Bundle {
+    /// Product version integer value.
+    /// - Warning: It removes all the characters expect decimal digits.
+    fileprivate var releaseVersionNumber: Int {
+        return Int(
+            Bundle.main.productVersion
+                .components(separatedBy: NSCharacterSet.decimalDigits.inverted)
+                .joined(separator: "")
+        ) ?? 1
     }
 }
