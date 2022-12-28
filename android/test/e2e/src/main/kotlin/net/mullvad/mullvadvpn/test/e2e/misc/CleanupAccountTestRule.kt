d54d5c27c17ca366833b69824c1cdf1d46855ce1
Albin <albin@mullvad.net>
2022-12-28 15:00:19 +0100

    Fix CleanupAccountTestRule method signature

--- android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/misc/CleanupAccountTestRule.kt
+++ android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/misc/CleanupAccountTestRule.kt
@@ -10,8 +10,8 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 class CleanupAccountTestRule : TestWatcher() {
-    override fun starting(description: Description?) {
-        Log.d(LOG_TAG, "Cleaning up account before test: ${description?.methodName}")
+    override fun starting(description: Description) {
+        Log.d(LOG_TAG, "Cleaning up account before test: ${description.methodName}")
         val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
         val validTestAccountToken = InstrumentationRegistry.getArguments()
             .getRequiredArgument(VALID_TEST_ACCOUNT_TOKEN_ARGUMENT_KEY)
