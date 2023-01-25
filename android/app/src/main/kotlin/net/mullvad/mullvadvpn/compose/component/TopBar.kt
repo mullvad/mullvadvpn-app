e97c43abc69eb84c117c980cd683936a5392b995
Albin <albin@mullvad.net>
2023-01-25 13:48:58 +0100

    Replace compose top bar logo text with vector

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/TopBar.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/TopBar.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,10 +16,8 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import net.mullvad.mullvadvpn.R
 
@@ -58,12 +57,12 @@ fun TopBar(
                 }
         )
 
-        CapsText(
-            text = stringResource(id = R.string.app_name),
-            fontWeight = FontWeight.Bold,
-            fontSize = 24.sp,
-            color = colorResource(id = R.color.white80),
+        Icon(
+            painter = painterResource(id = R.drawable.logo_text),
+            tint = colorResource(id = R.color.white80),
+            contentDescription = null, // No meaningful user info or action.
             modifier = Modifier
+                .height(16.dp)
                 .constrainAs(appName) {
                     centerVerticallyTo(parent)
                     start.linkTo(logo.end, margin = 8.dp)
