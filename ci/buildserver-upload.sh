5919526ac94d5da2c2b38fd3118ad7f96c6c3172
Oskar Nyberg <oskar@mullvad.net>
2023-01-25 13:38:11 +0100

    Create a common sha-file for all build-files and upload them at the same time

--- ci/buildserver-upload.sh
+++ ci/buildserver-upload.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 UPLOAD_DIR="/home/upload/upload"
+BUILD_ARTIFACT_EXTENSIONS="deb|rpm|exe|pkg|apk|aab"
 
 set -eu
 shopt -s nullglob
@@ -9,39 +10,40 @@ cd $UPLOAD_DIR
 
 while true; do
   sleep 10
-  for f_checksum in MullvadVPN-*.{deb,rpm,exe,pkg,apk,aab}.sha256; do
+
+  for f_checksums in *.sha256; do
     sleep 1
-    f="${f_checksum/.sha256/}"
-    if ! sha256sum --quiet -c "$f_checksum"; then
-      echo "Failed to verify checksum for $f"
+
+    version="${f_checksums/+*/}"
+    if ! sha256sum --quiet -c "$f_checksums"; then
+      echo "Failed to verify checksums for $version"
       continue
     fi
 
-    version=$(echo "$f" | sed -Ee 's/MullvadVPN-(.*)(\.exe|\.pkg|_amd64\.deb|_x86_64\.rpm|_arm64\.deb|_aarch64\.rpm|\.apk|\.aab)/\1/g')
     if [[ $version == *"-dev-"* ]]; then
-        upload_path="builds"
+      upload_path="builds"
     else
-        upload_path="releases"
+      upload_path="releases"
     fi
 
-    rsync -av --rsh='ssh -p 1122' "$f" "build@releases.mullvad.net:$upload_path/$version/" || continue
+    files=$(awk '{print $2}' < "$f_checksums")
+    for f in $files; do
+      file_upload_dir="$upload_path/$version"
+      if [[ ! $f =~ \.($BUILD_ARTIFACT_EXTENSIONS|asc)$ ]]; then
+        file_upload_dir="$file_upload_dir/additional-files"
+      fi
 
-    rm -f "$f.asc"
-    gpg -u A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF --pinentry-mode loopback --sign --armor --detach-sign "$f"
-    rsync -av --rsh='ssh -p 1122' "$f.asc" "build@releases.mullvad.net:$upload_path/$version/" || continue
-    yes | rm "$f" "$f_checksum" "$f.asc"
-  done
+      rsync -av --rsh='ssh -p 1122' "$f" "build@releases.mullvad.net:$file_upload_dir/" || continue
 
-  # Upload PDB files (Windows debugging info)
-  for f_checksum in pdb-*.sha256; do
-    sleep 1
-    f="${f_checksum/.sha256/}"
-    if ! sha256sum --quiet -c "$f_checksum"; then
-      echo "Failed to verify checksum for $f"
-      continue
-    fi
+      if [[ $f =~ \.($BUILD_ARTIFACT_EXTENSIONS)$ ]]; then
+        rm -f "$f.asc"
+        gpg -u A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF --pinentry-mode loopback --sign --armor --detach-sign "$f"
+        rsync -av --rsh='ssh -p 1122' "$f.asc" "build@releases.mullvad.net:$file_upload_dir/" || continue
+        rm -f "$f.asc"
+      fi
+    done
 
-    rsync -av --rsh='ssh -p 1122' "$f" "build@releases.mullvad.net:builds/pdb/" || continue
-    yes | rm "$f" "$f_checksum"
+    # shellcheck disable=SC2216
+    yes | rm "${files[@]}" "$f_checksums"
   done
 done
