e9f836cc1b6259d02a2a27b6ed428e89e626a8fa
Jonathan <jonathan@mullvad.net>
2023-01-26 10:10:36 +0100

    Further cleanup

--- talpid-tunnel-config-client/src/lib.rs
+++ talpid-tunnel-config-client/src/lib.rs
@@ -1,5 +1,5 @@
 use std::{fmt, net::IpAddr};
-use talpid_types::net::wireguard::{PresharedKey, PrivateKey, PublicKey};
+use talpid_types::net::wireguard::{PresharedKey, PublicKey};
 use tonic::transport::Channel;
 use zeroize::Zeroize;
 
