4f45f74380bd95b20f98bd0a78074d5563865486
Linus Färnstrand <faern@faern.net>
2023-01-27 13:33:15 +0100

    Do explicit zeroization of key material, clean up PQ code

--- talpid-tunnel-config-client/src/classic_mceliece.rs
+++ talpid-tunnel-config-client/src/classic_mceliece.rs
@@ -1,4 +1,6 @@
-use classic_mceliece_rust::{keypair_boxed, SharedSecret};
+use classic_mceliece_rust::{
+    keypair_boxed, Ciphertext, PublicKey, SecretKey, SharedSecret, CRYPTO_CIPHERTEXTBYTES,
+};
 
 /// The `keypair_boxed` function needs just under 1 MiB of stack in debug
 /// builds. Even though it probably works to run it directly on the main
@@ -6,7 +8,9 @@ use classic_mceliece_rust::{keypair_boxed, SharedSecret};
 /// keys on a separate thread with a large enough stack.
 const STACK_SIZE: usize = 2 * 1024 * 1024;
 
-pub use classic_mceliece_rust::{Ciphertext, PublicKey, SecretKey, CRYPTO_CIPHERTEXTBYTES};
+/// Use the smallest CME variant with NIST security level 3. This variant has significantly smaller
+/// keys than the larger variants, and is considered safe.
+pub const ALGORITHM_NAME: &str = "Classic-McEliece-460896f-round3";
 
 pub async fn generate_keys() -> (PublicKey<'static>, SecretKey<'static>) {
     let (tx, rx) = tokio::sync::oneshot::channel();
@@ -22,6 +26,21 @@ pub async fn generate_keys() -> (PublicKey<'static>, SecretKey<'static>) {
     rx.await.unwrap()
 }
 
-pub fn decapsulate(secret: &SecretKey, ciphertext: &Ciphertext) -> SharedSecret<'static> {
-    classic_mceliece_rust::decapsulate_boxed(ciphertext, secret)
+pub fn decapsulate(
+    secret: &SecretKey,
+    ciphertext_slice: &[u8],
+) -> Result<SharedSecret<'static>, super::Error> {
+    let ciphertext_array =
+        <[u8; CRYPTO_CIPHERTEXTBYTES]>::try_from(ciphertext_slice).map_err(|_| {
+            super::Error::InvalidCiphertextLength {
+                algorithm: ALGORITHM_NAME,
+                actual: ciphertext_slice.len(),
+                expected: CRYPTO_CIPHERTEXTBYTES,
+            }
+        })?;
+    let ciphertext = Ciphertext::from(ciphertext_array);
+    Ok(classic_mceliece_rust::decapsulate_boxed(
+        &ciphertext,
+        secret,
+    ))
 }
