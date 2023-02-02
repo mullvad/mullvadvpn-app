99a21deebec51f73502d4fd870d95a5b535ad7d2
saber safavi <saber.safavi@codic.se>
2023-02-02 10:08:22 +0100

    Add copose theme required resources

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/theme/Colors.kt
@@ -0,0 +1,12 @@
+package net.mullvad.mullvadvpn.compose.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val MullvadBlue = Color(0xFF294D73)
+val MullvadDarkBlue = Color(0xFF192E45)
+val MullvadGreen = Color(0xFF44AD4D)
+val MullvadRed = Color(0xFFE34039)
+val MullvadYellow = Color(0xFFFFD524)
+val MullvadBrown = Color(0xFFD2943B)
+val MullvadBeige = Color(0xFFFFCD86)
