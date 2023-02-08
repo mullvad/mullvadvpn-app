721a08d012982a86b43b66be6e391e19168b1db2
Oskar Nyberg <oskar@mullvad.net>
2023-02-08 09:37:35 +0100

    fixup: Fix file removal and dont include blockmap

--- ci/buildserver-upload.sh
+++ ci/buildserver-upload.sh
@@ -14,7 +14,9 @@ while true; do
   for checksums_path in *.sha256; do
     sleep 1
 
-    version="${checksums_path/+*/}"
+    # Strip everything from the last "+" in the file name to only keep the version and tag (if
+    # present).
+    version="${checksums_path%+*}"
     if ! sha256sum --quiet -c "$checksums_path"; then
       echo "Failed to verify checksums for $version"
       continue
@@ -41,9 +43,12 @@ while true; do
         rsync -av --rsh='ssh -p 1122' "$file.asc" "build@releases.mullvad.net:$file_upload_dir/" || continue
         rm -f "$file.asc"
       fi
+
+      # shellcheck disable=SC2216
+      yes | rm "$file"
     done
 
     # shellcheck disable=SC2216
-    yes | rm "${files[@]}" "$checksums_path"
+    yes | rm "$checksums_path"
   done
 done
