7479416268f10bddb8a3c31a64d4bf079ce9058a
David Lönnhager <david.l@mullvad.net>
2023-02-22 18:11:31 +0100

    fixup

--- mullvad-nsis/build.rs
+++ mullvad-nsis/build.rs
@@ -1,11 +1,14 @@
-extern crate cbindgen;
-use std::env;
-
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let mut config: cbindgen::Config = Default::default();
-    config.language = cbindgen::Language::Cxx;
-    cbindgen::generate_with_config(&crate_dir, config)
-        .unwrap()
-        .write_to_file("include/mullvad-nsis.h");
+    #[cfg(all(target_arch = "x86", target_os = "windows"))]
+    {
+        extern crate cbindgen;
+        use std::env;
+
+        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let mut config: cbindgen::Config = Default::default();
+        config.language = cbindgen::Language::Cxx;
+        cbindgen::generate_with_config(&crate_dir, config)
+            .unwrap()
+            .write_to_file("include/mullvad-nsis.h");
+    }
 }
