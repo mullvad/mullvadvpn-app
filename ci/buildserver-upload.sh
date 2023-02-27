2b1c36b3202206af46b7f0def71f1b43acb2ad04
Linus Färnstrand <faern@faern.net>
2023-02-27 14:03:37 +0100

    Change indentation 2->4 spaces in buildserver-upload.sh

--- ci/buildserver-upload.sh
+++ ci/buildserver-upload.sh
@@ -9,46 +9,46 @@ shopt -s nullglob
 cd $UPLOAD_DIR
 
 while true; do
-  sleep 10
+    sleep 10
 
-  for checksums_path in *.sha256; do
-    sleep 1
+    for checksums_path in *.sha256; do
+        sleep 1
 
-    # Strip everything from the last "+" in the file name to only keep the version and tag (if
-    # present).
-    version="${checksums_path%+*}"
-    if ! sha256sum --quiet -c "$checksums_path"; then
-      echo "Failed to verify checksums for $version"
-      continue
-    fi
+        # Strip everything from the last "+" in the file name to only keep the version and tag (if
+        # present).
+        version="${checksums_path%+*}"
+        if ! sha256sum --quiet -c "$checksums_path"; then
+            echo "Failed to verify checksums for $version"
+            continue
+        fi
 
-    if [[ $version == *"-dev-"* ]]; then
-      upload_path="builds"
-    else
-      upload_path="releases"
-    fi
+        if [[ $version == *"-dev-"* ]]; then
+            upload_path="builds"
+        else
+            upload_path="releases"
+        fi
 
-    files=$(awk '{print $2}' < "$checksums_path")
-    for file in $files; do
-      file_upload_dir="$upload_path/$version"
-      if [[ ! $file == MullvadVPN-* ]]; then
-        file_upload_dir="$file_upload_dir/additional-files"
-      fi
+        files=$(awk '{print $2}' < "$checksums_path")
+        for file in $files; do
+            file_upload_dir="$upload_path/$version"
+            if [[ ! $file == MullvadVPN-* ]]; then
+                file_upload_dir="$file_upload_dir/additional-files"
+            fi
 
-      rsync -av --rsh='ssh -p 1122' "$file" "build@releases.mullvad.net:$file_upload_dir/" || continue
+            rsync -av --rsh='ssh -p 1122' "$file" "build@releases.mullvad.net:$file_upload_dir/" || continue
 
-      if [[ $file == MullvadVPN-* ]]; then
-        rm -f "$file.asc"
-        gpg -u A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF --pinentry-mode loopback --sign --armor --detach-sign "$file"
-        rsync -av --rsh='ssh -p 1122' "$file.asc" "build@releases.mullvad.net:$file_upload_dir/" || continue
-        rm -f "$file.asc"
-      fi
+            if [[ $file == MullvadVPN-* ]]; then
+                rm -f "$file.asc"
+                gpg -u A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF --pinentry-mode loopback --sign --armor --detach-sign "$file"
+                rsync -av --rsh='ssh -p 1122' "$file.asc" "build@releases.mullvad.net:$file_upload_dir/" || continue
+                rm -f "$file.asc"
+            fi
 
-      # shellcheck disable=SC2216
-      yes | rm "$file"
+            # shellcheck disable=SC2216
+            yes | rm "$file"
+        done
+
+        # shellcheck disable=SC2216
+        yes | rm "$checksums_path"
     done
-
-    # shellcheck disable=SC2216
-    yes | rm "$checksums_path"
-  done
 done
