930670cd54360e67b796e84e36f57c7f6aa860bc
Jon Petersson <jon.petersson@kvadrat.se>
2023-03-09 11:42:04 +0100

    WIP: Migrate from traditional to diffable data sources

--- ios/MullvadVPN/SettingsViewController.swift
+++ ios/MullvadVPN/SettingsViewController.swift
@@ -18,16 +18,15 @@ class SettingsViewController: UITableViewController, SettingsDataSourceDelegate,
     SFSafariViewControllerDelegate
 {
     weak var delegate: SettingsViewControllerDelegate?
+    private var dataSource: SettingsDataSource?
+    private let interactor: SettingsInteractor
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
 
-    private let dataSource: SettingsDataSource
-
     init(interactor: SettingsInteractor) {
-        dataSource = SettingsDataSource(interactor: interactor)
-
+        self.interactor = interactor
         super.init(style: .grouped)
     }
 
@@ -55,8 +54,105 @@ class SettingsViewController: UITableViewController, SettingsDataSourceDelegate,
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 60
 
-        dataSource.tableView = tableView
-        dataSource.delegate = self
+        dataSource = SettingsDataSource(
+            tableView: tableView,
+            interactor: interactor
+        ) { [weak self] tableView, indexPath, itemIdentifier in
+            return self?.getCell(for: itemIdentifier, indexPath: indexPath)
+        }
+        dataSource?.delegate = self
+    }
+
+    private func getCell(
+        for item: SettingsDataSource.Item,
+        indexPath: IndexPath
+    ) -> UITableViewCell {
+        switch item {
+        case .account:
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingsDataSource.CellReuseIdentifiers.accountCell.rawValue,
+                for: indexPath
+            ) as! SettingsAccountCell
+            cell.titleLabel.text = NSLocalizedString(
+                "ACCOUNT_CELL_LABEL",
+                tableName: "Settings",
+                value: "Account",
+                comment: ""
+            )
+            cell.accountExpiryDate = interactor.deviceState.accountData?.expiry
+            cell.accessibilityIdentifier = "AccountCell"
+            cell.disclosureType = .chevron
+
+            return cell
+
+        case .preferences:
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingsDataSource.CellReuseIdentifiers.basicCell.rawValue,
+                for: indexPath
+            ) as! SettingsCell
+            cell.titleLabel.text = NSLocalizedString(
+                "PREFERENCES_CELL_LABEL",
+                tableName: "Settings",
+                value: "Preferences",
+                comment: ""
+            )
+            cell.detailTitleLabel.text = nil
+            cell.accessibilityIdentifier = "PreferencesCell"
+            cell.disclosureType = .chevron
+
+            return cell
+
+        case .version:
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingsDataSource.CellReuseIdentifiers.basicCell.rawValue,
+                for: indexPath
+            ) as! SettingsCell
+            cell.titleLabel.text = NSLocalizedString(
+                "APP_VERSION_CELL_LABEL",
+                tableName: "Settings",
+                value: "App version",
+                comment: ""
+            )
+            cell.detailTitleLabel.text = Bundle.main.productVersion
+            cell.accessibilityIdentifier = nil
+            cell.disclosureType = .none
+
+            return cell
+
+        case .problemReport:
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingsDataSource.CellReuseIdentifiers.basicCell.rawValue,
+                for: indexPath
+            ) as! SettingsCell
+            cell.titleLabel.text = NSLocalizedString(
+                "REPORT_PROBLEM_CELL_LABEL",
+                tableName: "Settings",
+                value: "Report a problem",
+                comment: ""
+            )
+            cell.detailTitleLabel.text = nil
+            cell.accessibilityIdentifier = nil
+            cell.disclosureType = .chevron
+
+            return cell
+
+        case .faq:
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingsDataSource.CellReuseIdentifiers.basicCell.rawValue,
+                for: indexPath
+            ) as! SettingsCell
+            cell.titleLabel.text = NSLocalizedString(
+                "FAQ_AND_GUIDES_CELL_LABEL",
+                tableName: "Settings",
+                value: "FAQ & Guides",
+                comment: ""
+            )
+            cell.detailTitleLabel.text = nil
+            cell.accessibilityIdentifier = nil
+            cell.disclosureType = .externalLink
+
+            return cell
+        }
     }
 
     // MARK: - IBActions
