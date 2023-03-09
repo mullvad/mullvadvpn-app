8fa2d43bd74ca7364245ad0bc3c491876b035c0a
Jon Petersson <jon.petersson@kvadrat.se>
2023-03-09 11:42:04 +0100

    Migrate SettingsDataSource from traditional to diffable

---
+++ ios/MullvadVPN/SettingsCellFactory.swift
@@ -0,0 +1,113 @@
+//
+//  SettingsCellFactory.swift
+//  MullvadVPN
+//
+//  Created by Jon Petersson on 2023-03-09.
+//  Copyright © 2023 Mullvad VPN AB. All rights reserved.
+//
+
+import UIKit
+
+struct SettingsCellFactory: CellFactoryProtocol {
+    let tableView: UITableView
+    private let interactor: SettingsInteractor
+
+    init(tableView: UITableView, interactor: SettingsInteractor) {
+        self.tableView = tableView
+        self.interactor = interactor
+    }
+
+    func makeCell(for item: SettingsDataSource.Item, indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+
+        switch item {
+        case .account:
+            cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingsDataSource.CellReuseIdentifiers.accountCell.rawValue,
+                for: indexPath
+            )
+        default:
+            cell = tableView.dequeueReusableCell(
+                withIdentifier: SettingsDataSource.CellReuseIdentifiers.basicCell.rawValue,
+                for: indexPath
+            )
+        }
+
+        configureCell(cell, item: item, indexPath: indexPath)
+
+        return cell
+    }
+
+    func configureCell(
+        _ cell: UITableViewCell,
+        item: SettingsDataSource.Item,
+        indexPath: IndexPath
+    ) {
+        switch item {
+        case .account:
+            guard let cell = cell as? SettingsAccountCell else { return }
+
+            cell.titleLabel.text = NSLocalizedString(
+                "ACCOUNT_CELL_LABEL",
+                tableName: "Settings",
+                value: "Account",
+                comment: ""
+            )
+            cell.accountExpiryDate = interactor.deviceState.accountData?.expiry
+            cell.accessibilityIdentifier = "AccountCell"
+            cell.disclosureType = .chevron
+
+        case .preferences:
+            guard let cell = cell as? SettingsCell else { return }
+
+            cell.titleLabel.text = NSLocalizedString(
+                "PREFERENCES_CELL_LABEL",
+                tableName: "Settings",
+                value: "Preferences",
+                comment: ""
+            )
+            cell.detailTitleLabel.text = nil
+            cell.accessibilityIdentifier = "PreferencesCell"
+            cell.disclosureType = .chevron
+
+        case .version:
+            guard let cell = cell as? SettingsCell else { return }
+
+            cell.titleLabel.text = NSLocalizedString(
+                "APP_VERSION_CELL_LABEL",
+                tableName: "Settings",
+                value: "App version",
+                comment: ""
+            )
+            cell.detailTitleLabel.text = Bundle.main.productVersion
+            cell.accessibilityIdentifier = nil
+            cell.disclosureType = .none
+
+        case .problemReport:
+            guard let cell = cell as? SettingsCell else { return }
+
+            cell.titleLabel.text = NSLocalizedString(
+                "REPORT_PROBLEM_CELL_LABEL",
+                tableName: "Settings",
+                value: "Report a problem",
+                comment: ""
+            )
+            cell.detailTitleLabel.text = nil
+            cell.accessibilityIdentifier = nil
+            cell.disclosureType = .chevron
+
+        case .faq:
+            guard let cell = cell as? SettingsCell else { return }
+
+            cell.titleLabel.text = NSLocalizedString(
+                "FAQ_AND_GUIDES_CELL_LABEL",
+                tableName: "Settings",
+                value: "FAQ & Guides",
+                comment: ""
+            )
+            cell.detailTitleLabel.text = nil
+            cell.accessibilityIdentifier = nil
+            cell.disclosureType = .externalLink
+        }
+    }
+}
