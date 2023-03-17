526bb7ec00c17a3d69e04962ed5331428a4597bf
saber safavi <saber.safavi@codic.se>
2023-03-17 15:15:49 +0100

    Migrate advanced settings fragment to use composified view

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/CustomDnsItemHolder.kt
+++
@@ -1,6 +0,0 @@
-package net.mullvad.mullvadvpn.ui.customdns
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-
-abstract class CustomDnsItemHolder(view: View) : ViewHolder(view)
