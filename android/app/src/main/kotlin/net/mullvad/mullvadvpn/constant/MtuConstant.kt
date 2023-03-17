6e15ab43e1620a5fa291322860127818b8de0cbe
saber safavi <saber.safavi@codic.se>
2023-03-17 15:15:49 +0100

    Add advanced settings vm and repo

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/MtuConstant.kt
@@ -0,0 +1,4 @@
+package net.mullvad.mullvadvpn.constant
+
+const val MTU_MIN_VALUE = 1280
+const val MTU_MAX_VALUE = 1420
---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/AdvancedSettingsViewModelState.kt
@@ -0,0 +1,100 @@
+package net.mullvad.mullvadvpn.viewmodel
+
+import net.mullvad.mullvadvpn.compose.state.AdvancedSettingsUiState
+
+data class AdvancedSettingsViewModelState(
+    val mtuValue: String,
+    val isCustomDnsEnabled: Boolean,
+    val isAllowLanEnabled: Boolean,
+    val customDnsList: List<CustomDnsItem>,
+    val dialogState: AdvancedSettingsDialogState
+) {
+    fun toUiState(): AdvancedSettingsUiState {
+        return when (dialogState) {
+            is AdvancedSettingsDialogState.MtuDialog -> AdvancedSettingsUiState.MtuDialogUiState(
+                mtu = mtuValue,
+                isCustomDnsEnabled = isCustomDnsEnabled,
+                isAllowLanEnabled = isAllowLanEnabled,
+                customDnsItems = customDnsList,
+                mtuEditValue = dialogState.mtuEditValue,
+            )
+            is AdvancedSettingsDialogState.DnsDialog -> AdvancedSettingsUiState.DnsDialogUiState(
+                mtu = mtuValue,
+                isCustomDnsEnabled = isCustomDnsEnabled,
+                isAllowLanEnabled = isAllowLanEnabled,
+                customDnsItems = customDnsList,
+                stagedDns = dialogState.stagedDns,
+            )
+            else -> AdvancedSettingsUiState.DefaultUiState(
+                mtu = mtuValue,
+                isCustomDnsEnabled = isCustomDnsEnabled,
+                isAllowLanEnabled = isAllowLanEnabled,
+                customDnsItems = customDnsList,
+            )
+        }
+    }
+
+    companion object {
+        private const val EMPTY_STRING = ""
+
+        fun default() = AdvancedSettingsViewModelState(
+            mtuValue = EMPTY_STRING,
+            isCustomDnsEnabled = false,
+            customDnsList = listOf(),
+            isAllowLanEnabled = false,
+            dialogState = AdvancedSettingsDialogState.NoDialog
+        )
+    }
+}
+
+sealed class AdvancedSettingsDialogState {
+    object NoDialog : AdvancedSettingsDialogState()
+
+    data class MtuDialog(
+        val mtuEditValue: String
+    ) : AdvancedSettingsDialogState()
+
+    data class DnsDialog(
+        val stagedDns: StagedDns
+    ) : AdvancedSettingsDialogState()
+}
+
+sealed interface StagedDns {
+    val item: CustomDnsItem
+    val validationResult: ValidationResult
+
+    data class NewDns(
+        override val item: CustomDnsItem,
+        override val validationResult: ValidationResult = ValidationResult.Success,
+    ) : StagedDns
+
+    data class EditDns(
+        override val item: CustomDnsItem,
+        override val validationResult: ValidationResult = ValidationResult.Success,
+        val index: Int
+    ) : StagedDns
+
+    sealed class ValidationResult {
+        object Success : ValidationResult()
+        object InvalidAddress : ValidationResult()
+        object DuplicateAddress : ValidationResult()
+    }
+
+    fun isValid() = (validationResult is ValidationResult.Success)
+}
+
+data class CustomDnsItem(
+    val address: String,
+    val isLocal: Boolean
+) {
+    companion object {
+        private const val EMPTY_STRING = ""
+
+        fun default(): CustomDnsItem {
+            return CustomDnsItem(
+                address = EMPTY_STRING,
+                isLocal = false
+            )
+        }
+    }
+}
