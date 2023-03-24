16b7f45c01df13a275b9e61d875cf7d726470dd5
saber safavi <saber.safavi@codic.se>
2023-03-24 16:19:29 +0100

    Add compose info dialog

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/ContentBlockersInfoDialog.kt
@@ -0,0 +1,15 @@
+package net.mullvad.mullvadvpn.compose.dialog
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import net.mullvad.mullvadvpn.R
+
+@Composable
+fun ContentBlockersInfoDialog(onDismiss: () -> Unit) {
+    InfoDialog(
+        message =
+        "${stringResource(id = R.string.dns_content_blockers_info)} \n\n\n " +
+            "${stringResource(id = R.string.dns_content_blockers_warning)}",
+        onDismiss = onDismiss
+    )
+}
