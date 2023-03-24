9e21d3fc7d91d40e1269851351f408d75240ef44
Andrej Mihajlov <and@mullvad.net>
2023-03-24 13:13:00 +0100

    Add changelog

---
+++ ios/MullvadVPN/Coordinators/App/ChangeLogCoordinator.swift
@@ -0,0 +1,46 @@
+//
+//  ChangeLogCoordinator.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 24/03/2023.
+//  Copyright © 2023 Mullvad VPN AB. All rights reserved.
+//
+
+import MullvadLogging
+import UIKit
+
+final class ChangeLogCoordinator: Coordinator {
+    private let logger = Logger(label: "ChangeLogCoordinator")
+
+    let navigationController: RootContainerViewController
+
+    var didFinish: ((ChangeLogCoordinator) -> Void)?
+
+    init(navigationController: RootContainerViewController) {
+        self.navigationController = navigationController
+    }
+
+    func start(animated: Bool) {
+        let controller = ChangeLogViewController()
+
+        controller.setApplicationVersion(Bundle.main.shortVersion)
+
+        do {
+            let string = try ChangeLog.readFromFile()
+
+            controller.setChangeLogText(string)
+        } catch {
+            logger.error(error: error, message: "Cannot read changelog from bundle.")
+        }
+
+        controller.onFinish = { [weak self] in
+            guard let self = self else { return }
+
+            ChangeLog.markAsSeen()
+
+            self.didFinish?(self)
+        }
+
+        navigationController.pushViewController(controller, animated: animated)
+    }
+}
