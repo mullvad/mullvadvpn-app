363a9897d5c4542dd04b77a1187b687375d99799
Albin <albin@mullvad.net>
2023-04-05 11:50:28 +0200

    Deprecate daemon provided app version property

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/VersionInfo.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/VersionInfo.kt
@@ -1,7 +1,7 @@
 package net.mullvad.mullvadvpn.ui
 
 data class VersionInfo(
-    val currentVersion: String?,
+    @Deprecated(message = "Use BuildConfig.VERSION_NAME") val currentVersion: String?,
     val upgradeVersion: String?,
     val isOutdated: Boolean,
     val isSupported: Boolean
