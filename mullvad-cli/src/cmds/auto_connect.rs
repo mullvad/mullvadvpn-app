03ce25f849d3e4709950c71739ba60af32dbc8b8
David Lönnhager <david.l@mullvad.net>
2023-04-07 01:30:34 +0200

    Overthink boolean options

--- mullvad-cli/src/cmds/auto_connect.rs
+++ mullvad-cli/src/cmds/auto_connect.rs
@@ -2,17 +2,14 @@ use anyhow::Result;
 use clap::Subcommand;
 use mullvad_management_interface::MullvadProxyClient;
 
-use super::on_off_parser;
+use super::BooleanOption;
 
 #[derive(Subcommand, Debug)]
 pub enum AutoConnect {
     /// Display the current auto-connect setting
     Get,
     /// Change auto-connect setting
-    Set {
-        #[arg(value_parser = on_off_parser())]
-        policy: bool,
-    },
+    Set { policy: BooleanOption },
 }
 
 impl AutoConnect {
@@ -23,17 +20,17 @@ impl AutoConnect {
         }
     }
 
-    async fn set(policy: bool) -> Result<()> {
+    async fn set(policy: BooleanOption) -> Result<()> {
         let mut rpc = MullvadProxyClient::new().await?;
-        rpc.set_auto_connect(policy).await?;
+        rpc.set_auto_connect(*policy).await?;
         println!("Changed auto-connect setting");
         Ok(())
     }
 
     async fn get() -> Result<()> {
         let mut rpc = MullvadProxyClient::new().await?;
-        let auto_connect = rpc.get_settings().await?.auto_connect;
-        println!("Autoconnect: {}", if auto_connect { "on" } else { "off" });
+        let auto_connect = BooleanOption::from(rpc.get_settings().await?.auto_connect);
+        println!("Autoconnect: {auto_connect}");
         Ok(())
     }
 }
--- mullvad-cli/src/cmds/mod.rs
+++ mullvad-cli/src/cmds/mod.rs
@@ -1,4 +1,5 @@
 use clap::builder::{PossibleValuesParser, TypedValueParser, ValueParser};
+use std::ops::Deref;
 
 pub mod account;
 pub mod auto_connect;
@@ -18,13 +19,62 @@ pub mod tunnel_state;
 pub mod version;
 
 /// A value parser that parses "on" or "off" into a boolean
-fn on_off_parser() -> ValueParser {
-    on_off_parser_custom("on", "off")
+#[derive(Debug, Clone, Copy)]
+pub struct BooleanOption {
+    state: bool,
+    on_label: &'static str,
+    off_label: &'static str,
 }
 
-/// A value parser that parses `on_label` into true, and `off_label` into false
-fn on_off_parser_custom(on_label: &'static str, off_label: &'static str) -> ValueParser {
-    ValueParser::new(
-        PossibleValuesParser::new([on_label, off_label]).map(move |val| val == on_label),
-    )
+impl Deref for BooleanOption {
+    type Target = bool;
+
+    fn deref(&self) -> &Self::Target {
+        &self.state
+    }
+}
+
+impl clap::builder::ValueParserFactory for BooleanOption {
+    type Parser = ValueParser;
+
+    /// A value parser that parses "on" or "off" into a `BooleanOption`
+    fn value_parser() -> Self::Parser {
+        Self::custom_parser("on", "off")
+    }
+}
+
+impl BooleanOption {
+    /// A value parser that parses `on_label` and `off_label` into a `BooleanOption`
+    fn custom_parser(on_label: &'static str, off_label: &'static str) -> ValueParser {
+        assert!(on_label != off_label);
+
+        ValueParser::new(
+            PossibleValuesParser::new([on_label, off_label])
+                .map(move |val| Self::with_labels(val == on_label, on_label, off_label)),
+        )
+    }
+
+    fn with_labels(state: bool, on_label: &'static str, off_label: &'static str) -> Self {
+        Self {
+            state,
+            on_label,
+            off_label,
+        }
+    }
+}
+
+impl From<bool> for BooleanOption {
+    fn from(state: bool) -> Self {
+        Self::with_labels(state, "on", "off")
+    }
+}
+
+impl std::fmt::Display for BooleanOption {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.state {
+            self.on_label.fmt(f)
+        } else {
+            self.off_label.fmt(f)
+        }
+    }
 }
