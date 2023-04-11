e5b09bd77cb0a85b6b793377638b1d49a178b531
Albin <albin@mullvad.net>
2023-04-11 10:36:09 +0200

    Fix chevron animation

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/Chevron.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/Chevron.kt
@@ -6,26 +6,26 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.R
 
 @Composable
 fun ChevronView(isExpanded: Boolean) {
     val resourceId = R.drawable.icon_chevron
     val rotation = remember { Animatable(90f) }
-    rememberCoroutineScope().let {
-        it.launch {
-            rotation.animateTo(
-                targetValue = 90f + if (isExpanded) 0f else 180f,
-                animationSpec = tween(100, easing = LinearEasing),
-            )
-        }
+
+    LaunchedEffect(isExpanded) {
+        rotation.animateTo(
+            targetValue = 90f + if (isExpanded) 0f else 180f,
+            animationSpec = tween(100, easing = LinearEasing)
+        )
     }
 
     Image(
