85fc6a73e657626e878f5fa2b4860aaaa02833c9
Mojgan <Mojgan.jelodar@codic.se>
2023-04-18 14:59:30 +0200

    Show the remained days when the time left is less than 2 years

--- ios/MullvadVPNTests/CustomDateComponentsFormattingTests.swift
+++ ios/MullvadVPNTests/CustomDateComponentsFormattingTests.swift
@@ -43,39 +43,6 @@ class CustomDateComponentsFormattingTests: XCTestCase {
         XCTAssertEqual(result, "\(expectedDays) days")
     }
 
-    func testCloseToOneDayFormatting() throws {
-        var dateComponents = DateComponents()
-        dateComponents.hour = 23
-        dateComponents.minute = 30
-
-        let (startDate, endDate) = makeDateRange(addingComponents: dateComponents)
-
-        let result = CustomDateComponentsFormatting.localizedString(
-            from: startDate,
-            to: endDate,
-            calendar: calendar,
-            unitsStyle: .full
-        )
-
-        XCTAssertEqual(result, "1 day")
-    }
-
-    func testLessThanOneMinuteFormatting() throws {
-        var dateComponents = DateComponents()
-        dateComponents.second = 59
-
-        let (startDate, endDate) = makeDateRange(addingComponents: dateComponents)
-
-        let result = CustomDateComponentsFormatting.localizedString(
-            from: startDate,
-            to: endDate,
-            calendar: calendar,
-            unitsStyle: .full
-        )
-
-        XCTAssertEqual(result, "Less than a minute")
-    }
-
     private func makeDateRange(addingComponents dateComponents: DateComponents) -> (Date, Date) {
         let startDate = Date()
         let endDate = calendar.date(byAdding: dateComponents, to: startDate)!
