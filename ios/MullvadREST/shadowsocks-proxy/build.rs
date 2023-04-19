e0a4c3e67e1193d3e005be08ff5bd92b04c93a7c
Emīls <emils@mullvad.net>
2023-04-19 18:45:38 +0100

    Improve rust crate

--- ios/MullvadREST/shadowsocks-proxy/build.rs
+++ ios/MullvadREST/shadowsocks-proxy/build.rs
@@ -1,3 +1,4 @@
+#[cfg(any(target_os = "macos", target_os = "ios"))]
 fn main() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
@@ -8,3 +9,6 @@ fn main() {
         .expect("failed to generate bindings")
         .write_to_file("include/shadowsocks.h");
 }
+
+#[cfg(not(any(target_os = "macos", target_os = "ios")))]
+fn main() {}
--- ios/MullvadREST/shadowsocks-proxy/src/lib.rs
+++ ios/MullvadREST/shadowsocks-proxy/src/lib.rs
@@ -7,15 +7,13 @@ use shadowsocks_service::{
 };
 use std::{
     io,
-    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, TcpListener},
+    net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener},
     str::FromStr,
-    sync::Once,
 };
 use tokio::sync::oneshot;
 
-use shadowsocks_service;
-
-const INIT_LOGGING: Once = Once::new();
+mod ffi;
+pub use ffi::{start_shadowsocks_proxy, stop_shadowsocks_proxy, ProxyHandle};
 
 pub fn run_http_proxy(
     bridge_addr: SocketAddr,
@@ -29,12 +27,6 @@ pub fn run_http_proxy(
     Ok((port, handle))
 }
 
-#[repr(C)]
-pub struct ProxyHandle {
-    pub context: *mut libc::c_void,
-    pub port: u16,
-}
-
 struct ShadowsocksRuntime {
     runtime: tokio::runtime::Runtime,
     config: Config,
@@ -42,12 +34,14 @@ struct ShadowsocksRuntime {
 }
 
 pub struct ShadowsocksHandle {
-    tx: oneshot::Sender<()>,
+    tx: oneshot::Sender<oneshot::Sender<()>>,
 }
 
 impl ShadowsocksHandle {
     pub fn stop(self) {
-        let _ = self.tx.send(());
+        let (shutdown_tx, shutdown_rx) = oneshot::channel();
+        let _ = self.tx.send(shutdown_tx);
+        let _ = shutdown_rx.blocking_recv();
     }
 }
 
@@ -71,6 +65,7 @@ impl ShadowsocksRuntime {
 
     pub fn run(self) -> io::Result<ShadowsocksHandle> {
         let (tx, rx) = oneshot::channel();
+        let (shutdown_tx, shutdown_rx) = oneshot::channel();
         let (startup_tx, startup_rx) = oneshot::channel();
         std::thread::spawn(move || {
             self.run_service_inner(rx, startup_tx);
@@ -79,11 +74,13 @@ impl ShadowsocksRuntime {
         match startup_rx.blocking_recv() {
             Ok(Ok(_)) => Ok(ShadowsocksHandle { tx }),
             Ok(Err(err)) => {
-                let _ = tx.send(());
+                let _ = tx.send(shutdown_tx);
+                let _ = shutdown_rx.blocking_recv();
                 Err(err)
             }
             Err(_) => {
-                let _ = tx.send(());
+                let _ = tx.send(shutdown_tx);
+                let _ = shutdown_rx.blocking_recv();
                 Err(io::Error::new(
                     io::ErrorKind::Other,
                     "Tokio runtime crashed",
@@ -94,7 +91,7 @@ impl ShadowsocksRuntime {
 
     fn run_service_inner(
         self,
-        rx: oneshot::Receiver<()>,
+        shutdown_rx: oneshot::Receiver<oneshot::Sender<()>>,
         startup_done_tx: oneshot::Sender<io::Result<()>>,
     ) {
         let Self {
@@ -113,7 +110,10 @@ impl ShadowsocksRuntime {
                     }
                 }
             });
-            let _ = runtime.block_on(rx);
+            if let Ok(shutdown_tx) = runtime.block_on(shutdown_rx) {
+                std::mem::drop(runtime);
+                let _ = shutdown_tx.send(());
+            }
         });
     }
 
@@ -151,114 +151,8 @@ impl ShadowsocksRuntime {
     }
 }
 
-#[no_mangle]
-pub extern "C" fn start_shadowsocks_proxy(
-    addr: *const u8,
-    addr_len: usize,
-    port: u16,
-    password: *const u8,
-    password_len: usize,
-    cipher: *const u8,
-    cipher_len: usize,
-    proxy_config: *mut ProxyHandle,
-) -> i32 {
-    INIT_LOGGING.call_once(|| {
-        oslog::OsLogger::new("net.mullvad.MullvadVPN.HTTPProxy")
-            .level_filter(log::LevelFilter::Debug)
-            .init()
-            .unwrap();
-    });
-
-    let bridge_ip = if let Some(addr) = unsafe { parse_ip_addr(addr, addr_len) } {
-        addr
-    } else {
-        return -1;
-    };
-
-    let bridge_addr = SocketAddr::new(bridge_ip, port);
-
-    let password = if let Some(password) = unsafe { parse_str(password, password_len) } {
-        password
-    } else {
-        return -1;
-    };
-
-    let cipher = if let Some(cipher) = unsafe { parse_str(cipher, cipher_len) } {
-        cipher
-    } else {
-        return -1;
-    };
-
-    let (port, handle) = match run_http_proxy(bridge_addr, &password, &cipher) {
-        Ok((port, handle)) => (port, handle),
-        Err(err) => {
-            log::error!("Failed to run HTTP proxy {}", err);
-            return err.raw_os_error().unwrap_or(-1);
-        }
-    };
-    let handle = Box::new(handle);
-
-    unsafe {
-        std::ptr::write(
-            proxy_config,
-            ProxyHandle {
-                port,
-                context: Box::into_raw(handle) as *mut _,
-            },
-        )
-    }
-
-    0
-}
-
-#[no_mangle]
-pub extern "C" fn stop_shadowsocks_proxy(proxy_config: *mut ProxyHandle) -> i32 {
-    let context_ptr = unsafe { (*proxy_config).context };
-    if context_ptr.is_null() {
-        return -1;
-    }
-
-    let proxy_handle: Box<ShadowsocksHandle> = unsafe { Box::from_raw(context_ptr as *mut _) };
-    proxy_handle.stop();
-    0
-}
-
 fn get_free_port() -> io::Result<u16> {
     let bind_addr: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 0);
     let port = TcpListener::bind(bind_addr)?.local_addr()?.port();
     Ok(port)
-}
-
-/// Constructs a new IP address from a pointer containing bytes representing an IP address.
-///
-/// SAFETY: `addr` must be a pointer to at least `addr_len` bytes.
-unsafe fn parse_ip_addr(addr: *const u8, addr_len: usize) -> Option<IpAddr> {
-    match addr_len {
-        4 => {
-            // SAFETY: addr pointer must point to at least addr_len bytes
-            let bytes = unsafe { std::slice::from_raw_parts(addr, addr_len) };
-            Some(Ipv4Addr::new(bytes[0], bytes[1], bytes[2], bytes[3]).into())
-        }
-        16 => {
-            // SAFETY: addr pointer must point to at least addr_len bytes
-            let bytes = unsafe { std::slice::from_raw_parts(addr, addr_len) };
-            let mut addr_arr = [0u8; 16];
-            addr_arr.as_mut_slice().copy_from_slice(&bytes);
-
-            Some(Ipv6Addr::from(addr_arr).into())
-        }
-        anything_else => {
-            log::error!("Bad IP address length {anything_else}");
-            None
-        }
-    }
-}
-
-/// Allocates a new string with the contents of `data` if it contains only valid UTF-8 bytes.
-///
-/// SAFETY: `data` must be a valid pointer to `len` amount of bytes
-unsafe fn parse_str(data: *const u8, len: usize) -> Option<String> {
-    // SAFETY: data pointer must be valid for the size of len
-    let bytes = unsafe { std::slice::from_raw_parts(data, len) };
-    String::from_utf8(bytes.to_vec()).ok()
 }
