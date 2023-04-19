d7d05e8b6e163a46f9405661167df384ecb9d1d7
Emīls <emils@mullvad.net>
2023-04-19 11:08:52 +0100

    Add shadowsocks-proxy crate

---
+++ ios/MullvadREST/shadowsocks-proxy/src/bin/run.rs
@@ -0,0 +1,18 @@
+use std::{net::SocketAddr, str::FromStr};
+
+fn main() {
+    let socketaddr = SocketAddr::from_str("185.65.135.117:443").unwrap();
+    let password = "mullvad";
+    let cipher = "aes-256-gcm";
+
+    let (port, handle) =
+        shadowsocks_proxy::run_socks(socketaddr, password, cipher).expect("failed to start SOCKS proxy");
+
+    println!("Running proxy on port {port}");
+
+    let _ = std::io::stdin().read_line(&mut String::new());
+    println!("Stopping proxy");
+    handle.stop();
+    println!("Done");
+}
+
---
+++ ios/MullvadREST/shadowsocks-proxy/src/bin/run_unsafe.rs
@@ -0,0 +1,50 @@
+use std::{
+    net::{Ipv4Addr, SocketAddr},
+    str::FromStr,
+};
+
+
+fn main() {
+    let socketaddr = SocketAddr::from_str("185.65.135.117:443").unwrap();
+    let password = "mullvad";
+    let cipher = "aes-256-gcm";
+
+    let cipher_ptr = cipher.as_ptr();
+    let cipher_size = cipher.as_bytes().len();
+
+    let password_ptr = password.as_ptr();
+    let password_size = password.as_bytes().len();
+
+    let addr = Ipv4Addr::from_str("185.65.135.117").unwrap();
+    let addr_bytes = addr.octets();
+    let addr_ptr = addr_bytes.as_ptr();
+
+    let mut ctx = shadowsocks_proxy::ProxyHandle {
+        port: 0,
+        context: std::ptr::null_mut(),
+    };
+
+    let retval = shadowsocks_proxy::start_shadowsocks_proxy(
+        addr_ptr,
+        addr_bytes.len(),
+        socketaddr.port(),
+        password_ptr,
+        password_size,
+        cipher_ptr,
+        cipher_size,
+        &mut ctx as *mut _,
+    );
+    if retval != 0 {
+        println!("Failed to start proxy - {retval}");
+        return;
+    }
+
+    println!("Running proxy on port {}", ctx.port);
+    let _ = std::io::stdin().read_line(&mut String::new());
+    println!("Stopping proxy");
+    let retval = shadowsocks_proxy::stop_shadowsocks_proxy(&mut ctx as *mut _);
+    if retval != 0 {
+        println!("Failed to stop proxy");
+    }
+    println!("Done");
+}
