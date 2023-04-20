1bc67c14215ef1e11ec0a2c2a1aeee13b74a4e17
Bug Magnet <marco.nikic@mullvad.net>
2023-04-20 09:14:41 +0200

    Create start and stop methods for the HTTP Socks proxy

--- ios/MullvadREST/HttpProxy.swift
+++ ios/MullvadREST/HttpProxy.swift
@@ -12,20 +12,43 @@ import Network
 
 public class HttpProxy {
     private let proxyConfig: ProxyHandle
+    private let addr: IPAddress
+    private let password: String
+    private let cipher: String
     
     public init(addr: IPAddress, port: UInt16, password: String, cipher: String ) {
         // TODO() make the FFI call
-        self.proxyConfig = ProxyHandle.init(context: nil, port: 0)
+        self.proxyConfig = ProxyHandle(context: nil, port: port)
+        self.addr = addr
+        self.password = password
+        self.cipher = cipher
     }
     
     func port() -> UInt16 {
         self.proxyConfig.port
     }
     
-    deinit {
+    /// Start the socks proxy
+    func start() {
+        // Get the raw bytes of `addr.rawValue`
+        addr.rawValue.withUnsafeBytes { unsafeAddressPointer in
+            
+            // Rebind the raw bytes to an array of bytes, and get a pointer to its beginning
+            let rawAddr = unsafeAddressPointer.bindMemory(to: UInt8.self).baseAddress
+            
+            // Get the raw bytes access to  `proxyConfig`
+            withUnsafePointer(to: proxyConfig) { config in
+                let configPointer = UnsafeMutablePointer(mutating: config)
+                start_shadowsocks_proxy(rawAddr, UInt(addr.rawValue.count), port(), password, UInt(password.count), cipher, UInt(cipher.count), configPointer)
+            }
+        }
+    }
+    
+    /// Stop the socks proxy
+    func stop() {
         let _ = withUnsafePointer(to: self.proxyConfig) { pointer in
             stop_shadowsocks_proxy(UnsafeMutablePointer(mutating: pointer))
         }
     }
-    
 }
+
