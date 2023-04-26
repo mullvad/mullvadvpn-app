6707f4076aa0fb325bfad116890a31cb43ec71d8
Andrej Mihajlov <and@mullvad.net>
2023-04-26 19:15:51 +0200

    PacketTunnel: rotate private key on mismatch with server

---
+++ ios/MullvadVPN/TunnelManager/WgKeyRotation.swift
@@ -0,0 +1,124 @@
+//
+//  WgKeyRotation.swift
+//  MullvadVPN
+//
+//  Created by pronebird on 24/05/2023.
+//  Copyright © 2023 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import struct MullvadTypes.Device
+import class WireGuardKitTypes.PrivateKey
+import class WireGuardKitTypes.PublicKey
+
+/**
+ A simple manager that implements manipulations related to marking the beginning and the completion of key rotation,
+ private key creation and other tasks relevant to handling the state of key rotation.
+ */
+struct WgKeyRotation {
+    /// Mutated device data value.
+    private(set) var data: StoredDeviceData
+
+    /// Initialize object with `StoredDeviceData` that the struct is going to manipulate.
+    init(data: StoredDeviceData) {
+        self.data = data
+    }
+
+    /**
+     Begin key rotation attempt by marking last rotation attempt and creating next private key if needed.
+     If the next private key was created during the preivous rotation attempt then continue using the same key.
+
+     Returns the public key that should be pushed to the backend.
+     */
+    mutating func beginAttempt() -> PublicKey {
+        // Mark the rotation attempt.
+        data.wgKeyData.lastRotationAttemptDate = Date()
+
+        // Fetch the next private key we're attempting to rotate to.
+        if let nextPrivateKey = data.wgKeyData.nextPrivateKey {
+            return nextPrivateKey.publicKey
+        } else {
+            // If not found then create a new one and store it.
+            let newKey = PrivateKey()
+            data.wgKeyData.nextPrivateKey = newKey
+            return newKey.publicKey
+        }
+    }
+
+    /**
+     Successfuly finish key rotation by swapping the current key with the next one, marking key creation date and
+     removing the date of last rotation attempt which indicates that the last rotation had succedeed and no new
+     rotation attempts were made.
+
+     Device related properties are refreshed from `Device` struct that the caller should have received from the API.
+     */
+    mutating func setCompleted(with updatedDevice: Device) {
+        guard let nextKey = data.wgKeyData.nextPrivateKey else { return }
+
+        // Update stored device data with properties from updated `Device` struct.
+        data.update(from: updatedDevice)
+
+        // Reset creation date so that next period key rotation could happen relative to this date.
+        data.wgKeyData.creationDate = Date()
+
+        // Swap old and new keys.
+        data.wgKeyData.privateKey = nextKey
+        data.wgKeyData.nextPrivateKey = nil
+
+        // Unset the date of last rotation attempt to mark the end of key rotation sequence.
+        data.wgKeyData.lastRotationAttemptDate = nil
+    }
+
+    /**
+     Returns the date of next key rotation, as it normally occurs in the app process using the following rules:
+
+     1. Returns the date relative to key creation date + 14 days, if last rotation attempt was successful.
+     2. Returns the date relative to last rotation attempt date + 24 hours, if last rotation attempt was unsuccessful.
+
+     If the date produced is in the past then `Date()` is returned instead.
+     */
+    var nextRotationDate: Date {
+        let rotationInterval: TimeInterval = 60 * 60 * 24 * 14
+        let retryInterval: TimeInterval = 60 * 60 * 24
+
+        let nextRotationDate = data.wgKeyData.lastRotationAttemptDate?.addingTimeInterval(retryInterval)
+            ?? data.wgKeyData.creationDate.addingTimeInterval(rotationInterval)
+
+        return max(nextRotationDate, Date())
+    }
+
+    /// Returns `true` if the app should rotate the private key.
+    var shouldRotateTheKey: Bool {
+        return nextRotationDate <= Date()
+    }
+
+    /**
+     Returns `true` if packet tunnel should perform key rotation.
+
+     During the startup packet tunnel rotates the key immediately if it detected that the key stored on server does not
+     match the key stored on device. In that case it passes `shouldRotateImmediately = true`.
+
+     To dampen the effect of packet tunnel entering into a restart cycle and going on a key rotation rampage,
+     this function adds a cooldown interval to prevent it from pushing keys too often.
+     */
+    func shouldPacketTunnelRotateTheKey(shouldRotateImmediately: Bool) -> Bool {
+        guard let lastRotationAttemptDate = data.wgKeyData.lastRotationAttemptDate else { return true }
+
+        let now = Date()
+
+        let retryInterval: TimeInterval = 60 * 60 * 24
+        let cooldownInterval: TimeInterval = 15
+
+        // Add cooldown interval when requested to rotate the key immediately.
+        if shouldRotateImmediately, lastRotationAttemptDate.distance(to: now) > cooldownInterval {
+            return true
+        }
+
+        let nextRotationAttempt = max(now, lastRotationAttemptDate.addingTimeInterval(retryInterval))
+        if nextRotationAttempt <= now {
+            return true
+        }
+
+        return false
+    }
+}
---
+++ ios/PacketTunnel/CheckDeviceOperation.swift
@@ -0,0 +1,256 @@
+//
+//  CheckDeviceOperation.swift
+//  PacketTunnel
+//
+//  Created by pronebird on 20/04/2023.
+//  Copyright © 2023 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import MullvadLogging
+import MullvadREST
+import MullvadTypes
+import Operations
+import class WireGuardKitTypes.PrivateKey
+import class WireGuardKitTypes.PublicKey
+
+final class CheckDeviceOperation: ResultOperation<DeviceCheck> {
+    private let logger = Logger(label: "CheckDeviceOperation")
+
+    private let accountsProxy: REST.AccountsProxy
+    private let devicesProxy: REST.DevicesProxy
+    private let shouldImmediatelyRotateKeyOnMismatch: Bool
+
+    private var tasks: [Cancellable] = []
+
+    init(
+        dispatchQueue: DispatchQueue,
+        accountsProxy: REST.AccountsProxy,
+        devicesProxy: REST.DevicesProxy,
+        shouldImmediatelyRotateKeyOnMismatch: Bool,
+        completionHandler: @escaping CompletionHandler
+    ) {
+        self.accountsProxy = accountsProxy
+        self.devicesProxy = devicesProxy
+        self.shouldImmediatelyRotateKeyOnMismatch = shouldImmediatelyRotateKeyOnMismatch
+
+        super.init(dispatchQueue: dispatchQueue, completionQueue: dispatchQueue, completionHandler: completionHandler)
+    }
+
+    override func main() {
+        startFlow { result in
+            self.finish(result: result)
+        }
+    }
+
+    override func operationDidCancel() {
+        tasks.forEach { $0.cancel() }
+    }
+
+    // MARK: - Flow
+
+    private func startFlow(completion: @escaping (Result<DeviceCheck, Error>) -> Void) {
+        do {
+            guard case let .loggedIn(accountData, deviceData) = try SettingsManager.readDeviceState() else {
+                throw InvalidDeviceStateError()
+            }
+
+            fetchData(
+                accountNumber: accountData.number,
+                deviceIdentifier: deviceData.identifier
+            ) { [self] accountResult, deviceResult in
+                self.didReceiveData(accountResult: accountResult, deviceResult: deviceResult, completion: completion)
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+
+    private func didReceiveData(
+        accountResult: Result<AccountData, Error>,
+        deviceResult: Result<Device, Error>,
+        completion: @escaping (Result<DeviceCheck, Error>) -> Void
+    ) {
+        do {
+            let accountVerdict = try accountVerdict(from: accountResult)
+            let deviceVerdict = try deviceVerdict(from: deviceResult)
+
+            if deviceVerdict == .keyMismatch {
+                rotateKeyIfNeeded { rotationResult in
+                    completion(rotationResult.map { rotationStatus in
+                        return DeviceCheck(
+                            accountVerdict: accountVerdict,
+                            deviceVerdict: rotationStatus.isSucceeded ? .good : .keyMismatch,
+                            keyRotationStatus: rotationStatus
+                        )
+                    })
+                }
+            } else {
+                completion(.success(DeviceCheck(
+                    accountVerdict: accountVerdict,
+                    deviceVerdict: deviceVerdict,
+                    keyRotationStatus: .none
+                )))
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+
+    // MARK: - Data fetch
+
+    private func fetchData(
+        accountNumber: String, deviceIdentifier: String,
+        completion: @escaping (Result<AccountData, Error>, Result<Device, Error>) -> Void
+    ) {
+        var accountResult: Result<AccountData, Error> = .failure(OperationError.cancelled)
+        var deviceResult: Result<Device, Error> = .failure(OperationError.cancelled)
+
+        let dispatchGroup = DispatchGroup()
+
+        dispatchGroup.enter()
+        let accountTask = accountsProxy
+            .getAccountData(accountNumber: accountNumber, retryStrategy: .noRetry) { result in
+                accountResult = result
+                dispatchGroup.leave()
+            }
+
+        dispatchGroup.enter()
+        let deviceTask = devicesProxy.getDevice(
+            accountNumber: accountNumber,
+            identifier: deviceIdentifier,
+            retryStrategy: .noRetry
+        ) { result in
+            deviceResult = result
+            dispatchGroup.leave()
+        }
+
+        tasks.append(contentsOf: [accountTask, deviceTask])
+
+        dispatchGroup.notify(queue: dispatchQueue) {
+            completion(accountResult, deviceResult)
+        }
+    }
+
+    // MARK: - Key rotation
+
+    private func rotateKeyIfNeeded(completion: @escaping (Result<KeyRotationStatus, Error>) -> Void) {
+        let deviceState: DeviceState
+        do {
+            deviceState = try SettingsManager.readDeviceState()
+        } catch {
+            completion(.failure(error))
+            return
+        }
+
+        guard case let .loggedIn(accountData, deviceData) = deviceState else {
+            completion(.failure(InvalidDeviceStateError()))
+            return
+        }
+
+        var keyRotation = WgKeyRotation(data: deviceData)
+        guard keyRotation.shouldPacketTunnelRotateTheKey(shouldRotateImmediately: shouldImmediatelyRotateKeyOnMismatch)
+        else {
+            completion(.success(.none))
+            return
+        }
+
+        logger.debug("Rotate private key from packet tunnel.")
+
+        let publicKey = keyRotation.beginAttempt()
+
+        do {
+            try SettingsManager.writeDeviceState(.loggedIn(accountData, keyRotation.data))
+        } catch {
+            completion(.failure(error))
+            return
+        }
+
+        let task = devicesProxy.rotateDeviceKey(
+            accountNumber: accountData.number,
+            identifier: deviceData.identifier,
+            publicKey: publicKey,
+            retryStrategy: .default
+        ) { result in
+            self.dispatchQueue.async {
+                let returnResult = result.tryMap { device -> KeyRotationStatus in
+                    try self.completeKeyRotation(device)
+                    return .succeeded(Date())
+                }
+                .flatMapError { error in
+                    if error.isOperationCancellationError {
+                        return .failure(error)
+                    } else {
+                        self.logger.error(error: error, message: "Failed to rotate device key.")
+                        return .success(.attempted(Date()))
+                    }
+                }
+
+                completion(returnResult)
+            }
+        }
+
+        tasks.append(task)
+    }
+
+    private func completeKeyRotation(_ device: Device) throws {
+        logger.debug("Successfully rotated device key. Persisting device state...")
+
+        let deviceState = try SettingsManager.readDeviceState()
+        guard case let .loggedIn(accountData, deviceData) = deviceState else { throw InvalidDeviceStateError() }
+
+        var keyRotation = WgKeyRotation(data: deviceData)
+        keyRotation.setCompleted(with: device)
+
+        do {
+            try SettingsManager.writeDeviceState(.loggedIn(accountData, keyRotation.data))
+        } catch {
+            logger.error(error: error, message: "Failed to persist device state.")
+
+            throw error
+        }
+    }
+
+    // MARK: - Private helpers
+
+    private func accountVerdict(from accountResult: Result<AccountData, Error>) throws -> AccountVerdict {
+        do {
+            let accountData = try accountResult.get()
+
+            if accountData.expiry > Date() {
+                return .good(accountData)
+            } else {
+                return .expired(accountData)
+            }
+        } catch {
+            if let error = error as? REST.Error, error.compareErrorCode(.invalidAccount) {
+                return .invalidAccount
+            } else {
+                throw error
+            }
+        }
+    }
+
+    private func deviceVerdict(from deviceResult: Result<Device, Error>) throws -> DeviceVerdict {
+        do {
+            let deviceState = try SettingsManager.readDeviceState()
+            guard let deviceData = deviceState.deviceData else { throw InvalidDeviceStateError() }
+
+            let device = try deviceResult.get()
+
+            return deviceData.wgKeyData.privateKey.publicKey.base64Key == device.pubkey ? .good : .keyMismatch
+        } catch {
+            if let error = error as? REST.Error, error.compareErrorCode(.deviceNotFound) {
+                return .revoked
+            } else {
+                throw error
+            }
+        }
+    }
+}
+
+private struct InvalidDeviceStateError: LocalizedError {
+    var errorDescription: String? {
+        return "Cannot complete device check because device is no longer logged in."
+    }
+}
