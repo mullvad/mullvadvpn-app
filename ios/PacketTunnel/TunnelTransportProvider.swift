c4596e676cdd4c3045efad79211c7e9424af1c7a
Bug Magnet <marco.nikic@mullvad.net>
2023-04-26 09:32:32 +0200

    Add shadow socks support for proxied requests in packet tunnel

--- ios/PacketTunnel/TunnelTransportProvider.swift
+++ ios/PacketTunnel/TunnelTransportProvider.swift
@@ -9,19 +9,48 @@
 import Foundation
 import MullvadREST
 import RelayCache
+import Logging
 
 final class TunnelTransportProvider: RESTTransportProvider {
     private let urlSessionTransport: REST.URLSessionTransport
     private let relayCache: RelayCache
+    private let logger = Logger(label: "TunnelTransportProvider")
+    private var currentTransport: RESTTransport?
 
     init(urlSessionTransport: REST.URLSessionTransport, relayCache: RelayCache) {
         self.urlSessionTransport = urlSessionTransport
         self.relayCache = relayCache
+        currentTransport = urlSessionTransport
     }
 
     func transport() -> MullvadREST.RESTTransport? {
-        urlSessionTransport
+        currentTransport
     }
 
-    func selectNextTransport() {}
+    func selectNextTransport() {
+        do {
+            let cachedRelays = try relayCache.read()
+            let shadowSocksConfiguration = cachedRelays.relays.bridge.shadowsocks.filter { $0.protocol == "tcp" }
+                .randomElement()
+            let shadowSocksBridgeRelay = cachedRelays.relays.bridge.relays.randomElement()
+
+            guard let shadowSocksConfiguration = shadowSocksConfiguration,
+                  let shadowSocksBridgeRelay = shadowSocksBridgeRelay
+            else {
+                logger.error("Could not get shadow socks bridge information.")
+                return
+            }
+
+            let shadowSocksURLSession = urlSessionTransport.urlSession
+            let shadowSocksTransport = REST.URLSessionShadowSocksTransport(
+                urlSession: shadowSocksURLSession,
+                shadowSocksConfiguration: shadowSocksConfiguration,
+                shadowSocksBridgeRelay: shadowSocksBridgeRelay
+            )
+            
+            currentTransport = shadowSocksTransport
+        } catch {
+            logger.error(error: error)
+        }
+    }
 }
