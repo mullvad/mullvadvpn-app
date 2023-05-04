f65eb97419f43a015c67c133ae672a4b86082b06
David Lönnhager <david.l@mullvad.net>
2023-05-04 14:41:33 +0200

    Rework macOS routing

--- talpid-core/src/offline/macos.rs
+++ talpid-core/src/offline/macos.rs
@@ -20,125 +20,151 @@
 //!
 //! [`SCNetworkReachability`]: https://developer.apple.com/documentation/systemconfiguration/scnetworkreachability-g7d
 //! [`NWPathMonitor`]: https://developer.apple.com/documentation/network/nwpathmonitor
-use futures::{channel::mpsc::UnboundedSender, Future, StreamExt};
-use std::sync::{Arc, Mutex, Weak};
-use talpid_types::ErrorExt;
+use futures::{channel::mpsc::UnboundedSender, StreamExt};
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc, Mutex,
+    },
+    time::Duration,
+};
+use talpid_routing::{DefaultRouteEvent, RouteManagerHandle};
+
+/// How long to wait before announcing changes to the offline state
+const DEBOUNCE_INTERVAL: Duration = Duration::from_secs(2);
 
 #[derive(err_derive::Error, Debug)]
 pub enum Error {
     #[error(display = "Failed to initialize route monitor")]
-    StartMonitorError(#[error(source)] talpid_routing::PlatformError),
+    StartMonitorError(#[error(source)] talpid_routing::Error),
 }
 
 pub struct MonitorHandle {
-    excluded_interface: Arc<Mutex<Option<String>>>,
+    state: Arc<Mutex<ConnectivityState>>,
     _notify_tx: Arc<UnboundedSender<bool>>,
 }
 
+struct ConnectivityState {
+    v4_connectivity: bool,
+    v6_connectivity: bool,
+}
+
+impl ConnectivityState {
+    fn get_connectivity(&self) -> bool {
+        self.v4_connectivity || self.v6_connectivity
+    }
+}
+
 impl MonitorHandle {
-    /// Host is considered to be offline if the IPv4 internet is considered to be unreachable by the
-    /// given reachability flags *or* there are no active physical interfaces.
+    /// Host is considered to be offline if macOS doesn't assign a non-tunnel default route
+    #[allow(clippy::unused_async)]
     pub async fn host_is_offline(&self) -> bool {
-        let excluded_interface: Option<String> = self
-            .excluded_interface
-            .lock()
-            .expect("excluded_interface lock poisoned")
-            .clone();
-        !exists_non_tunnel_default_route(excluded_interface).await
-    }
-
-    /// Sets excluded interface - excluded interfaces will be assuemd to not provide any
-    /// connectivity.
-    pub fn set_excluded_interface(&self, excluded_interface: Option<String>) {
-        *self
-            .excluded_interface
-            .lock()
-            .expect("excluded interface lock poisoned") = excluded_interface;
+        let state = self.state.lock().unwrap();
+        !state.get_connectivity()
     }
 }
 
-async fn exists_non_tunnel_default_route(excluded_interface: Option<String>) -> bool {
-    true
-    // match talpid_routing::get_default_routes(excluded_interface).await {
-    //     Ok((Some(node), _)) | Ok((None, Some(node))) => {
-    //         let route_exists = node
-    //             .get_device()
-    //             .map(|iface_name| !iface_name.contains("tun"))
-    //             .unwrap_or(true);
-    //         log::debug!("Assuming non-tunnel default route exists due to {:?}", node);
-    //         route_exists
-    //     }
-    //     Ok((None, None)) => {
-    //         log::debug!("No default routes exist, assuming machine is offline");
-    //         false
-    //     }
-    //     Err(err) => {
-    //         log::error!(
-    //             "{}",
-    //             err.display_chain_with_msg(
-    //                 "Failed to obtain default routes, assuming machine is online."
-    //             )
-    //         );
-    //         true
-    //     }
-    // }
-}
-pub async fn spawn_monitor(notify_tx: UnboundedSender<bool>) -> Result<MonitorHandle, Error> {
+pub async fn spawn_monitor(
+    notify_tx: UnboundedSender<bool>,
+    route_manager_handle: RouteManagerHandle,
+) -> Result<MonitorHandle, Error> {
     let notify_tx = Arc::new(notify_tx);
-    let excluded_interface = Arc::new(Mutex::new(None));
 
-    let context = OfflineStateContext {
-        sender: Arc::downgrade(&notify_tx),
-        is_offline: !exists_non_tunnel_default_route(None).await,
+    let (v4_connectivity, v6_connectivity) = match route_manager_handle.get_default_routes().await {
+        Ok((v4_route, v6_route)) => (v4_route.is_some(), v6_route.is_some()),
+        Err(error) => {
+            log::warn!("Failed to initialize offline monitor: {error}");
+            (true, true)
+        }
     };
 
-    let route_monitor = watch_route_monitor(context, excluded_interface.clone())?;
-    tokio::spawn(route_monitor);
+    let state = ConnectivityState {
+        v4_connectivity,
+        v6_connectivity,
+    };
+    let initial_connectivity = state.get_connectivity();
+    let state = Arc::new(Mutex::new(state));
+
+    let mut route_listener = route_manager_handle.default_route_listener().await?;
+    let weak_state = Arc::downgrade(&state);
+    let weak_notify_tx = Arc::downgrade(&notify_tx);
+
+    // Detect changes to the default route
+    tokio::spawn(async move {
+        let mut state_update_handle: Option<tokio::task::JoinHandle<()>> = None;
+        let prev_notified_state = Arc::new(AtomicBool::new(initial_connectivity));
+
+        while let Some(event) = route_listener.next().await {
+            let state = match weak_state.upgrade() {
+                Some(state) => state,
+                None => break,
+            };
+
+            let mut state = state.lock().unwrap();
+
+            log::trace!("Default route event: {event:?}");
+
+            let previous_connectivity = state.get_connectivity();
+
+            match event {
+                DefaultRouteEvent::AddedOrChangedV4 => {
+                    state.v4_connectivity = true;
+                }
+                DefaultRouteEvent::AddedOrChangedV6 => {
+                    state.v6_connectivity = true;
+                }
+                DefaultRouteEvent::RemovedV4 => {
+                    state.v4_connectivity = false;
+                }
+                DefaultRouteEvent::RemovedV6 => {
+                    state.v6_connectivity = false;
+                }
+            }
+
+            let new_connectivity = state.get_connectivity();
+            if previous_connectivity != new_connectivity {
+                if let Some(update_state) = state_update_handle.take() {
+                    update_state.abort();
+                }
+
+                let prev_notified = prev_notified_state.clone();
+
+                let notify_copy = weak_notify_tx.clone();
+                let update_task = tokio::spawn(async move {
+                    let notify_tx = match notify_copy.upgrade() {
+                        Some(tx) => tx,
+                        None => return,
+                    };
+
+                    // Debounce event updates
+                    tokio::time::sleep(DEBOUNCE_INTERVAL).await;
+
+                    if prev_notified.swap(new_connectivity, Ordering::AcqRel) == new_connectivity {
+                        // We don't care about network changes here
+                        return;
+                    }
+
+                    log::info!(
+                        "Connectivity changed: {}",
+                        if new_connectivity {
+                            "Connected"
+                        } else {
+                            "Offline"
+                        }
+                    );
+
+                    let _ = notify_tx.unbounded_send(!new_connectivity);
+                });
+
+                state_update_handle = Some(update_task);
+            }
+        }
+
+        log::trace!("Offline monitor exiting");
+    });
+
     Ok(MonitorHandle {
-        excluded_interface,
+        state,
         _notify_tx: notify_tx,
     })
-}
-
-fn watch_route_monitor(
-    mut context: OfflineStateContext,
-    excluded_interface: Arc<Mutex<Option<String>>>,
-) -> Result<impl Future<Output = ()>, Error> {
-    let mut monitor = talpid_routing::listen_for_default_route_changes()?;
-
-    Ok(async move {
-        while let Some(_route_change) = monitor.next().await {
-            let interface = excluded_interface
-                .lock()
-                .expect("excluded_interface lock poisoned")
-                .clone();
-
-            context.new_state(!exists_non_tunnel_default_route(interface).await);
-            if context.should_shut_down() {
-                break;
-            }
-        }
-        log::debug!("Stopping offline monitor");
-    })
-}
-
-#[derive(Clone)]
-struct OfflineStateContext {
-    sender: Weak<UnboundedSender<bool>>,
-    is_offline: bool,
-}
-
-impl OfflineStateContext {
-    fn should_shut_down(&self) -> bool {
-        self.sender.upgrade().is_none()
-    }
-
-    fn new_state(&mut self, is_offline: bool) {
-        if self.is_offline != is_offline {
-            self.is_offline = is_offline;
-            if let Some(sender) = self.sender.upgrade() {
-                let _ = sender.unbounded_send(is_offline);
-            }
-        }
-    }
 }
