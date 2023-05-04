7388a27c2005050fa1a9acc5bb9c7d489a503532
Jon Petersson <jon.petersson@kvadrat.se>
2023-05-04 14:10:11 +0200

    Add custom port selection to settings

--- ios/MullvadVPN/Coordinators/App/ApplicationCoordinator.swift
+++ ios/MullvadVPN/Coordinators/App/ApplicationCoordinator.swift
@@ -654,7 +654,8 @@ final class ApplicationCoordinator: Coordinator, Presenting, RootContainerViewCo
         let interactorFactory = SettingsInteractorFactory(
             storePaymentManager: storePaymentManager,
             tunnelManager: tunnelManager,
-            apiProxy: apiProxy
+            apiProxy: apiProxy,
+            relayCacheTracker: relayCacheTracker
         )
 
         let navigationController = CustomNavigationController()
