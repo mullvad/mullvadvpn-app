466ff791d293b2035226f31f9a938924db26c8ff
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-05-16 10:32:33 +0200

    Migrate select location fragment to compose

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/SelectLocationViewModelState.kt
@@ -0,0 +1,19 @@
+package net.mullvad.mullvadvpn.viewmodel
+
+import net.mullvad.mullvadvpn.compose.state.SelectLocationUiState
+import net.mullvad.mullvadvpn.relaylist.RelayItem
+import net.mullvad.mullvadvpn.relaylist.RelayList
+
+data class SelectLocationViewModelState(
+    val newRelayList: RelayList? = null,
+    val newSelectedItem: RelayItem? = null,
+) {
+    fun toUiState(): SelectLocationUiState =
+        newRelayList?.let {
+            SelectLocationUiState.ShowData(
+                countries = newRelayList.countries,
+                selectedRelay = newSelectedItem
+            )
+        }
+            ?: SelectLocationUiState.Loading
+}
