ad1973a041b4a5bf7c793932bd004a98ac088345
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-05-19 08:16:51 +0200

    Remove scopes in koin and use global scope everywhere

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/SplitTunnelingViewModel.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/SplitTunnelingViewModel.kt
@@ -5,36 +5,60 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.applist.AppData
 import net.mullvad.mullvadvpn.applist.ApplicationsProvider
 import net.mullvad.mullvadvpn.compose.state.SplitTunnelingUiState
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnectionContainer
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnectionManager
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnectionState
 import net.mullvad.mullvadvpn.ui.serviceconnection.SplitTunneling
+import net.mullvad.mullvadvpn.ui.serviceconnection.splitTunneling
 
 class SplitTunnelingViewModel(
     private val appsProvider: ApplicationsProvider,
-    private val splitTunneling: SplitTunneling,
+    private val serviceConnectionManager: ServiceConnectionManager,
     private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
     private val allApps = MutableStateFlow<List<AppData>?>(null)
     private val showSystemApps = MutableStateFlow(false)
 
+    private val _shared: SharedFlow<ServiceConnectionContainer> =
+        serviceConnectionManager.connectionState
+            .flatMapLatest { state ->
+                if (state is ServiceConnectionState.ConnectedReady) {
+                    flowOf(state.container)
+                } else {
+                    emptyFlow()
+                }
+            }
+            .shareIn(viewModelScope, SharingStarted.WhileSubscribed())
+
     private val vmState =
-        combine(splitTunneling.excludedAppsCallbackFlow(), allApps, showSystemApps) {
-                excludedApps,
-                allApps,
-                showSystemApps ->
-                SplitTunnelingViewModelState(
-                    excludedApps = excludedApps,
-                    allApps = allApps,
-                    showSystemApps = showSystemApps
-                )
+        _shared
+            .flatMapLatest { serviceConnection ->
+                combine(
+                    serviceConnection.splitTunneling.excludedAppsCallbackFlow(),
+                    allApps,
+                    showSystemApps
+                ) { excludedApps, allApps, showSystemApps ->
+                    SplitTunnelingViewModelState(
+                        excludedApps = excludedApps,
+                        allApps = allApps,
+                        showSystemApps = showSystemApps
+                    )
+                }
             }
             .stateIn(
                 viewModelScope,
@@ -53,22 +77,28 @@ class SplitTunnelingViewModel(
 
     init {
         viewModelScope.launch(dispatcher) {
-            if (!splitTunneling.enabled) splitTunneling.enabled = true
+            if (serviceConnectionManager.splitTunneling()?.enabled == false) {
+                serviceConnectionManager.splitTunneling()?.enabled = true
+            }
             fetchApps()
         }
     }
 
     override fun onCleared() {
-        splitTunneling.persist()
+        serviceConnectionManager.splitTunneling()?.persist()
         super.onCleared()
     }
-    
+
     fun onIncludeAppClick(packageName: String) {
-        viewModelScope.launch(dispatcher) { splitTunneling.includeApp(packageName) }
+        viewModelScope.launch(dispatcher) {
+            serviceConnectionManager.splitTunneling()?.includeApp(packageName)
+        }
     }
 
     fun onExcludeAppClick(packageName: String) {
-        viewModelScope.launch(dispatcher) { splitTunneling.excludeApp(packageName) }
+        viewModelScope.launch(dispatcher) {
+            serviceConnectionManager.splitTunneling()?.excludeApp(packageName)
+        }
     }
 
     fun onShowSystemAppsClicked(show: Boolean) {
