07bc3ac00078e102685843e0cda38e6df989aae2
Markus Pettersson <markus.pettersson@mullvad.net>
2023-05-22 13:08:08 +0200

    Print `mullvad-app` version even if `mullvad-daemon` is down
    Previously, if `mullvad-cli` could not establish a connection to
    `mullvad-daemon` for any reason, running `mullvad version` would not
    print any version information whatsoever. Now we do print the current
    build version of `mullvad-app`, which may or may not be in sync with the
    installed version of `mullvad-daemon`.
    
    We use the `mullvad-version` crate to figure out an appropriate response
    in case `mullvad-daemon` is down.

--- mullvad-cli/src/cmds/version.rs
+++ mullvad-cli/src/cmds/version.rs
@@ -2,27 +2,36 @@ use anyhow::{Context, Result};
 use mullvad_management_interface::MullvadProxyClient;
 
 pub async fn print() -> Result<()> {
-    let mut rpc = MullvadProxyClient::new().await?;
-    let current_version = rpc
+    println!("{:22}: {}", "Current version", mullvad_version::VERSION);
+
+    let mut rpc = MullvadProxyClient::new()
+        .await
+        .context("Failed to connect to mullvad daemon")?;
+
+    let daemon_version = rpc
         .get_current_version()
         .await
-        .context("Failed to get current version")?;
-    println!("{:21}: {}", "Current version", current_version);
+        .context("Failed to get current mullvad-daemon version")?;
+
+    if daemon_version != mullvad_version::VERSION {
+        println!("{:22}: {}", "mullvad-daemon version", daemon_version);
+    };
+
     let version_info = rpc
         .get_version_info()
         .await
         .context("Failed to get version info")?;
-    println!("{:21}: {}", "Is supported", version_info.supported);
+    println!("{:22}: {}", "Is supported", version_info.supported);
 
     if let Some(suggested_upgrade) = version_info.suggested_upgrade {
-        println!("{:21}: {}", "Suggested upgrade", suggested_upgrade);
+        println!("{:22}: {}", "Suggested upgrade", suggested_upgrade);
     } else {
-        println!("{:21}: none", "Suggested upgrade");
+        println!("{:22}: none", "Suggested upgrade");
     }
 
     if !version_info.latest_stable.is_empty() {
         println!(
-            "{:21}: {}",
+            "{:22}: {}",
             "Latest stable version", version_info.latest_stable
         );
     }
@@ -32,7 +41,7 @@ pub async fn print() -> Result<()> {
         .await
         .context("Failed to obtain settings")?;
     if settings.show_beta_releases {
-        println!("{:21}: {}", "Latest beta version", version_info.latest_beta);
+        println!("{:22}: {}", "Latest beta version", version_info.latest_beta);
     };
 
     Ok(())
