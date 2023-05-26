fb3e821b48942c3fa801582952ab8f6d22b68cd0
saber safavi <saber.safavi@codic.se>
2023-05-26 09:44:13 +0200

    Add new external link settings cell
    Modify navigation cell to support showing extra information.

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/theme/dimensions/Dimensions.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/theme/dimensions/Dimensions.kt
@@ -9,6 +9,8 @@ data class Dimensions(
     val cellHeight: Dp = 52.dp,
     val cellLabelVerticalPadding: Dp = 14.dp,
     val cellStartPadding: Dp = 22.dp,
+    val cellTopPadding: Dp = 6.dp,
+    val cellVerticalSpacing: Dp = 14.dp,
     val cityRowPadding: Dp = 34.dp,
     val countryRowPadding: Dp = 18.dp,
     val expandableCellChevronSize: Dp = 30.dp,
