1fef699bab7648ae0ed7cb51b147bd32d032fed6
saber safavi <saber.safavi@codic.se>
2023-05-26 10:14:37 +0200

    Add settings compose screen

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/notification/AccountExpiryNotification.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/notification/AccountExpiryNotification.kt
@@ -2,14 +2,12 @@ package net.mullvad.mullvadvpn.ui.notification
 
 import android.content.Context
 import net.mullvad.mullvadvpn.R
-import net.mullvad.mullvadvpn.util.TimeLeftFormatter
+import net.mullvad.mullvadvpn.util.format
 import org.joda.time.DateTime
 
 class AccountExpiryNotification(
-    context: Context,
+    val context: Context,
 ) : InAppNotification() {
-    private val timeLeftFormatter = TimeLeftFormatter(context.resources)
-
     init {
         status = StatusLevel.Error
         title = context.getString(R.string.account_credit_expires_soon)
@@ -19,7 +17,7 @@ class AccountExpiryNotification(
         val threeDaysFromNow = DateTime.now().plusDays(3)
 
         if (expiry != null && expiry.isBefore(threeDaysFromNow)) {
-            message = timeLeftFormatter.format(expiry)
+            message = context.resources.format(expiry)
             shouldShow = true
         } else {
             shouldShow = false
