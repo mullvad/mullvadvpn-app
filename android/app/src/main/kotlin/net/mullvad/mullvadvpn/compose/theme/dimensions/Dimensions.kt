c95a9d3e4cc173dd68508bd58687f8eecd8d6b22
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-05-31 09:46:20 +0200

    Order dimensions alphabetically

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/theme/dimensions/Dimensions.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/theme/dimensions/Dimensions.kt
@@ -4,23 +4,23 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 data class Dimensions(
-    val mediumPadding: Dp = 16.dp,
-    val smallPadding: Dp = 8.dp,
+    val cellEndPadding: Dp = 16.dp,
+    val cellFooterTopPadding: Dp = 6.dp,
+    val cellHeight: Dp = 52.dp,
+    val cellLabelVerticalPadding: Dp = 14.dp,
+    val cellStartPadding: Dp = 22.dp,
+    val indentedCellStartPadding: Dp = 38.dp,
+    val listIconSize: Dp = 24.dp,
     val listItemDivider: Dp = 1.dp,
     val listItemHeight: Dp = 50.dp,
     val listItemHeightExtra: Dp = 60.dp,
+    val loadingSpinnerPadding: Dp = 12.dp,
     val loadingSpinnerSize: Dp = 24.dp,
     val loadingSpinnerStrokeWidth: Dp = 3.dp,
-    val loadingSpinnerPadding: Dp = 12.dp,
-    val cellStartPadding: Dp = 22.dp,
-    val cellEndPadding: Dp = 16.dp,
-    val listIconSize: Dp = 24.dp,
+    val mediumPadding: Dp = 16.dp,
     val progressIndicatorSize: Dp = 60.dp,
-    val indentedCellStartPadding: Dp = 38.dp,
-    val cellHeight: Dp = 52.dp,
-    val cellLabelVerticalPadding: Dp = 14.dp,
-    val cellFooterTopPadding: Dp = 6.dp,
-    val selectableCellTextMargin: Dp = 12.dp
+    val selectableCellTextMargin: Dp = 12.dp,
+    val smallPadding: Dp = 8.dp
 )
 
 val defaultDimensions = Dimensions()
