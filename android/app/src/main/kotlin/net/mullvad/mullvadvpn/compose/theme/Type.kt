1dac600e83bd6ad84b09e259ea974e317c93f104
Matilda <matilda@mullvad.net>
2023-06-07 09:27:03 +0200

    Remove trailing commas and auto-format code

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/theme/Type.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/theme/Type.kt
@@ -6,77 +6,28 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-val mullvadTypography = Typography(
-    displayLarge =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif
-    ),
-    displayMedium =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif,
-    ),
-    displaySmall =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif
-    ),
-    headlineLarge =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Black
-    ),
-    headlineMedium =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif
-    ),
-    headlineSmall =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Black
-    ),
-    titleLarge =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Black
-    ),
-    titleMedium =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif
-    ),
-    titleSmall =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif
-    ),
-    bodyLarge =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif
-    ),
-    bodyMedium =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.SemiBold
-    ),
-    bodySmall =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.SemiBold
-    ),
-    labelLarge =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 18.sp,
-        lineHeight = 23.sp,
-    ),
-    labelMedium =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.SemiBold
-    ),
-    labelSmall =
-    TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.SemiBold
-    ),
-
+internal val MullvadMaterial3Typography =
+    Typography(
+        displayLarge = TextStyle(fontFamily = FontFamily.SansSerif),
+        displayMedium = TextStyle(fontFamily = FontFamily.SansSerif),
+        displaySmall = TextStyle(fontFamily = FontFamily.SansSerif),
+        headlineLarge = TextStyle(fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Black),
+        headlineMedium = TextStyle(fontFamily = FontFamily.SansSerif),
+        headlineSmall = TextStyle(fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Black),
+        titleLarge = TextStyle(fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Black),
+        titleMedium = TextStyle(fontFamily = FontFamily.SansSerif),
+        titleSmall = TextStyle(fontFamily = FontFamily.SansSerif),
+        bodyLarge = TextStyle(fontFamily = FontFamily.SansSerif),
+        bodyMedium = TextStyle(fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.SemiBold),
+        bodySmall = TextStyle(fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.SemiBold),
+        labelLarge =
+            TextStyle(
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 18.sp,
+                lineHeight = 23.sp
+            ),
+        labelMedium =
+            TextStyle(fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.SemiBold),
+        labelSmall = TextStyle(fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.SemiBold)
     )
-
