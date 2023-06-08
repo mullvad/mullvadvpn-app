2b2afc414970ebabc6fe78adf1b2d8f2b02919fc
Jon Petersson <jon.petersson@kvadrat.se>
2023-06-08 11:50:04 +0200

    Add custom deserialiser for RelayConstraints

--- ios/MullvadTypes/RelayConstraints.swift
+++ ios/MullvadTypes/RelayConstraints.swift
@@ -10,6 +10,8 @@ import Foundation
 
 public struct RelayConstraints: Codable, Equatable, CustomDebugStringConvertible {
     public var location: RelayConstraint<RelayLocation>
+
+    // Added in 2023.3
     public var port: RelayConstraint<UInt16>
 
     public var debugDescription: String {
@@ -22,5 +24,13 @@ public struct RelayConstraints: Codable, Equatable, CustomDebugStringConvertible
     ) {
         self.location = location
         self.port = port
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        location = try container.decode(RelayConstraint<RelayLocation>.self, forKey: .location)
+        
+        // Added in 2023.3
+        port = try container.decodeIfPresent(RelayConstraint<UInt16>.self, forKey: .port) ?? .any
     }
 }
