3c6c9653f709f31b44559dfc3af4008f1100dac1
App Appest <app@app-build-macos2.local>
2023-06-14 13:45:19 +0200

    Fix the build scripts

--- ci/ios/run-build.sh
+++ ci/ios/run-build.sh
@@ -6,20 +6,33 @@ set -eu -o pipefail
 # Add SSH key for iOS build VMs to be able to SSH into them without user interaction
 ssh-add ~/.ssh/ios-vm-key
 
-VM_BUILD_DIR="/Volumes/My Shared Files/build/ios"
+VM_BUILD_DIR="\"/Volumes/My Shared Files/build\""
+
 BUILD_SCRIPT="
-security unlock-keychain -p 'build' && \
-    (cd ${VM_BUILD_DIR}; bash build.sh)
+	set -eu
+	security unlock-keychain -p 'build' 
+	rm -rf ~/build 
+	# copying over build directory because the spaces in "/Volmes/My Shared Files/" fuck up Make and XCode
+	cp -r ${VM_BUILD_DIR} ~/build || true
+	cd ~/build/ios
+	rm -r Build
+	IOS_PROVISIONING_PROFILES_DIR=~/provisioning-profiles bash build.sh 
+	cp ~/build/ios/Build/MullvadVPN.ipa /Volumes/My\ Shared\ Files/build/ios/Build/
 "
+
 
 bash run-in-vm.sh "ios-build" "${BUILD_SCRIPT}" "build:mullvadvpn-app"
 mkdir -p build-output
 cp mullvadvpn-app/ios/Build/MullvadVPN.ipa build-output/MullvadVPN.ipa
 
-VM_UPLOAD_DIR="/Volumes/My Shared Files/build-output"
-VM_UPLOAD_IPA_PATH="${VM_UPLOAD_DIR}/MullvadVPN.ipa"
+VM_UPLOAD_IPA_PATH="/Volumes/My\ Shared\ Files/build-output/MullvadVPN.ipa"
 # TODO figure out path to API key
 API_KEY_PATH="~/ci/app-store-connect-key.json"
-UPLOAD_SCRIPT="(cd ci/; bundle exec fastlane pilot upload --api-key ${API_KEY_PATH} --ipa ${VM_UPLOAD_IPA_PATH})"
+UPLOAD_SCRIPT="(
+	cd ci/
+	source ~/.zshrc
+	source ~/.zprofile
+	bundle exec fastlane pilot upload --api-key-path ${API_KEY_PATH} --ipa ${VM_UPLOAD_IPA_PATH}
+)"
 
 bash run-in-vm.sh "ios-upload" "${UPLOAD_SCRIPT}" "build-output:build-output"
--- ci/ios/run-in-vm.sh
+++ ci/ios/run-in-vm.sh
@@ -6,9 +6,9 @@ VM_NAME=${1:?"No VM name provided"}
 SCRIPT=${2:?"No script provided"}
 SHARED_DIR=${3:?"No passthrough provided"}
 
-tart run ""--dir=${SHARED_DIR}"" "$VM_NAME" &
+tart run --no-graphics "--dir=${SHARED_DIR}" "$VM_NAME" &
 vm_pid=$!
-ssh admin@$(tart ip "$VM_NAME") zsh /dev/stdin <(echo "$SCRIPT")
+echo "$SCRIPT" | ssh admin@$(tart ip "$VM_NAME") zsh /dev/stdin
 script_status=$?
 
 kill $vm_pid
