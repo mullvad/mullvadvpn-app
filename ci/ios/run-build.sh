5a0c577e8899107ff26e5c690846b586bb92cb24
Emīls <emils@mullvad.net>
2023-06-20 16:54:44 +0200

    Update run-build.sh

--- ci/ios/run-build.sh
+++ ci/ios/run-build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env/ bash
-# Buildscript to run on our buildserver to create and upload an app.
+# Buildscript to run on our buildserver to create and upload an iOS app.
+#
+# The script expects two tart VMs, `ios-build` and `ios-upload` to exist and be
+# executable. The build VM is expected to have the appropriate environment to
+# be able to build the app and the upload VM - to upload the app. Both must be
+# reachable via SSH without a password via the SSH key located in
+# ~/.ssh/ios-vm-key.
+
 
 set -eu -o pipefail
 
@@ -7,23 +14,23 @@ set -eu -o pipefail
 ssh-add ~/.ssh/ios-vm-key
 
 VM_BUILD_DIR="\"/Volumes/My Shared Files/build\""
+MULLVAD_CHECKOUT_DIR="${MULLVAD_CHECKOUT_DIR:-mullvadvpn-app}"
 
 BUILD_SCRIPT="
 	set -eu
-	security unlock-keychain -p 'build' 
-	rm -rf ~/build 
-	# copying over build directory because the spaces in "/Volmes/My Shared Files/" fuck up Make and XCode
+	security unlock-keychain -p 'build'
+	rm -rf ~/build
 	cp -r ${VM_BUILD_DIR} ~/build || true
 	cd ~/build/ios
 	rm -r Build
-	IOS_PROVISIONING_PROFILES_DIR=~/provisioning-profiles bash build.sh 
+	IOS_PROVISIONING_PROFILES_DIR=~/provisioning-profiles bash build.sh
 	cp ~/build/ios/Build/MullvadVPN.ipa /Volumes/My\ Shared\ Files/build/ios/Build/
 "
 
 
-bash run-in-vm.sh "ios-build" "${BUILD_SCRIPT}" "build:mullvadvpn-app"
+bash run-in-vm.sh "ios-build" "${BUILD_SCRIPT}" "build:${MULLVAD_CHECKOUT_DIR}"
 mkdir -p build-output
-cp mullvadvpn-app/ios/Build/MullvadVPN.ipa build-output/MullvadVPN.ipa
+cp "${MULLVAD_CHECKOUT_DIR}/ios/Build/MullvadVPN.ipa" build-output/MullvadVPN.ipa
 
 VM_UPLOAD_IPA_PATH="/Volumes/My\ Shared\ Files/build-output/MullvadVPN.ipa"
 # TODO figure out path to API key
