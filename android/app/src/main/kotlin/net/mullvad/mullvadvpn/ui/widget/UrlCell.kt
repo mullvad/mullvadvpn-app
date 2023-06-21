c07c0785b3a857042f4454a77f953ebb92e278ac
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-06-21 10:59:26 +0200

    Add ?hide_nav variants to all mullvad.net links

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/UrlCell.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/UrlCell.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.util.AttributeSet
 import android.widget.ImageView
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.util.appendHideNavOnReleaseBuild
 
 open class UrlCell : Cell {
     private val externalLinkIcon =
@@ -35,7 +36,9 @@ open class UrlCell : Cell {
     private fun loadAttributes(attributes: AttributeSet?) {
         context.theme.obtainStyledAttributes(attributes, R.styleable.Url, 0, 0).apply {
             try {
-                getString(R.styleable.Url_url)?.let { urlString -> url = Uri.parse(urlString) }
+                getString(R.styleable.Url_url)?.let { urlString ->
+                    url = Uri.parse(urlString.appendHideNavOnReleaseBuild())
+                }
             } finally {
                 recycle()
             }
