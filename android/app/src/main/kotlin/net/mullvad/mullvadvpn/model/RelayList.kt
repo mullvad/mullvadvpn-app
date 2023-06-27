f56a18594bef31edde42583764e63c6f4dabfd78
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-06-27 16:16:52 +0200

    Expose wireguard port ranges to the android app

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/model/RelayList.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/model/RelayList.kt
@@ -3,4 +3,8 @@ package net.mullvad.mullvadvpn.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-@Parcelize data class RelayList(val countries: ArrayList<RelayListCountry>) : Parcelable
+@Parcelize
+data class RelayList(
+    val countries: ArrayList<RelayListCountry>,
+    val wireguardEndpointData: WireguardEndpointData
+) : Parcelable
