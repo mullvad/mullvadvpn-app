8467517877e05c3299e74f57f8e23650fc2ac942
Bug Magnet <marco.nikic@mullvad.net>
2023-06-27 14:42:53 +0200

    Enable Shadowsocks bridges selection via RelaySelector

--- ios/MullvadREST/ServerRelaysResponse.swift
+++ ios/MullvadREST/ServerRelaysResponse.swift
@@ -48,30 +48,6 @@ extension REST {
         public let ipv6AddrIn: IPv6Address
         public let publicKey: Data
         public let includeInCountry: Bool
-
-        public init(
-            hostname: String,
-            active: Bool,
-            owned: Bool,
-            location: String,
-            provider: String,
-            weight: UInt64,
-            ipv4AddrIn: IPv4Address,
-            ipv6AddrIn: IPv6Address,
-            publicKey: Data,
-            includeInCountry: Bool
-        ) {
-            self.hostname = hostname
-            self.active = active
-            self.owned = owned
-            self.location = location
-            self.provider = provider
-            self.weight = weight
-            self.ipv4AddrIn = ipv4AddrIn
-            self.ipv6AddrIn = ipv6AddrIn
-            self.publicKey = publicKey
-            self.includeInCountry = includeInCountry
-        }
     }
 
     public struct ServerWireguardTunnels: Codable, Equatable {
@@ -79,18 +55,6 @@ extension REST {
         public let ipv6Gateway: IPv6Address
         public let portRanges: [[UInt16]]
         public let relays: [ServerRelay]
-
-        public init(
-            ipv4Gateway: IPv4Address,
-            ipv6Gateway: IPv6Address,
-            portRanges: [[UInt16]],
-            relays: [REST.ServerRelay]
-        ) {
-            self.ipv4Gateway = ipv4Gateway
-            self.ipv6Gateway = ipv6Gateway
-            self.portRanges = portRanges
-            self.relays = relays
-        }
     }
 
     public struct ServerShadowsocks: Codable, Equatable {
@@ -98,38 +62,16 @@ extension REST {
         public let port: UInt16
         public let cipher: String
         public let password: String
-
-        public init(protocol: String, port: UInt16, cipher: String, password: String) {
-            self.protocol = `protocol`
-            self.port = port
-            self.cipher = cipher
-            self.password = password
-        }
     }
 
     public struct ServerBridges: Codable, Equatable {
         public let shadowsocks: [ServerShadowsocks]
         public let relays: [BridgeRelay]
-
-        public init(shadowsocks: [REST.ServerShadowsocks], relays: [BridgeRelay]) {
-            self.shadowsocks = shadowsocks
-            self.relays = relays
-        }
     }
 
     public struct ServerRelaysResponse: Codable, Equatable {
         public let locations: [String: ServerLocation]
         public let wireguard: ServerWireguardTunnels
         public let bridge: ServerBridges
-
-        public init(
-            locations: [String: REST.ServerLocation],
-            wireguard: REST.ServerWireguardTunnels,
-            bridge: ServerBridges
-        ) {
-            self.locations = locations
-            self.wireguard = wireguard
-            self.bridge = bridge
-        }
     }
 }
