ea37bf1efce0a3e9a69b8aa929047f474ac230e3
Andrej Mihajlov <and@mullvad.net>
2023-06-29 11:59:24 +0200

    Add integration test

---
+++ ios/TunnelObfuscationTests/TCPConnection.swift
@@ -0,0 +1,68 @@
+//
+//  TCPConnection.swift
+//  TunnelObfuscationTests
+//
+//  Created by pronebird on 27/06/2023.
+//  Copyright © 2023 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import Network
+
+/// Minimal implementation of TCP connection capable of receiving data.
+class TCPConnection {
+    private let dispatchQueue = DispatchQueue(label: "TCPConnection")
+    private let nwConnection: NWConnection
+
+    init(nwConnection: NWConnection) {
+        self.nwConnection = nwConnection
+    }
+
+    deinit {
+        cancel()
+    }
+
+    /// Establish TCP connection.
+    /// Waits for the underlying connection to become ready before returning control to the caller, otherwise throws an
+    /// error if connection state indicates as such.
+    func start() async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            nwConnection.stateUpdateHandler = { state in
+                switch state {
+                case .ready:
+                    continuation.resume(returning: ())
+                case let .failed(error):
+                    continuation.resume(throwing: error)
+                case .cancelled:
+                    continuation.resume(throwing: CancellationError())
+                default:
+                    return
+                }
+                // Reset state update handler after resuming continuation.
+                self.nwConnection.stateUpdateHandler = nil
+            }
+            nwConnection.start(queue: dispatchQueue)
+        }
+    }
+
+    func cancel() {
+        nwConnection.cancel()
+    }
+
+    func receiveData(minimumLength: Int, maximumLength: Int) async throws -> Data {
+        return try await withCheckedThrowingContinuation { continuation in
+            nwConnection.receive(
+                minimumIncompleteLength: minimumLength,
+                maximumLength: maximumLength
+            ) { content, contentContext, isComplete, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                } else if let content {
+                    continuation.resume(returning: content)
+                } else if isComplete {
+                    continuation.resume(returning: Data())
+                }
+            }
+        }
+    }
+}
