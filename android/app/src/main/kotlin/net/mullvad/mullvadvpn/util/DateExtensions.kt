f02cfb96f86e69c136e36a3e65c2fe86b7f0da29
saber safavi <saber.safavi@codic.se>
2023-07-06 11:19:46 +0200

    Add ViewModel and related MVVM parts

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/DateExtensions.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/DateExtensions.kt
@@ -1,6 +1,10 @@
 package net.mullvad.mullvadvpn.util
 
+import java.text.DateFormat
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
 fun DateTime.formatDate(): String = ISODateTimeFormat.date().print(this)
+
+fun DateTime.toExpiryDateString(): String =
+    DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT).format(this.toDate())
