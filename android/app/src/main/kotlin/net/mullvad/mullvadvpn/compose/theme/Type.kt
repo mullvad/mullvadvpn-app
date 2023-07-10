cbcf2b1929a398fa9e4cc732ba29472b0dec27e2
Matilda <matilda@mullvad.net>
2023-07-10 10:34:14 +0200

    Remove comment, color, and textalign

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/theme/Type.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/theme/Type.kt
@@ -1,11 +1,9 @@
 package net.mullvad.mullvadvpn.compose.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
 /**
@@ -40,12 +38,10 @@ internal val MullvadMaterial3Typography =
             fontWeight = FontWeight.Bold,
             fontSize = 32.sp,
             lineHeight = 36.sp,
-            textAlign = TextAlign.End,
-            color = Color.White,
         ),
         headlineMedium = TextStyle(fontFamily = FontFamily.SansSerif),
         headlineSmall = TextStyle(fontFamily = FontFamily.SansSerif),
-        titleLarge = //List-items (parent) + Button labels
+        titleLarge =
         TextStyle(
             fontFamily = FontFamily.SansSerif,
             fontWeight = FontWeight.Medium,
