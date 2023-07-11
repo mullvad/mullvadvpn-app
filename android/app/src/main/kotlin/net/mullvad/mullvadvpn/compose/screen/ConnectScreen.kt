76242d0a6c8a3a94336b66dd094d043936e89475
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-07-11 16:07:50 +0200

    Add ConnectScreen instrumental tests

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/ConnectScreen.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/ConnectScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import net.mullvad.mullvadvpn.R
@@ -26,6 +27,11 @@ import net.mullvad.mullvadvpn.compose.button.SwitchLocationButton
 import net.mullvad.mullvadvpn.compose.component.ConnectionStatusText
 import net.mullvad.mullvadvpn.compose.component.LocationInfo
 import net.mullvad.mullvadvpn.compose.state.ConnectUiState
+import net.mullvad.mullvadvpn.compose.test.CIRCULAR_PROGRESS_INDICATOR
+import net.mullvad.mullvadvpn.compose.test.CONNECT_BUTTON_TEST_TAG
+import net.mullvad.mullvadvpn.compose.test.LOCATION_INFO_TEST_TAG
+import net.mullvad.mullvadvpn.compose.test.RECONNECT_BUTTON_TEST_TAG
+import net.mullvad.mullvadvpn.compose.test.SELECT_LOCATION_BUTTON_TEST_TAG
 import net.mullvad.mullvadvpn.compose.theme.AppTheme
 import net.mullvad.mullvadvpn.compose.theme.Dimens
 import net.mullvad.mullvadvpn.model.TunnelState
@@ -72,6 +78,7 @@ fun ConnectScreen(
                             height = Dimens.progressIndicatorSize
                         )
                         .align(Alignment.CenterHorizontally)
+                        .testTag(CIRCULAR_PROGRESS_INDICATOR)
             )
         }
         Spacer(modifier = Modifier.height(Dimens.smallPadding))
@@ -97,7 +104,7 @@ fun ConnectScreen(
                     is TunnelState.Connecting -> uiState.tunnelRealState.endpoint
                     else -> null
                 },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth().testTag(LOCATION_INFO_TEST_TAG)
         )
         Spacer(modifier = Modifier.height(Dimens.buttonSeparation))
         val showLocation =
@@ -115,7 +122,10 @@ fun ConnectScreen(
                 is TunnelState.Error -> true
             }
         SwitchLocationButton(
-            modifier = Modifier.fillMaxWidth().height(Dimens.selectLocationButtonHeight),
+            modifier =
+                Modifier.fillMaxWidth()
+                    .height(Dimens.selectLocationButtonHeight)
+                    .testTag(SELECT_LOCATION_BUTTON_TEST_TAG),
             onClick = onSwitchLocationClick,
             showChevron = showLocation,
             text =
@@ -128,11 +138,15 @@ fun ConnectScreen(
         Spacer(modifier = Modifier.height(Dimens.buttonSeparation))
         ConnectionButton(
             state = uiState.tunnelUiState,
-            modifier = Modifier.fillMaxWidth().height(Dimens.connectButtonHeight),
+            modifier =
+                Modifier.fillMaxWidth()
+                    .height(Dimens.connectButtonHeight)
+                    .testTag(CONNECT_BUTTON_TEST_TAG),
             disconnectClick = onDisconnectClick,
             reconnectClick = onReconnectClick,
             cancelClick = onCancelClick,
             connectClick = onConnectClick,
+            reconnectButtonTestTag = RECONNECT_BUTTON_TEST_TAG
         )
     }
 }
