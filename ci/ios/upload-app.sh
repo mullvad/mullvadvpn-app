08f565cd261f33dcce0b2640220e63ab63f01343
Emīls <emils@mullvad.net>
2023-07-21 18:40:35 +0200

    Fix upload script

--- ci/ios/upload-app.sh
+++ ci/ios/upload-app.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 # Upload script to run in `ios-upload` VM to upload a newly built IPA to TestFlight
 
-VM_UPLOAD_IPA_PATH="/Volumes/My\ Shared\ Files/build-output/MullvadVPN.ipa"
-API_KEY_PATH="~/ci/app-store-connect-key.json"
+VM_UPLOAD_IPA_PATH="/Volumes/My Shared Files/build-output/MullvadVPN.ipa"
+API_KEY_PATH="$HOME/ci/app-store-connect-key.json"
 cd ci/
 source ~/.zshrc
 source ~/.zprofile
