6c265b9f819bfae7ba8def7bfe3c0d639042dd00
Bug Magnet <marco.nikic@mullvad.net>
2023-07-21 16:47:44 +0100

    Fix the settings being reset when upgrading from 2023.3 or lower

--- ios/MullvadVPN/SettingsManager/TunnelSettingsV2.swift
+++ ios/MullvadVPN/SettingsManager/TunnelSettingsV2.swift
@@ -43,12 +43,26 @@ struct StoredAccountData: Codable, Equatable {
     /// Account expiry.
     var expiry: Date
 
-    /// be set `true` when account is created and  be flipped to `false` when user adds more credit
+    /// Set to `true` when the account is created and flipped to `false` when the user adds more credit.
     var isNew = false
 
     /// Returns `true` if account has expired.
     var isExpired: Bool {
         expiry <= Date()
+    }
+}
+
+extension StoredAccountData {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.identifier = try container.decode(String.self, forKey: .identifier)
+        self.number = try container.decode(String.self, forKey: .number)
+        self.expiry = try container.decode(Date.self, forKey: .expiry)
+
+        // When the app is upgraded from 2023.3 or below, this field won't exist, and the auto synthesized init will fail.
+        // This leads to a reset of the settings. If the key isn't present, consider the account not new to avoid the issue.
+        let isNewAccount = try? container.decode(Bool.self, forKey: .isNew)
+        self.isNew = isNewAccount ?? false
     }
 }
 
