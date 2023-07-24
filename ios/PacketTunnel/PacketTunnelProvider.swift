01f0e0d6c4dec53f411165425bff1acebcd66a4c
Bug Magnet <marco.nikic@mullvad.net>
2023-07-24 09:54:13 +0100

    Cache the transportStrategy in the UserDefaults

--- ios/PacketTunnel/PacketTunnelProvider.swift
+++ ios/PacketTunnel/PacketTunnelProvider.swift
@@ -145,11 +145,17 @@ class PacketTunnelProvider: NEPacketTunnelProvider, TunnelMonitorDelegate {
         let urlSession = REST.makeURLSession()
         let urlSessionTransport = URLSessionTransport(urlSession: urlSession)
         let shadowsocksCache = ShadowsocksConfigurationCache(cacheDirectory: containerURL)
+
+        // This init cannot fail as long as the security group identifier is valid
+        let sharedUserDefaults = UserDefaults(suiteName: ApplicationConfiguration.securityGroupIdentifier)!
+        let transportStrategy = TransportStrategy(sharedUserDefaults)
+
         let transportProvider = TransportProvider(
             urlSessionTransport: urlSessionTransport,
             relayCache: relayCache,
             addressCache: addressCache,
             shadowsocksCache: shadowsocksCache,
+            transportStrategy: transportStrategy,
             constraintsUpdater: constraintsUpdater
         )
 
