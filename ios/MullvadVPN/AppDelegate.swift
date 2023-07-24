2dbf59551e088428318b73493d581f4ed29d54b1
Bug Magnet <marco.nikic@mullvad.net>
2023-07-24 09:54:13 +0100

    Cache the transportStrategy in the UserDefaults

--- ios/MullvadVPN/AppDelegate.swift
+++ ios/MullvadVPN/AppDelegate.swift
@@ -100,11 +100,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
         let urlSessionTransport = URLSessionTransport(urlSession: REST.makeURLSession())
         let shadowsocksCache = ShadowsocksConfigurationCache(cacheDirectory: containerURL)
+
+        // This init cannot fail as long as the security group identifier is valid
+        let sharedUserDefaults = UserDefaults(suiteName: ApplicationConfiguration.securityGroupIdentifier)!
+        let transportStrategy = TransportStrategy(sharedUserDefaults)
+
         let transportProvider = TransportProvider(
             urlSessionTransport: urlSessionTransport,
             relayCache: relayCache,
             addressCache: addressCache,
             shadowsocksCache: shadowsocksCache,
+            transportStrategy: transportStrategy,
             constraintsUpdater: constraintsUpdater
         )
 
