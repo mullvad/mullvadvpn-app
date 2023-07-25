bce3631e1ca5924c1c1414d5cced5f8ebbd8a6a0
Albin <albin@mullvad.net>
2023-07-25 15:31:54 +0200

    Move ListItemData to applist package

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/applist/ListItemData.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/applist/ListItemData.kt
@@ -1,8 +1,9 @@
-package net.mullvad.mullvadvpn.model
+package net.mullvad.mullvadvpn.applist
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.IntDef
 import androidx.annotation.StringRes
+import net.mullvad.mullvadvpn.model.WidgetState
 
 data class ListItemData
 private constructor(
