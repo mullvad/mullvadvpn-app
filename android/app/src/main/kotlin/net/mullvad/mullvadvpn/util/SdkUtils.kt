c726db88039f0700b58946f7292d87dbda3b5bbb
Albin <albin@mullvad.net>
2023-07-25 13:24:57 +0200

    Fix formatting

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/SdkUtils.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/SdkUtils.kt
@@ -5,7 +5,6 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
-import android.net.VpnService
 import android.os.Build
 import android.service.quicksettings.Tile
 
