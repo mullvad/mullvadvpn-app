819d7b21992f6a625f4a384b744af81ed4a5da90
Albin <albin@mullvad.net>
2023-07-25 15:31:54 +0200

    Move ListItemData to applist package

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/applist/ViewIntent.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/applist/ViewIntent.kt
@@ -1,7 +1,5 @@
 package net.mullvad.mullvadvpn.applist
 
-import net.mullvad.mullvadvpn.model.ListItemData
-
 sealed class ViewIntent {
     // In future we will have search intent
     data class ChangeApplicationGroup(val item: ListItemData) : ViewIntent()
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/ListItemListener.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/ListItemListener.kt
@@ -1,6 +1,6 @@
 package net.mullvad.mullvadvpn.ui
 
-import net.mullvad.mullvadvpn.model.ListItemData
+import net.mullvad.mullvadvpn.applist.ListItemData
 
 interface ListItemListener {
     fun onItemAction(item: ListItemData)
