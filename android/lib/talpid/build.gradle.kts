da3cda6e34632abf226a2d97ea3f500dd0354187
Albin <albin@mullvad.net>
2023-07-25 11:46:01 +0200

    Add empty talpid module

---
+++ android/lib/talpid/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id(Dependencies.Plugin.androidLibraryId)
+    id(Dependencies.Plugin.kotlinAndroidId)
+    id(Dependencies.Plugin.kotlinParcelizeId)
+}
+
+android {
+    namespace = "net.mullvad.talpid"
+    compileSdk = Versions.Android.compileSdkVersion
+
+    defaultConfig {
+        minSdk = Versions.Android.minSdkVersion
+        targetSdk = Versions.Android.targetSdkVersion
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = Versions.jvmTarget
+    }
+
+    lint {
+        lintConfig = file("${rootProject.projectDir}/config/lint.xml")
+        abortOnError = true
+        warningsAsErrors = true
+    }
+}
+
+dependencies {
+    implementation(Dependencies.Kotlin.stdlib)
+}
