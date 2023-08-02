dcddce5f1784776652e15feca946a5b6571adcec
saber safavi <saber.safavi@codic.se>
2023-08-02 11:14:13 +0200

    Fix copy button scaling issue

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/AccountNumberView.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/AccountNumberView.kt
@@ -1,15 +1,21 @@
 package net.mullvad.mullvadvpn.compose.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import net.mullvad.mullvadvpn.lib.common.util.groupPasswordModeWithSpaces
 import net.mullvad.mullvadvpn.lib.common.util.groupWithSpaces
 
 @Composable
-fun AccountNumberView(accountNumber: String, doObfuscateWithPasswordDots: Boolean) {
+fun AccountNumberView(
+    accountNumber: String,
+    doObfuscateWithPasswordDots: Boolean,
+    modifier: Modifier = Modifier
+) {
     InformationView(
         content =
             if (doObfuscateWithPasswordDots) accountNumber.groupPasswordModeWithSpaces()
             else accountNumber.groupWithSpaces(),
+        modifier = modifier,
         whenMissing = MissingPolicy.SHOW_SPINNER
     )
 }
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/Text.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/Text.kt
@@ -3,7 +3,12 @@ package net.mullvad.mullvadvpn.compose.component
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
@@ -13,6 +18,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+private val DEFAULT_TEXT_STEP = 1.sp
 
 @Composable
 fun CapsText(
@@ -50,5 +58,39 @@ fun CapsText(
         maxLines = maxLines,
         onTextLayout = onTextLayout,
         style = style,
+    )
+}
+
+@Composable
+fun AutoResizeText(
+    text: String,
+    minTextSize: TextUnit,
+    maxTextSize: TextUnit,
+    modifier: Modifier = Modifier,
+    style: TextStyle = LocalTextStyle.current,
+    maxLines: Int = Int.MAX_VALUE,
+) {
+    var fontSizeValue by remember { mutableStateOf(maxTextSize.value) }
+    var readyToDraw by remember { mutableStateOf(false) }
+
+    Text(
+        text = text,
+        maxLines = maxLines,
+        style = style,
+        fontSize = fontSizeValue.sp,
+        onTextLayout = {
+            if (it.didOverflowHeight && !readyToDraw) {
+                val nextFontSizeValue = fontSizeValue - DEFAULT_TEXT_STEP.value
+                if (nextFontSizeValue <= minTextSize.value) {
+                    fontSizeValue = minTextSize.value
+                    readyToDraw = true
+                } else {
+                    fontSizeValue = nextFontSizeValue
+                }
+            } else {
+                readyToDraw = true
+            }
+        },
+        modifier = modifier.drawWithContent { if (readyToDraw) drawContent() }
     )
 }
