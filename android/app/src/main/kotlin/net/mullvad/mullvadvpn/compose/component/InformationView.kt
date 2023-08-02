4dc6dafd543c0892fb4380dd29be13615161b441
saber safavi <saber.safavi@codic.se>
2023-08-02 11:14:13 +0200

    Fix copy button scaling issue

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/InformationView.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/InformationView.kt
@@ -1,15 +1,19 @@
 package net.mullvad.mullvadvpn.compose.component
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import net.mullvad.mullvadvpn.compose.theme.Dimens
+import net.mullvad.mullvadvpn.compose.theme.typeface.TypeScale.TextMedium
+import net.mullvad.mullvadvpn.compose.theme.typeface.TypeScale.TextSmall
 
 @Preview
 @Composable
@@ -24,48 +28,59 @@ private fun PreviewEmptyInformationView() {
 }
 
 @Composable
-fun InformationView(content: String, whenMissing: MissingPolicy = MissingPolicy.SHOW_VIEW) {
+fun InformationView(
+    content: String,
+    modifier: Modifier = Modifier,
+    whenMissing: MissingPolicy = MissingPolicy.SHOW_VIEW,
+    maxLines: Int = 1
+) {
     return if (content.isNotEmpty()) {
-        Text(
+        AutoResizeText(
             style = MaterialTheme.typography.titleSmall,
             text = content,
+            minTextSize = TextSmall,
+            maxTextSize = TextMedium,
+            maxLines = maxLines,
             modifier =
-                Modifier.padding(
+                modifier.padding(
                     start = Dimens.sideMargin,
-                    end = Dimens.sideMargin,
                     top = Dimens.smallPadding,
-                    bottom = Dimens.mediumPadding
+                    bottom = Dimens.smallPadding
                 )
         )
     } else {
         when (whenMissing) {
             MissingPolicy.SHOW_VIEW -> {
-                Text(
+                AutoResizeText(
                     style = MaterialTheme.typography.titleMedium,
                     text = content,
+                    minTextSize = TextSmall,
+                    maxTextSize = TextMedium,
+                    maxLines = maxLines,
                     modifier =
-                        Modifier.padding(
+                        modifier.padding(
                             start = Dimens.sideMargin,
-                            end = Dimens.sideMargin,
                             top = Dimens.smallPadding,
-                            bottom = Dimens.mediumPadding
+                            bottom = Dimens.smallPadding
                         )
                 )
             }
             MissingPolicy.HIDE_VIEW -> {}
             MissingPolicy.SHOW_SPINNER -> {
-                CircularProgressIndicator(
-                    modifier =
-                        Modifier.padding(
-                                start = Dimens.sideMargin,
-                                end = Dimens.sideMargin,
-                                top = Dimens.smallPadding,
-                                bottom = Dimens.mediumPadding
-                            )
-                            .height(Dimens.loadingSpinnerSizeMedium)
-                            .width(Dimens.loadingSpinnerSizeMedium),
-                    color = MaterialTheme.colorScheme.onSecondary
-                )
+                Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
+                    CircularProgressIndicator(
+                        modifier =
+                            Modifier.padding(
+                                    start = Dimens.sideMargin,
+                                    top = Dimens.smallPadding,
+                                    bottom = Dimens.smallPadding
+                                )
+                                .height(Dimens.loadingSpinnerSizeMedium)
+                                .width(Dimens.loadingSpinnerSizeMedium),
+                        color = MaterialTheme.colorScheme.onSecondary
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                }
             }
         }
     }
