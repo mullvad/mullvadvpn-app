87d9daabae985a05f555132311cc7ab0096e4d46
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-08-17 13:44:16 +0200

    Migrate welcome view to compose

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/WelcomeUiState.kt
@@ -0,0 +1,8 @@
+package net.mullvad.mullvadvpn.compose.state
+
+import net.mullvad.mullvadvpn.model.TunnelState
+
+data class WelcomeUiState(
+    val tunnelState: TunnelState = TunnelState.Disconnected,
+    val accountNumber: String? = null
+)
---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/AccountExpiryConstant.kt
@@ -0,0 +1,3 @@
+package net.mullvad.mullvadvpn.constant
+
+const val ACCOUNT_EXPIRY_POLL_INTERVAL: Long = 15 /* s */ * 1000 /* ms */
