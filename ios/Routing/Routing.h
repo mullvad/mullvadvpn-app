6d4c49e4b0517c8e8d3ce591ba66c0b7df8399b1
Jon Petersson <jon.petersson@kvadrat.se>
2023-08-18 15:35:20 +0200

    Introduce new Routing framework for high level router and coordinator code

---
+++ ios/Routing/Routing.h
@@ -0,0 +1,19 @@
+//
+//  Routing.h
+//  Routing
+//
+//  Created by Jon Petersson on 2023-08-18.
+//  Copyright © 2023 Mullvad VPN AB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Routing.
+FOUNDATION_EXPORT double RoutingVersionNumber;
+
+//! Project version string for Routing.
+FOUNDATION_EXPORT const unsigned char RoutingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Routing/PublicHeader.h>
+
+
