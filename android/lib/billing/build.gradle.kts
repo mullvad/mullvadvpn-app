b9dbd02c43cd0db1bc42ccbf414a833ef028badb
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-08-21 12:01:05 +0200

    Add billing client library and billing module

---
+++ android/lib/billing/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id(Dependencies.Plugin.androidLibraryId)
+    id(Dependencies.Plugin.kotlinAndroidId)
+}
+
+android {
+    namespace = "net.mullvad.mullvadvpn.lib.billing"
+    compileSdk = Versions.Android.compileSdkVersion
+
+    defaultConfig {
+        minSdk = Versions.Android.minSdkVersion
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = Versions.jvmTarget
+    }
+
+    lint {
+        lintConfig = file("${rootProject.projectDir}/config/lint.xml")
+        abortOnError = true
+        warningsAsErrors = true
+    }
+}
+
+dependencies {
+    //Billing library
+    implementation(Dependencies.billingClient)
+}
