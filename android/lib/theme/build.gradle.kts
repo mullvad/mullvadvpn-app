d2cb07cc7267e304390e99e5ba7481e96af21566
Albin <albin@mullvad.net>
2023-08-23 13:39:56 +0200

    Create empty theme lib

---
+++ android/lib/theme/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id(Dependencies.Plugin.androidLibraryId)
+    id(Dependencies.Plugin.kotlinAndroidId)
+}
+
+android {
+    namespace = "net.mullvad.mullvadvpn.lib.theme"
+    compileSdk = Versions.Android.compileSdkVersion
+
+    defaultConfig {
+        minSdk = Versions.Android.minSdkVersion
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.kotlinCompilerExtensionVersion
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = Versions.jvmTarget
+    }
+
+    lint {
+        lintConfig = file("${rootProject.projectDir}/config/lint.xml")
+        abortOnError = true
+        warningsAsErrors = true
+    }
+}
+
+dependencies {
+    implementation(Dependencies.Compose.material3)
+    implementation(Dependencies.Compose.ui)
+    implementation(Dependencies.Kotlin.stdlib)
+}
