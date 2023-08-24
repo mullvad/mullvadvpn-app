b9706b740c9bec48114855ca0bfc1c15e4eaaba1
saber safavi <saber.safavi@codic.se>
2023-08-24 10:51:29 +0200

    Add info dialog for device name

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/AccountUiState.kt
@@ -0,0 +1,28 @@
+package net.mullvad.mullvadvpn.viewmodel
+
+import net.mullvad.mullvadvpn.model.AccountExpiry
+import net.mullvad.mullvadvpn.model.DeviceState
+import org.joda.time.DateTime
+
+data class AccountUiState(
+    val deviceName: String,
+    val accountNumber: String?,
+    val accountExpiry: DateTime?,
+    val dialogState: AccountScreenDialogState = AccountScreenDialogState.NoDialog
+) {
+    companion object {
+        fun default() =
+            AccountUiState(
+                deviceName = DeviceState.Unknown.deviceName() ?: "",
+                accountNumber = DeviceState.Unknown.token(),
+                accountExpiry = AccountExpiry.Missing.date(),
+                dialogState = AccountScreenDialogState.NoDialog
+            )
+    }
+}
+
+sealed class AccountScreenDialogState {
+    data object NoDialog : AccountScreenDialogState()
+
+    data object DeviceNameInfoDialog : AccountScreenDialogState()
+}
