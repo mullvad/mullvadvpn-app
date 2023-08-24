a11342b711749c3a4a7a3f1e22ccf69a335d7f0c
saber safavi <saber.safavi@codic.se>
2023-08-24 10:51:29 +0200

    Add info dialog for device name

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/AccountViewModel.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/AccountViewModel.kt
@@ -3,13 +3,14 @@ package net.mullvad.mullvadvpn.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import net.mullvad.mullvadvpn.compose.state.AccountUiState
 import net.mullvad.mullvadvpn.repository.AccountRepository
 import net.mullvad.mullvadvpn.repository.DeviceRepository
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnectionManager
@@ -25,27 +26,24 @@ class AccountViewModel(
     private val _enterTransitionEndAction = MutableSharedFlow<Unit>()
     val viewActions = _viewActions.asSharedFlow()
 
+    private val dialogState =
+        MutableStateFlow<AccountScreenDialogState>(AccountScreenDialogState.NoDialog)
+
     private val vmState: StateFlow<AccountUiState> =
-        combine(deviceRepository.deviceState, accountRepository.accountExpiryState) {
+        combine(deviceRepository.deviceState, accountRepository.accountExpiryState, dialogState) {
                 deviceState,
-                accountExpiry ->
+                accountExpiry,
+                dialogState ->
                 AccountUiState(
-                    deviceName = deviceState.deviceName() ?: "",
-                    accountNumber = deviceState.token() ?: "",
-                    accountExpiry = accountExpiry.date()
+                    deviceName = deviceState.deviceName(),
+                    accountNumber = deviceState.token(),
+                    accountExpiry = accountExpiry.date(),
+                    dialogState = dialogState
                 )
             }
-            .stateIn(
-                viewModelScope,
-                SharingStarted.WhileSubscribed(),
-                AccountUiState(deviceName = "", accountNumber = "", accountExpiry = null)
-            )
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), AccountUiState.default())
     val uiState =
-        vmState.stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(),
-            AccountUiState(deviceName = "", accountNumber = "", accountExpiry = null)
-        )
+        vmState.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), AccountUiState.default())
 
     val enterTransitionEndAction = _enterTransitionEndAction.asSharedFlow()
 
@@ -61,6 +59,18 @@ class AccountViewModel(
 
     fun onLogoutClick() {
         accountRepository.logout()
+    }
+
+    fun onDeviceNameInfoClick() {
+        dialogState.update { AccountScreenDialogState.DeviceNameInfoDialog }
+    }
+
+    fun onDismissInfoClick() {
+        hideDialog()
+    }
+
+    private fun hideDialog() {
+        dialogState.update { AccountScreenDialogState.NoDialog }
     }
 
     fun onTransitionAnimationEnd() {
