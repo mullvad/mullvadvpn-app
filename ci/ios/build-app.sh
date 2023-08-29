3dac4fcc8a943b00cb8a28d1458bc09e1fc51b73
Emīls <emils@mullvad.net>
2023-08-29 14:40:40 +0200

    Bashify build-app.sh

--- ci/ios/build-app.sh
+++ ci/ios/build-app.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 # Buildscript to run inside a build VM to build a new IPA for the iOS app.
 
 # This single path really screws with XCode and wireguard-go's makefiles, which
@@ -12,7 +12,7 @@ VM_BUILD_DIR="/Volumes/My Shared Files/build"
 security unlock-keychain -p 'build'
 
 rm -rf ~/build
-cp -r ${VM_BUILD_DIR} ~/build || true
+cp -r "$VM_BUILD_DIR" ~/build || true
 cd ~/build/ios
 rm -r Build
 
@@ -23,4 +23,4 @@ IOS_PROVISIONING_PROFILES_DIR=~/provisioning-profiles \
     PATH=/usr/local/go/bin:$PATH \
     bash build.sh
 
-cp ~/build/ios/Build/MullvadVPN.ipa /Volumes/My\ Shared\ Files/build/ios/Build/
+cp "$HOME/build/ios/Build/MullvadVPN.ipa" "/Volumes/My Shared Files/build/ios/Build/"
