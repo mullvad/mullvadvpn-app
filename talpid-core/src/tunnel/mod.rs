96a071c953d69b800c14f59a1f5d47b241dddde1
Emīls <emils@mullvad.net>
2023-08-29 19:28:37 +0200

    Remove superfluous string escaping

--- talpid-core/src/tunnel/mod.rs
+++ talpid-core/src/tunnel/mod.rs
@@ -317,7 +317,7 @@ fn is_ipv6_enabled_in_os() -> bool {
     // Check registry if IPv6 is disabled on tunnel interfaces, as documented in
     // https://support.microsoft.com/en-us/help/929852/guidance-for-configuring-ipv6-in-windows-for-advanced-users
     let globally_enabled = RegKey::predef(HKEY_LOCAL_MACHINE)
-        .open_subkey(r#"SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters"#)
+        .open_subkey(r"SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters")
         .and_then(|ipv6_config| ipv6_config.get_value("DisabledComponents"))
         .map(|ipv6_disabled_bits: u32| (ipv6_disabled_bits & IPV6_DISABLED_ON_TUNNELS_MASK) == 0)
         .unwrap_or(true);
