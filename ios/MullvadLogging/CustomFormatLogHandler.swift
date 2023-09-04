43b4128be5d6a5261f1c9c17c0cbe04942a4b820
Bug Magnet <marco.nikic@mullvad.net>
2023-09-04 16:41:53 +0200

    Fix swiftlint warning function_parameter_count

--- ios/MullvadLogging/CustomFormatLogHandler.swift
+++ ios/MullvadLogging/CustomFormatLogHandler.swift
@@ -38,6 +38,7 @@ public struct CustomFormatLogHandler: LogHandler {
         }
     }
 
+    // swiftlint:disable:next function_parameter_count
     public func log(
         level: Logger.Level,
         message: Logger.Message,
