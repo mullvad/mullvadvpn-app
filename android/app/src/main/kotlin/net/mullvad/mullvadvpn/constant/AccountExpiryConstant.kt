668bbf078475d1b8f3c1a98707838ec5a8440692
saber safavi <saber.safavi@codic.se>
2023-09-05 12:54:08 +0200

    Migrate redeem voucher dialog into compose

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/AccountExpiryConstant.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/AccountExpiryConstant.kt
@@ -1,3 +1,5 @@
 package net.mullvad.mullvadvpn.constant
 
 const val ACCOUNT_EXPIRY_POLL_INTERVAL: Long = 15 /* s */ * 1000 /* ms */
+const val SECONDS_PER_DAY: Long =
+    60 /* seconds per minute */ * 60 /* minutes per hour */ * 24 /* hours per day */
