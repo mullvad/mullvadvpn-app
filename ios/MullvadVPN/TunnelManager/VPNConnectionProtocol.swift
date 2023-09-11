b23b5f3a0792c782e5e1dfd4717d414ccb7d4c2c
Bug Magnet <marco.nikic@mullvad.net>
2023-09-11 14:38:59 +0200

    Re enable extensions on NetworkExtension types that were accidentally removed

--- ios/MullvadVPN/TunnelManager/VPNConnectionProtocol.swift
+++ ios/MullvadVPN/TunnelManager/VPNConnectionProtocol.swift
@@ -9,6 +9,24 @@
 import Foundation
 import NetworkExtension
 
+protocol VPNTunnelProviderManagerProtocol: Equatable {
+    associatedtype SelfType: VPNTunnelProviderManagerProtocol
+    associatedtype ConnectionType: VPNConnectionProtocol
+
+    var isEnabled: Bool { get set }
+    var protocolConfiguration: NEVPNProtocol? { get set }
+    var localizedDescription: String? { get set }
+    var connection: ConnectionType { get }
+
+    init()
+
+    func loadFromPreferences(completionHandler: @escaping (Error?) -> Void)
+    func saveToPreferences(completionHandler: ((Error?) -> Void)?)
+    func removeFromPreferences(completionHandler: ((Error?) -> Void)?)
+
+    static func loadAllFromPreferences(completionHandler: @escaping ([SelfType]?, Error?) -> Void)
+}
+
 protocol VPNConnectionProtocol: NSObject {
     var status: NEVPNStatus { get }
     var connectedDate: Date? { get }
@@ -21,3 +39,7 @@ protocol VPNConnectionProtocol: NSObject {
 protocol VPNTunnelProviderSessionProtocol {
     func sendProviderMessage(_ messageData: Data, responseHandler: ((Data?) -> Void)?) throws
 }
+
+extension NEVPNConnection: VPNConnectionProtocol {}
+extension NETunnelProviderSession: VPNTunnelProviderSessionProtocol {}
+extension NETunnelProviderManager: VPNTunnelProviderManagerProtocol {}
