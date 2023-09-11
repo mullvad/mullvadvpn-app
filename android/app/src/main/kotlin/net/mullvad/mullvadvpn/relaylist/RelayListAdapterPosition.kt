526989e281c6feae75e38b8281069fe560b44be4
David Göransson <david.goransson90@gmail.com>
2023-09-11 16:57:08 +0200

    Remove unused classes

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/RelayListAdapterPosition.kt
+++
@@ -1,3 +0,0 @@
-package net.mullvad.mullvadvpn.relaylist
-
-data class RelayListAdapterPosition(var position: Int)
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/WidgetState.kt
+++
@@ -1,9 +0,0 @@
-package net.mullvad.mullvadvpn.ui.widget
-
-import androidx.annotation.DrawableRes
-
-sealed class WidgetState {
-    data class ImageState(@DrawableRes val imageRes: Int) : WidgetState()
-
-    data class SwitchState(val isChecked: Boolean) : WidgetState()
-}
