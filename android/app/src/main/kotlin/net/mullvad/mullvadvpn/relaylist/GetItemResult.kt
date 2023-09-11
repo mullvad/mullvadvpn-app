5dc350343d31f5ea88ab6660ab4e46b26989979f
David Göransson <david.goransson90@gmail.com>
2023-09-11 16:57:08 +0200

    Remove unused classes

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/GetItemResult.kt
+++
@@ -1,7 +0,0 @@
-package net.mullvad.mullvadvpn.relaylist
-
-sealed class GetItemResult {
-    data class Item(val item: RelayItem) : GetItemResult()
-
-    data class Count(val count: Int) : GetItemResult()
-}
