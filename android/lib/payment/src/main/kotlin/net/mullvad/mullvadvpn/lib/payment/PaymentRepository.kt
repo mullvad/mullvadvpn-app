f4a77279cedb81b94ea2c3dfaf0c1fa5518c8e56
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-09-12 16:12:18 +0200

    Add purchase verification function to payment repository

--- android/lib/payment/src/main/kotlin/net/mullvad/mullvadvpn/lib/payment/PaymentRepository.kt
+++ android/lib/payment/src/main/kotlin/net/mullvad/mullvadvpn/lib/payment/PaymentRepository.kt
@@ -3,9 +3,11 @@ package net.mullvad.mullvadvpn.lib.payment
 import kotlinx.coroutines.flow.singleOrNull
 import net.mullvad.mullvadvpn.lib.billing.BillingRepository
 import net.mullvad.mullvadvpn.lib.billing.model.BillingProduct
+import net.mullvad.mullvadvpn.lib.billing.model.BillingPurchase
 import net.mullvad.mullvadvpn.lib.billing.model.PurchaseEvent
 import net.mullvad.mullvadvpn.lib.billing.model.PurchaseFlowResult
 import net.mullvad.mullvadvpn.lib.billing.model.QueryProductResult
+import net.mullvad.mullvadvpn.lib.billing.model.QueryPurchasesResult
 
 class PaymentRepository(
     private val billingRepository: BillingRepository,
@@ -36,7 +38,7 @@ class PaymentRepository(
                 }
                 is PurchaseEvent.PurchaseCompleted -> {
                     // Verify towards api
-                    if (verifyPurchase()) {
+                    if (verifyPurchase(purchaseEvent.purchases.first())) {
                         PurchaseResult.PurchaseCompleted
                     } else {
                         PurchaseResult.VerificationError
@@ -54,6 +56,13 @@ class PaymentRepository(
         } else {
             // Return error
             return PurchaseResult.PurchaseError
+        }
+    }
+
+    suspend fun verifyPurchases() {
+        val result = billingRepository.queryPurchases()
+        if (result is QueryPurchasesResult.PurchaseFound) {
+            verifyPurchase(result.purchase)
         }
     }
 
@@ -75,7 +84,7 @@ class PaymentRepository(
         return "BOOPITOBOP"
     }
 
-    private fun verifyPurchase(): Boolean {
+    private fun verifyPurchase(purchase: BillingPurchase): Boolean {
         // Placeholder function
         return true
     }
