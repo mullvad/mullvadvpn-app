5a54835d4ead23be5dcda581dddb2719fbc4370f
Markus Pettersson <markus.pettersson@mullvad.net>
2023-09-13 11:50:46 +0200

    Update used access methods on a daemon settings change

--- mullvad-daemon/src/access_methods.rs
+++ mullvad-daemon/src/access_methods.rs
@@ -71,5 +71,16 @@ where
             self.event_listener
                 .notify_settings(self.settings.to_settings());
         };
+
+        // TODO: Could this be replaced by message passing? Yes plz.
+        let mut connection_modes = self.connection_modes.lock().unwrap();
+        *connection_modes = self
+            .settings
+            .api_access_methods
+            .api_access_methods
+            .clone()
+            .into_iter()
+            .map(|x| (x, 1))
+            .collect();
     }
 }
