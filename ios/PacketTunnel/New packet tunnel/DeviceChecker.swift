4afcef075c10e018116f226776a53674303d50b3
Andrej Mihajlov <and@mullvad.net>
2023-09-13 10:42:09 +0200

    DeviceChecker: add documentation

--- ios/PacketTunnel/New packet tunnel/DeviceChecker.swift
+++ ios/PacketTunnel/New packet tunnel/DeviceChecker.swift
@@ -24,6 +24,17 @@ class DeviceChecker: DeviceCheckProtocol {
         self.devicesProxy = devicesProxy
     }
 
+    /**
+     Start device diagnostics to determine the reason why the tunnel is not functional.
+
+     This involves the following steps:
+
+     1. Fetch account and device data.
+     2. Check account validity and whether it has enough time left.
+     3. Verify that current device is registered with backend and that both device and backend point to the same public
+        key.
+     4. Rotate WireGuard key on key mismatch.
+     */
     func start(rotateKeyOnMismatch: Bool) async throws -> DeviceCheck {
         let checkOperation = DeviceCheckOperation(
             dispatchQueue: dispatchQueue,
