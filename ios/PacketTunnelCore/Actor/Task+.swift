494604ac4e3feba102220fd7b2778b3c89a25ecb
Andrej Mihajlov <and@mullvad.net>
2023-09-19 17:21:02 +0200

    Add Task.sleep(millis:) helper

--- ios/PacketTunnelCore/Actor/Task+.swift
+++ ios/PacketTunnelCore/Actor/Task+.swift
@@ -13,4 +13,8 @@ extension Task where Success == Never, Failure == Never {
     static func sleep(seconds: UInt) async throws {
         try await sleep(nanoseconds: UInt64(seconds) * 1_000_000_000)
     }
+
+    static func sleep(millis: UInt) async throws {
+        try await sleep(nanoseconds: UInt64(millis) * 1_000_000)
+    }
 }
