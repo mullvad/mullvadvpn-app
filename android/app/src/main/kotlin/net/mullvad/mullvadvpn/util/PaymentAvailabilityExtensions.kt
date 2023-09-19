3613fb61e65b801e05b36bff36d08d87e8aaf0d8
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-09-19 15:16:30 +0200

    Add payment support to account screen

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/PaymentAvailabilityExtensions.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/PaymentAvailabilityExtensions.kt
@@ -6,8 +6,8 @@ import net.mullvad.mullvadvpn.lib.payment.model.PaymentAvailability
 fun PaymentAvailability.toPaymentState(): PaymentState =
     when (this) {
         PaymentAvailability.Error.ServiceUnavailable,
-        PaymentAvailability.Error.BillingUnavailable -> PaymentState.BillingError
-        is PaymentAvailability.Error.Other -> PaymentState.GenericError
+        PaymentAvailability.Error.BillingUnavailable -> PaymentState.Error.BillingError
+        is PaymentAvailability.Error.Other -> PaymentState.Error.GenericError
         is PaymentAvailability.ProductsAvailable -> PaymentState.PaymentAvailable(products)
         PaymentAvailability.ProductsUnavailable -> PaymentState.NoPayment
         PaymentAvailability.Loading -> PaymentState.Loading
