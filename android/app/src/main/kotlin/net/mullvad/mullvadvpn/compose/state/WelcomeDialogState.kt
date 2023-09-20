c879e71f244331782b8fa393d2725e8e5dd20720
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-09-20 11:29:31 +0200

    Add billing payment to welcome screen and view model

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/WelcomeDialogState.kt
@@ -0,0 +1,11 @@
+package net.mullvad.mullvadvpn.compose.state
+
+sealed interface WelcomeDialogState {
+    data object NoDialog : WelcomeDialogState
+
+    data object VerificationError : WelcomeDialogState
+
+    data object BillingError : WelcomeDialogState
+
+    data object PurchaseComplete : WelcomeDialogState
+}
