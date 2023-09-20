81339cfddb12ba4c8b749c369e27c6a25470a4c5
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-09-20 11:29:31 +0200

    Add billing payment to welcome screen and view model

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/WelcomeUiState.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/WelcomeUiState.kt
@@ -1,9 +1,12 @@
 package net.mullvad.mullvadvpn.compose.state
 
+import net.mullvad.mullvadvpn.lib.payment.model.PurchaseResult
 import net.mullvad.mullvadvpn.model.TunnelState
 
 data class WelcomeUiState(
     val tunnelState: TunnelState = TunnelState.Disconnected,
     val accountNumber: String? = null,
-    val deviceName: String? = null
+    val deviceName: String? = null,
+    val billingPaymentState: PaymentState = PaymentState.Loading,
+    val purchaseResult: PurchaseResult? = null
 )
