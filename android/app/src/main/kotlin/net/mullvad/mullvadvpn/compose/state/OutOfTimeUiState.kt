89bff7f4a3b147925717528209752135028634f7
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-09-20 14:05:15 +0200

    Migrate out of time view to compose

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/OutOfTimeUiState.kt
@@ -0,0 +1,5 @@
+package net.mullvad.mullvadvpn.compose.state
+
+import net.mullvad.mullvadvpn.model.TunnelState
+
+data class OutOfTimeUiState(val tunnelState: TunnelState = TunnelState.Disconnected)
