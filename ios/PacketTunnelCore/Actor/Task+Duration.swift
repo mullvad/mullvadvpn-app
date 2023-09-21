5bdd4e4f2b23ba18dd4b282dc7a1fe86b10a0797
Andrej Mihajlov <and@mullvad.net>
2023-09-21 14:08:49 +0200

    Task: simplify sleep(duration:)

--- ios/PacketTunnelCore/Actor/Task+Duration.swift
+++ ios/PacketTunnelCore/Actor/Task+Duration.swift
@@ -13,29 +13,14 @@ extension Task where Success == Never, Failure == Never {
     /**
      Suspends the current task for at least the given duration.
 
-     Negative duratons are clamped to zero.
+     Negative durations are clamped to zero.
 
      - Parameter duration: duration that determines how long the task should be suspended.
      */
     static func sleep(duration: Duration) async throws {
-        let nanoseconds = UInt64(max(0, duration.nanosecondsClamped))
+        let millis = UInt64(max(0, duration.milliseconds))
+        let nanos = millis.saturatingMultiplication(1_000_000)
 
-        try await Task.sleep(nanoseconds: nanoseconds)
-    }
-}
-
-private extension Duration {
-    /// The duration represented as nanoseconds, clamped to maximum expressible value.
-    var nanosecondsClamped: Int64 {
-        let secondsNanos = components.seconds.multipliedReportingOverflow(by: 1_000_000_000)
-        guard !secondsNanos.overflow else { return .max }
-
-        let attosNanos = components.attoseconds.dividedReportingOverflow(by: 1_000_000_000)
-        guard !attosNanos.overflow else { return .max }
-
-        let combinedNanos = secondsNanos.partialValue.addingReportingOverflow(attosNanos.partialValue)
-        guard !combinedNanos.overflow else { return .max }
-
-        return combinedNanos.partialValue
+        try await Task.sleep(nanoseconds: nanos)
     }
 }
