112bab25d5dfb63676ff0cd631e2747c07f9871a
David Göransson <david.goransson90@gmail.com>
2023-09-22 09:14:44 +0200

    Migrate login screen to compose
    Fixes removed and fixes some of the compose colors, recreates login states

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/AccountRepository.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/AccountRepository.kt
@@ -3,32 +3,36 @@ package net.mullvad.mullvadvpn.repository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.withContext
 import net.mullvad.mullvadvpn.lib.ipc.Event
 import net.mullvad.mullvadvpn.model.AccountCreationResult
 import net.mullvad.mullvadvpn.model.AccountExpiry
 import net.mullvad.mullvadvpn.model.AccountHistory
+import net.mullvad.mullvadvpn.model.LoginResult
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnectionManager
 import net.mullvad.mullvadvpn.ui.serviceconnection.accountDataSource
 import net.mullvad.mullvadvpn.util.flatMapReadyConnectionOrDefault
 
 class AccountRepository(
     private val serviceConnectionManager: ServiceConnectionManager,
-    dispatcher: CoroutineDispatcher = Dispatchers.IO
+    val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
     private val _cachedCreatedAccount = MutableStateFlow<String?>(null)
     val cachedCreatedAccount = _cachedCreatedAccount.asStateFlow()
 
-    val accountCreationEvents: SharedFlow<AccountCreationResult> =
+    private val accountCreationEvents: SharedFlow<AccountCreationResult> =
         serviceConnectionManager.connectionState
             .flatMapReadyConnectionOrDefault(flowOf()) { state ->
                 state.container.accountDataSource.accountCreationResult
@@ -49,7 +53,7 @@ class AccountRepository(
                 AccountExpiry.Missing
             )
 
-    val accountHistoryEvents: StateFlow<AccountHistory> =
+    val accountHistory: StateFlow<AccountHistory> =
         serviceConnectionManager.connectionState
             .flatMapReadyConnectionOrDefault(flowOf()) { state ->
                 state.container.accountDataSource.accountHistory
@@ -61,20 +65,26 @@ class AccountRepository(
                 AccountHistory.Missing
             )
 
-    val loginEvents: SharedFlow<Event.LoginEvent> =
+    private val loginEvents: SharedFlow<Event.LoginEvent> =
         serviceConnectionManager.connectionState
             .flatMapReadyConnectionOrDefault(flowOf()) { state ->
                 state.container.accountDataSource.loginEvents
             }
             .shareIn(CoroutineScope(dispatcher), SharingStarted.WhileSubscribed())
 
-    fun createAccount() {
-        serviceConnectionManager.accountDataSource()?.createAccount()
-    }
+    suspend fun createAccount(): AccountCreationResult =
+        withContext(dispatcher) {
+            val deferred = async { accountCreationEvents.first() }
+            serviceConnectionManager.accountDataSource()?.createAccount()
+            deferred.await()
+        }
 
-    fun login(accountToken: String) {
-        serviceConnectionManager.accountDataSource()?.login(accountToken)
-    }
+    suspend fun login(accountToken: String): LoginResult =
+        withContext(Dispatchers.IO) {
+            val deferred = async { loginEvents.first().result }
+            serviceConnectionManager.accountDataSource()?.login(accountToken)
+            deferred.await()
+        }
 
     fun logout() {
         clearCreatedAccountCache()
---
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/AccountToken.kt
@@ -0,0 +1,3 @@
+package net.mullvad.mullvadvpn.model
+
+@JvmInline value class AccountToken(val value: String)
