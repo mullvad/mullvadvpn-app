a88f4a887fdcfd981a86e48acb0c4b827293f309
Andrej Mihajlov <and@mullvad.net>
2023-09-22 09:45:06 +0200

    Document DispatchSource+

--- ios/PacketTunnelCore/Actor/DispatchSource+Async.swift
+++ ios/PacketTunnelCore/Actor/DispatchSource+Async.swift
@@ -9,6 +9,20 @@
 import Foundation
 
 extension DispatchSource {
+    /**
+     Schedule timer and receive timer events via `AsyncStream`.
+
+     - The stream only buffers the newest timestamp.
+     - The stream is infinite for repeating timers. Cancel `AsyncStream` in order to cancel the underlying timer.
+
+     - Parameters:
+        - startTime: When the timer should fire the first event.
+        - repeating: Repeat interval. The timer is considered being one-shot when `.never` is given and the stream will be finished right after the first
+                     event is fired.
+
+     - Returns: `AsyncStream` emitting `Date` at each timer invocation.
+     */
+
     static func scheduledTimer(on startTime: DispatchWallTime, repeating: DispatchTimeInterval) -> AsyncStream<Date> {
         return AsyncStream(bufferingPolicy: .bufferingNewest(1)) { continuation in
             let timer = DispatchSource.makeTimerSource()
