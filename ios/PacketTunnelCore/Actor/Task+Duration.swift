258fc9860030a479677228fad1539461bea727b3
Andrej Mihajlov <and@mullvad.net>
2023-09-25 17:18:17 +0200

    Actor: add Task.sleepUsingContinuousClock()

--- ios/PacketTunnelCore/Actor/Task+Duration.swift
+++ ios/PacketTunnelCore/Actor/Task+Duration.swift
@@ -9,6 +9,8 @@
 import Foundation
 import MullvadTypes
 
+private typealias TaskCancellationError = CancellationError
+
 extension Task where Success == Never, Failure == Never {
     /**
      Suspends the current task for at least the given duration.
@@ -17,10 +19,38 @@ extension Task where Success == Never, Failure == Never {
 
      - Parameter duration: duration that determines how long the task should be suspended.
      */
+    @available(iOS, introduced: 14.0, obsoleted: 16.0, message: "Replace with Task.sleep(for:tolerance:clock:).")
     static func sleep(duration: Duration) async throws {
         let millis = UInt64(max(0, duration.milliseconds))
         let nanos = millis.saturatingMultiplication(1_000_000)
 
         try await Task.sleep(nanoseconds: nanos)
+    }
+
+    /**
+     Suspends the current task for the given duration.
+
+     Negative durations are clamped to zero.
+
+     - Parameter duration: duration that determines how long the task should be suspended.
+     */
+    @available(iOS, introduced: 14.0, obsoleted: 16.0, message: "Replace with Task.sleep(for:tolerance:clock:).")
+    static func sleepUsingContinuousClock(for duration: Duration) async throws {
+        let timer = DispatchSource.makeTimerSource()
+
+        try await withTaskCancellationHandler {
+            try await withCheckedThrowingContinuation { continuation in
+                timer.setEventHandler {
+                    continuation.resume()
+                }
+                timer.setCancelHandler {
+                    continuation.resume(throwing: TaskCancellationError())
+                }
+                timer.schedule(wallDeadline: .now() + DispatchTimeInterval.milliseconds(duration.milliseconds))
+                timer.activate()
+            }
+        } onCancel: {
+            timer.cancel()
+        }
     }
 }
