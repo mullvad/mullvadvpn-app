926f975e0755727088e2929cbb44f49328d1ac8d
Markus Pettersson <markus.pettersson@mullvad.net>
2023-09-26 17:09:55 +0200

    Deprecate the name `ApiAccessMethod` in favor of `AccessMethodSetting`
    To decrease the confusion between `AccessMethod` & `ApiAccessMethod`.
    `AccessMethodSetting` adds some app-specific settings details on top of
    an `AccessMethod`, which is not too far fetched with the new naming convention.

--- mullvad-daemon/src/management_interface.rs
+++ mullvad-daemon/src/management_interface.rs
@@ -621,7 +621,7 @@ impl ManagementService for ManagementServiceImpl {
 
     async fn add_api_access_method(
         &self,
-        request: Request<types::ApiAccessMethodAdd>,
+        request: Request<types::AccessMethodSettingAdd>,
     ) -> ServiceResult<types::Uuid> {
         log::debug!("add_api_access_method");
         let request = request.into_inner();
@@ -656,7 +656,7 @@ impl ManagementService for ManagementServiceImpl {
 
     async fn update_api_access_method(
         &self,
-        request: Request<types::ApiAccessMethod>,
+        request: Request<types::AccessMethodSetting>,
     ) -> ServiceResult<()> {
         log::debug!("update_api_access_method");
         let access_method_update =
