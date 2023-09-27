57390402b22f5f45377fdcfff3baeebd9cd3780a
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-09-27 15:19:58 +0200

    Add billing payment to out of time screen and view model

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/OutOfTimeUiState.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/OutOfTimeUiState.kt
@@ -1,8 +1,11 @@
 package net.mullvad.mullvadvpn.compose.state
 
+import net.mullvad.mullvadvpn.lib.payment.model.PurchaseResult
 import net.mullvad.mullvadvpn.model.TunnelState
 
 data class OutOfTimeUiState(
     val tunnelState: TunnelState = TunnelState.Disconnected,
-    val deviceName: String
+    val deviceName: String = "",
+    val billingPaymentState: PaymentState = PaymentState.Loading,
+    val purchaseResult: PurchaseResult? = null
 )
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/fragment/OutOfTimeFragment.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/fragment/OutOfTimeFragment.kt
@@ -36,7 +36,11 @@ class OutOfTimeFragment : BaseFragment() {
                         onSettingsClick = ::openSettingsView,
                         onAccountClick = ::openAccountView,
                         openConnectScreen = ::advanceToConnectScreen,
-                        onDisconnectClick = vm::onDisconnectClick
+                        onDisconnectClick = vm::onDisconnectClick,
+                        onPurchaseBillingProductClick = vm::startBillingPayment,
+                        onTryFetchProductsAgain = vm::onTryFetchProductsAgain,
+                        onTryVerificationAgain = vm::verifyPurchases,
+                        onClosePurchaseResultDialog = vm::onClosePurchaseResultDialog,
                     )
                 }
             }
