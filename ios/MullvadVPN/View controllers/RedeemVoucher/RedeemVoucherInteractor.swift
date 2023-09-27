63ae31ae424255941b1fbdae3377aca67c7c397b
Bug Magnet <marco.nikic@mullvad.net>
2023-09-27 17:42:31 +0200

    Add setup for writing tests for TunnelManager

--- ios/MullvadVPN/View controllers/RedeemVoucher/RedeemVoucherInteractor.swift
+++ ios/MullvadVPN/View controllers/RedeemVoucher/RedeemVoucherInteractor.swift
@@ -12,7 +12,7 @@ import MullvadTypes
 
 final class RedeemVoucherInteractor {
     private let tunnelManager: TunnelManager
-    private let accountsProxy: REST.AccountsProxy
+    private let accountsProxy: AccountHandling
     private let shouldVerifyVoucherAsAccount: Bool
 
     private var tasks: [Cancellable] = []
@@ -23,7 +23,7 @@ final class RedeemVoucherInteractor {
 
     init(
         tunnelManager: TunnelManager,
-        accountsProxy: REST.AccountsProxy,
+        accountsProxy: AccountHandling,
         verifyVoucherAsAccount: Bool
     ) {
         self.tunnelManager = tunnelManager
