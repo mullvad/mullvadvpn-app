fd74f0a03058f294b8c92a49e34dd1f33d7be410
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-09-27 15:19:58 +0200

    Add billing payment to out of time screen and view model

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/OutOfTimeUiState.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/OutOfTimeUiState.kt
@@ -2,4 +2,8 @@ package net.mullvad.mullvadvpn.compose.state
 
 import net.mullvad.mullvadvpn.model.TunnelState
 
-data class OutOfTimeUiState(val tunnelState: TunnelState = TunnelState.Disconnected)
+data class OutOfTimeUiState(
+    val tunnelState: TunnelState = TunnelState.Disconnected,
+    val billingPaymentState: PaymentState = PaymentState.Loading,
+    val dialogState: OutOfTimeDialogState = OutOfTimeDialogState.NoDialog
+)
