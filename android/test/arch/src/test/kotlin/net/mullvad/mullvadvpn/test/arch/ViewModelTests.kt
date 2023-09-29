4f6c9b3f7950128885acca70c0bd908306d7dcb8
Albin <albin@mullvad.net>
2023-09-29 17:41:30 +0200

    Add konsist check for vm function return type

--- android/test/arch/src/test/kotlin/net/mullvad/mullvadvpn/test/arch/ViewModelTests.kt
+++ android/test/arch/src/test/kotlin/net/mullvad/mullvadvpn/test/arch/ViewModelTests.kt
@@ -2,10 +2,12 @@ package net.mullvad.mullvadvpn.test.arch
 
 import androidx.lifecycle.ViewModel
 import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.functions
 import com.lemonappdev.konsist.api.ext.list.modifierprovider.withPublicOrDefaultModifier
 import com.lemonappdev.konsist.api.ext.list.properties
 import com.lemonappdev.konsist.api.ext.list.withAllParentsOf
 import com.lemonappdev.konsist.api.verify.assert
+import com.lemonappdev.konsist.api.verify.assertNot
 import org.junit.Test
 
 class ViewModelTests {
@@ -20,6 +22,13 @@ class ViewModelTests {
     fun ensurePublicPropertiesUsePermittedNames() {
         allViewModels().properties().withPublicOrDefaultModifier().assert { property ->
             permittedPublicPropertyNames.contains(property.name)
+        }
+    }
+
+    @Test
+    fun ensurePublicFunctionsHaveNoReturnType() {
+        allViewModels().functions().withPublicOrDefaultModifier().assertNot { function ->
+            function.hasReturnType
         }
     }
 
