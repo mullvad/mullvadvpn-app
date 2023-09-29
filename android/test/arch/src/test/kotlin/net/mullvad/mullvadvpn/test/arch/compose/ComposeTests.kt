d305b982b24f74ed86597a8bb0a231d416761ee8
David Göransson <david.goransson90@gmail.com>
2023-09-29 20:10:05 +0200

    Add test ensure all composables are in compose package

---
+++ android/test/arch/src/test/kotlin/net/mullvad/mullvadvpn/test/arch/compose/ComposeTests.kt
@@ -0,0 +1,19 @@
+package net.mullvad.mullvadvpn.test.arch.compose
+
+import androidx.compose.runtime.Composable
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.withAllAnnotationsOf
+import com.lemonappdev.konsist.api.verify.assert
+import org.junit.Test
+
+class ComposeTests {
+    @Test
+    fun `all app composables are in the compose packages`() {
+        allAppComposeFunctions().assert {
+            it.resideInPackage("net.mullvad.mullvadvpn.compose..")
+        }
+    }
+
+    private fun allAppComposeFunctions() =
+        Konsist.scopeFromProduction("app").functions().withAllAnnotationsOf(Composable::class)
+}
