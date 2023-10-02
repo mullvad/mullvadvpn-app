14040bed47d5b2fba560e0f4e840cd50304415e8
David Göransson <david.goransson90@gmail.com>
2023-10-02 08:25:15 +0200

    Fix data class issues

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/SelectLocationScreen.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/SelectLocationScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/RelayCountry.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/RelayCountry.kt
@@ -5,7 +5,7 @@ import net.mullvad.mullvadvpn.model.GeographicLocationConstraint
 data class RelayCountry(
     override val name: String,
     override val code: String,
-    override var expanded: Boolean,
+    override val expanded: Boolean,
     val cities: List<RelayCity>
 ) : RelayItem {
     override val type = RelayItemType.Country
