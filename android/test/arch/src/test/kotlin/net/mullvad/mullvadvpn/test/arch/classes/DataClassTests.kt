d7382e86cdba58dbfe83e73485f42d68f4f73ee8
David Göransson <david.goransson90@gmail.com>
2023-10-02 08:25:15 +0200

    Fix data class issues

--- android/test/arch/src/test/kotlin/net/mullvad/mullvadvpn/test/arch/classes/DataClassTests.kt
+++ android/test/arch/src/test/kotlin/net/mullvad/mullvadvpn/test/arch/classes/DataClassTests.kt
@@ -3,11 +3,9 @@ package net.mullvad.mullvadvpn.test.arch.classes
 import com.lemonappdev.konsist.api.ext.list.modifierprovider.withDataModifier
 import com.lemonappdev.konsist.api.verify.assert
 import net.mullvad.mullvadvpn.test.arch.extensions.projectScope
-import org.junit.Ignore
 import org.junit.Test
 
 class DataClasses {
-    @Ignore("Code needs clean up")
     @Test
     fun `data classes use only immutable parameters`() {
         projectScope().classes(includeNested = true).withDataModifier().assert {
