a1cd136946fc78d7bda046ea97a8026a14e6aa8f
saber safavi <saber.safavi@codic.se>
2023-10-03 12:54:46 +0200

    Add MVVM parts

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/VoucherDialogViewModel.kt
@@ -0,0 +1,92 @@
+package net.mullvad.mullvadvpn.viewmodel
+
+import android.content.res.Resources
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.compose.state.LoginUiState
+import net.mullvad.mullvadvpn.compose.state.VoucherDialogState
+import net.mullvad.mullvadvpn.compose.state.VoucherDialogUiState
+import net.mullvad.mullvadvpn.model.VoucherSubmissionError
+import net.mullvad.mullvadvpn.model.VoucherSubmissionResult
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnectionContainer
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnectionManager
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnectionState
+import net.mullvad.mullvadvpn.ui.serviceconnection.VoucherRedeemer
+
+class VoucherDialogViewModel(
+    serviceConnectionManager: ServiceConnectionManager,
+    private val resources: Resources
+) : ViewModel() {
+
+    private val vmState = MutableStateFlow<VoucherDialogState>(VoucherDialogState.Default)
+    private val voucherInput = MutableStateFlow(LoginUiState.INITIAL.accountNumberInput)
+
+    private lateinit var voucherRedeemer: VoucherRedeemer
+    private val _shared: SharedFlow<ServiceConnectionContainer> =
+        serviceConnectionManager.connectionState
+            .flatMapLatest { state ->
+                if (state is ServiceConnectionState.ConnectedReady) {
+                    flowOf(state.container)
+                } else {
+                    emptyFlow()
+                }
+            }
+            .shareIn(viewModelScope, SharingStarted.WhileSubscribed())
+
+    var uiState =
+        _shared
+            .flatMapLatest { serviceConnection ->
+                voucherRedeemer = serviceConnection.voucherRedeemer
+                combine(vmState, voucherInput) { state, input ->
+                    VoucherDialogUiState(voucherInput = input, voucherViewModelState = state)
+                }
+            }
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), VoucherDialogUiState.INITIAL)
+
+    fun onRedeem(voucherCode: String) {
+        vmState.update { VoucherDialogState.Verifying }
+        viewModelScope.launch {
+            when (val result = voucherRedeemer.submit(voucherCode)) {
+                is VoucherSubmissionResult.Ok -> handleAddedTime(result.submission.timeAdded)
+                is VoucherSubmissionResult.Error -> setError(result.error)
+                else -> {
+                    vmState.update { VoucherDialogState.Default }
+                }
+            }
+        }
+    }
+
+    fun onVoucherInputChange(voucherString: String) {
+        voucherInput.value = voucherString
+    }
+
+    private fun handleAddedTime(timeAdded: Long) {
+        viewModelScope.launch { vmState.update { VoucherDialogState.Success(timeAdded) } }
+    }
+
+    private fun setError(error: VoucherSubmissionError) {
+        viewModelScope.launch {
+            val message =
+                resources.getString(
+                    when (error) {
+                        VoucherSubmissionError.InvalidVoucher -> R.string.invalid_voucher
+                        VoucherSubmissionError.VoucherAlreadyUsed -> R.string.voucher_already_used
+                        else -> R.string.error_occurred
+                    }
+                )
+            vmState.update { VoucherDialogState.Error(message) }
+        }
+    }
+}
