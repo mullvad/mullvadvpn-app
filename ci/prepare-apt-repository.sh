3665985094fc3da0a3fe9d5b409b473fc33997d7
Linus Färnstrand <linus@mullvad.net>
2023-10-06 13:45:31 +0200

    Cleanup, get gpg key better

--- ci/prepare-apt-repository.sh
+++ ci/prepare-apt-repository.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
+#
+# Usage: prepare-apt-repository <artifact dir> <app version> <repo dir>
+#
+# Will create a deb repository in <repo_dir> and add all .deb files from <artifact dir> matching
+# <app version> to the repository.
 
 set -eu
 
-CODE_SIGNING_KEY_FINGERPRINT=${CODE_SIGNING_KEY_FINGERPRINT:-"A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF"}
-
 artifact_dir=$1
 version=$2
-basedir=$3
-
-# # Slimmed down set for testing
-# SUPPORTED_CODENAMES=("bookworm")
-# SUPPORTED_CODENAMES+=("jammy")
+repo_dir=$3
 
 # Debian codenames we support.
 SUPPORTED_CODENAMES=("sid" "testing" "bookworm" "bullseye")
 # Ubuntu codenames we support (latest two LTS + latest non-LTS)
 SUPPORTED_CODENAMES+=("jammy" "focal" "lunar")
+
+# Use the default GPG key. Must be explicitly configured
+code_signing_key_fingerprint="$(grep -m 1 "default-key" ~/.gnupg/gpg.conf | awk '{print $2}')"
 
 function generate_repository_configuration {
     local codename=$1
@@ -26,7 +28,7 @@ Description: Mullvad package repository for Debian/Ubuntu
 Codename: $codename
 Architectures: amd64 arm64
 Components: main
-SignWith: $CODE_SIGNING_KEY_FINGERPRINT"
+SignWith: $code_signing_key_fingerprint"
 }
 
 function generate_deb_distributions_content {
@@ -42,14 +44,14 @@ function add_deb_to_repo {
     local deb_path=$1
     local codename=$2
     echo "Adding $deb_path to repository $codename"
-    reprepro -V --basedir "$basedir" --component main includedeb "$codename" "$deb_path"
+    reprepro -V --basedir "$repo_dir" --component main includedeb "$codename" "$deb_path"
 }
 
-echo "Generating deb repository into $basedir/"
-mkdir -p "$basedir/conf"
+echo "Generating deb repository into $repo_dir/"
+mkdir -p "$repo_dir/conf"
 
-echo "Writing repository configuration to $basedir/conf/distributions"
-generate_deb_distributions_content > "$basedir/conf/distributions"
+echo "Writing repository configuration to $repo_dir/conf/distributions"
+generate_deb_distributions_content > "$repo_dir/conf/distributions"
 echo ""
 
 for deb_path in "$artifact_dir"/MullvadVPN-"$version"*.deb; do
