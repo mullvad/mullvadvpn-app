87afcb081c9549d036f8b1a5bb1ab502485506ac
Markus Pettersson <markus.pettersson@mullvad.net>
2023-10-09 09:05:50 +0200

    Remove dependency on `duct`
    Remove the dependency on `duct` from `talpid-openvpn`, since we can use
    `tokio` to spawn processes instead.

--- talpid-openvpn/src/process/stoppable_process.rs
+++ talpid-openvpn/src/process/stoppable_process.rs
@@ -1,53 +1,41 @@
-use std::{
-    io, thread,
-    time::{Duration, Instant},
-};
-
-static POLL_INTERVAL_MS: Duration = Duration::from_millis(50);
+use std::{io, process::ExitStatus, time::Duration};
 
 /// A best effort attempt at stopping a subprocess whilst also ensuring that the subprocess is
 /// killed eventually.
+#[async_trait::async_trait]
 pub trait StoppableProcess
 where
     Self: Sized,
 {
     /// Gracefully stops a process.
-    fn stop(&self);
+    async fn stop(&self);
 
     /// Kills a process unconditionally. Implementations should strive to never fail.
-    fn kill(&self) -> io::Result<()>;
+    async fn kill(&self) -> io::Result<()>;
 
     /// Check if process is stopped. This method must not block.
-    fn has_stopped(&self) -> io::Result<bool>;
+    async fn has_stopped(&self) -> io::Result<bool>;
+
+    /// Wait until a process is stopped.
+    async fn wait(&self) -> io::Result<ExitStatus>;
 
     /// Attempts to stop a process gracefully in the given time period, otherwise kills the
     /// process.
-    fn nice_kill(&self, timeout: Duration) -> io::Result<()> {
+    async fn nice_kill(&self, timeout: Duration) -> io::Result<()> {
         log::debug!("Trying to stop child process gracefully");
-        self.stop();
-        if wait_timeout(self, timeout)? {
-            log::debug!("Child process terminated gracefully");
-        } else {
-            log::warn!(
+        self.stop().await;
+
+        // Wait for the process to die for a maximum of `timeout`.
+        let wait_result = tokio::time::timeout(timeout, self.wait()).await;
+        match wait_result {
+            Ok(_) => log::debug!("Child process terminated gracefully"),
+            Err(_) => {
+                log::warn!(
                 "Child process did not terminate gracefully within timeout, forcing termination"
             );
-            self.kill()?;
+                self.kill().await?;
+            }
         }
         Ok(())
     }
-}
-/// Wait for a process to die for a maximum of `timeout`. Returns true if the process died within
-/// the timeout.
-fn wait_timeout<T>(process: &T, timeout: Duration) -> io::Result<bool>
-where
-    T: StoppableProcess + Sized,
-{
-    let timer = Instant::now();
-    while timer.elapsed() < timeout {
-        if process.has_stopped()? {
-            return Ok(true);
-        }
-        thread::sleep(POLL_INTERVAL_MS);
-    }
-    Ok(false)
 }
