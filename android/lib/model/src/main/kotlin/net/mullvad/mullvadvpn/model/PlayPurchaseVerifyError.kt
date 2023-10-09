e10d2858f9274b08012f6ac26ea5e518a5da65d9
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-10-09 16:51:06 +0200

    Add play purchasing models

---
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/PlayPurchaseVerifyError.kt
@@ -0,0 +1,10 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+enum class PlayPurchaseVerifyError : Parcelable {
+    // TODO: Add more errors here.
+    OtherError
+}
