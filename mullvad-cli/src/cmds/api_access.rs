3535a9eff4dd2852f368a5ded81aceb0b645ab86
Markus Pettersson <markus.pettersson@mullvad.net>
2023-10-18 14:51:50 +0200

    Update `Socks5Local` type
    Update `Socks5Local` type to use `talpid_types::net::Endpoint` instead
    of separate `std::net::SocketAddr` + `talpid_types::net::TransportProtocol`.

--- mullvad-cli/src/cmds/api_access.rs
+++ mullvad-cli/src/cmds/api_access.rs
@@ -116,14 +116,16 @@ impl ApiAccess {
                 }
                 CustomAccessMethod::Socks5(socks) => match socks {
                     mullvad_types::access_method::Socks5::Local(local) => {
-                        let ip = cmd.params.ip.unwrap_or(local.peer.ip()).to_string();
-                        let port = cmd.params.port.unwrap_or(local.peer.port());
+                        let ip = cmd.params.ip.unwrap_or(local.peer.address.ip()).to_string();
+                        let port = cmd.params.port.unwrap_or(local.peer.address.port());
+                        let transport_protocol =
+                            cmd.params.transport.unwrap_or(local.peer.protocol);
                         let local_port = cmd.params.local_port.unwrap_or(local.port);
                         mullvad_types::access_method::Socks5Local::from_args(
                             ip,
                             port,
                             local_port,
-                            local.peer_transport_protocol,
+                            transport_protocol,
                         )
                         .map(AccessMethod::from)
                     }
@@ -324,8 +326,8 @@ pub enum AddSocks5Commands {
         ///
         /// By default, the transport protocol is assumed to be `TCP`, but it
         /// can optionally be set to `UDP` as well.
-        #[arg(default_value_t = TransportProtocol::Tcp)]
-        remote_transport_protocol: TransportProtocol,
+        #[arg(long, default_value_t = TransportProtocol::Tcp)]
+        transport: TransportProtocol,
         /// Disable the use of this custom access method. It has to be manually
         /// enabled at a later stage to be used when accessing the Mullvad API.
         #[arg(default_value_t = false, short, long)]
@@ -414,6 +416,9 @@ pub struct EditParams {
     /// The port of the remote proxy server [Socks5 (Local & Remote proxy), Shadowsocks]
     #[arg(long)]
     port: Option<u16>,
+    /// The transport protocol used when communicating with the remote proxy server [Socks5 (Local)]
+    #[arg(long)]
+    transport: Option<TransportProtocol>,
     /// The port that the server on localhost is listening on [Socks5 (Local proxy)]
     #[arg(long)]
     local_port: Option<u16>,
@@ -441,7 +446,7 @@ mod conversions {
                         remote_port,
                         name: _,
                         disabled: _,
-                        remote_transport_protocol,
+                        transport: remote_transport_protocol,
                     } => {
                         println!("Adding SOCKS5-proxy: localhost:{local_port} => {remote_ip}:{remote_port}/{remote_transport_protocol}");
                         daemon_types::Socks5Local::from_args(
@@ -600,10 +605,7 @@ mod pp {
                             }
                             writeln!(f)?;
                             print_option!("Protocol", "Socks5 (local)");
-                            print_option!(
-                                "Peer",
-                                format!("{}/{}", local.peer, local.peer_transport_protocol)
-                            );
+                            print_option!("Peer", local.peer);
                             print_option!("Local port", local.port);
                             Ok(())
                         }
