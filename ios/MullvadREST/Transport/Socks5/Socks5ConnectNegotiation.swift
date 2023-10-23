acc777d02bb0748fe94391b9ec4eea530c382731
Andrej Mihajlov <and@mullvad.net>
2023-10-23 17:12:55 +0200

    Add socks5 forwarding proxy

---
+++ ios/MullvadREST/Transport/Socks5/Socks5ConnectNegotiation.swift
@@ -0,0 +1,109 @@
+//
+//  Socks5ConnectNegotiation.swift
+//  MullvadTransport
+//
+//  Created by pronebird on 20/10/2023.
+//
+
+import Foundation
+import Network
+
+/// The object handling a connection negotiation with socks proxy.
+struct Socks5ConnectNegotiation {
+    /// Connection to the socks proxy.
+    let connection: NWConnection
+
+    /// Endpoint to which the client wants to initiate connection over socks proxy.
+    let endpoint: Socks5Endpoint
+
+    /// Completion handler invoked on success.
+    let onComplete: (Socks5ConnectReply) -> Void
+
+    /// Failure handler invoked on error.
+    let onFailure: (Error) -> Void
+
+    /// Initiate negotiation by sending a connect command to the socks proxy.
+    func perform() {
+        let connectCommand = Socks5ConnectCommand(endpoint: endpoint)
+
+        connection.send(content: connectCommand.rawData, completion: .contentProcessed { [self] error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else {
+                readPartialReply()
+            }
+        })
+    }
+
+    /// Read the preamble of the connect reply.
+    private func readPartialReply() {
+        // The length of the preamble of the CONNECT reply.
+        let replyPreambleLength = 4
+
+        connection.receive(exactLength: replyPreambleLength) { [self] data, _, _, error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else if let data {
+                do {
+                    try handlePartialReply(data: data)
+                } catch {
+                    onFailure(error)
+                }
+            } else {
+                onFailure(Socks5Error.unexpectedEndOfStream)
+            }
+        }
+    }
+
+    /**
+     Parse the bytes that comprise the preamble of a connect reply. Upon success read the endpoint data to produce the complete reply and finish negotiation.
+
+     The following fields are contained within the first 4 bytes: socks version, status code, reserved field, address type.
+     */
+    private func handlePartialReply(data: Data) throws {
+        // Parse partial reply that contains the status code and address type.
+        let (statusCode, addressType) = try parsePartialReply(data: data)
+
+        // Parse server bound endpoint to produce the complete reply.
+        let endpointReader = Socks5EndpointReader(
+            connection: connection,
+            addressType: addressType,
+            onComplete: { [self] endpoint in
+                let reply = Socks5ConnectReply(status: statusCode, serverBoundEndpoint: endpoint)
+                onComplete(reply)
+            },
+            onFailure: onFailure
+        )
+        endpointReader.perform()
+    }
+
+    /// Parse the bytes that comprise the preamble of reply without endpoint data.
+    private func parsePartialReply(data: Data) throws -> (Socks5StatusCode, Socks5AddressType) {
+        var iterator = data.makeIterator()
+
+        // Read the protocol version.
+        guard let version = iterator.next() else { throw Socks5Error.unexpectedEndOfStream }
+
+        // Verify the protocol version.
+        guard version == Socks5Constants.socksVersion else { throw Socks5Error.invalidSocksVersion }
+
+        // Read status code, reserved field and address type from reply.
+        guard let rawStatusCode = iterator.next(),
+              iterator.next() != nil, // skip reserved field
+              let rawAddressType = iterator.next() else {
+            throw Socks5Error.unexpectedEndOfStream
+        }
+
+        // Parse the status code.
+        guard let status = Socks5StatusCode(rawValue: rawStatusCode) else {
+            throw Socks5Error.invalidStatusCode(rawStatusCode)
+        }
+
+        // Parse the address type.
+        guard let addressType = Socks5AddressType(rawValue: rawAddressType) else {
+            throw Socks5Error.invalidAddressType
+        }
+
+        return (status, addressType)
+    }
+}
---
+++ ios/MullvadREST/Transport/Socks5/Socks5ForwardingProxy.swift
@@ -0,0 +1,281 @@
+//
+//  Socks5ForwardingProxy.swift
+//  MullvadTransport
+//
+//  Created by pronebird on 18/10/2023.
+//
+
+import Foundation
+import Network
+
+/**
+ The proxy that can forward data connection from local TCP port to remote TCP server over the socks proxy.
+
+ The forwarding socks proxy acts as a transparent proxy. The HTTP/S clients that don't support proxy configuration can be configured to direct their traffic at the
+ local TCP port opened by the forwarding socks proxy.
+
+ The forwarding proxy then takes care of negotiating with the remote socks proxy and transparently handles all traffic as if the HTTP/S client talks directly to the remote
+ server.
+
+ Refer to RFC1928 for more info on socks5: <https://datatracker.ietf.org/doc/html/rfc1928>
+ */
+public final class Socks5ForwardingProxy {
+    /// Socks proxy endpoint.
+    public let socksProxyEndpoint: NWEndpoint
+
+    /// Remote server that socks proxy should connect to.
+    public let remoteServerEndpoint: Socks5Endpoint
+
+    /// Local TCP port that clients should use to communicate with the remote server.
+    /// This property is set once the proxy is successfully started.
+    public var listenPort: UInt16? {
+        queue.sync {
+            switch state {
+            case let .started(listener, _):
+                return listener.port?.rawValue
+            case .stopped, .starting:
+                return nil
+            }
+        }
+    }
+
+    /**
+     Initializes a socks forwarding proxy accepting connections on local TCP port and establishing connection to the remote endpoint over socks proxy.
+
+     - Parameters:
+       - socksProxyEndpoint: socks proxy endpoint.
+       - remoteServerEndpoint: remote server that socks proxy should connect to.
+     */
+    public init(socksProxyEndpoint: NWEndpoint, remoteServerEndpoint: Socks5Endpoint) {
+        self.socksProxyEndpoint = socksProxyEndpoint
+        self.remoteServerEndpoint = remoteServerEndpoint
+    }
+
+    deinit {
+        stopInner()
+    }
+
+    /**
+     Start forwarding proxy.
+
+     Repeat calls do nothing, but accumulate the completion handler for invocation once the proxy moves to the next state.
+
+     - Parameter completion: completion handler that is called once the TCP listener is ready in the first time or failed before moving to the ready state.
+                             Invoked on main queue.
+     */
+    public func start(completion: @escaping (Error?) -> Void) {
+        queue.async {
+            self.startListener { error in
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+            }
+        }
+    }
+
+    /**
+     Stop forwarding proxy.
+
+     - Parameter completion: completion handler that's called immediately after cancelling the TCP listener. Invoked on main queue.
+     */
+    public func stop(completion: (() -> Void)? = nil) {
+        queue.async {
+            self.stopInner()
+
+            DispatchQueue.main.async {
+                completion?()
+            }
+        }
+    }
+
+    /**
+     Set error handler to receive unrecoverable errors at runtime.
+
+     - Parameter errorHandler: an error handler block. Invoked on main queue.
+     */
+    public func setErrorHandler(_ errorHandler: ((Error) -> Void)?) {
+        queue.async {
+            self.errorHandler = errorHandler
+        }
+    }
+
+    // MARK: - Private
+
+    private enum State {
+        /// Proxy is starting up.
+        case starting(listener: NWListener, completion: (Error?) -> Void)
+
+        /// Proxy is ready.
+        case started(listener: NWListener, openConnections: [Socks5Connection])
+
+        /// Proxy is not running.
+        case stopped
+    }
+
+    private let queue = DispatchQueue(label: "Socks5ForwardingProxy-queue")
+    private var state: State = .stopped
+    private var errorHandler: ((Error) -> Void)?
+
+    /**
+     Start TCP listener.
+
+     - Parameter completion: completion handler that is called once the TCP listener is ready or failed.
+     */
+    private func startListener(completion: @escaping (Error?) -> Void) {
+        switch state {
+        case .started:
+            completion(nil)
+
+        case let .starting(listener, previousCompletion):
+            // Accumulate completion handlers when requested to start multiple times in a row.
+            self.state = .starting(listener: listener, completion: { error in
+                previousCompletion(error)
+                completion(error)
+            })
+
+        case .stopped:
+            do {
+                let tcpListener = try makeTCPListener()
+                state = .starting(listener: tcpListener, completion: completion)
+                tcpListener.start(queue: queue)
+            } catch {
+                completion(Socks5Error.createTcpListener(error))
+            }
+        }
+    }
+
+    /**
+     Create new TCP listener.
+
+     - Throws: an instance of `NWError` if unable to initialize `NWListener`.
+     - Returns: a configured instance of `NWListener`.
+     */
+    private func makeTCPListener() throws -> NWListener {
+        let tcpListener = try NWListener(using: .tcp)
+        tcpListener.stateUpdateHandler = { [weak self] state in
+            self?.onListenerState(state)
+        }
+        tcpListener.newConnectionHandler = { [weak self] connection in
+            self?.onNewConnection(connection)
+        }
+        return tcpListener
+    }
+
+    /**
+     Reset block handlers and cancel an instance of `NWListener`.
+
+     - Parameter tcpListener: an instance of `NWListener`.
+     */
+    private func cancelListener(_ tcpListener: NWListener) {
+        tcpListener.stateUpdateHandler = nil
+        tcpListener.newConnectionHandler = nil
+        tcpListener.cancel()
+    }
+
+    private func stopInner() {
+        switch state {
+        case let .starting(listener, completion):
+            state = .stopped
+            cancelListener(listener)
+            DispatchQueue.main.async {
+                completion(Socks5Error.cancelledDuringStartup)
+            }
+
+        case let .started(listener, openConnections):
+            state = .stopped
+            cancelListener(listener)
+            openConnections.forEach { $0.cancel() }
+
+        case .stopped:
+            break
+        }
+    }
+
+    private func onReady() {
+        switch state {
+        case let .starting(listener, completion):
+            state = .started(listener: listener, openConnections: [])
+
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+
+        case .started, .stopped:
+            break
+        }
+    }
+
+    private func onFailure(_ error: Error) {
+        switch state {
+        case let .starting(_, completion):
+            state = .stopped
+
+            DispatchQueue.main.async {
+                completion(error)
+            }
+
+        case .started:
+            state = .stopped
+            DispatchQueue.main.async {
+                self.errorHandler?(error)
+            }
+
+        case .stopped:
+            break
+        }
+    }
+
+    private func onListenerState(_ listenerState: NWListener.State) {
+        switch listenerState {
+        case .setup, .cancelled:
+            break
+
+        case .ready:
+            onReady()
+
+        case let .failed(error), let .waiting(error):
+            onFailure(error)
+
+        @unknown default:
+            break
+        }
+    }
+
+    private func onNewConnection(_ connection: NWConnection) {
+        switch state {
+        case .starting, .stopped:
+            connection.cancel()
+
+        case .started(let listener, var openConnections):
+            let socks5Connection = Socks5Connection(
+                queue: queue,
+                localConnection: connection,
+                socksProxyEndpoint: socksProxyEndpoint,
+                remoteServerEndpoint: remoteServerEndpoint
+            )
+            socks5Connection.setStateHandler { [weak self] socks5Connection, state in
+                if case let .stopped(error) = state {
+                    self?.onEndConnection(socks5Connection, error: error)
+                }
+            }
+
+            openConnections.append(socks5Connection)
+            state = .started(listener: listener, openConnections: openConnections)
+
+            socks5Connection.start()
+        }
+    }
+
+    private func onEndConnection(_ connection: Socks5Connection, error: Error?) {
+        switch state {
+        case .stopped, .starting:
+            break
+
+        case .started(let listener, var openConnections):
+            guard let index = openConnections.firstIndex(where: { $0 === connection }) else { return }
+
+            openConnections.remove(at: index)
+            state = .started(listener: listener, openConnections: openConnections)
+        }
+    }
+}
