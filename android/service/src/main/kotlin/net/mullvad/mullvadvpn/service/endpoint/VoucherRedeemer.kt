9c7928305faa1d1ca1352e776d4cb893d0087607
David Göransson <david.goransson90@gmail.com>
2023-10-23 15:12:28 +0200

    Add quick fix to be able to refresh AccountCache from VoucherRedeemer

--- android/service/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/VoucherRedeemer.kt
+++ android/service/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/VoucherRedeemer.kt
@@ -6,10 +6,16 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.channels.trySendBlocking
+import net.mullvad.mullvadvpn.lib.common.util.parseAsDateTime
 import net.mullvad.mullvadvpn.lib.ipc.Event
 import net.mullvad.mullvadvpn.lib.ipc.Request
+import net.mullvad.mullvadvpn.model.AccountExpiry
+import net.mullvad.mullvadvpn.model.VoucherSubmissionResult
 
-class VoucherRedeemer(private val endpoint: ServiceEndpoint) {
+class VoucherRedeemer(
+    private val endpoint: ServiceEndpoint,
+    private val accountCache: AccountCache
+) {
     private val daemon
         get() = endpoint.intermittentDaemon
 
@@ -31,6 +37,12 @@ class VoucherRedeemer(private val endpoint: ServiceEndpoint) {
                 for (voucher in channel) {
                     val result = daemon.await().submitVoucher(voucher)
 
+                    // Let AccountCache know about the new expiry
+                    if (result is VoucherSubmissionResult.Ok) {
+                        val accountExpiry =
+                            AccountExpiry.Available(result.submission.newExpiry.parseAsDateTime()!!)
+                        accountCache.onAccountExpiryChange.notify(accountExpiry)
+                    }
                     endpoint.sendEvent(Event.VoucherSubmissionResult(voucher, result))
                 }
             } catch (exception: ClosedReceiveChannelException) {
