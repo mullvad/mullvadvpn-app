9f239f49f7c53919a78ea3fa3246ccbe406fa402
David Göransson <david.goransson90@gmail.com>
2023-10-24 13:41:21 +0200

    Update privacy strings

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/util/UnitConversion.kt
@@ -0,0 +1,11 @@
+package net.mullvad.mullvadvpn.compose.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+
+@Composable
+fun TextUnit.toDp(): Dp {
+    return with(LocalDensity.current) { this@toDp.toDp() }
+}
