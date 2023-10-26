43ce1f78014566057980302c32e52023b64803a7
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2023-10-26 12:10:18 +0200

    Expose ownership and provider for relays

--- android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/Relay.kt
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/Relay.kt
@@ -4,8 +4,13 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Relay(val hostname: String, val active: Boolean, val endpointData: RelayEndpointData) :
-    Parcelable {
+data class Relay(
+    val hostname: String,
+    val active: Boolean,
+    val owned: Boolean,
+    val provider: String,
+    val endpointData: RelayEndpointData
+) : Parcelable {
     val isWireguardRelay
         get() = endpointData is RelayEndpointData.Wireguard
 }
