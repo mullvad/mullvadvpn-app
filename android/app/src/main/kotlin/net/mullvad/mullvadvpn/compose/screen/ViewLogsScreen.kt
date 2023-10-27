7667e9ed590f168584b3ddabcf7c23396f659d29
David Göransson <david.goransson90@gmail.com>
2023-10-27 09:41:04 +0200

    Fix view logs crash

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/ViewLogsScreen.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/ViewLogsScreen.kt
@@ -1,22 +1,20 @@
 package net.mullvad.mullvadvpn.compose.screen
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import net.mullvad.mullvadvpn.R
@@ -31,7 +29,7 @@ import net.mullvad.mullvadvpn.viewmodel.ViewLogsUiState
 @Preview
 @Composable
 private fun PreviewViewLogsScreen() {
-    AppTheme { ViewLogsScreen(uiState = ViewLogsUiState("Lorem ipsum")) }
+    AppTheme { ViewLogsScreen(uiState = ViewLogsUiState(listOf("Lorem ipsum"))) }
 }
 
 @Preview
@@ -71,32 +69,19 @@ fun ViewLogsScreen(
                         Modifier.padding(Dimens.mediumPadding).align(Alignment.CenterHorizontally)
                 )
             } else {
-                SelectionContainer {
-                    val scrollState = rememberScrollState()
-                    Column(
-                        modifier =
-                            Modifier.drawVerticalScrollbar(
-                                scrollState,
-                                color =
-                                    MaterialTheme.colorScheme.primary.copy(alpha = AlphaScrollbar)
+                val scrollState = rememberScrollState()
+                val state = rememberLazyListState()
+                LazyColumn(
+                    state = state,
+                    modifier =
+                        Modifier.drawVerticalScrollbar(
+                                state,
+                                MaterialTheme.colorScheme.primary.copy(alpha = AlphaScrollbar)
                             )
-                    ) {
-                        TextField(
-                            modifier =
-                                Modifier.verticalScroll(scrollState)
-                                    .padding(horizontal = Dimens.smallPadding),
-                            value = uiState.allLines,
-                            textStyle = MaterialTheme.typography.bodySmall,
-                            onValueChange = {},
-                            readOnly = true,
-                            colors =
-                                TextFieldDefaults.colors(
-                                    focusedTextColor = Color.Black,
-                                    unfocusedTextColor = Color.Black,
-                                    disabledTextColor = Color.Black,
-                                    cursorColor = MaterialTheme.colorScheme.background,
-                                )
-                        )
+                            .padding(horizontal = Dimens.smallPadding)
+                ) {
+                    items(uiState.allLines) {
+                        Text(text = it, style = MaterialTheme.typography.bodySmall)
                     }
                 }
             }
