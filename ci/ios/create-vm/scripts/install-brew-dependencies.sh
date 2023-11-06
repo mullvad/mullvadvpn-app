460d0e4ccc92a96f511ace18a6ed14c899e8e143
Emīls <emils@mullvad.net>
2023-11-06 16:06:50 +0100

    Install a recent version of bash and make brew work with it

--- ci/ios/create-vm/scripts/install-brew-dependencies.sh
+++ ci/ios/create-vm/scripts/install-brew-dependencies.sh
@@ -7,6 +7,8 @@ source ~/.bash_profile
 
 if command -v brew &>/dev/null
 then
-    echo >&1 "Installing xcodes"
+    echo "Installing xcodes"
     brew install xcodesorg/made/xcodes
+    echo "Installing xcodes"
+    brew install bash
 fi
