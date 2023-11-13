fea4e4a92a75d802c9a5a4795953815f1d934a00
Pavin Joseph <pavinjoseph@gmail.com>
2023-11-14 02:50:37 +0530

    Improve installation fault tolerance
    Do not fail installation in containers without systemd init.

--- dist-assets/linux/after-install.sh
+++ dist-assets/linux/after-install.sh
@@ -4,5 +4,5 @@ set -eu
 chmod u+s "/usr/bin/mullvad-exclude"
 
 systemctl enable "/usr/lib/systemd/system/mullvad-daemon.service"
-systemctl start mullvad-daemon.service
+systemctl start mullvad-daemon.service || echo "Failed to start mullvad-daemon.service"
 systemctl enable "/usr/lib/systemd/system/mullvad-early-boot-blocking.service"
