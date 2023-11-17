e9386acfe6f7654aeefa02b5e444c107193f962e
saber safavi <saber.safavi@codic.se>
2023-11-17 13:52:55 +0100

    Add view model and ui test

---
+++ android/app/src/androidTest/kotlin/net/mullvad/mullvadvpn/compose/screen/RedeemVoucherDialogTest.kt
@@ -0,0 +1,149 @@
+package net.mullvad.mullvadvpn.compose.screen
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.verify
+import net.mullvad.mullvadvpn.compose.setContentWithTheme
+import net.mullvad.mullvadvpn.compose.state.VoucherDialogState
+import net.mullvad.mullvadvpn.compose.state.VoucherDialogUiState
+import net.mullvad.mullvadvpn.compose.test.VOUCHER_INPUT_TEST_TAG
+import net.mullvad.mullvadvpn.util.VoucherRegexHelper
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class RedeemVoucherDialogTest {
+    @get:Rule val composeTestRule = createComposeRule()
+
+    @Before
+    fun setup() {
+        mockkObject(VoucherRegexHelper)
+    }
+
+    @Test
+    fun testDismissDialog() {
+        // Arrange
+        val mockedClickHandler: (Boolean) -> Unit = mockk(relaxed = true)
+        composeTestRule.setContentWithTheme {
+            RedeemVoucherDialogScreen(
+                uiState = VoucherDialogUiState.INITIAL,
+                onVoucherInputChange = {},
+                onRedeem = {},
+                onDismiss = mockedClickHandler
+            )
+        }
+
+        // Act
+        composeTestRule.onNodeWithText(CANCEL_BUTTON_TEXT).performClick()
+
+        // Assert
+        verify { mockedClickHandler.invoke(false) }
+    }
+
+    @Test
+    fun testDismissDialogAfterSuccessfulRedeem() {
+        // Arrange
+        val mockedClickHandler: (Boolean) -> Unit = mockk(relaxed = true)
+        composeTestRule.setContentWithTheme {
+            RedeemVoucherDialogScreen(
+                uiState =
+                    VoucherDialogUiState(voucherViewModelState = VoucherDialogState.Success(0)),
+                onVoucherInputChange = {},
+                onRedeem = {},
+                onDismiss = mockedClickHandler
+            )
+        }
+
+        // Act
+        composeTestRule.onNodeWithText(GOT_IT_BUTTON_TEXT).performClick()
+
+        // Assert
+        verify { mockedClickHandler.invoke(true) }
+    }
+
+    @Test
+    fun testInsertVoucher() {
+        // Arrange
+        val mockedClickHandler: (String) -> Unit = mockk(relaxed = true)
+        composeTestRule.setContentWithTheme {
+            RedeemVoucherDialogScreen(
+                uiState = VoucherDialogUiState(),
+                onVoucherInputChange = mockedClickHandler,
+                onRedeem = {},
+                onDismiss = {}
+            )
+        }
+
+        // Act
+        composeTestRule.onNodeWithTag(VOUCHER_INPUT_TEST_TAG).performTextInput(DUMMY_VOUCHER)
+
+        // Assert
+        verify { mockedClickHandler.invoke(DUMMY_VOUCHER) }
+    }
+
+    @Test
+    fun testVerifyingState() {
+        // Arrange
+        composeTestRule.setContentWithTheme {
+            RedeemVoucherDialogScreen(
+                uiState =
+                    VoucherDialogUiState(voucherViewModelState = VoucherDialogState.Verifying),
+                onVoucherInputChange = {},
+                onRedeem = {},
+                onDismiss = {}
+            )
+        }
+
+        // Assert
+        composeTestRule.onNodeWithText("Verifying voucher…").assertExists()
+    }
+
+    @Test
+    fun testSuccessState() {
+        // Arrange
+        composeTestRule.setContentWithTheme {
+            RedeemVoucherDialogScreen(
+                uiState =
+                    VoucherDialogUiState(voucherViewModelState = VoucherDialogState.Success(0)),
+                onVoucherInputChange = {},
+                onRedeem = {},
+                onDismiss = {}
+            )
+        }
+
+        // Assert
+        composeTestRule.onNodeWithText("Voucher was successfully redeemed.").assertExists()
+    }
+
+    @Test
+    fun testErrorState() {
+        // Arrange
+        composeTestRule.setContentWithTheme {
+            RedeemVoucherDialogScreen(
+                uiState =
+                    VoucherDialogUiState(
+                        voucherViewModelState = VoucherDialogState.Error(ERROR_MESSAGE)
+                    ),
+                onVoucherInputChange = {},
+                onRedeem = {},
+                onDismiss = {}
+            )
+        }
+
+        // Assert
+        composeTestRule.onNodeWithText(ERROR_MESSAGE).assertExists()
+    }
+
+    companion object {
+        private const val REDEEM_BUTTON_TEXT = "Redeem"
+        private const val CANCEL_BUTTON_TEXT = "Cancel"
+        private const val GOT_IT_BUTTON_TEXT = "Got it!"
+        private const val DUMMY_VOUCHER = "DUMMY____VOUCHER"
+        private const val ERROR_MESSAGE = "error_message"
+    }
+}
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/RedeemVoucherDialog.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/RedeemVoucherDialog.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
@@ -29,6 +30,7 @@ import net.mullvad.mullvadvpn.compose.button.VariantButton
 import net.mullvad.mullvadvpn.compose.component.MullvadCircularProgressIndicatorSmall
 import net.mullvad.mullvadvpn.compose.state.VoucherDialogState
 import net.mullvad.mullvadvpn.compose.state.VoucherDialogUiState
+import net.mullvad.mullvadvpn.compose.test.VOUCHER_INPUT_TEST_TAG
 import net.mullvad.mullvadvpn.compose.textfield.CustomTextField
 import net.mullvad.mullvadvpn.compose.util.MAX_VOUCHER_LENGTH
 import net.mullvad.mullvadvpn.compose.util.vouchersVisualTransformation
@@ -230,7 +232,8 @@ private fun EnterVoucherBody(
         keyboardType = KeyboardType.Password,
         placeholderText = stringResource(id = R.string.voucher_hint),
         visualTransformation = vouchersVisualTransformation(),
-        isDigitsOnlyAllowed = false
+        isDigitsOnlyAllowed = false,
+        modifier = Modifier.testTag(VOUCHER_INPUT_TEST_TAG)
     )
     Spacer(modifier = Modifier.height(Dimens.smallPadding))
     Row(
