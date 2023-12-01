72b402f96ef5fe5476e2120d705424040fecd7ae
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-12-01 13:18:18 +0100

    Align the connecting to mullvad system services text in the center
    Co-Authored-By: Boban Sijuk <49131853+boki91@users.noreply.github.com>

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/LoadingScreen.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/LoadingScreen.kt
@@ -17,12 +17,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.compose.component.ScaffoldWithTopBar
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
+import net.mullvad.mullvadvpn.lib.theme.Dimens
 import net.mullvad.mullvadvpn.lib.theme.color.AlphaDescription
 
 @Preview
@@ -73,7 +75,9 @@ fun LoadingScreen(onSettingsCogClicked: () -> Unit = {}) {
                             MaterialTheme.colorScheme.onPrimary
                                 .copy(alpha = AlphaDescription)
                                 .compositeOver(backgroundColor),
-                        modifier = Modifier.padding(top = 12.dp)
+                        modifier =
+                            Modifier.padding(top = 12.dp).padding(horizontal = Dimens.sideMargin),
+                        textAlign = TextAlign.Center
                     )
                 }
             }
