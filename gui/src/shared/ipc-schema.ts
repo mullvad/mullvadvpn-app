6d12fb18722d156deb632b3361b0b7cce482d281
Oskar Nyberg <oskar@mullvad.net>
2023-12-05 19:01:26 +0100

    Add daita toggle

--- gui/src/shared/ipc-schema.ts
+++ gui/src/shared/ipc-schema.ts
@@ -14,6 +14,7 @@ import {
   IAccountData,
   IAppVersionInfo,
   ICustomList,
+  IDaitaSettings,
   IDevice,
   IDeviceRemoval,
   IDnsOptions,
@@ -192,6 +193,7 @@ export const ipcSchema = {
     testApiAccessMethodById: invoke<string, boolean>(),
     testCustomApiAccessMethod: invoke<CustomProxy, boolean>(),
     clearAllRelayOverrides: invoke<void, void>(),
+    setDaitaSettings: invoke<IDaitaSettings, void>(),
   },
   guiSettings: {
     '': notifyRenderer<IGuiSettingsState>(),
