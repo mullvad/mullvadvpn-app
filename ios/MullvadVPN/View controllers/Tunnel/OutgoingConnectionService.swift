3115ad04aff2c1e568fcac7051475967b6b34e65
Jon Petersson <jon.petersson@kvadrat.se>
2023-12-08 12:26:20 +0100

    Querying the outgoing connection IP should not affect the in IP

--- ios/MullvadVPN/View controllers/Tunnel/OutgoingConnectionService.swift
+++ ios/MullvadVPN/View controllers/Tunnel/OutgoingConnectionService.swift
@@ -23,7 +23,7 @@ final class OutgoingConnectionService: OutgoingConnectionServiceHandling {
 
     func getOutgoingConnectionInfo() async throws -> OutgoingConnectionInfo {
         let ipv4ConnectionInfo = try await outgoingConnectionProxy.getIPV4(retryStrategy: .default)
-        let ipv6ConnectionInfo = try await outgoingConnectionProxy.getIPV6(retryStrategy: .noRetry)
+        let ipv6ConnectionInfo = try? await outgoingConnectionProxy.getIPV6(retryStrategy: .noRetry)
         return OutgoingConnectionInfo(ipv4: ipv4ConnectionInfo, ipv6: ipv6ConnectionInfo)
     }
 }
@@ -33,12 +33,17 @@ struct OutgoingConnectionInfo {
     let ipv4: IPV4ConnectionData
 
     /// IPv6 exit connection.
-    let ipv6: IPV6ConnectionData
+    let ipv6: IPV6ConnectionData?
 
     var outAddress: String? {
-        let v4 = ipv4.exitIP ? "\(ipv4.ip)" : nil
-        let v6 = ipv6.exitIP ? "\(ipv6.ip)" : nil
-        let outAddress = [v4, v6].compactMap { $0 }.joined(separator: "\n")
+        let ipv4String = ipv4.exitIP ? "\(ipv4.ip)" : nil
+
+        var ipv6String: String?
+        if let ipv6 = ipv6, ipv6.exitIP {
+            ipv6String = "\(ipv6.ip)"
+        }
+
+        let outAddress = [ipv4String, ipv6String].compactMap { $0 }.joined(separator: "\n")
         return outAddress.isEmpty ? nil : outAddress
     }
 }
