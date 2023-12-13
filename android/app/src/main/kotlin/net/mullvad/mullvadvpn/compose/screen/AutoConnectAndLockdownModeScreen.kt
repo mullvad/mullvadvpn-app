5677ff9adff4e2d33452bb207680c1cdd45ffab3
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-12-13 13:52:31 +0100

    Create auto connect and lockdown mode screen
    Co-Authored-By: Boban Sijuk <49131853+Boki91@users.noreply.github.com>

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/AutoConnectAndLockdownModeScreen.kt
@@ -0,0 +1,37 @@
+package net.mullvad.mullvadvpn.compose.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.compose.component.AutoConnectCarousel
+import net.mullvad.mullvadvpn.compose.component.NavigateBackIconButton
+import net.mullvad.mullvadvpn.compose.component.ScaffoldWithLargeTopBarAndButton
+import net.mullvad.mullvadvpn.compose.transitions.SlideInFromRightTransition
+import net.mullvad.mullvadvpn.lib.common.util.openVpnSettings
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
+
+@Preview
+@Composable
+private fun PreviewAutoConnectAndLockdownModeScreen() {
+    AppTheme { AutoConnectAndLockdownModeScreen(EmptyDestinationsNavigator) }
+}
+
+@Destination(style = SlideInFromRightTransition::class)
+@Composable
+fun AutoConnectAndLockdownModeScreen(
+    navigator: DestinationsNavigator,
+) {
+    val context = LocalContext.current
+    ScaffoldWithLargeTopBarAndButton(
+        appBarTitle = stringResource(id = R.string.auto_connect_and_lockdown_mode_two_lines),
+        navigationIcon = { NavigateBackIconButton(navigator::navigateUp) },
+        buttonTitle = stringResource(id = R.string.go_to_vpn_settings),
+        onButtonClick = { context.openVpnSettings() },
+        content = { modifier -> AutoConnectCarousel() }
+    )
+}
