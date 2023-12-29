f7dfa9fd404a3565755ad723b99acd563dc16cd7
saber safavi <saber.safavi@codic.se>
2023-12-29 11:03:00 +0100

    Add automate logout ui tests using mockApi

---
+++ android/test/mockapi/src/main/kotlin/net/mullvad/mullvadvpn/test/mockapi/LogoutMockApiTest.kt
@@ -0,0 +1,40 @@
+package net.mullvad.mullvadvpn.test.mockapi
+
+import androidx.test.runner.AndroidJUnit4
+import androidx.test.uiautomator.By
+import junit.framework.Assert
+import net.mullvad.mullvadvpn.test.common.extension.clickAgreeOnPrivacyDisclaimer
+import net.mullvad.mullvadvpn.test.common.extension.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove
+import net.mullvad.mullvadvpn.test.common.extension.dismissChangelogDialogIfShown
+import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
+import net.mullvad.mullvadvpn.test.mockapi.util.currentUtcTimeWithOffsetZero
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LogoutMockApiTest : MockApiTest() {
+
+    @Test
+    fun testLoginWithValidCredentialsToUnexpiredAccountAndLogout() {
+        // Arrange
+        val validAccountToken = "1234123412341234"
+        apiDispatcher.apply {
+            expectedAccountToken = validAccountToken
+            accountExpiry = currentUtcTimeWithOffsetZero().plusMonths(1)
+        }
+
+        // Act
+        app.launch(endpoint)
+        device.clickAgreeOnPrivacyDisclaimer()
+        device.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove()
+        device.dismissChangelogDialogIfShown()
+        app.waitForLoginPrompt()
+        app.attemptLogin(validAccountToken)
+        device.findObjectWithTimeout(By.text("UNSECURED CONNECTION"))
+        app.clickAccountCog()
+        app.clickActionButtonByText("Log out")
+
+        // Assert
+        Assert.assertNotNull(device.findObjectWithTimeout(By.text("Login")))
+    }
+}
