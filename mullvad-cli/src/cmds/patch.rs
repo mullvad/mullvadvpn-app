efa285e3876fb919341b67aceca1083f781ec2e2
David Lönnhager <david.l@mullvad.net>
2024-01-10 20:20:42 +0100

    Simplify CLI patch module slightly

--- mullvad-cli/src/cmds/patch.rs
+++ mullvad-cli/src/cmds/patch.rs
@@ -2,15 +2,19 @@ use anyhow::{Context, Result};
 use mullvad_management_interface::MullvadProxyClient;
 use std::{
     fs::File,
-    io::{stdin, BufReader, Read},
+    io::{read_to_string, stdin, BufReader},
 };
 
-/// If source is specified, read from the provided file and send it as a settings patch to the
-/// daemon. Otherwise, read the patch from standard input.
+/// If source is specified (not "-"), read from the provided file and send it as a settings patch to
+/// the daemon. Otherwise, read the patch from standard input.
 pub async fn import(source: String) -> Result<()> {
-    let json_blob = tokio::task::spawn_blocking(|| get_blob(source))
-        .await
-        .unwrap()?;
+    let json_blob = tokio::task::spawn_blocking(move || match source.as_str() {
+        "-" => read_to_string(BufReader::new(stdin())).context("Failed to read from stdin"),
+        _ => read_to_string(File::open(&source)?)
+            .context(format!("Failed to read from path: {source}")),
+    })
+    .await
+    .unwrap()?;
 
     let mut rpc = MullvadProxyClient::new().await?;
     rpc.apply_json_settings(json_blob)
@@ -22,8 +26,8 @@ pub async fn import(source: String) -> Result<()> {
     Ok(())
 }
 
-/// If source is specified, write a patch to the file. Otherwise, write the patch to standard
-/// output.
+/// If source is specified (not "-"), write a patch to the file. Otherwise, write the patch to
+/// standard output.
 pub async fn export(dest: String) -> Result<()> {
     let mut rpc = MullvadProxyClient::new().await?;
     let blob = rpc
@@ -40,21 +44,4 @@ pub async fn export(dest: String) -> Result<()> {
             .await
             .context(format!("Failed to write to path {dest}")),
     }
-}
-
-fn get_blob(source: String) -> Result<String> {
-    match source.as_str() {
-        "-" => {
-            read_settings_from_reader(BufReader::new(stdin())).context("Failed to read from stdin")
-        }
-        _ => read_settings_from_reader(File::open(&source)?)
-            .context(format!("Failed to read from path: {source}")),
-    }
-}
-
-/// Read until EOF or until newline when the last pair of braces has been closed
-fn read_settings_from_reader(mut reader: impl Read) -> Result<String> {
-    let mut s = String::new();
-    reader.read_to_string(&mut s)?;
-    Ok(s)
 }
