f8c6ed3d0d7b8f6694c7880b64f4fdfa147452b1
Bug Magnet <marco.nikic@mullvad.net>
2024-01-10 16:56:40 +0100

    Add a staging configuration that automatically targets the staging environment

--- ios/MullvadREST/ApiHandlers/RESTDefaults.swift
+++ ios/MullvadREST/ApiHandlers/RESTDefaults.swift
@@ -9,12 +9,13 @@
 import Foundation
 import MullvadTypes
 
+// swiftlint:disable force_cast
 extension REST {
     /// Default API hostname.
-    public static let defaultAPIHostname = "api.mullvad.net"
+    public static let defaultAPIHostname = Bundle.main.infoDictionary!["ApiHostName"] as! String
 
     /// Default API endpoint.
-    public static let defaultAPIEndpoint = AnyIPEndpoint(string: "45.83.223.196:443")!
+    public static let defaultAPIEndpoint = AnyIPEndpoint(string: Bundle.main.infoDictionary!["ApiEndpoint"] as! String)!
 
     /// Disables API IP address cache when in staging environment and sticks to using default API endpoint instead.
     public static let isStagingEnvironment = false
@@ -22,3 +23,5 @@ extension REST {
     /// Default network timeout for API requests.
     public static let defaultAPINetworkTimeout: Duration = .seconds(10)
 }
+
+// swiftlint:enable force_cast
