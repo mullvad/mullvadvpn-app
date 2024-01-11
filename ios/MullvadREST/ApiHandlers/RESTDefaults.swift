d6cf9d47e7d17c80570bfaba85e454cf848e4f5d
Bug Magnet <marco.nikic@mullvad.net>
2024-01-11 14:02:49 +0100

    Move the bundle information to a place accessible from both executables and the test environment

--- ios/MullvadREST/ApiHandlers/RESTDefaults.swift
+++ ios/MullvadREST/ApiHandlers/RESTDefaults.swift
@@ -11,11 +11,15 @@ import MullvadTypes
 
 // swiftlint:disable force_cast
 extension REST {
+    /// The API hostname and endpoint are defined in the Info.plist of the MullvadREST framework bundle
+    /// This is due to not being able to target `Bundle.main` from a Unit Test environment as it gets its own bundle that would not contain the above variables.
+    private static let infoDictionary = Bundle(for: AddressCache.self).infoDictionary!
+
     /// Default API hostname.
-    public static let defaultAPIHostname = Bundle.main.infoDictionary!["ApiHostName"] as! String
+    public static let defaultAPIHostname = infoDictionary["ApiHostName"] as! String
 
     /// Default API endpoint.
-    public static let defaultAPIEndpoint = AnyIPEndpoint(string: Bundle.main.infoDictionary!["ApiEndpoint"] as! String)!
+    public static let defaultAPIEndpoint = AnyIPEndpoint(string: infoDictionary["ApiEndpoint"] as! String)!
 
     /// Disables API IP address cache when in staging environment and sticks to using default API endpoint instead.
     public static let isStagingEnvironment = false
