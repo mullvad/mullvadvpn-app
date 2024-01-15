02e4bce813a6ccab60a70947c105e1e8c139df87
Jon Petersson <jon.petersson@kvadrat.se>
2024-01-15 16:46:09 +0100

    Allow users to import settings by pasting JSON blobs

---
+++ ios/MullvadVPNTests/IPOverrideRepositoryTests.swift
@@ -0,0 +1,84 @@
+//
+//  IPOverrideRepositoryTests.swift
+//  MullvadVPNTests
+//
+//  Created by Jon Petersson on 2024-01-17.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+@testable import MullvadSettings
+import Network
+import XCTest
+
+final class IPOverrideRepositoryTests: XCTestCase {
+    static let store = InMemorySettingsStore<SettingNotFound>()
+    let repository = IPOverrideRepository()
+
+    override class func setUp() {
+        SettingsManager.unitTestStore = store
+    }
+
+    override class func tearDown() {
+        SettingsManager.unitTestStore = nil
+    }
+
+    override func tearDownWithError() throws {
+        repository.deleteAll()
+    }
+
+    func testAddOverride() throws {
+        let override = try IPOverride(hostname: "Host 1", ipv4Address: .any, ipv6Address: nil)
+        repository.add([override])
+
+        let storedOverrides = repository.fetchAll()
+        XCTAssertTrue(storedOverrides.count == 1)
+    }
+
+    func testAppendOverrideWithDifferentHostname() throws {
+        let override1 = try IPOverride(hostname: "Host 1", ipv4Address: .any, ipv6Address: nil)
+        let override2 = try IPOverride(hostname: "Host 2", ipv4Address: .any, ipv6Address: nil)
+        repository.add([override1, override2])
+
+        let storedOverrides = repository.fetchAll()
+        XCTAssertTrue(storedOverrides.count == 2)
+    }
+
+    func testOverwriteOverrideWithSameHostnameButDifferentAddresses() throws {
+        let override1 = try IPOverride(hostname: "Host 1", ipv4Address: .any, ipv6Address: nil)
+        let override2 = try IPOverride(hostname: "Host 1", ipv4Address: .allHostsGroup, ipv6Address: .broadcast)
+        repository.add([override1, override2])
+
+        let storedOverrides = repository.fetchAll()
+        XCTAssertTrue(storedOverrides.count == 1)
+        XCTAssertTrue(storedOverrides.first?.ipv4Address == .allHostsGroup)
+        XCTAssertTrue(storedOverrides.first?.ipv6Address == .broadcast)
+    }
+
+    func testFailedToOverwriteOverrideWithNilAddress() throws {
+        let override1 = try IPOverride(hostname: "Host 1", ipv4Address: .any, ipv6Address: .broadcast)
+        let override2 = try IPOverride(hostname: "Host 1", ipv4Address: .any, ipv6Address: nil)
+        repository.add([override1, override2])
+
+        let storedOverrides = repository.fetchAll()
+        XCTAssertTrue(storedOverrides.count == 1)
+        XCTAssertTrue(storedOverrides.first?.ipv6Address == .broadcast)
+    }
+
+    func testFetchOverrideByHostname() throws {
+        let hostname = "Host 1"
+        let override = try IPOverride(hostname: hostname, ipv4Address: .any, ipv6Address: nil)
+        repository.add([override])
+
+        let storedOverride = repository.fetchByHostname(hostname)
+        XCTAssertTrue(storedOverride?.hostname == hostname)
+    }
+
+    func testDeleteAllOverrides() throws {
+        let override = try IPOverride(hostname: "Host 1", ipv4Address: .any, ipv6Address: nil)
+        repository.add([override])
+        repository.deleteAll()
+
+        let storedOverrides = repository.fetchAll()
+        XCTAssertTrue(storedOverrides.isEmpty)
+    }
+}
---
+++ ios/MullvadVPNTests/IPOverrideTests.swift
@@ -0,0 +1,90 @@
+//
+//  IPOverrideTests.swift
+//  MullvadVPNTests
+//
+//  Created by Jon Petersson on 2024-01-30.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+@testable import MullvadSettings
+import XCTest
+
+final class IPOverrideTests: XCTestCase {
+    let repository = IPOverrideRepository()
+
+    func testCanParseOverrides() throws {
+        XCTAssertNoThrow(try parseData(from: overrides))
+    }
+
+    func testFailedToParseOverridesWithUnsupportedKeys() throws {
+        XCTAssertThrowsError(try parseData(from: overridesWithUnsupportedKeys))
+    }
+
+    func testFailedToParseOverridesWithMalformedValues() throws {
+        XCTAssertThrowsError(try parseData(from: overridesWithMalformedValues))
+    }
+
+    func testCreateOverrideWithOneAddress() throws {
+        XCTAssertNoThrow(try IPOverride(hostname: "Host 1", ipv4Address: .any, ipv6Address: nil))
+        XCTAssertNoThrow(try IPOverride(hostname: "Host 1", ipv4Address: nil, ipv6Address: .any))
+    }
+
+    func testFailedToCreateOverrideWithNoAddresses() throws {
+        XCTAssertThrowsError(try IPOverride(hostname: "Host 1", ipv4Address: nil, ipv6Address: nil))
+    }
+}
+
+extension IPOverrideTests {
+    private func parseData(from overrideString: String) throws -> [IPOverride] {
+        let data = overrideString.data(using: .utf8)!
+        let overrides = try repository.parse(data: data)
+
+        return overrides
+    }
+}
+
+extension IPOverrideTests {
+    private var overrides: String {
+        return """
+        {
+            "relay_overrides": [
+                {
+                    "hostname": "Host 1",
+                    "ipv4_addr_in": "127.0.0.1",
+                    "ipv6_addr_in": "::"
+                },
+                {
+                    "hostname": "Host 2",
+                    "ipv4_addr_in": "127.0.0.2",
+                    "ipv6_addr_in": "::1"
+                }
+            ]
+        }
+        """
+    }
+
+    private var overridesWithUnsupportedKeys: String {
+        return """
+        "{
+            "relay_overrides": [{
+                "name": "Host 1",
+                "hostname": "Host 1",
+                "ipv4_addr_in": "127.0.0.1",
+                "ipv6_addr_in": "::"
+            }]
+        }
+        """
+    }
+
+    private var overridesWithMalformedValues: String {
+        return """
+        "{
+            "relay_overrides": [{
+                "hostname": "Host 1",
+                "ipv4_addr_in": "127.0.0",
+                "ipv6_addr_in": "::"
+            }]
+        }
+        """
+    }
+}
