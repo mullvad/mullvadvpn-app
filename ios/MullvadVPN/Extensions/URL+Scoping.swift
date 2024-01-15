79012fb979f6ffb8e8670d31f2d691fddc7dae60
Jon Petersson <jon.petersson@kvadrat.se>
2024-01-15 16:46:09 +0100

    Allow users to import settings by pasting JSON blobs

---
+++ ios/MullvadVPN/Extensions/URL+Scoping.swift
@@ -0,0 +1,18 @@
+//
+//  URL+Scoping.swift
+//  MullvadVPN
+//
+//  Created by Jon Petersson on 2024-02-02.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    func securelyScoped(_ completionHandler: (Self) -> Void) {
+        if startAccessingSecurityScopedResource() {
+            completionHandler(self)
+            stopAccessingSecurityScopedResource()
+        }
+    }
+}
--- ios/MullvadVPN/View controllers/AccountDeletion/AccountDeletionContentView.swift
+++ ios/MullvadVPN/View controllers/AccountDeletion/AccountDeletionContentView.swift
@@ -71,7 +71,7 @@ class AccountDeletionContentView: UIView {
             This logs out all devices using this account and all \
             VPN access will be denied even if there is time left on the account. \
             Enter the last 4 digits of the account number and hit "Delete account" \
-            if you really want to delete the account :
+            if you really want to delete the account:
             """,
             comment: ""
         )
