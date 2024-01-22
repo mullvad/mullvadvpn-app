ac131e3b68998baf24889defe4532d9fd8fcf294
Niklas Berglund <niklas.berglund@gmail.com>
2024-01-22 17:57:48 +0100

    Add setup and teardown for iOS UI tests

---
+++ ios/MullvadVPNUITests/Test base classes/BaseUITestCase.swift
@@ -0,0 +1,138 @@
+//
+//  BaseTestCase.swift
+//  MullvadVPNUITests
+//
+//  Created by Niklas Berglund on 2024-01-12.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class BaseUITestCase: XCTestCase {
+    let app = XCUIApplication()
+    static let defaultTimeout = 5.0
+
+    // swiftlint:disable force_cast
+    let displayName = Bundle(for: BaseUITestCase.self)
+        .infoDictionary?["MullvadDisplayName"] as! String
+    let noTimeAccountNumber = Bundle(for: BaseUITestCase.self)
+        .infoDictionary?["MullvadNoTimeAccountNumber"] as! String
+    let hasTimeAccountNumber = Bundle(for: BaseUITestCase.self)
+        .infoDictionary?["MullvadHasTimeAccountNumber"] as! String
+    let fiveWireGuardKeysAccountNumber = Bundle(for: BaseUITestCase.self)
+        .infoDictionary?["MullvadFiveWireGuardKeysAccountNumber"] as! String
+    let iOSDevicePinCode = Bundle(for: BaseUITestCase.self)
+        .infoDictionary?["MullvadIOSDevicePinCode"] as! String
+    let adServingDomain = Bundle(for: BaseUITestCase.self)
+        .infoDictionary?["MullvadAdServingDomain"] as! String
+    // swiftlint:enable force_cast
+
+    /// Handle iOS add VPN configuration permission alert - allow and enter device PIN code
+    func allowAddVPNConfigurations() {
+        let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
+
+        let alertAllowButton = springboard.buttons.element(boundBy: 0)
+        if alertAllowButton.waitForExistence(timeout: Self.defaultTimeout) {
+            alertAllowButton.tap()
+        }
+
+        _ = springboard.buttons["1"].waitForExistence(timeout: Self.defaultTimeout)
+        springboard.typeText(iOSDevicePinCode)
+    }
+
+    // MARK: - Setup & teardown
+
+    /// Suite level teardown ran after test have executed
+    override class func tearDown() {
+        uninstallApp()
+    }
+
+    /// Test level setup
+    override func setUp() {
+        app.launch()
+    }
+
+    /// Test level teardown
+    override func tearDown() {
+        app.terminate()
+    }
+
+    /// Check if currently logged on to an account. Note that it is assumed that we are logged in if login view isn't currently shown.
+    func isLoggedIn() -> Bool {
+        return !app
+            .otherElements[AccessibilityIdentifier.loginView.rawValue]
+            .waitForExistence(timeout: 1.0)
+    }
+
+    func agreeToTermsOfServiceIfShown() {
+        let termsOfServiceIsShown = app.otherElements[
+            AccessibilityIdentifier
+                .termsOfServiceView.rawValue
+        ]
+        .waitForExistence(timeout: 1)
+
+        if termsOfServiceIsShown {
+            TermsOfServicePage(app)
+                .tapAgreeButton()
+
+            Alert(app) // Changes alert
+                .tapOkay()
+
+            LoginPage(app)
+        }
+    }
+
+    /// Login with specified account number. It is a prerequisite that the login page is currently shown.
+    func login(accountNumber: String) {
+        LoginPage(app)
+            .tapAccountNumberTextField()
+            .enterText(accountNumber)
+            .tapAccountNumberSubmitButton()
+            .verifyDeviceLabelShown()
+    }
+
+    func logoutIfLoggedIn() {
+        if isLoggedIn() {
+            HeaderBar(app)
+                .tapAccountButton()
+
+            AccountPage(app)
+                .tapLogOutButton()
+
+            LoginPage(app)
+        }
+    }
+
+    static func uninstallApp() {
+        let appName = "Mullvad VPN"
+
+        let timeout = TimeInterval(5)
+        let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
+        let spotlight = XCUIApplication(bundleIdentifier: "com.apple.Spotlight")
+
+        springboard.swipeDown()
+        spotlight.textFields["SpotlightSearchField"].typeText(appName)
+
+        let appIcon = spotlight.icons[appName].firstMatch
+        if appIcon.waitForExistence(timeout: timeout) {
+            appIcon.press(forDuration: 2)
+        } else {
+            XCTFail("Failed to find app icon named \(appName)")
+        }
+
+        let deleteAppButton = spotlight.buttons["Delete App"]
+        if deleteAppButton.waitForExistence(timeout: timeout) {
+            deleteAppButton.tap()
+        } else {
+            XCTFail("Failed to find 'Delete App'")
+        }
+
+        let finalDeleteButton = springboard.alerts.buttons["Delete"]
+        if finalDeleteButton.waitForExistence(timeout: timeout) {
+            finalDeleteButton.tap()
+        } else {
+            XCTFail("Failed to find 'Delete'")
+        }
+    }
+}
---
+++ ios/MullvadVPNUITests/Test base classes/LoggedInWithoutTimeUITestCase.swift
@@ -0,0 +1,25 @@
+//
+//  LoggedInWithoutTimeUITestCase.swift
+//  MullvadVPNUITests
+//
+//  Created by Niklas Berglund on 2024-01-23.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+
+/// Base class for tests that should start from a state of being logged on to an account without time left
+class LoggedInWithoutTimeUITestCase: BaseUITestCase {
+    override func setUp() {
+        super.setUp()
+
+        agreeToTermsOfServiceIfShown()
+        logoutIfLoggedIn()
+
+        login(accountNumber: noTimeAccountNumber)
+
+        // Relaunch app so that tests start from a deterministic state
+        app.terminate()
+        app.launch()
+    }
+}
