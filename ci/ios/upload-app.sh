e31776d803ae64a2314beb236cd02360ca6a5a6d
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-01-25 16:21:57 +0100

    SC2164

--- ci/ios/upload-app.sh
+++ ci/ios/upload-app.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+set -eu
 # Upload script to run in `ios-upload` VM to upload a newly built IPA to TestFlight
 
 VM_UPLOAD_IPA_PATH="/Volumes/My Shared Files/build-output/MullvadVPN.ipa"
 API_KEY_PATH="$HOME/ci/app-store-connect-key.json"
 cd ci/
+# shellcheck source=/dev/null
 source ~/.bash_profile
 bundle exec fastlane pilot upload --api-key-path "${API_KEY_PATH}" --ipa "${VM_UPLOAD_IPA_PATH}"
