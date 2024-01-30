93abed3393f0f03121451a90ff946a8cfb88acfb
Oskar Nyberg <oskar@mullvad.net>
2024-01-30 21:35:04 +0100

    Update disconnected from daemon system notification

---
+++ gui/src/shared/notifications/daemon-disconnected.ts
@@ -0,0 +1,22 @@
+import { messages } from '../../shared/gettext';
+import {
+  SystemNotification,
+  SystemNotificationCategory,
+  SystemNotificationProvider,
+  SystemNotificationSeverityType,
+} from './notification';
+
+export class DaemonDisconnectedNotificationProvider implements SystemNotificationProvider {
+  public mayDisplay = () => true;
+
+  public getSystemNotification(): SystemNotification {
+    return {
+      message: messages.pgettext(
+          'notifications',
+          'Connection might be unsecured. App lost contact with system service, please troubleshoot.',
+        ),
+      severity: SystemNotificationSeverityType.high,
+      category: SystemNotificationCategory.tunnelState,
+    };
+  }
+}
