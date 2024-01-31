93d4ed670e41de24dc4d9911d2744f7abc440a11
Emīls <emils@mullvad.net>
2024-01-31 10:23:09 +0100

    Add MullvadApi to MullvadVPNUITetsts

---
+++ ios/MullvadVPNUITests/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  MullvadVPN
+//
+//  Created by Emils on 31/01/2024.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+#include "mullvad-api.h"
+
+#endif /* BridgingHeader_h */
