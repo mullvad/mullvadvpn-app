ddc62bc7d488d42febe3ac49c41d212516d8a539
David Göransson <david.goransson90@gmail.com>
2024-02-02 17:49:05 +0100

    Simple animation

--- android/app/src/main/kotlin/net/mullvad/lib/map/MullvadMap.kt
+++ android/app/src/main/kotlin/net/mullvad/lib/map/MullvadMap.kt
@@ -2,6 +2,7 @@ package net.mullvad.lib.map
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -16,11 +17,12 @@ fun MullvadMap(
     mode: Boolean,
     fov: Float
 ) {
-    val animatedLat = animateFloatAsState(targetValue = tunnelState.location()?.latitude?.toFloat() ?: 0f, spring(stiffness = 50f))
-    val animatedLon = animateFloatAsState(targetValue = tunnelState.location()?.longitude?.toFloat() ?: 0f, spring(stiffness = 50f))
+    val animatedLat = animateFloatAsState(targetValue = tunnelState.location()?.latitude?.toFloat() ?: 0f, tween(durationMillis = 1000))
+    val animatedLon = animateFloatAsState(targetValue = tunnelState.location()?.longitude?.toFloat() ?: 0f, tween(durationMillis = 1000))
     val disconnectedZoom = 1.35f
     val connectedZoom = 1.25f
-    val zoom = animateFloatAsState(targetValue = if(tunnelState.isSecured()) connectedZoom else disconnectedZoom, spring(stiffness = 50f))
+
+    val zoom = animateFloatAsState(targetValue = if(tunnelState.isSecured()) connectedZoom else disconnectedZoom, tween(durationMillis = 1000))
 
     MapGLShader(
         modifier = modifier,
