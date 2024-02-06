a2332e1a49b53ed9ce55bed66b7270d3d7af049b
Emīls <emils@mullvad.net>
2024-02-06 23:07:37 +0100

    Add FFI to talpid-tunnel-config-client

---
+++ ios/MullvadPostQuantum/MullvadPostQuantum.h
@@ -0,0 +1,19 @@
+//
+//  MullvadPostQuantum.h
+//  MullvadPostQuantum
+//
+//  Created by Marco Nikic on 2024-02-27.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MullvadPostQuantum.
+FOUNDATION_EXPORT double MullvadPostQuantumVersionNumber;
+
+//! Project version string for MullvadPostQuantum.
+FOUNDATION_EXPORT const unsigned char MullvadPostQuantumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MullvadPostQuantum/PublicHeader.h>
+
+
