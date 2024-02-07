fb7ac08bd0133f7aa9249edc6d354f9249a50081
David Göransson <david.goransson90@gmail.com>
2024-02-07 11:24:57 +0100

    Fix animations

--- android/app/src/main/kotlin/net/mullvad/lib/map/MullvadMap.kt
+++ android/app/src/main/kotlin/net/mullvad/lib/map/MullvadMap.kt
@@ -1,21 +1,14 @@
 package net.mullvad.lib.map
 
-import androidx.compose.animation.core.AnimationVector2D
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.EaseInOut
-import androidx.compose.animation.core.MutableTransitionState
-import androidx.compose.animation.core.TwoWayConverter
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateValue
-import androidx.compose.animation.core.animateValueAsState
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 import net.mullvad.lib.map.data.LatLng
 import net.mullvad.lib.map.data.MapViewState
 import net.mullvad.lib.map.data.Marker
@@ -26,6 +19,7 @@ const val animationMinTime = 1300
 const val animationMaxTime = 2500
 const val unsecureZoom = 1.35f
 const val secureZoom = 1.25f
+const val completeAngle = 360f
 const val movementMultiplier = 1.5f
 
 @Composable
@@ -52,112 +46,80 @@ fun MullvadMap(
 
 @Composable
 fun animatedMapViewState(
-    cameraLocation: LatLng,
+    targetCameraLocation: LatLng,
     marker: Marker?,
     percent: Float,
 ): MapViewState {
     val tempPreviousLocation =
-        rememberPrevious(current = cameraLocation, shouldUpdate = { prev, curr -> prev != curr })
-            ?: cameraLocation
+        rememberPrevious(
+            current = targetCameraLocation,
+            shouldUpdate = { prev, curr -> prev != curr }
+        ) ?: targetCameraLocation
+    val previousLocation = remember(targetCameraLocation) { tempPreviousLocation }
 
-    val previousLocation = remember(cameraLocation) { tempPreviousLocation }
-    val distance = remember(cameraLocation) { cameraLocation.distanceTo(previousLocation).toInt() }
-
+    val distance =
+        remember(targetCameraLocation) { targetCameraLocation.distanceTo(previousLocation).toInt() }
     val duration = (distance * 20).coerceIn(animationMinTime, animationMaxTime)
 
-    val cameraLatitude = remember { mutableFloatStateOf(cameraLocation.latitude) }
+    val longitudeAnimation = remember { Animatable(targetCameraLocation.longitude) }
+    val latitudeAnimation = remember { Animatable(targetCameraLocation.latitude) }
+    val secureZoomAnimation = remember {
+        Animatable(if (marker?.type == MarkerType.SECURE) secureZoom else unsecureZoom)
+    }
+    val zoomOutMultiplier = remember { Animatable(1f) }
 
-    val animationVector by
-        remember(cameraLocation) {
-            derivedStateOf { shortestPath(previousLocation, cameraLocation) }
+    LaunchedEffect(targetCameraLocation) {
+        launch { latitudeAnimation.animateTo(targetCameraLocation.latitude, tween(duration)) }
+        launch {
+            val shortestVector =
+                shortestVector(
+                    longitudeAnimation.value,
+                    targetCameraLocation.longitude,
+                    completeAngle
+                )
+            longitudeAnimation.animateTo(longitudeAnimation.value + shortestVector, tween(duration))
         }
-
-    val mutableTransitionState = remember { MutableTransitionState(animationVector) }
-
-    val transition =
-        updateTransition(transitionState = mutableTransitionState, label = "cameraLocation")
-
-    transition.animateValue(
-        typeConverter =
-            TwoWayConverter(
-                convertToVector = { AnimationVector2D(it.latitude, it.longitude) },
-                convertFromVector = { vector: AnimationVector2D -> LatLng(vector.v1, vector.v2) }
-            ),
-        transitionSpec = { tween(duration) },
-        targetValueByState = { state -> state },
-        label = ""
-    )
-
-
-    val newLatLng =
-        if (transition.isRunning) {
-            cameraLocation + animationVector
-        } else {
-            cameraLocation
-        }
-
-    val animatedLatLng =
-        animateValueAsState(
-            targetValue = cameraLocation,
-            typeConverter =
-                TwoWayConverter(
-                    convertToVector = { AnimationVector2D(it.latitude, it.longitude) },
-                    convertFromVector = { LatLng(it.v1, it.v2) }
-                ),
-            animationSpec = tween(duration),
-            finishedListener = { cameraLatitude.value = it.latitude }
-        )
-
-    //    val animatedLat by
-    //        animateFloatAsState(
-    //            targetValue = cameraLatitude.latitude,
-    //            tween(durationMillis = duration),
-    //            label = "latitude"
-    //        )
-    //
-    //    val animatedLon =
-    //        animateFloatAsState(
-    //            targetValue = cameraLatitude.longitude,
-    //            tween(durationMillis = duration),
-    //            label = "longitude"
-    //        )
-
-    val securityZoom =
-        animateFloatAsState(
-            targetValue = if (marker?.type == MarkerType.SECURE) secureZoom else unsecureZoom,
-            animationSpec = tween(duration),
-            label = "secureZoom"
-        )
-    val movementMultiplierAnimation =
-        if (duration < 1700) {
-            remember { mutableFloatStateOf(1f) }
-        } else {
-            animateFloatAsState(
-                targetValue = if (previousLocation != cameraLatitude) 1f else 1.001f,
+        launch {
+            zoomOutMultiplier.animateTo(
+                targetValue = 1f,
                 animationSpec =
                     keyframes {
-                        durationMillis = duration
-                        movementMultiplier at (duration / 3) with EaseInOut
-                        1f at duration with EaseInOut
-                    },
-                label = "zoomMultiplier"
+                        if (duration < 1700) {
+                            durationMillis = duration
+                            1f at duration with EaseInOut
+                        } else {
+                            durationMillis = duration
+                            1.25f at (duration / 3) with EaseInOut
+                            1f at duration with EaseInOut
+                        }
+                    }
             )
         }
+    }
+
+    LaunchedEffect(marker?.type) {
+        launch {
+            secureZoomAnimation.animateTo(
+                targetValue = if (marker?.type == MarkerType.SECURE) secureZoom else unsecureZoom,
+                tween(2000)
+            )
+        }
+    }
 
     return MapViewState(
-        zoom = secureZoom * movementMultiplierAnimation.value,
-        cameraLatLng = transition.currentState,
+        zoom = secureZoomAnimation.value * zoomOutMultiplier.value * 0.9f,
+        cameraLatLng = LatLng(latitudeAnimation.value, longitudeAnimation.value.mod(completeAngle)),
         locationMarker = marker,
         percent = percent
     )
 }
 
-fun shortestPath(c1: LatLng, c2: LatLng): LatLng {
-    var longDiff = c2.latitude - c1.latitude
-    if (longDiff > 180) {
-        longDiff -= 360
-    } else if (longDiff < -180) {
-        longDiff += 360
+fun shortestVector(current: Float, newValue: Float, wrappingSize: Float): Float {
+    val diff = newValue - current
+    val maxDistance = wrappingSize / 2
+    return when {
+        diff > maxDistance -> diff - wrappingSize
+        diff < -maxDistance -> diff + wrappingSize
+        else -> diff
     }
-    return LatLng(c2.longitude - c1.longitude, longDiff)
 }
