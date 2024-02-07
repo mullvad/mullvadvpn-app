32375a1ccbf41dd298be4bc828bb0cef20fb37fa
Markus Pettersson <markus.pettersson@mullvad.net>
2024-02-07 17:52:31 +0100

    Log version using `mullvad-nsis` in Windows installer
    Expose the functionally for calling `RtlGetVersion` from
    `talpid-platform-metadata` through `mullvad-nsis`. This is used for
    getting the Windows build version during the windows installer.

--- mullvad-nsis/include/mullvad-nsis.h
+++ mullvad-nsis/include/mullvad-nsis.h
@@ -23,4 +23,6 @@ Status get_system_local_appdata(uint16_t *buffer, uintptr_t *buffer_size);
 
 Status create_privileged_directory(const uint16_t* path);
 
+Status get_system_version(uint16_t *buffer, uintptr_t *buffer_size);
+
 } // extern "C"
