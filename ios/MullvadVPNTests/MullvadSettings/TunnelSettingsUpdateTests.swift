b1367bd35c7a168560f526f49fea43c33eb13698
Andrew Bulhak <andrew.bulhak@mullvad.net>
2024-02-14 13:50:26 +0100

    Remove empty test setup/teardown

--- ios/MullvadVPNTests/MullvadSettings/TunnelSettingsUpdateTests.swift
+++ ios/MullvadVPNTests/MullvadSettings/TunnelSettingsUpdateTests.swift
@@ -12,13 +12,6 @@ import Network
 import XCTest
 
 final class TunnelSettingsUpdateTests: XCTestCase {
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
     func testApplyDNSSettings() {
         // Given:
