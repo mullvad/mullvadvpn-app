b65d049678b726157413ff7597841017560594f7
Markus Pettersson <markus.pettersson@mullvad.net>
2024-02-16 16:24:33 +0100

    Add `constraints` module
    This commit initializes a series of commits which aims to decompose
    `relay_constraints.rs` into smaller modules.

---
+++ mullvad-types/src/constraints/mod.rs
@@ -0,0 +1,33 @@
+//! Constrain yourself.
+
+mod constraint;
+mod set;
+
+// Re-export bits & pieces from `constraints.rs` as needed
+pub use constraint::Constraint;
+pub use set::Set;
+
+impl<T: Match<U>, U> Match<U> for Constraint<T> {
+    fn matches(&self, other: &U) -> bool {
+        match *self {
+            Constraint::Any => true,
+            Constraint::Only(ref value) => value.matches(other),
+        }
+    }
+}
+
+impl<T: Set<U>, U> Set<Constraint<U>> for Constraint<T> {
+    fn is_subset(&self, other: &Constraint<U>) -> bool {
+        match self {
+            Constraint::Any => other.is_any(),
+            Constraint::Only(ref constraint) => match other {
+                Constraint::Only(ref other_constraint) => constraint.is_subset(other_constraint),
+                _ => true,
+            },
+        }
+    }
+}
+
+pub trait Match<T> {
+    fn matches(&self, other: &T) -> bool;
+}
