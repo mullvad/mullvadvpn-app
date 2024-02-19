266f3e07875c2cc2d3893cc9b78d98babaafb651
Markus Pettersson <markus.pettersson@mullvad.net>
2024-02-19 17:39:51 +0100

    Add `Intersection` trait and implement it on `Constraint`
    Add `Intersection` trait and implement it on the `Constraint` type. Use
    `proptest` write a "proof" for associativity of `intersection`

--- mullvad-types/src/constraints/set.rs
+++ mullvad-types/src/constraints/set.rs
@@ -3,3 +3,14 @@
 pub trait Set<T> {
     fn is_subset(&self, other: &T) -> bool;
 }
+
+/// Any type that wish to implement `Intersection` should make sure that the following properties are upheld:
+/// - idempotency (if there is an identity element)
+/// - commutativity
+/// - associativity
+pub trait Intersection {
+    fn intersection(self, other: Self) -> Option<Self>
+    where
+        Self: PartialEq,
+        Self: Sized;
+}
