b349cc037f93836785128b3d58333a71e0d1c6ef
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-02-21 16:35:43 +0100

    Fixed some issues

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/CustomListLocationsUiState.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/CustomListLocationsUiState.kt
@@ -3,14 +3,17 @@ package net.mullvad.mullvadvpn.compose.state
 import net.mullvad.mullvadvpn.relaylist.RelayItem
 
 sealed interface CustomListLocationsUiState {
-    object Loading : CustomListLocationsUiState
+    val newList: Boolean
+
+    data class Loading(override val newList: Boolean = false) : CustomListLocationsUiState
 
     sealed interface Content : CustomListLocationsUiState {
         val searchTerm: String
 
-        data class Empty(override val searchTerm: String) : Content
+        data class Empty(override val newList: Boolean, override val searchTerm: String) : Content
 
         data class Data(
+            override val newList: Boolean = false,
             val availableLocations: List<RelayItem.Country> = emptyList(),
             val selectedLocations: Set<RelayItem> = emptySet(),
             override val searchTerm: String = ""
