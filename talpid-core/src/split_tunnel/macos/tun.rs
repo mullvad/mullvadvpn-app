5938798ab9dd329175e77bd7ae3a8040f777ab24
David Lönnhager <david.l@mullvad.net>
2024-02-21 23:16:27 +0100

    Silence drop warning

--- talpid-core/src/split_tunnel/macos/tun.rs
+++ talpid-core/src/split_tunnel/macos/tun.rs
@@ -569,7 +569,7 @@ async fn redirect_packets_for_pktap_stream(
                             }
                         }
                         RoutingDecision::Drop => {
-                            log::warn!("Dropped packet from pid {}", packet.header.pth_pid);
+                            log::trace!("Dropped packet from pid {}", packet.header.pth_pid);
                         }
                     }
                 }
