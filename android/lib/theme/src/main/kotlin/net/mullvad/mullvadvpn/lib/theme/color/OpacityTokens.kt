ff1caa42cdb265f3e616602d775ad2f5e2085cfe
mullmat <matilda@mullvad.net>
2024-02-23 13:36:12 +0100

    Add opacity tokens

--- android/lib/theme/src/main/kotlin/net/mullvad/mullvadvpn/lib/theme/color/OpacityTokens.kt
+++ android/lib/theme/src/main/kotlin/net/mullvad/mullvadvpn/lib/theme/color/OpacityTokens.kt
@@ -1,4 +1,39 @@
 package net.mullvad.mullvadvpn.lib.theme.color
 
-object OpacityTokens {
+import androidx.compose.ui.graphics.Color
+
+internal object OpacityTokens {
+    val BlueOnDarkBlue100 = Color(0xFF294d73)
+    val BlueOnDarkBlue90 = Color(0xFF274a6e)
+    val BlueOnDarkBlue80 = Color(0xFF26476a)
+    val BlueOnDarkBlue70 = Color(0xFF244465)
+    val BlueOnDarkBlue60 = Color(0xFF234161)
+    val BlueOnDarkBlue50 = Color(0xFF213e5c)
+    val BlueOnDarkBlue40 = Color(0xFF1f3a57)
+    val BlueOnDarkBlue30 = Color(0xFF1e3753)
+    val BlueOnDarkBlue20 = Color(0xFF1c344e)
+    val BlueOnDarkBlue10 = Color(0xFF1b314a)
+
+    val WhiteOnDarkBlue100 = Color(0xFFFFFFFF)
+    val WhiteOnDarkBlue90 = Color(0xFFE8EAEC)
+    val WhiteOnDarkBlue80 = Color(0xFFD1D5DA)
+    val WhiteOnDarkBlue70 = Color(0xFFBAC0C7)
+    val WhiteOnDarkBlue60 = Color(0xFFA3ABB5)
+    val WhiteOnDarkBlue50 = Color(0xFF8C96A2)
+    val WhiteOnDarkBlue40 = Color(0xFF75828F)
+    val WhiteOnDarkBlue30 = Color(0xFF5E6D7D)
+    val WhiteOnDarkBlue20 = Color(0xFF47586A)
+    val WhiteOnDarkBlue10 = Color(0xFF304358)
+
+    val WhiteOnBlue100 = Color(0xFFFFFFFF)
+    val WhiteOnBlue90 = Color(0xFFEAEDF1)
+    val WhiteOnBlue80 = Color(0xFFD4DBE3)
+    val WhiteOnBlue70 = Color(0xFFBFCAD5)
+    val WhiteOnBlue60 = Color(0xFFA9B8C7)
+    val WhiteOnBlue50 = Color(0xFF94A6B9)
+    val WhiteOnBlue40 = Color(0xFF7F94AB)
+    val WhiteOnBlue30 = Color(0xFF69829D)
+    val WhiteOnBlue20 = Color(0xFF54718F)
+    val WhiteOnBlue10 = Color(0xFF3E5F81)
+
 }
