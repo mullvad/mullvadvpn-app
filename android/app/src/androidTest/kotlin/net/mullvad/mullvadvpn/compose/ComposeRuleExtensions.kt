a52f61bdb16736a8754f1bf9ea74030490e16cdf
Albin <albin@mullvad.net>
2024-02-23 16:35:47 +0100

    Add compose extension that enables edge-to-edge drawing

--- android/app/src/androidTest/kotlin/net/mullvad/mullvadvpn/compose/ComposeRuleExtensions.kt
+++ android/app/src/androidTest/kotlin/net/mullvad/mullvadvpn/compose/ComposeRuleExtensions.kt
@@ -1,9 +1,24 @@
 package net.mullvad.mullvadvpn.compose
 
+import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.core.view.WindowCompat
+import androidx.test.core.app.ActivityScenario
 import de.mannodermaus.junit5.compose.ComposeContext
+import de.mannodermaus.junit5.compose.createAndroidComposeExtension
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 
 fun ComposeContext.setContentWithTheme(content: @Composable () -> Unit) {
     setContent { AppTheme { content() } }
 }
+
+@ExperimentalTestApi
+fun createEdgeToEdgeComposeExtension() =
+    createAndroidComposeExtension<ComponentActivity>(
+        scenarioSupplier = {
+            ActivityScenario.launch(ComponentActivity::class.java).onActivity {
+                WindowCompat.setDecorFitsSystemWindows(it.window, false)
+            } as ActivityScenario<ComponentActivity>
+        }
+    )
