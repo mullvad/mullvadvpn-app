cac4bb473f53802ded0601d784bf0af646756318
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-02-26 14:37:24 +0100

    Further improvements

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/MullvadCheckbox.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/MullvadCheckbox.kt
@@ -1,34 +1,21 @@
 package net.mullvad.mullvadvpn.compose.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import net.mullvad.mullvadvpn.lib.theme.Dimens
 import net.mullvad.mullvadvpn.lib.theme.color.selected
 
 @Composable
 fun MullvadCheckbox(checked: Boolean, onCheckedChange: (Boolean) -> Unit) {
-    Box(
-        modifier =
-            Modifier.size(Dimens.checkBoxSize).background(Color.White, MaterialTheme.shapes.small)
-    ) {
-        Checkbox(
-            modifier = Modifier.fillMaxSize(),
-            checked = checked,
-            onCheckedChange = onCheckedChange,
-            colors =
-                CheckboxDefaults.colors(
-                    checkedColor = Color.Transparent,
-                    uncheckedColor = Color.Transparent,
-                    checkmarkColor = MaterialTheme.colorScheme.selected
-                ),
-        )
-    }
+    Checkbox(
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+        colors =
+            CheckboxDefaults.colors(
+                checkedColor = MaterialTheme.colorScheme.onPrimary,
+                uncheckedColor = MaterialTheme.colorScheme.onPrimary,
+                checkmarkColor = MaterialTheme.colorScheme.selected
+            )
+    )
 }
