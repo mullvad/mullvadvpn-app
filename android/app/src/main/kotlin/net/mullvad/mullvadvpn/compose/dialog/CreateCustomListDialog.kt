16665d772e2bff22941639772abe2229e17700e2
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-02-26 14:37:24 +0100

    Further improvements

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/CreateCustomListDialog.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/CreateCustomListDialog.kt
@@ -9,6 +9,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -70,7 +75,10 @@ fun CreateCustomListDialog(
     onInputChanged: () -> Unit = {},
     onDismiss: () -> Unit = {}
 ) {
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
     val name = remember { mutableStateOf("") }
+
     AlertDialog(
         title = {
             Text(
@@ -108,9 +116,17 @@ fun CreateCustomListDialog(
                                 style = MaterialTheme.typography.bodySmall
                             )
                         }
-                    }
+                    },
+                    modifier =
+                        Modifier.focusRequester(focusRequester).onFocusChanged { focusState ->
+                            if (focusState.hasFocus) {
+                                keyboardController?.show()
+                            }
+                        }
                 )
             }
+
+            LaunchedEffect(Unit) { focusRequester.requestFocus() }
         },
         containerColor = MaterialTheme.colorScheme.background,
         titleContentColor = MaterialTheme.colorScheme.onBackground,
