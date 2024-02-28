af3feca42687b0681444fd3f4cf5bafa95bdb183
Albin <albin@mullvad.net>
2024-02-28 09:49:26 +0100

    Fix argument parsing in test repeat script

--- android/scripts/run-instrumented-tests-repeat.sh
+++ android/scripts/run-instrumented-tests-repeat.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+# Help script to invoke run-instrumented-tests.sh multiple times.
+#
+# Usage:
+# run-instrumented-tests-repeat.sh <repeat-count> [<args>]
+#
+# Example:
+# run-instrumented-tests-repeat.sh 2 --test-type mockapi --infra-flavor prod --billing-flavor oss
+
 set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
-TEST_TYPE=$1
-REPEAT_COUNT=$2
+REPEAT_COUNT=$1
 
 for ((i=1; i <= REPEAT_COUNT; i++))
 do
     echo "### Run $i ###"
-    ./run-instrumented-tests.sh "$TEST_TYPE"
+    ./run-instrumented-tests.sh "${@:2}"
 done
