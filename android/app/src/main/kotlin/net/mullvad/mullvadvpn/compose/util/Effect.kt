2cb2e75355eaec8075bf0339173caefd9ae8d0b7
David Göransson <david.goransson90@gmail.com>
2024-02-28 11:20:43 +0100

    Add new compose side effect collection utility functions

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/util/Effect.kt
@@ -0,0 +1,34 @@
+package net.mullvad.mullvadvpn.compose.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+inline fun <T> LaunchedEffectCollect(
+    sideEffect: Flow<T>,
+    key: Any = Unit,
+    crossinline collector: suspend CoroutineScope.(T) -> Unit
+) {
+    LaunchedEffect(key) { sideEffect.collect { collector(it) } }
+}
+
+// This function will restart collection on Start/Stop events, e.g if the user navigates to home
+// screen collection will stop, and then be restarted when the user opens the app again
+@Composable
+inline fun <T> CollectSideEffectWithLifecycle(
+    sideEffect: Flow<T>,
+    key: Any = Unit,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    crossinline collector: suspend CoroutineScope.(T) -> Unit
+) {
+    val lifecycle = LocalLifecycleOwner.current
+
+    LaunchedEffect(lifecycle, key) {
+        sideEffect.flowWithLifecycle(lifecycle.lifecycle, minActiveState).collect { collector(it) }
+    }
+}
