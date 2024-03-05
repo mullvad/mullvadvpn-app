26913607e56ee0ed7281ada1438b8af4ba746513
David Göransson <david.goransson90@gmail.com>
2024-03-05 11:08:39 +0100

    Add konsist test to ensure we do not use uiState variable name or collectAsState

--- android/test/arch/src/test/kotlin/net/mullvad/mullvadvpn/test/arch/compose/ComposeTest.kt
+++ android/test/arch/src/test/kotlin/net/mullvad/mullvadvpn/test/arch/compose/ComposeTest.kt
@@ -3,6 +3,7 @@ package net.mullvad.mullvadvpn.test.arch.compose
 import androidx.compose.runtime.Composable
 import com.lemonappdev.konsist.api.Konsist
 import com.lemonappdev.konsist.api.ext.list.withAllAnnotationsOf
+import com.lemonappdev.konsist.api.verify.assertFalse
 import com.lemonappdev.konsist.api.verify.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -12,6 +13,16 @@ class ComposeTest {
         allAppComposeFunctions().assertTrue {
             it.resideInPackage("net.mullvad.mullvadvpn.compose..")
         }
+
+    @Test
+    fun `ensure we don't use collectAsState`() =
+        Konsist.scopeFromProduction("app").imports.assertFalse {
+            it.name == "androidx.compose.runtime.collectAsState"
+        }
+
+    @Test
+    fun `ensure all composables do not refer to state as uiState`() =
+        allAppComposeFunctions().assertFalse { it.hasParameter { it.name == "uiState" } }
 
     private fun allAppComposeFunctions() =
         Konsist.scopeFromProduction("app").functions().withAllAnnotationsOf(Composable::class)
