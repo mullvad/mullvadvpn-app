50804eed5ecc9040ccd73bacfe804229238fc9b5
David Göransson <david.goransson90@gmail.com>
2024-03-05 09:56:18 +0100

    Fix long method lints

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/PrivacyDisclaimerScreen.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/PrivacyDisclaimerScreen.kt
@@ -2,10 +2,12 @@ package net.mullvad.mullvadvpn.compose.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -16,7 +18,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,8 +33,7 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.popUpTo
@@ -70,7 +71,7 @@ fun PrivacyDisclaimer(
     navigator: DestinationsNavigator,
 ) {
     val viewModel: PrivacyDisclaimerViewModel = koinViewModel()
-    val uiState = viewModel.uiState.collectAsState()
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
 
     val context = LocalContext.current
     val scope = rememberCoroutineScope()
@@ -105,112 +106,98 @@ fun PrivacyDisclaimer(
             }
         }
     }
-    PrivacyDisclaimerScreen(uiState.value, {}, viewModel::setPrivacyDisclosureAccepted)
+    PrivacyDisclaimerScreen(state, {}, viewModel::setPrivacyDisclosureAccepted)
 }
 
 @Composable
 fun PrivacyDisclaimerScreen(
-    uiState: PrivacyDisclaimerViewState,
+    state: PrivacyDisclaimerViewState,
     onPrivacyPolicyLinkClicked: () -> Unit,
     onAcceptClicked: () -> Unit,
 ) {
     val topColor = MaterialTheme.colorScheme.primary
     ScaffoldWithTopBar(topBarColor = topColor, onAccountClicked = null, onSettingsClicked = null) {
-        ConstraintLayout(
-            modifier =
-                Modifier.padding(it)
-                    .fillMaxSize()
-                    .background(color = MaterialTheme.colorScheme.background)
+        val scrollState = rememberScrollState()
+        Column(
+            Modifier.padding(it)
+                .padding(horizontal = Dimens.sideMargin, vertical = Dimens.screenVerticalMargin)
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.background)
+                .verticalScroll(scrollState)
+                .drawVerticalScrollbar(
+                    state = scrollState,
+                    color = MaterialTheme.colorScheme.onPrimary.copy(alpha = AlphaScrollbar)
+                ),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            val (body, actionButtons) = createRefs()
-            val sideMargin = Dimens.sideMargin
-            val scrollState = rememberScrollState()
+            Content(onPrivacyPolicyLinkClicked)
 
-            Column(
+            ButtonPanel(state.isStartingService, onAcceptClicked)
+        }
+    }
+}
+
+@Composable
+private fun Content(onPrivacyPolicyLinkClicked: () -> Unit) {
+    Column {
+        Text(
+            text = stringResource(id = R.string.privacy_disclaimer_title),
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold
+        )
+
+        val fontSize = 14.sp
+        Text(
+            text = stringResource(id = R.string.privacy_disclaimer_body_first_paragraph),
+            fontSize = fontSize,
+            color = MaterialTheme.colorScheme.onBackground,
+            modifier = Modifier.padding(top = 10.dp)
+        )
+
+        Spacer(modifier = Modifier.height(fontSize.toDp() + Dimens.smallPadding))
+
+        Text(
+            text = stringResource(id = R.string.privacy_disclaimer_body_second_paragraph),
+            fontSize = fontSize,
+            color = MaterialTheme.colorScheme.onBackground,
+        )
+
+        Row(modifier = Modifier.padding(top = 10.dp)) {
+            ClickableText(
+                text = AnnotatedString(stringResource(id = R.string.privacy_policy_label)),
+                onClick = { onPrivacyPolicyLinkClicked() },
+                style =
+                    TextStyle(
+                        fontSize = 12.sp,
+                        color = Color.White,
+                        textDecoration = TextDecoration.Underline
+                    )
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.icon_extlink),
+                contentDescription = null,
                 modifier =
-                    Modifier.constrainAs(body) {
-                            top.linkTo(parent.top)
-                            start.linkTo(parent.start)
-                            end.linkTo(parent.end)
-                            bottom.linkTo(actionButtons.top)
-                            width = Dimension.fillToConstraints
-                            height = Dimension.fillToConstraints
-                        }
-                        .drawVerticalScrollbar(
-                            state = scrollState,
-                            color = MaterialTheme.colorScheme.onPrimary.copy(alpha = AlphaScrollbar)
-                        )
-                        .verticalScroll(scrollState)
-                        .padding(sideMargin),
-            ) {
-                Text(
-                    text = stringResource(id = R.string.privacy_disclaimer_title),
-                    style = MaterialTheme.typography.headlineSmall,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    fontWeight = FontWeight.Bold
-                )
+                    Modifier.align(Alignment.CenterVertically)
+                        .padding(start = 2.dp, top = 2.dp)
+                        .width(10.dp)
+                        .height(10.dp)
+            )
+        }
+    }
+}
 
-                val fontSize = 14.sp
-                Text(
-                    text = stringResource(id = R.string.privacy_disclaimer_body_first_paragraph),
-                    fontSize = fontSize,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    modifier = Modifier.padding(top = 10.dp)
-                )
-
-                Spacer(modifier = Modifier.height(fontSize.toDp() + Dimens.smallPadding))
-
-                Text(
-                    text = stringResource(id = R.string.privacy_disclaimer_body_second_paragraph),
-                    fontSize = fontSize,
-                    color = MaterialTheme.colorScheme.onBackground,
-                )
-
-                Row(modifier = Modifier.padding(top = 10.dp)) {
-                    ClickableText(
-                        text = AnnotatedString(stringResource(id = R.string.privacy_policy_label)),
-                        onClick = { onPrivacyPolicyLinkClicked() },
-                        style =
-                            TextStyle(
-                                fontSize = 12.sp,
-                                color = Color.White,
-                                textDecoration = TextDecoration.Underline
-                            )
-                    )
-
-                    Image(
-                        painter = painterResource(id = R.drawable.icon_extlink),
-                        contentDescription = null,
-                        modifier =
-                            Modifier.align(Alignment.CenterVertically)
-                                .padding(start = 2.dp, top = 2.dp)
-                                .width(10.dp)
-                                .height(10.dp)
-                    )
-                }
-            }
-
-            Column(
-                modifier =
-                    Modifier.constrainAs(actionButtons) {
-                        top.linkTo(body.bottom, margin = sideMargin)
-                        start.linkTo(parent.start, margin = sideMargin)
-                        end.linkTo(parent.end, margin = sideMargin)
-                        bottom.linkTo(parent.bottom, margin = sideMargin)
-                        width = Dimension.fillToConstraints
-                        height = Dimension.preferredWrapContent
-                    },
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                if (uiState.isStartingService) {
-                    MullvadCircularProgressIndicatorMedium()
-                } else {
-                    PrimaryButton(
-                        text = stringResource(id = R.string.agree_and_continue),
-                        onClick = onAcceptClicked::invoke
-                    )
-                }
-            }
+@Composable
+private fun ButtonPanel(isStartingService: Boolean, onAcceptClicked: () -> Unit) {
+    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        if (isStartingService) {
+            MullvadCircularProgressIndicatorMedium()
+        } else {
+            PrimaryButton(
+                text = stringResource(id = R.string.agree_and_continue),
+                onClick = onAcceptClicked::invoke
+            )
         }
     }
 }
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/SettingsScreen.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package net.mullvad.mullvadvpn.compose.screen
 
+import android.content.Context
 import android.net.Uri
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
@@ -11,13 +12,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import net.mullvad.mullvadvpn.R
@@ -46,7 +47,7 @@ import org.koin.androidx.compose.koinViewModel
 private fun PreviewSettings() {
     AppTheme {
         SettingsScreen(
-            uiState =
+            state =
                 SettingsUiState(
                     appVersion = "2222.22",
                     isLoggedIn = true,
@@ -62,9 +63,9 @@ private fun PreviewSettings() {
 @Composable
 fun Settings(navigator: DestinationsNavigator) {
     val vm = koinViewModel<SettingsViewModel>()
-    val state by vm.uiState.collectAsState()
+    val state by vm.uiState.collectAsStateWithLifecycle()
     SettingsScreen(
-        uiState = state,
+        state = state,
         onVpnSettingCellClick = {
             navigator.navigate(VpnSettingsDestination) { launchSingleTop = true }
         },
@@ -81,7 +82,7 @@ fun Settings(navigator: DestinationsNavigator) {
 @ExperimentalMaterial3Api
 @Composable
 fun SettingsScreen(
-    uiState: SettingsUiState,
+    state: SettingsUiState,
     onVpnSettingCellClick: () -> Unit = {},
     onSplitTunnelingCellClick: () -> Unit = {},
     onReportProblemCellClick: () -> Unit = {},
@@ -98,113 +99,125 @@ fun SettingsScreen(
             state = lazyListState
         ) {
             item { Spacer(modifier = Modifier.height(Dimens.cellLabelVerticalPadding)) }
-            if (uiState.isLoggedIn) {
+            if (state.isLoggedIn) {
                 item {
                     NavigationComposeCell(
                         title = stringResource(id = R.string.settings_vpn),
                         onClick = { onVpnSettingCellClick() }
                     )
                 }
-
-                item {
-                    Spacer(modifier = Modifier.height(Dimens.cellVerticalSpacing))
-                    NavigationComposeCell(
-                        title = stringResource(id = R.string.split_tunneling),
-                        onClick = { onSplitTunnelingCellClick() }
-                    )
-                    Spacer(modifier = Modifier.height(Dimens.cellVerticalSpacing))
-                }
-            }
-            item {
-                NavigationComposeCell(
-                    title = stringResource(id = R.string.app_version),
-                    onClick = {
-                        context.openLink(
-                            Uri.parse(
-                                context.resources
-                                    .getString(R.string.download_url)
-                                    .appendHideNavOnPlayBuild(uiState.isPlayBuild)
-                            )
-                        )
-                    },
-                    bodyView =
-                        @Composable {
-                            if (!uiState.isPlayBuild) {
-                                NavigationCellBody(
-                                    content = uiState.appVersion,
-                                    contentBodyDescription =
-                                        stringResource(id = R.string.app_version),
-                                    isExternalLink = true,
-                                )
-                            } else {
-                                Text(
-                                    text = uiState.appVersion,
-                                    style = MaterialTheme.typography.labelMedium,
-                                    color = MaterialTheme.colorScheme.onSecondary
-                                )
-                            }
-                        },
-                    showWarning = uiState.isUpdateAvailable,
-                    isRowEnabled = !uiState.isPlayBuild
-                )
-            }
-            if (uiState.isUpdateAvailable) {
-                item {
-                    Text(
-                        text = stringResource(id = R.string.update_available_footer),
-                        style = MaterialTheme.typography.labelMedium,
-                        color = MaterialTheme.colorScheme.onSecondary,
-                        modifier =
-                            Modifier.background(MaterialTheme.colorScheme.secondary)
-                                .padding(
-                                    start = Dimens.cellStartPadding,
-                                    top = Dimens.cellTopPadding,
-                                    end = Dimens.cellStartPadding,
-                                    bottom = Dimens.cellLabelVerticalPadding,
-                                )
-                    )
-                }
+                item { Spacer(modifier = Modifier.height(Dimens.cellVerticalSpacing)) }
+                item { SplitTunneling(onSplitTunnelingCellClick) }
+                item { Spacer(modifier = Modifier.height(Dimens.cellVerticalSpacing)) }
             }
 
-            itemWithDivider {
-                Spacer(modifier = Modifier.height(Dimens.cellVerticalSpacing))
-                NavigationComposeCell(
-                    title = stringResource(id = R.string.report_a_problem),
-                    onClick = { onReportProblemCellClick() }
-                )
+            item { AppVersion(context, state) }
+
+            item { Spacer(modifier = Modifier.height(Dimens.cellVerticalSpacing)) }
+
+            itemWithDivider { ReportProblem(onReportProblemCellClick) }
+
+            if (!state.isPlayBuild) {
+                itemWithDivider { FaqAndGuides(context) }
             }
 
-            if (!uiState.isPlayBuild) {
-                itemWithDivider {
-                    val faqGuideLabel = stringResource(id = R.string.faqs_and_guides)
-                    NavigationComposeCell(
-                        title = faqGuideLabel,
-                        bodyView = @Composable { DefaultExternalLinkView(faqGuideLabel) },
-                        onClick = {
-                            context.openLink(
-                                Uri.parse(context.resources.getString(R.string.faqs_and_guides_url))
-                            )
-                        }
-                    )
-                }
-            }
-
-            itemWithDivider {
-                val privacyPolicyLabel = stringResource(id = R.string.privacy_policy_label)
-                NavigationComposeCell(
-                    title = privacyPolicyLabel,
-                    bodyView = @Composable { DefaultExternalLinkView(privacyPolicyLabel) },
-                    onClick = {
-                        context.openLink(
-                            Uri.parse(
-                                context.resources
-                                    .getString(R.string.privacy_policy_url)
-                                    .appendHideNavOnPlayBuild(uiState.isPlayBuild)
-                            )
-                        )
-                    }
-                )
-            }
+            itemWithDivider { PrivacyPolicy(context, state) }
         }
     }
+}
+
+@Composable
+private fun SplitTunneling(onSplitTunnelingCellClick: () -> Unit) {
+    NavigationComposeCell(
+        title = stringResource(id = R.string.split_tunneling),
+        onClick = onSplitTunnelingCellClick
+    )
+}
+
+@Composable
+private fun AppVersion(context: Context, state: SettingsUiState) {
+    NavigationComposeCell(
+        title = stringResource(id = R.string.app_version),
+        onClick = {
+            context.openLink(
+                Uri.parse(
+                    context.resources
+                        .getString(R.string.download_url)
+                        .appendHideNavOnPlayBuild(state.isPlayBuild)
+                )
+            )
+        },
+        bodyView =
+            @Composable {
+                if (!state.isPlayBuild) {
+                    NavigationCellBody(
+                        content = state.appVersion,
+                        contentBodyDescription = stringResource(id = R.string.app_version),
+                        isExternalLink = true,
+                    )
+                } else {
+                    Text(
+                        text = state.appVersion,
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.onSecondary
+                    )
+                }
+            },
+        showWarning = state.isUpdateAvailable,
+        isRowEnabled = !state.isPlayBuild
+    )
+
+    if (state.isUpdateAvailable) {
+        Text(
+            text = stringResource(id = R.string.update_available_footer),
+            style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.onSecondary,
+            modifier =
+                Modifier.background(MaterialTheme.colorScheme.secondary)
+                    .padding(
+                        start = Dimens.cellStartPadding,
+                        top = Dimens.cellTopPadding,
+                        end = Dimens.cellStartPadding,
+                        bottom = Dimens.cellLabelVerticalPadding,
+                    )
+        )
+    }
+}
+
+@Composable
+private fun ReportProblem(onReportProblemCellClick: () -> Unit) {
+    NavigationComposeCell(
+        title = stringResource(id = R.string.report_a_problem),
+        onClick = { onReportProblemCellClick() }
+    )
+}
+
+@Composable
+private fun FaqAndGuides(context: Context) {
+    val faqGuideLabel = stringResource(id = R.string.faqs_and_guides)
+    NavigationComposeCell(
+        title = faqGuideLabel,
+        bodyView = @Composable { DefaultExternalLinkView(faqGuideLabel) },
+        onClick = {
+            context.openLink(Uri.parse(context.resources.getString(R.string.faqs_and_guides_url)))
+        }
+    )
+}
+
+@Composable
+private fun PrivacyPolicy(context: Context, state: SettingsUiState) {
+    val privacyPolicyLabel = stringResource(id = R.string.privacy_policy_label)
+    NavigationComposeCell(
+        title = privacyPolicyLabel,
+        bodyView = @Composable { DefaultExternalLinkView(privacyPolicyLabel) },
+        onClick = {
+            context.openLink(
+                Uri.parse(
+                    context.resources
+                        .getString(R.string.privacy_policy_url)
+                        .appendHideNavOnPlayBuild(state.isPlayBuild)
+                )
+            )
+        }
+    )
 }
