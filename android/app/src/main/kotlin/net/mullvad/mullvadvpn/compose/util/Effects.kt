907bb7482c1ea954e0a11e487b1d7f91074b3c2e
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-03-06 11:16:56 +0100

    Made a workable solution for scroll to position in list

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/util/Effects.kt
@@ -0,0 +1,18 @@
+package net.mullvad.mullvadvpn.compose.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun RunOnKeyChange(key: Any, block: suspend CoroutineScope.() -> Unit) {
+    val scope = rememberCoroutineScope()
+    rememberSaveable(key) {
+        scope.launch {
+            block()
+        }
+        key
+    }
+}
