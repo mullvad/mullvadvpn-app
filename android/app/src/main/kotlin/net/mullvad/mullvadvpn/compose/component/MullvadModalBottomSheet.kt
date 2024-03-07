66b2d3ac2cb7812b55ec613e351d6395bf5d6f68
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-03-07 09:21:34 +0100

    Improved the bottom sheet implementation

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/MullvadModalBottomSheet.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/MullvadModalBottomSheet.kt
@@ -3,30 +3,31 @@ package net.mullvad.mullvadvpn.compose.component
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.compose.cell.HeaderCell
 import net.mullvad.mullvadvpn.compose.cell.IconCell
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun PreviewMullvadModalBottomSheet() {
+private fun PreviewMullvadModalBottomSheet() {
     AppTheme {
         MullvadModalBottomSheet(
-            sheetContent = { _, _ ->
+            sheetContent = {
                 HeaderCell(
                     text = "Title",
                 )
@@ -45,14 +46,14 @@ fun PreviewMullvadModalBottomSheet() {
 @Composable
 fun MullvadModalBottomSheet(
     modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
     backgroundColor: Color = MaterialTheme.colorScheme.surfaceContainer,
     onBackgroundColor: Color = MaterialTheme.colorScheme.onSurface,
     closeBottomSheet: () -> Unit,
-    sheetContent: @Composable ColumnScope.(onBackgroundColor: Color, onClose: () -> Unit) -> Unit
+    sheetContent: @Composable ColumnScope.() -> Unit
 ) {
-    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
-    val scope = rememberCoroutineScope()
-
+    // This is to avoid weird colors in the status bar and the navigation bar
+    val paddingValues = BottomSheetDefaults.windowInsets.asPaddingValues()
     ModalBottomSheet(
         onDismissRequest = closeBottomSheet,
         sheetState = sheetState,
@@ -61,15 +62,8 @@ fun MullvadModalBottomSheet(
         windowInsets = WindowInsets(0, 0, 0, 0), // No insets
         dragHandle = { BottomSheetDefaults.DragHandle(color = onBackgroundColor) }
     ) {
-        sheetContent(onBackgroundColor) {
-            scope
-                .launch { sheetState.hide() }
-                .invokeOnCompletion {
-                    if (!sheetState.isVisible) {
-                        closeBottomSheet()
-                    }
-                }
-        }
-        Spacer(modifier = Modifier.height(Dimens.cellHeight))
+        sheetContent()
+        Spacer(modifier = Modifier.height(Dimens.smallPadding))
+        Spacer(modifier = Modifier.height(paddingValues.calculateBottomPadding()))
     }
 }
