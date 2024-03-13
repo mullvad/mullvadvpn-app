1b8c2385266b72d26bfdc2f44df4cde6b2cd545d
Joakim Hulthe <joakim@hulthe.net>
2024-03-13 15:14:41 +0100

    Fix cross-checking talpid-wireguard for windows
    The `wireguard_go` cfg shouldn't be included when compiling towards
    windows. This change makes `cargo c --target x86_64-pc-windows-gnu`
    work on linux again.

--- talpid-wireguard/build.rs
+++ talpid-wireguard/build.rs
@@ -21,8 +21,9 @@ fn main() {
 
     println!("cargo:rustc-link-lib{link_type}=wg");
 
-    #[cfg(any(target_os = "linux", target_os = "macos", target_os = "android"))]
-    println!("cargo:rustc-cfg=wireguard_go");
+    if let "linux" | "macos" | "android" = target_os.as_str() {
+        println!("cargo:rustc-cfg=wireguard_go");
+    }
 }
 
 fn declare_libs_dir(base: &str) {
