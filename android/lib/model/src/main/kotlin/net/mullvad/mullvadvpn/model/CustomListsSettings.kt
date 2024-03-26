603eb04592eb62c5ea2432fa6683741ca8ab7f00
David Göransson <david.goransson@mullvad.net>
2024-03-26 15:47:51 +0100

    wip

--- android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/CustomListsSettings.kt
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/CustomListsSettings.kt
@@ -3,4 +3,4 @@ package net.mullvad.mullvadvpn.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-@Parcelize data class CustomListsSettings(val customLists: ArrayList<CustomList>) : Parcelable
+@Parcelize data class CustomListsSettings(val customLists: List<CustomList>) : Parcelable
