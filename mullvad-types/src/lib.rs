b958d5be764f73a778654a3a8c6c58f40fe34cbe
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-04-02 12:13:38 +0200

    Don't compile intersection doc code

--- mullvad-types/src/lib.rs
+++ mullvad-types/src/lib.rs
@@ -32,9 +32,8 @@ pub const TUNNEL_FWMARK: u32 = 0x6d6f6c65;
 /// The [`Intersection`] implementation of [`RelayQuery`] upholds the following properties:
 ///
 /// * If two [`RelayQuery`]s differ such that no relay matches both, [`Option::None`] is returned:
-/// ```rust
+/// ```rust, ignore
 /// # use mullvad_relay_selector::query::builder::RelayQueryBuilder;
-/// # use mullvad_types::Intersection;
 /// let query_a = RelayQueryBuilder::new().wireguard().build();
 /// let query_b = RelayQueryBuilder::new().openvpn().build();
 /// assert_eq!(query_a.intersection(query_b), None);
@@ -42,59 +41,7 @@ pub const TUNNEL_FWMARK: u32 = 0x6d6f6c65;
 ///
 /// * Otherwise, a new [`RelayQuery`] is returned where each constraint is
 /// as specific as possible. See [`Constraint`] for further details.
-/// ```rust
-/// # use crate::mullvad_relay_selector::*;
-/// # use crate::mullvad_relay_selector::query::*;
-/// # use crate::mullvad_relay_selector::query::builder::*;
-/// # use mullvad_types::relay_list::*;
-/// # use mullvad_types::Intersection;
-/// # use talpid_types::net::wireguard::PublicKey;
-///
-/// // The relay list used by `relay_selector` in this example
-/// let relay_list = RelayList {
-/// #   etag: None,
-/// #   openvpn: OpenVpnEndpointData { ports: vec![] },
-/// #   bridge: BridgeEndpointData {
-/// #       shadowsocks: vec![],
-/// #   },
-/// #   wireguard: WireguardEndpointData {
-/// #       port_ranges: vec![(53, 53), (4000, 33433), (33565, 51820), (52000, 60000)],
-/// #       ipv4_gateway: "10.64.0.1".parse().unwrap(),
-/// #       ipv6_gateway: "fc00:bbbb:bbbb:bb01::1".parse().unwrap(),
-/// #       udp2tcp_ports: vec![],
-/// #   },
-///     countries: vec![RelayListCountry {
-///         name: "Sweden".to_string(),
-/// #       code: "Sweden".to_string(),
-///         cities: vec![RelayListCity {
-///             name: "Gothenburg".to_string(),
-/// #           code: "Gothenburg".to_string(),
-/// #           latitude: 57.70887,
-/// #           longitude: 11.97456,
-///             relays: vec![Relay {
-///                 hostname: "se9-wireguard".to_string(),
-///                 ipv4_addr_in: "185.213.154.68".parse().unwrap(),
-/// #               ipv6_addr_in: Some("2a03:1b20:5:f011::a09f".parse().unwrap()),
-/// #               include_in_country: false,
-/// #               active: true,
-/// #               owned: true,
-/// #               provider: "31173".to_string(),
-/// #               weight: 1,
-/// #               endpoint_data: RelayEndpointData::Wireguard(WireguardRelayEndpointData {
-/// #                   public_key: PublicKey::from_base64(
-/// #                       "BLNHNoGO88LjV/wDBa7CUUwUzPq/fO2UwcGLy56hKy4=",
-/// #                   )
-/// #                   .unwrap(),
-/// #               }),
-/// #               location: None,
-///             }],
-///         }],
-///     }],
-/// };
-///
-/// # let relay_selector = RelaySelector::from_list(SelectorConfig::default(), relay_list.clone());
-/// # let city = |country, city| GeographicLocationConstraint::city(country, city);
-///
+/// ```rust, ignore
 /// let query_a = RelayQueryBuilder::new().wireguard().build();
 /// let query_b = RelayQueryBuilder::new().location(city("Sweden", "Gothenburg")).build();
 ///
