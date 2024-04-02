305809765d8e085ce093ff0d5c307454720b6ac2
Bug Magnet <marco.nikic@mullvad.net>
2024-04-02 10:11:27 +0200

    Fix a race condition bug involving timers and async tasks

--- ios/PacketTunnelCore/Actor/Task+Duration.swift
+++ ios/PacketTunnelCore/Actor/Task+Duration.swift
@@ -40,10 +40,21 @@ extension Task where Success == Never, Failure == Never {
 
         try await withTaskCancellationHandler {
             try await withCheckedThrowingContinuation { continuation in
+                // The `continuation` handler should never be `resume`'d more than once.
+                // Setting the eventHandler on the timer after it has been cancelled will be ignored.
+                // https://github.com/apple/swift-corelibs-libdispatch/blob/77766345740dfe3075f2f60bead854b29b0cfa24/src/source.c#L338
+                // Therefore, set a flag indicating `resume` has already been called to avoid `resume`ing more than once.
+                // Cancelling the timer does not cancel an  event handler that is already running however,
+                // the cancel handler will be scheduled after the event handler has finished.
+                // https://developer.apple.com/documentation/dispatch/1385604-dispatch_source_cancel
+                // Therefore, there it is safe to do this here.
+                var hasResumed = false
                 timer.setEventHandler {
+                    hasResumed = true
                     continuation.resume()
                 }
                 timer.setCancelHandler {
+                    guard hasResumed == false else { return }
                     continuation.resume(throwing: TaskCancellationError())
                 }
                 timer.schedule(wallDeadline: .now() + DispatchTimeInterval.milliseconds(duration.milliseconds))
