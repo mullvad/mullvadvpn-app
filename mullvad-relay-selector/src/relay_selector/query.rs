055bb4873e9f6eb560fc10922e3f560f8ace1fa8
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-04-02 09:57:55 +0200

    Move doctest of `RelayQuery` intersection back

--- mullvad-relay-selector/src/relay_selector/query.rs
+++ mullvad-relay-selector/src/relay_selector/query.rs
@@ -71,6 +71,86 @@ use talpid_types::net::{proxy::CustomProxy, IpVersion, TunnelType};
 /// This example demonstrates creating a `RelayQuery` which can then be passed
 /// to the [`crate::RelaySelector`] to find a relay that matches the criteria.
 /// See [`builder`] for more info on how to construct queries.
+///
+/// ## Intersecting two queries
+///
+/// * If two [`RelayQuery`]s differ such that no relay matches both, [`Option::None`] is returned:
+/// ```rust
+/// # use mullvad_relay_selector::query::builder::RelayQueryBuilder;
+/// # use mullvad_types::Intersection;
+/// let query_a = RelayQueryBuilder::new().wireguard().build();
+/// let query_b = RelayQueryBuilder::new().openvpn().build();
+/// assert_eq!(query_a.intersection(query_b), None);
+/// ```
+///
+/// * Otherwise, a new [`RelayQuery`] is returned where each constraint is
+/// as specific as possible. See [`Constraint`] for further details.
+/// ```rust
+/// # use crate::mullvad_relay_selector::*;
+/// # use crate::mullvad_relay_selector::query::*;
+/// # use crate::mullvad_relay_selector::query::builder::*;
+/// # use mullvad_types::relay_list::*;
+/// # use mullvad_types::Intersection;
+/// # use talpid_types::net::wireguard::PublicKey;
+///
+/// // The relay list used by `relay_selector` in this example
+/// let relay_list = RelayList {
+/// #   etag: None,
+/// #   openvpn: OpenVpnEndpointData { ports: vec![] },
+/// #   bridge: BridgeEndpointData {
+/// #       shadowsocks: vec![],
+/// #   },
+/// #   wireguard: WireguardEndpointData {
+/// #       port_ranges: vec![(53, 53), (4000, 33433), (33565, 51820), (52000, 60000)],
+/// #       ipv4_gateway: "10.64.0.1".parse().unwrap(),
+/// #       ipv6_gateway: "fc00:bbbb:bbbb:bb01::1".parse().unwrap(),
+/// #       udp2tcp_ports: vec![],
+/// #   },
+///     countries: vec![RelayListCountry {
+///         name: "Sweden".to_string(),
+/// #       code: "Sweden".to_string(),
+///         cities: vec![RelayListCity {
+///             name: "Gothenburg".to_string(),
+/// #           code: "Gothenburg".to_string(),
+/// #           latitude: 57.70887,
+/// #           longitude: 11.97456,
+///             relays: vec![Relay {
+///                 hostname: "se9-wireguard".to_string(),
+///                 ipv4_addr_in: "185.213.154.68".parse().unwrap(),
+/// #               ipv6_addr_in: Some("2a03:1b20:5:f011::a09f".parse().unwrap()),
+/// #               include_in_country: false,
+/// #               active: true,
+/// #               owned: true,
+/// #               provider: "31173".to_string(),
+/// #               weight: 1,
+/// #               endpoint_data: RelayEndpointData::Wireguard(WireguardRelayEndpointData {
+/// #                   public_key: PublicKey::from_base64(
+/// #                       "BLNHNoGO88LjV/wDBa7CUUwUzPq/fO2UwcGLy56hKy4=",
+/// #                   )
+/// #                   .unwrap(),
+/// #               }),
+/// #               location: None,
+///             }],
+///         }],
+///     }],
+/// };
+///
+/// # let relay_selector = RelaySelector::from_list(SelectorConfig::default(), relay_list.clone());
+/// # let city = |country, city| GeographicLocationConstraint::city(country, city);
+///
+/// let query_a = RelayQueryBuilder::new().wireguard().build();
+/// let query_b = RelayQueryBuilder::new().location(city("Sweden", "Gothenburg")).build();
+///
+/// let result = relay_selector.get_relay_by_query(query_a.intersection(query_b).unwrap());
+/// assert!(result.is_ok());
+/// ```
+///
+/// This way, if the mullvad app wants to check if the user's relay settings
+/// are compatible with any other [`RelayQuery`], for examples those defined by
+/// [`RETRY_ORDER`] , taking the intersection between them will never result in
+/// a situation where the app can override the user's preferences.
+///
+/// [`RETRY_ORDER`]: crate::RETRY_ORDER
 #[derive(Debug, Clone, Eq, PartialEq, Intersection)]
 pub struct RelayQuery {
     pub location: Constraint<LocationConstraint>,
@@ -89,7 +169,7 @@ impl RelayQuery {
     /// Note that the following identity applies for any `other_query`:
     /// ```rust
     /// # use mullvad_relay_selector::query::RelayQuery;
-    /// # use crate::mullvad_relay_selector::query::Intersection;
+    /// # use mullvad_types::Intersection;
     ///
     /// # let other_query = RelayQuery::new();
     /// assert_eq!(RelayQuery::new().intersection(other_query.clone()), Some(other_query));
