34acd3a84ed934b95c695bcac6dba2d7044e5188
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-04-03 08:31:38 +0200

    Remove `build.e2e.sh`

--- gui/build-e2e.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-COMMIT=$(git rev-parse --short=6 HEAD)
-npm run build-test-executable
-cp ../dist/mullvadvpn-app-e2e-tests "$HOME/.cache/mullvad-test/packages/app-e2e-tests-2024.1-beta2-dev-${COMMIT}_amd64-unknown-linux-gnu"
-cp ../dist/mullvadvpn-app-e2e-tests "$HOME/.cache/mullvad-test/packages/app-e2e-tests-2024.1-beta2-dev-${COMMIT}_x86_64-unknown-linux-gnu"
