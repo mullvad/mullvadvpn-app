b600072da527d252373fc09a82f4839d4dd5fd3b
David Göransson <david.goransson@mullvad.net>
2024-04-03 09:37:56 +0200

    Fix formatting

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/CreateCustomListUiState.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/CreateCustomListUiState.kt
@@ -1,6 +1,5 @@
 package net.mullvad.mullvadvpn.compose.state
 
-import net.mullvad.mullvadvpn.model.CreateCustomListError
 import net.mullvad.mullvadvpn.usecase.customlists.CreateCustomListWithLocationsError
 
 data class CreateCustomListUiState(val error: CreateCustomListWithLocationsError? = null)
