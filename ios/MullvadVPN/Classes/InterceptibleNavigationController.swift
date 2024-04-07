f0faaaa29144dfeb33b3bb297d5ac5ccb2b6fa46
Jon Petersson <jon.petersson@kvadrat.se>
2024-04-07 22:33:45 +0200

    Intercept back button when leaving an unsaved custom list

---
+++ ios/MullvadVPN/Classes/InterceptibleNavigationController.swift
@@ -0,0 +1,36 @@
+//
+//  InterceptibleNavigationController.swift
+//  MullvadVPN
+//
+//  Created by Jon Petersson on 2024-04-05.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import UIKit
+
+class InterceptibleNavigationController: CustomNavigationController {
+    var shouldPopViewController: ((UIViewController) -> Bool)?
+    var shouldPopToViewController: ((UIViewController) -> Bool)?
+
+    // Called when popping the topmost view controller in the stack, eg. by pressing a navigation
+    // bar back button.
+    override func popViewController(animated: Bool) -> UIViewController? {
+        guard let viewController = viewControllers.last else { return nil }
+
+        if shouldPopViewController?(viewController) ?? true {
+            return super.popViewController(animated: animated)
+        } else {
+            return nil
+        }
+    }
+
+    // Called when popping to a specific view controller, eg. by long pressing a navigation bar
+    // back button (revealing a navigation menu) and selecting a destination view controller.
+    override func popToViewController(_ viewController: UIViewController, animated: Bool) -> [UIViewController]? {
+        if shouldPopToViewController?(viewController) ?? true {
+            return super.popToViewController(viewController, animated: animated)
+        } else {
+            return nil
+        }
+    }
+}
