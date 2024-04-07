2ac9458aeebc9c1d568cb32eb70638c8dab1f412
Jon Petersson <jon.petersson@kvadrat.se>
2024-04-07 22:33:45 +0200

    Intercept back button when leaving an unsaved custom list

--- ios/MullvadVPN/Coordinators/CustomLists/EditCustomListCoordinator.swift
+++ ios/MullvadVPN/Coordinators/CustomLists/EditCustomListCoordinator.swift
@@ -78,17 +78,10 @@ extension EditCustomListCoordinator: CustomListViewControllerDelegate {
         let coordinator = EditLocationsCoordinator(
             navigationController: navigationController,
             nodes: nodes,
-            customList: list
+            subject: subject
         )
 
-        coordinator.didFinish = { [weak self] locationsCoordinator, customList in
-            guard let self else { return }
-            subject.send(CustomListViewModel(
-                id: customList.id,
-                name: customList.name,
-                locations: customList.locations,
-                tableSections: subject.value.tableSections
-            ))
+        coordinator.didFinish = { locationsCoordinator in
             locationsCoordinator.removeFromParent()
         }
 
