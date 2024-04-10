0d9ba0152927f2fd35a2fc35466a7c91123055fe
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-04-10 13:02:42 +0200

    Make play calls go through managementservice

--- android/lib/billing/src/main/kotlin/net/mullvad/mullvadvpn/lib/billing/PlayPurchaseRepository.kt
+++ android/lib/billing/src/main/kotlin/net/mullvad/mullvadvpn/lib/billing/PlayPurchaseRepository.kt
@@ -1,28 +1,15 @@
 package net.mullvad.mullvadvpn.lib.billing
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import net.mullvad.mullvadvpn.lib.daemon.grpc.ManagementService
 import net.mullvad.mullvadvpn.model.PlayPurchase
-import net.mullvad.mullvadvpn.model.PlayPurchaseInitResult
-import net.mullvad.mullvadvpn.model.PlayPurchaseVerifyResult
 
-class PlayPurchaseRepository {
-    suspend fun initializePlayPurchase(): PlayPurchaseInitResult {
-        TODO()
-        //        val result = messageHandler.trySendRequest(Request.InitPlayPurchase)
-        //
-        //        return if (result) {
-        //            messageHandler.events<Event.PlayPurchaseInitResultEvent>().first().result
-        //        } else {
-        //            PlayPurchaseInitResult.Error(PlayPurchaseInitError.OtherError)
-        //        }
-    }
+class PlayPurchaseRepository(
+    private val managementService: ManagementService
+) {
+    suspend fun initializePlayPurchase() = managementService.initializePlayPurchase()
 
-    suspend fun verifyPlayPurchase(purchase: PlayPurchase): PlayPurchaseVerifyResult {
-        TODO()
-        //        val result = messageHandler.trySendRequest(Request.VerifyPlayPurchase(purchase))
-        //        return if (result) {
-        //            messageHandler.events<Event.PlayPurchaseVerifyResultEvent>().first().result
-        //        } else {
-        //            PlayPurchaseVerifyResult.Error(PlayPurchaseVerifyError.OtherError)
-        //        }
-    }
+    suspend fun verifyPlayPurchase(purchase: PlayPurchase) =
+        managementService.verifyPlayPurchase(purchase)
 }
