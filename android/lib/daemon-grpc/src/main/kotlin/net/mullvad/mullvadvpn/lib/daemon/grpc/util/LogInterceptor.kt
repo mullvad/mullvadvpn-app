92cd2aa664c62f683c0407f261b0aedb982c712e
David Göransson <david.goransson@mullvad.net>
2024-04-16 10:58:49 +0200

    Clean up ManagementService a bit

---
+++ android/lib/daemon-grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/daemon/grpc/util/LogInterceptor.kt
@@ -0,0 +1,19 @@
+package net.mullvad.mullvadvpn.lib.daemon.grpc.util
+
+import android.util.Log
+import io.grpc.CallOptions
+import io.grpc.Channel
+import io.grpc.ClientCall
+import io.grpc.ClientInterceptor
+import io.grpc.MethodDescriptor
+
+internal class LogInterceptor : ClientInterceptor {
+    override fun <ReqT : Any?, RespT : Any?> interceptCall(
+        method: MethodDescriptor<ReqT, RespT>?,
+        callOptions: CallOptions?,
+        next: Channel?
+    ): ClientCall<ReqT, RespT> {
+        Log.d("ManagementServiceInterceptor", "Intercepted call: ${method?.fullMethodName}")
+        return next!!.newCall(method, callOptions)
+    }
+}
