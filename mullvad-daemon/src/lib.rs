c38473869969588860aea4d0bd1e38cefc8250a1
Markus Pettersson <markus.pettersson@mullvad.net>
2024-04-19 11:40:08 +0200

    Enable missing daita-related code on Linux

--- mullvad-daemon/src/lib.rs
+++ mullvad-daemon/src/lib.rs
@@ -43,7 +43,7 @@ use mullvad_relay_selector::{
 };
 #[cfg(target_os = "android")]
 use mullvad_types::account::{PlayPurchase, PlayPurchasePaymentToken};
-#[cfg(target_os = "windows")]
+#[cfg(any(target_os = "windows", target_os = "linux"))]
 use mullvad_types::wireguard::DaitaSettings;
 use mullvad_types::{
     access_method::{AccessMethod, AccessMethodSetting},
@@ -258,7 +258,7 @@ pub enum DaemonCommand {
     /// Set whether to enable PQ PSK exchange in the tunnel
     SetQuantumResistantTunnel(ResponseTx<(), settings::Error>, QuantumResistantState),
     /// Set DAITA settings for the tunnel
-    #[cfg(target_os = "windows")]
+    #[cfg(any(target_os = "windows", target_os = "linux"))]
     SetDaitaSettings(ResponseTx<(), settings::Error>, DaitaSettings),
     /// Set DNS options or servers to use
     SetDnsOptions(ResponseTx<(), settings::Error>, DnsOptions),
@@ -1229,7 +1229,7 @@ where
                 self.on_set_quantum_resistant_tunnel(tx, quantum_resistant_state)
                     .await
             }
-            #[cfg(target_os = "windows")]
+            #[cfg(any(target_os = "windows", target_os = "linux"))]
             SetDaitaSettings(tx, daita_settings) => {
                 self.on_set_daita_settings(tx, daita_settings).await
             }
@@ -2240,7 +2240,7 @@ where
         }
     }
 
-    #[cfg(target_os = "windows")]
+    #[cfg(any(target_os = "windows", target_os = "linux"))]
     async fn on_set_daita_settings(
         &mut self,
         tx: ResponseTx<(), settings::Error>,
