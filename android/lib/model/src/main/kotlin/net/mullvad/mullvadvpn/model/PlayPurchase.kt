8c7beb0efd10ff8cd4525bd7c341ab09519e35d2
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-04-25 08:12:15 +0200

    Add support for google play payment

--- android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/PlayPurchase.kt
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/PlayPurchase.kt
@@ -1,6 +1,3 @@
 package net.mullvad.mullvadvpn.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize data class PlayPurchase(val productId: String, val purchaseToken: String) : Parcelable
+data class PlayPurchase(val productId: String, val purchaseToken: PlayPurchasePaymentToken)
