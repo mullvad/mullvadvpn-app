fe4ffd2c2aba652d2812c6c87e9cac986b228cfa
David Lönnhager <david.l@mullvad.net>
2024-04-28 19:13:16 +0200

    Make test build setup more like app container

--- test/build.sh
+++ test/build.sh
@@ -2,35 +2,23 @@
 
 set -eu
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_DIR="$SCRIPT_DIR/.."
+cd "$SCRIPT_DIR"
+
+source "$REPO_DIR/scripts/utils/log"
+
 if [[ -z ${TARGET:-""} ]]; then
-    echo "\$TARGET must be specified"
+    log_error "TARGET must be specified"
     exit 1
 fi
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-APP_DIR="$SCRIPT_DIR/.."
-cd "$SCRIPT_DIR"
+source "$REPO_DIR/scripts/utils/log"
 
-if [[ $TARGET == x86_64-unknown-linux-gnu ]]; then
-    CARGO_REGISTRY_VOLUME_NAME=${CARGO_REGISTRY_VOLUME_NAME:-"cargo-registry"}
-
-    if ! podman image exists mullvadvpn-app-tests; then
-        container_image=$(cat "$APP_DIR/building/linux-container-image.txt")
-        podman build -t mullvadvpn-app-tests --build-arg IMAGE="${container_image}" .
-    fi
-
-    podman run --rm -it \
-        -v "${CARGO_REGISTRY_VOLUME_NAME}":/root/.cargo/registry:Z \
-        -v "${APP_DIR}":/src:z \
-        -e CARGO_TARGET_DIR=/src/test/target \
-        mullvadvpn-app-tests \
-        /bin/bash -c "cd /src/test/; cargo build --bin test-runner --bin connection-checker --release --target ${TARGET}"
-else
-    cargo build \
-        --bin test-runner \
-        --bin connection-checker \
-        --release --target "${TARGET}"
-fi
+cargo build \
+    --bin test-runner \
+    --bin connection-checker \
+    --release --target "${TARGET}"
 
 # Only build runner image for Windows
 if [[ $TARGET == x86_64-pc-windows-gnu ]]; then
