91820f49cf38a44593ce170e412a27bb9ea503bd
David Lönnhager <david.l@mullvad.net>
2024-04-29 17:14:03 +0200

    Add apparmor profile on Ubuntu 24.04 or newer

--- dist-assets/linux/after-install.sh
+++ dist-assets/linux/after-install.sh
@@ -6,3 +6,30 @@ chmod u+s "/usr/bin/mullvad-exclude"
 systemctl enable "/usr/lib/systemd/system/mullvad-daemon.service"
 systemctl start mullvad-daemon.service || echo "Failed to start mullvad-daemon.service"
 systemctl enable "/usr/lib/systemd/system/mullvad-early-boot-blocking.service"
+
+# return 0 if version $1 is greater than or equal to $2
+function version_is_ge {
+    [ "$1" = "$2" ] && return 0
+    printf '%s\n' "$2" "$1" | sort -C -V
+}
+
+# Ubuntu 24.04 or newer: Install apparmor profile to allow Electron sandbox to work
+# This disables user namespace restrictions
+os=$(grep -oP '^ID=\K.+' /etc/os-release | tr -d '"')
+version=$(grep -oP '^VERSION_ID=\K.+' /etc/os-release | tr -d '"')
+
+if [[ "$os" == "ubuntu" ]] && version_is_ge "$version" "24.04"; then
+    echo "Creating apparmor profile"
+
+    cat <<EOF > /etc/apparmor.d/mullvad
+abi <abi/4.0>,
+include <tunables/global>
+
+profile mullvad /opt/Mullvad\ VPN/mullvad-gui flags=(unconfined) {
+  userns,
+  # Site-specific additions and overrides. See local/README for details.
+  include if exists <local/mullvad>
+}
+EOF
+    apparmor_parser -r /etc/apparmor.d/mullvad || echo "Failed to reload apparmor profile"
+fi
