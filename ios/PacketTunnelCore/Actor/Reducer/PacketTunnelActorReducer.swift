f126cd3d1cf9f35d8f3bb0efc84cc75dbed5d786
Andrew Bulhak <andrew.bulhak@mullvad.net>
2024-04-29 14:05:39 +0200

    Transfer PacketTunnelActor state-logic to a stateless reducer function

---
+++ ios/PacketTunnelCore/Actor/Reducer/PacketTunnelActorReducer.swift
@@ -0,0 +1,9 @@
+//
+//  PacketTunnelActorReducer.swift
+//  PacketTunnelCore
+//
+//  Created by Andrew Bulhak on 2024-04-26.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
