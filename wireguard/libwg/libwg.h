af59262047937f98253a8e7e8f5d6f5ef6ce1836
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-05-02 11:54:14 +0200

    Delete `wireguard` folder and `libmaybenot`.
    These are moved into `wireguard-go-rs`

--- wireguard/libwg/libwg.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <stdbool.h>
-
-/// Activate DAITA for the specified tunnel.
-bool wgActivateDaita(int8_t* machines, int32_t tunnelHandle, uint32_t eventsCapacity, uint32_t actionsCapacity);
-char* wgGetConfig(int32_t tunnelHandle);
-int32_t wgSetConfig(int32_t tunnelHandle, char* cSettings);
-void wgFreePtr(void*);
