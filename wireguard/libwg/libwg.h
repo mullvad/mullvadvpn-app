8d3f5b9d570d41b0907c92fba6e760f2532b3547
Joakim Hulthe <joakim@hulthe.net>
2024-05-02 11:54:14 +0200

    Use cbindgen for libmaybenot

--- wireguard/libwg/libwg.h
+++ wireguard/libwg/libwg.h
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 /// Activate DAITA for the specified tunnel.
-bool wgActivateDaita(char* machines, int32_t tunnelHandle, uint32_t eventsCapacity, uint32_t actionsCapacity);
+bool wgActivateDaita(int8_t* machines, int32_t tunnelHandle, uint32_t eventsCapacity, uint32_t actionsCapacity);
 char* wgGetConfig(int32_t tunnelHandle);
 int32_t wgSetConfig(int32_t tunnelHandle, char* cSettings);
 void wgFreePtr(void*);
