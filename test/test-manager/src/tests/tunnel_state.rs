f2a02a6c05f1e75eb331401359866dcd86bcb385
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-05-02 11:41:22 +0200

    Update `test-manager`

--- test/test-manager/src/tests/tunnel_state.rs
+++ test/test-manager/src/tests/tunnel_state.rs
@@ -369,6 +369,8 @@ pub async fn test_connected_state(
                     obfuscation: None,
                     entry_endpoint: None,
                     tunnel_interface: _,
+                    #[cfg(target_os = "linux")]
+                        daita: _,
                 },
             ..
         } => {
