b6e6200f8ba855e883a9c3fccf7b15d4a66e2132
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-05-02 11:54:14 +0200

    Add `wireguard-go-rs`

---
+++ wireguard-go-rs/build-wireguard-go.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+# This script is used to build wireguard-go libraries for all the platforms.
+
+set -eu
+
+function is_android_build {
+    for arg in "$@"
+    do
+        case "$arg" in
+            "--android")
+                return 0
+        esac
+    done
+    return 1
+}
+
+function is_docker_build {
+    for arg in "$@"
+    do
+        case "$arg" in
+            "--no-docker")
+                return 1
+        esac
+    done
+    return 0
+}
+
+function unix_target_triple {
+    local platform
+    platform="$(uname -s)"
+    if [[ ("${platform}" == "Linux") ]]; then
+        local arch
+        arch="$(uname -m)"
+        echo "${arch}-unknown-linux-gnu"
+    elif [[ ("${platform}" == "Darwin") ]]; then
+        local arch
+        arch="$(uname -m)"
+        if [[ ("${arch}" == "arm64") ]]; then
+            arch="aarch64"
+        fi
+        echo "${arch}-apple-darwin"
+    else
+        echo "Can't deduce target dir for $platform"
+        return 1
+    fi
+}
+
+
+function build_unix {
+    echo "Building wireguard-go for $1"
+
+    # Flags for cross compiling
+    if [[ "$(unix_target_triple)" != "$1" ]]; then
+        # Linux arm
+        if [[ "$1" == "aarch64-unknown-linux-gnu" ]]; then
+            export CGO_ENABLED=1
+            export GOARCH=arm64
+            export CC=aarch64-linux-gnu-gcc
+        fi
+
+        # Environment flags for cross compiling on macOS
+        if [[ "$1" == *-apple-darwin ]]; then
+            export CGO_ENABLED=1
+            export GOOS=darwin
+
+            local arch=x86_64
+            export GOARCH=amd64
+            if [[ "$1" == aarch64-* ]]; then
+                arch=arm64
+                export GOARCH=arm64
+            fi
+
+            CC="$(xcrun -sdk "$SDKROOT" --find clang) -arch $arch -isysroot $SDKROOT"
+            export CC
+            export CFLAGS="-isysroot $SDKROOT -arch $arch -I$SDKROOT/usr/include"
+            export LD_LIBRARY_PATH="$SDKROOT/usr/lib"
+            export CGO_CFLAGS="-isysroot $SDKROOT -arch $arch"
+            export CGO_LDFLAGS="-isysroot $SDKROOT -arch $arch"
+        fi
+    fi
+
+    pushd libwg
+        # TODO: pass OUT_DIR as an arg maybe? instead of relying on cargo setting it globally?
+        go build -v -o "$OUT_DIR"/libwg.a -buildmode c-archive
+    popd
+}
+
+function build_android {
+    echo "Building for android"
+
+    if is_docker_build "$@"; then
+        ../building/container-run.sh android wireguard/libwg/build-android.sh
+    else
+        ./libwg/build-android.sh
+    fi
+}
+
+function build_wireguard_go {
+    if is_android_build "$@"; then
+        build_android "$@"
+        return
+    fi
+
+    local platform
+    platform="$(uname -s)";
+    case  "$platform" in
+        Linux*|Darwin*) build_unix "${1:-$(unix_target_triple)}";;
+        *)
+            echo "Unsupported platform"
+            return 1
+            ;;
+    esac
+}
+
+# Ensure we are in the correct directory for the execution of this script
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$script_dir"
+build_wireguard_go "$@"
---
+++ wireguard-go-rs/libwg/go.mod
@@ -0,0 +1,16 @@
+module github.com/mullvad/mullvadvpn-app/wireguard/libwg
+
+go 1.21
+
+require (
+	golang.org/x/sys v0.12.0
+	golang.zx2c4.com/wireguard v0.0.0-20230223181233-21636207a675
+)
+
+require (
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
+)
+
+replace golang.zx2c4.com/wireguard => ../wireguard-go
