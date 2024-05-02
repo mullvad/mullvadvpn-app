23963f9074aef8bcc89c3db080c0a850d4201dc7
David Lönnhager <david.l@mullvad.net>
2024-05-02 11:54:13 +0200

    Add `wireguard-go-rs`
    - Activate DAITA and add `wgActivateDaita` and `wgReceiveEvent` FFI
    - Implement `start_daita` on Wireguard-go tunnel type
    - Mention DAITA in `wireguard-go-rs` description
    - Do not compile `wireguard-go-rs` on Windows
    - Handle DAITA closed on `nil` event
    - Handle daita action timeouts in libwg
    - Remove noisy log lines
    - Remove `maybenot_on_action` callback
    - Remove unused link to `../build/lib` for `talpid-wireguard`
    
    Co-authored-by: Sebastian Holmin <sebastian.holmin@mullvad.net>

--- talpid-wireguard/build.rs
+++ talpid-wireguard/build.rs
@@ -4,22 +4,6 @@ fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set");
 
     declare_libs_dir("../dist-assets/binaries");
-    declare_libs_dir("../build/lib");
-
-    let link_type = match target_os.as_str() {
-        "android" => "",
-        "linux" | "macos" => "=static",
-        // We would like to avoid panicking on windows even if we can not link correctly
-        // because we would like to be able to run check and clippy.
-        // This does not allow for correct linking or buijding.
-        #[cfg(not(windows))]
-        "windows" => "",
-        #[cfg(windows)]
-        "windows" => "dylib",
-        _ => panic!("Unsupported platform: {target_os}"),
-    };
-
-    println!("cargo:rustc-link-lib{link_type}=wg");
 
     add_wireguard_go_cfg(&target_os);
 }
---
+++ wireguard-go-rs/src/lib.rs
@@ -0,0 +1,69 @@
+#![cfg(unix)]
+pub type Fd = std::os::unix::io::RawFd;
+use std::ffi::{c_char, c_void};
+pub type WgLogLevel = u32;
+pub type LoggingCallback =
+    unsafe extern "system" fn(level: WgLogLevel, msg: *const c_char, context: *mut c_void);
+
+extern "C" {
+    /// Creates a new wireguard tunnel, uses the specific interface name, MTU and file descriptors
+    /// for the tunnel device and logging.
+    ///
+    /// Positive return values are tunnel handles for this specific wireguard tunnel instance.
+    /// Negative return values signify errors. All error codes are opaque.
+    #[cfg(not(target_os = "android"))]
+    pub fn wgTurnOn(
+        mtu: isize,
+        settings: *const i8,
+        fd: Fd,
+        logging_callback: Option<LoggingCallback>,
+        logging_context: *mut c_void,
+    ) -> i32;
+
+    // Android
+    #[cfg(target_os = "android")]
+    pub fn wgTurnOn(
+        settings: *const i8,
+        fd: Fd,
+        logging_callback: Option<LoggingCallback>,
+        logging_context: *mut c_void,
+    ) -> i32;
+
+    // Pass a handle that was created by wgTurnOn to stop a wireguard tunnel.
+    pub fn wgTurnOff(handle: i32) -> i32;
+
+    // Returns the file descriptor of the tunnel IPv4 socket.
+    pub fn wgGetConfig(handle: i32) -> *mut c_char;
+
+    // Sets the config of the WireGuard interface.
+    pub fn wgSetConfig(handle: i32, settings: *const i8) -> i32;
+
+    /// Activate DAITA for the specified peer.
+    ///
+    /// `tunnel_handle` must come from [wgTurnOn]. `machines` is a string containing LF-separated
+    /// maybenot machines.
+    ///
+    /// # Safety:
+    /// - `peer_public_key` must point to a 32 byte array.
+    /// - `machines` must point to a null-terminated UTF-8 string.
+    /// - Neither pointer will be written to by `wgActivateDaita`.
+    /// - Neither pointer will be read from after `wgActivateDaita` has returned.
+    pub fn wgActivateDaita(
+        tunnel_handle: i32,
+        peer_public_key: *const u8,
+        machines: *const c_char,
+        events_capacity: u32,
+        actions_capacity: u32,
+    ) -> i32;
+
+    // Frees a pointer allocated by the go runtime - useful to free return value of wgGetConfig
+    pub fn wgFreePtr(ptr: *mut c_void);
+
+    // Returns the file descriptor of the tunnel IPv4 socket.
+    #[cfg(target_os = "android")]
+    pub fn wgGetSocketV4(handle: i32) -> Fd;
+
+    // Returns the file descriptor of the tunnel IPv6 socket.
+    #[cfg(target_os = "android")]
+    pub fn wgGetSocketV6(handle: i32) -> Fd;
+}
