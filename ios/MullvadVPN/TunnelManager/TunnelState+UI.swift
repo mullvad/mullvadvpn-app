29066a4e62233683c8c7eb64ce369ce4e1296ae7
Andrew Bulhak <andrew.bulhak@mullvad.net>
2024-05-03 15:26:12 +0200

    Add tests to TunnelState

---
+++ ios/MullvadVPN/TunnelManager/TunnelState+UI.swift
@@ -0,0 +1,9 @@
+//
+//  TunnelState+UI.swift
+//  MullvadVPN
+//
+//  Created by Andrew Bulhak on 2024-05-03.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
---
+++ ios/MullvadVPNTests/TunnelStateTests.swift
@@ -0,0 +1,36 @@
+//
+//  TunnelStateTests.swift
+//  MullvadVPNTests
+//
+//  Created by Andrew Bulhak on 2024-05-03.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import XCTest
+
+final class TunnelStateTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
