639d57d8117b7d792e427faea7fdc462ed611c52
Joakim Hulthe <joakim@hulthe.net>
2024-05-06 14:45:03 +0200

    Dynamically link wireguard go on android

--- wireguard-go-rs/build.rs
+++ wireguard-go-rs/build.rs
@@ -1,15 +1,24 @@
 use core::{panic, str};
-use std::env;
+use std::{env, path::PathBuf};
 // use std::path::PathBuf;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").expect("Missing OUT_DIR");
     eprintln!("OUT_DIR: {out_dir}");
 
-    let output = std::process::Command::new("bash")
-        .arg("./build-wireguard-go.sh")
-        .output()
-        .expect("build-wireguard-go.sh failed");
+    let target_os = env::var("CARGO_CFG_TARGET_OS").expect("Missing 'CARGO_CFG_TARGET_OS");
+    let mut cmd = std::process::Command::new("bash");
+    cmd.arg("./build-wireguard-go.sh");
+
+    match target_os.as_str() {
+        "linux" => {}
+        "android" => {
+            cmd.arg("--android").arg("--no-docker");
+        }
+        _ => unimplemented!("building wireguard-go-rs for {target_os} is not implemented."),
+    }
+
+    let output = cmd.output().expect("build-wireguard-go.sh failed");
     if !output.status.success() {
         let stdout = str::from_utf8(&output.stdout).unwrap();
         let stderr = str::from_utf8(&output.stderr).unwrap();
@@ -20,23 +29,35 @@ fn main() {
     }
 
     // declare_libs_dir("../dist-assets/binaries");
-    // declare_libs_dir("../build/lib");
+    declare_libs_dir("../build/lib");
 
-    println!("cargo:rustc-link-search={out_dir}");
-    println!("cargo:rustc-link-lib=static=wg");
+    println!("cargo::rustc-link-search={out_dir}");
+
+    if target_os == "android" {
+        // NOTE: Go programs does not support being statically linked on android
+        // so we need to dynamically link to libwg
+        println!("cargo::rustc-link-lib=wg")
+    } else {
+        // other platforms can statically link to libwg just fine
+        // TODO: consider doing dynamic linking everywhere, to keep things simpler
+        println!("cargo::rustc-link-lib=static=wg");
+    }
+
     // TODO: check that these are correct
-    println!("cargo:rerun-if-changed=libwg");
+    println!("cargo::rerun-if-changed=libwg");
 }
 
-// fn declare_libs_dir(base: &str) {
-//     let target_triplet = env::var("TARGET").expect("TARGET is not set");
-//     let lib_dir = manifest_dir().join(base).join(target_triplet);
-//     println!("cargo:rerun-if-changed={}", lib_dir.display());
-//     println!("cargo:rustc-link-search={}", lib_dir.display());
-// }
+/// Tell linker to check `base`/$TARGET for shared libraries.
+fn declare_libs_dir(base: &str) {
+    let target_triplet = env::var("TARGET").expect("TARGET is not set");
+    let lib_dir = manifest_dir().join(base).join(target_triplet);
+    println!("cargo::rerun-if-changed={}", lib_dir.display());
+    println!("cargo::rustc-link-search={}", lib_dir.display());
+}
 
-// fn manifest_dir() -> PathBuf {
-//     env::var("CARGO_MANIFEST_DIR")
-//         .map(PathBuf::from)
-//         .expect("CARGO_MANIFEST_DIR env var not set")
-// }
+/// Get the directory containing `Cargo.toml`
+fn manifest_dir() -> PathBuf {
+    env::var("CARGO_MANIFEST_DIR")
+        .map(PathBuf::from)
+        .expect("CARGO_MANIFEST_DIR env var not set")
+}
