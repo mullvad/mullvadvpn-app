e4a043c0d31f737216fd20516d0008dfb70e770b
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-05-06 21:06:21 +0200

    Add `daita` feature to `wireguard-go-rs`

--- wireguard-go-rs/src/lib.rs
+++ wireguard-go-rs/src/lib.rs
@@ -6,6 +6,7 @@ pub type LoggingCallback =
     unsafe extern "system" fn(level: WgLogLevel, msg: *const c_char, context: *mut c_void);
 
 // Needed to link libwg with cmaybenot
+#[cfg(feature = "daita")]
 use cmaybenot as _;
 
 extern "C" {
