56e219e80ff87f8bffc950f75b3f46d84101f508
Markus Pettersson <markus.pettersson@mullvad.net>
2024-05-13 14:33:54 +0200

    Fix cargo build script warning
    Replace `cargo:key=value` with `cargo::key=value`

--- mullvad-cli/build.rs
+++ mullvad-cli/build.rs
@@ -18,8 +18,8 @@ fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set");
 
     // Enable Daita by default on Linux and Windows.
-    println!("cargo:rustc-check-cfg=cfg(daita)");
+    println!("cargo::rustc-check-cfg=cfg(daita)");
     if let "linux" | "windows" = target_os.as_str() {
-        println!(r#"cargo:rustc-cfg=daita"#);
+        println!(r#"cargo::rustc-cfg=daita"#);
     }
 }
