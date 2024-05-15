197f501e5b38049fb23f5a2f7907d14b9ca0e7a5
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-05-15 22:16:14 +0200

    Some fixes

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/AppVersionInfoRepository.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/AppVersionInfoRepository.kt
@@ -1,17 +1,13 @@
 package net.mullvad.mullvadvpn.ui.serviceconnection
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import net.mullvad.mullvadvpn.lib.daemon.grpc.ManagementService
 import net.mullvad.mullvadvpn.ui.VersionInfo
 
 class AppVersionInfoRepository(private val managementService: ManagementService) {
     fun versionInfo(): Flow<VersionInfo> =
-        combine(
-            managementService.versionInfo,
-            managementService.settings.map { it.showBetaReleases }
-        ) { appVersionInfo, showBetaReleases ->
+        managementService.versionInfo.map { appVersionInfo ->
             VersionInfo(
                 suggestedUpgradeVersion = appVersionInfo.suggestedUpgrade,
                 isSupported = appVersionInfo.supported,
