41f47c4fcdd63483ffa0dbab2e2e17bd5a7ddab4
Markus Pettersson <markus.pettersson@mullvad.net>
2024-05-17 16:36:56 +0200

    Add `daita` as a Cargo cfg variable
    Gate DAITA compilation on `"cargo::rustc-cfg=daita"` emitted in build
    files per platform.

--- mullvad-cli/src/format.rs
+++ mullvad-cli/src/format.rs
@@ -174,7 +174,7 @@ fn format_relay_connection(
         "\nQuantum resistant tunnel: no"
     };
 
-    #[cfg(any(target_os = "windows", target_os = "linux"))]
+    #[cfg(daita)]
     let daita = if !verbose {
         ""
     } else if endpoint.daita {
@@ -182,7 +182,7 @@ fn format_relay_connection(
     } else {
         "\nDAITA: no"
     };
-    #[cfg(not(any(target_os = "windows", target_os = "linux")))]
+    #[cfg(not(daita))]
     let daita = "";
 
     let mut bridge_type = String::new();
--- talpid-wireguard/build.rs
+++ talpid-wireguard/build.rs
@@ -9,17 +9,23 @@ fn main() {
 }
 
 fn add_wireguard_go_cfg(target_os: &str) {
-    println!("cargo:rustc-check-cfg=cfg(wireguard_go)");
+    println!("cargo::rustc-check-cfg=cfg(wireguard_go)");
     if matches!(target_os, "linux" | "macos" | "android") {
-        println!("cargo:rustc-cfg=wireguard_go");
+        println!("cargo::rustc-cfg=wireguard_go");
+    }
+
+    // Enable Daita by default on Linux and Windows.
+    println!("cargo::rustc-check-cfg=cfg(daita)");
+    if matches!(target_os, "linux" | "windows") {
+        println!(r#"cargo::rustc-cfg=daita"#);
     }
 }
 
 fn declare_libs_dir(base: &str) {
     let target_triplet = env::var("TARGET").expect("TARGET is not set");
     let lib_dir = manifest_dir().join(base).join(target_triplet);
-    println!("cargo:rerun-if-changed={}", lib_dir.display());
-    println!("cargo:rustc-link-search={}", lib_dir.display());
+    println!("cargo::rerun-if-changed={}", lib_dir.display());
+    println!("cargo::rustc-link-search={}", lib_dir.display());
 }
 
 fn manifest_dir() -> PathBuf {
