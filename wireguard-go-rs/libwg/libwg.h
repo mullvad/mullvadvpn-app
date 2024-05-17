d095e4d76710a4063d9bcf596d6a971223fbf163
Markus Pettersson <markus.pettersson@mullvad.net>
2024-05-17 16:39:02 +0200

    Add `wireguard-go-rs`

--- wireguard-go-rs/libwg/libwg.h
+++ wireguard-go-rs/libwg/libwg.h
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 /// Activate DAITA for the specified tunnel.
-bool wgActivateDaita(int32_t tunnelHandle, uint8_t* noisePublic, char* machines, uint32_t eventsCapacity, uint32_t actionsCapacity);
+int32_t wgActivateDaita(int32_t tunnelHandle, uint8_t* noisePublic, char* machines, uint32_t eventsCapacity, uint32_t actionsCapacity);
 char* wgGetConfig(int32_t tunnelHandle);
 int32_t wgSetConfig(int32_t tunnelHandle, char* cSettings);
 void wgFreePtr(void*);
