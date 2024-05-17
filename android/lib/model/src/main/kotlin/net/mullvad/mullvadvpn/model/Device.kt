5d5adc67a97ea8ec12ade6340a8e8753090b2666
David Göransson <david.goransson@mullvad.net>
2024-05-17 13:50:29 +0200

    Bake in ext fun

--- android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/Device.kt
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/model/Device.kt
@@ -6,11 +6,11 @@ import org.joda.time.DateTime
 
 @Parcelize
 data class Device(val id: DeviceId, private val name: String, val created: DateTime) : Parcelable {
-    fun displayName(): String = name.capitalizeFirstCharOfEachWord()
-}
-
-private fun String.capitalizeFirstCharOfEachWord(): String {
-    return split(" ")
-        .joinToString(" ") { word -> word.replaceFirstChar { firstChar -> firstChar.uppercase() } }
-        .trimEnd()
+    fun displayName(): String =
+        name
+            .split(" ")
+            .joinToString(" ") { word ->
+                word.replaceFirstChar { firstChar -> firstChar.uppercase() }
+            }
+            .trimEnd()
 }
