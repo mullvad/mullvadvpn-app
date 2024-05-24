ea655631fad76c578b91f527a9ced3e5ccced505
Markus Pettersson <markus.pettersson@mullvad.net>
2024-05-24 08:46:58 +0200

    Remove dead code
    Clean up import gated by non-existing `daita` feature

--- wireguard-go-rs/src/lib.rs
+++ wireguard-go-rs/src/lib.rs
@@ -5,10 +5,6 @@ pub type WgLogLevel = u32;
 pub type LoggingCallback =
     unsafe extern "system" fn(level: WgLogLevel, msg: *const c_char, context: *mut c_void);
 
-// Needed to link libwg with cmaybenot
-#[cfg(feature = "daita")]
-use cmaybenot as _;
-
 extern "C" {
     /// Creates a new wireguard tunnel, uses the specific interface name, MTU and file descriptors
     /// for the tunnel device and logging.
