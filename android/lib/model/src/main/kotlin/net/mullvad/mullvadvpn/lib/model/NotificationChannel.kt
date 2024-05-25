76d63375dcef4eb086bcb0360a1fc84cafe80072
David Göransson <david.goransson@mullvad.net>
2024-05-25 16:42:53 +0200

    Restore channel id

--- android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/NotificationChannel.kt
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/NotificationChannel.kt
@@ -4,12 +4,12 @@ sealed interface NotificationChannel {
     val id: NotificationChannelId
 
     data object TunnelUpdates : NotificationChannel {
-        private const val CHANNEL_ID = "tunnel_state_notification"
+        private const val CHANNEL_ID = "vpn_tunnel_status"
         override val id: NotificationChannelId = NotificationChannelId(CHANNEL_ID)
     }
 
     data object AccountUpdates : NotificationChannel {
-        private const val CHANNEL_ID = "account_updates"
+        private const val CHANNEL_ID = "mullvad_account_time"
         override val id: NotificationChannelId = NotificationChannelId(CHANNEL_ID)
     }
 }
