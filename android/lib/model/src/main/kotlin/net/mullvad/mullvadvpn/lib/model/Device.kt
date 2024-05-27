d821a36c5ddc06e0ad5e9d9a6c6020533e921f0d
David Göransson <david.goransson@mullvad.net>
2024-05-27 10:44:11 +0200

    Add startcase extension function

--- android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/Device.kt
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/Device.kt
@@ -2,13 +2,11 @@ package net.mullvad.mullvadvpn.lib.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import net.mullvad.mullvadvpn.lib.model.extensions.startCase
 import org.joda.time.DateTime
 
 @Parcelize
 data class Device(val id: DeviceId, private val name: String, val creationDate: DateTime) :
     Parcelable {
-    fun displayName(): String =
-        name.split(" ").joinToString(" ") { word ->
-            word.replaceFirstChar { firstChar -> firstChar.uppercase() }
-        }
+    fun displayName(): String = name.startCase()
 }
