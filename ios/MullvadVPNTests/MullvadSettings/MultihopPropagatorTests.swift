8279346d015391ae6becfb07e70e17546c2c38c5
mojganii <mojgan.jelodar@codic.se>
2024-05-30 13:22:00 +0200

    Upgrade settings schema to associate with multihop

---
+++ ios/MullvadVPNTests/MullvadSettings/MultihopPropagatorTests.swift
@@ -0,0 +1,31 @@
+//
+//  MultihopPropagatorTests.swift
+//  MullvadVPNTests
+//
+//  Created by Mojgan on 2024-05-29.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+@testable import MullvadSettings
+import XCTest
+
+class MultihopPropagatorTests: XCTestCase {
+    func testMultipleListener() {
+        let multihopStateListener = MultihopStateListener()
+        let multihopPropagator = MultihopUpdater(listener: multihopStateListener)
+
+        var count = 0
+
+        multihopPropagator.addObserver(MultihopObserverBlock(didUpdateMultihop: { _, _ in
+            count += 1
+        }))
+
+        multihopPropagator.addObserver(MultihopObserverBlock(didUpdateMultihop: { _, _ in
+            count += 1
+        }))
+
+        multihopStateListener.onNewMultihop?(.on)
+
+        XCTAssertEqual(count, 2)
+    }
+}
