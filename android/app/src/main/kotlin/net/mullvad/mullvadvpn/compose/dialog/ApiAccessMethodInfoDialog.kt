6d715779ab5bd899028d54583706a58bda9b734e
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-06-04 17:16:59 +0200

    Improve layout of api access info dialog

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/ApiAccessMethodInfoDialog.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/ApiAccessMethodInfoDialog.kt
@@ -9,22 +9,26 @@ import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 import com.ramcosta.composedestinations.spec.DestinationStyle
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.compose.component.textResource
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
 
 @Preview
 @Composable
 private fun PreviewApiAccessMethodInfoDialog() {
-    ApiAccessMethodInfoDialog(EmptyDestinationsNavigator)
+    AppTheme { ApiAccessMethodInfoDialog(EmptyDestinationsNavigator) }
 }
 
 @Destination(style = DestinationStyle.Dialog::class)
 @Composable
 fun ApiAccessMethodInfoDialog(navigator: DestinationsNavigator) {
     InfoDialog(
-        message = stringResource(id = R.string.api_access_method_info_first_line),
-        additionalInfo =
+        message =
             buildString {
+                appendLine(stringResource(id = R.string.api_access_method_info_first_line))
+                appendLine()
                 appendLine(stringResource(id = R.string.api_access_method_info_second_line))
+                appendLine()
                 appendLine(textResource(id = R.string.api_access_method_info_third_line))
+                appendLine()
                 appendLine(textResource(id = R.string.api_access_method_info_fourth_line))
             },
         onDismiss = navigator::navigateUp
