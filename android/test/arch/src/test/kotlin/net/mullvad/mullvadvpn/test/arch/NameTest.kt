561c4d8262fde4d70c13fb1de0642bee521fbbe3
Albin <albin@mullvad.net>
2024-06-07 17:21:14 +0200

    Add test for naming of declarations and parameters

---
+++ android/test/arch/src/test/kotlin/net/mullvad/mullvadvpn/test/arch/NameTest.kt
@@ -0,0 +1,33 @@
+package net.mullvad.mullvadvpn.test.arch
+
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.parameters
+import com.lemonappdev.konsist.api.provider.KoNameProvider
+import com.lemonappdev.konsist.api.verify.assertFalse
+import java.util.stream.Stream
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
+
+class NameTest {
+    @TestFactory
+    fun `ensure no disallowed declaration or parameter names`(): Stream<DynamicTest> =
+        DISALLOWED_DECLARATION_OR_PARAMETER_NAMES.stream().map { disallowedName ->
+            val testName = "ensure no declarations or parameters include: $disallowedName"
+            dynamicTest(testName) {
+                Konsist.scopeFromProject()
+                    .let { it.declarations() + it.functions().parameters }
+                    .filterIsInstance<KoNameProvider>()
+                    .assertFalse(testName = testName) { it.hasNameContaining(disallowedName) }
+            }
+        }
+
+    companion object {
+        private val DISALLOWED_DECLARATION_OR_PARAMETER_NAMES =
+            listOf(
+                "accountToken",
+                "AccountToken",
+                "ACCOUNT_TOKEN",
+            )
+    }
+}
