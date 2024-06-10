5a28f7fee006f8cb80db1fff59c8eef834f9f30c
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-06-10 11:51:15 +0200

    Add generic delete confirmation dialog

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/DeleteApiAccessMethodConfirmationDialog.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/DeleteApiAccessMethodConfirmationDialog.kt
@@ -1,20 +1,6 @@
 package net.mullvad.mullvadvpn.compose.dialog
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -22,13 +8,10 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.result.ResultBackNavigator
 import com.ramcosta.composedestinations.spec.DestinationStyle
 import net.mullvad.mullvadvpn.R
-import net.mullvad.mullvadvpn.compose.button.NegativeButton
-import net.mullvad.mullvadvpn.compose.button.PrimaryButton
 import net.mullvad.mullvadvpn.compose.state.DeleteApiAccessMethodUiState
 import net.mullvad.mullvadvpn.compose.util.LaunchedEffectCollect
 import net.mullvad.mullvadvpn.lib.model.ApiAccessMethodId
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
-import net.mullvad.mullvadvpn.lib.theme.Dimens
 import net.mullvad.mullvadvpn.viewmodel.DeleteApiAccessMethodConfirmationSideEffect
 import net.mullvad.mullvadvpn.viewmodel.DeleteApiAccessMethodConfirmationViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -72,44 +55,18 @@ fun DeleteApiAccessMethodConfirmationDialog(
     onDelete: () -> Unit = {},
     onBack: () -> Unit = {}
 ) {
-    AlertDialog(
-        onDismissRequest = onBack,
-        icon = {
-            Icon(
-                modifier = Modifier.fillMaxWidth().height(Dimens.dialogIconHeight),
-                painter = painterResource(id = R.drawable.icon_alert),
-                contentDescription = stringResource(id = R.string.remove_button),
-                tint = Color.Unspecified
-            )
-        },
-        title = {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text =
-                        stringResource(
-                            id = R.string.delete_method_question,
-                        )
-                )
-                if (state.deleteError != null) {
-                    Text(
-                        text = stringResource(id = R.string.error_occurred),
-                        style = MaterialTheme.typography.bodySmall,
-                        color = MaterialTheme.colorScheme.error,
-                        modifier = Modifier.padding(top = Dimens.smallPadding)
-                    )
-                }
+    DeleteConfirmationDialog(
+        onDelete = onDelete,
+        onBack = onBack,
+        message =
+            stringResource(
+                id = R.string.delete_method_question,
+            ),
+        errorMessage =
+            if (state.deleteError != null) {
+                stringResource(id = R.string.error_occurred)
+            } else {
+                null
             }
-        },
-        dismissButton = {
-            PrimaryButton(
-                modifier = Modifier.focusRequester(FocusRequester()),
-                onClick = onBack,
-                text = stringResource(id = R.string.cancel)
-            )
-        },
-        confirmButton = {
-            NegativeButton(onClick = onDelete, text = stringResource(id = R.string.delete))
-        },
-        containerColor = MaterialTheme.colorScheme.background
     )
 }
---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/DeleteConfirmationDialog.kt
@@ -0,0 +1,90 @@
+package net.mullvad.mullvadvpn.compose.dialog
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.compose.button.NegativeButton
+import net.mullvad.mullvadvpn.compose.button.PrimaryButton
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
+import net.mullvad.mullvadvpn.lib.theme.Dimens
+
+@Preview
+@Composable
+private fun PreviewDeleteConfirmationDialog() {
+    AppTheme {
+        DeleteConfirmationDialog(
+            message = "Do you want to delete Cookie?",
+            errorMessage = null,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewDeleteConfirmationDialogError() {
+    AppTheme {
+        DeleteConfirmationDialog(
+            message = "Do you want to delete Cookie?",
+            errorMessage = "Cookie can not be deleted"
+        )
+    }
+}
+
+@Composable
+fun DeleteConfirmationDialog(
+    message: String,
+    errorMessage: String?,
+    onDelete: () -> Unit = {},
+    onBack: () -> Unit = {}
+) {
+    AlertDialog(
+        onDismissRequest = onBack,
+        icon = {
+            Icon(
+                modifier = Modifier.fillMaxWidth().height(Dimens.dialogIconHeight),
+                painter = painterResource(id = R.drawable.icon_alert),
+                contentDescription = stringResource(id = R.string.remove_button),
+                tint = Color.Unspecified
+            )
+        },
+        title = {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = message)
+                if (errorMessage != null) {
+                    Text(
+                        text = errorMessage,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.padding(top = Dimens.smallPadding)
+                    )
+                }
+            }
+        },
+        dismissButton = {
+            PrimaryButton(
+                modifier = Modifier.focusRequester(FocusRequester()),
+                onClick = onBack,
+                text = stringResource(id = R.string.cancel)
+            )
+        },
+        confirmButton = {
+            NegativeButton(onClick = onDelete, text = stringResource(id = R.string.delete))
+        },
+        containerColor = MaterialTheme.colorScheme.background
+    )
+}
