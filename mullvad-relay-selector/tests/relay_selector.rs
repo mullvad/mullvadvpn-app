24f0a64b5fe3c0742ae4c990dcdaaecf2c9fea69
David Lönnhager <david.l@mullvad.net>
2024-06-10 11:49:22 +0200

    Add Shadowsocks to retry order
    Co-authored-by: Sebastian Holmin <sebastian.holmin@mullvad.net>

--- mullvad-relay-selector/tests/relay_selector.rs
+++ mullvad-relay-selector/tests/relay_selector.rs
@@ -251,14 +251,16 @@ fn assert_retry_order() {
             .port(443)
             .build(),
         // 5
-        RelayQueryBuilder::new().wireguard().udp2tcp().build(),
+        RelayQueryBuilder::new().wireguard().shadowsocks().build(),
         // 6
+        RelayQueryBuilder::new().wireguard().udp2tcp().build(),
+        // 7
         RelayQueryBuilder::new()
             .wireguard()
             .udp2tcp()
             .ip_version(IpVersion::V6)
             .build(),
-        // 7
+        // 8
         RelayQueryBuilder::new()
             .openvpn()
             .transport_protocol(TransportProtocol::Tcp)
