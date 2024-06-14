42c63d707450ccebb04454c8e99b8634be19f979
David Göransson <david.goransson@mullvad.net>
2024-06-14 15:00:32 +0200

    Add UseCase tests

---
+++ android/test/arch/src/test/kotlin/net/mullvad/mullvadvpn/test/arch/UseCaseTest.kt
@@ -0,0 +1,29 @@
+package net.mullvad.mullvadvpn.test.arch
+
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.functions
+import com.lemonappdev.konsist.api.ext.list.modifierprovider.withPublicOrDefaultModifier
+import com.lemonappdev.konsist.api.verify.assertTrue
+import org.junit.jupiter.api.Test
+
+class UseCaseTest {
+    @Test
+    fun `ensure all use cases end with UseCase suffix`() =
+        allUseCasesFiles().assertTrue { it.name.endsWith("UseCase") }
+
+    @Test
+    fun `ensure every use cases is public`() =
+        allUseCases().assertTrue { it.hasPublicOrDefaultModifier }
+
+    @Test
+    fun `ensure every public functions method is named 'invoke' with operator modifier`() =
+        allUseCases().functions().withPublicOrDefaultModifier().assertTrue {
+            it.name == "invoke" && it.hasOperatorModifier
+        }
+
+    private fun allUseCasesFiles() =
+        Konsist.scopeFromProduction().files.filter { it.resideInPath("..usecase..") }
+
+    private fun allUseCases() =
+        Konsist.scopeFromProduction().classes().filter { it.resideInPackage("..usecase..") }
+}
