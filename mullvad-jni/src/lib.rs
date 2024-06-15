7b25da21883878abfc88210cdfd74b4a5034949a
David Lönnhager <david.l@mullvad.net>
2024-06-15 15:05:06 +0200

    Move API override code to own module

--- mullvad-jni/src/lib.rs
+++ mullvad-jni/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg(target_os = "android")]
 
+mod api;
 mod classes;
 mod is_null;
 mod problem_report;
@@ -20,9 +21,6 @@ use std::{
     sync::{mpsc, Arc, Once},
 };
 use talpid_types::{android::AndroidContext, ErrorExt};
-
-#[cfg(feature = "api-override")]
-use std::net::{IpAddr, SocketAddr};
 
 const LOG_FILENAME: &str = "daemon.log";
 
@@ -132,7 +130,7 @@ fn start(
 
     #[cfg(feature = "api-override")]
     if !api_endpoint.is_null() {
-        let api_endpoint = api_endpoint_from_java(&env, apiEndpoint);
+        let api_endpoint = api::api_endpoint_from_java(&env, api_endpoint);
         log::debug!("Overriding API endpoint: {api_endpoint:?}");
         if mullvad_api::API.override_init(api_endpoint).is_err() {
             log::warn!("Ignoring API settings (already initialized)");
@@ -244,96 +242,6 @@ fn initialize_logging(log_dir: &Path) -> Result<(), String> {
     log_panics::init();
 
     Ok(())
-}
-
-#[cfg(feature = "api-override")]
-fn api_endpoint_from_java(env: &JnixEnv<'_>, object: JObject<'_>) -> mullvad_api::ApiEndpoint {
-    let mut endpoint = mullvad_api::ApiEndpoint::from_env_vars();
-
-    let address = env
-        .call_method(object, "component1", "()Ljava/net/InetSocketAddress;", &[])
-        .expect("missing ApiEndpoint.address")
-        .l()
-        .expect("ApiEndpoint.address is not an InetSocketAddress");
-
-    endpoint.address = Some(
-        try_socketaddr_from_java(env, address).expect("received unresolved InetSocketAddress"),
-    );
-    endpoint.host = try_hostname_from_java(env, address);
-    endpoint.disable_address_cache = env
-        .call_method(object, "component2", "()Z", &[])
-        .expect("missing ApiEndpoint.disableAddressCache")
-        .z()
-        .expect("ApiEndpoint.disableAddressCache is not a bool");
-    endpoint.disable_tls = env
-        .call_method(object, "component3", "()Z", &[])
-        .expect("missing ApiEndpoint.disableTls")
-        .z()
-        .expect("ApiEndpoint.disableTls is not a bool");
-
-    endpoint
-}
-
-/// Converts InetSocketAddress to a SocketAddr. Return `None` if the
-/// hostname is unresolved.
-#[cfg(feature = "api-override")]
-fn try_socketaddr_from_java(env: &JnixEnv<'_>, address: JObject<'_>) -> Option<SocketAddr> {
-    let class = env.get_class("java/net/InetSocketAddress");
-
-    let method_id = env
-        .get_method_id(&class, "getAddress", "()Ljava/net/InetAddress;")
-        .expect("Failed to get method ID for InetSocketAddress.getAddress()");
-    let return_type = JavaType::Object("java/net/InetAddress".to_owned());
-
-    let ip_addr = env
-        .call_method_unchecked(address, method_id, return_type, &[])
-        .expect("Failed to call InetSocketAddress.getAddress()")
-        .l()
-        .expect("Call to InetSocketAddress.getAddress() did not return an object");
-
-    if ip_addr.is_null() {
-        return None;
-    }
-
-    let method_id = env
-        .get_method_id(&class, "getPort", "()I")
-        .expect("Failed to get method ID for InetSocketAddress.getPort()");
-    let return_type = JavaType::Primitive(Primitive::Int);
-
-    let port = env
-        .call_method_unchecked(address, method_id, return_type, &[])
-        .expect("Failed to call InetSocketAddress.getPort()")
-        .i()
-        .expect("Call to InetSocketAddress.getPort() did not return an int");
-
-    Some(SocketAddr::new(
-        IpAddr::from_java(env, ip_addr),
-        u16::try_from(port).expect("invalid port"),
-    ))
-}
-
-/// Returns the hostname for an InetSocketAddress. This may be an IP address converted to
-/// a string.
-#[cfg(feature = "api-override")]
-fn try_hostname_from_java(env: &JnixEnv<'_>, address: JObject<'_>) -> Option<String> {
-    let class = env.get_class("java/net/InetSocketAddress");
-
-    let method_id = env
-        .get_method_id(&class, "getHostString", "()Ljava/lang/String;")
-        .expect("Failed to get method ID for InetSocketAddress.getHostString()");
-    let return_type = JavaType::Object("java/lang/String".to_owned());
-
-    let hostname = env
-        .call_method_unchecked(address, method_id, return_type, &[])
-        .expect("Failed to call InetSocketAddress.getHostString()")
-        .l()
-        .expect("Call to InetSocketAddress.getHostString() did not return an object");
-
-    if hostname.is_null() {
-        return None;
-    }
-
-    Some(String::from_java(env, hostname))
 }
 
 fn create_android_context(
