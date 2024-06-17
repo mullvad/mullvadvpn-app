69b47670f36cb58f656c7b6679dfc218208fd8d7
Albin <albin@mullvad.net>
2024-06-17 15:21:10 +0200

    Use temp dirs in lockfile script

--- android/scripts/update-lockfile.sh
+++ android/scripts/update-lockfile.sh
@@ -5,6 +5,18 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
+GRADLE_TASKS=("assemble" "compileDebugUnitTestKotlin" "assembleAndroidTest" "lint")
+
+TEMP_GRADLE_HOME_DIR=$(mktemp -d)
+TEMP_GRADLE_PROJECT_CACHE_DIR=$(mktemp -d)
+
+function cleanup {
+    echo "Cleaning up temp dirs..."
+    rm -rf -- "$TEMP_GRADLE_HOME_DIR" "$TEMP_GRADLE_PROJECT_CACHE_DIR"
+}
+
+trap cleanup EXIT
+
 echo "### Updating dependency lockfile ###"
 echo ""
 
@@ -12,4 +24,4 @@ echo "Removing old components..."
 sed -i '/<components>/,/<\/components>/d' ../gradle/verification-metadata.xml
 
 echo "Generating new components..."
-../gradlew -q -p .. -M sha256 assemble compileDebugUnitTestKotlin assembleAndroidTest lint
+../gradlew -q -p .. -g "$TEMP_GRADLE_HOME_DIR" --project-cache-dir "$TEMP_GRADLE_PROJECT_CACHE_DIR" -M sha256 "${GRADLE_TASKS[@]}"
