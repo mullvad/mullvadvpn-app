b0bdc4203b88a53f245702c57c03d6604851fbb2
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-06-18 17:53:14 +0200

    Show disconnect button when connected in welcome screen

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/WelcomeViewModel.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/WelcomeViewModel.kt
@@ -28,7 +28,7 @@ class WelcomeViewModel(
     private val accountRepository: AccountRepository,
     deviceRepository: DeviceRepository,
     private val paymentUseCase: PaymentUseCase,
-    connectionProxy: ConnectionProxy,
+    private val connectionProxy: ConnectionProxy,
     private val pollAccountExpiry: Boolean = true,
     private val isPlayBuild: Boolean
 ) : ViewModel() {
@@ -76,6 +76,10 @@ class WelcomeViewModel(
                 _uiSideEffect.send(UiSideEffect.OpenAccountView(token))
             }
         }
+    }
+
+    fun onDisconnectClick() {
+        viewModelScope.launch { connectionProxy.disconnect() }
     }
 
     private fun verifyPurchases() {
