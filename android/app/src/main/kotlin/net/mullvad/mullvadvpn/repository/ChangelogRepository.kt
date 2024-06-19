116e3aa5334e1ed006d5bfc566df7061a68ecd51
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-06-19 23:38:07 +0200

    Parse changelog with bullet point list instead of just newlines

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/ChangelogRepository.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/ChangelogRepository.kt
@@ -2,9 +2,12 @@ package net.mullvad.mullvadvpn.repository
 
 import android.content.SharedPreferences
 import net.mullvad.mullvadvpn.util.IChangelogDataProvider
+import net.mullvad.mullvadvpn.util.trimAll
 
 private const val MISSING_VERSION_CODE = -1
 private const val NEWLINE_CHAR = '\n'
+private const val SPACE_STRING = " "
+private const val BULLET_POINT_CHAR = '-'
 private const val LAST_SHOWED_CHANGELOG_VERSION_CODE = "last_showed_changelog_version_code"
 
 class ChangelogRepository(
@@ -18,7 +21,10 @@ class ChangelogRepository(
     fun setVersionCodeOfMostRecentChangelogShowed(versionCode: Int) =
         preferences.edit().putInt(LAST_SHOWED_CHANGELOG_VERSION_CODE, versionCode).apply()
 
-    fun getLastVersionChanges(): List<String> {
-        return dataProvider.getChangelog().split(NEWLINE_CHAR).filter { it.isNotEmpty() }
-    }
+    fun getLastVersionChanges(): List<String> =
+        dataProvider
+            .getChangelog()
+            .split(BULLET_POINT_CHAR)
+            .map { it.split(NEWLINE_CHAR).trimAll().joinToString(SPACE_STRING).trim() }
+            .filter { it.isNotEmpty() }
 }
