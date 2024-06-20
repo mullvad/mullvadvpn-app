100b12ef4862d0cc04dd441ba47afa1b2a0192c5
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-06-20 13:54:48 +0200

    Fix incorrect path to go runtime patch

--- android/fdroid-build/init.sh
+++ android/fdroid-build/init.sh
@@ -27,7 +27,7 @@ cd "$HOME"
 curl -sf -L -o go.tgz https://go.dev/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz
 echo "$GOLANG_HASH go.tgz" | sha256sum -c
 tar -xzvf go.tgz
-patch -p1 -f -N -r- -d "$HOME/go" < "$REPO_DIR/wireguard/libwg/goruntime-boottime-over-monotonic.diff"
+patch -p1 -f -N -r- -d "$HOME/go" < "$REPO_DIR/android/docker/goruntime-boottime-over-monotonic.diff"
 
 # Configure Cargo for cross-compilation
 sed -e "s|{NDK_PATH}|$NDK_PATH|g" "$SCRIPT_DIR/cargo-config.toml.template" > "$HOME/.cargo/config"
