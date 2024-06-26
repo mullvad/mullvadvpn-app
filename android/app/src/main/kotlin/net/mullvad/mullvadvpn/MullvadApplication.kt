a4fbed079435932febfb3314918b1d2c2744fc7c
Albin <albin@mullvad.net>
2024-06-26 10:33:37 +0200

    Remove outdated multi-process comment

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/MullvadApplication.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/MullvadApplication.kt
@@ -10,10 +10,6 @@ import org.koin.core.context.startKoin
 
 private const val LOG_TAG = "mullvad"
 
-/**
- * In Android, separate instances of the application class (MullvadApplication) will be instantiated
- * for each process. That also means that a only common logic should be placed here.
- */
 class MullvadApplication : Application() {
     override fun onCreate() {
         super.onCreate()
