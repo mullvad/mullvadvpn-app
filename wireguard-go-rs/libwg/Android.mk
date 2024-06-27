5799fd5791c8fc1d82943aaa368074be98e88c13
Albin <albin@mullvad.net>
2024-06-27 10:54:38 +0200

    wip fix makefile

--- wireguard-go-rs/libwg/Android.mk
+++ wireguard-go-rs/libwg/Android.mk
@@ -27,11 +27,13 @@ GOBUILDVERSION := 1.21.3
 GOBUILDTARBALL := https://go.dev/dl/go$(GOBUILDVERSION).$(GOBUILDOS)-$(GOBUILDARCH).tar.gz
 GOBUILDVERSION_NEEDED := go version go$(GOBUILDVERSION) $(GOBUILDOS)/$(GOBUILDARCH)
 
+
 $(DESTDIR)/libwg.so:
 	mkdir -p $(DESTDIR)
 	go get -tags "linux android"
 	chmod -fR +w "$(GOPATH)/pkg/mod"
-	go build -tags "linux android" -ldflags="-X main.socketDirectory=/data/data/$(ANDROID_PACKAGE_NAME)/cache/wireguard" -v -o "$@" -buildmode c-shared
+	make --directory wireguard-go libmaybenot.a LIBDEST=$(DESTDIR)
+	go build -tags "linux android daita" -ldflags="-X main.socketDirectory=/data/data/$(ANDROID_PACKAGE_NAME)/cache/wireguard" -v -o "$@" -buildmode c-shared
 	rm -f $(DESTDIR)/libwg.h
 
 
