c09fbae84740e683deadb864055dfffa1444f005
Max Zettlmeißl <max@zettlmeissl.de>
2024-07-20 06:20:25 +0200

    Only accept an IPv4 as valid first argument
    While `request_ephemeral_peer` accepts a generic `IpAddr`, the
    function `new_client` used within forces an `Ipv4Addr`.
    
    In the long run it would be best to change the library to support IPv6
    (provided that the tunnel configuration service listens to an IPv6) or
    to change the signature of `request_ephemeral_peer` to expect an IPv4.

--- talpid-tunnel-config-client/examples/psk-exchange.rs
+++ talpid-tunnel-config-client/examples/psk-exchange.rs
@@ -4,13 +4,13 @@
 // Usage: ./psk-exchange <tuncf_server_ip> <wireguard_public_key>
 // e. g. ./psk-exchange 10.64.0.1 NkECLsf+VbZUjve7RVN6sE3NYUcYUmUn8qpFugqbXFk=
 
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr};
 use talpid_types::net::wireguard::{PrivateKey, PublicKey};
 
 #[tokio::main]
 async fn main() {
     let mut args = std::env::args().skip(1);
-    let tuncfg_server_ip: IpAddr = args
+    let tuncfg_server_ip: Ipv4Addr = args
         .next()
         .expect("Give tuncfg server IP as first argument")
         .parse()
@@ -24,7 +24,7 @@ async fn main() {
     let ephemeral_private_key = PrivateKey::new_from_random();
 
     let ephemeral_peer = talpid_tunnel_config_client::request_ephemeral_peer(
-        tuncfg_server_ip,
+        IpAddr::V4(tuncfg_server_ip),
         public_key, // Parent connection's public key.
         ephemeral_private_key.public_key(),
         true,  // Whether to negotiate a "PQ-safe" PSK.
