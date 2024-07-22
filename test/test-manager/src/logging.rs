cd82c2f151f83a08461522e3411998a3724ed83b
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-07-22 15:33:38 +0200

    Filter `tarpc` logs at warn level

--- test/test-manager/src/logging.rs
+++ test/test-manager/src/logging.rs
@@ -30,6 +30,7 @@ impl Logger {
             logger.filter_module("tower", log::LevelFilter::Info);
             logger.filter_module("hyper", log::LevelFilter::Info);
             logger.filter_module("rustls", log::LevelFilter::Info);
+            logger.filter_module("tarpc", log::LevelFilter::Warn);
             logger.filter_level(log::LevelFilter::Info);
             logger.parse_env(env_logger::DEFAULT_FILTER_ENV);
 
