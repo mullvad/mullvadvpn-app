124e9c597186b09a207406c3785a51235da083af
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-07-23 01:14:47 +0200

    Split `ci-runtest.sh` into two scripts
    Move scripts that are not intented to be ran
    directly by the user into a `scripts` directory.
    
    Also refactor and improve the scripts.

--- test/scripts/container-run.sh
+++ test/scripts/container-run.sh
@@ -11,9 +11,10 @@ if [ ! -d "$PACKAGE_FOLDER" ]; then
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPO_DIR="$SCRIPT_DIR/.."
+REPO_DIR="$SCRIPT_DIR/../.."
 cd "$SCRIPT_DIR"
 
+# shellcheck disable=SC1091
 source "$REPO_DIR/scripts/utils/log"
 
 if [[ "$(uname -s)" != "Linux" ]]; then
@@ -24,7 +25,6 @@ fi
 container_image=$(cat "$REPO_DIR/building/linux-container-image.txt")
 podman build -t mullvadvpn-app-tests --build-arg IMAGE="${container_image}" .
 
-set -x
 exec "$CONTAINER_RUNNER" run --rm -it \
     -v "${CARGO_REGISTRY_VOLUME_NAME}":/root/.cargo/registry:Z \
     -v "${REPO_DIR}":/build:z \
