c813da262686da9f1a8bd1f293cc61dd74f128af
David Göransson <david.goransson@mullvad.net>
2024-07-24 16:07:42 +0200

    Refactor usecase

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/usecase/customlists/FilterCustomListsRelayItemUseCase.kt
@@ -0,0 +1,30 @@
+package net.mullvad.mullvadvpn.usecase.customlists
+
+import kotlinx.coroutines.flow.combine
+import net.mullvad.mullvadvpn.lib.model.Constraint
+import net.mullvad.mullvadvpn.lib.model.Ownership
+import net.mullvad.mullvadvpn.lib.model.Providers
+import net.mullvad.mullvadvpn.lib.model.RelayItem
+import net.mullvad.mullvadvpn.relaylist.filterOnOwnershipAndProvider
+import net.mullvad.mullvadvpn.repository.RelayListFilterRepository
+import kotlin.collections.mapNotNull
+
+class FilterCustomListsRelayItemUseCase(
+    private val customListsRelayItemUseCase: CustomListsRelayItemUseCase,
+    private val relayListFilterRepository: RelayListFilterRepository
+) {
+
+    operator fun invoke() =
+        combine(
+            customListsRelayItemUseCase(),
+            relayListFilterRepository.selectedOwnership,
+            relayListFilterRepository.selectedProviders,
+        ) { customLists, selectedOwnership, selectedProviders ->
+            customLists.filterOnOwnershipAndProvider(selectedOwnership, selectedProviders)
+        }
+
+    private fun List<RelayItem.CustomList>.filterOnOwnershipAndProvider(
+        ownership: Constraint<Ownership>,
+        providers: Constraint<Providers>
+    ) = mapNotNull { it.filterOnOwnershipAndProvider(ownership, providers) }
+}
