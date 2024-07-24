2b3d64c06329ec4a772f5249245a62bf4d9112f8
David Göransson <david.goransson@mullvad.net>
2024-07-24 13:32:17 +0200

    Fixes

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/cell/FilterCell.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/cell/FilterCell.kt
@@ -23,7 +23,10 @@ import net.mullvad.mullvadvpn.lib.theme.Dimens
 @Preview
 @Composable
 private fun PreviewFilterCell() {
-    AppTheme { FilterRow(emptyList(), {}, {}) }
+    AppTheme {
+        FilterRow(
+            listOf(FilterChip.Ownership(Ownership.MullvadOwned), FilterChip.Provider(2)), {}, {})
+    }
 }
 
 @Composable
@@ -41,37 +44,32 @@ fun FilterRow(
                     horizontal = Dimens.searchFieldHorizontalPadding,
                 )
                 .fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(Dimens.chipSpace)
-    ) {
-        Text(
-            text = stringResource(id = R.string.filtered),
-            color = MaterialTheme.colorScheme.onPrimary,
-            style = MaterialTheme.typography.labelMedium
-        )
-        filters.forEach {
-            when (it) {
-                is FilterChip.Ownership ->
-                    OwnershipFilterChip(it.ownership, onRemoveOwnershipFilter)
-                is FilterChip.Provider -> ProviderFilterChip(it.count, onRemoveProviderFilter)
+        horizontalArrangement = Arrangement.spacedBy(Dimens.chipSpace)) {
+            Text(
+                text = stringResource(id = R.string.filtered),
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.labelMedium)
+            filters.forEach {
+                when (it) {
+                    is FilterChip.Ownership ->
+                        OwnershipFilterChip(it.ownership, onRemoveOwnershipFilter)
+                    is FilterChip.Provider -> ProviderFilterChip(it.count, onRemoveProviderFilter)
+                }
             }
         }
-    }
 }
 
 @Composable
 fun ProviderFilterChip(providers: Int, onRemoveClick: () -> Unit) {
     MullvadFilterChip(
         text = stringResource(id = R.string.number_of_providers, providers),
-        onRemoveClick = onRemoveClick
-    )
+        onRemoveClick = onRemoveClick)
 }
 
 @Composable
 fun OwnershipFilterChip(ownership: Ownership, onRemoveClick: () -> Unit) {
     MullvadFilterChip(
-        text = stringResource(ownership.stringResources()),
-        onRemoveClick = onRemoveClick
-    )
+        text = stringResource(ownership.stringResources()), onRemoveClick = onRemoveClick)
 }
 
 private fun Ownership.stringResources(): Int =
