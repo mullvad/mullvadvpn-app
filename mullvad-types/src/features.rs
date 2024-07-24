69f0089661eafb09b821a96db060a8a8e837642a
Markus Pettersson <markus.pettersson@mullvad.net>
2024-07-24 09:10:37 +0200

    Add documentation

--- mullvad-types/src/features.rs
+++ mullvad-types/src/features.rs
@@ -2,11 +2,17 @@ use std::collections::HashSet;
 
 use serde::{Deserialize, Serialize};
 
+/// Feature indicators are active settings that should be shown to the user to make them aware of
+/// what is affecting their connection at any given time.
+///
+/// Note that the feature indicators are not ordered.
 #[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct FeatureIndicators {
     pub active_features: HashSet<FeatureIndicator>,
 }
 
+/// All possible feature indicators. These represent a subset of all VPN settings in a
+/// non-technical fashion.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum FeatureIndicator {
     QuantumResistance,
