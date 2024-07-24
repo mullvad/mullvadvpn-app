dd431a03023f6eb60920f1ca9bc520c2ca590b41
David Göransson <david.goransson@mullvad.net>
2024-07-24 16:07:42 +0200

    Refactor usecase

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/usecase/customlists/CustomListsRelayItemUseCase.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/usecase/customlists/CustomListsRelayItemUseCase.kt
@@ -1,14 +1,8 @@
 package net.mullvad.mullvadvpn.usecase.customlists
 
 import kotlinx.coroutines.flow.combine
-import net.mullvad.mullvadvpn.lib.model.Constraint
-import net.mullvad.mullvadvpn.lib.model.Ownership
-import net.mullvad.mullvadvpn.lib.model.Providers
-import net.mullvad.mullvadvpn.lib.model.RelayItem
-import net.mullvad.mullvadvpn.relaylist.filterOnOwnershipAndProvider
 import net.mullvad.mullvadvpn.relaylist.toRelayItemCustomList
 import net.mullvad.mullvadvpn.repository.CustomListsRepository
-import net.mullvad.mullvadvpn.repository.RelayListFilterRepository
 import net.mullvad.mullvadvpn.repository.RelayListRepository
 
 class CustomListsRelayItemUseCase(
@@ -22,24 +16,4 @@ class CustomListsRelayItemUseCase(
             relayList ->
             customLists?.map { it.toRelayItemCustomList(relayList) } ?: emptyList()
         }
-}
-
-class FilterCustomListsRelayItemUseCase(
-    private val customListsRelayItemUseCase: CustomListsRelayItemUseCase,
-    private val relayListFilterRepository: RelayListFilterRepository
-) {
-
-    operator fun invoke() =
-        combine(
-            customListsRelayItemUseCase(),
-            relayListFilterRepository.selectedOwnership,
-            relayListFilterRepository.selectedProviders,
-        ) { customLists, selectedOwnership, selectedProviders ->
-            customLists.filterOnOwnershipAndProvider(selectedOwnership, selectedProviders)
-        }
-
-    private fun List<RelayItem.CustomList>.filterOnOwnershipAndProvider(
-        ownership: Constraint<Ownership>,
-        providers: Constraint<Providers>
-    ) = mapNotNull { it.filterOnOwnershipAndProvider(ownership, providers) }
 }
