be88f779b8f4995dd8934a7fe7745193caf3c28a
David Göransson <david.goransson@mullvad.net>
2024-07-26 12:07:21 +0200

    Update depedencies

--- android/buildSrc/src/main/kotlin/Versions.kt
+++ android/buildSrc/src/main/kotlin/Versions.kt
@@ -1,39 +1,39 @@
 object Versions {
-    const val commonsValidator = "1.8.0"
+    const val commonsValidator = "1.9.0"
     const val jodaTime = "2.12.7"
     const val junit = "5.10.2"
     const val jvmTarget = "17"
     const val kermit = "2.0.4"
     const val konsist = "0.15.1"
     const val kotlin = "2.0.0"
-    const val kotlinx = "1.8.0"
+    const val kotlinx = "1.8.1"
     const val leakCanary = "2.13"
-    const val mockk = "1.13.11"
+    const val mockk = "1.13.12"
     const val mockWebserver = "4.12.0"
-    const val turbine = "1.0.0"
+    const val turbine = "1.1.0"
     const val billingClient = "6.2.0"
 
     object Android {
         const val compileSdkVersion = 34
-        const val junit = "1.4.0"
+        const val junit = "1.5.0"
         const val minSdkVersion = 26
         const val targetSdkVersion = 34
         const val volley = "1.2.1"
     }
 
     object AndroidX {
-        const val activityCompose = "1.9.0"
+        const val activityCompose = "1.9.1"
         const val appcompat = "1.7.0"
         const val coreKtx = "1.13.1"
         const val coreSplashscreen = "1.1.0-rc01"
-        const val espresso = "3.5.1"
-        const val lifecycle = "2.8.3"
-        const val fragmentTesting = "1.6.2"
-        const val test = "1.5.0"
-        const val testMonitor = "1.6.1"
-        const val testOrchestrator = "1.4.2"
-        const val testRunner = "1.5.2"
-        const val uiautomator = "2.3.0"
+        const val espresso = "3.6.1"
+        const val lifecycle = "2.8.4"
+        const val fragmentTesting = "1.8.2"
+        const val test = "1.6.1"
+        const val testMonitor = "1.7.1"
+        const val testOrchestrator = "1.5.0"
+        const val testRunner = "1.6.1"
+        const val uiautomator = "2.4.0-alpha01"
     }
 
     object Arrow {
@@ -41,7 +41,7 @@ object Versions {
     }
 
     object Compose {
-        const val base = "1.7.0-beta05"
+        const val base = "1.7.0-beta06"
         const val destinations = "2.1.0-beta10"
         const val constrainLayout = "1.0.1"
         const val foundation = base
@@ -65,7 +65,7 @@ object Versions {
         const val dependencyCheck = "10.0.3"
         const val detekt = "1.23.6"
         const val gradleVersions = "0.51.0"
-        const val junit5 = "1.10.0.0"
+        const val junit5 = "1.10.2.0"
         const val ktfmt = "0.17.0"
         // Ksp version is linked with kotlin version, find matching release here:
         // https://github.com/google/ksp/releases
