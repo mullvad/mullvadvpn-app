1fde31986deb4e3934b1b5799514f0c2dd9375c8
David Göransson <david.goransson@mullvad.net>
2024-07-30 15:01:56 +0200

    Fix run test locally script

--- android/scripts/run-instrumented-tests-locally.sh
+++ android/scripts/run-instrumented-tests-locally.sh
@@ -7,4 +7,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"/..
 ./gradlew assembleOssProdAndroidTest
 ./gradlew app:assembleOssProdDebug
-"$SCRIPT_DIR"/run-instrumented-tests.sh app
+export REPORT_DIR=$(mktemp -d)
+echo REPORTDIR
+echo $REPORT_DIR
+"$SCRIPT_DIR"/run-instrumented-tests.sh --test-type app --infra-flavor prod --billing-flavor oss
+
