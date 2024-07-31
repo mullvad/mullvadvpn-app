4fec19f1c4d60e2a5f638ddd2e30e94802338537
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-07-31 15:18:10 +0200

    Fix unit tests

--- android/app/src/test/kotlin/net/mullvad/mullvadvpn/viewmodel/DeviceRevokedViewModelTest.kt
+++ android/app/src/test/kotlin/net/mullvad/mullvadvpn/viewmodel/DeviceRevokedViewModelTest.kt
@@ -1,14 +1,13 @@
 package net.mullvad.mullvadvpn.viewmodel
 
 import app.cash.turbine.test
+import arrow.core.right
 import io.mockk.MockKAnnotations
-import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.coVerifyOrder
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.just
 import io.mockk.mockk
 import io.mockk.unmockkAll
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -70,7 +69,7 @@ class DeviceRevokedViewModelTest {
     @Test
     fun `onGoToLoginClicked should invoke logout on AccountRepository`() {
         // Arrange
-        coEvery { mockConnectionProxy.disconnect() } returns true
+        coEvery { mockConnectionProxy.disconnect() } returns true.right()
         coEvery { mockedAccountRepository.logout() } just Runs
 
         // Act
@@ -83,7 +82,7 @@ class DeviceRevokedViewModelTest {
     @Test
     fun `onGoToLoginClicked should invoke disconnect before logout when connected`() {
         // Arrange
-        coEvery { mockConnectionProxy.disconnect() } returns true
+        coEvery { mockConnectionProxy.disconnect() } returns true.right()
         coEvery { mockedAccountRepository.logout() } just Runs
 
         // Act
