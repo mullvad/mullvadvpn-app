05a9dfd1d5654e614e2f5fe1f7473f1d2e0e4b06
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-08-05 13:49:16 +0200

    Remove `must_succede` test macro parameter
    This simplifes handling of test results.

--- test/test-manager/test_macro/src/lib.rs
+++ test/test-manager/test_macro/src/lib.rs
@@ -23,9 +23,6 @@ use test_rpc::meta::Os;
 /// * `priority` - The order in which tests will be run where low numbers run before high numbers
 ///   and tests with the same number run in undefined order. `priority` defaults to 0.
 ///
-/// * `must_succeed` - If the testing suite stops running if this test fails. `must_succeed`
-///   defaults to false.
-///
 /// * `always_run` - If the test should always run regardless of what test filters are provided by
 ///   the user. `always_run` defaults to false.
 ///
@@ -51,10 +48,10 @@ use test_rpc::meta::Os;
 ///
 /// ## Create a test with custom parameters
 ///
-/// This test will run early in the test loop and must succeed and will always run.
+/// This test will run early in the test loop and will always run.
 ///
 /// ```ignore
-/// #[test_function(priority = -1337, must_succeed = true, always_run = true)]
+/// #[test_function(priority = -1337, always_run = true)]
 /// pub async fn test_function(
 ///     rpc: ServiceClient,
 ///     mut mullvad_client: mullvad_management_interface::MullvadProxyClient,
@@ -109,7 +106,6 @@ fn parse_marked_test_function(
 fn get_test_macro_parameters(attributes: &syn::AttributeArgs) -> Result<MacroParameters> {
     let mut priority = None;
     let mut always_run = false;
-    let mut must_succeed = false;
     let mut targets = vec![];
 
     for attribute in attributes {
@@ -128,11 +124,6 @@ fn get_test_macro_parameters(attributes: &syn::AttributeArgs) -> Result<MacroPar
             match lit {
                 Lit::Bool(lit_bool) => always_run = lit_bool.value(),
                 _ => bail!(nv, "'always_run' should have a bool value"),
-            }
-        } else if nv.path.is_ident("must_succeed") {
-            match lit {
-                Lit::Bool(lit_bool) => must_succeed = lit_bool.value(),
-                _ => bail!(nv, "'must_succeed' should have a bool value"),
             }
         } else if nv.path.is_ident("target_os") {
             let Lit::Str(lit_str) = lit else {
@@ -157,7 +148,6 @@ fn get_test_macro_parameters(attributes: &syn::AttributeArgs) -> Result<MacroPar
     Ok(MacroParameters {
         priority,
         always_run,
-        must_succeed,
         targets,
     })
 }
@@ -176,7 +166,6 @@ fn create_test(test_function: TestFunction) -> proc_macro2::TokenStream {
         .collect();
 
     let always_run = test_function.macro_parameters.always_run;
-    let must_succeed = test_function.macro_parameters.must_succeed;
 
     let func_name = test_function.name;
     let function_mullvad_version = test_function.function_parameters.mullvad_client.version();
@@ -219,7 +208,6 @@ fn create_test(test_function: TestFunction) -> proc_macro2::TokenStream {
             func: #wrapper_closure,
             priority: #test_function_priority,
             always_run: #always_run,
-            must_succeed: #must_succeed,
         });
     }
 }
@@ -233,7 +221,6 @@ struct TestFunction {
 struct MacroParameters {
     priority: Option<i32>,
     always_run: bool,
-    must_succeed: bool,
     targets: Vec<Os>,
 }
 
