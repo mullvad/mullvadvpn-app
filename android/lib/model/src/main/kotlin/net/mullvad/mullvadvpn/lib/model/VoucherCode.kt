d2f2463e9db65fc5fed52c90c1661f50fafc7e24
David Göransson <david.goransson@mullvad.net>
2024-08-06 09:28:37 +0200

    Add voucher code domain model

---
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/VoucherCode.kt
@@ -0,0 +1,30 @@
+package net.mullvad.mullvadvpn.lib.model
+
+import arrow.core.Either
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+
+@JvmInline
+value class VoucherCode private constructor(val value: String) {
+
+    companion object {
+        // Parsing reference: docs/adr/0018-distinguish-voucher-codes-from-account-numbers.md
+        fun fromString(value: String): Either<ParseVoucherCodeError, VoucherCode> = either {
+            val trimmedValue = value.trim()
+            ensure(trimmedValue.length >= MIN_VOUCHER_LENGTH) {
+                ParseVoucherCodeError.TooShort(trimmedValue)
+            }
+            ensure(!value.all { it.isDigit() }) { ParseVoucherCodeError.AllDigit(trimmedValue) }
+            VoucherCode(trimmedValue)
+        }
+
+        const val MIN_VOUCHER_LENGTH = 16
+    }
+}
+
+sealed interface ParseVoucherCodeError {
+
+    data class AllDigit(val value: String) : ParseVoucherCodeError
+
+    data class TooShort(val value: String) : ParseVoucherCodeError
+}
