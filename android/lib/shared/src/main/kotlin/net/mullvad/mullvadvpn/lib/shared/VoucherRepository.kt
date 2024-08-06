2bc5ce8ef89f6a8276ce0afb8685307f6a5a40b6
David Göransson <david.goransson@mullvad.net>
2024-08-06 09:30:45 +0200

    Integrate VoucherCode and error message to voucher screen

--- android/lib/shared/src/main/kotlin/net/mullvad/mullvadvpn/lib/shared/VoucherRepository.kt
+++ android/lib/shared/src/main/kotlin/net/mullvad/mullvadvpn/lib/shared/VoucherRepository.kt
@@ -1,12 +1,13 @@
 package net.mullvad.mullvadvpn.lib.shared
 
 import net.mullvad.mullvadvpn.lib.daemon.grpc.ManagementService
+import net.mullvad.mullvadvpn.lib.model.VoucherCode
 
 class VoucherRepository(
     private val managementService: ManagementService,
     private val accountRepository: AccountRepository
 ) {
-    suspend fun submitVoucher(voucher: String) =
+    suspend fun submitVoucher(voucher: VoucherCode) =
         managementService.submitVoucher(voucher).onRight {
             accountRepository.onVoucherRedeemed(it.newExpiryDate)
         }
