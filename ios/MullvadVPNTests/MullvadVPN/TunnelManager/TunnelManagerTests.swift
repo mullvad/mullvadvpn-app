aad94894e0a73da7c45f61f10d9bab1f033ea1ea
mojganii <mojgan.jelodar@codic.se>
2024-08-09 13:53:07 +0200

    Fix getting stuck in blocked state after reconnecting

--- ios/MullvadVPNTests/MullvadVPN/TunnelManager/TunnelManagerTests.swift
+++ ios/MullvadVPNTests/MullvadVPN/TunnelManager/TunnelManagerTests.swift
@@ -8,14 +8,25 @@
 import MullvadREST
 
 @testable import MullvadMockData
+@testable import MullvadRESTTests
 @testable import MullvadSettings
 @testable import MullvadTypes
 @testable import WireGuardKitTypes
 
 import XCTest
 
-final class TunnelManagerTests: XCTestCase {
+class TunnelManagerTests: XCTestCase {
     static let store = InMemorySettingsStore<SettingNotFound>()
+    private var tunnelObserver: TunnelObserver!
+
+    var application: UIApplicationStub!
+    var relayCacheTracker: RelayCacheTrackerStub!
+    var accountProxy: AccountsProxyStub!
+    var accessTokenManager: AccessTokenManagerStub!
+    var devicesProxy: DevicesProxyStub!
+    var apiProxy: APIProxyStub!
+
+    var transportProvider: TransportProvider!
 
     override class func setUp() {
         SettingsManager.unitTestStore = store
@@ -25,65 +36,100 @@ final class TunnelManagerTests: XCTestCase {
         SettingsManager.unitTestStore = nil
     }
 
-    func testTunnelManager() {
-        let application = UIApplicationStub()
-        let tunnelStore = TunnelStoreStub()
-        let relayCacheTracker = RelayCacheTrackerStub()
-        let accountProxy = AccountsProxyStub()
-        let devicesProxy = DevicesProxyStub(deviceResult: .success(Device.mock(publicKey: PrivateKey().publicKey)))
-        let apiProxy = APIProxyStub()
-        let accessTokenManager = AccessTokenManagerStub()
-        let relaySelector = RelaySelectorStub.nonFallible()
-        let tunnelManager = TunnelManager(
-            application: application,
-            tunnelStore: tunnelStore,
-            relayCacheTracker: relayCacheTracker,
-            accountsProxy: accountProxy,
-            devicesProxy: devicesProxy,
-            apiProxy: apiProxy,
-            accessTokenManager: accessTokenManager,
-            relaySelector: relaySelector
+    override func setUp() async throws {
+        application = UIApplicationStub()
+        relayCacheTracker = RelayCacheTrackerStub()
+        accountProxy = AccountsProxyStub()
+        accessTokenManager = AccessTokenManagerStub()
+        devicesProxy = DevicesProxyStub(deviceResult: .success(Device.mock(publicKey: PrivateKey().publicKey)))
+        apiProxy = APIProxyStub()
+
+        transportProvider = TransportProvider(
+            urlSessionTransport: URLSessionTransport(urlSession: REST.makeURLSession()),
+            addressCache: REST.AddressCache(
+                canWriteToCache: true,
+                cacheDirectory: FileManager.default.temporaryDirectory
+            ),
+            transportStrategy: TransportStrategy(
+                datasource: AccessMethodRepositoryStub(accessMethods: [PersistentAccessMethod(
+                    id: UUID(),
+                    name: "direct",
+                    isEnabled: true,
+                    proxyConfiguration: .direct
+                )]),
+                shadowsocksLoader: ShadowsocksLoader(
+                    cache: ShadowsocksConfigurationCacheStub(),
+                    relaySelector: ShadowsocksRelaySelectorStub(relays: .mock()),
+                    constraintsUpdater: RelayConstraintsUpdater(),
+                    multihopUpdater: MultihopUpdater(listener: MultihopStateListener())
+                )
+            )
         )
-        XCTAssertNotNil(tunnelManager)
+
+        try SettingsManager.writeSettings(LatestTunnelSettings())
+    }
+
+    override func tearDown() async throws {
+        application = nil
+        relayCacheTracker = nil
+        accountProxy = nil
+        accessTokenManager = nil
+        devicesProxy = nil
+        apiProxy = nil
+        transportProvider = nil
+        tunnelObserver = nil
     }
 
     func testLogInStartsKeyRotations() async throws {
-        let application = UIApplicationStub()
-        let tunnelStore = TunnelStoreStub()
-        let relayCacheTracker = RelayCacheTrackerStub()
-        var accountProxy = AccountsProxyStub()
-        let devicesProxy = DevicesProxyStub(deviceResult: .success(Device.mock(publicKey: PrivateKey().publicKey)))
-        let apiProxy = APIProxyStub()
-        let accessTokenManager = AccessTokenManagerStub()
         accountProxy.createAccountResult = .success(REST.NewAccountData.mockValue())
-        let relaySelector = RelaySelectorStub.nonFallible()
+
         let tunnelManager = TunnelManager(
             application: application,
-            tunnelStore: tunnelStore,
+            tunnelStore: TunnelStoreStub(backgroundTaskProvider: application),
             relayCacheTracker: relayCacheTracker,
             accountsProxy: accountProxy,
             devicesProxy: devicesProxy,
             apiProxy: apiProxy,
             accessTokenManager: accessTokenManager,
-            relaySelector: relaySelector
+            relaySelector: RelaySelectorStub.nonFallible()
         )
+
         _ = try await tunnelManager.setNewAccount()
         XCTAssertEqual(tunnelManager.isRunningPeriodicPrivateKeyRotation, true)
     }
 
     func testLogOutStopsKeyRotations() async throws {
-        let application = UIApplicationStub()
-        let tunnelStore = TunnelStoreStub()
-        let relayCacheTracker = RelayCacheTrackerStub()
-        var accountProxy = AccountsProxyStub()
-        let devicesProxy = DevicesProxyStub(deviceResult: .success(Device.mock(publicKey: PrivateKey().publicKey)))
-        let apiProxy = APIProxyStub()
-        let accessTokenManager = AccessTokenManagerStub()
         accountProxy.createAccountResult = .success(REST.NewAccountData.mockValue())
-        let relaySelector = RelaySelectorStub.nonFallible()
+
         let tunnelManager = TunnelManager(
             application: application,
-            tunnelStore: tunnelStore,
+            tunnelStore: TunnelStoreStub(backgroundTaskProvider: application),
+            relayCacheTracker: relayCacheTracker,
+            accountsProxy: accountProxy,
+            devicesProxy: devicesProxy,
+            apiProxy: apiProxy,
+            accessTokenManager: accessTokenManager,
+            relaySelector: RelaySelectorStub.nonFallible()
+        )
+        _ = try await tunnelManager.setNewAccount()
+        await tunnelManager.unsetAccount()
+        XCTAssertEqual(tunnelManager.isRunningPeriodicPrivateKeyRotation, false)
+    }
+
+    /// This test verifies tunnel gets out of `blockedState` after constraints are satisfied.
+    func testExitBlockedStateAfterSatisfyingConstraints() async throws {
+        let blockedExpectation = expectation(description: "Relay constraints aren't satisfied!")
+        let connectedExpectation = expectation(description: "Connected!")
+
+        accountProxy.createAccountResult = .success(REST.NewAccountData.mockValue())
+
+        let relaySelector = RelaySelectorStub { _, _ in
+            try RelaySelectorStub.unsatisfied().selectRelays(with: RelayConstraints(), connectionAttemptCount: 0)
+        }
+
+        let tunnelManager = TunnelManager(
+            application: application,
+            tunnelStore: TunnelStore(application: application),
             relayCacheTracker: relayCacheTracker,
             accountsProxy: accountProxy,
             devicesProxy: devicesProxy,
@@ -91,8 +137,47 @@ final class TunnelManagerTests: XCTestCase {
             accessTokenManager: accessTokenManager,
             relaySelector: relaySelector
         )
+
+        let simulatorTunnelProviderHost = SimulatorTunnelProviderHost(
+            relaySelector: relaySelector,
+            transportProvider: transportProvider
+        )
+        SimulatorTunnelProvider.shared.delegate = simulatorTunnelProviderHost
+
+        let tunnelObserver = TunnelBlockObserver(
+            didUpdateTunnelStatus: { _, tunnelStatus in
+                switch tunnelStatus.state {
+                case let .error(blockedStateReason) where blockedStateReason == .noRelaysSatisfyingConstraints:
+                    blockedExpectation.fulfill()
+                    relaySelector.selectedRelaysResult = { relayConstraints, connectionAttemptCount in
+                        try RelaySelectorStub.nonFallible().selectRelays(
+                            with: relayConstraints,
+                            connectionAttemptCount: connectionAttemptCount
+                        )
+                    }
+                    tunnelManager.reconnectTunnel(selectNewRelay: true)
+
+                case .connected:
+                    connectedExpectation.fulfill()
+                default:
+                    return
+                }
+            }
+        )
+
+        self.tunnelObserver = tunnelObserver
+        tunnelManager.addObserver(tunnelObserver)
+
         _ = try await tunnelManager.setNewAccount()
-        await tunnelManager.unsetAccount()
-        XCTAssertEqual(tunnelManager.isRunningPeriodicPrivateKeyRotation, false)
+
+        XCTAssertTrue(tunnelManager.deviceState.isLoggedIn)
+
+        tunnelManager.startTunnel()
+
+        await fulfillment(
+            of: [blockedExpectation, connectedExpectation],
+            timeout: .UnitTest.timeout,
+            enforceOrder: true
+        )
     }
 }
