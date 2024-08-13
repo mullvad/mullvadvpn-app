ce5b989473dafef31adf51105041381227ab0ff3
David Lönnhager <david.l@mullvad.net>
2024-08-13 16:25:23 +0200

    Build `libpcap` without dbus

--- test/test-manager/build.rs
+++ test/test-manager/build.rs
@@ -1,4 +1,7 @@
 fn main() {
     // Rebuild if SSH provision script changes
     println!("cargo::rerun-if-changed=../scripts/ssh-setup.sh");
+
+    println!("cargo::rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+    println!("cargo::rustc-link-lib=static=pcap");
 }
