c10d39e647dde697a8dde73156ad4ee568b8da45
Joakim Hulthe <joakim@hulthe.net>
2024-08-13 12:43:41 +0200

    Add `use_anywhere` feature indicator

--- mullvad-types/src/features.rs
+++ mullvad-types/src/features.rs
@@ -39,6 +39,7 @@ pub enum FeatureIndicator {
     CustomMtu,
     CustomMssFix,
     Daita,
+    DaitaUseAnywhere,
 }
 
 impl std::fmt::Display for FeatureIndicator {
@@ -58,6 +59,7 @@ impl std::fmt::Display for FeatureIndicator {
             FeatureIndicator::CustomMtu => "Custom MTU",
             FeatureIndicator::CustomMssFix => "Custom MSS",
             FeatureIndicator::Daita => "DAITA",
+            FeatureIndicator::DaitaUseAnywhere => "Use Anywhere (DAITA)",
         };
         write!(f, "{feature}")
     }
