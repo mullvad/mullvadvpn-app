e141580b842834161a362a227b59fc233b6659de
Joakim Hulthe <joakim.hulthe@mullvad.net>
2024-08-19 17:15:58 +0200

    Remove daita cfg from mullvad-cli

--- mullvad-cli/build.rs
+++ mullvad-cli/build.rs
@@ -15,11 +15,4 @@ fn main() {
         ));
         res.compile().expect("Unable to generate windows resources");
     }
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set");
-
-    // Enable DAITA by default on desktop
-    println!("cargo::rustc-check-cfg=cfg(daita)");
-    if let "linux" | "windows" | "macos" = target_os.as_str() {
-        println!(r#"cargo::rustc-cfg=daita"#);
-    }
 }
