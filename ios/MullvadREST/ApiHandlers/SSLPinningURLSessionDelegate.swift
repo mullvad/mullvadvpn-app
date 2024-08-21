8ff7f7ca9dbbd892f5a3cfc747188c26bfa916bb
Bug Magnet <marco.nikic@mullvad.net>
2024-08-21 15:26:05 +0200

    Modify the SSL Pinning logic to support conncheck as well

--- ios/MullvadREST/ApiHandlers/SSLPinningURLSessionDelegate.swift
+++ ios/MullvadREST/ApiHandlers/SSLPinningURLSessionDelegate.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import MullvadLogging
+import Network
 import Security
 
 final class SSLPinningURLSessionDelegate: NSObject, URLSessionDelegate {
@@ -29,17 +30,32 @@ final class SSLPinningURLSessionDelegate: NSObject, URLSessionDelegate {
         completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void
     ) {
         if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
-           let serverTrust = challenge.protectionSpace.serverTrust,
-           verifyServerTrust(serverTrust) {
-            completionHandler(.useCredential, URLCredential(trust: serverTrust))
-        } else {
-            completionHandler(.rejectProtectionSpace, nil)
+           let serverTrust = challenge.protectionSpace.serverTrust {
+            /// If a request is going through a local shadowsocks proxy, the host would be a localhost address,`
+            /// which would not appear in the list of valid host names in the root certificate.
+            /// The same goes for direct connections to the API, the host would be the IP address of the endpoint.
+            /// Certificates, cannot be signed for IP addresses, in such case, specify that the host name is `defaultAPIHostname`
+            var hostName = challenge.protectionSpace.host
+            let overridenHostnames = [
+                "\(IPv4Address.loopback)",
+                "\(IPv6Address.loopback)",
+                "\(REST.defaultAPIEndpoint.ip)",
+            ]
+            if overridenHostnames.contains(hostName) {
+                hostName = sslHostname
+            }
+
+            if verifyServerTrust(serverTrust, for: hostName) {
+                completionHandler(.useCredential, URLCredential(trust: serverTrust))
+                return
+            }
         }
+        completionHandler(.rejectProtectionSpace, nil)
     }
 
     // MARK: - Private
 
-    private func verifyServerTrust(_ serverTrust: SecTrust) -> Bool {
+    private func verifyServerTrust(_ serverTrust: SecTrust, for sslHostname: String) -> Bool {
         var secResult: OSStatus
 
         // Set SSL policy
