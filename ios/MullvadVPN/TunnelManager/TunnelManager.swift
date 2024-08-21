57ed7726499af977b042c34c934609503aea7b31
Jon Petersson <jon.petersson@kvadrat.se>
2024-08-21 15:05:23 +0200

    Fix out-of-time notifications

--- ios/MullvadVPN/TunnelManager/TunnelManager.swift
+++ ios/MullvadVPN/TunnelManager/TunnelManager.swift
@@ -1109,7 +1109,7 @@ final class TunnelManager: StorePaymentObserver {
 
 extension TunnelManager {
     enum AccountExpirySimulationOption {
-        case closeToExpiry
+        case closeToExpiry(days: Int)
         case expired
         case active
 
@@ -1121,9 +1121,9 @@ extension TunnelManager {
             case .active:
                 return calendar.date(byAdding: .year, value: 1, to: now)
 
-            case .closeToExpiry:
+            case let .closeToExpiry(days):
                 return calendar.date(
-                    byAdding: DateComponents(day: NotificationConfiguration.closeToExpiryTriggerInterval, second: 5),
+                    byAdding: DateComponents(day: days, second: 5),
                     to: now
                 )
 
