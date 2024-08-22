b98b40d38ae578e5a9e5bd457cd9a925659d1eef
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-08-22 12:01:18 +0200

    Rename delete confirmation dialog and make more dialogs use it

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/ReportProblemNoEmailDialog.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/ReportProblemNoEmailDialog.kt
@@ -1,14 +1,7 @@
 package net.mullvad.mullvadvpn.compose.dialog
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.dropUnlessResumed
@@ -18,10 +11,7 @@ import com.ramcosta.composedestinations.result.EmptyResultBackNavigator
 import com.ramcosta.composedestinations.result.ResultBackNavigator
 import com.ramcosta.composedestinations.spec.DestinationStyle
 import net.mullvad.mullvadvpn.R
-import net.mullvad.mullvadvpn.compose.button.NegativeButton
-import net.mullvad.mullvadvpn.compose.button.PrimaryButton
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
-import net.mullvad.mullvadvpn.lib.theme.Dimens
 
 @Preview
 @Composable
@@ -32,38 +22,13 @@ private fun PreviewReportProblemNoEmailDialog() {
 @Destination<RootGraph>(style = DestinationStyle.Dialog::class)
 @Composable
 fun ReportProblemNoEmail(resultBackNavigator: ResultBackNavigator<Boolean>) {
-    AlertDialog(
-        onDismissRequest = dropUnlessResumed { resultBackNavigator.navigateBack() },
-        icon = {
-            Icon(
-                painter = painterResource(id = R.drawable.icon_alert),
-                contentDescription = null,
-                modifier = Modifier.size(Dimens.dialogIconHeight),
-                tint = MaterialTheme.colorScheme.error
-            )
-        },
-        text = {
-            Text(
-                text = stringResource(id = R.string.confirm_no_email),
-                modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.bodySmall,
-                color = MaterialTheme.colorScheme.onSurface
-            )
-        },
-        dismissButton = {
-            PrimaryButton(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = dropUnlessResumed { resultBackNavigator.navigateBack() },
-                text = stringResource(id = R.string.back)
-            )
-        },
-        confirmButton = {
-            NegativeButton(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = dropUnlessResumed { resultBackNavigator.navigateBack(result = true) },
-                text = stringResource(id = R.string.send_anyway)
-            )
-        },
-        containerColor = MaterialTheme.colorScheme.surface
+    NegativeConfirmationDialog(
+        message = stringResource(id = R.string.confirm_no_email),
+        errorMessage = null,
+        confirmationText = stringResource(id = R.string.send_anyway),
+        cancelText = stringResource(id = R.string.back),
+        messageStyle = MaterialTheme.typography.bodySmall,
+        onBack = dropUnlessResumed { resultBackNavigator.navigateBack() },
+        onConfirm = dropUnlessResumed { resultBackNavigator.navigateBack(result = true) },
     )
 }
