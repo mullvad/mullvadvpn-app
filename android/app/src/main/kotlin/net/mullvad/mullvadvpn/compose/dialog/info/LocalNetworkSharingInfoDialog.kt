a1fad72e14c7ca4f0279933d2c040eb4948ae1e8
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-08-22 12:00:51 +0200

    Move all info dialogs to their own package

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/info/LocalNetworkSharingInfoDialog.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/info/LocalNetworkSharingInfoDialog.kt
@@ -1,30 +1,33 @@
-package net.mullvad.mullvadvpn.compose.dialog
+package net.mullvad.mullvadvpn.compose.dialog.info
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.dropUnlessResumed
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 import com.ramcosta.composedestinations.spec.DestinationStyle
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.compose.component.textResource
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
+
+@Preview
+@Composable
+private fun PreviewLocalNetworkSharingInfoDialog() {
+    AppTheme { LocalNetworkSharingInfo(EmptyDestinationsNavigator) }
+}
 
 @Destination<RootGraph>(style = DestinationStyle.Dialog::class)
 @Composable
-fun ContentBlockersInfo(navigator: DestinationsNavigator) {
+fun LocalNetworkSharingInfo(navigator: DestinationsNavigator) {
     InfoDialog(
-        message =
-            buildString {
-                appendLine(stringResource(id = R.string.dns_content_blockers_info))
-                append(stringResource(id = R.string.dns_content_blockers_warning))
-            },
+        message = stringResource(id = R.string.local_network_sharing_info),
         additionalInfo =
             buildString {
-                appendLine(textResource(id = R.string.dns_content_blockers_custom_dns_warning))
-                appendLine(
-                    stringResource(id = R.string.settings_changes_effect_warning_content_blocker)
-                )
+                appendLine(stringResource(id = R.string.local_network_sharing_additional_info))
+                appendLine(textResource(id = R.string.local_network_sharing_ip_ranges))
             },
         onDismiss = dropUnlessResumed { navigator.navigateUp() }
     )
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/info/UdpOverTcpPortInfoDialog.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/info/UdpOverTcpPortInfoDialog.kt
@@ -1,4 +1,4 @@
-package net.mullvad.mullvadvpn.compose.dialog
+package net.mullvad.mullvadvpn.compose.dialog.info
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
@@ -10,25 +10,19 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 import com.ramcosta.composedestinations.spec.DestinationStyle
 import net.mullvad.mullvadvpn.R
-import net.mullvad.mullvadvpn.compose.component.textResource
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 
 @Preview
 @Composable
-private fun PreviewLocalNetworkSharingInfoDialog() {
-    AppTheme { LocalNetworkSharingInfo(EmptyDestinationsNavigator) }
+private fun PreviewUdpOverTcpPortInfoDialog() {
+    AppTheme { UdpOverTcpPortInfo(EmptyDestinationsNavigator) }
 }
 
 @Destination<RootGraph>(style = DestinationStyle.Dialog::class)
 @Composable
-fun LocalNetworkSharingInfo(navigator: DestinationsNavigator) {
+fun UdpOverTcpPortInfo(navigator: DestinationsNavigator) {
     InfoDialog(
-        message = stringResource(id = R.string.local_network_sharing_info),
-        additionalInfo =
-            buildString {
-                appendLine(stringResource(id = R.string.local_network_sharing_additional_info))
-                appendLine(textResource(id = R.string.local_network_sharing_ip_ranges))
-            },
+        message = stringResource(id = R.string.udp_over_tcp_port_info),
         onDismiss = dropUnlessResumed { navigator.navigateUp() }
     )
 }
