30ffd4ab18e8fb098a2dbe7066f4f4db149578d0
Niklas Berglund <niklas.berglund@gmail.com>
2024-08-23 15:05:23 +0200

    Temporary debug logging

--- android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/interactor/AppInteractor.kt
+++ android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/interactor/AppInteractor.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
+import co.touchlab.kermit.Logger
 import net.mullvad.mullvadvpn.lib.endpoint.CustomApiEndpointConfiguration
 import net.mullvad.mullvadvpn.lib.endpoint.putApiEndpointConfigurationExtra
 import net.mullvad.mullvadvpn.test.common.constant.APP_LAUNCH_TIMEOUT
@@ -124,6 +125,7 @@ class AppInteractor(
     }
 
     private fun String.extractIpAddress(): String {
+        Logger.v("Extracting IP address from: $this")
         return split(" ")[1].split(" ")[0]
     }
 }
