b78118e2649a5250b362b9e5ea82df427f43dd0c
Niklas Berglund <niklas.berglund@gmail.com>
2024-08-26 17:05:00 +0200

    Add minimal leak tests for Android

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/Switch.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/Switch.kt
@@ -15,7 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.compose.test.SWITCH_TEST_TAG
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
 import net.mullvad.mullvadvpn.lib.theme.color.AlphaDisabled
@@ -55,7 +57,7 @@ fun MullvadSwitch(
     Switch(
         checked = checked,
         onCheckedChange = onCheckedChange,
-        modifier = modifier,
+        modifier = modifier.testTag(SWITCH_TEST_TAG),
         thumbContent = thumbContent,
         enabled = enabled,
         colors = colors,
