a1935318424607bd28f78db8767b48b9297d12f6
Niklas Berglund <niklas.berglund@gmail.com>
2024-08-26 17:05:00 +0200

    Add minimal leak tests for Android

---
+++ android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/annotations/HasDependencyOnLocalAPI.kt
@@ -0,0 +1,33 @@
+package net.mullvad.mullvadvpn.test.e2e.annotations
+
+import net.mullvad.mullvadvpn.test.e2e.BuildConfig
+import org.junit.jupiter.api.extension.ConditionEvaluationResult
+import org.junit.jupiter.api.extension.ExecutionCondition
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
+
+/**
+ * Annotation for tests making use of local APIs such as the firewall or packet capture APIs, which
+ * can only run in the office environment.
+ */
+@Retention(AnnotationRetention.RUNTIME)
+@ExtendWith(HasDependencyOnLocalAPI.ShouldRunWhenHaveAccessToLocalAPI::class)
+annotation class HasDependencyOnLocalAPI {
+    class ShouldRunWhenHaveAccessToLocalAPI : ExecutionCondition {
+        override fun evaluateExecutionCondition(
+            context: ExtensionContext?
+        ): ConditionEvaluationResult {
+            val enable = BuildConfig.ENABLE_ACCESS_TO_LOCAL_API_TESTS.toBoolean() ?: false
+
+            return if (enable) {
+                ConditionEvaluationResult.enabled(
+                    "Running test which requires access to local APIs."
+                )
+            } else {
+                ConditionEvaluationResult.disabled(
+                    "Skipping test which requires access to local APIs."
+                )
+            }
+        }
+    }
+}
