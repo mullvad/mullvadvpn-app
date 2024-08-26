58cfbd8fc5d21412e903634faea837051030d9c4
Niklas Berglund <niklas.berglund@gmail.com>
2024-08-26 17:05:00 +0200

    Add minimal leak tests for Android

---
+++ android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/model/Packet.kt
@@ -0,0 +1,25 @@
+package net.mullvad.mullvadvpn.test.e2e.model
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import net.mullvad.mullvadvpn.test.e2e.serializer.PacketSerializer
+import org.joda.time.DateTime
+
+@Serializable(with = PacketSerializer::class)
+sealed interface Packet {
+    @SerialName("timestamp") val date: DateTime
+    val fromPeer: Boolean
+}
+
+@Serializable
+data class RxPacket(
+    @SerialName("timestamp") @Contextual override val date: DateTime,
+    @SerialName("from_peer") override val fromPeer: Boolean,
+) : Packet
+
+@Serializable
+data class TxPacket(
+    @SerialName("timestamp") @Contextual override val date: DateTime,
+    @SerialName("from_peer") override val fromPeer: Boolean,
+) : Packet
