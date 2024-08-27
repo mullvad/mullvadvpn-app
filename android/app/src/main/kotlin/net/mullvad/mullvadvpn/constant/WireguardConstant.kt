03b3fc5e84573560d4aa8da7dfe6350070856afa
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-08-27 16:35:43 +0200

    Removed shadow socks predefined ports

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/WireguardConstant.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/WireguardConstant.kt
@@ -1,7 +1,10 @@
 package net.mullvad.mullvadvpn.constant
 
 import net.mullvad.mullvadvpn.lib.model.Port
+import net.mullvad.mullvadvpn.lib.model.PortRange
 
 val WIREGUARD_PRESET_PORTS = listOf(Port(51820), Port(53))
 val UDP2TCP_PRESET_PORTS = listOf(Port(80), Port(5001))
-val SHADOWSOCKS_PRESET_PORTS = listOf(Port(80), Port(443))
+val SHADOWSOCKS_PRESET_PORTS = emptyList<Port>()
+val SHADOWSOCKS_AVAILABLE_PORTS =
+    listOf(PortRange(IntRange(0, 65535))) // Currently we consider all ports to be available
