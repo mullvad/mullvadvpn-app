9a1655163ad0ea1412a6ee67724e5d14e2ba4504
Albin <albin@mullvad.net>
2024-08-29 13:00:11 +0200

    Build wg with daita support for android

--- mullvad-management-interface/build.rs
+++ mullvad-management-interface/build.rs
@@ -1,11 +1,7 @@
 fn main() {
     tonic_build::compile_protos("proto/management_interface.proto").unwrap();
 
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set");
-
-    // Enable DAITA by default on desktop
+    // Enable DAITA by default on desktop and android
     println!("cargo::rustc-check-cfg=cfg(daita)");
-    if let "linux" | "windows" | "macos" = target_os.as_str() {
-        println!(r#"cargo::rustc-cfg=daita"#);
-    }
+    println!(r#"cargo::rustc-cfg=daita"#);
 }
