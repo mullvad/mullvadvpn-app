93dd4c574ca0f7bae46b9ab8a259e465ab856069
Oskar <oskar@mullvad.net>
2024-08-30 10:46:43 +0200

    Fix missing daemon event obfuscation type convertion

--- gui/src/shared/daemon-rpc-types.ts
+++ gui/src/shared/daemon-rpc-types.ts
@@ -112,7 +112,7 @@ export function tunnelTypeToString(tunnel: TunnelType): string {
 }
 
 export type RelayProtocol = 'tcp' | 'udp';
-export type EndpointObfuscationType = 'udp2tcp';
+export type EndpointObfuscationType = 'udp2tcp' | 'shadowsocks';
 
 export type Constraint<T> = 'any' | { only: T };
 export type LiftedConstraint<T> = 'any' | T;
