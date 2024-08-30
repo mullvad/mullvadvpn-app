fcb807afe788f572805e93152c9ee811f30d1972
David Göransson <david.goransson@mullvad.net>
2024-08-30 10:47:35 +0200

    Fix notification throttling

--- android/service/src/main/kotlin/net/mullvad/mullvadvpn/service/notifications/NotificationManager.kt
+++ android/service/src/main/kotlin/net/mullvad/mullvadvpn/service/notifications/NotificationManager.kt
@@ -5,7 +5,10 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationManagerCompat
+import kotlin.time.Duration.Companion.milliseconds
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.lib.model.Notification
@@ -13,6 +16,7 @@ import net.mullvad.mullvadvpn.lib.model.NotificationUpdate
 import net.mullvad.mullvadvpn.service.notifications.accountexpiry.toNotification
 import net.mullvad.mullvadvpn.service.notifications.tunnelstate.toNotification
 
+@OptIn(FlowPreview::class)
 class NotificationManager(
     private val notificationManagerCompat: NotificationManagerCompat,
     notificationProviders: List<NotificationProvider<Notification>>,
@@ -23,7 +27,7 @@ class NotificationManager(
     init {
         scope.launch {
             notificationProviders
-                .map { it.notifications }
+                .map { it.notifications.debounce(200.milliseconds) }
                 .merge()
                 .collect { notificationUpdate ->
                     when (notificationUpdate) {
