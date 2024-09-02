37ef6262bf774e064fc20d146d78596f067cf8b9
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-09-02 16:39:19 +0200

    Fix range

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/WireguardConstant.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/WireguardConstant.kt
@@ -8,4 +8,4 @@ val UDP2TCP_PRESET_PORTS = listOf(Port(80), Port(5001))
 val SHADOWSOCKS_PRESET_PORTS = emptyList<Port>()
 val SHADOWSOCKS_AVAILABLE_PORTS =
     // Currently we consider all ports to be available
-    listOf(PortRange(IntRange(Port.MIN_VALUE, Port.MAX_VALUE)))
+    listOf(PortRange(Port.MIN_VALUE..Port.MAX_VALUE))
