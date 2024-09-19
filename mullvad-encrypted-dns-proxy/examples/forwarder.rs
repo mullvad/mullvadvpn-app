b79916760c3019cbe843a502af41544423f86224
Linus Färnstrand <linus@mullvad.net>
2024-09-19 17:14:11 +0200

    Rewrite parts of the Encrypted DNS proxy parsing and handling

--- mullvad-encrypted-dns-proxy/examples/forwarder.rs
+++ mullvad-encrypted-dns-proxy/examples/forwarder.rs
@@ -1,6 +1,6 @@
 use std::env::args;
 
-use mullvad_encrypted_dns_proxy::{config::Obfuscator, config_resolver, forwarder};
+use mullvad_encrypted_dns_proxy::{config_resolver, forwarder};
 use tokio::net::TcpListener;
 
 /// This can be tested out by using curl:
@@ -8,24 +8,30 @@ use tokio::net::TcpListener;
 ///  where $addr and $port are the listening address of the proxy (bind_addr).
 #[tokio::main]
 async fn main() {
-    let mut configs =
+    let bind_addr = args().nth(1).unwrap_or("127.0.0.1:0".to_string());
+
+    let configs =
         config_resolver::resolve_configs(config_resolver::default_resolvers(), "frakta.eu")
             .await
             .expect("Failed to resolve configs");
 
-    let bind_addr = args().nth(1).unwrap_or("127.0.0.1:0".to_string());
-    let obfuscator = configs.xor.pop().expect("No XOR config");
-    println!("Obfuscator in use - {:?}", obfuscator);
-    let obfuscator: Box<dyn Obfuscator> = Box::new(obfuscator);
+    let proxy_config = configs
+        .into_iter()
+        .find(|c| c.obfuscation.is_some())
+        .expect("No XOR config");
+    println!("Proxy config in use: {:?}", proxy_config);
+
     let listener = TcpListener::bind(bind_addr)
         .await
         .expect("Failed to bind listener socket");
+
     let listen_addr = listener
         .local_addr()
         .expect("failed to obtain listen address");
     println!("Listening on {listen_addr}");
+
     while let Ok((client_conn, _client_addr)) = listener.accept().await {
-        let connected = crate::forwarder::Forwarder::connect(obfuscator.clone())
+        let connected = crate::forwarder::Forwarder::connect(&proxy_config)
             .await
             .expect("failed to connect to obfuscator");
         let _ = connected.forward(client_conn).await;
