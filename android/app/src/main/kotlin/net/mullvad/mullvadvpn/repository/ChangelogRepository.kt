d88431cc223d8e906ede6a4c1e9c0a717931bb27
David Göransson <david.goransson@mullvad.net>
2024-09-20 11:12:16 +0200

    Add support for hypens in a changelog entry

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/ChangelogRepository.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/ChangelogRepository.kt
@@ -6,7 +6,6 @@ import net.mullvad.mullvadvpn.util.trimAll
 
 private const val MISSING_VERSION_CODE = -1
 private const val NEWLINE_CHAR = '\n'
-private const val SPACE_STRING = " "
 private const val BULLET_POINT_CHAR = '-'
 private const val LAST_SHOWED_CHANGELOG_VERSION_CODE = "last_showed_changelog_version_code"
 
@@ -22,9 +21,9 @@ class ChangelogRepository(
         preferences.edit().putInt(LAST_SHOWED_CHANGELOG_VERSION_CODE, versionCode).apply()
 
     fun getLastVersionChanges(): List<String> =
-        dataProvider
-            .getChangelog()
-            .split(BULLET_POINT_CHAR)
-            .map { it.split(NEWLINE_CHAR).trimAll().joinToString(SPACE_STRING).trim() }
+        // Prepend with a new line char so each entry consists of NEWLINE_CHAR + BULLET_POINT_CHAR
+        (NEWLINE_CHAR + dataProvider.getChangelog())
+            .split(NEWLINE_CHAR.toString() + BULLET_POINT_CHAR)
+            .trimAll()
             .filter { it.isNotEmpty() }
 }
