7daa119d45a661a43dd0da8fa6c28f1e0c480c66
David Göransson <david.goransson@mullvad.net>
2024-10-02 09:47:44 +0200

    Fix usage of deprecated lambda

--- android/lib/daemon-grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/daemon/grpc/util/ManagedChannel.kt
+++ android/lib/daemon-grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/daemon/grpc/util/ManagedChannel.kt
@@ -3,13 +3,11 @@ package net.mullvad.mullvadvpn.lib.daemon.grpc.util
 import co.touchlab.kermit.Logger
 import io.grpc.ConnectivityState
 import io.grpc.ManagedChannel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.suspendCancellableCoroutine
 
-@OptIn(ExperimentalCoroutinesApi::class)
 internal fun ManagedChannel.connectivityFlow(): Flow<ConnectivityState> {
     return callbackFlow {
         var currentState = getState(false)
@@ -20,7 +18,9 @@ internal fun ManagedChannel.connectivityFlow(): Flow<ConnectivityState> {
             currentState = suspendCancellableCoroutine {
                 notifyWhenStateChanged(currentState) {
                     // If we are cancelled we will just log
-                    it.resume(getState(false)) { Logger.w("Resume while cancelled", it) }
+                    it.resume(getState(false)) { cause, value, _ ->
+                        Logger.w("Resume while cancelled, value: $value", cause)
+                    }
                 }
             }
         }
