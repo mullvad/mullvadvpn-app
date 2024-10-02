62497011c88d52e80f9b36f84939600d56dfa6e6
Oskar <oskar@mullvad.net>
2024-10-02 08:10:04 +0200

    Remove withAppContext HOC

--- gui/src/renderer/context.tsx
+++ gui/src/renderer/context.tsx
@@ -15,25 +15,6 @@ const missingContextError = new Error(
   'The context value is empty. Make sure to wrap the component in AppContext.Provider.',
 );
 
-type PropsWithoutAppContext<Props> = Omit<Props, 'app'>;
-
-export default function withAppContext<Props>(
-  BaseComponent: React.ComponentType<PropsWithoutAppContext<Props> & IAppContext>,
-) {
-  // Exclude the IAppContext from props since those are injected props
-  const wrappedComponent = (props: PropsWithoutAppContext<Props>) => {
-    const appContext = useAppContext();
-    return <BaseComponent app={appContext} {...props} />;
-  };
-
-  if (window.env.development) {
-    wrappedComponent.displayName =
-      'withAppContext(' + (BaseComponent.displayName || BaseComponent.name) + ')';
-  }
-
-  return wrappedComponent;
-}
-
 export function useAppContext(): App {
   const appContext = useContext(AppContext);
   if (appContext) {
