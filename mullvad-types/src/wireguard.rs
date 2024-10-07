aaf11005823bfd79b65d1a4fbaae36c3e3922e51
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-10-07 11:36:50 +0200

    Set "Direct only" to `false` as default
    For android, it is set to true, as multihop is not supported.
    
    Note that in the daemon, the setting is called
    `use_multihop_if_necessary` and has the inverse meaning.

--- mullvad-types/src/wireguard.rs
+++ mullvad-types/src/wireguard.rs
@@ -81,12 +81,34 @@ impl FromStr for QuantumResistantState {
 pub struct QuantumResistantStateParseError;
 
 #[cfg(daita)]
-#[derive(Debug, Default, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct DaitaSettings {
     pub enabled: bool,
 
-    #[serde(default)]
+    #[cfg_attr(target_os = "android", serde(skip))]
+    #[serde(default = "DaitaSettings::default_use_multihop_if_necessary")]
+    /// Whether to use multihop if the selected relay is not DAITA-compatible. Note that this is
+    /// the inverse of of "Direct only" in the GUI.
     pub use_multihop_if_necessary: bool,
+}
+
+#[cfg(daita)]
+impl DaitaSettings {
+    /// This setting should be enabled by default, expect on Android where multihop is not
+    /// supported.
+    const fn default_use_multihop_if_necessary() -> bool {
+        cfg!(not(target_os = "android"))
+    }
+}
+
+#[cfg(daita)]
+impl Default for DaitaSettings {
+    fn default() -> Self {
+        Self {
+            enabled: false,
+            use_multihop_if_necessary: Self::default_use_multihop_if_necessary(),
+        }
+    }
 }
 
 /// Contains account specific wireguard data
