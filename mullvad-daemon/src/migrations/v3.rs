9ccca53a3b7b68420fab9c04a10f53f1a841ec1e
Markus Pettersson <markus.pettersson@mullvad.net>
2024-10-10 14:46:12 +0200

    Implement v3 -> v4 settings migration

--- mullvad-daemon/src/migrations/v3.rs
+++ mullvad-daemon/src/migrations/v3.rs
@@ -91,9 +91,12 @@ fn version_matches(settings: &serde_json::Value) -> bool {
 mod test {
     use crate::migrations::load_seed;
 
+    use super::migrate;
+
     #[test]
     fn v3_to_v4_migration() {
         let mut settings = load_seed("v3.json");
+        migrate(&mut settings).unwrap();
         insta::assert_snapshot!(serde_json::to_string_pretty(&settings).unwrap());
     }
 }
