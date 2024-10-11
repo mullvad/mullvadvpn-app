a65602a903ca2add2689bfa4b03a1036c2ed722c
Oskar <oskar@mullvad.net>
2024-10-11 17:11:54 +0200

    Split utility-hooks into two files

--- gui/src/renderer/lib/utility-hooks.ts
+++ gui/src/renderer/lib/utility-hooks.ts
@@ -7,9 +7,6 @@ import React, {
   useState,
 } from 'react';
 
-import { LiftedConstraint, TunnelProtocol } from '../../shared/daemon-rpc-types';
-import { useSelector } from '../redux/store';
-
 export function useMounted() {
   const mountedRef = useRef(false);
   const isMounted = useCallback(() => mountedRef.current, []);
@@ -53,29 +50,6 @@ export function useBoolean(initialValue = false) {
   const toggle = useCallback(() => setValue((value) => !value), []);
 
   return [value, setTrue, setFalse, toggle] as const;
-}
-
-export function useNormalRelaySettings() {
-  const relaySettings = useSelector((state) => state.settings.relaySettings);
-  return 'normal' in relaySettings ? relaySettings.normal : undefined;
-}
-
-// Some features are considered core privacy features and when enabled prevent OpenVPN from being
-// used. This hook returns the tunnelprotocol with the exception that it always returns WireGuard
-// when any of those features are enabled.
-export function useTunnelProtocol(): LiftedConstraint<TunnelProtocol> {
-  const relaySettings = useNormalRelaySettings();
-  const multihop = relaySettings?.wireguard.useMultihop ?? false;
-  const daita = useSelector((state) => state.settings.wireguard.daita?.enabled ?? false);
-  const quantumResistant = useSelector((state) => state.settings.wireguard.quantumResistant);
-  const openVpnDisabled = daita || multihop || quantumResistant;
-
-  return openVpnDisabled ? 'wireguard' : (relaySettings?.tunnelProtocol ?? 'any');
-}
-
-export function useNormalBridgeSettings() {
-  const bridgeSettings = useSelector((state) => state.settings.bridgeSettings);
-  return bridgeSettings.normal;
 }
 
 // This hook returns a function that can be used to force a rerender of a component, and
