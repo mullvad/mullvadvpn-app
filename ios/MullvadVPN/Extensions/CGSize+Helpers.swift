0fbaa5a5feef0ee9e667f219c8353c7b37c4fb78
mojganii <mojgan.jelodar@codic.se>
2024-10-11 15:43:55 +0200

    Refactor buttons to use UIButton.Configuration

---
+++ ios/MullvadVPN/Extensions/CGSize+Helpers.swift
@@ -0,0 +1,18 @@
+//
+//  CGSize+Helpers.swift
+//  MullvadVPN
+//
+//  Created by Mojgan on 2024-10-10.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import UIKit
+
+extension CGSize {
+    // Function to deduct insets from CGSize
+    func deducting(insets: NSDirectionalEdgeInsets) -> CGSize {
+        let newWidth = width - (insets.leading + insets.trailing)
+        let newHeight = height - (insets.top + insets.bottom)
+        return CGSize(width: newWidth, height: newHeight)
+    }
+}
