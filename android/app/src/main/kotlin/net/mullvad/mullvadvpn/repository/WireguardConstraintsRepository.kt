63d491b901522b9f5ad9f839ff104d5c454eb48f
David Göransson <david.goransson@mullvad.net>
2024-10-14 11:13:16 +0200

    TODO(Drop) Hack to test Multihop in Android client

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/WireguardConstraintsRepository.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/WireguardConstraintsRepository.kt
@@ -5,7 +5,7 @@ import net.mullvad.mullvadvpn.lib.model.Constraint
 import net.mullvad.mullvadvpn.lib.model.Port
 
 class WireguardConstraintsRepository(private val managementService: ManagementService) {
-    suspend fun setWireguardPort(port: Constraint<Port>) = managementService.setWireguardPort(port)
+    suspend fun setWireguardPort(port: Constraint<Port>) = managementService.setMultihop(true)
 
     suspend fun setMultihop(enabled: Boolean) = managementService.setMultihop(enabled)
 }
