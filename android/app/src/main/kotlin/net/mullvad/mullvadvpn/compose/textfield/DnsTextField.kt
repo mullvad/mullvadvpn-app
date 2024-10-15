14464f79de7335f57166c67f3b992e321d9ab5f4
David Göransson <david.goransson@mullvad.net>
2024-10-15 10:49:10 +0200

    Update error icon for api access method

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/textfield/DnsTextField.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/textfield/DnsTextField.kt
@@ -1,17 +1,9 @@
 package net.mullvad.mullvadvpn.compose.textfield
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Error
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
-import net.mullvad.mullvadvpn.lib.theme.Dimens
 
 @Composable
 fun DnsTextField(
@@ -37,22 +29,4 @@ fun DnsTextField(
         isDigitsOnlyAllowed = false,
         isValidValue = isValidValue,
     )
-}
-
-@Composable
-private fun ErrorSupportingText(text: String) {
-    Row(modifier = Modifier.padding(top = Dimens.miniPadding)) {
-        Icon(
-            imageVector = Icons.Default.Error,
-            contentDescription = null,
-            modifier = Modifier.size(Dimens.smallIconSize),
-            tint = MaterialTheme.colorScheme.error,
-        )
-        Text(
-            text = text,
-            color = MaterialTheme.colorScheme.onSecondary,
-            style = MaterialTheme.typography.bodySmall,
-            modifier = Modifier.padding(horizontal = Dimens.smallPadding),
-        )
-    }
 }
