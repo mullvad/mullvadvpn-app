381f294af3a3c7dac3fe402225e7205e486928cd
Niklas Berglund <niklas.berglund@gmail.com>
2024-10-18 11:08:01 +0200

    Implement basic of page object pattern

---
+++ android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/Page.kt
@@ -0,0 +1,16 @@
+package net.mullvad.mullvadvpn.test.common.page
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+
+sealed class Page {
+    protected val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+    abstract fun assertIsDisplayed()
+}
+
+inline fun <reified T : Page> on(scope: T.() -> Unit = {}) {
+    val page = T::class.java.getConstructor().newInstance()
+    page.assertIsDisplayed()
+    return page.scope()
+}
---
+++ android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/PrivacyPage.kt
@@ -0,0 +1,38 @@
+package net.mullvad.mullvadvpn.test.common.page
+
+import android.os.Build
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.Until
+import net.mullvad.mullvadvpn.test.common.constant.DEFAULT_TIMEOUT
+import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
+
+class PrivacyPage internal constructor() : Page() {
+    override fun assertIsDisplayed() {
+        // uiDevice.findObjectWithTimeout(By.res("connect_card_header_test_tag"))
+    }
+
+    fun clickAgreeOnPrivacyDisclaimer() {
+        uiDevice.findObjectWithTimeout(By.text("Agree and continue")).click()
+    }
+
+    fun clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove(
+        timeout: Long = DEFAULT_TIMEOUT
+    ) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            // Skipping as notification permissions are not shown.
+            return
+        }
+
+        val selector = By.text("Allow")
+
+        uiDevice.wait(Until.hasObject(selector), timeout)
+
+        try {
+            uiDevice.findObjectWithTimeout(selector).click()
+        } catch (e: IllegalArgumentException) {
+            throw IllegalArgumentException(
+                "Failed to allow notification permission within timeout ($timeout)"
+            )
+        }
+    }
+}
