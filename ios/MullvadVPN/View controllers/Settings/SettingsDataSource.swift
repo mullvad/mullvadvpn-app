3a2632a6ca677521e1e4fc7939c5e2126ba03c5c
Jon Petersson <jon.petersson@mullvad.net>
2024-10-22 14:17:29 +0200

    Remove explanatory text for Direct only in settings

--- ios/MullvadVPN/View controllers/Settings/SettingsDataSource.swift
+++ ios/MullvadVPN/View controllers/Settings/SettingsDataSource.swift
@@ -44,20 +44,6 @@ final class SettingsDataSource: UITableViewDiffableDataSource<SettingsDataSource
         case main
         case version
         case problemReport
-
-        var sectionFooter: String? {
-            switch self {
-            case .daita:
-                NSLocalizedString(
-                    "SETTINGS_DAITA_SECTION_FOOTER",
-                    tableName: "Settings",
-                    value: "Makes it possible to use DAITA with any server and is automatically enabled.",
-                    comment: ""
-                )
-            default:
-                nil
-            }
-        }
     }
 
     enum Item: String {
@@ -151,41 +137,15 @@ final class SettingsDataSource: UITableViewDiffableDataSource<SettingsDataSource
     }
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        let sectionIdentifier = snapshot().sectionIdentifiers[section]
-
-        return switch sectionIdentifier {
-        case .main:
-            interactor.deviceState.isLoggedIn ? 0 : UITableView.automaticDimension
-        case .daita, .version, .problemReport:
-            UITableView.automaticDimension
-        }
+        UITableView.automaticDimension
     }
 
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let sectionIdentifier = snapshot().sectionIdentifiers[section]
-        guard let sectionFooterText = sectionIdentifier.sectionFooter else { return nil }
-
-        guard let headerView = tableView
-            .dequeueReusableView(withIdentifier: HeaderFooterReuseIdentifier.primary)
-        else { return nil }
-
-        var contentConfiguration = UIListContentConfiguration.mullvadGroupedFooter(tableStyle: .plain)
-        contentConfiguration.text = sectionFooterText
-
-        headerView.contentConfiguration = contentConfiguration
-
-        return headerView
+        nil
     }
 
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        let sectionIdentifier = snapshot().sectionIdentifiers[section]
-
-        return switch sectionIdentifier {
-        case .daita:
-            UITableView.automaticDimension
-        case .main, .version, .problemReport:
-            0
-        }
+        0
     }
 
     // MARK: - Private
@@ -211,13 +171,13 @@ final class SettingsDataSource: UITableViewDiffableDataSource<SettingsDataSource
 
         if interactor.deviceState.isLoggedIn {
             snapshot.appendSections([.daita])
+            snapshot.appendItems([.daita], toSection: .daita)
+            snapshot.appendItems([.daitaDirectOnly], toSection: .daita)
         }
 
         snapshot.appendSections([.main])
 
         if interactor.deviceState.isLoggedIn {
-            snapshot.appendItems([.daita], toSection: .daita)
-            snapshot.appendItems([.daitaDirectOnly], toSection: .daita)
             snapshot.appendItems([.vpnSettings], toSection: .main)
         }
 
