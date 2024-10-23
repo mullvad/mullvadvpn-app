55518a38d3bc14d9546e54f215d1855e270240fe
Jon Petersson <jon.petersson@mullvad.net>
2024-10-23 12:32:30 +0200

    Fix relay selector for smart routing

--- ios/MullvadVPNTests/MullvadREST/Relay/RelayPickingTests.swift
+++ ios/MullvadVPNTests/MullvadREST/Relay/RelayPickingTests.swift
@@ -16,6 +16,8 @@ import XCTest
 class RelayPickingTests: XCTestCase {
     let sampleRelays = ServerRelaysResponseStubs.sampleRelays
 
+    // MARK: Single-/multihop
+
     func testSinglehopPicker() throws {
         let constraints = RelayConstraints(
             entryLocations: .only(UserSelectedRelays(locations: [.hostname("se", "sto", "se2-wireguard")])),
@@ -75,6 +77,10 @@ class RelayPickingTests: XCTestCase {
         }
     }
 
+    // MARK: DAITA/Direct only
+
+    // DAITA - ON, Direct only - OFF, Multihop - OFF, Exit supports DAITA - FALSE
+    // Direct only is off, so we should automatically pick the entry that is closest to exit.
     func testDirectOnlyOffDaitaOnForSinglehopWithoutDaitaRelay() throws {
         let constraints = RelayConstraints(
             exitLocations: .only(UserSelectedRelays(locations: [.hostname("se", "got", "se10-wireguard")]))
@@ -93,6 +99,8 @@ class RelayPickingTests: XCTestCase {
         XCTAssertEqual(selectedRelays.exit.hostname, "se10-wireguard")
     }
 
+    // DAITA - ON, Direct only - ON, Multihop - OFF, Exit supports DAITA - FALSE
+    // Go into blocked state since Direct only requires a DAITA entry.
     func testDirectOnlyOnDaitaOnForSinglehopWithoutDaitaRelay() throws {
         let constraints = RelayConstraints(
             exitLocations: .only(UserSelectedRelays(locations: [.hostname("se", "got", "se10-wireguard")]))
@@ -108,6 +116,8 @@ class RelayPickingTests: XCTestCase {
         XCTAssertThrowsError(try picker.pick())
     }
 
+    // DAITA - ON, Direct only - OFF, Multihop - OFF, Exit supports DAITA - TRUE
+    // Select the DAITA entry, no automatic routing needed.
     func testDirectOnlyOffDaitaOnForSinglehopWithDaitaRelay() throws {
         let constraints = RelayConstraints(
             exitLocations: .only(UserSelectedRelays(locations: [.hostname("es", "mad", "es1-wireguard")]))
@@ -122,10 +132,12 @@ class RelayPickingTests: XCTestCase {
 
         let selectedRelays = try picker.pick()
 
-        XCTAssertEqual(selectedRelays.entry?.hostname, "us-nyc-wg-301") // New York relay is closest to exit relay.
+        XCTAssertNil(selectedRelays.entry)
         XCTAssertEqual(selectedRelays.exit.hostname, "es1-wireguard")
     }
 
+    // DAITA - ON, Direct only - ON, Multihop - OFF, Exit supports DAITA - TRUE
+    // Select the DAITA entry.
     func testDirectOnlyOnDaitaOnForSinglehopWithDaitaRelay() throws {
         let constraints = RelayConstraints(
             exitLocations: .only(UserSelectedRelays(locations: [.hostname("es", "mad", "es1-wireguard")]))
@@ -144,6 +156,9 @@ class RelayPickingTests: XCTestCase {
         XCTAssertEqual(selectedRelays.exit.hostname, "es1-wireguard")
     }
 
+    // DAITA - ON, Direct only - OFF, Multihop - ON, Entry supports DAITA - TRUE
+    // Direct only is off, so we should automatically pick the entry that is closest to exit, ignoring
+    // selected multihop entry.
     func testDirectOnlyOffDaitaOnForMultihopWithDaitaRelay() throws {
         let constraints = RelayConstraints(
             entryLocations: .only(UserSelectedRelays(locations: [.hostname("us", "nyc", "us-nyc-wg-301")])),
@@ -163,6 +178,9 @@ class RelayPickingTests: XCTestCase {
         XCTAssertEqual(selectedRelays.exit.hostname, "se10-wireguard")
     }
 
+    // DAITA - ON, Direct only - OFF, Multihop - ON, Entry supports DAITA - FALSE
+    // Direct only is off, so we should automatically pick the entry that is closest to exit, ignoring
+    // selected multihop entry.
     func testDirectOnlyOffDaitaOnForMultihopWithoutDaitaRelay() throws {
         let constraints = RelayConstraints(
             entryLocations: .only(UserSelectedRelays(locations: [.hostname("se", "got", "se10-wireguard")])),
@@ -182,6 +200,8 @@ class RelayPickingTests: XCTestCase {
         XCTAssertEqual(selectedRelays.exit.hostname, "se10-wireguard")
     }
 
+    // DAITA - ON, Direct only - ON, Multihop - ON, Entry supports DAITA - FALSE
+    // Go into blocked state since Direct only requires a DAITA entry.
     func testDirectOnlyOnDaitaOnForMultihopWithoutDaitaRelay() throws {
         let constraints = RelayConstraints(
             entryLocations: .only(UserSelectedRelays(locations: [.hostname("se", "got", "se10-wireguard")])),
