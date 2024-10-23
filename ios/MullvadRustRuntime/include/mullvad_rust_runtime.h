d05d4fbc69594cbb21c00f2f9a94f7bd38218f72
Emīls <emils@mullvad.net>
2024-10-23 21:19:12 +0200

    Use IAN TCP connection for ephemeral peer exchange

--- ios/MullvadRustRuntime/include/mullvad_rust_runtime.h
+++ ios/MullvadRustRuntime/include/mullvad_rust_runtime.h
@@ -20,14 +20,30 @@ typedef uint8_t TunnelObfuscatorProtocol;
  */
 typedef struct EncryptedDnsProxyState EncryptedDnsProxyState;
 
+typedef struct ExchangeCancelToken ExchangeCancelToken;
+
 typedef struct ProxyHandle {
   void *context;
   uint16_t port;
 } ProxyHandle;
 
 typedef struct EphemeralPeerCancelToken {
-  void *context;
+  struct ExchangeCancelToken *cancel_token;
 } EphemeralPeerCancelToken;
+
+typedef struct WgTcpConnectionFuncs {
+  int32_t (*open_fn)(int32_t tunnelHandle, const char *address, uint64_t timeout);
+  int32_t (*close_fn)(int32_t tunnelHandle, int32_t socketHandle);
+  int32_t (*recv_fn)(int32_t tunnelHandle, int32_t socketHandle, uint8_t *data, int32_t len);
+  int32_t (*send_fn)(int32_t tunnelHandle, int32_t socketHandle, const uint8_t *data, int32_t len);
+} WgTcpConnectionFuncs;
+
+typedef struct EphemeralPeerParameters {
+  uint64_t peer_exchange_timeout;
+  bool enable_post_quantum;
+  bool enable_daita;
+  struct WgTcpConnectionFuncs funcs;
+} EphemeralPeerParameters;
 
 extern const uint16_t CONFIG_SERVICE_PORT;
 
@@ -84,7 +100,7 @@ int32_t encrypted_dns_proxy_stop(struct ProxyHandle *proxy_config);
  * `sender` must be pointing to a valid instance of a `EphemeralPeerCancelToken` created by the
  * `PacketTunnelProvider`.
  */
-void cancel_ephemeral_peer_exchange(const struct EphemeralPeerCancelToken *sender);
+void cancel_ephemeral_peer_exchange(struct EphemeralPeerCancelToken *sender);
 
 /**
  * Called by the Swift side to signal that the Rust `EphemeralPeerCancelToken` can be safely dropped
@@ -94,33 +110,7 @@ void cancel_ephemeral_peer_exchange(const struct EphemeralPeerCancelToken *sende
  * `sender` must be pointing to a valid instance of a `EphemeralPeerCancelToken` created by the
  * `PacketTunnelProvider`.
  */
-void drop_ephemeral_peer_exchange_token(const struct EphemeralPeerCancelToken *sender);
-
-/**
- * Called by Swift whenever data has been written to the in-tunnel TCP connection when exchanging
- * quantum-resistant pre shared keys, or ephemeral peers.
- *
- * If `bytes_sent` is 0, this indicates that the connection was closed or that an error occurred.
- *
- * # Safety
- * `sender` must be pointing to a valid instance of a `write_tx` created by the `IosTcpProvider`
- * Callback to call when the TCP connection has written data.
- */
-void handle_sent(uintptr_t bytes_sent, const void *sender);
-
-/**
- * Called by Swift whenever data has been read from the in-tunnel TCP connection when exchanging
- * quantum-resistant pre shared keys, or ephemeral peers.
- *
- * If `data` is null or empty, this indicates that the connection was closed or that an error
- * occurred. An empty buffer is sent to the underlying reader to signal EOF.
- *
- * # Safety
- * `sender` must be pointing to a valid instance of a `read_tx` created by the `IosTcpProvider`
- *
- * Callback to call when the TCP connection has received data.
- */
-void handle_recv(const uint8_t *data, uintptr_t data_len, const void *sender);
+void drop_ephemeral_peer_exchange_token(struct EphemeralPeerCancelToken *sender);
 
 /**
  * Entry point for requesting ephemeral peers on iOS.
@@ -132,29 +122,11 @@ void handle_recv(const uint8_t *data, uintptr_t data_len, const void *sender);
  * connection instances.
  * `cancel_token` should be owned by the caller of this function.
  */
-int32_t request_ephemeral_peer(const uint8_t *public_key,
-                               const uint8_t *ephemeral_key,
-                               const void *packet_tunnel,
-                               const void *tcp_connection,
-                               struct EphemeralPeerCancelToken *cancel_token,
-                               uint64_t peer_exchange_timeout,
-                               bool enable_post_quantum,
-                               bool enable_daita);
-
-/**
- * Called when there is data to send on the TCP connection.
- * The TCP connection must write data on the wire, then call the `handle_sent` function.
- */
-extern void swift_nw_tcp_connection_send(const void *connection,
-                                         const void *data,
-                                         uintptr_t data_len,
-                                         const void *sender);
-
-/**
- * Called when there is data to read on the TCP connection.
- * The TCP connection must read data from the wire, then call the `handle_read` function.
- */
-extern void swift_nw_tcp_connection_read(const void *connection, const void *sender);
+struct EphemeralPeerCancelToken *request_ephemeral_peer(const uint8_t *public_key,
+                                                        const uint8_t *ephemeral_key,
+                                                        const void *packet_tunnel,
+                                                        int32_t tunnel_handle,
+                                                        struct EphemeralPeerParameters peer_parameters);
 
 /**
  * Called when the preshared post quantum key is ready,
--- mullvad-ios/src/ephemeral_peer_proxy/mod.rs
+++ mullvad-ios/src/ephemeral_peer_proxy/mod.rs
@@ -1,58 +1,66 @@
 #![cfg(target_os = "ios")]
-pub mod ios_runtime;
+pub mod peer_exchange;
 pub mod ios_tcp_connection;
 
-use ios_runtime::run_ephemeral_peer_exchange;
-use ios_tcp_connection::ConnectionContext;
+use peer_exchange::EphemeralPeerExchange;
 use libc::c_void;
-use std::sync::{Arc, Mutex, Weak};
-use tokio::sync::mpsc;
 
 use std::sync::Once;
 static INIT_LOGGING: Once = Once::new();
 
-#[repr(C)]
-pub struct EphemeralPeerCancelToken {
-    // Must keep a pointer to a valid std::sync::Arc<tokio::mpsc::UnboundedSender>
-    pub context: *mut c_void,
-}
-
+#[derive(Clone)]
 pub struct PacketTunnelBridge {
     pub packet_tunnel: *const c_void,
-    pub tcp_connection: *const c_void,
+    pub tunnel_handle: i32,
 }
 
+// # Safety
+// This is safe as long as the PacketTunnel class outlives the instance of the PacketTunnelBridge,
+// and thus the ephemeral peer exchange. Since the peer exchange takes place in the packet tunnel
+// process on iOS, it is certain _enough_ this will be the case.
+// It is safe to implement Send for PacketTunnelBridge because the packet_tunnel
+unsafe impl Send for PacketTunnelBridge {}
+
+#[repr(C)]
+#[derive(Clone,Copy)]
 pub struct EphemeralPeerParameters {
     pub peer_exchange_timeout: u64,
     pub enable_post_quantum: bool,
     pub enable_daita: bool,
+    pub funcs: ios_tcp_connection::WgTcpConnectionFuncs,
+}
+
+#[repr(C)]
+pub struct EphemeralPeerCancelToken {
+    cancel_token: *mut peer_exchange::ExchangeCancelToken,
 }
 
 impl EphemeralPeerCancelToken {
+    fn new(cancel_token: peer_exchange::ExchangeCancelToken) -> Self {
+        let ptr = Box::into_raw(Box::new(cancel_token));
+        Self { cancel_token: ptr }
+    }
+
     /// # Safety
     /// This function can only be called when the context pointer is valid.
     unsafe fn cancel(&self) {
-        // # Safety
-        // Try to take the value, if there is a value, we can safely send the message, otherwise,
-        // assume it has been dropped and nothing happens
-        let connection_context: Arc<Mutex<ConnectionContext>> =
-            unsafe { Arc::from_raw(self.context as _) };
-        if let Ok(mut connection) = connection_context.lock() {
-            connection.shutdown();
-        }
+        let token = unsafe { std::ptr::read(self.cancel_token) };
+        token.cancel();
 
-        // Call std::mem::forget here to avoid dropping the channel.
-        std::mem::forget(connection_context);
+        std::mem::forget(token);
+    }
+
+    fn to_ptr(self) -> *mut Self {
+        let boxed = Box::new(self);
+        Box::into_raw(boxed)
     }
 }
 
 impl Drop for EphemeralPeerCancelToken {
     fn drop(&mut self) {
-        let _: Arc<Mutex<ConnectionContext>> = unsafe { Arc::from_raw(self.context as _) };
+        let _token = unsafe { Box::from_raw(self.cancel_token) };
     }
 }
-
-unsafe impl Send for EphemeralPeerCancelToken {}
 
 /// Called by the Swift side to signal that the ephemeral peer exchange should be cancelled.
 /// After this call, the cancel token is no longer valid.
@@ -61,8 +69,8 @@ unsafe impl Send for EphemeralPeerCancelToken {}
 /// `sender` must be pointing to a valid instance of a `EphemeralPeerCancelToken` created by the
 /// `PacketTunnelProvider`.
 #[no_mangle]
-pub unsafe extern "C" fn cancel_ephemeral_peer_exchange(sender: *const EphemeralPeerCancelToken) {
-    let sender = unsafe { &*sender };
+pub unsafe extern "C" fn cancel_ephemeral_peer_exchange(sender: *mut EphemeralPeerCancelToken) {
+    let sender = unsafe { Box::from_raw(sender) };
     sender.cancel();
 }
 
@@ -74,51 +82,11 @@ pub unsafe extern "C" fn cancel_ephemeral_peer_exchange(sender: *const Ephemeral
 /// `PacketTunnelProvider`.
 #[no_mangle]
 pub unsafe extern "C" fn drop_ephemeral_peer_exchange_token(
-    sender: *const EphemeralPeerCancelToken,
+    sender: *mut EphemeralPeerCancelToken,
 ) {
-    let _sender = unsafe { std::ptr::read(sender) };
-}
 
-/// Called by Swift whenever data has been written to the in-tunnel TCP connection when exchanging
-/// quantum-resistant pre shared keys, or ephemeral peers.
-///
-/// If `bytes_sent` is 0, this indicates that the connection was closed or that an error occurred.
-///
-/// # Safety
-/// `sender` must be pointing to a valid instance of a `write_tx` created by the `IosTcpProvider`
-/// Callback to call when the TCP connection has written data.
-#[no_mangle]
-pub unsafe extern "C" fn handle_sent(bytes_sent: usize, sender: *const c_void) {
-    let weak_tx: Weak<mpsc::UnboundedSender<usize>> = unsafe { Weak::from_raw(sender as _) };
-    if let Some(send_tx) = weak_tx.upgrade() {
-        _ = send_tx.send(bytes_sent);
-    }
-}
-
-/// Called by Swift whenever data has been read from the in-tunnel TCP connection when exchanging
-/// quantum-resistant pre shared keys, or ephemeral peers.
-///
-/// If `data` is null or empty, this indicates that the connection was closed or that an error
-/// occurred. An empty buffer is sent to the underlying reader to signal EOF.
-///
-/// # Safety
-/// `sender` must be pointing to a valid instance of a `read_tx` created by the `IosTcpProvider`
-///
-/// Callback to call when the TCP connection has received data.
-#[no_mangle]
-pub unsafe extern "C" fn handle_recv(data: *const u8, mut data_len: usize, sender: *const c_void) {
-    let weak_tx: Weak<mpsc::UnboundedSender<Box<[u8]>>> = unsafe { Weak::from_raw(sender as _) };
-
-    if data.is_null() {
-        data_len = 0;
-    }
-    let mut bytes = vec![0u8; data_len];
-    if !data.is_null() {
-        std::ptr::copy_nonoverlapping(data, bytes.as_mut_ptr(), data_len);
-    }
-    if let Some(read_tx) = weak_tx.upgrade() {
-        _ = read_tx.send(bytes.into_boxed_slice());
-    }
+    // drop the cancel token
+    let _sender = unsafe { Box::from_raw(sender) };
 }
 
 /// Entry point for requesting ephemeral peers on iOS.
@@ -134,12 +102,9 @@ pub unsafe extern "C" fn request_ephemeral_peer(
     public_key: *const u8,
     ephemeral_key: *const u8,
     packet_tunnel: *const c_void,
-    tcp_connection: *const c_void,
-    cancel_token: *mut EphemeralPeerCancelToken,
-    peer_exchange_timeout: u64,
-    enable_post_quantum: bool,
-    enable_daita: bool,
-) -> i32 {
+    tunnel_handle: i32,
+    peer_parameters: EphemeralPeerParameters,
+) -> *mut EphemeralPeerCancelToken {
     INIT_LOGGING.call_once(|| {
         let _ = oslog::OsLogger::new("net.mullvad.MullvadVPN.TTCC")
             .level_filter(log::LevelFilter::Debug)
@@ -154,33 +119,19 @@ pub unsafe extern "C" fn request_ephemeral_peer(
         Err(err) => {
             log::error!("Failed to obtain a handle to a tokio runtime: {err}");
 
-            return -1;
+            return std::ptr::null_mut();
         }
     };
 
     let packet_tunnel_bridge = PacketTunnelBridge {
         packet_tunnel,
-        tcp_connection,
-    };
-    let peer_parameters = EphemeralPeerParameters {
-        peer_exchange_timeout,
-        enable_post_quantum,
-        enable_daita,
+        tunnel_handle,
     };
 
-    match unsafe {
-        run_ephemeral_peer_exchange(
-            pub_key,
-            eph_key,
-            packet_tunnel_bridge,
-            peer_parameters,
-            handle,
-        )
-    } {
-        Ok(token) => {
-            unsafe { std::ptr::write(cancel_token, token) };
-            0
-        }
-        Err(_) => -1,
-    }
+
+    let cancel_token =
+        EphemeralPeerExchange::new(pub_key, eph_key, packet_tunnel_bridge, peer_parameters)
+            .run(handle);
+
+    EphemeralPeerCancelToken::new(cancel_token).to_ptr()
 }
