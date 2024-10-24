2e7a742496c3593586435b7333f55e0af51c47f3
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-10-24 16:48:26 +0200

    Add always on warning to lan info dialog

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/info/LocalNetworkSharingInfoDialog.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/info/LocalNetworkSharingInfoDialog.kt
@@ -11,6 +11,7 @@ import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 import com.ramcosta.composedestinations.spec.DestinationStyle
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.compose.component.textResource
+import net.mullvad.mullvadvpn.constant.HTML_NEWLINE_STRING
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 
 @Preview
@@ -28,6 +29,12 @@ fun LocalNetworkSharingInfo(navigator: DestinationsNavigator) {
             buildString {
                 appendLine(stringResource(id = R.string.local_network_sharing_additional_info))
                 appendLine(textResource(id = R.string.local_network_sharing_ip_ranges))
+                // A html linebreak is specifically added since a normal linebreak is
+                // removed by the html parser
+                appendLine(HTML_NEWLINE_STRING)
+                appendLine(
+                    textResource(id = R.string.local_network_sharing_info_block_connections_warning)
+                )
             },
         onDismiss = dropUnlessResumed { navigator.navigateUp() },
     )
