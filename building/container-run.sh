fb0ee1686908566b481589e5615276cfcf7ec2cf
Joakim Hulthe <joakim.hulthe@mullvad.net>
2024-10-25 13:30:57 +0200

    cursed horror

--- building/container-run.sh
+++ building/container-run.sh
@@ -59,4 +59,4 @@ exec "$CONTAINER_RUNNER" run --rm -it \
     -v "$CARGO_REGISTRY_VOLUME_NAME:/root/.cargo/registry:Z" \
     "${optional_gradle_cache_volume[@]}" \
     "${optional_android_credentials_volume[@]}" \
-    "$container_image_name" bash -c "apt-get update -y && apt-get install -y libarchive-tools && $optional_mold $*"
+    "$container_image_name" bash -c "apt-get update -y &> /dev/null && apt-get install -y libarchive-tools &> /dev/null && $optional_mold $*"
