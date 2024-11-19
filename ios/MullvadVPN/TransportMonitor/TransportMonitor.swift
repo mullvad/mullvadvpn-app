f8830e6c9964f32138d76558e56c9d3c74ab7508
Bug Magnet <marco.nikic@mullvad.net>
2024-11-19 10:30:51 +0100

    Fix typo

--- ios/MullvadVPN/TransportMonitor/TransportMonitor.swift
+++ ios/MullvadVPN/TransportMonitor/TransportMonitor.swift
@@ -37,14 +37,14 @@ final class TransportMonitor: RESTTransportProvider {
             tunnel.status == .connecting || tunnel.status == .reasserting || tunnel.status == .connected
         }
 
-        if let tunnel, shouldByPassVPN(tunnel: tunnel) {
+        if let tunnel, shouldBypassVPN(tunnel: tunnel) {
             return PacketTunnelTransport(tunnel: tunnel)
         } else {
             return transportProvider.makeTransport()
         }
     }
 
-    private func shouldByPassVPN(tunnel: any TunnelProtocol) -> Bool {
+    private func shouldBypassVPN(tunnel: any TunnelProtocol) -> Bool {
         switch tunnel.status {
         case .connected:
             if case .error = tunnelManager.tunnelStatus.state {
