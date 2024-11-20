6768b0267ac8cd181e572874121e3ab33a933956
Oliver <oliver@mohlin.dev>
2024-11-20 09:20:43 +0100

    Move and rename common settings component

--- desktop/packages/mullvad-vpn/src/renderer/components/Layout.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/Layout.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 import { colors } from '../../config.json';
-import { measurements } from './common-styles';
+import { measurements, spacings } from './common-styles';
 import HeaderBar from './HeaderBar';
+import { NavigationScrollbars } from './NavigationBar';
 
 export const Header = styled(HeaderBar)({
   flex: 0,
@@ -16,10 +17,6 @@ export const Container = styled.div({
   overflow: 'hidden',
 });
 
-export const SettingsContainer = styled(Container)({
-  backgroundColor: colors.darkBlue,
-});
-
 export const Layout = styled.div({
   display: 'flex',
   flexDirection: 'column',
@@ -27,14 +24,36 @@ export const Layout = styled.div({
   height: '100vh',
 });
 
+export const SettingsContainer = styled(Container)({
+  backgroundColor: colors.darkBlue,
+});
+
+export const SettingsNavigationScrollbars = styled(NavigationScrollbars)({
+  flex: 1,
+});
+
+export const SettingsContent = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  flex: 1,
+  overflow: 'visible',
+  marginBottom: measurements.viewMargin,
+});
+
+export const SettingsStack = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: spacings.spacing5,
+});
+
 export const Footer = styled.div({
   display: 'flex',
   flexDirection: 'column',
   flex: 0,
-  paddingTop: '18px',
-  paddingLeft: measurements.viewMargin,
-  paddingRight: measurements.viewMargin,
-  paddingBottom: measurements.viewMargin,
+  padding: measurements.viewMargin,
+  [`${SettingsContent} &&`]: {
+    paddingBottom: 0,
+  },
 });
 
 export const Spacing = styled.div<{ height: string }>((props) => ({
--- desktop/packages/mullvad-vpn/src/renderer/components/SettingsStyles.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/SettingsStyles.tsx
@@ -3,34 +3,9 @@ import styled from 'styled-components';
 import * as AppButton from './AppButton';
 import * as Cell from './cell';
 import { measurements, spacings } from './common-styles';
-import { NavigationScrollbars } from './NavigationBar';
 
 export const StyledCellIcon = styled(Cell.UntintedIcon)({
   marginRight: spacings.spacing3,
-});
-
-export const StyledNavigationScrollbars = styled(NavigationScrollbars)({
-  flex: 1,
-});
-
-export const StyledContent = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  flex: 1,
-  overflow: 'visible',
-  marginBottom: measurements.viewMargin,
-});
-
-export const StyledSettingsContent = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: spacings.spacing6,
-});
-
-export const StyledSettingsGroups = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: spacings.spacing5,
 });
 
 export const StyledQuitButton = styled(AppButton.RedButton)({
