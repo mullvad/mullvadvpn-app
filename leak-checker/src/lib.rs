b69667346b7ffc9f034d67e8fb370c792281ab44
Joakim Hulthe <joakim@hulthe.net>
2024-11-21 16:15:20 +0100

    Finalize leak checker and implement in daemon

--- leak-checker/src/lib.rs
+++ leak-checker/src/lib.rs
@@ -1,5 +1,6 @@
-use std::net::IpAddr;
+use std::{fmt, net::IpAddr};
 
+#[cfg(feature = "am-i-mullvad")]
 pub mod am_i_mullvad;
 pub mod traceroute;
 mod util;
@@ -16,9 +17,42 @@ pub enum LeakInfo {
     /// Managed to reach another network node on the physical interface, bypassing firewall rules.
     NodeReachableOnInterface {
         reachable_nodes: Vec<IpAddr>,
-        interface: String,
+        interface: Interface,
     },
 
     /// Queried a <https://am.i.mullvad.net>, and was not mullvad.
+    #[cfg(feature = "am-i-mullvad")]
     AmIMullvad { ip: IpAddr },
+}
+
+#[derive(Clone)]
+pub enum Interface {
+    Name(String),
+
+    #[cfg(target_os = "windows")]
+    Luid(windows_sys::Win32::NetworkManagement::Ndis::NET_LUID_LH),
+
+    #[cfg(target_os = "macos")]
+    Index(std::num::NonZeroU32),
+}
+
+impl From<String> for Interface {
+    fn from(name: String) -> Self {
+        Interface::Name(name)
+    }
+}
+
+impl fmt::Debug for Interface {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Name(arg0) => f.debug_tuple("Name").field(arg0).finish(),
+
+            // SAFETY: u64 is valid for all bit patterns, so reading the union as a u64 is safe.
+            #[cfg(target_os = "windows")]
+            Self::Luid(arg0) => f.debug_tuple("Luid").field(&unsafe { arg0.Value }).finish(),
+
+            #[cfg(target_os = "macos")]
+            Self::Index(arg0) => f.debug_tuple("Luid").field(arg0).finish(),
+        }
+    }
 }
