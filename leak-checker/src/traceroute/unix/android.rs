ce54b8d94f22f52601bde54ebc29d541f1b900c8
Joakim Hulthe <joakim@hulthe.net>
2024-11-21 16:15:20 +0100

    Finalize leak checker and implement in daemon

---
+++ leak-checker/src/traceroute/unix/android.rs
@@ -0,0 +1,20 @@
+use socket2::Socket;
+
+use crate::{util::Ip, Interface};
+
+use super::{common::bind_socket_to_interface, linux, Traceroute};
+
+pub struct TracerouteAndroid;
+
+impl Traceroute for TracerouteAndroid {
+    type AsyncIcmpSocket = linux::AsyncIcmpSocketImpl;
+
+    fn bind_socket_to_interface(
+        socket: &Socket,
+        interface: &Interface,
+        ip_version: Ip,
+    ) -> anyhow::Result<()> {
+        // can't use the same method as desktop-linux here beacuse reasons
+        bind_socket_to_interface(socket, interface, ip_version)
+    }
+}
