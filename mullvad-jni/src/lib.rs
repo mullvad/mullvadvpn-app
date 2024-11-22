fb766fe8137518413ad68fc383c22216a1b37f57
David Lönnhager <david.l@mullvad.net>
2024-11-22 07:02:16 +0100

    fixup: clarify runtime drop behavior

--- mullvad-jni/src/lib.rs
+++ mullvad-jni/src/lib.rs
@@ -124,6 +124,7 @@ pub extern "system" fn Java_net_mullvad_mullvadvpn_service_MullvadDaemon_shutdow
         _ = context.runtime.block_on(context.running_daemon);
 
         // Dropping the tokio runtime will block if there are any tasks in flight.
+        // That is, until all async tasks yield *and* all blocking threads have stopped.
     }
 }
 
