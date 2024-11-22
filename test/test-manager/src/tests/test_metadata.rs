d9abb1c52634f03030497adfdaebb3a86c042043
Markus Pettersson <markus.pettersson@mullvad.net>
2024-11-22 21:29:06 +0100

    fixup! WIP Add location overrides per test to test-manager config

--- test/test-manager/src/tests/test_metadata.rs
+++ test/test-manager/src/tests/test_metadata.rs
@@ -1,13 +1,15 @@
 use super::TestWrapperFunction;
 use test_rpc::{meta::Os, mullvad_daemon::MullvadClientVersion};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct TestMetadata {
     pub name: &'static str,
     pub targets: &'static [Os],
     pub mullvad_client_version: MullvadClientVersion,
     pub func: TestWrapperFunction,
     pub priority: Option<i32>,
+    // TODO: Document
+    pub location: Option<Vec<String>>,
 }
 
 // Register our test metadata struct with inventory to allow submitting tests of this type.
