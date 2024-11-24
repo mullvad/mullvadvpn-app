e76b2932e833d661e5dae458dded4345d1d20e2c
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-11-24 23:18:04 +0100

    Implement multihop

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/WireguardConstraintsRepository.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/WireguardConstraintsRepository.kt
@@ -1,11 +1,29 @@
 package net.mullvad.mullvadvpn.repository
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.stateIn
 import net.mullvad.mullvadvpn.lib.daemon.grpc.ManagementService
 import net.mullvad.mullvadvpn.lib.model.Constraint
 import net.mullvad.mullvadvpn.lib.model.Port
+import net.mullvad.mullvadvpn.lib.model.RelayItemId
 
-class WireguardConstraintsRepository(private val managementService: ManagementService) {
+class WireguardConstraintsRepository(
+    private val managementService: ManagementService,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
+) {
+    val wireguardConstraints =
+        managementService.settings
+            .mapNotNull { it.relaySettings.relayConstraints.wireguardConstraints }
+            .stateIn(CoroutineScope(dispatcher), SharingStarted.Eagerly, null)
+
     suspend fun setWireguardPort(port: Constraint<Port>) = managementService.setWireguardPort(port)
 
     suspend fun setMultihop(enabled: Boolean) = managementService.setMultihop(enabled)
+
+    suspend fun setEntryLocation(relayItemId: RelayItemId) =
+        managementService.setEntryLocation(relayItemId)
 }
