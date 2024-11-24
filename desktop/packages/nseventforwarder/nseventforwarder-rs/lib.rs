6a79d89316226d7f286c4086bc2a3ef928f50b9d
Markus Pettersson <markus.pettersson@mullvad.net>
2024-11-24 20:24:12 +0100

    Clean up nseventfowrader code
    - Move `Arc` closer to actual use
    - Clearly separate the callback on nsveent from the nodejs callback
    - Name the NSEvent that macOS gives us

--- desktop/packages/nseventforwarder/nseventforwarder-rs/lib.rs
+++ desktop/packages/nseventforwarder/nseventforwarder-rs/lib.rs
@@ -2,6 +2,7 @@
 #![cfg(target_os = "macos")]
 #![warn(clippy::undocumented_unsafe_blocks)]
 
+use std::ptr::NonNull;
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread::JoinHandle;
 
@@ -45,38 +46,40 @@ impl NSEventForwarder {
 ///
 /// Returns a stop function to call when the original callback shouldn't be called anymore.
 fn start(mut cx: FunctionContext<'_>) -> JsResult<'_, JsFunction> {
-    // Set up neon stuff
-    let callback = cx.argument::<JsFunction>(0)?.root(&mut cx);
-    let callback: Arc<Root<JsFunction>> = Arc::new(callback);
+    // Set up neon stuff.
+    // These will be moved into the spawned thread
+    let nodejs_callback = cx.argument::<JsFunction>(0)?.root(&mut cx);
     let channel = cx.channel();
-
     // Start a long-running thread which handles incoming NS events
     // When a new event is received, call the callback passed to us from the JavaScript caller
     let (stop_tx, stop_rx) = mpsc::channel();
     let join_handle = std::thread::spawn(move || {
-        // Scaffolding for calling the JavaScript callback function
-        let call_callback = move || {
-            let cb = Arc::clone(&callback);
+        // Each time the nodejs callback is triggered, we need to reference the nodejs
+        // function reference. As such, we keep it from being garbage-collected with the
+        // Root handle type and allow sharing it with the RCBlock via an Arc.
+        let nodesjs_callback: Arc<Root<JsFunction>> = Arc::new(nodejs_callback);
+        // Create a callback which will be called on the registered NSEvents.
+        // When called schedules a closure to execute in nodejs thread that invoked start.
+        let nsevent_callback = move |_nsevent: NonNull<NSEvent>| {
+            let nodejs_callback = Arc::clone(&nodesjs_callback);
             channel.send(move |mut cx| {
                 let this = JsNull::new(&mut cx);
-                let _ = cb.to_inner(&mut cx).call(&mut cx, this, []);
+                let _ = nodejs_callback.to_inner(&mut cx).call(&mut cx, this, []);
                 Ok(())
             })
         };
         // Start monitoring incoming NS events
-        let block = RcBlock::new(move |_event| {
-            call_callback();
-        });
         // SAFETY: This function is trivially safe to call.
         // Note: Make sure to cancel this handler with [NSEvent::removeMonitor] to unregister the
         // listener.
         let mut handler = unsafe {
             NSEvent::addGlobalMonitorForEventsMatchingMask_handler(
                 NSEventMask::LeftMouseDown | NSEventMask::RightMouseDown,
-                &block,
+                &RcBlock::new(move |nsevent| {
+                    nsevent_callback(nsevent);
+                }),
             )
         };
-
         // Listen for stop signal
         let _ = stop_rx.recv();
         if let Some(handler) = handler.take() {
@@ -101,6 +104,8 @@ fn start(mut cx: FunctionContext<'_>) -> JsResult<'_, JsFunction> {
     let _ = nseventmonitor_context.insert(new_context);
     drop(nseventmonitor_context);
 
+    // Return a stop function to be invoked from the node runtime to deregister the NSEvent
+    // callback.
     JsFunction::new(&mut cx, stop)
 }
 
