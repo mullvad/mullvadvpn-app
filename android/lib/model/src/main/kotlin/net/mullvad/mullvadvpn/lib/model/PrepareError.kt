739ed04246d953010e6ecffc20b6fd93860dd09e
David Göransson <david.goransson@mullvad.net>
2024-11-26 12:52:27 +0100

    Clarify comments

--- android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/PrepareError.kt
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/PrepareError.kt
@@ -5,10 +5,10 @@ import android.content.Intent
 sealed interface PrepareResult
 
 sealed interface PrepareError : PrepareResult {
-    // Result from VpnService.prepare() being invoked with legacy VPN app has always-on
+    // Legacy VPN profile is active as Always-on
     data object OtherLegacyAlwaysOnVpn : PrepareError
 
-    // Prepare gives intent but there is other always VPN app
+    // Another VPN app is active as Always-on
     data class OtherAlwaysOnApp(val appName: String) : PrepareError
 
     data class NotPrepared(val prepareIntent: Intent) : PrepareError
