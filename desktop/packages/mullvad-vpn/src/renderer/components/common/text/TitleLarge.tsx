d3d07b3496c51d658538f7c8def278d64d82c102
Oliver <oliver@mohlin.dev>
2024-12-02 13:44:24 +0100

    Add text components based on typography tokens

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/common/text/TitleLarge.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+import { typography } from '../../../tokens';
+import { Text } from './Text';
+export const TitleLarge = styled(Text)({
+  ...typography['titleLarge'],
+});
