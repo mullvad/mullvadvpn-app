fecdaa8428534136cffbc12fdc5ac40e245d2a57
Oliver <oliver@mohlin.dev>
2024-12-02 13:43:24 +0100

    Add spacing, color and typography tokens

---
+++ desktop/packages/mullvad-vpn/src/renderer/tokens/index.ts
@@ -0,0 +1,3 @@
+export * from './colors';
+export * from './spacings';
+export * from './typography';
---
+++ desktop/packages/mullvad-vpn/src/renderer/tokens/spacings.ts
@@ -0,0 +1,15 @@
+export enum Spacings {
+  spacing1 = '4px',
+  spacing2 = '6px',
+  spacing3 = '8px',
+  spacing4 = '12px',
+  spacing5 = '16px',
+  spacing6 = '24px',
+  spacing7 = '32px',
+  spacing8 = '40px',
+  spacing9 = '48px',
+  spacing10 = '56px',
+  spacing11 = '64px',
+  spacing12 = '72px',
+  spacing13 = '80px',
+}
