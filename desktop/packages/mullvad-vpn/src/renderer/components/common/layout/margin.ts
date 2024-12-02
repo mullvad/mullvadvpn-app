c7f3f55be60c9fb4eaedc3678ee48858a3f161f1
Oliver <oliver@mohlin.dev>
2024-12-02 13:45:06 +0100

    Add layout components based on spacing tokens

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/common/layout/margin.ts
@@ -0,0 +1,40 @@
+import { Spacings } from '../../../tokens';
+import { LayoutSpacings } from './types';
+
+export const all = (margin: Spacings) => ({ margin });
+
+const vertical = (margin: Spacings) => ({
+  ...top(margin),
+  ...bottom(margin),
+});
+
+const horizontal = (margin: Spacings) => ({
+  ...left(margin),
+  ...right(margin),
+});
+
+const top = (marginTop: Spacings) => ({
+  marginTop,
+});
+
+const right = (marginRight: Spacings) => ({
+  marginRight,
+});
+
+const bottom = (marginBottom: Spacings) => ({
+  marginBottom,
+});
+
+const left = (marginLeft: Spacings) => ({
+  marginLeft,
+});
+
+export const margin: Record<keyof LayoutSpacings, (value: Spacings) => React.CSSProperties> = {
+  all,
+  vertical,
+  horizontal,
+  top,
+  right,
+  bottom,
+  left,
+};
