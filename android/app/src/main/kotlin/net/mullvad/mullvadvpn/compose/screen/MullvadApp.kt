6d203f656d40ab6faaeb08cecf9bd28cf3f75e76
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-12-03 08:44:53 +0100

    Move dialog handling to activity

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/MullvadApp.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/MullvadApp.kt
@@ -1,19 +1,14 @@
 package net.mullvad.mullvadvpn.compose.screen
 
-import android.content.Intent
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.navigation.NavHostController
-import arrow.core.merge
 import co.touchlab.kermit.Logger
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.generated.NavGraphs
@@ -21,15 +16,6 @@ import com.ramcosta.composedestinations.generated.destinations.NoDaemonDestinati
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import com.ramcosta.composedestinations.utils.rememberDestinationsNavigator
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.filter
-import net.mullvad.mullvadvpn.compose.util.CreateVpnProfile
-import net.mullvad.mullvadvpn.lib.common.constant.KEY_REQUEST_VPN_PROFILE
-import net.mullvad.mullvadvpn.lib.common.util.prepareVpnSafe
-import net.mullvad.mullvadvpn.lib.model.PrepareError
-import net.mullvad.mullvadvpn.lib.model.Prepared
-import net.mullvad.mullvadvpn.util.getActivity
 import net.mullvad.mullvadvpn.viewmodel.DaemonScreenEvent
 import net.mullvad.mullvadvpn.viewmodel.MullvadAppViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -46,27 +32,6 @@ fun MullvadApp() {
     DisposableEffect(Unit) {
         navHostController.addOnDestinationChangedListener(mullvadAppViewModel)
         onDispose { navHostController.removeOnDestinationChangedListener(mullvadAppViewModel) }
-    }
-
-    // Get intents
-    val launchVpnPermission =
-        rememberLauncherForActivityResult(CreateVpnProfile()) { _ -> mullvadAppViewModel.connect() }
-    val activity = LocalContext.current.getActivity() as ComponentActivity
-    LaunchedEffect(navHostController) {
-        activity
-            .intents()
-            .filter { it.action == KEY_REQUEST_VPN_PROFILE }
-            .collect {
-                val prepareResult = activity.prepareVpnSafe().merge()
-                when (prepareResult) {
-                    is PrepareError.NotPrepared ->
-                        launchVpnPermission.launch(prepareResult.prepareIntent)
-                    // If legacy or other always on connect at let daemon generate a error state
-                    is PrepareError.OtherLegacyAlwaysOnVpn,
-                    is PrepareError.OtherAlwaysOnApp,
-                    Prepared -> mullvadAppViewModel.connect()
-                }
-            }
     }
 
     DestinationsNavHost(
@@ -92,14 +57,3 @@ fun MullvadApp() {
         }
     }
 }
-
-private fun ComponentActivity.intents() =
-    callbackFlow<Intent> {
-        send(intent)
-
-        val listener: (Intent) -> Unit = { trySend(it) }
-
-        addOnNewIntentListener(listener)
-
-        awaitClose { removeOnNewIntentListener(listener) }
-    }
