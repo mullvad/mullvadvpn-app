a7e4fd638778b6b888ffa4d309b4d485eaa7f386
Jon Petersson <jon.petersson@mullvad.net>
2024-12-04 10:18:22 +0100

    Add foundation for new connection view

---
+++ ios/MullvadVPN/Views/MainButton.swift
@@ -0,0 +1,34 @@
+//
+//  Untitled.swift
+//  MullvadVPN
+//
+//  Created by Jon Petersson on 2024-12-04.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainButton: View {
+    var text: String
+    var style: MainButtonStyle.Style
+
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action, label: {
+            HStack {
+                Spacer()
+                Text(text)
+                Spacer()
+            }
+        })
+        .buttonStyle(MainButtonStyle(style))
+        .cornerRadius(UIMetrics.MainButton.cornerRadius)
+    }
+}
+
+#Preview {
+    MainButton(text: "Connect", style: .default) {
+        print("Tapped")
+    }
+}
