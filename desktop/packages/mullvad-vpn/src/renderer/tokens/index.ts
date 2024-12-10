42781a1f9845b04bbcfb4c2c36fcd6d83f98d3d2
Oliver <oliver@mohlin.dev>
2024-12-10 09:59:43 +0100

    Add radius token

--- desktop/packages/mullvad-vpn/src/renderer/tokens/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/tokens/index.ts
@@ -1,3 +1,4 @@
 export * from './colors';
+export * from './radius';
 export * from './spacings';
 export * from './typography';
---
+++ desktop/packages/mullvad-vpn/src/renderer/tokens/radius.ts
@@ -0,0 +1,6 @@
+export enum Radius {
+  radius4 = '4px',
+  radius8 = '8px',
+  radius11 = '11px',
+  radius12 = '12px',
+}
