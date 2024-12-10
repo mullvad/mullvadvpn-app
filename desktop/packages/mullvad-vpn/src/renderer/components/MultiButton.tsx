662eae9872baf4dddc08e668e1fbacd40111a36e
Oliver <oliver@mohlin.dev>
2024-12-10 14:25:11 +0100

    Use Button component in connection panel

--- desktop/packages/mullvad-vpn/src/renderer/components/MultiButton.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/MultiButton.tsx
@@ -1,42 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SIDE_BUTTON_WIDTH = 44;
+import { ButtonProps } from './common/molecules';
 
 const ButtonRow = styled.div({
   display: 'flex',
-  flexDirection: 'row',
+  gap: '1px',
 });
 
 const MainButton = styled.button({
-  display: 'flex',
-  flex: 1,
   borderTopRightRadius: 0,
   borderBottomRightRadius: 0,
+  paddingLeft: '44px',
+  '&:focus-visible': {
+    zIndex: 10,
+  },
 });
 
 const SideButton = styled.button({
-  display: 'flex',
   borderTopLeftRadius: 0,
   borderBottomLeftRadius: 0,
-  width: SIDE_BUTTON_WIDTH,
-  marginLeft: '1px !important',
+  '&:focus-visible': {
+    zIndex: 10,
+  },
 });
 
-export interface MultiButtonCompatibleProps {
-  className?: string;
-  textOffset?: number;
+interface MultiButtonProps {
+  mainButton: React.ComponentType<ButtonProps>;
+  sideButton: React.ComponentType<ButtonProps>;
 }
 
-interface IMultiButtonProps {
-  mainButton: React.ComponentType<MultiButtonCompatibleProps>;
-  sideButton: React.ComponentType<MultiButtonCompatibleProps>;
-}
-
-export function MultiButton(props: IMultiButtonProps) {
+export function MultiButton(props: MultiButtonProps) {
   return (
     <ButtonRow>
-      <MainButton as={props.mainButton} textOffset={SIDE_BUTTON_WIDTH + 1} />
+      <MainButton as={props.mainButton} />
       <SideButton as={props.sideButton} />
     </ButtonRow>
   );
