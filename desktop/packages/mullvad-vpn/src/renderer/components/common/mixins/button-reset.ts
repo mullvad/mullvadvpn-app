ffeae023c73a718728a7827d757d311106a00598
Oliver <oliver@mohlin.dev>
2024-12-10 14:09:42 +0100

    Add Button and IconButton components

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/common/mixins/button-reset.ts
@@ -0,0 +1,10 @@
+export const buttonReset = {
+  border: 'none',
+  padding: 0,
+  margin: 0,
+  font: 'inherit',
+  color: 'inherit',
+  textAlign: 'inherit',
+  lineHeight: 'inherit',
+  cursor: 'default',
+} as React.CSSProperties;
