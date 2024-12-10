0a0f42f05690594ad99ec21d156ab0c740a9b3f5
Oliver <oliver@mohlin.dev>
2024-12-10 14:17:38 +0100

    Add Container component

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/common/layout/Container.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Spacings } from '../../../tokens';
+import { Flex, FlexProps } from './Flex';
+
+export interface ContainerProps extends FlexProps {
+  size?: '3' | '4';
+  children: React.ReactNode;
+}
+
+const sizes: Record<'3' | '4', string> = {
+  '3': `calc(100% - ${Spacings.spacing6} * 2)`,
+  '4': `calc(100% - ${Spacings.spacing5} * 2)`,
+};
+
+const StyledFlex = styled(Flex)<{ $size: string }>((props) => ({
+  width: props.$size,
+  margin: 'auto',
+}));
+
+export const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
+  ({ size = '4', ...props }, ref) => {
+    return <StyledFlex ref={ref} $size={sizes[size]} {...props} />;
+  },
+);
+
+Container.displayName = 'Container';
