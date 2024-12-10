101406af03e85a35becec2ecfd3f1c439ee564e9
Oliver <oliver@mohlin.dev>
2024-12-10 14:09:42 +0100

    Add Button and IconButton components

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/common/molecules/IconButton.tsx
@@ -0,0 +1,65 @@
+import React, { forwardRef } from 'react';
+import styled from 'styled-components';
+
+import { Colors } from '../../../tokens';
+import ImageView from '../../ImageView';
+import { buttonReset } from '../mixins';
+
+export interface IconButtonProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'children'> {
+  variant?: 'primary' | 'secondary';
+  size?: 'small' | 'regular';
+  icon: string;
+}
+
+const variants = {
+  primary: {
+    background: Colors.white,
+    hover: Colors.white60,
+    disabled: Colors.white50,
+  },
+  secondary: {
+    background: Colors.white60,
+    hover: Colors.white80,
+    disabled: Colors.white50,
+  },
+} as const;
+
+const sizes = {
+  small: 16,
+  regular: 24,
+};
+
+const StyledButton = styled.button({
+  ...buttonReset,
+
+  background: 'transparent',
+  height: 'var(--size)',
+  width: 'var(--size)',
+  '&:focus-visible': {
+    outline: `2px solid ${Colors.white}`,
+    outlineOffset: '2px',
+    borderRadius: '100%',
+  },
+});
+
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ icon, variant = 'primary', size: sizeProp = 'regular', disabled, ...props }, ref) => {
+    const styles = variants[variant];
+    const size = sizes[sizeProp];
+    return (
+      <StyledButton ref={ref} disabled={disabled} {...props}>
+        <ImageView
+          source={icon}
+          tintColor={styles.background}
+          tintHoverColor={styles.hover}
+          disabled={disabled}
+          height={size}
+          width={size}
+        />
+      </StyledButton>
+    );
+  },
+);
+
+IconButton.displayName = 'Button';
