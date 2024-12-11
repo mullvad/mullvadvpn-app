559164a51b2008f5eb22c3a4cd5eee3b74ccf5e5
mojganii <mojgan.jelodar@codic.se>
2024-12-11 15:13:09 +0100

    Add FeaturesIndicatoresView

---
+++ ios/MullvadVPN/View controllers/Tunnel/FeatureIndicators/ChipsView/ChipViewModelProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  ChipViewModelProtocol.swift
+//  MullvadVPN
+//
+//  Created by Mojgan on 2024-12-05.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import SwiftUI
+protocol ChipViewModelProtocol: ObservableObject {
+    var chips: [ChipModel] { get }
+}
+
+class FeaturesIndicatoresMockViewModel: ChipViewModelProtocol {
+    @Published var chips: [ChipModel] = [
+        ChipModel(name: LocalizedStringKey("DAITA")),
+        ChipModel(name: LocalizedStringKey("Obfuscation")),
+        ChipModel(name: LocalizedStringKey("Quantum resistance")),
+        ChipModel(name: LocalizedStringKey("Multihop")),
+        ChipModel(name: LocalizedStringKey("DNS content blockers")),
+        ChipModel(name: LocalizedStringKey("Custom DNS")),
+        ChipModel(name: LocalizedStringKey("Server IP override")),
+    ]
+}
