ec92179b146508ec8483f2f0e7f4dd5a838917dd
Emīls <emils@mullvad.net>
2024-12-11 13:26:13 +0100

    Install go@1.21

--- ci/ios/create-vm/scripts/install-go.sh
+++ ci/ios/create-vm/scripts/install-go.sh
@@ -7,7 +7,7 @@ source ~/.bash_profile
 
 if command -v brew &>/dev/null
 then
-    echo >&1 "Installing go@1.20"
-    brew install go@1.20
-    echo "export PATH='/opt/homebrew/opt/go@1.20/bin:$PATH'" >> ~/.bash_profile
+    echo >&1 "Installing go@1.21"
+    brew install go@1.21
+    echo "export PATH='/opt/homebrew/opt/go@1.21/bin:$PATH'" >> ~/.bash_profile
 fi
