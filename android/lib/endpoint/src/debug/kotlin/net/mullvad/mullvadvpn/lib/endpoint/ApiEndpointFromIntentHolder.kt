f35e848ee075b8770735a87758c9fa2631dd071d
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-12-11 20:14:37 +0100

    Fixes based on feedback

--- android/lib/endpoint/src/debug/kotlin/net/mullvad/mullvadvpn/lib/endpoint/ApiEndpointFromIntentHolder.kt
+++ android/lib/endpoint/src/debug/kotlin/net/mullvad/mullvadvpn/lib/endpoint/ApiEndpointFromIntentHolder.kt
@@ -1,12 +1,10 @@
 package net.mullvad.mullvadvpn.lib.endpoint
 
-import android.content.Intent
-
 class ApiEndpointFromIntentHolder {
     var apiEndpointOverride: ApiEndpointOverride? = null
         private set
 
-    fun handleIntent(intent: Intent) {
-        apiEndpointOverride = intent.getApiEndpointConfigurationExtras()
+    fun setApiEndpointOverride(apiEndpointOverride: ApiEndpointOverride?) {
+        this.apiEndpointOverride = apiEndpointOverride
     }
 }
--- android/lib/endpoint/src/release/kotlin/net/mullvad/mullvadvpn/lib/endpoint/ApiEndpointFromIntentHolder.kt
+++ android/lib/endpoint/src/release/kotlin/net/mullvad/mullvadvpn/lib/endpoint/ApiEndpointFromIntentHolder.kt
@@ -1,12 +1,11 @@
 package net.mullvad.mullvadvpn.lib.endpoint
 
-import android.content.Intent
-
 // Overridding the API endpoint is not supported in release builds
 class ApiEndpointFromIntentHolder {
     val apiEndpointOverride: ApiEndpointOverride? = null
 
-    fun handleIntent(intent: Intent) {
+    @Suppress("UnusedParameter")
+    fun setApiEndpointOverride(apiEndpointOverride: ApiEndpointOverride?) {
         // No-op
     }
 }
