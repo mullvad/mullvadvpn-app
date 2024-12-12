bb3452008e445407f45da99ef8540863bd3a62ee
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2024-12-12 23:17:49 +0100

    Cleaned up some code

--- building/container-run.sh
+++ building/container-run.sh
@@ -15,7 +15,7 @@ CARGO_TARGET_VOLUME_NAME=${CARGO_TARGET_VOLUME_NAME:-"cargo-target"}
 CARGO_REGISTRY_VOLUME_NAME=${CARGO_REGISTRY_VOLUME_NAME:-"cargo-registry"}
 GRADLE_CACHE_VOLUME_NAME=${GRADLE_CACHE_VOLUME_NAME:-"gradle-cache"}
 ANDROID_CREDENTIALS_DIR=${ANDROID_CREDENTIALS_DIR:-""}
-CONTAINER_RUNNER=${CONTAINER_RUNNER:-"docker"}
+CONTAINER_RUNNER=${CONTAINER_RUNNER:-"podman"}
 # Temporarily do not use mold for linking by default due to it causing build errors.
 # There's a separate issue (DES-1177) to address this problem.
 # Build servers also opt out of this and instead use GNU ld.
@@ -34,10 +34,10 @@ case ${1-:""} in
     ;;
     android)
         container_image_name=$(cat "$SCRIPT_DIR/android-container-image.txt")
-        optional_gradle_cache_volume=(-v "$GRADLE_CACHE_VOLUME_NAME:/root/.gradle")
+        optional_gradle_cache_volume=(-v "$GRADLE_CACHE_VOLUME_NAME:/root/.gradle:Z")
 
         if [ -n "$ANDROID_CREDENTIALS_DIR" ]; then
-            optional_android_credentials_volume=(-v "$ANDROID_CREDENTIALS_DIR:$REPO_MOUNT_TARGET/android/credentials")
+            optional_android_credentials_volume=(-v "$ANDROID_CREDENTIALS_DIR:$REPO_MOUNT_TARGET/android/credentials:Z")
         fi
 
         shift 1
@@ -54,8 +54,9 @@ fi
 
 set -x
 exec "$CONTAINER_RUNNER" run --rm -it \
-    -v "$REPO_DIR:$REPO_MOUNT_TARGET" \
-    -v "$CARGO_TARGET_VOLUME_NAME:/cargo-target" \
-    -v "$CARGO_REGISTRY_VOLUME_NAME:/root/.cargo/registry" \
+    -v "$REPO_DIR:$REPO_MOUNT_TARGET:Z" \
+    -v "$CARGO_TARGET_VOLUME_NAME:/cargo-target:Z" \
+    -v "$CARGO_REGISTRY_VOLUME_NAME:/root/.cargo/registry:Z" \
     "${optional_gradle_cache_volume[@]}" \
+    "${optional_android_credentials_volume[@]}" \
     "$container_image_name" bash -c "$optional_mold $*"
