2470e1e2f1a0154ed48dcf4a7d015c7a222849b9
Andrew Bulhak <andrew.bulhak@mullvad.net>
2024-12-19 16:57:13 +0100

    Fix testVerifySettingsStillChanged to work with new UI

--- ios/MullvadVPNUITests/SettingsMigrationTests.swift
+++ ios/MullvadVPNUITests/SettingsMigrationTests.swift
@@ -175,7 +175,6 @@ class SettingsMigrationTests: BaseUITestCase {
             .verifyCustomWireGuardPortSelected(portNumber: wireGuardPort)
             .tapWireGuardObfuscationExpandButton()
             .verifyWireGuardObfuscationOnSelected()
-            .tapUDPOverTCPPortExpandButton()
             .verifyUDPOverTCPPort80Selected()
             .tapQuantumResistantTunnelExpandButton()
             .verifyQuantumResistantTunnelOnSelected()
