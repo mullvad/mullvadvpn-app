b9b7ed20c55f83543bdbccb18a91971a367f1343
David Göransson <david.goransson@mullvad.net>
2024-12-20 10:25:17 +0100

    Make UI state have constraints

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/FilterConstrainExtensions.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/FilterConstrainExtensions.kt
@@ -11,15 +11,9 @@ fun Ownership?.toOwnershipConstraint(): Constraint<Ownership> =
         else -> Constraint.Only(this)
     }
 
-fun Constraint<Providers>.toSelectedProviders(allProviders: List<ProviderId>): List<ProviderId> =
-    when (this) {
-        Constraint.Any -> allProviders
-        is Constraint.Only -> value.providers.toList()
-    }
-
 fun List<ProviderId>.toConstraintProviders(allProviders: List<ProviderId>): Constraint<Providers> =
     if (size == allProviders.size) {
         Constraint.Any
     } else {
-        Constraint.Only(Providers(toHashSet()))
+        Constraint.Only(this)
     }
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/RelayFilterUiState.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/RelayFilterUiState.kt
@@ -1,35 +1,43 @@
 package net.mullvad.mullvadvpn.compose.state
 
+import net.mullvad.mullvadvpn.lib.model.Constraint
 import net.mullvad.mullvadvpn.lib.model.Ownership
 import net.mullvad.mullvadvpn.lib.model.ProviderId
 
 data class RelayFilterUiState(
     private val providerToOwnerships: Map<ProviderId, Set<Ownership>> = emptyMap(),
-    val selectedOwnership: Ownership? = null,
-    val selectedProviders: List<ProviderId> = emptyList(),
+    val selectedOwnership: Constraint<Ownership> = Constraint.Any,
+    val selectedProviders: Constraint<List<ProviderId>> = Constraint.Any,
 ) {
     val allProviders: List<ProviderId> = providerToOwnerships.keys.toList().sorted()
 
     val selectableOwnerships: List<Ownership> =
-        if (selectedProviders.isEmpty()) {
-                Ownership.entries
-            } else {
-                providerToOwnerships
-                    .filterKeys { it in selectedProviders }
-                    .values
-                    .flatten()
-                    .distinct()
-            }
-            .sorted()
+        when (selectedProviders) {
+            Constraint.Any -> Ownership.entries
+            is Constraint.Only ->
+                if (selectedProviders.value.isEmpty()) {
+                    Ownership.entries
+                } else {
+                    providerToOwnerships
+                        .filterKeys { it in selectedProviders.value }
+                        .values
+                        .flatten()
+                        .distinct()
+                }
+        }.sorted()
 
     val selectableProviders: List<ProviderId> =
-        if (selectedOwnership != null) {
-            providerToOwnerships.filterValues { selectedOwnership in it }.keys.toList().sorted()
-        } else {
-            allProviders
+        when (selectedOwnership) {
+            Constraint.Any -> allProviders
+            is Constraint.Only ->
+                providerToOwnerships
+                    .filterValues { selectedOwnership.value in it }
+                    .keys
+                    .toList()
+                    .sorted()
         }
 
-    val isApplyButtonEnabled = selectedProviders.isNotEmpty()
+    val isApplyButtonEnabled = selectedProviders.getOrNull()?.isNotEmpty() != false
 
-    val isAllProvidersChecked = allProviders.size == selectedProviders.size
+    val isAllProvidersChecked = selectedProviders is Constraint.Any
 }
