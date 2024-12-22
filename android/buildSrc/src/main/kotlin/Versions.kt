db3c2caa2b4a772af58c165ffd9fca14ce9c5916
David Göransson <david.goransson@mullvad.net>
2024-12-22 21:00:29 +0100

    Bump junit to 5.11.4

--- android/buildSrc/src/main/kotlin/Versions.kt
+++ android/buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
     const val minSdkVersion = 26
     const val targetSdkVersion = 35
 
-    const val junitJupiter = "5.11.3"
+    const val junitJupiter = "5.11.4"
     const val junit5Android = "1.6.0"
     const val junit5Plugin = "1.11.2.0"
 }
