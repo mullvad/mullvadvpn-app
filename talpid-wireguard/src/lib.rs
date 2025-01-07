c66d8c2897e3259af968fb3aee65e58980ff4eaa
Markus Pettersson <markus.pettersson@mullvad.net>
2025-01-07 14:28:38 +0100

    Fix bug where `block_on` was called from an async context

--- talpid-wireguard/src/lib.rs
+++ talpid-wireguard/src/lib.rs
@@ -278,7 +278,13 @@ impl WireguardMonitor {
                     // timing out on Windows for 2024.9-beta1. These verbose data usage logs are
                     // a temporary measure to help us understand the issue. They can be removed
                     // if the issue is resolved.
-                    log_tunnel_data_usage(&config, &tunnel).await;
+                    if let Err(err) =
+                        tokio::task::spawn_blocking(move || log_tunnel_data_usage(&config, &tunnel))
+                            .await
+                    {
+                        log::error!("Failed to log tunnel data during setup phase");
+                        log::error!("{err}");
+                    }
                     return Err(e);
                 }
 
@@ -989,8 +995,12 @@ impl WireguardMonitor {
     }
 }
 
-async fn log_tunnel_data_usage(config: &Config, tunnel: &Arc<AsyncMutex<Option<TunnelType>>>) {
-    let tunnel = tunnel.lock().await;
+/// Log the tunnel stats from the current tunnel.
+///
+/// This will log the amount of outgoing and incoming data to and from the exit (and entry) relay
+/// so far.
+fn log_tunnel_data_usage(config: &Config, tunnel: &Arc<AsyncMutex<Option<TunnelType>>>) {
+    let tunnel = tunnel.blocking_lock();
     let Some(tunnel) = &*tunnel else { return };
     let Ok(tunnel_stats) = tunnel.get_tunnel_stats() else {
         return;
