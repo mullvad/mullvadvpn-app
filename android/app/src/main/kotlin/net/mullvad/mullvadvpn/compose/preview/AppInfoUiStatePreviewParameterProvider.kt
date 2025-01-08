8ef9492a3ac7ba91065f015de1e47755e2bae74e
David Göransson <david.goransson@mullvad.net>
2025-01-08 09:41:57 +0100

    Update changelog presentation

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/preview/AppInfoUiStatePreviewParameterProvider.kt
@@ -0,0 +1,19 @@
+package net.mullvad.mullvadvpn.compose.preview
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import net.mullvad.mullvadvpn.ui.VersionInfo
+import net.mullvad.mullvadvpn.viewmodel.AppInfoUiState
+
+class AppInfoUiStatePreviewParameterProvider : PreviewParameterProvider<AppInfoUiState> {
+    override val values: Sequence<AppInfoUiState> =
+        sequenceOf(
+            AppInfoUiState(
+                version = VersionInfo(currentVersion = "2024.9", isSupported = true),
+                isPlayBuild = true,
+            ),
+            AppInfoUiState(
+                version = VersionInfo(currentVersion = "2024.9", isSupported = false),
+                isPlayBuild = true,
+            ),
+        )
+}
--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/di/UiModule.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/di/UiModule.kt
@@ -43,6 +43,7 @@ import net.mullvad.mullvadvpn.usecase.FilterChipUseCase
 import net.mullvad.mullvadvpn.usecase.FilteredRelayListUseCase
 import net.mullvad.mullvadvpn.usecase.InternetAvailableUseCase
 import net.mullvad.mullvadvpn.usecase.LastKnownLocationUseCase
+import net.mullvad.mullvadvpn.usecase.NewChangelogNotificationUseCase
 import net.mullvad.mullvadvpn.usecase.NewDeviceNotificationUseCase
 import net.mullvad.mullvadvpn.usecase.OutOfTimeUseCase
 import net.mullvad.mullvadvpn.usecase.PaymentUseCase
@@ -127,8 +128,8 @@ val uiModule = module {
     single { androidContext().assets }
     single { androidContext().contentResolver }
 
-    single { ChangelogRepository(get()) }
-    single { UserPreferencesRepository(get()) }
+    single { ChangelogRepository(get(), get(), get()) }
+    single { UserPreferencesRepository(get(), get()) }
     single { SettingsRepository(get()) }
     single { MullvadProblemReport(get(), get<DaemonConfig>().apiEndpointOverride, get()) }
     single { RelayOverridesRepository(get()) }
@@ -152,6 +153,7 @@ val uiModule = module {
     single { TunnelStateNotificationUseCase(get()) }
     single { VersionNotificationUseCase(get(), BuildConfig.ENABLE_IN_APP_VERSION_NOTIFICATIONS) }
     single { NewDeviceNotificationUseCase(get(), get()) }
+    single { NewChangelogNotificationUseCase(get()) }
     single { OutOfTimeUseCase(get(), get(), MainScope()) }
     single { InternetAvailableUseCase(get()) }
     single { SystemVpnSettingsAvailableUseCase(androidContext()) }
@@ -166,7 +168,7 @@ val uiModule = module {
     single { SelectedLocationUseCase(get(), get()) }
     single { FilterChipUseCase(get(), get(), get(), get()) }
 
-    single { InAppNotificationController(get(), get(), get(), get(), MainScope()) }
+    single { InAppNotificationController(get(), get(), get(), get(), get(), MainScope()) }
 
     single<IChangelogDataProvider> { ChangelogDataProvider(get()) }
 
@@ -188,12 +190,13 @@ val uiModule = module {
 
     // View models
     viewModel { AccountViewModel(get(), get(), get(), IS_PLAY_BUILD) }
-    viewModel { ChangelogViewModel(get(), get()) }
+    viewModel { ChangelogViewModel(get(), get(), get()) }
     viewModel {
         AppInfoViewModel(get(), get(), get(), IS_PLAY_BUILD, get(named(SELF_PACKAGE_NAME)))
     }
     viewModel {
         ConnectViewModel(
+            get(),
             get(),
             get(),
             get(),
