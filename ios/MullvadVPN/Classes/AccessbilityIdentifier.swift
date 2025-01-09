d2e1fee4ace1b32994283ec4c572f1f7338fe2f6
Emīls <emils@mullvad.net>
2025-01-09 12:56:03 +0100

    Remove identifier

--- ios/MullvadVPN/Classes/AccessbilityIdentifier.swift
+++ ios/MullvadVPN/Classes/AccessbilityIdentifier.swift
@@ -38,7 +38,6 @@ public enum AccessibilityIdentifier: Equatable {
     case loginTextFieldButton
     case logoutButton
     case purchaseButton
-    case storeKit2Button
     case redeemVoucherButton
     case restorePurchasesButton
     case secureConnectionButton
