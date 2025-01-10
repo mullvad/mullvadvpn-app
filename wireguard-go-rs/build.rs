6cf2af0d9c959e8b5ef35feb52a8a1558e68d434
David Lönnhager <david.l@mullvad.net>
2025-01-10 09:38:31 +0100

    Build wireguard-go via wireguard-go-rs on Windows

--- wireguard-go-rs/build.rs
+++ wireguard-go-rs/build.rs
@@ -1,6 +1,8 @@
 use std::{
     borrow::BorrowMut,
     env,
+    fs::{self, File},
+    io::{BufRead, BufReader, BufWriter, Write},
     path::{Path, PathBuf},
     process::Command,
     str,
@@ -18,23 +20,24 @@ fn main() -> anyhow::Result<()> {
     println!("cargo::rerun-if-changed=libwg");
 
     match target_os.as_str() {
-        "linux" => build_static_lib(Os::Linux, true)?,
-        "macos" => build_static_lib(Os::MacOs, true)?,
+        "windows" => build_desktop_lib(Os::Windows, true)?,
+        "linux" => build_desktop_lib(Os::Linux, true)?,
+        "macos" => build_desktop_lib(Os::MacOs, true)?,
         "android" => build_android_dynamic_lib(true)?,
-        // building wireguard-go-rs for windows is not implemented
         _ => {}
     }
 
     Ok(())
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 enum Os {
+    Windows,
     MacOs,
     Linux,
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 enum Arch {
     Amd64,
     Arm64,
@@ -64,7 +67,9 @@ impl AndroidTarget {
 fn host_os() -> anyhow::Result<Os> {
     // this ugliness is a limitation of rust, where we can't directly
     // access the target triple of the build script.
-    if cfg!(target_os = "linux") {
+    if cfg!(target_os = "windows") {
+        Ok(Os::Windows)
+    } else if cfg!(target_os = "linux") {
         Ok(Os::Linux)
     } else if cfg!(target_os = "macos") {
         Ok(Os::MacOs)
@@ -83,8 +88,8 @@ fn host_arch() -> anyhow::Result<Arch> {
     }
 }
 
-/// Compile libwg as a static library and place it in `OUT_DIR`.
-fn build_static_lib(target_os: Os, daita: bool) -> anyhow::Result<()> {
+/// Compile libwg as a library and place it in `OUT_DIR`.
+fn build_desktop_lib(target_os: Os, daita: bool) -> anyhow::Result<()> {
     let out_dir = env::var("OUT_DIR").context("Missing OUT_DIR")?;
     let target_arch =
         env::var("CARGO_CFG_TARGET_ARCH").context("Missing 'CARGO_CFG_TARGET_ARCH")?;
@@ -95,14 +100,8 @@ fn build_static_lib(target_os: Os, daita: bool) -> anyhow::Result<()> {
         _ => bail!("Unsupported architecture: {target_arch}"),
     };
 
-    let out_file = format!("{out_dir}/libwg.a");
     let mut go_build = Command::new("go");
-    go_build
-        .args(["build", "-v", "-o", &out_file])
-        .args(["-buildmode", "c-archive"])
-        .args(if daita { &["--tags", "daita"][..] } else { &[] })
-        .env("CGO_ENABLED", "1")
-        .current_dir("./libwg");
+    go_build.env("CGO_ENABLED", "1").current_dir("./libwg");
 
     // are we cross compiling?
     let cross_compiling = host_os()? != target_os || host_arch()? != target_arch;
@@ -113,7 +112,58 @@ fn build_static_lib(target_os: Os, daita: bool) -> anyhow::Result<()> {
     };
 
     match target_os {
+        Os::Windows => {
+            let target_dir = Path::new(&out_dir)
+                .ancestors()
+                .nth(3)
+                .context("Failed to find target dir")?;
+
+            if daita {
+                build_shared_maybenot_lib(target_dir).context("Failed to build maybenot")?;
+            }
+
+            let dll_path = target_dir.join("libwg.dll");
+
+            println!("cargo::rerun-if-changed={}", dll_path.display());
+            println!(
+                "cargo::rerun-if-changed={}",
+                target_dir.join("libwg.lib").display()
+            );
+
+            go_build
+                .args(["build", "-v"])
+                .arg("-o")
+                .arg(&dll_path)
+                .args(if daita { &["--tags", "daita"][..] } else { &[] });
+            // Build dynamic lib
+            go_build.args(["-buildmode", "c-shared"]);
+
+            go_build.env("GOOS", "windows");
+
+            generate_windows_lib(target_arch, target_dir)?;
+
+            println!("cargo::rustc-link-search={}", target_dir.to_str().unwrap());
+            println!("cargo::rustc-link-lib=dylib=libwg");
+
+            // Build using zig
+            match target_arch {
+                Arch::Amd64 => {
+                    go_build.env("CC", "zig cc -target x86_64-windows");
+                }
+                Arch::Arm64 => {
+                    go_build.env("CC", "zig cc -target aarch64-windows");
+                }
+            }
+        }
         Os::Linux => {
+            let out_file = format!("{out_dir}/libwg.a");
+            go_build
+                .args(["build", "-v", "-o", &out_file])
+                .args(if daita { &["--tags", "daita"][..] } else { &[] });
+
+            // Build static lib
+            go_build.args(["-buildmode", "c-archive"]);
+
             go_build.env("GOOS", "linux");
 
             if cross_compiling {
@@ -122,8 +172,20 @@ fn build_static_lib(target_os: Os, daita: bool) -> anyhow::Result<()> {
                     Arch::Amd64 => bail!("cross-compiling to linux x86_64 is not implemented"),
                 };
             }
+
+            // make sure to link to the resulting binary
+            println!("cargo::rustc-link-search={out_dir}");
+            println!("cargo::rustc-link-lib=static=wg");
         }
         Os::MacOs => {
+            let out_file = format!("{out_dir}/libwg.a");
+            go_build
+                .args(["build", "-v", "-o", &out_file])
+                .args(if daita { &["--tags", "daita"][..] } else { &[] });
+
+            // Build static lib
+            go_build.args(["-buildmode", "c-archive"]);
+
             go_build.env("GOOS", "darwin");
 
             if cross_compiling {
@@ -144,14 +206,14 @@ fn build_static_lib(target_os: Os, daita: bool) -> anyhow::Result<()> {
                 go_build.env("CGO_LDFLAGS", format!("-isysroot {sdkroot} -arch {c_arch}"));
                 go_build.env("LD_LIBRARY_PATH", format!("{sdkroot}/usr/lib"));
             }
+
+            // make sure to link to the resulting binary
+            println!("cargo::rustc-link-search={out_dir}");
+            println!("cargo::rustc-link-lib=static=wg");
         }
     }
 
     exec(go_build)?;
-
-    // make sure to link to the resulting binary
-    println!("cargo::rustc-link-search={out_dir}");
-    println!("cargo::rustc-link-lib=static=wg");
 
     // if daita is enabled, also enable the corresponding rust feature flag
     if daita {
@@ -159,6 +221,181 @@ fn build_static_lib(target_os: Os, daita: bool) -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+// Build dynamically library for maybenot
+fn build_shared_maybenot_lib(out_dir: impl AsRef<Path>) -> anyhow::Result<()> {
+    let target_triple = env::var("TARGET").context("Missing 'TARGET'")?;
+    let profile = env::var("PROFILE").context("Missing 'PROFILE'")?;
+
+    let mut build_command = Command::new("cargo");
+
+    std::fs::create_dir_all("../build")?;
+
+    let mut tmp_build_dir = Path::new("../build").canonicalize()?;
+
+    // Strip \\?\ prefix. Note that doing this directly on Path/PathBuf fails
+    let path_str = tmp_build_dir.to_str().unwrap();
+    if path_str.starts_with(r"\\?\") {
+        tmp_build_dir = PathBuf::from(&path_str[4..]);
+    }
+
+    tmp_build_dir = tmp_build_dir.join("target");
+
+    build_command
+        .current_dir("./libwg/wireguard-go/maybenot/crates/maybenot-ffi")
+        .env("RUSTFLAGS", "-C metadata=maybenot-ffi -Ctarget-feature=+crt-static")
+        // Set temporary target dir to prevent deadlock
+        .env("CARGO_TARGET_DIR", &tmp_build_dir)
+        .arg("build")
+        .args(["--target", &target_triple]);
+
+    exec(build_command)?;
+
+    let artifacts_dir = tmp_build_dir.join(target_triple).join(profile);
+
+    // Copy library to actual target dir
+    for filename in ["maybenot_ffi.dll", "maybenot_ffi.lib"] {
+        fs::copy(
+            artifacts_dir.join(filename),
+            out_dir.as_ref().join(filename),
+        )
+        .with_context(|| format!("Failed to copy {filename}"))?;
+    }
+
+    Ok(())
+}
+
+/// Generate a library for the exported functions. Required for linking.
+/// This requires `lib.exe` in the path.
+fn generate_windows_lib(arch: Arch, out_dir: impl AsRef<Path>) -> anyhow::Result<()> {
+    let exports_def_path = out_dir.as_ref().join("exports.def");
+    generate_exports_def(&exports_def_path).context("Failed to generate exports.def")?;
+    generate_lib_from_exports_def(arch, &exports_def_path)
+        .context("Failed to generate lib from exports.def")
+}
+
+fn find_lib_exe() -> anyhow::Result<PathBuf> {
+    let msbuild_exe = find_msbuild_exe()?;
+
+    // Find lib.exe relative to msbuild.exe, in ../../../../ relative to msbuild
+    let search_path = msbuild_exe
+        .ancestors()
+        .nth(3)
+        .context("Unexpected msbuild.exe path")?;
+
+    let path_is_lib_exe = |file: &Path| file.ends_with("Hostx64/x64/lib.exe");
+
+    find_file(search_path, &path_is_lib_exe)?.context("No lib.exe relative to msbuild.exe")
+}
+
+/// Recursively search for file until 'condition' returns true
+fn find_file(
+    dir: impl AsRef<Path>,
+    condition: &impl Fn(&Path) -> bool,
+) -> anyhow::Result<Option<PathBuf>> {
+    for path in std::fs::read_dir(dir).context("Failed to read dir")? {
+        let entry = path.context("Failed to read dir entry")?;
+        let path = entry.path();
+        if path.is_dir() {
+            if let Some(result) = find_file(&path, condition)? {
+                // TODO: distinguish between err and no result
+                return Ok(Some(result));
+            }
+        }
+        if condition(&path) {
+            return Ok(Some(path.to_owned()));
+        }
+    }
+    Ok(None)
+}
+
+/// Find msbuild.exe in PATH
+fn find_msbuild_exe() -> anyhow::Result<PathBuf> {
+    let path = std::env::var_os("PATH").context("Missing PATH var")?;
+    std::env::split_paths(&path)
+        .find(|path| path.join("msbuild.exe").exists())
+        .context("msbuild.exe not found in PATH")
+}
+
+/// Generate exports.def from wireguard-go source
+fn generate_lib_from_exports_def(arch: Arch, exports_path: impl AsRef<Path>) -> anyhow::Result<()> {
+    let lib_path = exports_path
+        .as_ref()
+        .parent()
+        .context("Missing parent")?
+        .join("libwg.lib");
+    let path = exports_path.as_ref().to_str().context("Non-UTF8 path")?;
+
+    let lib_exe = find_lib_exe()?;
+
+    let mut lib_exe = Command::new(lib_exe);
+    lib_exe.args([
+        format!("/def:{path}"),
+        format!("/out:{}", lib_path.to_str().context("Non-UTF8 lib path")?),
+    ]);
+
+    match arch {
+        Arch::Amd64 => {
+            lib_exe.arg("/machine:X64");
+        }
+        Arch::Arm64 => {
+            lib_exe.arg("/machine:ARM64");
+        }
+    }
+
+    exec(lib_exe)?;
+
+    Ok(())
+}
+
+/// Generate exports.def from wireguard-go source
+fn generate_exports_def(exports_path: impl AsRef<Path>) -> anyhow::Result<()> {
+    let file = File::create(exports_path).context("Failed to create file")?;
+    let mut file = BufWriter::new(file);
+
+    writeln!(file, "LIBRARY libwg").context("Write LIBRARY statement")?;
+    writeln!(file, "EXPORTS").context("Write EXPORTS statement")?;
+
+    let mut libwg_exports = vec![];
+    for path in &[
+        "./libwg/libwg.go",
+        "./libwg/libwg_windows.go",
+        "./libwg/libwg_daita.go",
+    ] {
+        libwg_exports.extend(gather_exports(path).context("Failed to find exports")?);
+    }
+
+    for export in libwg_exports {
+        writeln!(file, "\t{export}").context("Failed to output exported function")?;
+    }
+
+    Ok(())
+}
+
+/// Return functions exported from .go file
+fn gather_exports(go_src_path: impl AsRef<Path>) -> anyhow::Result<Vec<String>> {
+    let go_src_path = go_src_path.as_ref();
+    let mut exports = vec![];
+    let file = File::open(go_src_path)
+        .with_context(|| format!("Failed to open go source: {}", go_src_path.display()))?;
+
+    for line in BufReader::new(file).lines() {
+        let line = line.context("Failed to read line in go src")?;
+        let mut words = line.split_whitespace();
+
+        // Is this an export?
+        let Some("//export") = words.next() else {
+            continue;
+        };
+
+        let exported_func = words
+            .next()
+            .with_context(|| format!("Invalid export on line: {line}"))?;
+        exports.push(exported_func.to_owned());
+    }
+
+    Ok(exports)
 }
 
 /// Compile libwg as a dynamic library for android and place it in [`android_output_path`].
