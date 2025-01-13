00d6caff7af106a2b7fc4a095d40954210901cb0
mojganii <mojgan.jelodar@codic.se>
2025-01-13 10:32:55 +0100

    Update Direct only pop up design

--- ios/MullvadVPN/Coordinators/Settings/DAITA/DAITASettingsPromptItem.swift
+++ ios/MullvadVPN/Coordinators/Settings/DAITA/DAITASettingsPromptItem.swift
@@ -23,7 +23,7 @@ enum DAITASettingsPromptItem: CustomStringConvertible {
             case .daita:
                 "DAITA"
             case .directOnly:
-                "direct only"
+                "Enable \"Direct only\""
             }
         }
     }
