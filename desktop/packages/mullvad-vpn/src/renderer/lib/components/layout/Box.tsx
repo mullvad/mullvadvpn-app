759107aeb529896d3c960ed186191443051f1665
Oliver <oliver@mohlin.dev>
2025-01-14 09:32:57 +0100

    Add Box component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/layout/Box.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+import { Layout, LayoutProps } from './Layout';
+
+interface BoxProps extends LayoutProps {
+  $width?: string;
+  $height?: string;
+  center?: boolean;
+}
+
+const StyledBox = styled(Layout)<BoxProps>((props) => ({
+  display: 'block',
+  boxSizing: 'border-box',
+  height: props.$height,
+  width: props.$width,
+}));
+
+const StyledCenter = styled.div({
+  display: 'grid',
+  placeItems: 'center',
+  height: '100%',
+  width: '100%',
+});
+
+export const Box = ({ center, children, ...props }: React.PropsWithChildren<BoxProps>) => {
+  const content = center ? <StyledCenter>{children}</StyledCenter> : children;
+  return <StyledBox {...props}>{content}</StyledBox>;
+};
