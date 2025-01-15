4ac2f07fa10ad80300ba46c0c23205b81321ea67
Markus Pettersson <markus.pettersson@mullvad.net>
2025-01-15 17:11:47 +0100

    Add more elaborate error message

--- wireguard-go-rs/build.rs
+++ wireguard-go-rs/build.rs
@@ -266,9 +266,9 @@ fn build_shared_maybenot_lib(out_dir: impl AsRef<Path>) -> anyhow::Result<()> {
         ("maybenot_ffi.dll", "maybenot_ffi.dll"),
         ("maybenot_ffi.dll.lib", "maybenot.lib"),
     ] {
+        let src = artifacts_dir.join(src_filename);
         let dest = out_dir.as_ref().join(dest_filename);
-        fs::copy(artifacts_dir.join(src_filename), &dest)
-            .with_context(|| format!("Failed to copy {src_filename}"))?;
+        fs::copy(&src, &dest).with_context(|| format!("Failed to copy {src_filename}",))?;
     }
 
     Ok(())
