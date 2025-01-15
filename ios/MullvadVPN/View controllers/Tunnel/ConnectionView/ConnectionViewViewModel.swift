aa9751170117272a2182bc2aeeb460ef668b8362
Andrew Bulhak <andrew.bulhak@mullvad.net>
2025-01-15 15:19:41 +0100

    Put custom list name or (unfmtd) location name into connect button

--- ios/MullvadVPN/View controllers/Tunnel/ConnectionView/ConnectionViewViewModel.swift
+++ ios/MullvadVPN/View controllers/Tunnel/ConnectionView/ConnectionViewViewModel.swift
@@ -7,6 +7,8 @@
 //
 
 import Combine
+import MullvadSettings
+import MullvadTypes
 import SwiftUI
 
 class ConnectionViewViewModel: ObservableObject {
@@ -28,6 +30,9 @@ class ConnectionViewViewModel: ObservableObject {
     @Published var outgoingConnectionInfo: OutgoingConnectionInfo?
     @Published var showsActivityIndicator = false
 
+    let relayConstraints: RelayConstraints
+    let customListRepository: CustomListRepositoryProtocol
+
     var combinedState: Publishers.CombineLatest<
         Published<TunnelStatus>.Publisher,
         Published<Bool>.Publisher
@@ -43,8 +48,24 @@ class ConnectionViewViewModel: ObservableObject {
         }
     }
 
-    init(tunnelStatus: TunnelStatus) {
+    var connectionName: String? {
+        if case let .only(loc) = relayConstraints.exitLocations {
+            loc.customListSelection.flatMap { customListRepository.fetch(by: $0.listId)?.name } ?? loc.locations.first?
+                .stringRepresentation
+        } else {
+            "Somewhere"
+        }
+//        nil
+    }
+
+    init(
+        tunnelStatus: TunnelStatus,
+        relayConstraints: RelayConstraints,
+        customListRepository: CustomListRepositoryProtocol
+    ) {
         self.tunnelStatus = tunnelStatus
+        self.relayConstraints = relayConstraints
+        self.customListRepository = customListRepository
     }
 
     func update(tunnelStatus: TunnelStatus) {
@@ -139,7 +160,7 @@ extension ConnectionViewViewModel {
     var localizedTitleForSelectLocationButton: LocalizedStringKey {
         switch tunnelStatus.state {
         case .disconnecting, .pendingReconnect, .disconnected, .waitingForConnectivity(.noNetwork):
-            LocalizedStringKey("Select location")
+            LocalizedStringKey(connectionName ?? "Select location")
         case .connecting, .connected, .reconnecting, .waitingForConnectivity(.noConnection),
              .negotiatingEphemeralPeer, .error:
             LocalizedStringKey("Switch location")
