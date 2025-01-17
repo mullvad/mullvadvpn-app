9484d8a40d0106acf78d3a7f15a23196673b2ff7
Oliver <oliver@mohlin.dev>
2025-01-17 09:59:25 +0100

    Fix icon buttons in select location entry view

--- desktop/packages/mullvad-vpn/src/renderer/components/cell/SideButton.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/cell/SideButton.tsx
@@ -4,22 +4,25 @@ import { colors } from '../../../config.json';
 import { measurements } from '../common-styles';
 import { buttonColor, ButtonColors } from './styles';
 
-export const SideButton = styled.button<ButtonColors>(buttonColor, {
-  position: 'relative',
-  alignSelf: 'stretch',
-  paddingLeft: measurements.horizontalViewMargin,
-  paddingRight: measurements.horizontalViewMargin,
-  border: 0,
+export const SideButton = styled.button<ButtonColors & { $noSeparator?: boolean }>(
+  buttonColor,
+  (props) => ({
+    position: 'relative',
+    alignSelf: 'stretch',
+    paddingLeft: measurements.horizontalViewMargin,
+    paddingRight: measurements.horizontalViewMargin,
+    border: 0,
 
-  '&&::before': {
-    content: '""',
-    position: 'absolute',
-    margin: 'auto',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    height: '50%',
-    width: '1px',
-    backgroundColor: colors.darkBlue,
-  },
-});
+    '&&::before': {
+      content: props.$noSeparator ? 'none' : '""',
+      position: 'absolute',
+      margin: 'auto',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      height: '50%',
+      width: '1px',
+      backgroundColor: colors.darkBlue,
+    },
+  }),
+);
