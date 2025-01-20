7ca198cb1f7462040c17e5c3377b89350f64d8db
Steffen Ernst <steffen.ernst@mullvad.net>
2025-01-20 11:42:40 +0100

    Simplify code to create purchase action sheet

--- ios/MullvadVPN/Coordinators/OutOfTimeCoordinator.swift
+++ ios/MullvadVPN/Coordinators/OutOfTimeCoordinator.swift
@@ -89,35 +89,7 @@ class OutOfTimeCoordinator: Coordinator, Presenting, @preconcurrency OutOfTimeVi
         products: [SKProduct],
         didRequestPurchase: @escaping (SKProduct) -> Void
     ) {
-        let localizedString = NSLocalizedString(
-            "ADD_TIME_BUTTON",
-            tableName: "Welcome",
-            value: "Add Time",
-            comment: ""
-        )
-        let alert = UIAlertController(title: localizedString, message: nil, preferredStyle: .actionSheet)
-        products.sortedByPrice().forEach { product in
-            guard let localizedTitle = product.customLocalizedTitle else {
-                return
-            }
-            let action = UIAlertAction(title: localizedTitle, style: .default, handler: { _ in
-                alert.dismiss(animated: true, completion: {
-                    didRequestPurchase(product)
-                })
-            })
-            action
-                .accessibilityIdentifier =
-                "\(AccessibilityIdentifier.purchaseButton.asString)_\(product.productIdentifier)"
-            alert.addAction(action)
-        }
-        let cancelAction = UIAlertAction(title: NSLocalizedString(
-            "PRODUCT_LIST_CANCEL_BUTTON",
-            tableName: "Welcome",
-            value: "Cancel",
-            comment: ""
-        ), style: .cancel)
-        cancelAction.accessibilityIdentifier = AccessibilityIdentifier.cancelPurchaseListButton.asString
-        alert.addAction(cancelAction)
+        let alert = UIAlertController.showInAppPurchaseAlert(products: products, didRequestPurchase: didRequestPurchase)
         presentationContext.present(alert, animated: true)
     }
 
