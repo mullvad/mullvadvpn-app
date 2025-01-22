875b700c33592448a0df85f738ab430e1472aa1b
Markus Pettersson <markus.pettersson@mullvad.net>
2025-01-22 13:09:30 +0100

    Merge branch 'remove-see-full-changelog-button-from-changelog-view-des-1651'

--- desktop/packages/mullvad-vpn/src/renderer/components/views/changelog/ChangelogView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/changelog/ChangelogView.tsx
@@ -1,15 +1,10 @@
-import { useCallback } from 'react';
 import styled from 'styled-components';
 
-import { links } from '../../../../config.json';
 import { messages } from '../../../../shared/gettext';
-import { useAppContext } from '../../../context';
-import { BodySmall, Button, Flex, TitleBig, TitleLarge } from '../../../lib/components';
-import { Container } from '../../../lib/components';
+import { BodySmall, Container, Flex, TitleBig, TitleLarge } from '../../../lib/components';
 import { Colors, Spacings } from '../../../lib/foundations';
 import { useHistory } from '../../../lib/history';
 import { useSelector } from '../../../redux/store';
-import ImageView from '../../ImageView';
 import { BackAction } from '../../KeyboardNavigation';
 import { Layout, SettingsContainer } from '../../Layout';
 import {
@@ -19,7 +14,6 @@ import {
   NavigationScrollbars,
   TitleBarItem,
 } from '../../NavigationBar';
-import SettingsHeader from '../../SettingsHeader';
 
 const StyledList = styled(Flex)({
   listStyleType: 'disc',
@@ -29,21 +23,11 @@ const StyledList = styled(Flex)({
   },
 });
 
-const StyledFooter = styled(Flex)({
-  position: 'sticky',
-  minHeight: '64px',
-  bottom: 0,
-  background: Colors.darkBlue,
-});
-
 export const ChangelogView = () => {
   const { pop } = useHistory();
-  const { openUrl } = useAppContext();
   const changelog = useSelector((state) => state.userInterface.changelog);
   const version = useSelector((state) => state.version.current);
 
-  const url = links.changelog;
-  const openDownloadLink = useCallback(() => openUrl(url), [openUrl, url]);
   return (
     <BackAction action={pop}>
       <Layout>
@@ -56,48 +40,37 @@ export const ChangelogView = () => {
             </NavigationBar>
 
             <NavigationScrollbars>
-              <SettingsHeader>
-                <TitleBig as={'h1'}>{messages.pgettext('changelog-view', 'What’s new')}</TitleBig>
-              </SettingsHeader>
-              <Flex $flexDirection="column" $gap={Spacings.spacing3}>
+              <Flex $flexDirection="column" $gap={Spacings.spacing6}>
                 <Container size="4">
-                  <TitleLarge as="h2">{version}</TitleLarge>
+                  <TitleBig as={'h1'}>{messages.pgettext('changelog-view', 'What’s new')}</TitleBig>
                 </Container>
-                <Container size="3" $flexDirection="column">
-                  {changelog.length ? (
-                    <StyledList as="ul" $flexDirection="column" $gap={Spacings.spacing5}>
-                      {changelog.map((item, i) => (
-                        <BodySmall as="li" key={i} color={Colors.white60}>
-                          {item}
-                        </BodySmall>
-                      ))}
-                    </StyledList>
-                  ) : (
-                    <BodySmall color={Colors.white60}>
-                      {messages.pgettext(
-                        'changelog-view',
-                        'No updates or changes were made in this release for this platform.',
-                      )}
-                    </BodySmall>
-                  )}
-                </Container>
+                <Flex $flexDirection="column" $gap={Spacings.spacing3}>
+                  <Container size="4">
+                    <TitleLarge as="h2">{version}</TitleLarge>
+                  </Container>
+                  <Container size="3" $flexDirection="column">
+                    {changelog.length ? (
+                      <StyledList as="ul" $flexDirection="column" $gap={Spacings.spacing5}>
+                        {changelog.map((item, i) => (
+                          <BodySmall as="li" key={i} color={Colors.white60}>
+                            {item}
+                          </BodySmall>
+                        ))}
+                      </StyledList>
+                    ) : (
+                      <BodySmall color={Colors.white60}>
+                        {messages.pgettext(
+                          'changelog-view',
+                          'No updates or changes were made in this release for this platform.',
+                        )}
+                      </BodySmall>
+                    )}
+                  </Container>
+                </Flex>
               </Flex>
             </NavigationScrollbars>
           </NavigationContainer>
         </SettingsContainer>
-        <StyledFooter $alignItems="center" $justifyContent="center">
-          <Button
-            onClick={openDownloadLink}
-            trailing={
-              <ImageView
-                source="icon-extLink"
-                aria-label={messages.pgettext('accessibility', 'Opens externally')}
-                tintColor={Colors.white}
-              />
-            }>
-            {messages.pgettext('changelog', 'See full changelog')}
-          </Button>
-        </StyledFooter>
       </Layout>
     </BackAction>
   );
