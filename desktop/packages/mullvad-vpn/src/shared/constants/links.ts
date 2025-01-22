2ee27df9ceccb6c52bdb6f9851f2b745590744ce
Oliver <oliver@mohlin.dev>
2025-01-22 08:36:20 +0100

    Move links to constants

---
+++ desktop/packages/mullvad-vpn/src/shared/constants/links.ts
@@ -0,0 +1,8 @@
+// This should only contain links to the mullvad website
+// No links to other websites should be added
+export const links = {
+  purchase: 'https://mullvad.net/account/',
+  faq: 'https://mullvad.net/help/tag/mullvad-app/',
+  privacyGuide: 'https://mullvad.net/help/first-steps-towards-online-privacy/',
+  download: 'https://mullvad.net/download/vpn/',
+} as const;
