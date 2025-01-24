70870af778b0ee6f87ce8752cff65c5a2d754a6e
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-01-24 11:15:52 +0100

    Bundle a pre-generated relay list instead of generating one
    The android app will try to bundle the relays.json
    found under dist-assets/relays/ if it exists.
    Otherwise it will rely on the relay list
    downloaded by the api.
    
    For alpha, beta and stable release builds it is required
    to have a bundled relays.json file.

--- android/service/src/main/kotlin/net/mullvad/mullvadvpn/service/util/ContextExtensions.kt
+++ android/service/src/main/kotlin/net/mullvad/mullvadvpn/service/util/ContextExtensions.kt
@@ -1,23 +1,31 @@
 package net.mullvad.mullvadvpn.service.util
 
 import android.content.Context
+import co.touchlab.kermit.Logger
 import java.io.File
+import java.io.FileNotFoundException
 import java.io.FileOutputStream
 
-fun Context.extractAndOverwriteIfAssetMoreRecent(assetName: String) {
+fun Context.extractAndOverwriteIfAssetMoreRecent(assetName: String, requireAssetFile: Boolean) {
     val forceOverwriteIfMoreRecent = lastUpdatedTime() > File(filesDir, assetName).lastModified()
     val destination = File(filesDir, assetName)
 
     if (!destination.exists() || forceOverwriteIfMoreRecent) {
-        extractFile(assetName, destination)
+        extractFile(assetName, destination, requireAssetFile)
     }
 }
 
 private fun Context.lastUpdatedTime(): Long =
     packageManager.getPackageInfo(packageName, 0).lastUpdateTime
 
-private fun Context.extractFile(asset: String, destination: File) {
-    val destinationStream = FileOutputStream(destination)
-    assets.open(asset).copyTo(destinationStream)
-    destinationStream.close()
+private fun Context.extractFile(asset: String, destination: File, requireAssetFile: Boolean) {
+    if (assets.list("")?.contains(asset) == true) {
+        val destinationStream = FileOutputStream(destination)
+        assets.open(asset).copyTo(destinationStream)
+        destinationStream.close()
+    } else if (requireAssetFile) {
+        throw FileNotFoundException("Asset $asset not found")
+    } else {
+        Logger.i("Asset $asset not found")
+    }
 }
