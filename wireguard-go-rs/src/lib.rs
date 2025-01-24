c97c5239f9319eef5abf3067861f39e8ede6b50c
Linus Färnstrand <linus@mullvad.net>
2025-01-24 10:32:39 +0100

    Remove unused function

--- wireguard-go-rs/src/lib.rs
+++ wireguard-go-rs/src/lib.rs
@@ -10,7 +10,7 @@
 
 use core::{
     ffi::{c_char, CStr},
-    mem::{ManuallyDrop, MaybeUninit},
+    mem::ManuallyDrop,
     slice,
 };
 use util::OnDrop;
@@ -234,25 +234,6 @@ impl Drop for Tunnel {
         if let Err(e) = result_from_code(code) {
             log::error!("Failed to stop wireguard-go tunnel,oerror_code={code} ({e:?})")
         }
-    }
-}
-
-/// Check whether `machines` contains a valid, LF-separated maybenot machines. Return an error
-/// otherwise.
-pub fn validate_maybenot_machines(machines: &CStr) -> Result<(), Error> {
-    use maybenot_ffi::MaybenotResult;
-
-    let mut framework = MaybeUninit::uninit();
-    // SAFETY: `machines` is a null-terminated string, and `&mut framework` is a valid pointer
-    let result =
-        unsafe { maybenot_ffi::maybenot_start(machines.as_ptr(), 0.0, 0.0, &mut framework) };
-
-    if result as u32 == MaybenotResult::Ok as u32 {
-        // SAFETY: `maybenot_start` succeeded, so `framework` points to a valid framework
-        unsafe { maybenot_ffi::maybenot_stop(framework.assume_init()) };
-        Ok(())
-    } else {
-        Err(Error::Other)
     }
 }
 
