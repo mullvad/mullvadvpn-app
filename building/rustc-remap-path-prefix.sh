4e270b2893232551c68f5d21b63796e7de9aa0be
Albin <albin@mullvad.net>
2025-01-27 14:41:31 +0100

    Add support for target dir in rustc remapping

--- building/rustc-remap-path-prefix.sh
+++ building/rustc-remap-path-prefix.sh
@@ -6,10 +6,18 @@
 
 set -eu
 
-SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 CARGO_HOME_PATH=${CARGO_HOME:-$HOME/.cargo}
 RUSTUP_HOME_PATH=${RUSTUP_HOME:-$HOME/.rustup}
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+CARGO_TARGET_DIR_PATH=${CARGO_TARGET_DIR:-$SOURCE_DIR/target}
 
-echo "--remap-path-prefix $CARGO_HOME_PATH=/CARGO_HOME \
+# The order is significant. Iterated over in reverse, so last argument is
+# treated as most significant. Since CARGO_TARGET_DIR_PATH might be located
+# under $SOURCE_DIR, it's important to keep it last.
+# See: https://github.com/rust-lang/rust/blob/55459598c250d985eb5f840306dfb59f267c03b6/compiler/rustc_span/src/source_map.rs#L1125-L1127
+echo "\
+--remap-path-prefix $CARGO_HOME_PATH=/CARGO_HOME \
 --remap-path-prefix $RUSTUP_HOME_PATH=/RUSTUP_HOME \
---remap-path-prefix $SOURCE_DIR=/SOURCE_DIR"
+--remap-path-prefix $SOURCE_DIR=/SOURCE_DIR \
+--remap-path-prefix $CARGO_TARGET_DIR_PATH=/CARGO_TARGET_DIR \
+" | xargs
