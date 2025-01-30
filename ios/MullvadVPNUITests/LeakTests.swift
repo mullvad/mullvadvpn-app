e4c7eaa7f6133435b0ca803716e8f82e7c067e76
Bug Magnet <marco.nikic@mullvad.net>
2025-01-30 14:30:13 +0100

    Remove LeakRule protocol for now, improve leak test names

--- ios/MullvadVPNUITests/LeakTests.swift
+++ ios/MullvadVPNUITests/LeakTests.swift
@@ -9,13 +9,21 @@
 import XCTest
 
 class LeakTests: LoggedInWithTimeUITestCase {
+    static let capturedStreamStartTimestamp: Double = 8
+    static let capturedStreamEndTimestamp: Double = 3
+
     override func tearDown() {
         FirewallClient().removeRules()
         super.tearDown()
     }
 
-    /// Send UDP traffic to a host, connect to relay and make sure while connected to relay no traffic  leaked went directly to the host
-    func testNoLeak() throws {
+    /// Send UDP traffic to a host, connect to relay and make sure while connected to relay no traffic leaked went directly to the host
+    func testConnectionStartedBeforeTunnelShouldNotLeakOutside() throws {
+        let skipReason = """
+        Connections started before the packet tunnel will leak as long as
+        includeAllNetworks is not set to true when starting the tunnel.
+        """
+        try XCTSkipIf(true, skipReason)
         let targetIPAddress = Networking.getAlwaysReachableIPAddress()
         startPacketCapture()
         let trafficGenerator = TrafficGenerator(destinationHost: targetIPAddress, port: 80)
@@ -39,12 +47,16 @@ class LeakTests: LoggedInWithTimeUITestCase {
 
         var capturedStreams = stopPacketCapture()
         // For now cut the beginning and and end of the stream to trim out the part where the tunnel connection was not up
-        capturedStreams = PacketCaptureClient.trimPackets(streams: capturedStreams, secondsStart: 8, secondsEnd: 3)
+        capturedStreams = PacketCaptureClient.trimPackets(
+            streams: capturedStreams,
+            secondsStart: Self.capturedStreamStartTimestamp,
+            secondsEnd: Self.capturedStreamEndTimestamp
+        )
         LeakCheck.assertNoLeaks(streams: capturedStreams, rules: [NoTrafficToHostLeakRule(host: targetIPAddress)])
     }
 
     /// Send UDP traffic to a host, connect to relay and then disconnect to intentionally leak traffic and make sure that the test catches the leak
-    func testShouldLeak() throws {
+    func testTrafficCapturedOutsideOfTunnelShouldLeak() throws {
         let targetIPAddress = Networking.getAlwaysReachableIPAddress()
         startPacketCapture()
         let trafficGenerator = TrafficGenerator(destinationHost: targetIPAddress, port: 80)
@@ -72,7 +84,6 @@ class LeakTests: LoggedInWithTimeUITestCase {
         // Keep the tunnel connection for a while
         RunLoop.current.run(until: .now + 5)
 
-        app.launch()
         TunnelControlPage(app)
             .tapDisconnectButton()
 
@@ -82,7 +93,11 @@ class LeakTests: LoggedInWithTimeUITestCase {
 
         var capturedStreams = stopPacketCapture()
         // For now cut the beginning and and end of the stream to trim out the part where the tunnel connection was not up
-        capturedStreams = PacketCaptureClient.trimPackets(streams: capturedStreams, secondsStart: 8, secondsEnd: 3)
+        capturedStreams = PacketCaptureClient.trimPackets(
+            streams: capturedStreams,
+            secondsStart: Self.capturedStreamStartTimestamp,
+            secondsEnd: Self.capturedStreamEndTimestamp
+        )
         LeakCheck.assertLeaks(streams: capturedStreams, rules: [NoTrafficToHostLeakRule(host: targetIPAddress)])
     }
 }
