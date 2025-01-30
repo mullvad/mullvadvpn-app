340c99e6c9e82b0fda64df031852ea290b9b2c04
Emīls <emils@mullvad.net>
2025-01-30 12:42:57 +0100

    Tidy up a bit?

---
+++ mullvad-ios/src/api/completion.rs
@@ -0,0 +1,40 @@
+use std::sync::{Arc, Mutex};
+
+use super::response::SwiftMullvadApiResponse;
+
+extern "C" {
+    pub fn completion_finish(
+        response: SwiftMullvadApiResponse,
+        completion_cookie: CompletionCookie,
+    );
+}
+
+#[repr(C)]
+pub struct CompletionCookie(pub *mut std::ffi::c_void);
+unsafe impl Send for CompletionCookie {}
+
+#[derive(Clone)]
+pub struct SwiftCompletionHandler {
+    inner: Arc<Mutex<Option<CompletionCookie>>>,
+}
+
+impl SwiftCompletionHandler {
+    pub fn new(cookie: CompletionCookie) -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(Some(cookie))),
+        }
+    }
+
+    pub fn finish(&self, response: SwiftMullvadApiResponse) {
+        let Ok(mut maybe_cookie) = self.inner.lock() else {
+            log::error!("Response handler panicked");
+            return;
+        };
+
+        let Some(cookie) = maybe_cookie.take() else {
+            return;
+        };
+
+        unsafe { completion_finish(response, cookie) };
+    }
+}
---
+++ mullvad-ios/src/api/mod.rs
@@ -0,0 +1,84 @@
+use std::{ffi::CStr, ptr::null_mut, sync::Arc};
+
+use completion::CompletionCookie;
+use mullvad_api::{
+    proxy::{ApiConnectionMode, StaticConnectionModeProvider}, rest::MullvadRestHandle, ApiEndpoint, ApiProxy, Runtime
+};
+use response::SwiftMullvadApiResponse;
+
+mod api;
+mod cancellation;
+mod completion;
+mod response;
+
+#[derive(Debug)]
+enum Error {
+    Rest(mullvad_api::rest::Error),
+    CString(std::ffi::NulError),
+}
+
+#[repr(C)]
+pub struct SwiftApiContext(*const ApiContext);
+impl SwiftApiContext {
+    pub fn new(context: ApiContext) -> SwiftApiContext {
+        SwiftApiContext(Arc::into_raw(Arc::new(context)))
+    }
+
+    pub unsafe fn to_rust_context(self) -> Arc<ApiContext> {
+        Arc::increment_strong_count(self.0);
+        Arc::from_raw(self.0)
+    }
+}
+
+struct ApiContext {
+    api_client: Runtime,
+    rest_client: MullvadRestHandle,
+}
+impl ApiContext {
+    pub fn rest_handle(&self) -> MullvadRestHandle {
+        self.rest_client.clone()
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn mullvad_api_init_new(host: *const u8, address: *const u8) -> SwiftApiContext {
+    let host = unsafe { CStr::from_ptr(host.cast()) };
+    let address = unsafe { CStr::from_ptr(address.cast()) };
+
+    let Ok(host) = host.to_str() else {
+        return SwiftApiContext(null_mut());
+    };
+
+    let Ok(address) = address.to_str() else {
+        return SwiftApiContext(null_mut());
+    };
+
+    let endpoint = ApiEndpoint {
+        host: Some(String::from(host)),
+        address: Some(address.parse().unwrap()),
+    };
+
+    let tokio_handle = match crate::mullvad_ios_runtime() {
+        Ok(tokio_handle) => tokio_handle,
+        Err(err) => {
+            log::error!("Failed to obtain a handle to a tokio runtime: {err}");
+            return SwiftApiContext(null_mut());
+        }
+    };
+
+    let api_context = tokio_handle.clone().block_on(async move {
+        // It is imperative that the REST runtime is created within an async context, otherwise
+        // ApiAvailability panics.
+        let api_client = mullvad_api::Runtime::new(tokio_handle, &endpoint);
+        let rest_client = api_client
+            .mullvad_rest_handle(StaticConnectionModeProvider::new(ApiConnectionMode::Direct));
+
+        ApiContext {
+            api_client,
+            rest_client,
+        }
+    });
+
+    SwiftApiContext::new(api_context)
+}
+
