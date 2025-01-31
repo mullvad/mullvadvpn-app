d076ed84e626ab05437fb09c1fe47d9970dc0f05
Oskar <oskar@mullvad.net>
2025-01-31 11:29:50 +0100

    Improve structure of prepare-release script

--- desktop/scripts/prepare-release.sh
+++ desktop/scripts/prepare-release.sh
@@ -5,10 +5,17 @@
 
 set -eu
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
+REPO_ROOT=../../
+
+source $REPO_ROOT/scripts/utils/log
+
 for argument in "$@"; do
     case "$argument" in
         -*)
-            echo "Unknown option \"$argument\""
+            log_error "Unknown option \"$argument\""
             exit 1
             ;;
         *)
@@ -17,39 +24,52 @@ for argument in "$@"; do
     esac
 done
 
-if [[ -z ${PRODUCT_VERSION+x} ]]; then
-    echo "Please give the release version as an argument to this script."
-    echo "For example: '2018.1-beta3' for a beta release, or '2018.6' for a stable one."
-    exit 1
-fi
+changes_path=../packages/mullvad-vpn/changes.txt
+changelog_path=$REPO_ROOT/CHANGELOG.md
+product_version_path=$REPO_ROOT/dist-assets/desktop-product-version.txt
 
-if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
-    echo "Dirty working directory! Will not accept that for an official release."
-    exit 1
-fi
+function checks {
+    if [[ -z ${PRODUCT_VERSION+x} ]]; then
+        log_error "Please give the release version as an argument to this script."
+        log_error "For example: '2018.1-beta3' for a beta release, or '2018.6' for a stable one."
+        exit 1
+    fi
 
-desktop_changes_path=desktop/packages/mullvad-vpn/changes.txt
-if [[ $(grep "CHANGE THIS BEFORE A RELEASE" $desktop_changes_path) != "" ]]; then
-    echo "It looks like you did not update $desktop_changes_path"
-    exit 1
-fi
+    if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
+        log_error "Dirty working directory! Will not accept that for an official release."
+        exit 1
+    fi
 
-if [[ $(grep "^## \\[$PRODUCT_VERSION\\] - " CHANGELOG.md) == "" ]]; then
-    echo "It looks like you did not add $PRODUCT_VERSION to the changelog?"
-    echo "Please make sure the changelog is up to date and correct before you proceed."
-    exit 1
-fi
+    if [[ $(grep "CHANGE THIS BEFORE A RELEASE" $changes_path) != "" ]]; then
+        log_error "It looks like you did not update $changes_path"
+        exit 1
+    fi
 
-echo "$PRODUCT_VERSION" > dist-assets/desktop-product-version.txt
-git commit -S -m "Update desktop app version to $PRODUCT_VERSION" \
-    dist-assets/desktop-product-version.txt
+    if [[ $(grep "^## \\[$PRODUCT_VERSION\\] - " $changelog_path) == "" ]]; then
+        log_error "It looks like you did not add $PRODUCT_VERSION to the changelog?"
+        log_error "Please make sure the changelog is up to date and correct before you proceed."
+        exit 1
+    fi
+}
 
-echo "Tagging current git commit with release tag $PRODUCT_VERSION..."
-git tag -s "$PRODUCT_VERSION" -m "$PRODUCT_VERSION"
+function update_product_version {
+    echo "$PRODUCT_VERSION" > $product_version_path
+    git commit -S -m "Update desktop app version to $PRODUCT_VERSION" \
+        $product_version_path
+}
 
-echo "================================================="
-echo "| DONE preparing for a release!                 |"
-echo "|    Now push the tag created by this script    |"
-echo "|    after you have verified it is correct:     |"
-echo "|        $ git push origin $PRODUCT_VERSION"
-echo "================================================="
+function create_tag {
+    echo "Tagging current git commit with release tag $PRODUCT_VERSION..."
+    git tag -s "$PRODUCT_VERSION" -m "$PRODUCT_VERSION"
+}
+
+checks
+update_product_version
+create_tag
+
+log_success "================================================="
+log_success "| DONE preparing for a release!                 |"
+log_success "|    Now push the tag created by this script    |"
+log_success "|    after you have verified it is correct:     |"
+log_success "|        $ git push origin $PRODUCT_VERSION"
+log_success "================================================="
