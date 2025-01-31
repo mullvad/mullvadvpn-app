31595f172368fad9468d3c4871e1e6286bae40ea
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-01-31 09:18:53 +0100

    Open the store page on fdroid version

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/BuildConstant.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/BuildConstant.kt
@@ -3,3 +3,4 @@ package net.mullvad.mullvadvpn.constant
 import net.mullvad.mullvadvpn.BuildConfig
 
 const val IS_PLAY_BUILD = BuildConfig.FLAVOR_billing == "play"
+const val IS_FDROID_BUILD = BuildConfig.BUILD_TYPE == "fdroid"
