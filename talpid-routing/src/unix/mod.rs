9b73fcd2a57b672b1585535d010258e044a60a49
Markus Pettersson <markus.pettersson@mullvad.net>
2025-02-04 16:09:01 +0100

    Request (poll) initial NetworkState at startup

--- talpid-routing/src/unix/mod.rs
+++ talpid-routing/src/unix/mod.rs
@@ -11,6 +11,8 @@ use futures::channel::{
     oneshot,
 };
 use std::sync::Arc;
+#[cfg(target_os = "android")]
+use talpid_types::android::AndroidContext;
 
 #[cfg(any(target_os = "linux", target_os = "macos"))]
 use futures::stream::Stream;
@@ -165,6 +167,7 @@ impl RouteManagerHandle {
     pub async fn spawn(
         #[cfg(target_os = "linux")] fwmark: u32,
         #[cfg(target_os = "linux")] table_id: u32,
+        #[cfg(target_os = "android")] android_context: AndroidContext,
     ) -> Result<Self, Error> {
         let (manage_tx, manage_rx) = mpsc::unbounded();
         let manage_tx = Arc::new(manage_tx);
@@ -175,6 +178,8 @@ impl RouteManagerHandle {
             table_id,
             #[cfg(target_os = "macos")]
             Arc::downgrade(&manage_tx),
+            #[cfg(target_os = "android")]
+            android_context,
         )
         .await?;
         tokio::spawn(manager.run(manage_rx));
