8eb3d4fb84902f9fe98da3407af091749f32b78b
Oliver <oliver@mohlin.dev>
2025-02-04 14:08:33 +0100

    Make IconButton composable

--- desktop/packages/mullvad-vpn/src/renderer/components/app-main-header/components/AppMainHeaderSettingsButton.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/app-main-header/components/AppMainHeaderSettingsButton.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 
 import { messages } from '../../../../shared/gettext';
-import { IconButtonProps, MainHeader } from '../../../lib/components';
+import { IconButton, IconButtonProps, MainHeader } from '../../../lib/components';
 import { transitions, useHistory } from '../../../lib/history';
 import { RoutePath } from '../../../lib/routes';
 
@@ -17,10 +17,8 @@ export function AppMainHeaderSettingsButton(props: MainHeaderSettingsButtonProps
   }, [history, props.disabled]);
 
   return (
-    <MainHeader.IconButton
-      icon="settings-filled"
-      onClick={openSettings}
-      aria-label={messages.gettext('Settings')}
-    />
+    <MainHeader.IconButton onClick={openSettings} aria-label={messages.gettext('Settings')}>
+      <IconButton.Icon icon="settings-filled" />{' '}
+    </MainHeader.IconButton>
   );
 }
