05c9fd021fb832534eed957b197f56fdc155a7ed
David Lönnhager <david.l@mullvad.net>
2025-02-05 19:51:44 +0100

    fixup! Add script for generating PNGs

--- installer-downloader/convert-assets.py
+++ installer-downloader/convert-assets.py
@@ -2,11 +2,6 @@
 
 # Convert svg assets to png assets
 
-import os
-
-os.add_dll_directory(r'C:\Program Files\UniConvertor-2.0rc5\dlls')
-os.environ['path'] += r';C:\Program Files\UniConvertor-2.0rc5\dlls'
-
 from cairosvg import svg2png
 
 svg2png(url="src/logo-icon.svg", write_to="src/logo-icon.png", output_width=32)
