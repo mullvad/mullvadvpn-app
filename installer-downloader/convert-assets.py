3ef6a270804aa9fa53f9655aa2845eaf4e00ce44
David Lönnhager <david.l@mullvad.net>
2025-02-05 19:52:09 +0100

    Move downloader resources to assets directory

--- installer-downloader/convert-assets.py
+++ installer-downloader/convert-assets.py
@@ -4,5 +4,5 @@
 
 from cairosvg import svg2png
 
-svg2png(url="src/logo-icon.svg", write_to="src/logo-icon.png", output_width=32)
-svg2png(url="src/logo-text.svg", write_to="src/logo-text.png", output_width=122)
+svg2png(url="assets/logo-icon.svg", write_to="assets/logo-icon.png", output_width=32)
+svg2png(url="assets/logo-text.svg", write_to="assets/logo-text.png", output_width=122)
