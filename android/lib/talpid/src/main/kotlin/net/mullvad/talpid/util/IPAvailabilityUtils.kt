1c6696e3b1ad908fd2967056de2ac3685e3577f5
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-02-05 10:50:07 +0100

    Fixes based on feedback

--- android/lib/talpid/src/main/kotlin/net/mullvad/talpid/util/IPAvailabilityUtils.kt
+++ android/lib/talpid/src/main/kotlin/net/mullvad/talpid/util/IPAvailabilityUtils.kt
@@ -14,10 +14,13 @@ object IPAvailabilityUtils {
         isIPAvailable(InetAddress.getByName(PUBLIC_IPV6_ADDRESS), protect)
 
     // Fake a connection to a public ip address using a UDP socket.
+    // We don't care about the result of the connection, only that it is possible to create.
+    // This is done this way since otherwise there is not way to check the availability of an ip
+    // version on the underlying network if the VPN is turned on.
     // Since we are protecting the socket it will use the underlying network regardless
     // if the VPN is turned on or not.
     // If the ip version is not supported on the underlying network it will trigger a socket
-    // exception. If not it should return the local ip address.
+    // exception. Otherwise we assume it is available.
     private inline fun <reified T : InetAddress> isIPAvailable(
         ip: T,
         protect: (socket: DatagramSocket) -> Unit,
@@ -26,7 +29,7 @@ object IPAvailabilityUtils {
         return try {
             protect(socket)
             socket.connect(InetSocketAddress(ip, 1))
-            socket.localAddress.hostAddress?.isNotEmpty() == true && socket.localAddress is T
+            true
         } catch (_: SocketException) {
             Logger.e("Socket could not be set up")
             false
