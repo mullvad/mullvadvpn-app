4435961a269da74714e57e29274a87b4084d1548
David Lönnhager <david.l@mullvad.net>
2025-02-06 09:54:57 +0100

    Build loader main on unsupported platforms

--- installer-downloader/src/main.rs
+++ installer-downloader/src/main.rs
@@ -1,23 +1,39 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use installer_downloader::controller;
-use installer_downloader::delegate;
-use installer_downloader::resource;
-
-#[cfg(target_os = "macos")]
-mod cacao_impl;
 
 #[cfg(target_os = "windows")]
 mod winapi_impl;
 
-fn main() {
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("failed to create tokio runtime");
-    let _guard = runtime.enter();
+#[cfg(target_os = "macos")]
+mod cacao_impl;
 
-    #[cfg(target_os = "macos")]
-    cacao_impl::main();
-    #[cfg(target_os = "windows")]
-    winapi_impl::main();
+#[cfg(any(target_os = "windows", target_os = "macos"))]
+mod inner {
+    pub use installer_downloader::controller;
+    pub use installer_downloader::delegate;
+    pub use installer_downloader::resource;
+
+    pub fn run() {
+        let rt = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .expect("failed to create tokio runtime");
+        let _guard = rt.enter();
+
+        #[cfg(target_os = "windows")]
+        super::winapi_impl::main();
+
+        #[cfg(target_os = "macos")]
+        super::cacao_impl::main();
+    }
+}
+
+#[cfg(not(any(target_os = "windows", target_os = "macos")))]
+mod inner {
+    pub fn run() {}
+}
+
+use inner::*;
+
+fn main() {
+    run()
 }
