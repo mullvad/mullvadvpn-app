bb578652a1c0d699b6d58d9f8c8aadcd78ba8ed1
David Lönnhager <david.l@mullvad.net>
2025-02-06 18:03:57 +0100

    Implement serde::Deserialize for Mullvad version type

--- mullvad-version/src/lib.rs
+++ mullvad-version/src/lib.rs
@@ -160,6 +160,16 @@ impl FromStr for Version {
     }
 }
 
+#[cfg(feature = "serde")]
+impl<'de> serde::Deserialize<'de> for Version {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: serde::Deserializer<'de>
+    {
+        let s = String::deserialize(deserializer)?;
+        FromStr::from_str(&s).map_err(serde::de::Error::custom)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
