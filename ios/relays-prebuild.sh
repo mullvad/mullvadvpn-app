d1f67e0cdd06e3603f6e3a2226261935a9777323
Jon Petersson <joneth@fastmail.com>
2025-02-10 14:05:51 +0100

    Change cached relay list and how it is updated

--- ios/relays-prebuild.sh
+++ ios/relays-prebuild.sh
@@ -13,15 +13,6 @@ else
   API_ENDPOINT="api.mullvad.net"
 fi
 
-if [ "$CONFIGURATION" == "Release" ]; then
-  echo "Remove relays file"
-  if [ -f "$RELAYS_FILE" ]; then
-    rm "$RELAYS_FILE"
-  else
-    echo "Relays file does not exist"
-  fi
-fi
-
 if [ ! -f "$RELAYS_FILE" ]; then
   echo "Download relays file"
   curl https://"$API_ENDPOINT"/app/v1/relays -s -o "$RELAYS_FILE"
