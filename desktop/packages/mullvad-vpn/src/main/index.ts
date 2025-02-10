533025bad984167b91297cd061c283b713dd8eb2
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-02-10 09:53:13 +0100

    Update import paths changed due to vite migration
    After switching to vite from gulp the paths are
    resolved differently. Previously the __dirname was
    pointing to the desktop folder, but now it is
    correctly resolved to the file's actual folder.

--- desktop/packages/mullvad-vpn/src/main/index.ts
+++ desktop/packages/mullvad-vpn/src/main/index.ts
@@ -82,7 +82,10 @@ const ALLOWED_PERMISSIONS = ['clipboard-sanitized-write'];
 const SANDBOX_DISABLED = app.commandLine.hasSwitch('no-sandbox');
 const UPDATE_NOTIFICATION_DISABLED = process.env.MULLVAD_DISABLE_UPDATE_NOTIFICATION === '1';
 
-const GEO_DIR = path.resolve(__dirname, '../../assets/geo');
+// TODO: Double check if we can use this import path
+// or if we should copy the files to a folder within
+// the mullvad-vpn workspace
+const GEO_DIR = path.resolve(__dirname, '../../../../dist-assets/geo');
 
 class ApplicationMain
   implements
