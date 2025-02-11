92ee07aaf4f551c05a353136e1430a3950262fac
Steffen Ernst <steffen.ernst@mullvad.net>
2025-02-11 13:47:56 +0100

    Fix network sharing text line breaks

--- ios/MullvadVPN/View controllers/VPNSettings/VPNSettingsViewController.swift
+++ ios/MullvadVPN/View controllers/VPNSettings/VPNSettingsViewController.swift
@@ -172,7 +172,6 @@ extension VPNSettingsViewController: @preconcurrency VPNSettingsDataSourceDelega
                         : "Disabling"
                 ) “Local network sharing” requires restarting the VPN connection, which will disconnect you and briefly expose your traffic. 
                 To prevent this, manually enable Airplane Mode and turn off Wi-Fi before continuing. 
-
                 Would you like to continue to enable “Local network sharing”?
                 """,
                 comment: ""
