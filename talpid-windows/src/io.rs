65c4bfc222588b6b0708389b69f25a01475657aa
Joakim Hulthe <joakim.hulthe@mullvad.net>
2025-02-11 15:39:22 +0100

    Add safety comments in talpid_windows::io

--- talpid-windows/src/io.rs
+++ talpid-windows/src/io.rs
@@ -4,18 +4,24 @@ use windows_sys::Win32::System::IO::OVERLAPPED;
 use crate::sync::Event;
 
 /// Abstraction over `OVERLAPPED`.
+///
+/// - https://learn.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-overlapped
+/// - https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-createeventw
 pub struct Overlapped {
     overlapped: OVERLAPPED,
     event: Option<Event>,
 }
 
+// SAFETY: Both OVERLAPPED and Event is used for async I/O, so this *should* be safe.
 unsafe impl Send for Overlapped {}
+// SAFETY: See above.
 unsafe impl Sync for Overlapped {}
 
 impl Overlapped {
     /// Creates an `OVERLAPPED` object with `hEvent` set.
     pub fn new(event: Option<Event>) -> io::Result<Self> {
         let mut overlapped = Overlapped {
+            // SAFETY: OVERLAPPED is a C struct and can safely be zeroed.
             overlapped: unsafe { mem::zeroed() },
             event: None,
         };
