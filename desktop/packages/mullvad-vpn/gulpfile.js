c6cb8ea9ee0ee51d076c23f3b148d564b73c9052
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-02-17 16:08:20 +0100

    Add gulp tasks to build standalone files
    Previously the standalone files were built at the
    same time as the application with the build npm
    script. However with the migration to vite they
    have to be created separately as they are
    not referenced in the application and as such
    will not be built, which is as expected.

--- desktop/packages/mullvad-vpn/gulpfile.js
+++ desktop/packages/mullvad-vpn/gulpfile.js
@@ -23,8 +23,15 @@ task('set-test-env', function (done) {
 task('clean', function (done) {
   fs.rm('./build', { recursive: true, force: true }, done);
 });
+task('clean-standalone', function (done) {
+  fs.rm('./build-standalone', { recursive: true, force: true }, done);
+});
 task('build-vite-prod', function (done) {
   execSync('npm run build:vite');
+  done();
+});
+task('build-standalone-transpile', function (done) {
+  execSync('npx tsc -p tsconfig.standalone.json');
   done();
 });
 task('build-proto', scripts.buildProto);
@@ -41,6 +48,7 @@ task(
 task('build', series('clean', 'set-prod-env', assets.copyAll, scripts.build));
 task('build-vite', series('clean', 'set-prod-env', 'build-vite-prod', assets.copyAllVite));
 task('build-test', series('clean', 'set-test-env', 'build-vite-prod', assets.copyAllVite));
+task('build-standalone', series('clean-standalone', 'set-prod-env', 'build-standalone-transpile'));
 task('pack-win', series('build-vite', dist.packWin));
 task('pack-linux', series('build-vite', dist.packLinux));
 task('pack-mac', series('build-vite', dist.packMac));
