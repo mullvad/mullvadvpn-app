64650e86451da32dcc4e89dc920f9cd3ccea41b9
David Lönnhager <david.l@mullvad.net>
2025-02-17 15:33:10 +0100

    Add improved error messages
    Co-authored-by: Joakim Hulthe <joakim.hulthe@mullvad.net>

--- installer-downloader/convert-assets.py
+++ installer-downloader/convert-assets.py
@@ -6,3 +6,4 @@ from cairosvg import svg2png
 
 svg2png(url="assets/logo-icon.svg", write_to="assets/logo-icon.png", output_width=32)
 svg2png(url="assets/logo-text.svg", write_to="assets/logo-text.png", output_width=122)
+svg2png(url="assets/alert-circle.svg", write_to="assets/alert-circle.png", output_width=32)
