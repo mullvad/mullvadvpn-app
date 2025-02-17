9d7593b3fe54b2c8c76a282b70c1a19902527622
David Lönnhager <david.l@mullvad.net>
2025-02-17 09:44:00 +0100

    Implement beta downloader

--- installer-downloader/src/resource.rs
+++ installer-downloader/src/resource.rs
@@ -16,6 +16,9 @@ pub const BETA_PREFACE_DESC: &str = "Want to try the new Beta version? ";
 /// Beta link text
 pub const BETA_LINK_TEXT: &str = "Click here!";
 
+/// Stable link text
+pub const STABLE_LINK_TEXT: &str = "Back to stable version";
+
 /// Dimensions of cancel button (including padding)
 pub const CANCEL_BUTTON_SIZE: (usize, usize) = (150, 40);
 
