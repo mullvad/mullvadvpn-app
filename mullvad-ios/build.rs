c571bbae70f32d8df6e4f3b617ed5a7c1b7495db
Emīls <emils@mullvad.net>
2025-02-20 10:26:16 +0100

    Add dep to talpid-futures

--- mullvad-ios/build.rs
+++ mullvad-ios/build.rs
@@ -1,16 +1,17 @@
-fn main() {
-    #[cfg(target_os = "macos")]
-    match std::env::var("TARGET").unwrap().as_str() {
-        "aarch64-apple-ios" | "aarch64-apple-ios-sim" => {
-            let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-            cbindgen::Builder::new()
-                .with_autogen_warning("// This file is generated automatically. To update it forcefully, run `cargo run -p mullvad-ios --target aarch64-apple-ios`.")
-                .with_crate(crate_dir)
-                .with_language(cbindgen::Language::C)
-                .generate()
-                .expect("failed to generate bindings")
-                .write_to_file("../ios/MullvadRustRuntime/include/mullvad_rust_runtime.h");
-        }
-        &_ => (),
-    }
-}
+fn main() {}
+// fn main() {
+//     #[cfg(target_os = "macos")]
+//     match std::env::var("TARGET").unwrap().as_str() {
+//         "aarch64-apple-ios" | "aarch64-apple-ios-sim" => {
+//             let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+//             cbindgen::Builder::new()
+//                 .with_autogen_warning("// This file is generated automatically. To update it forcefully, run `cargo run -p mullvad-ios --target aarch64-apple-ios`.")
+//                 .with_crate(crate_dir)
+//                 .with_language(cbindgen::Language::C)
+//                 .generate()
+//                 .expect("failed to generate bindings")
+//                 .write_to_file("../ios/MullvadRustRuntime/include/mullvad_rust_runtime.h");
+//         }
+//         &_ => (),
+//     }
+// }
