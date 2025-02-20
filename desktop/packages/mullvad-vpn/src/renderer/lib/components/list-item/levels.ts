d72754dd2d07e5cfa7416c78815ab1b7e1db86ab
Oliver <oliver@mohlin.dev>
2025-02-20 09:32:33 +0100

    Add ListItem component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/levels.ts
@@ -0,0 +1,10 @@
+// TODO: These are colors are from the new colors tokens
+// which are currently not implemented, should be replaced
+// with the new color tokens once they are.
+export const levels = {
+  0: { enabled: 'rgba(41, 77, 115, 1)', disabled: 'rgba(31, 58, 87, 1)' },
+  1: { enabled: 'rgba(35, 65, 97, 1)', disabled: 'rgba(31, 58, 87, 1)' },
+  2: { enabled: 'rgba(31, 58, 87, 1)', disabled: 'rgba(28, 52, 78, 1)' },
+  3: { enabled: 'rgba(28, 52, 78, 1)', disabled: 'rgba(27, 49, 74, 1)' },
+  4: { enabled: 'rgba(27, 49, 74, 1)', disabled: 'rgba(27, 49, 74, 1)' },
+} as const;
