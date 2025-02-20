2438b437c0b19ab459bcc3b92726185ef4dd62d9
Oliver <oliver@mohlin.dev>
2025-02-20 09:32:33 +0100

    Add ListItem component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/ListItemContent.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+import { Flex, FlexProps } from '../../flex';
+import { levels } from '../levels';
+import { useListItem } from '../ListItemContext';
+
+const sizes = {
+  full: '100%',
+  small: '44px',
+};
+
+export const StyledFlex = styled(Flex)<{
+  $level: keyof typeof levels;
+  $disabled?: boolean;
+  $size: 'full' | 'small';
+}>`
+  width: ${({ $size }) => sizes[$size]};
+  height: 100%;
+  background-color: ${({ $disabled, $level }) =>
+    $disabled ? levels[$level].disabled : levels[$level].enabled};
+  &&:has(> :last-child:nth-child(1)) {
+    &&:has(img) {
+      justify-content: center;
+    }
+  }
+`;
+
+export interface ListItemContainerProps extends FlexProps {
+  size?: 'full' | 'small';
+}
+
+export const ListItemContent = ({ size = 'full', ...props }: ListItemContainerProps) => {
+  const { level } = useListItem();
+  return (
+    <StyledFlex
+      $size={size}
+      $level={level}
+      $alignItems="center"
+      $justifyContent="space-between"
+      $gap="small"
+      $padding={{
+        horizontal: 'medium',
+      }}
+      {...props}
+    />
+  );
+};
