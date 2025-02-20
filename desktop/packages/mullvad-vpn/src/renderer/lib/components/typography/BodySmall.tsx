de388004728c51e27feaa45e79411d70b562fb1d
Oliver <oliver@mohlin.dev>
2025-02-20 09:25:47 +0100

    Use as prop for polymorphism in Text

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/typography/BodySmall.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/typography/BodySmall.tsx
@@ -1,6 +1,8 @@
+import { WebTarget } from 'styled-components';
+
 import { Text, TextProps } from './Text';
 
-export type BodySmallProps = Omit<TextProps, 'variant'>;
+export type BodySmallProps = Omit<TextProps<WebTarget>, 'variant'>;
 
 export const BodySmall = ({ children, ...props }: BodySmallProps) => (
   <Text variant="bodySmall" {...props}>
