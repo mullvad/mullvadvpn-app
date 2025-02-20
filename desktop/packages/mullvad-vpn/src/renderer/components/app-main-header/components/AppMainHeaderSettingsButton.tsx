822ea46d9c7059c9a45debad681de570c46cbbdb
Oliver <oliver@mohlin.dev>
2025-02-20 10:59:56 +0100

    Add update notification to settings button

--- desktop/packages/mullvad-vpn/src/renderer/components/app-main-header/components/AppMainHeaderSettingsButton.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/app-main-header/components/AppMainHeaderSettingsButton.tsx
@@ -1,14 +1,27 @@
 import { useCallback } from 'react';
+import styled from 'styled-components';
 
 import { messages } from '../../../../shared/gettext';
 import { IconButton, IconButtonProps, MainHeader } from '../../../lib/components';
+import { Notification } from '../../../lib/components/notification';
 import { transitions, useHistory } from '../../../lib/history';
 import { RoutePath } from '../../../lib/routes';
-
+import { useSelector } from '../../../redux/store';
 export type MainHeaderSettingsButtonProps = Omit<IconButtonProps, 'icon'>;
+
+const StyledNotification = styled(Notification)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+const StyledDiv = styled.div`
+  position: relative;
+`;
 
 export function AppMainHeaderSettingsButton(props: MainHeaderSettingsButtonProps) {
   const history = useHistory();
+  const suggestedUpgrade = useSelector((state) => state.version.suggestedUpgrade);
 
   const openSettings = useCallback(() => {
     if (!props.disabled) {
@@ -18,7 +31,14 @@ export function AppMainHeaderSettingsButton(props: MainHeaderSettingsButtonProps
 
   return (
     <MainHeader.IconButton onClick={openSettings} aria-label={messages.gettext('Settings')}>
-      <IconButton.Icon icon="settings-filled" />{' '}
+      {suggestedUpgrade ? (
+        <StyledDiv>
+          <IconButton.Icon icon="settings-partial" />
+          <StyledNotification variant="warning" size="tiny" />
+        </StyledDiv>
+      ) : (
+        <IconButton.Icon icon="settings-filled" />
+      )}
     </MainHeader.IconButton>
   );
 }
