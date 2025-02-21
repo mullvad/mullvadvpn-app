fbc7f28235fc527ea9cd83ff2dc51feadfee1b3e
David Göransson <david.goransson@mullvad.net>
2025-02-21 13:10:13 +0100

    Remove cached VpnConfig in TalpidVpnService

--- android/lib/talpid/src/main/kotlin/net/mullvad/talpid/TalpidVpnService.kt
+++ android/lib/talpid/src/main/kotlin/net/mullvad/talpid/TalpidVpnService.kt
@@ -42,8 +42,6 @@ open class TalpidVpnService : LifecycleVpnService() {
             }
         }
 
-    private var currentTunConfig: TunConfig? = null
-
     // Used by JNI
     lateinit var connectivityListener: ConnectivityListener
 
@@ -56,12 +54,7 @@ open class TalpidVpnService : LifecycleVpnService() {
 
     // Used by JNI
     fun openTun(config: TunConfig): CreateTunResult =
-        synchronized(this) {
-            createTun(config).merge().also {
-                currentTunConfig = config
-                activeTunStatus = it
-            }
-        }
+        synchronized(this) { createTun(config).merge().also { activeTunStatus = it } }
 
     // Used by JNI
     fun closeTun(): Unit =
