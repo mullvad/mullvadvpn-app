23270c35f82304dc939acd5bd7931d800c8fc6f8
Joakim Hulthe <joakim@hulthe.net>
2025-02-21 14:22:12 +0100

    Implement error view for macos

--- installer-downloader/src/cacao_impl/delegate.rs
+++ installer-downloader/src/cacao_impl/delegate.rs
@@ -2,7 +2,7 @@ use std::sync::{Arc, Mutex};
 
 use cacao::{control::Control, layout::Layout};
 
-use super::ui::{Action, AppWindow};
+use super::ui::{Action, AppWindow, ErrorView};
 use crate::delegate::{AppDelegate, AppDelegateQueue};
 
 impl AppDelegate for AppWindow {
@@ -28,6 +28,12 @@ impl AppDelegate for AppWindow {
             let cb = Action::CancelClick(cb.clone());
             cacao::appkit::App::<super::ui::AppImpl, _>::dispatch_main(cb);
         });
+    }
+
+    fn on_beta_link<F>(&mut self, callback: F)
+    where
+        F: Fn() + Send + 'static,
+    {
     }
 
     fn set_status_text(&mut self, text: &str) {
@@ -94,6 +100,66 @@ impl AppDelegate for AppWindow {
 
     fn queue(&self) -> Self::Queue {
         Queue {}
+    }
+
+    fn quit(&mut self) {
+        cacao::appkit::App::<super::ui::AppImpl, _>::dispatch_main(Action::Quit);
+    }
+
+    fn on_stable_link<F>(&mut self, callback: F)
+    where
+        F: Fn() + Send + 'static,
+    {
+        println!("todo. on stable link");
+    }
+
+    fn show_stable_text(&mut self) {
+        println!("todo. show stable text");
+    }
+
+    fn hide_stable_text(&mut self) {
+        println!("todo. hide stable text");
+    }
+
+    fn show_error_message(&mut self, message: installer_downloader::delegate::ErrorMessage) {
+        let on_cancel = self.error_cancel_callback.clone().map(|cb| {
+            move || {
+                let cb = Action::ErrorCancel(cb.clone());
+                cacao::appkit::App::<super::ui::AppImpl, _>::dispatch_main(cb);
+            }
+        });
+
+        let on_retry = self.error_retry_callback.clone().map(|cb| {
+            move || {
+                let cb = Action::ErrorRetry(cb.clone());
+                cacao::appkit::App::<super::ui::AppImpl, _>::dispatch_main(cb);
+            }
+        });
+
+        self.error_view = Some(ErrorView::new(
+            &self.main_view,
+            message,
+            on_cancel,
+            on_retry,
+        ));
+    }
+
+    fn hide_error_message(&mut self) {
+        self.error_view.take();
+    }
+
+    fn on_error_message_retry<F>(&mut self, callback: F)
+    where
+        F: Fn() + Send + 'static,
+    {
+        self.error_retry_callback = Some(Self::sync_callback(callback));
+    }
+
+    fn on_error_message_cancel<F>(&mut self, callback: F)
+    where
+        F: Fn() + Send + 'static,
+    {
+        self.error_cancel_callback = Some(Self::sync_callback(callback));
     }
 }
 
