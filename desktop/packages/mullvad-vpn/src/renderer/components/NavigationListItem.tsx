3aca2d9d06ac5cf72c64ca3030cdc05b6e67f8fb
Oliver <oliver@mohlin.dev>
2025-02-21 13:05:52 +0100

    Add NavigationListItem component

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/NavigationListItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { ListItem, ListItemProps } from '../lib/components/list-item';
+import { useHistory } from '../lib/history';
+import { RoutePath } from '../lib/routes';
+
+export type NavigationListItemProps = ListItemProps & {
+  to: RoutePath;
+};
+
+export function NavigationListItem({ to, children, ...props }: NavigationListItemProps) {
+  const history = useHistory();
+  const navigate = React.useCallback(() => history.push(to), [history, to]);
+
+  return (
+    <ListItem {...props}>
+      <ListItem.Item>
+        <ListItem.Trigger onClick={navigate}>
+          <ListItem.Content>{children}</ListItem.Content>
+        </ListItem.Trigger>
+      </ListItem.Item>
+    </ListItem>
+  );
+}
