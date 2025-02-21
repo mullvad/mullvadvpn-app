df1e07ca3154ae2e10e601be2d5decaab43c1346
Markus Pettersson <markus.pettersson@mullvad.net>
2025-02-21 11:58:46 +0100

    Detect native CPU arch

--- talpid-platform-metadata/src/lib.rs
+++ talpid-platform-metadata/src/lib.rs
@@ -1,3 +1,4 @@
+mod arch;
 #[cfg(target_os = "linux")]
 #[path = "linux.rs"]
 mod imp;
@@ -19,3 +20,6 @@ pub use self::imp::MacosVersion;
 #[cfg(windows)]
 pub use self::imp::WindowsVersion;
 pub use self::imp::{extra_metadata, short_version, version};
+
+pub use arch::get_native_arch;
+pub use arch::Architecture;
