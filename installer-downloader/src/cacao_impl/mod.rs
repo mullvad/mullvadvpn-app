c9a6b57e2ab296600dce8f4b495d5efbb7c2646c
Markus Pettersson <markus.pettersson@mullvad.net>
2025-02-24 10:22:34 +0100

    Push error handling of `get_arch` outwards

--- installer-downloader/src/cacao_impl/mod.rs
+++ installer-downloader/src/cacao_impl/mod.rs
@@ -1,6 +1,7 @@
 use std::sync::Mutex;
 
 use cacao::appkit::App;
+use installer_downloader::environment::{Environment, Error as EnvError};
 use ui::{Action, AppImpl};
 
 mod delegate;
@@ -9,8 +10,16 @@ mod ui;
 pub fn main() {
     let app = App::new("net.mullvad.downloader", AppImpl::default());
 
+    // Load "global" values and resources
+    let environment = match Environment::load() {
+        Ok(env) => env,
+        Err(EnvError::Arch) => {
+            unreachable!("The CPU architecture will always be retrievable on macOS")
+        }
+    };
+
     let cb: Mutex<Option<ui::MainThreadCallback>> = Mutex::new(Some(Box::new(|self_| {
-        crate::controller::initialize_controller(self_);
+        crate::controller::initialize_controller(self_, environment);
     })));
     cacao::appkit::App::<ui::AppImpl, _>::dispatch_main(Action::QueueMain(cb));
 
