d4fd0a19fed2effc65fd144106a5ad9adc047dd6
David Lönnhager <david.l@mullvad.net>
2025-02-25 17:51:07 +0100

    Make meta tool build on Linux

--- mullvad-update/src/client/verify.rs
+++ mullvad-update/src/client/verify.rs
@@ -1,8 +1,7 @@
 use anyhow::Context;
-use sha2::Digest;
 use tokio::{
     fs,
-    io::{AsyncRead, AsyncReadExt, BufReader},
+    io::{AsyncRead, BufReader},
 };
 
 use std::{future::Future, path::Path};
@@ -21,11 +20,6 @@ pub trait AppVerifier: 'static + Clone {
 /// Checksum verifier that uses SHA256
 #[derive(Clone)]
 pub struct Sha256Verifier;
-
-impl Sha256Verifier {
-    /// Maximum number of bytes to read at a time
-    const BUF_SIZE: usize = 1024 * 1024;
-}
 
 impl AppVerifier for Sha256Verifier {
     /// The checksum
@@ -49,34 +43,11 @@ impl AppVerifier for Sha256Verifier {
 }
 
 impl Sha256Verifier {
-    pub async fn generate_hash(
-        mut reader: impl AsyncRead + Unpin,
-    ) -> anyhow::Result<[u8; 32]>
-    {
-        let mut hasher = sha2::Sha256::new();
-
-        // Read data into hasher
-        let mut buffer = vec![0u8; Self::BUF_SIZE];
-        loop {
-            let read_n = reader
-                .read(&mut buffer)
-                .await
-                .context("Error reading bin file")?;
-            if read_n == 0 {
-                // We're done
-                break;
-            }
-            hasher.update(&buffer[..read_n]);
-        }
-
-        Ok(hasher.finalize().into())
-    }
-
     async fn verify_inner(
         reader: impl AsyncRead + Unpin,
         expected_hash: [u8; 32],
     ) -> anyhow::Result<()> {
-        let actual_hash = Self::generate_hash(reader).await?;
+        let actual_hash = crate::hash::checksum(reader).await?;
 
         // Verify that hash is correct
         if expected_hash != actual_hash {
@@ -90,6 +61,7 @@ impl Sha256Verifier {
 #[cfg(test)]
 mod test {
     use rand::RngCore;
+    use sha2::Digest;
     use std::io::Cursor;
 
     use super::*;
