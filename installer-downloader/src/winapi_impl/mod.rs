398ab0cbd0dd345d5d80829e5a59eee26309b6d7
Markus Pettersson <markus.pettersson@mullvad.net>
2025-02-25 13:45:12 +0100

    fixup! Push error handling of `get_arch` outwards

--- installer-downloader/src/winapi_impl/mod.rs
+++ installer-downloader/src/winapi_impl/mod.rs
@@ -1,5 +1,5 @@
+use installer_downloader::environment::{Environment, Error as EnvError};
 use native_windows_gui::{self as nwg, modal_fatal_message, ControlHandle};
-use talpid_platform_metadata::get_native_arch;
 
 use crate::delegate::{AppDelegate, AppDelegateQueue};
 
@@ -16,39 +16,23 @@ pub fn main() {
     let queue = window.borrow().queue();
 
     // Load "global" values and resources
-    let Ok(env) = Environment::load() else {
-        let parent = ControlHandle::NoHandle;
-        let title = "Failed to initialize";
-        let content = "Failed to detect CPU architecture";
-        modal_fatal_message(parent, title, content)
+    let environment = match Environment::load() {
+        Ok(env) => env,
+        Err(error) => show_fatal_error_(error),
     };
 
     queue.queue_main(|window| {
-        crate::controller::initialize_controller(window);
+        crate::controller::initialize_controller(window, environment);
     });
 
     nwg::dispatch_thread_events();
 }
 
-struct Environment {
-    architecture: mullvad_update::format::Architecture,
-}
-
-impl Environment {
-    pub fn load() -> Result<Self, ()> {
-        let Some(architecture) = Self::get_arch().ok().flatten() else {
-            return Err(());
-        };
-
-        Ok(Environment { architecture })
-    }
-
-    /// Try to map the host's CPU architecture to one of the CPU architectures the Mullvad VPN app
-    /// supports.
-    fn get_arch() -> Result<Option<VersionArchitecture>, std::io::Error> {
-        match talpid_platform_metadata::get_native_arch()?? {
-            talpid_platform_metadata::Architecture::X86 => VersionArchitecture::X86,
-            talpid_platform_metadata::Architecture::Arm64 => VersionArchitecture::Arm64,
-        }
-    }
+fn show_fatal_error_(error: EnvError) -> ! {
+    let parent = ControlHandle::NoHandle;
+    let title = "Failed to initialize";
+    let content = match error {
+        EnvError::Arch => "Failed to detect CPU architecture",
+    };
+    modal_fatal_message(parent, title, content)
 }
