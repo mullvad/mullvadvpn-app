583e28c03f5e8374e15df9df9a07fee25c4f0e8b
David Lönnhager <david.l@mullvad.net>
2025-02-26 14:58:34 +0100

    Allow ed25519 secret to be entered in stdin in meta tool

--- mullvad-update/meta/src/io_util.rs
+++ mullvad-update/meta/src/io_util.rs
@@ -10,18 +10,21 @@ use tokio::fs;
 pub async fn wait_for_confirm(prompt: &str) -> bool {
     const DEFAULT: bool = true;
 
-    print!("{prompt}");
-    if DEFAULT {
-        println!(" [Y/n]");
-    } else {
-        println!(" [y/N]");
-    }
+    let prompt = prompt.to_owned();
 
-    tokio::task::spawn_blocking(|| {
-        let mut s = String::new();
+    tokio::task::spawn_blocking(move || {
         let stdin = std::io::stdin();
 
         loop {
+            let mut s = String::new();
+
+            print!("{prompt}");
+            if DEFAULT {
+                println!(" [Y/n]");
+            } else {
+                println!(" [y/N]");
+            }
+
             stdin.read_line(&mut s).context("Failed to read line")?;
 
             match s.trim().to_ascii_lowercase().as_str() {
@@ -35,6 +38,29 @@ pub async fn wait_for_confirm(prompt: &str) -> bool {
     .await
     .unwrap()
     .unwrap_or(false)
+}
+
+/// Wait for user to respond with any input, ignoring empty responses
+pub async fn wait_for_input(prompt: &str) -> anyhow::Result<String> {
+    let prompt = prompt.to_owned();
+    tokio::task::spawn_blocking(move || {
+        let stdin = std::io::stdin();
+
+        loop {
+            let mut s = String::new();
+
+            println!("{prompt}");
+
+            stdin.read_line(&mut s).context("Failed to read line")?;
+
+            match s.trim().to_ascii_lowercase().as_str() {
+                "" => continue,
+                input => break Ok(input.to_owned()),
+            }
+        }
+    })
+    .await
+    .unwrap()
 }
 
 /// Recursively create directories and write to 'file'
