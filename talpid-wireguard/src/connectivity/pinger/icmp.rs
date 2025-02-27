41f565c500e16a66dac0dd4557cd700b1feae287
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-02-27 12:03:19 +0100

    Merge branch 'investigate-go-crash-issue-with-runtimegc-droid-1825'

--- talpid-wireguard/src/connectivity/pinger/icmp.rs
+++ talpid-wireguard/src/connectivity/pinger/icmp.rs
@@ -62,11 +62,19 @@ impl Pinger {
     /// Creates a new `Pinger`.
     pub fn new(
         addr: Ipv4Addr,
-        #[cfg(not(target_os = "windows"))] interface_name: String,
+        #[cfg(any(target_os = "linux", target_os = "macos"))] interface_name: String,
     ) -> Result<Self> {
         let addr = SocketAddr::new(addr.into(), 0);
-        let sock =
-            Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4)).map_err(Error::Open)?;
+        let sock = Socket::new(
+            Domain::IPV4,
+            if cfg!(target_os = "android") {
+                Type::DGRAM
+            } else {
+                Type::RAW
+            },
+            Some(Protocol::ICMPV4),
+        )
+        .map_err(Error::Open)?;
         sock.set_nonblocking(true).map_err(Error::Open)?;
 
         #[cfg(target_os = "linux")]
