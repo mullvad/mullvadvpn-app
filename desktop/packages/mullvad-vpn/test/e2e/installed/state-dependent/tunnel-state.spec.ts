4e3f29cb9b8420202559a43fc8c18b31c875e2fa
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-02-27 14:54:39 +0100

    Ensure we only target one element with inIp locator
    This locator currently can be resolved to two elements.
    
    See:
    https://github.com/mullvad/mullvadvpn-app/actions/runs/13564177651/job/37914262416#step:9:985

--- desktop/packages/mullvad-vpn/test/e2e/installed/state-dependent/tunnel-state.spec.ts
+++ desktop/packages/mullvad-vpn/test/e2e/installed/state-dependent/tunnel-state.spec.ts
@@ -37,7 +37,7 @@ test('App should connect', async () => {
   await expectConnected(page);
 
   const relay = page.getByTestId('hostname-line');
-  const inIp = page.getByText('In', { exact: true }).locator('+ span');
+  const inIp = page.getByTestId('in-ip');
   // If IPv6 is enabled, there will be two "Out" IPs, one for IPv4 and one for IPv6
   // Selecting the first resolves to the IPv4 address regardless of the IP setting
   const outIp = page.locator(':text("Out") + div > span').first();
