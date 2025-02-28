7f861060c2754f2b3d75e1fe42751a0568fc356e
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-02-28 16:50:37 +0100

    FIXUP showcase

--- desktop/packages/mullvad-vpn/src/renderer/components/AppRouter.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/AppRouter.tsx
@@ -1,4 +1,5 @@
 import CountComponent from './custom-context-component';
+import ReactContextComponent from './react-context-component/react-context';
 
 export default function AppRouter() {
   return <CountComponent heading="Count with me" initialCount={0} />;
