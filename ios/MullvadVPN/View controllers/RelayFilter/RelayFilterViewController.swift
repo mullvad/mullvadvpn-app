1825abf6f810c6ceef97f7a688440ab24dba02e5
mojganii <mojgan.jelodar@codic.se>
2025-03-03 20:29:05 +0100

    Refactor RelayFilterDataSource

--- ios/MullvadVPN/View controllers/RelayFilter/RelayFilterViewController.swift
+++ ios/MullvadVPN/View controllers/RelayFilter/RelayFilterViewController.swift
@@ -85,6 +85,7 @@ class RelayFilterViewController: UIViewController {
         tableView.estimatedRowHeight = 60
         tableView.estimatedSectionHeaderHeight = tableView.estimatedRowHeight
         tableView.allowsMultipleSelection = true
+        tableView.allowsSelection = true
 
         view.addSubview(tableView)
         buttonContainerView.addArrangedSubview(descriptionLabel)
@@ -103,17 +104,16 @@ class RelayFilterViewController: UIViewController {
     }
 
     private func setupDataSource() {
-        viewModel
-            .$relayFilter
+        viewModel.$relayFilter
             .receive(on: DispatchQueue.main)
             .removeDuplicates()
             .sink { [weak self] filter in
                 guard let self else { return }
                 let filterDescriptor = viewModel.getFilteredRelays(filter)
+                descriptionLabel.isEnabled = filterDescriptor.isEnabled
                 applyButton.isEnabled = filterDescriptor.isEnabled
                 applyButton.setTitle(filterDescriptor.title, for: .normal)
                 descriptionLabel.text = filterDescriptor.description
-                descriptionLabel.isEnabled = filterDescriptor.isEnabled
             }
             .store(in: &disposeBag)
         dataSource = RelayFilterDataSource(tableView: tableView, viewModel: viewModel)
