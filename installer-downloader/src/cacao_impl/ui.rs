a4266bc3e2750bf45d9f8c4054a10d0bd3d65fad
David Lönnhager <david.l@mullvad.net>
2025-03-03 16:09:55 +0100

    fixup: remove TODO

--- installer-downloader/src/cacao_impl/ui.rs
+++ installer-downloader/src/cacao_impl/ui.rs
@@ -118,8 +118,7 @@ impl Dispatcher for AppImpl {
         }
     }
 
-    fn on_background_message(&self, _message: Self::Message) {
-    }
+    fn on_background_message(&self, _message: Self::Message) {}
 }
 
 #[derive(Default)]
