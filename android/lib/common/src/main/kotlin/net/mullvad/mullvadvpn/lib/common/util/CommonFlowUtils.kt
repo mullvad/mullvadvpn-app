b8a30cea2497c44d371841eb691165fe8c886038
David Göransson <david.goransson@mullvad.net>
2025-03-03 13:39:04 +0100

    Fix connectivity listener
    Fixes an issue where another VPN app or user having unforuenate
    timing of turning on airplane mode and conencting at almost the same
    time would leave a lingering network cached in the scan. This fix will
    start with the all networks state and give the networkEvents flow 1
    second to start up and emit the actual network state.

--- android/lib/common/src/main/kotlin/net/mullvad/mullvadvpn/lib/common/util/CommonFlowUtils.kt
+++ android/lib/common/src/main/kotlin/net/mullvad/mullvadvpn/lib/common/util/CommonFlowUtils.kt
@@ -1,9 +1,26 @@
 package net.mullvad.mullvadvpn.lib.common.util
 
+import kotlin.time.Duration
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.withIndex
 import kotlinx.coroutines.withTimeoutOrNull
 
 suspend fun <T> Flow<T>.firstOrNullWithTimeout(timeMillis: Long): T? {
     return withTimeoutOrNull(timeMillis) { firstOrNull() }
 }
+
+@OptIn(FlowPreview::class)
+fun <T> Flow<T>.debounceFirst(timeout: Duration): Flow<T> =
+    withIndex()
+        .debounce {
+            if (it.index == 0) {
+                timeout
+            } else {
+                Duration.ZERO
+            }
+        }
+        .map { it.value }
