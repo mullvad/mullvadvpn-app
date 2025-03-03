5dc4fff60e8bbe81a949c7e49abd9802241558e7
David Lönnhager <david.l@mullvad.net>
2025-03-03 11:29:02 +0100

    Simplify cfg conditions in installer-downloader

--- installer-downloader/src/lib.rs
+++ installer-downloader/src/lib.rs
@@ -1,14 +1,9 @@
-#[cfg(any(target_os = "windows", target_os = "macos"))]
+#![cfg(any(target_os = "windows", target_os = "macos"))]
+
 pub mod controller;
-#[cfg(any(target_os = "windows", target_os = "macos"))]
 pub mod delegate;
-#[cfg(any(target_os = "windows", target_os = "macos"))]
 pub mod environment;
-#[cfg(any(target_os = "windows", target_os = "macos"))]
 pub mod log;
-#[cfg(any(target_os = "windows", target_os = "macos"))]
 pub mod resource;
-#[cfg(any(target_os = "windows", target_os = "macos"))]
 pub mod temp;
-#[cfg(any(target_os = "windows", target_os = "macos"))]
 pub mod ui_downloader;
