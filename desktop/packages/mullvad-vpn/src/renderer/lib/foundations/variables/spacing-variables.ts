58d1dd41bcfd35f60267c45b183917cd0fc74034
Oliver <oliver@mohlin.dev>
2025-03-04 09:24:31 +0100

    Rename spacing tokens

--- desktop/packages/mullvad-vpn/src/renderer/lib/foundations/variables/spacing-variables.ts
+++ desktop/packages/mullvad-vpn/src/renderer/lib/foundations/variables/spacing-variables.ts
@@ -1,33 +1,25 @@
-import { SpacingTokens } from '../tokens';
+import { spacingTokens } from '../tokens';
 
 export const spacings = {
-  '--spacing-1': SpacingTokens.spacing1,
-  '--spacing-2': SpacingTokens.spacing2,
-  '--spacing-3': SpacingTokens.spacing3,
-  '--spacing-4': SpacingTokens.spacing4,
-  '--spacing-5': SpacingTokens.spacing5,
-  '--spacing-6': SpacingTokens.spacing6,
-  '--spacing-7': SpacingTokens.spacing7,
-  '--spacing-8': SpacingTokens.spacing8,
-  '--spacing-9': SpacingTokens.spacing9,
-  '--spacing-10': SpacingTokens.spacing10,
-  '--spacing-11': SpacingTokens.spacing11,
-  '--spacing-12': SpacingTokens.spacing12,
-  '--spacing-13': SpacingTokens.spacing13,
-};
+  '--spc-4': spacingTokens.spc4,
+  '--spc-8': spacingTokens.spc8,
+  '--spc-16': spacingTokens.spc16,
+  '--spc-24': spacingTokens.spc24,
+  '--spc-32': spacingTokens.spc32,
+} as const;
 
 export enum Spacings {
-  spacing1 = 'var(--spacing-1)',
-  spacing2 = 'var(--spacing-2)',
-  spacing3 = 'var(--spacing-3)',
-  spacing4 = 'var(--spacing-4)',
-  spacing5 = 'var(--spacing-5)',
-  spacing6 = 'var(--spacing-6)',
-  spacing7 = 'var(--spacing-7)',
-  spacing8 = 'var(--spacing-8)',
-  spacing9 = 'var(--spacing-9)',
-  spacing10 = 'var(--spacing-10)',
-  spacing11 = 'var(--spacing-11)',
-  spacing12 = 'var(--spacing-12)',
-  spacing13 = 'var(--spacing-13)',
+  spacing1 = 'var(--spc-4)',
+  spacing2 = 'var(--spc-6)',
+  spacing3 = 'var(--spc-8)',
+  spacing4 = 'var(--spc-12)',
+  spacing5 = 'var(--spc-16)',
+  spacing6 = 'var(--spc-24)',
+  spacing7 = 'var(--spc-32)',
+  spacing8 = 'var(--spc-40)',
+  spacing9 = 'var(--spc-48)',
+  spacing10 = 'var(--spc-56)',
+  spacing11 = 'var(--spc-64)',
+  spacing12 = 'var(--spc-72)',
+  spacing13 = 'var(--spc-80)',
 }
