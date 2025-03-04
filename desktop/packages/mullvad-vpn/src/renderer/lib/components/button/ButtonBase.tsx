d5899f4d4408c378dcd3755d8e874e77af23e026
Oliver <oliver@mohlin.dev>
2025-03-04 14:02:30 +0100

    Add ButtonBase

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/button/ButtonBase.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ButtonBase = styled.button({
+  border: 'none',
+  padding: 0,
+  margin: 0,
+  font: 'inherit',
+  color: 'inherit',
+  textAlign: 'inherit',
+  lineHeight: 'inherit',
+  cursor: 'default',
+});
