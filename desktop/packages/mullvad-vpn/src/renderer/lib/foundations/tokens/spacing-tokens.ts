1dc58b1ef80ac89b01eeb1d71d2594ed6df007fb
Oliver <oliver@mohlin.dev>
2025-03-07 10:24:26 +0100

    Remove spacing tokens that are not part of design system

--- desktop/packages/mullvad-vpn/src/renderer/lib/foundations/tokens/spacing-tokens.ts
+++ desktop/packages/mullvad-vpn/src/renderer/lib/foundations/tokens/spacing-tokens.ts
@@ -1,15 +1,7 @@
 export const spacingTokens = {
   spc4: '4px',
-  spc6: '6px',
   spc8: '8px',
-  spc12: '12px',
   spc16: '16px',
   spc24: '24px',
   spc32: '32px',
-  spc40: '40px',
-  spc48: '48px',
-  spc56: '56px',
-  spc64: '64px',
-  spc72: '72px',
-  spc80: '80px',
 } as const;
