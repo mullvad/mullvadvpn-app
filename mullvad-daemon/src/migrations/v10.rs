f37c6bfb25cdaf12258dec8dd23f45284a5e4868
Sebastian Holmin <sebastian.holmin@mullvad.net>
2025-03-07 15:00:18 +0100

    Simply tests and improve coverage

--- mullvad-daemon/src/migrations/v10.rs
+++ mullvad-daemon/src/migrations/v10.rs
@@ -30,9 +30,13 @@ fn relay_settings(settings: &mut serde_json::Value) -> Option<&mut serde_json::V
 }
 
 fn migrate_tunnel_type(settings: &mut serde_json::Value) -> Result<()> {
-    let Some(ref mut normal) = relay_settings(settings) else {
-        return Ok(());
-    };
+    if let Some(ref mut normal) = relay_settings(settings) {
+        migrate_tunnel_type_inner(normal)?;
+    }
+    Ok(())
+}
+
+fn migrate_tunnel_type_inner(normal: &mut serde_json::Value) -> Result<()> {
     match normal.get_mut("tunnel_protocol") {
         // Migrate tunnel protocol "any"
         Some(serde_json::Value::String(s)) if s == "any" => {
@@ -72,483 +76,96 @@ fn migrate_tunnel_type(settings: &mut serde_json::Value) -> Result<()> {
         }
         // Unexpected result. Do nothing.
         None => (),
-    }
+    };
     Ok(())
 }
 #[cfg(test)]
 mod test {
-    use mullvad_types::settings::SettingsVersion;
+    use serde_json::json;
 
-    use super::{migrate, version};
+    use crate::migrations::v10::migrate_tunnel_type_inner;
 
     /// Tunnel protocol "any" is migrated to wireguard
     #[test]
     fn test_v10_to_v11_migration() {
-        // TODO: Also test the case where the location is not an openvpn relay and the tunnel type
-        // is any
-        let mut old_settings = serde_json::from_str(V10_SETTINGS).unwrap();
-
-        assert_eq!(version(&old_settings), Some(SettingsVersion::V10));
-        migrate(&mut old_settings).unwrap();
-        let new_settings: serde_json::Value = serde_json::from_str(V11_SETTINGS).unwrap();
-        assert_eq!(version(&new_settings), Some(SettingsVersion::V11));
-
-        eprintln!(
-            "old_settings: {}",
-            serde_json::to_string_pretty(&old_settings).unwrap()
-        );
-        eprintln!(
-            "new_settings: {}",
-            serde_json::to_string_pretty(&new_settings).unwrap()
-        );
+        let mut old_settings = json!({
+            "tunnel_protocol": "any",
+            "location": {
+                "only": {
+                    "location": {
+                        "country": "se"
+                    }
+                }
+            }
+        });
+        migrate_tunnel_type_inner(&mut old_settings).unwrap();
+        let new_settings: serde_json::Value = json!({
+            "tunnel_protocol": "wireguard",
+            "location": {
+                "only": {
+                    "location": {
+                        "country": "se"
+                    }
+                }
+            }
+        });
         assert_eq!(&old_settings, &new_settings);
     }
 
     /// Tunnel protocol "any" is migrated to openvpn, since the location is an openvpn relay
     #[test]
     fn test_v10_to_v11_migration_openvpn_location() {
-        // TODO: Also test the case where the location is not an openvpn relay and the tunnel type
-        // is any
-        let mut old_settings = serde_json::from_str(V10_SETTINGS_OPENVPN_LOCATION).unwrap();
-
-        assert_eq!(version(&old_settings), Some(SettingsVersion::V10));
-        migrate(&mut old_settings).unwrap();
-        let new_settings: serde_json::Value =
-            serde_json::from_str(V11_SETTINGS_OPENVPN_LOCATION).unwrap();
-        assert_eq!(version(&new_settings), Some(SettingsVersion::V11));
-
-        eprintln!(
-            "old_settings: {}",
-            serde_json::to_string_pretty(&old_settings).unwrap()
-        );
-        eprintln!(
-            "new_settings: {}",
-            serde_json::to_string_pretty(&new_settings).unwrap()
-        );
+        let mut old_settings = json!({
+            "tunnel_protocol": "any",
+            "location": {
+                "only": {
+                    "location": {
+                        "hostname": "at-vie-ovpn-001"
+                    }
+                }
+            }
+        });
+        migrate_tunnel_type_inner(&mut old_settings).unwrap();
+        let new_settings: serde_json::Value = json!({
+            "tunnel_protocol": "openvpn",
+            "location": {
+                "only": {
+                    "location": {
+                        "hostname": "at-vie-ovpn-001"
+                    }
+                }
+            }
+        });
         assert_eq!(&old_settings, &new_settings);
     }
 
-    /// Tunnel protocol is set to any, but the location is not an openvpn relay
-    pub const V10_SETTINGS: &str = r#"
-        {
-          "relay_settings": {
-            "normal": {
-              "location": {
-                "only": {
-                  "location": {
-                    "country": "se"
-                  }
-                }
-              },
-              "providers": "any",
-              "ownership": "any",
-              "tunnel_protocol": "any",
-              "wireguard_constraints": {
-                "port": "any",
-                "ip_version": "any",
-                "use_multihop": false,
-                "entry_location": {
-                  "only": {
-                    "location": {
-                      "country": "se"
-                    }
-                  }
-                }
-              },
-              "openvpn_constraints": {
-                "port": "any"
-              }
-            }
-          },
-          "bridge_settings": {
-            "bridge_type": "normal",
-            "normal": {
-              "location": "any",
-              "providers": "any",
-              "ownership": "any"
+    /// Tunnel protocol '"only" = { "tunnel_protocol" = '$tunnel_protocol' } is migrated to
+    /// '"tunnel_protocol" = '$tunnel_protocol'
+    #[test]
+    fn test_v10_to_v11_migration_only_protocol() {
+        let mut old_settings = json!({
+            "tunnel_protocol": {
+                "only": "wireguard"
             },
-            "custom": null
-          },
-          "obfuscation_settings": {
-            "selected_obfuscation": "auto",
-            "udp2tcp": {
-              "port": "any"
-            }
-          },
-          "bridge_state": "auto",
-          "custom_lists": {
-            "custom_lists": []
-          },
-          "api_access_methods": {
-            "direct": {
-              "id": "d81121bf-c942-4ca4-971f-8ea6581bc915",
-              "name": "Direct",
-              "enabled": true,
-              "access_method": {
-                "built_in": "direct"
-              }
-            },
-            "mullvad_bridges": {
-              "id": "92135711-534d-4950-963d-93e446a792e4",
-              "name": "Mullvad Bridges",
-              "enabled": true,
-              "access_method": {
-                "built_in": "bridge"
-              }
-            },
-            "custom": []
-          },
-          "allow_lan": false,
-          "block_when_disconnected": false,
-          "auto_connect": false,
-          "tunnel_options": {
-            "openvpn": {
-              "mssfix": null
-            },
-            "wireguard": {
-              "mtu": null,
-              "quantum_resistant": "auto",
-              "rotation_interval": null
-            },
-            "generic": {
-              "enable_ipv6": false
-            },
-            "dns_options": {
-              "state": "default",
-              "default_options": {
-                "block_ads": false,
-                "block_trackers": false,
-                "block_malware": false,
-                "block_adult_content": false,
-                "block_gambling": false,
-                "block_social_media": false
-              },
-              "custom_options": {
-                "addresses": []
-              }
-            }
-          },
-          "relay_overrides": [],
-          "show_beta_releases": true,
-          "settings_version": 10
-        }
-        "#;
-
-    /// Tunnel protocol is migrated to wireguard
-    pub const V11_SETTINGS: &str = r#"
-        {
-          "relay_settings": {
-            "normal": {
-              "location": {
-                "only": {
-                  "location": {
-                    "country": "se"
-                  }
-                }
-              },
-              "providers": "any",
-              "ownership": "any",
-              "tunnel_protocol": "wireguard",
-              "wireguard_constraints": {
-                "port": "any",
-                "ip_version": "any",
-                "use_multihop": false,
-                "entry_location": {
-                  "only": {
-                    "location": {
-                      "country": "se"
-                    }
-                  }
-                }
-              },
-              "openvpn_constraints": {
-                "port": "any"
-              }
-            }
-          },
-          "bridge_settings": {
-            "bridge_type": "normal",
-            "normal": {
-              "location": "any",
-              "providers": "any",
-              "ownership": "any"
-            },
-            "custom": null
-          },
-          "obfuscation_settings": {
-            "selected_obfuscation": "auto",
-            "udp2tcp": {
-              "port": "any"
-            }
-          },
-          "bridge_state": "auto",
-          "custom_lists": {
-            "custom_lists": []
-          },
-          "api_access_methods": {
-            "direct": {
-              "id": "d81121bf-c942-4ca4-971f-8ea6581bc915",
-              "name": "Direct",
-              "enabled": true,
-              "access_method": {
-                "built_in": "direct"
-              }
-            },
-            "mullvad_bridges": {
-              "id": "92135711-534d-4950-963d-93e446a792e4",
-              "name": "Mullvad Bridges",
-              "enabled": true,
-              "access_method": {
-                "built_in": "bridge"
-              }
-            },
-            "custom": []
-          },
-          "allow_lan": false,
-          "block_when_disconnected": false,
-          "auto_connect": false,
-          "tunnel_options": {
-            "openvpn": {
-              "mssfix": null
-            },
-            "wireguard": {
-              "mtu": null,
-              "quantum_resistant": "auto",
-              "rotation_interval": null
-            },
-            "generic": {
-              "enable_ipv6": false
-            },
-            "dns_options": {
-              "state": "default",
-              "default_options": {
-                "block_ads": false,
-                "block_trackers": false,
-                "block_malware": false,
-                "block_adult_content": false,
-                "block_gambling": false,
-                "block_social_media": false
-              },
-              "custom_options": {
-                "addresses": []
-              }
-            }
-          },
-          "relay_overrides": [],
-          "show_beta_releases": true,
-          "settings_version": 11
-        }
-        "#;
-
-    /// This settings blob contains no constraint for tunnel type
-    pub const V10_SETTINGS_OPENVPN_LOCATION: &str = r#"
-{
-  "relay_settings": {
-    "normal": {
-      "location": {
-        "only": {
-          "location": {
-            "hostname": "at-vie-ovpn-001"
-          }
-        }
-      },
-      "providers": "any",
-      "ownership": "any",
-      "tunnel_protocol": "any",
-      "wireguard_constraints": {
-        "port": "any",
-        "ip_version": "any",
-        "use_multihop": false,
-        "entry_location": {
-          "only": {
             "location": {
-              "country": "se"
+                "only": {
+                    "location": {
+                        "country": "se"
+                    }
+                }
             }
-          }
-        }
-      },
-      "openvpn_constraints": {
-        "port": "any"
-      }
-    }
-  },
-  "bridge_settings": {
-    "bridge_type": "normal",
-    "normal": {
-      "location": "any",
-      "providers": "any",
-      "ownership": "any"
-    },
-    "custom": null
-  },
-  "obfuscation_settings": {
-    "selected_obfuscation": "auto",
-    "udp2tcp": {
-      "port": "any"
-    }
-  },
-  "bridge_state": "auto",
-  "custom_lists": {
-    "custom_lists": []
-  },
-  "api_access_methods": {
-    "direct": {
-      "id": "d81121bf-c942-4ca4-971f-8ea6581bc915",
-      "name": "Direct",
-      "enabled": true,
-      "access_method": {
-        "built_in": "direct"
-      }
-    },
-    "mullvad_bridges": {
-      "id": "92135711-534d-4950-963d-93e446a792e4",
-      "name": "Mullvad Bridges",
-      "enabled": true,
-      "access_method": {
-        "built_in": "bridge"
-      }
-    },
-    "custom": []
-  },
-  "allow_lan": false,
-  "block_when_disconnected": false,
-  "auto_connect": false,
-  "tunnel_options": {
-    "openvpn": {
-      "mssfix": null
-    },
-    "wireguard": {
-      "mtu": null,
-      "quantum_resistant": "auto",
-      "rotation_interval": null
-    },
-    "generic": {
-      "enable_ipv6": false
-    },
-    "dns_options": {
-      "state": "default",
-      "default_options": {
-        "block_ads": false,
-        "block_trackers": false,
-        "block_malware": false,
-        "block_adult_content": false,
-        "block_gambling": false,
-        "block_social_media": false
-      },
-      "custom_options": {
-        "addresses": []
-      }
-    }
-  },
-  "relay_overrides": [],
-  "show_beta_releases": true,
-  "settings_version": 10
-}
-"#;
-
-    /// This settings blob does not contain an "any" tunnel type
-    pub const V11_SETTINGS_OPENVPN_LOCATION: &str = r#"
-{
-  "relay_settings": {
-    "normal": {
-      "location": {
-        "only": {
-          "location": {
-            "hostname": "at-vie-ovpn-001"
-          }
-        }
-      },
-      "providers": "any",
-      "ownership": "any",
-      "tunnel_protocol": "openvpn",
-      "wireguard_constraints": {
-        "port": "any",
-        "ip_version": "any",
-        "use_multihop": false,
-        "entry_location": {
-          "only": {
+        });
+        migrate_tunnel_type_inner(&mut old_settings).unwrap();
+        let new_settings: serde_json::Value = json!({
+            "tunnel_protocol": "wireguard",
             "location": {
-              "country": "se"
+                "only": {
+                    "location": {
+                        "country": "se"
+                    }
+                }
             }
-          }
-        }
-      },
-      "openvpn_constraints": {
-        "port": "any"
-      }
+        });
+        assert_eq!(&old_settings, &new_settings);
     }
-  },
-  "bridge_settings": {
-    "bridge_type": "normal",
-    "normal": {
-      "location": "any",
-      "providers": "any",
-      "ownership": "any"
-    },
-    "custom": null
-  },
-  "obfuscation_settings": {
-    "selected_obfuscation": "auto",
-    "udp2tcp": {
-      "port": "any"
-    }
-  },
-  "bridge_state": "auto",
-  "custom_lists": {
-    "custom_lists": []
-  },
-  "api_access_methods": {
-    "direct": {
-      "id": "d81121bf-c942-4ca4-971f-8ea6581bc915",
-      "name": "Direct",
-      "enabled": true,
-      "access_method": {
-        "built_in": "direct"
-      }
-    },
-    "mullvad_bridges": {
-      "id": "92135711-534d-4950-963d-93e446a792e4",
-      "name": "Mullvad Bridges",
-      "enabled": true,
-      "access_method": {
-        "built_in": "bridge"
-      }
-    },
-    "custom": []
-  },
-  "allow_lan": false,
-  "block_when_disconnected": false,
-  "auto_connect": false,
-  "tunnel_options": {
-    "openvpn": {
-      "mssfix": null
-    },
-    "wireguard": {
-      "mtu": null,
-      "quantum_resistant": "auto",
-      "rotation_interval": null
-    },
-    "generic": {
-      "enable_ipv6": false
-    },
-    "dns_options": {
-      "state": "default",
-      "default_options": {
-        "block_ads": false,
-        "block_trackers": false,
-        "block_malware": false,
-        "block_adult_content": false,
-        "block_gambling": false,
-        "block_social_media": false
-      },
-      "custom_options": {
-        "addresses": []
-      }
-    }
-  },
-  "relay_overrides": [],
-  "show_beta_releases": true,
-  "settings_version": 11
-}
-"#;
 }
