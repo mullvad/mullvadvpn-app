d5633d5925ac10f42563327d9ab89571ba35bcb4
Bug Magnet <marco.nikic@mullvad.net>
2025-03-11 16:54:00 +0100

    Build and cache relay list per configuration excluding Release

--- ios/relays-prebuild.sh
+++ ios/relays-prebuild.sh
@@ -5,7 +5,12 @@ if [ -z "$PROJECT_DIR" ]; then
   exit 1
 fi
 
-RELAYS_FILE="$PROJECT_DIR/MullvadREST/Assets/relays.json"
+# Do not download the file for release builds, a different script will take care of that.
+if [ "$CONFIGURATION" == "Release" ]; then
+  return 0
+fi
+
+BACKUP_FILE="$CONFIGURATION_TEMP_DIR/relays.json"
 
 if [ "$CONFIGURATION" == "Staging" ]; then
   API_ENDPOINT="api.stagemole.eu"
@@ -13,7 +18,10 @@ else
   API_ENDPOINT="api.mullvad.net"
 fi
 
-if [ ! -f "$RELAYS_FILE" ]; then
-  echo "Download relays file"
-  curl https://"$API_ENDPOINT"/app/v1/relays -s -o "$RELAYS_FILE"
+if [ ! -f "$BACKUP_FILE" ]; then
+  echo "Downloading relays file for $CONFIGURATION"
+  curl https://"$API_ENDPOINT"/app/v1/relays -s -o "$BACKUP_FILE"
 fi
+
+RELAYS_FILE="$PROJECT_DIR/MullvadREST/Assets/relays.json"
+cp "$BACKUP_FILE" "$RELAYS_FILE"
