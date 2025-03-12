d479a5cfc8632cc9b00fcf6d2aa43f465dc868e5
David Göransson <david.goransson@mullvad.net>
2025-03-12 19:28:11 +0100

    Bump junit5 android to 1.7.0

--- android/buildSrc/src/main/kotlin/Versions.kt
+++ android/buildSrc/src/main/kotlin/Versions.kt
@@ -7,6 +7,6 @@ object Versions {
     const val ndkVersion = "27.2.12479018"
 
     const val junitJupiter = "5.12.0"
-    const val junit5Android = "1.6.0"
+    const val junit5Android = "1.7.0"
     const val junit5Plugin = "1.11.3.0"
 }
