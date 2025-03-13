c45445a5b45ea4249b5f492aad2c5a48a34b7352
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-03-13 12:00:36 +0100

    Remove suggestedUpgrade in unsupported version notification
    It is never used and therefore does not need it.

--- desktop/packages/mullvad-vpn/src/main/version.ts
+++ desktop/packages/mullvad-vpn/src/main/version.ts
@@ -95,7 +95,6 @@ export default class Version {
       new UnsupportedVersionNotificationProvider({
         supported: latestVersionInfo.supported,
         consistent: this.currentVersionData.isConsistent,
-        suggestedUpgrade: latestVersionInfo.suggestedUpgrade,
         suggestedIsBeta,
       }),
       new UpdateAvailableNotificationProvider({
