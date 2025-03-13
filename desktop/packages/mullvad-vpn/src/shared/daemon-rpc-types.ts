d7c26d3bf64df8e1d519c6fa70fd86e36ab45369
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-03-13 12:10:59 +0100

    Update IAppVersionInfo property suggestedUpgrade
    To reflect changes in management interface where
    suggestedUpgrade has changed from a string to a
    message with its own fields.

--- desktop/packages/mullvad-vpn/src/shared/daemon-rpc-types.ts
+++ desktop/packages/mullvad-vpn/src/shared/daemon-rpc-types.ts
@@ -381,9 +381,15 @@ export interface IDnsOptions {
   };
 }
 
+export type AppVersionInfoSuggestedUpgrade = {
+  changelog?: string;
+  verifiedInstallerPath?: string;
+  version: string;
+};
+
 export interface IAppVersionInfo {
   supported: boolean;
-  suggestedUpgrade?: string;
+  suggestedUpgrade?: AppVersionInfoSuggestedUpgrade;
   suggestedIsBeta?: boolean;
 }
 
