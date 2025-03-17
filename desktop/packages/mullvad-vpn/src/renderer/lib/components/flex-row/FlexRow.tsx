ba2fb236126b6b3af12d097f59a97b70b021528c
Oliver <oliver@mohlin.dev>
2025-03-17 14:09:48 +0100

    Add FlexRow component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/flex-row/FlexRow.tsx
@@ -0,0 +1,5 @@
+import { Flex, FlexProps } from '../flex';
+
+type FlexRowProps = Omit<FlexProps, '$flexDirection'>;
+
+export const FlexRow = (props: FlexRowProps) => <Flex $flexDirection="row" {...props} />;
