ed28219f9798598dde86fce0e4cfd04679468517
Markus Pettersson <markus.pettersson@mullvad.net>
2025-03-18 12:35:47 +0100

    Fix fall back to building test-runner if necessary

--- test/scripts/utils/lib.sh
+++ test/scripts/utils/lib.sh
@@ -161,11 +161,7 @@ function run_tests_for_os {
         exit 1
     fi
 
-    if [ -n "${TEST_DIST_DIR+x}" ]; then
-        if [ ! -x "${TEST_DIST_DIR%/}/test-runner" ]; then
-            executable_not_found_in_dist_error test-runner
-        fi
-
+    if [ -n "${TEST_DIST_DIR+x}" ] && [ -x "${TEST_DIST_DIR%/}/test-runner" ]; then
         echo "**********************************"
         echo "* Using test-runner in $TEST_DIST_DIR"
         echo "**********************************"
