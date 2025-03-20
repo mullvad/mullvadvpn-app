c103566b2a0bec8a24c9900a064f73e97e40d97d
Joakim Hulthe <joakim.hulthe@mullvad.net>
2025-03-20 19:57:53 +0100

    Add boringtun feature to daemon, and set worker thread count

--- mullvad-daemon/src/runtime.rs
+++ mullvad-daemon/src/runtime.rs
@@ -2,6 +2,13 @@ use tokio::runtime;
 
 pub fn new_multi_thread() -> runtime::Builder {
     let mut builder = runtime::Builder::new_multi_thread();
+
+    // When using boringtun, network I/O will happen on tokio threads.
+    // in that case, we want all the threads. In other cases, limit thread count to 4.
+    if cfg!(not(feature = "boringtun")) {
+        builder.worker_threads(4);
+    }
+
     builder.enable_all();
     builder
 }
