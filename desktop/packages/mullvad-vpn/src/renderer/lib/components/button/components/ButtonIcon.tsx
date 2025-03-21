23b21385f62c8320083049e41cc8889e5a44724d
Oliver <oliver@mohlin.dev>
2025-03-21 11:26:51 +0100

    Change ButtonIcon color when disabled

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/button/components/ButtonIcon.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/button/components/ButtonIcon.tsx
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 
+import { Colors } from '../../../foundations';
 import { Icon, IconProps } from '../../icon';
+import { useButtonContext } from '../ButtonContext';
 
 type ButtonIconProps = Omit<IconProps, 'size'>;
 
 export const StyledIcon = styled(Icon)({});
 
 export const ButtonIcon = ({ ...props }: ButtonIconProps) => {
-  return <StyledIcon size="medium" aria-hidden="true" {...props} />;
+  const { disabled } = useButtonContext();
+  return (
+    <StyledIcon
+      size="medium"
+      aria-hidden="true"
+      color={disabled ? Colors.white40 : Colors.white}
+      {...props}
+    />
+  );
 };
