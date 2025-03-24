69621c7ac0156cfa1f822af74c2abdad743d61a2
Oliver <oliver@mohlin.dev>
2025-03-24 15:51:05 +0100

    Update ListItem sub component types

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/ListItemText.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/ListItemText.tsx
@@ -2,15 +2,9 @@ import { Colors } from '../../../foundations';
 import { Text, TextProps } from '../../typography';
 import { useListItem } from '../ListItemContext';
 
-export type ListItemProps<E extends React.ElementType = 'span'> = Omit<TextProps<E>, 'variant'> & {
-  variant?: Extract<TextProps<E>['variant'], 'labelTiny' | 'footnoteMini'>;
-};
+export type ListItemProps<E extends React.ElementType = 'span'> = TextProps<E>;
 
-export const ListItemText = ({ variant = 'labelTiny', children, ...props }: ListItemProps) => {
+export const ListItemText = <E extends React.ElementType = 'span'>(props: ListItemProps<E>) => {
   const { disabled } = useListItem();
-  return (
-    <Text variant={variant} color={disabled ? Colors.white40 : Colors.white60} {...props}>
-      {children}
-    </Text>
-  );
+  return <Text variant="labelTiny" color={disabled ? Colors.white40 : Colors.white60} {...props} />;
 };
