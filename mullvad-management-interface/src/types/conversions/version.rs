0d2e89459d4ca94e8daba5bd8fd0b10990b202dc
David Lönnhager <david.l@mullvad.net>
2025-03-25 16:31:49 +0100

    Implement new app version (TEMP)

--- mullvad-management-interface/src/types/conversions/version.rs
+++ mullvad-management-interface/src/types/conversions/version.rs
@@ -1,23 +1,62 @@
+use std::path::PathBuf;
+
 use crate::types::proto;
+
+use super::FromProtobufTypeError;
 
 impl From<mullvad_types::version::AppVersionInfo> for proto::AppVersionInfo {
     fn from(version_info: mullvad_types::version::AppVersionInfo) -> Self {
         Self {
             supported: version_info.supported,
-            latest_stable: version_info.latest_stable,
-            latest_beta: version_info.latest_beta,
-            suggested_upgrade: version_info.suggested_upgrade,
+            suggested_upgrade: version_info
+                .suggested_upgrade
+                .map(proto::SuggestedUpgrade::from),
         }
     }
 }
 
-impl From<proto::AppVersionInfo> for mullvad_types::version::AppVersionInfo {
-    fn from(version_info: proto::AppVersionInfo) -> Self {
-        Self {
+impl TryFrom<proto::AppVersionInfo> for mullvad_types::version::AppVersionInfo {
+    type Error = FromProtobufTypeError;
+
+    fn try_from(version_info: proto::AppVersionInfo) -> Result<Self, Self::Error> {
+        Ok(Self {
             supported: version_info.supported,
-            latest_stable: version_info.latest_stable,
-            latest_beta: version_info.latest_beta,
-            suggested_upgrade: version_info.suggested_upgrade,
+            suggested_upgrade: version_info
+                .suggested_upgrade
+                .map(mullvad_types::version::SuggestedUpgrade::try_from)
+                .transpose()?,
+        })
+    }
+}
+
+impl From<mullvad_types::version::SuggestedUpgrade> for proto::SuggestedUpgrade {
+    fn from(suggested_upgrade: mullvad_types::version::SuggestedUpgrade) -> Self {
+        Self {
+            version: suggested_upgrade.version.to_string(),
+            changelog: suggested_upgrade.changelog,
+            verified_installer_path: suggested_upgrade
+                .verified_installer_path
+                .and_then(|path| path.to_str().map(str::to_owned)),
         }
+    }
+}
+
+impl TryFrom<proto::SuggestedUpgrade> for mullvad_types::version::SuggestedUpgrade {
+    type Error = FromProtobufTypeError;
+
+    fn try_from(suggested_upgrade: proto::SuggestedUpgrade) -> Result<Self, Self::Error> {
+        // TODO: we probably don't need to convert in this direction
+        let version = suggested_upgrade.version.parse().map_err(|_err| {
+            FromProtobufTypeError::InvalidArgument("invalid Mullvad app version")
+        })?;
+        let verified_installer_path = suggested_upgrade
+            .verified_installer_path
+            .map(|path| PathBuf::from(&path));
+
+        Ok(Self {
+            version,
+            changelog: suggested_upgrade.changelog,
+            verified_installer_path,
+        })
     }
 }
--- mullvad-types/src/version.rs
+++ mullvad-types/src/version.rs
@@ -14,37 +14,14 @@ pub struct AppVersionInfo {
     ///
     /// The user should really upgrade when this is false.
     pub supported: bool,
-    /// Latest stable version
-    pub latest_stable: AppVersion,
-    /// Equal to `latest_stable` when the newest release is a stable release. But will contain
-    /// beta versions when those are out for testing.
-    pub latest_beta: AppVersion,
     /// Whether should update to newer version
-    pub suggested_upgrade: Option<AppVersion>,
+    pub suggested_upgrade: Option<SuggestedUpgrade>,
 }
 
-pub type AppVersion = String;
-
-
-/// AppVersionInfo represents the current stable and the current latest release versions of the
-/// Mullvad VPN app.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct AppVersionInfo2 {
-    /// False if Mullvad has stopped supporting the currently running version. This could mean
-    /// a number of things. For example:
-    /// * API endpoints it uses might not work any more.
-    /// * Software bundled with this version, such as OpenVPN or OpenSSL, has known security
-    ///   issues, so using it is no longer recommended.
-    ///
-    /// The user should really upgrade when this is false.
-    pub supported: bool,
-    /// Whether should update to newer version
-    pub suggested_upgrade: Option<AppVersion>,
-}
-
 pub struct SuggestedUpgrade {
     /// Version available for update
-    pub version: AppVersion,
+    pub version: mullvad_version::Version,
     /// Changelog
     pub changelog: Option<String>,
     /// Path to the available installer, iff it has been verified
