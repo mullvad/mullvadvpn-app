e722b7a3905c2fdab0e982f6aa0d15f63afc878b
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-03-26 13:47:18 +0100

    Handle open app page when not app store is installed

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/extensions/UriHandlerExtensions.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/extensions/UriHandlerExtensions.kt
@@ -9,22 +9,24 @@ import net.mullvad.mullvadvpn.lib.common.util.createAccountUri
 import net.mullvad.mullvadvpn.lib.model.WebsiteAuthToken
 
 @Composable
-fun UriHandler.createOpenAccountPageHook(): (WebsiteAuthToken?) -> Unit {
+fun UriHandler.createOpenAccountPageHook(onFailure: () -> Unit = {}): (WebsiteAuthToken?) -> Unit {
     val accountUrl = stringResource(id = R.string.account_url)
     return { token ->
         val accountUri = createAccountUri(accountUrl, token).toString()
-        safeOpenUri(accountUri)
+        safeOpenUri(accountUri, onFailure)
     }
 }
 
-fun UriHandler.createUriHook(uri: String): () -> Unit = { safeOpenUri(uri) }
+fun UriHandler.createUriHook(uri: String, onFailure: () -> Unit = {}): () -> Unit = {
+    safeOpenUri(uri, onFailure)
+}
 
-private fun UriHandler.safeOpenUri(uri: String) {
+private fun UriHandler.safeOpenUri(uri: String, onFailure: () -> Unit) {
     try {
         openUri(uri)
     } catch (e: IllegalArgumentException) {
         // E.g user has no browser or invalid uri
         Logger.e("Failed to open uri: $uri", e)
-        e.cause?.let { Logger.e("cause:", it) }
+        onFailure()
     }
 }
