976514d42c1319377808f39f60ba52ef8c102933
Oliver <oliver@mohlin.dev>
2025-03-26 14:10:38 +0100

    Move common hooks to respective folders

--- desktop/packages/mullvad-vpn/src/renderer/redux/version/hooks/useVersionConsistent.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/redux/version/hooks/useVersionConsistent.tsx
@@ -1,4 +1,4 @@
-import { useSelector } from '../../../../redux/store';
+import { useSelector } from '../../store';
 
 export const useVersionConsistent = () => {
   return { consistent: useSelector((state) => state.version.consistent) };
