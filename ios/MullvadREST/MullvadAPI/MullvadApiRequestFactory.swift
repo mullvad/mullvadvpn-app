8fbe0054654841d52e44ddf2d669f330e9c70080
mojganii <mojgan.jelodar@codic.se>
2025-03-26 15:24:08 +0100

    Add metadata support to send problem report

--- ios/MullvadREST/MullvadAPI/MullvadApiRequestFactory.swift
+++ ios/MullvadREST/MullvadAPI/MullvadApiRequestFactory.swift
@@ -24,28 +24,34 @@ public struct MullvadApiRequestFactory: Sendable {
 
             let rawCompletionPointer = Unmanaged.passRetained(completionPointer).toOpaque()
 
-            return switch request {
+            switch request {
             case let .getAddressList(retryStrategy):
-                MullvadApiCancellable(handle: mullvad_api_get_addresses(
+                return MullvadApiCancellable(handle: mullvad_api_get_addresses(
                     apiContext.context,
                     rawCompletionPointer,
                     retryStrategy.toRustStrategy()
                 ))
 
             case let .getRelayList(retryStrategy, etag: etag):
-                MullvadApiCancellable(handle: mullvad_api_get_relays(
+                return MullvadApiCancellable(handle: mullvad_api_get_relays(
                     apiContext.context,
                     rawCompletionPointer,
                     retryStrategy.toRustStrategy(),
                     etag
                 ))
             case let .sendProblemReport(retryStrategy, problemReportRequest):
-                MullvadApiCancellable(handle: mullvad_api_send_problem_report(
-                    apiContext.context,
-                    rawCompletionPointer,
-                    retryStrategy.toRustStrategy(),
-                    problemReportRequest.toRust()
-                ))
+                let rustRequest = RustProblemReportRequest(from: problemReportRequest)
+                return MullvadApiCancellable(
+                    handle: mullvad_api_send_problem_report(
+                        apiContext.context,
+                        rawCompletionPointer,
+                        retryStrategy.toRustStrategy(),
+                        rustRequest.toRust()
+                    ),
+                    deinitializer: {
+                        rustRequest.release()
+                    }
+                )
             }
         }
     }
@@ -53,25 +59,4 @@ public struct MullvadApiRequestFactory: Sendable {
 
 extension REST {
     public typealias MullvadApiRequestHandler = (((MullvadApiResponse) throws -> Void)?) -> MullvadApiCancellable
-}
-
-private extension REST.ProblemReportRequest {
-    func toRust() -> UnsafePointer<SwiftProblemReportRequest> {
-        let structPointer = UnsafeMutablePointer<SwiftProblemReportRequest>.allocate(capacity: 1)
-
-        let addressPointer = address.toUnsafePointer()
-        let messagePointer = message.toUnsafePointer()
-        let logPointer = log.toUnsafePointer()
-
-        structPointer.initialize(to: SwiftProblemReportRequest(
-            address: addressPointer,
-            address_len: UInt(address.utf8.count),
-            message: messagePointer,
-            message_len: UInt(message.utf8.count),
-            log: logPointer,
-            log_len: UInt(log.utf8.count)
-        ))
-
-        return UnsafePointer(structPointer)
-    }
 }
---
+++ ios/MullvadREST/Rust/RustProblemReportRequest.swift
@@ -0,0 +1,57 @@
+//
+//  RustProblemReportRequest.swift
+//  MullvadVPN
+//
+//  Created by Mojgan on 2025-03-21.
+//  Copyright © 2025 Mullvad VPN AB. All rights reserved.
+//
+import MullvadLogging
+import MullvadRustRuntime
+
+final public class RustProblemReportRequest {
+    typealias StringPointer = (pointer: UnsafePointer<UInt8>?, size: UInt)?
+    private let logger = Logger(label: "RustProblemReportRequest")
+    private let problemReportMetaData: ProblemReportMetadata
+    private let addressPointer: StringPointer
+    private let messagePointer: StringPointer
+    private let logPointer: StringPointer
+    public init(from request: REST.ProblemReportRequest) {
+        addressPointer = request.address.toUnsafePointer()
+        messagePointer = request.message.toUnsafePointer()
+        logPointer = request.log.toUnsafePointer()
+
+        self.problemReportMetaData = swift_problem_report_meta_data_new()
+
+        for (key, value) in request.metadata {
+            key.withCString { keyPtr in
+                value.withCString { valuePtr in
+                    if swift_problem_report_meta_data_add(problemReportMetaData, keyPtr, valuePtr) == false {
+                        logger
+                            .error(
+                                "Failed to add metadata. Key: '\(key)' might be invalid or contain unsupported characters."
+                            )
+                    }
+                }
+            }
+        }
+    }
+
+    public func toRust() -> SwiftProblemReportRequest {
+        return SwiftProblemReportRequest(
+            address: addressPointer?.pointer ?? nil,
+            address_len: addressPointer?.size ?? 0,
+            message: messagePointer?.pointer ?? nil,
+            message_len: messagePointer?.size ?? 0,
+            log: logPointer?.pointer ?? nil,
+            log_len: logPointer?.size ?? 0,
+            meta_data: problemReportMetaData
+        )
+    }
+
+    public func release() {
+        swift_problem_report_meta_data_free(problemReportMetaData)
+        addressPointer?.pointer?.deallocate()
+        messagePointer?.pointer?.deallocate()
+        logPointer?.pointer?.deallocate()
+    }
+}
