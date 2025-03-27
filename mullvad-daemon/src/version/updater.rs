f5ad446023edc8e4ab9947a22810cdba60532a19
David Lönnhager <david.l@mullvad.net>
2025-03-27 21:47:49 +0100

    Simplify updater design

--- mullvad-daemon/src/version/updater.rs
+++ mullvad-daemon/src/version/updater.rs
@@ -6,46 +6,31 @@ AppUpgradeDownloadProgress
 AppUpgradeVerifyingInstaller
 */
 
-// TODO: How should interruptions be handled?
-// Probably magically resume when going from Idle/DownloadFailed -> Downloading
-
-// TODO: handle abort
-// TODO: handle beginning/resuming download
-
 use std::path::PathBuf;
 use std::time::Duration;
-use futures::channel::mpsc;
+use futures::{channel::{mpsc, oneshot}, SinkExt};
 use mullvad_update::app::AppDownloaderParameters;
 
 pub struct AppUpdater {
-    state: UpdateState,
-    event_tx: mpsc::Sender<UpdateStateEvent>,
+    abort_handle: tokio::task::JoinHandle<anyhow::Result<()>>,
 }
 
-pub enum UpdaterCommand {
-    /// Download or resume download
-    DownloadAndVerify {
-        
-    },
-    /// Abort the current operation
-    Abort,
-}
+pub type AbortHandle = oneshot::Sender<()>;
 
-/// App updater state
-pub enum UpdateState {
-    Idle,
-    /// An installer is being downloaded
+/// App updater event
+pub enum UpdateEvent {
+    /// Download progress update
     Downloading {
         /// A fraction in `[0,1]` that describes how much of the installer has been downloaded
         complete_frac: f32,
         /// Estimated time left
         time_left: Duration,
     },
-    /// Failed to download installer
+    /// Download failed due to some error
     DownloadFailed,
-    /// The downloaded installer is being verified
+    /// Download completed, so verifying now
     Verifying,
-    /// VerificationFailed
+    /// The verification failed due to some error
     VerificationFailed,
     /// There is a downloaded and verified installer available
     Verified {
@@ -53,38 +38,20 @@ pub enum UpdateState {
     },
 }
 
-pub type UpdateStateEvent = UpdateState;
-
 impl AppUpdater {
+    /// Begin or resume download of `version`
     pub fn spawn(
-        event_tx: mpsc::Sender<UpdateStateEvent>,
-    ) -> mpsc::Sender<UpdaterCommand> {
-        let (event_tx, event_rx) = mpsc::channel(1);
-        tokio::spawn(async move {
-            Self {
-                state: UpdateState::Idle,
-                event_tx,
-            }
-            .run(event_rx)
-            .await
-        });
-        updater
-    }
+        version: mullvad_update::version::Version,
+        event_tx: mpsc::Sender<UpdateEvent>,
+    ) -> AppUpdater {
+        // TODO: select url
+        let url = version.urls[0].clone();
 
-    fn new(event_tx: mpsc::Sender<UpdateStateEvent>) -> Self {
-        Self {
-            state: UpdateState::Idle,
-            event_tx,
-        }
-    }
-
-    fn begin_download(&mut self, version: mullvad_update::version::Version) {
         let params = AppDownloaderParameters {
             app_version: version.version,
-            // TODO: select url
-            app_url: version.urls[0],
+            app_url: url.clone(),
             app_size: version.size,
-            app_progress: ProgressUpdater::default(),
+            app_progress: ProgressUpdater::new(event_tx.clone(), url),
             app_sha256: version.sha256,
             // TODO: mkdir
             cache_dir: cache_dir().join("mullvad-update"),
@@ -92,53 +59,51 @@ impl AppUpdater {
         let downloader = HttpAppDownloader::from(params);
 
         // TODO: begin download + verify
-    }
 
-    async fn run(mut self, mut event_rx: mpsc::Receiver<UpdateStateEvent>) {
-        loop {
-            match self.state {
-                UpdateState::Idle => {
-                    // Check for updates
-                    // If update available, download
-                    // If download fails, go to DownloadFailed
-                    // If download succeeds, go to Verifying
-                }
-                UpdateState::Downloading { .. } => {
-                    // Check for download progress
-                    // If download fails, go to DownloadFailed
-                    // If download succeeds, go to Verifying
-                }
-                UpdateState::DownloadFailed => {
-                    // Retry download
-                }
-                UpdateState::Verifying => {
-                    // Check for verification progress
-                    // If verification fails, go to VerificationFailed
-                    // If verification succeeds, go to Verified
-                }
-                UpdateState::VerificationFailed => {
-                    // Retry verification
-                }
-                UpdateState::Verified { .. } => {
-                    // Notify user that an update is available
-                    // If user accepts, start upgrade
-                }
+        tokio::spawn(async move {
+            if let Err(error) = downloader.download_executable().await {
+                event_tx.send(UpdateEvent::DownloadFailed);
+                return;
             }
-            if let Some(event) = event_rx.next().await {
-                self.state = event;
+
+            event_tx.send(UpdateEvent::Verifying);
+
+            if let Err(error) = downloader.verify().await {
+                event_tx.send(UpdateEvent::VerificationFailed);
+                return;
             }
-        }
+
+            event_tx.send(UpdateEvent::Verified { verified_installer_path: downloader.bin_path() });
+        });
+
+        AppUpdater { abort_handle }
     }
 }
 
 #[derive(Default)]
 struct ProgressUpdater {
     //began_download: Duration,
+    url: String,
+    event_tx: mpsc::Sender<UpdateEvent>,
     complete_frac: f32,
 }
 
+impl ProgressUpdater {
+    fn new(
+        url: String,
+        event_tx: mpsc::Sender<UpdateEvent>,
+    ) -> Self {
+        Self {
+            url,
+            ..Default::default()
+        }
+    }
+}
+
 impl mullvad_update::client::fetch::ProgressUpdater for ProgressUpdater {
-    fn set_url(&mut self, url: &str) {}
+    fn set_url(&mut self, url: &str) {
+        // ignored since we already know the URL
+    }
 
     fn set_progress(&mut self, fraction_complete: f32) {
         if (self.complete_frac - fraction_complete).abs() < 0.01 {
