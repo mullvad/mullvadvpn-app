1cfb017c173ef98c41e15defa19e91e01a1a610c
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-03-27 14:58:19 +0100

    Add ability to start installer

--- desktop/packages/mullvad-vpn/src/main/app-upgrade.ts
+++ desktop/packages/mullvad-vpn/src/main/app-upgrade.ts
@@ -1,7 +1,13 @@
+import { execFile } from 'child_process';
+import fs from 'fs/promises';
+import { promisify } from 'util';
+
 import { DaemonAppUpgradeEvent } from '../shared/daemon-rpc-types';
 import log from '../shared/logging';
 import { DaemonRpc, SubscriptionListener } from './daemon-rpc';
 import { IpcMainEventChannel } from './ipc-event-channel';
+
+const execFilePromise = promisify(execFile);
 
 export default class AppUpgrade {
   public constructor(private daemonRpc: DaemonRpc) {}
@@ -13,6 +19,17 @@ export default class AppUpgrade {
 
     IpcMainEventChannel.app.handleUpgradeAbort(() => {
       this.daemonRpc.appUpgradeAbort();
+    });
+
+    IpcMainEventChannel.app.handleUpgradeInstallerStart(async (verifiedInstallerPath: string) => {
+      try {
+        await this.startInstaller(verifiedInstallerPath);
+        IpcMainEventChannel.app.notifyUpgradeEvent?.({
+          type: 'APP_UPGRADE_STATUS_STARTED_INSTALLER',
+        });
+      } catch {
+        IpcMainEventChannel.app.notifyUpgradeError?.('START_INSTALLER_FAILED');
+      }
     });
   }
 
@@ -33,5 +50,26 @@ export default class AppUpgrade {
     this.daemonRpc.subscribeAppUpgradeEventListener(daemonAppUpgradeEventListener);
 
     return daemonAppUpgradeEventListener;
+  }
+
+  private async startInstaller(verifiedInstallerPath: string) {
+    await this.isInstallerExecutable(verifiedInstallerPath);
+    await this.executeInstaller(verifiedInstallerPath);
+  }
+
+  private async executeInstaller(verifiedInstallerPath: string) {
+    try {
+      await execFilePromise(verifiedInstallerPath);
+    } catch {
+      throw new Error(`Could not start installer at path: ${verifiedInstallerPath}`);
+    }
+  }
+
+  private async isInstallerExecutable(verifiedInstallerPath: string) {
+    try {
+      await fs.access(verifiedInstallerPath, fs.constants.X_OK);
+    } catch {
+      throw new Error(`An executable installer is not available at path: ${verifiedInstallerPath}`);
+    }
   }
 }
