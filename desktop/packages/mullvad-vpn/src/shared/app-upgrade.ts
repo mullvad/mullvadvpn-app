a0efcb76c92480966903bad89709f68a12d67bdb
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-03-27 15:45:56 +0100

    Add AppUpgrade events related to starting the installer

--- desktop/packages/mullvad-vpn/src/shared/app-upgrade.ts
+++ desktop/packages/mullvad-vpn/src/shared/app-upgrade.ts
@@ -1,5 +1,17 @@
 import { DaemonAppUpgradeError, DaemonAppUpgradeEventStatus } from './daemon-rpc-types';
 
-export type AppUpgradeEvent = DaemonAppUpgradeEventStatus;
+export type AppUpgradeEventStatusStartingInstaller = {
+  type: 'APP_UPGRADE_STATUS_STARTING_INSTALLER';
+};
+
+export type AppUpgradeEventStatusStartedInstaller = {
+  type: 'APP_UPGRADE_STATUS_STARTED_INSTALLER';
+};
+
+export type AppUpgradeEventStatus =
+  | AppUpgradeEventStatusStartingInstaller
+  | AppUpgradeEventStatusStartedInstaller;
+
+export type AppUpgradeEvent = DaemonAppUpgradeEventStatus | AppUpgradeEventStatus;
 
 export type AppUpgradeError = DaemonAppUpgradeError | 'START_INSTALLER_FAILED';
