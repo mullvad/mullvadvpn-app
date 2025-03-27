c6b1dc2e7e6faed71b7c38933a79e16dfbdc91b5
Oliver <oliver@mohlin.dev>
2025-03-27 09:26:56 +0100

    Move settings view to separate folder

--- desktop/packages/mullvad-vpn/src/renderer/components/AppRouter.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/AppRouter.tsx
@@ -27,7 +27,6 @@ import MultihopSettings from './MultihopSettings';
 import OpenVpnSettings from './OpenVpnSettings';
 import ProblemReport from './ProblemReport';
 import SelectLanguage from './SelectLanguage';
-import Settings from './Settings';
 import SettingsImport from './SettingsImport';
 import SettingsTextImport from './SettingsTextImport';
 import Shadowsocks from './Shadowsocks';
@@ -36,7 +35,7 @@ import Support from './Support';
 import TooManyDevices from './TooManyDevices';
 import UdpOverTcp from './UdpOverTcp';
 import UserInterfaceSettings from './UserInterfaceSettings';
-import { AppInfoView, ChangelogView } from './views';
+import { AppInfoView, ChangelogView, SettingsView } from './views';
 import VpnSettings from './VpnSettings';
 import WireguardSettings from './WireguardSettings';
 
@@ -62,7 +61,7 @@ export default function AppRouter() {
         <Route exact path={RoutePath.timeAdded} component={TimeAdded} />
         <Route exact path={RoutePath.setupFinished} component={SetupFinished} />
         <Route exact path={RoutePath.account} component={Account} />
-        <Route exact path={RoutePath.settings} component={Settings} />
+        <Route exact path={RoutePath.settings} component={SettingsView} />
         <Route exact path={RoutePath.selectLanguage} component={SelectLanguage} />
         <Route exact path={RoutePath.userInterfaceSettings} component={UserInterfaceSettings} />
         <Route exact path={RoutePath.multihopSettings} component={MultihopSettings} />
