08050cd7bc895b40fc6687171fc5ac0f8a88d649
Oliver <oliver@mohlin.dev>
2025-03-28 13:12:55 +0100

    Update animation component to allow passing multiple animations

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/animate/hooks/useAnimations.tsx
@@ -0,0 +1,29 @@
+import { css, RuleSet } from 'styled-components';
+
+import { Animation } from '../Animate';
+import { animations } from '../animations';
+import { createAnimationDeclaration } from '../utils';
+
+export const useAnimations = (values: Animation[]) => {
+  const inAnimations: Array<{ name: string; rule: RuleSet }> = [];
+  const outAnimations: Array<{ name: string; rule: RuleSet }> = [];
+
+  values.forEach((animation) => {
+    if (animation.type === 'fade') {
+      inAnimations.push(animations.fade.in);
+      outAnimations.push(animations.fade.out);
+    } else if (animation.type === 'wipe' && animation.direction === 'vertical') {
+      inAnimations.push(animations.wipeDown.in);
+      outAnimations.push(animations.wipeDown.out);
+    }
+  });
+
+  return css`
+    ${inAnimations.map((animation) => animation.rule)}
+    ${outAnimations.map((animation) => animation.rule)}
+    ${createAnimationDeclaration(outAnimations)}
+    &&[data-show='true'] {
+      ${createAnimationDeclaration(inAnimations)}
+    }
+  `;
+};
