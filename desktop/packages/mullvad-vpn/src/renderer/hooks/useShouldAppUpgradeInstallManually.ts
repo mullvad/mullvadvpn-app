5cedc5be8fb16ca61f39f07fb96e8ebe7f027535
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-03-28 16:13:01 +0100

    Refactor hook useShouldAppUpgradeInstallManually
    Remove the need for one of the early returns.

--- desktop/packages/mullvad-vpn/src/renderer/hooks/useShouldAppUpgradeInstallManually.ts
+++ desktop/packages/mullvad-vpn/src/renderer/hooks/useShouldAppUpgradeInstallManually.ts
@@ -9,22 +9,20 @@ export const useShouldAppUpgradeInstallManually = () => {
   const hasAppUpgradeVerifiedInstallerPath = useHasAppUpgradeVerifiedInstallerPath();
   const hasAppUpgradeEvent = useHasAppUpgradeEvent();
 
-  if (!hasAppUpgradeVerifiedInstallerPath) {
-    return false;
-  }
+  if (hasAppUpgradeVerifiedInstallerPath) {
+    if (hasAppUpgradeError) {
+      if (appUpgradeError === 'START_INSTALLER_FAILED') {
+        return true;
+      }
 
-  if (hasAppUpgradeError) {
-    if (appUpgradeError === 'START_INSTALLER_FAILED') {
-      return true;
+      return false;
     }
 
-    return false;
-  }
-
-  // The absence of the appUpgradeEvent means that the upgrade has been downloaded
-  // and the app has been exited and restarted.
-  if (!hasAppUpgradeEvent) {
-    return true;
+    // The absence of the appUpgradeEvent means that the upgrade has been downloaded
+    // and the app has been exited and restarted.
+    if (!hasAppUpgradeEvent) {
+      return true;
+    }
   }
 
   return false;
