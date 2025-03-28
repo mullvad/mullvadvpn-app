e3d6f98fca91ae592ba269574fbc4d44b4d5c821
Oliver <oliver@mohlin.dev>
2025-03-28 13:10:49 +0100

    Rename ListItemText props type

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/ListItemText.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/ListItemText.tsx
@@ -1,9 +1,9 @@
 import { Text, TextProps } from '../../typography';
 import { useListItem } from '../ListItemContext';
 
-export type ListItemProps<E extends React.ElementType = 'span'> = TextProps<E>;
+export type ListItemTextProps<E extends React.ElementType = 'span'> = TextProps<E>;
 
-export const ListItemText = <E extends React.ElementType = 'span'>(props: ListItemProps<E>) => {
+export const ListItemText = <E extends React.ElementType = 'span'>(props: ListItemTextProps<E>) => {
   const { disabled } = useListItem();
   return <Text variant="labelTiny" color={disabled ? 'whiteAlpha40' : 'whiteAlpha60'} {...props} />;
 };
