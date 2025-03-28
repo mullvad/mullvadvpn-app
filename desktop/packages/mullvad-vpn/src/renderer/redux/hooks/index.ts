62e10a28859833eb00bdd58eaa1425bbd6a0946b
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-03-28 15:04:56 +0100

    Sort exports

--- desktop/packages/mullvad-vpn/src/renderer/redux/hooks/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/redux/hooks/index.ts
@@ -1,5 +1,5 @@
 export * from '../account/hooks';
 export * from '../connection/hooks';
 export * from '../settings/hooks';
-export * from '../version/hooks';
 export * from '../userinterface/hooks';
+export * from '../version/hooks';
