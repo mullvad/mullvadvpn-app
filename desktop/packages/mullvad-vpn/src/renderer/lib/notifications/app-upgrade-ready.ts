8be97cad8cf36ca9dd9dbf43307df31177ec85d8
Oliver <oliver@mohlin.dev>
2025-03-29 15:42:08 +0100

    Add app upgrade ready notification

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/notifications/app-upgrade-ready.ts
@@ -0,0 +1,58 @@
+import { sprintf } from 'sprintf-js';
+
+import { messages } from '../../../shared/gettext';
+import { InAppNotification, InAppNotificationProvider } from '../../../shared/notifications';
+import { RoutePath } from '../routes';
+
+interface AppUpgradeReadyNotificationContext {
+  shouldAppUpgradeInstallManually: boolean;
+  suggestedUpgradeVersion?: string;
+}
+
+export class AppUpgradeReadyNotificationProvider implements InAppNotificationProvider {
+  public constructor(private context: AppUpgradeReadyNotificationContext) {}
+
+  public mayDisplay = () => {
+    return this.context.shouldAppUpgradeInstallManually;
+  };
+
+  public getInAppNotification(): InAppNotification {
+    return {
+      indicator: 'warning',
+      title:
+        // TRANSLATORS: Notification title when the app upgrade is ready to install.
+        messages.pgettext('in-app-notifications', 'READY TO INSTALL UPDATE'),
+      subtitle: [
+        {
+          content: sprintf(
+            // TRANSLATORS: Notification subtitle when the app upgrade is ready to install.
+            // TRANSLATORS: Available placeholders:
+            // TRANSLATORS: - %(suggestedUpgradeVersion)s: Upgrade version to be installed.
+            messages.pgettext(
+              'in-app-notifications',
+              '%(suggestedUpgradeVersion)s is ready to be installed.',
+            ),
+            {
+              suggestedUpgradeVersion: this.context.suggestedUpgradeVersion,
+            },
+          ),
+        },
+        {
+          content:
+            // TRANSLATORS: Notification subtitle when the app upgrade is ready to install.
+            messages.pgettext('in-app-notifications', 'Click here to install update.'),
+          action: {
+            type: 'navigate-internal',
+            link: {
+              // TODO: Change route
+              to: RoutePath.changelog,
+              'aria-label':
+                // TRANSLATORS: Accessibility label for link to app upgrade view.
+                messages.pgettext('accessibility', 'Go to app update page'),
+            },
+          },
+        },
+      ],
+    };
+  }
+}
--- desktop/packages/mullvad-vpn/src/renderer/lib/notifications/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/lib/notifications/index.ts
@@ -5,3 +5,4 @@ export * from './no-open-vpn-server-available';
 export * from './unsupported-wireguard-port';
 export * from './app-upgrade-progress';
 export * from './app-upgrade-error';
+export * from './app-upgrade-ready';
