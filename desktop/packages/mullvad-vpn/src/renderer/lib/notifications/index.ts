ba92e15f9e32f7b6693d34c06d373f1589881172
Oliver <oliver@mohlin.dev>
2025-03-29 15:27:26 +0100

    Add app upgrade progress notification

--- desktop/packages/mullvad-vpn/src/renderer/lib/notifications/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/lib/notifications/index.ts
@@ -3,3 +3,5 @@ export * from './new-version';
 export * from './open-vpn-support-ending';
 export * from './no-open-vpn-server-available';
 export * from './unsupported-wireguard-port';
+export * from './app-upgrade-progress';
+export * from './app-upgrade-error';
