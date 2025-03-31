811325ef32d45c853203a1bcb63a741b9d973b16
David Göransson <david.goransson@mullvad.net>
2025-03-31 16:24:53 +0200

    Disable pre-generation of PQ keys on Android
    By generating 2 PQ keys on start we slow the start up of the android app
    with out about 20%. On android we don't have the daemon running
    continously and by default PQ nor Multihop is enabled, thus all our
    users doing a cold start of the app will be 20% slower. Ideally the
    solution would be to conditionally change the buffer size depending on
    what features are active, e.g PQ gives buffer of 1, PQ and Multihop
    gives a buffer of 2.

--- talpid-tunnel-config-client/src/classic_mceliece.rs
+++ talpid-tunnel-config-client/src/classic_mceliece.rs
@@ -11,7 +11,10 @@ const STACK_SIZE: usize = 2 * 1024 * 1024;
 /// Number of McEliece key pairs to buffer. Note that, using the below algorithm, they take up
 /// around 537 kB each. We therefore only buffer two, which is the largest useful amount, in case of
 /// multihop.
+#[cfg(not(target_os = "android"))]
 pub const BUFSIZE: usize = 2;
+#[cfg(target_os = "android")]
+pub const BUFSIZE: usize = 0;
 
 /// Use the smallest CME variant with NIST security level 3. This variant has significantly smaller
 /// keys than the larger variants, and is considered safe.
@@ -41,8 +44,8 @@ fn spawn_keypair_worker(bufsize: usize) -> mpsc::Receiver<KeyPair> {
     // * The computation takes a long time and must not block the async runtime thread.
     tokio::spawn(async move {
         loop {
-            // We do not want generate the key before we know it can be sent, as they take a lot of
-            // space. Note that `tokio::sync::mpsc` doesn't allow zero capacity channels,
+            // We do not want to generate the key before we know it can be sent, as they take a lot
+            // of space. Note that `tokio::sync::mpsc` doesn't allow zero capacity channels,
             // otherwise we could reduce the channel capacity by one, use `send_blocking` and simply
             // store one of the keys in the stack of the thread.
             let Ok(permit) = tx.reserve().await else {
@@ -64,28 +67,34 @@ fn spawn_keypair_worker(bufsize: usize) -> mpsc::Receiver<KeyPair> {
 }
 
 pub async fn generate_keys() -> KeyPair {
-    let mut rx = KEYPAIR_RX
-        .get_or_init(|| Mutex::new(spawn_keypair_worker(BUFSIZE)))
-        .lock()
-        .await;
+    if BUFSIZE == 0 {
+        keypair_boxed(&mut rand::thread_rng())
+    } else {
+        let mut rx = KEYPAIR_RX
+            .get_or_init(|| Mutex::new(spawn_keypair_worker(BUFSIZE)))
+            .lock()
+            .await;
 
-    let max_retry_attempts = 10;
+        let max_retry_attempts = 10;
 
-    for _ in 0..max_retry_attempts {
-        match rx.recv().await {
-            Some(keypair) => return keypair,
-            None => {
-                // The key generation worker has stopped for some reason. Try to start it again.
-                let _old_rx = mem::replace(&mut *rx, spawn_keypair_worker(BUFSIZE));
+        for _ in 0..max_retry_attempts {
+            match rx.recv().await {
+                Some(keypair) => return keypair,
+                None => {
+                    // The key generation worker has stopped for some reason. Try to start it again.
+                    let _old_rx = mem::replace(&mut *rx, spawn_keypair_worker(BUFSIZE));
+                }
             }
         }
+        panic!("Failed to start key generation worker")
     }
-
-    panic!("Failed to start key generation worker")
 }
 
 /// Spawn a worker which computes and buffers [`BUFSIZE`] of McEliece key pairs, used by PQ tunnels.
 pub fn spawn_keypair_generator() {
+    if BUFSIZE == 0 {
+        return;
+    };
     let mutex = KEYPAIR_RX.get_or_init(|| Mutex::new(spawn_keypair_worker(BUFSIZE)));
 
     // Check if the keygen worker stopped. If so, spawn it again.
