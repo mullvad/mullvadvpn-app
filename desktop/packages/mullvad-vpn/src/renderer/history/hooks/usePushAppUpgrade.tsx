4e857a8912bfcb8b39df8f7554e8c20a037ba3c6
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-03-31 16:24:40 +0200

    Update usePushAppUpgrade to use correct route
    It didn't exist when the hook was added, but it exists now.

--- desktop/packages/mullvad-vpn/src/renderer/history/hooks/usePushAppUpgrade.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/history/hooks/usePushAppUpgrade.tsx
@@ -6,5 +6,5 @@ import { RoutePath } from '../../lib/routes';
 export const usePushAppUpgrade = () => {
   const history = useHistory();
   // TODO: Change to navigate to in app upgrade view once available
-  return React.useCallback(() => history.push(RoutePath.account), [history]);
+  return React.useCallback(() => history.push(RoutePath.appUpgrade), [history]);
 };
