ecf9c6d5b0300e79296ce9d683de05a3f36cdd14
mojganii <mojgan.jelodar@codic.se>
2025-04-02 15:17:14 +0200

    Add abstraction layer for DeviceHandling to call relevant APIs from Mullvad API

---
+++ ios/MullvadREST/MullvadAPI/APIHandlers/MullvadDeviceProxy.swift
@@ -0,0 +1,75 @@
+//
+//  MullvadDeviceProxy.swift
+//  MullvadVPN
+//
+//  Created by Mojgan on 2025-04-02.
+//  Copyright © 2025 Mullvad VPN AB. All rights reserved.
+//
+import MullvadRustRuntime
+import MullvadTypes
+import Operations
+import WireGuardKitTypes
+
+extension REST {
+    final class MullvadDeviceProxy: DeviceHandling, @unchecked Sendable {
+        let transportProvider: APITransportProviderProtocol
+        let dispatchQueue: DispatchQueue
+        let operationQueue = AsyncOperationQueue()
+        let responseDecoder: JSONDecoder
+
+        public init(
+            transportProvider: APITransportProviderProtocol,
+            dispatchQueue: DispatchQueue,
+            responseDecoder: JSONDecoder
+        ) {
+            self.transportProvider = transportProvider
+            self.dispatchQueue = dispatchQueue
+            self.responseDecoder = responseDecoder
+        }
+
+        func getDevice(
+            accountNumber: String,
+            identifier: String,
+            retryStrategy: REST.RetryStrategy,
+            completion: @escaping ProxyCompletionHandler<Device>
+        ) -> Cancellable {
+            AnyCancellable()
+        }
+
+        func getDevices(
+            accountNumber: String,
+            retryStrategy: REST.RetryStrategy,
+            completion: @escaping ProxyCompletionHandler<[Device]>
+        ) -> Cancellable {
+            AnyCancellable()
+        }
+
+        func createDevice(
+            accountNumber: String,
+            request: REST.CreateDeviceRequest,
+            retryStrategy: REST.RetryStrategy,
+            completion: @escaping ProxyCompletionHandler<Device>
+        ) -> Cancellable {
+            AnyCancellable()
+        }
+
+        func deleteDevice(
+            accountNumber: String,
+            identifier: String,
+            retryStrategy: REST.RetryStrategy,
+            completion: @escaping ProxyCompletionHandler<Bool>
+        ) -> Cancellable {
+            AnyCancellable()
+        }
+
+        func rotateDeviceKey(
+            accountNumber: String,
+            identifier: String,
+            publicKey: PublicKey,
+            retryStrategy: REST.RetryStrategy,
+            completion: @escaping ProxyCompletionHandler<Device>
+        ) -> Cancellable {
+            AnyCancellable()
+        }
+    }
+}
