5621d4e55cb41a423d847d15d43c17a10f2706a4
Oliver <oliver@mohlin.dev>
2025-04-02 10:41:02 +0200

    Move ListItem hover state styling to trigger

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-item/ListItemItem.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-item/ListItemItem.tsx
@@ -1,19 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import { useBackgroundColor } from './hooks';
 
 export interface ListItemItemProps {
   children: React.ReactNode;
 }
 
-const StyledDiv = styled.div`
-  min-height: 44px;
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr;
-  &&:has(> :last-child:nth-child(2)) {
-    grid-template-columns: 1fr 44px;
-  }
+const StyledDiv = styled.div<{ $backgroundColor: string }>`
+  ${({ $backgroundColor }) => {
+    return css`
+      --background-color: ${$backgroundColor};
+      background-color: var(--background-color);
+      min-height: 44px;
+      width: 100%;
+      display: grid;
+      grid-template-columns: 1fr;
+      background-color: var(--background-color);
+      &&:has(> :last-child:nth-child(2)) {
+        grid-template-columns: 1fr 56px;
+      }
+    `;
+  }}
 `;
 
 export const ListItemItem = ({ children }: ListItemItemProps) => {
-  return <StyledDiv>{children}</StyledDiv>;
+  const backgroundColor = useBackgroundColor();
+  return <StyledDiv $backgroundColor={backgroundColor}>{children}</StyledDiv>;
 };
