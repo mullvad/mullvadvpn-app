ef45bc24786318c6eb7079ef1dd985e50237c731
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-03 21:19:53 +0200

    Reuse IChangelog type for suggested upgrade changelog

--- desktop/packages/mullvad-vpn/src/shared/daemon-rpc-types.ts
+++ desktop/packages/mullvad-vpn/src/shared/daemon-rpc-types.ts
@@ -1,3 +1,5 @@
+import { IChangelog } from './ipc-types';
+
 export interface IAccountData {
   expiry: string;
 }
@@ -424,7 +426,7 @@ export interface IDnsOptions {
 }
 
 export type AppVersionInfoSuggestedUpgrade = {
-  changelog?: string;
+  changelog?: IChangelog;
   verifiedInstallerPath?: string;
   version: string;
 };
