77b8433a08a6610e668de657fe1ff9fdaa421504
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-04 17:10:55 +0200

    Add app-upgrade view

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/upgrade-label/components/index.ts
@@ -0,0 +1,7 @@
+export * from './connection-blocked';
+export * from './download-progress';
+export * from './download-started';
+export * from './installer-ready';
+export * from './starting-installer';
+export * from './upgrade-error';
+export * from './verifying-installer';
