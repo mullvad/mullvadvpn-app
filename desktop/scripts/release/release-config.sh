fdb6c648770e680a9045efe2cde60be98175a83d
Linus Färnstrand <linus@mullvad.net>
2025-04-04 13:13:07 +0200

    Move ARTIFACT_DIR to config script and source it

---
+++ desktop/scripts/release/release-config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Configuration variables shared between the release scripts in this directory.
+
+# Where to download app installers locally during the release process.
+export ARTIFACT_DIR="artifacts"
