c365d58b734fa83217b69c4b5d7378c538d843ef
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-04 17:10:55 +0200

    Add app-upgrade view

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/upgrade-button/UpgradeButton.tsx
@@ -0,0 +1,20 @@
+import { messages } from '../../../../../../shared/gettext';
+import { useAppContext } from '../../../../../context';
+import { Button } from '../../../../../lib/components';
+import { useDisabled } from './hooks';
+
+export function UpgradeButton() {
+  const { appUpgrade } = useAppContext();
+  const disabled = useDisabled();
+
+  return (
+    <Button disabled={disabled} onClick={appUpgrade}>
+      <Button.Text>
+        {
+          // TRANSLATORS: Button text to download and install an update
+          messages.pgettext('app-upgrade-view', 'Download and install')
+        }
+      </Button.Text>
+    </Button>
+  );
+}
