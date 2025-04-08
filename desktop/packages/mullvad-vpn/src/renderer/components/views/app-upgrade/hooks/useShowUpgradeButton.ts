f9d3f31c3f50ef465495477d090eb3a1f9d9a944
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-08 13:48:39 +0200

    Refactor to remove early return

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/useShowUpgradeButton.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/useShowUpgradeButton.ts
@@ -9,14 +9,12 @@ export const useShowUpgradeButton = () => {
   const hasAppUpgradeError = useHasAppUpgradeError();
   const hasAppUpgradeInitiated = useHasAppUpgradeInitiated();
 
-  if (hasAppUpgradeError) {
-    return false;
-  }
-
+  if (!hasAppUpgradeError) {
   // If we don't have an event type yet it is because the user has not attempted
   // an upgrade yet.
   if (!hasAppUpgradeInitiated || appUpgradeEventType === 'APP_UPGRADE_STATUS_ABORTED') {
     return true;
+    }
   }
 
   return false;
