370ad7df185489ef2289484509b7445097a65582
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-08 13:49:21 +0200

    Ensure upgrade button is hidden if user should install upgrade manually

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/useShowUpgradeButton.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/useShowUpgradeButton.ts
@@ -2,18 +2,20 @@ import {
   useAppUpgradeEventType,
   useHasAppUpgradeError,
   useHasAppUpgradeInitiated,
+  useShouldAppUpgradeInstallManually,
 } from '../../../../hooks';
 
 export const useShowUpgradeButton = () => {
   const appUpgradeEventType = useAppUpgradeEventType();
   const hasAppUpgradeError = useHasAppUpgradeError();
   const hasAppUpgradeInitiated = useHasAppUpgradeInitiated();
+  const shouldAppUpgradeInstallManually = useShouldAppUpgradeInstallManually();
 
-  if (!hasAppUpgradeError) {
-  // If we don't have an event type yet it is because the user has not attempted
-  // an upgrade yet.
-  if (!hasAppUpgradeInitiated || appUpgradeEventType === 'APP_UPGRADE_STATUS_ABORTED') {
-    return true;
+  if (!hasAppUpgradeError && !shouldAppUpgradeInstallManually) {
+    // If we don't have an event type yet it is because the user has not attempted
+    // an upgrade yet.
+    if (!hasAppUpgradeInitiated || appUpgradeEventType === 'APP_UPGRADE_STATUS_ABORTED') {
+      return true;
     }
   }
 
