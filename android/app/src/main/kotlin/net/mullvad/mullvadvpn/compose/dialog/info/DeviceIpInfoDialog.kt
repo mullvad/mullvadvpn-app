5a151f38ac0b767bbc1800221514425e92935943
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-04-08 15:04:55 +0200

    Replace subtext with info dialog for device ip version

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/info/DeviceIpInfoDialog.kt
@@ -0,0 +1,19 @@
+package net.mullvad.mullvadvpn.compose.dialog.info
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.dropUnlessResumed
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.spec.DestinationStyle
+import net.mullvad.mullvadvpn.R
+
+@Destination<RootGraph>(style = DestinationStyle.Dialog::class)
+@Composable
+fun DeviceIpInfo(navigator: DestinationsNavigator) {
+    InfoDialog(
+        message = stringResource(R.string.device_ip_info),
+        onDismiss = dropUnlessResumed { navigator.navigateUp() },
+    )
+}
