5ebad2fb4f764ecd76142874a98ade016ed22ef3
Oliver <oliver@mohlin.dev>
2025-04-08 10:24:49 +0200

    Refactor ListItemTrigger disabled state handling

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trigger/ListItemTrigger.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trigger/ListItemTrigger.tsx
@@ -1,38 +1,42 @@
 import styled, { css } from 'styled-components';
 
 import { Colors } from '../../../../foundations';
-import { TransientProps } from '../../../../types';
 import { ButtonBase } from '../../../button';
 import { ListItemProps } from '../../ListItem';
 import { useListItem } from '../../ListItemContext';
 
 // TODO: Colors should be replace with
 // with new color tokens once they are implemented.
-const StyledButton = styled(ButtonBase)<TransientProps<Pick<ListItemProps, 'disabled'>>>`
+const StyledButton = styled(ButtonBase)<Pick<ListItemProps, 'disabled'>>`
   display: flex;
   width: 100%;
-  ${({ $disabled }) => {
-    return css`
-      --background: transparent;
-      background-color: var(--background);
-      ${!$disabled &&
-      css`
+  --background: transparent;
+  background-color: var(--background);
+
+  &&:focus-visible {
+    outline: 2px solid ${Colors.white};
+    outline-offset: -2px;
+    z-index: 10;
+  }
+
+  ${({ disabled }) => {
+    if (!disabled) {
+      return css`
         --background: rgba(41, 77, 115, 1);
+
         &:hover {
           --background: rgba(62, 95, 129, 1);
           background-color: var(--background);
         }
+
         &:active {
           --background: rgba(84, 113, 143, 1);
           background-color: var(--background);
         }
-      `}
-      &&:focus-visible {
-        outline: 2px solid ${Colors.white};
-        outline-offset: -2px;
-        z-index: 10;
-      }
-    `;
+      `;
+    }
+
+    return null;
   }}
 `;
 
@@ -40,5 +44,5 @@ export type ListItemTriggerProps = React.HtmlHTMLAttributes<HTMLButtonElement>;
 
 export const ListItemTrigger = (props: ListItemTriggerProps) => {
   const { disabled } = useListItem();
-  return <StyledButton $disabled={disabled} disabled={disabled} {...props} />;
+  return <StyledButton disabled={disabled} {...props} />;
 };
