949ca8555ec8d5d51501a07c8f4b2fd3be07c523
mojganii <mojgan.jelodar@codic.se>
2025-04-09 17:00:15 +0200

    Improve sendProblemReport based on feedbacks

--- mullvad-ios/src/api_client/problem_report.rs
+++ mullvad-ios/src/api_client/problem_report.rs
@@ -4,11 +4,11 @@ use mullvad_api::{
 };
 use std::ffi::CStr;
 use std::os::raw::c_char;
-use talpid_future::retry::retry_future;
 
 use super::{
     cancellation::{RequestCancelHandle, SwiftCancelHandle},
     completion::{CompletionCookie, SwiftCompletionHandler},
+    do_request_with_empty_body,
     response::SwiftMullvadApiResponse,
     retry_strategy::{RetryStrategy, SwiftRetryStrategy},
     SwiftApiContext,
@@ -16,17 +16,34 @@ use super::{
 
 use mullvad_api::rest::Error;
 use std::collections::BTreeMap;
-use std::slice;
 use tokio::task::JoinHandle;
 
-#[no_mangle]
+/// Send a problem report via the Mullvad API client.
+///
+/// # Safety
+///
+/// `api_context` must be pointing to a valid instance of `SwiftApiContext`. A `SwiftApiContext` is created
+/// by calling `mullvad_api_init_new`.
+///
+/// `completion_cookie` must be pointing to a valid instance of `CompletionCookie`. `CompletionCookie` is
+/// safe because the pointer in `MullvadApiCompletion` is valid for the lifetime of the process where this
+/// type is intended to be used.
+///
+/// the string properties of `SwiftProblemReportRequest` must be pointers to a null terminated strings.
+///
+/// This function is not safe to call multiple times with the same `CompletionCookie`.
+///
+/// Ownership of the `CompletionCookie` is transferred to the Rust side upon calling this function.
+/// After this call, the pointer must be considered invalid and must not be reused or accessed in any way.
+/// Calling this function again with the same cookie, either sequentially or concurrently, results in undefined behavior.
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn mullvad_api_send_problem_report(
     api_context: SwiftApiContext,
     completion_cookie: *mut libc::c_void,
     retry_strategy: SwiftRetryStrategy,
     request: SwiftProblemReportRequest,
 ) -> SwiftCancelHandle {
-    let completion_handler = SwiftCompletionHandler::new(CompletionCookie(completion_cookie));
+    let completion_handler = SwiftCompletionHandler::new(CompletionCookie::new(completion_cookie));
     let completion = completion_handler.clone();
 
     let Ok(tokio_handle) = crate::mullvad_ios_runtime() else {
@@ -37,19 +54,15 @@ pub unsafe extern "C" fn mullvad_api_send_problem_report(
     let api_context = api_context.into_rust_context();
     let retry_strategy = unsafe { retry_strategy.into_rust() };
 
-    let problem_report_request = match unsafe {
-        ProblemReportRequest::from_swift_parameters(request)
-    } {
-        Some(req) => req,
-        None => {
-            let err = Error::ApiError(
-                rest::StatusCode::BAD_REQUEST,
-                "Failed to send problem report: invalid address, message, or log data.".to_string(),
-            );
-            log::error!("{err:?}");
-            completion.finish(SwiftMullvadApiResponse::rest_error(err));
-            return SwiftCancelHandle::empty();
-        }
+    let result = unsafe { ProblemReportRequest::from_swift_parameters(request) };
+    let Some(problem_report_request) = result else {
+        let err = Error::ApiError(
+            rest::StatusCode::BAD_REQUEST,
+            "Failed to send problem report: invalid address, message, or log data.".to_string(),
+        );
+        log::error!("{err:?}");
+        completion.finish(SwiftMullvadApiResponse::rest_error(err));
+        return SwiftCancelHandle::empty();
     };
 
     let task: JoinHandle<()> = tokio_handle.spawn(async move {
@@ -83,53 +96,50 @@ async fn mullvad_api_send_problem_report_inner(
             &problem_report_request.address,
             &problem_report_request.message,
             &(String::from_utf8_lossy(&problem_report_request.log)),
-            &problem_report_request.meta_data,
+            &problem_report_request.metadata,
         )
     };
 
-    let should_retry = |result: &Result<_, rest::Error>| match result {
-        Err(err) => err.is_network_error(),
-        Ok(_) => false,
-    };
-
-    retry_future(future_factory, should_retry, retry_strategy.delays()).await?;
-    SwiftMullvadApiResponse::ok().await
+    do_request_with_empty_body(retry_strategy, future_factory).await
 }
 
 #[repr(C)]
 pub struct SwiftProblemReportRequest {
-    address: *const u8,
-    address_len: usize,
-    message: *const u8,
-    message_len: usize,
-    log: *const u8,
-    log_len: usize,
-    meta_data: ProblemReportMetadata,
+    address: *const c_char,
+    message: *const c_char,
+    log: *const c_char,
+    metadata: ProblemReportMetadata,
 }
 
 struct ProblemReportRequest {
     address: String,
     message: String,
     log: Vec<u8>,
-    meta_data: BTreeMap<String, String>,
+    metadata: BTreeMap<String, String>,
 }
 
 unsafe impl Send for SwiftProblemReportRequest {}
 
 impl ProblemReportRequest {
+    // SAFETY: the members of `SwiftProblemReportRequest` must point to null-terminated strings
     unsafe fn from_swift_parameters(request: SwiftProblemReportRequest) -> Option<Self> {
-        let address_slice = slice::from_raw_parts(request.address, request.address_len);
-        let message_slice = slice::from_raw_parts(request.message, request.message_len);
-        let log_slice = slice::from_raw_parts(request.log, request.log_len);
+        fn get_string(ptr: *const c_char) -> String {
+            if ptr.is_null() {
+                return String::new();
+            }
+            // Safety: `ptr` must be a valid, null-terminated C string.
+            let cstr = unsafe { CStr::from_ptr(ptr) };
+            cstr.to_str().map(ToOwned::to_owned).unwrap_or_default()
+        }
 
-        let address = String::from_utf8(address_slice.to_vec()).ok()?;
-        let message = String::from_utf8(message_slice.to_vec()).ok()?;
-        let log = log_slice.to_vec();
+        let address = get_string(request.address);
+        let message = get_string(request.message);
+        let log = get_string(request.log).into();
 
-        let meta_data = if request.meta_data.inner.is_null() {
+        let metadata = if request.metadata.inner.is_null() {
             BTreeMap::new()
         } else {
-            let swift_map = &request.meta_data;
+            let swift_map = &request.metadata;
             let mut converted_map = BTreeMap::new();
 
             if let Some(inner) = swift_map.inner.as_ref() {
@@ -144,7 +154,7 @@ impl ProblemReportRequest {
             address,
             message,
             log,
-            meta_data,
+            metadata,
         })
     }
 }
@@ -161,11 +171,23 @@ impl Map {
         Map(BTreeMap::new())
     }
 
+    /// Add key and value pair to the map
+    ///
+    /// # Safety
+    ///
+    /// `self` must be a valid, exclusive pointer to `Map`, initialized
+    /// - `key` must be a null-terminated UTF-8 string, containing LF-separated machines.
+    /// - `value` must be a valid pointer to some valid and aligned pointer-sized memory.
     unsafe fn add(&mut self, key: *const c_char, value: *const c_char) -> bool {
-        if key.is_null() || value.is_null() {
-            log::error!("Failed to add metadata: key or value is NULL.");
-            return false;
-        }
+        assert!(
+            !key.is_null(),
+            "key must not be null (violates safety contract)"
+        );
+        assert!(
+            !value.is_null(),
+            "value must not be null (violates safety contract)"
+        );
+
         let key = unsafe { CStr::from_ptr(key) };
         let value = unsafe { CStr::from_ptr(value) };
 
@@ -189,20 +211,30 @@ impl Map {
 }
 
 #[no_mangle]
-pub extern "C" fn swift_problem_report_meta_data_new() -> ProblemReportMetadata {
+pub extern "C" fn swift_problem_report_metadata_new() -> ProblemReportMetadata {
     let map = Box::new(Map::new());
     ProblemReportMetadata {
         inner: Box::into_raw(map),
     }
 }
 
-#[no_mangle]
-pub extern "C" fn swift_problem_report_meta_data_add(
+/// Add key and value pair to the `ProblemReportMetadata`
+///
+/// # Safety
+///
+/// `self` must be a valid, exclusive pointer to `ProblemReportMetadata`, initialized
+/// - `key` must be a null-terminated UTF-8 string, containing LF-separated machines.
+/// - `value` must be a valid pointer to some valid and aligned pointer-sized memory.
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn swift_problem_report_metadata_add(
     map: ProblemReportMetadata,
     key: *const c_char,
     value: *const c_char,
 ) -> bool {
+    // Safety: We are assuming that `map.inner` is not null and that it is properly initialized.
     if let Some(inner) = unsafe { map.inner.as_mut() } {
+        // Safety: We assume that the `inner` object is valid and mutable. The `add` method is
+        // safe to call because we know `inner` is a mutable reference to the underlying data.
         unsafe { inner.add(key, value) }
     } else {
         false
@@ -210,11 +242,12 @@ pub extern "C" fn swift_problem_report_meta_data_add(
 }
 
 #[no_mangle]
-pub extern "C" fn swift_problem_report_meta_data_free(mut map: ProblemReportMetadata) {
+pub extern "C" fn swift_problem_report_metadata_free(mut map: ProblemReportMetadata) {
     if !map.inner.is_null() {
+        // SAFETY: `map.inner` must be properly aligned and non-null
+        // The caller must guarantee that `map.inner` is not null and has not been freed
         unsafe {
             drop(Box::from_raw(map.inner));
-            map.inner = std::ptr::null_mut();
         }
     }
 }
