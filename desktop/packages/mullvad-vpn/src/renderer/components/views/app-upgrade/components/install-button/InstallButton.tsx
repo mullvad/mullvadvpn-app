e73ffa933d6189c12ea8d604952b64cd460d8533
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-09 21:33:48 +0200

    Disable Install update button when installer is starting or started

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/install-button/InstallButton.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/install-button/InstallButton.tsx
@@ -1,12 +1,14 @@
 import { messages } from '../../../../../../shared/gettext';
 import { useAppContext } from '../../../../../context';
 import { Button } from '../../../../../lib/components';
+import { useDisabled } from './hooks';
 
 export function InstallButton() {
   const { appUpgradeInstallerStart } = useAppContext();
+  const disabled = useDisabled();
 
   return (
-    <Button onClick={appUpgradeInstallerStart}>
+    <Button disabled={disabled} onClick={appUpgradeInstallerStart}>
       <Button.Text>
         {
           // TRANSLATORS: Button text to install an update
