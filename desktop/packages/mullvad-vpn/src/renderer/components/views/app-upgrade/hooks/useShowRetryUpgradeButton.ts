cd17aa3e157003816c62c66ac8c13baf5d5fa587
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-09 21:43:28 +0200

    Remove extraneous condition in logic to show RetryUpgrade button
    The switch on the appUpgradeError is sufficient

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/useShowRetryUpgradeButton.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/useShowRetryUpgradeButton.ts
@@ -1,21 +1,15 @@
-import { useHasAppUpgradeError } from '../../../../hooks';
 import { useAppUpgradeError } from '../../../../redux/hooks';
 
 export const useShowRetryUpgradeButton = () => {
   const { appUpgradeError } = useAppUpgradeError();
-  const hasAppUpgradeError = useHasAppUpgradeError();
 
-  if (hasAppUpgradeError) {
-    switch (appUpgradeError) {
-      case 'DOWNLOAD_FAILED':
-      case 'GENERAL_ERROR':
-      case 'START_INSTALLER_FAILED':
-      case 'VERIFICATION_FAILED':
-        return true;
-      default:
-        return false;
-    }
+  switch (appUpgradeError) {
+    case 'DOWNLOAD_FAILED':
+    case 'GENERAL_ERROR':
+    case 'START_INSTALLER_FAILED':
+    case 'VERIFICATION_FAILED':
+      return true;
+    default:
+      return false;
   }
-
-  return false;
 };
