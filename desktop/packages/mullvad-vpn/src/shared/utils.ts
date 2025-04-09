ad50a23ae20cc7bca69e1c2412f041a252ea4a3a
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-09 13:35:01 +0200

    Add isNumber utility

--- desktop/packages/mullvad-vpn/src/shared/utils.ts
+++ desktop/packages/mullvad-vpn/src/shared/utils.ts
@@ -3,3 +3,7 @@ export type NonEmptyArray<T> = [T, ...T[]];
 export function hasValue<T>(value: T): value is NonNullable<T> {
   return value !== undefined && value !== null;
 }
+
+export function isNumber(number: unknown): number is number {
+  return !Number.isNaN(number);
+}
