247ec3e3b32c5330ca22f9ce114975fc74a2650a
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-09 13:47:05 +0200

    Handle new AppUpgrade download initiated event type

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/useMessage.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/useMessage.ts
@@ -20,6 +20,7 @@ export const useMessage = () => {
   }
 
   switch (appUpgradeEventType) {
+    case 'APP_UPGRADE_STATUS_DOWNLOAD_INITIATED':
     case 'APP_UPGRADE_STATUS_DOWNLOAD_STARTED':
       // TRANSLATORS: Status text displayed below a progress bar when the download of an update is starting
       return messages.pgettext('app-upgrade-view', 'Starting download...');
