090bff42326468a6519b579f4fdeaeba81c106f7
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-11 15:54:44 +0200

    Show Upgrade label if installer should be manually started

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/useShowUpgradeLabel.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/useShowUpgradeLabel.ts
@@ -1,12 +1,18 @@
-import { useHasAppUpgradeError, useIsAppUpgradePending } from '../../../../hooks';
+import {
+  useHasAppUpgradeError,
+  useIsAppUpgradePending,
+  useShouldAppUpgradeInstallManually,
+} from '../../../../hooks';
 import { useConnectionIsBlocked } from '../../../../redux/hooks';
 
 export const useShowUpgradeLabel = () => {
   const { isBlocked } = useConnectionIsBlocked();
   const hasAppUpgradeError = useHasAppUpgradeError();
   const isAppUpgradePending = useIsAppUpgradePending();
+  const shouldAppUpgradeInstallManually = useShouldAppUpgradeInstallManually();
 
-  const showUpgradeLabel = isBlocked || hasAppUpgradeError || isAppUpgradePending;
+  const showUpgradeLabel =
+    isBlocked || hasAppUpgradeError || isAppUpgradePending || shouldAppUpgradeInstallManually;
 
   return showUpgradeLabel;
 };
