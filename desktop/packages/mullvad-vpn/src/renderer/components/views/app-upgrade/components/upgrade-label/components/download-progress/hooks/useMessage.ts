98fc3e0758cbe05f7bb12f3aaa21f793015696e0
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-11 15:45:38 +0200

    Handle change to useAppUpgradeEvent selector return format

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/upgrade-label/components/download-progress/hooks/useMessage.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/upgrade-label/components/download-progress/hooks/useMessage.ts
@@ -4,10 +4,10 @@ import { messages } from '../../../../../../../../../shared/gettext';
 import { useAppUpgradeEvent } from '../../../../../../../../redux/hooks';
 
 export const useMessage = () => {
-  const { appUpgradeEvent } = useAppUpgradeEvent();
+  const { event } = useAppUpgradeEvent();
 
-  if (appUpgradeEvent?.type === 'APP_UPGRADE_STATUS_DOWNLOAD_PROGRESS') {
-    const { server } = appUpgradeEvent;
+  if (event?.type === 'APP_UPGRADE_STATUS_DOWNLOAD_PROGRESS') {
+    const { server } = event;
 
     return sprintf(
       // TRANSLATORS: Label displayed above a progress bar informing the user which server
