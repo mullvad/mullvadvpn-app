9fa5c75f794ef251529b99b48640696f777e51a2
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-11 15:43:56 +0200

    Handle change to useAppUpgradeError selector return format

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/hooks/useGetMessageError.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/hooks/useGetMessageError.ts
@@ -2,13 +2,13 @@ import { messages } from '../../../../../../../../../shared/gettext';
 import { useAppUpgradeError } from '../../../../../../../../redux/hooks';
 
 export const useGetMessageError = () => {
-  const { appUpgradeError } = useAppUpgradeError();
+  const { error } = useAppUpgradeError();
 
   const getMessageError = () => {
     if (
-      appUpgradeError === 'START_INSTALLER_FAILED' ||
-      appUpgradeError === 'START_INSTALLER_AUTOMATIC_FAILED' ||
-      appUpgradeError === 'VERIFICATION_FAILED'
+      error === 'START_INSTALLER_FAILED' ||
+      error === 'START_INSTALLER_AUTOMATIC_FAILED' ||
+      error === 'VERIFICATION_FAILED'
     ) {
       // TRANSLATORS: Status text displayed below a progress bar when the download of an update is complete
       return messages.pgettext('app-upgrade-view', 'Download complete!');
