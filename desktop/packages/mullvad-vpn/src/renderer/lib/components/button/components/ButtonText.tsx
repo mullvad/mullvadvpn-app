f3aaa8fafc32e26c000ec4f1b8b2b057be4cc306
Oliver <oliver@mohlin.dev>
2025-04-11 14:38:56 +0200

    Rename Button styled components

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/button/components/ButtonText.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/button/components/ButtonText.tsx
@@ -6,9 +6,9 @@ import { BodySmallSemiBold, BodySmallSemiBoldProps } from '../../typography';
 import { useButtonContext } from '../ButtonContext';
 
 export type ButtonTextProps<T extends React.ElementType = 'span'> = BodySmallSemiBoldProps<T>;
-export const StyledText = styled(BodySmallSemiBold)``;
+export const StyledButtonText = styled(BodySmallSemiBold)``;
 
 export function ButtonText<T extends React.ElementType = 'span'>(props: ButtonTextProps<T>) {
   const { disabled } = useButtonContext();
-  return <StyledText color={disabled ? Colors.white40 : Colors.white} {...props} />;
+  return <StyledButtonText color={disabled ? Colors.white40 : Colors.white} {...props} />;
 }
