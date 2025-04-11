6a1ba466459fd52c456880bde24653a9c1eead3a
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-11 15:53:14 +0200

    Refactor upgrade error message function to return string directly
    Instead of in an array.

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/upgrade-label/components/upgrade-error/hooks/useMessage.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/upgrade-label/components/upgrade-error/hooks/useMessage.ts
@@ -16,38 +16,36 @@ export const useMessage = () => {
 
   switch (error) {
     case 'DOWNLOAD_FAILED':
-      return [
-        // TRANSLATORS: Label displayed when an error occurred due to the download failing
-        messages.pgettext(
-          'app-upgrade-view',
-          'Unable to download update. Check your connection and/or firewall then try again. If this problem persists, please contact support.',
-        ),
-      ];
+      // TRANSLATORS: Label displayed when an error occurred due to the download failing
+      return messages.pgettext(
+        'app-upgrade-view',
+        'Unable to download update. Check your connection and/or firewall then try again. If this problem persists, please contact support.',
+      );
+    case 'INSTALLER_FAILED':
+      // TRANSLATORS: Label displayed when an error occurred within the installer
+      return messages.pgettext(
+        'app-upgrade-view',
+        'Installer encountered an error. Try again. If this problem persists, please contact support.',
+      );
     case 'START_INSTALLER_AUTOMATIC_FAILED':
     case 'START_INSTALLER_FAILED':
-      return [
-        // TRANSLATORS: Label displayed when an error occurred due to the installer failing to start
-        // TRANSLATORS: and the suggested resolution is to download the update again.
-        messages.pgettext(
-          'app-upgrade-view',
-          'Could not start the update installer, try downloading it again. If this problem persists, please contact support.',
-        ),
-      ];
+      // TRANSLATORS: Label displayed when an error occurred due to the installer failing to start
+      // TRANSLATORS: and the suggested resolution is to download the update again.
+      return messages.pgettext(
+        'app-upgrade-view',
+        'Could not start the update installer, try downloading it again. If this problem persists, please contact support.',
+      );
     case 'VERIFICATION_FAILED':
-      return [
-        // TRANSLATORS: Label displayed when an error occurred due to the installer failed verification
-        messages.pgettext(
-          'app-upgrade-view',
-          'Verification failed. Try again. If this problem persists, please contact support.',
-        ),
-      ];
+      // TRANSLATORS: Label displayed when an error occurred due to the installer failed verification
+      return messages.pgettext(
+        'app-upgrade-view',
+        'Verification failed. Try again. If this problem persists, please contact support.',
+      );
     default:
-      return [
-        // TRANSLATORS: Label displayed when an unknown error occurred
-        messages.pgettext(
-          'app-upgrade-view',
-          'An unknown error occurred. Please try again. If this problem persists, please contact support.',
-        ),
-      ];
+      // TRANSLATORS: Label displayed when an unknown error occurred
+      return messages.pgettext(
+        'app-upgrade-view',
+        'An unknown error occurred. Please try again. If this problem persists, please contact support.',
+      );
   }
 };
