089b7738b2817c9e1a09cf736ca88a26de01b979
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-11 15:48:55 +0200

    Add hook to return whether to show ManualDownloadButton

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/hooks/index.ts
@@ -3,6 +3,7 @@ export * from './usePresent';
 export * from './useShowCancelButton';
 export * from './useShowDownloadProgress';
 export * from './useShowInstallButton';
+export * from './useShowManualDownloadButton';
 export * from './useShowReportProblemButton';
 export * from './useShowRetryUpgradeButton';
 export * from './useShowUpgradeButton';
