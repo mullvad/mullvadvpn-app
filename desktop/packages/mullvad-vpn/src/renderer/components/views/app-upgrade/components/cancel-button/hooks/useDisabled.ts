4c2d8e6cddd6b131e6f2f6186eab29dd05d26924
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-04-14 14:10:12 +0200

    Enable Cancel button when download is initiated

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/cancel-button/hooks/useDisabled.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/cancel-button/hooks/useDisabled.ts
@@ -4,6 +4,7 @@ export const useDisabled = () => {
   const appUpgradeEventType = useAppUpgradeEventType();
 
   switch (appUpgradeEventType) {
+    case 'APP_UPGRADE_STATUS_DOWNLOAD_INITIATED':
     case 'APP_UPGRADE_STATUS_DOWNLOAD_PROGRESS':
     case 'APP_UPGRADE_STATUS_DOWNLOAD_STARTED':
       return false;
