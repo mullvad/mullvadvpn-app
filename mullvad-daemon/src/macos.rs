faf16f7d69877a3962eb4fe7e01ee1feed2d1bfd
David Lönnhager <david.l@mullvad.net>
2025-04-15 12:57:26 +0200

    Detect and run cleanup when Mullvad app is removed
    Co-authored-by: Joakim Hulthe <joakim.hulthe@mullvad.net>

--- mullvad-daemon/src/macos.rs
+++ mullvad-daemon/src/macos.rs
@@ -1,4 +1,11 @@
-use std::io;
+use std::{fmt, io, path::Path, process::Stdio};
+
+use anyhow::{anyhow, Context};
+use notify::{RecursiveMode, Watcher};
+use std::io::Write;
+use tokio::{fs::File, process::Command};
+
+use crate::device::AccountManagerHandle;
 
 /// Bump filehandle limit
 pub fn bump_filehandle_limit() {
@@ -34,4 +41,119 @@ pub fn bump_filehandle_limit() {
             status
         );
     }
+}
+
+/// Detect when the app bundle is deleted
+pub async fn handle_app_bundle_removal(
+    account_manager_handle: AccountManagerHandle,
+) -> anyhow::Result<()> {
+    /// Uninstall script to run if the .app disappears
+    const UNINSTALL_SCRIPT: &[u8] = include_bytes!("../../dist-assets/uninstall_macos.sh");
+
+    /// Path to extract the uninstall script to.
+    /// This directory must be owned by root to prevent privilege escalation.
+    const UNINSTALL_SCRIPT_PATH: &str = "/var/root/uninstall_mullvad.sh";
+
+    /// Mullvad app install path
+    const APP_PATH: &str = "/Applications/Mullvad VPN.app";
+
+    let daemon_path = std::env::current_exe().context("Failed to get daemon path")?;
+
+    // Ignore app removal if the daemon isn't installed in the app directory
+    if !daemon_path.starts_with(APP_PATH) {
+        log::trace!("Stopping handle_app_bundle_removal as the daemon is not installed");
+        return Ok(());
+    }
+
+    let (fs_notify_tx, mut fs_notify_rx) = tokio::sync::mpsc::channel(1);
+    let mut fs_watcher =
+        notify::recommended_watcher(move |event: notify::Result<notify::Event>| {
+            // Ignore access events
+            let is_access_event = event.map(|evt| evt.kind.is_access()).unwrap_or(false);
+
+            // Check if the daemon binary still exists
+            if !is_access_event && !daemon_path.exists() {
+                _ = fs_notify_tx.try_send(());
+            }
+        })
+        .context("Failed to start filesystem watcher")?;
+
+    fs_watcher
+        .watch(Path::new(APP_PATH), RecursiveMode::Recursive)
+        .context(anyhow!("Failed to watch {APP_PATH}"))?;
+
+    fs_notify_rx
+        .recv()
+        .await
+        .context("Filesystem watcher stopped unexpectedly")?;
+    drop(fs_watcher);
+
+    // Create file to log output from uninstallation process.
+    // This is useful since the daemon will be killed during uninstallation.
+    let mut log_file = async {
+        let log_path: std::path::PathBuf = mullvad_paths::log_dir()?.join("uninstall.log");
+
+        let file = File::create(log_path).await?;
+        anyhow::Ok(file.into_std().await)
+    }
+    .await
+    .inspect_err(|e| {
+        log::warn!("Failed to create uninstaller log-file: {e:#?}");
+    })
+    .ok();
+
+    // Log to both daemon log and uninstaller log.
+    let mut log = |msg: fmt::Arguments<'_>| {
+        log::info!("{msg}");
+        if let Some(log_file) = &mut log_file {
+            let _ = writeln!(log_file, "{msg}");
+        }
+    };
+
+    log(format_args!("{APP_PATH} was removed. Running uninstaller."));
+
+    tokio::fs::write(UNINSTALL_SCRIPT_PATH, UNINSTALL_SCRIPT)
+        .await
+        .context("Failed to write uninstall script")?;
+
+    // If reset_firewall errors, log the error and continue anyway.
+    log(format_args!("Resetting firewall"));
+    if let Err(error) = reset_firewall() {
+        log(format_args!("{error:#?}"));
+    }
+
+    // Remove the current device from the account
+    log(format_args!("Logging out"));
+    if let Err(error) = account_manager_handle.logout().await {
+        log(format_args!("Failed to remove device: {error:#?}"));
+    }
+
+    // This will kill the daemon.
+    log(format_args!("Running {UNINSTALL_SCRIPT_PATH:?}"));
+    let mut cmd = Command::new("/bin/bash");
+    cmd
+        .arg(UNINSTALL_SCRIPT_PATH)
+        // Don't prompt for confirmation.
+        .arg("--yes")
+        // Spawn as its own process group.
+        // This prevents the command from being killed when the daemon is killed.
+        .process_group(0)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null());
+
+    if let Some(log_file) = log_file {
+        cmd.stdout(log_file.try_clone().context("Failed to clone log fd")?);
+        cmd.stderr(log_file);
+    };
+    cmd.spawn().context("Failed to spawn uninstaller script")?;
+
+    Ok(())
+}
+
+fn reset_firewall() -> anyhow::Result<()> {
+    talpid_core::firewall::Firewall::new()
+        .context("Failed to create firewall instance")?
+        .reset_policy()
+        .context("Failed to reset firewall policy")
 }
