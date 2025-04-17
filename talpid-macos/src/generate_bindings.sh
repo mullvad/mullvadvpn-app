8224635896905f24744f9384f06f37fd151d2b7d
David Lönnhager <david.l@mullvad.net>
2025-04-17 15:06:33 +0200

    Add more process helper functions to talpid-macos

---
+++ talpid-macos/src/generate_bindings.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This script generates bindings for certain pcap and pktap symbols.
+# bindgen is required: cargo install bindgen-cli
+
+set -eu
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
+MACOS_SDK_PATH="$(xcrun --sdk macosx --show-sdk-path)"
+PROC_INFO_PATH="$MACOS_SDK_PATH/usr/include/sys/proc_info.h"
+
+bindgen "$PROC_INFO_PATH" -o ./bindings.rs \
+    --allowlist-item "^PROC_PIDFDVNODEPATHINFO" \
+    --allowlist-item "^PROX_FDTYPE_VNODE" \
+    --allowlist-item "^vnode_fdinfowithpath"
