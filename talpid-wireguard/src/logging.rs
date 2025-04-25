961913ad9379217bc971e7ff5e85f839bcf07a87
Sebastian Holmin <sebastian.holmin@mullvad.net>
2025-04-25 10:21:22 +0200

    Make compile on Windows

--- talpid-wireguard/src/logging.rs
+++ talpid-wireguard/src/logging.rs
@@ -47,7 +47,9 @@ pub fn clean_up_logging(ordinal: u64) {
 #[allow(dead_code)]
 pub enum LogLevel {
     Verbose,
+    #[cfg_attr(not(feature = "boringtun"), allow(dead_code))]
     Info,
+    #[cfg_attr(not(feature = "boringtun"), allow(dead_code))]
     Warning,
     Error,
 }
