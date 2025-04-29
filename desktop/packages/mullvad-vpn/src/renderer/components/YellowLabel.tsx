031e6436a68e84748b451d496fcf5e79631a1ed6
Oliver <oliver@mohlin.dev>
2025-04-29 16:40:30 +0200

    Use new colors in YellowLabel

--- desktop/packages/mullvad-vpn/src/renderer/components/YellowLabel.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/YellowLabel.tsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
-import { DeprecatedColors } from '../lib/foundations';
+import { colors } from '../lib/foundations';
 
 export default styled.span({
   display: 'inline-block',
   fontFamily: 'Open Sans',
-  color: DeprecatedColors.blue,
+  color: colors.brandBlue,
   fontSize: '12px',
   fontWeight: 800,
   lineHeight: '20px',
   padding: '1px 8px',
   marginLeft: '8px',
-  background: DeprecatedColors.yellow,
+  background: colors.brandYellow,
   borderRadius: '5px',
   textAlign: 'center',
   verticalAlign: 'middle',
