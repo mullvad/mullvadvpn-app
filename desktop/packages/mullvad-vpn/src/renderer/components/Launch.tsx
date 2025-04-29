144d26f2e01f9fb01a994eb5cddb88d5ab1974b1
Oliver <oliver@mohlin.dev>
2025-04-29 11:21:51 +0200

    Use new colors in launch view

--- desktop/packages/mullvad-vpn/src/renderer/components/Launch.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/Launch.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { messages } from '../../shared/gettext';
 import { useAppContext } from '../context';
 import { Button } from '../lib/components';
-import { DeprecatedColors } from '../lib/foundations';
+import { colors } from '../lib/foundations';
 import { TransitionType, useHistory } from '../lib/history';
 import { RoutePath } from '../lib/routes';
 import { useBoolean } from '../lib/utility-hooks';
@@ -26,7 +26,7 @@ export default function Launch() {
 }
 
 const StyledFooter = styled(Footer)({
-  backgroundColor: DeprecatedColors.blue,
+  backgroundColor: colors.brandBlue,
   transition: 'opacity 250ms ease-in-out',
 });
 
@@ -34,14 +34,14 @@ const StyledFooterInner = styled.div({
   display: 'flex',
   flexDirection: 'column',
   flex: 1,
-  backgroundColor: DeprecatedColors.darkBlue,
+  backgroundColor: colors.brandDarkBlue,
   borderRadius: '8px',
   margin: 0,
   padding: '16px',
 });
 
 const StyledFooterMessage = styled.span(tinyText, {
-  color: DeprecatedColors.white,
+  color: colors.white100,
   margin: `8px 0 ${measurements.buttonVerticalMargin} 0`,
 });
 
