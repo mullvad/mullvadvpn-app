b715464e94e0123794539011b8564d39f00c9a84
Oliver <oliver@mohlin.dev>
2025-04-29 11:47:52 +0200

    Use new colors in RedeemVoucher

--- desktop/packages/mullvad-vpn/src/renderer/components/RedeemVoucherStyles.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/RedeemVoucherStyles.tsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-import { DeprecatedColors } from '../lib/foundations';
+import { colors } from '../lib/foundations';
 import { normalText, smallText, tinyText } from './common-styles';
 import FormattableTextInput from './FormattableTextInput';
 
 export const StyledLabel = styled.span(smallText, {
-  color: DeprecatedColors.white,
+  color: colors.white100,
   marginBottom: '9px',
 });
 
@@ -15,19 +15,19 @@ export const StyledInput = styled(FormattableTextInput)(normalText, {
   padding: '14px',
   fontWeight: 600,
   lineHeight: '26px',
-  color: DeprecatedColors.blue,
-  backgroundColor: DeprecatedColors.white,
+  color: colors.brandBlue,
+  backgroundColor: colors.white100,
   border: 'none',
   borderRadius: '4px',
   '&&::placeholder': {
-    color: DeprecatedColors.blue40,
+    color: colors.whiteOnBlue60,
   },
 });
 
 export const StyledResponse = styled.span(tinyText, {
   lineHeight: '20px',
   marginTop: '8px',
-  color: DeprecatedColors.white,
+  color: colors.white100,
 });
 
 export const StyledProgressResponse = styled(StyledResponse)({
@@ -35,7 +35,7 @@ export const StyledProgressResponse = styled(StyledResponse)({
 });
 
 export const StyledErrorResponse = styled(StyledResponse)({
-  color: DeprecatedColors.red,
+  color: colors.brandRed,
 });
 
 export const StyledEmptyResponse = styled.span({
@@ -46,6 +46,6 @@ export const StyledEmptyResponse = styled.span({
 export const StyledTitle = styled.span(smallText, {
   lineHeight: '22px',
   fontWeight: 400,
-  color: DeprecatedColors.white,
+  color: colors.white100,
   marginBottom: '5px',
 });
