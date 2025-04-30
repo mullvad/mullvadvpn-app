40ad2925b8ab5288e25d0a416af496dc8fe24f5b
Oliver <oliver@mohlin.dev>
2025-04-30 11:33:28 +0200

    Use new colors in connection panel

--- desktop/packages/mullvad-vpn/src/renderer/components/main-view/FeatureIndicators.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/main-view/FeatureIndicators.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { strings } from '../../../shared/constants';
 import { FeatureIndicator } from '../../../shared/daemon-rpc-types';
 import { messages } from '../../../shared/gettext';
-import { DeprecatedColors } from '../../lib/foundations';
+import { colors } from '../../lib/foundations';
 import { useStyledRef } from '../../lib/utility-hooks';
 import { useSelector } from '../../redux/store';
 import { tinyText } from '../common-styles';
@@ -28,7 +28,7 @@ const StyledTitle = styled.h2(tinyText, {
   margin: '0 0 2px',
   fontSize: '10px',
   lineHeight: '15px',
-  color: DeprecatedColors.white60,
+  color: colors.white60,
 });
 
 const StyledFeatureIndicators = styled.div({
@@ -50,8 +50,8 @@ const StyledFeatureIndicatorLabel = styled.span(tinyText, (props) => ({
   justifyContent: 'center',
   alignItems: 'center',
   borderRadius: '4px',
-  background: DeprecatedColors.darkerBlue,
-  color: DeprecatedColors.white,
+  background: colors.brandDarkBlue,
+  color: colors.white100,
   fontWeight: 400,
   whiteSpace: 'nowrap',
   visibility: 'hidden',
@@ -59,17 +59,17 @@ const StyledFeatureIndicatorLabel = styled.span(tinyText, (props) => ({
   // Style clickable feature indicators with a border and on-hover effect
   boxSizing: 'border-box', // make border act as padding rather than margin
   border: 'solid 1px',
-  borderColor: props.onClick ? DeprecatedColors.blue : DeprecatedColors.darkerBlue,
+  borderColor: props.onClick ? colors.brandBlue : colors.brandDarkBlue,
   transition: 'background ease-in-out 300ms',
   '&&:hover': {
-    background: props.onClick ? DeprecatedColors.blue60 : undefined,
+    background: props.onClick ? colors.blue60 : undefined,
   },
 }));
 
 const StyledBaseEllipsis = styled.span<{ $display: boolean }>(tinyText, (props) => ({
   position: 'absolute',
   top: `${LINE_HEIGHT + GAP}px`,
-  color: DeprecatedColors.white,
+  color: colors.white100,
   padding: '2px 8px 2px 16px',
   display: props.$display ? 'inline' : 'none',
 }));
--- desktop/packages/mullvad-vpn/src/renderer/components/main-view/Hostname.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/main-view/Hostname.tsx
@@ -2,7 +2,7 @@ import { sprintf } from 'sprintf-js';
 import styled from 'styled-components';
 
 import { messages } from '../../../shared/gettext';
-import { DeprecatedColors } from '../../lib/foundations';
+import { colors } from '../../lib/foundations';
 import { IConnectionReduxState } from '../../redux/connection/reducers';
 import { useSelector } from '../../redux/store';
 import { smallText } from '../common-styles';
@@ -14,7 +14,7 @@ const StyledAccordion = styled(ConnectionPanelAccordion)({
 });
 
 const StyledHostname = styled.span(smallText, {
-  color: DeprecatedColors.white60,
+  color: colors.white60,
   fontWeight: '400',
   flexShrink: 0,
   minHeight: '1em',
