043156b6d4c821ab37f8b906dd48a9494882ada7
Markus Pettersson <markus.pettersson@mullvad.net>
2025-05-07 09:54:45 +0200

    WIP final-v2.zip

--- talpid-types/src/drop_guard.rs
+++ talpid-types/src/drop_guard.rs
@@ -5,7 +5,16 @@ pub fn on_drop<F: FnOnce()>(f: F) -> OnDrop<F> {
 }
 
 /// A type that executes a function when dropped.
-pub struct OnDrop<F: FnOnce()>(Option<F>);
+pub struct OnDrop<F: FnOnce() = Box<dyn FnOnce()>>(Option<F>);
+
+impl<F: FnOnce() + 'static> OnDrop<F> {
+    /// Box the inner function and erase its type.
+    pub fn boxed(mut self) -> OnDrop {
+        let f = self.0.take();
+        let f = f.map(|f| Box::new(f) as Box<_>);
+        OnDrop(f)
+    }
+}
 
 impl<F: FnOnce()> Drop for OnDrop<F> {
     fn drop(&mut self) {
