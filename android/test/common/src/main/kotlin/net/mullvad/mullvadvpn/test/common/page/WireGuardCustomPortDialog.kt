6bd3752d00d675c10e4a438d272a68f044036510
David Göransson <david.goransson@mullvad.net>
2025-05-11 23:18:58 +0200

    Remove unused code

--- android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/WireGuardCustomPortDialog.kt
+++ android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/WireGuardCustomPortDialog.kt
@@ -23,8 +23,4 @@ class WireGuardCustomPortDialog internal constructor() : Page() {
     fun clickCancel() {
         uiDevice.findObjectWithTimeout(cancelSelector).click()
     }
-
-    companion object {
-        const val TEXT_FIELD_TEST_TAG = "custom_port_dialog_input_test_tag"
-    }
 }
