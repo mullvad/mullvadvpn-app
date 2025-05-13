51650d53c72f053a03d4dde0f8b87780c81297d5
David Lönnhager <david.l@mullvad.net>
2025-05-13 15:09:44 +0200

    Enable all worker threads

--- mullvad-daemon/src/runtime.rs
+++ mullvad-daemon/src/runtime.rs
@@ -1,8 +1,21 @@
+use std::num::NonZero;
 use tokio::runtime;
+
+const MIN_NUM_THREADS: NonZero<usize> = unsafe { NonZero::new_unchecked(4) };
 
 pub fn new_multi_thread() -> runtime::Builder {
     let mut builder = runtime::Builder::new_multi_thread();
-    builder.worker_threads(4).enable_all();
+    match std::thread::available_parallelism() {
+        Ok(num_cpus) if num_cpus < MIN_NUM_THREADS => {
+            builder.worker_threads(num_cpus.into());
+        }
+        // Use default number of workers
+        Ok(_) => (),
+        Err(error) => {
+            log::warn!("Failed to retrieve number of CPU cores: {error}");
+        }
+    }
+    builder.enable_all();
     builder
 }
 
