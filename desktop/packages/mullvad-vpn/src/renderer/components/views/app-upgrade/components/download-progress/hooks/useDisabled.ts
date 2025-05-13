bb26810b6b28d5cfa607c1bb4ec61a95da3399f1
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-05-13 15:40:56 +0200

    Do not disable progress bar if update is already downloaded

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useDisabled.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useDisabled.ts
@@ -1,10 +1,19 @@
-import { useAppUpgradeEventType, useHasAppUpgradeError } from '../../../../../../hooks';
+import {
+  useAppUpgradeEventType,
+  useHasAppUpgradeError,
+  useHasAppUpgradeVerifiedInstallerPath,
+} from '../../../../../../hooks';
 import { useConnectionIsBlocked } from '../../../../../../redux/hooks';
 
 export const useDisabled = () => {
   const { isBlocked } = useConnectionIsBlocked();
   const appUpgradeEventType = useAppUpgradeEventType();
   const hasAppUpgradeError = useHasAppUpgradeError();
+  const hasAppUpgradeVerifiedInstallerPath = useHasAppUpgradeVerifiedInstallerPath();
+
+  if (hasAppUpgradeVerifiedInstallerPath) {
+    return false;
+  }
 
   const disabled =
     hasAppUpgradeError || isBlocked || appUpgradeEventType === 'APP_UPGRADE_STATUS_ABORTED';
