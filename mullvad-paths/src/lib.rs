1e66ab7d59b0695e836c4d58dff4a1ac7717b855
David Lönnhager <david.l@mullvad.net>
2025-05-14 15:48:17 +0200

    Delete mullvad directories if their permissions are unexpected

--- mullvad-paths/src/lib.rs
+++ mullvad-paths/src/lib.rs
@@ -2,6 +2,8 @@
 
 #[cfg(any(target_os = "linux", target_os = "macos"))]
 use std::fs;
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+use std::path::Path;
 use std::{io, path::PathBuf};
 
 #[cfg(windows)]
@@ -13,6 +15,12 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
     #[error("Failed to create directory {0}")]
     CreateDirFailed(String, #[source] io::Error),
+
+    #[error("Failed to remove directory {0}")]
+    RemoveDir(String, #[source] io::Error),
+
+    #[error("Failed to get directory permissions on {0}")]
+    GetDirPermissionFailed(String, #[source] io::Error),
 
     #[error("Failed to set directory permissions on {0}")]
     SetDirPermissionFailed(String, #[source] io::Error),
@@ -39,6 +47,26 @@ const PRODUCT_NAME: &str = "mullvad-vpn";
 #[cfg(windows)]
 pub const PRODUCT_NAME: &str = "Mullvad VPN";
 
+#[cfg(unix)]
+#[derive(Clone, Copy, PartialEq)]
+enum Permissions {
+    /// Do not set any particular permissions. They will be inherited instead.
+    Any,
+    /// Only root should have write access. Other users will have
+    /// read and execute permissions (0o755).
+    ReadExecOnly,
+}
+
+#[cfg(unix)]
+impl Permissions {
+    fn fs_permissions(self) -> Option<fs::Permissions> {
+        match self {
+            Permissions::Any => None,
+            Permissions::ReadExecOnly => Some(std::os::unix::fs::PermissionsExt::from_mode(0o755)),
+        }
+    }
+}
+
 #[cfg(windows)]
 fn get_allusersprofile_dir() -> Result<PathBuf> {
     match std::env::var_os("ALLUSERSPROFILE") {
@@ -47,14 +75,54 @@ fn get_allusersprofile_dir() -> Result<PathBuf> {
     }
 }
 
-#[cfg(any(target_os = "linux", target_os = "macos"))]
-fn create_and_return(dir: PathBuf, permissions: Option<fs::Permissions>) -> Result<PathBuf> {
-    fs::create_dir_all(&dir).map_err(|e| Error::CreateDirFailed(dir.display().to_string(), e))?;
-    if let Some(permissions) = permissions {
-        fs::set_permissions(&dir, permissions)
-            .map_err(|e| Error::SetDirPermissionFailed(dir.display().to_string(), e))?;
+#[cfg(unix)]
+fn create_and_return(dir: PathBuf, permissions: Permissions) -> Result<PathBuf> {
+    use std::os::unix::fs::{DirBuilderExt, PermissionsExt};
+
+    let mut dir_builder = fs::DirBuilder::new();
+    let fs_perms = permissions.fs_permissions();
+    if let Some(fs_perms) = fs_perms.as_ref() {
+        dir_builder.mode(fs_perms.mode());
     }
-    Ok(dir)
+    match dir_builder.create(&dir) {
+        Ok(()) => Ok(dir),
+        // The directory already exists
+        Err(error) if error.kind() == io::ErrorKind::AlreadyExists => {
+            // If the permissions are wrong, delete the directory
+            if !dir_is_root_owned(&dir, fs_perms.as_ref())? {
+                fs::remove_dir_all(&dir)
+                    .or_else(|err| {
+                        // ENOTDIR: If the path is not a directory, try to remove the file
+                        if err.raw_os_error() == Some(20) {
+                            fs::remove_file(&dir)
+                        } else {
+                            Err(err)
+                        }
+                    })
+                    .map_err(|e| Error::RemoveDir(dir.display().to_string(), e))?;
+                // Try to create it again
+                return create_and_return(dir, permissions);
+            }
+            // Correct permissions, so we're done
+            Ok(dir)
+        }
+        // Fail on any other error
+        Err(error) => Err(Error::CreateDirFailed(dir.display().to_string(), error)),
+    }
+}
+
+#[cfg(unix)]
+fn dir_is_root_owned(dir: &Path, perms: Option<&fs::Permissions>) -> Result<bool> {
+    use std::os::unix::fs::{MetadataExt, PermissionsExt};
+
+    const RELEVANT_BITS: u32 = 0o777;
+
+    let meta = fs::symlink_metadata(&dir)
+        .map_err(|e| Error::GetDirPermissionFailed(dir.display().to_string(), e))?;
+    let matching_perms = perms
+        .map(|perms| (perms.mode() & RELEVANT_BITS) == (meta.permissions().mode() & RELEVANT_BITS))
+        .unwrap_or(true);
+    Ok(matching_perms && meta.uid() == 0)
 }
 
 #[cfg(windows)]
