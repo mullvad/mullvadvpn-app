a3312fda3c49a4e0eaa7005232dd0bc3c6b053da
Oliver <oliver@mohlin.dev>
2025-05-14 09:22:04 +0200

    Add isInRanges utility function

--- desktop/packages/mullvad-vpn/src/shared/utils.ts
+++ desktop/packages/mullvad-vpn/src/shared/utils.ts
@@ -3,3 +3,7 @@ export type NonEmptyArray<T> = [T, ...T[]];
 export function hasValue<T>(value: T): value is NonNullable<T> {
   return value !== undefined && value !== null;
 }
+
+export function isInRanges(value: number, ranges: [number, number][]): boolean {
+  return ranges.some(([min, max]) => value >= min && value <= max);
+}
