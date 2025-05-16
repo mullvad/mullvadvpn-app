4954edeedfe31af32090018ce95a36f0c0c8be4a
David Lönnhager <david.l@mullvad.net>
2025-05-16 11:33:55 +0200

    fixup

--- mullvad-paths/src/lib.rs
+++ mullvad-paths/src/lib.rs
@@ -77,43 +77,50 @@ fn get_allusersprofile_dir() -> Result<PathBuf> {
 
 #[cfg(unix)]
 fn create_and_return(dir: PathBuf, permissions: Permissions) -> Result<PathBuf> {
+    use std::os::unix::fs::{DirBuilderExt, PermissionsExt};
+
+    let mut dir_builder = fs::DirBuilder::new();
     let fs_perms = permissions.fs_permissions();
-    if permissions == Permissions::ReadExecOnly && !dir_is_root_owned(&dir, fs_perms.clone())? {
-        // If the directory is not owned by root, we delete it, so that an attacker
-        // cannot sneak anything in before we create it
-        log::debug!(
-            "Removing directory with unexpected ownership or permissions: {}",
-            dir.display()
-        );
-        fs::remove_dir_all(&dir)
-            .or_else(|err| {
-                // ENOTDIR: If the path is not a directory, try to remove the file
-                if err.raw_os_error() == Some(20) {
-                    fs::remove_file(&dir)
-                } else {
-                    Err(err)
-                }
-            })
-            .map_err(|e| Error::RemoveDir(dir.display().to_string(), e))?;
+    if let Some(fs_perms) = fs_perms.as_ref() {
+        dir_builder.mode(fs_perms.mode());
     }
-    fs::create_dir_all(&dir).map_err(|e| Error::CreateDirFailed(dir.display().to_string(), e))?;
-    if let Some(fs_perms) = fs_perms {
-        fs::set_permissions(&dir, fs_perms)
-            .map_err(|e| Error::SetDirPermissionFailed(dir.display().to_string(), e))?;
+    match dir_builder.create(&dir) {
+        Ok(()) => Ok(dir),
+        // The directory already exists
+        Err(error) if error.kind() == io::ErrorKind::AlreadyExists => {
+            // If the permissions are wrong, delete the directory
+            if !dir_is_root_owned(&dir, fs_perms.as_ref())? {
+                fs::remove_dir_all(&dir)
+                    .or_else(|err| {
+                        // ENOTDIR: If the path is not a directory, try to remove the file
+                        if err.raw_os_error() == Some(20) {
+                            fs::remove_file(&dir)
+                        } else {
+                            Err(err)
+                        }
+                    })
+                    .map_err(|e| Error::RemoveDir(dir.display().to_string(), e))?;
+                // Try to create it again
+                return create_and_return(dir, permissions);
+            }
+            // Correct permissions, so we're done
+            Ok(dir)
+        }
+        // Fail on any other error
+        Err(error) => Err(Error::CreateDirFailed(dir.display().to_string(), error)),
     }
-    Ok(dir)
 }
 
 #[cfg(unix)]
-fn dir_is_root_owned(dir: &Path, perms: Option<fs::Permissions>) -> Result<bool> {
+fn dir_is_root_owned(dir: &Path, perms: Option<&fs::Permissions>) -> Result<bool> {
     use std::os::unix::fs::{MetadataExt, PermissionsExt};
 
-    const DIR_BIT: u32 = 0o040000;
+    const RELEVANT_BITS: u32 = 0o777;
 
     let meta = fs::symlink_metadata(&dir)
         .map_err(|e| Error::GetDirPermissionFailed(dir.display().to_string(), e))?;
     let matching_perms = perms
-        .map(|perms| perms.mode() == meta.permissions().mode() & (!DIR_BIT))
+        .map(|perms| (perms.mode() & RELEVANT_BITS) == (meta.permissions().mode() & RELEVANT_BITS))
         .unwrap_or(true);
     Ok(matching_perms && meta.uid() == 0)
 }
