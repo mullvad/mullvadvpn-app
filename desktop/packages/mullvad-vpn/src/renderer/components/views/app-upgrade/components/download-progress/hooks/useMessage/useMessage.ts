9d8dfc6c611fa3b369e167fda321ccaf584baa9d
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-05-19 19:39:46 +0200

    Refactor messages hook to make use of AppUpgrade step
    Instead of just looking at the event type.

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/useMessage.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/useMessage.ts
@@ -4,6 +4,7 @@ import {
   useHasAppUpgradeError,
   useHasAppUpgradeVerifiedInstallerPath,
 } from '../../../../../../../hooks';
+import { convertEventTypeToStep } from '../../../../../../../redux/app-upgrade/helpers';
 import { useConnectionIsBlocked } from '../../../../../../../redux/hooks';
 import { useGetMessageError, useGetMessageTimeLeft } from './hooks';
 
@@ -12,14 +13,20 @@ export const useMessage = () => {
   const appUpgradeEventType = useAppUpgradeEventType();
   const getMessageError = useGetMessageError();
   const getMessageTimeLeft = useGetMessageTimeLeft();
-  const hasAppUpgradeError = useHasAppUpgradeError();
   const hasAppUpgradeVerifiedInstallerPath = useHasAppUpgradeVerifiedInstallerPath();
+  const hasAppUpgradeError = useHasAppUpgradeError();
+  const step = convertEventTypeToStep(appUpgradeEventType);
 
-  if (hasAppUpgradeVerifiedInstallerPath && !appUpgradeEventType) {
+  if (
+    (step === 'initial' && hasAppUpgradeVerifiedInstallerPath) ||
+    step === 'launch' ||
+    step === 'verify'
+  ) {
+    // TRANSLATORS: Status text displayed below a progress bar when the download of an update is complete
     return messages.pgettext('app-upgrade-view', 'Download complete!');
   }
 
-  if (isBlocked || appUpgradeEventType === 'APP_UPGRADE_STATUS_ABORTED') {
+  if (step === 'pause' || isBlocked) {
     // TRANSLATORS: Status text displayed below a progress bar when the download of an update has been paused
     return messages.pgettext('app-upgrade-view', 'Download paused');
   }
@@ -28,23 +35,16 @@ export const useMessage = () => {
     return getMessageError();
   }
 
-  switch (appUpgradeEventType) {
-    case 'APP_UPGRADE_STATUS_DOWNLOAD_INITIATED':
-    case 'APP_UPGRADE_STATUS_DOWNLOAD_STARTED':
-      // TRANSLATORS: Status text displayed below a progress bar when the download of an update is starting
-      return messages.pgettext('app-upgrade-view', 'Starting download...');
-    case 'APP_UPGRADE_STATUS_DOWNLOAD_PROGRESS':
-      return getMessageTimeLeft();
-    case 'APP_UPGRADE_STATUS_AUTOMATIC_STARTING_INSTALLER':
-    case 'APP_UPGRADE_STATUS_EXITED_INSTALLER':
-    case 'APP_UPGRADE_STATUS_MANUAL_START_INSTALLER':
-    case 'APP_UPGRADE_STATUS_MANUAL_STARTING_INSTALLER':
-    case 'APP_UPGRADE_STATUS_STARTED_INSTALLER':
-    case 'APP_UPGRADE_STATUS_VERIFIED_INSTALLER':
-    case 'APP_UPGRADE_STATUS_VERIFYING_INSTALLER':
-      // TRANSLATORS: Status text displayed below a progress bar when the download of an update is complete
-      return messages.pgettext('app-upgrade-view', 'Download complete!');
-    default:
-      return null;
+  if (step === 'download') {
+    if (appUpgradeEventType === 'APP_UPGRADE_STATUS_DOWNLOAD_PROGRESS') {
+      const messageTimeLeft = getMessageTimeLeft();
+
+      return messageTimeLeft;
+    }
+
+    // TRANSLATORS: Status text displayed below a progress bar when the download of an update is starting
+    return messages.pgettext('app-upgrade-view', 'Starting download...');
   }
+
+  return null;
 };
