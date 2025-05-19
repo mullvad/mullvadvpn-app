d63229c841b928da5a220f60ee4793b78d2ddaf8
Oliver <oliver@mohlin.dev>
2025-05-19 15:58:38 +0200

    Add vpn settings route object model

--- desktop/packages/mullvad-vpn/test/e2e/route-object-models/routes-object-model.ts
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/routes-object-model.ts
@@ -5,17 +5,20 @@ import { MainRouteObjectModel } from './main';
 import { SelectLanguageRouteObjectModel } from './select-language';
 import { SettingsRouteObjectModel } from './settings/settings-route-object-model';
 import { UserInterfaceSettingsRouteObjectModel } from './user-interface-settings';
+import { VpnSettingsRouteObjectModel } from './vpn-settings';
 
 export class RoutesObjectModel {
   readonly main: MainRouteObjectModel;
   readonly settings: SettingsRouteObjectModel;
   readonly userInterfaceSettings: UserInterfaceSettingsRouteObjectModel;
   readonly selectLanguage: SelectLanguageRouteObjectModel;
+  readonly vpnSettings: VpnSettingsRouteObjectModel;
 
   constructor(page: Page, utils: TestUtils) {
     this.selectLanguage = new SelectLanguageRouteObjectModel(page, utils);
     this.main = new MainRouteObjectModel(page, utils);
     this.settings = new SettingsRouteObjectModel(page, utils);
     this.userInterfaceSettings = new UserInterfaceSettingsRouteObjectModel(page, utils);
+    this.vpnSettings = new VpnSettingsRouteObjectModel(page, utils);
   }
 }
