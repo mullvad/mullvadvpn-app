70d43b6627ad9f2ac68b704dbfdcd53238e6fb54
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-05-19 19:41:10 +0200

    Make sure error footer is only shown if app is also not blocked

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/footer/hooks/useStep.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/footer/hooks/useStep.ts
@@ -1,12 +1,14 @@
 import { AppUpgradeStep } from '../../../../../../../shared/app-upgrade';
 import { useAppUpgradeEventType, useHasAppUpgradeError } from '../../../../../../hooks';
 import { convertEventTypeToStep } from '../../../../../../redux/app-upgrade/helpers';
+import { useConnectionIsBlocked } from '../../../../../../redux/hooks';
 
 export const useStep = (): AppUpgradeStep => {
+  const { isBlocked } = useConnectionIsBlocked();
   const eventType = useAppUpgradeEventType();
   const hasAppUpgradeError = useHasAppUpgradeError();
 
-  if (hasAppUpgradeError) {
+  if (hasAppUpgradeError && !isBlocked) {
     return 'error';
   }
 
