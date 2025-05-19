575a5d8d174eb4e4f40d54aef9144b3cb63e60bf
Oliver <oliver@mohlin.dev>
2025-05-19 14:34:46 +0200

    Add settings route object model

---
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/settings/selectors.ts
@@ -0,0 +1,5 @@
+import { Page } from 'playwright';
+
+export const createSelectors = (page: Page) => ({
+  userInterfaceButton: () => page.getByRole('button', { name: 'User interface settings' }),
+});
