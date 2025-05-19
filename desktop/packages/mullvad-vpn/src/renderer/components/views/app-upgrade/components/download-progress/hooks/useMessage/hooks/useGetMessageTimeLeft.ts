150446da8257086b85b209c09f1222d333aaa5eb
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-05-19 19:43:24 +0200

    Refactor messages hook to extract messages to a separate file
    To make it easier to parse the logic.

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/hooks/useGetMessageTimeLeft.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/hooks/useGetMessageTimeLeft.ts
@@ -1,8 +1,6 @@
-import { sprintf } from 'sprintf-js';
-
-import { messages } from '../../../../../../../../../shared/gettext';
 import { isNumber } from '../../../../../../../../../shared/utils';
 import { useAppUpgradeEvent } from '../../../../../../../../redux/hooks';
+import { translations } from '../constants';
 
 export const useGetMessageTimeLeft = () => {
   const { event } = useAppUpgradeEvent();
@@ -16,36 +14,14 @@ export const useGetMessageTimeLeft = () => {
         if (timeLeft > 90) {
           const minutes = Math.round(timeLeft / 60);
 
-          return sprintf(
-            // TRANSLATORS: Status text displayed below a progress bar when the update is being downloaded
-            // TRANSLATORS: Available placeholders:
-            // TRANSLATORS: %(minutes)s - Will be replaced with remaining minutes until download is complete
-            messages.pgettext('app-upgrade-view', 'About %(minutes)s minutes remaining...'),
-            {
-              minutes,
-            },
-          );
+          return translations.getDownloadMinutesRemaining(minutes);
         }
 
         if (timeLeft > 3) {
-          return sprintf(
-            // TRANSLATORS: Status text displayed below a progress bar when the update is being downloaded
-            // TRANSLATORS: Available placeholders:
-            // TRANSLATORS: %(second)s - Will be replaced with remaining seconds until download is complete
-            messages.pgettext('app-upgrade-view', 'About %(seconds)s seconds remaining...'),
-            {
-              seconds: timeLeft,
-            },
-          );
+          return translations.getDownloadSecondsRemaining(timeLeft);
         }
 
-        return sprintf(
-          // TRANSLATORS: Status text displayed below a progress bar when the update is being downloaded
-          messages.pgettext('app-upgrade-view', 'A few seconds remaining...'),
-          {
-            seconds: timeLeft,
-          },
-        );
+        return translations.downloadFewSecondsRemaining;
       }
     }
 
