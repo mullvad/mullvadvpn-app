8bf7f67fb0318739308930c03c3a63947fb32ea3
Oliver <oliver@mohlin.dev>
2025-05-19 14:34:55 +0200

    Add user interface settings route object model

---
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/user-interface-settings/index.ts
@@ -0,0 +1,2 @@
+export * from './user-interface-settings-route-object-model';
+export * from './selectors';
---
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/user-interface-settings/user-interface-settings-route-object-model.ts
@@ -0,0 +1,30 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/renderer/lib/routes';
+import { MockedTestUtils } from '../../mocked/mocked-utils';
+import { createSelectors } from './selectors';
+
+export class UserInterfaceSettingsRouteObjectModel {
+  readonly page: Page;
+  readonly utils: MockedTestUtils;
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(page: Page, utils: MockedTestUtils) {
+    this.page = page;
+    this.utils = utils;
+    this.selectors = createSelectors(page);
+  }
+
+  async gotoSelectLanguage() {
+    await this.selectors.languageButton().click();
+    await this.utils.waitForRoute(RoutePath.selectLanguage);
+  }
+
+  async waitForRoute() {
+    await this.utils.waitForRoute(RoutePath.userInterfaceSettings);
+  }
+
+  getLocalizedLanguageButton(language: string) {
+    return this.selectors.languageButtonLabel(language);
+  }
+}
