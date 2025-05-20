b780720435d38cfad88c0c733cad493109750f34
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-05-20 22:45:40 +0200

    Make sure type is exhausted when checking error value

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/hooks/useGetMessageError.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/download-progress/hooks/useMessage/hooks/useGetMessageError.ts
@@ -5,16 +5,22 @@ export const useGetMessageError = () => {
   const { error } = useAppUpgradeError();
 
   const getMessageError = () => {
-    switch (error) {
-      case 'DOWNLOAD_FAILED':
-        return translations.downloadFailed;
-      case 'INSTALLER_FAILED':
-      case 'START_INSTALLER_FAILED':
-      case 'VERIFICATION_FAILED':
-        return translations.downloadComplete;
-      default:
-        return null;
+    if (error) {
+      switch (error) {
+        case 'DOWNLOAD_FAILED':
+          return translations.downloadFailed;
+        case 'INSTALLER_FAILED':
+        case 'START_INSTALLER_FAILED':
+        case 'VERIFICATION_FAILED':
+          return translations.downloadComplete;
+        case 'GENERAL_ERROR':
+          return null;
+        default:
+          return error satisfies never;
+      }
     }
+
+    return null;
   };
 
   return getMessageError;
