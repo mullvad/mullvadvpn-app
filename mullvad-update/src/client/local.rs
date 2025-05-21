2b07cdc68d9cc57ac363b11c52f8cdf92475b553
Markus Pettersson <markus.pettersson@mullvad.net>
2025-05-21 15:20:47 +0200

    Implement DirectoryVersionInfoProvider

--- mullvad-update/src/client/local.rs
+++ mullvad-update/src/client/local.rs
@@ -1,20 +1,36 @@
 //! This module implements fetching of information about app versions from disk.
 
 use std::path::PathBuf;
+use tokio::fs;
 
-use super::version_provider::VersionInfoProvider;
+use crate::{format::Response, version::VersionInfo};
 
 /// Obtain version data from disk
 pub struct DirectoryVersionInfoProvider {
     /// Path to directory containing the metadata file.
     pub directory: PathBuf,
+    /// TODO
+    pub version_info: VersionInfo,
 }
 
-impl VersionInfoProvider for DirectoryVersionInfoProvider {
-    fn get_version_info(
+impl DirectoryVersionInfoProvider {
+    pub const METADATA: &str = "metadata.json";
+
+    /// Read metadata.json from the local directory
+    async fn new(
         &self,
+        directory: PathBuf,
         params: crate::version::VersionParameters,
-    ) -> impl std::future::Future<Output = anyhow::Result<crate::version::VersionInfo>> + Send {
-        std::future::ready(todo!())
+    ) -> anyhow::Result<Self> {
+        let metadata_file = self.directory.join(Self::METADATA);
+        let bytes = fs::read(metadata_file).await?;
+        let response: Response = serde_json::from_slice(&bytes)?;
+
+        let version_info = VersionInfo::try_from_response(&params, response)?;
+
+        Ok(Self {
+            directory,
+            version_info,
+        })
     }
 }
