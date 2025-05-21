1e708a684a0c27d5ff4c7346b1aa345578dca19e
Markus Pettersson <markus.pettersson@mullvad.net>
2025-05-21 15:46:03 +0200

    fixup! fixup! fixup! Implement DirectoryVersionInfoProvider

--- mullvad-update/src/client/local.rs
+++ mullvad-update/src/client/local.rs
@@ -3,7 +3,10 @@
 use std::path::PathBuf;
 use tokio::fs;
 
-use crate::{format::Response, version::VersionInfo};
+use crate::{
+    format::Response,
+    version::{VersionInfo, VersionParameters},
+};
 
 /// Obtain version data from disk
 pub struct DirectoryVersionInfoProvider {
@@ -17,11 +20,8 @@ impl DirectoryVersionInfoProvider {
     pub const METADATA: &str = "metadata.json";
 
     /// Read metadata.json from the local directory
-    pub async fn new(
-        directory: PathBuf,
-        params: crate::version::VersionParameters,
-    ) -> anyhow::Result<Self> {
-        let metadata_file = self.directory.join(Self::METADATA);
+    pub async fn new(directory: PathBuf, params: VersionParameters) -> anyhow::Result<Self> {
+        let metadata_file = directory.join(Self::METADATA);
         let bytes = fs::read(metadata_file).await?;
         let response: Response = serde_json::from_slice(&bytes)?;
 
