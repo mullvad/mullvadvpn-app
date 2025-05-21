ebad134f4605e5cf210b663d51b62b1de2a1f02b
Markus Pettersson <markus.pettersson@mullvad.net>
2025-05-21 14:52:48 +0200

    fixup! fixup! WIP Add DirectoryVersionInfoProvider

--- mullvad-update/src/client/local.rs
+++ mullvad-update/src/client/local.rs
@@ -7,7 +7,7 @@ use super::version_provider::VersionInfoProvider;
 /// Obtain version data from disk
 pub struct DirectoryVersionInfoProvider {
     /// Path to directory containing the metadata file.
-    directory: PathBuf,
+    pub directory: PathBuf,
 }
 
 impl VersionInfoProvider for DirectoryVersionInfoProvider {
