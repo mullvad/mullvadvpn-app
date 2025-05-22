bc6ac6d9056d86cedc358209bbd7a8a7f00b5d25
Bug Magnet <marco.nikic@mullvad.net>
2025-05-22 12:59:40 +0200

    Make UITests compile in Xcode 16 dot 1

--- ios/MullvadVPNUITests/Networking/Networking.swift
+++ ios/MullvadVPNUITests/Networking/Networking.swift
@@ -173,9 +173,9 @@ class Networking {
         var request = URLRequest(url: mullvadDNSLeakURL)
         request.setValue("application/json", forHTTPHeaderField: "accept")
 
-        var requestData: Data?
-        var requestResponse: URLResponse?
-        var requestError: Error?
+        nonisolated(unsafe) var requestData: Data?
+        nonisolated(unsafe) var requestResponse: URLResponse?
+        nonisolated(unsafe) var requestError: Error?
         let completionHandlerInvokedExpectation = XCTestExpectation(
             description: "Completion handler for the request is invoked"
         )
