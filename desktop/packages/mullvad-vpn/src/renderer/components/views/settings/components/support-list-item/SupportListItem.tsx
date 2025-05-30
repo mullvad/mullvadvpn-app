7feb43fb8c45afd131aa2e31d2d3899f2960d5ea
Oliver <oliver@mohlin.dev>
2025-05-30 10:59:04 +0200

    Use NavigationListItem in setting views

--- desktop/packages/mullvad-vpn/src/renderer/components/views/settings/components/support-list-item/SupportListItem.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/settings/components/support-list-item/SupportListItem.tsx
@@ -1,19 +1,17 @@
 import { messages } from '../../../../../../shared/gettext';
 import { RoutePath } from '../../../../../../shared/routes';
-import { Icon } from '../../../../../lib/components';
-import { ListItem } from '../../../../../lib/components/list-item';
 import { NavigationListItem } from '../../../../NavigationListItem';
 
 export function SupportListItem() {
   return (
     <NavigationListItem to={RoutePath.support}>
-      <ListItem.Label>
+      <NavigationListItem.Label>
         {
           // TRANSLATORS: Navigation button to the 'Support' view
           messages.pgettext('settings-view', 'Support')
         }
-      </ListItem.Label>
-      <Icon icon="chevron-right" />
+      </NavigationListItem.Label>
+      <NavigationListItem.Icon icon="chevron-right" />
     </NavigationListItem>
   );
 }
