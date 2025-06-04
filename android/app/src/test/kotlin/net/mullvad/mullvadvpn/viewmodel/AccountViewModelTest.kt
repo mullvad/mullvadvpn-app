4bc204db0125c220f98b298c219d5fc2eb4bc15f
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-06-04 09:14:43 +0200

    Fix unit tests

--- android/app/src/test/kotlin/net/mullvad/mullvadvpn/viewmodel/AccountViewModelTest.kt
+++ android/app/src/test/kotlin/net/mullvad/mullvadvpn/viewmodel/AccountViewModelTest.kt
@@ -1,34 +1,28 @@
 package net.mullvad.mullvadvpn.viewmodel
 
-import android.app.Activity
 import app.cash.turbine.test
 import arrow.core.right
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import java.time.ZonedDateTime
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runTest
-import net.mullvad.mullvadvpn.compose.state.PaymentState
 import net.mullvad.mullvadvpn.data.UUID
 import net.mullvad.mullvadvpn.lib.common.test.TestCoroutineRule
-import net.mullvad.mullvadvpn.lib.common.test.assertLists
 import net.mullvad.mullvadvpn.lib.model.AccountNumber
 import net.mullvad.mullvadvpn.lib.model.Device
 import net.mullvad.mullvadvpn.lib.model.DeviceId
 import net.mullvad.mullvadvpn.lib.model.DeviceState
 import net.mullvad.mullvadvpn.lib.payment.model.PaymentAvailability
-import net.mullvad.mullvadvpn.lib.payment.model.PaymentProduct
-import net.mullvad.mullvadvpn.lib.payment.model.ProductId
-import net.mullvad.mullvadvpn.lib.payment.model.PurchaseResult
 import net.mullvad.mullvadvpn.lib.shared.AccountRepository
 import net.mullvad.mullvadvpn.lib.shared.DeviceRepository
 import net.mullvad.mullvadvpn.usecase.PaymentUseCase
+import net.mullvad.mullvadvpn.util.Lc
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -54,17 +48,14 @@ class AccountViewModelTest {
             DeviceState.LoggedIn(accountNumber = dummyAccountNumber, device = dummyDevice)
         )
     private val paymentAvailability = MutableStateFlow<PaymentAvailability?>(null)
-    private val purchaseResult = MutableStateFlow<PurchaseResult?>(null)
     private val accountExpiryState = MutableStateFlow(null)
 
     private lateinit var viewModel: AccountViewModel
 
     @BeforeEach
     fun setup() {
-        mockkStatic(PURCHASE_RESULT_EXTENSIONS_CLASS)
         every { mockAccountRepository.accountData } returns accountExpiryState
         every { mockDeviceRepository.deviceState } returns deviceState
-        coEvery { mockPaymentUseCase.purchaseResult } returns purchaseResult
         coEvery { mockPaymentUseCase.paymentAvailability } returns paymentAvailability
         coEvery { mockAccountRepository.getAccountData() } returns null
 
@@ -73,7 +64,6 @@ class AccountViewModelTest {
                 accountRepository = mockAccountRepository,
                 deviceRepository = mockDeviceRepository,
                 paymentUseCase = mockPaymentUseCase,
-                isPlayBuild = false,
             )
     }
 
@@ -89,7 +79,8 @@ class AccountViewModelTest {
             deviceState.value =
                 DeviceState.LoggedIn(accountNumber = dummyAccountNumber, device = dummyDevice)
             val result = awaitItem()
-            assertEquals(dummyAccountNumber, result.accountNumber)
+            assertIs<Lc.Content<AccountUiState>>(result)
+            assertEquals(dummyAccountNumber, result.value.accountNumber)
         }
     }
 
@@ -105,120 +96,7 @@ class AccountViewModelTest {
         coVerify { mockAccountRepository.logout() }
     }
 
-    @Test
-    fun `when paymentAvailability emits ProductsUnavailable uiState should be NoPayment`() =
-        runTest {
-            // Act, Assert
-            viewModel.uiState.test {
-                awaitItem() // Default state
-                paymentAvailability.tryEmit(PaymentAvailability.ProductsUnavailable)
-                val result = awaitItem().billingPaymentState
-                assertIs<PaymentState.NoPayment>(result)
-            }
-        }
-
-    @Test
-    fun `when paymentAvailability emits ErrorOther uiState should be ErrorGeneric`() = runTest {
-        // Act, Assert
-        viewModel.uiState.test {
-            awaitItem() // Default state
-            paymentAvailability.tryEmit(PaymentAvailability.Error.Other(mockk()))
-            val result = awaitItem().billingPaymentState
-            assertIs<PaymentState.Error.Generic>(result)
-        }
-    }
-
-    @Test
-    fun `when paymentAvailability emits ErrorBillingUnavailable uiState should be ErrorBilling`() =
-        runTest {
-            // Act, Assert
-            viewModel.uiState.test {
-                awaitItem() // Default state
-                paymentAvailability.tryEmit(PaymentAvailability.Error.BillingUnavailable)
-                val result = awaitItem().billingPaymentState
-                assertIs<PaymentState.Error.Billing>(result)
-            }
-        }
-
-    @Test
-    fun `when paymentAvailability emits ProductsAvailable uiState should be Available with products`() =
-        runTest {
-            // Arrange
-            val mockProduct: PaymentProduct = mockk()
-            val expectedProductList = listOf(mockProduct)
-
-            // Act, Assert
-            viewModel.uiState.test {
-                awaitItem() // Default state
-                paymentAvailability.tryEmit(
-                    PaymentAvailability.ProductsAvailable(listOf(mockProduct))
-                )
-                val result = awaitItem().billingPaymentState
-                assertIs<PaymentState.PaymentAvailable>(result)
-                assertLists(expectedProductList, result.products)
-            }
-        }
-
-    @Test
-    fun `startBillingPayment should invoke purchaseProduct on PaymentUseCase`() {
-        // Arrange
-        val mockProductId = ProductId("MOCK")
-        val mockActivityProvider = mockk<() -> Activity>()
-
-        // Act
-        viewModel.startBillingPayment(mockProductId, mockActivityProvider)
-
-        // Assert
-        coVerify { mockPaymentUseCase.purchaseProduct(mockProductId, mockActivityProvider) }
-    }
-
-    @Test
-    fun `onClosePurchaseResultDialog with success should invoke fetchAccountExpiry on AccountRepository`() {
-        // Arrange
-
-        // Act
-        viewModel.onClosePurchaseResultDialog(success = true)
-
-        // Assert
-        coVerify { mockAccountRepository.getAccountData() }
-    }
-
-    @Test
-    fun `onClosePurchaseResultDialog with success should invoke resetPurchaseResult on PaymentUseCase`() {
-        // Arrange
-
-        // Act
-        viewModel.onClosePurchaseResultDialog(success = true)
-
-        // Assert
-        coVerify { mockPaymentUseCase.resetPurchaseResult() }
-    }
-
-    @Test
-    fun `onClosePurchaseResultDialog with success false should invoke queryPaymentAvailability on PaymentUseCase`() {
-        // Arrange
-
-        // Act
-        viewModel.onClosePurchaseResultDialog(success = false)
-
-        // Assert
-        coVerify { mockPaymentUseCase.queryPaymentAvailability() }
-    }
-
-    @Test
-    fun `onClosePurchaseResultDialog with success false should invoke resetPurchaseResult on PaymentUseCase`() {
-        // Arrange
-
-        // Act
-        viewModel.onClosePurchaseResultDialog(success = false)
-
-        // Assert
-        coVerify { mockPaymentUseCase.resetPurchaseResult() }
-    }
-
     companion object {
-        private const val PURCHASE_RESULT_EXTENSIONS_CLASS =
-            "net.mullvad.mullvadvpn.util.PurchaseResultExtensionsKt"
         private const val DUMMY_DEVICE_NAME = "fake_name"
     }
 }
