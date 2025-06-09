05486ea54875c8921109911427efe7a410a19f74
Bug Magnet <marco.nikic@mullvad.net>
2025-06-09 11:55:08 +0200

    Introduce a generic HostingController with sensible default values

---
+++ ios/MullvadVPN/Containers/Root/UIHostingRootController.swift
@@ -0,0 +1,34 @@
+//
+//  UIHostingRootController.swift
+//  MullvadVPN
+//
+//  Created by Marco Nikic on 2025-06-09.
+//  Copyright © 2025 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor
+public final class UIHostingRootController<Content: View>: UIHostingController<Content>, RootContainment {
+    let preferredHeaderBarPresentation: HeaderBarPresentation
+    let prefersHeaderBarHidden: Bool
+    let prefersDeviceInfoBarHidden: Bool
+
+    init(
+        preferredHeaderBarPresentation: HeaderBarPresentation =
+            HeaderBarPresentation(style: .default, showsDivider: false),
+        prefersHeaderBarHidden: Bool = false,
+        prefersDeviceInfoBarHidden: Bool = true,
+        rootView: Content
+    ) {
+        self.preferredHeaderBarPresentation = preferredHeaderBarPresentation
+        self.prefersHeaderBarHidden = prefersHeaderBarHidden
+        self.prefersDeviceInfoBarHidden = prefersDeviceInfoBarHidden
+        super.init(rootView: rootView)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
