b176627133940597c854d349eed8112f67795543
David Lönnhager <david.l@mullvad.net>
2025-06-13 14:28:38 +0200

    Do not rely on DNS for API in installer-downloader

--- mullvad-update/src/client/api.rs
+++ mullvad-update/src/client/api.rs
@@ -1,5 +1,6 @@
 //! This module implements fetching of information about app versions
 
+use std::net::IpAddr;
 use std::path::PathBuf;
 
 use anyhow::Context;
@@ -11,6 +12,8 @@ use crate::format;
 use crate::version::{VersionInfo, VersionParameters};
 
 use super::version_provider::VersionInfoProvider;
+
+use mullvad_api_constants::*;
 
 /// Available platforms in the default metadata repository
 #[derive(Debug, Clone, Copy)]
@@ -56,6 +59,8 @@ impl MetaRepositoryPlatform {
 pub struct HttpVersionInfoProvider {
     /// Endpoint for GET request
     url: String,
+    /// Optional host to resolve (to the IP) without DNS
+    resolve: Option<(&'static str, IpAddr)>,
     /// Accepted root certificate. Defaults are used unless specified
     pinned_certificate: Option<reqwest::Certificate>,
     /// If set, the response metadata will be serialized and written to this path
@@ -80,6 +85,7 @@ impl From<MetaRepositoryPlatform> for HttpVersionInfoProvider {
     fn from(platform: MetaRepositoryPlatform) -> Self {
         HttpVersionInfoProvider {
             url: platform.url(),
+            resolve: Some((API_HOST_DEFAULT, API_IP_DEFAULT)),
             pinned_certificate: Some(crate::defaults::PINNED_CERTIFICATE.clone()),
             dump_to_path: None,
         }
@@ -138,7 +144,7 @@ impl HttpVersionInfoProvider {
         &self,
         deserialize_fn: impl FnOnce(&[u8]) -> anyhow::Result<format::SignedResponse>,
     ) -> anyhow::Result<format::SignedResponse> {
-        let raw_json = Self::get(&self.url, self.pinned_certificate.clone()).await?;
+        let raw_json = Self::get(&self.url, self.pinned_certificate.clone(), self.resolve).await?;
         let signed_response = deserialize_fn(&raw_json)?;
         if let Some(path) = &self.dump_to_path {
             fs::write(path, raw_json)
@@ -149,9 +155,15 @@ impl HttpVersionInfoProvider {
     }
 
     /// Perform a simple GET request, with a size limit, and return it as bytes
+    ///
+    /// # Arguments
+    /// `url` - URL to fetch
+    /// `pinned_certificate` - Optional pinned certificate for TLS verification
+    /// `resolve` - Optional host to resolve (to the IP) without DNS
     async fn get(
         url: &str,
         pinned_certificate: Option<reqwest::Certificate>,
+        resolve: Option<(&'static str, IpAddr)>,
     ) -> anyhow::Result<Vec<u8>> {
         let mut req_builder = reqwest::Client::builder();
         req_builder = req_builder.min_tls_version(reqwest::tls::Version::TLS_1_3);
@@ -160,6 +172,11 @@ impl HttpVersionInfoProvider {
             req_builder = req_builder
                 .tls_built_in_root_certs(false)
                 .add_root_certificate(pinned_certificate);
+        }
+
+        // Resolve name without DNS
+        if let Some((host, addr)) = resolve {
+            req_builder = req_builder.resolve(host, (addr, 0).into());
         }
 
         // Initiate GET request
@@ -223,21 +240,25 @@ mod test {
 
         // Start HTTP server
         let mut server = mockito::Server::new_async().await;
-        let _mock = server
+        let _mock: mockito::Mock = server
             .mock("GET", "/version")
             // Respond with some version response payload
             .with_body(include_bytes!("../../test-version-response.json"))
             .create();
 
-        let url = format!("{}/version", server.url());
+        // Resolve some host to our mockito server
+        let host = "fakeurl.biz";
+        let url = format!("http://{host}:{}/version", server.socket_address().port());
+        let resolve = (host, server.socket_address().ip());
 
         let temp_dump_dir = TempDir::new().await.unwrap();
         let temp_dump = temp_dump_dir.join("metadata.json");
 
         // Construct query and provider
         let info_provider = HttpVersionInfoProvider {
-            url,
+            url: url.to_string(),
             pinned_certificate: None,
+            resolve: Some(resolve),
             dump_to_path: Some(temp_dump.clone()),
         };
 
