ced6456945c038b05e7222c13001488eb323d422
Oliver <oliver@mohlin.dev>
2025-06-15 06:50:06 +0200

    Add View component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/view/View.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+import { Colors, colors, ColorVariables } from '../../foundations';
+import { Flex, FlexProps } from '../flex';
+import { Container } from './components';
+
+export type ViewProps = FlexProps & {
+  backgroundColor?: Colors;
+};
+
+export const StyledView = styled(Flex)<{ $backgroundColor?: ColorVariables }>`
+  height: 100vh;
+  max-width: 100%;
+  background-color: ${({ $backgroundColor }) => $backgroundColor || undefined};
+`;
+
+function View({ backgroundColor = 'blue', ...props }: ViewProps) {
+  return (
+    <StyledView $backgroundColor={colors[backgroundColor]} $flexDirection="column" {...props} />
+  );
+}
+
+const ViewNamespace = Object.assign(View, {
+  Container: Container,
+});
+
+export { ViewNamespace as View };
