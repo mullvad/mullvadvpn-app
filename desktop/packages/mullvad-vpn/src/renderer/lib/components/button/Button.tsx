09502a122d429dd5f7613aed36c460850454b812
Oliver <oliver@mohlin.dev>
2025-06-16 08:36:05 +0200

    Use native button instead of ButtonBase component

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/button/Button.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/button/Button.tsx
@@ -40,7 +40,7 @@ const styles = {
   },
 };
 
-export const StyledButton = styled(ButtonBase)<ButtonProps>`
+export const StyledButton = styled.button<ButtonProps>`
   ${({ width: sizeProp = 'fill', variant: variantProp = 'primary' }) => {
     const variant = styles.variants[variantProp];
     const size = styles.flex[sizeProp];
