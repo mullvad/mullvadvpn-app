4ba80a9d2a0df8d7801bce8cfe1b9b976b98ed08
Oliver <oliver@mohlin.dev>
2025-06-17 14:04:23 +0200

    Add Footer component to launch view

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/launch/components/footer/components/default-footer/DefaultFooter.tsx
@@ -0,0 +1,30 @@
+import { messages } from '../../../../../../../../shared/gettext';
+import { Button } from '../../../../../../../lib/components';
+import { FlexColumn } from '../../../../../../../lib/components/flex-column';
+import { useBoolean } from '../../../../../../../lib/utility-hooks';
+import { TroubleshootingModal } from '../../../troubleshooting-modal';
+import { FooterText } from '../footer-text';
+
+export function DefaultLaunchFooter() {
+  const [dialogOpen, showDialog, hideDialog] = useBoolean();
+
+  return (
+    <>
+      <FlexColumn $gap="medium">
+        <FooterText>
+          {
+            // TRANSLATORS: Message in launch view when the mullvad service cannot be contacted.
+            messages.pgettext(
+              'launch-view',
+              'Unable to contact the Mullvad system service, your connection might be unsecure. Please troubleshoot or send a problem report by clicking the "Learn more" button.',
+            )
+          }
+        </FooterText>
+        <Button onClick={showDialog}>
+          <Button.Text>{messages.gettext('Learn more')}</Button.Text>
+        </Button>
+      </FlexColumn>
+      <TroubleshootingModal isOpen={dialogOpen} onClose={hideDialog} />
+    </>
+  );
+}
