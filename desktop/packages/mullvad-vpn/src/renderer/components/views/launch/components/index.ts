dd0eff09b59b56c9b33f71474aba2fe701687644
Oliver <oliver@mohlin.dev>
2025-06-17 14:05:25 +0200

    Refactor LaunchView to use sub components

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/launch/components/index.ts
@@ -0,0 +1,6 @@
+export * from './footer';
+export * from './footer/components/default-footer';
+export * from './footer/components/macos-permission-footer';
+export * from './footer/components/restart-daemon-footer';
+export * from './status-text';
+export * from './troubleshooting-modal';
