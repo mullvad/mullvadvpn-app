a66fb94661b97e4a862207193352cae46a37ec2c
Oliver <oliver@mohlin.dev>
2025-06-17 14:04:23 +0200

    Add Footer component to launch view

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/launch/components/footer/components/restart-daemon-footer/RestartDaemonFooter.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { messages } from '../../../../../../../../shared/gettext';
+import { useAppContext } from '../../../../../../../context';
+import { Button } from '../../../../../../../lib/components';
+import { FlexColumn } from '../../../../../../../lib/components/flex-column';
+import { useBoolean } from '../../../../../../../lib/utility-hooks';
+import { useUserInterfaceDaemonStatus } from '../../../../../../../redux/hooks';
+import { TroubleshootingModal } from '../../../troubleshooting-modal';
+
+export function RestartDaemonFooter() {
+  const { tryStartDaemon } = useAppContext();
+  const { daemonStatus } = useUserInterfaceDaemonStatus();
+  const [dialogOpen, showDialog, hideDialog] = useBoolean();
+
+  const handleTryAgain = React.useCallback(() => {
+    tryStartDaemon();
+  }, [tryStartDaemon]);
+
+  return (
+    <>
+      <FlexColumn $gap="medium">
+        <Button onClick={handleTryAgain} disabled={daemonStatus === 'start-requested'}>
+          <Button.Text>
+            {
+              // TRANSLATORS: Button label for trying to restart the daemon again.
+              messages.pgettext('launch-view', 'Try again')
+            }
+          </Button.Text>
+        </Button>
+        <Button onClick={showDialog}>
+          <Button.Text>
+            {
+              // TRANSLATORS: Button label for opening dialog with troubleshooting details.
+              messages.pgettext('launch-view', 'Details')
+            }
+          </Button.Text>
+        </Button>
+      </FlexColumn>
+      <TroubleshootingModal isOpen={dialogOpen} onClose={hideDialog} />
+    </>
+  );
+}
