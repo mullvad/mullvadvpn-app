716fe4e7520c73dfdb73fb65ec39b0868a139036
David Lönnhager <david.l@mullvad.net>
2025-06-18 18:12:53 +0200

    fixup

--- mullvad-setup/src/service.rs
+++ mullvad-setup/src/service.rs
@@ -16,6 +16,7 @@ const SERVICE_NAME: &str = "mullvadvpn";
 const SERVICE_DESC: &str = "Mullvad VPN";
 const WAIT_STATUS_TIMEOUT: Duration = Duration::from_secs(8);
 
+/// Start Mullvad VPN service and wait until it is running
 pub async fn start() -> Result<(), Error> {
     tokio::task::spawn_blocking(|| start_and_wait_for_service())
         .await
