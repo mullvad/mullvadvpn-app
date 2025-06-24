c97f7901b91ddf51bf8560e82b75e49a78785ca9
Oliver <oliver@mohlin.dev>
2025-06-24 14:39:22 +0200

    Add launch route object model

--- desktop/packages/mullvad-vpn/test/e2e/route-object-models/routes-object-model.ts
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/routes-object-model.ts
@@ -2,6 +2,7 @@ import { Page } from 'playwright';
 
 import { TestUtils } from '../utils';
 import { FilterRouteObjectModel } from './filter';
+import { LaunchRouteObjectModel } from './launch';
 import { MainRouteObjectModel } from './main';
 import { SelectLanguageRouteObjectModel } from './select-language';
 import { SelectLocationRouteObjectModel } from './select-location';
@@ -11,6 +12,7 @@ import { VpnSettingsRouteObjectModel } from './vpn-settings';
 
 export class RoutesObjectModel {
   readonly main: MainRouteObjectModel;
+  readonly launch: LaunchRouteObjectModel;
   readonly settings: SettingsRouteObjectModel;
   readonly userInterfaceSettings: UserInterfaceSettingsRouteObjectModel;
   readonly selectLanguage: SelectLanguageRouteObjectModel;
@@ -21,6 +23,7 @@ export class RoutesObjectModel {
   constructor(page: Page, utils: TestUtils) {
     this.selectLanguage = new SelectLanguageRouteObjectModel(page, utils);
     this.main = new MainRouteObjectModel(page, utils);
+    this.launch = new LaunchRouteObjectModel(page, utils);
     this.settings = new SettingsRouteObjectModel(page, utils);
     this.userInterfaceSettings = new UserInterfaceSettingsRouteObjectModel(page, utils);
     this.filter = new FilterRouteObjectModel(page, utils);
