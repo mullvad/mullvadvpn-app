018f64e19d50fc93f5d3b48df0e8869b96ee2ba5
Oliver <oliver@mohlin.dev>
2025-06-26 16:09:12 +0200

    Show platform based restart step in troubleshooting modal

--- desktop/packages/mullvad-vpn/src/renderer/components/views/launch/components/troubleshooting-modal/TroubleshootingModal.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/launch/components/troubleshooting-modal/TroubleshootingModal.tsx
@@ -1,15 +1,21 @@
 import { useCallback } from 'react';
+import styled from 'styled-components';
 
 import { messages } from '../../../../../../shared/gettext';
 import { RoutePath } from '../../../../../../shared/routes';
 import { Button } from '../../../../../lib/components';
 import { TransitionType, useHistory } from '../../../../../lib/history';
 import { ModalAlert, ModalAlertType, ModalMessage, ModalMessageList } from '../../../../Modal';
+import { useTroubleshootingSteps } from './hooks';
 
 export type TroubleshootingModalProps = {
   isOpen: boolean;
   onClose: () => void;
 };
+
+const StyledModalMessage = styled(ModalMessage)`
+  margin-top: 0;
+`;
 
 export function TroubleshootingModal({ isOpen, onClose }: TroubleshootingModalProps) {
   const { push } = useHistory();
@@ -17,6 +23,9 @@ export function TroubleshootingModal({ isOpen, onClose }: TroubleshootingModalPr
     onClose();
     push(RoutePath.problemReport, { transition: TransitionType.show });
   }, [onClose, push]);
+
+  const steps = useTroubleshootingSteps();
+
   return (
     <ModalAlert
       isOpen={isOpen}
@@ -44,34 +53,9 @@ export function TroubleshootingModal({ isOpen, onClose }: TroubleshootingModalPr
           )
         }
       </ModalMessage>
-      <ModalMessage>
-        <ModalMessageList>
-          <li>
-            {
-              // TRANSLATORS: List item in troubleshooting modal advising user to restart background process.
-              messages.pgettext('launch-view', 'Restarting the Mullvad background process')
-            }
-          </li>
-          <li>
-            {
-              // TRANSLATORS: List item in troubleshooting modal advising user to restart their computer.
-              messages.pgettext('launch-view', 'Restarting your computer')
-            }
-          </li>
-          <li>
-            {
-              // TRANSLATORS: List item in troubleshooting modal advising user to reinstall the app.
-              messages.pgettext('launch-view', 'Reinstalling the app')
-            }
-          </li>
-          <li>
-            {
-              // TRANSLATORS: List item in troubleshooting modal advising user disable third party antivirus.
-              messages.pgettext('launch-view', 'Disabling third party antivirus software')
-            }
-          </li>
-        </ModalMessageList>
-      </ModalMessage>
+      <StyledModalMessage>
+        <ModalMessageList>{steps}</ModalMessageList>
+      </StyledModalMessage>
       <ModalMessage>
         {
           // TRANSLATORS: Message in troubleshooting modal advising user to send a problem report if the steps do not work.
