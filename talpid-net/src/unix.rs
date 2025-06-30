e4a270d7a75f791a4dab739c6b6c5b028dcc3bda
Markus Pettersson <markus.pettersson@mullvad.net>
2025-06-30 22:23:26 +0200

    Fix type error for musl targets

--- talpid-net/src/unix.rs
+++ talpid-net/src/unix.rs
@@ -1,6 +1,10 @@
 #![cfg(any(target_os = "linux", target_os = "macos"))]
 
-use std::{ffi::c_uint, io, os::fd::AsRawFd};
+use std::{
+    ffi::{c_uint, c_ulong},
+    io,
+    os::fd::AsRawFd,
+};
 
 use nix::{errno::Errno, net::if_::if_nametoindex};
 use socket2::Domain;
@@ -26,9 +30,9 @@ const SIOCSIFMTU: u64 = 0x80206934;
 #[cfg(target_os = "macos")]
 const SIOCGIFMTU: u64 = 0xc0206933;
 #[cfg(target_os = "linux")]
-const SIOCSIFMTU: u64 = libc::SIOCSIFMTU;
+const SIOCSIFMTU: c_ulong = libc::SIOCSIFMTU;
 #[cfg(target_os = "linux")]
-const SIOCGIFMTU: u64 = libc::SIOCSIFMTU;
+const SIOCGIFMTU: c_ulong = libc::SIOCSIFMTU;
 
 pub fn set_mtu(interface_name: &str, mtu: u16) -> Result<(), io::Error> {
     let sock = socket2::Socket::new(
@@ -56,8 +60,14 @@ pub fn set_mtu(interface_name: &str, mtu: u16) -> Result<(), io::Error> {
     };
     ifr.ifr_ifru.ifru_mtu = mtu as i32;
 
+    // For some reason, libc crate defines ioctl to take a c_int (which is defined as i32), but the c_ulong type is defined as u64:
+    // https://docs.rs/libc/latest/x86_64-unknown-linux-musl/libc/fn.ioctl.html
+    // https://docs.rs/libc/latest/x86_64-unknown-linux-musl/libc/type.c_ulong.html
+    // https://docs.rs/libc/latest/x86_64-unknown-linux-musl/libc/constant.SIOCSIFMTU.html
+    #[allow(clippy::useless_conversion)]
+    let request = SIOCSIFMTU.try_into().unwrap();
     // SAFETY: SIOCSIFMTU expects an ifreq with an MTU and interface set
-    if unsafe { libc::ioctl(sock.as_raw_fd(), SIOCSIFMTU, &ifr) } < 0 {
+    if unsafe { libc::ioctl(sock.as_raw_fd(), request, &ifr) } < 0 {
         let e = std::io::Error::last_os_error();
         log::error!("{}", e.display_chain_with_msg("SIOCSIFMTU failed"));
         return Err(e);
@@ -90,8 +100,14 @@ pub fn get_mtu(interface_name: &str) -> Result<u16, io::Error> {
         )
     };
 
+    // For some reason, libc crate defines ioctl to take a c_int (which is defined as i32), but the c_ulong type is defined as u64:
+    // https://docs.rs/libc/latest/x86_64-unknown-linux-musl/libc/fn.ioctl.html
+    // https://docs.rs/libc/latest/x86_64-unknown-linux-musl/libc/type.c_ulong.html
+    // https://docs.rs/libc/latest/x86_64-unknown-linux-musl/libc/constant.SIOCGIFMTU.html
+    #[allow(clippy::useless_conversion)]
+    let request = SIOCGIFMTU.try_into().unwrap();
     // SAFETY: SIOCGIFMTU expects an ifreq with an interface set
-    if unsafe { libc::ioctl(sock.as_raw_fd(), SIOCGIFMTU, &ifr) } < 0 {
+    if unsafe { libc::ioctl(sock.as_raw_fd(), request, &ifr) } < 0 {
         let e = std::io::Error::last_os_error();
         log::error!("{}", e.display_chain_with_msg("SIOCGIFMTU failed"));
         return Err(e);
