9f66006786be76f741307cc916c6e660106b3848
David Göransson <david.goransson@mullvad.net>
2025-07-01 15:39:07 +0200

    Clean up mullvad checkbox

--- android/lib/ui/designsystem/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/designsystem/MullvadCheckbox.kt
+++ android/lib/ui/designsystem/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/designsystem/MullvadCheckbox.kt
@@ -1,14 +1,15 @@
 package net.mullvad.mullvadvpn.lib.ui.designsystem
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
@@ -19,31 +20,37 @@ import net.mullvad.mullvadvpn.lib.theme.color.selected
 private fun PreviewMullvadCheckbox() {
     AppTheme {
         Column(
-            Modifier.background(color = MaterialTheme.colorScheme.primary),
+            Modifier.background(color = MaterialTheme.colorScheme.background),
             verticalArrangement = Arrangement.spacedBy(Dimens.smallSpacer),
         ) {
-            MullvadCheckbox(checked = false) {}
-            MullvadCheckbox(checked = true) {}
+            MullvadCheckbox(checked = false, null)
+            MullvadCheckbox(checked = true, null)
+            MullvadCheckbox(checked = false, null, enabled = false)
+            MullvadCheckbox(checked = true, null, enabled = false)
         }
     }
 }
 
 @Composable
 fun MullvadCheckbox(
-    checkedColor: Color = MaterialTheme.colorScheme.onPrimary,
-    uncheckedColor: Color = MaterialTheme.colorScheme.onPrimary,
-    checkmarkColor: Color = MaterialTheme.colorScheme.selected,
     checked: Boolean,
-    onCheckedChange: (Boolean) -> Unit,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colors: CheckboxColors =
+        CheckboxDefaults.colors(
+            checkedColor = MaterialTheme.colorScheme.onPrimary,
+            uncheckedColor = MaterialTheme.colorScheme.onPrimary,
+            checkmarkColor = MaterialTheme.colorScheme.selected,
+        ),
+    interactionSource: MutableInteractionSource? = null,
 ) {
     Checkbox(
         checked = checked,
         onCheckedChange = onCheckedChange,
-        colors =
-            CheckboxDefaults.colors(
-                checkedColor = checkedColor,
-                uncheckedColor = uncheckedColor,
-                checkmarkColor = checkmarkColor,
-            ),
+        modifier = modifier,
+        enabled = enabled,
+        colors = colors,
+        interactionSource = interactionSource,
     )
 }
