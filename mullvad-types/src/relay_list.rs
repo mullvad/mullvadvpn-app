024a473718f3e10c694f2a034f15dcc06cf6b31f
Markus Pettersson <markus.pettersson@mullvad.net>
2025-07-07 08:06:04 +0200

    Fix broken doc test

--- mullvad-types/src/relay_list.rs
+++ mullvad-types/src/relay_list.rs
@@ -208,7 +208,7 @@ impl PartialEq for Relay {
     /// # Example
     ///
     /// ```rust
-    /// # use mullvad_types::{relay_list::Relay, relay_list::{RelayEndpointData, WireguardRelayEndpointData}};
+    /// # use mullvad_types::{relay_list::{Relay, Features}, relay_list::{RelayEndpointData, WireguardRelayEndpointData}};
     /// # use talpid_types::net::wireguard::PublicKey;
     ///
     /// let relay = Relay {
@@ -238,6 +238,7 @@ impl PartialEq for Relay {
     ///     #   latitude: 57.71,
     ///     #   longitude: 11.97,
     ///     # },
+    ///     # features: Features::default(),
     /// };
     ///
     /// let mut different_relay = relay.clone();
