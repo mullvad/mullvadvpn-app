3098b31a2246b039edba6232aab89b49fd0959f8
mojganii <mojgan.jelodar@mullvad.net>
2025-07-08 10:47:05 +0200

    Fix dynamic size issues for filter chips

--- ios/MullvadVPN/View controllers/RelayFilter/ChipFlowLayout.swift
+++ ios/MullvadVPN/View controllers/RelayFilter/ChipFlowLayout.swift
@@ -8,38 +8,61 @@
 
 import UIKit
 
-class ChipFlowLayout: UICollectionViewCompositionalLayout {
-    init() {
-        super.init { _, _ -> NSCollectionLayoutSection? in
-            // Create an item with flexible size
-            let itemSize = NSCollectionLayoutSize(widthDimension: .estimated(50), heightDimension: .estimated(20))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            item.edgeSpacing = NSCollectionLayoutEdgeSpacing(
-                leading: .fixed(0),
-                top: .fixed(0),
-                trailing: .fixed(0),
-                bottom: .fixed(0)
-            )
-
-            // Create a group that fills the available width and wraps items with proper spacing
-            let groupSize = NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1.0),
-                heightDimension: .estimated(20)
-            )
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-            group.interItemSpacing = .fixed(UIMetrics.FilterView.interChipViewSpacing)
-            group.contentInsets = .zero
-
-            // Create a section with zero inter-group spacing and no content insets
-            let section = NSCollectionLayoutSection(group: group)
-            section.interGroupSpacing = UIMetrics.FilterView.interChipViewSpacing
-            section.contentInsets = .zero
-
-            return section
-        }
+class ChipFlowLayout: UICollectionViewFlowLayout {
+    override init() {
+        super.init()
+        estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        scrollDirection = .vertical
+        minimumInteritemSpacing = UIMetrics.FilterView.interChipViewSpacing
+        minimumLineSpacing = UIMetrics.FilterView.interChipViewSpacing
+        sectionInset = .zero
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let originalAttributes = super.layoutAttributesForElements(in: rect) else {
+            return nil
+        }
+
+        let attributes = originalAttributes.compactMap { $0.copy() as? UICollectionViewLayoutAttributes }
+
+        // Detect RTL
+        let languageCode = Locale.current.languageCode ?? "en"
+        let isRTL = Locale.characterDirection(forLanguage: languageCode) == .rightToLeft
+
+        var currentLineY: CGFloat = -1
+        var currentLineAttributes: [UICollectionViewLayoutAttributes] = []
+
+        for attribute in attributes where attribute.representedElementCategory == .cell {
+            if abs(attribute.frame.origin.y - currentLineY) > 1 {
+                // Align previous line before starting new
+                align(attributes: currentLineAttributes, isRTL: isRTL)
+                currentLineY = attribute.frame.origin.y
+                currentLineAttributes = [attribute]
+            } else {
+                currentLineAttributes.append(attribute)
+            }
+        }
+
+        // Align last line
+        align(attributes: currentLineAttributes, isRTL: isRTL)
+
+        return attributes
+    }
+
+    private func align(attributes: [UICollectionViewLayoutAttributes], isRTL: Bool) {
+        guard !attributes.isEmpty else { return }
+
+        var currentX = isRTL ? collectionViewContentSize.width - sectionInset.right : sectionInset.left
+
+        for attr in isRTL ? attributes.reversed() : attributes {
+            var frame = attr.frame
+            frame.origin.x = currentX - (isRTL ? frame.width : 0)
+            attr.frame = frame
+            currentX += (isRTL ? -1 : 1) * (frame.width + minimumInteritemSpacing)
+        }
     }
 }
