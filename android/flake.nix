184619196eba1d5f5f45d7a72090c2fffc513c17
Albin <albin@mullvad.net>
2025-07-16 21:59:46 +0200

    Disable autoPatchelfHook for macOS

--- android/flake.nix
+++ android/flake.nix
@@ -31,6 +31,15 @@
         overlays = [
           (import rust-overlay)
           devshell.overlays.default
+          # Fix that disables autoPatchelfHook on macOS.
+          # Should be addressed upstream in nixpkgs.
+          (final: prev: {
+            protoc-gen-grpc-java = prev.protoc-gen-grpc-java.overrideAttrs (old: {
+              nativeBuildInputs =
+                pkgs.lib.remove prev.autoPatchelfHook (old.nativeBuildInputs or [])
+                ++ pkgs.lib.optionals prev.stdenv.isLinux [prev.autoPatchelfHook];
+            });
+          })
         ];
       };
 
