b518ac255af1bd57efda70569ede2952055d23fd
David Lönnhager <david.l@mullvad.net>
2025-07-18 08:53:08 +0200

    Set MTU in Unix tun provider

--- talpid-wireguard/src/boringtun/mod.rs
+++ talpid-wireguard/src/boringtun/mod.rs
@@ -500,6 +500,8 @@ fn get_tunnel_for_userspace(
     tun_config.ipv6_gateway = config.ipv6_gateway;
     tun_config.mtu = config.mtu;
 
+    // FIXME: mtu is not set
+
     let _ = routes;
 
     #[cfg(windows)]
