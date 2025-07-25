fb9effa74a4430cb64bcaa46767f325297bdb156
Oliver <oliver@mohlin.dev>
2025-07-25 11:36:12 +0200

    Move WireguardSettingsButton component to separate folder

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/wireguard-settings-button/WireguardSettingsButton.tsx
@@ -0,0 +1,40 @@
+import { sprintf } from 'sprintf-js';
+
+import { strings } from '../../../../../../shared/constants';
+import { messages } from '../../../../../../shared/gettext';
+import { RoutePath } from '../../../../../../shared/routes';
+import { RelaySettingsRedux } from '../../../../../redux/settings/reducers';
+import { useSelector } from '../../../../../redux/store';
+import { NavigationListItem } from '../../../../NavigationListItem';
+
+function mapRelaySettingsToProtocol(relaySettings: RelaySettingsRedux) {
+  if ('normal' in relaySettings) {
+    const { tunnelProtocol } = relaySettings.normal;
+    return tunnelProtocol;
+    // since the GUI doesn't display custom settings, just display the default ones.
+    // If the user sets any settings, then those will be applied.
+  } else if ('customTunnelEndpoint' in relaySettings) {
+    return undefined;
+  } else {
+    throw new Error('Unknown type of relay settings.');
+  }
+}
+
+export function WireguardSettingsButton() {
+  const tunnelProtocol = useSelector((state) =>
+    mapRelaySettingsToProtocol(state.settings.relaySettings),
+  );
+
+  return (
+    <NavigationListItem to={RoutePath.wireguardSettings} disabled={tunnelProtocol === 'openvpn'}>
+      <NavigationListItem.Label>
+        {sprintf(
+          // TRANSLATORS: %(wireguard)s will be replaced with the string "WireGuard"
+          messages.pgettext('vpn-settings-view', '%(wireguard)s settings'),
+          { wireguard: strings.wireguard },
+        )}
+      </NavigationListItem.Label>
+      <NavigationListItem.Icon icon="chevron-right" />
+    </NavigationListItem>
+  );
+}
