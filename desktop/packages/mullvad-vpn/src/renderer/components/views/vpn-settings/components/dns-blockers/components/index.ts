0a2e5de9f1a7fd5fbe9c2a3e7d041cc0a6003c23
Oliver <oliver@mohlin.dev>
2025-07-25 11:18:49 +0200

    Move DnsBlockers component to separate folder

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/dns-blockers/components/index.ts
@@ -0,0 +1,6 @@
+export * from './block-ads';
+export * from './block-adult-content';
+export * from './block-gambling';
+export * from './block-malware';
+export * from './block-social-media';
+export * from './block-trackers';
