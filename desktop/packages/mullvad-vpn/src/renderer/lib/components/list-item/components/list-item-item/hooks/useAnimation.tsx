1ccbcf1c47b01e92f353fbc12f7d91296d12a91f
Oliver <oliver@mohlin.dev>
2025-07-25 10:31:34 +0200

    Add flash animation support to ListItem component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-item/hooks/useAnimation.tsx
@@ -0,0 +1,33 @@
+import { css, keyframes } from 'styled-components';
+
+import { colors } from '../../../../../foundations';
+import { useListItem } from '../../../ListItemContext';
+
+const flash = keyframes`
+  0%   { background-color: var(--background-color) }
+  50%  { background-color: ${colors.whiteOnBlue20} }
+  100% { background-color: var(--background-color) }
+`;
+
+const dim = keyframes`
+  0%   { opacity: 100% }
+  25%  { opacity: 50% }
+  50%  { opacity: 50% }
+  75%  { opacity: 50% }
+  100% { opacity: 100% }
+`;
+
+export const useAnimation = () => {
+  const { animation } = useListItem();
+  if (animation === 'flash') {
+    return css`
+      animation: ${flash} 0.75s ease-in-out 0s 2 normal forwards;
+    `;
+  }
+  if (animation === 'dim') {
+    return css`
+      animation: ${dim} 1.5s ease-in-out 0s 1 normal forwards;
+    `;
+  }
+  return undefined;
+};
