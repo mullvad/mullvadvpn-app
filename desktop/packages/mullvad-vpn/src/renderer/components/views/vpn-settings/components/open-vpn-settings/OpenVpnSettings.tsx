3c5d4383f0a1724f2af4eca2d573b54c667636ae
Oliver <oliver@mohlin.dev>
2025-07-25 12:05:26 +0200

    Refactor VpnSettingsView components to use consistent names

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/open-vpn-settings/OpenVpnSettings.tsx
@@ -0,0 +1,24 @@
+import { sprintf } from 'sprintf-js';
+
+import { strings } from '../../../../../../shared/constants';
+import { messages } from '../../../../../../shared/gettext';
+import { RoutePath } from '../../../../../../shared/routes';
+import { useTunnelProtocol } from '../../../../../lib/relay-settings-hooks';
+import { NavigationListItem } from '../../../../NavigationListItem';
+
+export function OpenVpnSettings() {
+  const tunnelProtocol = useTunnelProtocol();
+
+  return (
+    <NavigationListItem to={RoutePath.openVpnSettings} disabled={tunnelProtocol === 'wireguard'}>
+      <NavigationListItem.Label>
+        {sprintf(
+          // TRANSLATORS: %(openvpn)s will be replaced with the string "OpenVPN"
+          messages.pgettext('vpn-settings-view', '%(openvpn)s settings'),
+          { openvpn: strings.openvpn },
+        )}
+      </NavigationListItem.Label>
+      <NavigationListItem.Icon icon="chevron-right" />
+    </NavigationListItem>
+  );
+}
