8a0950f44a674ff48f439c92b97c3627d6ba72ec
Oliver <oliver@mohlin.dev>
2025-07-25 11:30:34 +0200

    Move LockdownMode component to separate folder

--- desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/VpnSettingsView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/VpnSettingsView.tsx
@@ -1,34 +1,22 @@
 import { useCallback, useMemo } from 'react';
 import { sprintf } from 'sprintf-js';
-import styled from 'styled-components';
 
 import { strings, urls } from '../../../../shared/constants';
 import { TunnelProtocol } from '../../../../shared/daemon-rpc-types';
 import { messages } from '../../../../shared/gettext';
 import log from '../../../../shared/logging';
 import { RoutePath } from '../../../../shared/routes';
-import { useAppContext } from '../../../context';
-import { Button } from '../../../lib/components';
 import { useRelaySettingsUpdater } from '../../../lib/constraint-updater';
-import { spacings } from '../../../lib/foundations';
 import { useHistory } from '../../../lib/history';
 import { useTunnelProtocol } from '../../../lib/relay-settings-hooks';
-import { useBoolean } from '../../../lib/utility-hooks';
 import { RelaySettingsRedux } from '../../../redux/settings/reducers';
 import { useSelector } from '../../../redux/store';
 import { AppNavigationHeader } from '../..';
-import {
-  AriaDescription,
-  AriaDetails,
-  AriaInput,
-  AriaInputGroup,
-  AriaLabel,
-} from '../../AriaGroup';
+import { AriaDescription, AriaInputGroup } from '../../AriaGroup';
 import * as Cell from '../../cell';
 import Selector, { SelectorItem } from '../../cell/Selector';
 import CustomDnsSettings from '../../CustomDnsSettings';
 import { ExternalLink } from '../../ExternalLink';
-import InfoButton from '../../InfoButton';
 import { BackAction } from '../../KeyboardNavigation';
 import {
   Layout,
@@ -37,7 +25,6 @@ import {
   SettingsGroup,
   SettingsStack,
 } from '../../Layout';
-import { ModalAlert, ModalAlertType, ModalMessage } from '../../Modal';
 import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationListItem } from '../../NavigationListItem';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
@@ -49,11 +36,8 @@ import {
   DnsBlockers,
   EnableIpv6,
   KillSwitchInfo,
+  LockdownMode,
 } from './components';
-
-const StyledInfoButton = styled(InfoButton)({
-  marginRight: spacings.medium,
-});
 
 export function VpnSettingsView() {
   const { pop } = useHistory();
@@ -122,100 +106,6 @@ export function VpnSettingsView() {
         </SettingsContainer>
       </Layout>
     </BackAction>
-  );
-}
-
-function LockdownMode() {
-  const blockWhenDisconnected = useSelector((state) => state.settings.blockWhenDisconnected);
-  const { setBlockWhenDisconnected: setBlockWhenDisconnectedImpl } = useAppContext();
-
-  const [confirmationDialogVisible, showConfirmationDialog, hideConfirmationDialog] =
-    useBoolean(false);
-
-  const setBlockWhenDisconnected = useCallback(
-    async (blockWhenDisconnected: boolean) => {
-      try {
-        await setBlockWhenDisconnectedImpl(blockWhenDisconnected);
-      } catch (e) {
-        const error = e as Error;
-        log.error('Failed to update block when disconnected', error.message);
-      }
-    },
-    [setBlockWhenDisconnectedImpl],
-  );
-
-  const setLockDownMode = useCallback(
-    async (newValue: boolean) => {
-      if (newValue) {
-        showConfirmationDialog();
-      } else {
-        await setBlockWhenDisconnected(false);
-      }
-    },
-    [setBlockWhenDisconnected, showConfirmationDialog],
-  );
-
-  const confirmLockdownMode = useCallback(async () => {
-    hideConfirmationDialog();
-    await setBlockWhenDisconnected(true);
-  }, [hideConfirmationDialog, setBlockWhenDisconnected]);
-
-  return (
-    <>
-      <AriaInputGroup>
-        <Cell.Container>
-          <AriaLabel>
-            <Cell.InputLabel>
-              {messages.pgettext('vpn-settings-view', 'Lockdown mode')}
-            </Cell.InputLabel>
-          </AriaLabel>
-          <AriaDetails>
-            <StyledInfoButton>
-              <ModalMessage>
-                {messages.pgettext(
-                  'vpn-settings-view',
-                  'The difference between the Kill Switch and Lockdown Mode is that the Kill Switch will prevent any leaks from happening during automatic tunnel reconnects, software crashes and similar accidents.',
-                )}
-              </ModalMessage>
-              <ModalMessage>
-                {messages.pgettext(
-                  'vpn-settings-view',
-                  'With Lockdown Mode enabled, you must be connected to a Mullvad VPN server to be able to reach the internet. Manually disconnecting or quitting the app will block your connection.',
-                )}
-              </ModalMessage>
-            </StyledInfoButton>
-          </AriaDetails>
-          <AriaInput>
-            <Cell.Switch isOn={blockWhenDisconnected} onChange={setLockDownMode} />
-          </AriaInput>
-        </Cell.Container>
-      </AriaInputGroup>
-      <ModalAlert
-        isOpen={confirmationDialogVisible}
-        type={ModalAlertType.caution}
-        buttons={[
-          <Button variant="destructive" key="confirm" onClick={confirmLockdownMode}>
-            <Button.Text>{messages.gettext('Enable anyway')}</Button.Text>
-          </Button>,
-          <Button key="back" onClick={hideConfirmationDialog}>
-            <Button.Text>{messages.gettext('Back')}</Button.Text>
-          </Button>,
-        ]}
-        close={hideConfirmationDialog}>
-        <ModalMessage>
-          {messages.pgettext(
-            'vpn-settings-view',
-            'Attention: enabling this will always require a Mullvad VPN connection in order to reach the internet.',
-          )}
-        </ModalMessage>
-        <ModalMessage>
-          {messages.pgettext(
-            'vpn-settings-view',
-            'The app’s built-in kill switch is always on. This setting will additionally block the internet if clicking Disconnect or Quit.',
-          )}
-        </ModalMessage>
-      </ModalAlert>
-    </>
   );
 }
 
