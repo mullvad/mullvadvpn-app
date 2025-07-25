85e418bbff2786964873c091db4a2cc6556b96c7
Oliver <oliver@mohlin.dev>
2025-07-25 10:32:00 +0200

    Add Listbox component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/listbox/components/listbox-option/components/listbox-option-item/ListboxOptionItem.tsx
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components';
+
+import { colors } from '../../../../../../foundations';
+import { ListItem } from '../../../../../list-item';
+import { ListItemItemProps } from '../../../../../list-item/components';
+import { useListboxContext } from '../../../listbox-context';
+import { useListboxOptionContext } from '../listbox-option-context/ListboxOptionContext';
+
+export type ListItemOptionItemProps = ListItemItemProps;
+
+export const StyledListItemOptionItem = styled(ListItem.Item)<{ $selected: boolean }>`
+  ${({ $selected }) => {
+    return css`
+      background-color: ${$selected ? colors.green : undefined};
+    `;
+  }}
+`;
+
+export function ListboxOptionItem(props: ListItemOptionItemProps) {
+  const { value: selectedValue } = useListboxContext();
+  const { value } = useListboxOptionContext();
+  const selected = value === selectedValue;
+  return <StyledListItemOptionItem $selected={selected} {...props} />;
+}
