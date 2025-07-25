3aa6a102cb516210235aea4981efb1b6a080daae
Oliver <oliver@mohlin.dev>
2025-07-25 12:05:26 +0200

    Refactor VpnSettingsView components to use consistent names

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/dns-blocker-settings/components/index.ts
@@ -0,0 +1,6 @@
+export * from './block-ads-setting';
+export * from './block-adult-content-setting';
+export * from './block-gambling-setting';
+export * from './block-malware-setting';
+export * from './block-social-media-setting';
+export * from './block-trackers-setting';
--- desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/tunnel-protocol-setting/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/tunnel-protocol-setting/index.ts
@@ -1,1 +1,1 @@
-export * from './TunnelProtocolSetting';
+export * from './TunnelProtocol';
