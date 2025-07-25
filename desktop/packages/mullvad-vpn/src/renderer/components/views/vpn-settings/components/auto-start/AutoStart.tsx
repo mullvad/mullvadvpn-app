cebda005ac2c90e5605b9096600083b9cea57b7f
Oliver <oliver@mohlin.dev>
2025-07-25 10:49:23 +0200

    Move AutoStart component into separate folder

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/auto-start/AutoStart.tsx
@@ -0,0 +1,40 @@
+import { useCallback } from 'react';
+
+import { messages } from '../../../../../../shared/gettext';
+import log from '../../../../../../shared/logging';
+import { useAppContext } from '../../../../../context';
+import { useSelector } from '../../../../../redux/store';
+import { AriaInput, AriaInputGroup, AriaLabel } from '../../../../AriaGroup';
+import * as Cell from '../../../../cell';
+
+export function AutoStart() {
+  const autoStart = useSelector((state) => state.settings.autoStart);
+  const { setAutoStart: setAutoStartImpl } = useAppContext();
+
+  const setAutoStart = useCallback(
+    async (autoStart: boolean) => {
+      try {
+        await setAutoStartImpl(autoStart);
+      } catch (e) {
+        const error = e as Error;
+        log.error(`Cannot set auto-start: ${error.message}`);
+      }
+    },
+    [setAutoStartImpl],
+  );
+
+  return (
+    <AriaInputGroup>
+      <Cell.Container>
+        <AriaLabel>
+          <Cell.InputLabel>
+            {messages.pgettext('vpn-settings-view', 'Launch app on start-up')}
+          </Cell.InputLabel>
+        </AriaLabel>
+        <AriaInput>
+          <Cell.Switch isOn={autoStart} onChange={setAutoStart} />
+        </AriaInput>
+      </Cell.Container>
+    </AriaInputGroup>
+  );
+}
