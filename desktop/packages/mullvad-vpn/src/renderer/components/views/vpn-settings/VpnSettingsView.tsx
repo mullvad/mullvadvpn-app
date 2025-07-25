29038fa55ab235d39a48edea8434914452a40cf4
Oliver <oliver@mohlin.dev>
2025-07-25 11:23:33 +0200

    Move EnableIpv6 component to separate folder

--- desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/VpnSettingsView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/VpnSettingsView.tsx
@@ -42,7 +42,7 @@ import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationListItem } from '../../NavigationListItem';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
 import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
-import { AllowLan, AutoConnect, AutoStart, DnsBlockers } from './components';
+import { AllowLan, AutoConnect, AutoStart, DnsBlockers, EnableIpv6 } from './components';
 
 const StyledInfoButton = styled(InfoButton)({
   marginRight: spacings.medium,
@@ -115,52 +115,6 @@ export function VpnSettingsView() {
         </SettingsContainer>
       </Layout>
     </BackAction>
-  );
-}
-
-function EnableIpv6() {
-  const enableIpv6 = useSelector((state) => state.settings.enableIpv6);
-  const { setEnableIpv6: setEnableIpv6Impl } = useAppContext();
-
-  const setEnableIpv6 = useCallback(
-    async (enableIpv6: boolean) => {
-      try {
-        await setEnableIpv6Impl(enableIpv6);
-      } catch (e) {
-        const error = e as Error;
-        log.error('Failed to update enable IPv6', error.message);
-      }
-    },
-    [setEnableIpv6Impl],
-  );
-
-  return (
-    <AriaInputGroup>
-      <Cell.Container>
-        <AriaLabel>
-          <Cell.InputLabel>{messages.pgettext('vpn-settings-view', 'Enable IPv6')}</Cell.InputLabel>
-        </AriaLabel>
-        <AriaDetails>
-          <StyledInfoButton>
-            <ModalMessage>
-              {messages.pgettext(
-                'vpn-settings-view',
-                'When this feature is enabled, IPv6 can be used alongside IPv4 in the VPN tunnel to communicate with internet services.',
-              )}
-            </ModalMessage>
-            <ModalMessage>
-              {messages.pgettext(
-                'vpn-settings-view',
-                'IPv4 is always enabled and the majority of websites and applications use this protocol. We do not recommend enabling IPv6 unless you know you need it.',
-              )}
-            </ModalMessage>
-          </StyledInfoButton>
-        </AriaDetails>
-        <AriaInput>
-          <Cell.Switch isOn={enableIpv6} onChange={setEnableIpv6} />
-        </AriaInput>
-      </Cell.Container>
-    </AriaInputGroup>
   );
 }
 
