400781c639ce0a1c3de3bb13aad5534fc868ed41
Oliver <oliver@mohlin.dev>
2025-07-25 12:05:26 +0200

    Refactor VpnSettingsView components to use consistent names

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/dns-blocker-settings/components/block-trackers-setting/BlockTrackersSetting.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+import { messages } from '../../../../../../../../shared/gettext';
+import { colors, spacings } from '../../../../../../../lib/foundations';
+import { AriaInput, AriaInputGroup, AriaLabel } from '../../../../../../AriaGroup';
+import * as Cell from '../../../../../../cell';
+import { useDns } from '../../hooks';
+
+const StyledSectionItem = styled(Cell.Container)({
+  backgroundColor: colors.blue40,
+});
+
+const IndentedValueLabel = styled(Cell.ValueLabel)({
+  marginLeft: spacings.medium,
+});
+
+export function BlockTrackersSetting() {
+  const [dns, setBlockTrackers] = useDns('blockTrackers');
+
+  return (
+    <AriaInputGroup>
+      <StyledSectionItem disabled={dns.state === 'custom'}>
+        <AriaLabel>
+          <IndentedValueLabel>
+            {
+              // TRANSLATORS: Label for settings that enables tracker blocking.
+              messages.pgettext('vpn-settings-view', 'Trackers')
+            }
+          </IndentedValueLabel>
+        </AriaLabel>
+        <AriaInput>
+          <Cell.Switch
+            isOn={dns.state === 'default' && dns.defaultOptions.blockTrackers}
+            onChange={setBlockTrackers}
+          />
+        </AriaInput>
+      </StyledSectionItem>
+    </AriaInputGroup>
+  );
+}
