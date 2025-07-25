83544cd8947c7c9890e217a51886ecf0379c1d9b
Oliver <oliver@mohlin.dev>
2025-07-25 10:32:00 +0200

    Add Listbox component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/listbox/Listbox.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { ListItem, ListItemProps } from '../list-item';
+import { ListboxLabel, ListboxOption, ListboxOptions, ListboxProvider } from './components';
+
+export type ListboxProps<T> = ListItemProps & {
+  onValueChange?: (value: T) => Promise<void>;
+  value?: T;
+};
+
+function Listbox<T>({ value, onValueChange, children, ...props }: ListboxProps<T>) {
+  const labelId = React.useId();
+
+  return (
+    <ListboxProvider labelId={labelId} value={value} onValueChange={onValueChange}>
+      <ListItem role="listbox" aria-labelledby={labelId} {...props}>
+        {children}
+      </ListItem>
+    </ListboxProvider>
+  );
+}
+
+const ListboxNamespace = Object.assign(Listbox, {
+  Item: ListItem.Item,
+  Content: ListItem.Content,
+  Label: ListboxLabel,
+  Group: ListItem.Group,
+  Text: ListItem.Text,
+  Footer: ListItem.Footer,
+  Icon: ListItem.Icon,
+  Option: ListboxOption,
+  Options: ListboxOptions,
+});
+
+export { ListboxNamespace as Listbox };
