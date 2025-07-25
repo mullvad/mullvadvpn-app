36c652ca8af3451782dd1379fa3fc2348bd88473
Oliver <oliver@mohlin.dev>
2025-07-25 10:31:34 +0200

    Add flash animation support to ListItem component

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/ListItemContext.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/ListItemContext.tsx
@@ -1,21 +1,31 @@
 import { createContext, ReactNode, useContext } from 'react';
 
 import { levels } from './levels';
+import { ListItemAnimation } from './ListItem';
 
 interface ListItemContextType {
   level: keyof typeof levels;
   disabled?: boolean;
+  animation?: ListItemAnimation;
 }
 
 const ListItemContext = createContext<ListItemContextType | undefined>(undefined);
 
 interface ListItemProviderProps extends ListItemContextType {
+  animation?: ListItemAnimation;
   children: ReactNode;
 }
 
-export const ListItemProvider = ({ level, disabled, children }: ListItemProviderProps) => {
+export const ListItemProvider = ({
+  level,
+  disabled,
+  animation,
+  children,
+}: ListItemProviderProps) => {
   return (
-    <ListItemContext.Provider value={{ level, disabled }}>{children}</ListItemContext.Provider>
+    <ListItemContext.Provider value={{ level, disabled, animation }}>
+      {children}
+    </ListItemContext.Provider>
   );
 };
 
