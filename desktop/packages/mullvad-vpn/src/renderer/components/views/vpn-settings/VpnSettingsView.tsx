648b6deead1068be094af7718ee32580311f7f66
Oliver <oliver@mohlin.dev>
2025-07-25 10:53:17 +0200

    Move AllowLan to separate folder

--- desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/VpnSettingsView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/VpnSettingsView.tsx
@@ -43,7 +43,7 @@ import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationListItem } from '../../NavigationListItem';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
 import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
-import { AutoStart } from './components';
+import { AllowLan, AutoStart } from './components';
 
 const StyledInfoButton = styled(InfoButton)({
   marginRight: spacings.medium,
@@ -55,11 +55,6 @@ const StyledTitleLabel = styled(Cell.SectionTitle)({
 
 const StyledSectionItem = styled(Cell.Container)({
   backgroundColor: colors.blue40,
-});
-
-const LanIpRanges = styled.ul({
-  listStyle: 'disc outside',
-  marginLeft: spacings.large,
 });
 
 const IndentedValueLabel = styled(Cell.ValueLabel)({
@@ -162,50 +157,6 @@ function AutoConnect() {
           </Cell.CellFooterText>
         </AriaDescription>
       </Cell.CellFooter>
-    </AriaInputGroup>
-  );
-}
-
-function AllowLan() {
-  const allowLan = useSelector((state) => state.settings.allowLan);
-  const { setAllowLan } = useAppContext();
-
-  return (
-    <AriaInputGroup>
-      <Cell.Container>
-        <AriaLabel>
-          <Cell.InputLabel>
-            {messages.pgettext('vpn-settings-view', 'Local network sharing')}
-          </Cell.InputLabel>
-        </AriaLabel>
-        <AriaDetails>
-          <StyledInfoButton>
-            <ModalMessage>
-              {messages.pgettext(
-                'vpn-settings-view',
-                'This feature allows access to other devices on the local network, such as for sharing, printing, streaming, etc.',
-              )}
-            </ModalMessage>
-            <ModalMessage>
-              {messages.pgettext(
-                'vpn-settings-view',
-                'It does this by allowing network communication outside the tunnel to local multicast and broadcast ranges as well as to and from these private IP ranges:',
-              )}
-              <LanIpRanges>
-                <li>10.0.0.0/8</li>
-                <li>172.16.0.0/12</li>
-                <li>192.168.0.0/16</li>
-                <li>169.254.0.0/16</li>
-                <li>fe80::/10</li>
-                <li>fc00::/7</li>
-              </LanIpRanges>
-            </ModalMessage>
-          </StyledInfoButton>
-        </AriaDetails>
-        <AriaInput>
-          <Cell.Switch isOn={allowLan} onChange={setAllowLan} />
-        </AriaInput>
-      </Cell.Container>
     </AriaInputGroup>
   );
 }
---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/allow-lan/AllowLan.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+import { messages } from '../../../../../../shared/gettext';
+import { useAppContext } from '../../../../../context';
+import { spacings } from '../../../../../lib/foundations';
+import { useSelector } from '../../../../../redux/store';
+import { AriaDetails, AriaInput, AriaInputGroup, AriaLabel } from '../../../../AriaGroup';
+import * as Cell from '../../../../cell';
+import InfoButton from '../../../../InfoButton';
+import { ModalMessage } from '../../../../Modal';
+
+const StyledInfoButton = styled(InfoButton)({
+  marginRight: spacings.medium,
+});
+
+const LanIpRanges = styled.ul({
+  listStyle: 'disc outside',
+  marginLeft: spacings.large,
+});
+
+export function AllowLan() {
+  const allowLan = useSelector((state) => state.settings.allowLan);
+  const { setAllowLan } = useAppContext();
+
+  return (
+    <AriaInputGroup>
+      <Cell.Container>
+        <AriaLabel>
+          <Cell.InputLabel>
+            {messages.pgettext('vpn-settings-view', 'Local network sharing')}
+          </Cell.InputLabel>
+        </AriaLabel>
+        <AriaDetails>
+          <StyledInfoButton>
+            <ModalMessage>
+              {messages.pgettext(
+                'vpn-settings-view',
+                'This feature allows access to other devices on the local network, such as for sharing, printing, streaming, etc.',
+              )}
+            </ModalMessage>
+            <ModalMessage>
+              {messages.pgettext(
+                'vpn-settings-view',
+                'It does this by allowing network communication outside the tunnel to local multicast and broadcast ranges as well as to and from these private IP ranges:',
+              )}
+              <LanIpRanges>
+                <li>10.0.0.0/8</li>
+                <li>172.16.0.0/12</li>
+                <li>192.168.0.0/16</li>
+                <li>169.254.0.0/16</li>
+                <li>fe80::/10</li>
+                <li>fc00::/7</li>
+              </LanIpRanges>
+            </ModalMessage>
+          </StyledInfoButton>
+        </AriaDetails>
+        <AriaInput>
+          <Cell.Switch isOn={allowLan} onChange={setAllowLan} />
+        </AriaInput>
+      </Cell.Container>
+    </AriaInputGroup>
+  );
+}
