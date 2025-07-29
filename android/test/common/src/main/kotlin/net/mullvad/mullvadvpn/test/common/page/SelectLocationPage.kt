b87607c267c569fabecdb69dbfde7ccb534081f1
David Göransson <david.goransson@mullvad.net>
2025-07-29 15:37:07 +0200

    Check for both tag and name in location cells for e2e tests

--- android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/SelectLocationPage.kt
+++ android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/SelectLocationPage.kt
@@ -2,6 +2,7 @@ package net.mullvad.mullvadvpn.test.common.page
 
 import androidx.test.uiautomator.By
 import net.mullvad.mullvadvpn.lib.ui.tag.EXPAND_BUTTON_TEST_TAG
+import net.mullvad.mullvadvpn.lib.ui.tag.GEOLOCATION_NAME_TAG
 import net.mullvad.mullvadvpn.lib.ui.tag.SELECT_LOCATION_SCREEN_TEST_TAG
 import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
 
@@ -11,7 +12,11 @@ class SelectLocationPage internal constructor() : Page() {
     }
 
     fun clickLocationExpandButton(locationName: String) {
-        val locationCell = uiDevice.findObjectWithTimeout(By.text(locationName)).parent.parent
+        val locationCell =
+            uiDevice
+                .findObjectWithTimeout(By.textContains(locationName).res(GEOLOCATION_NAME_TAG))
+                .parent
+                .parent
         val expandButton = locationCell.findObjectWithTimeout(By.res(EXPAND_BUTTON_TEST_TAG))
         expandButton.click()
     }
