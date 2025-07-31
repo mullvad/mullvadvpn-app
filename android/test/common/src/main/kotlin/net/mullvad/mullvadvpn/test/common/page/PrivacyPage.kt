36ca473e3cef88a7ee3284e71d39dd16cd2b2f31
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-07-31 09:34:16 +0200

    Fix tests failing when the notification allowed dialog is not shown

--- android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/PrivacyPage.kt
+++ android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/PrivacyPage.kt
@@ -1,9 +1,6 @@
 package net.mullvad.mullvadvpn.test.common.page
 
-import android.os.Build
 import androidx.test.uiautomator.By
-import androidx.test.uiautomator.Until
-import net.mullvad.mullvadvpn.test.common.constant.DEFAULT_TIMEOUT
 import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
 
 class PrivacyPage internal constructor() : Page() {
@@ -17,24 +14,5 @@ class PrivacyPage internal constructor() : Page() {
 
     fun clickAgreeOnPrivacyDisclaimer() {
         uiDevice.findObjectWithTimeout(agreeSelector).click()
-    }
-
-    fun clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove(
-        timeout: Long = DEFAULT_TIMEOUT
-    ) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-            // Skipping as notification permissions are not shown.
-            return
-        }
-
-        uiDevice.wait(Until.hasObject(allowSelector), timeout)
-
-        try {
-            uiDevice.findObjectWithTimeout(allowSelector).click()
-        } catch (e: IllegalArgumentException) {
-            throw IllegalArgumentException(
-                "Failed to allow notification permission within timeout ($timeout)"
-            )
-        }
     }
 }
