cc3e7acc15d948e37c5105c03c3c24ad488c1f05
Kalle Lindström <karl.lindstrom@mullvad.net>
2025-08-04 16:08:14 +0200

    Use goAsync() api and remove TimeChangedReceiver

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/UserPreferencesRepository.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/UserPreferencesRepository.kt
@@ -1,13 +1,10 @@
 package net.mullvad.mullvadvpn.repository
 
 import androidx.datastore.core.DataStore
-import co.touchlab.kermit.Logger
-import java.io.IOException
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import net.mullvad.mullvadvpn.lib.model.BuildVersion
 
@@ -15,21 +12,9 @@ class UserPreferencesRepository(
     private val userPreferencesStore: DataStore<UserPreferences>,
     private val buildVersion: BuildVersion,
 ) {
+    fun preferencesFlow(): Flow<UserPreferences> = userPreferencesStore.data
 
-    // Note: this should not be made into a StateFlow. See:
-    // https://developer.android.com/reference/kotlin/androidx/datastore/core/DataStore#data()
-    val preferencesFlow: Flow<UserPreferences> =
-        userPreferencesStore.data.catch { exception ->
-            // dataStore.data throws an IOException when an error is encountered when reading data
-            if (exception is IOException) {
-                Logger.e("Error reading user preferences file, falling back to default.", exception)
-                emit(UserPreferences.getDefaultInstance())
-            } else {
-                throw exception
-            }
-        }
-
-    suspend fun preferences(): UserPreferences = preferencesFlow.first()
+    suspend fun preferences(): UserPreferences = userPreferencesStore.data.first()
 
     suspend fun setPrivacyDisclosureAccepted() {
         userPreferencesStore.updateData { prefs ->
