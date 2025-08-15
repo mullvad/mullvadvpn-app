8b7e2dba790e90a0ac885adfeded15eb97fb99f0
Markus Pettersson <markus.pettersson@mullvad.net>
2025-08-15 09:51:40 +0200

    Use objc2_service_management to get rid of a lot of unsafe

--- mullvad-daemon/src/macos_launch_daemon.rs
+++ mullvad-daemon/src/macos_launch_daemon.rs
@@ -5,19 +5,13 @@
 //! must be checked so that the user can be directed to approve the launch
 //! daemon in the system settings.
 
-#![allow(clippy::undocumented_unsafe_blocks)] // Remove me if you dare.
+use objc2_foundation::{NSOperatingSystemVersion, NSProcessInfo, NSURL, ns_string};
+use objc2_service_management::{SMAppService, SMAppServiceStatus};
 
-use std::ffi::CStr;
-
-use objc2::{class, msg_send, runtime::AnyObject};
-use objc2_foundation::{NSOperatingSystemVersion, NSProcessInfo};
-
-type Id = *mut AnyObject;
-
-// TODO: Replace with obcj2-service-management
-// Framework that contains `SMAppService`.
-#[link(name = "ServiceManagement", kind = "framework")]
-unsafe extern "C" {}
+/// Path to the plist that defines the Mullvad launch daemon.
+/// It must be kept in sync with the path defined in
+/// `dist-assets/pkg-scripts/postinstall`.
+const DAEMON_PLIST_PATH: &str = "/Library/LaunchDaemons/net.mullvad.daemon.plist";
 
 /// Authorization status of the Mullvad daemon.
 #[repr(i32)]
@@ -36,20 +30,31 @@ pub fn get_status() -> LaunchDaemonStatus {
     if os_version.majorVersion < 13 {
         return LaunchDaemonStatus::Ok;
     }
-    get_status_for_url(&daemon_plist_url())
+    // SAFETY: daemon_plist_path is not an empty path & it is a valid system path.
+    // https://developer.apple.com/documentation/foundation/nsurl/fileurl(withpath:)#parameters
+    let daemon_plist_url = unsafe { NSURL::fileURLWithPath(ns_string!(DAEMON_PLIST_PATH)) };
+    get_status_for_url(&daemon_plist_url)
 }
 
-fn get_status_for_url(url: &Object) -> LaunchDaemonStatus {
-    let status: libc::c_long =
-        unsafe { msg_send![class!(SMAppService), statusForLegacyURL: url.0] };
-
+fn get_status_for_url(url: &NSURL) -> LaunchDaemonStatus {
+    // SAFETY: Apple does not state *anything* regarding safety requirements of this function:
+    // https://developer.apple.com/documentation/servicemanagement/smappservice/statusforlegacyplist(at:)
+    // But using a bit of reasoning & the [guidelines of objc2](https://github.com/madsmtm/objc2/blob/master/crates/header-translator/README.md#what-is-required-for-a-method-to-be-safe):
+    // """
+    // What is required for a method to be safe?
+    // 1. The method must not take a raw pointer; one could trivially pass ptr::invalid() and cause UB with that.
+    // 2. Any extra requirements that the method states in its documentation must be upheld.
+    // """
+    // we can conclude that:
+    // (1.) is upheld by the virtue of url being a reference, since references are always valid.
+    // (2.) is trivially upheld since Apple does not state safety requirements.
+    let status = unsafe { SMAppService::statusForLegacyURL(url) };
     match status {
-        // SMAppServiceStatusNotRegistered | SMAppServiceStatusNotFound
-        0 | 3 => LaunchDaemonStatus::NotFound,
-        // SMAppServiceStatusEnabled
-        1 => LaunchDaemonStatus::Ok,
-        // SMAppServiceStatusRequiresApproval
-        2 => LaunchDaemonStatus::NotAuthorized,
+        SMAppServiceStatus::NotRegistered | SMAppServiceStatus::NotFound => {
+            LaunchDaemonStatus::NotFound
+        }
+        SMAppServiceStatus::Enabled => LaunchDaemonStatus::Ok,
+        SMAppServiceStatus::RequiresApproval => LaunchDaemonStatus::NotAuthorized,
         // Unknown status
         _ => LaunchDaemonStatus::Unknown,
     }
@@ -58,34 +63,4 @@ fn get_status_for_url(url: &Object) -> LaunchDaemonStatus {
 fn get_os_version() -> NSOperatingSystemVersion {
     let process_info = NSProcessInfo::processInfo();
     process_info.operatingSystemVersion()
-}
-
-/// Returns an `NSURL` instance for `DAEMON_PLIST_PATH`.
-fn daemon_plist_url() -> Object {
-    /// Path to the plist that defines the Mullvad launch daemon.
-    /// It must be kept in sync with the path defined in
-    /// `dist-assets/pkg-scripts/postinstall`.
-    const DAEMON_PLIST_PATH: &CStr = c"/Library/LaunchDaemons/net.mullvad.daemon.plist";
-
-    let nsstr_inst: Id = unsafe { msg_send![class!(NSString), alloc] };
-    let nsstr_inst: Id =
-        unsafe { msg_send![nsstr_inst, initWithUTF8String: DAEMON_PLIST_PATH.as_ptr()] };
-
-    let nsurl_inst: Id = unsafe { msg_send![class!(NSURL), alloc] };
-    let nsurl_inst: Id = unsafe { msg_send![nsurl_inst, initWithString: nsstr_inst] };
-
-    let _: () = unsafe { msg_send![nsstr_inst, release] };
-
-    assert!(!nsurl_inst.is_null());
-
-    Object(nsurl_inst)
-}
-
-/// Calls `[self.0 release]` when the wrapped instance is dropped.
-struct Object(Id);
-
-impl Drop for Object {
-    fn drop(&mut self) {
-        let _: () = unsafe { msg_send![self.0, release] };
-    }
 }
