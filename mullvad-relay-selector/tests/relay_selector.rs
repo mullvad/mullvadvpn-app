1d3de5aa5dfd9db0451782dfdf5828abf0d64d4c
David Lönnhager <david.l@mullvad.net>
2025-08-18 10:04:16 +0200

    fixup again

--- mullvad-relay-selector/tests/relay_selector.rs
+++ mullvad-relay-selector/tests/relay_selector.rs
@@ -32,6 +32,7 @@ use mullvad_types::{
         WireguardRelayEndpointData,
     },
 };
+use vec1::vec1;
 
 static DUMMY_LOCATION: LazyLock<Location> = LazyLock::new(|| Location {
     country: "Sweden".to_string(),
@@ -71,17 +72,14 @@ static RELAYS: LazyLock<RelayList> = LazyLock::new(|| RelayList {
                     endpoint_data: RelayEndpointData::Wireguard(
                         WireguardRelayEndpointData::new(WIREGUARD_PUBKEY.clone())
                             .set_daita(true)
-                            .set_quic(
-                                Quic::new(
-                                    vec![
-                                        "185.213.154.68".parse().unwrap(),
-                                        "2a03:1b20:5:f011::a09f".parse().unwrap(),
-                                    ],
-                                    "Bearer test".to_owned(),
-                                    "se9-wireguard.blockerad.eu".to_owned(),
-                                )
-                                .unwrap(),
-                            ),
+                            .set_quic(Quic::new(
+                                vec1![
+                                    "185.213.154.68".parse().unwrap(),
+                                    "2a03:1b20:5:f011::a09f".parse().unwrap(),
+                                ],
+                                "Bearer test".to_owned(),
+                                "se9-wireguard.blockerad.eu".to_owned(),
+                            )),
                     ),
                     location: DUMMY_LOCATION.clone(),
                 },
