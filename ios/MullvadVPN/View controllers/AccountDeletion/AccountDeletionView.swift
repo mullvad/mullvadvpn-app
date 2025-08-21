c397ede6c866a6548f62e37cf098cc25691be7e6
Andrew Bulhak <andrew.bulhak@mullvad.net>
2025-08-21 14:59:42 +0200

    Make changes from PR

--- ios/MullvadVPN/View controllers/AccountDeletion/AccountDeletionView.swift
+++ ios/MullvadVPN/View controllers/AccountDeletion/AccountDeletionView.swift
@@ -11,59 +11,63 @@ import SwiftUI
 struct AccountDeletionView: View {
     @ObservedObject var viewModel: AccountDeletionViewModel
 
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text(NSLocalizedString("Account deletion", comment: ""))
-                .font(.mullvadLarge)
-                .foregroundStyle(Color.white)
-                .padding(.init(top: 0, leading: 0, bottom: 8, trailing: 0))
+    @ScaledMetric var spinnerSize = 20.0
+    @ScaledMetric var spinnerStatusGap = 10.0
 
-            Text(viewModel.messageText)
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Account deletion")
+                    .font(.mullvadLarge)
+                    .foregroundStyle(Color.white)
+                    .padding(.bottom, 8)
+
+                Text(viewModel.messageText)
+                    .foregroundStyle(Color.white)
+                    .padding(.bottom, 8)
+
+                Text(
+                    """
+                    This logs out all devices using this account and all \
+                    VPN access will be denied even if there is time left on the account. \
+                    Enter the last 4 digits of the account number and hit "Delete account" \
+                    if you really want to delete the account:
+                    """
+                )
                 .font(.mullvadSmallSemiBold)
                 .foregroundStyle(Color.white)
-                .padding(.init(top: 0, leading: 0, bottom: 16, trailing: 0))
+                .padding(.bottom, 8)
 
-            Text(NSLocalizedString(
-                """
-                This logs out all devices using this account and all \
-                VPN access will be denied even if there is time left on the account. \
-                Enter the last 4 digits of the account number and hit "Delete account" \
-                if you really want to delete the account:
-                """,
-                comment: ""
-            ))
-            .font(.mullvadMiniSemiBold)
-            .foregroundStyle(Color.white)
-            .padding(.init(top: 0, leading: 0, bottom: 8, trailing: 0))
+                // accountTextField
+                MullvadPrimaryTextField(
+                    label: "Last 4 digits", placeholder: "XXXX", text: $viewModel.enteredAccountNumberSuffix,
+                    keyboardType: .numberPad
+                )
+                .padding(.bottom, 4)
 
-            // accountTextField
-            MullvadPrimaryTextField(
-                label: "", placeholder: "", text: $viewModel.enteredAccountNumberSuffix
-            )
-            .padding(.init(top: 0, leading: 0, bottom: 4, trailing: 0))
+                // Status information
+                HStack {
+                    if viewModel.isWorking {
+                        ProgressView()
+                            .progressViewStyle(MullvadProgressViewStyle(size: spinnerSize))
+                        Spacer().frame(width: spinnerStatusGap)
+                    }
 
-            // Status information
-            HStack {
-                if viewModel.isWorking {
-                    ActivityIndicator()
-                        .frame(width: 30, height: 20)
+                    Text(viewModel.statusText)
+                        .font(.mullvadSmall)
                         .foregroundStyle(Color.white)
                 }
 
-                Text(viewModel.statusText)
-                    .font(Font(UIFont.preferredFont(forTextStyle: .body)))
-                    .foregroundStyle(Color.white)
-            }
+                Spacer()
 
-            Spacer()
+                MainButton(text: "Delete Account", style: .danger) {
+                    viewModel.deleteButtonTapped()
+                }
+                .disabled(!viewModel.canDelete)
 
-            MainButton(text: "Delete Account", style: .danger) {
-                viewModel.deleteButtonTapped()
-            }
-            .disabled(!viewModel.canDelete)
-
-            MainButton(text: "Cancel", style: .default) {
-                viewModel.cancelButtonTapped()
+                MainButton(text: "Cancel", style: .default) {
+                    viewModel.cancelButtonTapped()
+                }
             }
         }
         .padding(16)
