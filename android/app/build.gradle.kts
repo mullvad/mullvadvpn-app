62388e929b3f05bbc5cf6c6d2ed29cd60f6be229
Kalle Lindström <karl.lindstrom@mullvad.net>
2025-08-22 15:05:02 +0200

    Add inital baseline profile generation
    To fix reproducibility issues and to improve startup performance this
    PR adds a baseline profile generation module in test/baselineprofile.
    
    The baseline profile plugin requires Junit4 so that is also added as a
    dependency.
    
    A baseline-prof.txt was also generated by running
    `./gradlew generatePlayProdReleaseBaselineProfile` and checked in.
    
    The tests that generate the baselineprofile currently only start the app
    and do not yet log in and navigate around the UI. This should be
    improved later on to improve the startup performance.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ plugins {
     alias(libs.plugins.compose)
     alias(libs.plugins.protobuf.core)
     alias(libs.plugins.junit5.android)
+    alias(libs.plugins.baselineprofile)
     id("me.sigptr.rust-android")
 }
 
@@ -299,15 +300,32 @@ if (getBooleanProperty("mullvad.app.build.cargo.cleanBuild")) {
     tasks["clean"].dependsOn("cargoClean")
 }
 
+baselineProfile { warnings { disabledVariants = false } }
+
 androidComponents {
     beforeVariants { variantBuilder ->
         variantBuilder.enable =
             variantBuilder.let { currentVariant ->
-                val enabledVariants =
-                    enabledAppVariantTriples.map { (billing, infra, buildType) ->
-                        billing + infra.capitalized() + buildType.capitalized()
-                    }
-                enabledVariants.contains(currentVariant.name)
+                val buildType = currentVariant.buildType ?: ""
+
+                val billing =
+                    currentVariant.productFlavors
+                        .find { it.first == FlavorDimensions.BILLING }
+                        ?.second
+
+                val infra =
+                    currentVariant.productFlavors
+                        .find { it.first == FlavorDimensions.INFRASTRUCTURE }
+                        ?.second
+
+                enabledAppVariantTriples.any { (enabledBilling, enabledInfra, enabledBuildType) ->
+                    billing == enabledBilling &&
+                        infra == enabledInfra &&
+                        // For e.g. the baseline profile generation build types with names like
+                        // "benchmarkRelease" and "nonMinifiedRelease" are created and must not be
+                        // filtered out.
+                        buildType.contains(enabledBuildType, ignoreCase = true)
+                }
             }
     }
 }
@@ -362,6 +380,8 @@ dependencies {
     implementation(projects.tile)
     implementation(projects.lib.theme)
     implementation(projects.service)
+    implementation(libs.androidx.profileinstaller)
+    "baselineProfile"(project(":test:baselineprofile"))
 
     // Play implementation
     playImplementation(projects.lib.billing)
