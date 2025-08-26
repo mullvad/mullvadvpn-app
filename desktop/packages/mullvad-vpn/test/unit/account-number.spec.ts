ecbd673da7a1fb17873ce032a2cf772189f11efd
Oskar <oskar@mullvad.net>
2025-08-26 16:37:52 +0200

    Add isAccountNumber utility function

---
+++ desktop/packages/mullvad-vpn/test/unit/account-number.spec.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+
+import { isAccountNumber } from '../../src/shared/utils';
+
+describe('IAccountData cache', () => {
+  it('Should identify valid account numbers', () => {
+    // Allowed account numbers can range between 10 and 16 digits
+    expect(isAccountNumber('1234567890123456')).to.be.true;
+    expect(isAccountNumber('123456789012')).to.be.true;
+    expect(isAccountNumber('1234567890')).to.be.true;
+  });
+
+  it('Should reject too long account number', () => {
+    expect(isAccountNumber('12345678901234567')).to.be.false;
+  });
+
+  it('Should reject non-digit account numbers', () => {
+    expect(isAccountNumber('123456789012345a')).to.be.false;
+    expect(isAccountNumber('12345678901e')).to.be.false;
+    expect(isAccountNumber('123456789a')).to.be.false;
+  });
+
+  it('Should reject too short account number', () => {
+    expect(isAccountNumber('123456789')).to.be.false;
+  });
+});
