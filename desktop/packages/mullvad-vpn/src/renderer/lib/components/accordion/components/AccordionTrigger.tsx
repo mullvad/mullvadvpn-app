d239a8966bf803075df1358eecc8eee7dd94cc1c
Oliver <oliver@mohlin.dev>
2025-08-27 10:29:02 +0200

    Update Accordion component to support animations and disabled state

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionTrigger.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionTrigger.tsx
@@ -4,18 +4,25 @@ import styled from 'styled-components';
 import { colors } from '../../../foundations';
 import { useAccordionContext } from '../AccordionContext';
 import { StyledAccordionHeader } from './AccordionHeader';
+import { StyledAccordionIcon } from './AccordionIcon';
 
 export type AccordionTriggerProps = {
   children?: React.ReactNode;
-};
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const StyledAccordionTrigger = styled.button`
   background-color: transparent;
   &&:hover > ${StyledAccordionHeader} {
     background-color: ${colors.blue60};
   }
+  &&:hover > ${StyledAccordionIcon} {
+    background-color: ${colors.whiteAlpha60};
+  }
   &&:active > ${StyledAccordionHeader} {
     background-color: ${colors.blue40};
+  }
+  &&:active > ${StyledAccordionIcon} {
+    background-color: ${colors.whiteAlpha40};
   }
   &&:focus-visible {
     outline: 2px solid ${colors.white};
@@ -24,7 +31,7 @@ const StyledAccordionTrigger = styled.button`
 `;
 
 export function AccordionTrigger({ children }: AccordionTriggerProps) {
-  const { contentId, triggerId, expanded, onExpandedChange } = useAccordionContext();
+  const { contentId, triggerId, titleId, expanded, onExpandedChange } = useAccordionContext();
 
   const onClick = React.useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -37,6 +44,7 @@ export function AccordionTrigger({ children }: AccordionTriggerProps) {
   return (
     <StyledAccordionTrigger
       id={triggerId}
+      aria-labelledby={titleId}
       aria-controls={contentId}
       aria-expanded={expanded ? 'true' : 'false'}
       onClick={onClick}>
