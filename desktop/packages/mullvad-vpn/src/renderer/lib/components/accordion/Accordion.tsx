c52e39f1c38816667fd0949cf57354fb9a358761
Oliver <oliver@mohlin.dev>
2025-08-27 10:29:02 +0200

    Update Accordion component to support animations and disabled state

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/Accordion.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/Accordion.tsx
@@ -1,35 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { FlexColumn } from '../flex-column';
 import { AccordionProvider } from './AccordionContext';
 import { AccordionHeader, AccordionTrigger } from './components';
 import { AccordionContent } from './components/AccordionContent';
 import { AccordionIcon } from './components/AccordionIcon';
 import { AccordionTitle } from './components/AccordionTitle';
 
+export type AccordionAnimation = 'flash' | 'dim';
+
 export type AccordionProps = {
   expanded?: boolean;
   onExpandedChange?: (open: boolean) => void;
+  disabled?: boolean;
+  animation?: AccordionAnimation;
   children?: React.ReactNode;
-};
+} & React.ComponentPropsWithRef<'div'>;
 
-const StyledAccordion = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
+const StyledAccordion = styled(FlexColumn)`
   width: 100%;
 `;
 
-function Accordion({ expanded = false, onExpandedChange: onOpenChange, children }: AccordionProps) {
+function Accordion({
+  expanded = false,
+  onExpandedChange: onOpenChange,
+  disabled,
+  animation,
+  children,
+  ...props
+}: AccordionProps) {
   const triggerId = React.useId();
   const contentId = React.useId();
+  const titleId = React.useId();
   return (
     <AccordionProvider
       triggerId={triggerId}
       contentId={contentId}
+      titleId={titleId}
       expanded={expanded}
-      onExpandedChange={onOpenChange}>
-      <StyledAccordion>{children}</StyledAccordion>
+      onExpandedChange={onOpenChange}
+      disabled={disabled}
+      animation={animation}>
+      <StyledAccordion $flex={1} {...props}>
+        {children}
+      </StyledAccordion>
     </AccordionProvider>
   );
 }
