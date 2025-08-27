3575461a82638a022b4941ea0f2c187d6dd9890c
Oliver <oliver@mohlin.dev>
2025-08-27 10:29:02 +0200

    Update Accordion component to support animations and disabled state

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionTitle.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionTitle.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { Text } from '../../typography';
+import { useAccordionContext } from '../AccordionContext';
 
 export type AccordionTitleProps = {
   children?: React.ReactNode;
@@ -9,8 +10,13 @@ export type AccordionTitleProps = {
 export const StyledTitleLabel = styled(Text)``;
 
 export function AccordionTitle({ children }: AccordionTitleProps) {
+  const { titleId, disabled } = useAccordionContext();
   return (
-    <StyledTitleLabel $padding="medium" color="white" variant="titleMedium">
+    <StyledTitleLabel
+      id={titleId}
+      $padding="medium"
+      color={disabled ? 'whiteAlpha60' : 'white'}
+      variant="titleMedium">
       {children}
     </StyledTitleLabel>
   );
