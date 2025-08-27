e41fb611d28c164e9ef8431596acd655d1205358
Oliver <oliver@mohlin.dev>
2025-08-27 10:29:02 +0200

    Update Accordion component to support animations and disabled state

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionHeader.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionHeader.tsx
@@ -1,29 +1,42 @@
-import styled from 'styled-components';
+import styled, { css, RuleSet } from 'styled-components';
 
 import { colors } from '../../../foundations';
 import { Flex } from '../../flex';
-import { StyledAccordionIcon } from './AccordionIcon';
+import { useAccordionContext } from '../AccordionContext';
+import { useAnimation } from '../hooks';
 
 export type AccordionHeaderProps = {
   children?: React.ReactNode;
 };
 
-export const StyledAccordionHeader = styled(Flex)`
-  background-color: ${colors.blue};
-  width: 100%;
-  min-height: 48px;
-  margin-bottom: 1px;
+export const StyledAccordionHeader = styled(Flex)<{
+  $animation?: RuleSet<object>;
+  $disabled?: boolean;
+}>`
+  ${({ $animation, $disabled }) => {
+    const backgroundColor = $disabled ? colors.blue40 : colors.blue;
+    return css`
+      --background-color: ${backgroundColor};
 
-  && > ${StyledAccordionIcon} {
-    margin-left: auto;
-  }
+      margin-bottom: 1px;
+      background-color: var(--background-color);
+      min-height: 48px;
+      width: 100%;
+      ${$animation}
+    `;
+  }}
 `;
 
 export function AccordionHeader({ children }: AccordionHeaderProps) {
+  const animation = useAnimation();
+  const { disabled } = useAccordionContext();
   return (
     <StyledAccordionHeader
       $padding={{ horizontal: 'medium', vertical: 'small' }}
-      $alignItems="center">
+      $alignItems="center"
+      $justifyContent="space-between"
+      $animation={animation}
+      $disabled={disabled}>
       {children}
     </StyledAccordionHeader>
   );
