73b4e57d029f6a5361072655e7dc45ceefb12ce4
Oliver <oliver@mohlin.dev>
2025-08-28 06:51:22 +0200

    Add DefaultListboxOption component

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/default-listbox-option/DefaultListboxOption.tsx
@@ -0,0 +1,21 @@
+import { ListboxOptionProps } from '../../lib/components/listbox/components';
+import { Listbox } from '../../lib/components/listbox/Listbox';
+
+export type DefaultListboxOptionProps<T> = ListboxOptionProps<T>;
+
+export function DefaultListboxOption<T>({ children, ...props }: DefaultListboxOptionProps<T>) {
+  return (
+    <Listbox.Option level={1} {...props}>
+      <Listbox.Option.Trigger>
+        <Listbox.Option.Item>
+          <Listbox.Option.Content>
+            <Listbox.Option.Group>
+              <Listbox.Option.Icon icon="checkmark" aria-hidden="true" />
+              <Listbox.Option.Label>{children}</Listbox.Option.Label>
+            </Listbox.Option.Group>
+          </Listbox.Option.Content>
+        </Listbox.Option.Item>
+      </Listbox.Option.Trigger>
+    </Listbox.Option>
+  );
+}
