7f1ec747522939784f72188ad43b83507a090dcd
Oskar <oskar@mullvad.net>
2025-08-28 16:55:22 +0200

    Add tests of warnings when clearing account history

---
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/login/login-route-object-model.ts
@@ -0,0 +1,57 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class LoginRouteObjectModel {
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(
+    // @ts-expect-error page is currently unused.
+    private readonly page: Page,
+    private readonly utils: TestUtils,
+  ) {
+    this.selectors = createSelectors(page);
+  }
+
+  async waitForRoute() {
+    await this.utils.waitForRoute(RoutePath.login);
+  }
+
+  async createNewAccount() {
+    await this.selectors.createNewAccountButton().click();
+  }
+
+  getCreateNewAccountConfirmationMessage() {
+    return this.selectors.createNewAccountMessage();
+  }
+
+  async confirmCreateNewAccount() {
+    await this.selectors.confirmCreateNewAccountButton().click();
+  }
+
+  async cancelCreateNewAccount() {
+    await this.selectors.cancelDialogButton().click();
+  }
+
+  async clearAccountHistory() {
+    await this.selectors.clearAccountHistory().click();
+  }
+
+  getAccountHistoryItemButton() {
+    return this.selectors.accountHistoryItemButton();
+  }
+
+  getClearAccountHistoryConfirmationMessage() {
+    return this.selectors.clearAccountHistoryMessage();
+  }
+
+  async confirmClearAccountHistory() {
+    await this.selectors.confirmClearAccountHistoryButton().click();
+  }
+
+  async cancelClearAccountHistory() {
+    await this.selectors.cancelDialogButton().click();
+  }
+}
---
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/login/selectors.ts
@@ -0,0 +1,15 @@
+import { Page } from 'playwright';
+
+export const createSelectors = (page: Page) => ({
+  createNewAccountButton: () => page.getByRole('button', { name: 'Create account' }),
+  createNewAccountMessage: () => page.getByText('Do you want to create a new account?'),
+  confirmCreateNewAccountButton: () => page.getByRole('button', { name: 'Create new account' }),
+
+  accountHistoryItemButton: () => page.getByRole('button', { name: 'Login with account number' }),
+  clearAccountHistory: () => page.getByRole('button', { name: 'Forget account number' }),
+  clearAccountHistoryMessage: () =>
+    page.getByText('Do you want to remove the saved account number?'),
+  confirmClearAccountHistoryButton: () => page.getByRole('button', { name: 'Remove' }),
+
+  cancelDialogButton: () => page.getByRole('button', { name: 'Cancel' }),
+});
