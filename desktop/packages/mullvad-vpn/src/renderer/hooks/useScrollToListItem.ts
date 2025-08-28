49648f534772799f2e994cd939fc7f1ee94d816b
Oliver <oliver@mohlin.dev>
2025-08-28 06:52:36 +0200

    Add scroll to anchor and list item hooks

---
+++ desktop/packages/mullvad-vpn/src/renderer/hooks/useScrollToListItem.ts
@@ -0,0 +1,25 @@
+import { ListItemAnimation } from '../lib/components/list-item';
+import { useHistory } from '../lib/history';
+import { useScrollToReference } from '.';
+
+export const useScrollToListItem = (
+  ref?: React.RefObject<HTMLDivElement | null>,
+  id?: string,
+):
+  | {
+      animation: ListItemAnimation;
+    }
+  | undefined => {
+  const { location, action } = useHistory();
+  const { state } = location;
+
+  const isPop = action === 'POP';
+  const anchorId = state?.options?.find((option) => option.type === 'scroll-to-anchor')?.id;
+  const scroll = id === anchorId && !isPop;
+  useScrollToReference(ref, scroll);
+
+  if (anchorId === undefined || isPop) return undefined;
+  return {
+    animation: scroll ? 'flash' : 'dim',
+  };
+};
