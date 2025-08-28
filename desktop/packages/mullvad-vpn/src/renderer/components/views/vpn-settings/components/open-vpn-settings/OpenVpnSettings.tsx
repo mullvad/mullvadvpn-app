484018267328313eac9ef37451acb626f28911a3
Oliver <oliver@mohlin.dev>
2025-08-28 07:45:23 +0200

    Link feature indicators to settings in vpn settings

--- desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/open-vpn-settings/OpenVpnSettings.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/open-vpn-settings/OpenVpnSettings.tsx
@@ -3,14 +3,19 @@ import { sprintf } from 'sprintf-js';
 import { strings } from '../../../../../../shared/constants';
 import { messages } from '../../../../../../shared/gettext';
 import { RoutePath } from '../../../../../../shared/routes';
+import { useScrollToListItem } from '../../../../../hooks';
 import { useTunnelProtocol } from '../../../../../lib/relay-settings-hooks';
 import { NavigationListItem } from '../../../../NavigationListItem';
 
 export function OpenVpnSettings() {
   const tunnelProtocol = useTunnelProtocol();
+  const scrollToAnchor = useScrollToListItem();
 
   return (
-    <NavigationListItem to={RoutePath.openVpnSettings} disabled={tunnelProtocol === 'wireguard'}>
+    <NavigationListItem
+      to={RoutePath.openVpnSettings}
+      disabled={tunnelProtocol === 'wireguard'}
+      animation={scrollToAnchor?.animation}>
       <NavigationListItem.Label>
         {sprintf(
           // TRANSLATORS: %(openvpn)s will be replaced with the string "OpenVPN"
