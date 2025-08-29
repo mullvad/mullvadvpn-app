16397e22a774431c19f962ce3412a77f7dce8348
Markus Pettersson <markus.pettersson@mullvad.net>
2025-08-29 18:39:08 +0200

    Add script for building the app for OpenWRT
    Static linking using musl + boringtun instead of wireguard-go.

---
+++ openwrt/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Script for building a statically linked Mullvad VPN app.
+#
+# # Prerequisites
+# - zig (https://ziglang.org/download/)
+# - cargo-zigbuild (https://github.com/rust-cross/cargo-zigbuild)
+
+set -eux
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export LIBMNL_LIB_DIR="$SCRIPT_DIR/../dist-assets/binaries/x86_64-unknown-linux-musl"
+export LIBNFTNL_LIB_DIR="$SCRIPT_DIR/../dist-assets/binaries/x86_64-unknown-linux-musl"
+
+# Install the appropriate rust target(s)
+rustup target add x86_64-unknown-linux-musl
+
+# Build the app fully statically linked! :-)
+RUSTFLAGS="-C target-feature=+crt-static" cargo zigbuild --target x86_64-unknown-linux-musl --features boringtun "$@"
