ac4eab56ea8899b6ae14129ba58930eff009a674
Oliver <oliver@mohlin.dev>
2025-09-02 06:26:59 +0200

    Add TextField component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/text-field/components/text-field-input/TextFieldInput.tsx
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components';
+
+import { colors, Radius, spacings } from '../../../../foundations';
+import { useTextFieldContext } from '../text-field-context/TextFieldContext';
+
+export type TextFieldInputProps = React.ComponentPropsWithRef<'input'>;
+
+export const StyledTextField = styled.input<{ $disabled?: boolean; $invalid?: boolean }>`
+  ${({ $invalid, $disabled }) => {
+    // TODO: Add color to tokens
+    const borderColor = $invalid ? 'rgba(235, 93, 64, 1)' : colors.chalkAlpha40;
+    const backgroundColor = $disabled ? colors.whiteOnDarkBlue5 : colors.blue40;
+    const color = $disabled ? colors.whiteAlpha20 : colors.white;
+    return css`
+      all: unset;
+      font-family: var(--font-family-open-sans);
+      background-color: ${backgroundColor};
+      padding: ${spacings.small};
+      border: 1px solid ${colors.whiteAlpha60};
+      border-color: ${borderColor};
+      font-size: 14px;
+      border-radius: ${Radius.radius4};
+      color: ${color};
+      width: 100%;
+
+      &&::placeholder {
+        color: ${colors.whiteAlpha60};
+      }
+
+      &&:not(:disabled):not([aria-invalid='true']):hover {
+        border-color: ${colors.chalkAlpha80};
+      }
+      &&:not(:disabled):not([aria-invalid='true']):focus-visible {
+        border-color: ${colors.chalk};
+      }
+    `;
+  }}
+`;
+
+export function TextFieldInput(props: TextFieldInputProps) {
+  const { disabled, invalid } = useTextFieldContext();
+
+  return (
+    <StyledTextField
+      type="text"
+      $disabled={disabled}
+      disabled={disabled}
+      $invalid={invalid}
+      aria-invalid={invalid}
+      {...props}
+    />
+  );
+}
