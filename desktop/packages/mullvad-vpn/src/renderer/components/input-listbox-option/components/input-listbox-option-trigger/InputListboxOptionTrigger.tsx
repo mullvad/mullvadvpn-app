f18e9efcd56dd9d26dc5ab55efb28ddb321130ca
Oliver <oliver@mohlin.dev>
2025-09-02 06:47:17 +0200

    Add InputListboxOption component

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/input-listbox-option/components/input-listbox-option-trigger/InputListboxOptionTrigger.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { useListboxContext } from '../../../../lib/components/listbox/components';
+import { ListboxOptionTriggerProps } from '../../../../lib/components/listbox/components/listbox-option/components';
+import { useListboxOptionContext } from '../../../../lib/components/listbox/components/listbox-option/components/listbox-option-context/ListboxOptionContext';
+import { StyledListItemOptionItem } from '../../../../lib/components/listbox/components/listbox-option/components/listbox-option-item/ListboxOptionItem';
+import { colors } from '../../../../lib/foundations';
+import { useInputListboxOption } from '../input-listbox-option-context';
+
+export type InputListboxOptionTriggerProps = ListboxOptionTriggerProps;
+
+export const StyledListItemOptionTrigger = styled.li`
+  &&[aria-selected='true'] {
+    &:hover {
+      ${StyledListItemOptionItem} {
+        background-color: ${colors.green};
+      }
+    }
+    &:active {
+      ${StyledListItemOptionItem} {
+        background-color: ${colors.green};
+      }
+    }
+  }
+`;
+
+export const InputListboxOptionTrigger = ({
+  children,
+  ...props
+}: InputListboxOptionTriggerProps) => {
+  const { value } = useListboxOptionContext();
+  const { inputRef } = useInputListboxOption();
+
+  const { value: selectedValue, focusedValue, setFocusedValue } = useListboxContext();
+  const selected = value === selectedValue;
+  const focused = value === focusedValue;
+
+  React.useEffect(() => {
+    if (focused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [value, focused, inputRef]);
+
+  const handleFocus = React.useCallback(() => {
+    if (focused) return;
+    setFocusedValue(value);
+    inputRef.current?.focus();
+  }, [focused, inputRef, setFocusedValue, value]);
+
+  return (
+    <StyledListItemOptionTrigger
+      role="option"
+      aria-selected={selected}
+      tabIndex={selected && focusedValue === undefined ? 0 : -1}
+      onFocus={handleFocus}
+      {...props}>
+      {children}
+    </StyledListItemOptionTrigger>
+  );
+};
