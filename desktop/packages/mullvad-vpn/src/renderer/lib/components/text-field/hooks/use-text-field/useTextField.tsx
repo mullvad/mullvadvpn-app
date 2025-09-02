7cc5dbc66f484623f1949e6603908e4e241825f0
Oliver <oliver@mohlin.dev>
2025-09-02 06:26:59 +0200

    Add TextField component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/text-field/hooks/use-text-field/useTextField.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export type UseTextFieldOptions = {
+  inputRef: React.RefObject<HTMLInputElement | null>;
+  defaultValue?: string;
+  validate?: (value: string) => boolean;
+  format?: (value: string) => string;
+};
+
+export type UseTextFieldReturn = {
+  value: string;
+  invalid: boolean;
+  dirty: boolean;
+  reset: () => void;
+  focus: () => void;
+  blur: () => void;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  inputRef: React.RefObject<HTMLInputElement | null>;
+};
+
+export function useTextField({
+  inputRef,
+  defaultValue,
+  format,
+  validate,
+}: UseTextFieldOptions): UseTextFieldReturn {
+  const [value, setValue] = React.useState(defaultValue ?? '');
+  const [invalid, setInvalid] = React.useState(validate ? !validate(value) : false);
+  const [dirty, setDirty] = React.useState(false);
+
+  const reset = React.useCallback(() => {
+    setValue(defaultValue ?? '');
+    setInvalid(false);
+    setDirty(false);
+  }, [defaultValue]);
+
+  const focus = React.useCallback(() => {
+    inputRef.current?.focus();
+  }, [inputRef]);
+
+  const blur = React.useCallback(() => {
+    inputRef.current?.blur();
+  }, [inputRef]);
+
+  const handleChange = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = event.target.value;
+      const formattedValue = format ? format(newValue) : newValue;
+      const invalid = validate ? !validate(formattedValue) : false;
+      setInvalid(invalid);
+      setValue(formattedValue);
+      setDirty(true);
+    },
+
+    [format, validate],
+  );
+
+  return { value, invalid, dirty, reset, blur, focus, handleChange, inputRef };
+}
