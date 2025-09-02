74006550599a418e306b3906f1db051a62506428
Oliver <oliver@mohlin.dev>
2025-09-02 06:26:59 +0200

    Add TextField component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/text-field/TextField.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { FlexColumn } from '../flex-column';
+import { TextFieldInput, TextFieldLabel, TextFieldProvider } from './components';
+
+export type TextFieldProps = React.PropsWithChildren<{
+  invalid?: boolean;
+  value?: string;
+  disabled?: boolean;
+}>;
+
+function TextField({ children, ...props }: TextFieldProps) {
+  const labelId = React.useId();
+  return (
+    <TextFieldProvider labelId={labelId} {...props}>
+      <FlexColumn $gap="tiny">{children}</FlexColumn>
+    </TextFieldProvider>
+  );
+}
+
+const TextFieldNamespace = Object.assign(TextField, {
+  Input: TextFieldInput,
+  Label: TextFieldLabel,
+});
+
+export { TextFieldNamespace as TextField };
