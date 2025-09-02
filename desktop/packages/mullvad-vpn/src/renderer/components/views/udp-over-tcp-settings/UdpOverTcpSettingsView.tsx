9f083dbeb5306c4c98750f1451929d1c7df48e63
Oliver <oliver@mohlin.dev>
2025-09-02 08:29:11 +0200

    Move udp over tcp settings components to separate folders

--- desktop/packages/mullvad-vpn/src/renderer/components/views/udp-over-tcp-settings/UdpOverTcpSettingsView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/udp-over-tcp-settings/UdpOverTcpSettingsView.tsx
@@ -1,29 +1,20 @@
-import { useCallback, useMemo } from 'react';
 import styled from 'styled-components';
 
-import {
-  liftConstraint,
-  LiftedConstraint,
-  wrapConstraint,
-} from '../../../../shared/daemon-rpc-types';
 import { messages } from '../../../../shared/gettext';
-import { useAppContext } from '../../../context';
 import { useHistory } from '../../../lib/history';
-import { useSelector } from '../../../redux/store';
 import { AppNavigationHeader } from '../..';
-import { AriaInputGroup } from '../../AriaGroup';
 import * as Cell from '../../cell';
-import Selector, { SelectorItem } from '../../cell/Selector';
+import { SelectorItem } from '../../cell/Selector';
 import { BackAction } from '../../KeyboardNavigation';
 import { Layout, SettingsContainer } from '../../Layout';
-import { ModalMessage } from '../../Modal';
 import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
 import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
+import { Udp2tcpPortSetting } from './components';
 
-const UDP2TCP_PORTS = [80, 5001];
+export const UDP2TCP_PORTS = [80, 5001];
 
-function mapPortToSelectorItem(value: number): SelectorItem<number> {
+export function mapPortToSelectorItem(value: number): SelectorItem<number> {
   return { label: value.toString(), value };
 }
 
@@ -34,7 +25,7 @@ const StyledContent = styled.div({
   marginBottom: '2px',
 });
 
-const StyledSelectorContainer = styled.div({
+export const StyledSelectorContainer = styled.div({
   flex: 0,
 });
 
@@ -70,53 +61,5 @@ export function UdpOverTcpSettingsView() {
         </SettingsContainer>
       </Layout>
     </BackAction>
-  );
-}
-
-function Udp2tcpPortSetting() {
-  const { setObfuscationSettings } = useAppContext();
-  const obfuscationSettings = useSelector((state) => state.settings.obfuscationSettings);
-
-  const port = liftConstraint(obfuscationSettings.udp2tcpSettings.port);
-  const portItems: SelectorItem<number>[] = useMemo(
-    () => UDP2TCP_PORTS.map(mapPortToSelectorItem),
-    [],
-  );
-
-  const selectPort = useCallback(
-    async (port: LiftedConstraint<number>) => {
-      await setObfuscationSettings({
-        ...obfuscationSettings,
-        udp2tcpSettings: {
-          ...obfuscationSettings.udp2tcpSettings,
-          port: wrapConstraint(port),
-        },
-      });
-    },
-    [setObfuscationSettings, obfuscationSettings],
-  );
-
-  return (
-    <AriaInputGroup>
-      <StyledSelectorContainer>
-        <Selector
-          // TRANSLATORS: The title for the UDP-over-TCP port selector.
-          title={messages.pgettext('wireguard-settings-view', 'UDP-over-TCP port')}
-          details={
-            <ModalMessage>
-              {messages.pgettext(
-                'wireguard-settings-view',
-                'Which TCP port the UDP-over-TCP obfuscation protocol should connect to on the VPN server.',
-              )}
-            </ModalMessage>
-          }
-          items={portItems}
-          value={port}
-          onSelect={selectPort}
-          thinTitle
-          automaticValue={'any' as const}
-        />
-      </StyledSelectorContainer>
-    </AriaInputGroup>
   );
 }
