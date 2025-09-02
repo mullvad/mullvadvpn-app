8c7c03ee6199f6df919723559232a7b421fafc89
Oliver <oliver@mohlin.dev>
2025-09-02 11:51:54 +0200

    Update DefaultListboxOption to send rest props to trigger

--- desktop/packages/mullvad-vpn/src/renderer/components/default-listbox-option/DefaultListboxOption.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/default-listbox-option/DefaultListboxOption.tsx
@@ -1,12 +1,20 @@
 import { ListboxOptionProps } from '../../lib/components/listbox/components';
+import { ListboxOptionTriggerProps } from '../../lib/components/listbox/components/listbox-option/components';
 import { Listbox } from '../../lib/components/listbox/Listbox';
 
-export type DefaultListboxOptionProps<T> = ListboxOptionProps<T>;
+export type DefaultListboxOptionProps<T> = ListboxOptionProps<T> &
+  Omit<ListboxOptionTriggerProps, keyof ListboxOptionProps<T>>;
 
-export function DefaultListboxOption<T>({ children, ...props }: DefaultListboxOptionProps<T>) {
+export function DefaultListboxOption<T>({
+  value,
+  animation,
+  disabled,
+  children,
+  ...props
+}: DefaultListboxOptionProps<T>) {
   return (
-    <Listbox.Option level={1} {...props}>
-      <Listbox.Option.Trigger>
+    <Listbox.Option level={1} value={value} animation={animation} disabled={disabled}>
+      <Listbox.Option.Trigger {...props}>
         <Listbox.Option.Item>
           <Listbox.Option.Content>
             <Listbox.Option.Group>
