9329ca42ee9c9ffcce948903ea89926ca81c2502
Oliver <oliver@mohlin.dev>
2025-09-03 14:40:21 +0200

    Use ListItem components in Accordion

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionTitle.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionTitle.tsx
@@ -1,23 +1,10 @@
-import styled from 'styled-components';
-
-import { Text } from '../../typography';
+import { ListItem } from '../../list-item';
+import { TextProps } from '../../typography';
 import { useAccordionContext } from '../AccordionContext';
 
-export type AccordionTitleProps = {
-  children?: React.ReactNode;
-};
-
-export const StyledTitleLabel = styled(Text)``;
+export type AccordionTitleProps = TextProps;
 
 export function AccordionTitle({ children }: AccordionTitleProps) {
-  const { titleId, disabled } = useAccordionContext();
-  return (
-    <StyledTitleLabel
-      id={titleId}
-      $padding="medium"
-      color={disabled ? 'whiteAlpha60' : 'white'}
-      variant="titleMedium">
-      {children}
-    </StyledTitleLabel>
-  );
+  const { titleId } = useAccordionContext();
+  return <ListItem.Label id={titleId}>{children}</ListItem.Label>;
 }
