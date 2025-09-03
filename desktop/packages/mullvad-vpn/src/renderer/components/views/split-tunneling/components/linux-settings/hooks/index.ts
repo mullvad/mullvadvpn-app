43968819ad14af435d937421cab0d617a1fdab85
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-09-03 16:35:34 +0200

    Show spinner when application list is not ready to display

--- desktop/packages/mullvad-vpn/src/renderer/components/views/split-tunneling/components/linux-settings/hooks/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/split-tunneling/components/linux-settings/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './use-filtered-applications';
 export * from './use-launch-application';
 export * from './use-show-linux-application-list';
 export * from './use-show-no-search-result';
+export * from './use-show-spinner';
