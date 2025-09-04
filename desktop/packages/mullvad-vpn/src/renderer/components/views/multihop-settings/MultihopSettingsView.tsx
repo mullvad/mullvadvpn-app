54de71ed52ae2db9a6e81152a20589bd0b7aa856
Oliver <oliver@mohlin.dev>
2025-09-04 11:19:30 +0200

    Move multihop settings to separate folders

--- desktop/packages/mullvad-vpn/src/renderer/components/views/multihop-settings/MultihopSettingsView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/multihop-settings/MultihopSettingsView.tsx
@@ -1,24 +1,16 @@
-import { useCallback } from 'react';
-import { sprintf } from 'sprintf-js';
 import styled from 'styled-components';
 
-import { strings } from '../../../../shared/constants';
 import { messages } from '../../../../shared/gettext';
-import log from '../../../../shared/logging';
-import { useScrollToListItem } from '../../../hooks';
 import { Flex } from '../../../lib/components';
-import { useRelaySettingsUpdater } from '../../../lib/constraint-updater';
 import { useHistory } from '../../../lib/history';
-import { useSelector } from '../../../redux/store';
 import { AppNavigationHeader } from '../..';
-import { AriaDescription } from '../../AriaGroup';
 import * as Cell from '../../cell';
 import { BackAction } from '../../KeyboardNavigation';
 import { Layout, SettingsContainer } from '../../Layout';
 import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
 import SettingsHeader, { HeaderSubTitle, HeaderTitle } from '../../SettingsHeader';
-import { ToggleListItem } from '../../toggle-list-item';
+import { MultihopSetting } from './components';
 
 const PATH_PREFIX = process.env.NODE_ENV === 'development' ? '../' : '';
 
@@ -63,71 +55,5 @@ export function MultihopSettingsView() {
         </SettingsContainer>
       </Layout>
     </BackAction>
-  );
-}
-
-function MultihopSetting() {
-  const relaySettings = useSelector((state) => state.settings.relaySettings);
-  const relaySettingsUpdater = useRelaySettingsUpdater();
-
-  const { ref, animation } = useScrollToListItem('multihop-setting');
-
-  const multihop = 'normal' in relaySettings ? relaySettings.normal.wireguard.useMultihop : false;
-  const unavailable =
-    'normal' in relaySettings ? relaySettings.normal.tunnelProtocol === 'openvpn' : true;
-
-  const setMultihop = useCallback(
-    async (enabled: boolean) => {
-      try {
-        await relaySettingsUpdater((settings) => {
-          settings.wireguardConstraints.useMultihop = enabled;
-          return settings;
-        });
-      } catch (e) {
-        const error = e as Error;
-        log.error('Failed to update WireGuard multihop settings', error.message);
-      }
-    },
-    [relaySettingsUpdater],
-  );
-
-  return (
-    <>
-      <ToggleListItem
-        ref={ref}
-        animation={animation}
-        disabled={unavailable}
-        checked={multihop && !unavailable}
-        onCheckedChange={setMultihop}>
-        <ToggleListItem.Label>{messages.gettext('Enable')}</ToggleListItem.Label>
-        <ToggleListItem.Switch />
-      </ToggleListItem>
-      {unavailable ? (
-        <Cell.CellFooter>
-          <AriaDescription>
-            <Cell.CellFooterText>{featureUnavailableMessage()}</Cell.CellFooterText>
-          </AriaDescription>
-        </Cell.CellFooter>
-      ) : null}
-    </>
-  );
-}
-
-function featureUnavailableMessage() {
-  const tunnelProtocol = messages.pgettext('vpn-settings-view', 'Tunnel protocol');
-  const multihop = messages.pgettext('wireguard-settings-view', 'Multihop');
-
-  return sprintf(
-    messages.pgettext(
-      // TRANSLATORS: Informs the user that the feature is only available when WireGuard
-      // TRANSLATORS: is selected.
-      // TRANSLATORS: Available placeholders:
-      // TRANSLATORS: %(wireguard)s - will be replaced with WireGuard
-      // TRANSLATORS: %(tunnelProtocol)s - the name of the tunnel protocol setting
-      // TRANSLATORS: %(setting)s - the name of the setting
-      'wireguard-settings-view',
-      'Switch to “%(wireguard)s” in Settings > %(tunnelProtocol)s to make %(setting)s available.',
-    ),
-    { wireguard: strings.wireguard, tunnelProtocol, setting: multihop },
   );
 }
