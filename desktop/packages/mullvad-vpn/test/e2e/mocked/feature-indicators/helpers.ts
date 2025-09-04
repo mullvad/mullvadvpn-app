71ea6c968d730df61528538713d657cf17983455
Oliver <oliver@mohlin.dev>
2025-09-04 11:47:27 +0200

    Add tests for clicking feature indicators

---
+++ desktop/packages/mullvad-vpn/test/e2e/mocked/feature-indicators/helpers.ts
@@ -0,0 +1,60 @@
+import { Page } from 'playwright';
+
+import {
+  FeatureIndicator,
+  ILocation,
+  ITunnelEndpoint,
+  TunnelState,
+} from '../../../../src/shared/daemon-rpc-types';
+import { RoutesObjectModel } from '../../route-object-models';
+import { MockedTestUtils } from '../mocked-utils';
+
+const endpoint: ITunnelEndpoint = {
+  address: 'wg10:80',
+  protocol: 'tcp',
+  quantumResistant: false,
+  tunnelType: 'wireguard',
+  daita: false,
+};
+
+const mockDisconnectedLocation: ILocation = {
+  country: 'Sweden',
+  city: 'Gothenburg',
+  latitude: 58,
+  longitude: 12,
+  mullvadExitIp: false,
+};
+
+const mockConnectedLocation: ILocation = { ...mockDisconnectedLocation, mullvadExitIp: true };
+
+export const createHelpers = ({
+  utils,
+}: {
+  page: Page;
+  routes: RoutesObjectModel;
+  utils: MockedTestUtils;
+}) => {
+  const connectWithFeatures = async (featureIndicators: FeatureIndicator[] | undefined) => {
+    await utils.sendMockIpcResponse<TunnelState>({
+      channel: 'tunnel-',
+      response: {
+        state: 'connected',
+        details: { endpoint, location: mockConnectedLocation },
+        featureIndicators: featureIndicators,
+      },
+    });
+  };
+
+  const disconnect = () =>
+    utils.sendMockIpcResponse<TunnelState>({
+      channel: 'tunnel-',
+      response: {
+        state: 'disconnected',
+        lockedDown: false,
+      },
+    });
+
+  return { connectWithFeatures, disconnect };
+};
+
+export type FeatureIndicatorsHelpers = ReturnType<typeof createHelpers>;
