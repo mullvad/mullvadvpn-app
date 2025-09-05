3436b000bf59480273bdc61c800199ded4d36d8c
Oliver <oliver@mohlin.dev>
2025-09-05 07:30:54 +0200

    Rename useListItem to useListItemContext

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/listbox/components/listbox-option/components/listbox-option-label/ListboxOptionLabel.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/listbox/components/listbox-option/components/listbox-option-label/ListboxOptionLabel.tsx
@@ -1,11 +1,11 @@
 import { Text, TextProps } from '../../../../..';
-import { useListItem } from '../../../../../list-item/ListItemContext';
+import { useListItemContext } from '../../../../../list-item/ListItemContext';
 
 export type SelectListItemOptionLabelProps<E extends React.ElementType = 'span'> = TextProps<E>;
 
 export const ListboxOptionLabel = <E extends React.ElementType = 'span'>(
   props: SelectListItemOptionLabelProps<E>,
 ) => {
-  const { disabled } = useListItem();
+  const { disabled } = useListItemContext();
   return <Text variant="bodySmall" color={disabled ? 'whiteAlpha40' : 'white'} {...props} />;
 };
