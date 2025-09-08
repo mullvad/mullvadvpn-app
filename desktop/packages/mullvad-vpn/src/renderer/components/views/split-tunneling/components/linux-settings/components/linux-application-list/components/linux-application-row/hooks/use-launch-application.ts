c5e43beb544668b262d5501489040856102ce14e
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-09-08 12:17:54 +0200

    Show unsupported dialog on app launch if split tunneling unsupported

--- desktop/packages/mullvad-vpn/src/renderer/components/views/split-tunneling/components/linux-settings/components/linux-application-list/components/linux-application-row/hooks/use-launch-application.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/split-tunneling/components/linux-settings/components/linux-application-list/components/linux-application-row/hooks/use-launch-application.ts
@@ -1,20 +1,31 @@
 import { useCallback } from 'react';
 
+import { useLinuxSettingsContext } from '../../../../../LinuxSettingsContext';
 import { useLinuxApplicationRowContext } from '../LinuxApplicationRowContext';
 import { useHasApplicationWarning } from './use-has-application-warning';
 
 export function useLaunchApplication() {
   const { application, onSelect, setShowWarningDialog } = useLinuxApplicationRowContext();
+  const { setShowUnsupportedDialog, splitTunnelingSupported } = useLinuxSettingsContext();
   const hasApplicationWarning = useHasApplicationWarning();
 
   const launchApplication = useCallback(() => {
-    if (hasApplicationWarning) {
+    if (splitTunnelingSupported === false) {
+      setShowUnsupportedDialog(true);
+    } else if (hasApplicationWarning) {
       setShowWarningDialog(true);
     } else {
       setShowWarningDialog(false);
       onSelect?.(application);
     }
-  }, [application, hasApplicationWarning, onSelect, setShowWarningDialog]);
+  }, [
+    application,
+    hasApplicationWarning,
+    onSelect,
+    setShowUnsupportedDialog,
+    setShowWarningDialog,
+    splitTunnelingSupported,
+  ]);
 
   return launchApplication;
 }
