8bcde96de18a7a9ea5efc7aaddf0c22d9e71d222
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-09-09 10:34:07 +0200

    Add method to go to split tunneling settings route

--- desktop/packages/mullvad-vpn/test/e2e/route-object-models/settings/selectors.ts
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/settings/selectors.ts
@@ -5,4 +5,5 @@ export const createSelectors = (page: Page) => ({
   daitaSettingsButton: () => page.getByRole('button', { name: 'Daita' }),
   userInterfaceButton: () => page.getByRole('button', { name: 'User interface settings' }),
   vpnSettingsButton: () => page.getByRole('button', { name: 'VPN settings' }),
+  splitTunnelingSettingsButton: () => page.getByRole('button', { name: 'Split tunneling' }),
 });
