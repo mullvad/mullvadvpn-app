2b52a80e48422b8afeac44010e7d37151340650b
Oliver <oliver@mohlin.dev>
2025-09-11 07:58:50 +0200

    Move feature indicators tests into describe block

--- desktop/packages/mullvad-vpn/test/e2e/mocked/feature-indicators/feature-indicators.spec.ts
+++ desktop/packages/mullvad-vpn/test/e2e/mocked/feature-indicators/feature-indicators.spec.ts
@@ -31,97 +31,103 @@ const mockConnectedLocation: ILocation = { ...mockDisconnectedLocation, mullvadE
 let page: Page;
 let util: MockedTestUtils;
 
-test.beforeAll(async () => {
-  ({ page, util } = await startMockedApp());
-  await util.waitForRoute(RoutePath.main);
-});
-
-test.afterAll(async () => {
-  await page.close();
-});
-
-test('App should show no feature indicators', async () => {
-  await util.ipc.tunnel[''].notify({
-    state: 'connected',
-    details: { endpoint, location: mockConnectedLocation },
-    featureIndicators: undefined,
+test.describe('Feature indicators', () => {
+  test.beforeAll(async () => {
+    ({ page, util } = await startMockedApp());
+    await util.waitForRoute(RoutePath.main);
   });
 
-  await expectConnected(page);
-  await expectFeatureIndicators(page, []);
-
-  const ellipsis = page.getByText(/^\d more.../);
-  await expect(ellipsis).not.toBeVisible();
-
-  await page.getByTestId('connection-panel-chevron').click();
-  await expect(ellipsis).not.toBeVisible();
-
-  await expectFeatureIndicators(page, []);
-  await page.getByTestId('connection-panel-chevron').click();
-});
-
-test('App should show feature indicators', async () => {
-  await util.ipc.tunnel[''].notify({
-    state: 'connected',
-    details: { endpoint, location: mockConnectedLocation },
-    featureIndicators: [
-      FeatureIndicator.daita,
-      FeatureIndicator.udp2tcp,
-      FeatureIndicator.customMssFix,
-      FeatureIndicator.customMtu,
-      FeatureIndicator.lanSharing,
-      FeatureIndicator.serverIpOverride,
-      FeatureIndicator.customDns,
-      FeatureIndicator.lockdownMode,
-      FeatureIndicator.quantumResistance,
-      FeatureIndicator.multihop,
-    ],
+  test.afterAll(async () => {
+    await page.close();
   });
 
-  // Make sure panel is collapsed before checking indicator visibility.
-  const ellipsis = page.getByText(/^\d more.../);
-  await expect(ellipsis).toBeVisible();
+  test('App should show no feature indicators', async () => {
+    await util.ipc.tunnel[''].notify({
+      state: 'connected',
+      details: { endpoint, location: mockConnectedLocation },
+      featureIndicators: undefined,
+    });
 
-  await expectConnected(page);
-  await expectFeatureIndicators(page, ['DAITA', 'Quantum resistance'], false);
-  await expectHiddenFeatureIndicator(page, 'Mssfix');
+    await expectConnected(page);
+    await expectFeatureIndicators(page, []);
 
-  await page.getByTestId('connection-panel-chevron').click();
-  await expect(ellipsis).not.toBeVisible();
+    const ellipsis = page.getByText(/^\d more.../);
+    await expect(ellipsis).not.toBeVisible();
 
-  await expectFeatureIndicators(page, [
-    'DAITA',
-    'Quantum resistance',
-    'Mssfix',
-    'MTU',
-    'Obfuscation',
-    'Local network sharing',
-    'Lockdown mode',
-    'Multihop',
-    'Custom DNS',
-    'Server IP override',
-  ]);
+    await page.getByTestId('connection-panel-chevron').click();
+    await expect(ellipsis).not.toBeVisible();
+
+    await expectFeatureIndicators(page, []);
+    await page.getByTestId('connection-panel-chevron').click();
+  });
+
+  test('App should show feature indicators', async () => {
+    await util.ipc.tunnel[''].notify({
+      state: 'connected',
+      details: { endpoint, location: mockConnectedLocation },
+      featureIndicators: [
+        FeatureIndicator.daita,
+        FeatureIndicator.udp2tcp,
+        FeatureIndicator.customMssFix,
+        FeatureIndicator.customMtu,
+        FeatureIndicator.lanSharing,
+        FeatureIndicator.serverIpOverride,
+        FeatureIndicator.customDns,
+        FeatureIndicator.lockdownMode,
+        FeatureIndicator.quantumResistance,
+        FeatureIndicator.multihop,
+      ],
+    });
+
+    // Make sure panel is collapsed before checking indicator visibility.
+    const ellipsis = page.getByText(/^\d more.../);
+    await expect(ellipsis).toBeVisible();
+
+    await expectConnected(page);
+    await expectFeatureIndicators(page, ['DAITA', 'Quantum resistance'], false);
+    await expectHiddenFeatureIndicator(page, 'Mssfix');
+
+    await page.getByTestId('connection-panel-chevron').click();
+    await expect(ellipsis).not.toBeVisible();
+
+    await expectFeatureIndicators(page, [
+      'DAITA',
+      'Quantum resistance',
+      'Mssfix',
+      'MTU',
+      'Obfuscation',
+      'Local network sharing',
+      'Lockdown mode',
+      'Multihop',
+      'Custom DNS',
+      'Server IP override',
+    ]);
+  });
+
+  async function expectHiddenFeatureIndicator(page: Page, hiddenIndicator: string) {
+    const indicators = page.getByTestId('feature-indicator');
+    const indicator = indicators.getByText(hiddenIndicator, { exact: true });
+
+    // Make sure at least one is visible to not run the "not visible" check before they become
+    // visible.
+    await expect(indicators.first()).toBeVisible();
+
+    await expect(indicator).toHaveCount(1);
+    await expect(indicator).not.toBeVisible();
+  }
+
+  async function expectFeatureIndicators(
+    page: Page,
+    expectedIndicators: Array<string>,
+    only = true,
+  ) {
+    const indicators = page.getByTestId('feature-indicator');
+    if (only) {
+      await expect(indicators).toHaveCount(expectedIndicators.length);
+    }
+
+    for (const indicator of expectedIndicators) {
+      await expect(indicators.getByText(indicator, { exact: true })).toBeVisible();
+    }
+  }
 });
-
-async function expectHiddenFeatureIndicator(page: Page, hiddenIndicator: string) {
-  const indicators = page.getByTestId('feature-indicator');
-  const indicator = indicators.getByText(hiddenIndicator, { exact: true });
-
-  // Make sure at least one is visible to not run the "not visible" check before they become
-  // visible.
-  await expect(indicators.first()).toBeVisible();
-
-  await expect(indicator).toHaveCount(1);
-  await expect(indicator).not.toBeVisible();
-}
-
-async function expectFeatureIndicators(page: Page, expectedIndicators: Array<string>, only = true) {
-  const indicators = page.getByTestId('feature-indicator');
-  if (only) {
-    await expect(indicators).toHaveCount(expectedIndicators.length);
-  }
-
-  for (const indicator of expectedIndicators) {
-    await expect(indicators.getByText(indicator, { exact: true })).toBeVisible();
-  }
-}
