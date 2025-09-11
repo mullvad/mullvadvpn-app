68f083d751a8c42a665fce2239cb7f73055bd97d
Oliver <oliver@mohlin.dev>
2025-09-11 10:31:48 +0200

    Add and use SettingsListbox throughout app

--- desktop/packages/mullvad-vpn/src/renderer/components/settings-listbox/components/split-listbox-option/components/split-listbox-option-item/SplitListboxOptionItem.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/settings-listbox/components/split-listbox-option/components/split-listbox-option-item/SplitListboxOptionItem.tsx
@@ -1,4 +1,4 @@
-import { Listbox } from '../../../../lib/components/listbox/Listbox';
+import { Listbox } from '../../../../../../lib/components/listbox/Listbox';
 
 export type ListBoxOptionWithNavigationProps = React.ComponentPropsWithRef<'li'>;
 
