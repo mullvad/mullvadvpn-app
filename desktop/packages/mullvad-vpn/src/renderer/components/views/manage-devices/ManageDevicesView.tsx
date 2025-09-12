9a894fada64308e601d8f46cac32e574ddcc15c8
Oliver <oliver@mohlin.dev>
2025-09-12 16:36:40 +0200

    Implement manage devices view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/manage-devices/ManageDevicesView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/manage-devices/ManageDevicesView.tsx
@@ -1,3 +1,64 @@
+import { IDevice } from '../../../../shared/daemon-rpc-types';
+import { messages } from '../../../../shared/gettext';
+import { Spinner, Text } from '../../../lib/components';
+import { FlexColumn } from '../../../lib/components/flex-column';
+import { View } from '../../../lib/components/view';
+import { useHistory } from '../../../lib/history';
+import { AppNavigationHeader } from '../../app-navigation-header';
+import { DeviceListItem } from '../../device-list-item';
+import { BackAction } from '../../KeyboardNavigation';
+import List from '../../List';
+import { NavigationContainer } from '../../NavigationContainer';
+import { NavigationScrollbars } from '../../NavigationScrollbars';
+import { useGetDevices } from './hooks';
+
+const getDeviceKey = (device: IDevice): string => device.id;
+
 export function ManageDevicesView() {
-  return <div>Manage Devices View</div>;
+  const { pop } = useHistory();
+
+  const { loading, devices } = useGetDevices();
+  return (
+    <BackAction action={pop}>
+      <View backgroundColor="darkBlue">
+        <NavigationContainer>
+          <AppNavigationHeader
+            title={
+              // TRANSLATORS: Title label in navigation bar for the manage devices view.
+              messages.pgettext('device-management', 'Manage devices')
+            }
+          />
+          <NavigationScrollbars>
+            <FlexColumn $gap="medium">
+              <View.Container>
+                <FlexColumn $gap="small">
+                  <Text variant="titleBig">
+                    {messages.pgettext('device-management', 'Manage devices')}
+                  </Text>
+                  <Text variant="labelTiny" color="whiteAlpha60">
+                    {
+                      // TRANSLATORS: Subtitle text in the manage devices view, explaining
+                      // TRANSLATORS: devices and what they can do in the manage devices view.
+                      messages.pgettext(
+                        'device-management',
+                        'View and manage all your logged in devices. You can have up to 5 devices on one account at a time. Each device gets a name when logged in to help you tell them apart easily.',
+                      )
+                    }
+                  </Text>
+                </FlexColumn>
+              </View.Container>
+              {loading && <Spinner />}
+              {!loading && (
+                <div>
+                  <List items={devices} getKey={getDeviceKey}>
+                    {(device) => <DeviceListItem device={device} />}
+                  </List>
+                </div>
+              )}
+            </FlexColumn>
+          </NavigationScrollbars>
+        </NavigationContainer>
+      </View>
+    </BackAction>
+  );
 }
