ed36ba8045634fac65f3b0e45cd63f8f87a5bf92
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-09-12 16:03:45 +0200

    Add info and dialog for if split tunneling is unsupported

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/split-tunneling/components/linux-settings/components/header-description/HeaderDescription.tsx
@@ -0,0 +1,59 @@
+import { sprintf } from 'sprintf-js';
+
+import { strings } from '../../../../../../../../shared/constants';
+import { messages } from '../../../../../../../../shared/gettext';
+import { Icon, Link } from '../../../../../../../lib/components';
+import { FlexColumn } from '../../../../../../../lib/components/flex-column';
+import { FlexRow } from '../../../../../../../lib/components/flex-row';
+import { useLinuxSettingsContext } from '../../LinuxSettingsContext';
+import { useShowUnsupportedDialog } from './hooks';
+
+export function HeaderDescription() {
+  const { splitTunnelingSupported } = useLinuxSettingsContext();
+  const message = sprintf(
+    // TRANSLATORS: Information about split tunneling not being supported on the system.
+    // TRANSLATORS: Available placeholders:
+    // TRANSLATORS: %(splitTunneling)s - will be replaced with Split tunneling
+    messages.pgettext(
+      'split-tunneling-view',
+      '%(splitTunneling)s is not supported by your system.',
+    ),
+    {
+      splitTunneling: strings.splitTunneling,
+    },
+  );
+  const showUnsupportedDialog = useShowUnsupportedDialog();
+
+  if (splitTunnelingSupported === false) {
+    return (
+      <FlexRow>
+        <FlexColumn $justifyContent="center" $margin={{ right: 'small' }}>
+          <Icon size="small" color="whiteAlpha60" icon="info-circle" />
+        </FlexColumn>
+        <FlexColumn>
+          <span>
+            {message}
+            &nbsp;
+            <Link
+              aria-description={message}
+              as="button"
+              onClick={showUnsupportedDialog}
+              variant="labelTiny">
+              <Link.Text>
+                {
+                  // TRANSLATORS: Link for learning more
+                  messages.pgettext('split-tunneling-view', 'Click here to learn more')
+                }
+              </Link.Text>
+            </Link>
+          </span>
+        </FlexColumn>
+      </FlexRow>
+    );
+  }
+
+  return messages.pgettext(
+    'split-tunneling-view',
+    'Click on an app to launch it. Its traffic will bypass the VPN tunnel until you close it.',
+  );
+}
---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/split-tunneling/components/linux-settings/components/unsupported-dialog/UnsupportedDialog.tsx
@@ -0,0 +1,45 @@
+import { useCallback } from 'react';
+import { sprintf } from 'sprintf-js';
+
+import { strings } from '../../../../../../../../shared/constants';
+import { messages } from '../../../../../../../../shared/gettext';
+import { Button } from '../../../../../../../lib/components';
+import { ModalAlert, ModalAlertType } from '../../../../../../Modal';
+import { useLinuxSettingsContext } from '../../LinuxSettingsContext';
+
+export function UnsupportedDialog() {
+  const { showUnsupportedDialog, setShowUnsupportedDialog } = useLinuxSettingsContext();
+  const hideUnsupportedDialog = useCallback(() => {
+    setShowUnsupportedDialog(false);
+  }, [setShowUnsupportedDialog]);
+
+  const unsupportedMessage = sprintf(
+    // TRANSLATORS: Information about split tunneling being unavailable due to
+    // TRANSLATORS: missing support in the user's operating system.
+    // TRANSLATORS: Available placeholders:
+    // TRANSLATORS: %(splitTunneling)s - will be replaced with Split tunneling
+    messages.pgettext(
+      'split-tunneling-view',
+      'To use %(splitTunneling)s, please change to a Linux kernel version that supports cgroup v1.',
+    ),
+    {
+      splitTunneling: strings.splitTunneling,
+    },
+  );
+
+  const buttons = [
+    <Button key="cancel" onClick={hideUnsupportedDialog}>
+      <Button.Text>{messages.gettext('Got it!')}</Button.Text>
+    </Button>,
+  ];
+
+  return (
+    <ModalAlert
+      isOpen={showUnsupportedDialog}
+      type={ModalAlertType.info}
+      message={unsupportedMessage}
+      buttons={buttons}
+      close={hideUnsupportedDialog}
+    />
+  );
+}
