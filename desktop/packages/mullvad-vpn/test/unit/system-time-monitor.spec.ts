a170405804d5c669c559782d7923052bc8da904b
Oskar <oskar@mullvad.net>
2025-09-12 16:21:06 +0200

    Add check for if system time changes

---
+++ desktop/packages/mullvad-vpn/test/unit/system-time-monitor.spec.ts
@@ -0,0 +1,29 @@
+import { expect, spy } from 'chai';
+import sinon from 'sinon';
+
+import { systemTimeMonitor } from '../../src/main/system-time-monitor';
+
+describe('IAccountData cache', () => {
+  let clock: sinon.SinonFakeTimers;
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers({ shouldAdvanceTime: true });
+  });
+
+  afterEach(() => {
+    clock.restore();
+  });
+
+  it('should notify when system clock changes', () => {
+    const systemTimeListener = spy();
+
+    clock.setSystemTime(new Date('2025-01-01'));
+    systemTimeMonitor(systemTimeListener);
+    clock.setSystemTime(new Date('2025-01-02'));
+    clock.tick(1001);
+    clock.setSystemTime(new Date('2025-01-01'));
+    clock.tick(1900);
+
+    expect(systemTimeListener).to.have.been.called.twice;
+  });
+});
