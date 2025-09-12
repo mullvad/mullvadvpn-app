00edbb24b0f64195685e0860b7f58aaad58d37ff
Oliver <oliver@mohlin.dev>
2025-09-12 13:58:48 +0200

    Refactor account view to use components from component library

--- desktop/packages/mullvad-vpn/src/renderer/components/views/account/components/device-name-row/DeviceNameRow.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/account/components/device-name-row/DeviceNameRow.tsx
@@ -1,14 +1,22 @@
-import { Flex } from '../../../../../lib/components';
+import styled from 'styled-components';
+
+import { Flex, Text } from '../../../../../lib/components';
 import { useSelector } from '../../../../../redux/store';
 import DeviceInfoButton from '../../../../DeviceInfoButton';
-import { DeviceRowValue } from '../../AccountStyles';
+
+const StyledText = styled(Text)`
+  text-transform: capitalize;
+`;
 
 export function DeviceNameRow() {
   const deviceName = useSelector((state) => state.account.deviceName);
+
   return (
-    <Flex $gap="small" $alignItems="center">
-      <DeviceRowValue>{deviceName}</DeviceRowValue>
-      <DeviceInfoButton />
+    <Flex $justifyContent="space-between">
+      <Flex $gap="small" $alignItems="center">
+        <StyledText variant="bodySmallSemibold">{deviceName}</StyledText>
+        <DeviceInfoButton />
+      </Flex>
     </Flex>
   );
 }
