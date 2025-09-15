5aa19ac4a9d21cc8c53fcd7ca63d6114c75e852f
Oliver <oliver@mohlin.dev>
2025-09-15 15:12:54 +0200

    Add ref to polymorphic props

--- desktop/packages/mullvad-vpn/src/renderer/lib/types/polymorphic-props.ts
+++ desktop/packages/mullvad-vpn/src/renderer/lib/types/polymorphic-props.ts
@@ -1,4 +1,4 @@
 export type PolymorphicProps<E extends React.ElementType, Props = object> = Props &
-  Omit<React.ComponentPropsWithoutRef<E>, keyof Props> & {
+  Omit<React.ComponentPropsWithRef<E>, keyof Props> & {
     as?: E;
   };
