00df068104fde1b0f85ac1d4ef3b201180233c4d
Oliver <oliver@mohlin.dev>
2025-09-18 14:21:19 +0200

    Replace history state after scrolling to list item

--- desktop/packages/mullvad-vpn/src/renderer/hooks/useScrollToAnchor.ts
+++ desktop/packages/mullvad-vpn/src/renderer/hooks/useScrollToAnchor.ts
@@ -3,10 +3,12 @@ import { useEffect } from 'react';
 export const useScrollToReference = <T extends Element = HTMLDivElement>(
   ref?: React.RefObject<T | null>,
   scroll?: boolean,
+  onScrolled?: () => void,
 ) => {
   useEffect(() => {
     if (scroll) {
       ref?.current?.scrollIntoView({ behavior: 'instant', block: 'start' });
+      onScrolled?.();
     }
-  }, [ref, scroll]);
+  }, [onScrolled, ref, scroll]);
 };
--- desktop/packages/mullvad-vpn/src/renderer/hooks/useScrollToListItem.ts
+++ desktop/packages/mullvad-vpn/src/renderer/hooks/useScrollToListItem.ts
@@ -12,15 +12,31 @@ export const useScrollToListItem = <T extends Element = HTMLDivElement>(
   animation?: ListItemAnimation;
 } => {
   const ref = React.useRef<T>(null);
-  const { location, action } = useHistory();
+  const history = useHistory();
+  const { location } = history;
   const { state } = location;
 
-  const isPop = action === 'POP';
   const anchorId = state?.options?.find((option) => option.type === 'scroll-to-anchor')?.id;
-  const scroll = id === anchorId && !isPop;
-  useScrollToReference(ref, scroll);
+  const scroll = id === anchorId && anchorId !== undefined;
 
-  if (anchorId === undefined || isPop) {
+  const handleScrolled = React.useCallback(() => {
+    const options = state?.options?.filter((option) => {
+      if (option.type === 'scroll-to-anchor') {
+        return option.id !== anchorId;
+      }
+
+      return true;
+    });
+
+    history.replace(location, {
+      ...state,
+      options,
+    });
+  }, [anchorId, history, location, state]);
+
+  useScrollToReference(ref, scroll, handleScrolled);
+
+  if (anchorId === undefined) {
     return {
       ref: undefined,
       animation: undefined,
