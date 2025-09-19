df467cb2b597f94416a5b5d360d0a16254c16708
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-09-19 13:36:11 +0200

    Update to only set focus if active element is default after mount

--- desktop/packages/mullvad-vpn/src/renderer/hooks/useInitialFocus.ts
+++ desktop/packages/mullvad-vpn/src/renderer/hooks/useInitialFocus.ts
@@ -1,16 +1,17 @@
 import React from 'react';
 
 import { useFocusReference } from './useFocusReference';
-import { useIsDefaultFocusOnLoad } from './useIsDefaultFocusOnLoad';
+import { useIsDefaultActiveElementAfterMount } from './useIsDefaultActiveElementAfterMount';
 
 export const useInitialFocus = <T extends HTMLElement = HTMLDivElement>(): {
   ref?: React.RefObject<T | null>;
 } => {
   const ref = React.useRef<T>(null);
 
-  const isDefaultFocus = useIsDefaultFocusOnLoad();
+  const isDefaultFocus = useIsDefaultActiveElementAfterMount();
+  const shouldFocus = isDefaultFocus === true;
 
-  useFocusReference(ref, isDefaultFocus);
+  useFocusReference(ref, shouldFocus);
 
   if (!isDefaultFocus)
     return {
