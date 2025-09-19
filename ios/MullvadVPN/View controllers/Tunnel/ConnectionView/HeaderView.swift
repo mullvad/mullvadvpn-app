2155e7298c88122a0ab2000b4f141b480f529b2a
Bug Magnet <marco.nikic@mullvad.net>
2025-09-19 11:11:47 +0200

    Change the layout of the HeaderView, Remove compression on relay

--- ios/MullvadVPN/View controllers/Tunnel/ConnectionView/HeaderView.swift
+++ ios/MullvadVPN/View controllers/Tunnel/ConnectionView/HeaderView.swift
@@ -22,45 +22,45 @@ extension ConnectionView {
                     isExpanded.toggle()
                 }
             } label: {
-                HStack(alignment: .top) {
-                    VStack(alignment: .leading, spacing: 0) {
+                VStack(alignment: .leading, spacing: 0) {
+                    HStack(alignment: .top) {
                         Text(viewModel.localizedTitleForSecureLabel)
                             .textCase(.uppercase)
                             .font(.title3.weight(.semibold))
                             .foregroundStyle(viewModel.textColorForSecureLabel.color)
                             .accessibilityIdentifier(viewModel.accessibilityIdForSecureLabel.asString)
                             .accessibilityLabel(viewModel.localizedAccessibilityLabelForSecureLabel)
-                        if let titleForCountryAndCity {
-                            Text(titleForCountryAndCity)
-                                .font(.title3.weight(.semibold))
-                                .foregroundStyle(UIColor.primaryTextColor.color)
-                                .padding(.top, 4)
-                        }
-                        if let titleForServer {
-                            Text(titleForServer)
-                                .font(.body)
-                                .foregroundStyle(UIColor.primaryTextColor.color.opacity(0.6))
-                                .padding(.top, 2)
-                                .accessibilityIdentifier(AccessibilityIdentifier.connectionPanelServerLabel.asString)
-                                .multilineTextAlignment(.leading)
-                        }
-                    }
-
-                    Group {
-                        Spacer()
-                        Button {
-                            withAnimation {
-                                isExpanded.toggle()
+                        Group {
+                            Spacer()
+                            Button {
+                                withAnimation {
+                                    isExpanded.toggle()
+                                }
+                            } label: {
+                                Image(.iconChevronUp)
+                                    .renderingMode(.template)
+                                    .rotationEffect(isExpanded ? .degrees(-180) : .degrees(0))
+                                    .foregroundStyle(.white)
+                                    .accessibilityIdentifier(AccessibilityIdentifier.relayStatusCollapseButton.asString)
                             }
-                        } label: {
-                            Image(.iconChevronUp)
-                                .renderingMode(.template)
-                                .rotationEffect(isExpanded ? .degrees(-180) : .degrees(0))
-                                .foregroundStyle(.white)
-                                .accessibilityIdentifier(AccessibilityIdentifier.relayStatusCollapseButton.asString)
                         }
+                        .showIf(viewModel.showsConnectionDetails)
                     }
-                    .showIf(viewModel.showsConnectionDetails)
+                    if let titleForCountryAndCity {
+                        Text(titleForCountryAndCity)
+                            .font(.title3.weight(.semibold))
+                            .foregroundStyle(UIColor.primaryTextColor.color)
+                            .padding(.top, 4)
+                    }
+                    if let titleForServer {
+                        Text(titleForServer)
+                            .font(.body)
+                            .foregroundStyle(UIColor.primaryTextColor.color.opacity(0.6))
+                            .padding(.top, 2)
+                            .accessibilityIdentifier(AccessibilityIdentifier.connectionPanelServerLabel.asString)
+                            .multilineTextAlignment(.leading)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
                 }
                 .onAppear {
                     titleForServer = viewModel.titleForServer
