b17272a57c38cba9755eedbda3d0df5b9f395774
Oliver <oliver@mohlin.dev>
2025-09-23 06:53:01 +0200

    Use SettingsToggleListItem component in user interface settings

--- desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/monochromatic-tray-icon-setting/MonochromaticTrayIconSetting.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/monochromatic-tray-icon-setting/MonochromaticTrayIconSetting.tsx
@@ -1,35 +1,24 @@
 import { messages } from '../../../../../../shared/gettext';
 import { useAppContext } from '../../../../../context';
 import { useSelector } from '../../../../../redux/store';
-import { AriaDescription, AriaInput, AriaInputGroup, AriaLabel } from '../../../../AriaGroup';
-import * as Cell from '../../../../cell';
+import { SettingsToggleListItem } from '../../../../settings-toggle-list-item';
 
 export function MonochromaticTrayIconSetting() {
   const monochromaticIcon = useSelector((state) => state.settings.guiSettings.monochromaticIcon);
   const { setMonochromaticIcon } = useAppContext();
 
   return (
-    <AriaInputGroup>
-      <Cell.Container>
-        <AriaLabel>
-          <Cell.InputLabel>
-            {messages.pgettext('user-interface-settings-view', 'Monochromatic tray icon')}
-          </Cell.InputLabel>
-        </AriaLabel>
-        <AriaInput>
-          <Cell.Switch isOn={monochromaticIcon} onChange={setMonochromaticIcon} />
-        </AriaInput>
-      </Cell.Container>
-      <Cell.CellFooter>
-        <AriaDescription>
-          <Cell.CellFooterText>
-            {messages.pgettext(
-              'user-interface-settings-view',
-              'Use a monochromatic tray icon instead of a colored one.',
-            )}
-          </Cell.CellFooterText>
-        </AriaDescription>
-      </Cell.CellFooter>
-    </AriaInputGroup>
+    <SettingsToggleListItem
+      checked={monochromaticIcon}
+      onCheckedChange={setMonochromaticIcon}
+      description={messages.pgettext(
+        'user-interface-settings-view',
+        'Use a monochromatic tray icon instead of a colored one.',
+      )}>
+      <SettingsToggleListItem.Label>
+        {messages.pgettext('user-interface-settings-view', 'Monochromatic tray icon')}
+      </SettingsToggleListItem.Label>
+      <SettingsToggleListItem.Switch />
+    </SettingsToggleListItem>
   );
 }
