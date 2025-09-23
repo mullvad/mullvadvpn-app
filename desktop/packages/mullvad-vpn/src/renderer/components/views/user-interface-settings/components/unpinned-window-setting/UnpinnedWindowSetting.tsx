cf8299c521a3c0c315fde5013576bcae4a3791f3
Oliver <oliver@mohlin.dev>
2025-09-23 06:53:01 +0200

    Use SettingsToggleListItem component in user interface settings

--- desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/unpinned-window-setting/UnpinnedWindowSetting.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/unpinned-window-setting/UnpinnedWindowSetting.tsx
@@ -1,35 +1,24 @@
 import { messages } from '../../../../../../shared/gettext';
 import { useAppContext } from '../../../../../context';
 import { useSelector } from '../../../../../redux/store';
-import { AriaDescription, AriaInput, AriaInputGroup, AriaLabel } from '../../../../AriaGroup';
-import * as Cell from '../../../../cell';
+import { SettingsToggleListItem } from '../../../../settings-toggle-list-item';
 
 export function UnpinnedWindowSetting() {
   const unpinnedWindow = useSelector((state) => state.settings.guiSettings.unpinnedWindow);
   const { setUnpinnedWindow } = useAppContext();
 
   return (
-    <AriaInputGroup>
-      <Cell.Container>
-        <AriaLabel>
-          <Cell.InputLabel>
-            {messages.pgettext('user-interface-settings-view', 'Unpin app from taskbar')}
-          </Cell.InputLabel>
-        </AriaLabel>
-        <AriaInput>
-          <Cell.Switch isOn={unpinnedWindow} onChange={setUnpinnedWindow} />
-        </AriaInput>
-      </Cell.Container>
-      <Cell.CellFooter>
-        <AriaDescription>
-          <Cell.CellFooterText>
-            {messages.pgettext(
-              'user-interface-settings-view',
-              'Enable to move the app around as a free-standing window.',
-            )}
-          </Cell.CellFooterText>
-        </AriaDescription>
-      </Cell.CellFooter>
-    </AriaInputGroup>
+    <SettingsToggleListItem
+      checked={unpinnedWindow}
+      onCheckedChange={setUnpinnedWindow}
+      description={messages.pgettext(
+        'user-interface-settings-view',
+        'Enable to move the app around as a free-standing window.',
+      )}>
+      <SettingsToggleListItem.Label>
+        {messages.pgettext('user-interface-settings-view', 'Unpin app from taskbar')}
+      </SettingsToggleListItem.Label>
+      <SettingsToggleListItem.Switch />
+    </SettingsToggleListItem>
   );
 }
