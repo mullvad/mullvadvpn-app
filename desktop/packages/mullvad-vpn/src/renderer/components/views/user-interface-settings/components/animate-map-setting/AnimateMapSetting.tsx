027c0bf5f36a89ffe2f557224404dd94bf7af6a2
Oliver <oliver@mohlin.dev>
2025-09-23 06:53:01 +0200

    Use SettingsToggleListItem component in user interface settings

--- desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/animate-map-setting/AnimateMapSetting.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/animate-map-setting/AnimateMapSetting.tsx
@@ -1,32 +1,21 @@
 import { messages } from '../../../../../../shared/gettext';
 import { useAppContext } from '../../../../../context';
 import { useSelector } from '../../../../../redux/store';
-import { AriaDescription, AriaInput, AriaInputGroup, AriaLabel } from '../../../../AriaGroup';
-import * as Cell from '../../../../cell';
+import { SettingsToggleListItem } from '../../../../settings-toggle-list-item';
 
 export function AnimateMapSetting() {
   const animateMap = useSelector((state) => state.settings.guiSettings.animateMap);
   const { setAnimateMap } = useAppContext();
 
   return (
-    <AriaInputGroup>
-      <Cell.Container>
-        <AriaLabel>
-          <Cell.InputLabel>
-            {messages.pgettext('user-interface-settings-view', 'Animate map')}
-          </Cell.InputLabel>
-        </AriaLabel>
-        <AriaInput>
-          <Cell.Switch isOn={animateMap} onChange={setAnimateMap} />
-        </AriaInput>
-      </Cell.Container>
-      <Cell.CellFooter>
-        <AriaDescription>
-          <Cell.CellFooterText>
-            {messages.pgettext('user-interface-settings-view', 'Animate map movements.')}
-          </Cell.CellFooterText>
-        </AriaDescription>
-      </Cell.CellFooter>
-    </AriaInputGroup>
+    <SettingsToggleListItem
+      checked={animateMap}
+      onCheckedChange={setAnimateMap}
+      description={messages.pgettext('user-interface-settings-view', 'Animate map movements.')}>
+      <SettingsToggleListItem.Label>
+        {messages.pgettext('user-interface-settings-view', 'Animate map')}
+      </SettingsToggleListItem.Label>
+      <SettingsToggleListItem.Switch />
+    </SettingsToggleListItem>
   );
 }
--- desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/start-minimized-setting/StartMinimizedSetting.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/start-minimized-setting/StartMinimizedSetting.tsx
@@ -1,35 +1,24 @@
 import { messages } from '../../../../../../shared/gettext';
 import { useAppContext } from '../../../../../context';
 import { useSelector } from '../../../../../redux/store';
-import { AriaDescription, AriaInput, AriaInputGroup, AriaLabel } from '../../../../AriaGroup';
-import * as Cell from '../../../../cell';
+import { SettingsToggleListItem } from '../../../../settings-toggle-list-item';
 
 export function StartMinimizedSetting() {
   const startMinimized = useSelector((state) => state.settings.guiSettings.startMinimized);
   const { setStartMinimized } = useAppContext();
 
   return (
-    <AriaInputGroup>
-      <Cell.Container>
-        <AriaLabel>
-          <Cell.InputLabel>
-            {messages.pgettext('user-interface-settings-view', 'Start minimized')}
-          </Cell.InputLabel>
-        </AriaLabel>
-        <AriaInput>
-          <Cell.Switch isOn={startMinimized} onChange={setStartMinimized} />
-        </AriaInput>
-      </Cell.Container>
-      <Cell.CellFooter>
-        <AriaDescription>
-          <Cell.CellFooterText>
-            {messages.pgettext(
-              'user-interface-settings-view',
-              'Show only the tray icon when the app starts.',
-            )}
-          </Cell.CellFooterText>
-        </AriaDescription>
-      </Cell.CellFooter>
-    </AriaInputGroup>
+    <SettingsToggleListItem
+      checked={startMinimized}
+      onCheckedChange={setStartMinimized}
+      description={messages.pgettext(
+        'user-interface-settings-view',
+        'Show only the tray icon when the app starts.',
+      )}>
+      <SettingsToggleListItem.Label>
+        {messages.pgettext('user-interface-settings-view', 'Start minimized')}
+      </SettingsToggleListItem.Label>
+      <SettingsToggleListItem.Switch />
+    </SettingsToggleListItem>
   );
 }
