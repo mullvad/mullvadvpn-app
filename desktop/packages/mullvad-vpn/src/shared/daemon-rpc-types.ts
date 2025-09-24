45bf181c02ee489b6d0d14d910490b440954533e
David Lönnhager <david.l@mullvad.net>
2025-09-24 14:47:01 +0200

    Add LWO to UI

--- desktop/packages/mullvad-vpn/src/shared/daemon-rpc-types.ts
+++ desktop/packages/mullvad-vpn/src/shared/daemon-rpc-types.ts
@@ -399,6 +399,7 @@ export interface IRelayListHostname {
   daita: boolean;
   // The absence of this value signals that the relay does not deploy QUIC.
   quic?: Quic;
+  lwo: boolean;
 }
 
 export type Quic = {
@@ -527,6 +528,7 @@ export enum ObfuscationType {
   udp2tcp,
   shadowsocks,
   quic,
+  lwo,
 }
 
 export type ObfuscationSettings = {
