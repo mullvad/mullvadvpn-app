0d5f564e6352f0a8c86d83e5e166f9b089e92474
Oskar <oskar@mullvad.net>
2025-09-25 08:24:14 +0200

    Make waitForNextRoute race proof and rename it

--- desktop/packages/mullvad-vpn/test/e2e/route-object-models/navigation/navigation-object-model.ts
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/navigation/navigation-object-model.ts
@@ -14,8 +14,7 @@ export class NavigationObjectModel {
   }
 
   async goBack() {
-    await this.navigationSelectors.backButton().click();
-    await this.utils.waitForNextRoute();
+    await this.utils.waitForRouteChange(() => this.navigationSelectors.backButton().click());
   }
 
   async gotoRoot() {
