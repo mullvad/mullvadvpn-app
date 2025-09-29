aaa9053f5e1c40cc33920125b2d98298d9f09c55
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-09-29 08:00:54 +0200

    Remove the use of well-known value types

--- android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/ManagementService.kt
+++ android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/ManagementService.kt
@@ -8,10 +8,7 @@ import arrow.optics.copy
 import arrow.optics.dsl.index
 import arrow.optics.typeclasses.Index
 import co.touchlab.kermit.Logger
-import com.google.protobuf.BoolValue
 import com.google.protobuf.Empty
-import com.google.protobuf.StringValue
-import com.google.protobuf.UInt32Value
 import io.grpc.ConnectivityState
 import io.grpc.Status
 import io.grpc.StatusException
@@ -310,7 +307,7 @@ class ManagementService(
             .mapLeft { DeviceUpdateError(it) }
 
     suspend fun getDeviceList(token: AccountNumber): Either<GetDeviceListError, List<Device>> =
-        Either.catch { grpc.listDevices(StringValue.of(token.value)) }
+        Either.catch { grpc.listDevices(token.value.toStringValue()) }
             .map { it.devicesList.map(ManagementInterface.Device::toDomain) }
             .onLeft { Logger.e("Get device list error") }
             .mapLeft { GetDeviceListError.Unknown(it) }
@@ -337,7 +334,7 @@ class ManagementService(
             .mapLeft(ConnectError::Unknown)
 
     suspend fun disconnect(disconnectReason: DisconnectReason): Either<ConnectError, Boolean> =
-        Either.catch { grpc.disconnectTunnel(StringValue.of(disconnectReason.logString)).value }
+        Either.catch { grpc.disconnectTunnel(disconnectReason.logString.toStringValue()).value }
             .onLeft { Logger.e("Disconnect error") }
             .mapLeft(ConnectError::Unknown)
 
@@ -369,13 +366,13 @@ class ManagementService(
         grpc.getCurrentApiAccessMethod(Empty.getDefaultInstance()).toDomain()
 
     suspend fun logoutAccount(): Either<LogoutAccountError, Unit> =
-        Either.catch { grpc.logoutAccount(StringValue.of("android-ui")) }
+        Either.catch { grpc.logoutAccount("android-ui".toStringValue()) }
             .onLeft { Logger.e("Logout account error") }
             .mapLeft(LogoutAccountError::Unknown)
             .mapEmpty()
 
     suspend fun loginAccount(accountNumber: AccountNumber): Either<LoginAccountError, Unit> =
-        Either.catch { grpc.loginAccount(StringValue.of(accountNumber.value)) }
+        Either.catch { grpc.loginAccount(accountNumber.value.toStringValue()) }
             .mapLeftStatus {
                 when (it.status.code) {
                     Status.Code.UNAUTHENTICATED -> LoginAccountError.InvalidAccount
@@ -445,7 +442,7 @@ class ManagementService(
         accountNumber: AccountNumber
     ): Either<GetAccountDataError, AccountData> =
         Either.catch {
-                grpc.getAccountData(StringValue.of(accountNumber.value)).toDomain(accountNumber)
+                grpc.getAccountData(accountNumber.value.toStringValue()).toDomain(accountNumber)
             }
             .onLeft { Logger.e("Get account data error") }
             .mapLeft(GetAccountDataError::Unknown)
@@ -544,13 +541,17 @@ class ManagementService(
             .mapEmpty()
 
     suspend fun setWireguardMtu(value: Int): Either<SetWireguardMtuError, Unit> =
-        Either.catch { grpc.setWireguardMtu(UInt32Value.of(value)) }
+        Either.catch { grpc.setWireguardMtu(value.toUInt32Value()) }
             .onLeft { Logger.e("Set wireguard mtu error") }
             .mapLeft(SetWireguardMtuError::Unknown)
             .mapEmpty()
 
     suspend fun resetWireguardMtu(): Either<SetWireguardMtuError, Unit> =
-        Either.catch { grpc.setWireguardMtu(UInt32Value.newBuilder().clearValue().build()) }
+        Either.catch {
+                grpc.setWireguardMtu(
+                    ManagementInterface.UInt32Value.newBuilder().clearValue().build()
+                )
+            }
             .onLeft { Logger.e("Reset wireguard mtu error") }
             .mapLeft(SetWireguardMtuError::Unknown)
             .mapEmpty()
@@ -632,13 +633,13 @@ class ManagementService(
             .mapEmpty()
 
     suspend fun setAllowLan(allow: Boolean): Either<SetAllowLanError, Unit> =
-        Either.catch { grpc.setAllowLan(BoolValue.of(allow)) }
+        Either.catch { grpc.setAllowLan(allow.toBoolValue()) }
             .onLeft { Logger.e("Set allow lan error") }
             .mapLeft(SetAllowLanError::Unknown)
             .mapEmpty()
 
     suspend fun setDaitaEnabled(enabled: Boolean): Either<SetDaitaSettingsError, Unit> =
-        Either.catch { grpc.setEnableDaita(BoolValue.of(enabled)) }
+        Either.catch { grpc.setEnableDaita(enabled.toBoolValue()) }
             .mapLeft(SetDaitaSettingsError::Unknown)
             .mapEmpty()
 
@@ -729,7 +730,7 @@ class ManagementService(
             .mapEmpty()
 
     suspend fun deleteCustomList(id: CustomListId): Either<DeleteCustomListError, Unit> =
-        Either.catch { grpc.deleteCustomList(StringValue.of(id.value)) }
+        Either.catch { grpc.deleteCustomList(id.value.toStringValue()) }
             .onLeft { Logger.e("Delete custom list error") }
             .mapLeft(::UnknownCustomListError)
             .mapEmpty()
@@ -741,7 +742,7 @@ class ManagementService(
             .mapEmpty()
 
     suspend fun applySettingsPatch(json: String): Either<SettingsPatchError, Unit> =
-        Either.catch { grpc.applyJsonSettings(StringValue.of(json)) }
+        Either.catch { grpc.applyJsonSettings(json.toStringValue()) }
             .mapLeftStatus {
                 when (it.status.code) {
                     // Currently we only get invalid argument errors from daemon via gRPC
@@ -800,7 +801,7 @@ class ManagementService(
     suspend fun submitVoucher(
         voucher: VoucherCode
     ): Either<RedeemVoucherError, RedeemVoucherSuccess> =
-        Either.catch { grpc.submitVoucher(StringValue.of(voucher.value)).toDomain() }
+        Either.catch { grpc.submitVoucher(voucher.value.toStringValue()).toDomain() }
             .mapLeftStatus {
                 when (it.status.code) {
                     Status.Code.INVALID_ARGUMENT,
@@ -834,7 +835,7 @@ class ManagementService(
             .mapEmpty()
 
     suspend fun addSplitTunnelingApp(app: PackageName): Either<AddSplitTunnelingAppError, Unit> =
-        Either.catch { grpc.addSplitTunnelApp(StringValue.of(app.value)) }
+        Either.catch { grpc.addSplitTunnelApp(app.value.toStringValue()) }
             .onLeft { Logger.e("Add split tunneling app error") }
             .mapLeft(AddSplitTunnelingAppError::Unknown)
             .mapEmpty()
@@ -842,7 +843,7 @@ class ManagementService(
     suspend fun removeSplitTunnelingApp(
         app: PackageName
     ): Either<RemoveSplitTunnelingAppError, Unit> =
-        Either.catch { grpc.removeSplitTunnelApp(StringValue.of(app.value)) }
+        Either.catch { grpc.removeSplitTunnelApp(app.value.toStringValue()) }
             .onLeft { Logger.e("Remove split tunneling app error") }
             .mapLeft(RemoveSplitTunnelingAppError::Unknown)
             .mapEmpty()
@@ -850,7 +851,7 @@ class ManagementService(
     suspend fun setSplitTunnelingState(
         enabled: Boolean
     ): Either<RemoveSplitTunnelingAppError, Unit> =
-        Either.catch { grpc.setSplitTunnelState(BoolValue.of(enabled)) }
+        Either.catch { grpc.setSplitTunnelState(enabled.toBoolValue()) }
             .onLeft { Logger.e("Set split tunneling state error") }
             .mapLeft(RemoveSplitTunnelingAppError::Unknown)
             .mapEmpty()
@@ -959,12 +960,12 @@ class ManagementService(
             .mapEmpty()
 
     suspend fun setIpv6Enabled(enabled: Boolean): Either<SetDaitaSettingsError, Unit> =
-        Either.catch { grpc.setEnableIpv6(BoolValue.of(enabled)) }
+        Either.catch { grpc.setEnableIpv6(enabled.toBoolValue()) }
             .mapLeft(SetDaitaSettingsError::Unknown)
             .mapEmpty()
 
     suspend fun setRecentsEnabled(enabled: Boolean): Either<SetWireguardConstraintsError, Unit> =
-        Either.catch { grpc.setEnableRecents(BoolValue.of(enabled)) }
+        Either.catch { grpc.setEnableRecents(enabled.toBoolValue()) }
             .mapLeft(SetWireguardConstraintsError::Unknown)
             .mapEmpty()
 
@@ -996,6 +997,15 @@ class ManagementService(
             .map { it.toDomain() }
             .onLeft { Logger.e("Get all shadowsocks ciphers error") }
             .mapLeft(GetShadowsocksCiphersError::Unknown)
+
+    private fun Boolean.toBoolValue() =
+        ManagementInterface.BoolValue.newBuilder().setValue(this).build()
+
+    private fun String.toStringValue() =
+        ManagementInterface.StringValue.newBuilder().setValue(this).build()
+
+    private fun Int.toUInt32Value() =
+        ManagementInterface.UInt32Value.newBuilder().setValue(this).build()
 
     private fun <A> Either<A, Empty>.mapEmpty() = map {}
 
