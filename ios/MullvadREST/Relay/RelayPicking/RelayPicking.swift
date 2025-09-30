68bbc261af3b47118ea7bd0de752421a57d65741
Jon Petersson <jon.petersson@mullvad.net>
2025-09-30 11:15:03 +0200

    Do not override ip on certain obfuscations

--- ios/MullvadREST/Relay/RelayPicking/RelayPicking.swift
+++ ios/MullvadREST/Relay/RelayPicking/RelayPicking.swift
@@ -21,53 +21,58 @@ extension RelayPicking {
     func findBestMatch(
         from candidates: [RelayWithLocation<REST.ServerRelay>],
         closeTo location: Location? = nil,
-        useObfuscatedPortIfAvailable: Bool
+        applyObfuscatedIps: Bool
     ) throws -> SelectedRelay {
-        var match = try RelaySelector.WireGuard.pickCandidate(
+        let match = try RelaySelector.WireGuard.pickCandidate(
             from: candidates,
             wireguard: obfuscation.allRelays.wireguard,
-            portConstraint: useObfuscatedPortIfAvailable
+            portConstraint: applyObfuscatedIps
                 ? obfuscation.port
                 : tunnelSettings.relayConstraints.port,
             numberOfFailedAttempts: connectionAttemptCount,
             closeTo: location
         )
 
-        if useObfuscatedPortIfAvailable && obfuscation.method == .shadowsocks {
-            match = applyShadowsocksIpAddress(in: match)
-        }
-
         return SelectedRelay(
-            endpoint: match.endpoint,
+            endpoint: match.endpoint.override(
+                ipv4Relay: IPv4Endpoint(
+                    ip: applyObfuscatedIps
+                        ? applyObfuscatedIpAddresses(match: match)
+                        : match.endpoint.ipv4Relay.ip,
+                    port: match.endpoint.ipv4Relay.port
+                )),
             hostname: match.relay.hostname,
             location: match.location,
             features: match.relay.features
         )
     }
 
-    private func applyShadowsocksIpAddress(in match: RelaySelectorMatch) -> RelaySelectorMatch {
-        let port = match.endpoint.ipv4Relay.port
-        let portRanges = RelaySelector.parseRawPortRanges(obfuscation.allRelays.wireguard.shadowsocksPortRanges)
-        let portIsWithinRange = portRanges.contains(where: { $0.contains(port) })
-
-        var endpoint = match.endpoint
-
-        // If the currently selected obfuscation port is not within the allowed range (as specified
-        // in the relay list), we should use one of the extra Shadowsocks IP addresses instead of
-        // the default one.
-        if !portIsWithinRange {
-            var ipv4Address = match.endpoint.ipv4Relay.ip
-            if let shadowsocksAddress = match.relay.shadowsocksExtraAddrIn?.randomElement() {
-                ipv4Address = IPv4Address(shadowsocksAddress) ?? ipv4Address
-            }
-
-            endpoint = match.endpoint.override(
-                ipv4Relay: IPv4Endpoint(
-                    ip: ipv4Address,
-                    port: port
-                ))
+    private func applyObfuscatedIpAddresses(match: RelaySelectorMatch) -> IPv4Address {
+        switch obfuscation.method {
+            // If the currently selected obfuscation port is not within the allowed range (as specified
+            // in the relay list), we should use one of the extra Shadowsocks IP addresses instead of
+            // the default one.
+        case .shadowsocks where !shadowsocksPortIsWithinRange(match.endpoint.ipv4Relay.port):
+            applyShadowsocksIpAddress(in: match)
+        case .quic:
+            applyQuicIpAddress(in: match)
+        case .automatic, .off, .on, .udpOverTcp, .shadowsocks:
+            match.endpoint.ipv4Relay.ip
         }
+    }
 
-        return RelaySelectorMatch(endpoint: endpoint, relay: match.relay, location: match.location)
+    private func shadowsocksPortIsWithinRange(_ port: UInt16) -> Bool {
+        let portRanges = RelaySelector.parseRawPortRanges(obfuscation.allRelays.wireguard.shadowsocksPortRanges)
+        return portRanges.contains(where: { $0.contains(port) })
+    }
+
+    private func applyShadowsocksIpAddress(in match: RelaySelectorMatch) -> IPv4Address {
+        let defaultIpv4Address = match.endpoint.ipv4Relay.ip
+        return match.relay.shadowsocksExtraAddrIn?.compactMap({ IPv4Address($0) }).randomElement() ?? defaultIpv4Address
+    }
+
+    private func applyQuicIpAddress(in match: RelaySelectorMatch) -> IPv4Address {
+        let defaultIpv4Address = match.endpoint.ipv4Relay.ip
+        return match.relay.features?.quic?.addrIn.compactMap({ IPv4Address($0) }).randomElement() ?? defaultIpv4Address
     }
 }
