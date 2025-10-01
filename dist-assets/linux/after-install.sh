bf0db039a6f98132ae53528611d0d35969d85f77
Markus Pettersson <markus.pettersson@mullvad.net>
2025-10-01 14:45:41 +0200

    Install AppArmor profile if AppArmor is available

--- dist-assets/linux/after-install.sh
+++ dist-assets/linux/after-install.sh
@@ -7,18 +7,16 @@ systemctl enable "/usr/lib/systemd/system/mullvad-daemon.service"
 systemctl start mullvad-daemon.service || echo "Failed to start mullvad-daemon.service"
 systemctl enable "/usr/lib/systemd/system/mullvad-early-boot-blocking.service"
 
-# return 0 if version $1 is greater than or equal to $2
-function version_is_ge {
-    [ "$1" = "$2" ] && return 0
-    printf '%s\n' "$2" "$1" | sort -C -V
+# Detect if the system is using apparmor. Valid exit codes are: 0, 1, 2 (man aa-status).
+function exists() {
+    command -v "$1" >/dev/null 2>&1
 }
 
-# Ubuntu 24.04 or newer: Install apparmor profile to allow Electron sandbox to work
-# This disables user namespace restrictions
-os=$(grep -oP '^ID=\K.+' /etc/os-release | tr -d '"')
-version=$(grep -oP '^VERSION_ID=\K.+' /etc/os-release | tr -d '"')
+if exists aa-status; then
+    # If that's the case, install our apparmor profile and try to reload apparmor.
+    # The apparmor profile allow Electron sandbox to work.
+    # This disables user namespace restrictions.
 
-if [[ "$os" == "ubuntu" ]] && version_is_ge "$version" "24.04"; then
     echo "Creating apparmor profile"
     cp /opt/Mullvad\ VPN/resources/apparmor_mullvad /etc/apparmor.d/mullvad
     apparmor_parser -r /etc/apparmor.d/mullvad || echo "Failed to reload apparmor profile"
