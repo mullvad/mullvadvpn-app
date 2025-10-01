ff13cdc598ea7dfc856950bc2427db9ec8e96014
Oskar <oskar@mullvad.net>
2025-10-01 18:06:29 +0200

    Go to split tunneling after ipc handlers are setup in test

--- desktop/packages/mullvad-vpn/test/e2e/mocked/split-tunneling/split-tunneling.spec.ts
+++ desktop/packages/mullvad-vpn/test/e2e/mocked/split-tunneling/split-tunneling.spec.ts
@@ -10,11 +10,14 @@ let page: Page;
 let util: MockedTestUtils;
 let routes: RoutesObjectModel;
 
-const startup = async () => {
+const startup = async (postLaunch?: () => Promise<void>) => {
   ({ page, util } = await startMockedApp());
   routes = new RoutesObjectModel(page, util);
 
   await util.expectRoute(RoutePath.main);
+
+  await postLaunch?.();
+
   await routes.main.gotoSettings();
   await routes.settings.gotoSplitTunnelingSettings();
 };
@@ -29,12 +32,10 @@ test.describe('Linux Split tunneling unsupported', () => {
   });
 
   test.beforeAll(async () => {
-    await startup();
-  });
-
-  test.beforeAll(async () => {
-    await util.ipc.linuxSplitTunneling.isSplitTunnelingSupported.handle(false);
-    await util.ipc.linuxSplitTunneling.getApplications.handle(linuxApplicationsList);
+    await startup(async () => {
+      await util.ipc.linuxSplitTunneling.isSplitTunnelingSupported.handle(false);
+      await util.ipc.linuxSplitTunneling.getApplications.handle(linuxApplicationsList);
+    });
   });
 
   test('App should show unsupported dialog when link in header is clicked', async () => {
@@ -77,12 +78,10 @@ test.describe('Linux Split tunneling supported', () => {
   });
 
   test.beforeAll(async () => {
-    await startup();
-  });
-
-  test.beforeAll(async () => {
-    await util.ipc.linuxSplitTunneling.isSplitTunnelingSupported.handle(true);
-    await util.ipc.linuxSplitTunneling.getApplications.handle(linuxApplicationsList);
+    await startup(async () => {
+      await util.ipc.linuxSplitTunneling.isSplitTunnelingSupported.handle(true);
+      await util.ipc.linuxSplitTunneling.getApplications.handle(linuxApplicationsList);
+    });
   });
 
   test('App list items should be shown', async () => {
