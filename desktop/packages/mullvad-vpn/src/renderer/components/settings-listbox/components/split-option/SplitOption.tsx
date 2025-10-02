b949a019f5b2f6556adc551899ce84e768e73da0
Oliver <oliver@mohlin.dev>
2025-10-02 09:24:53 +0200

    Support keyboard navigation on SplitOption

--- desktop/packages/mullvad-vpn/src/renderer/components/settings-listbox/components/split-option/SplitOption.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/settings-listbox/components/split-option/SplitOption.tsx
@@ -1,14 +1,32 @@
+import React from 'react';
+
 import { Flex } from '../../../../lib/components';
 import { Listbox } from '../../../../lib/components/listbox';
 import { ListboxOptionProps } from '../../../../lib/components/listbox/components';
+import { useOptions } from '../../../../lib/hooks';
 import { SplitOptionItem, SplitOptionNavigateButton } from './components';
 
 export type SplitOptionProps<T> = ListboxOptionProps<T>;
 
 function SplitOption<T>({ children, ...props }: SplitOptionProps<T>) {
+  const optionsRef = React.useRef<HTMLDivElement>(null);
+  const [focusedIndex, setFocusedIndex] = React.useState<number | undefined>(undefined);
+  const { handleKeyboardNavigation, handleBlur, handleFocus } = useOptions({
+    optionsRef,
+    orientation: 'horizontal',
+    selector: '[data-split-button="true"]:not([aria-disabled="true"])',
+    focusedIndex,
+    setFocusedIndex,
+  });
+
   return (
-    <Listbox.Option level={1} {...props}>
-      <Flex>{children}</Flex>
+    <Listbox.Option
+      level={1}
+      onKeyDown={handleKeyboardNavigation}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      {...props}>
+      <Flex ref={optionsRef}>{children}</Flex>
     </Listbox.Option>
   );
 }
