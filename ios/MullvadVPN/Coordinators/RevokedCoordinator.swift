aff9f4f9049ae76444acd73d7ca057830774f8cf
Jon Petersson <jon.petersson@mullvad.net>
2025-10-02 14:53:18 +0200

    Rewrite revoked device view in SwiftUI

--- ios/MullvadVPN/Coordinators/RevokedCoordinator.swift
+++ ios/MullvadVPN/Coordinators/RevokedCoordinator.swift
@@ -7,6 +7,7 @@
 //
 
 import Routing
+import SwiftUI
 import UIKit
 
 final class RevokedCoordinator: Coordinator {
@@ -22,13 +23,16 @@ final class RevokedCoordinator: Coordinator {
 
     func start(animated: Bool) {
         let interactor = RevokedDeviceInteractor(tunnelManager: tunnelManager)
-        let controller = RevokedDeviceViewController(interactor: interactor)
+        let viewModel = RevokedDeviceViewModel(interactor: interactor)
 
-        controller.didFinish = { [weak self] in
+        var view = RevokedDeviceView(viewModel: viewModel)
+        view.onLogout = { [weak self] in
             guard let self else { return }
-
             didFinish?(self)
         }
+
+        let controller = UIHostingController(rootView: view)
+        controller.view.setAccessibilityIdentifier(.revokedDeviceView)
 
         navigationController.pushViewController(controller, animated: animated)
     }
