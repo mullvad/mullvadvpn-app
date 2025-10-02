56e43f6fd1a43407dc624a62536c7508cb4b7be4
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-10-02 14:40:39 +0200

    Move Linux Split tunneling unsupported test to top level
    So as to not interfer with other tests, see inline comments.

--- desktop/packages/mullvad-vpn/test/e2e/mocked/split-tunneling/split-tunneling.spec.ts
+++ desktop/packages/mullvad-vpn/test/e2e/mocked/split-tunneling/split-tunneling.spec.ts
@@ -10,7 +10,15 @@ let page: Page;
 let util: MockedTestUtils;
 let routes: RoutesObjectModel;
 
-test.describe('Split tunneling', () => {
+test.describe('Linux Split tunneling unsupported', () => {
+  if (process.platform !== 'linux') {
+    test.skip();
+  }
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
   test.beforeAll(async () => {
     ({ page, util } = await startMockedApp());
     routes = new RoutesObjectModel(page, util);
@@ -20,49 +28,37 @@ test.describe('Split tunneling', () => {
     await routes.settings.gotoSplitTunnelingSettings();
   });
 
-  test.afterAll(async () => {
-    await page.close();
+  test.beforeAll(async () => {
+    await util.ipc.linuxSplitTunneling.isSplitTunnelingSupported.handle(false);
+    await util.ipc.linuxSplitTunneling.getApplications.handle(linuxApplicationsList);
   });
 
-  test.describe('Linux Split tunneling unsupported', () => {
-    if (process.platform !== 'linux') {
-      test.skip();
-    }
+  test('App should show unsupported dialog when link in header is clicked', async () => {
+    // Open the unsupported dialog
+    await routes.splitTunnelingSettings.openUnsupportedDialog();
+    const unsupportedText = routes.splitTunnelingSettings.getSplitTunnelingUnsupportedDialogText();
+    await expect(unsupportedText).toBeVisible();
 
-    test.beforeAll(async () => {
-      await util.ipc.linuxSplitTunneling.isSplitTunnelingSupported.handle(false);
-      await util.ipc.linuxSplitTunneling.getApplications.handle(linuxApplicationsList);
-    });
+    // Close the unsupported dialog
+    await routes.splitTunnelingSettings.closeUnsupportedDialog();
+    await expect(unsupportedText).not.toBeVisible();
+  });
 
-    test('App should show unsupported dialog when link in header is clicked', async () => {
-      // Open the unsupported dialog
-      await routes.splitTunnelingSettings.openUnsupportedDialog();
-      const unsupportedText =
-        routes.splitTunnelingSettings.getSplitTunnelingUnsupportedDialogText();
-      await expect(unsupportedText).toBeVisible();
+  test('App list items should be shown even when split tunneling is unsupported', async () => {
+    // Apps should be shown if split tunneling is unsupported
+    const linuxApplications = routes.splitTunnelingSettings.getLinuxApplications();
+    await expect(linuxApplications).toHaveCount(3);
+  });
 
-      // Close the unsupported dialog
-      await routes.splitTunnelingSettings.closeUnsupportedDialog();
-      await expect(unsupportedText).not.toBeVisible();
-    });
+  test('App list items should show unsupported dialog when clicked', async () => {
+    // Ensure clicking an application in the list makes the unsupported dialog visible
+    const linuxApplications = routes.splitTunnelingSettings.getLinuxApplications();
+    await linuxApplications.first().click();
+    const unsupportedText = routes.splitTunnelingSettings.getSplitTunnelingUnsupportedDialogText();
+    await expect(unsupportedText).toBeVisible();
 
-    test('App list items should be shown even when split tunneling is unsupported', async () => {
-      // Apps should be shown if split tunneling is unsupported
-      const linuxApplications = routes.splitTunnelingSettings.getLinuxApplications();
-      await expect(linuxApplications).toHaveCount(3);
-    });
-
-    test('App list items should show unsupported dialog when clicked', async () => {
-      // Ensure clicking an application in the list makes the unsupported dialog visible
-      const linuxApplications = routes.splitTunnelingSettings.getLinuxApplications();
-      await linuxApplications.first().click();
-      const unsupportedText =
-        routes.splitTunnelingSettings.getSplitTunnelingUnsupportedDialogText();
-      await expect(unsupportedText).toBeVisible();
-
-      // Close the unsupported dialog
-      await routes.splitTunnelingSettings.closeUnsupportedDialog();
-      await expect(unsupportedText).not.toBeVisible();
-    });
+    // Close the unsupported dialog
+    await routes.splitTunnelingSettings.closeUnsupportedDialog();
+    await expect(unsupportedText).not.toBeVisible();
   });
 });
