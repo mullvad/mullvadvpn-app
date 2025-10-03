9e112251e246c1fa4931296d7243afdd06465e35
Oliver <oliver@mohlin.dev>
2025-10-03 11:43:06 +0200

    Use AnimatedList component in device list

--- desktop/packages/mullvad-vpn/src/renderer/components/device-list/DeviceList.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/device-list/DeviceList.tsx
@@ -1,7 +1,7 @@
 import { IDevice } from '../../../shared/daemon-rpc-types';
 import { Flex, Spinner } from '../../lib/components';
+import { AnimatedList } from '../../lib/components/animated-list';
 import { DeviceListItem } from '../device-list-item';
-import List from '../List';
 import { useGetDevices } from './hooks';
 
 const getDeviceKey = (device: IDevice): string => device.id;
@@ -13,9 +13,13 @@ export function DeviceList() {
     <Flex $flexDirection="column" $alignItems="center">
       {!showList && <Spinner size="big" />}
       {showList && (
-        <List items={devices} getKey={getDeviceKey} skipInitialAddTransition>
-          {(device) => <DeviceListItem device={device} />}
-        </List>
+        <AnimatedList>
+          {devices.map((device) => (
+            <AnimatedList.Item key={getDeviceKey(device)}>
+              <DeviceListItem device={device} />
+            </AnimatedList.Item>
+          ))}
+        </AnimatedList>
       )}
     </Flex>
   );
