e25e9779c23392f5b8d697fc441fa22683102253
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-10-06 16:32:14 +0200

    Enable the user to include their account token in problem reports

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/di/UiModule.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/di/UiModule.kt
@@ -137,6 +137,7 @@ val uiModule = module {
             context = androidContext(),
             apiEndpointOverride = getOrNull(),
             apiEndpointFromIntentHolder = get(),
+            accountRepository = get(),
         )
     }
     single { RelayOverridesRepository(get()) }
@@ -268,7 +269,7 @@ val uiModule = module {
     viewModel { VoucherDialogViewModel(get()) }
     viewModel { VpnSettingsViewModel(get(), get(), get(), get(), get(), get()) }
     viewModel { WelcomeViewModel(get(), get(), get(), get(), isPlayBuild = IS_PLAY_BUILD) }
-    viewModel { ReportProblemViewModel(get(), get()) }
+    viewModel { ReportProblemViewModel(get(), get(), get()) }
     viewModel { ViewLogsViewModel(get()) }
     viewModel { OutOfTimeViewModel(get(), get(), get(), get(), get(), isPlayBuild = IS_PLAY_BUILD) }
     viewModel { FilterViewModel(get(), get()) }
