a76e8ffb91c974ad12ea990482d823d7e6181555
mojganii <mojgan.jelodar@mullvad.net>
2025-10-10 11:50:04 +0200

    Store recent connections

---
+++ ios/MullvadSettings/RecentConnectionRepository.swift
@@ -0,0 +1,51 @@
+//
+//  RecentConnectionRepository.swift
+//  MullvadVPN
+//
+//  Created by Mojgan on 2025-09-24.
+//  Copyright © 2025 Mullvad VPN AB. All rights reserved.
+//
+import MullvadLogging
+
+public struct RecentConnectionRepository: RecentConnectionRepositoryProtocol {
+    private let maxLimit: Int
+    private let settingsParser: SettingsParser = {
+        SettingsParser(decoder: JSONDecoder(), encoder: JSONEncoder())
+    }()
+
+    init(maxLimit: Int = 50) {
+        self.maxLimit = maxLimit
+    }
+
+    public func add(_ recentConnection: RecentConnection) throws {
+        var recentConnections = try all()
+        if let index = recentConnections.firstIndex(of: recentConnection) {
+            recentConnections.remove(at: index)
+        }
+        recentConnections.insert(recentConnection, at: 0)
+
+        // keep at most max items (safe even if the list has fewer than max)
+        try write(Array(recentConnections.prefix(maxLimit)))
+    }
+
+    public func clear() throws {
+        try write([])
+    }
+
+    public func all() throws -> [RecentConnection] {
+        try read()
+    }
+}
+
+private extension RecentConnectionRepository {
+    private func read() throws -> [RecentConnection] {
+        let data = try SettingsManager.store.read(key: .recentConnections)
+
+        return try settingsParser.parseUnversionedPayload(as: [RecentConnection].self, from: data)
+    }
+
+    private func write(_ list: [RecentConnection]) throws {
+        let data = try settingsParser.produceUnversionedPayload(list)
+        try SettingsManager.store.write(data, for: .recentConnections)
+    }
+}
