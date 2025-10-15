efbb4ba802b6345f9415c203220d68dd39d1bcb6
Oliver <oliver@mohlin.dev>
2025-10-15 09:58:09 +0200

    Update selectors in too many devices e2e test

--- desktop/packages/mullvad-vpn/test/e2e/route-object-models/too-many-devices/selectors.ts
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/too-many-devices/selectors.ts
@@ -2,4 +2,6 @@ import { Page } from 'playwright';
 
 export const createSelectors = (page: Page) => ({
   continueButton: () => page.getByRole('button', { name: 'Continue' }),
+  removeDeviceButtons: () => page.getByRole('button', { name: 'Remove device named' }),
+  confirmRemoveDeviceButton: () => page.getByRole('button', { name: 'Remove', exact: true }),
 });
