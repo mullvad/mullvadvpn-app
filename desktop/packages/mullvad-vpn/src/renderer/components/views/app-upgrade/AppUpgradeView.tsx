67e12894086bcf630a0dccf5e19e15f2269f62ee
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-10-20 10:59:49 +0200

    Make upgrade details conditionally render based on if an upgrade exists
    Render NoUpdateAvailable if the app upgrade does not exist

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/AppUpgradeView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/AppUpgradeView.tsx
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
 
+import { messages } from '../../../../shared/gettext';
 import { useHistory } from '../../../lib/history';
+import { AppNavigationHeader } from '../../app-navigation-header';
 import { BackAction } from '../../KeyboardNavigation';
+import { SettingsContainer } from '../../Layout';
 import { Layout } from '../../Layout';
+import { NavigationContainer } from '../../NavigationContainer';
+import { NavigationScrollbars } from '../../NavigationScrollbars';
 import { Footer, UpgradeDetails } from './components';
+import { NoUpgradeAvailable } from './components/no-upgrade-available';
+import { useHasUpgrade } from './hooks';
 
 const StyledFooter = styled.div`
   // TODO: Use color from Colors
@@ -15,11 +22,24 @@ const StyledFooter = styled.div`
 
 export const AppUpgradeView = () => {
   const { pop } = useHistory();
+  const hasUpgrade = useHasUpgrade();
 
   return (
     <BackAction action={pop}>
       <Layout>
-        <UpgradeDetails />
+        <SettingsContainer>
+          <NavigationContainer>
+            <AppNavigationHeader
+              title={
+                // TRANSLATORS: Title in navigation bar
+                messages.pgettext('app-upgrade-view', 'Update available')
+              }
+            />
+            <NavigationScrollbars>
+              {hasUpgrade ? <UpgradeDetails /> : <NoUpgradeAvailable />}
+            </NavigationScrollbars>
+          </NavigationContainer>
+        </SettingsContainer>
         <StyledFooter>
           <Footer />
         </StyledFooter>
