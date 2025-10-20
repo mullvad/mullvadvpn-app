3664a46719bc019ad69f9826a3aded66240951c3
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-10-20 10:53:28 +0200

    Add NoUpgradeAvailable component

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/no-upgrade-available/no-upgrade-available.tsx
@@ -0,0 +1,48 @@
+import { sprintf } from 'sprintf-js';
+
+import { messages } from '../../../../../../shared/gettext';
+import { TitleBig } from '../../../../../lib/components';
+import { EmptyState } from '../../../../../lib/components/empty-state';
+import { useVersionCurrent } from '../../../../../redux/hooks';
+import { AppUpgradeHeader } from '../app-upgrade-header';
+
+export function NoUpgradeAvailable() {
+  const { current } = useVersionCurrent();
+
+  return (
+    <AppUpgradeHeader>
+      <TitleBig as="h2">
+        {
+          // TRANSLATORS: Main title for the update available view
+          messages.pgettext('app-upgrade-view', 'Update available')
+        }
+      </TitleBig>
+      <EmptyState
+        variant="success"
+        $alignSelf="stretch"
+        $margin={{
+          top: 'medium',
+        }}>
+        <EmptyState.StatusIcon />
+        <EmptyState.TextContainer>
+          <EmptyState.Title>
+            {
+              // TRANSLATORS: The user has the latest version of the app and does not need to be update
+              messages.pgettext('app-upgrade-view', 'You are using the latest version')
+            }
+          </EmptyState.Title>
+          <EmptyState.Subtitle>
+            {
+              // TRANSLATORS: The the latest version of the app
+              // TRANSLATORS: Available placeholders:
+              // TRANSLATORS: %(version)s - The version number of the latest app version
+              sprintf(messages.pgettext('app-upgrade-view', 'Latest version: %(version)s'), {
+                version: current,
+              })
+            }
+          </EmptyState.Subtitle>
+        </EmptyState.TextContainer>
+      </EmptyState>
+    </AppUpgradeHeader>
+  );
+}
