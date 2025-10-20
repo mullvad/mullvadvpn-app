b35b3ee95fd60b99e08407512ddd765de2933b6a
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-10-20 11:58:17 +0200

    Disable UpgradeButton when no upgrade exists

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/footer/components/initial-footer/components/start-upgrade/StartUpgrade.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/footer/components/initial-footer/components/start-upgrade/StartUpgrade.tsx
@@ -1,12 +1,15 @@
 import { messages } from '../../../../../../../../../../shared/gettext';
 import { Flex } from '../../../../../../../../../lib/components';
+import { useHasUpgrade } from '../../../../../../hooks';
 import { UpgradeButton } from '../../../../../upgrade-button';
 
 export function StartUpgrade() {
+  const hasUpgrade = useHasUpgrade();
+
   return (
     <Flex $padding="large" $flexDirection="column">
       <Flex $flexDirection="column">
-        <UpgradeButton>
+        <UpgradeButton disabled={!hasUpgrade}>
           {
             // TRANSLATORS: Button text to download and install an update
             messages.pgettext('app-upgrade-view', 'Download & install')
