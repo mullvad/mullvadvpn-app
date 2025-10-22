93b350ed313932a8955c0b9905026d837287ee40
Oliver <oliver@mohlin.dev>
2025-10-22 06:23:23 +0200

    Add ImageProps to IconBadge component

--- desktop/packages/mullvad-vpn/src/renderer/lib/icon-badge/IconBadge.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/icon-badge/IconBadge.tsx
@@ -1,14 +1,18 @@
-import { Image } from '../components';
+import styled from 'styled-components';
 
-export interface IconBadgeProps {
+import { Image, ImageProps } from '../components';
+
+export type IconBadgeProps = Omit<ImageProps, 'source'> & {
   state: 'positive' | 'negative';
-}
+};
 
 const sources = {
   positive: 'positive',
   negative: 'negative',
 };
 
-export const IconBadge = ({ state }: IconBadgeProps) => {
-  return <Image source={sources[state]} width={48} height={48} />;
+export const StyledIconBadge = styled(Image)``;
+
+export const IconBadge = ({ state, ...props }: IconBadgeProps) => {
+  return <StyledIconBadge source={sources[state]} width={48} height={48} {...props} />;
 };
