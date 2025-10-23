9dc96581cb49a35a3b4de8d1e5f821a60b4f6103
Oliver <oliver@mohlin.dev>
2025-10-23 15:35:22 +0200

    Add tests for censorship circumvention view

--- desktop/packages/mullvad-vpn/test/e2e/route-object-models/censorship-circumvention/selectors.ts
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/censorship-circumvention/selectors.ts
@@ -5,8 +5,11 @@ export const createSelectors = (page: Page) => ({
   udpOverTcpOption: () => page.getByRole('option', { name: 'UDP-over-TCP' }),
   automaticOption: () =>
     page
-      .getByRole('listbox', { name: 'Obfuscation' })
+      .getByRole('listbox', { name: 'Method' })
       .getByRole('option', { name: 'Automatic', exact: true }),
   shadowsocksSettingsButton: () => page.getByRole('button', { name: 'Shadowsocks settings' }),
   shadowsocksOption: () => page.getByRole('option', { name: 'Shadowsocks' }),
+  lwoOption: () => page.getByRole('option', { name: 'LWO' }),
+  quicOption: () => page.getByRole('option', { name: 'QUIC' }),
+  portObfuscationOption: () => page.getByRole('option', { name: 'Port', exact: true }),
 });
