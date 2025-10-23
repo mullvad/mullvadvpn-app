03406a479fbd7b382ed5ba02a1108e33e53f0288
Oliver <oliver@mohlin.dev>
2025-10-23 07:37:49 +0200

    Move port setting to WireGuard port view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/wireguard-port/WireguardPortView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/wireguard-port/WireguardPortView.tsx
@@ -1,3 +1,49 @@
-export function WireGuardPortView() {
-  return <div>WireGuard Port</div>;
+import { sprintf } from 'sprintf-js';
+
+import { strings } from '../../../../shared/constants';
+import { messages } from '../../../../shared/gettext';
+import { View } from '../../../lib/components/view';
+import { useHistory } from '../../../lib/history';
+import { AppNavigationHeader } from '../..';
+import { BackAction } from '../../KeyboardNavigation';
+import { SettingsContent } from '../../Layout';
+import { NavigationContainer } from '../../NavigationContainer';
+import { NavigationScrollbars } from '../../NavigationScrollbars';
+import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
+import { PortSetting } from './components';
+
+export function WireguardPortView() {
+  const { pop } = useHistory();
+
+  return (
+    <View backgroundColor="darkBlue">
+      <BackAction action={pop}>
+        <NavigationContainer>
+          <AppNavigationHeader
+            title={sprintf(messages.pgettext('wireguard-settings-view', '%(wireGuard)s port'), {
+              wireGuard: strings.wireguard,
+            })}
+          />
+
+          <NavigationScrollbars>
+            <SettingsHeader>
+              <HeaderTitle>
+                {sprintf(
+                  // TRANSLATORS: Page title for WireGuard port view
+                  messages.pgettext('wireguard-settings-view', '%(wireGuard)s port'),
+                  {
+                    wireGuard: strings.wireguard,
+                  },
+                )}
+              </HeaderTitle>
+            </SettingsHeader>
+
+            <SettingsContent>
+              <PortSetting />
+            </SettingsContent>
+          </NavigationScrollbars>
+        </NavigationContainer>
+      </BackAction>
+    </View>
+  );
 }
