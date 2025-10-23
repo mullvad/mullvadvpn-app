643f4419b388bd7b14e1e0cb1df8ec891d61d0b6
Oliver <oliver@mohlin.dev>
2025-10-23 07:04:02 +0200

    Move port and obfuscation settings into censorship circumvention view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/censorship-circumvention/CensorshipCircumventionView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/censorship-circumvention/CensorshipCircumventionView.tsx
@@ -1,3 +1,47 @@
+import { messages } from '../../../../shared/gettext';
+import { View } from '../../../lib/components/view';
+import { useHistory } from '../../../lib/history';
+import { AppNavigationHeader } from '../..';
+import { BackAction } from '../../KeyboardNavigation';
+import { SettingsContent, SettingsGroup } from '../../Layout';
+import { NavigationContainer } from '../../NavigationContainer';
+import { NavigationScrollbars } from '../../NavigationScrollbars';
+import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
+import { ObfuscationSettings, PortSetting } from './components';
+
 export function CensorshipCircumventionView() {
-  return <div></div>;
+  const { pop } = useHistory();
+
+  return (
+    <View backgroundColor="darkBlue">
+      <BackAction action={pop}>
+        <NavigationContainer>
+          <AppNavigationHeader
+            title={messages.pgettext('censorship-circumvention-view', 'Censorship circumvention')}
+          />
+
+          <NavigationScrollbars>
+            <SettingsHeader>
+              <HeaderTitle>
+                {
+                  // TRANSLATORS: Page title for censorship circumvention settings view
+                  messages.pgettext('censorship-circumvention-view', 'Censorship circumvention')
+                }
+              </HeaderTitle>
+            </SettingsHeader>
+
+            <SettingsContent>
+              <SettingsGroup>
+                <ObfuscationSettings />
+              </SettingsGroup>
+
+              <SettingsGroup>
+                <PortSetting />
+              </SettingsGroup>
+            </SettingsContent>
+          </NavigationScrollbars>
+        </NavigationContainer>
+      </BackAction>
+    </View>
+  );
 }
--- desktop/packages/mullvad-vpn/src/renderer/components/views/wireguard-settings/components/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/wireguard-settings/components/index.ts
@@ -1,5 +1,3 @@
 export * from './ip-version-setting';
 export * from './mtu-setting';
-export * from './obfuscation-settings';
-export * from './port-setting';
 export * from './quantum-resistant-setting';
