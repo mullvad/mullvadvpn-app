e69983cf21b921b525595fedce84b0a4dbbb63c7
Oliver <oliver@mohlin.dev>
2025-10-23 07:15:32 +0200

    Update vpn settings rom to go to censorship circumvention settings

--- desktop/packages/mullvad-vpn/test/e2e/route-object-models/vpn-settings/vpn-settings-route-object-model.ts
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/vpn-settings/vpn-settings-route-object-model.ts
@@ -16,9 +16,9 @@ export class VpnSettingsRouteObjectModel extends NavigationObjectModel {
     this.selectors = createSelectors(page);
   }
 
-  async gotoWireguardSettings() {
-    await this.selectors.wireguardSettingsButton().click();
-    await this.utils.expectRoute(RoutePath.wireguardSettings);
+  async gotoCensorshipCircumvention() {
+    await this.selectors.censorshipCircumventionButton().click();
+    await this.utils.expectRoute(RoutePath.censorshipCircumvention);
   }
 
   getAutoConnectSwitch() {
