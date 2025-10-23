58b8af672479fbe2c25e48397f4da8bac56431b1
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-10-23 17:43:36 +0200

    Allow to pass in custom components to formatHtml

--- desktop/packages/mullvad-vpn/src/renderer/lib/html-formatter.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/html-formatter.tsx
@@ -1,58 +1,54 @@
 import React, { JSX } from 'react';
 import styled from 'styled-components';
 
+import { type ValueOfArray } from '../../shared/utility-types';
 import { colors } from './foundations';
 
 const Bold = styled.span({ fontWeight: 700 });
 const Emphasis = styled.em({ color: colors.white, fontWeight: 600 });
 
-// When a new tag is added here, it must also be added to allowed tags in
-// our verify translations script.
-const testMap: Partial<
-  Record<
-    keyof JSX.IntrinsicElements,
-    {
-      test: RegExp;
-      replace: RegExp;
-    }
-  >
-> = {
-  b: {
-    test: /(<b>.*?<\/b>)/g,
-    replace: /<b>|<\/b>/g,
-  },
-  em: {
-    test: /(<em>.*?<\/em>)/g,
-    replace: /<em>|<\/em>/g,
-  },
-} as const;
+export const ALLOWED_TAGS = ['b', 'br', 'em'] as const;
+export type AllowedTags = ValueOfArray<typeof ALLOWED_TAGS>;
 
-const componentMap: Partial<
-  Record<keyof JSX.IntrinsicElements, React.ComponentType<{ children: React.ReactNode }>>
-> = {
+export type TransformerComponent = React.ComponentType<{ children: React.ReactNode }>;
+export type TransformerMap = Record<AllowedTags, TransformerComponent>;
+
+const defaultTransformers: Partial<TransformerMap> = {
   b: Bold,
   em: Emphasis,
-} as const;
+};
 
-export function formatHtml(inputString: string): React.ReactElement {
+export function formatHtml(
+  inputString: string,
+  customTransformers?: Partial<TransformerMap>,
+): React.ReactElement {
+  const transformers = {
+    ...defaultTransformers,
+    ...customTransformers,
+  };
+
   const inputStringArray: Array<string | JSX.Element> = [inputString];
 
-  const transformedStrings = Object.entries(testMap).reduce((strings, [key, { test, replace }]) => {
+  const transformedStrings = Object.entries(transformers).reduce((strings, [key, Component]) => {
     const newStrings = strings.flatMap((value) => {
       if (typeof value === 'string') {
+        const matchPattern = `(<${key}>.*?</${key}>)`;
+        const transformerMatcher = new RegExp(matchPattern, 'g');
+
         // If the value is a string we should see if our current transformer should transform it.
         return value
-          .split(test)
+          .split(transformerMatcher)
           .filter((v) => v.length > 0)
           .map((substring) => {
             // Create a new RegExp object to avoid `lastIndex` side effects, see:
             // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/lastIndex#avoiding_side_effects
-            const tester = new RegExp(test);
 
             // If the value is matched for the current transformer then it should be turned into a component
-            if (tester.test(substring)) {
-              const Component = componentMap[key as keyof typeof componentMap]!;
-              const valueWithoutTags = substring.replaceAll(replace, '');
+            if (transformerMatcher.test(substring)) {
+              const replacePattern = `<${key}>|</${key}>`;
+              const transformReplacer = new RegExp(replacePattern, 'g');
+
+              const valueWithoutTags = substring.replaceAll(transformReplacer, '');
 
               return <Component key={substring}>{valueWithoutTags}</Component>;
             } else {
