9e6f58de9946f28db271e74f3680f03fbd9104e3
Oliver <oliver@mohlin.dev>
2025-10-23 15:04:59 +0200

    Refactor WireGuard settings rom to censorship circumumvention rom

--- desktop/packages/mullvad-vpn/test/e2e/route-object-models/censorship-circumvention/selectors.ts
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/censorship-circumvention/selectors.ts
@@ -3,7 +3,7 @@ import { Page } from 'playwright';
 export const createSelectors = (page: Page) => ({
   udpOverTcpSettingsButton: () => page.getByRole('button', { name: 'UDP-over-TCP settings' }),
   udpOverTcpOption: () => page.getByRole('option', { name: 'UDP-over-TCP' }),
-  automaticObfuscationOption: () =>
+  automaticOption: () =>
     page
       .getByRole('listbox', { name: 'Obfuscation' })
       .getByRole('option', { name: 'Automatic', exact: true }),
