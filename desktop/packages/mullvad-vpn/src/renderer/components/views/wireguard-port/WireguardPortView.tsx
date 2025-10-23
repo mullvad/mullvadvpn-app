21f35aa29eaab82a1839f70a3b0bc2b4b20071d9
Oliver <oliver@mohlin.dev>
2025-10-23 07:37:49 +0200

    Move port setting to WireGuard port view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/wireguard-port/WireguardPortView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/wireguard-port/WireguardPortView.tsx
@@ -1,3 +1,41 @@
-export function WireGuardPortView() {
-  return <div>WireGuard Port</div>;
+import { messages } from '../../../../shared/gettext';
+import { View } from '../../../lib/components/view';
+import { useHistory } from '../../../lib/history';
+import { AppNavigationHeader } from '../..';
+import { BackAction } from '../../KeyboardNavigation';
+import { SettingsContent } from '../../Layout';
+import { NavigationContainer } from '../../NavigationContainer';
+import { NavigationScrollbars } from '../../NavigationScrollbars';
+import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
+import { PortSetting } from './components';
+
+export function WireguardPortView() {
+  const { pop } = useHistory();
+
+  return (
+    <View backgroundColor="darkBlue">
+      <BackAction action={pop}>
+        <NavigationContainer>
+          <AppNavigationHeader
+            title={messages.pgettext('wireguard-settings-view', 'WireGuard port')}
+          />
+
+          <NavigationScrollbars>
+            <SettingsHeader>
+              <HeaderTitle>
+                {
+                  // TRANSLATORS: Page title for WireGuard port view
+                  messages.pgettext('wireguard-settings-view', 'WireGuard port')
+                }
+              </HeaderTitle>
+            </SettingsHeader>
+
+            <SettingsContent>
+              <PortSetting />
+            </SettingsContent>
+          </NavigationScrollbars>
+        </NavigationContainer>
+      </BackAction>
+    </View>
+  );
 }
