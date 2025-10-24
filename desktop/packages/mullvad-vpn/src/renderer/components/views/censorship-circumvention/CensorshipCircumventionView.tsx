74cfdcb6e363ec7c96e7ca8910b8b5fa81fbbe86
Oliver <oliver@mohlin.dev>
2025-10-24 05:43:10 +0200

    Add description text in censorship circumvention view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/censorship-circumvention/CensorshipCircumventionView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/censorship-circumvention/CensorshipCircumventionView.tsx
@@ -1,9 +1,11 @@
 import { messages } from '../../../../shared/gettext';
+import { Text } from '../../../lib/components';
+import { FlexColumn } from '../../../lib/components/flex-column';
 import { View } from '../../../lib/components/view';
+import { Container } from '../../../lib/components/view/components';
 import { useHistory } from '../../../lib/history';
 import { AppNavigationHeader } from '../..';
 import { BackAction } from '../../KeyboardNavigation';
-import { SettingsContent, SettingsGroup } from '../../Layout';
 import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
 import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
@@ -29,12 +31,30 @@ export function CensorshipCircumventionView() {
                 }
               </HeaderTitle>
             </SettingsHeader>
+            <FlexColumn $gap="medium">
+              <Container size="4" $flexDirection="column" $gap="medium">
+                <Text variant="labelTinySemiBold" color="whiteAlpha60">
+                  {
+                    // TRANSLATORS: First paragraph of description text in censorship circumvention settings view
+                    messages.pgettext(
+                      'censorship-circumvention-view',
+                      'Obfuscation methods makes your encrypted VPN traffic look like something else. This can be used to help circumvent censorship and other types of filtering, where a plain connection would be blocked.',
+                    )
+                  }
+                </Text>
+                <Text variant="labelTinySemiBold" color="whiteAlpha60">
+                  {
+                    // TRANSLATORS: Second paragraph of description text in censorship circumvention settings view
+                    messages.pgettext(
+                      'censorship-circumvention-view',
+                      'When “Automatic” is selected, the app tries all methods when connecting until it finds one that works.',
+                    )
+                  }
+                </Text>
+              </Container>
 
-            <SettingsContent>
-              <SettingsGroup>
-                <MethodSetting />
-              </SettingsGroup>
-            </SettingsContent>
+              <MethodSetting />
+            </FlexColumn>
           </NavigationScrollbars>
         </NavigationContainer>
       </BackAction>
