422c5f6c4e5325d2aa31a888606b89fc571a8c25
Oliver <oliver@mohlin.dev>
2025-10-24 05:24:29 +0200

    Add port obfuscation feature indicator

--- desktop/packages/mullvad-vpn/src/renderer/components/views/main/components/connection-panel/components/feature-indicators/hooks/use-get-feature-indicator/useGetFeatureIndicator.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/main/components/connection-panel/components/feature-indicators/hooks/use-get-feature-indicator/useGetFeatureIndicator.ts
@@ -170,6 +170,10 @@ export const useGetFeatureIndicator = () => {
       label: messages.pgettext('wireguard-settings-view', 'Obfuscation'),
       onClick: gotoObfuscation,
     },
+    [FeatureIndicator.port]: {
+      label: messages.pgettext('wireguard-settings-view', 'Obfuscation'),
+      onClick: gotoObfuscation,
+    },
     [FeatureIndicator.multihop]: {
       label:
         // TRANSLATORS: This refers to the multihop setting in the VPN settings view. This is
