766cb4f4edd66aca587899ce19b95c48397b32b2
Oskar <oskar@mullvad.net>
2025-10-27 08:06:33 +0100

    Make release script run from within repo instead of separate directory

--- desktop/scripts/release/release-config.sh
+++ desktop/scripts/release/release-config.sh
@@ -2,7 +2,10 @@
 
 # Configuration variables shared between the release scripts in this directory.
 
+# Where the release scripts and programs store temporary data
+export DATA_DIR="$HOME/.local/share/mullvad-release"
+
 # Where to download app installers locally during the release process.
 # This value is also hardcoded into the `mullvad-release` binary and
 # has to be in sync with that value
-export ARTIFACT_DIR="artifacts"
+export ARTIFACT_DIR="$DATA_DIR/artifacts"
