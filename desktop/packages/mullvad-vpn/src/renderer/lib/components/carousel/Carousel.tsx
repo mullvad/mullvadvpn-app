9127dacc507ddec2c81657c9047507493b7fd6d1
Oliver <oliver@mohlin.dev>
2025-10-27 12:39:37 +0100

    Move CarouselControls component to separate folder

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/Carousel.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/Carousel.tsx
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 
 import { NonEmptyArray } from '../../../../shared/utils';
 import { useStyledRef } from '../../utility-hooks';
-import { Flex, IconButton, Layout } from '..';
-import { PageIndicator } from './components';
+import { CarouselControls } from './components';
 
 const PAGE_GAP = 16;
 
@@ -116,45 +115,5 @@ export function Carousel(props: PageSliderProps) {
         numberOfPages={props.content.length}
       />
     </StyledPageSliderContainer>
-  );
-}
-
-interface CarouselControlsProps {
-  pageNumber: number;
-  numberOfPages: number;
-  hasNext: boolean;
-  hasPrev: boolean;
-  next: () => void;
-  prev: () => void;
-  goToPage: (page: number) => void;
-}
-
-const StyledGrid = styled(Layout)`
-  display: grid;
-  grid-template-columns: 1fr auto 1fr;
-  align-items: center;
-`;
-
-function CarouselControls(props: CarouselControlsProps) {
-  return (
-    <StyledGrid $margin={{ top: 'small' }}>
-      <div>{/* spacer to make page indicators centered */}</div>
-      <Flex $gap="small">
-        {[...Array(props.numberOfPages)].map((_, i) => {
-          const current = i === props.pageNumber;
-          return (
-            <PageIndicator key={i} disabled={current} pageNumber={i} goToPage={props.goToPage} />
-          );
-        })}
-      </Flex>
-      <Flex $justifyContent="right" $gap="small">
-        <IconButton disabled={!props.hasPrev} onClick={props.prev}>
-          <IconButton.Icon icon="chevron-left" />
-        </IconButton>
-        <IconButton disabled={!props.hasNext} onClick={props.next}>
-          <IconButton.Icon icon="chevron-right" />
-        </IconButton>
-      </Flex>
-    </StyledGrid>
   );
 }
