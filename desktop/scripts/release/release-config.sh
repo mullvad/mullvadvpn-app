4cf4d4b6db905b749e15f11b5e2f10494a219021
Oskar <oskar@mullvad.net>
2025-10-27 08:07:02 +0100

    Verify artifacts using code signing key in repo

--- desktop/scripts/release/release-config.sh
+++ desktop/scripts/release/release-config.sh
@@ -9,3 +9,7 @@ export DATA_DIR="$HOME/.local/share/mullvad-release"
 # This value is also hardcoded into the `mullvad-release` binary and
 # has to be in sync with that value
 export ARTIFACT_DIR="$DATA_DIR/artifacts"
+
+# Mullvad code signing key and fingerprint
+export MULLVAD_CODE_SIGNING_KEY_PATH="../../../ci/keys/1.mullvad_signing.pub"
+export MULLVAD_CODE_SIGNING_KEY_FINGERPRINT="A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF"
