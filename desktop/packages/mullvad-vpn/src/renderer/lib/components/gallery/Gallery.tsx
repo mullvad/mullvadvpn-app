c98799a46ffe934258b73726ef9a78b07fd81cce
Oliver <oliver@mohlin.dev>
2025-10-28 06:51:05 +0100

    Add Gallery component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/gallery/Gallery.tsx
@@ -0,0 +1,21 @@
+import { Flex, FlexProps } from '../flex';
+import { Image } from '../image';
+import { GalleryText, GalleryTextGroup } from './components';
+
+export type GalleryProps = FlexProps;
+
+function Gallery({ children, ...props }: GalleryProps) {
+  return (
+    <Flex $flexDirection="column" $gap="small" {...props}>
+      {children}
+    </Flex>
+  );
+}
+
+const GalleryNamespace = Object.assign(Gallery, {
+  Image: Image,
+  Text: GalleryText,
+  TextGroup: GalleryTextGroup,
+});
+
+export { GalleryNamespace as Gallery };
