c2f3511a01e5b2c19cf153300f37c1cf5ab2dd94
Oliver <oliver@mohlin.dev>
2025-10-28 06:57:19 +0100

    Use Carousel components in daita view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/daita-settings/DaitaSettingsView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/daita-settings/DaitaSettingsView.tsx
@@ -1,151 +1,134 @@
-import React from 'react';
 import { sprintf } from 'sprintf-js';
-import styled from 'styled-components';
 
 import { strings } from '../../../../shared/constants';
 import { messages } from '../../../../shared/gettext';
 import { DaitaDirectOnlySetting, DaitaSetting } from '../../../features/daita/components';
-import { Flex, Icon, Image, LabelTiny, LabelTinySemiBold, Text } from '../../../lib/components';
+import { Flex, Icon, Text } from '../../../lib/components';
 import { Carousel } from '../../../lib/components/carousel';
+import { FlexColumn } from '../../../lib/components/flex-column';
+import { View } from '../../../lib/components/view';
 import { useHistory } from '../../../lib/history';
 import { AppNavigationHeader } from '../..';
-import * as Cell from '../../cell';
 import { BackAction } from '../../KeyboardNavigation';
-import { Layout, SettingsContainer } from '../../Layout';
 import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
-import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
+import { HeaderTitle } from '../../SettingsHeader';
 import { useShowDaitaMultihopInfo } from './hooks';
-
-const StyledLabelTinySemiBold = styled(LabelTinySemiBold).attrs({ color: 'whiteAlpha60' })`
-  display: inline-block;
-`;
-
-const StyledLabelTiny = styled(LabelTiny).attrs({ color: 'whiteAlpha60' })`
-  display: inline-block;
-`;
-
-const StyledIllustration = styled(Image)({
-  width: '100%',
-  padding: '8px 0 8px',
-});
 
 export function DaitaSettingsView() {
   const { pop } = useHistory();
   const showDaitaMultihopInfo = useShowDaitaMultihopInfo();
 
   return (
-    <BackAction action={pop}>
-      <Layout>
-        <SettingsContainer>
-          <NavigationContainer>
-            <AppNavigationHeader title={strings.daita} />
+    <View backgroundColor="darkBlue">
+      <BackAction action={pop}>
+        <NavigationContainer>
+          <AppNavigationHeader title={strings.daita} />
 
-            <NavigationScrollbars>
-              <SettingsHeader>
-                <HeaderTitle>{strings.daita}</HeaderTitle>
-                {showDaitaMultihopInfo && (
-                  <Flex $gap="small" $alignItems="center">
-                    <Icon icon="info-circle" color="whiteOnBlue60" size="small" />
-                    <Text variant="labelTinySemiBold" color="whiteAlpha60">
-                      {messages.pgettext(
-                        'wireguard-settings-view',
-                        'Multihop is being used to enable DAITA for your selected location',
-                      )}
-                    </Text>
-                  </Flex>
-                )}
-                <Carousel
-                  content={[
-                    <React.Fragment key="without-daita">
-                      <StyledIllustration source="daita-off-illustration" />
-                      <Flex $flexDirection="column" $gap="medium">
-                        <StyledLabelTinySemiBold>
-                          {messages.pgettext(
-                            // TRANSLATORS: Information to the user that with this setting enabled
-                            // TRANSLATORS: their network and device's battery life will be
-                            // TRANSLATORS: affected negatively.
-                            'wireguard-settings-view',
-                            'Attention: This increases network traffic and will also negatively affect speed, latency, and battery usage. Use with caution on limited plans.',
-                          )}
-                        </StyledLabelTinySemiBold>
-                        <StyledLabelTiny>
-                          {sprintf(
-                            messages.pgettext(
-                              // TRANSLATORS: Information to the user what the DAITA setting does.
-                              // TRANSLATORS: Available placeholders:
-                              // TRANSLATORS: %(daita)s - Will be replaced with DAITA
-                              // TRANSLATORS: %(daitaFull)s - Will be replaced with Defence against AI-guided Traffic Analysis
+          <NavigationScrollbars>
+            <Flex $flexDirection="column" $gap="medium">
+              <View.Container>
+                <FlexColumn $gap="medium">
+                  <HeaderTitle>{strings.daita}</HeaderTitle>
+                  {showDaitaMultihopInfo && (
+                    <Flex $gap="small" $alignItems="center">
+                      <Icon icon="info-circle" color="whiteOnBlue60" size="small" />
+                      <Text variant="labelTinySemiBold" color="whiteAlpha60">
+                        {messages.pgettext(
+                          'wireguard-settings-view',
+                          'Multihop is being used to enable DAITA for your selected location',
+                        )}
+                      </Text>
+                    </Flex>
+                  )}
+
+                  <Carousel
+                    content={[
+                      <Carousel.Page key="without-daita">
+                        <Carousel.Image source="daita-off-illustration" />
+                        <Carousel.TextGroup>
+                          <Carousel.Text variant="labelTinySemiBold">
+                            {messages.pgettext(
+                              // TRANSLATORS: Information to the user that with this setting enabled their network and device's battery life will be
+                              // TRANSLATORS: affected negatively.
                               'wireguard-settings-view',
-                              '%(daita)s (%(daitaFull)s) hides patterns in your encrypted VPN traffic.',
-                            ),
-                            { daita: strings.daita, daitaFull: strings.daitaFull },
-                          )}
-                        </StyledLabelTiny>
-                        <StyledLabelTiny>
-                          {messages.pgettext(
-                            // TRANSLATORS: Information to the user on the background why the DAITA setting exists.
-                            'wireguard-settings-view',
-                            'By using sophisticated AI it’s possible to analyze the traffic of data packets going in and out of your device (even if the traffic is encrypted).',
-                          )}
-                        </StyledLabelTiny>
-                      </Flex>
-                    </React.Fragment>,
-                    <React.Fragment key="with-daita">
-                      <StyledIllustration source="daita-on-illustration" />
-                      <Flex $flexDirection="column" $gap="medium">
-                        <StyledLabelTiny>
-                          {sprintf(
-                            messages.pgettext(
+                              'Attention: This increases network traffic and will also negatively affect speed, latency, and battery usage. Use with caution on limited plans.',
+                            )}
+                          </Carousel.Text>
+                          <Carousel.Text>
+                            {sprintf(
+                              messages.pgettext(
+                                // TRANSLATORS: Information to the user what the DAITA setting does.
+                                // TRANSLATORS: Available placeholders:
+                                // TRANSLATORS: %(daita)s - Will be replaced with DAITA
+                                // TRANSLATORS: %(daitaFull)s - Will be replaced with Defence against AI-guided Traffic Analysis
+                                'wireguard-settings-view',
+                                '%(daita)s (%(daitaFull)s) hides patterns in your encrypted VPN traffic.',
+                              ),
+                              { daita: strings.daita, daitaFull: strings.daitaFull },
+                            )}
+                          </Carousel.Text>
+                          <Carousel.Text>
+                            {messages.pgettext(
                               // TRANSLATORS: Information to the user on the background why the DAITA setting exists.
                               'wireguard-settings-view',
-                              'If an observer monitors these data packets, %(daita)s makes it significantly harder for them to identify which websites you are visiting or with whom you are communicating.',
-                            ),
-                            { daita: strings.daita },
-                          )}
-                        </StyledLabelTiny>
-                        <StyledLabelTiny>
-                          {sprintf(
-                            messages.pgettext(
-                              // TRANSLATORS: Information to the user what the DAITA setting does.
-                              // TRANSLATORS: Available placeholders:
-                              // TRANSLATORS: %(daita)s - Will be replaced with DAITA
-                              'wireguard-settings-view',
-                              '%(daita)s does this by carefully adding network noise and making all network packets the same size.',
-                            ),
-                            { daita: strings.daita },
-                          )}
-                        </StyledLabelTiny>
-                        <StyledLabelTiny>
-                          {sprintf(
-                            messages.pgettext(
-                              // TRANSLATORS: Information to the user that DAITA is not available
-                              // TRANSLATORS: on all servers, however in the background the multihop
-                              // TRANSLATORS: feature is used automatically which enables the use
-                              // TRANSLATORS: of DAITA with any server.
-                              // TRANSLATORS: Available placeholders:
-                              // TRANSLATORS: %(daita)s - Will be replaced with DAITA
-                              'wireguard-settings-view',
-                              'Not all our servers are %(daita)s-enabled. Therefore, we use multihop automatically to enable %(daita)s with any server.',
-                            ),
-                            { daita: strings.daita },
-                          )}
-                        </StyledLabelTiny>
-                      </Flex>
-                    </React.Fragment>,
-                  ]}
-                />
-              </SettingsHeader>
-              <SettingsContainer>
-                <Cell.Group>
-                  <DaitaSetting />
-                  <DaitaDirectOnlySetting />
-                </Cell.Group>
-              </SettingsContainer>
-            </NavigationScrollbars>
-          </NavigationContainer>
-        </SettingsContainer>
-      </Layout>
-    </BackAction>
+                              'By using sophisticated AI it’s possible to analyze the traffic of data packets going in and out of your device (even if the traffic is encrypted).',
+                            )}
+                          </Carousel.Text>
+                        </Carousel.TextGroup>
+                      </Carousel.Page>,
+                      <Carousel.Page key="with-daita">
+                        <Carousel.Image source="daita-on-illustration" />
+                        <Carousel.TextGroup>
+                          <Carousel.Text>
+                            {sprintf(
+                              messages.pgettext(
+                                // TRANSLATORS: Information to the user on the background why the DAITA setting exists.
+                                'wireguard-settings-view',
+                                'If an observer monitors these data packets, %(daita)s makes it significantly harder for them to identify which websites you are visiting or with whom you are communicating.',
+                              ),
+                              { daita: strings.daita },
+                            )}
+                          </Carousel.Text>
+                          <Carousel.Text>
+                            {sprintf(
+                              messages.pgettext(
+                                // TRANSLATORS: Information to the user what the DAITA setting does.
+                                // TRANSLATORS: Available placeholders:
+                                // TRANSLATORS: %(daita)s - Will be replaced with DAITA
+                                'wireguard-settings-view',
+                                '%(daita)s does this by carefully adding network noise and making all network packets the same size.',
+                              ),
+                              { daita: strings.daita },
+                            )}
+                          </Carousel.Text>
+                          <Carousel.Text>
+                            {sprintf(
+                              messages.pgettext(
+                                // TRANSLATORS: Information to the user that DAITA is not available on all servers, however in the background the multihop
+                                // TRANSLATORS: feature is used automatically which enables the use of DAITA with any server.
+                                // TRANSLATORS: Available placeholders:
+                                // TRANSLATORS: %(daita)s - Will be replaced with DAITA
+                                'wireguard-settings-view',
+                                'Not all our servers are %(daita)s-enabled. Therefore, we use multihop automatically to enable %(daita)s with any server.',
+                              ),
+                              { daita: strings.daita },
+                            )}
+                          </Carousel.Text>
+                        </Carousel.TextGroup>
+                      </Carousel.Page>,
+                    ]}
+                  />
+                </FlexColumn>
+              </View.Container>
+              <FlexColumn $padding={{ bottom: 'medium' }}>
+                <DaitaSetting />
+                <DaitaDirectOnlySetting />
+              </FlexColumn>
+            </Flex>
+          </NavigationScrollbars>
+        </NavigationContainer>
+      </BackAction>
+    </View>
   );
 }
