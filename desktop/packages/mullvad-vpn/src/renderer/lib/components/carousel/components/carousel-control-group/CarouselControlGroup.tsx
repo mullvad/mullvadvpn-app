73ce471270d85e0e882d0fdf66b3ea4e5319084e
Oliver <oliver@mohlin.dev>
2025-10-29 06:47:18 +0100

    Refactor Carousel control components and expose from namespace

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-control-group/CarouselControlGroup.tsx
@@ -0,0 +1,11 @@
+import { Flex } from '../../../flex';
+
+export type CarouselControlGroupProps = React.ComponentPropsWithRef<'div'>;
+
+export function CarouselControlGroup({ children, ...props }: CarouselControlGroupProps) {
+  return (
+    <Flex $gap="small" {...props}>
+      {children}
+    </Flex>
+  );
+}
