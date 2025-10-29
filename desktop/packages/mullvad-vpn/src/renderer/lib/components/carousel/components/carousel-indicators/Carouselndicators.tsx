8b123fae4139696dcfea9a2aea2d643acfec7e41
Oliver <oliver@mohlin.dev>
2025-10-29 08:21:31 +0100

    Add ariaLabelTemplate prop to CarouselIndicators

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-indicators/Carouselndicators.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-indicators/Carouselndicators.tsx
@@ -1,14 +1,26 @@
+import { sprintf } from 'sprintf-js';
+
 import { useCarouselContext } from '../../CarouselContext';
 import { CarouselControlGroup } from '../carousel-control-group';
 import { CarouselIndicator } from '../carousel-indicator';
 
-export function CarouselIndicators() {
+export type CarouselIndicatorsProps = React.ComponentPropsWithRef<'div'> & {
+  ariaLabelTemplate?: string;
+};
+
+export function CarouselIndicators({ ariaLabelTemplate, ...props }: CarouselIndicatorsProps) {
   const { numberOfSlides, slideIndex } = useCarouselContext();
+
   return (
-    <CarouselControlGroup>
+    <CarouselControlGroup {...props}>
       {[...Array(numberOfSlides)].map((_, i) => {
+        const ariaLabel = ariaLabelTemplate
+          ? sprintf(ariaLabelTemplate, { index: i + 1 })
+          : undefined;
         const current = i === slideIndex;
-        return <CarouselIndicator key={i} disabled={current} slideToGoTo={i} />;
+        return (
+          <CarouselIndicator key={i} disabled={current} slideToGoTo={i} aria-label={ariaLabel} />
+        );
       })}
     </CarouselControlGroup>
   );
