b897e09eb2da270a1ff493e389a927436f3c4589
Oliver <oliver@mohlin.dev>
2025-10-29 07:41:04 +0100

    Use template literals in CarouselSlide and CarouselSlides

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-slide/CarouselSlide.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-slide/CarouselSlide.tsx
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 
 export type CarouselSlideProps = React.ComponentPropsWithRef<'div'>;
 
-const StyledSlide = styled.div({
-  display: 'inline-block',
-  width: '100%',
-  whiteSpace: 'normal',
-  verticalAlign: 'top',
-  scrollSnapAlign: 'start',
-});
+const StyledSlide = styled.div`
+  display: inline-block;
+  width: 100%;
+  white-space: normal;
+  vertical-align: top;
+  scroll-snap-align: start;
+`;
 
 export function CarouselSlide({ children, ...props }: CarouselSlideProps) {
   return (
