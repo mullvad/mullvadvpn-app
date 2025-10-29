8e55b5bbdc4af598ebe82092457f3f92c550769c
Oliver <oliver@mohlin.dev>
2025-10-29 07:33:39 +0100

    Remove slide gap

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-slide/CarouselSlide.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-slide/CarouselSlide.tsx
@@ -2,18 +2,12 @@ import styled from 'styled-components';
 
 export type CarouselSlideProps = React.ComponentPropsWithRef<'div'>;
 
-const SLIDE_GAP = 16;
-
 const StyledSlide = styled.div({
   display: 'inline-block',
   width: '100%',
   whiteSpace: 'normal',
   verticalAlign: 'top',
   scrollSnapAlign: 'start',
-
-  '&&:not(:last-child)': {
-    marginRight: `${SLIDE_GAP}px`,
-  },
 });
 
 export function CarouselSlide({ children, ...props }: CarouselSlideProps) {
