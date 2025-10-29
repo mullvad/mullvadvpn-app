cbeec68b5d71131772ab2fae9dd43756a4654956
Oliver <oliver@mohlin.dev>
2025-10-29 14:18:54 +0100

    Rename OffOption to NoneOption

--- desktop/packages/mullvad-vpn/src/renderer/components/views/censorship-circumvention/components/method-setting/components/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/censorship-circumvention/components/method-setting/components/index.ts
@@ -1,6 +1,6 @@
 export * from './automatic-option';
 export * from './lwo-option';
-export * from './off-option';
+export * from './none-option';
 export * from './quic-option';
 export * from './shadowsocks-option';
 export * from './udp-over-tcp-option';
