4abf24961565ce2e24a38e35e1acb89bd51e53a4
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-10-30 11:48:42 +0100

    Add html formatting in new device notification subtitle
    Automatic HTML formatting of the subtitle has been removed and has to
    be added explicitly where it should be used.

--- desktop/packages/mullvad-vpn/src/renderer/lib/notifications/new-device.ts
+++ desktop/packages/mullvad-vpn/src/renderer/lib/notifications/new-device.ts
@@ -2,6 +2,7 @@ import { sprintf } from 'sprintf-js';
 
 import { messages } from '../../../shared/gettext';
 import { InAppNotification, InAppNotificationProvider } from '../../../shared/notifications';
+import { formatHtml } from '../html-formatter';
 import { formatDeviceName } from '../utils';
 
 interface NewDeviceNotificationContext {
@@ -19,15 +20,17 @@ export class NewDeviceNotificationProvider implements InAppNotificationProvider 
     return {
       indicator: 'success',
       title: messages.pgettext('in-app-notifications', 'NEW DEVICE CREATED'),
-      subtitle: sprintf(
-        // TRANSLATORS: Notification text when a new device has been created.
-        // TRANSLATORS: Available placeholders:
-        // TRANSLATORS: - %(deviceName)s: Name of created device.
-        messages.pgettext(
-          'in-app-notifications',
-          'This device is now named <em>%(deviceName)s</em>. See more under "Manage devices" in Account.',
+      subtitle: formatHtml(
+        sprintf(
+          // TRANSLATORS: Notification text when a new device has been created.
+          // TRANSLATORS: Available placeholders:
+          // TRANSLATORS: - %(deviceName)s: Name of created device.
+          messages.pgettext(
+            'in-app-notifications',
+            'This device is now named <em>%(deviceName)s</em>. See more under "Manage devices" in Account.',
+          ),
+          { deviceName: formatDeviceName(this.context.deviceName) },
         ),
-        { deviceName: formatDeviceName(this.context.deviceName) },
       ),
       action: { type: 'close', close: this.context.close },
     };
