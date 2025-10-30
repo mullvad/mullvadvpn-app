fdacba0c92232c89ca9281aa9ffb5218be68e9b2
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-10-30 11:45:07 +0100

    Remove automatic HTML formatting of subtitle/subtitle.content
    Each individual notification will have to add back html formatting
    themselves, if they need it.

--- desktop/packages/mullvad-vpn/src/renderer/components/NotificationSubtitle.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/NotificationSubtitle.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import { InAppNotification, InAppNotificationSubtitle } from '../../shared/notifications';
 import { LabelTinySemiBold } from '../lib/components';
 import { Link } from '../lib/components/link';
-import { formatHtml } from '../lib/html-formatter';
 import { ExternalLink } from './ExternalLink';
 import { InternalLink } from './InternalLink';
 
@@ -13,34 +12,33 @@ export type NotificationSubtitleProps = Pick<InAppNotification, 'subtitle'>;
 const StyledLink = styled(Link)``;
 
 const formatSubtitle = (subtitle: InAppNotificationSubtitle) => {
-  const content = formatHtml(subtitle.content);
   if (subtitle.action) {
     switch (subtitle.action.type) {
       case 'navigate-internal':
         return (
           <InternalLink variant="labelTinySemiBold" {...subtitle.action.link}>
-            <InternalLink.Text>{content}</InternalLink.Text>
+            <InternalLink.Text>{subtitle.content}</InternalLink.Text>
           </InternalLink>
         );
       case 'navigate-external':
         return (
           <ExternalLink variant="labelTinySemiBold" {...subtitle.action.link}>
-            <ExternalLink.Text>{content}</ExternalLink.Text>
+            <ExternalLink.Text>{subtitle.content}</ExternalLink.Text>
             <ExternalLink.Icon icon="external" />
           </ExternalLink>
         );
       case 'run-function':
         return (
           <StyledLink forwardedAs="button" variant="labelTinySemiBold" {...subtitle.action.button}>
-            <StyledLink.Text>{content}</StyledLink.Text>
+            <StyledLink.Text>{subtitle.content}</StyledLink.Text>
           </StyledLink>
         );
-
       default:
         break;
     }
   }
-  return content;
+
+  return subtitle.content;
 };
 
 export const NotificationSubtitle = ({ subtitle, ...props }: NotificationSubtitleProps) => {
@@ -51,7 +49,7 @@ export const NotificationSubtitle = ({ subtitle, ...props }: NotificationSubtitl
   if (!Array.isArray(subtitle)) {
     return (
       <LabelTinySemiBold color="whiteAlpha60" {...props}>
-        {formatHtml(subtitle)}
+        {subtitle}
       </LabelTinySemiBold>
     );
   }
