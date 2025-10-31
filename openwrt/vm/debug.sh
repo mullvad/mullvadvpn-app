2fa22e49fc54bc96924c461b5a63409a95f4deea
Markus Pettersson <markus.pettersson@mullvad.net>
2025-10-31 22:09:56 +0100

    Add instructions on how to bootstrap an OpenWRT VM

---
+++ openwrt/vm/debug.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Start the OpenWRT VM in non-persistant mode (to muck around)
+#
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+VM=openwrt.qcow2
+echo "Starting $VM"
+
+qemu-system-x86_64 -M q35 \
+  -drive file="$SCRIPT_DIR/$VM",id=d0,if=none,bus=0,unit=0 \
+  -device ide-hd,drive=d0,bus=ide.0 -nic user,hostfwd=tcp::1337-:22 \
+  -snapshot -nographic
