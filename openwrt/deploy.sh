f06390e9dfa6d5ac664a8192d7a3a5290729057d
Markus Pettersson <markus.pettersson@mullvad.net>
2025-10-31 21:09:41 +0100

    Simplify `deploy.sh` because `package.sh` exists

--- openwrt/deploy.sh
+++ openwrt/deploy.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 # Assuming that an OpenWRT x86 VM is running on localhost and forwards ssh to local port 1337.
+#
+# Run package.sh before running this script to creat the .ipk archive :-)
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ARTIFACTS="$SCRIPT_DIR/../target/x86_64-unknown-linux-musl/release"
+if [ -z "$1" ]; then
+    echo "No .ipk was supplied"
+    echo "Usage: $0 <mullvad_version.arch>.ipk"
+    exit 1
+fi
 
 OPENWRT_USER=root
 OPENWRT_DIR=/root
 
-# Add these binaries to `/usr/bin` so that they are in $PATH.
-scp -P 1337 "$ARTIFACTS/mullvad" "$OPENWRT_USER@localhost:/usr/bin"
-scp -P 1337 "$ARTIFACTS/mullvad-daemon" "$OPENWRT_USER@localhost:/usr/sbin"
-
-# The hack.sh script are workarounds for known issues with mullvad on OpenWRT. See `known-issues.txt` for more info.
-scp -P 1337 "$SCRIPT_DIR/hack.sh" "$OPENWRT_USER@localhost:$OPENWRT_DIR"
+scp -P 1337 "$1" "$OPENWRT_USER@localhost:$OPENWRT_DIR"
