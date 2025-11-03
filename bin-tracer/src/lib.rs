5ae3aeb21225d653c8cef56fca1e0596af3d8d67
Sebastian Holmin <sebastian.holmin@mullvad.net>
2025-11-03 09:44:45 +0100

    More WIP

--- bin-tracer/src/lib.rs
+++ bin-tracer/src/lib.rs
@@ -2,14 +2,15 @@ pub mod utils;
 
 use std::{
     io::Write,
-    sync::{Mutex, RwLock, atomic::AtomicU64},
+    sync::{Mutex, OnceLock, RwLock, atomic::AtomicU64},
 };
 
 use serde::{Deserialize, Serialize};
 use tracing_core::{
-    Dispatch, Kind, Metadata, Subscriber,
+    Callsite, Dispatch, Event, Kind, Metadata, Subscriber,
     callsite::{self, DefaultCallsite},
-    span,
+    field::{self, FieldSet},
+    identify_callsite, span,
 };
 use tracing_serde_structured::{AsSerde, SerializeFieldSet, SerializeId, SerializeMetadata};
 
@@ -24,37 +25,71 @@ pub struct BinarySubscriber {
 #[derive(Serialize, Deserialize)]
 #[serde(bound(deserialize = "'de: 'a"))]
 pub struct BinaryLog<'a> {
-    callsites: Vec<SerializeMetadata<'a>>,
+    metadata_list: Vec<SerializeMetadata<'a>>,
     actions: Vec<TracingAction>,
+}
+
+struct LateInitCallsite(OnceLock<DefaultCallsite>);
+
+impl Callsite for LateInitCallsite {
+    fn set_interest(&self, interest: tracing_core::Interest) {
+        self.0
+            .get()
+            .expect("Callsite impl must not be used before initialization")
+            .set_interest(interest)
+    }
+
+    fn metadata(&self) -> &Metadata<'_> {
+        self.0
+            .get()
+            .expect("Callsite impl must not be used before initialization")
+            .metadata()
+    }
 }
 
 impl BinaryLog<'static> {
     pub fn replay_to_subscriber<S: Subscriber>(&mut self, subscriber: &S) {
-        for callsite in self.callsites.drain(..) {
+        for metadata in self.metadata_list.drain(..) {
+            let callsite = Box::leak(Box::new(LateInitCallsite(OnceLock::new())));
+
+            let SerializeFieldSet::De(cow_strings) = metadata.fields else {
+                unreachable!("We just deserialzied the metadata");
+            };
+            let field_names: Vec<&'static str> = cow_strings
+                .into_iter()
+                .map(|s| Box::leak(s.to_string().into_boxed_str()) as &'static str)
+                .collect();
+            let field_names = Box::new(field_names).leak();
+
+            let fields = FieldSet::new(&field_names[..], identify_callsite!(callsite));
+
             let metadata = Metadata::new(
-                callsite.name.as_str(),
-                callsite.target.as_str(),
-                match callsite.level {
+                metadata.name.as_str(),
+                metadata.target.as_str(),
+                match metadata.level {
                     tracing_serde_structured::SerializeLevel::Trace => tracing_core::Level::TRACE,
                     tracing_serde_structured::SerializeLevel::Debug => tracing_core::Level::DEBUG,
                     tracing_serde_structured::SerializeLevel::Info => tracing_core::Level::INFO,
                     tracing_serde_structured::SerializeLevel::Warn => tracing_core::Level::WARN,
                     tracing_serde_structured::SerializeLevel::Error => tracing_core::Level::ERROR,
                 },
-                callsite.file.map(|f| f.as_str()),
-                callsite.line,
-                callsite.module_path.map(|p| p.as_str()),
-                callsite.fields,
-                if callsite.is_event {
+                metadata.file.map(|f| f.as_str()),
+                metadata.line,
+                metadata.module_path.map(|p| p.as_str()),
+                fields,
+                if metadata.is_event {
                     Kind::EVENT
                 } else {
                     Kind::SPAN
                 },
             );
-            let metadata = Box::new(metadata);
+            let metadata = Box::leak(Box::new(metadata));
+            callsite.0.get_or_init(|| DefaultCallsite::new(metadata));
             // TODO: Figure out how to map SerializeMetadata<'static> to a &'static Metadata<'static>
-            let _ = subscriber.register_callsite(Box::leak(metadata));
+            // TODO: Respect the returned Interest
+            let _ = subscriber.register_callsite(metadata);
         }
+        let mut id_map = std::collections::HashMap::new();
         for action in &self.actions {
             match action {
                 TracingAction::NewSpan {
@@ -62,21 +97,93 @@ impl BinaryLog<'static> {
                     metadata_id,
                     values,
                     parent,
-                } => subscriber.new_span(span),
+                } => {
+                    let metadata: &'static Metadata<'static> = todo!(); // TODO: lookup by metadata_id
+                    let values = todo!(); // TODO: convert Values to ValueSet
+                    // TODO: create a map entry from the returned Id to SerializeId, use it later
+                    let span = span::Attributes::new(metadata, values);
+                    let new_id = subscriber.new_span(&span);
+                    id_map.insert(id.id, new_id);
+                }
                 TracingAction::Event {
                     metadata_id,
                     values,
                     parent,
-                } => todo!(),
-                TracingAction::Enter { span } => todo!(),
-                TracingAction::Exit { span } => todo!(),
+                } => {
+                    // let metadata = callsite.metadata();
+                    let metadata: &'static Metadata<'static> = todo!(); // TODO: lookup by metadata_id
+                    let fields = metadata.fields();
+                    // TODO: values must be an array of constant size to implement ValidLen
+                    // impl<'a, const N: usize> ValidLen<'a> for [(&'a Field, Option<&'a (dyn Value + 'a)>); N] {}
+                    // How am I supposed to get that?
+                    let values = fields
+                        .iter()
+                        .zip(values)
+                        .map(|(field, value)| {
+                            (
+                                &field,
+                                value.as_ref().map(|v| v as &dyn tracing_core::field::Value),
+                            )
+                        })
+                        .collect::<Vec<_>>();
+                    // let message_field = fields.field("message").unwrap();
+                    // #[allow(trivial_casts)] // The compiler is lying, it can't infer this cast
+                    // let values = [(
+                    //     &message_field,
+                    //     Some(&message as &dyn tracing_core::field::Value),
+                    // )];
+
+                    // This function is hidden from docs, but we have to use it
+                    // because there is no other way of obtaining a `ValueSet`.
+                    // It's not entirely clear why it is private. See this issue:
+                    // https://github.com/tokio-rs/tracing/issues/2363
+                    let values = fields.value_set(&values[..]);
+                    let event = Event::new(metadata, &values);
+                    subscriber.event(&event);
+                }
+                TracingAction::Enter { span } => {
+                    let id = id_map[&span.id];
+                    subscriber.enter(&id);
+                }
+                TracingAction::Exit { span } => {
+                    let id = id_map[&span.id];
+                    subscriber.exit(&id);
+                }
                 TracingAction::Record { span, values } => todo!(),
-                TracingAction::RecordFollowsFrom { span, follows } => todo!(),
-                TracingAction::CloneSpan { id } => todo!(),
-                TracingAction::DropSpan { id } => todo!(),
+                TracingAction::RecordFollowsFrom { span, follows } => {
+                    let id = id_map[&span.id];
+                    let follows_id = id_map[&follows.id];
+                    subscriber.record_follows_from(&id, &follows_id);
+                }
+                TracingAction::CloneSpan { id } => {
+                    let original_id = id_map[&id.id];
+                    let _same_id = subscriber.clone_span(&original_id);
+                    // id_map.insert(id.id, _same_id); // This should be the same ID
+                }
+                TracingAction::TryClose { id } => {
+                    let span_id = id_map.remove(&id.id).unwrap();
+                    subscriber.try_close(span_id);
+                }
             }
         }
     }
+}
+
+fn make_field_set(metadata: SerializeMetadata<'_>) -> FieldSet {
+    let callsite = Box::leak(Box::new(LateInitCallsite(OnceLock::new())));
+
+    let SerializeFieldSet::De(cow_strings) = metadata.fields else {
+        unreachable!("We just deserialzied the metadata");
+    };
+    let field_names: Vec<&'static str> = cow_strings
+        .into_iter()
+        .map(|s| Box::leak(s.to_string().into_boxed_str()) as &'static str)
+        .collect();
+    let field_names = Box::new(field_names).leak();
+
+    let fields = FieldSet::new(&field_names[..], identify_callsite!(callsite));
+    callsite.0.get_or_init(|| DefaultCallsite::new(metadata));
+    fields
 }
 
 impl Drop for BinarySubscriber {
@@ -89,7 +196,10 @@ impl Drop for BinarySubscriber {
             .map(|m| m.as_serde())
             .collect();
         let actions = self.actions.lock().unwrap().drain(..).collect();
-        let log = BinaryLog { callsites, actions };
+        let log = BinaryLog {
+            metadata_list: callsites,
+            actions,
+        };
         let file = std::fs::File::create(LOG_PATH).unwrap();
         let mut writer = std::io::BufWriter::new(file);
         serde_json::to_writer(&mut writer, &log).unwrap();
@@ -148,7 +258,7 @@ pub enum TracingAction {
     CloneSpan {
         id: SerializeId,
     },
-    DropSpan {
+    TryClose {
         id: SerializeId,
     },
 }
@@ -272,12 +382,20 @@ impl Subscriber for BinarySubscriber {
         id.clone()
     }
 
-    fn drop_span(&self, id: span::Id) {
+    fn try_close(&self, id: span::Id) -> bool {
         self.actions
             .lock()
             .unwrap()
-            .push(TracingAction::DropSpan { id: id.as_serde() });
+            .push(TracingAction::TryClose { id: id.as_serde() });
+        false
     }
+
+    // fn drop_span(&self, id: span::Id) {
+    //     self.actions
+    //         .lock()
+    //         .unwrap()
+    //         .push(TracingAction::DropSpan { id: id.as_serde() });
+    // }
 
     // Don't think we need these
     // fn on_register_dispatch(&self, subscriber: &tracing_core::Dispatch) {
@@ -291,12 +409,6 @@ impl Subscriber for BinarySubscriber {
     // fn event_enabled(&self, event: &tracing_core::Event<'_>) -> bool {
     //     let _ = event;
     //     true
-    // }
-
-    // fn try_close(&self, id: span::Id) -> bool {
-    //     #[allow(deprecated)]
-    //     self.drop_span(id);
-    //     false
     // }
 
     // fn current_span(&self) -> tracing_core::span::Current {
