a39bfb731faabd7128586b54e8d8c4d3a238ba92
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2025-11-04 16:00:46 +0100

    Add link to Manage devices view in new device notification

--- desktop/packages/mullvad-vpn/src/renderer/lib/notifications/new-device.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/notifications/new-device.tsx
@@ -2,6 +2,8 @@ import { sprintf } from 'sprintf-js';
 
 import { messages } from '../../../shared/gettext';
 import { InAppNotification, InAppNotificationProvider } from '../../../shared/notifications';
+import { RoutePath } from '../../../shared/routes';
+import { InternalLink } from '../../components/InternalLink';
 import { formatHtml } from '../html-formatter';
 import { formatDeviceName } from '../utils';
 
@@ -27,10 +29,17 @@ export class NewDeviceNotificationProvider implements InAppNotificationProvider 
           // TRANSLATORS: - %(deviceName)s: Name of created device.
           messages.pgettext(
             'in-app-notifications',
-            'This device is now named <em>%(deviceName)s</em>. See more under "Manage devices" in Account.',
+            'This device is now named <em>%(deviceName)s</em>. See more under <a>Manage devices</a> in Account.',
           ),
           { deviceName: formatDeviceName(this.context.deviceName) },
         ),
+        {
+          a: (value) => (
+            <InternalLink variant="labelTinySemiBold" to={RoutePath.manageDevices}>
+              <InternalLink.Text>{value}</InternalLink.Text>
+            </InternalLink>
+          ),
+        },
       ),
       action: { type: 'close', close: this.context.close },
     };
