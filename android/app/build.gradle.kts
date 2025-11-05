80f9c51e87cae713cfd299b375b701eaa8fd4b5e
David Göransson <david.goransson@mullvad.net>
2025-11-05 08:39:13 +0100

    Enable PseudoLocales for debug builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -110,6 +110,7 @@ android {
                 "proguard-rules.pro",
             )
         }
+        getByName(BuildTypes.DEBUG) { isPseudoLocalesEnabled = true }
         create(BuildTypes.FDROID) {
             initWith(buildTypes.getByName(BuildTypes.RELEASE))
             signingConfig = null
