661f608d2606018513aacdcc28cabf5afaa97600
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-11-05 15:35:12 +0100

    Move port from relay settings to obfuscation settings

--- android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/ObfuscationSettings.kt
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/ObfuscationSettings.kt
@@ -7,6 +7,7 @@ data class ObfuscationSettings(
     val selectedObfuscationMode: ObfuscationMode,
     val udp2tcp: Udp2TcpObfuscationSettings,
     val shadowsocks: ShadowsocksSettings,
+    val port: Constraint<Port>,
 ) {
     companion object
 }
