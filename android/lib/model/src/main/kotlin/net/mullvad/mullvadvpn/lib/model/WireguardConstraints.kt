d5c75d84e63089a375e8b4e7346d027ff5af6efa
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-11-05 15:35:12 +0100

    Move port from relay settings to obfuscation settings

--- android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/WireguardConstraints.kt
+++ android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/WireguardConstraints.kt
@@ -4,7 +4,6 @@ import arrow.optics.optics
 
 @optics
 data class WireguardConstraints(
-    val port: Constraint<Port>,
     val isMultihopEnabled: Boolean,
     val entryLocation: Constraint<RelayItemId>,
     val ipVersion: Constraint<IpVersion>,
