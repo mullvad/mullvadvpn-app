071ad02a8520aad5e12cab3ea1312b13d8ad9d93
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-11-05 10:34:11 +0100

    Delete partner user accounts after test completion

--- android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/misc/AccountTestRule.kt
+++ android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/misc/AccountTestRule.kt
@@ -1,15 +1,20 @@
 package net.mullvad.mullvadvpn.test.e2e.misc
 
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.extension.AfterEachCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 
-class AccountTestRule(val withTime: Boolean = true) : BeforeEachCallback {
+class AccountTestRule(val withTime: Boolean = true) : BeforeEachCallback, AfterEachCallback {
     lateinit var validAccountNumber: String
     lateinit var invalidAccountNumber: String
 
     override fun beforeEach(context: ExtensionContext): Unit = runBlocking {
         validAccountNumber = AccountProvider.getValidAccountNumber(withTime)
         invalidAccountNumber = AccountProvider.getInvalidAccountNumber()
+    }
+
+    override fun afterEach(context: ExtensionContext?): Unit = runBlocking {
+        AccountProvider.tryDeletePartnerAccount(validAccountNumber)
     }
 }
