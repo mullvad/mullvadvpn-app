b87c35f12f839a9f48270ed87f1f5eb451d1acbb
Steffen <steffen.ernst@mullvad.net>
2025-11-06 08:44:35 +0100

    Add new ios multihop selection view

--- ios/MullvadVPN/View controllers/SelectLocation/Views/SelectLocationView.swift
+++ ios/MullvadVPN/View controllers/SelectLocation/Views/SelectLocationView.swift
@@ -4,46 +4,93 @@ import SwiftUI
 struct SelectLocationView<ViewModel>: View where ViewModel: SelectLocationViewModel {
     @ObservedObject var viewModel: ViewModel
 
-    var showSearchField: Bool { !viewModel.showDAITAInfo || viewModel.multihopContext == .exit }
+    @State private var isAtTopOfList: Bool = false
+    @State private var headerHeight: CGFloat = 0
+
+    var showSearchField: Bool {
+        return !viewModel.showDAITAInfo || viewModel.multihopContext == .exit
+    }
 
     var body: some View {
-        VStack(spacing: 16) {
-            if viewModel.isMultihopEnabled {
-                SegmentedControl(
-                    segments: MultihopContext.allCases,
-                    selectedSegment: $viewModel.multihopContext
+        // Simply animating the MultihopSelectionView while scrolling leads to a slow
+        // down of the scrolling during the animation. Instead of changing the size of the scroll
+        // view, the top margin of the content is changed which solves the animation issues.
+        ZStack(alignment: .topLeading) {
+            VStack(spacing: 16) {
+                MultihopSelectionView(
+                    hops: (viewModel.isMultihopEnabled ? MultihopContext.allCases : [MultihopContext.exit])
+                        .map {
+                            Hop(
+                                multihopContext: $0,
+                                selectedLocation: $0 == .entry
+                                    ? viewModel.entryContext.selectedLocation : viewModel.exitContext.selectedLocation
+                            )
+                        },
+                    selectedMultihopContext: $viewModel.multihopContext,
+                    isExpanded: isAtTopOfList
                 )
-                .padding(.horizontal)
-            }
-            if showSearchField {
-                MullvadSecondaryTextField(
-                    placeholder: "Search for locations or servers",
-                    text: $viewModel.searchText
-                )
-                .padding(.horizontal)
-                .animation(.default, value: showSearchField)
-                .transition(.move(edge: .top).combined(with: .opacity))
-            }
-            switch viewModel.multihopContext {
-            case .exit:
-                ExitLocationView(
-                    viewModel: viewModel,
-                    context: $viewModel.exitContext
-                )
-                .transition(
-                    .move(edge: .trailing).combined(with: .opacity)
-                )
-                .geometryGroup()
-            case .entry:
-                EntryLocationView(viewModel: viewModel)
-                    .transition(
-                        .move(edge: .leading).combined(with: .opacity)
+                .padding(.horizontal, 16)
+                if showSearchField {
+                    MullvadSecondaryTextField(
+                        placeholder: "Search for locations or servers",
+                        text: $viewModel.searchText
                     )
-                    .geometryGroup()
+                    .padding(.horizontal)
+                    .transition(.move(edge: .top).combined(with: .opacity))
+                }
+            }
+            .padding(.vertical)
+            .background(Color.mullvadDarkBackground)
+            .zIndex(1)
+            .sizeOfView { size in
+                withAnimation {
+                    headerHeight = size.height
+                }
+            }
+            VStack {
+                // Prevent scroll content from touching navigation bar to avoid a change of appearence
+                // see `UINavigationBar+Appearance.swift`
+                Spacer()
+                    .frame(height: 1)
+                Group {
+                    switch viewModel.multihopContext {
+                    case .exit:
+                        ExitLocationView(
+                            viewModel: viewModel,
+                            context: $viewModel.exitContext,
+                            onShowsTopOfTheListChange: { onTop in
+                                withAnimation {
+                                    isAtTopOfList = onTop
+                                }
+                            }
+                        )
+                        .transition(
+                            .move(edge: .trailing).combined(with: .opacity)
+                        )
+                    case .entry:
+                        EntryLocationView(
+                            viewModel: viewModel,
+                            onShowsTopOfTheListChange: { onTop in
+                                withAnimation {
+                                    isAtTopOfList = onTop
+                                }
+                            }
+                        )
+                        .transition(
+                            .move(edge: .leading).combined(with: .opacity)
+                        )
+                    }
+                }
+                .geometryGroup()
+                // Adds margin to the top of the scroll content. The scroll views size stays untouched
+                // which seems to be the solution to animation issues.
+                .contentMargins(.top, headerHeight - 1)
+                .zIndex(0)
             }
         }
+        .animation(.default, value: showSearchField)
         .animation(.default, value: viewModel.multihopContext)
-        .background(Color.mullvadBackground)
+        .background(Color.mullvadDarkBackground)
         .navigationTitle("Select location")
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
