0d73fe96375a06213d8a32f4710d35c94013f74e
Oliver <oliver@mohlin.dev>
2025-11-07 07:38:33 +0100

    Extract beta switch

--- desktop/packages/mullvad-vpn/src/renderer/components/views/app-info/components/beta-list-item/BetaListItem.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/app-info/components/beta-list-item/BetaListItem.tsx
@@ -1,36 +1,48 @@
+import React from 'react';
+
 import { messages } from '../../../../../../shared/gettext';
-import { useSettingsShowBetaReleases, useVersionIsBeta } from '../../../../../redux/hooks';
-import { SettingsToggleListItem } from '../../../../settings-toggle-list-item';
+import { useVersionIsBeta } from '../../../../../redux/hooks';
+import { SettingsListItem } from '../../../../settings-list-item';
+import { BetaSwitch } from '../beta-switch';
 
 export function BetaListItem() {
   const { isBeta } = useVersionIsBeta();
-  const { showBetaReleases, setShowBetaReleases } = useSettingsShowBetaReleases();
+
+  const labelId = React.useId();
+  const descriptionId = React.useId();
 
   return (
-    <SettingsToggleListItem
-      checked={showBetaReleases}
-      onCheckedChange={setShowBetaReleases}
-      disabled={isBeta}
-      description={
-        isBeta
-          ? // TRANSLATORS: Description for beta program switch when using a beta version.
-            messages.pgettext(
-              'app-info-view',
-              'This option is unavailable while using a beta version.',
-            )
-          : // TRANSLATORS: Description for beta program switch.
-            messages.pgettext(
-              'app-info-view',
-              'Enable to get notified when new beta versions of the app are released.',
-            )
-      }>
-      <SettingsToggleListItem.Label>
-        {
-          // TRANSLATORS: Label for switch to toggle beta program.
-          messages.pgettext('app-info-view', 'Beta program')
-        }
-      </SettingsToggleListItem.Label>
-      <SettingsToggleListItem.Switch />
-    </SettingsToggleListItem>
+    <SettingsListItem disabled={isBeta}>
+      <SettingsListItem.Item>
+        <SettingsListItem.Content>
+          <BetaSwitch labelId={labelId} descriptionId={descriptionId}>
+            <BetaSwitch.Label variant="titleMedium">
+              {
+                // TRANSLATORS: Label for switch to toggle beta program.
+                messages.pgettext('app-info-view', 'Beta program')
+              }
+            </BetaSwitch.Label>
+            <BetaSwitch.Trigger>
+              <BetaSwitch.Thumb />
+            </BetaSwitch.Trigger>
+          </BetaSwitch>
+        </SettingsListItem.Content>
+      </SettingsListItem.Item>
+      <SettingsListItem.Footer>
+        <SettingsListItem.Text id={descriptionId}>
+          {isBeta
+            ? // TRANSLATORS: Description for beta program switch when using a beta version.
+              messages.pgettext(
+                'app-info-view',
+                'This option is unavailable while using a beta version.',
+              )
+            : // TRANSLATORS: Description for beta program switch.
+              messages.pgettext(
+                'app-info-view',
+                'Enable to get notified when new beta versions of the app are released.',
+              )}
+        </SettingsListItem.Text>
+      </SettingsListItem.Footer>
+    </SettingsListItem>
   );
 }
