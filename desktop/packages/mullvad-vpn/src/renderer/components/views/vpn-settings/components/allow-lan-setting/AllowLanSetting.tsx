f1a732eb0a6395da9908a02071779f0e4a6da9c5
Oliver <oliver@mohlin.dev>
2025-11-10 07:23:06 +0100

    Extract allow lan switch component

--- desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/allow-lan-setting/AllowLanSetting.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/allow-lan-setting/AllowLanSetting.tsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 import { messages } from '../../../../../../shared/gettext';
-import { useAppContext } from '../../../../../context';
 import { spacings } from '../../../../../lib/foundations';
-import { useSelector } from '../../../../../redux/store';
 import InfoButton from '../../../../InfoButton';
 import { ModalMessage } from '../../../../Modal';
-import { SettingsToggleListItem } from '../../../../settings-toggle-list-item';
+import { SettingsListItem } from '../../../../settings-list-item';
+import { AllowLanSwitch } from './AllowLanSwitch';
 
 const LanIpRanges = styled.ul({
   listStyle: 'disc outside',
@@ -14,42 +13,45 @@ const LanIpRanges = styled.ul({
 });
 
 export function AllowLanSetting() {
-  const allowLan = useSelector((state) => state.settings.allowLan);
-  const { setAllowLan } = useAppContext();
-
   return (
-    <SettingsToggleListItem
-      anchorId="allow-lan-setting"
-      checked={allowLan}
-      onCheckedChange={setAllowLan}>
-      <SettingsToggleListItem.Label>
-        {messages.pgettext('vpn-settings-view', 'Local network sharing')}
-      </SettingsToggleListItem.Label>
-      <SettingsToggleListItem.Group>
-        <InfoButton>
-          <ModalMessage>
-            {messages.pgettext(
-              'vpn-settings-view',
-              'This feature allows access to other devices on the local network, such as for sharing, printing, streaming, etc.',
-            )}
-          </ModalMessage>
-          <ModalMessage>
-            {messages.pgettext(
-              'vpn-settings-view',
-              'It does this by allowing network communication outside the tunnel to local multicast and broadcast ranges as well as to and from these private IP ranges:',
-            )}
-            <LanIpRanges>
-              <li>10.0.0.0/8</li>
-              <li>172.16.0.0/12</li>
-              <li>192.168.0.0/16</li>
-              <li>169.254.0.0/16</li>
-              <li>fe80::/10</li>
-              <li>fc00::/7</li>
-            </LanIpRanges>
-          </ModalMessage>
-        </InfoButton>
-        <SettingsToggleListItem.Switch />
-      </SettingsToggleListItem.Group>
-    </SettingsToggleListItem>
+    <SettingsListItem anchorId="allow-lan-setting">
+      <SettingsListItem.Item>
+        <SettingsListItem.Content>
+          <AllowLanSwitch>
+            <AllowLanSwitch.Label variant="titleMedium">
+              {messages.pgettext('vpn-settings-view', 'Local network sharing')}
+            </AllowLanSwitch.Label>
+            <SettingsListItem.Group>
+              <InfoButton>
+                <ModalMessage>
+                  {messages.pgettext(
+                    'vpn-settings-view',
+                    'This feature allows access to other devices on the local network, such as for sharing, printing, streaming, etc.',
+                  )}
+                </ModalMessage>
+                <ModalMessage>
+                  {messages.pgettext(
+                    'vpn-settings-view',
+                    'It does this by allowing network communication outside the tunnel to local multicast and broadcast ranges as well as to and from these private IP ranges:',
+                  )}
+                  <LanIpRanges>
+                    <li>10.0.0.0/8</li>
+                    <li>172.16.0.0/12</li>
+                    <li>192.168.0.0/16</li>
+                    <li>169.254.0.0/16</li>
+                    <li>fe80::/10</li>
+                    <li>fc00::/7</li>
+                  </LanIpRanges>
+                </ModalMessage>
+              </InfoButton>
+
+              <AllowLanSwitch.Trigger>
+                <AllowLanSwitch.Thumb />
+              </AllowLanSwitch.Trigger>
+            </SettingsListItem.Group>
+          </AllowLanSwitch>
+        </SettingsListItem.Content>
+      </SettingsListItem.Item>
+    </SettingsListItem>
   );
 }
