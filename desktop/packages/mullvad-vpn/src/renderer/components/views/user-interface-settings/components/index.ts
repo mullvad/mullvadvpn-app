e4cfed77bd200c30b9f06747d0f9d6343b5c8f30
Oliver <oliver@mohlin.dev>
2025-11-10 10:26:38 +0100

    Move start minimized setting and switch to feature folder

--- desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/index.ts
@@ -2,4 +2,3 @@ export * from './animate-map-setting';
 export * from './language-list-item';
 export * from './monochromatic-tray-icon-setting';
 export * from './notifications-setting';
-export * from './start-minimized-setting';
