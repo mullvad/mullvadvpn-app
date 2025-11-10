a10f2635574f902db071b88ba21481d483130a23
Oliver <oliver@mohlin.dev>
2025-11-10 07:09:37 +0100

    Extract enable ipv6 switch component

--- desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/enable-ipv6-setting/EnableIpv6Setting.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/enable-ipv6-setting/EnableIpv6Setting.tsx
@@ -1,60 +1,58 @@
-import { useCallback } from 'react';
+import React from 'react';
 
 import { messages } from '../../../../../../shared/gettext';
-import log from '../../../../../../shared/logging';
-import { useAppContext } from '../../../../../context';
-import { useSelector } from '../../../../../redux/store';
 import InfoButton from '../../../../InfoButton';
 import { ModalMessage } from '../../../../Modal';
-import { SettingsToggleListItem } from '../../../../settings-toggle-list-item';
+import { SettingsListItem } from '../../../../settings-list-item';
+import { EnableIpv6Switch } from './EnableIpv6Switch';
 
 export function EnableIpv6Setting() {
-  const enableIpv6 = useSelector((state) => state.settings.enableIpv6);
-  const { setEnableIpv6: setEnableIpv6Impl } = useAppContext();
-
-  const setEnableIpv6 = useCallback(
-    async (enableIpv6: boolean) => {
-      try {
-        await setEnableIpv6Impl(enableIpv6);
-      } catch (e) {
-        const error = e as Error;
-        log.error('Failed to update enable IPv6', error.message);
-      }
-    },
-    [setEnableIpv6Impl],
-  );
-
+  const descriptionId = React.useId();
   return (
-    <SettingsToggleListItem
-      checked={enableIpv6}
-      onCheckedChange={setEnableIpv6}
-      description={
-        // TRANSLATORS: Description of in-tunnel IPv6 setting.
-        messages.pgettext('vpn-settings-view', 'Enable to allow IPv6 traffic through the tunnel.')
-      }>
-      <SettingsToggleListItem.Label>
-        {
-          // TRANSLATORS: Title of in-tunnel IPv6 setting.
-          messages.pgettext('vpn-settings-view', 'In-tunnel IPv6')
-        }
-      </SettingsToggleListItem.Label>
-      <SettingsToggleListItem.Group>
-        <InfoButton>
-          <ModalMessage>
-            {messages.pgettext(
+    <SettingsListItem>
+      <SettingsListItem.Item>
+        <SettingsListItem.Content>
+          <EnableIpv6Switch>
+            <EnableIpv6Switch.Label variant="titleMedium">
+              {
+                // TRANSLATORS: Title of in-tunnel IPv6 setting.
+                messages.pgettext('vpn-settings-view', 'In-tunnel IPv6')
+              }
+            </EnableIpv6Switch.Label>
+            <SettingsListItem.Group>
+              <InfoButton>
+                <ModalMessage>
+                  {messages.pgettext(
+                    'vpn-settings-view',
+                    'When this feature is enabled, IPv6 can be used alongside IPv4 in the VPN tunnel to communicate with internet services.',
+                  )}
+                </ModalMessage>
+                <ModalMessage>
+                  {messages.pgettext(
+                    'vpn-settings-view',
+                    'IPv4 is always enabled and the majority of websites and applications use this protocol. We do not recommend enabling IPv6 unless you know you need it.',
+                  )}
+                </ModalMessage>
+              </InfoButton>
+
+              <EnableIpv6Switch.Trigger aria-describedby={descriptionId}>
+                <EnableIpv6Switch.Thumb />
+              </EnableIpv6Switch.Trigger>
+            </SettingsListItem.Group>
+          </EnableIpv6Switch>
+        </SettingsListItem.Content>
+      </SettingsListItem.Item>
+      <SettingsListItem.Footer>
+        <SettingsListItem.Text id={descriptionId}>
+          {
+            // TRANSLATORS: Description of in-tunnel IPv6 setting.
+            messages.pgettext(
               'vpn-settings-view',
-              'When this feature is enabled, IPv6 can be used alongside IPv4 in the VPN tunnel to communicate with internet services.',
-            )}
-          </ModalMessage>
-          <ModalMessage>
-            {messages.pgettext(
-              'vpn-settings-view',
-              'IPv4 is always enabled and the majority of websites and applications use this protocol. We do not recommend enabling IPv6 unless you know you need it.',
-            )}
-          </ModalMessage>
-        </InfoButton>
-        <SettingsToggleListItem.Switch />
-      </SettingsToggleListItem.Group>
-    </SettingsToggleListItem>
+              'Enable to allow IPv6 traffic through the tunnel.',
+            )
+          }
+        </SettingsListItem.Text>
+      </SettingsListItem.Footer>
+    </SettingsListItem>
   );
 }
---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/enable-ipv6-setting/EnableIpv6Switch.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import log from '../../../../../../shared/logging';
+import { useAppContext } from '../../../../../context';
+import { useEnableIpv6 } from '../../../../../features/tunnel/hooks';
+import { Switch, SwitchProps } from '../../../../../lib/components/switch';
+
+export type EnableIpv6SwitchProps = SwitchProps;
+
+function EnableIpv6Switch({ children, ...props }: EnableIpv6SwitchProps) {
+  const enableIpv6 = useEnableIpv6();
+  const { setEnableIpv6: setEnableIpv6Impl } = useAppContext();
+
+  const setEnableIpv6 = React.useCallback(
+    async (enableIpv6: boolean) => {
+      try {
+        await setEnableIpv6Impl(enableIpv6);
+      } catch (e) {
+        const error = e as Error;
+        log.error('Failed to update enable IPv6', error.message);
+      }
+    },
+    [setEnableIpv6Impl],
+  );
+
+  return (
+    <Switch checked={enableIpv6} onCheckedChange={setEnableIpv6} {...props}>
+      {children}
+    </Switch>
+  );
+}
+
+const EnableIpv6SwitchNamespace = Object.assign(EnableIpv6Switch, {
+  Label: Switch.Label,
+  Thumb: Switch.Thumb,
+  Trigger: Switch.Trigger,
+});
+
+export { EnableIpv6SwitchNamespace as EnableIpv6Switch };
