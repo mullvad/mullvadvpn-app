768c8ad02d71894a2116a251677d4bf36090d4c8
Oliver <oliver@mohlin.dev>
2025-11-10 07:44:52 +0100

    Extract auto start switch component

--- desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/auto-start-setting/AutoStartSetting.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/auto-start-setting/AutoStartSetting.tsx
@@ -1,33 +1,22 @@
-import { useCallback } from 'react';
-
 import { messages } from '../../../../../../shared/gettext';
-import log from '../../../../../../shared/logging';
-import { useAppContext } from '../../../../../context';
-import { useSelector } from '../../../../../redux/store';
-import { SettingsToggleListItem } from '../../../../settings-toggle-list-item';
+import { SettingsListItem } from '../../../../settings-list-item';
+import { AutoStartSwitch } from './AutoStartSwitch';
 
 export function AutoStartSetting() {
-  const autoStart = useSelector((state) => state.settings.autoStart);
-  const { setAutoStart: setAutoStartImpl } = useAppContext();
-
-  const setAutoStart = useCallback(
-    async (autoStart: boolean) => {
-      try {
-        await setAutoStartImpl(autoStart);
-      } catch (e) {
-        const error = e as Error;
-        log.error(`Cannot set auto-start: ${error.message}`);
-      }
-    },
-    [setAutoStartImpl],
-  );
-
   return (
-    <SettingsToggleListItem checked={autoStart} onCheckedChange={setAutoStart}>
-      <SettingsToggleListItem.Label>
-        {messages.pgettext('vpn-settings-view', 'Launch app on start-up')}
-      </SettingsToggleListItem.Label>
-      <SettingsToggleListItem.Switch />
-    </SettingsToggleListItem>
+    <SettingsListItem>
+      <SettingsListItem.Item>
+        <SettingsListItem.Content>
+          <AutoStartSwitch>
+            <AutoStartSwitch.Label variant="titleMedium">
+              {messages.pgettext('vpn-settings-view', 'Launch app on start-up')}
+            </AutoStartSwitch.Label>
+            <AutoStartSwitch.Trigger>
+              <AutoStartSwitch.Thumb />
+            </AutoStartSwitch.Trigger>
+          </AutoStartSwitch>
+        </SettingsListItem.Content>
+      </SettingsListItem.Item>
+    </SettingsListItem>
   );
 }
