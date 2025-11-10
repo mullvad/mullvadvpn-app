cd10d4bda635d0a98134cfbbc8a747d3e36f0432
Oliver <oliver@mohlin.dev>
2025-11-10 07:48:06 +0100

    Move auto start setting and switch to feature folder

--- desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/vpn-settings/components/index.ts
@@ -1,5 +1,4 @@
 export * from './custom-dns-settings';
-export * from './auto-start-setting';
 export * from './dns-blocker-settings';
 export * from './ip-override-settings';
 export * from './kill-switch-setting';
