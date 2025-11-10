94a2f3c8c73939396291640c485e1b6651c83880
Oliver <oliver@mohlin.dev>
2025-11-10 10:57:08 +0100

    Add use enable system notification hook

--- desktop/packages/mullvad-vpn/src/renderer/features/client/hooks/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/features/client/hooks/index.ts
@@ -1,4 +1,5 @@
 export * from './use-auto-connect';
 export * from './use-auto-start';
+export * from './use-enable-system-notifications';
 export * from './use-start-minimized';
 export * from './use-unpinned-window';
