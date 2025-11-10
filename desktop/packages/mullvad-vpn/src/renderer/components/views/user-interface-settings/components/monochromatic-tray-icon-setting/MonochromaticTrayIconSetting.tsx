f4f7de950b7d635979ba54be5de60ff4fd996248
Oliver <oliver@mohlin.dev>
2025-11-10 11:23:01 +0100

    Extract monochromatic tray icon switch component

--- desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/monochromatic-tray-icon-setting/MonochromaticTrayIconSetting.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/monochromatic-tray-icon-setting/MonochromaticTrayIconSetting.tsx
@@ -1,24 +1,33 @@
+import React from 'react';
+
 import { messages } from '../../../../../../shared/gettext';
-import { useAppContext } from '../../../../../context';
-import { useSelector } from '../../../../../redux/store';
-import { SettingsToggleListItem } from '../../../../settings-toggle-list-item';
+import { SettingsListItem } from '../../../../settings-list-item';
+import { MonochromaticTrayIconSwitch } from './MonochromaticTrayIconSwitch';
 
 export function MonochromaticTrayIconSetting() {
-  const monochromaticIcon = useSelector((state) => state.settings.guiSettings.monochromaticIcon);
-  const { setMonochromaticIcon } = useAppContext();
-
+  const descriptionId = React.useId();
   return (
-    <SettingsToggleListItem
-      checked={monochromaticIcon}
-      onCheckedChange={setMonochromaticIcon}
-      description={messages.pgettext(
-        'user-interface-settings-view',
-        'Use a monochromatic tray icon instead of a colored one.',
-      )}>
-      <SettingsToggleListItem.Label>
-        {messages.pgettext('user-interface-settings-view', 'Monochromatic tray icon')}
-      </SettingsToggleListItem.Label>
-      <SettingsToggleListItem.Switch />
-    </SettingsToggleListItem>
+    <SettingsListItem>
+      <SettingsListItem.Item>
+        <SettingsListItem.Content>
+          <MonochromaticTrayIconSwitch>
+            <MonochromaticTrayIconSwitch.Label variant="titleMedium">
+              {messages.pgettext('user-interface-settings-view', 'Monochromatic tray icon')}
+            </MonochromaticTrayIconSwitch.Label>
+            <MonochromaticTrayIconSwitch.Trigger aria-describedby={descriptionId}>
+              <MonochromaticTrayIconSwitch.Thumb />
+            </MonochromaticTrayIconSwitch.Trigger>
+          </MonochromaticTrayIconSwitch>
+        </SettingsListItem.Content>
+      </SettingsListItem.Item>
+      <SettingsListItem.Footer>
+        <SettingsListItem.Text id={descriptionId}>
+          {messages.pgettext(
+            'user-interface-settings-view',
+            'Use a monochromatic tray icon instead of a colored one.',
+          )}
+        </SettingsListItem.Text>
+      </SettingsListItem.Footer>
+    </SettingsListItem>
   );
 }
---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/user-interface-settings/components/monochromatic-tray-icon-setting/MonochromaticTrayIconSwitch.tsx
@@ -0,0 +1,24 @@
+import { useAppContext } from '../../../../../context';
+import { useMonochromaticTrayIcon } from '../../../../../features/client/hooks';
+import { Switch, SwitchProps } from '../../../../../lib/components/switch';
+
+export type MonochromaticTrayIconSwitchProps = SwitchProps;
+
+function MonochromaticTrayIconSwitch({ children, ...props }: MonochromaticTrayIconSwitchProps) {
+  const monochromaticIcon = useMonochromaticTrayIcon();
+  const { setMonochromaticIcon } = useAppContext();
+
+  return (
+    <Switch checked={monochromaticIcon} onCheckedChange={setMonochromaticIcon} {...props}>
+      {children}
+    </Switch>
+  );
+}
+
+const MonochromaticTrayIconSwitchNamespace = Object.assign(MonochromaticTrayIconSwitch, {
+  Label: Switch.Label,
+  Thumb: Switch.Thumb,
+  Trigger: Switch.Trigger,
+});
+
+export { MonochromaticTrayIconSwitchNamespace as MonochromaticTrayIconSwitch };
