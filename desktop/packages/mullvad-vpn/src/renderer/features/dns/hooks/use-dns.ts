172b3bd845cfa8356f1705478f3e7c5fe170f7e7
Oliver <oliver@mohlin.dev>
2025-11-11 07:56:54 +0100

    Add and use dns hooks

--- desktop/packages/mullvad-vpn/src/renderer/features/dns/hooks/use-dns.ts
+++ desktop/packages/mullvad-vpn/src/renderer/features/dns/hooks/use-dns.ts
@@ -1,24 +1,9 @@
-import { useCallback } from 'react';
-
-import { IDnsOptions } from '../../../../shared/daemon-rpc-types';
 import { useAppContext } from '../../../context';
 import { useSelector } from '../../../redux/store';
 
-export function useDns(setting: keyof IDnsOptions['defaultOptions']) {
+export function useDns() {
   const dns = useSelector((state) => state.settings.dns);
   const { setDnsOptions } = useAppContext();
 
-  const updateBlockSetting = useCallback(
-    (enabled: boolean) =>
-      setDnsOptions({
-        ...dns,
-        defaultOptions: {
-          ...dns.defaultOptions,
-          [setting]: enabled,
-        },
-      }),
-    [setting, dns, setDnsOptions],
-  );
-
-  return [dns, updateBlockSetting] as const;
+  return { dns, setDns: setDnsOptions };
 }
