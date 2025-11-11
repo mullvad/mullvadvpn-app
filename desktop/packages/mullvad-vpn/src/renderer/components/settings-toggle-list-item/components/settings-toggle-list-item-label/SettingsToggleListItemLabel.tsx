0c646410b63bf841644076a1c34979009025e701
Oliver <oliver@mohlin.dev>
2025-11-11 08:15:04 +0100

    Remove SettingsToggleListItem component

--- desktop/packages/mullvad-vpn/src/renderer/components/settings-toggle-list-item/components/settings-toggle-list-item-label/SettingsToggleListItemLabel.tsx
+++
@@ -1,8 +0,0 @@
-import { Switch } from '../../../../lib/components/switch';
-import { SwitchLabelProps } from '../../../../lib/components/switch/components/switch-label';
-
-export type SettingsToggleListItemLabelProps = SwitchLabelProps;
-
-export function SettingsToggleListItemLabel(props: SettingsToggleListItemLabelProps) {
-  return <Switch.Label variant="titleMedium" {...props} />;
-}
