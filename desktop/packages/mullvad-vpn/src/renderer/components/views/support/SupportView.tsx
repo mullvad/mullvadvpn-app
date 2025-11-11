c702a561673ad97e236bbb24e6dcb197d7e17243
Oliver <oliver@mohlin.dev>
2025-11-11 09:16:28 +0100

    Use layout components in support view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/support/SupportView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/support/SupportView.tsx
@@ -1,53 +1,40 @@
-import styled from 'styled-components';
-
 import { messages } from '../../../../shared/gettext';
+import { FlexColumn } from '../../../lib/components/flex-column';
+import { View } from '../../../lib/components/view';
 import { useHistory } from '../../../lib/history';
 import { AppNavigationHeader } from '../..';
-import * as Cell from '../../cell';
 import { BackAction } from '../../KeyboardNavigation';
-import { Layout, SettingsContainer } from '../../Layout';
 import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
 import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
 import { FaqButton, ProblemReportButton } from './components';
-
-const StyledContent = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  flex: 1,
-  marginBottom: '2px',
-});
 
 export function SupportView() {
   const { pop } = useHistory();
 
   return (
     <BackAction action={pop}>
-      <Layout>
-        <SettingsContainer>
-          <NavigationContainer>
-            <AppNavigationHeader
-              title={
-                // TRANSLATORS: Title label in navigation bar
-                messages.pgettext('support-view', 'Support')
-              }
-            />
+      <View backgroundColor="darkBlue">
+        <NavigationContainer>
+          <AppNavigationHeader
+            title={
+              // TRANSLATORS: Title label in navigation bar
+              messages.pgettext('support-view', 'Support')
+            }
+          />
 
-            <NavigationScrollbars>
-              <SettingsHeader>
-                <HeaderTitle>{messages.pgettext('support-view', 'Support')}</HeaderTitle>
-              </SettingsHeader>
+          <NavigationScrollbars>
+            <SettingsHeader>
+              <HeaderTitle>{messages.pgettext('support-view', 'Support')}</HeaderTitle>
+            </SettingsHeader>
 
-              <StyledContent>
-                <Cell.Group>
-                  <ProblemReportButton />
-                  <FaqButton />
-                </Cell.Group>
-              </StyledContent>
-            </NavigationScrollbars>
-          </NavigationContainer>
-        </SettingsContainer>
-      </Layout>
+            <FlexColumn>
+              <ProblemReportButton />
+              <FaqButton />
+            </FlexColumn>
+          </NavigationScrollbars>
+        </NavigationContainer>
+      </View>
     </BackAction>
   );
 }
