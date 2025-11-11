f52f69a9e23793996007c20930e6eebe53a1514d
Oliver <oliver@mohlin.dev>
2025-11-11 09:20:58 +0100

    Use ListItem component for FAQs and guides

--- desktop/packages/mullvad-vpn/src/renderer/components/views/support/components/faq-button/FaqButton.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/support/components/faq-button/FaqButton.tsx
@@ -3,9 +3,8 @@ import { useCallback } from 'react';
 import { urls } from '../../../../../../shared/constants';
 import { messages } from '../../../../../../shared/gettext';
 import { useAppContext } from '../../../../../context';
+import { ListItem } from '../../../../../lib/components/list-item';
 import { useSelector } from '../../../../../redux/store';
-import { AriaDescribed, AriaDescription, AriaDescriptionGroup } from '../../../../AriaGroup';
-import * as Cell from '../../../../cell';
 
 export function FaqButton() {
   const isOffline = useSelector((state) => state.connection.isBlocked);
@@ -14,23 +13,22 @@ export function FaqButton() {
   const openFaq = useCallback(() => openUrl(urls.faq), [openUrl]);
 
   return (
-    <AriaDescriptionGroup>
-      <AriaDescribed>
-        <Cell.CellButton disabled={isOffline} onClick={openFaq}>
-          <Cell.Label>
-            {
-              // TRANSLATORS: Link to the webpage
-              messages.pgettext('support-view', 'FAQs & Guides')
-            }
-          </Cell.Label>
-          <AriaDescription>
-            <Cell.CellTintedIcon
-              icon="external"
-              aria-label={messages.pgettext('accessibility', 'Opens externally')}
-            />
-          </AriaDescription>
-        </Cell.CellButton>
-      </AriaDescribed>
-    </AriaDescriptionGroup>
+    <ListItem disabled={isOffline}>
+      <ListItem.Trigger
+        onClick={openFaq}
+        aria-description={messages.pgettext('accessibility', 'Opens externally')}>
+        <ListItem.Item>
+          <ListItem.Content>
+            <ListItem.Label>
+              {
+                // TRANSLATORS: Link to the webpage
+                messages.pgettext('support-view', 'FAQs & Guides')
+              }
+            </ListItem.Label>
+            <ListItem.Icon icon="external" />
+          </ListItem.Content>
+        </ListItem.Item>
+      </ListItem.Trigger>
+    </ListItem>
   );
 }
