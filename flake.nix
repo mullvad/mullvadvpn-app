62a57f8bded066983c8db76bafbbcaae8955a5c4
Albin <albin@mullvad.net>
2025-11-13 06:39:15 +0000

    Restructure android nix configuration

--- flake.nix
+++ flake.nix
@@ -35,88 +35,20 @@
           ];
         };
 
-        rust-toolchain =
-          (pkgs.buildPackages.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override
-            {
-              extensions = [ "rust-analyzer" ];
-              targets = [
-                "aarch64-linux-android"
-                "armv7-linux-androideabi"
-                "x86_64-linux-android"
-                "i686-linux-android"
-              ];
-            };
+        android = import ./nix/android-toolchain.nix {
+          inherit
+            pkgs
+            nixpkgs
+            android-nixpkgs
+            system
+            rust-overlay
+            ;
+        };
 
-        versions =
-          (builtins.fromTOML (
-            builtins.concatStringsSep "\n" (
-              builtins.filter (line: !(builtins.match "^[[:space:]]*#" line != null)) (
-                nixpkgs.lib.splitString "\n" (builtins.readFile ./android/gradle/libs.versions.toml)
-              )
-            )
-          )).versions;
-
-        compileSdkVersion = versions."compile-sdk";
-        buildToolsVersion = versions."build-tools";
-        minSdkVersion = versions."min-sdk";
-        ndkVersion = versions.ndk;
-
-        android-sdk = android-nixpkgs.sdk.${system} (
-          sdkPkgs: with sdkPkgs; [
-            (builtins.getAttr "platforms-android-${compileSdkVersion}" sdkPkgs)
-            (builtins.getAttr "build-tools-${builtins.replaceStrings [ "." ] [ "-" ] buildToolsVersion}" sdkPkgs)
-            (builtins.getAttr "ndk-${builtins.replaceStrings [ "." ] [ "-" ] ndkVersion}" sdkPkgs)
-            cmdline-tools-latest
-            platform-tools
-          ]
-        );
       in
       {
-        overlay = final: prev: {
-          inherit (self.packages.${system}) android-sdk;
-        };
-
-        packages = {
-          inherit android-sdk;
-        };
-
-        devShells.default = pkgs.devshell.mkShell {
-          name = "mullvad-android-devshell";
-          packages = [
-            android-sdk
-            rust-toolchain
-            pkgs.protoc-gen-grpc-java
-            pkgs.gcc
-            pkgs.gnumake
-            pkgs.protobuf
-            pkgs.jdk17
-            pkgs.python314
-          ]
-          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
-
-          env = import ./nix/android-env.nix {
-            inherit
-              pkgs
-              android-sdk
-              buildToolsVersion
-              ndkVersion
-              minSdkVersion
-              ;
-          };
-          # Unfortunately rich menus with package, description and category
-          # is only supported using the TOML format and not when using mkShell.
-          # The two cannot be combined and TOML format by itself doesn't support
-          # the way we dynamically configure the devshell.
-          commands = [
-            {
-              name = "tasks";
-              command = "cd android && ./gradlew tasks";
-            }
-            {
-              name = "build";
-              command = "cd android && ./gradlew assembleOssProdDebug";
-            }
-          ];
+        devShells.default = import ./nix/android-devshell.nix {
+          inherit pkgs android;
         };
       }
     );
