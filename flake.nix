cdf28292ae27459d34ead6bb3a22d2ba45428993
Albin <albin@mullvad.net>
2025-11-16 19:03:54 +0100

    Add desktop devshell

--- flake.nix
+++ flake.nix
@@ -39,6 +39,10 @@
           inherit pkgs rust-overlay;
         };
 
+        desktop = import ./nix/desktop-toolchain.nix {
+          inherit pkgs common-toolchain;
+        };
+
         android = import ./nix/android-toolchain.nix {
           inherit
             pkgs
@@ -51,9 +55,17 @@
 
       in
       {
-        devShells.default = import ./nix/android-devshell.nix {
-          inherit pkgs android;
-        };
+        devShells =
+          pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+            default = import ./nix/desktop-devshell.nix {
+              inherit pkgs desktop;
+            };
+          }
+          // {
+            android = import ./nix/android-devshell.nix {
+              inherit pkgs android;
+            };
+          };
       }
     );
 }
