8d539a6ef0c06e0e88dcc0a047fe4a5a30eca005
Oliver <oliver@mohlin.dev>
2025-11-17 08:12:31 +0100

    Update Container component styles

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/container/Container.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/container/Container.tsx
@@ -1,24 +1,26 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { spacings } from '../../foundations';
 import { Flex, FlexProps } from '../flex';
 
-type Indent = 'medium' | 'large';
+type Indent = 'small' | 'medium' | 'large';
 
 export type ContainerProps = FlexProps & {
   indent: Indent;
 };
 
 const widths: Record<Indent, string> = {
+  small: `calc(100% - ${spacings.small} * 2)`,
   medium: `calc(100% - ${spacings.medium} * 2)`,
   large: `calc(100% - ${spacings.large} * 2)`,
 };
 
 export const StyledContainer = styled(Flex)<{ $width: string }>`
-  ${({ $width }) => ({
-    width: $width,
-    margin: 'auto',
-  })}
+  ${({ $width }) => css`
+    width: ${$width};
+    margin-left: auto;
+    margin-right: auto;
+  `};
 `;
 
 export function Container({ indent, ...props }: ContainerProps) {
