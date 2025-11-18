68b29ba8d83b8d73e7fad085afe5bd7a60f4e237
Markus Pettersson <markus.pettersson@mullvad.net>
2025-11-18 11:32:02 +0100

    Add support for selecting port 443 with UDP2TCP

--- mullvad-relay-selector/src/constants.rs
+++ mullvad-relay-selector/src/constants.rs
@@ -1,4 +1,4 @@
 //! Constants used throughout the relay selector
 
 /// All the valid ports when using UDP2TCP obfuscation.
-pub(crate) const UDP2TCP_PORTS: [u16; 2] = [80, 5001];
+pub(crate) const UDP2TCP_PORTS: [u16; 3] = [80, 443, 5001];
