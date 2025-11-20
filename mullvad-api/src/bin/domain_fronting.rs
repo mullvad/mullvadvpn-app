92cebdd14dcdaab6c879215bc3a88960ee41487c
Bug Magnet <marco.nikic@mullvad.net>
2025-11-20 08:48:24 +0100

    Make the POC work

--- mullvad-api/src/bin/domain_fronting.rs
+++ mullvad-api/src/bin/domain_fronting.rs
@@ -1,6 +1,10 @@
 use clap::Parser;
+use http::{Method, Request};
+use http_body_util::{BodyExt, Empty};
+use hyper::body::Bytes;
 use hyper_util::rt::TokioIo;
 use mullvad_api::domain_fronting::DomainFronting;
+use tokio::io::{self, AsyncWriteExt as _};
 
 #[derive(Parser, Debug)]
 pub struct Arguments {
@@ -21,12 +25,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .init();
 
     let Arguments { front, host } = Arguments::parse();
-    // Do we want the HTTP2 feature for hyper ?, no we don't
     println!("front: {:?} host: {:?}", front, host);
-    // Use this to resolve DNS
-
-    let domain_front = DomainFronting::new(host, front);
-
+    let domain_front = DomainFronting::new(front.clone());
     let tls_stream = domain_front
         .try_connect()
         .await
@@ -40,8 +40,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         if let Err(err) = conn.await {
             println!("Connection failed: {:?}", err);
         }
-    })
-    .await;
+    });
+
+    // Build the request
+    let req = Request::builder()
+        .method(Method::GET)
+        .header(hyper::header::HOST, host)
+        .header(hyper::header::ACCEPT, "*/*")
+        .body(Empty::<Bytes>::new())?;
+    println!("request: {:?}", req);
+    let mut res = sender.send_request(req).await?;
+
+    println!("Response: {}", res.status());
+    println!("Headers: {:#?}\n", res.headers());
+
+    // Stream the body, writing each chunk to stdout as we get it
+    // (instead of buffering and printing at the end).
+    while let Some(next) = res.frame().await {
+        let frame = next?;
+        if let Some(chunk) = frame.data_ref() {
+            io::stdout().write_all(chunk).await?;
+        }
+    }
+
+    println!("\n\nDone!");
 
     Ok(())
 }
