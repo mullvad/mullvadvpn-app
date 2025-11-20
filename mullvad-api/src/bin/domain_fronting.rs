62af695c83344b38fd55fa5b18ab574d030d8f5c
Bug Magnet <marco.nikic@mullvad.net>
2025-11-20 13:43:42 +0100

    Make it compile behind a feature flag

--- mullvad-api/src/bin/domain_fronting.rs
+++ mullvad-api/src/bin/domain_fronting.rs
@@ -1,69 +1,84 @@
-use clap::Parser;
-use http::{Method, Request};
-use http_body_util::{BodyExt, Empty};
-use hyper::body::Bytes;
-use hyper_util::rt::TokioIo;
-use mullvad_api::domain_fronting::DomainFronting;
-use tokio::io::{self, AsyncWriteExt as _};
-
-#[derive(Parser, Debug)]
-pub struct Arguments {
-    /// The domain used to hide the actual destination.
-    #[arg(long)]
-    front: String,
-
-    /// The host being reached via `front`.
-    #[arg(long)]
-    host: String,
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    imp::main().await
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Info)
-        .parse_default_env()
-        .init();
+#[cfg(not(feature = "domain-fronting"))]
+pub mod imp {
+    pub async fn main() -> anyhow::Result<()> {
+        unimplemented!(
+            "cargo run -p mullvad-api --features domain-fronting --bin domain_fronting -- --front <FRONT_DOMAIN> --host <HOST_DOMAIN>"
+        )
+    }
+}
 
-    let Arguments { front, host } = Arguments::parse();
-    println!("front: {:?} host: {:?}", front, host);
-    let domain_front = DomainFronting::new(front.clone());
-    let tls_stream = domain_front
-        .try_connect()
-        .await
-        .expect("Could not resolve {front}");
+#[cfg(feature = "domain-fronting")]
+mod imp {
+    use clap::Parser;
+    use http::{Method, Request};
+    use http_body_util::{BodyExt, Empty};
+    use hyper::body::Bytes;
+    use hyper_util::rt::TokioIo;
+    use mullvad_api::domain_fronting::DomainFronting;
+    use tokio::io::{self, AsyncWriteExt as _};
 
-    let io = TokioIo::new(tls_stream);
+    #[derive(Parser, Debug)]
+    pub struct Arguments {
+        /// The domain used to hide the actual destination.
+        #[arg(long)]
+        front: String,
 
-    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
-
-    tokio::task::spawn(async move {
-        if let Err(err) = conn.await {
-            println!("Connection failed: {:?}", err);
-        }
-    });
-
-    // Build the request
-    let req = Request::builder()
-        .method(Method::GET)
-        .header(hyper::header::HOST, host)
-        .header(hyper::header::ACCEPT, "*/*")
-        .body(Empty::<Bytes>::new())?;
-    println!("request: {:?}", req);
-    let mut res = sender.send_request(req).await?;
-
-    println!("Response: {}", res.status());
-    println!("Headers: {:#?}\n", res.headers());
-
-    // Stream the body, writing each chunk to stdout as we get it
-    // (instead of buffering and printing at the end).
-    while let Some(next) = res.frame().await {
-        let frame = next?;
-        if let Some(chunk) = frame.data_ref() {
-            io::stdout().write_all(chunk).await?;
-        }
+        /// The host being reached via `front`.
+        #[arg(long)]
+        host: String,
     }
 
-    println!("\n\nDone!");
+    pub async fn main() -> anyhow::Result<()> {
+        env_logger::builder()
+            .filter_level(log::LevelFilter::Info)
+            .parse_default_env()
+            .init();
 
-    Ok(())
+        let Arguments { front, host } = Arguments::parse();
+        println!("front: {:?} host: {:?}", front, host);
+        let domain_front = DomainFronting::new(front.clone());
+        let tls_stream = domain_front
+            .connect()
+            .await
+            .expect("Could not resolve {front}");
+
+        let io = TokioIo::new(tls_stream);
+
+        let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+
+        tokio::task::spawn(async move {
+            if let Err(err) = conn.await {
+                println!("Connection failed: {:?}", err);
+            }
+        });
+
+        // Build the request
+        let req = Request::builder()
+            .method(Method::GET)
+            .header(hyper::header::HOST, host)
+            .header(hyper::header::ACCEPT, "*/*")
+            .body(Empty::<Bytes>::new())?;
+        println!("request: {:?}", req);
+        let mut res = sender.send_request(req).await?;
+
+        println!("Response: {}", res.status());
+        println!("Headers: {:#?}\n", res.headers());
+
+        // Stream the body, writing each chunk to stdout as it is received
+        // (instead of buffering and printing at the end).
+        while let Some(next) = res.frame().await {
+            let frame = next?;
+            if let Some(chunk) = frame.data_ref() {
+                io::stdout().write_all(chunk).await?;
+            }
+        }
+
+        println!("\n\nDone!");
+        Ok(())
+    }
 }
