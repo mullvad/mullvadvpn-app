401753fd979942d12ae085025cbf90007eb84f47
Oliver <oliver@mohlin.dev>
2025-11-21 11:57:20 +0100

    Move filter view to views folder

--- desktop/packages/mullvad-vpn/src/renderer/components/views/filter/FilterView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/filter/FilterView.tsx
@@ -1,26 +1,26 @@
 import { useCallback, useMemo, useState } from 'react';
 import styled from 'styled-components';
 
-import { Ownership } from '../../shared/daemon-rpc-types';
-import { messages } from '../../shared/gettext';
-import { Button, Icon } from '../lib/components';
-import { View } from '../lib/components/view';
-import { useRelaySettingsUpdater } from '../lib/constraint-updater';
-import { filterLocations, filterLocationsByEndPointType } from '../lib/filter-locations';
-import { colors } from '../lib/foundations';
-import { useHistory } from '../lib/history';
-import { useNormalRelaySettings } from '../lib/relay-settings-hooks';
-import { IRelayLocationCountryRedux } from '../redux/settings/reducers';
-import { useSelector } from '../redux/store';
-import { AppNavigationHeader } from '.';
-import { AriaInputGroup } from './AriaGroup';
-import * as Cell from './cell';
-import Selector from './cell/Selector';
-import { normalText } from './common-styles';
-import { FilterAccordion } from './FilterAccordion';
-import { BackAction } from './KeyboardNavigation';
-import { NavigationContainer } from './NavigationContainer';
-import { NavigationScrollbars } from './NavigationScrollbars';
+import { Ownership } from '../../../../shared/daemon-rpc-types';
+import { messages } from '../../../../shared/gettext';
+import { Button, Icon } from '../../../lib/components';
+import { View } from '../../../lib/components/view';
+import { useRelaySettingsUpdater } from '../../../lib/constraint-updater';
+import { filterLocations, filterLocationsByEndPointType } from '../../../lib/filter-locations';
+import { colors } from '../../../lib/foundations';
+import { useHistory } from '../../../lib/history';
+import { useNormalRelaySettings } from '../../../lib/relay-settings-hooks';
+import { IRelayLocationCountryRedux } from '../../../redux/settings/reducers';
+import { useSelector } from '../../../redux/store';
+import { AppNavigationHeader } from '../../app-navigation-header';
+import { AriaInputGroup } from '../../AriaGroup';
+import * as Cell from '../../cell';
+import Selector from '../../cell/Selector';
+import { normalText } from '../../common-styles';
+import { FilterAccordion } from '../../FilterAccordion';
+import { BackAction } from '../../KeyboardNavigation';
+import { NavigationContainer } from '../../NavigationContainer';
+import { NavigationScrollbars } from '../../NavigationScrollbars';
 
 const StyledNavigationScrollbars = styled(NavigationScrollbars)({
   backgroundColor: colors.darkBlue,
