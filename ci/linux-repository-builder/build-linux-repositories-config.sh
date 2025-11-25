0b2df9ecdd72db792f7a15e5fa9b1938fd9f0fb0
Linus Färnstrand <linus@mullvad.net>
2025-11-25 15:46:21 +0100

    Restructure SUPPORTED_DEB_CODENAMES for readability
    Also clarify that we have stopped using the explicit codenames
    and new ones should not be added.

--- ci/linux-repository-builder/build-linux-repositories-config.sh
+++ ci/linux-repository-builder/build-linux-repositories-config.sh
@@ -9,16 +9,19 @@ export CODE_SIGNING_KEY_FINGERPRINT="A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF"
 # Debian codenames we support.
 # On top of these we also add the name of the repository as a codename as
 # well. Meaning the `stable` repository will also have a `stable` codename,
-# and `beta` will have `beta` as a codename.
-SUPPORTED_DEB_CODENAMES=("sid" "testing" "trixie" "bookworm" "bullseye")
-# Ubuntu codenames we support. Latest two LTS. But when adding a new
-# don't immediately remove the oldest one. Allow for some transition period
-# with the last three.
-SUPPORTED_DEB_CODENAMES+=("noble" "jammy" "focal")
-# ... + latest non-LTS Ubuntu. We officially only support the latest non-LTS.
-# But to not cause too much disturbance just when Ubuntu is released, we keep
-# the last two codenames working in the repository.
-SUPPORTED_DEB_CODENAMES+=("plucky" "oracular")
+# and `beta` will have `beta` as a codename. We are transitioning to users
+# using these fixed codenames, and away from distro specific code names.
+# SO DO NOT ADD NEW CODENAMES HERE.
+SUPPORTED_DEB_CODENAMES=("sid" "testing")
+SUPPORTED_DEB_CODENAMES+=("trixie") # 13
+SUPPORTED_DEB_CODENAMES+=("bookworm") # 12
+SUPPORTED_DEB_CODENAMES+=("bullseye") # 11
+# Ubuntu codenames we support. Same as above: DO NOT ADD NEW CODENAMES HERE.
+SUPPORTED_DEB_CODENAMES+=("plucky") # 25.04
+SUPPORTED_DEB_CODENAMES+=("oracular") # 24.10
+SUPPORTED_DEB_CODENAMES+=("noble") # 24.04 LTS
+SUPPORTED_DEB_CODENAMES+=("jammy") # 22.04 LTS
+SUPPORTED_DEB_CODENAMES+=("focal") # 20.04 LTS
 export SUPPORTED_DEB_CODENAMES
 
 export SUPPORTED_RPM_ARCHITECTURES=("x86_64" "aarch64")
