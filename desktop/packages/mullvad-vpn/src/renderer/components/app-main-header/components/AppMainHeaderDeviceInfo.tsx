720d5908c5fe0882f490bc99bc713b95c97a1a69
Oskar <oskar@mullvad.net>
2025-11-27 08:19:25 +0100

    Update time left label every hour

--- desktop/packages/mullvad-vpn/src/renderer/components/app-main-header/components/AppMainHeaderDeviceInfo.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/app-main-header/components/AppMainHeaderDeviceInfo.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { sprintf } from 'sprintf-js';
 import styled from 'styled-components';
 
 import { closeToExpiry, formatRemainingTime, hasExpired } from '../../../../shared/account-expiry';
 import { messages } from '../../../../shared/gettext';
 import { Flex, FootnoteMini } from '../../../lib/components';
+import { useInterval } from '../../../lib/hooks';
 import { formatDeviceName } from '../../../lib/utils';
 import { useSelector } from '../../../redux/store';
 
@@ -25,11 +27,11 @@ export const AppMainHeaderDeviceInfo = () => {
   const deviceName = useSelector((state) => state.account.deviceName);
   const accountExpiry = useSelector((state) => state.account.expiry);
   const isOutOfTime = accountExpiry ? hasExpired(accountExpiry) : false;
-  const formattedExpiry = isOutOfTime
-    ? sprintf(messages.ngettext('1 day', '%d days', 0), 0)
-    : accountExpiry
-      ? formatRemainingTime(accountExpiry)
-      : '';
+
+  const [timeLeft, setTimeLeft] = useState(formatTimeLeft(accountExpiry));
+
+  // The time left value must be recalculated recurringly since it should change when time passes.
+  useInterval(() => setTimeLeft(formatTimeLeft(accountExpiry)), 60 * 60 * 1_000);
 
   return (
     <StyledFlex $gap="large" $margin={{ top: 'tiny' }}>
@@ -48,10 +50,19 @@ export const AppMainHeaderDeviceInfo = () => {
       {accountExpiry && !closeToExpiry(accountExpiry) && !isOutOfTime && (
         <StyledTimeLeftLabel color="whiteAlpha80">
           {sprintf(messages.pgettext('device-management', 'Time left: %(timeLeft)s'), {
-            timeLeft: formattedExpiry,
+            timeLeft: timeLeft,
           })}
         </StyledTimeLeftLabel>
       )}
     </StyledFlex>
   );
 };
+
+function formatTimeLeft(accountExpiry?: string): string {
+  const isOutOfTime = accountExpiry ? hasExpired(accountExpiry) : false;
+  return isOutOfTime
+    ? sprintf(messages.ngettext('1 day', '%d days', 0), 0)
+    : accountExpiry
+      ? formatRemainingTime(accountExpiry)
+      : '';
+}
