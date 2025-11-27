0322b50095d91eac9be039e077130618218ec419
Oliver <oliver@mohlin.dev>
2025-11-27 08:24:22 +0100

    Add and use createRange util function

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-indicators/Carouselndicators.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-indicators/Carouselndicators.tsx
@@ -1,12 +1,15 @@
+import { createRange } from '../../../../utils';
 import { useCarouselContext } from '../../CarouselContext';
 import { CarouselControlGroup } from '../carousel-control-group';
 import { CarouselIndicator } from '../carousel-indicator';
 
 export function CarouselIndicators() {
   const { numberOfSlides, slideIndex } = useCarouselContext();
+
+  const range = createRange(numberOfSlides);
   return (
     <CarouselControlGroup>
-      {[...Array(numberOfSlides)].map((_, i) => {
+      {range.map((_, i) => {
         const current = i === slideIndex;
         return <CarouselIndicator key={i} disabled={current} slideToGoTo={i} />;
       })}
