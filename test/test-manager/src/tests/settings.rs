4ce36910be5a317036cf96eb73c0647ff8bcf6d2
Markus Pettersson <markus.pettersson@mullvad.net>
2025-12-02 12:08:34 +0100

    Add `test_reset_settings` end-to-end test

--- test/test-manager/src/tests/settings.rs
+++ test/test-manager/src/tests/settings.rs
@@ -3,7 +3,9 @@ use super::{
     helpers::{connect_and_wait, send_guest_probes},
 };
 
+use anyhow::{Context, ensure};
 use mullvad_management_interface::MullvadProxyClient;
+use mullvad_types::settings::{DefaultDnsOptions, DnsOptions, Settings};
 use std::net::SocketAddr;
 use test_macro::test_function;
 use test_rpc::ServiceClient;
@@ -183,5 +185,66 @@ pub async fn test_lockdown(
         "observed outgoing packets to internet: {detected_probes:?}"
     );
 
+    Ok(())
+}
+
+/// Reset settings to their default values.
+///
+/// The account should still be logged in after resetting settings.
+#[test_function]
+pub async fn test_reset_settings(
+    _: TestContext,
+    _: ServiceClient,
+    mut mullvad_client: MullvadProxyClient,
+) -> anyhow::Result<()> {
+    // Change a bunch of random settings to (deterministically) non-default values.
+    {
+        let err = "Failed to generate random settings";
+        mullvad_client.set_allow_lan(true).await.context(err)?;
+        mullvad_client
+            .set_dns_options(DnsOptions {
+                default_options: DefaultDnsOptions::new().block_ads().block_malware(),
+                ..Default::default()
+            })
+            .await
+            .context(err)?;
+        mullvad_client.set_lockdown_mode(true).await.context(err)?;
+    }
+    // Note: We do not double-check that the daemon reports these settings changes. We trust other
+    // E2E tests to cover this.
+    // Soft-reset settings by invoking the `reset-settings` rpc.
+    mullvad_client
+        .reset_settings()
+        .await
+        .context("Failed to reset settings")?;
+    // Check that all changed settings have indeed been reset.
+    let daemon = mullvad_client
+        .get_settings()
+        .await
+        .context("Failed to get settings")?;
+    let default = Settings::default();
+    // Santiy-check that data that shouldn't have been touched have indeed been kept intact.
+    // Are we still logged in?
+    // Are the custom lists intact?
+    ensure!(
+        mullvad_client.get_account_history().await?.is_some(),
+        "Logged out after reset settings RPC"
+    );
+    // Note: We can not compare `daemon` with `default` directly because Settings::default is
+    // non-deterministic ..
+    ensure!(
+        daemon.lockdown_mode == default.lockdown_mode,
+        "Lockdown mode was not reset"
+    );
+    ensure!(
+        daemon.allow_lan == default.allow_lan,
+        "Lockdown mode was not reset"
+    );
+    ensure!(
+        daemon.tunnel_options == default.tunnel_options,
+        "Lockdown mode was not reset"
+    );
+    // TODO: check other data as well, such as log files. Do this after having written such a test
+    // first.
     Ok(())
 }
