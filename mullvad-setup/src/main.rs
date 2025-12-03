f4589b0235c2bcc12cf02966be737b7a298d2963
Joakim Hulthe <joakim.hulthe@mullvad.net>
2025-12-03 14:16:16 +0100

    todo

--- mullvad-setup/src/main.rs
+++ mullvad-setup/src/main.rs
@@ -167,6 +167,7 @@ async fn reset_firewall() -> Result<(), Error> {
     Firewall::new(
         #[cfg(target_os = "linux")]
         mullvad_types::TUNNEL_FWMARK,
+        // TODO split-tunneling?
         #[cfg(target_os = "linux")]
         None,
     )
