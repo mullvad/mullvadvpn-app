eb3d3f16e424fdec3c28ff23c33ca35d4c5996af
Joakim Hulthe <joakim.hulthe@mullvad.net>
2025-12-03 14:16:16 +0100

    todo

--- mullvad-daemon/src/early_boot_firewall.rs
+++ mullvad-daemon/src/early_boot_firewall.rs
@@ -14,6 +14,7 @@ pub enum Error {
 }
 
 pub async fn initialize_firewall() -> Result<(), Error> {
+    // TODO split-tunneling?
     let mut firewall = Firewall::new(mullvad_types::TUNNEL_FWMARK, None)?;
     let allow_lan = get_allow_lan().await.unwrap_or_else(|err| {
         log::info!(
