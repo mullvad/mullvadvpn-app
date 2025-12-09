2bd522d7f7cf003f9fe7f9bc180314519842e3d6
Oliver <oliver@mohlin.dev>
2025-12-09 11:56:47 +0100

    Use view components in debug view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/debug/DebugView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/debug/DebugView.tsx
@@ -1,59 +1,40 @@
 import { useCallback } from 'react';
-import styled from 'styled-components';
 
 import { Button } from '../../../lib/components';
-import { FlexColumn } from '../../../lib/components/flex-column';
-import { spacings } from '../../../lib/foundations';
+import { View } from '../../../lib/components/view';
 import { useHistory } from '../../../lib/history';
 import { useBoolean } from '../../../lib/utility-hooks';
 import { AppNavigationHeader } from '../..';
-import { measurements } from '../../common-styles';
 import { BackAction } from '../../KeyboardNavigation';
-import { Layout, SettingsContainer } from '../../Layout';
 import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
 import SettingsHeader, { HeaderTitle } from '../../SettingsHeader';
-
-const StyledContent = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  flex: 1,
-  marginBottom: '2px',
-});
-
-const StyledButtonGroup = styled.div({
-  margin: `${spacings.large} ${measurements.horizontalViewMargin}`,
-});
 
 export function DebugView() {
   const { pop } = useHistory();
 
   return (
-    <BackAction action={pop}>
-      <Layout>
-        <SettingsContainer>
-          <NavigationContainer>
-            <AppNavigationHeader title="Developer tools" />
+    <View backgroundColor="darkBlue">
+      <BackAction action={pop}>
+        <NavigationContainer>
+          <AppNavigationHeader title="Developer tools" />
 
-            <NavigationScrollbars>
+          <NavigationScrollbars>
+            <View.Content>
               <SettingsHeader>
                 <HeaderTitle>Developer tools</HeaderTitle>
               </SettingsHeader>
 
-              <StyledContent>
-                <StyledButtonGroup>
-                  <FlexColumn gap="medium">
-                    <ThrowErrorButton />
-                    <UnhandledRejectionButton />
-                    <ErrorDuringRender />
-                  </FlexColumn>
-                </StyledButtonGroup>
-              </StyledContent>
-            </NavigationScrollbars>
-          </NavigationContainer>
-        </SettingsContainer>
-      </Layout>
-    </BackAction>
+              <View.Container indent="small" flexDirection="column" gap="medium">
+                <ThrowErrorButton />
+                <UnhandledRejectionButton />
+                <ErrorDuringRender />
+              </View.Container>
+            </View.Content>
+          </NavigationScrollbars>
+        </NavigationContainer>
+      </BackAction>
+    </View>
   );
 }
 
