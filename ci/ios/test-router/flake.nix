29cf5c83bbacfdb05ba32e788ecc4042eef1ce17
Emīls <emils@mullvad.net>
2025-12-10 08:58:26 +0100

    Update router to NixOS-25.11

--- ci/ios/test-router/flake.nix
+++ ci/ios/test-router/flake.nix
@@ -2,7 +2,7 @@
   description = "Config for our testing router";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs =
