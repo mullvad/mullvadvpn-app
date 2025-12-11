34c5a2bb7377fce954962eea1f14e250ef3c86b1
Linus Färnstrand <linus@mullvad.net>
2025-12-11 14:58:50 +0100

    Fix android

--- talpid-wireguard/src/lib.rs
+++ talpid-wireguard/src/lib.rs
@@ -462,7 +462,6 @@ impl WireguardMonitor {
         let should_negotiate_ephemeral_peer = config.quantum_resistant || config.daita;
 
         let (cancel_token, cancel_receiver) = connectivity::CancelToken::new();
-        #[expect(unused_mut)]
         let mut connectivity_monitor = connectivity::Check::new(
             config.ipv4_gateway,
             args.retry_attempt,
