1fd3b8612d663cb322d276e2b23e189121dbb676
Linus Färnstrand <linus@mullvad.net>
2025-12-11 19:12:29 +0100

    Fix the LogLevel stuff on Android

--- talpid-wireguard/src/logging.rs
+++ talpid-wireguard/src/logging.rs
@@ -48,9 +48,21 @@ pub fn clean_up_logging(ordinal: u64) {
 pub enum LogLevel {
     #[cfg_attr(all(windows, not(feature = "wireguard-go")), expect(dead_code))]
     Verbose,
-    #[cfg_attr(all(target_os = "linux", feature = "wireguard-go"), expect(dead_code))]
+    #[cfg_attr(
+        all(
+            any(target_os = "linux", target_os = "android"),
+            feature = "wireguard-go"
+        ),
+        expect(dead_code)
+    )]
     Info,
-    #[cfg_attr(all(target_os = "linux", feature = "wireguard-go"), expect(dead_code))]
+    #[cfg_attr(
+        all(
+            any(target_os = "linux", target_os = "android"),
+            feature = "wireguard-go"
+        ),
+        expect(dead_code)
+    )]
     Warning,
     Error,
 }
