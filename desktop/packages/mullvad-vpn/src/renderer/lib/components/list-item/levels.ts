8491f668de4de3671bef014acb3a87091833ae08
Oliver <oliver@mohlin.dev>
2025-12-11 08:44:23 +0100

    Update design for ListItem component

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/levels.ts
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/levels.ts
@@ -2,8 +2,7 @@ import { colors, spacings } from '../../foundations';
 
 export const levels = {
   0: { enabled: colors.blue, disabled: colors.blue40, indent: spacings.medium },
-  1: { enabled: colors.blue60, disabled: colors.blue40, indent: spacings.medium },
-  2: { enabled: colors.blue40, disabled: colors.blue20, indent: spacings.big },
-  3: { enabled: colors.blue20, disabled: colors.blue10, indent: '48px' },
-  4: { enabled: colors.blue10, disabled: colors.blue10, indent: '64px' },
+  1: { enabled: colors.blue60, disabled: colors.blue40, indent: '32px' },
+  2: { enabled: colors.blue40, disabled: colors.blue20, indent: '48px' },
+  3: { enabled: colors.blue20, disabled: colors.blue10, indent: '64px' },
 } as const;
