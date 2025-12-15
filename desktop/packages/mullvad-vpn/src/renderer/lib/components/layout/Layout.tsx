anthunyy/thevadvpn-app

Make Layout and Flex component props non-transient

desktop/packages/mullvad-vpn/src/renderer/lib/components/layout/Layout.tsx
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 import { Spacings } from '../../foundations';
+import { TransientProps } from '../../types';
 import { margin } from './margin';
 import { padding } from './padding';
 import { LayoutSpacings } from './types';
 
-export interface LayoutProps {
-  $margin?: Spacings | LayoutSpacings;
-  $padding?: Spacings | LayoutSpacings;
-}
+export type LayoutProps = React.ComponentPropsWithRef<'div'> & {
+  margin?: Spacings | LayoutSpacings;
+  padding?: Spacings | LayoutSpacings;
+};
 
 const combine = (
   funcs: Record<keyof LayoutSpacings, (value: Spacings) => React.CSSProperties>,
@@ -26,7 +27,13 @@ const combine = (
   return result;
 };
 
-export const Layout = styled.div<LayoutProps>(({ $margin, $padding }) => ({
-  ...combine(margin, $margin),
-  ...combine(padding, $padding),
-}));
+const StyledDiv = styled.div<TransientProps<LayoutProps>>`
+  ${({ $margin, $padding }) => ({
+    ...combine(margin, $margin),
+    ...combine(padding, $padding),
+  })}
+`;
+
+export function Layout({ margin, padding, ...props }: LayoutProps) {
+  return <StyledDiv $margin={margin} $padding={padding} {...props} />;
+}

mullvad/mullvadvpn-app

desktop/packages/mullvad-vpn/src/renderer/lib/components/layout/Layout.tsx
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 import { Spacings } from '../../foundations';
+import { TransientProps } from '../../types';
 import { margin } from './margin';
 import { padding } from './padding';
 import { LayoutSpacings } from './types';
 
-export interface LayoutProps {
-  $margin?: Spacings | LayoutSpacings;
-  $padding?: Spacings | LayoutSpacings;
-}
+export type LayoutProps = React.ComponentPropsWithRef<'div'> & {
+  margin?: Spacings | LayoutSpacings;
+  padding?: Spacings | LayoutSpacings;
+};
 
 const combine = (
   funcs: Record<keyof LayoutSpacings, (value: Spacings) => React.CSSProperties>,
@@ -26,7 +27,13 @@ const combine = (
   return result;
 };
 
-export const Layout = styled.div<LayoutProps>(({ $margin, $padding }) => ({
-  ...combine(margin, $margin),
-  ...combine(padding, $padding),
-}));
+const StyledLayout = styled.div<TransientProps<LayoutProps>>`
+  ${({ $margin, $padding }) => ({
+    ...combine(margin, $margin),
+    ...combine(padding, $padding),
+  })}
+`;
+
+export function Layout({ margin, padding, ...props }: LayoutProps) {
+  return <StyledLayout $margin={margin} $padding={padding} {...props} />;
+}