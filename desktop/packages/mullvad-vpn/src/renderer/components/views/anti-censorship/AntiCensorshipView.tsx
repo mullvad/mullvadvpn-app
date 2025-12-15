anthunyy/thevadvpn-app

Use ViewContent component to set bottom margin in views using View

desktop/packages/mullvad-vpn/src/renderer/components/views/anti-censorship/AntiCensorshipView.tsx
@@ -22,38 +22,40 @@ export function AntiCensorshipView() {
           />
 
           <NavigationScrollbars>
-            <SettingsHeader>
-              <HeaderTitle>
-                {
-                  // TRANSLATORS: Page title for anti censorship settings view
-                  messages.pgettext('anti-censorship-view', 'Anti-censorship')
-                }
-              </HeaderTitle>
-            </SettingsHeader>
-            <FlexColumn gap="medium">
-              <View.Container indent="medium" flexDirection="column" gap="medium">
-                <Text variant="labelTiny" color="whiteAlpha60">
+            <View.Content>
+              <SettingsHeader>
+                <HeaderTitle>
                   {
-                    // TRANSLATORS: First paragraph of description text in anti-censorship view
-                    messages.pgettext(
-                      'anti-censorship-view',
-                      'These methods may be useful in situations where you are blocked from reaching Mullvad. When "Automatic" is selected, the app will attempt all methods until one works.',
-                    )
+                    // TRANSLATORS: Page title for anti censorship settings view
+                    messages.pgettext('anti-censorship-view', 'Anti-censorship')
                   }
-                </Text>
-                <Text variant="labelTinySemiBold" color="whiteAlpha60">
-                  {
-                    // TRANSLATORS: Second paragraph of description text in anti-censorship view
-                    messages.pgettext(
-                      'anti-censorship-view',
-                      'Please note that these methods do not improve performance, and may increase system utilization and battery consumption.',
-                    )
-                  }
-                </Text>
-              </View.Container>
+                </HeaderTitle>
+              </SettingsHeader>
+              <FlexColumn gap="medium">
+                <View.Container indent="medium" flexDirection="column" gap="medium">
+                  <Text variant="labelTiny" color="whiteAlpha60">
+                    {
+                      // TRANSLATORS: First paragraph of description text in anti-censorship view
+                      messages.pgettext(
+                        'anti-censorship-view',
+                        'These methods may be useful in situations where you are blocked from reaching Mullvad. When "Automatic" is selected, the app will attempt all methods until one works.',
+                      )
+                    }
+                  </Text>
+                  <Text variant="labelTinySemiBold" color="whiteAlpha60">
+                    {
+                      // TRANSLATORS: Second paragraph of description text in anti-censorship view
+                      messages.pgettext(
+                        'anti-censorship-view',
+                        'Please note that these methods do not improve performance, and may increase system utilization and battery consumption.',
+                      )
+                    }
+                  </Text>
+                </View.Container>
 
-              <MethodSetting />
-            </FlexColumn>
+                <MethodSetting />
+              </FlexColumn>
+            </View.Content>
           </NavigationScrollbars>
         </NavigationContainer>
       </BackAction>

desktop/packages/mullvad-vpn/src/renderer/components/views/login/LoginView.tsx
@@ -130,44 +130,42 @@ class Login extends React.Component<IProps, IState> {
         <AppMainHeader>
           <AppMainHeader.SettingsButton disabled={!allowInteraction} />
         </AppMainHeader>
-        <View.Container
-          flexDirection="column"
-          indent="medium"
-          justifyContent="center"
-          padding={{ bottom: 'large' }}>
-          <FlexColumn gap="medium">
-            <Flex justifyContent="center">
-              {this.props.showBlockMessage ? <BlockMessage /> : this.getStatusIcon()}
-            </Flex>
-
-            <FlexColumn
-              gap="large"
-              margin={{ horizontal: 'small' }}
-              justifyContent="center"
-              flexGrow={1}>
-              <FlexColumn gap="small">
-                <Text as="h1" variant="titleBig" aria-live="polite">
-                  {this.formTitle()}
-                </Text>
-
-                {this.createLoginForm()}
-              </FlexColumn>
+        <View.Content>
+          <View.Container flexDirection="column" indent="medium" justifyContent="center">
+            <FlexColumn gap="medium">
               <Flex justifyContent="center">
-                <StyledLine margin={{ vertical: 'small', right: 'small' }} />
-                <Text variant="labelTinySemiBold">
-                  {
-                    // TRANSLATORS: Text shown between two horizontal lines above the "create account" button.
-                    // TRANSLATORS: In this context it is used to separate the users alternative of logging in
-                    // TRANSLATORS: or creating a new account, "Login or Create a new account".
-                    messages.pgettext('login-view', 'Or')
-                  }
-                </Text>
-                <StyledLine margin={{ vertical: 'small', left: 'small' }} />
+                {this.props.showBlockMessage ? <BlockMessage /> : this.getStatusIcon()}
               </Flex>
+
+              <View.Container
+                gap="large"
+                indent="small"
+                justifyContent="center"
+                flexDirection="column">
+                <FlexColumn gap="small">
+                  <Text as="h1" variant="titleBig" aria-live="polite">
+                    {this.formTitle()}
+                  </Text>
+
+                  {this.createLoginForm()}
+                </FlexColumn>
+                <Flex justifyContent="center">
+                  <StyledLine margin={{ vertical: 'small', right: 'small' }} />
+                  <Text variant="labelTinySemiBold">
+                    {
+                      // TRANSLATORS: Text shown between two horizontal lines above the "create account" button.
+                      // TRANSLATORS: In this context it is used to separate the users alternative of logging in
+                      // TRANSLATORS: or creating a new account, "Login or Create a new account".
+                      messages.pgettext('login-view', 'Or')
+                    }
+                  </Text>
+                  <StyledLine margin={{ vertical: 'small', left: 'small' }} />
+                </Flex>
+              </View.Container>
+              {this.createFooter()}
             </FlexColumn>
-            {this.createFooter()}
-          </FlexColumn>
-        </View.Container>
+          </View.Container>
+        </View.Content>
       </View>
     );
   }

desktop/packages/mullvad-vpn/src/renderer/components/views/manage-devices/ManageDevicesView.tsx
@@ -19,8 +19,8 @@ export function ManageDevicesView() {
 
   return (
     <ManageDevicesProvider isLoading={isLoading} isFetching={isFetching} refetchDevices={refetch}>
-      <BackAction action={pop}>
-        <View backgroundColor="darkBlue">
+      <View backgroundColor="darkBlue">
+        <BackAction action={pop}>
           <NavigationContainer>
             <AppNavigationHeader
               title={
@@ -29,31 +29,33 @@ export function ManageDevicesView() {
               }
             />
             <NavigationScrollbars>
-              <FlexColumn gap="medium">
-                <View.Container indent="medium" flexDirection="column" gap="small">
-                  <Text variant="titleBig">
-                    {
-                      // TRANSLATORS: Title text in the manage devices view
-                      messages.pgettext('device-management', 'Manage devices')
-                    }
-                  </Text>
-                  <Text variant="labelTiny" color="whiteAlpha60">
-                    {
-                      // TRANSLATORS: Subtitle text in the manage devices view, explaining
-                      // TRANSLATORS: devices and what they can do in the manage devices view.
-                      messages.pgettext(
-                        'device-management',
-                        'View and manage all your logged in devices. You can have up to 5 devices on one account at a time. Each device gets a name when logged in to help you tell them apart easily.',
-                      )
-                    }
-                  </Text>
-                </View.Container>
-                {showDeviceList ? <DeviceList devices={devices} /> : <DevicesState />}
-              </FlexColumn>
+              <View.Content>
+                <FlexColumn gap="medium">
+                  <View.Container indent="medium" flexDirection="column" gap="small">
+                    <Text variant="titleBig">
+                      {
+                        // TRANSLATORS: Title text in the manage devices view
+                        messages.pgettext('device-management', 'Manage devices')
+                      }
+                    </Text>
+                    <Text variant="labelTiny" color="whiteAlpha60">
+                      {
+                        // TRANSLATORS: Subtitle text in the manage devices view, explaining
+                        // TRANSLATORS: devices and what they can do in the manage devices view.
+                        messages.pgettext(
+                          'device-management',
+                          'View and manage all your logged in devices. You can have up to 5 devices on one account at a time. Each device gets a name when logged in to help you tell them apart easily.',
+                        )
+                      }
+                    </Text>
+                  </View.Container>
+                  {showDeviceList ? <DeviceList devices={devices} /> : <DevicesState />}
+                </FlexColumn>
+              </View.Content>
             </NavigationScrollbars>
           </NavigationContainer>
-        </View>
-      </BackAction>
+        </BackAction>
+      </View>
     </ManageDevicesProvider>
   );
 }

desktop/packages/mullvad-vpn/src/renderer/components/views/too-many-devices/TooManyDevicesView.tsx
@@ -48,44 +48,42 @@ export function TooManyDevicesView() {
         <AppMainHeader.SettingsButton />
       </AppMainHeader>
       <StyledCustomScrollbars fillContainer>
-        <FlexColumn gap="large">
-          <View.Container flexDirection="column" indent="medium">
-            <Flex justifyContent="center" margin={{ top: 'large' }}>
-              <IconBadge key={imageSource} state={imageSource} />
-            </Flex>
-          </View.Container>
-          {devices !== undefined && (
-            <>
-              <View.Container flexDirection="column" indent="medium" gap="small">
-                <Text variant="titleLarge" data-testid="title">
-                  {title}
-                </Text>
-                <Text variant="labelTiny">{subtitle}</Text>
-              </View.Container>
-              <DeviceList devices={devices} />
-            </>
-          )}
-
-          {devices !== undefined && (
-            <View.Container
-              flexDirection="column"
-              indent="medium"
-              gap="medium"
-              padding={{ bottom: 'large' }}>
-              <Button variant="success" onClick={continueLogin} disabled={continueButtonDisabled}>
-                <Button.Text>
-                  {
-                    // TRANSLATORS: Button for continuing login process.
-                    messages.pgettext('device-management', 'Continue with login')
-                  }
-                </Button.Text>
-              </Button>
-              <Button onClick={cancel}>
-                <Button.Text>{messages.gettext('Back')}</Button.Text>
-              </Button>
+        <View.Content>
+          <FlexColumn gap="large">
+            <View.Container flexDirection="column" indent="medium">
+              <Flex justifyContent="center" margin={{ top: 'large' }}>
+                <IconBadge key={imageSource} state={imageSource} />
+              </Flex>
             </View.Container>
-          )}
-        </FlexColumn>
+            {devices !== undefined && (
+              <>
+                <View.Container flexDirection="column" indent="medium" gap="small">
+                  <Text variant="titleLarge" data-testid="title">
+                    {title}
+                  </Text>
+                  <Text variant="labelTiny">{subtitle}</Text>
+                </View.Container>
+                <DeviceList devices={devices} />
+              </>
+            )}
+
+            {devices !== undefined && (
+              <View.Container flexDirection="column" indent="medium" gap="medium">
+                <Button variant="success" onClick={continueLogin} disabled={continueButtonDisabled}>
+                  <Button.Text>
+                    {
+                      // TRANSLATORS: Button for continuing login process.
+                      messages.pgettext('device-management', 'Continue with login')
+                    }
+                  </Button.Text>
+                </Button>
+                <Button onClick={cancel}>
+                  <Button.Text>{messages.gettext('Back')}</Button.Text>
+                </Button>
+              </View.Container>
+            )}
+          </FlexColumn>
+        </View.Content>
       </StyledCustomScrollbars>
     </View>
   );

mullvad/mullvadvpn-app

desktop/packages/mullvad-vpn/src/renderer/components/views/anti-censorship/AntiCensorshipView.tsx
@@ -22,38 +22,40 @@ export function AntiCensorshipView() {
           />
 
           <NavigationScrollbars>
-            <SettingsHeader>
-              <HeaderTitle>
-                {
-                  // TRANSLATORS: Page title for anti censorship settings view
-                  messages.pgettext('anti-censorship-view', 'Anti-censorship')
-                }
-              </HeaderTitle>
-            </SettingsHeader>
-            <FlexColumn gap="medium">
-              <View.Container marginInline="medium" flexDirection="column" gap="medium">
-                <Text variant="labelTiny" color="whiteAlpha60">
+            <View.Content>
+              <SettingsHeader>
+                <HeaderTitle>
                   {
-                    // TRANSLATORS: First paragraph of description text in anti-censorship view
-                    messages.pgettext(
-                      'anti-censorship-view',
-                      'These methods may be useful in situations where you are blocked from reaching Mullvad. When "Automatic" is selected, the app will attempt all methods until one works.',
-                    )
+                    // TRANSLATORS: Page title for anti censorship settings view
+                    messages.pgettext('anti-censorship-view', 'Anti-censorship')
                   }
-                </Text>
-                <Text variant="labelTinySemiBold" color="whiteAlpha60">
-                  {
-                    // TRANSLATORS: Second paragraph of description text in anti-censorship view
-                    messages.pgettext(
-                      'anti-censorship-view',
-                      'Please note that these methods do not improve performance, and may increase system utilization and battery consumption.',
-                    )
-                  }
-                </Text>
-              </View.Container>
+                </HeaderTitle>
+              </SettingsHeader>
+              <FlexColumn gap="medium">
+                <View.Container marginInline="medium" flexDirection="column" gap="medium">
+                  <Text variant="labelTiny" color="whiteAlpha60">
+                    {
+                      // TRANSLATORS: First paragraph of description text in anti-censorship view
+                      messages.pgettext(
+                        'anti-censorship-view',
+                        'These methods may be useful in situations where you are blocked from reaching Mullvad. When "Automatic" is selected, the app will attempt all methods until one works.',
+                      )
+                    }
+                  </Text>
+                  <Text variant="labelTinySemiBold" color="whiteAlpha60">
+                    {
+                      // TRANSLATORS: Second paragraph of description text in anti-censorship view
+                      messages.pgettext(
+                        'anti-censorship-view',
+                        'Please note that these methods do not improve performance, and may increase system utilization and battery consumption.',
+                      )
+                    }
+                  </Text>
+                </View.Container>
 
-              <MethodSetting />
-            </FlexColumn>
+                <MethodSetting />
+              </FlexColumn>
+            </View.Content>
           </NavigationScrollbars>
         </NavigationContainer>
       </BackAction>

desktop/packages/mullvad-vpn/src/renderer/components/views/login/LoginView.tsx
@@ -130,44 +130,42 @@ class Login extends React.Component<IProps, IState> {
         <AppMainHeader>
           <AppMainHeader.SettingsButton disabled={!allowInteraction} />
         </AppMainHeader>
-        <View.Container
-          flexDirection="column"
-          marginInline="medium"
-          justifyContent="center"
-          padding={{ bottom: 'large' }}>
-          <FlexColumn gap="medium">
-            <Flex justifyContent="center">
-              {this.props.showBlockMessage ? <BlockMessage /> : this.getStatusIcon()}
-            </Flex>
-
-            <FlexColumn
-              gap="large"
-              margin={{ horizontal: 'small' }}
-              justifyContent="center"
-              flexGrow={1}>
-              <FlexColumn gap="small">
-                <Text as="h1" variant="titleBig" aria-live="polite">
-                  {this.formTitle()}
-                </Text>
-
-                {this.createLoginForm()}
-              </FlexColumn>
+        <View.Content>
+          <View.Container flexDirection="column" marginInline="medium" justifyContent="center">
+            <FlexColumn gap="medium">
               <Flex justifyContent="center">
-                <StyledLine margin={{ vertical: 'small', right: 'small' }} />
-                <Text variant="labelTinySemiBold">
-                  {
-                    // TRANSLATORS: Text shown between two horizontal lines above the "create account" button.
-                    // TRANSLATORS: In this context it is used to separate the users alternative of logging in
-                    // TRANSLATORS: or creating a new account, "Login or Create a new account".
-                    messages.pgettext('login-view', 'Or')
-                  }
-                </Text>
-                <StyledLine margin={{ vertical: 'small', left: 'small' }} />
+                {this.props.showBlockMessage ? <BlockMessage /> : this.getStatusIcon()}
               </Flex>
+
+              <View.Container
+                gap="large"
+                marginInline="small"
+                justifyContent="center"
+                flexDirection="column">
+                <FlexColumn gap="small">
+                  <Text as="h1" variant="titleBig" aria-live="polite">
+                    {this.formTitle()}
+                  </Text>
+
+                  {this.createLoginForm()}
+                </FlexColumn>
+                <Flex justifyContent="center">
+                  <StyledLine margin={{ vertical: 'small', right: 'small' }} />
+                  <Text variant="labelTinySemiBold">
+                    {
+                      // TRANSLATORS: Text shown between two horizontal lines above the "create account" button.
+                      // TRANSLATORS: In this context it is used to separate the users alternative of logging in
+                      // TRANSLATORS: or creating a new account, "Login or Create a new account".
+                      messages.pgettext('login-view', 'Or')
+                    }
+                  </Text>
+                  <StyledLine margin={{ vertical: 'small', left: 'small' }} />
+                </Flex>
+              </View.Container>
+              {this.createFooter()}
             </FlexColumn>
-            {this.createFooter()}
-          </FlexColumn>
-        </View.Container>
+          </View.Container>
+        </View.Content>
       </View>
     );
   }

desktop/packages/mullvad-vpn/src/renderer/components/views/manage-devices/ManageDevicesView.tsx
@@ -19,8 +19,8 @@ export function ManageDevicesView() {
 
   return (
     <ManageDevicesProvider isLoading={isLoading} isFetching={isFetching} refetchDevices={refetch}>
-      <BackAction action={pop}>
-        <View backgroundColor="darkBlue">
+      <View backgroundColor="darkBlue">
+        <BackAction action={pop}>
           <NavigationContainer>
             <AppNavigationHeader
               title={
@@ -29,31 +29,33 @@ export function ManageDevicesView() {
               }
             />
             <NavigationScrollbars>
-              <FlexColumn gap="medium">
-                <View.Container marginInline="medium" flexDirection="column" gap="small">
-                  <Text variant="titleBig">
-                    {
-                      // TRANSLATORS: Title text in the manage devices view
-                      messages.pgettext('device-management', 'Manage devices')
-                    }
-                  </Text>
-                  <Text variant="labelTiny" color="whiteAlpha60">
-                    {
-                      // TRANSLATORS: Subtitle text in the manage devices view, explaining
-                      // TRANSLATORS: devices and what they can do in the manage devices view.
-                      messages.pgettext(
-                        'device-management',
-                        'View and manage all your logged in devices. You can have up to 5 devices on one account at a time. Each device gets a name when logged in to help you tell them apart easily.',
-                      )
-                    }
-                  </Text>
-                </View.Container>
-                {showDeviceList ? <DeviceList devices={devices} /> : <DevicesState />}
-              </FlexColumn>
+              <View.Content>
+                <FlexColumn gap="medium">
+                  <View.Container marginInline="medium" flexDirection="column" gap="small">
+                    <Text variant="titleBig">
+                      {
+                        // TRANSLATORS: Title text in the manage devices view
+                        messages.pgettext('device-management', 'Manage devices')
+                      }
+                    </Text>
+                    <Text variant="labelTiny" color="whiteAlpha60">
+                      {
+                        // TRANSLATORS: Subtitle text in the manage devices view, explaining
+                        // TRANSLATORS: devices and what they can do in the manage devices view.
+                        messages.pgettext(
+                          'device-management',
+                          'View and manage all your logged in devices. You can have up to 5 devices on one account at a time. Each device gets a name when logged in to help you tell them apart easily.',
+                        )
+                      }
+                    </Text>
+                  </View.Container>
+                  {showDeviceList ? <DeviceList devices={devices} /> : <DevicesState />}
+                </FlexColumn>
+              </View.Content>
             </NavigationScrollbars>
           </NavigationContainer>
-        </View>
-      </BackAction>
+        </BackAction>
+      </View>
     </ManageDevicesProvider>
   );
 }

desktop/packages/mullvad-vpn/src/renderer/components/views/too-many-devices/TooManyDevicesView.tsx
@@ -48,44 +48,42 @@ export function TooManyDevicesView() {
         <AppMainHeader.SettingsButton />
       </AppMainHeader>
       <StyledCustomScrollbars fillContainer>
-        <FlexColumn gap="large">
-          <View.Container flexDirection="column" marginInline="medium">
-            <Flex justifyContent="center" margin={{ top: 'large' }}>
-              <IconBadge key={imageSource} state={imageSource} />
-            </Flex>
-          </View.Container>
-          {devices !== undefined && (
-            <>
-              <View.Container flexDirection="column" marginInline="medium" gap="small">
-                <Text variant="titleLarge" data-testid="title">
-                  {title}
-                </Text>
-                <Text variant="labelTiny">{subtitle}</Text>
-              </View.Container>
-              <DeviceList devices={devices} />
-            </>
-          )}
-
-          {devices !== undefined && (
-            <View.Container
-              flexDirection="column"
-              marginInline="medium"
-              gap="medium"
-              padding={{ bottom: 'large' }}>
-              <Button variant="success" onClick={continueLogin} disabled={continueButtonDisabled}>
-                <Button.Text>
-                  {
-                    // TRANSLATORS: Button for continuing login process.
-                    messages.pgettext('device-management', 'Continue with login')
-                  }
-                </Button.Text>
-              </Button>
-              <Button onClick={cancel}>
-                <Button.Text>{messages.gettext('Back')}</Button.Text>
-              </Button>
+        <View.Content>
+          <FlexColumn gap="large">
+            <View.Container flexDirection="column" marginInline="medium">
+              <Flex justifyContent="center" margin={{ top: 'large' }}>
+                <IconBadge key={imageSource} state={imageSource} />
+              </Flex>
             </View.Container>
-          )}
-        </FlexColumn>
+            {devices !== undefined && (
+              <>
+                <View.Container flexDirection="column" marginInline="medium" gap="small">
+                  <Text variant="titleLarge" data-testid="title">
+                    {title}
+                  </Text>
+                  <Text variant="labelTiny">{subtitle}</Text>
+                </View.Container>
+                <DeviceList devices={devices} />
+              </>
+            )}
+
+            {devices !== undefined && (
+              <View.Container flexDirection="column" marginInline="medium" gap="medium">
+                <Button variant="success" onClick={continueLogin} disabled={continueButtonDisabled}>
+                  <Button.Text>
+                    {
+                      // TRANSLATORS: Button for continuing login process.
+                      messages.pgettext('device-management', 'Continue with login')
+                    }
+                  </Button.Text>
+                </Button>
+                <Button onClick={cancel}>
+                  <Button.Text>{messages.gettext('Back')}</Button.Text>
+                </Button>
+              </View.Container>
+            )}
+          </FlexColumn>
+        </View.Content>
       </StyledCustomScrollbars>
     </View>
   );