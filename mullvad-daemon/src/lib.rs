769ecf1bd6640c7eedf4ffd27e9257fa56a32b02
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-12-16 10:50:40 +0100

    Add release version script to be run from a qubes computer

--- mullvad-daemon/src/lib.rs
+++ mullvad-daemon/src/lib.rs
@@ -73,8 +73,6 @@ use mullvad_types::{
 };
 #[cfg(not(target_os = "android"))]
 use mullvad_update::version::Rollout;
-#[cfg(target_os = "android")]
-use mullvad_update::version::SUPPORTED_VERSION;
 use relay_list::{RELAYS_FILENAME, RelayListUpdater, RelayListUpdaterHandle};
 use settings::SettingsPersister;
 use std::collections::BTreeSet;
@@ -972,14 +970,13 @@ impl Daemon {
                 .expect("App version to be parsable");
             Rollout::threshold(seed, version)
         };
-        #[cfg(target_os = "android")]
-        let rollout = SUPPORTED_VERSION;
         let version_handle = version::router::spawn_version_router(
             api_handle.clone(),
             api_handle.availability.clone(),
             config.cache_dir.clone(),
             internal_event_tx.to_specialized_sender(),
             settings.show_beta_releases,
+            #[cfg(not(target_os = "android"))]
             rollout,
             app_upgrade_broadcast,
         );
