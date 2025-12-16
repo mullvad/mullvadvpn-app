anthunyy/thevadvpn-app

Use view components in changelog view

desktop/packages/mullvad-vpn/src/renderer/components/views/changelog/ChangelogView.tsx
@@ -1,11 +1,11 @@
 import { messages } from '../../../../shared/gettext';
-import { Container, Flex, TitleBig, TitleLarge } from '../../../lib/components';
+import { Flex, TitleBig, TitleLarge } from '../../../lib/components';
+import { View } from '../../../lib/components/view';
 import { useHistory } from '../../../lib/history';
 import { useVersionCurrent } from '../../../redux/hooks';
 import { AppNavigationHeader } from '../../';
 import { ChangelogList } from '../../changelog-list';
 import { BackAction } from '../../KeyboardNavigation';
-import { Layout, SettingsContainer } from '../../Layout';
 import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
 import { NoChangelogUpdates } from './components';
@@ -18,46 +18,44 @@ export const ChangelogView = () => {
   const showChangelogList = useShowChangelogList();
 
   return (
-    <BackAction action={pop}>
-      <Layout>
-        <SettingsContainer>
-          <NavigationContainer>
-            <AppNavigationHeader
-              title={
-                // TRANSLATORS: Heading for the view of the changes and updates in the
-                // TRANSLATORS: current version compared to the old version.
-                messages.pgettext('changelog-view', 'What’s new')
-              }
-            />
+    <View backgroundColor="darkBlue">
+      <BackAction action={pop}>
+        <NavigationContainer>
+          <AppNavigationHeader
+            title={
+              // TRANSLATORS: Heading for the view of the changes and updates in the
+              // TRANSLATORS: current version compared to the old version.
+              messages.pgettext('changelog-view', 'What’s new')
+            }
+          />
 
-            <NavigationScrollbars>
-              <Flex flexDirection="column" gap="large">
-                <Container indent="medium">
-                  <TitleBig as="h1">
-                    {
-                      // TRANSLATORS: Heading for the view of the changes and updates in the
-                      // TRANSLATORS: current version compared to the old version.
-                      messages.pgettext('changelog-view', 'What’s new')
-                    }
-                  </TitleBig>
-                </Container>
-                <Flex flexDirection="column" gap="small">
-                  <Container indent="medium">
-                    <TitleLarge as="h2">{current}</TitleLarge>
-                  </Container>
-                  <Container indent="large" flexDirection="column">
-                    {showChangelogList ? (
-                      <ChangelogList changelog={changelog} />
-                    ) : (
-                      <NoChangelogUpdates />
-                    )}
-                  </Container>
-                </Flex>
+          <NavigationScrollbars>
+            <View.Content gap="large">
+              <View.Container indent="medium">
+                <TitleBig as="h1">
+                  {
+                    // TRANSLATORS: Heading for the view of the changes and updates in the
+                    // TRANSLATORS: current version compared to the old version.
+                    messages.pgettext('changelog-view', 'What’s new')
+                  }
+                </TitleBig>
+              </View.Container>
+              <Flex flexDirection="column" gap="small">
+                <View.Container indent="medium">
+                  <TitleLarge as="h2">{current}</TitleLarge>
+                </View.Container>
+                <View.Container indent="large" flexDirection="column">
+                  {showChangelogList ? (
+                    <ChangelogList changelog={changelog} />
+                  ) : (
+                    <NoChangelogUpdates />
+                  )}
+                </View.Container>
               </Flex>
-            </NavigationScrollbars>
-          </NavigationContainer>
-        </SettingsContainer>
-      </Layout>
-    </BackAction>
+            </View.Content>
+          </NavigationScrollbars>
+        </NavigationContainer>
+      </BackAction>
+    </View>
   );
 };

mullvad/mullvadvpn-app

desktop/packages/mullvad-vpn/src/renderer/components/views/changelog/ChangelogView.tsx
@@ -1,11 +1,11 @@
 import { messages } from '../../../../shared/gettext';
-import { Container, Flex, TitleBig, TitleLarge } from '../../../lib/components';
+import { Flex, TitleBig, TitleLarge } from '../../../lib/components';
+import { View } from '../../../lib/components/view';
 import { useHistory } from '../../../lib/history';
 import { useVersionCurrent } from '../../../redux/hooks';
 import { AppNavigationHeader } from '../../';
 import { ChangelogList } from '../../changelog-list';
 import { BackAction } from '../../KeyboardNavigation';
-import { Layout, SettingsContainer } from '../../Layout';
 import { NavigationContainer } from '../../NavigationContainer';
 import { NavigationScrollbars } from '../../NavigationScrollbars';
 import { NoChangelogUpdates } from './components';
@@ -18,46 +18,44 @@ export const ChangelogView = () => {
   const showChangelogList = useShowChangelogList();
 
   return (
-    <BackAction action={pop}>
-      <Layout>
-        <SettingsContainer>
-          <NavigationContainer>
-            <AppNavigationHeader
-              title={
-                // TRANSLATORS: Heading for the view of the changes and updates in the
-                // TRANSLATORS: current version compared to the old version.
-                messages.pgettext('changelog-view', 'What’s new')
-              }
-            />
+    <View backgroundColor="darkBlue">
+      <BackAction action={pop}>
+        <NavigationContainer>
+          <AppNavigationHeader
+            title={
+              // TRANSLATORS: Heading for the view of the changes and updates in the
+              // TRANSLATORS: current version compared to the old version.
+              messages.pgettext('changelog-view', 'What’s new')
+            }
+          />
 
-            <NavigationScrollbars>
-              <Flex flexDirection="column" gap="large">
-                <Container horizontalMargin="medium">
-                  <TitleBig as="h1">
-                    {
-                      // TRANSLATORS: Heading for the view of the changes and updates in the
-                      // TRANSLATORS: current version compared to the old version.
-                      messages.pgettext('changelog-view', 'What’s new')
-                    }
-                  </TitleBig>
-                </Container>
-                <Flex flexDirection="column" gap="small">
-                  <Container horizontalMargin="medium">
-                    <TitleLarge as="h2">{current}</TitleLarge>
-                  </Container>
-                  <Container horizontalMargin="large" flexDirection="column">
-                    {showChangelogList ? (
-                      <ChangelogList changelog={changelog} />
-                    ) : (
-                      <NoChangelogUpdates />
-                    )}
-                  </Container>
-                </Flex>
+          <NavigationScrollbars>
+            <View.Content gap="large">
+              <View.Container horizontalMargin="medium">
+                <TitleBig as="h1">
+                  {
+                    // TRANSLATORS: Heading for the view of the changes and updates in the
+                    // TRANSLATORS: current version compared to the old version.
+                    messages.pgettext('changelog-view', 'What’s new')
+                  }
+                </TitleBig>
+              </View.Container>
+              <Flex flexDirection="column" gap="small">
+                <View.Container horizontalMargin="medium">
+                  <TitleLarge as="h2">{current}</TitleLarge>
+                </View.Container>
+                <View.Container horizontalMargin="large" flexDirection="column">
+                  {showChangelogList ? (
+                    <ChangelogList changelog={changelog} />
+                  ) : (
+                    <NoChangelogUpdates />
+                  )}
+                </View.Container>
               </Flex>
-            </NavigationScrollbars>
-          </NavigationContainer>
-        </SettingsContainer>
-      </Layout>
-    </BackAction>
+            </View.Content>
+          </NavigationScrollbars>
+        </NavigationContainer>
+      </BackAction>
+    </View>
   );
 };