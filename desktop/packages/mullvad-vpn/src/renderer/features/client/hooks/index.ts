d07d12ca28e1fed2cfa2045d1a673f7cb89fa4d8
Oliver <oliver@mohlin.dev>
2025-12-17 12:20:19 +0100

    Add useLocale hook

--- desktop/packages/mullvad-vpn/src/renderer/features/client/hooks/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/features/client/hooks/index.ts
@@ -3,5 +3,6 @@ export * from './use-auto-connect';
 export * from './use-auto-start';
 export * from './use-enable-system-notifications';
 export * from './use-monochromatic-tray-icon';
+export * from './use-locale';
 export * from './use-start-minimized';
 export * from './use-unpinned-window';
