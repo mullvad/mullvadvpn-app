88fa800d7660655461d358810695f330126d0d8d
Oliver <oliver@mohlin.dev>
2025-12-18 10:16:06 +0100

    Update DeviceList and related components to align with updated design

--- desktop/packages/mullvad-vpn/src/renderer/components/device-list/DeviceList.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/device-list/DeviceList.tsx
@@ -1,19 +1,50 @@
+import styled from 'styled-components';
+
 import { IDevice } from '../../../shared/daemon-rpc-types';
 import { AnimatedList } from '../../lib/components/animated-list';
+import { ListItemPositions } from '../../lib/components/list-item';
+import { StyledListItemItem } from '../../lib/components/list-item/components';
+import { useSelector } from '../../redux/store';
 import { DeviceListItem } from '../device-list-item';
 
 export type DeviceListProps = {
   devices: IDevice[];
 };
 
+const StyledAnimatedList = styled(AnimatedList)`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledAnimatedListItem = styled(AnimatedList.Item)`
+  ${StyledListItemItem} {
+    transition: border-radius 0.15s ease-out;
+  }
+`;
+
 export function DeviceList({ devices }: DeviceListProps) {
+  const currentDeviceName = useSelector((state) => state.account.deviceName);
+  const currentDevice = devices.find((device) => device.name === currentDeviceName);
+  const nonCurrentDevices = devices.filter((device) => device.name !== currentDeviceName);
+
   return (
-    <AnimatedList>
-      {devices.map((device) => (
-        <AnimatedList.Item key={device.id}>
-          <DeviceListItem device={device} />
-        </AnimatedList.Item>
-      ))}
-    </AnimatedList>
+    <>
+      {currentDevice && <DeviceListItem device={currentDevice} />}
+      <StyledAnimatedList>
+        {nonCurrentDevices.map((device, idx) => {
+          let position: ListItemPositions | undefined = 'middle';
+          if (idx === 0) {
+            position = 'first';
+          } else if (idx === nonCurrentDevices.length - 1) {
+            position = 'last';
+          }
+          return (
+            <StyledAnimatedListItem key={device.id}>
+              <DeviceListItem position={position} device={device} />
+            </StyledAnimatedListItem>
+          );
+        })}
+      </StyledAnimatedList>
+    </>
   );
 }
