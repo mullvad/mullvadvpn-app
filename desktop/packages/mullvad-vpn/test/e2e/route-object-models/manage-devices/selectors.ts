f06a127355c6c99009f108004108c2d4c53c5f93
Oliver <oliver@mohlin.dev>
2025-12-18 10:16:06 +0100

    Update DeviceList and related components to align with updated design

--- desktop/packages/mullvad-vpn/test/e2e/route-object-models/manage-devices/selectors.ts
+++ desktop/packages/mullvad-vpn/test/e2e/route-object-models/manage-devices/selectors.ts
@@ -1,6 +1,7 @@
 import { Page } from 'playwright';
 
 export const createSelectors = (page: Page) => ({
+  currentDeviceText: () => page.getByText('Current device'),
   deviceListItems: () => page.getByRole('list'),
   deviceListItem: (name: string) => page.getByRole('listitem').filter({ hasText: name }),
   removeDeviceButton: (name: string) => page.getByLabel(`Remove device named ${name}`),
