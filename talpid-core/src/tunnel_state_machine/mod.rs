322920d7be122dbc5cb98614165c094f509b5a7c
Sebastian Holmin <sebastian.holmin@hotmail.com>
2025-12-18 23:22:41 +0100

    Instrument `TunnelStateMachine::run`

--- talpid-core/src/tunnel_state_machine/mod.rs
+++ talpid-core/src/tunnel_state_machine/mod.rs
@@ -502,6 +502,7 @@ impl TunnelStateMachine {
         .unwrap()
     }
 
+    #[tracing::instrument(skip_all, name = "State transition")]
     fn run(mut self, change_listener: impl Sender<TunnelStateTransition> + Send + 'static) {
         use EventConsequence::*;
 
