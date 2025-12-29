c978d890ada9fc3399d0bb3f8ae71f0b97808055
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2025-12-29 13:56:59 +0100

    Update user account expiry before showing notification
    Since it is possible for users to update their account time
    outside of the app the account will expire soon notification
    can refer to an outdated expiry date.
    
    To avoid this we will always start a background work to check
    the latest expiry date before sending a notification.
    
    If it is not possible to update the expiry date it will not show
    a notification.

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/receiver/NotificationAlarmReceiver.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/receiver/NotificationAlarmReceiver.kt
@@ -3,42 +3,39 @@ package net.mullvad.mullvadvpn.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
+import androidx.work.WorkManager
 import co.touchlab.kermit.Logger
-import java.time.Duration
-import java.time.ZonedDateTime
-import net.mullvad.mullvadvpn.service.notifications.accountexpiry.AccountExpiryNotificationProvider
-import net.mullvad.mullvadvpn.usecase.ScheduleNotificationAlarmUseCase
-import net.mullvad.mullvadvpn.util.goAsync
-import net.mullvad.mullvadvpn.util.serializable
+import net.mullvad.mullvadvpn.worker.ExpiryNotificationWorker
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 class NotificationAlarmReceiver : BroadcastReceiver(), KoinComponent {
-    private val notificationProvider by inject<AccountExpiryNotificationProvider>()
-    private val scheduleNotificationAlarmUseCase by inject<ScheduleNotificationAlarmUseCase>()
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-
-        val expiry: ZonedDateTime? = intent?.serializable(ACCOUNT_EXPIRY_KEY)
-        if (expiry == null) {
-            Logger.e("NotificationAlarmReceiver: Intent missing account expiry")
-            return
-        }
-
+    override fun onReceive(context: Context, intent: Intent?) {
+        // It is not possible to bind to a service from a notification alarm receiver so we will use
+        // a worker instead.
         Logger.d("Account expiry alarm triggered")
-        val untilExpiry = Duration.between(ZonedDateTime.now(), expiry)
 
-        notificationProvider.showNotification(untilExpiry)
-
-        goAsync {
-            // Only schedule the next alarm if we still have time left on the account.
-            if (context != null && expiry > ZonedDateTime.now()) {
-                scheduleNotificationAlarmUseCase(accountExpiry = expiry, customContext = context)
-            }
-        }
-    }
-
-    companion object {
-        const val ACCOUNT_EXPIRY_KEY: String = "account_expiry_key"
+        val work =
+            OneTimeWorkRequestBuilder<ExpiryNotificationWorker>()
+                .apply {
+                    // Setting expedited on android 12 or lower will cause the work manager to
+                    // request a wake lock. We want to avoid using wakelocks so we disable expedited
+                    // on android 12 and lower.
+                    if (
+                        android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU
+                    ) {
+                        setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                    }
+                }
+                .setConstraints(
+                    Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+                )
+                .build()
+        WorkManager.getInstance(context).enqueue(work)
+        return
     }
 }
