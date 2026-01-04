b7deb59b221f12cccef6cce4d8e19508626b8aa1
Emīls <emils@mullvad.net>
2026-01-04 21:15:47 +0100

    Navigate to IP version from pills

--- ios/MullvadVPN/View controllers/VPNSettings/VPNSettingsDataSource.swift
+++ ios/MullvadVPN/View controllers/VPNSettings/VPNSettingsDataSource.swift
@@ -250,6 +250,7 @@ final class VPNSettingsDataSource: UITableViewDiffableDataSource<
             switch section {
             case .obfuscation: .wireGuardObfuscation
             case .quantumResistance: .quantumResistance
+            case .ipVersion: .ipVersion
             default: nil
             }
 
@@ -518,6 +519,12 @@ final class VPNSettingsDataSource: UITableViewDiffableDataSource<
                 .appendItems(
                     Item.quantumResistance,
                     toSection: .quantumResistance
+                )
+        } else if onlyShowSection == .ipVersion {
+            snapshot
+                .appendItems(
+                    Item.ipVersion,
+                    toSection: .ipVersion
                 )
         }
 
