d4f94fc9fa08e5adaaabb6cc7d7cc669279b50fb
Emīls <emils@mullvad.net>
2026-01-09 16:57:19 +0100

    Move access method disabling to base class

--- ios/MullvadVPNUITests/Payment/PaymentTests.swift
+++ ios/MullvadVPNUITests/Payment/PaymentTests.swift
@@ -147,28 +147,6 @@ class PaymentTests: LoggedOutUITestCase {
 
 @available(iOS 26.0, *)
 extension PaymentTests {
-    private func disableBridgesAccessMethod() {
-        HeaderBar(app)
-            .tapSettingsButton()
-
-        SettingsPage(app)
-            .tapAPIAccessCell()
-
-        APIAccessPage(app)
-            .getAccessMethodCell(accessibilityId: AccessibilityIdentifier.accessMethodBridgesCell)
-            .tap()
-
-        EditAccessMethodPage(app)
-            .tapEnableMethodSwitch()
-            .tapBackButton()
-
-        // Navigate back to main screen
-        let backButton = app.navigationBars.firstMatch.buttons.firstMatch
-        backButton.tap()
-
-        SettingsPage(app)
-            .tapDoneButton()
-    }
 
     private func createAndLogInToNewAccount() -> String {
         LoginPage(app)
