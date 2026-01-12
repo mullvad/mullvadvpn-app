589f79c3ad3b00c061de4352cef6159b8c2db6a9
Emīls <emils@mullvad.net>
2026-01-13 00:00:34 +0100

    Consolidate relay stubs

--- ios/MullvadMockData/MullvadREST/RelaySelectorStub.swift
+++ ios/MullvadMockData/MullvadREST/RelaySelectorStub.swift
@@ -41,40 +41,40 @@ public final class RelaySelectorStub: RelaySelectorProtocol {
     ) throws -> RelayCandidates {
         return try candidatesResult?() ?? RelayCandidates(entryRelays: [], exitRelays: [])
     }
+
+    private static let relay = SelectedRelay(
+        endpoint: SelectedEndpoint(
+            socketAddress: .ipv4(IPv4Endpoint(ip: .loopback, port: 1300)),
+            ipv4Gateway: .loopback,
+            ipv6Gateway: .loopback,
+            publicKey: PrivateKey().publicKey.rawValue,
+            obfuscation: .off
+        ),
+        hostname: "se-got",
+        location: Location(
+            country: "",
+            countryCode: "se",
+            city: "",
+            cityCode: "got",
+            latitude: 0,
+            longitude: 0
+        ),
+        features: nil
+    )
+
+    public static let selectedRelays = SelectedRelays(
+        entry: relay,
+        exit: relay,
+        retryAttempt: 0
+    )
 }
 
 extension RelaySelectorStub {
     /// Returns a relay selector that never fails.
     public static func nonFallible() -> RelaySelectorStub {
-        let publicKey = PrivateKey().publicKey.rawValue
-
         return RelaySelectorStub(
             selectedRelaysResult: { _ in
-                let cityRelay = SelectedRelay(
-                    endpoint: SelectedEndpoint(
-                        socketAddress: .ipv4(IPv4Endpoint(ip: .loopback, port: 1300)),
-                        ipv4Gateway: .loopback,
-                        ipv6Gateway: .loopback,
-                        publicKey: publicKey,
-                        obfuscation: .off
-                    ),
-                    hostname: "se-got",
-                    location: Location(
-                        country: "",
-                        countryCode: "se",
-                        city: "",
-                        cityCode: "got",
-                        latitude: 0,
-                        longitude: 0
-                    ),
-                    features: nil
-                )
-
-                return SelectedRelays(
-                    entry: cityRelay,
-                    exit: cityRelay,
-                    retryAttempt: 0
-                )
+                return Self.selectedRelays
             }, candidatesResult: nil)
     }
 
@@ -89,27 +89,4 @@ extension RelaySelectorStub {
             })
     }
 
-    public static let selectedRelays = SelectedRelays(
-        entry: nil,
-        exit: SelectedRelay(
-            endpoint: SelectedEndpoint(
-                socketAddress: .ipv4(IPv4Endpoint(ip: .loopback, port: 42)),
-                ipv4Gateway: IPv4Address.loopback,
-                ipv6Gateway: IPv6Address.loopback,
-                publicKey: Data(),
-                obfuscation: .off
-            ),
-            hostname: "se-got-wg-001",
-            location: Location(
-                country: "Sweden",
-                countryCode: "se",
-                city: "Gothenburg",
-                cityCode: "got",
-                latitude: 42,
-                longitude: 42
-            ),
-            features: nil
-        ),
-        retryAttempt: 0
-    )
 }
