e6895ca3fa578ae708b17773bf45eed7e01345ac
Oliver <oliver@mohlin.dev>
2026-01-14 16:56:19 +0100

    Refactor SettingsListbox to expose ListItem through namespace

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/listbox/Listbox.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/listbox/Listbox.tsx
@@ -1,40 +1,44 @@
 import React from 'react';
 
-import { ListItem, ListItemProps } from '../list-item';
-import { ListboxItem, ListboxLabel, ListboxOption, ListboxOptions } from './components';
+import { ListItem } from '../list-item';
+import {
+  ListboxFooter,
+  ListboxFooterText,
+  ListboxHeader,
+  ListboxItem,
+  ListboxLabel,
+  ListboxOption,
+  ListboxOptions,
+} from './components';
 import { ListboxProvider } from './ListboxContext';
 
-export type ListboxProps<T> = ListItemProps & {
+export type ListboxProps<T> = React.PropsWithChildren<{
   onValueChange?: (value: T) => Promise<void>;
   value?: T;
   labelId?: string;
-};
+}>;
 
-function Listbox<T>({
-  value,
-  onValueChange,
-  labelId: labelIdProp,
-  children,
-  ...props
-}: ListboxProps<T>) {
+function Listbox<T>({ value, onValueChange, labelId: labelIdProp, children }: ListboxProps<T>) {
   const labelId = React.useId();
 
   return (
     <ListboxProvider labelId={labelIdProp ?? labelId} value={value} onValueChange={onValueChange}>
-      <ListItem position="first" {...props}>
+      <div tabIndex={-1} role="region" aria-labelledby={labelIdProp ?? labelId}>
         {children}
-      </ListItem>
+      </div>
     </ListboxProvider>
   );
 }
 
 const ListboxNamespace = Object.assign(Listbox, {
+  Header: ListboxHeader,
   Item: ListboxItem,
   Content: ListItem.Content,
   Label: ListboxLabel,
   Group: ListItem.Group,
   Text: ListItem.Text,
-  Footer: ListItem.Footer,
+  Footer: ListboxFooter,
+  FooterText: ListboxFooterText,
   Icon: ListItem.Icon,
   Option: ListboxOption,
   Options: ListboxOptions,
