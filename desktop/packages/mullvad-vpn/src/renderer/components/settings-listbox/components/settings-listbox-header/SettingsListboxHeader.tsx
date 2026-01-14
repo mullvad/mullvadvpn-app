439d0615ceb55a200d23d5103419f13e107af9ea
Oliver <oliver@mohlin.dev>
2026-01-14 16:50:03 +0100

    Add SettingsListboxHeader component

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/settings-listbox/components/settings-listbox-header/SettingsListboxHeader.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+import { useScrollToListItem } from '../../../../hooks';
+import { Listbox } from '../../../../lib/components/listbox';
+import { ListboxHeaderProps } from '../../../../lib/components/listbox/components';
+import { useSettingsListboxContext } from '../../SettingsListboxContext';
+
+export type SettingsListboxHeaderProps = ListboxHeaderProps;
+
+export const StyledSettingsListboxHeader = styled(Listbox.Header)``;
+
+export function SettingsListboxHeader({ children, ...props }: SettingsListboxHeaderProps) {
+  const { anchorId } = useSettingsListboxContext();
+  const { ref, animation } = useScrollToListItem(anchorId);
+  return (
+    <StyledSettingsListboxHeader ref={ref} animation={animation} {...props}>
+      {children}
+    </StyledSettingsListboxHeader>
+  );
+}
