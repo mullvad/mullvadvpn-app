6fee8727b2f06c92f406c8710aa422380d787ee0
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-01-15 16:50:04 +0100

    fixup! Move gotatun conversion functions to conversions.rs

--- talpid-wireguard/src/gotatun/conversions.rs
+++ talpid-wireguard/src/gotatun/conversions.rs
@@ -1,8 +1,57 @@
 //! Conversions between [`gotatun`]-types and `talpid_wireguard`-types.
 
-use std::time::SystemTime;
+use std::{str::FromStr, time::SystemTime};
 
-use crate::stats::{DaitaStats, Stats};
+use gotatun::device::{Peer, daita::Machine};
+use talpid_tunnel_config_client::DaitaSettings;
+use talpid_types::net::wireguard::PeerConfig;
+
+use crate::{
+    TunnelError,
+    stats::{DaitaStats, Stats},
+};
+
+/// Convert a [`PeerConfig`] into a GotaTun [`Peer`].
+///
+/// Returns [`TunnelError::StartDaita`] if the maybenot machines fails to parse.
+fn to_gotatun_peer(peer: &PeerConfig, daita: Option<&DaitaSettings>) -> Result<Peer, TunnelError> {
+    let PeerConfig {
+        public_key,
+        allowed_ips,
+        endpoint,
+        psk,
+        constant_packet_size: _,
+    } = peer.clone();
+
+    let mut peer = Peer::new((*public_key.as_bytes()).into())
+        .with_allowed_ips(allowed_ips)
+        .with_endpoint(endpoint);
+
+    if let Some(psk) = psk {
+        // TODO: implement zeroize in gotatun
+        peer = peer.with_preshared_key(*psk.as_bytes());
+    }
+
+    if let Some(daita) = daita {
+        let daita = gotatun::device::daita::DaitaSettings {
+            maybenot_machines: daita
+                .client_machines
+                .iter()
+                // TODO: deserialize machines earlier. Preferably when getting them from the gRPC service.
+                .map(|machine_str| Machine::from_str(machine_str))
+                .collect::<Result<_, _>>()
+                .map_err(|e| TunnelError::StartDaita(Box::new(e)))?,
+            max_padding_frac: daita.max_padding_frac,
+            max_blocking_frac: daita.max_blocking_frac,
+            // TODO: tweak to sane values
+            max_blocked_packets: 1024,
+            min_blocking_capacity: 50,
+        };
+        peer = peer.with_daita(daita);
+    }
+
+    Ok(peer)
+}
 
 impl From<gotatun::device::configure::Stats> for Stats {
     fn from(peer_stats: gotatun::device::configure::Stats) -> Self {
