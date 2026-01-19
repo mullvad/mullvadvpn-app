6b13a9d05277f7eebfafac121b63993c0e47f869
Sebastian Holmin <sebastian.holmin@mullvad.net>
2026-01-19 15:27:54 +0000

    Make android not depened on `mullvad-version`

--- mullvad-types/src/version.rs
+++ mullvad-types/src/version.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, path::PathBuf};
+use std::fmt::Display;
 
 use serde::{Deserialize, Serialize};
 
@@ -14,12 +14,14 @@ pub struct AppVersionInfo {
     ///
     /// The user should really upgrade when this is false.
     pub current_version_supported: bool,
+    #[cfg(not(target_os = "android"))]
     /// A newer version that may be upgraded to
     pub suggested_upgrade: Option<SuggestedUpgrade>,
 }
 
 impl Display for AppVersionInfo {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        #[cfg(not(target_os = "android"))]
         if let Some(suggested_upgrade) = &self.suggested_upgrade {
             writeln!(f, "Suggested upgrade: {}", suggested_upgrade.version)?;
             if let Some(path) = &suggested_upgrade.verified_installer_path {
@@ -36,13 +38,14 @@ impl Display for AppVersionInfo {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[cfg(not(target_os = "android"))]
 pub struct SuggestedUpgrade {
     /// Version available for update
     pub version: mullvad_version::Version,
     /// Changelog
     pub changelog: String,
     /// Path to the available installer, iff it has been verified
-    pub verified_installer_path: Option<PathBuf>,
+    pub verified_installer_path: Option<std::path::PathBuf>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
