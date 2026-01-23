c84661efdc672fd56ee6821a9bbefd8fa1ba2601
Oliver <oliver@mohlin.dev>
2026-01-23 11:27:38 +0100

    Add new radius tokens

--- desktop/packages/mullvad-vpn/src/renderer/lib/foundations/tokens/radius-tokens.ts
+++ desktop/packages/mullvad-vpn/src/renderer/lib/foundations/tokens/radius-tokens.ts
@@ -1,6 +1,11 @@
 export enum RadiusTokens {
   radius4 = '4px',
   radius8 = '8px',
-  radius11 = '11px',
   radius12 = '12px',
+  radius16 = '16px',
+  radius20 = '20px',
+  radius24 = '24px',
+  radius32 = '32px',
+  radius48 = '48px',
+  radiusFull = '1000px',
 }
