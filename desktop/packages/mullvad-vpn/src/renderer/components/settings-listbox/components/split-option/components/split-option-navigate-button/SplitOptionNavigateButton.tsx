45bd313894a5a14e132a4125271df7067848eca2
Oliver <oliver@mohlin.dev>
2026-01-25 14:54:37 +0100

    Use ListItemTrailingAction in SplitOptionNavigateButton

--- desktop/packages/mullvad-vpn/src/renderer/components/settings-listbox/components/split-option/components/split-option-navigate-button/SplitOptionNavigateButton.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/settings-listbox/components/split-option/components/split-option-navigate-button/SplitOptionNavigateButton.tsx
@@ -1,47 +1,12 @@
 import { useCallback } from 'react';
-import styled from 'styled-components';
 
 import { RoutePath } from '../../../../../../../shared/routes';
-import { Flex, Icon } from '../../../../../../lib/components';
-import { colors } from '../../../../../../lib/foundations';
+import { ListItem } from '../../../../../../lib/components/list-item';
 import { useHistory } from '../../../../../../lib/history';
 
 export type NavigationOptionNavigateProps = {
   to: RoutePath;
 } & React.ComponentPropsWithRef<'button'>;
-
-const StyledFlex = styled(Flex)`
-  background-color: ${colors.blue60};
-  height: 100%;
-`;
-
-const StyledSplitOptionNavigateButton = styled.button`
-  position: relative;
-  &&::before {
-    content: '';
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 1px;
-    height: 22px;
-    background-color: ${colors.darkBlue};
-  }
-  &&:hover {
-    ${StyledFlex} {
-      background-color: ${colors.blue};
-    }
-  }
-  &&:active {
-    ${StyledFlex} {
-      background-color: ${colors.whiteOnBlue20};
-    }
-  }
-  &&:focus-visible {
-    outline: 2px solid ${colors.white};
-    outline-offset: -2px;
-    z-index: 10;
-  }
-`;
 
 export function SplitOptionNavigateButton({
   to,
@@ -54,10 +19,10 @@ export function SplitOptionNavigateButton({
   }, [history, to]);
 
   return (
-    <StyledSplitOptionNavigateButton data-split-button onClick={navigate} tabIndex={-1} {...props}>
-      <StyledFlex justifyContent="center" alignItems="center" padding={{ horizontal: 'medium' }}>
-        <Icon icon={'chevron-right'} aria-hidden="true" />
-      </StyledFlex>
-    </StyledSplitOptionNavigateButton>
+    <ListItem.Trigger data-split-button onClick={navigate} tabIndex={-1} {...props}>
+      <ListItem.TrailingAction>
+        <ListItem.TrailingAction.Icon icon={'chevron-right'} aria-hidden="true" />
+      </ListItem.TrailingAction>
+    </ListItem.Trigger>
   );
 }
