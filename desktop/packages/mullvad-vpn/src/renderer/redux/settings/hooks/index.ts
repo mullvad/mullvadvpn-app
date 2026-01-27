16e2778c08ed2bb0fda2bc9bcd7113e26dbfc1f3
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-01-27 19:59:38 +0100

    Add redux state for if split tunneling is supported

--- desktop/packages/mullvad-vpn/src/renderer/redux/settings/hooks/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/redux/settings/hooks/index.ts
@@ -1,3 +1,4 @@
+export * from './use-settings-split-tunneling-supported';
 export * from './useSettingsShowBetaReleases';
 export * from './useSettingsRelaySettings';
 export * from './useSettingsDaitaEnabled';
