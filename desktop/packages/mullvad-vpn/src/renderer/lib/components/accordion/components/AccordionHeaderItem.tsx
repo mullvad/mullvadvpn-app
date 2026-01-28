0b591fd03f7b24c0803f3e737e67cec001ad8b3a
Oliver <oliver@mohlin.dev>
2026-01-28 15:02:06 +0100

    Move accordion header border styling into AccordionHeader

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionHeaderItem.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionHeaderItem.tsx
@@ -2,24 +2,11 @@ import styled from 'styled-components';
 
 import { ListItem } from '../../list-item';
 import { ListItemItemProps } from '../../list-item/components';
-import { useAccordionContext } from '../AccordionContext';
 
 export type AccordionHeaderItemProps = ListItemItemProps;
 
-export const StyledAccordionHeaderItem = styled(ListItem.Item)`
-  transition: border-radius 0.15s ease-out;
-  &[data-expanded='true'] {
-    margin-bottom: 1px;
-    border-bottom-left-radius: 0;
-    border-bottom-right-radius: 0;
-  }
-`;
+export const StyledAccordionHeaderItem = styled(ListItem.Item)``;
 
 export function AccordionHeaderItem({ children, ...props }: AccordionHeaderItemProps) {
-  const { expanded } = useAccordionContext();
-  return (
-    <StyledAccordionHeaderItem data-expanded={expanded} {...props}>
-      {children}
-    </StyledAccordionHeaderItem>
-  );
+  return <StyledAccordionHeaderItem {...props}>{children}</StyledAccordionHeaderItem>;
 }
