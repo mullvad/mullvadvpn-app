6bd4adabf3d7c743b087b9e6ed98093ee9c8d876
Oliver <oliver@mohlin.dev>
2026-01-28 13:30:55 +0100

    Add ListItemTriggerContext and conditionally disable trailing action icon

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trailing-action/components/list-item-trailing-action-icon/ListItemTrailingActionIcon.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trailing-action/components/list-item-trailing-action-icon/ListItemTrailingActionIcon.tsx
@@ -1,9 +1,14 @@
 import { Icon, IconProps } from '../../../../../icon';
 import { useListItemContext } from '../../../../ListItemContext';
+import { useListItemTriggerContext } from '../../../list-item-trigger/ListItemTriggerContext';
 
 type ListItemIconProps = Omit<IconProps, 'size'>;
 
 export function ListItemTrailingActionIcon({ ...props }: ListItemIconProps) {
-  const { disabled } = useListItemContext();
+  const { disabled: listItemDisabled } = useListItemContext();
+  const { disabled: listItemTriggerDisabled } = useListItemTriggerContext();
+
+  const disabled = listItemTriggerDisabled ?? listItemDisabled;
+
   return <Icon aria-hidden="true" color={disabled ? 'whiteAlpha40' : 'white'} {...props} />;
 }
