ef9958089d7ec559a876254e942a298d570e3e3f
Oliver <oliver@mohlin.dev>
2026-01-28 15:02:06 +0100

    Move accordion header border styling into AccordionHeader

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/accordion-header/AccordionHeader.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/accordion-header/AccordionHeader.tsx
@@ -1,11 +1,29 @@
 import styled from 'styled-components';
 
 import { ListItem, ListItemProps } from '../../../list-item';
+import { StyledListItemTrailingAction } from '../../../list-item/components';
 import { useAccordionContext } from '../../AccordionContext';
+import { StyledAccordionContent } from '../AccordionContent';
+import { StyledAccordionHeaderItem } from '../AccordionHeaderItem';
 
 export type AccordionHeaderProps = ListItemProps;
 
-export const StyledAccordionHeader = styled(ListItem)``;
+export const StyledAccordionHeader = styled(ListItem)`
+  ${StyledAccordionHeaderItem}, ${StyledListItemTrailingAction} {
+    transition: border-radius 0.15s ease-out;
+  }
+  &:has(+ ${StyledAccordionContent}) {
+    ${StyledAccordionHeaderItem} {
+      margin-bottom: 1px;
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+    ${StyledListItemTrailingAction} {
+      margin-bottom: 1px;
+      border-bottom-right-radius: 0;
+    }
+  }
+`;
 
 export function AccordionHeader({ children, ...props }: AccordionHeaderProps) {
   const { disabled } = useAccordionContext();
