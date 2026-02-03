25c259f1e04c18de70c25a4ca9b2db6ce1ec56d7
Kalle Lindström <karl.lindstrom@mullvad.net>
2026-02-03 15:12:15 +0100

    Implement sigsum verification for the relay list
    This is a draft PR for adding the sigsum verification logic for the
    relay list.
    
    This code will only work in stagemole and devmole for now but
    should successfuly verify the relay list.
    
    It replaces the current api call for fetching the relay list with two
    new calls:
    
    1. /trl/v0/timestamps/latest
      fetches the latest sigsum signature and digest data for the
      relay list.
    2. /trl/v0/data/
      fetches the actual relay list given a sigsum digest.

--- mullvad-api/src/rest.rs
+++ mullvad-api/src/rest.rs
@@ -1,7 +1,7 @@
 #[cfg(target_os = "android")]
 pub use crate::https_client::SocketBypassRequest;
 use crate::{
-    DnsResolver,
+    DnsResolver, SigsumPublicKey,
     access::AccessTokenStore,
     availability::ApiAvailability,
     https_client::{HttpsConnector, HttpsConnectorHandle, InnerConnectionMode},
@@ -86,6 +86,14 @@ pub enum Error {
     /// Body exceeded size limit
     #[error("Body exceeded size limit")]
     BodyTooLarge,
+
+    // TODO: better way to do this? Can't use DeserializeError because it expects a serde_json::Error
+    #[error("Failed to deserialize sigsum data")]
+    SigsumDeserializeError,
+
+    /// Body contained invalid UTF-8
+    #[error("Body contained invalid UTF-8")]
+    InvalidUtf8Error,
 }
 
 impl From<Infallible> for Error {
@@ -762,6 +770,7 @@ pub struct MullvadRestHandle {
     pub(crate) service: RequestServiceHandle,
     pub factory: RequestFactory,
     pub availability: ApiAvailability,
+    pub sigsum_trusted_pubkeys: Vec<SigsumPublicKey>,
 }
 
 impl MullvadRestHandle {
@@ -769,11 +778,13 @@ impl MullvadRestHandle {
         service: RequestServiceHandle,
         factory: RequestFactory,
         availability: ApiAvailability,
+        sigsum_trusted_pubkeys: Vec<SigsumPublicKey>,
     ) -> Self {
         Self {
             service,
             factory,
             availability,
+            sigsum_trusted_pubkeys,
         }
     }
 
