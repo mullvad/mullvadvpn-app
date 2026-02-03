1eba1b675bb4bc295cb3fd55037aa2c9322b1dce
Kalle Lindström <karl.lindstrom@mullvad.net>
2026-02-03 15:12:15 +0100

    Implement sigsum verification for the relay list
    This is a draft PR for adding the sigsum verification logic for the
    relay list.
    
    This code will only work in stagemole and devmole for now but
    should successfuly verify the relay list.
    
    It replaces the current api call for fetching the relay list with two
    new calls:
    
    1. /trl/v0/timestamps/latest
      fetches the latest sigsum signature and digest data for the
      relay list.
    2. /trl/v0/data/
      fetches the actual relay list given a sigsum digest.

--- mullvad-api/mullvad-api-constants/src/lib.rs
+++ mullvad-api/mullvad-api-constants/src/lib.rs
@@ -5,8 +5,10 @@ pub mod env {
     pub const API_ADDR_VAR: &str = "MULLVAD_API_ADDR";
     pub const API_FORCE_DIRECT_VAR: &str = "MULLVAD_API_FORCE_DIRECT";
     pub const DISABLE_TLS_VAR: &str = "MULLVAD_API_DISABLE_TLS";
+    pub const SIGSUM_TRUSTED_PUBKEYS_VAR: &str = "MULLVAD_SIGSUM_TRUSTED_PUBKEYS";
 }
 
 pub const API_HOST_DEFAULT: &str = "api.mullvad.net";
 pub const API_IP_DEFAULT: IpAddr = IpAddr::V4(Ipv4Addr::new(45, 83, 223, 196));
 pub const API_PORT_DEFAULT: u16 = 443;
+pub const SIGSUM_TRUSTED_PUBKEYS_DEFAULT: &str = include_str!("trusted-sigsum-signing-pubkeys");
