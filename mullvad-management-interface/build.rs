162674cb07697ac2cb0e41d057b5a2854180d52a
Sebastian Holmin <sebastian.holmin@mullvad.net>
2026-02-05 17:57:05 +0100

    WIP: add new relay selector api proto

--- mullvad-management-interface/build.rs
+++ mullvad-management-interface/build.rs
@@ -1,5 +1,14 @@
 fn main() {
-    tonic_build::compile_protos("proto/management_interface.proto").unwrap();
+    // Compile both proto files together so they can reference each other
+    tonic_build::configure()
+        .compile_protos(
+            &[
+                "proto/management_interface.proto",
+                "proto/relay_selector.proto",
+            ],
+            &["proto/"],
+        )
+        .unwrap();
 
     // Enable DAITA by default on desktop and android
     println!("cargo::rustc-check-cfg=cfg(daita)");
--- mullvad-management-interface/src/lib.rs
+++ mullvad-management-interface/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod client;
+pub mod relay_selector;
 pub mod types;
 
 #[cfg(unix)]
