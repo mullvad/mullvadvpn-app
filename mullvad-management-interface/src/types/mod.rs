a84e839314b6ecb1a1cfedb63bd3ea84e45c3b9b
Sebastian Holmin <sebastian.holmin@mullvad.net>
2026-02-05 17:57:05 +0100

    Add new relay selector gRPC service
    Move relay selector mod next to management interface

--- mullvad-management-interface/src/types/mod.rs
+++ mullvad-management-interface/src/types/mod.rs
@@ -1,10 +1,29 @@
 #[expect(clippy::allow_attributes)]
 mod proto {
-    tonic::include_proto!("mullvad_daemon.management_interface");
+    // The management_interface and relay_selector modules need to be siblings
+    // to reflect the file structure of the protos.
+    pub mod management_interface {
+        tonic::include_proto!("mullvad_daemon.management_interface");
+    }
+    pub use management_interface::*;
+
+    pub mod relay_selector {
+        tonic::include_proto!("mullvad_daemon.relay_selector");
+    }
 }
+pub use proto::*;
+
 mod conversions;
 
 pub use prost_types::{Duration, Timestamp};
 
 pub use conversions::*;
-pub use proto::*;
+
+// Re-export the relay selector service types
+pub use proto::relay_selector::relay_selector_service_client;
+pub use proto::relay_selector::relay_selector_service_server;
+
+// Re-export the client and server types for convenience
+pub type RelaySelectorServiceClient =
+    relay_selector_service_client::RelaySelectorServiceClient<crate::Channel>;
+pub use relay_selector_service_server::{RelaySelectorService, RelaySelectorServiceServer};
