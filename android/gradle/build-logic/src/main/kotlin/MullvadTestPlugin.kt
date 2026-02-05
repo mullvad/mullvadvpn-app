f58299a75a9c508f978007a14b3ca28451b8dc1e
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-02-06 00:47:55 +0100

    Fix detekt issue

--- android/gradle/build-logic/src/main/kotlin/MullvadTestPlugin.kt
+++ android/gradle/build-logic/src/main/kotlin/MullvadTestPlugin.kt
@@ -1,12 +1,13 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.UnknownTaskException
 
 class MullvadTestPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.tasks.register("testAllUnitTests") {
             dependsOn(try {
                 target.tasks.getByName("testDebugUnitTest")
-            } catch (e: Exception) {
+            } catch (e: UnknownTaskException) {
                 // Modules with flavors will not have normal test task so we test with ossProdDebug
                 target.tasks.getByName("testOssProdDebugUnitTest")
             })
