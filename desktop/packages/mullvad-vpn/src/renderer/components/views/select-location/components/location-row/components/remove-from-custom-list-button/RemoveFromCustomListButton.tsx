fcecf0fb9cd638067d6ad50a225e687d673f571b
Oliver <oliver@mohlin.dev>
2026-02-05 12:29:14 +0100

    Move functions updating custom lists to feature hook

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-row/components/remove-from-custom-list-button/RemoveFromCustomListButton.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-row/components/remove-from-custom-list-button/RemoveFromCustomListButton.tsx
@@ -1,43 +1,31 @@
 import React from 'react';
 
-import { compareRelayLocationGeographical } from '../../../../../../../../shared/daemon-rpc-types';
-import log from '../../../../../../../../shared/logging';
 import { useCustomLists } from '../../../../../../../features/location/hooks';
 import { LocationListItem } from '../../../../../../location-list-item';
 import type { LocationListItemIconButtonProps } from '../../../../../../location-list-item/components';
 import { useLocationRowContext } from '../../LocationRowContext';
 
-export type RemoveFromCustomListButtonProps = LocationListItemIconButtonProps;
+export type RemoveFromCustomListButtonProps = LocationListItemIconButtonProps & {};
 
 export function RemoveFromCustomListButton(props: RemoveFromCustomListButtonProps) {
-  const { source } = useLocationRowContext();
-  const { customLists, updateCustomList } = useCustomLists();
-  const onRemoveFromList = React.useCallback(async () => {
-    if (source.location.customList) {
-      // Find the list and remove the location from it.
-      const list = customLists.find((list) => list.id === source.location.customList);
-      if (list !== undefined) {
-        const updatedList = {
-          ...list,
-          locations: list.locations.filter((location) => {
-            return !compareRelayLocationGeographical(location, source.location);
-          }),
-        };
+  const {
+    source: { location },
+  } = useLocationRowContext();
+  const { removeLocationFromCustomList } = useCustomLists();
+  const [loading, setLoading] = React.useState(false);
 
-        try {
-          await updateCustomList(updatedList);
-        } catch (e) {
-          const error = e as Error;
-          log.error(`Failed to edit custom list ${source.location.customList}: ${error.message}`);
-        }
-      }
+  const onRemoveFromList = React.useCallback(async () => {
+    setLoading(true);
+    if ('customList' in location && 'country' in location && location.customList) {
+      await removeLocationFromCustomList(location.customList, location);
     }
-  }, [customLists, source.location, updateCustomList]);
+    setLoading(false);
+  }, [location, removeLocationFromCustomList]);
 
   return (
     <>
       <LocationListItem.HeaderTrailingAction>
-        <LocationListItem.IconButton onClick={onRemoveFromList} {...props}>
+        <LocationListItem.IconButton onClick={onRemoveFromList} disabled={loading} {...props}>
           <LocationListItem.IconButton.Icon icon="remove-circle" />
         </LocationListItem.IconButton>
       </LocationListItem.HeaderTrailingAction>
