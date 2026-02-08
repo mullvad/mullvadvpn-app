c1492a3d6721ab56a1556217eb40c97c4c786cd4
Oliver <oliver@mohlin.dev>
2026-02-08 19:22:26 +0100

    Add aria labels to buttons in select location view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-row/components/delete-custom-list-button/DeleteCustomListButton.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-row/components/delete-custom-list-button/DeleteCustomListButton.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { sprintf } from 'sprintf-js';
 
+import { messages } from '../../../../../../../../shared/gettext';
 import { LocationListItem } from '../../../../../../location-list-item';
 import type { LocationListItemIconButtonProps } from '../../../../../../location-list-item/components';
 import type { CustomListLocation } from '../../../../select-location-types';
@@ -23,7 +25,17 @@ export function DeleteCustomListButton({ customList, ...props }: DeleteCustomLis
   return (
     <>
       <LocationListItem.HeaderTrailingAction>
-        <LocationListItem.IconButton onClick={show} {...props}>
+        <LocationListItem.IconButton
+          onClick={show}
+          aria-label={sprintf(
+            // TRANSLATORS: Accessibility label for button to delete a custom list.
+            // TRANSLATORS: The placeholder is replaced with the name of the custom list.
+            messages.pgettext('accessibility', 'Delete custom list %(listName)s'),
+            {
+              listName: customList.label,
+            },
+          )}
+          {...props}>
           <LocationListItem.IconButton.Icon icon="cross-circle" />
         </LocationListItem.IconButton>
       </LocationListItem.HeaderTrailingAction>
