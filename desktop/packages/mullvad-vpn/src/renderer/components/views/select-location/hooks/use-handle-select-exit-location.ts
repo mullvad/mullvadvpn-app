a37f58a1c1d14626ab0f99dc09a9bcc6a43a259b
Oliver <oliver@mohlin.dev>
2026-02-08 18:19:51 +0100

    Use select location and custom lists feature hooks

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/hooks/use-handle-select-exit-location.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { type RelayLocation } from '../../../../../shared/daemon-rpc-types';
+import { useAppContext } from '../../../../context';
+import { useSelectLocation } from '../../../../features/location/hooks';
+import { useHistory } from '../../../../lib/history';
+
+export function useHandleSelectExitLocation() {
+  const { selectExitLocation } = useSelectLocation();
+  const history = useHistory();
+  const { connectTunnel } = useAppContext();
+
+  const handleSelectExitLocation = React.useCallback(
+    async (relayLocation: RelayLocation) => {
+      history.pop();
+      await selectExitLocation(relayLocation);
+      await connectTunnel();
+    },
+    [connectTunnel, history, selectExitLocation],
+  );
+
+  return handleSelectExitLocation;
+}
