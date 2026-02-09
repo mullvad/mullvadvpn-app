362f98499da6875aba8e24c9ff204142f9669ca3
Andrew Bulhak <andrew.bulhak@mullvad.net>
2026-02-09 17:53:44 +0100

    Replace SwiftAddressCacheWrapper with AddressCache from Runtime

--- ios/MullvadVPN/AppDelegate.swift
+++ ios/MullvadVPN/AppDelegate.swift
@@ -36,7 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     private(set) var accountsProxy: RESTAccountHandling!
     nonisolated(unsafe) private(set) var devicesProxy: DeviceHandling!
 
-    private(set) var addressCacheTracker: AddressCacheUpdateScheduler!
+    private(set) var addressCacheUpdateScheduler: AddressCacheUpdateScheduler!
     nonisolated(unsafe) private(set) var relayCacheTracker: RelayCacheTracker!
     nonisolated(unsafe) private(set) var storePaymentManager: StorePaymentManager!
     nonisolated(unsafe) private var apiTransportMonitor: APITransportMonitor!
@@ -73,9 +73,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let containerURL = ApplicationConfiguration.containerURL
         migrationManager = MigrationManager(cacheDirectory: containerURL)
         configureLogging()
-
-        addressCache = REST.AddressCache(canWriteToCache: true, cacheDirectory: containerURL)
-        addressCache.loadFromFile()
 
         let ipOverrideWrapper = IPOverrideWrapper(
             relayCache: RelayCache(cacheDirectory: containerURL),
@@ -136,7 +133,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             apiProxy: apiProxy
         )
 
-        addressCacheTracker = AddressCacheUpdateScheduler(
+        addressCacheUpdateScheduler = AddressCacheUpdateScheduler(
             backgroundTaskProvider: backgroundTaskProvider,
             apiProxy: apiProxy,
             apiContext: apiContext
@@ -275,13 +272,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     @objc private func didBecomeActive(_ notification: Notification) {
         tunnelManager.startPeriodicPrivateKeyRotation()
         relayCacheTracker.startPeriodicUpdates()
-        addressCacheTracker.startPeriodicUpdates()
+        addressCacheUpdateScheduler.startPeriodicUpdates()
     }
 
     @objc private func willResignActive(_ notification: Notification) {
         tunnelManager.stopPeriodicPrivateKeyRotation()
         relayCacheTracker.stopPeriodicUpdates()
-        addressCacheTracker.stopPeriodicUpdates()
+        addressCacheUpdateScheduler.stopPeriodicUpdates()
     }
 
     @objc private func didEnterBackground(_ notification: Notification) {
@@ -346,7 +343,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             forTaskWithIdentifier: BackgroundTask.addressCacheUpdate.identifier,
             using: .main
         ) { [self] task in
-            addressCacheTracker.updateEndpoints { [self] result in
+            addressCacheUpdateScheduler.updateEndpoints { [self] result in
                 scheduleAddressCacheUpdateTask()
                 task.setTaskCompleted(success: result.isSuccess)
             }
@@ -400,7 +397,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     private func scheduleAddressCacheUpdateTask() {
         do {
-            let date = addressCacheTracker.nextScheduleDate()
+            let date = addressCacheUpdateScheduler.nextScheduleDate()
 
             let request = BGProcessingTaskRequest(identifier: BackgroundTask.addressCacheUpdate.identifier)
             request.requiresNetworkConnectivity = true
