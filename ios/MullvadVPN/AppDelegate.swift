27aa1055888d4498806a768b3e25edca7793ced6
Andrew Bulhak <andrew.bulhak@mullvad.net>
2026-02-09 18:15:46 +0100

    Remove AddressCache from almost everywhere

--- ios/MullvadVPN/AppDelegate.swift
+++ ios/MullvadVPN/AppDelegate.swift
@@ -74,6 +74,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         migrationManager = MigrationManager(cacheDirectory: containerURL)
         configureLogging()
 
+        // This is still used in one place: SSLPinningURLSessionDelegate.urlSession(_:didReceive:completionHandler:)
+        addressCache = REST.AddressCache(canWriteToCache: true, cacheDirectory: containerURL)
+        addressCache.loadFromFile()
+
         let ipOverrideWrapper = IPOverrideWrapper(
             relayCache: RelayCache(cacheDirectory: containerURL),
             ipOverrideRepository: ipOverrideRepository
@@ -111,7 +115,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             domain: REST.encryptedDNSHostname,
             shadowsocksProvider: shadowsocksLoader,
             accessMethodWrapper: opaqueAccessMethodSettingsWrapper,
-            addressCacheProvider: addressCache,
             accessMethodChangeListeners: [accessMethodRepository, shadowsocksCacheCleaner]
         )
 
