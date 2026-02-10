0f4ba8d73706cb5c3706a4b91dafe2b3101d2b2c
Emīls <emils@mullvad.net>
2026-02-10 15:30:27 +0100

    Allow for using non-tls connections on the server

--- mullvad-api/src/bin/domain_fronting_server.rs
+++ mullvad-api/src/bin/domain_fronting_server.rs
@@ -4,7 +4,7 @@ use hyper::{server::conn::http1, service::service_fn};
 use hyper_util::rt::TokioIo;
 use mullvad_api::domain_fronting::server::Sessions;
 use rustls_pki_types::{CertificateDer, pem::PemObject};
-use std::{fs::File, io::BufReader, net::SocketAddr, path::PathBuf, sync::Arc};
+use std::{fs::File, io::BufReader, net::SocketAddr, path::{Path, PathBuf}, sync::Arc};
 use tokio::net::TcpListener;
 use tokio_rustls::{TlsAcceptor, rustls::ServerConfig};
 use tracing_subscriber::{EnvFilter, filter::LevelFilter};
@@ -16,13 +16,13 @@ struct Args {
     #[clap(short = 'H', long)]
     hostname: String,
 
-    /// Path to certificate file (PEM format)
+    /// Path to certificate file (PEM format). If omitted, plain TCP is used.
     #[clap(short = 'c', long)]
-    cert_path: PathBuf,
+    cert_path: Option<PathBuf>,
 
-    /// Path to private key file (PEM format)
+    /// Path to private key file (PEM format). Required if cert_path is set.
     #[clap(short = 'k', long)]
-    key_path: PathBuf,
+    key_path: Option<PathBuf>,
 
     /// Upstream socket address to forward CONNECT requests to
     #[clap(short = 'u', long)]
@@ -38,8 +38,8 @@ struct Args {
 }
 
 fn load_tls_config(
-    cert_path: &PathBuf,
-    key_path: &PathBuf,
+    cert_path: &std::path::Path,
+    key_path: &std::path::Path,
 ) -> Result<ServerConfig, Box<dyn std::error::Error>> {
     // Load certificate chain
     let cert_file = File::open(cert_path)?;
@@ -67,47 +67,68 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let bind_addr: SocketAddr = format!("0.0.0.0:{}", args.port).parse()?;
 
-    log::info!("Starting TLS domain fronting server on {}", bind_addr);
-    log::info!("Hostname: {}", args.hostname);
-    log::info!("Cert path: {}", args.cert_path.display());
-    log::info!("Key path: {}", args.key_path.display());
-    log::info!("Upstream: {}", args.upstream);
+    let tls_acceptor = match (&args.cert_path, &args.key_path) {
+        (Some(cert_path), Some(key_path)) => {
+            log::info!("Starting TLS domain fronting server on {}", bind_addr);
+            log::info!("Cert path: {}", cert_path.display());
+            log::info!("Key path: {}", key_path.display());
+            let tls_config = load_tls_config(cert_path, key_path)?;
+            Some(TlsAcceptor::from(Arc::new(tls_config)))
+        }
+        (None, None) => {
+            log::info!("Starting plain TCP domain fronting server on {}", bind_addr);
+            log::warn!("No TLS certificate provided - running without encryption");
+            None
+        }
+        _ => {
+            return Err("Both --cert-path and --key-path must be provided together".into());
+        }
+    };
 
-    // Load TLS configuration
-    let tls_config = load_tls_config(&args.cert_path, &args.key_path)?;
-    let tls_acceptor = TlsAcceptor::from(Arc::new(tls_config));
+    log::info!("Hostname: {}", args.hostname);
+    log::info!("Upstream: {}", args.upstream);
 
     let listener = TcpListener::bind(bind_addr).await?;
 
     let sessions = Sessions::new(args.upstream, args.session_header);
     loop {
         let (stream, addr) = listener.accept().await?;
-        let acceptor = tls_acceptor.clone();
 
         log::debug!("Accepted connection from {addr}");
 
         let sessions = sessions.clone();
+        let tls_acceptor = tls_acceptor.clone();
         tokio::spawn(async move {
-            // Perform TLS handshake
-            match acceptor.accept(stream).await {
-                Ok(tls_stream) => {
-                    let io = TokioIo::new(tls_stream);
-                    let service = service_fn(move |req| {
-                        sessions.clone().handle_request(req).map(Ok::<_, String>)
-                    });
-
-                    if let Err(err) = http1::Builder::new()
-                        .serve_connection(io, service)
-                        .with_upgrades()
-                        .await
-                    {
-                        log::error!("Error serving connection from {addr}: {err}");
+            match tls_acceptor {
+                Some(acceptor) => match acceptor.accept(stream).await {
+                    Ok(tls_stream) => {
+                        serve_connection(TokioIo::new(tls_stream), sessions, addr).await;
                     }
-                }
-                Err(err) => {
-                    log::error!("TLS handshake failed for {addr}: {err}");
+                    Err(err) => {
+                        log::error!("TLS handshake failed for {addr}: {err}");
+                    }
+                },
+                None => {
+                    serve_connection(TokioIo::new(stream), sessions, addr).await;
                 }
             }
         });
+    }
+}
+
+async fn serve_connection<S>(io: S, sessions: Sessions, addr: SocketAddr)
+where
+    S: hyper::rt::Read + hyper::rt::Write + Unpin + 'static,
+{
+    let service = service_fn(move |req| {
+        sessions.clone().handle_request(req).map(Ok::<_, String>)
+    });
+
+    if let Err(err) = http1::Builder::new()
+        .serve_connection(io, service)
+        .with_upgrades()
+        .await
+    {
+        log::error!("Error serving connection from {addr}: {err}");
     }
 }
