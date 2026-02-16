a435b0c2eda8a364c6eadfe9bdbb409d8bee9d5b
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-02-16 15:50:02 +0100

    WIP

--- desktop/packages/management-interface/scripts/container-run-generate-bindings.sh
+++ desktop/packages/management-interface/scripts/container-run-generate-bindings.sh
@@ -11,16 +11,13 @@ CONTAINER_RUNNER=${CONTAINER_RUNNER:-"podman"}
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROTO_DIR="$( cd "$SCRIPT_DIR/../../../../mullvad-management-interface/proto" && pwd )"
-IMAGE_NAME="grpc-js-ts-bindings"
+IMAGE_NAME=$(cat "$SCRIPT_DIR/../linux-container-image.txt")
 OUT_DIR="$SCRIPT_DIR/../dist"
-MOUNT_TARGET_BASE="/build"
-
-cd "$SCRIPT_DIR"
 
 mkdir -p "$OUT_DIR"
 
 # TODO: Verify that container exists or exit with error message
 exec "$CONTAINER_RUNNER" run --rm -it \
-    -v "$PROTO_DIR:$MOUNT_TARGET_BASE/proto:Z" \
-    -v "$OUT_DIR:$MOUNT_TARGET_BASE/dist:Z" \
-    "$IMAGE_NAME" bash -c ". ~/.bashrc && /build/generate-bindings.sh"
+    -v "$PROTO_DIR:/proto:Z" \
+    -v "$OUT_DIR:/proto-bindings:Z" \
+    "$IMAGE_NAME" bash
--- desktop/packages/management-interface/scripts/container/generate-bindings.sh
+++ desktop/packages/management-interface/scripts/container/generate-bindings.sh
@@ -2,7 +2,7 @@
 
 # This script runs within the management interface container
 # and is used to generate JS bindings for each proto files in the
-# /build/proto folder and then output them to the /build/dist folder
+# /proto folder and then output them to the /proto-bindings folder
 #
 # This script is invoked by the scripts/container-run-generate-bindings.sh
 # script. Please refer to the documentation for more information on how to use
@@ -10,28 +10,24 @@
 
 set -eu
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-NODE_MODULES_BIN_DIR="$( cd "$SCRIPT_DIR/node_modules/.bin" && pwd )"
-GRPC_TOOLS_NODE_PROTOC="$NODE_MODULES_BIN_DIR/grpc_tools_node_protoc"
-TS_PROTOC_PLUGIN="$NODE_MODULES_BIN_DIR/protoc-gen-ts"
+GRPC_TOOLS_NODE_PROTOC="/grpc-tools-binaries/grpc_tools_node_protoc"
+TS_PROTOC_PLUGIN="/grpc-tools-binaries/protoc-gen-ts"
 
-PROTO_DIR="/build/proto"
-OUT_DIR="/build/dist"
+IN_DIR="/proto"
+OUT_DIR="/proto-bindings"
 
-cd "$SCRIPT_DIR"
-
-for PROTO_FILE in "$PROTO_DIR"/*.proto; do
+for PROTO_FILE in "$IN_DIR"/*.proto; do
     PROTO_FILENAME="$(basename "$PROTO_FILE")"
 
     "$GRPC_TOOLS_NODE_PROTOC" \
         --js_out=import_style=commonjs,binary:$OUT_DIR \
         --grpc_out=grpc_js:$OUT_DIR \
-        --proto_path=$PROTO_DIR \
-        "$PROTO_DIR/$PROTO_FILENAME"
+        --proto_path=$IN_DIR \
+        "$IN_DIR/$PROTO_FILENAME"
 
     "$GRPC_TOOLS_NODE_PROTOC" \
         --plugin=protoc-gen-ts="$TS_PROTOC_PLUGIN" \
         --ts_out=grpc_js:$OUT_DIR \
-        --proto_path=$PROTO_DIR \
-        "$PROTO_DIR/$PROTO_FILENAME"
+        --proto_path=$IN_DIR \
+        "$IN_DIR/$PROTO_FILENAME"
 done
