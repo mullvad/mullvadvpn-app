2e01176531687df059e825225c63c538b7207211
Oliver <oliver@mohlin.dev>
2026-02-18 08:08:12 +0100

    Replace styled components with lib components in select location view

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/SelectLocationStyles.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/SelectLocationStyles.tsx
@@ -3,26 +3,8 @@ import styled from 'styled-components';
 import * as Cell from '../../cell';
 import { ScopeBar } from './components';
 
-export const StyledContent = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  flex: 1,
-  overflow: 'visible',
-});
-
 export const StyledScopeBar = styled(ScopeBar)({
   marginBottom: '16px',
-});
-
-export const StyledNavigationBarAttachment = styled.div({
-  padding: '0 16px 16px',
-});
-
-export const StyledSelectionUnavailable = styled(Cell.CellFooter)({
-  display: 'flex',
-  flexDirection: 'column',
-  paddingTop: 0,
-  marginTop: 0,
 });
 
 export const StyledSelectionUnavailableText = styled(Cell.CellFooterText)({
