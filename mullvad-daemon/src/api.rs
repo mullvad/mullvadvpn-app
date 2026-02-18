1852ea83cb03b188834f9258bbe4176d72914090
Andrew Bulhak <andrew.bulhak@mullvad.net>
2026-02-18 13:34:27 +0100

    Fix AddressCache breakage in daemon

--- mullvad-daemon/src/api.rs
+++ mullvad-daemon/src/api.rs
@@ -6,6 +6,7 @@ use crate::DaemonCommand;
 use crate::DaemonEventSender;
 use futures::{StreamExt, channel::mpsc};
 use mullvad_api::AddressCache;
+use mullvad_api::FileAddressCacheBacking;
 use mullvad_api::{
     access_mode::AccessMethodResolver,
     availability::ApiAvailability,
@@ -25,14 +26,14 @@ use talpid_types::net::{AllowedClients, Connectivity, proxy::CustomProxy};
 pub struct DaemonAccessMethodResolver {
     relay_selector: RelaySelector,
     encrypted_dns_proxy_cache: EncryptedDnsProxyState,
-    address_cache: AddressCache,
+    address_cache: AddressCache<FileAddressCacheBacking>,
 }
 
 impl DaemonAccessMethodResolver {
     pub fn new(
         relay_selector: RelaySelector,
         encrypted_dns_proxy_cache: EncryptedDnsProxyState,
-        address_cache: AddressCache,
+        address_cache: AddressCache<FileAddressCacheBacking>,
     ) -> Self {
         Self {
             relay_selector,
