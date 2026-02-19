0682b500e6908cd23444822e85bbbaf439db13bc
Oliver <oliver@mohlin.dev>
2026-02-19 07:59:30 +0100

    Add use debounce hook

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/hooks/use-debounce.ts
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = React.useState<T>(value);
+  const latestValueRef = React.useRef<T>(value);
+
+  React.useEffect(() => {
+    latestValueRef.current = value;
+  }, [value]);
+
+  React.useEffect(() => {
+    if (delay <= 0) {
+      setDebouncedValue(latestValueRef.current);
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(latestValueRef.current);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
