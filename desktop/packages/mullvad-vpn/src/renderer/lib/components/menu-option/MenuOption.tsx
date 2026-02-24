dfb140f616dfb327601d2580337fd34cdd431be3
Oliver <oliver@mohlin.dev>
2026-02-24 12:33:10 +0100

    Add MenuOption component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/menu-option/MenuOption.tsx
@@ -0,0 +1,17 @@
+import { MenuOptionItem, MenuOptionTrigger } from './components';
+import { MenuOptionProvider } from './MenuOptionContext';
+
+export type MenuOptionProps = React.PropsWithChildren<{
+  disabled?: boolean;
+}>;
+
+function MenuOption({ disabled, children }: MenuOptionProps) {
+  return <MenuOptionProvider disabled={disabled}>{children}</MenuOptionProvider>;
+}
+
+const MenuOptionNamespace = Object.assign(MenuOption, {
+  Item: MenuOptionItem,
+  Trigger: MenuOptionTrigger,
+});
+
+export { MenuOptionNamespace as MenuOption };
