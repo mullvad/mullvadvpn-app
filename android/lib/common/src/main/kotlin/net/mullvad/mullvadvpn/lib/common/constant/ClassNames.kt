f1016ca488720c032b9aa7ca3dbb6b8880146f81
David Göransson <david.goransson@mullvad.net>
2026-02-24 10:24:37 +0100

    Fix broken class names

--- android/lib/common/src/main/kotlin/net/mullvad/mullvadvpn/lib/common/constant/ClassNames.kt
+++ android/lib/common/src/main/kotlin/net/mullvad/mullvadvpn/lib/common/constant/ClassNames.kt
@@ -5,5 +5,5 @@ package net.mullvad.mullvadvpn.lib.common.constant
 internal const val MULLVAD_PACKAGE_NAME = "net.mullvad.mullvadvpn"
 
 // Classes
-const val MAIN_ACTIVITY_CLASS = "$MULLVAD_PACKAGE_NAME.ui.MainActivity"
-const val VPN_SERVICE_CLASS = "$MULLVAD_PACKAGE_NAME.service.MullvadVpnService"
+const val MAIN_ACTIVITY_CLASS = "$MULLVAD_PACKAGE_NAME.app.MainActivity"
+const val VPN_SERVICE_CLASS = "$MULLVAD_PACKAGE_NAME.app.service.MullvadVpnService"
