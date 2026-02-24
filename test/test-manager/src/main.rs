76654d62e17a9af7d882c4abe3e5b69f7c1248d8
Joakim Hulthe <joakim@hulthe.net>
2026-02-24 09:51:47 +0100

    Replace wireguard-go with GotaTun in test-manager
    Unfortunately, creating a TUN device requires the test-manager to run as
    root, since macOS has nothing like setcap in Linux. I implemented this
    by the `test-manager` sudoing itself if it's not invoked as root. We did
    already require passwordless sudo on the test server for the same reason
    (creating the tun device using wireguard-go), so this is fine.

--- test/test-manager/src/main.rs
+++ test/test-manager/src/main.rs
@@ -279,6 +279,9 @@ async fn inner_main() -> Result<()> {
             #[cfg(target_os = "linux")]
             container::relaunch_with_rootlesskit(vnc).await;
 
+            #[cfg(target_os = "macos")]
+            container::relaunch_with_sudo().await;
+
             let mut config = config.clone();
             config.runtime_opts.keep_changes = keep_changes;
             config.runtime_opts.display = if vnc.is_some() {
@@ -311,6 +314,9 @@ async fn inner_main() -> Result<()> {
         } => {
             #[cfg(target_os = "linux")]
             container::relaunch_with_rootlesskit(vnc).await;
+
+            #[cfg(target_os = "macos")]
+            container::relaunch_with_sudo().await;
 
             let mut config = config.clone();
             config.runtime_opts.display = match (display, vnc.is_some()) {
