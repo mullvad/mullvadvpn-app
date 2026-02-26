3b3245d7bfcd699e4277cd834a0700c16e24caae
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-02-26 16:14:26 +0100

    Fix broken docstrings in mullvad-types

--- mullvad-types/src/settings/mod.rs
+++ mullvad-types/src/settings/mod.rs
@@ -209,7 +209,7 @@ impl SplitApp {
         self.0.as_os_str().to_str().map(str::to_string)
     }
 
-    /// This is the String-representation as expected by [`SetExcludedApps`].
+    /// This is the String-representation as expected by `TunnelCommand::SetExcludedApps`
     pub fn to_tunnel_command_repr(self) -> std::ffi::OsString {
         self.0.as_os_str().to_owned()
     }
