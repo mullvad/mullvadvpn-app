f81154a6f2de043342d6278ba80d44463fa6ae84
Kalle Lindström <karl.lindstrom@mullvad.net>
2026-02-26 16:05:33 +0100

    Fix wrong transition to Anti-censorship screen

--- android/lib/ui/util/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/util/Modifier.kt
+++ android/lib/ui/util/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/util/Modifier.kt
@@ -1,11 +1,13 @@
 package net.mullvad.mullvadvpn.lib.ui.util
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@Composable
 fun <T> Modifier.applyIfNotNull(
     value: T?,
     and: Boolean = true,
-    block: Modifier.(T) -> Modifier,
+    block: @Composable Modifier.(T) -> Modifier,
 ): Modifier =
     if (value != null && and) {
         this.then(Modifier.block(value))
@@ -13,7 +15,8 @@ fun <T> Modifier.applyIfNotNull(
         this
     }
 
-fun Modifier.applyIf(condition: Boolean, block: Modifier.() -> Modifier): Modifier =
+@Composable
+fun Modifier.applyIf(condition: Boolean, block: @Composable Modifier.() -> Modifier): Modifier =
     if (condition) {
         this.then(Modifier.block())
     } else {
