6729e188fddb4a74c6bf5d0cf3bb5673c9e25f6a
Oliver <oliver@mohlin.dev>
2026-02-26 15:46:31 +0100

    Move scroll logic for location accordion into hook

--- desktop/packages/mullvad-vpn/src/renderer/components/location-list-item/components/location-list-item-accordion-content/LocationListItemAccordionContent.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/location-list-item/components/location-list-item-accordion-content/LocationListItemAccordionContent.tsx
@@ -1,60 +1,11 @@
-import React from 'react';
-
 import { Accordion } from '../../../../lib/components/accordion';
-import { useAccordionContext } from '../../../../lib/components/accordion/AccordionContext';
 import type { AccordionContentProps } from '../../../../lib/components/accordion/components';
-import { useScrollPositionContext } from '../../../views/select-location/ScrollPositionContext';
-import { useLocationListItemAccordionContext } from '../location-list-item-accordion/LocationListItemAccordionContext';
+import { useEffectScrollOnExpand } from './hooks';
 
 export type LocationListItemAccordionContentProps = AccordionContentProps;
 
 export const LocationListItemAccordionContent = (props: LocationListItemAccordionContentProps) => {
-  const { userTriggeredExpand, setUserTriggeredExpand } = useLocationListItemAccordionContext();
-  const { headerRef, content } = useAccordionContext();
-  const { spacePreAllocationViewRef, scrollIntoView, resetHeight, scrollViewRef } =
-    useScrollPositionContext();
+  useEffectScrollOnExpand();
 
-  React.useLayoutEffect(() => {
-    if (!content || !userTriggeredExpand) {
-      return;
-    }
-
-    const viewportBottom = scrollViewRef.current?.getScrollOffsetHeight() ?? window.innerHeight;
-    const { scrollHeight } = content;
-
-    const rect = content.getBoundingClientRect();
-
-    const predictedBottom = rect.top + scrollHeight;
-    const predictedRect = new DOMRect(rect.left, rect.top, rect.width, scrollHeight);
-
-    const contentIsLargerThanViewport = scrollHeight > viewportBottom;
-    if (contentIsLargerThanViewport) {
-      spacePreAllocationViewRef.current?.allocate(viewportBottom + predictedRect.height);
-      headerRef.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      resetHeight();
-      return;
-    }
-
-    const contentIsBelowViewport = predictedBottom > viewportBottom;
-    if (contentIsBelowViewport) {
-      const isBelow = predictedRect.bottom > viewportBottom;
-      if (isBelow) {
-        spacePreAllocationViewRef.current?.allocate(viewportBottom + predictedRect.height);
-        scrollIntoView(predictedRect);
-        resetHeight();
-      }
-    }
-
-    setUserTriggeredExpand(false);
-  }, [
-    content,
-    headerRef,
-    resetHeight,
-    scrollIntoView,
-    scrollViewRef,
-    setUserTriggeredExpand,
-    spacePreAllocationViewRef,
-    userTriggeredExpand,
-  ]);
   return <Accordion.Content {...props} />;
 };
