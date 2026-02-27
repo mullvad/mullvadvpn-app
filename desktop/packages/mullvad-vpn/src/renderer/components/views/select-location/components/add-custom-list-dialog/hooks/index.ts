7742d8d0dedc53300dafcf55b5610e67ae1accc6
Oliver <oliver@mohlin.dev>
2026-02-27 08:56:53 +0100

    Use shared hook for custom list name validation

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/add-custom-list-dialog/hooks/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/add-custom-list-dialog/hooks/index.ts
@@ -1,4 +1,3 @@
 export * from './use-handle-click-create-custom-list';
 export * from './use-handle-custom-list-name-change';
 export * from './use-handle-submit-add-custom-list';
-export * from './use-is-custom-list-name-valid';
