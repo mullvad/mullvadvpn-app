774f587e0ad5e3b90dd66f90c1a464609e709c61
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-02-27 13:32:15 +0100

    Add `libdbus` feature to talpid-dbus

--- talpid-dbus/src/systemd_resolved.rs
+++ talpid-dbus/src/systemd_resolved.rs
@@ -1,9 +1,9 @@
-#[cfg(not(feature = "zbus"))]
+#[cfg(all(feature = "libdbus", not(feature = "zbus")))]
 mod dbus_rs;
 #[cfg(feature = "zbus")]
 mod zbus;
 
-#[cfg(not(feature = "zbus"))]
+#[cfg(all(feature = "libdbus", not(feature = "zbus")))]
 pub use dbus_rs::*;
 #[cfg(feature = "zbus")]
 pub use zbus::*;
