b3f268200f307877f5c3fca11c2d157b147b134c
David Göransson <david.goransson@mullvad.net>
2026-03-02 16:36:46 +0100

    UiState proposal

--- android/lib/feature/location/impl/src/main/kotlin/net/mullvad/mullvadvpn/feature/location/impl/SelectLocationUiState.kt
+++ android/lib/feature/location/impl/src/main/kotlin/net/mullvad/mullvadvpn/feature/location/impl/SelectLocationUiState.kt
@@ -4,6 +4,7 @@ import net.mullvad.mullvadvpn.lib.model.ErrorStateCause
 import net.mullvad.mullvadvpn.lib.model.HopSelection
 import net.mullvad.mullvadvpn.lib.model.MultihopRelayListType
 import net.mullvad.mullvadvpn.lib.model.RelayListType
+import net.mullvad.mullvadvpn.lib.ui.component.relaylist.RelayListItem
 import net.mullvad.mullvadvpn.lib.usecase.FilterChip
 
 data class SelectLocationUiState(
@@ -19,3 +20,56 @@ data class SelectLocationUiState(
     val relayListType =
         if (multihopEnabled) RelayListType.Multihop(multihopListSelection) else RelayListType.Single
 }
+
+sealed interface SUiState {
+    val hopSelection: HopSelection
+    val tunnelErrorStateCause: ErrorStateCause?
+    val hopList: HopList
+    val filterChips: List<FilterChip>
+
+    val isSearchButtonEnabled: Boolean
+    val isFilterButtonEnabled: Boolean
+    val isRecentsEnabled: Boolean
+
+    fun multihopEnabled() = hopSelection is HopSelection.Multi
+}
+
+data class SSingleUiState(
+    override val hopSelection: HopSelection.Single,
+    override val filterChips: List<FilterChip>,
+    override val hopList: Singlehop,
+    override val tunnelErrorStateCause: ErrorStateCause?,
+    override val isSearchButtonEnabled: Boolean,
+    override val isFilterButtonEnabled: Boolean,
+    override val isRecentsEnabled: Boolean,
+) : SUiState
+
+data class SAutohopUiState(
+    override val hopSelection: HopSelection.Multi,
+    override val filterChips: List<FilterChip>,
+    override val hopList: Autohop,
+    override val tunnelErrorStateCause: ErrorStateCause?,
+    val multihopListSelection: MultihopRelayListType,
+    override val isSearchButtonEnabled: Boolean,
+    override val isFilterButtonEnabled: Boolean,
+    override val isRecentsEnabled: Boolean,
+) : SUiState
+
+data class SMultiUiState(
+    override val hopSelection: HopSelection.Multi,
+    override val filterChips: List<FilterChip>,
+    override val hopList: Multihop,
+    override val tunnelErrorStateCause: ErrorStateCause?,
+    val multihopListSelection: MultihopRelayListType,
+    override val isSearchButtonEnabled: Boolean,
+    override val isFilterButtonEnabled: Boolean,
+    override val isRecentsEnabled: Boolean,
+) : SUiState
+
+sealed interface HopList
+
+data class Singlehop(val exitList: List<RelayListItem>) : HopList
+
+data class Autohop(val exitList: List<RelayListItem>) : HopList
+
+data class Multihop(val entryList: List<RelayListItem>, val exitList: List<RelayListItem>) : HopList
