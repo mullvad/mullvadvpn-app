3af7d6d217f441106ccdf9bf595ba8f80faba748
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-03-02 18:09:04 +0100

    fixup! Replace grpc-node from npm with container which builds from source

--- desktop/packages/management-interface/scripts/container-run-generate-bindings.sh
+++ desktop/packages/management-interface/scripts/container-run-generate-bindings.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Run the management-interface container and generate JS bindings from proto files.
+# Run the container to generate node gRPC bindings from .proto files.
 #
 # Requires the container to have been built first, please refer to the documentation
 # for more info on how to use this script.
@@ -8,19 +8,14 @@
 set -eu
 
 CONTAINER_RUNNER=${CONTAINER_RUNNER:-"podman"}
+IMAGE_HASH="TODO_HASH_CHANGE_ME"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROTO_DIR="$( cd "$SCRIPT_DIR/../../../../mullvad-management-interface/proto" && pwd )"
-IMAGE_NAME="grpc-js-ts-bindings"
+IMAGE_NAME="ghcr.io/mullvad/mullvadvpn-app-build-node-grpc-bindings:$IMAGE_HASH"
 OUT_DIR="$SCRIPT_DIR/../dist"
-MOUNT_TARGET_BASE="/build"
 
-cd "$SCRIPT_DIR"
-
-mkdir -p "$OUT_DIR"
-
-# TODO: Verify that container exists or exit with error message
 exec "$CONTAINER_RUNNER" run --rm -it \
-    -v "$PROTO_DIR:$MOUNT_TARGET_BASE/proto:Z" \
-    -v "$OUT_DIR:$MOUNT_TARGET_BASE/dist:Z" \
-    "$IMAGE_NAME" bash -c ". ~/.bashrc && /build/generate-bindings.sh"
+    -v "$PROTO_DIR:/proto:Z" \
+    -v "$OUT_DIR:/proto-bindings:Z" \
+    "$IMAGE_NAME"
