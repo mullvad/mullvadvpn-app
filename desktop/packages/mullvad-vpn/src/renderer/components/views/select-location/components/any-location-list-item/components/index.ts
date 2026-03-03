b045c2a6dbe8be0e000e94312dd24398187ec97c
Oliver <oliver@mohlin.dev>
2026-03-03 10:11:20 +0100

    Rename remove location from custom list button

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/any-location-list-item/components/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/any-location-list-item/components/index.ts
@@ -3,4 +3,4 @@ export * from './custom-list-trailing-actions';
 export * from './delete-custom-list-button';
 export * from './edit-custom-list-button';
 export * from './geographical-location-trailing-actions';
-export * from './remove-from-custom-list-button';
+export * from './remove-location-from-custom-list-button';
