2768a1ccb45f191ee806307f1ad4699bd73c8250
Oliver <oliver@mohlin.dev>
2026-03-04 07:24:00 +0100

    Move location filter utils and hooks to feature folder

--- desktop/packages/mullvad-vpn/src/renderer/components/views/filter/hooks/use-filtered-providers.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/filter/hooks/use-filtered-providers.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 
 import { Ownership } from '../../../../../shared/daemon-rpc-types';
-import { filterLocations } from '../../../../lib/filter-locations';
+import { filterLocationsByOwnershipAndProviders } from '../../../../features/locations/utils';
 import { useSelector } from '../../../../redux/store';
 import { providersFromRelays } from '../utils';
 
@@ -10,7 +10,11 @@ export function useFilteredProviders(providers: string[], ownership: Ownership):
   const locations = useSelector((state) => state.settings.relayLocations);
 
   const availableProviders = useMemo(() => {
-    const relaylistForFilters = filterLocations(locations, ownership, providers);
+    const relaylistForFilters = filterLocationsByOwnershipAndProviders(
+      locations,
+      ownership,
+      providers,
+    );
     return providersFromRelays(relaylistForFilters);
   }, [locations, ownership, providers]);
 
