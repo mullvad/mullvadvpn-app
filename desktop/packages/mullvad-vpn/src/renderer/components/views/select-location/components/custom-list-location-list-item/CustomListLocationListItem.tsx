923a111068253a21ac8308d7056eb0610c587faa
Oliver <oliver@mohlin.dev>
2026-03-04 11:51:13 +0100

    Add and use utility function for creating list item key and level prop

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list-item/CustomListLocationListItem.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list-item/CustomListLocationListItem.tsx
@@ -1,5 +1,6 @@
 import { type CustomListLocation } from '../../../../../features/locations/types';
 import { AnimatedList } from '../../../../../lib/components/animated-list';
+import { getLocationListItemMapProps } from '../../utils';
 import { AnyLocationListItem, type AnyLocationListItemProps } from '../any-location-list-item';
 import { GeographicalLocationListItem } from '../geographical-location-list-item';
 import {
@@ -36,14 +37,19 @@ function CustomListLocationListItemImpl({
       {...props}>
       <AnimatedList>
         {customList.locations.map((child, idx) => {
+          const { key, nextLevel } = getLocationListItemMapProps(child, level);
+
+          // Since list item is wrapped with animated list item, we need to manually
+          // tell it what position it is in the list.
+          const position = idx !== customList.locations.length - 1 ? 'middle' : undefined;
           return (
-            <AnimatedList.Item key={Object.values(child.details).join('-')}>
+            <AnimatedList.Item key={key}>
               <GeographicalLocationListItem
                 location={child}
                 rootLocation="customList"
                 disabled={disabled || loading}
-                level={level !== undefined ? level + 1 : undefined}
-                position={idx !== customList.locations.length - 1 ? 'middle' : undefined}
+                level={nextLevel}
+                position={position}
                 onSelect={handleSelectCustomList}
                 {...props}
               />
