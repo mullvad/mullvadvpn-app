10995264fe6b479fbc1d2dab59b607ce7a5f0b6b
Oliver <oliver@mohlin.dev>
2026-03-06 12:27:43 +0100

    Add TODO comment about restructuring ListItemTrailingActionIcon

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trailing-action/components/list-item-trailing-action-icon/ListItemTrailingActionIcon.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trailing-action/components/list-item-trailing-action-icon/ListItemTrailingActionIcon.tsx
@@ -6,6 +6,8 @@ type ListItemIconProps = Omit<IconProps, 'size'>;
 
 export function ListItemTrailingActionIcon({ ...props }: ListItemIconProps) {
   const { disabled: listItemDisabled } = useListItemContext();
+  // TODO: Restructure this component to be a child of trigger to clarify
+  // that it uses that context.
   const { disabled: listItemTriggerDisabled } = useListItemTriggerContext();
 
   const disabled = listItemTriggerDisabled ?? listItemDisabled;
