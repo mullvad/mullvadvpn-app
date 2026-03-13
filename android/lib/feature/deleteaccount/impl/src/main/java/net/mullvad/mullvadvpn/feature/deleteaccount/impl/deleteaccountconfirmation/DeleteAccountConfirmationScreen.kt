f5cb6a2a54176f9c2f887a935139ee9b065982a7
David Göransson <david.goransson@mullvad.net>
2026-03-13 15:21:44 +0100

    Merge branch 'prevent-paste-in-account-deletion-droid-2548'

--- android/lib/feature/deleteaccount/impl/src/main/java/net/mullvad/mullvadvpn/feature/deleteaccount/impl/deleteaccountconfirmation/DeleteAccountConfirmationScreen.kt
+++ android/lib/feature/deleteaccount/impl/src/main/java/net/mullvad/mullvadvpn/feature/deleteaccount/impl/deleteaccountconfirmation/DeleteAccountConfirmationScreen.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.byValue
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Visibility
@@ -26,7 +28,6 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,23 +36,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.autofill.ContentType
-import androidx.compose.ui.platform.ClipEntry
-import androidx.compose.ui.platform.Clipboard
-import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.autofill.ContentDataType
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.platform.NativeClipboard
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.contentType
+import androidx.compose.ui.semantics.contentDataType
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.compose.dropUnlessResumed
 import com.ramcosta.composedestinations.annotation.Destination
@@ -228,8 +226,6 @@ private fun AccountNumberInput(
     }
 
     var showPassword by remember { mutableStateOf(false) }
-    val localClipboard = LocalClipboard.current
-    val clipboard = remember(localClipboard) { NoOpClipboardManager(localClipboard) }
 
     val transformation =
         remember(showPassword, showLastChar) {
@@ -237,41 +233,42 @@ private fun AccountNumberInput(
         }
 
     val keyboardController = LocalSoftwareKeyboardController.current
-    CompositionLocalProvider(LocalClipboard provides clipboard) {
-        TextField(
-            state = textFieldState,
-            modifier =
-                // Fix for DPad navigation
-                Modifier.semantics { contentType = ContentType.Password }.fillMaxWidth(),
-            trailingIcon = {
-                IconButton(onClick = { showPassword = !showPassword }) {
-                    Icon(
-                        imageVector =
-                            if (showPassword) Icons.Outlined.VisibilityOff
-                            else Icons.Outlined.Visibility,
-                        contentDescription =
-                            if (showPassword) stringResource(id = R.string.hide_account_number)
-                            else stringResource(id = R.string.show_account_number),
-                    )
-                }
+    TextField(
+        state = textFieldState,
+        modifier = Modifier.semantics { contentDataType = ContentDataType.None }.fillMaxWidth(),
+        trailingIcon = {
+            IconButton(onClick = { showPassword = !showPassword }) {
+                Icon(
+                    imageVector =
+                        if (showPassword) Icons.Outlined.VisibilityOff
+                        else Icons.Outlined.Visibility,
+                    contentDescription =
+                        if (showPassword) stringResource(id = R.string.hide_account_number)
+                        else stringResource(id = R.string.show_account_number),
+                )
+            }
+        },
+        placeholder = { Text(stringResource(R.string.account_delete_placeholder)) },
+        keyboardOptions =
+            KeyboardOptions(
+                imeAction = ImeAction.Done,
+                autoCorrectEnabled = false,
+                keyboardType = KeyboardType.accountNumberKeyboardType(LocalContext.current),
+            ),
+        onKeyboardAction = { keyboardController?.hide() },
+        inputTransformation =
+            InputTransformation.byValue { current, proposed ->
+                if (proposed.isDigitsOnly() && levenshtein(current, proposed) <= 1) proposed
+                else current
             },
-            placeholder = { Text(stringResource(R.string.account_delete_placeholder)) },
-            keyboardOptions =
-                KeyboardOptions(
-                    imeAction = ImeAction.Done,
-                    autoCorrectEnabled = false,
-                    keyboardType = KeyboardType.accountNumberKeyboardType(LocalContext.current),
-                ),
-            onKeyboardAction = { keyboardController?.hide() },
-            outputTransformation = transformation,
-            lineLimits = TextFieldLineLimits.SingleLine,
-            enabled = !state.isLoading,
-            colors = mullvadDarkTextFieldColors(),
-            textStyle = MaterialTheme.typography.bodyLarge.copy(textDirection = TextDirection.Ltr),
-            isError = state.verificationError != null,
-            supportingText = state.verificationError?.let { { Text(it.toErrorMessage()) } },
-        )
-    }
+        outputTransformation = transformation,
+        lineLimits = TextFieldLineLimits.SingleLine,
+        enabled = !state.isLoading,
+        colors = mullvadDarkTextFieldColors(),
+        textStyle = MaterialTheme.typography.bodyLarge.copy(textDirection = TextDirection.Ltr),
+        isError = state.verificationError != null,
+        supportingText = state.verificationError?.let { { Text(it.toErrorMessage()) } },
+    )
 }
 
 @Composable
@@ -341,19 +338,4 @@ private fun DeleteAccountConfirmationBottomBar(
             isEnabled = !isLoading,
         )
     }
-}
-
-// Hack to disable pasting
-class NoOpClipboardManager(private val clipboard: Clipboard) : Clipboard {
-
-    override suspend fun getClipEntry(): ClipEntry? {
-        return null
-    }
-
-    override suspend fun setClipEntry(clipEntry: ClipEntry?) {
-        // Do nothing
-    }
-
-    override val nativeClipboard: NativeClipboard
-        get() = clipboard.nativeClipboard
 }
---
+++ android/lib/feature/deleteaccount/impl/src/main/java/net/mullvad/mullvadvpn/feature/deleteaccount/impl/deleteaccountconfirmation/Levensthein.kt
@@ -0,0 +1,42 @@
+package net.mullvad.mullvadvpn.feature.deleteaccount.impl.deleteaccountconfirmation
+
+import java.lang.Integer.min
+
+@Suppress("ReturnCount")
+fun levenshtein(lhs: CharSequence, rhs: CharSequence): Int {
+    if (lhs == rhs) {
+        return 0
+    }
+    if (lhs.isEmpty()) {
+        return rhs.length
+    }
+    if (rhs.isEmpty()) {
+        return lhs.length
+    }
+
+    val lhsLength = lhs.length + 1
+    val rhsLength = rhs.length + 1
+
+    var cost = Array(lhsLength) { it }
+    var newCost = Array(lhsLength) { 0 }
+
+    for (i in 1..<rhsLength) {
+        newCost[0] = i
+
+        for (j in 1..<lhsLength) {
+            val match = if (lhs[j - 1] == rhs[i - 1]) 0 else 1
+
+            val costReplace = cost[j - 1] + match
+            val costInsert = cost[j] + 1
+            val costDelete = newCost[j - 1] + 1
+
+            newCost[j] = min(min(costInsert, costDelete), costReplace)
+        }
+
+        val swap = cost
+        cost = newCost
+        newCost = swap
+    }
+
+    return cost[lhsLength - 1]
+}
