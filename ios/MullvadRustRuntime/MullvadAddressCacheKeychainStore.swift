450015a855f5efc7faf25869364c02fbc449bd0e
Emīls <emils@mullvad.net>
2026-03-13 18:08:54 +0100

    Make address cache work in UI process

--- ios/MullvadRustRuntime/MullvadAddressCacheKeychainStore.swift
+++ ios/MullvadRustRuntime/MullvadAddressCacheKeychainStore.swift
@@ -8,9 +8,15 @@
 
 import MullvadSettings
 
+/// Whether the settings store is available. It requires `ApplicationSecurityGroupIdentifier`
+/// to be present in the main bundle's Info.plist, which is not the case in e.g. UI test runners.
+private let isSettingsStoreAvailable: Bool = Bundle.main
+    .object(forInfoDictionaryKey: "ApplicationSecurityGroupIdentifier") != nil
+
 /// Store the address cache, given to us by the Rust code,  to the keychain
 @_cdecl("swift_store_address_cache")
 func storeAddressCache(_ pointer: UnsafeRawPointer, dataSize: UInt64) {
+    guard isSettingsStoreAvailable else { return }
     let data = Data(bytes: pointer, count: Int(dataSize))
     // if writing to the Keychain fails, it will do so silently.
     try? SettingsManager.writeAddressCache(data)
@@ -18,6 +24,7 @@ func storeAddressCache(_ pointer: UnsafeRawPointer, dataSize: UInt64) {
 
 @_cdecl("swift_read_address_cache")
 func readAddressCache() -> SwiftData {
+    guard isSettingsStoreAvailable else { return SwiftData(data: NSData()) }
     let data = (try? SettingsManager.readAddressCache()) ?? Data()
     return SwiftData(data: data as NSData)
 }
