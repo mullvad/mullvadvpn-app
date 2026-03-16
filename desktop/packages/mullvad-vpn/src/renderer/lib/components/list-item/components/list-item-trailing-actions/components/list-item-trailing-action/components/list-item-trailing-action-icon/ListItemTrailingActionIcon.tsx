fad0692fb105f814049d37f6211c53d6da1ad707
Oliver <oliver@mohlin.dev>
2026-03-16 07:48:29 +0100

    Remove trailing action icon dependency on trigger

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trailing-actions/components/list-item-trailing-action/components/list-item-trailing-action-icon/ListItemTrailingActionIcon.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trailing-actions/components/list-item-trailing-action/components/list-item-trailing-action-icon/ListItemTrailingActionIcon.tsx
@@ -1,16 +1,10 @@
 import { Icon, type IconProps } from '../../../../../../../icon';
 import { useListItemContext } from '../../../../../../ListItemContext';
-import { useListItemTriggerContext } from '../../../../../list-item-trigger/ListItemTriggerContext';
 
 export type ListItemTrailingActionIconProps = Omit<IconProps, 'size'>;
 
 export function ListItemTrailingActionIcon(props: ListItemTrailingActionIconProps) {
-  const { disabled: listItemDisabled } = useListItemContext();
-  // TODO: Restructure this component to be a child of trigger to clarify
-  // that it uses that context.
-  const { disabled: listItemTriggerDisabled } = useListItemTriggerContext();
-
-  const disabled = listItemTriggerDisabled ?? listItemDisabled;
+  const { disabled } = useListItemContext();
 
   return <Icon aria-hidden="true" color={disabled ? 'whiteAlpha40' : 'white'} {...props} />;
 }
