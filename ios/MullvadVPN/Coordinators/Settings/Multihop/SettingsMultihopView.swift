094e039b5b25d4a177e786852a55f13701ca5de4
Andrew Bulhak <andrew.bulhak@mullvad.net>
2026-03-16 16:08:15 +0100

    Replace the multihop switch view with a tristate list

--- ios/MullvadVPN/Coordinators/Settings/Multihop/SettingsMultihopView.swift
+++ ios/MullvadVPN/Coordinators/Settings/Multihop/SettingsMultihopView.swift
@@ -11,21 +11,130 @@ import SwiftUI
 
 struct SettingsMultihopView<ViewModel>: View where ViewModel: TunnelSettingsObservable<MultihopState> {
     @StateObject var tunnelViewModel: ViewModel
+    @State private var alert: MullvadAlert?
+
+    private struct OptionSpec: Identifiable {
+        let id: MultihopState
+        let label: String
+        let accessibilityIdentifier: AccessibilityIdentifier
+        let helpText: [LocalizedStringKey]?
+    }
+
+    private let options: [OptionSpec] = [
+        .init(
+            id: .whenNeeded,
+            label: "When needed",
+            accessibilityIdentifier: .multihopWhenNeeded,
+            helpText: [
+                "If your selected location does not support your preferences multihop will be used automatically to connect to that location via a compatible server. This will be indicated by the \(Image("IconSmartLocation")) symbol",
+                "",
+                "Attention: This will ignore filter settings for the entry server that is being automatically selected.",
+            ]),
+        .init(
+            id: .always,
+            label: "Always",
+            accessibilityIdentifier: .multihopAlways,
+            helpText: [
+                "Always connect via an entry server. The location can either be set manually or automatically in the \"Select location\" view."
+            ]),
+        .init(
+            id: .never,
+            label: "Never",
+            accessibilityIdentifier: .multihopNever,
+            helpText: nil
+        ),
+    ]
 
     var body: some View {
         SettingsInfoContainerView {
             VStack(alignment: .leading, spacing: 8) {
                 SettingsInfoView(viewModel: dataViewModel)
 
-                SwitchRowView(
-                    isOn: $tunnelViewModel.value.isUserSelected,
-                    text: NSLocalizedString("Enable", comment: ""),
-                    accessibilityId: .multihopSwitch
-                )
-                .padding(.leading, UIMetrics.contentInsets.left)
-                .padding(.trailing, UIMetrics.contentInsets.right)
+                #if DEBUG
+                    VStack(spacing: 1) {
+                        HStack {
+                            Text("Mode")
+                            Spacer()
+                        }
+                        .padding(EdgeInsets(UIMetrics.SettingsCell.defaultLayoutMargins))
+                        .background(Color(UIColor.primaryColor))
+                        ForEach(options) { option in
+                            HStack(spacing: 1) {
+                                HStack {
+                                    Image(uiImage: UIImage.tick).opacity(tunnelViewModel.value == option.id ? 1.0 : 0.0)
+                                        .foregroundStyle(
+                                            (tunnelViewModel.value == option.id)
+                                                ? Color(UIColor.Cell.Background.selected)
+                                                : Color(UIColor.Cell.titleTextColor)
+                                        )
+                                    Spacer().frame(width: UIMetrics.SettingsCell.selectableSettingsCellLeftViewSpacing)
+                                    Text(option.label)
+                                        .foregroundStyle(
+                                            (tunnelViewModel.value == option.id)
+                                                ? Color(UIColor.Cell.Background.selected)
+                                                : Color(UIColor.Cell.titleTextColor)
+                                        )
+                                    Spacer()
+                                }
+                                .padding(EdgeInsets(UIMetrics.SettingsCell.defaultLayoutMargins))
+                                .background(
+                                    Color(UIColor.Cell.Background.indentationLevelZero)
+                                )
+                                if let helpText = option.helpText {
+                                    VStack {
+                                        Spacer()
+                                        Button(action: {
+                                            self.alert = MullvadAlert(
+                                                type: .info, messages: helpText,
+                                                actions: [
+                                                    .init(
+                                                        type: .default,
+                                                        title: "Got it!",
+                                                        identifier: .includeAllNetworksNotificationsAlertDismissButton,
+                                                        handler: {
+                                                            self.alert = nil
+                                                        }
+                                                    )
+                                                ])
+                                        }) {
+                                            Image(.iconInfo)
+                                        }
+                                        .tint(.white)
+                                        Spacer()
+                                    }
+                                    .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
+                                    .background(
+                                        Color(UIColor.Cell.Background.indentationLevelZero)
+                                    )
+                                }
+                            }
+                            .foregroundColor(Color(UIColor.Cell.titleTextColor))
+                            .onTapGesture {
+                                tunnelViewModel.value = option.id
+                            }
+                            .accessibilityIdentifier(option.accessibilityIdentifier.asString)
+                        }
+                    }
+                    .cornerRadius(16)
+                    .padding(.leading, UIMetrics.contentInsets.left)
+                    .padding(.trailing, UIMetrics.contentInsets.right)
+                    .listStyle(.plain)
+                    .listRowSpacing(UIMetrics.TableView.separatorHeight)
+                    .environment(\.defaultMinListRowHeight, 0)
+                    .background(Color(.secondaryColor))
+                    .foregroundColor(Color(.primaryTextColor))
+                #else
+                    SwitchRowView(
+                        isOn: $tunnelViewModel.value.isUserSelected,
+                        text: NSLocalizedString("Enable", comment: ""),
+                        accessibilityId: .multihopSwitch
+                    )
+                    .padding(.leading, UIMetrics.contentInsets.left)
+                    .padding(.trailing, UIMetrics.contentInsets.right)
+                #endif
             }
         }
+        .mullvadAlert(item: $alert)
     }
 }
 
