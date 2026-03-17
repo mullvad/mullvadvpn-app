cc292c77004fbae9fb4e8b7f46e59fac4fbebce2
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-03-17 21:36:38 +0100

    Remove AnyLocationListItem

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/index.ts
@@ -4,7 +4,6 @@ export * from './daita-filter-chip';
 export * from './filter-chips';
 export * from './disabled-entry-selection';
 export * from './country-location-list';
-export * from './any-location-list-item';
 export * from './location-list-item';
 export * from './location-search-field';
 export * from './lwo-filter-chip';
