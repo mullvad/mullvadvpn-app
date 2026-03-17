d85e32de110d039a8c58a4a34c34bbf860093fae
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-03-17 21:49:39 +0100

    Rewrite CustomListLocationList
    This component will now handle a single custom list and render
    its root elements and its locations.

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/CustomListLocationList.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/CustomListLocationList.tsx
@@ -1,87 +1,101 @@
-import styled from 'styled-components';
+import { useCallback, useState } from 'react';
+import { sprintf } from 'sprintf-js';
 
 import { messages } from '../../../../../../shared/gettext';
-import { Container, Text } from '../../../../../lib/components';
+import { CustomListLocation } from '../../../../../features/locations/types';
+import { FootnoteMiniSemiBold } from '../../../../../lib/components';
 import { AnimatedList } from '../../../../../lib/components/animated-list';
 import { FlexColumn } from '../../../../../lib/components/flex-column';
-import { spacings } from '../../../../../lib/foundations';
-import { useHasCustomLists } from '../../hooks';
-import { useSelectLocationViewContext } from '../../SelectLocationViewContext';
 import { getLocationListItemMapProps } from '../../utils';
 import { CustomListLocationListItem } from '../custom-list-location-list-item';
-import { CustomListsSectionTitle } from './components';
-import {
-  CustomListLocationListProvider,
-  useCustomListLocationListContext,
-} from './CustomListLocationListContext';
+import { LocationListItem } from '../location-list-item';
+import { useLocationListsContext } from '../location-lists/LocationListsContext';
+import { CustomListTrailingActions } from './components';
+import { CustomListLocationListProvider } from './CustomListLocationListContext';
 
-const StyledAnimatedList = styled(AnimatedList)`
-  display: flex;
-  flex-direction: column;
-`;
+export type CustomListLocationListProps = {
+  customList: CustomListLocation;
+  disabled?: boolean;
+};
 
-const StyledAnimatedListItem = styled(AnimatedList.Item)`
-  margin-bottom: ${spacings.tiny};
-`;
+export function CustomListLocationListImpl({
+  customList,
+  disabled: disabledProp,
+}: CustomListLocationListProps) {
+  const [expanded, setExpanded] = useState(customList.expanded);
+  const { handleSelect } = useLocationListsContext();
 
-function CustomListLocationListImpl() {
-  const { customListLocations } = useSelectLocationViewContext();
-  const { addingCustomList } = useCustomListLocationListContext();
+  const showEmptySubtitle = customList.locations.length === 0;
+  const disabled = customList.disabled || disabledProp;
+  const showChildren = customList.expanded || expanded;
 
-  const hasCustomLists = useHasCustomLists();
-  const showAddCustomListText = !hasCustomLists && !addingCustomList;
-  const showAddLocationToCustomListText = hasCustomLists;
+  const handleClick = useCallback(() => {
+    void handleSelect(customList);
+  }, [customList, handleSelect]);
+
+  const renderChildren = () => {
+    return customList.locations.map((locationChild, index) => {
+      const { key, nextLevel } = getLocationListItemMapProps(locationChild, 0);
+      const position = index !== customList.locations.length - 1 ? 'middle' : undefined;
+
+      return (
+        <AnimatedList.Item key={key}>
+          <CustomListLocationListItem
+            position={position}
+            location={locationChild}
+            level={nextLevel}
+            disabled={disabled}
+          />
+        </AnimatedList.Item>
+      );
+    });
+  };
 
   return (
-    <FlexColumn gap="tiny">
-      <CustomListsSectionTitle />
-      <FlexColumn>
-        <StyledAnimatedList>
-          {customListLocations.map((customList) => {
-            const { key } = getLocationListItemMapProps(customList);
-            return (
-              <StyledAnimatedListItem key={key}>
-                <CustomListLocationListItem customList={customList} level={0} />
-              </StyledAnimatedListItem>
-            );
-          })}
-        </StyledAnimatedList>
-
-        {showAddCustomListText && (
-          <Text variant="labelTiny" color="whiteAlpha60">
-            {
-              // TRANSLATORS: Message shown when the user has no custom lists.
-              // TRANSLATORS: Instructs the user how to create a custom list.
-              messages.pgettext(
-                'select-location-view',
-                'Add a custom list by clicking the “+” icon',
-              )
-            }
-          </Text>
-        )}
-        {showAddLocationToCustomListText && (
-          <Container horizontalMargin="medium">
-            <Text variant="labelTiny" color="whiteAlpha60">
+    <LocationListItem root selected={customList.selected}>
+      <LocationListItem.Accordion
+        expanded={expanded}
+        onExpandedChange={setExpanded}
+        disabled={disabled}>
+        <LocationListItem.Header level={0}>
+          <LocationListItem.HeaderTrigger
+            onClick={handleClick}
+            aria-label={sprintf(
+              // TRANSLATORS: Accessibility label for a button that connects to a location.
+              // TRANSLATORS: Available placeholders:
+              // TRANSLATORS: %(location)s - The name of the location that will be connected to when the button is clicked.
+              messages.pgettext('accessibility', 'Connect to %(location)s'),
               {
-                // TRANSLATORS: Message shown in the custom list section when the user has at least one custom list.
-                // TRANSLATORS: Instructs the user how to add locations to the custom list.
-                messages.pgettext(
-                  'select-location-view',
-                  'Click "+" on a location to add it to a list',
-                )
-              }
-            </Text>
-          </Container>
-        )}
-      </FlexColumn>
-    </FlexColumn>
+                location: customList.label,
+              },
+            )}>
+            <LocationListItem.HeaderItem>
+              <FlexColumn>
+                <LocationListItem.HeaderTitle>{customList.label}</LocationListItem.HeaderTitle>
+                {showEmptySubtitle && (
+                  <FootnoteMiniSemiBold color="whiteAlpha60">
+                    {
+                      // TRANSLATORS: Label for custom lists that don't have any locations added to them yet.
+                      messages.pgettext('select-location-view', 'Empty')
+                    }
+                  </FootnoteMiniSemiBold>
+                )}
+              </FlexColumn>
+            </LocationListItem.HeaderItem>
+          </LocationListItem.HeaderTrigger>
+          <CustomListTrailingActions customList={customList} />
+        </LocationListItem.Header>
+        <LocationListItem.AccordionContent>
+          <AnimatedList>{showChildren ? renderChildren() : null}</AnimatedList>
+        </LocationListItem.AccordionContent>
+      </LocationListItem.Accordion>
+    </LocationListItem>
   );
 }
-
-export function CustomListLocationList() {
+export function CustomListLocationList({ ...props }: CustomListLocationListProps) {
   return (
     <CustomListLocationListProvider>
-      <CustomListLocationListImpl />
+      <CustomListLocationListImpl {...props} />
     </CustomListLocationListProvider>
   );
 }
