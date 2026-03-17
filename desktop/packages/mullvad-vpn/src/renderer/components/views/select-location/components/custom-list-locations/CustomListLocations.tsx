be023d3a7c1ed146d4ed33493760444af8bfe677
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-03-17 21:42:37 +0100

    Add CustomListLocationLists
    This component handles rendering of a custom list and its children.

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-locations/CustomListLocations.tsx
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+
+import { messages } from '../../../../../../shared/gettext';
+import { Container, Text } from '../../../../../lib/components';
+import { AnimatedList } from '../../../../../lib/components/animated-list';
+import { FlexColumn } from '../../../../../lib/components/flex-column';
+import { spacings } from '../../../../../lib/foundations';
+import { useHasCustomLists } from '../../hooks';
+import { useSelectLocationViewContext } from '../../SelectLocationViewContext';
+import { getLocationListItemMapProps } from '../../utils';
+import { CustomListLocationList } from '../custom-list-location-list';
+import { CustomListsSectionTitle } from './components';
+import {
+  CustomListLocationsProvider,
+  useCustomListLocationsContext,
+} from './CustomListLocationsContext';
+
+const StyledAnimatedList = styled(AnimatedList)`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledAnimatedListItem = styled(AnimatedList.Item)`
+  margin-bottom: ${spacings.tiny};
+`;
+
+function CustomListLocationsImpl() {
+  const { addingCustomList } = useCustomListLocationsContext();
+  const { customListLocations } = useSelectLocationViewContext();
+
+  const hasCustomLists = useHasCustomLists();
+  const showAddCustomListText = !hasCustomLists && !addingCustomList;
+  const showAddLocationToCustomListText = hasCustomLists;
+
+  return (
+    <FlexColumn gap="tiny">
+      <CustomListsSectionTitle />
+      <FlexColumn>
+        <StyledAnimatedList>
+          {customListLocations.map((customList) => {
+            const { key } = getLocationListItemMapProps(customList, undefined);
+            return (
+              <StyledAnimatedListItem key={key}>
+                <CustomListLocationList customList={customList} />
+              </StyledAnimatedListItem>
+            );
+          })}
+        </StyledAnimatedList>
+
+        {showAddCustomListText && (
+          <Text variant="labelTiny" color="whiteAlpha60">
+            {
+              // TRANSLATORS: Message shown when the user has no custom lists.
+              // TRANSLATORS: Instructs the user how to create a custom list.
+              messages.pgettext(
+                'select-location-view',
+                'Add a custom list by clicking the “+” icon',
+              )
+            }
+          </Text>
+        )}
+        {showAddLocationToCustomListText && (
+          <Container horizontalMargin="medium">
+            <Text variant="labelTiny" color="whiteAlpha60">
+              {
+                // TRANSLATORS: Message shown in the custom list section when the user has at least one custom list.
+                // TRANSLATORS: Instructs the user how to add locations to the custom list.
+                messages.pgettext(
+                  'select-location-view',
+                  'Click “+“ on a location to add it to a list',
+                )
+              }
+            </Text>
+          </Container>
+        )}
+      </FlexColumn>
+    </FlexColumn>
+  );
+}
+
+export function CustomListLocations() {
+  return (
+    <CustomListLocationsProvider>
+      <CustomListLocationsImpl />
+    </CustomListLocationsProvider>
+  );
+}
---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-locations/components/custom-lists-section-title/CustomListsSectionTitle.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { messages } from '../../../../../../../../shared/gettext';
+import { CreateCustomListDialog } from '../../../../../../../features/custom-lists/components';
+import {
+  SectionTitle,
+  type SectionTitleProps,
+} from '../../../../../../../lib/components/section-title';
+import { useCustomListLocationsContext } from '../../CustomListLocationsContext';
+
+export type CustomListsSectionTitleProps = SectionTitleProps;
+
+export function CustomListsSectionTitle({ ...props }: CustomListsSectionTitleProps) {
+  const {
+    addingCustomList,
+    setAddingCustomList,
+    addCustomListDialogOpen,
+    setAddCustomListDialogOpen,
+  } = useCustomListLocationsContext();
+
+  const handleOnClick = React.useCallback(() => {
+    setAddCustomListDialogOpen(true);
+  }, [setAddCustomListDialogOpen]);
+
+  return (
+    <SectionTitle {...props}>
+      <SectionTitle.Title>
+        {messages.pgettext('select-location-view', 'Custom lists')}
+      </SectionTitle.Title>
+      <SectionTitle.Divider />
+      <SectionTitle.IconButton
+        onClick={handleOnClick}
+        aria-label={
+          // TRANSLATORS: This is an accessibility label for a button that opens a dialog to create a new custom list.
+          messages.pgettext('accessibility', 'Add a new custom list')
+        }>
+        <SectionTitle.IconButton.Icon icon="add" />
+      </SectionTitle.IconButton>
+      <CreateCustomListDialog
+        open={addCustomListDialogOpen}
+        onOpenChange={setAddCustomListDialogOpen}
+        loading={addingCustomList}
+        onLoadingChange={setAddingCustomList}
+      />
+    </SectionTitle>
+  );
+}
