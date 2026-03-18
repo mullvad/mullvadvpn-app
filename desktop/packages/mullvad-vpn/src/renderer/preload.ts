524a6a6ccedb4b36d5f93460f281ec1da00b18d6
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-03-18 15:47:31 +0100

    REMOVE Force window.env.e2e to true

--- desktop/packages/mullvad-vpn/src/renderer/preload.ts
+++ desktop/packages/mullvad-vpn/src/renderer/preload.ts
@@ -5,11 +5,11 @@ import { IpcRendererEventChannel } from './lib/ipc-event-channel';
 contextBridge.exposeInMainWorld('ipc', IpcRendererEventChannel);
 
 contextBridge.exposeInMainWorld('env', {
-  e2e: process.env.CI,
+  e2e: true,
   development: process.env.NODE_ENV === 'development',
   platform: process.platform,
 });
 
-if (process.env.CI) {
-  contextBridge.exposeInMainWorld('__REACT_DEVTOOLS_GLOBAL_HOOK__', { isDisabled: true });
-}
+// if (process.env.CI) {
+//   contextBridge.exposeInMainWorld('__REACT_DEVTOOLS_GLOBAL_HOOK__', { isDisabled: true });
+// }
