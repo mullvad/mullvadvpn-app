e317cb71a9ff590e1c767db30f90a9c81e852743
Oliver <oliver@mohlin.dev>
2026-03-29 08:21:02 +0200

    Add expandable content component

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionContent.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/accordion/components/AccordionContent.tsx
@@ -1,43 +1,23 @@
-import { AnimatePresence, motion } from 'motion/react';
 import React from 'react';
 import styled from 'styled-components';
 
+import { Expandable } from '../../expandable';
 import { useAccordionContext } from '../AccordionContext';
 
 export type AccordionContentProps = {
   children?: React.ReactNode;
 };
 
-export const StyledAccordionContent = styled(motion.div)`
-  width: 100%;
-  overflow: hidden;
-`;
-
-const variants = {
-  collapsed: { height: 0, opacity: 0 },
-  expanded: { height: 'auto', opacity: 1 },
-};
+export const StyledAccordionContent = styled(Expandable.Content)``;
 
 export function AccordionContent({ children, ...props }: AccordionContentProps) {
   const { contentId, triggerId, expanded, setContent } = useAccordionContext();
 
   return (
-    <AnimatePresence initial={false}>
-      {expanded && (
-        <StyledAccordionContent
-          id={contentId}
-          ref={setContent}
-          aria-labelledby={triggerId}
-          role="region"
-          variants={variants}
-          initial="collapsed"
-          animate="expanded"
-          exit="collapsed"
-          transition={{ duration: 0.25, ease: 'easeInOut' }}
-          {...props}>
-          {children}
-        </StyledAccordionContent>
-      )}
-    </AnimatePresence>
+    <Expandable expanded={expanded} {...props}>
+      <StyledAccordionContent id={contentId} ref={setContent} aria-labelledby={triggerId}>
+        {children}
+      </StyledAccordionContent>
+    </Expandable>
   );
 }
