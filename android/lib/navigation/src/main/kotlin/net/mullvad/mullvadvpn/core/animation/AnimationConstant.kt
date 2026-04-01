52005f5bdd492ce81aea7b4afd63511c61c75357
Kalle Lindström <karl.lindstrom@mullvad.net>
2026-04-01 09:39:32 +0200

    Reduce transition animation time

--- android/lib/navigation/src/main/kotlin/net/mullvad/mullvadvpn/core/animation/AnimationConstant.kt
+++ android/lib/navigation/src/main/kotlin/net/mullvad/mullvadvpn/core/animation/AnimationConstant.kt
@@ -2,4 +2,4 @@ package net.mullvad.mullvadvpn.core.animation
 
 const val ENTER_TRANSITION_SCALE_IN_FACTOR = 1.1f
 const val ENTER_TRANSITION_SLIDE_FACTOR = 0.25f
-const val TRANSITION_DEFAULT_DURATION_MS = 300
+const val TRANSITION_DEFAULT_DURATION_MS = 250
