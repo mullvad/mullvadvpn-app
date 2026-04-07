2bd57b5ed0355baa1213de44c9cf30e704bac730
Markus Pettersson <markus.pettersson@mullvad.net>
2026-04-07 10:33:23 +0200

    Revert "Merge branch 'fix-xcompiling-wggors'"
    This reverts commit 045451b71094c0175f1ca3d9963e1ea4f2e6ed0e, reversing
    changes made to bbbab6fc0275c3070473fd7cf35660fb7569b9ba.

--- wireguard-go-rs/build.rs
+++ wireguard-go-rs/build.rs
@@ -21,9 +21,11 @@ fn main() -> anyhow::Result<()> {
     println!("cargo::rerun-if-changed=libwg");
 
     let out_dir = env::var("OUT_DIR").context("Missing OUT_DIR")?;
-    if target_os()? == Os::Windows && host_os() == Os::Windows {
-        build_windows_dynamic_lib(&out_dir)?;
+    match target_os()? {
+        Os::Windows if !is_cross_compiling()? => build_windows_dynamic_lib(&out_dir)?,
+        _ => (),
     }
+
     Ok(())
 }
 
@@ -134,6 +136,11 @@ fn build_windows_dynamic_lib(out_dir: &str) -> anyhow::Result<()> {
     println!("cargo::rustc-link-lib=dylib=libwg");
 
     Ok(())
+}
+
+/// Return whether compiling for an architecture or OS other than the host
+fn is_cross_compiling() -> anyhow::Result<bool> {
+    Ok(host_os() != target_os()? || host_arch() != target_arch()?)
 }
 
 // Build dynamically library for maybenot
