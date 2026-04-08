5e6981415cdcb5ada44e8cd9b641660f8f8cb416
Oliver <oliver@mohlin.dev>
2026-04-08 16:28:49 +0200

    Add has recents boolean to recents feature hook

--- desktop/packages/mullvad-vpn/src/renderer/features/locations/hooks/use-recents.ts
+++ desktop/packages/mullvad-vpn/src/renderer/features/locations/hooks/use-recents.ts
@@ -4,8 +4,11 @@ import { useSelector } from '../../../redux/store';
 export function useRecents() {
   const recents = useSelector((state) => state.settings.recents);
   const { setEnabledRecents } = useAppContext();
+  const hasRecents = recents !== undefined;
+
   return {
     recents,
+    hasRecents,
     setEnabledRecents,
   };
 }
