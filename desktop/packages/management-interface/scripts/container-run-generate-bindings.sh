c274a3f51b9ca126ba7272de6990ca6ac1d01694
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-04-13 16:33:16 +0200

    Add support for running container on ARM hosts
    In order to run the container on an ARM host the
    `--platform linux/amd64` argument must be used.

--- desktop/packages/management-interface/scripts/container-run-generate-bindings.sh
+++ desktop/packages/management-interface/scripts/container-run-generate-bindings.sh
@@ -12,11 +12,18 @@ IMAGE_HASH="4c6c9f0924"
 IMAGE_NAME="ghcr.io/mullvad/mullvadvpn-app-build-node-grpc-bindings:$IMAGE_HASH"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_DIR="$( cd "$SCRIPT_DIR/../../../../" && pwd )"
 PROTO_DIR="$( cd "$SCRIPT_DIR/../../../../mullvad-management-interface/proto" && pwd )"
 OUT_DIR="$SCRIPT_DIR/../dist"
 
+source "$REPO_DIR/scripts/utils/host"
+
+if [[ $HOST == "aarch64"* ]]; then
+    ARM_COMMANDS=(--platform linux/amd64)
+fi
+
 set -x
-exec "$CONTAINER_RUNNER" run --rm -it \
+exec "$CONTAINER_RUNNER" run --rm -it "${ARM_COMMANDS[@]}" \
     -v "$PROTO_DIR:/proto:Z" \
     -v "$OUT_DIR:/proto-bindings:Z" \
     "$IMAGE_NAME"
