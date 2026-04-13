e9b685890a8ffd77af881f798b98f0193df9e003
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-04-13 17:06:40 +0200

    fixup! Add support for running container on ARM hosts

--- desktop/packages/management-interface/scripts/container-run-generate-bindings.sh
+++ desktop/packages/management-interface/scripts/container-run-generate-bindings.sh
@@ -12,18 +12,20 @@ IMAGE_HASH="4c6c9f0924"
 IMAGE_NAME="ghcr.io/mullvad/mullvadvpn-app-build-node-grpc-bindings:$IMAGE_HASH"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPO_DIR="$( cd "$SCRIPT_DIR/../../../../" && pwd )"
-PROTO_DIR="$( cd "$SCRIPT_DIR/../../../../mullvad-management-interface/proto" && pwd )"
 OUT_DIR="$SCRIPT_DIR/../dist"
+REPO_DIR="$( cd "$SCRIPT_DIR/../../../../" && pwd )"
+PROTO_DIR="$( cd "$REPO_DIR/mullvad-management-interface/proto" && pwd )"
 
 source "$REPO_DIR/scripts/utils/host"
 
+# If the host system is ARM-based we must specify the guest platform,
+# otherwise an error will be throw about mismatching architecture
 if [[ $HOST == "aarch64"* ]]; then
-    ARM_COMMANDS=(--platform linux/amd64)
+    RUN_COMMANDS=(--platform linux/amd64)
 fi
 
 set -x
-exec "$CONTAINER_RUNNER" run --rm -it "${ARM_COMMANDS[@]}" \
+exec "$CONTAINER_RUNNER" run --rm -it "${RUN_COMMANDS[@]}" \
     -v "$PROTO_DIR:/proto:Z" \
     -v "$OUT_DIR:/proto-bindings:Z" \
     "$IMAGE_NAME"
