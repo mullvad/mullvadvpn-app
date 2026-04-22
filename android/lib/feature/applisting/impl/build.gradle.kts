ae6897e053ea9cb9850c3beaf5d71d4564a3bfb0
Albin <albin@mullvad.net>
2026-04-22 12:00:27 +0200

    Fix gradle config after agp upgrade

--- android/lib/feature/applisting/impl/build.gradle.kts
+++ android/lib/feature/applisting/impl/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.mullvad.android.library)
     alias(libs.plugins.mullvad.android.library.feature.impl)
-    alias(libs.plugins.kotlin.android)
 }
 
 android { namespace = "net.mullvad.mullvadvpn.feature.applisting.impl" }
