2026b2c05d728e7e6cf4300a411cbc3272c36aaf
Emīls <emils@mullvad.net>
2026-04-23 22:50:22 +0200

    Add domain fronting to iOS

--- ios/PacketTunnel/PacketTunnelProvider/PacketTunnelProvider.swift
+++ ios/PacketTunnel/PacketTunnelProvider/PacketTunnelProvider.swift
@@ -249,7 +249,9 @@ class PacketTunnelProvider: NEPacketTunnelProvider, @unchecked Sendable {
         apiContext = try! MullvadApiContext(
             host: REST.defaultAPIHostname,
             address: REST.defaultAPIEndpoint.description,
-            domain: REST.encryptedDNSHostname,
+            encryptedDnsDomain: REST.encryptedDNSHostname,
+            domainFrontingFront: REST.domainFrontingFront,
+            domainFrontingProxyHost: REST.domainFrontingProxyHost,
             shadowsocksProvider: shadowsocksLoader,
             accessMethodWrapper: opaqueAccessMethodSettingsWrapper,
             accessMethodChangeListeners: [accessMethodRepository, shadowsocksCacheCleaner]
