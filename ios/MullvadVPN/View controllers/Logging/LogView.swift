8b730d2d188ecde2e7c89f6e074733d71173a270
Jon Petersson <jon.petersson@mullvad.net>
2026-04-23 22:57:08 +0200

    Tweaks

--- ios/MullvadVPN/View controllers/Logging/LogView.swift
+++ ios/MullvadVPN/View controllers/Logging/LogView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LogView: UIView, UITableViewDataSource {
+class LogView: UIView, UITableViewDataSource, UITableViewDelegate {
     private let panelHeight: CGFloat = 230
     private let safeTop: CGFloat = 60
     private let safeBottom: CGFloat = 40
@@ -20,9 +20,10 @@ class LogView: UIView, UITableViewDataSource {
     private var entries: [String] = []
     private var dragStartY: CGFloat = 0
 
+    // Snaps to top, middle and bottom.
     private lazy var snapYPositions: [CGFloat] = {
-        let screen = UIScreen.main.bounds.height
-        return [safeTop, (screen - panelHeight) / 2, screen - panelHeight - safeBottom]
+        let screenHeight = UIScreen.main.bounds.height
+        return [safeTop, (screenHeight - panelHeight) / 2, screenHeight - panelHeight - safeBottom]
     }()
 
     init(viewModel: LogViewModel) {
@@ -39,15 +40,14 @@ class LogView: UIView, UITableViewDataSource {
     }
 
     private func setUp() {
-        let screen = UIScreen.main.bounds
         frame = CGRect(
             x: 8,
-            y: snapYPositions.last!,
-            width: screen.width - 16,
+            y: snapYPositions.first!,
+            width: UIScreen.main.bounds.width - 16,
             height: panelHeight
         )
 
-        backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        backgroundColor = .black.withAlphaComponent(0.7)
         layer.cornerRadius = 12
         clipsToBounds = true
 
@@ -56,45 +56,44 @@ class LogView: UIView, UITableViewDataSource {
         setUpHandle()
         setUpTableView()
 
-        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        addGestureRecognizer(pan)
+        addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:))))
     }
 
     private func setUpHandle() {
-        handleView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        handleView.backgroundColor = .white.withAlphaComponent(0.5)
         handleView.layer.cornerRadius = 2
-        handleView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(handleView)
 
-        NSLayoutConstraint.activate([
-            handleView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
-            handleView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            handleView.widthAnchor.constraint(equalToConstant: 36),
-            handleView.heightAnchor.constraint(equalToConstant: 4),
-        ])
+        addConstrainedSubviews([handleView]) {
+            handleView.pinEdgeToSuperview(.top(8))
+            handleView.centerXAnchor.constraint(equalTo: centerXAnchor)
+            handleView.widthAnchor.constraint(equalToConstant: 36)
+            handleView.heightAnchor.constraint(equalToConstant: 4)
+        }
     }
 
     private func setUpTableView() {
         tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "LogCell")
+
         tableView.backgroundColor = .clear
         tableView.separatorStyle = .none
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "LogCell")
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(tableView)
+        tableView.showsVerticalScrollIndicator = true
+        tableView.indicatorStyle = .white
+        tableView.verticalScrollIndicatorInsets.right = 2
 
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: handleView.bottomAnchor, constant: 8),
-            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
-        ])
+        addConstrainedSubviews([tableView]) {
+            tableView.pinEdgesToSuperview(.all().excluding(.top))
+            tableView.topAnchor.constraint(equalTo: handleView.bottomAnchor, constant: 8)
+        }
     }
 
     private func addEntry(_ entry: String) {
         entries.append(entry)
+
         let indexPath = IndexPath(row: entries.count - 1, section: 0)
         tableView.insertRows(at: [indexPath], with: .none)
-        tableView.scrollToRow(at: indexPath, at: .bottom, animated: false)
+        tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
     }
 
     // MARK: - Dragging
@@ -108,7 +107,8 @@ class LogView: UIView, UITableViewDataSource {
             frame.origin.y = dragStartY + translation
         case .ended, .cancelled:
             let currentY = frame.origin.y
-            let targetY = snapYPositions.min(by: { abs($0 - currentY) < abs($1 - currentY) })!
+            let targetY = snapYPositions.min(by: { abs($0 - currentY) < abs($1 - currentY) }) ?? safeTop
+
             UIView.animate(
                 withDuration: 0.35,
                 delay: 0,
@@ -131,13 +131,40 @@ class LogView: UIView, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LogCell", for: indexPath)
+
         var config = cell.defaultContentConfiguration()
         config.text = entries[indexPath.row]
         config.textProperties.font = .monospacedSystemFont(ofSize: 11, weight: .regular)
         config.textProperties.color = .white
+
         cell.contentConfiguration = config
         cell.backgroundColor = .clear
         cell.selectionStyle = .none
+
         return cell
+    }
+
+    // MARK: - UITableViewDelegate
+
+    func tableView(_ tableView: UITableView, shouldShowMenuForRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        canPerformAction action: Selector,
+        forRowAt indexPath: IndexPath,
+        withSender sender: Any?
+    ) -> Bool {
+        action == #selector(copy(_:))
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        performAction action: Selector,
+        forRowAt indexPath: IndexPath,
+        withSender sender: Any?
+    ) {
+        UIPasteboard.general.string = entries[indexPath.row]
     }
 }
