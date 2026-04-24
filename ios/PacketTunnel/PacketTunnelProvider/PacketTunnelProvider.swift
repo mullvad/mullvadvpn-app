8990935945855676c335c0f0e3e14c3a37316a81
Jon Petersson <jon.petersson@mullvad.net>
2026-04-24 14:13:50 +0200

    Cleanup

--- ios/PacketTunnel/PacketTunnelProvider/PacketTunnelProvider.swift
+++ ios/PacketTunnel/PacketTunnelProvider/PacketTunnelProvider.swift
@@ -311,9 +311,11 @@ extension PacketTunnelProvider {
         )
         #if DEBUG
             loggerBuilder.addOSLogOutput(subsystem: ApplicationTarget.packetTunnel.bundleIdentifier)
-            loggerBuilder.addInAppLogOutput(observer: InAppLogBlockObserver {
-                print($0)
-            })
+            loggerBuilder.addInAppLogOutput(
+                observer: InAppLogBlockObserver {
+                    print($0)
+                }
+            )
         #endif
         loggerBuilder.install(redactor)
 
