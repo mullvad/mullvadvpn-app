514f1bc08f4c9f95c1bd9b2abd52d6d05ed5ca2c
David Lönnhager <david.l@mullvad.net>
2026-04-27 16:34:19 +0200

    fixup: generic

--- mullvad-nsis/src/logger.rs
+++ mullvad-nsis/src/logger.rs
@@ -10,7 +10,7 @@
 use std::fmt::Write as FmtWrite;
 use std::fs::File;
 use std::io::{self, Write};
-use std::path::PathBuf;
+use std::path::Path;
 use std::ptr;
 use std::sync::{Mutex, OnceLock};
 
@@ -56,8 +56,8 @@ struct Logger {
 }
 
 impl Logger {
-    fn new(path: PathBuf) -> io::Result<Self> {
-        let file = File::create(&path)?;
+    fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        let file = File::create(path)?;
         Ok(Logger { file })
     }
 
