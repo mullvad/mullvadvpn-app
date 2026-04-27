c23373a5d076e4936d64e0a2b1ae40276a5e3d04
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-04-27 13:13:54 +0200

    fixup! Replace Modal with Dialog component in DeviceListItem's ConfirmDialog

--- desktop/packages/mullvad-vpn/src/renderer/components/device-list-item/components/confirm-dialog/ConfirmDialog.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/device-list-item/components/confirm-dialog/ConfirmDialog.tsx
@@ -2,6 +2,7 @@ import { sprintf } from 'sprintf-js';
 
 import { messages } from '../../../../../shared/gettext';
 import { Dialog } from '../../../../lib/components/dialog';
+import { FlexColumn } from '../../../../lib/components/flex-column';
 import { formatHtml } from '../../../../lib/html-formatter';
 import { formatDeviceName } from '../../../../lib/utils';
 import { useDeviceListItemContext } from '../../DeviceListItemContext';
@@ -16,24 +17,27 @@ export function ConfirmDialog({ isOpen }: { isOpen: boolean }) {
       <Dialog.Portal>
         <Dialog.Popup>
           <Dialog.PopupContent>
-            <Dialog.Text>
-              {formatHtml(
-                sprintf(
-                  // TRANSLATORS: Text displayed above button which logs out another device.
-                  // TRANSLATORS: The text enclosed in "<b></b>" will appear bold.
-                  // TRANSLATORS: Available placeholders:
-                  // TRANSLATORS: %(deviceName)s - The name of the device to log out.
-                  messages.pgettext('device-management', 'Remove <em>%(deviceName)s?</em>'),
-                  { deviceName: formatDeviceName(device.name) },
-                ),
-              )}
-            </Dialog.Text>
-            <Dialog.Text variant="labelTinySemiBold" color="whiteAlpha60">
-              {messages.pgettext(
-                'device-management',
-                'The device will be removed from the list and logged out.',
-              )}
-            </Dialog.Text>
+            <Dialog.Icon icon="info-circle" />
+            <FlexColumn>
+              <Dialog.Text>
+                {formatHtml(
+                  sprintf(
+                    // TRANSLATORS: Text displayed above button which logs out another device.
+                    // TRANSLATORS: The text enclosed in "<b></b>" will appear bold.
+                    // TRANSLATORS: Available placeholders:
+                    // TRANSLATORS: %(deviceName)s - The name of the device to log out.
+                    messages.pgettext('device-management', 'Remove <em>%(deviceName)s?</em>'),
+                    { deviceName: formatDeviceName(device.name) },
+                  ),
+                )}
+              </Dialog.Text>
+              <Dialog.Text variant="labelTinySemiBold" color="whiteAlpha60">
+                {messages.pgettext(
+                  'device-management',
+                  'The device will be removed from the list and logged out.',
+                )}
+              </Dialog.Text>
+            </FlexColumn>
             <Dialog.ButtonGroup>
               <Dialog.Button onClick={handleRemoveDevice} disabled={deleting}>
                 <Dialog.Button.Text>
