14e23279e8a1121affcc1c675bdcbf8603773ccf
David Lönnhager <david.l@mullvad.net>
2026-04-30 16:28:44 +0200

    Bundle relay list at release time on desktop
    The relay list is produced and committed in
    desktop/scripts/release/1-prepare-release instead of build.sh.

--- android/scripts/prepare-release.sh
+++ android/scripts/prepare-release.sh
@@ -39,18 +39,7 @@ if [[ $PRODUCT_VERSION != *"alpha"* &&
     exit 1
 fi
 
-echo "### Generating relay list ###"
-mkdir -p dist-assets/relays
-cargo run -q -p mullvad-api --bin relay_list > dist-assets/relays/relays.json
-if [[ ! -f dist-assets/relays/relays.json ]]; then
-    echo "Error: Relay list missing."
-    exit 1
-elif test ! -n "$(git status --porcelain | grep dist-assets/relays/)"; then
-    echo "Relay list unchanged, skipping commit."
-else
-    git add dist-assets/relays/relays.json
-    git commit -S -m "Add relay list to bundle with $PRODUCT_VERSION"
-fi
+scripts/commit-relay-list "$PRODUCT_VERSION"
 echo ""
 
 echo "### Generating version information ###"
