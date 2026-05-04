4e01935901feb0992cb6d3ee70b3ce6d7061ebe4
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-05-04 15:22:03 +0200

    Remove line number

--- android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/ConnectionTest.kt
+++ android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/ConnectionTest.kt
@@ -145,7 +145,7 @@ class ConnectionTest : EndToEndTest() {
                 // Currently it takes ~60 seconds to connect with wg obfuscation automatic and UDP
                 // traffic blocked so we need to be very forgiving
                 // The order of obfuscation methods in automatic mode can be found here:
-                // mullvad-relay-selector/src/relay_selector/mod.rs:48.
+                // mullvad-relay-selector/src/relay_selector/mod.rs
                 waitForConnectedLabel(timeout = VERY_FORGIVING_WIREGUARD_OFF_CONNECTION_TIMEOUT)
             }
         }
