c8bd68bde1b0504a0352fd062e7df91d56c7930f
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-05-08 09:34:19 +0200

    Fix formatting

--- android/lib/payment/src/main/kotlin/net/mullvad/mullvadvpn/lib/payment/util/PaymentAvailability.kt
+++ android/lib/payment/src/main/kotlin/net/mullvad/mullvadvpn/lib/payment/util/PaymentAvailability.kt
@@ -3,7 +3,7 @@ package net.mullvad.mullvadvpn.lib.payment.util
 import net.mullvad.mullvadvpn.lib.payment.model.PaymentAvailability
 
 fun PaymentAvailability?.hasPendingPayment(): Boolean =
-    when(this) {
+    when (this) {
         is PaymentAvailability.ProductsAvailable -> this.products.any { it.status != null }
         else -> false
     }
