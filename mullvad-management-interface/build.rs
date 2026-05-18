964169ec4c903a82b117d171359adc03028d6594
Markus Pettersson <markus.pettersson@mullvad.net>
2026-05-18 20:13:33 +0200

    Upgrade tonic to 0.14.6

--- mullvad-management-interface/build.rs
+++ mullvad-management-interface/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     // Compile both proto files together so they can reference each other
-    tonic_build::configure()
+    tonic_prost_build::configure()
+        .with_extended_rust_types(true)
         .compile_protos(
             &[
                 "proto/management_interface.proto",
