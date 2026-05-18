13cf19a2ef3d7fd1d8a05d25c61acc53e73138da
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-05-18 15:02:15 +0200

    Revert "Add support for domain fronting api access method"
    This reverts commit c243130eb3c967a39206d84a65a0cf8aa6d88f67.

--- android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/mapper/FromDomain.kt
+++ android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/mapper/FromDomain.kt
@@ -216,10 +216,6 @@ internal fun ApiAccessMethod.fromDomain(): ManagementInterface.AccessMethod =
                     it.setEncryptedDnsProxy(
                         ManagementInterface.AccessMethod.EncryptedDnsProxy.getDefaultInstance()
                     )
-                is ApiAccessMethod.DomainFronting ->
-                    it.setDomainFronting(
-                        ManagementInterface.AccessMethod.DomainFronting.getDefaultInstance()
-                    )
             }
         }
         .build()
