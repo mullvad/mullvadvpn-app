9a51f6d996a116bb45ebab4e0e52c866887ef8d2
Markus Pettersson <markus.pettersson@mullvad.net>
2026-05-18 16:13:41 +0200

    Update desktop gRPC bindings

--- desktop/packages/management-interface/dist/management_interface_pb.d.ts
+++ desktop/packages/management-interface/dist/management_interface_pb.d.ts
@@ -2127,8 +2127,8 @@ export class WireguardConstraints extends jspb.Message {
     getAllowedIpsList(): Array<string>;
     setAllowedIpsList(value: Array<string>): WireguardConstraints;
     addAllowedIps(value: string, index?: number): string;
-    getUseMultihop(): boolean;
-    setUseMultihop(value: boolean): WireguardConstraints;
+    getMultihop(): WireguardConstraints.Multihop;
+    setMultihop(value: WireguardConstraints.Multihop): WireguardConstraints;
 
     hasEntryLocation(): boolean;
     clearEntryLocation(): void;
@@ -2155,11 +2155,18 @@ export namespace WireguardConstraints {
     export type AsObject = {
         ipVersion?: IpVersion,
         allowedIpsList: Array<string>,
-        useMultihop: boolean,
+        multihop: WireguardConstraints.Multihop,
         entryLocation?: LocationConstraint.AsObject,
         entryProvidersList: Array<string>,
         entryOwnership: Ownership,
     }
+
+    export enum Multihop {
+    AUTO = 0,
+    ALWAYS = 1,
+    NEVER = 2,
+    }
+
 }
 
 export class CustomRelaySettings extends jspb.Message { 
@@ -3548,7 +3555,7 @@ export enum FeatureIndicator {
     SERVER_IP_OVERRIDE = 12,
     CUSTOM_MTU = 13,
     DAITA = 14,
-    DAITA_MULTIHOP = 15,
+    MULTIHOP_AUTO = 15,
 }
 
 export enum Ownership {
