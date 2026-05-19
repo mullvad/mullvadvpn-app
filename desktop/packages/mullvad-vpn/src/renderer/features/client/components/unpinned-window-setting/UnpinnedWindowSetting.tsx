d207dad736c2e0c9f27b32f34ac92594891dc800
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-05-19 14:57:30 +0200

    Update the label for the unpin app switch to be platform specific
    On macOS the bar is called "menu bar" and on Windows it is called
    "taskbar".

--- desktop/packages/mullvad-vpn/src/renderer/features/client/components/unpinned-window-setting/UnpinnedWindowSetting.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/features/client/components/unpinned-window-setting/UnpinnedWindowSetting.tsx
@@ -3,19 +3,26 @@ import React from 'react';
 import { messages } from '../../../../../shared/gettext';
 import { SettingsListItem } from '../../../../components/settings-list-item';
 import { ListItemProps } from '../../../../lib/components/list-item';
+import { isPlatform } from '../../../../utils';
 import { UnpinnedWindowSwitch } from '../unpinned-window-switch/UnpinnedWindowSwitch';
 
 export type UnpinnedWindowSettingProps = Omit<ListItemProps, 'children'>;
 
 export function UnpinnedWindowSetting(props: UnpinnedWindowSettingProps) {
   const descriptionId = React.useId();
+  const messageLabel = isPlatform('darwin')
+    ? // This line is here to prevent the following one to be moved up here by prettier
+      // TRANSLATORS: Label for a setting to unpin the app window from the macOS "menu bar"
+      messages.pgettext('user-interface-settings-view', 'Unpin app from menu bar')
+    : // This line is here to prevent the following one to be moved up here by prettier
+      // TRANSLATORS: Label for a setting to unpin the app window from the Windows "taskbar"
+      messages.pgettext('user-interface-settings-view', 'Unpin app from taskbar');
+
   return (
     <SettingsListItem {...props}>
       <SettingsListItem.Item>
         <UnpinnedWindowSwitch descriptionId={descriptionId}>
-          <UnpinnedWindowSwitch.Label>
-            {messages.pgettext('user-interface-settings-view', 'Unpin app from taskbar')}
-          </UnpinnedWindowSwitch.Label>
+          <UnpinnedWindowSwitch.Label>{messageLabel}</UnpinnedWindowSwitch.Label>
           <SettingsListItem.Item.ActionGroup>
             <UnpinnedWindowSwitch.Input />
           </SettingsListItem.Item.ActionGroup>
