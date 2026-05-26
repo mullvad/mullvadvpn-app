9929ac31fa08f09462980f5ac62e275eec651828
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-05-27 01:29:07 +0200

    Fixes based on feedback

--- android/lib/feature/apiaccess/impl/src/main/java/net/mullvad/mullvadvpn/feature/apiaccess/impl/screen/edit/ApiAccessMethodTextField.kt
+++ android/lib/feature/apiaccess/impl/src/main/java/net/mullvad/mullvadvpn/feature/apiaccess/impl/screen/edit/ApiAccessMethodTextField.kt
@@ -2,7 +2,6 @@ package net.mullvad.mullvadvpn.feature.apiaccess.impl.screen.edit
 
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.InputTransformation
-import androidx.compose.foundation.text.input.byValue
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,7 +16,6 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.core.text.isDigitsOnly
 import kotlinx.coroutines.flow.collectLatest
 import net.mullvad.mullvadvpn.lib.ui.component.textfield.ErrorSupportingText
 import net.mullvad.mullvadvpn.lib.ui.component.textfield.mullvadDarkTextFieldColors
@@ -29,9 +27,8 @@ fun ApiAccessMethodTextField(
     modifier: Modifier = Modifier,
     onValueChanged: (String) -> Unit,
     labelText: String?,
-    maxCharLength: Int = Int.MAX_VALUE,
+    inputTransformation: InputTransformation? = null,
     isValidValue: Boolean,
-    isDigitsOnlyAllowed: Boolean,
     errorText: String?,
     capitalization: KeyboardCapitalization = KeyboardCapitalization.None,
     imeAction: ImeAction = ImeAction.Next,
@@ -58,14 +55,7 @@ fun ApiAccessMethodTextField(
             labelText?.let { { Text(text = it, maxLines = 1, overflow = TextOverflow.Ellipsis) } },
         supportingText = errorText?.let { { ErrorSupportingText(errorText) } },
         isError = !isValidValue,
-        inputTransformation =
-            InputTransformation.byValue { current, proposed ->
-                when {
-                    proposed.length > maxCharLength -> return@byValue current
-                    proposed.isDigitsOnly().not() && isDigitsOnlyAllowed -> return@byValue current
-                    else -> proposed
-                }
-            },
+        inputTransformation = inputTransformation,
         onKeyboardAction = {
             if (imeAction == ImeAction.Done) {
                 focusManager.clearFocus()
