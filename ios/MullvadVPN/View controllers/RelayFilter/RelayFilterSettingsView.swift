92ffa914f3cc57e480b621e320270ccda80c3095
Andrew Bulhak <andrew.bulhak@mullvad.net>
2026-05-27 13:55:09 +0200

    Make improvements to UI display logic, et cetera

--- ios/MullvadVPN/View controllers/RelayFilter/RelayFilterSettingsView.swift
+++ ios/MullvadVPN/View controllers/RelayFilter/RelayFilterSettingsView.swift
@@ -8,11 +8,6 @@
 
 import SwiftUI
 
-protocol RelayFilterSettingsViewModelProtocol {
-    var filters: [SelectLocationFilter] { get }
-    func onFilterTapped(_ filterr: SelectLocationFilter)
-}
-
 extension RelayFilterSelection {
     struct SettingsView<SettingsViewModel: RelayFilterSettingsViewModelProtocol & ObservableObject>: View {
         @ObservedObject var viewModel: SettingsViewModel
@@ -22,24 +17,25 @@ extension RelayFilterSelection {
                 if !viewModel.filters.isEmpty {
                     ActiveFilterView(
                         activeFilter: viewModel.filters,
-                        automaticLocationIsActive: false
+                        automaticLocationIsActive: viewModel.automaticLocationIsActive
                     ) { filter in
                         viewModel.onFilterTapped(filter)
                     } onRemove: { _ in
                     }
-                    HStack {
-                        Image.mullvadIconInfo.resizable().frame(width: 14, height: 14).opacity(0.6)
-                        // Should this be "Filters are overridden when using the automatic location", which is an existing localised text already used elsewhere in the UI?
-                        Text("Filters are overridden when using an automatic location").font(.mullvadMini)
-                            .foregroundStyle(
-                                Color.mullvadTextSecondary)
-                        Spacer()
+                    if viewModel.automaticLocationIsActive {
+                        HStack {
+                            Image.mullvadIconInfo.resizable().frame(width: 14, height: 14).opacity(0.6)
+                            Text("Filters are overridden when using an automatic location").font(.mullvadMini)
+                                .foregroundStyle(
+                                    Color.mullvadTextSecondary)
+                            Spacer()
+                        }
+                        .padding(EdgeInsets(top: 4, leading: 16, bottom: 0, trailing: 0))
                     }
-                    .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 0))
                 }
 
             }
-            .padding(EdgeInsets(top: 0, leading: 0, bottom: 16, trailing: 16))
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 4, trailing: 16))
             .background(Color.mullvadBackground)
         }
     }
@@ -47,9 +43,11 @@ extension RelayFilterSelection {
 
 private final class MockSettingsViewModel: RelayFilterSettingsViewModelProtocol, ObservableObject {
     var filters: [SelectLocationFilter]
+    var automaticLocationIsActive: Bool
 
-    init(filters: [SelectLocationFilter]) {
+    init(filters: [SelectLocationFilter], automaticLocationIsActive: Bool) {
         self.filters = filters
+        self.automaticLocationIsActive = automaticLocationIsActive
     }
 
     func onFilterTapped(_ filterr: SelectLocationFilter) {}
@@ -58,6 +56,7 @@ private final class MockSettingsViewModel: RelayFilterSettingsViewModelProtocol,
 #Preview {
     RelayFilterSelection.SettingsView<MockSettingsViewModel>(
         viewModel: MockSettingsViewModel(
-            filters: [.daita, .obfuscation]
+            filters: [.daita, .obfuscation],
+            automaticLocationIsActive: true
         ))
 }
