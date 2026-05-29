080fec5352565b9cdf5594c7bd09b05442e419a8
Emīls <emils@mullvad.net>
2026-05-29 11:47:56 +0200

    Port `raas` to MacOS and perform traffic manipulation
    Porting it to MacOS allows e.g. iOS developers, to more easily run the
    iOS end-to-end test suite in a non-office environment. 
    Writing new, and maintain existing, end-to-end tests therefore
    becomes easier to do.
    
    `raas` is used to simulate certain censorship-like conditions, mainly
    during end-to-end-tests.
    
    This simulation requires a low-level packet inspection to see where
    it is going and whether it should be blocked or not. What to block is
    evaluated based on rules configured over the HTTP API (already
    present).
    
    Traffic flows through the machine running `raas`:
    
    > `iPhone` -> `MacOS computer` -> `upstream`
    
    Because `pf` is more limited in the ability to express filtering
    than `nftables`, we forward received traffic through 2 tunnel devices:
    
    > `en0` -> `tun1` -> `tun2` -> upstream
    
    Traffic coming in on `en0` is redirected to `tun1`, where we are then
    able to inspect packet headers and enforce the configured rules.
    If allowed, the packet is passed into `utun2` which is then NAT'ed to
    the internet.
    
    Reverse forwarding rules have also been put in place to ensure proper
    communication.
    
    The program requires root in order to:
    
    - Enable port forwarding (`sysctl`)
    - Set firewall rules using (`pfctl`)

---
+++ ci/ios/test-router/raas/src/web/capture.rs
@@ -0,0 +1,94 @@
+use axum::{
+    body::Body,
+    extract::{Json, Path, State},
+    http::{StatusCode, header},
+    response::IntoResponse,
+};
+use std::{collections::BTreeSet, net::IpAddr};
+use uuid::Uuid;
+
+use crate::capture::Error;
+
+#[derive(serde::Deserialize, Clone)]
+pub struct NewCapture {
+    pub label: Uuid,
+}
+
+pub async fn start(
+    State(state): State<super::State>,
+    Json(capture): Json<NewCapture>,
+) -> impl IntoResponse {
+    let label = capture.label;
+
+    let result = async {
+        let mut state = state.capture.lock().await;
+        log::info!("Starting capture for label {label}");
+        state.start(label)
+    }
+    .await;
+
+    respond_with_result(result, StatusCode::OK)
+}
+
+pub async fn stop(Path(label): Path<Uuid>, State(state): State<super::State>) -> impl IntoResponse {
+    let result = async {
+        let mut state = state.capture.lock().await;
+        state.stop(label).await?;
+        log::info!("Stopped capture for label {label}");
+        Ok(())
+    }
+    .await;
+
+    respond_with_result(result, StatusCode::OK)
+}
+
+pub async fn get(Path(label): Path<Uuid>, State(state): State<super::State>) -> impl IntoResponse {
+    let state = state.capture.lock().await;
+
+    let stream = match state.get(label).await {
+        Ok(stream) => stream,
+        Err(err) => {
+            return (StatusCode::SERVICE_UNAVAILABLE, format!("{err}\n")).into_response();
+        }
+    };
+
+    let body = Body::from_stream(tokio_util::io::ReaderStream::new(stream));
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        header::CONTENT_TYPE,
+        header::HeaderValue::from_static("application/pcap"),
+    );
+    headers.insert(
+        header::CONTENT_DISPOSITION,
+        header::HeaderValue::from_static("attachment; filename=\"dump.pcap\""),
+    );
+
+    (headers, body).into_response()
+}
+
+pub async fn parse(
+    Path(label): Path<Uuid>,
+    State(state): State<super::State>,
+    Json(host_addrs): Json<BTreeSet<IpAddr>>,
+) -> impl IntoResponse {
+    let state = state.capture.lock().await;
+
+    let stream = match state.get(label).await {
+        Ok(stream) => stream,
+        Err(err) => {
+            return (StatusCode::SERVICE_UNAVAILABLE, format!("{err}\n")).into_response();
+        }
+    };
+
+    match crate::capture::parse_pcap(stream, host_addrs).await {
+        Ok(parsed_connections) => (StatusCode::OK, Json(parsed_connections)).into_response(),
+        Err(err) => (StatusCode::SERVICE_UNAVAILABLE, format!("{err}\n")).into_response(),
+    }
+}
+
+fn respond_with_result(result: Result<(), Error>, success_code: StatusCode) -> impl IntoResponse {
+    match result {
+        Ok(_) => (success_code, String::new()),
+        Err(err) => (StatusCode::SERVICE_UNAVAILABLE, format!("{err}\n")),
+    }
+}
