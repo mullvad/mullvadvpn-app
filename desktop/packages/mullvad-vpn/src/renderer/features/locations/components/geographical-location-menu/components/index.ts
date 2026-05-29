8a370a709af8a1acb03e0ce807b72c3269cf1d7e
Oliver <oliver@mohlin.dev>
2026-05-29 14:21:06 +0200

    Add set as entry and exit options to location menu

--- desktop/packages/mullvad-vpn/src/renderer/features/locations/components/geographical-location-menu/components/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/features/locations/components/geographical-location-menu/components/index.ts
@@ -1,1 +1,4 @@
 export * from './add-location-to-custom-list-menu-option';
+export * from './create-custom-list-menu-option';
+export * from './set-as-entry-menu-option';
+export * from './set-as-exit-menu-option';
