a0853b26655ea4838290b8f0d2da127968877d0e
Daneo Van Overloop <daneo.van.overloop@mullvad.net>
2026-05-29 13:24:22 +0200

    Use flake-parts to support multi-architecture builds
    - One can build using `nix build .#raas` without having to add
    `-aarch64` or  `-linux`.
    - Adding support for other architectures becomes just a tad less
    repetitive.

--- ci/ios/test-router/flake.nix
+++ ci/ios/test-router/flake.nix
@@ -3,85 +3,90 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs =
-    { self, nixpkgs }:
-    {
-      nixosConfigurations.app-team-ios-lab = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          (import ./router-config.nix {
-            hostname = "app-team-ios-tests";
-            lanMac = "a0:ce:c8:ab:bd:2d";
-            wanMac = "88:ae:dd:64:e1:55";
-            lanIp = "192.168.105.1/24";
-            wgIpv4 = "10.64.9.184/32";
-            wgIpv6 = "fc00:bbbb:bbbb:bb01::a40:9b8/128";
-          })
-          ./app-team-ios-lab.nix
-          {
-            boot.loader.systemd-boot.enable = true;
-            boot.loader.efi.canTouchEfiVariables = true;
-            hardware = {
-              cpu.intel.updateMicrocode = true;
-              enableRedistributableFirmware = true;
-            };
-          }
-        ];
+  outputs = inputs@{ self, nixpkgs, flake-parts }:
+    flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
+      imports = [
+        # Optional: use external flake logic, e.g.
+        # inputs.foo.flakeModules.default
+      ];
+      flake = {
+        # Put your original flake attributes here.
+        nixosConfigurations.app-team-ios-lab = nixpkgs.lib.nixosSystem {
+          modules = [
+            (import ./router-config.nix {
+              hostname = "app-team-ios-tests";
+              lanMac = "a0:ce:c8:ab:bd:2d";
+              wanMac = "88:ae:dd:64:e1:55";
+              lanIp = "192.168.105.1/24";
+              wgIpv4 = "10.64.9.184/32";
+              wgIpv6 = "fc00:bbbb:bbbb:bb01::a40:9b8/128";
+            })
+            ./app-team-ios-lab.nix
+            {
+              boot.loader.systemd-boot.enable = true;
+              boot.loader.efi.canTouchEfiVariables = true;
+              hardware = {
+                cpu.intel.updateMicrocode = true;
+                enableRedistributableFirmware = true;
+              };
+            }
+          ];
+        };
+
+        nixosConfigurations.app-team-android-lab = nixpkgs.lib.nixosSystem {
+          modules = [
+            (import ./router-config.nix {
+              hostname = "app-team-android-tests";
+              lanMac = "00:24:9b:8d:1e:f3";
+              wanMac = "1c:69:7a:ad:73:a6";
+              lanIp = "192.168.105.1/24";
+              wgIpv4 = "10.64.17.146/32";
+              wgIpv6 = "fc00:bbbb:bbbb:bb01::a40:1192/128";
+            })
+            ./app-team-android-lab.nix
+            {
+              boot.loader.systemd-boot.enable = true;
+              boot.loader.efi.canTouchEfiVariables = true;
+              hardware = {
+                cpu.intel.updateMicrocode = true;
+                enableRedistributableFirmware = true;
+              };
+            }
+          ];
+        };
+
+        nixosConfigurations.app-team-ios-lab-iso = nixpkgs.lib.nixosSystem {
+          modules = [
+            (import ./router-config.nix {
+              hostname = "app-team-ios-tests";
+              lanMac = "48:21:0b:36:bb:52";
+              wanMac = "48:21:0b:36:43:a3";
+              lanIp = "192.168.105.1/24";
+              wgIpv4 = "10.64.9.184/32";
+              wgIpv6 = "fc00:bbbb:bbbb:bb01::a40:9b8/128";
+            })
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+            {
+              isoImage.squashfsCompression = "lz4";
+            }
+          ];
+        };
+
       };
 
-      nixosConfigurations.app-team-android-lab = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          (import ./router-config.nix {
-            hostname = "app-team-android-tests";
-            lanMac = "00:24:9b:8d:1e:f3";
-            wanMac = "1c:69:7a:ad:73:a6";
-            lanIp = "192.168.105.1/24";
-            wgIpv4 = "10.64.17.146/32";
-            wgIpv6 = "fc00:bbbb:bbbb:bb01::a40:1192/128";
-          })
-          ./app-team-android-lab.nix
-          {
-            boot.loader.systemd-boot.enable = true;
-            boot.loader.efi.canTouchEfiVariables = true;
-            hardware = {
-              cpu.intel.updateMicrocode = true;
-              enableRedistributableFirmware = true;
-            };
-          }
-        ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { config, pkgs, ... }: {
+        packages.raas =
+          with import nixpkgs { system = "x86_64-linux"; };
+          pkgs.callPackage ./raas.nix { };
       };
 
-      nixosConfigurations.app-team-ios-lab-iso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          (import ./router-config.nix {
-            hostname = "app-team-ios-tests";
-            lanMac = "48:21:0b:36:bb:52";
-            wanMac = "48:21:0b:36:43:a3";
-            lanIp = "192.168.105.1/24";
-            wgIpv4 = "10.64.9.184/32";
-            wgIpv6 = "fc00:bbbb:bbbb:bb01::a40:9b8/128";
-          })
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-          {
-            isoImage.squashfsCompression = "lz4";
-          }
-        ];
-      };
-
-      packages.x86_64-linux.raas =
-        with import nixpkgs { system = "x86_64-linux"; };
-        pkgs.callPackage ./raas.nix { };
-
-      packages.aarch64-darwin.raas =
-        with import nixpkgs { system = "aarch64-darwin"; };
-        pkgs.callPackage ./raas.nix { };
-
-      packages.x86_64-darwin.raas =
-        with import nixpkgs { system = "x86_64-darwin"; };
-        pkgs.callPackage ./raas.nix { };
-    };
+    });
 }
