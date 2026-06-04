d21de4d7a5388764fde437d11553bd9e2b76a5d5
Markus Pettersson <markus.pettersson@mullvad.net>
2026-06-04 11:53:39 +0200

    Make `installer-downloader` respect `RUST_LOG`

--- installer-downloader/src/log.rs
+++ installer-downloader/src/log.rs
@@ -1,4 +1,4 @@
-use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 const LOG_FILENAME: &str = "mullvad-loader.log";
 const DATE_TIME_FORMAT_STR: &str = "[%Y-%m-%d %H:%M:%S%.3f]";
@@ -11,8 +11,10 @@ pub fn init() {
     let (non_blocking_file_appender, _file_appender_guard) =
         tracing_appender::non_blocking(file_appender);
 
+    let log_level = EnvFilter::from_default_env().add_directive(LevelFilter::DEBUG.into());
+
     tracing_subscriber::fmt()
-        .with_max_level(LevelFilter::DEBUG)
+        .with_env_filter(log_level)
         .with_writer(non_blocking_file_appender)
         .with_timer(tracing_subscriber::fmt::time::ChronoUtc::new(
             DATE_TIME_FORMAT_STR.to_string(),
