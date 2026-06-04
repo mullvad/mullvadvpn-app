df34ffe7a7f4132320aee004e70bd09102b490d3
Markus Pettersson <markus.pettersson@mullvad.net>
2026-06-04 13:11:01 +0200

    Fix logs not flusing properly in `installer-downloader`

--- installer-downloader/src/log.rs
+++ installer-downloader/src/log.rs
@@ -3,12 +3,11 @@ use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 const LOG_FILENAME: &str = "mullvad-loader.log";
 const DATE_TIME_FORMAT_STR: &str = "[%Y-%m-%d %H:%M:%S%.3f]";
 
-pub fn init() {
-    let Ok(log_dir) = mullvad_paths::frontend_log_dir() else {
-        return;
-    };
+pub fn init() -> Option<tracing_appender::non_blocking::WorkerGuard> {
+    let log_dir = mullvad_paths::frontend_log_dir().ok()?;
+
     let file_appender = tracing_appender::rolling::never(log_dir, LOG_FILENAME);
-    let (non_blocking_file_appender, _file_appender_guard) =
+    let (non_blocking_file_appender, file_appender_guard) =
         tracing_appender::non_blocking(file_appender);
 
     let log_level = EnvFilter::from_default_env().add_directive(LevelFilter::DEBUG.into());
@@ -19,5 +18,7 @@ pub fn init() {
         .with_timer(tracing_subscriber::fmt::time::ChronoUtc::new(
             DATE_TIME_FORMAT_STR.to_string(),
         ))
-        .init()
+        .init();
+
+    Some(file_appender_guard)
 }
