5b0388a17be3efe72f3358652a5d93648f2a6ee2
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-06-05 15:02:24 +0200

    Use Relay selector filtered locations as base for available locations

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/country-locations/hooks/use-relay-count.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/country-locations/hooks/use-relay-count.ts
@@ -1,9 +1,9 @@
-import { useRelayLocations } from '../../../../../../features/locations/hooks';
 import { useSelectLocationViewContext } from '../../../SelectLocationViewContext';
+import { useRelayLocationsFilteredByLocationType } from './use-relay-locations-filtered-by-location-type';
 
 export function useRelayCount() {
   const { countryLocations } = useSelectLocationViewContext();
-  const { relayLocations } = useRelayLocations();
+  const relayLocationsFiltered = useRelayLocationsFilteredByLocationType();
 
   const visibleRelays = countryLocations.reduce(
     (countryAcc, country) =>
@@ -11,11 +11,10 @@ export function useRelayCount() {
     0,
   );
 
-  const totalRelays = relayLocations.reduce(
-    (countryAcc, country) =>
-      countryAcc + country.cities.reduce((cityAcc, city) => cityAcc + city.relays.length, 0),
-    0,
-  );
+  const matchedRelays = relayLocationsFiltered.matches.length;
+  const discardedRelays = relayLocationsFiltered.discards.length;
+
+  const totalRelays = matchedRelays + discardedRelays;
 
   return { visibleRelays, totalRelays };
 }
