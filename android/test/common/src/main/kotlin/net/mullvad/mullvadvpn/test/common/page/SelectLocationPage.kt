d8993f65f92ae20e64c54f27595beb2cc94ecccf
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-06-05 13:28:07 +0200

    Fix scroll to for server override

--- android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/SelectLocationPage.kt
+++ android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/SelectLocationPage.kt
@@ -2,6 +2,7 @@ package net.mullvad.mullvadvpn.test.common.page
 
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.Direction
+import androidx.test.uiautomator.UiObjectNotFoundException
 import androidx.test.uiautomator.Until
 import androidx.test.uiautomator.waitForStableInActiveWindow
 import net.mullvad.mullvadvpn.lib.ui.tag.EXPAND_BUTTON_TEST_TAG
@@ -44,6 +45,11 @@ class SelectLocationPage internal constructor() : Page() {
         scrollView2.scrollUntil(direction, Until.hasObject(By.text(text)))
     }
 
+    fun scrollPercentage(direction: Direction, percentage: Float) {
+        val scrollView2 = uiDevice.findObjectWithTimeout(By.res(SELECT_LOCATION_LIST_TEST_TAG))
+        scrollView2.scroll(direction, percentage)
+    }
+
     fun clickMenuButton() {
         uiDevice.findObjectWithTimeout(By.res(SELECT_LOCATION_MENU_BUTTON_TEST_TAG)).click()
     }
@@ -59,10 +65,18 @@ class SelectLocationPage internal constructor() : Page() {
     fun expandAndClickRelay(testRelay: TestRelay) {
         clickLocationExpandButton(testRelay.country)
         uiDevice.waitForStableInActiveWindow()
-        scrollUntilCell(testRelay.city)
-        clickLocationExpandButton(testRelay.city)
+        scrollUntilText(testRelay.city, Direction.DOWN)
+        // Due to the fab obstructing the expand button we can sometimes scroll until the city is
+        // visible but the expand button for the city is not. In that case we will scroll a bit more
+        // and try again.
+        try {
+            clickLocationExpandButton(testRelay.city)
+        } catch (_: UiObjectNotFoundException) {
+            scrollPercentage(Direction.DOWN, 0.1f)
+            clickLocationExpandButton(testRelay.city)
+        }
         uiDevice.waitForStableInActiveWindow()
-        scrollUntilCell(testRelay.relay)
+        scrollUntilText(testRelay.relay, Direction.DOWN)
         clickLocationCell(testRelay.relay)
     }
 
