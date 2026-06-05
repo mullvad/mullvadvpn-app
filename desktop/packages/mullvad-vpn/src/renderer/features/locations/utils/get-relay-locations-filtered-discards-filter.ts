3f0d0ccb8238db72b93f862c97fc629a9da2b6a7
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-06-05 15:51:40 +0200

    Add utils to filtering locations from Relay selector filtered locations

---
+++ desktop/packages/mullvad-vpn/src/renderer/features/locations/utils/get-relay-locations-filtered-discards-filter.ts
@@ -0,0 +1,38 @@
+import { RelaySelectorRelayDiscard } from '../../../../shared/relay-selector-rpc-types';
+import { IRelayLocationRelayRedux } from '../../../redux/settings/reducers';
+
+export function getRelayLocationsFilteredDiscardsFilter(
+  relayDiscards: RelaySelectorRelayDiscard[],
+  multihop: boolean,
+) {
+  return (relay: IRelayLocationRelayRedux) =>
+    relayDiscards.some((discardedRelay) => {
+      if (discardedRelay.relay.hostname === relay.hostname) {
+        // Check if a discarded relay should be included in the filtered result
+        const result = Object.entries(discardedRelay.why).every(([key, value]) => {
+          // Always include inactive servers
+          if (key === 'inactive') {
+            return true;
+          }
+
+          // If multihop is enabled we want to be able to present the conflicting server in the
+          // relay list.
+          if (key === 'conflictWithOtherHop' && multihop) {
+            return true;
+          }
+
+          // If a relay was discarded by the Relay selector, and the reasons for discarding the
+          // relay wasn't already handled by the special cases handled above, then it should be
+          // filtered out by us as well.
+          //
+          // If the `value` is true it means that the relay was discarded by the Relay selector,
+          // and as such it will be filtered out here too.
+          return !value;
+        });
+
+        return result;
+      }
+
+      return false;
+    });
+}
