baaf154d094dcea55a74494b29fbfb942ede0f87
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-06-08 16:57:19 +0200

    Add check to prepare release script for release note length

--- android/scripts/prepare-release.sh
+++ android/scripts/prepare-release.sh
@@ -31,11 +31,24 @@ if [[ -n "$(git status --porcelain)" ]]; then
     exit 1
 fi
 
+release_notes=$(<"android/src/main/play/release-notes/en-US/default.txt")
 if [[ $PRODUCT_VERSION != *"alpha"* &&
     $(grep "^## \\[android/$PRODUCT_VERSION\\] - " android/CHANGELOG.md) == "" ]]; then
 
     echo "It looks like you did not add $PRODUCT_VERSION to the changelog?"
     echo "Please make sure the changelog is up to date and correct before you proceed."
+    exit 1
+
+    if [[ -z $release_notes ]]; then
+        echo "The release notes file is empty!"
+        echo "Beta and Stable require a release notes file."
+        exit 1
+    fi
+fi
+
+if [[ [[ "${#release_notes}" -gt 500 ]]; then
+    echo "The number of characters in the relase notes may not exceed 500"
+    echo "Current number of charachers ${#release_notes}"
     exit 1
 fi
 
