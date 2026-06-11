196d9d8cb6d4a527b3abf4d392f0e40602f92a8c
Markus Pettersson <markus.pettersson@mullvad.net>
2026-06-11 13:34:34 +0200

    Test scenario 3A

--- mullvad-daemon/src/migrations/multihop/mod.rs
+++ mullvad-daemon/src/migrations/multihop/mod.rs
@@ -107,6 +107,28 @@ mod test {
         Ok(())
     }
 
+    /// Scenario 3A.
+    /// # Expected outcome
+    /// Multihop: Never
+    /// Filters: Copied to entry.
+    #[test]
+    fn scenario_3a() -> anyhow::Result<()> {
+        let settings = SettingsBuilder::new()
+            .multihop(false)
+            .daita(true)
+            .direct_only(false)
+            .magic_multihop(false)
+            .filters(true)
+            .build();
+        let scenario = migration::detect(&settings);
+        assert_eq!(scenario, Scenario::ThreeA);
+        let mut settings = json!(settings);
+        insta::assert_snapshot!(serde_json::to_string_pretty(&settings)?);
+        migration::migrate(&mut settings, scenario);
+        insta::assert_snapshot!(serde_json::to_string_pretty(&settings)?);
+        Ok(())
+    }
+
     /// Scenario 3B.
     /// # Expected outcome
     /// Multihop: Always
