bc3dffe9697f8204ed5c9240ede7cdd5077ebd3e
Markus Pettersson <markus.pettersson@mullvad.net>
2026-06-16 13:22:16 +0200

    Run relay selector in migration
    Make relay selector not depend on custom lists. Move `RETRY_ORDER` to
    daemon crate.
    
    Co-authored-by: Sebastian Holmin <sebastian.holmin@mullvad.net>

--- mullvad-daemon/src/migrations/multihop/settings/mod.rs
+++ mullvad-daemon/src/migrations/multihop/settings/mod.rs
@@ -1,6 +1,6 @@
 //! Common types for pre- and post migration.
 
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashSet};
 
 use serde::{Deserialize, Serialize};
 
@@ -18,9 +18,8 @@ pub enum Constraint<T> {
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum LocationConstraint {
-    Location(Value),
-    /// If the entry location is a custom list, just re-use it.
-    CustomList(Value),
+    Location(GeographicLocationConstraint),
+    CustomList { list_id: String },
 }
 
 impl Default for LocationConstraint {
@@ -29,7 +28,22 @@ impl Default for LocationConstraint {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+impl From<LocationConstraint> for mullvad_types::relay_constraints::LocationConstraint {
+    fn from(constraint: LocationConstraint) -> Self {
+        use mullvad_types::relay_constraints::LocationConstraint::*;
+        match constraint {
+            LocationConstraint::Location(geographic_location_constraint) => {
+                Location(geographic_location_constraint.into())
+            }
+            LocationConstraint::CustomList { list_id } => CustomList {
+                list_id: list_id.parse().expect("TODO: Do not unwrap"),
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
 pub enum GeographicLocationConstraint {
     /// A country is represented by its two letter country code.
     Country(String),
@@ -45,15 +59,62 @@ impl Default for GeographicLocationConstraint {
     }
 }
 
+impl From<GeographicLocationConstraint>
+    for mullvad_types::relay_constraints::GeographicLocationConstraint
+{
+    fn from(constraint: GeographicLocationConstraint) -> Self {
+        use mullvad_types::relay_constraints::GeographicLocationConstraint::*;
+        match constraint {
+            GeographicLocationConstraint::Country(country) => Country(country),
+            GeographicLocationConstraint::City(country, city) => City(country, city),
+            GeographicLocationConstraint::Hostname(country, city, hostname) => {
+                Hostname(country, city, hostname)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct CustomListsSettings {
+    custom_lists: Vec<CustomList>,
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct CustomList {
+    id: String,
+    pub name: String,
+    pub locations: BTreeSet<GeographicLocationConstraint>,
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Ownership {
     MullvadOwned,
     Rented,
 }
 
+impl From<Ownership> for mullvad_types::relay_constraints::Ownership {
+    fn from(value: Ownership) -> Self {
+        match value {
+            Ownership::MullvadOwned => Self::MullvadOwned,
+            Ownership::Rented => Self::Rented,
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Providers {
     providers: HashSet<String>,
+}
+
+impl TryFrom<Providers> for mullvad_types::relay_constraints::Providers {
+    type Error = ();
+
+    fn try_from(value: Providers) -> Result<Self, Self::Error> {
+        match Self::new(value.providers) {
+            Ok(providers) => Ok(providers),
+            Err(_no_providers) => Err(()),
+        }
+    }
 }
 
 #[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Deserialize, Serialize)]
