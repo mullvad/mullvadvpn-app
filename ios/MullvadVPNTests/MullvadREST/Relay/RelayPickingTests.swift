ddcf3d49a30dfcd952ecf6dfd658d72356c1d3bd
Andrew Bulhak <andrew.bulhak@mullvad.net>
2026-06-17 17:25:35 +0200

    Update tests

--- ios/MullvadVPNTests/MullvadREST/Relay/RelayPickingTests.swift
+++ ios/MullvadVPNTests/MullvadREST/Relay/RelayPickingTests.swift
@@ -46,7 +46,7 @@ class RelayPickingTests: XCTestCase {
             exitLocations: .only(UserSelectedRelays(locations: [.hostname("se", "got", "se10-wireguard")]))
         )
 
-        var settings = LatestTunnelSettings()
+        var settings = LatestTunnelSettings(tunnelMultihopState: .always)
         settings.relayConstraints = constraints
 
         let picker = MultihopPicker(
@@ -67,7 +67,8 @@ class RelayPickingTests: XCTestCase {
             exitLocations: .only(UserSelectedRelays(locations: [.hostname("se", "got", "se10-wireguard")]))
         )
 
-        var settings = LatestTunnelSettings()
+        var settings = LatestTunnelSettings(tunnelMultihopState: .never)
+
         settings.relayConstraints = constraints
 
         let picker = MultihopPicker(
