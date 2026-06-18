97a3c5d197076034e9e4700da208a987ef97b716
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-06-18 15:49:39 +0200

    Fix android compilation errors

--- android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/mapper/FromDomain.kt
+++ android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/mapper/FromDomain.kt
@@ -336,10 +336,7 @@ internal fun ExitConstraints.fromDomain(): RelaySelector.ExitConstraints =
         .build()
 
 internal fun DaitaSettings.fromDomain(): ManagementInterface.DaitaSettings =
-    ManagementInterface.DaitaSettings.newBuilder()
-        .setEnabled(enabled)
-        .setDirectOnly(directOnly)
-        .build()
+    ManagementInterface.DaitaSettings.newBuilder().setEnabled(enabled).build()
 
 internal fun Constraint<Providers>.fromDomain1(): List<RelaySelector.Provider> =
     when (this) {
