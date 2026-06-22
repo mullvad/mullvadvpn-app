170218ca10ca6874bd90e4e074f261cf83a75b98
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-06-22 17:53:19 +0200

    Revert "Merge branch 'yank-domain-fronting'"
    This reverts commit f23169ce6473f6922424343cb6fe29bc8faf9263, reversing
    changes made to c04a90ab368ad962134e8265f745d53ac43e648f.

--- ios/MullvadTypes/PersistentProxyConfiguration.swift
+++ ios/MullvadTypes/PersistentProxyConfiguration.swift
@@ -19,6 +19,9 @@ public enum PersistentProxyConfiguration: Codable, Equatable, Sendable, CustomDe
     /// Communication over proxy address from a DNS.
     case encryptedDNS
 
+    /// Communication via domain fronting.
+    case domainFronting
+
     /// Communication over shadowsocks.
     case shadowsocks(ShadowsocksConfiguration)
 
@@ -33,6 +36,8 @@ public enum PersistentProxyConfiguration: Codable, Equatable, Sendable, CustomDe
             return "Bridges"
         case .encryptedDNS:
             return "Encrypted DNS"
+        case .domainFronting:
+            return "Domain fronting"
         case .shadowsocks(let config):
             return "Shadowsocks: \(config)"
         case .socks5(let config):
