d2609935ac0e198ad019c391f4bc502005ae217c
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-06-25 16:16:14 +0200

    Update type for `disabledLocation` to be the same for all hooks

--- desktop/packages/mullvad-vpn/src/renderer/features/locations/utils/is-relay-disabled.ts
+++ desktop/packages/mullvad-vpn/src/renderer/features/locations/utils/is-relay-disabled.ts
@@ -1,19 +1,19 @@
-import {
-  compareRelayLocation,
-  type RelayLocation,
-  type RelayLocationRelay,
-} from '../../../../shared/daemon-rpc-types';
+import { compareRelayLocation, type RelayLocationRelay } from '../../../../shared/daemon-rpc-types';
 import type { IRelayLocationRelayRedux } from '../../../redux/settings/reducers';
+import { useDisabledLocation } from '../hooks';
 import { DisabledReason } from '../types';
 
 export function isRelayDisabled(
   relay: IRelayLocationRelayRedux,
   location: RelayLocationRelay,
-  disabledLocation?: { location: RelayLocation; reason: DisabledReason },
+  disabledLocation?: ReturnType<typeof useDisabledLocation>,
 ): DisabledReason | undefined {
   if (!relay.active) {
     return DisabledReason.inactive;
-  } else if (disabledLocation && compareRelayLocation(location, disabledLocation.location)) {
+  } else if (
+    disabledLocation?.location &&
+    compareRelayLocation(location, disabledLocation.location)
+  ) {
     return disabledLocation.reason;
   } else {
     return undefined;
