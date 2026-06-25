70305a51b98fd6d843b608c52e44f1b9a9919b29
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-06-25 16:09:23 +0200

    Update `Icon` component to add a "placeholder" component
    This component serves the use case where we want to occupy the same
    dimensions as if we were to render an icon, but we don't want to render
    an actual icon.
    
    Can be used to ensure that a sibling component preserves its offset even
    when an icon should not be shown.

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/icon/Icon.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/icon/Icon.tsx
@@ -4,11 +4,21 @@ import { Colors, colors } from '../../foundations';
 import { icons } from './types';
 
 export type IconProps = {
-  icon: keyof typeof icons;
+  icon: keyof typeof icons | 'placeholder';
   size?: 'tiny' | 'small' | 'medium' | 'large' | 'big';
   color?: Colors;
   className?: string;
 } & React.HTMLAttributes<HTMLDivElement>;
+
+export const StyledPlaceholder = styled.div<{ $size: number }>`
+  ${({ $size }) => {
+    return css`
+      flex-shrink: 0;
+      width: ${$size}px;
+      height: ${$size}px;
+    `;
+  }}
+`;
 
 export const StyledIcon = styled.div<{ $color: string; $size: number; $src: string }>`
   ${({ $size, $src, $color }) => {
@@ -33,13 +43,19 @@ export const iconSizes = {
 
 export const Icon = ({
   icon: iconProp,
-  size = 'medium',
+  size: sizeProp = 'medium',
   color: colorProp = 'white',
   ...props
 }: IconProps) => {
+  const size = iconSizes[sizeProp];
+
+  if (iconProp === 'placeholder') {
+    return <StyledPlaceholder $size={size} />;
+  }
+
   const icon = icons[iconProp];
   const src = iconProp.startsWith('data:') ? iconProp : `assets/icons/${icon}.svg`;
 
   const color = colors[colorProp];
-  return <StyledIcon $src={src} $size={iconSizes[size]} $color={color} role="img" {...props} />;
+  return <StyledIcon $src={src} $size={size} $color={color} role="img" {...props} />;
 };
