11356d21a84055079af1c0e86ad5a968d1193d57
Oliver <oliver@mohlin.dev>
2026-06-26 11:49:18 +0200

    Add SelectLocationSelector component

---
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/select-location-selector/components/select-location-selector-item/hooks/index.ts
@@ -0,0 +1,7 @@
+export * from './use-focus-first-focusable-heading';
+export * from './use-handle-clear-button-click';
+export * from './use-handle-clear-button-click';
+export * from './use-handle-focus-exit';
+export * from './use-handle-input-key-down';
+export * from './use-handle-reset';
+export * from './use-handle-value-change';
