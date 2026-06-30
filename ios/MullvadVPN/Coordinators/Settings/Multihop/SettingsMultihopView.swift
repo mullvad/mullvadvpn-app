d3ad02994f520e2887875eb5708534c0ae8f495c
Emīls <emils@mullvad.net>
2026-06-30 15:14:03 +0200

    Update when needed copy

--- ios/MullvadVPN/Coordinators/Settings/Multihop/SettingsMultihopView.swift
+++ ios/MullvadVPN/Coordinators/Settings/Multihop/SettingsMultihopView.swift
@@ -155,11 +155,9 @@ extension SettingsMultihopView {
             VStack(alignment: .leading) {
                 Text("When needed")
                     .fontWeight(.bold)
-                Text(
-                    "To ensure your current settings work with your selected location, and to "
-                        + "avoid blocking your connection, the app might automatically multihop via "
-                        + "a different entry server."
-                )
+                Text("Not all our locations/servers support every feature in the app. "
+                     + "If your selected location/server doesn't support the features you've enabled, "
+                     + "the app will automatically multihop via a compatible server.")
                 Text(
                     "This will be indicated by the \(UIImage.Multihop.whenNeeded.scaledIcon(fromBaseSize: 14, to: .subheadline, offset: .init(x: 0, y: 2))) symbol"
                 )
@@ -203,11 +201,11 @@ extension SettingsMultihopView {
     private struct WhenNeededAlert: View {
         var body: some View {
             VStack(alignment: .leading, spacing: 16) {
-                Text(
-                    "To ensure your current settings work with your selected location, and to "
-                        + "avoid blocking your connection, the app might automatically multihop via "
-                        + "a different entry server."
+                Text("Not all our locations/servers support every feature in the app. If your selected"
+                     + " location/server doesn't support the features you've enabled, the app will"
+                     + "automatically multihop via a compatible server."
                 )
+                Text("This ensures your connection does not get blocked due to incompatible settings.")
                 Text(
                     "This will be indicated by the \(UIImage.Multihop.whenNeeded.scaledIcon(fromBaseSize: 15, to: .body, offset: .init(x: 0, y: 2))) symbol."
                 )
