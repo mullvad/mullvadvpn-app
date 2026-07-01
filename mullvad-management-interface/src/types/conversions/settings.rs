88d3e8580ff2e53324f57b4ca3993170c4543110
Kalle Lindström <karl.lindstrom@mullvad.net>
2026-07-01 09:12:34 +0200

    Add automatic entry mulithop recent

--- mullvad-management-interface/src/types/conversions/settings.rs
+++ mullvad-management-interface/src/types/conversions/settings.rs
@@ -324,6 +324,9 @@ impl From<mullvad_types::settings::Recent> for proto::Recent {
                     exit: Some(exit.into()),
                 })),
             },
+            mullvad_types::settings::Recent::AutomaticEntryMultihop(exit) => Self {
+                r#type: Some(proto::recent::Type::AutomaticEntryMultihop(exit.into())),
+            },
         }
     }
 }
