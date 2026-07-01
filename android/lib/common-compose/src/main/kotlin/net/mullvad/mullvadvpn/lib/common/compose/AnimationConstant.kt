391e40534c65bc4adb15a33aa55e1b8d11ab6439
David Göransson <david.goransson@mullvad.net>
2026-07-01 23:11:59 +0200

    Fix secure zoom

--- android/lib/common-compose/src/main/kotlin/net/mullvad/mullvadvpn/lib/common/compose/AnimationConstant.kt
+++ android/lib/common-compose/src/main/kotlin/net/mullvad/mullvadvpn/lib/common/compose/AnimationConstant.kt
@@ -6,8 +6,8 @@ import net.mullvad.mullvadvpn.lib.model.Longitude
 
 const val MINIMUM_LOADING_TIME_MILLIS = 500L
 
-const val SECURE_ZOOM = 1.15f
-const val UNSECURE_ZOOM = 1.20f
+const val CONNECTED_ZOOM = 0f
+const val DEFAULT_ZOOM = .05f
 const val SECURE_ZOOM_ANIMATION_MILLIS = 2000
 
 const val SETTINGS_HIGHLIGHT_REPEAT_COUNT = 3
