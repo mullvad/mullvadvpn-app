d1fdfac50b2481802128c16981179f0cee407506
Andrew Bulhak <andrew.bulhak@mullvad.net>
2026-07-06 19:29:23 +0200

    Adjust spacing and screen layout for location filter pills

--- ios/MullvadVPN/View controllers/SelectLocation/Views/ActiveFilterView.swift
+++ ios/MullvadVPN/View controllers/SelectLocation/Views/ActiveFilterView.swift
@@ -15,55 +15,59 @@ struct ActiveFilterView: View {
             }
     }
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(sortedFilters, id: \.hashValue) { filter in
-                    Button {
-                        onSelect(filter)
-                    } label: {
-                        HStack {
-                            Text(filter.labelText(style: labelStyle))
-                                .foregroundStyle(
-                                    automaticLocationIsActive && filter.isOverriddenByAutomaticLocation
-                                        ? Color.MullvadText.disabled
-                                        : Color.mullvadTextPrimary
-                                )
-                            if filter.isRemovable {
-                                Button {
-                                    onRemove(filter)
-                                } label: {
-                                    Image(systemName: "xmark")
-                                        .tint(Color.mullvadTextPrimary)
+        VStack {
+            ScrollView(.horizontal) {
+                if !activeFilter.isEmpty {
+                    HStack {
+                        ForEach(sortedFilters, id: \.hashValue) { filter in
+                            Button {
+                                onSelect(filter)
+                            } label: {
+                                HStack {
+                                    Text(filter.labelText(style: labelStyle))
+                                        .foregroundStyle(
+                                            automaticLocationIsActive && filter.isOverriddenByAutomaticLocation
+                                            ? Color.MullvadText.disabled
+                                            : Color.mullvadTextPrimary
+                                        )
+                                    if filter.isRemovable {
+                                        Button {
+                                            onRemove(filter)
+                                        } label: {
+                                            Image(systemName: "xmark")
+                                                .tint(Color.mullvadTextPrimary)
+                                        }
+                                        .accessibilityIdentifier(.relayFilterChipCloseButton)
+                                    }
                                 }
-                                .accessibilityIdentifier(.relayFilterChipCloseButton)
+                                .font(.mullvadMiniSemiBold)
+                                .padding(8)
+                                .background {
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .foregroundStyle(Color.MullvadButton.primary)
+                                }
                             }
-                        }
-                        .font(.mullvadMiniSemiBold)
-                        .padding(8)
-                        .background {
-                            RoundedRectangle(cornerRadius: 8)
-                                .foregroundStyle(Color.MullvadButton.primary)
+                            .accessibilityIdentifier(filter.accessibilityIdentifier)
                         }
                     }
-                    .accessibilityIdentifier(filter.accessibilityIdentifier)
+                    .padding(.horizontal)
                 }
             }
-            .padding(.horizontal)
-        }
-        .scrollIndicators(.never)
-
-        if automaticLocationIsActive && activeFilter.contains(where: { $0.isOverriddenByAutomaticLocation }) {
-            HStack(alignment: .center, spacing: 8) {
-                UIImage.Buttons.info
-                    .scaledIcon(fromBaseSize: 14, to: .footnote)
-                    .foregroundStyle(Color.white)
-                Text("Filters are overridden when using an automatic location")
-                    .font(.mullvadMini)
-                    .foregroundStyle(Color.MullvadText.onBackground)
-                Spacer()
+            .scrollIndicators(.never)
+            
+            if automaticLocationIsActive && activeFilter.contains(where: { $0.isOverriddenByAutomaticLocation }) {
+                HStack(alignment: .center, spacing: 8) {
+                    UIImage.Buttons.info
+                        .scaledIcon(fromBaseSize: 14, to: .footnote)
+                        .foregroundStyle(Color.white)
+                    Text("Filters are overridden when using an automatic location")
+                        .font(.mullvadMini)
+                        .foregroundStyle(Color.MullvadText.onBackground)
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.top, -4)
             }
-            .padding(.horizontal)
-            .padding(.top, -4)
         }
     }
 }
