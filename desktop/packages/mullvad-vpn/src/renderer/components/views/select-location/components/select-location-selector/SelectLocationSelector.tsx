ec88a60924fb08c2080a9fcf6ebfe1f474efdb8e
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-07-07 16:45:25 +0200

    fixup! Add SelectLocationSelector component

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/select-location-selector/SelectLocationSelector.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/select-location-selector/SelectLocationSelector.tsx
@@ -3,34 +3,48 @@ import { LocationType } from '../../../../../features/locations/types';
 import { useMultihop } from '../../../../../features/multihop/hooks';
 import { LocationSelector } from '../../../../../lib/components/location-selector';
 import { useSelectLocationViewContext } from '../../SelectLocationViewContext';
+import { SelectLocationSelectorEntryItem, SelectLocationSelectorExitItem } from './components';
+import { useHandleSelectedItemChange, useIsLocationSelectorExpanded } from './hooks';
 import {
-  useHandleSelectedItemChange,
-  useIsLocationSelectorExpanded,
-  useLocationSelectorItems,
-} from './hooks';
-import { SelectLocationSelectorProvider } from './SelectLocationSelectorContext';
+  SelectLocationSelectorProvider,
+  useSelectLocationSelectorContext,
+} from './SelectLocationSelectorContext';
 
 function SelectLocationSelectorImpl() {
   const { multihop } = useMultihop();
   const { locationType } = useSelectLocationViewContext();
+  const { isolatedItem } = useSelectLocationSelectorContext();
 
-  const items = useLocationSelectorItems();
   const expanded = useIsLocationSelectorExpanded();
   const handleSelectedItemChange = useHandleSelectedItemChange();
 
   const selectedItem = locationType === LocationType.entry ? 'entry' : 'exit';
+
+  const showSelectLocationSelectorEntryItem = multihop !== 'never' && isolatedItem !== 'exit';
+  const showSelectLocationSelectorExitItem = isolatedItem !== 'entry';
 
   return (
     <LocationSelector
       selectedItem={selectedItem}
       onSelectedItemChange={handleSelectedItemChange}
       expanded={expanded}
-      variant={multihop ? 'secondary' : 'primary'}>
+      variant={multihop === 'never' ? 'primary' : 'secondary'}>
       <LocationSelector.Row position="top">
         <LocationSelector.Row.Icon icon="device" />
         <LocationSelector.Row.Label>{messages.gettext('Your device')}</LocationSelector.Row.Label>
       </LocationSelector.Row>
-      <LocationSelector.Items>{items}</LocationSelector.Items>
+      <LocationSelector.Items>
+        {/* NOTE: The components must have a `key` assigned as the `LocationSelector.Items`
+         * component uses `motion` components under the hood, which requires all children
+         * to use keys.
+         */}
+        {showSelectLocationSelectorEntryItem ? (
+          <SelectLocationSelectorEntryItem key="entry" type="entry" />
+        ) : null}
+        {showSelectLocationSelectorExitItem ? (
+          <SelectLocationSelectorExitItem key="exit" type="exit" />
+        ) : null}
+      </LocationSelector.Items>
       <LocationSelector.Row position="bottom">
         <LocationSelector.Row.Icon icon="internet" />
         <LocationSelector.Row.Label>{messages.gettext('Internet')}</LocationSelector.Row.Label>
--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/select-location-selector/hooks/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/select-location-selector/hooks/index.ts
@@ -1,4 +1,3 @@
 export * from './use-handle-selected-item-change';
 export * from './use-location-name';
 export * from './use-is-location-selector-expanded';
-export * from './use-location-selector-items';
