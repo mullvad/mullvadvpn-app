db2a28487f1cc986727bdb67ec3ddab080d7d881
Emīls <emils@mullvad.net>
2026-07-08 21:18:21 +0200

    Update scripts

--- ci/ios/test-router/raas/poc/post_up.sh
+++ ci/ios/test-router/raas/poc/post_up.sh
@@ -3,6 +3,9 @@
 CLIENT_IP="$1"
 IFACE="$2"
 
+echo client IP - "$CLIENT_IP"
+echo interface - "$IFACE"
+
 echo "Enabling port forwarding, will prompt for sudo password"
 sysctl -w net.inet.ip.forwarding=1
 
@@ -12,8 +15,9 @@ CIDR_NET="${WIRELESS_IP%.*}.0/24"
 
 echo "Applying forwarding rules for $IFACE, routing $CLIENT_IP via $CIDR_NET"
 pfctl -F all
-sed "s/utun[0-9][0-9]*/$IFACE/g" pf.utun4.conf \
+sed "s/utun[0-9][0-9]*/$IFACE/g" ./poc/pf.utun4.conf \
   | sed "s|192.168.91.0/24|${CIDR_NET}|" \
   | sed "s|192.168.91.84|${CLIENT_IP}|" \
-  | sudo pfctl -f -
+  | tee ./pf-rules \
+  | pfctl -f -
 pfctl -e || true
