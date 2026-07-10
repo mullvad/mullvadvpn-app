6c5b03334192aec17d1e9268d9e4e9a12b03ac11
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-07-10 17:03:34 +0200

    Improve handling of selection an entry location
    - Handle when selecting a location from a search
    - Add transitions to make the animations more fluent

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-lists/hooks/use-handle-select-entry-location.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-lists/hooks/use-handle-select-entry-location.ts
@@ -2,20 +2,51 @@ import React from 'react';
 
 import { useRelayLocations } from '../../../../../../features/locations/hooks';
 import { type AnyLocation, LocationType } from '../../../../../../features/locations/types';
+import { waitForAnimations } from '../../../../../../lib/utils';
 import { useSelectLocationViewContext } from '../../../SelectLocationViewContext';
 
 export function useHandleSelectEntryLocation() {
-  const { setLocationType, searchTerm } = useSelectLocationViewContext();
+  const { viewRef, setLocationType, searchTerm, setSearchTerm, setIsolatedItem } =
+    useSelectLocationViewContext();
   const { selectEntryRelayLocation } = useRelayLocations();
 
   const handleSelectEntryLocation = React.useCallback(
-    async (entryLocation: AnyLocation) => {
+    (entryLocation: AnyLocation) => {
       if (!searchTerm) {
-        setLocationType(LocationType.exit);
+        React.startTransition(async () => {
+          setLocationType(LocationType.exit);
+          await waitForAnimations(viewRef.current);
+
+          React.startTransition(async () => {
+            await selectEntryRelayLocation(entryLocation.details);
+          });
+        });
+      } else {
+        // If the user selects a location from a search, we can't immediately switch
+        // to show the `exit` locations as the view contents would jump around
+        // too much and confuse the user. To avoid this we just update the selected
+        // entry, which will cause an animation to mark it in the location lists
+        // as selected, and when that animation has finished we can switch to show
+        // `exit` locations and then we can expand the .
+        React.startTransition(async () => {
+          await selectEntryRelayLocation(entryLocation.details);
+          setSearchTerm('');
+
+          React.startTransition(() => {
+            setLocationType(LocationType.exit);
+            setIsolatedItem(undefined);
+          });
+        });
       }
-      await selectEntryRelayLocation(entryLocation.details);
     },
-    [searchTerm, selectEntryRelayLocation, setLocationType],
+    [
+      viewRef,
+      searchTerm,
+      selectEntryRelayLocation,
+      setIsolatedItem,
+      setLocationType,
+      setSearchTerm,
+    ],
   );
 
   return handleSelectEntryLocation;
