0d066f09187dd6d2655f62ac64c90db4ae42af47
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-07-10 17:01:28 +0200

    Allow selecting an entry location when searching

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-lists/hooks/use-handle-select-exit-location.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-lists/hooks/use-handle-select-exit-location.ts
@@ -5,25 +5,21 @@ import { useAppContext } from '../../../../../../context';
 import { useRelayLocations } from '../../../../../../features/locations/hooks';
 import type { AnyLocation } from '../../../../../../features/locations/types';
 import { TransitionType, useHistory } from '../../../../../../lib/history';
-import { useSelectLocationViewContext } from '../../../SelectLocationViewContext';
 
 export function useHandleSelectExitLocation() {
-  const { searchTerm } = useSelectLocationViewContext();
   const { selectExitRelayLocation } = useRelayLocations();
   const history = useHistory();
   const { connectTunnel } = useAppContext();
 
   const handleSelectExitLocation = React.useCallback(
     async (location: AnyLocation) => {
-      if (!searchTerm) {
-        history.push(RoutePath.main, {
-          transition: TransitionType.dismiss,
-        });
-        await connectTunnel();
-      }
+      history.push(RoutePath.main, {
+        transition: TransitionType.dismiss,
+      });
       await selectExitRelayLocation(location.details);
+      await connectTunnel();
     },
-    [connectTunnel, history, searchTerm, selectExitRelayLocation],
+    [connectTunnel, history, selectExitRelayLocation],
   );
 
   return handleSelectExitLocation;
