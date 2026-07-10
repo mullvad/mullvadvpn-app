3df3402d94b4c0deee37b13d0992c535642a791b
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-07-10 17:03:34 +0200

    Handle selection of an entry location from a search

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-lists/hooks/use-handle-select-entry-location.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-lists/hooks/use-handle-select-entry-location.ts
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { startTransition } from 'react';
 
 import { useRelayLocations } from '../../../../../../features/locations/hooks';
 import { type AnyLocation, LocationType } from '../../../../../../features/locations/types';
+import { waitForAnimations } from '../../../../../../lib/utils';
 import { useSelectLocationViewContext } from '../../../SelectLocationViewContext';
 
 export function useHandleSelectEntryLocation() {
-  const { setLocationType, searchTerm } = useSelectLocationViewContext();
+  const { entryLocationListsContainerRef, setLocationType, searchTerm, setSearchTerm } =
+    useSelectLocationViewContext();
   const { selectEntryRelayLocation } = useRelayLocations();
 
   const handleSelectEntryLocation = React.useCallback(
     async (entryLocation: AnyLocation) => {
       if (!searchTerm) {
         setLocationType(LocationType.exit);
+        await selectEntryRelayLocation(entryLocation.details);
+      } else {
+        // If the user selects a location from a search, we can't immediately switch
+        // to show the `exit` locations as the view contents would jump around
+        // too much and confuse the user. To avoid this we just update the selected
+        // entry, which will cause an animation to mark it in the location lists
+        // as selected, and when that animation has finished we can switch to show
+        // `exit` locations.
+        startTransition(async () => {
+          await selectEntryRelayLocation(entryLocation.details);
+          await waitForAnimations(entryLocationListsContainerRef.current);
+          setSearchTerm('');
+          setLocationType(LocationType.exit);
+        });
       }
-      await selectEntryRelayLocation(entryLocation.details);
     },
-    [searchTerm, selectEntryRelayLocation, setLocationType],
+    [
+      entryLocationListsContainerRef,
+      searchTerm,
+      selectEntryRelayLocation,
+      setLocationType,
+      setSearchTerm,
+    ],
   );
 
   return handleSelectEntryLocation;
