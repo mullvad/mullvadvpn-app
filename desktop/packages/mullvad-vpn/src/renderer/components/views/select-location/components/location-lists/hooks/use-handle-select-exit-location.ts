0888ef12d49af4babe9fdfba169fbf24979c9707
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-07-10 17:01:28 +0200

    Handle selection of an exit location from a search

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-lists/hooks/use-handle-select-exit-location.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-lists/hooks/use-handle-select-exit-location.ts
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { startTransition } from 'react';
 
 import { RoutePath } from '../../../../../../../shared/routes';
 import { useAppContext } from '../../../../../../context';
 import { useRelayLocations } from '../../../../../../features/locations/hooks';
 import type { AnyLocation } from '../../../../../../features/locations/types';
 import { TransitionType, useHistory } from '../../../../../../lib/history';
+import { waitForAnimations } from '../../../../../../lib/utils';
 import { useSelectLocationViewContext } from '../../../SelectLocationViewContext';
 
 export function useHandleSelectExitLocation() {
-  const { searchTerm } = useSelectLocationViewContext();
+  const { exitLocationListsContainerRef, searchTerm } = useSelectLocationViewContext();
   const { selectExitRelayLocation } = useRelayLocations();
   const history = useHistory();
   const { connectTunnel } = useAppContext();
@@ -19,11 +20,24 @@ export function useHandleSelectExitLocation() {
         history.push(RoutePath.main, {
           transition: TransitionType.dismiss,
         });
+        await selectExitRelayLocation(location.details);
         await connectTunnel();
+      } else {
+        // When the user selects an `exit` location from a search we want to
+        // wait for the location to be marked as selected in the list before
+        // we go to the `main` Route. This is added to mirror the behavior of
+        // the `entry` selection handler.
+        startTransition(async () => {
+          await selectExitRelayLocation(location.details);
+          await waitForAnimations(exitLocationListsContainerRef.current);
+          history.push(RoutePath.main, {
+            transition: TransitionType.dismiss,
+          });
+          await connectTunnel();
+        });
       }
-      await selectExitRelayLocation(location.details);
     },
-    [connectTunnel, history, searchTerm, selectExitRelayLocation],
+    [connectTunnel, exitLocationListsContainerRef, history, searchTerm, selectExitRelayLocation],
   );
 
   return handleSelectExitLocation;
