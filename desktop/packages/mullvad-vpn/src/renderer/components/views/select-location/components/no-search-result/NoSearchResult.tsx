8d5da90031df2339360f43345fa92b379f028cc8
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-07-16 12:31:25 +0200

    Update `NoSearchResult` to be more in line with design
    It still deviates from the design in that it should be expanded
    vertically to have the `Button` be at the bottom of the screen.

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/no-search-result/NoSearchResult.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/no-search-result/NoSearchResult.tsx
@@ -1,29 +1,58 @@
+import React from 'react';
 import { sprintf } from 'sprintf-js';
+import styled from 'styled-components';
 
 import { messages } from '../../../../../../shared/gettext';
-import { LabelTinySemiBold } from '../../../../../lib/components';
-import { FlexColumn } from '../../../../../lib/components/flex-column';
+import { Button, Flex, Icon, LabelTiny } from '../../../../../lib/components';
+import { spacings } from '../../../../../lib/foundations';
 import { formatHtml } from '../../../../../lib/html-formatter';
 import { useSelectLocationViewContext } from '../../SelectLocationViewContext';
 
+const StyledFlex = styled(Flex)`
+  padding: ${spacings.medium} ${spacings.small};
+`;
+
 export function NoSearchResult() {
-  const { searchTerm } = useSelectLocationViewContext();
+  const { searchTerm, setSearchTerm } = useSelectLocationViewContext();
+
+  const handleClearSearch = React.useCallback(() => {
+    React.startTransition(() => {
+      setSearchTerm('');
+    });
+  }, [setSearchTerm]);
+
   return (
-    <FlexColumn
-      justifyContent="center"
-      alignItems="center"
-      aria-live="assertive"
-      aria-atomic="true">
-      <LabelTinySemiBold color="whiteAlpha60" textAlign="center">
-        {formatHtml(
-          sprintf(messages.gettext('No result for <b>%(searchTerm)s</b>.'), {
-            searchTerm,
-          }),
-        )}
-      </LabelTinySemiBold>
-      <LabelTinySemiBold color="whiteAlpha60" textAlign="center">
-        {messages.gettext('Try a different search.')}
-      </LabelTinySemiBold>
-    </FlexColumn>
+    <StyledFlex gap="medium" flexDirection="column" alignItems="space-between" flexGrow={1}>
+      <Flex
+        flexGrow={1}
+        flexDirection="column"
+        justifyContent="center"
+        aria-live="assertive"
+        aria-atomic="true">
+        <Flex flexDirection="column" gap="medium" alignItems="center">
+          <Icon icon="search" size="big" />
+          <Flex flexDirection="column">
+            <LabelTiny color="whiteAlpha60" textAlign="center">
+              {formatHtml(
+                sprintf(
+                  messages.pgettext('select-location-view', 'No result for: “%(searchTerm)s“'),
+                  {
+                    searchTerm,
+                  },
+                ),
+              )}
+            </LabelTiny>
+            <LabelTiny color="whiteAlpha60" textAlign="center">
+              {messages.gettext('Try a different search.')}
+            </LabelTiny>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex alignItems="flex-end" flexShrink={1}>
+        <Button onClick={handleClearSearch}>
+          <Button.Text>{messages.pgettext('select-location-view', 'Clear search')}</Button.Text>
+        </Button>
+      </Flex>
+    </StyledFlex>
   );
 }
