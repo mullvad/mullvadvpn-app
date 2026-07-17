baae6ad899238a8d34ddcfa4c3c125a93376292c
Bug Magnet <marco.nikic@mullvad.net>
2026-07-17 11:52:08 +0200

    Implement redesigned settings view

---
+++ ios/MullvadVPN/Coordinators/Settings/AntiCensorshipView.swift
@@ -0,0 +1,205 @@
+//
+//  AntiCensorshipView.swift
+//  MullvadVPN
+//
+//  Created by Marco Nikic on 2026-07-21.
+//  Copyright © 2026 Mullvad VPN AB. All rights reserved.
+//
+
+import MullvadSettings
+import SwiftUI
+
+struct AntiCensorshipView: View {
+    let settingsInteractor: VPNSettingsInteractor
+    @Bindable var settings: ObservableVPNSettings
+    let itemFactory = SegmentedListItemFactory()
+
+    struct Option: Identifiable {
+        typealias ID = String
+        let state: WireGuardObfuscationState
+        let accessibilityIdentifier: AccessibilityIdentifier
+
+        var id: ID {
+            state.description
+        }
+    }
+
+    init(settingsInteractor: VPNSettingsInteractor, settings: ObservableVPNSettings) {
+        self.settingsInteractor = settingsInteractor
+        self.settings = settings
+    }
+
+    let availableObfuscations: [Option] =
+        [
+            .init(state: .automatic, accessibilityIdentifier: .wireGuardObfuscationAutomatic),
+            .init(state: .shadowsocks, accessibilityIdentifier: .wireGuardObfuscationShadowsocks),
+            .init(state: .udpOverTcp, accessibilityIdentifier: .wireGuardObfuscationUdpOverTcp),
+            .init(state: .quic, accessibilityIdentifier: .wireGuardObfuscationQuic),
+            .init(state: .lwo, accessibilityIdentifier: .wireGuardObfuscationLwo),
+            .init(state: .off, accessibilityIdentifier: .wireGuardObfuscationOff),
+        ]
+
+    var body: some View {
+        ScrollViewReader { proxy in
+            antiCensorshipViewContents()
+                .onAppear {
+                    forceScrollToSection(proxy: proxy)
+                }
+        }
+        .navigationTitle("Anti-censorship")
+        .background(Color(.secondaryColor))
+    }
+
+    @ViewBuilder
+    func antiCensorshipViewContents() -> some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                headerText()
+                Spacer()
+                SegmentedListItem(
+                    userInteraction: .enabledWithoutHighlight,
+                    leading: {
+                        itemFactory.leading(
+                            for:
+                                .generic(title: "Method")
+                        )
+                    },
+                    groupedContent: {
+                        Group {
+                            ForEach(Array(availableObfuscations.enumerated()), id: \.element.id) {
+                                (index: Int, option: Option) in
+                                let state = option.state
+                                let accessibilityIdentifier = option.accessibilityIdentifier
+                                SegmentedListItem(
+                                    level: 1,
+                                    isLastInList: index == availableObfuscations.count - 1,
+                                    accessibilityIdentifier: accessibilityIdentifier,
+                                    leading: {
+                                        leadingView(for: state)
+                                    },
+                                    segment: {
+                                        segmentView(for: state)
+                                    },
+                                    onSelect: {
+                                        settings.tunnelSettings.wireGuardObfuscation.state = state
+                                    }
+                                )
+                                .id(option.id)
+                            }
+                        }
+                    }
+                )
+                .onChange(of: settings.tunnelSettings.wireGuardObfuscation, initial: false) { oldValue, newValue in
+                    // Prevent spamming updates if the same cell is pressed multiple times in a row
+                    if oldValue != newValue {
+                        updateSettings()
+                    }
+                }
+            }
+            .padding(.leading, UIMetrics.contentInsets.left)
+            .padding(.trailing, UIMetrics.contentInsets.right)
+            .padding(.bottom, UIMetrics.contentInsets.bottom)
+        }
+    }
+
+    func forceScrollToSection(proxy: ScrollViewProxy) {
+        proxy.scrollTo(settings.tunnelSettings.wireGuardObfuscation.state.description)
+    }
+
+    func updateSettings() {
+        settingsInteractor.tunnelManager.updateSettings([
+            .obfuscation(settings.tunnelSettings.wireGuardObfuscation)
+        ])
+    }
+
+    @ViewBuilder
+    func headerText() -> some View {
+        Text(
+            """
+            These methods may be useful in situations where you are blocked from reaching Mullvad. When "Automatic" is selected, the app will attempt all methods until one works.
+            """
+        )
+        .font(.mullvadTiny)
+        .foregroundStyle(Color.mullvadTextSecondary)
+        Spacer()
+        Spacer()
+        Text(
+            """
+            Please note that these methods do not improve performance, and may increase system utilization and battery consumption.
+            """
+        )
+        .font(.mullvadTinySemiBold)
+        .foregroundStyle(Color.mullvadTextSecondary)
+    }
+
+    @ViewBuilder
+    func leadingView(for state: WireGuardObfuscationState) -> some View {
+        let obfuscation = settings.tunnelSettings.wireGuardObfuscation
+        let title = state.description
+        let subtitle: String? =
+            switch state {
+            case .shadowsocks: "Port: \(obfuscation.shadowsocksPort)"
+            case .lwo: "Port: \(obfuscation.lwoPort)"
+            case .udpOverTcp: "Port: \(obfuscation.udpOverTcpPort)"
+            default: nil
+            }
+
+        let isSelected = state == obfuscation.state
+        itemFactory.leading(for: .generic(title: title, subtitle: subtitle, level: 1, isSelected: isSelected))
+    }
+
+    @ViewBuilder
+    func segmentView(for state: WireGuardObfuscationState) -> some View {
+        if [WireGuardObfuscationState.automatic, WireGuardObfuscationState.quic, WireGuardObfuscationState.off]
+            .contains(state)
+        {
+            EmptyView()
+        } else {
+            NavigationLink {
+                switch state {
+                case .shadowsocks:
+                    ShadowsocksObfuscationSettingsView(
+                        port: $settings.tunnelSettings.wireGuardObfuscation.shadowsocksPort
+                    )
+                    .navigationTitle("Shadowsocks")
+                    .onDisappear {
+                        updateSettings()
+                    }
+                case .udpOverTcp:
+                    UDPOverTCPObfuscationSettingsView(
+                        port: $settings.tunnelSettings.wireGuardObfuscation.udpOverTcpPort
+                    )
+                    .navigationTitle("UDP-over-TCP")
+                    .onDisappear {
+                        updateSettings()
+                    }
+                case .lwo:
+                    let viewModel = TunnelLwoObfuscationSettingsViewModel(
+                        port: $settings.tunnelSettings.wireGuardObfuscation.lwoPort,
+                        portRanges: settingsInteractor.cachedRelays?.relays.wireguard.portRanges ?? [])
+                    LwoObfuscationSettingsView(
+                        viewModel: viewModel
+                    )
+                    .navigationTitle("LWO")
+                    .onDisappear {
+                        updateSettings()
+                    }
+                default: EmptyView()
+                }
+            } label: {
+                switch state {
+                case .shadowsocks:
+                    itemFactory.image(for: .chevron)
+                        .accessibilityIdentifier(.wireGuardObfuscationShadowsocksPort)
+                case .udpOverTcp:
+                    itemFactory.image(for: .chevron)
+                        .accessibilityIdentifier(.wireGuardObfuscationUdpOverTcpPort)
+                case .lwo:
+                    itemFactory.image(for: .chevron)
+                        .accessibilityIdentifier(.wireGuardObfuscationLwoPort)
+                default: EmptyView()
+                }
+            }
+        }
+    }
+}
---
+++ ios/MullvadVPN/Coordinators/Settings/QuantumResistanceView.swift
@@ -0,0 +1,67 @@
+//
+//  QuantumResistanceView.swift
+//  MullvadVPN
+//
+//  Created by Marco Nikic on 2026-07-30.
+//  Copyright © 2026 Mullvad VPN AB. All rights reserved.
+//
+
+import MullvadSettings
+import SwiftUI
+
+struct QuantumResistanceView: View {
+    private let itemFactory = SegmentedListItemFactory()
+    @Binding var isQuantumResistanceEnabled: Bool
+    @State private var alert: MullvadAlert?
+
+    var body: some View {
+        SegmentedListItem(
+            userInteraction: .enabledWithoutHighlight,
+            accessibilityIdentifier: .quantumResistantTunnelCell,
+            leading: {
+                itemFactory.leading(
+                    for: .generic(title: NSLocalizedString("Quantum-resistant tunnel", comment: ""))
+                )
+            },
+            trailing: {
+                itemFactory.trailing(
+                    for: .custom(items: [
+                        .button(
+                            icon: .info,
+                            onSelect: {
+                                alert = getQuantumResistanceAlert(completion: { alert = nil })
+                            },
+                            sizing: .button
+                        ),
+                        .toggle(
+                            isOn: $isQuantumResistanceEnabled,
+                            isDisabled: false,
+                            accessibilityID: isQuantumResistanceEnabled ? .quantumResistanceOff : .quantumResistanceOn
+                        ),
+                        .padding(),
+                    ])
+                )
+            }
+        )
+        .mullvadAlert(item: $alert)
+    }
+
+    func getQuantumResistanceAlert(completion: @escaping () -> Void) -> MullvadAlert {
+        MullvadAlert(
+            type: .info,
+            messages: [
+                "This feature makes the WireGuard tunnel resistant to potential attacks from quantum computers.",
+                """
+                It does this by performing an extra key exchange using a quantum safe algorithm and mixing the result into WireGuard’s regular encryption. This extra step uses approximately 500 kiB of traffic every time a new tunnel is established.
+                """,
+            ], customView: nil,
+            actions: [
+                MullvadAlert.Action(type: .primary, title: "Got it!", handler: completion)
+            ])
+    }
+}
+
+#Preview {
+    @Previewable @State var quantumResistance = false
+    QuantumResistanceView(isQuantumResistanceEnabled: $quantumResistance)
+}
