86c7c90cddc8dbc48782dd8b595a5d2e00b80888
David Lönnhager <david.l@mullvad.net>
2026-07-20 16:52:05 +0200

    Make it impossible to use SocketBypass without BypassedSocket wrapper

--- talpid-net/src/bypass.rs
+++ talpid-net/src/bypass.rs
@@ -2,29 +2,32 @@ use std::{io, sync::Arc};
 
 use socket2::SockRef;
 
+/// Guard against using [SocketBypass] without [BypassedSocket].
+pub struct BypassToken(());
+
 /// A trait for implementing socket bypass. This lets individual sockets be excluded (leak) from
 /// VPN tunnel traffic.
 pub trait SocketBypass: Send + Sync {
     /// Begin socket bypass. When called, the socket must be excluded from tunnel traffic until
     /// [Self::revoke_bypass] has been called and the socket has been destroyed.
-    fn bypass_socket(&self, socket: SockRef<'_>) -> io::Result<()>;
+    fn bypass_socket(&self, socket: SockRef<'_>, token: &BypassToken) -> io::Result<()>;
 
     /// Allow the bypass to cease.
     ///
     /// When this has succeeded, there is no longer any guarantee that the socket will be
     /// excluded. The bypass must not outlast the lifetime of the socket lifetime, but it may cease
     /// immediately when this is called (depending on the implementation).
-    fn revoke_bypass(&self, socket: SockRef<'_>) -> io::Result<()>;
+    fn revoke_bypass(&self, socket: SockRef<'_>, token: &BypassToken) -> io::Result<()>;
 }
 
 pub struct NoopBypass;
 
 impl SocketBypass for NoopBypass {
-    fn bypass_socket(&self, _: SockRef<'_>) -> io::Result<()> {
+    fn bypass_socket(&self, _: SockRef<'_>, _: &BypassToken) -> io::Result<()> {
         Ok(())
     }
 
-    fn revoke_bypass(&self, _: SockRef<'_>) -> io::Result<()> {
+    fn revoke_bypass(&self, _: SockRef<'_>, _: &BypassToken) -> io::Result<()> {
         Ok(())
     }
 }
@@ -47,7 +50,7 @@ impl BypassedSocket {
     ) -> io::Result<BypassedSocket> {
         let socket = s.into().try_clone()?;
 
-        bypass.bypass_socket(SockRef::from(&socket))?;
+        bypass.bypass_socket(SockRef::from(&socket), &BypassToken(()))?;
 
         Ok(BypassedSocket { bypass, socket })
     }
@@ -55,7 +58,10 @@ impl BypassedSocket {
 
 impl Drop for BypassedSocket {
     fn drop(&mut self) {
-        if let Err(err) = self.bypass.revoke_bypass(SockRef::from(&self.socket)) {
+        if let Err(err) = self
+            .bypass
+            .revoke_bypass(SockRef::from(&self.socket), &BypassToken(()))
+        {
             log::error!("Failed to revoke socket bypass: {err}");
         }
     }
