d50ce3f33d20b4325fca1f42991562181f468a88
Oliver <oliver@mohlin.dev>
2026-07-22 10:58:08 +0200

    Remove margins and label from `FilterChips`

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/filter-chips/FilterChips.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/filter-chips/FilterChips.tsx
@@ -1,6 +1,5 @@
-import { messages } from '../../../../../../shared/gettext';
 import { useActiveFilters } from '../../../../../features/locations/hooks/use-active-filters';
-import { Flex, LabelTinySemiBold } from '../../../../../lib/components';
+import { FlexRow } from '../../../../../lib/components/flex-row';
 import { useSelectLocationViewContext } from '../../SelectLocationViewContext';
 import { DaitaFilterChip } from '../daita-filter-chip';
 import { LwoFilterChip } from '../lwo-filter-chip';
@@ -19,20 +18,12 @@ export function FilterChips() {
   } = useActiveFilters(locationType);
 
   return (
-    <Flex
-      gap="small"
-      alignItems="center"
-      flexWrap="wrap"
-      margin={{ horizontal: 'small', bottom: 'medium' }}>
-      <LabelTinySemiBold>
-        {messages.pgettext('select-location-view', 'Filtered:')}
-      </LabelTinySemiBold>
-
+    <FlexRow gap="small" alignItems="center" flexWrap="wrap">
       {isOwnershipFilterActive && <OwnershipFilterChip />}
       {isProvidersFilterActive && <ProvidersFilterChip />}
       {isDaitaFilterActive && <DaitaFilterChip />}
       {isQuicFilterActive && <QuicFilterChip />}
       {isLwoFilterActive && <LwoFilterChip />}
-    </Flex>
+    </FlexRow>
   );
 }
