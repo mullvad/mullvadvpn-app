eab11136543445cd644286c817933ada775dc279
Oliver <oliver@mohlin.dev>
2026-07-27 08:20:40 +0200

    Add flex property to dialog popup content

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/dialog/components/dialog-popup-content/DialogPopupContent.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/dialog/components/dialog-popup-content/DialogPopupContent.tsx
@@ -10,6 +10,8 @@ const StyledDialogContainer = styled(Alert)`
   > ${StyledAlertIcon}:first-child {
     margin-top: ${spacings.small};
   }
+
+  flex: 1;
 `;
 export function DialogPopupContent(props: DialogPopupContentProps) {
   return <StyledDialogContainer {...props} />;
