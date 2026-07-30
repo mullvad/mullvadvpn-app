b1b5756c6748dbd06db2d3fad28123b7390d303b
Oliver <oliver@mohlin.dev>
2026-07-30 15:25:44 +0200

    Update scroll behavior when changing location type

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/providers-filter-chip/ProvidersFilterChip.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/providers-filter-chip/ProvidersFilterChip.tsx
@@ -7,24 +7,21 @@ import { FilterChip, type FilterChipProps } from '../../../../../lib/components'
 import { useRelaySettingsUpdater } from '../../../../../lib/constraint-updater';
 import { useNormalRelaySettings } from '../../../../../lib/relay-settings-hooks';
 import { useFilteredProviders } from '../../../filter/hooks';
-import { useScrollPositionContext } from '../../ScrollPositionContext';
 
 export type ProvidersFilterChip = FilterChipProps;
 
 export function ProvidersFilterChip(props: ProvidersFilterChip) {
   const relaySettingsUpdater = useRelaySettingsUpdater();
-  const { resetScrollPositions } = useScrollPositionContext();
   const relaySettings = useNormalRelaySettings();
   const ownership = relaySettings?.ownership ?? Ownership.any;
   const providers = relaySettings?.providers ?? [];
   const filteredProviders = useFilteredProviders(providers, ownership);
 
   const onClearProviders = React.useCallback(async () => {
-    resetScrollPositions();
     if (relaySettings) {
       await relaySettingsUpdater((settings) => ({ ...settings, providers: [] }));
     }
-  }, [relaySettingsUpdater, resetScrollPositions, relaySettings]);
+  }, [relaySettingsUpdater, relaySettings]);
 
   return (
     <FilterChip aria-label={messages.gettext('Clear')} onClick={onClearProviders} {...props}>
