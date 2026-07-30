2d2b129201bef1ae3ac4986747aba23c19e7b76a
Jon Petersson <jon.petersson@mullvad.net>
2026-07-30 13:56:30 +0200

    Rewrite login screen in SwiftUI

--- ios/MullvadVPN/Classes/AccessbilityIdentifier.swift
+++ ios/MullvadVPN/Classes/AccessbilityIdentifier.swift
@@ -26,6 +26,7 @@ public enum AccessibilityIdentifier: Equatable {
     case continueWithLoginButton
     case collapseButton
     case expandButton
+    case loginButton
     case createAccountButton
     case createAccountConfirmationButton
     case createAccountCancelButton
