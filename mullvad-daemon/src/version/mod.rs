e554d709e083357b7dd1b3828f515ed69eccc9a1
Linus <linus@mullvad.net>
2026-07-31 12:57:40 +0000

    Remove git commit date from daemon startup log
    This was the only hard dependency on the `git` binary in the Rust
    build: the build script panicked if `git` was missing, and silently
    baked in an empty string when built outside a git repository.
    
    The date was not used for anything except printing in the --version
    output of the daemon. So purely decoration with very little value.

--- mullvad-daemon/src/version/mod.rs
+++ mullvad-daemon/src/version/mod.rs
@@ -40,9 +40,6 @@ pub enum Error {
     UpdateAborted,
 }
 
-/// Contains the date of the git commit this was built from
-pub const COMMIT_DATE: &str = include_str!(concat!(env!("OUT_DIR"), "/git-commit-date.txt"));
-
 pub fn is_beta_version() -> bool {
     mullvad_version::VERSION.contains("beta")
 }
@@ -53,9 +50,8 @@ pub fn is_dev_version() -> bool {
 
 pub fn log_version() {
     log::info!(
-        "Starting {} - {} {}",
+        "Starting {} - {}",
         env!("CARGO_PKG_NAME"),
         mullvad_version::VERSION,
-        COMMIT_DATE,
     )
 }
