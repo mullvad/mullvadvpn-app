d01bd68765cec5617b919097919cf52cebb68be2
David Göransson <david.goransson@mullvad.net>
2026-08-04 21:06:06 +0200

    no-op drop rule

--- android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/ConnectionTest4.kt
+++ android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/ConnectionTest4.kt
@@ -73,7 +73,7 @@ class ConnectionTest4 : EndToEndTest() {
         on<ConnectPage>()
 
         // Block everything except the default relay IP. After this the API is no longer reachable.
-        createFirewallRules { DropRule.blockAllTrafficExceptToDestinationRule(testRelayIp) }
+//        createFirewallRules { DropRule.blockAllTrafficExceptToDestinationRule(testRelayIp) }
 
         // Restarting the activity will re-create the daemon which will try to reach the API.
         targetActivity.finishAffinity()
