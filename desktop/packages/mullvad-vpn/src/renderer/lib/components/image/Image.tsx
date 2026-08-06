5d6dbaebf7471ab3aa4bb76fd5fa57a5c0d5d5ca
Oliver <oliver@mohlin.dev>
2026-08-06 15:01:32 +0200

    Export `Image` styled component

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/image/Image.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/image/Image.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export type ImageProps = Omit<React.ComponentPropsWithRef<'img'>, 'src'> & {
   source: string;
 };
 
+export const StyledImage = styled.img``;
+
 export const Image = ({ source, ...props }: ImageProps) => {
   const url = source.startsWith('data:') ? source : `assets/images/${source}.svg`;
 
-  return <img src={url} {...props} />;
+  return <StyledImage src={url} {...props} />;
 };
