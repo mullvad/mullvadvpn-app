8169439fe50165a938b8aa1303184fe191b7f19e
Oliver <oliver@mohlin.dev>
2026-08-12 09:56:51 +0200

    Add `min-height` to `CarouselIndicators`

--- desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-controls/components/carousel-indicators/Carouselndicators.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/carousel/components/carousel-controls/components/carousel-indicators/Carouselndicators.tsx
@@ -5,14 +5,16 @@ import { Flex } from '../../../../../flex';
 import { useCarouselContext } from '../../../../CarouselContext';
 import { CarouselIndicator } from './components';
 
-export const StyledCarouselIndicators = styled(Flex)``;
+export const StyledCarouselIndicators = styled(Flex)`
+  min-height: 24px;
+`;
 
 export function CarouselIndicators() {
   const { numberOfSlides, slideIndex } = useCarouselContext();
 
   const range = createRange(numberOfSlides);
   return (
-    <StyledCarouselIndicators gap="small">
+    <StyledCarouselIndicators gap="small" alignItems="center">
       {range.map((_, i) => {
         const current = i === slideIndex;
         return <CarouselIndicator key={i} disabled={current} slideToGoTo={i} />;
