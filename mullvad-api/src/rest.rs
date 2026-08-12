f60dc3170a5a9d6e398a6d4282890108780b72f7
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-08-12 15:16:25 +0200

    Use a lower http idle timeout for domain fronting

--- mullvad-api/src/rest.rs
+++ mullvad-api/src/rest.rs
@@ -34,11 +34,6 @@ const USER_AGENT: &str = "mullvad-app";
 
 pub type Result<T> = std::result::Result<T, Error>;
 const DEFAULT_TIMEOUT: Duration = Duration::from_secs(10);
-/// How long an HTTP connection may sit idle in hyper's connection pool before
-/// it is evicted. Closes pooled keepalive sockets (and their upstream proxy
-/// connections, if any) after a quiet period without affecting in-flight
-/// requests.
-const POOL_IDLE_TIMEOUT: Duration = Duration::from_secs(60);
 
 /// Describes all the ways a REST request can fail
 #[derive(thiserror::Error, Debug, Clone)]
@@ -170,6 +165,9 @@ impl<T: ConnectionModeProvider + 'static> RequestService<T> {
             let api_connection_mode = connection_mode_provider.initial();
             InnerConnectionMode::from(api_connection_mode)
         };
+
+        let idle_timeout = proxy_config.idle_timeout();
+
         let connector = HttpsConnector::new(
             dns_resolver,
             proxy_config,
@@ -180,7 +178,7 @@ impl<T: ConnectionModeProvider + 'static> RequestService<T> {
         );
         let client =
             hyper_util::client::legacy::Client::builder(hyper_util::rt::TokioExecutor::new())
-                .pool_idle_timeout(Some(POOL_IDLE_TIMEOUT))
+                .pool_idle_timeout(Some(idle_timeout))
                 .pool_timer(hyper_util::rt::TokioTimer::new())
                 .build(connector.clone());
 
