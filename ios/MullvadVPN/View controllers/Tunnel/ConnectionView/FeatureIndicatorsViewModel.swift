344e33f56c8d887079274b54859f981d3453d362
Emīls <emils@mullvad.net>
2026-08-13 11:03:22 +0200

    Add DEBUG GotaTun feature-indicator chip
    Shows a chip in the connection view when the GotaTun debug setting is
    active, so test builds make it obvious which backend is running.

--- ios/MullvadVPN/View controllers/Tunnel/ConnectionView/FeatureIndicatorsViewModel.swift
+++ ios/MullvadVPN/View controllers/Tunnel/ConnectionView/FeatureIndicatorsViewModel.swift
@@ -56,6 +56,10 @@ class FeatureIndicatorsViewModel: ChipViewModelProtocol {
             break
         }
 
+        #if NEVER_IN_PRODUCTION
+            features.append(GotaTunFeature())
+        #endif
+
         return
             features
             .filter { $0.isEnabled }
