e3addb6019a1a0c21406b83cee34857898a06abc
Jon Petersson <jon.petersson@mullvad.net>
2026-08-19 14:13:13 +0200

    Fix UI tests

--- ios/MullvadTypes/Task+AnyCancellable.swift
+++ ios/MullvadTypes/Task+AnyCancellable.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2026 Mullvad VPN AB. All rights reserved.
 //
 
+// Bridge to allow new async/await pattern via Task to be compatible with
+// old task grouping where Cancellable is expected.
 extension Task {
     public var cancellable: AnyCancellable {
         AnyCancellable {
