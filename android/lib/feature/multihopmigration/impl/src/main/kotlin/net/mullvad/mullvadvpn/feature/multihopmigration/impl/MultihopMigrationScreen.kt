86d3fad87052a7d818828168bb300d00c8b240f4
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-08-20 12:58:11 +0200

    Add missing arrow for back button

--- android/lib/feature/multihopmigration/impl/src/main/kotlin/net/mullvad/mullvadvpn/feature/multihopmigration/impl/MultihopMigrationScreen.kt
+++ android/lib/feature/multihopmigration/impl/src/main/kotlin/net/mullvad/mullvadvpn/feature/multihopmigration/impl/MultihopMigrationScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material.icons.rounded.Info
@@ -506,8 +507,17 @@ private fun BottomBar(
 
             Row(modifier = Modifier.fillMaxWidth()) {
                 if (backButtonWidth > 0.dp) {
+                    val backMessage =
+                        stringResourceWithIcons(
+                            R.string.back_with_icon,
+                            DescribedIcon(
+                                icon = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                                contentDescription = stringResource(R.string.back),
+                            ),
+                        )
                     PrimaryOutlinedButton(
-                        text = stringResource(R.string.back),
+                        text = backMessage.text,
+                        inlineContent = backMessage.inlineContent,
                         onClick = onBackClick,
                         modifier = Modifier.width(backButtonWidth).alpha(backButtonAlpha),
                     )
