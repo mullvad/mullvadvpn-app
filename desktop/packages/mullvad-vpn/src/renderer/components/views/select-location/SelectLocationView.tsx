9b0bcf209bb80173b3121c837ad8511c4a3f1360
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-08-21 15:58:34 +0200

    WIP

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/SelectLocationView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/SelectLocationView.tsx
@@ -4,27 +4,50 @@ import styled from 'styled-components';
 
 import { messages } from '../../../../shared/gettext';
 import { LocationType } from '../../../features/locations/types';
+import { useMeasure } from '../../../hooks';
 import { Carousel } from '../../../lib/components/carousel';
 import { FlexColumn } from '../../../lib/components/flex-column';
 import { View } from '../../../lib/components/view';
-import { colors } from '../../../lib/foundations';
+import { colors, spacings } from '../../../lib/foundations';
 import { useHistory } from '../../../lib/history';
 import { AppNavigationHeader } from '../../';
 import { BackAction } from '../../keyboard-navigation';
 import { NavigationContainer } from '../../NavigationContainer';
 import { HeaderMenuIconButton, SelectLocationSelector } from './components';
 import { LocationSlide } from './components/location-slide';
+import { useIsLocationSelectorExpanded } from './hooks';
 import {
   SelectLocationViewProvider,
   useSelectLocationViewContext,
 } from './SelectLocationViewContext';
 
-const StyledStickyContainer = styled.div`
-  position: sticky;
+const StyledLocationSelectorContainerWrapper = styled.div`
+  position: relative;
   top: 0;
-  z-index: 20;
+  z-index: 100;
   width: 100%;
   background-color: ${colors.darkBlue};
+`;
+
+const StyledLocationSelectorContainer = styled.div`
+  padding-bottom: ${spacings.small};
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  width: 100%;
+  z-index: 100;
+  background-color: ${colors.darkBlue};
+`;
+
+const StyledLocationSelectorMeasurementsContainer = styled.div`
+  position: fixed;
+  visibility: hidden;
+  transform: translateX(-100%);
+  width: 100vw;
+  left: 0;
+  top: 0;
+  z-index: 1000;
 `;
 
 export function SelectLocationViewImpl() {
@@ -38,27 +61,50 @@ export function SelectLocationViewImpl() {
 
   const onClose = useCallback(() => history.pop(), [history]);
 
+  const [expandedHeaderHeightRef, { height: expandedHeaderHeight }] = useMeasure();
+  const [collapsedHeaderHeightRef, { height: collapsedHeaderHeight }] = useMeasure();
+
+  const expanded = useIsLocationSelectorExpanded();
+
   return (
     <View backgroundColor="darkBlue">
+      <StyledLocationSelectorMeasurementsContainer ref={expandedHeaderHeightRef}>
+        <SelectLocationSelector expanded={true} />
+      </StyledLocationSelectorMeasurementsContainer>
+      <StyledLocationSelectorMeasurementsContainer ref={collapsedHeaderHeightRef}>
+        <SelectLocationSelector expanded={false} />
+      </StyledLocationSelectorMeasurementsContainer>
       <BackAction action={onClose}>
         <NavigationContainer>
-          <StyledStickyContainer>
-            <AppNavigationHeader
-              title={
-                // TRANSLATORS: Title label in navigation bar
-                messages.pgettext('select-location-nav', 'Select location')
-              }
-              titleVisible>
-              <HeaderMenuIconButton />
-            </AppNavigationHeader>
-            <FlexColumn margin={{ horizontal: 'medium' }} padding={{ bottom: 'small' }} gap="small">
-              <SelectLocationSelector />
-            </FlexColumn>
-          </StyledStickyContainer>
+          <AppNavigationHeader
+            title={
+              // TRANSLATORS: Title label in navigation bar
+              messages.pgettext('select-location-nav', 'Select location')
+            }
+            titleVisible>
+            <HeaderMenuIconButton />
+          </AppNavigationHeader>
+          <StyledLocationSelectorContainerWrapper>
+            <StyledLocationSelectorContainer>
+              <FlexColumn margin={{ horizontal: 'medium' }}>
+                <SelectLocationSelector expanded={expanded} />
+              </FlexColumn>
+            </StyledLocationSelectorContainer>
+          </StyledLocationSelectorContainerWrapper>
           <Carousel disableScroll slideIndex={slideIndex} onSlideIndexChange={setSlideIndex}>
             <Carousel.Slides>
-              <LocationSlide type={LocationType.entry} />
-              <LocationSlide type={LocationType.exit} />
+              <LocationSlide
+                index={0}
+                type={LocationType.entry}
+                collapsedHeaderHeight={collapsedHeaderHeight}
+                expandedHeaderHeight={expandedHeaderHeight}
+              />
+              <LocationSlide
+                index={1}
+                type={LocationType.exit}
+                collapsedHeaderHeight={collapsedHeaderHeight}
+                expandedHeaderHeight={expandedHeaderHeight}
+              />
             </Carousel.Slides>
           </Carousel>
         </NavigationContainer>
