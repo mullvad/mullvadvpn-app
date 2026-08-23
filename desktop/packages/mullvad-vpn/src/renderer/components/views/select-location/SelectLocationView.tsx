64a85f64b3293d66d18de581188d9c556dbb472f
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-08-23 22:05:16 +0200

    Show `LocationSelector` above dynamically sized `LocationSlide` content

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/SelectLocationView.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/SelectLocationView.tsx
@@ -4,27 +4,50 @@ import styled from 'styled-components';
 
 import { messages } from '../../../../shared/gettext';
 import { LocationType } from '../../../features/locations/types';
+import { useMeasure } from '../../../hooks';
 import { Carousel } from '../../../lib/components/carousel';
 import { FlexColumn } from '../../../lib/components/flex-column';
 import { View } from '../../../lib/components/view';
-import { colors } from '../../../lib/foundations';
+import { colors, spacings } from '../../../lib/foundations';
 import { useHistory } from '../../../lib/history';
 import { AppNavigationHeader } from '../../';
 import { BackAction } from '../../keyboard-navigation';
 import { NavigationContainer } from '../../NavigationContainer';
 import { HeaderMenuIconButton, SelectLocationSelector } from './components';
 import { LocationSlide } from './components/location-slide';
+import { useIsLocationSelectorExpanded } from './hooks';
 import {
   SelectLocationViewProvider,
   useSelectLocationViewContext,
 } from './SelectLocationViewContext';
 
-const StyledStickyContainer = styled.div`
-  position: sticky;
+const StyledLocationSelectorOuterContainer = styled.div`
+  position: relative;
   top: 0;
-  z-index: 20;
+  z-index: 100; // NOTE: Must be higher than the StyledTrack in CustomScrollbars
   width: 100%;
   background-color: ${colors.darkBlue};
+`;
+
+const StyledLocationSelectorInnerContainer = styled.div`
+  // NOTE: Keep padding-bottom in sync with padding-top in the StyledCarouselSlide
+  // styled component in the LocationSlide component.
+  padding-bottom: ${spacings.small};
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  width: 100%;
+  background-color: ${colors.darkBlue};
+`;
+
+const StyledLocationSelectorMeasurementsContainer = styled.div`
+  position: fixed;
+  visibility: hidden;
+  transform: translateX(-100%);
+  width: 100vw;
+  left: 0;
+  top: 0;
 `;
 
 export function SelectLocationViewImpl() {
@@ -38,11 +61,26 @@ export function SelectLocationViewImpl() {
 
   const onClose = useCallback(() => history.pop(), [history]);
 
+  const [expandedHeaderHeightRef, { height: expandedHeaderHeight }] = useMeasure();
+  const [collapsedHeaderHeightRef, { height: collapsedHeaderHeight }] = useMeasure();
+  const [navigationHeaderHeightRef, { height: navigationHeaderHeight }] = useMeasure();
+
+  const expanded = useIsLocationSelectorExpanded();
+
   return (
     <View backgroundColor="darkBlue">
+      {/* NOTE: We are measuring the SelectLocationSelector in its expanded and collapsed state
+       * in order to know the exact height of the rendered elements in their different states.
+       */}
+      <StyledLocationSelectorMeasurementsContainer ref={expandedHeaderHeightRef}>
+        <SelectLocationSelector expanded={true} />
+      </StyledLocationSelectorMeasurementsContainer>
+      <StyledLocationSelectorMeasurementsContainer ref={collapsedHeaderHeightRef}>
+        <SelectLocationSelector expanded={false} />
+      </StyledLocationSelectorMeasurementsContainer>
       <BackAction action={onClose}>
         <NavigationContainer>
-          <StyledStickyContainer>
+          <FlexColumn ref={navigationHeaderHeightRef}>
             <AppNavigationHeader
               title={
                 // TRANSLATORS: Title label in navigation bar
@@ -51,14 +89,30 @@ export function SelectLocationViewImpl() {
               titleVisible>
               <HeaderMenuIconButton />
             </AppNavigationHeader>
-            <FlexColumn margin={{ horizontal: 'medium' }} padding={{ bottom: 'small' }} gap="small">
-              <SelectLocationSelector />
-            </FlexColumn>
-          </StyledStickyContainer>
+          </FlexColumn>
+          <StyledLocationSelectorOuterContainer>
+            <StyledLocationSelectorInnerContainer>
+              <FlexColumn margin={{ horizontal: 'medium' }}>
+                <SelectLocationSelector expanded={expanded} />
+              </FlexColumn>
+            </StyledLocationSelectorInnerContainer>
+          </StyledLocationSelectorOuterContainer>
           <Carousel disableScroll slideIndex={slideIndex} onSlideIndexChange={setSlideIndex}>
             <Carousel.Slides>
-              <LocationSlide type={LocationType.entry} />
-              <LocationSlide type={LocationType.exit} />
+              <LocationSlide
+                collapsedHeaderHeight={collapsedHeaderHeight}
+                expandedHeaderHeight={expandedHeaderHeight}
+                navigationHeaderHeight={navigationHeaderHeight}
+                slideActive={slideIndex === 0}
+                type={LocationType.entry}
+              />
+              <LocationSlide
+                collapsedHeaderHeight={collapsedHeaderHeight}
+                expandedHeaderHeight={expandedHeaderHeight}
+                navigationHeaderHeight={navigationHeaderHeight}
+                slideActive={slideIndex === 1}
+                type={LocationType.exit}
+              />
             </Carousel.Slides>
           </Carousel>
         </NavigationContainer>
--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-slide/LocationSlide.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/location-slide/LocationSlide.tsx
@@ -1,53 +1,113 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { LocationType } from '../../../../../features/locations/types';
 import { Carousel } from '../../../../../lib/components/carousel';
 import { View } from '../../../../../lib/components/view';
+import { spacings } from '../../../../../lib/foundations';
 import type { IScrollEvent } from '../../../../CustomScrollbars';
 import { NavigationScrollbars } from '../../../../NavigationScrollbars';
+import { useIsLocationSelectorIsolated } from '../../hooks';
 import { useSelectLocationViewContext } from '../../SelectLocationViewContext';
 import { LocationLists } from '../location-lists';
-import { SpacePreAllocationView } from '../space-pre-allocation-view';
 import { LocationSlideContextProvider, useLocationSlideContext } from './LocationSlideContext';
 
 export type LocationSlideProps = {
+  collapsedHeaderHeight: number;
+  expandedHeaderHeight: number;
+  navigationHeaderHeight: number;
+  slideActive: boolean;
   type: LocationType;
 };
 
-const StyledCarouselSlide = styled(Carousel.Slides.Slide)`
-  // TODO: Replace 182 with real calculated height of header
-  height: calc(100vh - 182px);
+const StyledCarouselSlide = styled(Carousel.Slides.Slide)<{
+  $containerOffset: number;
+  $navigationHeaderHeight: number;
+}>`
+  ${({ $containerOffset, $navigationHeaderHeight }) => {
+    return css`
+      height: calc(100vh - ${$containerOffset}px - ${$navigationHeaderHeight}px);
+      margin-top: ${$containerOffset}px;
+      // NOTE: Keep padding-top in sync with padding-bottom in the StyledLocationSelectorInnerContainer
+      // styled component in the SelectLocationView component.
+      padding-top: ${spacings.small};
+    `;
+  }}
 `;
 
-function LocationSlideImpl({ type }: LocationSlideProps) {
-  const { spacePreAllocationViewRef, scrollViewRef } = useLocationSlideContext();
+const StyledContentContainer = styled.div<{
+  $marginTop: number;
+}>`
+  ${({ $marginTop }) => {
+    return css`
+      margin-top: ${$marginTop}px;
+    `;
+  }}
+`;
+
+function LocationSlideImpl({
+  collapsedHeaderHeight,
+  expandedHeaderHeight,
+  navigationHeaderHeight,
+  slideActive,
+  type,
+}: LocationSlideProps) {
+  const { resetScroll, scrollViewRef } = useLocationSlideContext();
   const { setScrollTop } = useSelectLocationViewContext();
+  const isLocationSelectorIsolated = useIsLocationSelectorIsolated();
+
+  const headerHeightDiff = expandedHeaderHeight - collapsedHeaderHeight;
+  const containerOffset = isLocationSelectorIsolated ? 0 : collapsedHeaderHeight;
+  const contentOffset = isLocationSelectorIsolated ? collapsedHeaderHeight : headerHeightDiff;
+
+  const headersCalculated =
+    collapsedHeaderHeight > 0 && expandedHeaderHeight > 0 && navigationHeaderHeight > 0;
+  React.useEffect(() => {
+    if (slideActive && headersCalculated) {
+      resetScroll();
+    }
+  }, [resetScroll, slideActive, headersCalculated]);
 
   const handleScroll = React.useCallback(
     (event: IScrollEvent) => {
-      setScrollTop(event.scrollTop);
+      if (slideActive) {
+        setScrollTop(event.scrollTop);
+      }
     },
-    [setScrollTop],
+    [setScrollTop, slideActive],
   );
 
   return (
-    <StyledCarouselSlide>
-      <NavigationScrollbars onScroll={handleScroll} ref={scrollViewRef}>
-        <SpacePreAllocationView ref={spacePreAllocationViewRef}>
+    <StyledCarouselSlide
+      $containerOffset={containerOffset}
+      $navigationHeaderHeight={navigationHeaderHeight}>
+      <NavigationScrollbars onScroll={handleScroll} ref={scrollViewRef} trackOffset={contentOffset}>
+        <StyledContentContainer $marginTop={contentOffset}>
           <View.Container horizontalMargin="medium" flexDirection="column">
             <LocationLists type={type} />
           </View.Container>
-        </SpacePreAllocationView>
+        </StyledContentContainer>
       </NavigationScrollbars>
     </StyledCarouselSlide>
   );
 }
 
-export function LocationSlide({ type }: LocationSlideProps) {
+export function LocationSlide({
+  collapsedHeaderHeight,
+  expandedHeaderHeight,
+  navigationHeaderHeight,
+  slideActive,
+  type,
+}: LocationSlideProps) {
   return (
     <LocationSlideContextProvider>
-      <LocationSlideImpl type={type} />
+      <LocationSlideImpl
+        collapsedHeaderHeight={collapsedHeaderHeight}
+        expandedHeaderHeight={expandedHeaderHeight}
+        navigationHeaderHeight={navigationHeaderHeight}
+        slideActive={slideActive}
+        type={type}
+      />
     </LocationSlideContextProvider>
   );
 }
