d05bac138e6fc2c6391ec1b971ae96d0b111eb51
Steffen <steffen.ernst@mullvad.net>
2026-08-25 13:13:07 +0200

    Update ios action box component

--- ios/MullvadVPN/Views/ActionBox.swift
+++ ios/MullvadVPN/Views/ActionBox.swift
@@ -10,22 +10,120 @@
 
 import SwiftUI
 
-struct ActionBox<Style: ToggleStyle>: View {
-    @State var isChecked: Bool
+struct ActionBox: View {
+    struct Action {
+        let onAction: () -> Void
+        let label: LocalizedStringKey
+    }
 
-    let label: String
-    let toggleStyle: Style
+    struct AdditionalInfo {
+        var warningTitle: LocalizedStringKey
+        var warningMessage: LocalizedStringKey
+    }
 
-    var didToggle: (Bool) -> Void
+    @Binding var isChecked: Bool
+    @State private var isExpanded = false
+    let toggleTitle: LocalizedStringKey
+    var additionalInfo: AdditionalInfo?
+    var action: Action?
 
     var body: some View {
-        Toggle(NSLocalizedString(label, comment: ""), isOn: $isChecked)
-            .padding(UIMetrics.ActionBox.padding)
-            .border(Color.MullvadActionBox.border)
-            .cornerRadius(UIMetrics.ActionBox.cornerRadius)
-            .toggleStyle(toggleStyle)
-            .onChange(of: isChecked) { _, newValue in
-                didToggle(newValue)
+        VStack(alignment: .leading) {
+            Button {
+                withAnimation {
+                    isChecked.toggle()
+                }
+            } label: {
+                HStack {
+                    Toggle("", isOn: $isChecked.animation())
+                        .toggleStyle(CheckboxToggleStyle(accessibilityId: .checkbox))
+                    Text(toggleTitle)
+                        .font(.mullvadTiny)
+                        .foregroundStyle(Color.mullvadTextPrimary)
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                }
             }
+            if let additionalInfo, isChecked {
+
+                VStack(alignment: .leading) {
+                    Button {
+                        withAnimation {
+                            isExpanded.toggle()
+                        }
+                    } label: {
+                        HStack {
+                            Image.mullvadIconWarning
+                            Text(additionalInfo.warningTitle)
+                                .font(Font.mullvadTinySemiBold)
+                                .foregroundStyle(Color.mullvadTextPrimary)
+                            Spacer()
+                            Button {
+                                withAnimation {
+                                    isExpanded.toggle()
+                                }
+                            } label: {
+                                Image.mullvadIconChevron
+                                    .rotationEffect(
+                                        .degrees(
+                                            isExpanded ? -90 : 90
+                                        )
+                                    )
+                            }
+                        }
+                    }
+                    if isExpanded {
+                        Text(additionalInfo.warningMessage)
+                            .font(Font.mullvadTiny)
+                            .foregroundStyle(Color.mullvadTextPrimary)
+                        if let action {
+                            MullvadButton(
+                                text: action.label,
+                                style: .primary,
+                                action: action.onAction
+                            )
+                        }
+                    }
+                }
+                .padding(8)
+                .background(Color.mullvadDarkBackground)
+                .cornerRadius(UIMetrics.ActionBox.cornerRadius)
+            }
+        }
+        .geometryGroup()
+        .padding(8)
+        .background(Color.mullvadBackground)
+        .cornerRadius(UIMetrics.ActionBox.cornerRadius)
+        .overlay(
+            RoundedRectangle(cornerRadius: UIMetrics.ActionBox.cornerRadius)
+                .stroke(Color.MullvadActionBox.border, lineWidth: 1)
+        )
     }
+}
+
+#Preview {
+    @Previewable @State var isChecked: Bool = false
+    VStack {
+        Spacer()
+        ActionBox(
+            isChecked: $isChecked,
+            toggleTitle: "By checking this box I agree to the risks involved with proceeding with this action.",
+            additionalInfo: .init(
+                warningTitle: "This impacts your anonymity",
+                warningMessage:
+                    """
+                    The Internet traffic in the excluded applications will not go through
+                    the VPN. Your own IP address will be exposed. 
+                    When you exclude some apps, other apps may be unintentionally excluded too.
+                    """),
+            action: .init(onAction: {}, label: "Action")
+        )
+        ActionBox(
+            isChecked: $isChecked,
+            toggleTitle: "By checking this box I agree to the risks involved with proceeding with this action."
+        )
+        Spacer()
+    }
+    .padding()
+    .background(Color.mullvadBackground)
 }
