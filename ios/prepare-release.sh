92f439174f567cca2108bad1d9b81b1c3336a329
Samuel Hammersberg <samuel@loafey.se>
2026-08-26 16:16:00 +0200

    Update `prepare-release.sh` to use sigsum api

--- ios/prepare-release.sh
+++ ios/prepare-release.sh
@@ -3,8 +3,10 @@
 API_ENDPOINT="api.mullvad.net"
 RELAYS_FILE="MullvadREST/Assets/relays.json"
 
+DIGEST=$(curl https://"$API_ENDPOINT"/trl/v1/timestamps/latest -s | head -n 1 | grep -o '"digest":"[^"]*' | grep -o '[^"]*$')
+
 echo "Download relays file"
-curl https://"$API_ENDPOINT"/app/v1/relays -s -o "$RELAYS_FILE"
+curl https://"$API_ENDPOINT"/trl/v1/data/"$DIGEST" -s -o "$RELAYS_FILE"
 
 git add -f "$RELAYS_FILE"
 git commit -m "Add updated relay list to release build"
