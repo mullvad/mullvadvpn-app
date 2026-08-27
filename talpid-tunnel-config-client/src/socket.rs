7f47dde7da5bb3e753f6493874a2f176e21f0a8e
Sebastian Holmin <sebastian.holmin@mullvad.net>
2026-08-27 11:58:46 +0200

    !fixup

--- talpid-tunnel-config-client/src/socket.rs
+++ talpid-tunnel-config-client/src/socket.rs
@@ -13,7 +13,7 @@
 use std::io;
 use std::net::SocketAddr;
 use std::time::Duration;
-use tokio::net::TcpSocket as StdTcpSocket;
+use tokio::net::TcpSocket as TokioTcpSocket;
 use tokio::net::TcpStream;
 
 /// Time before TCP starts sending keepalive probes.
@@ -25,8 +25,51 @@ const KEEPALIVE_INTERVAL: Duration = Duration::from_secs(2);
 /// Number of unacknowledged keepalive probes before giving up.
 const KEEPALIVE_RETRIES: u32 = 3;
 
+pub struct TcpSocket {
+    socket: TokioTcpSocket,
+}
+
+impl TcpSocket {
+    pub fn new() -> io::Result<Self> {
+        let socket = TokioTcpSocket::new_v4()?;
+        #[cfg(unix)]
+        try_set_tcp_sock_mtu(&socket);
+        set_reliability_params(&socket);
+        Ok(Self { socket })
+    }
+
+    /// Clone this socket into a new connectable `TcpSocket` sharing the
+    /// same kernel socket object (and its configured options). Each clone
+    /// can be connected independently.
+    pub fn try_clone(&self) -> io::Result<Self> {
+        let dup = socket2::SockRef::from(&self.socket).try_clone()?;
+        // `from_std_stream` expects an unconnected, nonblocking socket.
+        // The dup inherits there invariants from `TcpSocket`
+        let socket = TokioTcpSocket::from_std_stream(std::net::TcpStream::from(dup));
+        Ok(Self { socket })
+    }
+
+    pub async fn connect(self, addr: SocketAddr) -> io::Result<TcpStream> {
+        self.socket.connect(addr).await
+    }
+}
+
+#[cfg(unix)]
+impl std::os::fd::AsFd for TcpSocket {
+    fn as_fd(&self) -> std::os::fd::BorrowedFd<'_> {
+        self.socket.as_fd()
+    }
+}
+
+#[cfg(windows)]
+impl std::os::windows::io::AsSocket for TcpSocket {
+    fn as_socket(&self) -> std::os::windows::io::BorrowedSocket<'_> {
+        self.socket.as_socket()
+    }
+}
+
 /// Set TCP parameters that prioritize reliability and low latency over throughput.
-fn set_reliability_params(socket: &StdTcpSocket) {
+fn set_reliability_params(socket: &TokioTcpSocket) {
     // Send small gRPC frames immediately.
     if let Err(e) = socket.set_nodelay(true) {
         log::error!("Failed to set TCP_NODELAY on tunnel config socket: {e}");
@@ -56,106 +99,30 @@ fn set_reliability_params(socket: &StdTcpSocket) {
     }
 }
 
+/// MTU to set on the tunnel config client socket. We want a low value to prevent fragmentation.
+/// Especially on Android, we've found that the real MTU is often lower than the default MTU, and
+/// we cannot lower it further. This causes the outer packets to be dropped. Also, MTU detection
+/// will likely occur after the PQ handshake, so we cannot assume that the MTU is already
+/// correctly configured.
+/// This is set to the lowest possible IPv4 MTU.
 #[cfg(unix)]
-mod sys {
-    use super::*;
+const CONFIG_CLIENT_MTU: u16 = 576;
 
+#[cfg(unix)]
+fn try_set_tcp_sock_mtu(sock: &impl std::os::fd::AsFd) {
     use nix::sys::socket::{setsockopt, sockopt::TcpMaxSeg};
-    use std::os::fd::{AsFd, BorrowedFd};
 
-    /// MTU to set on the tunnel config client socket. We want a low value to prevent fragmentation.
-    /// Especially on Android, we've found that the real MTU is often lower than the default MTU, and
-    /// we cannot lower it further. This causes the outer packets to be dropped. Also, MTU detection
-    /// will likely occur after the PQ handshake, so we cannot assume that the MTU is already
-    /// correctly configured.
-    /// This is set to the lowest possible IPv4 MTU.
-    const CONFIG_CLIENT_MTU: u16 = 576;
-
-    pub struct TcpSocket {
-        socket: StdTcpSocket,
-    }
-
-    impl TcpSocket {
-        pub fn new() -> io::Result<Self> {
-            let socket = StdTcpSocket::new_v4()?;
-            try_set_tcp_sock_mtu(&socket);
-            set_reliability_params(&socket);
-            Ok(Self { socket })
-        }
-
-        /// Clone this socket into a new connectable `TcpSocket` sharing the
-        /// same kernel socket object (and its configured options). Each clone
-        /// can be connected independently.
-        pub fn try_clone(&self) -> io::Result<Self> {
-            let dup = socket2::SockRef::from(&self.socket).try_clone()?;
-            // `from_std_stream` expects an unconnected, nonblocking socket;
-            // the dup satisfies both (inherited from the original).
-            let socket = StdTcpSocket::from_std_stream(std::net::TcpStream::from(dup));
-            Ok(Self { socket })
-        }
-
-        pub async fn connect(self, addr: SocketAddr) -> io::Result<TcpStream> {
-            self.socket.connect(addr).await
-        }
-    }
-
-    impl AsFd for TcpSocket {
-        fn as_fd(&self) -> BorrowedFd<'_> {
-            self.socket.as_fd()
-        }
-    }
-
-    fn try_set_tcp_sock_mtu(sock: &impl AsFd) {
-        let mss = u32::from(desired_mss());
-        log::debug!("Tunnel config TCP socket MSS: {mss}");
-        if let Err(e) = setsockopt(sock, TcpMaxSeg, &mss) {
-            log::error!("Failed to set MSS on tunnel config TCP socket: {e}");
-        };
-    }
-
-    const fn desired_mss() -> u16 {
-        const IPV4_HEADER_SIZE: u16 = 20;
-        const MAX_TCP_HEADER_SIZE: u16 = 60;
-        let mtu = CONFIG_CLIENT_MTU.saturating_sub(IPV4_HEADER_SIZE);
-        mtu.saturating_sub(MAX_TCP_HEADER_SIZE)
-    }
+    let mss = u32::from(desired_mss());
+    log::debug!("Tunnel config TCP socket MSS: {mss}");
+    if let Err(e) = setsockopt(sock, TcpMaxSeg, &mss) {
+        log::error!("Failed to set MSS on tunnel config TCP socket: {e}");
+    };
 }
 
-#[cfg(windows)]
-mod sys {
-    use super::*;
-    use std::os::windows::io::{AsSocket, BorrowedSocket};
-
-    pub struct TcpSocket {
-        socket: StdTcpSocket,
-    }
-
-    impl TcpSocket {
-        pub fn new() -> io::Result<Self> {
-            let socket = StdTcpSocket::new_v4()?;
-            set_reliability_params(&socket);
-            Ok(Self { socket })
-        }
-
-        /// Clone this socket into a new connectable `TcpSocket` sharing the
-        /// same kernel socket object (and its configured options). Each clone
-        /// can be connected independently.
-        pub fn try_clone(&self) -> io::Result<Self> {
-            let dup = socket2::SockRef::from(&self.socket).try_clone()?;
-            let socket = StdTcpSocket::from_std_stream(std::net::TcpStream::from(dup));
-            Ok(Self { socket })
-        }
-
-        pub async fn connect(self, addr: SocketAddr) -> io::Result<TcpStream> {
-            self.socket.connect(addr).await
-        }
-    }
-
-    impl AsSocket for TcpSocket {
-        fn as_socket(&self) -> BorrowedSocket<'_> {
-            self.socket.as_socket()
-        }
-    }
+#[cfg(unix)]
+const fn desired_mss() -> u16 {
+    const IPV4_HEADER_SIZE: u16 = 20;
+    const MAX_TCP_HEADER_SIZE: u16 = 60;
+    let mtu = CONFIG_CLIENT_MTU.saturating_sub(IPV4_HEADER_SIZE);
+    mtu.saturating_sub(MAX_TCP_HEADER_SIZE)
 }
-
-pub use sys::*;
