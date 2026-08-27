d59efb0f58cb0e5c6a27cbf919a1f840ebbd25c4
Samuel Hammersberg <samuel@loafey.se>
2026-08-27 10:22:35 +0200

    Update account.rs to use uniffi

--- mullvad-ios/src/ios/api_client/account.rs
+++ mullvad-ios/src/ios/api_client/account.rs
@@ -1,42 +1,23 @@
-use std::ffi::CStr;
-use std::os::raw::c_char;
+use std::sync::Arc;
 
 use mullvad_api::{
     AccountsProxy,
     rest::{self, MullvadRestHandle},
 };
 
+use crate::api_client::ApiContext;
+
 use super::{
-    SwiftApiContext,
-    cancellation::{RequestCancelHandle, SwiftCancelHandle},
-    do_request, do_request_with_empty_body,
-    response::SwiftMullvadApiResponse,
-    retry_strategy::{RetryStrategy, SwiftRetryStrategy},
+    cancellation::RequestCancelHandle, do_request, do_request_with_empty_body,
+    response::ApiResponse, retry_strategy::RetryStrategy,
 };
 
-/// # Safety
-///
-/// `api_context` must be pointing to a valid instance of `SwiftApiContext`. A `SwiftApiContext` is created
-/// by calling `mullvad_api_init_new`.
-///
-/// `account_number` must be a pointer to a null terminated string.
-///
-/// `retry_strategy` must have been created by a call to either of the following functions
-/// `mullvad_api_retry_strategy_never`, `mullvad_api_retry_strategy_constant` or `mullvad_api_retry_strategy_exponential`
-///
-/// This function is not safe to call multiple times with the same `CompletionCookie`.
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn mullvad_ios_get_account(
-    api_context: SwiftApiContext,
-    retry_strategy: SwiftRetryStrategy,
-    account_number: *const c_char,
-) -> SwiftCancelHandle {
-    // SAFETY: See param documentation for `account_number`.
-    let account_number = unsafe { CStr::from_ptr(account_number.cast()) }
-        .to_str()
-        .unwrap();
-    let account_number = String::from(account_number);
-
+#[uniffi::export]
+pub fn mullvad_ios_get_account(
+    api_context: Arc<ApiContext>,
+    retry_strategy: Arc<RetryStrategy>,
+    account_number: String,
+) -> Arc<RequestCancelHandle> {
     RequestCancelHandle::new(
         api_context,
         retry_strategy,
@@ -47,70 +28,42 @@ pub unsafe extern "C" fn mullvad_ios_get_account(
         )
         .await
         {
-            Ok(response) => completion.finish(response),
+            Ok(response) => completion.finish(Arc::new(response)),
             Err(err) => {
                 log::error!("{err:?}");
-                completion.finish(SwiftMullvadApiResponse::rest_error(err));
+                completion.finish(Arc::new(ApiResponse::rest_error(err)));
             }
         },
     )
-    .into_swift()
 }
 
-/// # Safety
-///
-/// `api_context` must be pointing to a valid instance of `SwiftApiContext`. A `SwiftApiContext` is created
-/// by calling `mullvad_api_init_new`.
-///
-/// `retry_strategy` must have been created by a call to either of the following functions
-/// `mullvad_api_retry_strategy_never`, `mullvad_api_retry_strategy_constant` or `mullvad_api_retry_strategy_exponential`
-///
-/// This function is not safe to call multiple times with the same `CompletionCookie`.
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn mullvad_ios_create_account(
-    api_context: SwiftApiContext,
-    retry_strategy: SwiftRetryStrategy,
-) -> SwiftCancelHandle {
+#[uniffi::export]
+pub fn mullvad_ios_create_account(
+    api_context: Arc<ApiContext>,
+    retry_strategy: Arc<RetryStrategy>,
+) -> Arc<RequestCancelHandle> {
     RequestCancelHandle::new(
         api_context,
         retry_strategy,
         async move |api_context, retry_strategy, completion_handler| {
             match mullvad_ios_create_account_inner(api_context.rest_handle(), retry_strategy).await
             {
-                Ok(response) => completion_handler.finish(response),
+                Ok(response) => completion_handler.finish(Arc::new(response)),
                 Err(err) => {
                     log::error!("{err:?}");
-                    completion_handler.finish(SwiftMullvadApiResponse::rest_error(err));
+                    completion_handler.finish(Arc::new(ApiResponse::rest_error(err)));
                 }
             }
         },
     )
-    .into_swift()
 }
 
-/// # Safety
-///
-/// `api_context` must be pointing to a valid instance of `SwiftApiContext`. A `SwiftApiContext` is created
-/// by calling `mullvad_api_init_new`.
-///
-/// `account_number` must be a pointer to a null terminated string.
-///
-/// `retry_strategy` must have been created by a call to either of the following functions
-/// `mullvad_api_retry_strategy_never`, `mullvad_api_retry_strategy_constant` or `mullvad_api_retry_strategy_exponential`
-///
-/// This function is not safe to call multiple times with the same `CompletionCookie`.
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn mullvad_ios_delete_account(
-    api_context: SwiftApiContext,
-    retry_strategy: SwiftRetryStrategy,
-    account_number: *const c_char,
-) -> SwiftCancelHandle {
-    // SAFETY: See param documentation for `account_number`.
-    let account_number = unsafe { CStr::from_ptr(account_number.cast()) }
-        .to_str()
-        .unwrap();
-    let account_number = String::from(account_number);
-
+#[uniffi::export]
+pub fn mullvad_ios_delete_account(
+    api_context: Arc<ApiContext>,
+    retry_strategy: Arc<RetryStrategy>,
+    account_number: String,
+) -> Arc<RequestCancelHandle> {
     RequestCancelHandle::new(
         api_context,
         retry_strategy,
@@ -122,22 +75,21 @@ pub unsafe extern "C" fn mullvad_ios_delete_account(
             )
             .await
             {
-                Ok(response) => completion_handler.finish(response),
+                Ok(response) => completion_handler.finish(Arc::new(response)),
                 Err(err) => {
                     log::error!("{err:?}");
-                    completion_handler.finish(SwiftMullvadApiResponse::rest_error(err));
+                    completion_handler.finish(Arc::new(ApiResponse::rest_error(err)));
                 }
             }
         },
     )
-    .into_swift()
 }
 
 async fn mullvad_ios_get_account_inner(
     rest_client: MullvadRestHandle,
     retry_strategy: RetryStrategy,
     account_number: String,
-) -> Result<SwiftMullvadApiResponse, rest::Error> {
+) -> Result<ApiResponse, rest::Error> {
     let api = AccountsProxy::new(rest_client);
     let future_factory = || api.get_data_response(account_number.clone());
 
@@ -147,7 +99,7 @@ async fn mullvad_ios_get_account_inner(
 async fn mullvad_ios_create_account_inner(
     rest_client: MullvadRestHandle,
     retry_strategy: RetryStrategy,
-) -> Result<SwiftMullvadApiResponse, rest::Error> {
+) -> Result<ApiResponse, rest::Error> {
     let api = AccountsProxy::new(rest_client);
     let future_factory = || api.create_account_response();
 
@@ -158,7 +110,7 @@ async fn mullvad_ios_delete_account_inner(
     rest_client: MullvadRestHandle,
     retry_strategy: RetryStrategy,
     account_number: String,
-) -> Result<SwiftMullvadApiResponse, rest::Error> {
+) -> Result<ApiResponse, rest::Error> {
     let api = AccountsProxy::new(rest_client);
     let future_factory = || api.delete_account(account_number.clone());
 
