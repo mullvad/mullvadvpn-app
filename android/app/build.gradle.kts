0a813c26ea69374ae8411f70e22f6a4585d0b244
Kalle Lindström <karl.lindstrom@mullvad.net>
2026-09-02 13:26:18 +0200

    Do not show version notification for alpha and dev

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -201,11 +201,18 @@ androidComponents {
     }
 
     onVariants {
+        val shouldShowVersionNotification =
+            if (hasProperty("mullvad.app.config.inAppVersionNotifications.enable")) {
+                getBooleanProperty("mullvad.app.config.inAppVersionNotifications.enable")
+            } else {
+                !(appVersion.isDev || appVersion.isAlpha)
+            }
+
         it.buildConfigFields!!.put(
             "ENABLE_IN_APP_VERSION_NOTIFICATIONS",
             BuildConfigField(
                 "boolean",
-                getBooleanProperty("mullvad.app.config.inAppVersionNotifications.enable"),
+                shouldShowVersionNotification,
                 "Show in-app version notifications",
             ),
         )
