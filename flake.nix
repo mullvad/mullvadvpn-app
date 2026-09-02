5c53f68041753987436cea3ac89d80eb1e539808
Albin <albin@mullvad.net>
2026-09-02 21:26:34 +0200

    Bump flake nixpkgs from 25.11 (EOL) to 26.05
    Bump from EOL to current stable channel in order to
    receive potential security and bug fixes.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Mullvad VPN development environments";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
     devshell.url = "github:numtide/devshell";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
