7d856c4d15ab134a6f1987d889a53fd6df3c51d9
David Lönnhager <david.l@mullvad.net>
2026-09-02 13:37:30 +0200

    Extend DNS cache flush timeout
    Mitigates timeouts likely related to large hosts files

--- talpid-dns/src/windows/dnsapi.rs
+++ talpid-dns/src/windows/dnsapi.rs
@@ -7,7 +7,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-static FLUSH_TIMEOUT: Duration = Duration::from_secs(5);
+const FLUSH_TIMEOUT: Duration = Duration::from_secs(10);
 static DNSAPI_HANDLE: OnceLock<DnsApi> = OnceLock::new();
 
 const MAX_CONCURRENT_FLUSHES: usize = 5;
@@ -65,7 +65,7 @@ impl DnsApi {
             // SAFETY: this function is trivially safe to call
             let result = if unsafe { (DnsFlushResolverCache)() } {
                 let elapsed = begin.elapsed();
-                if elapsed >= FLUSH_TIMEOUT {
+                if elapsed >= Duration::from_secs(5) {
                     log::warn!(
                         "Flushing system DNS cache took {} seconds",
                         elapsed.as_secs()
