527d931653ebda558b0c86f316fc205035010da3
Samuel Hammersberg <samuel@loafey.se>
2026-09-02 09:40:15 +0200

    Change api functions into methods

--- mullvad-ios/src/ios/api_client/api.rs
+++ mullvad-ios/src/ios/api_client/api.rs
@@ -1,97 +1,93 @@
-use std::sync::Arc;
-
+use super::{
+    cancellation::RequestCancelHandle, do_request, response::ApiResponse, retry_request,
+    retry_strategy::RetryStrategy,
+};
+use crate::api_client::{ApiContext, access_method_settings::AccessMethodSettingWrapper};
 use mullvad_api::{
     ApiProxy, ETag, RelayListProxy,
     rest::{self, MullvadRestHandle},
 };
-
-use crate::api_client::{ApiContext, access_method_settings::AccessMethodSettingWrapper};
-
-use super::{
-    cancellation::RequestCancelHandle, do_request, response::ApiResponse,
-    retry_request, retry_strategy::RetryStrategy,
-};
+use std::sync::Arc;
 
 #[uniffi::export]
-pub fn mullvad_ios_get_addresses(
-    api_context: Arc<ApiContext>,
-    retry_strategy: Arc<RetryStrategy>,
-) -> Arc<RequestCancelHandle> {
-    RequestCancelHandle::new(
-        api_context,
-        retry_strategy,
-        async move |api_context, retry_strategy, completion_handler| {
-            match mullvad_ios_get_addresses_inner(api_context.rest_handle(), retry_strategy).await {
+impl ApiContext {
+    pub fn get_addresses(
+        self: Arc<Self>,
+        retry_strategy: Arc<RetryStrategy>,
+    ) -> Arc<RequestCancelHandle> {
+        RequestCancelHandle::new(
+            self,
+            retry_strategy,
+            async move |api_context, retry_strategy, completion_handler| match get_addresses_inner(
+                api_context.rest_handle(),
+                retry_strategy,
+            )
+            .await
+            {
                 Ok(response) => completion_handler.finish(Arc::new(response)),
                 Err(err) => {
                     log::error!("{err:?}");
                     completion_handler.finish(Arc::new(ApiResponse::rest_error(err)));
                 }
-            }
-        },
-    )
-}
+            },
+        )
+    }
 
-#[uniffi::export]
-pub fn mullvad_ios_api_addrs_available(
-    api_context: Arc<ApiContext>,
-    retry_strategy: Arc<RetryStrategy>,
-    access_method_setting: Arc<AccessMethodSettingWrapper>,
-) -> Arc<RequestCancelHandle> {
-    let access_method_setting = access_method_setting.inner.clone();
+    pub fn api_addrs_available(
+        self: Arc<Self>,
+        retry_strategy: Arc<RetryStrategy>,
+        access_method_setting: Arc<AccessMethodSettingWrapper>,
+    ) -> Arc<RequestCancelHandle> {
+        let access_method_setting = access_method_setting.inner.clone();
 
-    RequestCancelHandle::new(
-        api_context,
-        retry_strategy,
-        async move |api_context, retry_strategy, completion_handler| match api_context
-            .access_mode_handler
-            .resolve(access_method_setting.clone())
-            .await
-        {
-            Ok(Some(resolved_connection_mode)) => {
-                let oneshot_client = api_context
-                    .api_client
-                    .mullvad_rest_handle(resolved_connection_mode.connection_mode.into_provider());
+        RequestCancelHandle::new(
+            self,
+            retry_strategy,
+            async move |api_context, retry_strategy, completion_handler| match api_context
+                .access_mode_handler
+                .resolve(access_method_setting.clone())
+                .await
+            {
+                Ok(Some(resolved_connection_mode)) => {
+                    let oneshot_client = api_context.api_client.mullvad_rest_handle(
+                        resolved_connection_mode.connection_mode.into_provider(),
+                    );
 
-                match mullvad_ios_api_addrs_available_inner(oneshot_client, retry_strategy).await {
-                    Ok(_) => completion_handler.finish(Arc::new(ApiResponse::ok())),
-                    Err(err) => {
-                        log::error!("{err:?}");
-                        completion_handler
-                            .finish(Arc::new(ApiResponse::rest_error(err)));
+                    match api_addrs_available_inner(oneshot_client, retry_strategy).await {
+                        Ok(_) => completion_handler.finish(Arc::new(ApiResponse::ok())),
+                        Err(err) => {
+                            log::error!("{err:?}");
+                            completion_handler.finish(Arc::new(ApiResponse::rest_error(err)));
+                        }
                     }
                 }
-            }
-            Ok(None) => {
-                log::error!("Invalid access method configuration, {access_method_setting:?}");
-                completion_handler.finish(Arc::new(ApiResponse::access_method_error(
-                    mullvad_api::access_mode::Error::Resolve {
-                        access_method: access_method_setting.access_method,
-                    },
-                )));
-            }
-            Err(err) => {
-                log::error!("{err:?}");
-                completion_handler
-                    .finish(Arc::new(ApiResponse::access_method_error(err)));
-            }
-        },
-    )
-}
+                Ok(None) => {
+                    log::error!("Invalid access method configuration, {access_method_setting:?}");
+                    completion_handler.finish(Arc::new(ApiResponse::access_method_error(
+                        mullvad_api::access_mode::Error::Resolve {
+                            access_method: access_method_setting.access_method,
+                        },
+                    )));
+                }
+                Err(err) => {
+                    log::error!("{err:?}");
+                    completion_handler.finish(Arc::new(ApiResponse::access_method_error(err)));
+                }
+            },
+        )
+    }
 
-#[uniffi::export]
-pub fn mullvad_ios_get_relays(
-    api_context: Arc<ApiContext>,
-    retry_strategy: Arc<RetryStrategy>,
-    etag: Option<String>,
-) -> Arc<RequestCancelHandle> {
-    let maybe_etag: Option<ETag> = etag.map(ETag);
+    pub fn get_relays(
+        self: Arc<Self>,
+        retry_strategy: Arc<RetryStrategy>,
+        etag: Option<String>,
+    ) -> Arc<RequestCancelHandle> {
+        let maybe_etag: Option<ETag> = etag.map(ETag);
 
-    RequestCancelHandle::new(
-        api_context,
-        retry_strategy,
-        async move |api_context, retry_strategy, completion_handler| {
-            match mullvad_ios_get_relays_inner(
+        RequestCancelHandle::new(
+            self,
+            retry_strategy,
+            async move |api_context, retry_strategy, completion_handler| match get_relays_inner(
                 api_context.rest_handle(),
                 retry_strategy,
                 maybe_etag,
@@ -103,12 +99,12 @@ pub fn mullvad_ios_get_relays(
                     log::error!("{err:?}");
                     completion_handler.finish(Arc::new(ApiResponse::rest_error(err)));
                 }
-            }
-        },
-    )
+            },
+        )
+    }
 }
 
-async fn mullvad_ios_get_addresses_inner(
+async fn get_addresses_inner(
     rest_client: MullvadRestHandle,
     retry_strategy: RetryStrategy,
 ) -> Result<ApiResponse, rest::Error> {
@@ -119,7 +115,7 @@ async fn mullvad_ios_get_addresses_inner(
     do_request(retry_strategy, future_factory).await
 }
 
-async fn mullvad_ios_get_relays_inner(
+async fn get_relays_inner(
     rest_client: MullvadRestHandle,
     retry_strategy: RetryStrategy,
     etag: Option<ETag>,
@@ -131,7 +127,7 @@ async fn mullvad_ios_get_relays_inner(
     do_request(retry_strategy, future_factory).await
 }
 
-async fn mullvad_ios_api_addrs_available_inner(
+async fn api_addrs_available_inner(
     rest_client: MullvadRestHandle,
     retry_strategy: RetryStrategy,
 ) -> Result<bool, rest::Error> {
