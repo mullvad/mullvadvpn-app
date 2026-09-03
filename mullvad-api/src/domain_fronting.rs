4861895492b924b92b8e2d93716990d1cc405799
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-09-03 12:55:42 +0200

    fixup: Make df configurable again

--- mullvad-api/src/domain_fronting.rs
+++ mullvad-api/src/domain_fronting.rs
@@ -1,9 +1,9 @@
 //! Built-in domain fronting access method configuration.
 
-use std::{net::SocketAddr, sync::LazyLock, time::Duration};
+use std::{net::SocketAddr, str::FromStr, sync::LazyLock, time::Duration};
 
 use http::Uri;
-use serde::{Deserialize, Deserializer, Serialize, de::Error as _};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use tracing::{Level, instrument};
 
 use crate::proxy::{ApiConnectionMode, ProxyConfig};
@@ -20,23 +20,34 @@ pub const USE_HTTP2: bool = true;
 /// CDN77 has an idle timeout of 5 seconds, so we set our timeout just below that.
 pub const IDLE_TIMEOUT: Duration = Duration::from_millis(4_500);
 
+pub type DfConfig = domain_fronting::DomainFronting;
+
 /// Resolve the built-in domain fronting configuration.
 ///
 /// Performs DNS resolution of the front domain and returns the
 /// corresponding [`ApiConnectionMode`].
-#[instrument(level = Level::TRACE, ret)]
 pub async fn resolve() -> Option<ApiConnectionMode> {
-    // TODO: DNS lookup can be slow/flaky
-    let df = domain_fronting::DomainFronting::new(FRONT.clone(), PROXY_HOST.to_string());
-    let proxy_config = df
+    let default = DfConfig::new(FRONT.clone(), PROXY_HOST.to_string());
+    resolve_with(&default).await
+}
+
+/// Resolve a custom domain fronting configuration.
+///
+/// Performs DNS resolution of the front domain and returns the
+/// corresponding [`ApiConnectionMode`].
+#[instrument(level = Level::TRACE, ret)]
+pub async fn resolve_with(config: &DfConfig) -> Option<ApiConnectionMode> {
+    let proxy_config = config
         .proxy_config()
         .await
         .inspect_err(|e| tracing::warn!("Failed to resolve domain fronting config: {e}"))
         .ok()?;
 
-    let addr = DomainFrontingAddr {
+    let addr = DfConfigResolved {
         addr: proxy_config.addr,
-        _front: FRONT.to_string(),
+        front: config.front().clone(),
+        proxy_host: config.proxy_host().to_string(),
+        session_key: config.session_key().to_string(),
     };
 
     Some(ApiConnectionMode::Proxied(ProxyConfig::DomainFronting(
@@ -45,31 +56,41 @@ pub async fn resolve() -> Option<ApiConnectionMode> {
 }
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
-pub struct DomainFrontingAddr {
+pub struct DfConfigResolved {
     /// Resolved address of the fronting domain.
     pub addr: SocketAddr,
 
     /// Domain that will be used to connect to a CDN.
-    ///
-    /// This is used as a cache-key, to guard against [`FRONT`] changing on crate updates.
-    #[serde(rename = "front", deserialize_with = "deserialize_front_uri")]
-    _front: String,
+    #[serde(serialize_with = "ser_display", deserialize_with = "de_from_str")]
+    front: Uri,
+
+    /// HTTP `HOST` header.
+    proxy_host: String,
+
+    session_key: String,
 }
 
-impl DomainFrontingAddr {
+impl DfConfigResolved {
     pub fn config(&self) -> domain_fronting::DomainFronting {
-        domain_fronting::DomainFronting::new(FRONT.clone(), PROXY_HOST.to_string())
+        domain_fronting::DomainFronting::new(self.front.clone(), self.proxy_host.clone())
+            .with_session_key(self.session_key.clone())
     }
 }
 
-/// Deserialize a fronting URI and assert that it equals [`FRONT`].
-pub fn deserialize_front_uri<'de, D>(deserializer: D) -> Result<String, D::Error>
+fn ser_display<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
 where
+    T: std::fmt::Display,
+    S: Serializer,
+{
+    serializer.collect_str(value)
+}
+
+fn de_from_str<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: FromStr,
+    T::Err: std::fmt::Display,
     D: Deserializer<'de>,
 {
-    let front: &str = Deserialize::deserialize(deserializer)?;
-    if front != FRONT_STR {
-        return Err(D::Error::custom("Invalid fronting domain (Stale cache)"));
-    }
-    Ok(FRONT_STR.to_string())
+    let s = String::deserialize(deserializer)?;
+    s.parse().map_err(serde::de::Error::custom)
 }
