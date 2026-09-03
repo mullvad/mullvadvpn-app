097b482bb293b339f03a233d89f030a016d77961
Steffen <steffen.ernst@mullvad.net>
2026-09-03 15:45:44 +0200

    Add tvos target

---
+++ ios/MullvadVPNtvOS/ContentView.swift
@@ -0,0 +1,33 @@
+// This Source Code Form is subject to the terms of the GPLv3 License.
+// You can obtain a copy of the license at https://www.gnu.org/licenses/gpl-3.0.en.html.
+//
+// This file incorporates work covered by the following copyright and
+// permission notice:
+//
+//   Copyright (c) Mullvad VPN AB. All rights reserved.
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
+import MullvadMockData
+import MullvadREST
+import MullvadRustRuntime
+import MullvadTypes
+internal import Network
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
