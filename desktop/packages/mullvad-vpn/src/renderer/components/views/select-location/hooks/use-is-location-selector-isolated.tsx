7b237da24b55a614ba6dd190b48a26adde953e1c
Oliver <oliver@mohlin.dev>
2026-09-04 08:17:08 +0200

    Refactor `scrollTop` into `isLocationSelectorExpanded`

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/hooks/use-is-location-selector-isolated.tsx
+++
@@ -1,7 +0,0 @@
-import { useSelectLocationViewContext } from '../SelectLocationViewContext';
-
-export function useIsLocationSelectorIsolated() {
-  const { isolatedItem } = useSelectLocationViewContext();
-
-  return isolatedItem !== undefined;
-}
