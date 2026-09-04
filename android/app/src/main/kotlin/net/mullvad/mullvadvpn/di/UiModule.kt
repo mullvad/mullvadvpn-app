9a8890544b5e3fc14b9cd58a7b44d8d0b0736c64
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-09-04 08:40:42 +0200

    Fix UiModule

--- android/app/src/main/kotlin/net/mullvad/mullvadvpn/di/UiModule.kt
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/di/UiModule.kt
@@ -379,7 +379,7 @@ val uiModule = module {
             isPlayBuild = IS_PLAY_BUILD,
         )
     }
-    viewModel { SplashViewModel(get(), get(), get(), get()) }
+    viewModel { SplashViewModel(get(), get(), get()) }
     viewModel { VoucherDialogViewModel(get(), get()) }
     viewModel { params -> VpnSettingsViewModel(navArgs = params.get(), get(), get(), get(), get()) }
     viewModel { params -> AntiCensorshipSettingsViewModel(isModal = params.get(), get()) }
