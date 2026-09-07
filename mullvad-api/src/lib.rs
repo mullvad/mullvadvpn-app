9d9151778483f8b7f6ffdb7f84b687d92d5ea63e
Samuel Hammersberg <samuel@loafey.se>
2026-09-07 14:20:45 +0200

    Make `rest_handle` usable by other AddressCacheBacking implementations

--- mullvad-api/src/lib.rs
+++ mullvad-api/src/lib.rs
@@ -414,7 +414,9 @@ impl Runtime {
             socket_bypass_tx,
         })
     }
+}
 
+impl<B: AddressCacheBacking> Runtime<B> {
     /// Returns a new request service handle
     pub fn rest_handle(&self, dns_resolver: impl DnsResolver) -> rest::RequestServiceHandle {
         self.new_request_service(
@@ -426,9 +428,7 @@ impl Runtime {
             false,
         )
     }
-}
 
-impl<B: AddressCacheBacking> Runtime<B> {
     pub async fn with_cache_backing(
         handle: tokio::runtime::Handle,
         endpoint: &ApiEndpoint,
