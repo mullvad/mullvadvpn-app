a5777410137dbff6e77f589faed9da32330f88dc
Albin <albin@mullvad.net>
2026-09-07 23:05:57 +0200

    Enable extra compiler checks across all modules

--- android/gradle/build-logic/src/main/kotlin/KotlinToolchainPlugin.kt
+++ android/gradle/build-logic/src/main/kotlin/KotlinToolchainPlugin.kt
@@ -1,9 +1,14 @@
+import com.android.build.gradle.api.AndroidBasePlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper
 import utilities.libs
+
+private val COMPILER_ARGS = listOf("-Wextra")
 
 class KotlinToolchainPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -13,6 +18,17 @@ class KotlinToolchainPlugin : Plugin<Project> {
             plugins.withType(KotlinBasePluginWrapper::class.java) {
                 extensions.configure<KotlinProjectExtension> {
                     jvmToolchain(libs.findVersion("jvm-toolchain").get().toString().toInt())
+                }
+            }
+
+            plugins.withType(AndroidBasePlugin::class.java) {
+                extensions.configure<KotlinAndroidProjectExtension> {
+                    compilerOptions { freeCompilerArgs.addAll(COMPILER_ARGS) }
+                }
+            }
+            plugins.withType(KotlinBasePluginWrapper::class.java) {
+                extensions.configure<KotlinJvmProjectExtension> {
+                    compilerOptions { freeCompilerArgs.addAll(COMPILER_ARGS) }
                 }
             }
         }
