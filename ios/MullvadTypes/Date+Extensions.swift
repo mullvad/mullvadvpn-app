80273e8e9a7d683badad7913733c185a225ea949
Andrew Bulhak <andrew.bulhak@mullvad.net>
2026-09-07 12:26:48 +0200

    Refactor and simplify throttling mechanisms/time interval computation

---
+++ ios/MullvadTypes/Date+Extensions.swift
@@ -0,0 +1,16 @@
+// This Source Code Form is subject to the terms of the GPLv3 License.
+// You can obtain a copy of the license at https://www.gnu.org/licenses/gpl-3.0.en.html.
+//
+// This file incorporates work covered by the following copyright and
+// permission notice:
+//
+//   Copyright (c) Mullvad VPN AB. All rights reserved.
+//
+// SPDX-License-Identifier: GPL-3.0-only
+
+extension Date {
+    // arithmetic between Dates and Durations
+    public static func + (lhs: Date, rhs: Duration) -> Date {
+        lhs.addingTimeInterval(rhs.timeInterval)
+    }
+}
