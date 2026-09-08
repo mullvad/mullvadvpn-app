ed2f93ecacca16cca605c76077ee3ec4d07ccf5c
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-09-08 12:56:48 +0200

    Suppress specific warnings in lib:grpc instead of all

--- android/lib/grpc/build.gradle.kts
+++ android/lib/grpc/build.gradle.kts
@@ -11,11 +11,13 @@ android {
 
     kotlin {
         compilerOptions {
-            // Protobuf and gRPC Kotlin generators emits explicit visibility modifiers,
-            // so we exclude that check.
-            freeCompilerArgs.addAll("-Xwarning-level=REDUNDANT_VISIBILITY_MODIFIER:disabled")
-            // This is due to a warning in the generated code from Wire.
-            allWarningsAsErrors = false
+            // Wire protobuf generator emits some warnings that we can not fix,
+            // so we exclude those in the check.
+            freeCompilerArgs.addAll(
+                "-Xwarning-level=REDUNDANT_VISIBILITY_MODIFIER:disabled",
+                "-Xwarning-level=CAN_BE_VAL_DELAYED_INITIALIZATION:disabled",
+                "-Xwarning-level=UNNECESSARY_NOT_NULL_ASSERTION:disabled",
+            )
         }
     }
 }
