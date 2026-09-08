1b7ceb196a0be26238ae24b2e5a7757d6708c04d
Oliver <oliver@mohlin.dev>
2026-09-08 13:07:51 +0200

    Add and use `LocationSlide` component

--- desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/index.ts
+++ desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/index.ts
@@ -21,3 +21,4 @@ export * from './select-location-selector';
 export * from './select-location-header';
 export * from './select-location-selector-internet-row';
 export * from './select-location-selector-device-row';
+export * from './location-slide';
