b60e7d179073f0d1ebd66426bf0e13c288aaeb85
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-09-08 14:59:10 +0200

    Restore cio engine for router calls

--- android/test/e2e/build.gradle.kts
+++ android/test/e2e/build.gradle.kts
@@ -112,7 +112,7 @@ dependencies {
     implementation(libs.junit5.android.test.runner)
     implementation(libs.kotlin.stdlib)
     implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.cio)
     implementation(libs.ktor.client.auth)
     implementation(libs.ktor.client.logging)
     implementation(libs.ktor.serialization.kotlinx.json)
