efff15e3683bea21b88c2928fe7c353284505d13
Guilherme Andrade <g@gandrade.net>
2026-09-11 20:50:03 +0100

    Run formatter

--- android/lib/repository/src/main/kotlin/net/mullvad/mullvadvpn/lib/repository/CustomListsRepository.kt
+++ android/lib/repository/src/main/kotlin/net/mullvad/mullvadvpn/lib/repository/CustomListsRepository.kt
@@ -1,10 +1,10 @@
 package net.mullvad.mullvadvpn.lib.repository
 
+import android.icu.text.Collator
 import arrow.core.Either
 import arrow.core.raise.either
 import arrow.core.raise.ensureNotNull
 import co.touchlab.kermit.Logger
-import android.icu.text.Collator
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
