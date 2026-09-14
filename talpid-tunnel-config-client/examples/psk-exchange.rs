184a34360440ed533a4ed6c36ea69925891c1dbe
David Lönnhager <david.l@mullvad.net>
2026-09-14 18:54:06 +0200

    Request ephemeral peers over any stream (AsyncRead/AsyncWrite)
    This lets us use `talpid-netstack` instead of a `TcpStream`.

--- talpid-tunnel-config-client/examples/psk-exchange.rs
+++ talpid-tunnel-config-client/examples/psk-exchange.rs
@@ -1,15 +1,18 @@
-//! Example client implementing the quantum resistant tunnel PSK exchange.
-//! Useful to test this crate's implementation.
+//! Example client implementing the quantum resistant tunnel PSK exchange, through an existing
+//! tunnel. Useful to test this crate's implementation.
 
 // Usage: ./psk-exchange <tuncfg_server_ip> <wireguard_public_key>
 // e. g. ./psk-exchange 10.64.0.1 NkECLsf+VbZUjve7RVN6sE3NYUcYUmUn8qpFugqbXFk=
 
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use talpid_tunnel_config_client::CONFIG_SERVICE_PORT;
 use talpid_types::net::wireguard::{PrivateKey, PublicKey};
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() {
     let mut args = std::env::args().skip(1);
-    let tuncfg_server_ip = args
+    let tuncfg_server_ip: Ipv4Addr = args
         .next()
         .expect("Give tuncfg server IP as first argument")
         .parse()
@@ -22,22 +25,21 @@ async fn main() {
     // which can also be provided by other means.
     let ephemeral_private_key = PrivateKey::new_from_random();
 
-    let tcp_socket = talpid_tunnel_config_client::socket::TcpSocket::new()
-        .expect("Failed to create tunnel config socket");
-    let ephemeral_peer = talpid_tunnel_config_client::request_ephemeral_peer(
-        tuncfg_server_ip,
+    let stream = TcpStream::connect(SocketAddr::new(
+        IpAddr::V4(tuncfg_server_ip),
+        CONFIG_SERVICE_PORT,
+    ))
+    .await
+    .expect("Failed to connect to the config service");
+    let ephemeral_peer = talpid_tunnel_config_client::request_ephemeral_peer_over_stream(
+        stream,
         public_key, // Parent connection's public key.
         ephemeral_private_key.public_key(),
         true,  // Whether to negotiate a "PQ-safe" PSK.
         false, // Whether to use DAITA (Does not work with Linux kernel WireGuard.)
-        &tcp_socket,
     )
     .await
     .unwrap();
-
-    if let Some(info) = tcp_socket.query_tcp_info() {
-        println!("TCP info after request: {info:?}");
-    }
 
     println!("Private key: {ephemeral_private_key}");
     // Use fmt::Debug since Serialize is not implemented for PresharedKey.
