77a00b147c1f0517391488936dd15644b9f1e06b
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-09-14 14:05:56 +0200

    Use separate RequestServices for IPv4/IPv6 geoip lookups

--- mullvad-api/src/rest.rs
+++ mullvad-api/src/rest.rs
@@ -152,7 +152,7 @@ impl Error {
 /// It allows for on-demand termination of in-flight requests.
 ///
 /// TLS connections are established by [`HttpsConnector`] and may be reused for multiple request.
-pub(crate) struct RequestService<C> {
+pub struct RequestService<C> {
     host: Arc<str>,
     requests_rx: mpsc::UnboundedReceiver<SendRequest>,
     access_tokens: AccessTokenStore,
@@ -326,6 +326,7 @@ impl<C: ConnectionModeProvider + 'static> RequestService<C> {
     }
 
     async fn send_request(&mut self, request: Request<Full<Bytes>>) -> Result<Response<Incoming>> {
+        let method = request.request.method().clone();
         let uri = request.uri().clone();
 
         let t = request.timeout;
@@ -340,6 +341,8 @@ impl<C: ConnectionModeProvider + 'static> RequestService<C> {
             .flatten();
 
         // TODO: retry once if error is due to access token expiry
+
+        tracing::trace!("{method} {uri:?}");
 
         if let Err(err) = &result
             && err.is_network_error()
