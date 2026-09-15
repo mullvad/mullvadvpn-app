955acbe4a7af05fd5b175fd03cac2263edc2ced8
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-09-15 21:34:54 +0200

    Refactor memoized functions recursively calling themselves
    It is not currently supported by React compiler to have a function
    that is memoized be calling itself recursively.
    
    To work around this limitation we declare a non-memoized function
    within the memoized function which can recursively call itself instead.

--- desktop/packages/mullvad-vpn/src/renderer/components/Map.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/Map.tsx
@@ -116,25 +116,29 @@ function MapInner(props: MapInnerProps) {
   const onSizeChange = useEffectEvent(onSizeChangeImpl);
 
   const animationFrameCallback = useEffectEvent((now: number) => {
-    now *= 0.001; // convert to seconds
+    const runAnimations = () => {
+      now *= 0.001; // convert to seconds
 
-    // Propagate location change to the map
-    if (newParams.current) {
-      mapRef.current?.setLocation(
-        newParams.current.location,
-        newParams.current.connectionState,
-        now,
-        props.animate,
-      );
-      newParams.current = undefined;
-    }
+      // Propagate location change to the map
+      if (newParams.current) {
+        mapRef.current?.setLocation(
+          newParams.current.location,
+          newParams.current.connectionState,
+          now,
+          props.animate,
+        );
+        newParams.current = undefined;
+      }
 
-    mapRef.current?.draw(now);
+      mapRef.current?.draw(now);
 
-    // Stops rendering if pause is true. This happens when there is no ongoing movements
-    if (!pause.current) {
-      render();
-    }
+      // Stops rendering if pause is true. This happens when there is no ongoing movements
+      if (!pause.current) {
+        requestAnimationFrame(runAnimations);
+      }
+    };
+
+    runAnimations();
   });
 
   // These lint rules are disabled for now because the react plugin for eslint does
