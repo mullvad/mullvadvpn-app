5666ee5e9702fef723bc40e3967901d231044f54
David Lönnhager <david.l@mullvad.net>
2026-09-15 17:30:49 +0200

    fixup! Negotiate ephemeral peers without a local proxy socket on iOS

--- mullvad-ios/src/tunnel_adapter/ffi/mod.rs
+++ mullvad-ios/src/tunnel_adapter/ffi/mod.rs
@@ -117,8 +117,6 @@ impl TunnelCallbackHandler for CallbackBridge {
     fn on_error(&self, error: TunnelError) {
         let mapped_error = match &error {
             TunnelError::ObfuscationProxyError(ObfuscationProxyError::LocalSocketError(
-                local_socket_error,
-            )) => match local_socket_error {
                 tunnel_obfuscation::Error::BindLocalUdp(_)
                 | tunnel_obfuscation::Error::BindRemoteUdp(_)
                 | tunnel_obfuscation::Error::ConnectRemoteUdp(_)
@@ -128,10 +126,9 @@ impl TunnelCallbackHandler for CallbackBridge {
                 | tunnel_obfuscation::Error::CreateUdp2TcpObfuscator(
                     tunnel_obfuscation::udp2tcp::Error::ConnectTcp(_)
                     | tunnel_obfuscation::udp2tcp::Error::CreateTcpSocket(_),
-                ) => GotaTunFfiError::BindSockets(format!("{error}")),
-                _ => GotaTunFfiError::Internal(format!("{error}")),
-            },
-            TunnelError::ICMPSocketError(_) => GotaTunFfiError::BindSockets(format!("{error}")),
+                ),
+            ))
+            | TunnelError::ICMPSocketError(_) => GotaTunFfiError::BindSockets(format!("{error}")),
             _ => GotaTunFfiError::Internal(format!("{error}")),
         };
         self.0.on_error(mapped_error);
