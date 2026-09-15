50a4a4e36787c3b0a0648d13ed3784121370856a
Tobias Järvelöv <tobias.jarvelov@mullvad.net>
2026-09-15 20:44:44 +0200

    Remove comments disabling React ESLint rules which are no longer needed

--- desktop/packages/mullvad-vpn/src/renderer/components/Map.tsx
+++ desktop/packages/mullvad-vpn/src/renderer/components/Map.tsx
@@ -178,21 +178,11 @@ function MapInner(props: MapInnerProps) {
   useEffect(() => {
     addEventListener('resize', onSizeChange);
     return () => removeEventListener('resize', onSizeChange);
-    // These lint rules are disabled for now because the react plugin for eslint does
-    // not understand that useEffectEvent should not be added to the dependency array.
-    // Enable these rules again when eslint can lint useEffectEvent properly.
-    // eslint-disable-next-line react-compiler/react-compiler
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     const unsubscribe = window.ipc.window.listenScaleFactorChange(onSizeChange);
     return () => unsubscribe();
-    // These lint rules are disabled for now because the react plugin for eslint does
-    // not understand that useEffectEvent should not be added to the dependency array.
-    // Enable these rules again when eslint can lint useEffectEvent properly.
-    // eslint-disable-next-line react-compiler/react-compiler
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const devicePixelRatio = window.devicePixelRatio;
