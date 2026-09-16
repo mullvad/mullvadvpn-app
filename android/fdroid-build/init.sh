bfb89a363003af921f7b59d2ae95f55ac29ef7ef
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-09-16 09:26:39 +0200

    Check for hash after downloading rustup

--- android/fdroid-build/init.sh
+++ android/fdroid-build/init.sh
@@ -4,8 +4,11 @@ set -eux
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+RUSTUP_SHA256_CHECKSUM="7d0ea0f8eba7fa1ebfe998091cd7ec4501e33ec5ca6b884eb4d894d7da5170af"
+
 # Install Rust
 curl -sf -L https://sh.rustup.rs > /tmp/rustup.sh
+echo "$RUSTUP_SHA256_CHECKSUM /tmp/rustup.sh" | sha256sum -c
 chmod +x /tmp/rustup.sh
 /tmp/rustup.sh -y
 # shellcheck source=/dev/null
