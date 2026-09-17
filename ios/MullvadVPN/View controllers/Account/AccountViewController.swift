ad080504bf46ee779bb04f9c213dde0d10c08eea
Emīls <emils@mullvad.net>
2026-09-17 23:16:54 +0200

    Add debug action to invalidate device

--- ios/MullvadVPN/View controllers/Account/AccountViewController.swift
+++ ios/MullvadVPN/View controllers/Account/AccountViewController.swift
@@ -270,19 +270,27 @@ class AccountViewController: UIViewController, @unchecked Sendable {
                 }
             )
         )
-        #if NEVER_IN_PRODUCTION
-            let gotaTunEnabled = PacketTunnelDebugSettings.useGotaTun
-            sheetController.addAction(
-                UIAlertAction(
-                    title: "Use GotaTun: \(gotaTunEnabled ? "ON" : "OFF")",
-                    style: .default,
-                    handler: { [weak self] _ in
-                        PacketTunnelDebugSettings.useGotaTun = !gotaTunEnabled
-                        self?.tunnelManager.reapplyTunnelConfiguration()
-                    }
-                )
+        let gotaTunEnabled = PacketTunnelDebugSettings.useGotaTun
+        sheetController.addAction(
+            UIAlertAction(
+                title: "Use GotaTun: \(gotaTunEnabled ? "ON" : "OFF")",
+                style: .default,
+                handler: { [weak self] _ in
+                    PacketTunnelDebugSettings.useGotaTun = !gotaTunEnabled
+                    self?.tunnelManager.reapplyTunnelConfiguration()
+                }
             )
-        #endif
+        )
+
+        sheetController.addAction(
+            UIAlertAction(
+                title: "Invalidate WireGuard key",
+                style: .default,
+                handler: { [weak self] _ in
+                    self?.tunnelManager.invalidateWireGuardKey()
+                }
+            )
+        )
 
         sheetController.addAction(
             UIAlertAction(
