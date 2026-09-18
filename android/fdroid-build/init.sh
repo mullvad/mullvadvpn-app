c06fdb7debbdb5570b1eab44989525a3dd7f9c47
Jonatan Rhodin <jonatan.rhodin@mullvad.net>
2026-09-18 13:13:28 +0200

    Pin rustup version in init.sh

--- android/fdroid-build/init.sh
+++ android/fdroid-build/init.sh
@@ -4,13 +4,13 @@ set -eux
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-RUSTUP_SHA256_CHECKSUM="7d0ea0f8eba7fa1ebfe998091cd7ec4501e33ec5ca6b884eb4d894d7da5170af"
+RUSTUP_VERSION="1.29.1"
+RUSTUP_SHA256_CHECKSUM="dda7234360b7f578ca8b0ddcb80145646fa61a67c1720a5abc7051b35c9fcb71"
 
 # Install Rust
-curl -sf -L https://sh.rustup.rs > /tmp/rustup.sh
-echo "$RUSTUP_SHA256_CHECKSUM /tmp/rustup.sh" | sha256sum -c
-chmod +x /tmp/rustup.sh
-/tmp/rustup.sh -y
+curl -sf -L https://static.rust-lang.org/rustup/archive/$RUSTUP_VERSION/x86_64-unknown-linux-gnu/rustup-init > /tmp/rustup-init
+echo "$RUSTUP_SHA256_CHECKSUM /tmp/rustup-init" | sha256sum -c
+/tmp/rustup-init -y
 # shellcheck source=/dev/null
 source "$HOME/.cargo/env"
 rustup set profile minimal
