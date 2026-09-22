233f8234c05a1fc9ff43d4f6f7f7b68f96d838ef
Oliver <oliver@mohlin.dev>
2026-09-22 15:57:21 +0200

    Keep identity of locations not changed by filter

--- desktop/packages/mullvad-vpn/src/renderer/features/locations/utils/filter-countries.ts
+++ desktop/packages/mullvad-vpn/src/renderer/features/locations/utils/filter-countries.ts
@@ -7,12 +7,35 @@ export function filterCountries(
   countries: IRelayLocationCountryRedux[],
   filter: (relay: IRelayLocationRelayRedux) => boolean,
 ): IRelayLocationCountryRedux[] {
-  return countries
-    .map((country) => ({
-      ...country,
-      cities: country.cities
-        .map((city) => ({ ...city, relays: city.relays.filter(filter) }))
-        .filter((city) => city.relays.length > 0),
-    }))
+  let anyCountryChanged = false;
+
+  const filteredCountries = countries
+    .map((country) => {
+      let anyCityChanged = false;
+      const cities = country.cities
+        .map((city) => {
+          const relays = city.relays.filter(filter);
+          if (relays.length === city.relays.length) {
+            return city;
+          }
+
+          anyCityChanged = true;
+          return { ...city, relays };
+        })
+        .filter((city) => city.relays.length > 0);
+
+      if (!anyCityChanged && cities.length === country.cities.length) {
+        return country;
+      }
+
+      anyCountryChanged = true;
+      return { ...country, cities };
+    })
     .filter((country) => country.cities.length > 0);
+
+  if (!anyCountryChanged && filteredCountries.length === countries.length) {
+    return countries;
+  }
+
+  return filteredCountries;
 }
