916306ab4cfbce14adb0244d2ea022d4d46f722d
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-09-22 15:22:59 +0200

    fixup! fix geoip

--- mullvad-daemon/src/geoip.rs
+++ mullvad-daemon/src/geoip.rs
@@ -7,7 +7,6 @@ use mullvad_api::{
     availability::ApiAvailability,
     proxy::ApiConnectionMode,
     rest::{Error, RequestService, RequestServiceHandle},
-    DnsResolver,
 };
 use mullvad_types::location::{AmIMullvad, GeoIpLocation, LocationEventData};
 use std::sync::LazyLock;
