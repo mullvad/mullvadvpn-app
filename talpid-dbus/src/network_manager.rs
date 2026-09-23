51edef70f2cdb36713cc62e7e2f4957f92edc349
Markus Pettersson <markus.pettersson@mullvad.net>
2026-09-23 09:52:35 +0200

    fixup! Use gotatun if NetworkManager manages DNS

--- talpid-dbus/src/network_manager.rs
+++ talpid-dbus/src/network_manager.rs
@@ -508,12 +508,6 @@ impl NetworkManager {
             Self::update_dns_config(&mut settings, "ipv6", v6_dns);
         }
 
-        if let Some(wg_config) = settings.get_mut("wireguard")
-            && !wg_config.contains_key("fwmark")
-        {
-            log::error!("WireGuard config doesn't contain the firewall mark");
-        }
-
         self.reapply_settings(&device_path, settings, version_id)?;
         Ok(settings_backup)
     }
